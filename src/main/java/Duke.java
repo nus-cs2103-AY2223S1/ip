a0a058346f05a96e67a20c29ccc657cbe3ee94dd
junlee1991 <julio_harjo@hotmail.sg>
2022-08-07 01:42:20 +0800

    Level-2 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> ls = new ArrayList<String>();
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String command = sc.nextLine();
-        switch(command){
-            case "list":
-                System.out.println("list");
-                break;
-            case "blah":
-                System.out.println("blah");
-                break;
-            case "bye":
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
+
+        while(!(command.equals("bye"))) {
+            // case 1: list
+            if(command.equals("list")){
+                for(int i = 0; i < ls.size(); i++){
+                    int index = i + 1;
+                    System.out.println(index + ". " + ls.get(i));
+                }
+            }
+            else{   // add commands
+                ls.add(command);
+                System.out.println("added: " + command);
+            }
+            // remember to update to the next line
+            command = sc.nextLine();
         }
+        System.out.println("Bye. Hope to see you again soon!");
         sc.close();
     }
 }
+
