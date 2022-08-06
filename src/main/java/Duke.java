3994b9ee2712a489f7f9da86d581fae7bd60f271
junlee1991 <julio_harjo@hotmail.sg>
2022-08-07 03:11:33 +0800

    Level-3 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> ls = new ArrayList<String>();
+        ArrayList<Task> ls = new ArrayList<Task>();
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String command = sc.nextLine();
@@ -12,13 +12,28 @@ public class Duke {
         while(!(command.equals("bye"))) {
             // case 1: list
             if(command.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < ls.size(); i++){
                     int index = i + 1;
-                    System.out.println(index + ". " + ls.get(i));
+                    System.out.println(index + ". " + ls.get(i).toString());
                 }
             }
+            //case 2: mark
+            else if(command.startsWith("mark")){
+                System.out.println("Nice! I've marked this task as done:");
+                String[] s = command.split(" ");
+                int index = Integer.parseInt(s[1]);
+                ls.get(index - 1).setDone(); // minus -1 because of list starts from 1.
+            }
+            // case 3: unmark
+            else if(command.startsWith("unmark")) {
+                System.out.println("OK, I've marked this task as not done yet:");
+                String[] s = command.split(" ");
+                int index = Integer.parseInt(s[1]);
+                ls.get(index - 1).setNotDone();
+            }
             else{   // add commands
-                ls.add(command);
+                ls.add(new Task(command));
                 System.out.println("added: " + command);
             }
             // remember to update to the next line
@@ -28,4 +43,6 @@ public class Duke {
         sc.close();
     }
 }
+
+
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+
+    private String name;
+    private boolean status = false;
+
+    public Task(String name){
+        this.name = name;
+    }
+
+    public boolean getStatus(){
+        return this.status;
+    }
+
+    public void setDone(){
+        status = true;
+    }
+
+    public void setNotDone(){
+        status = false;
+    }
+
+    /**
+     * Overriden toString function. Prints the completion status and the task name
+     * @return String representation of this task.
+     */
+    @Override
+    public String toString(){
+        if(status == true)
+            return "[x] " + name;
+        else
+            return "[ ] " + name;
+    }
+}
