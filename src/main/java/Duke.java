23c4fd47170898d97e60bbf2a83597e1531c1e31
cowlinn <2017.colin.toh@ejc.edu.sg>
2022-08-07 16:07:26 +0800

    Level-2
    Added Add,List functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 public class Duke {
 
-    private static String[] dukeTasks = new String[100];
+    private static List<String> dukeInputs = new ArrayList<>();
+    private static final String ENDING_MESSAGE = "That's all? Hope to see you again soon :)";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,14 +19,22 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String input = "";
-        while (!input.equals("bye")) {
-            input = sc.next();
-            System.out.println(input);
+        while (true) {
+            // 1) Read the input
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < dukeInputs.size(); i++) {
+                    System.out.println(i+1 + ". " + dukeInputs.get(i));
+                }
+            } else { //will only reach this line if it is not bye / list
+                dukeInputs.add(input);
+                System.out.println("added: " + input);
+            }
         }
-        //At this point, input == "bye"
-        //print exit stuff
-        System.out.println("That's all? Hope to see you again soon!");
-
+        //always remember to greet!
+        System.out.println(ENDING_MESSAGE);
     }
 }
 
