dfc1ad02d688a0c962e4f1584b9c9a6cf96a2306
cowlinn <2017.colin.toh@ejc.edu.sg>
2022-08-07 17:37:51 +0800

    Level 3
    Added Mark As Donne and Tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,12 @@ import java.util.List;
 import java.util.ArrayList;
 public class Duke {
 
-    private static List<String> dukeInputs = new ArrayList<>();
+    private static List<Task> dukeInputs = new ArrayList<>();
     private static final String ENDING_MESSAGE = "That's all? Hope to see you again soon :)";
+    private static final String LIST_HEADER = "Here are the tasks in your list:";
+    private static final String DONE_MESSAGE = "Nice! I've marked this task as done:";
+    private static final String UNDONE_MESSAGE = "OK, I've marked this task as not done yet:";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,27 +18,56 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
 
-        System.out.println("Hello! I'm Duke"); //Can use newline character
-        System.out.println("What can I do for you?");
+        System.out.println("Hello! I'm Duke \n" + "What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
         String input = "";
         while (true) {
-            // 1) Read the input
+            // Read the input
             input = sc.nextLine();
-            if (input.equals("bye")) {
+            if (input.startsWith("mark")) {
+                //split by space, then the second value
+                int taskIndex = Integer.valueOf(input.split(" ", 0)[1]) - 1;
+                dukeInputs.get(taskIndex).setDone();
+                Task currentTask = dukeInputs.get(taskIndex);
+                String completionStatus = "[" + currentTask.getStatusIcon() + "] ";
+
+                System.out.println(DONE_MESSAGE);
+                System.out.println("  " + completionStatus + currentTask.description);
+
+            }
+
+            else if (input.startsWith("unmark")) {
+                int taskIndex = Integer.valueOf(input.split(" ", 0)[1]) - 1;
+                dukeInputs.get(taskIndex).removeDone();
+                Task currentTask = dukeInputs.get(taskIndex);
+                String completionStatus = "[" + currentTask.getStatusIcon() + "] ";
+
+                System.out.println(UNDONE_MESSAGE);
+                System.out.println("  " + completionStatus + currentTask.description);
+
+            }
+
+            else if (input.equals("bye")) {
+                System.out.println(ENDING_MESSAGE);
                 break;
-            } else if (input.equals("list")) {
+            }
+
+            else if (input.equals("list")) {
+                System.out.println(LIST_HEADER);
                 for (int i = 0; i < dukeInputs.size(); i++) {
-                    System.out.println(i+1 + ". " + dukeInputs.get(i));
+                    Task currentTask = dukeInputs.get(i);
+                    String completionStatus = "[" + currentTask.getStatusIcon() + "] ";
+                    System.out.println(i + 1 + ". " + completionStatus + currentTask.description);
                 }
-            } else { //will only reach this line if it is not bye / list
-                dukeInputs.add(input);
+            }
+
+            else {
+                Task newTask = new Task(input);
+                dukeInputs.add(newTask);
                 System.out.println("added: " + input);
             }
         }
-        //always remember to greet!
-        System.out.println(ENDING_MESSAGE);
     }
 }
 
