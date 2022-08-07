86fc9a30944208528bb7629c0cf85568397d5296
cowlinn <2017.colin.toh@ejc.edu.sg>
2022-08-07 15:51:11 +0800

    Level - 1
    Added Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
 public class Duke {
+
+    private static String[] dukeTasks = new String[100];
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        System.out.println("Hello! I'm Duke"); //Can use newline character
+        System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("bye")) {
+            input = sc.next();
+            System.out.println(input);
+        }
+        //At this point, input == "bye"
+        //print exit stuff
+        System.out.println("That's all? Hope to see you again soon!");
+
     }
 }
+
