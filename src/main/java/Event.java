069362ebf87862bcdabd9f66b66c7671d67abd1d
cowlinn <2017.colin.toh@ejc.edu.sg>
2022-08-09 12:55:12 +0800

    Level 8
    Changed individual task classes to support datetime

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,25 @@
-public class Event extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
-    public Event(String description, String by) {
+public class Event extends Task {
+
+    private static final String TYPE = "[E]";
+    protected LocalDate by;
+
+    public Event(String description, String unformattedDate) {
         super(description);
-        this.by = by;
+        by = LocalDate.parse(unformattedDate); //TODO: figure out how to parse multiple types
+    }
+
+
+    public String CustomFormatter(LocalDate ld) {
+        return ld.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at:" + by + ")";
+
+        return TYPE + super.toString() + "(at:" + CustomFormatter(by) + ")";
     }
 }
