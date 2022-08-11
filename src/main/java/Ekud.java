c803655590319938c91db7622c3cc289a09ddc71
vvidday <davids89034@gmail.com>
2022-08-11 19:10:02 +0800

    feat: chat bot stores text from user and displays data when requested

--- src/main/java/Ekud.java
+++ src/main/java/Ekud.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class Ekud {
     private final Task[] taskList;
+    private int numberOfTasks;
     private final String logo = "_____________             .___\n" + "\\_   _____/  | ____ __  __| _/\n" + 
     " |    __)_|  |/ /  |  \\/ __ |\n" + " |        \\    <|  |  / /_/ | \n" + "/_______  /__|_ \\____/\\____ | \n" + 
     "        \\/     \\/          \\/ \n";
 
     public Ekud() {
         this.taskList = new Task[100];
+        this.numberOfTasks = 0;
     }
 
     public void start() {
@@ -20,14 +22,37 @@ public class Ekud {
             String command = sc.nextLine();
             switch(command) {
                 case "bye":
-                    sendMessage("Bye. Hope to see you again soon!");
+                    this.sendMessage("Bye. Hope to see you again soon!");
                     active = false;
                     break;
+                case "list":
+                    this.printTasks();
+                    break;
                 default:
-                    sendMessage(command);
+                    if(!addTask(command)) {
+                        this.sendMessage("Sorry, you've reached the maximum limit of 100 tasks.");
+                    }
+                    this.sendMessage(String.format("added: %s", command));
             }
         }
         sc.close();
+    }
+
+    private void printTasks() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < this.numberOfTasks; i++) {
+            builder.append(String.format("%d. %s\n", i + 1, this.taskList[i].getDescription()));
+        }
+        this.sendMessage(builder.toString());
+    }
+
+    private boolean addTask(String description) {
+        if (this.numberOfTasks == 100) {
+            return false;
+        }
+        Task task = new Task(description);
+        this.taskList[this.numberOfTasks++] = task;
+        return true;
     }
 
     private String indentMessage(String message) {
