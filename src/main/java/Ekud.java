5125039a09b24b616ad8b5116fea7f8f849b6d74
David <davids89034@gmail.com>
2022-08-11 13:33:56 +0800

    changed name of chatbot

---
+++ src/main/java/Ekud.java
@@ -0,0 +1,9 @@
+public class Ekud {
+    public static void main(String[] args) {
+        String logo = "_____________             .___\n" + "\\_   _____/  | ____ __  __| _/\n" + 
+        " |    __)_|  |/ /  |  \\/ __ |\n" + " |        \\    <|  |  / /_/ | \n" + "/_______  /__|_ \\____/\\____ | \n" + 
+        "        \\/     \\/          \\/ \n";
+
+        System.out.println("Hello from\n" + logo);
+    }
+}
