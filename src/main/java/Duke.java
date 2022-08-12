9a7ab76a6adc4ac6cf171b88635ea1426b1d9809
domoboom <berzindominic@gmail.com>
2022-08-13 05:28:17 +0800

    Duke Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        System.out.println("What can I do for you?");
+
+        while (in.hasNext()) {
+            String next = in.nextLine();
+            if (next.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(next);
+        }
+
+
     }
 }
