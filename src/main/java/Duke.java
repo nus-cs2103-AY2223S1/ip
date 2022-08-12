53eba2142b64556b82f14e6ace6d8a132b6ebed5
izzahaj <izzah.jalil02@gmail.com>
2022-08-13 01:55:36 +0800

    Added ability to store and list input text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    public static final String BORDER = ">>========================="
+            + "===========[**]============"
+            + "========================<<";
+    private static ArrayList<Task> taskList = new ArrayList<Task>();
+
+    public static void list() {
+        System.out.println(BORDER);
+        taskList.forEach(t -> System.out.println("  " + (taskList.indexOf(t) + 1) + ". " + t));
+        System.out.println(BORDER);
+    }
+
+    public static void addTask(String command) {
+        Task task = new Task(command);
+        taskList.add(task);
+        System.out.println(BORDER + "\n  added: " + command + "\n" + BORDER);
+    }
+
+    public static void quit() {
+        System.out.println(BORDER + "\n"
+                + "  Good riddance, I say. With all due disrespect, leave me alone next time.\n"
+                + BORDER);
+    }
+
     public static void main(String[] args) {
 
         String mort = "                               .---.        .-----------\n"
@@ -13,23 +37,24 @@ public class Duke {
                 + ">>==================================UU[**]UU==================================<<\n"
                 + "                                    '//||\\\\`\n"
                 + "                                      ''``";
-        String border = ">>====================================[**]====================================<<";
+
         System.out.println(mort);
         System.out.println("  Oh, it's you again...\n  Mort, begrudgingly at your service.");
         Scanner sc = new Scanner(System.in);
-        System.out.println("  Hmph, what do you want now?\n" + border);
+        System.out.println("  Hmph, what do you want now?\n" + BORDER);
 
         while (true) {
             System.out.println();
             String command = sc.nextLine();
             System.out.println();
-            if (command.compareTo("bye") == 0) {
-                System.out.println(border + "\n"
-                        + "  Good riddance, I say. With all due disrespect, leave me alone next time.\n"
-                        + border);
-                break;
+            if (command.compareTo("bye") == 0) { // replace list of keywords with enums later
+                quit();
+                return;
+            } else if (command.compareTo("list") == 0) {
+                list();
+            } else {
+                addTask(command);
             }
-            System.out.println(border + "\n  " + command + "\n" + border);
         }
 
     }
