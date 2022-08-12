387a87d719e2ab81149639a73a2efa865b350dc4
see leng <seelengxd@gmail.com>
2022-08-12 18:59:56 +0800

    Level-1: add greet, echo, exit
    Also changed colour of Duke logo to cyan to make it look nice

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,56 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    // For adding some colour
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void printWithIndent(String toPrint) {
+        System.out.println("\t" + toPrint.replace("\n", "\n\t"));
+    }
+
+    public static void printLine() {
+        printWithIndent("____________________________________________________________");
+    }
+
+    public static void greet() {
+        printLine();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printWithIndent("Hello from\n" + ANSI_CYAN + logo + ANSI_RESET);
+        printWithIndent("How can I help you today?");
+        printLine();
+    }
+
+    public static void echo(String input) {
+        printLine();
+        printWithIndent(input);
+        printLine();
+    }
+
+    public static boolean isExit(String input) {
+        return input.equals("bye");
+    }
+
+    public static void exit() {
+        printLine();
+        printWithIndent("Bye. Hope to see you again soon!");
+        printLine();
+    }
+    public static void main(String[] args) {
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine().strip();
+            if (isExit(input)) {
+                exit();
+                break;
+            } else {
+                echo(input);
+            }
+        }
     }
 }
