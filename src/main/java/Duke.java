8c92735db737dda28c3a92cd4aea788502d2a9bc
jetlfj <e0726397@u.nus.edu>
2022-08-12 23:19:51 +0800

    add mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,32 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
-    private LinkedList<String> list;
+    public class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String s) {
+            this.description = s;
+            isDone = false;
+        }
+
+        public String mark() {
+            this.isDone = true;
+            return this.toString();
+        }
+
+        public String unmark() {
+            this.isDone = false;
+            return this.toString();
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[%s] %s", (isDone ? "X" : " "), description);
+        }
+    }
+
+    private LinkedList<Task> list;
     private static final String LINE = "--------------------------------------------------";
 
     private void run() {
@@ -12,20 +37,29 @@ public class Duke {
 
         while (true) {
             String input = sc.nextLine().trim();
+            String[] inputArr = input.split(" ", 2);
             System.out.println(LINE);
 
-            if (input.equals("bye")) {
+            if (inputArr[0].equals("bye")) {
                 System.out.printf("%s%n%s%n","Bye. Hope to see you again soon!", LINE);
                 break;
-            } else if (input.equals("list")) {
+            } else if (inputArr[0].equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= list.size(); i++) {
                     System.out.printf("%d. %s%n", i, list.get(i - 1));
                 }
-                System.out.println(LINE);
+            } else if (inputArr[0].equals("mark")) {
+                int idx = Integer.parseInt(inputArr[1]);
+                System.out.printf("Nice! I've marked this task as done:%n%s%n", list.get(idx - 1).mark());
+            } else if (inputArr[0].equals("unmark")) {
+                int idx = Integer.parseInt(inputArr[1]);
+                System.out.printf("OK, I've marked this task as not done yet:%n%s%n", list.get(idx - 1).unmark());
             } else {
-                list.add(input);
-                System.out.printf("added: %s%n%s%n", input, LINE);
+                list.add(new Task(input));
+                System.out.printf("added: %s%n", input);
             }
+
+            System.out.println(LINE);
         }
     }
 
