e2142e754f99e74460086859c141ece519888554
tinenhao <tinenhao@u.nus.edu>
2022-08-12 17:05:21 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String initMsg = "Hello! I'm Duke \n What can I do for you?";
+        String byeMsg = "Bye. Hope to see you soon again!";
+        Scanner scan = new Scanner(System.in);
+        printMessage(initMsg);
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            printMessage(input);
+        }
+        printMessage(byeMsg);
+    }
+
+    public static void printMessage(String s) {
+        System.out.println("--------------------------------------");
+        System.out.println(s);
+        System.out.println("--------------------------------------");
     }
 }
