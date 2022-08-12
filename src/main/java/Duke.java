e74f591e3c507800dda3da8d1dbc8e6ab7211a40
peppapighs <ptk.prasertsuk@gmail.com>
2022-08-12 17:53:40 +0800

    Level-1: greeting text & reading commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "______       _     \n" +
+            "| ___ \\     | |    \n" +
+            "| |_/ / ___ | |__  \n" +
+            "| ___ \\/ _ \\| '_ \\ \n" +
+            "| |_/ / (_) | |_) |\n" +
+            "\\____/ \\___/|_.__/ \n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+
+        while (true) {
+            System.out.print("> ");
+            String input = scanner.next();
+
+            if (input.equals("bye")) {
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
