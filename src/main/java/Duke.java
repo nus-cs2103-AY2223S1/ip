33d4e812d9b6b57f2405f111b770f55f4a2b6485
izzahaj <izzah.jalil02@gmail.com>
2022-08-13 00:40:26 +0800

    Implemented skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String mort = "                               .---.        .-----------\n"
+                + "                              /     \\  __  /    ------\n"
+                + "                             / /     \\(  )/    -----\n"
+                + "                            //////   ' \\/ `   ---\n"
+                + "                           //// / // :    : ---\n"
+                + "                          // /   /  /`    '--\n"
+                + "                         //          //..\\\\\n"
+                + ">>==================================UU[**]UU==================================<<\n"
+                + "                                    '//||\\\\`\n"
+                + "                                      ''``";
+        String border = ">>====================================[**]====================================<<";
+        System.out.println(mort);
+        System.out.println("  Oh, it's you again...\n  Mort, begrudgingly at your service.");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("  Hmph, what do you want now?\n" + border);
+
+        while (true) {
+            System.out.println();
+            String command = sc.nextLine();
+            System.out.println();
+            if (command.compareTo("bye") == 0) {
+                System.out.println(border + "\n"
+                        + "  Good riddance, I say. With all due disrespect, leave me alone next time.\n"
+                        + border);
+                break;
+            }
+            System.out.println(border + "\n  " + command + "\n" + border);
+        }
+
     }
 }
