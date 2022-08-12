c592489be07b3892be90eeb4421c66e23a507712
teoyuqi <teo.yuqi@gmail.com>
2022-08-12 21:28:02 +0800

    Add List attribute

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,15 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    /**
+     * 'List' attribute to store inputs.
+     */
+    private List<String> dukeList = new ArrayList<>();
+
     /**
      * Function to echo an input string onto stdout.
      * @param input Input to be printed on stdout.
