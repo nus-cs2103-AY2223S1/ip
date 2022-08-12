89e9f10f3c6bad196a18623d46f151ee77e09d63
teoyuqi <teo.yuqi@gmail.com>
2022-08-12 21:44:37 +0800

    Add javadocs

--- src/main/java/DukeList.java
+++ src/main/java/DukeList.java
@@ -5,14 +5,21 @@ import java.util.List;
 
 public class DukeList {
     /**
-     * 'List' attribute to store inputs.
+     * 'List' object attribute to store String inputs given.
      */
     private static List<String> inputList = new ArrayList<>();
 
+    /**
+     * Method to add String input to 'inputList'.
+     * @param input String object to be added to 'inputList'.
+     */
     public void add(String input) {
         inputList.add(input);
     }
 
+    /**
+     * Method to print inputs stored in 'inputList'.
+     */
     public void print() {
         for (int i = 0; i < inputList.toArray().length; i++) {
             System.out.printf("%d. %s%n", i + 1, inputList.get(i));
