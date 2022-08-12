fdfe3bf150ecc558a02856af53b998fe22e6b3fa
Ng Xing Yu <ng.xingyu@u.nus.edu>
2022-08-12 23:51:04 +0800

    Implement Greet Echo Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public Duke(String name) {
+        speak(String.format("Hello! I'm %s\nWhat can I do for you?", name));
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke("Duke");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            if (!duke.callback(sc.next())) {
+                break;
+            }
+        }
+    }
+
+    private boolean callback(String input) {
+        if (input.compareTo("bye") == 0) {
+            goodbye();
+            return false;
+        }
+        speak(input);
+        return true;
+    }
+
+    private void speak(String text) {
+        String line = "_".repeat(20) + '\n';
+        System.out.println(line);
+        System.out.println(text);
+        System.out.println(line);
+    }
+
+    private void goodbye() {
+        speak("Bye. Hope to see you again soon!");
     }
 }
