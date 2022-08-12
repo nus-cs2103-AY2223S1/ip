2431aa54a1a9cdb52a9221ae26a2d4a4fc0b5c73
jetlfj <e0726397@u.nus.edu>
2022-08-12 22:52:13 +0800

    add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE = "--------------------------------------------------";
+
+    private void run() {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%s%n%50s%n%50s%n%s%n", LINE, "Hello! I'm Cortana", "What can I do for you?", LINE);
+
+        while (true) {
+            String input = sc.nextLine().trim();
+            System.out.println(LINE);
+
+            if (input.equals("bye")) {
+                System.out.printf("%50s%n%s%n","Bye. Hope to see you again soon!", LINE);
+                break;
+            } else {
+                System.out.printf("%50s%n%s%n", input, LINE);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        new Duke().run();
     }
 }
