df7fea28f765e4939bc764a172970b7c9422dc37
jetlfj <e0726397@u.nus.edu>
2022-08-12 23:03:17 +0800

    add add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,30 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
+    private LinkedList<String> list;
     private static final String LINE = "--------------------------------------------------";
 
     private void run() {
+        list = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
-        System.out.printf("%s%n%50s%n%50s%n%s%n", LINE, "Hello! I'm Cortana", "What can I do for you?", LINE);
+        System.out.printf("%s%n%s%n%s%n%s%n", LINE, "Hello! I'm Cortana", "What can I do for you?", LINE);
 
         while (true) {
             String input = sc.nextLine().trim();
             System.out.println(LINE);
 
             if (input.equals("bye")) {
-                System.out.printf("%50s%n%s%n","Bye. Hope to see you again soon!", LINE);
+                System.out.printf("%s%n%s%n","Bye. Hope to see you again soon!", LINE);
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.printf("%d. %s%n", i, list.get(i - 1));
+                }
+                System.out.println(LINE);
             } else {
-                System.out.printf("%50s%n%s%n", input, LINE);
+                list.add(input);
+                System.out.printf("added: %s%n%s%n", input, LINE);
             }
         }
     }
