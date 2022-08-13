a673925e77ef87375ccd2316371f18d0a9119c7f
Rezwan Arefin <rezwanarefin01@gmail.com>
2022-08-13 23:08:05 +0800

    Add functionality to add a task and show them

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,61 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
-        System.out.println(">> Hello! I am Duke. How can I help you?");
+    private static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String GREET_MESSAGE = "Hello! I am Duke. How can I help you?";
+    private static final String BYE_MESSAGE = "Bye. Hope to see you soon!";
 
+    private static void printResponse(String response) {
+        System.out.println(">> " + response);
+    }
+
+    private static String getUserCommand(Scanner sc) {
+        System.out.print("<< ");
+        return sc.nextLine();
+    }
+
+    private static void greet() {
+        System.out.println(LOGO);
+        Duke.printResponse(GREET_MESSAGE);
+    }
+
+    private static void startInteraction() {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.print("<< ");
-            String text = sc.nextLine();
+        ArrayList<String> tasks = new ArrayList<>();
 
-            if (text.equals("bye")) {
-                System.out.println(">> Bye. Hope to see you again soon!");
+        boolean hasEnded = false;
+        while (!hasEnded) {
+            String command = Duke.getUserCommand(sc);
+            switch (command.toLowerCase()) {
+            case "list":
+                StringBuilder response = new StringBuilder();
+                response.append("List of tasks:\n");
+                for (int i = 0; i < tasks.size(); ++i) {
+                    response.append(String.format("\t %d. %s", i + 1, tasks.get(i)));
+                    if (i + 1 < tasks.size()) {
+                        response.append("\n");
+                    }
+                }
+                Duke.printResponse(response.toString());
                 break;
+            case "bye":
+                Duke.printResponse(BYE_MESSAGE);
+                hasEnded = true;
+                break;
+            default:
+                tasks.add(command);
+                Duke.printResponse("Added task: " + command);
             }
-            System.out.println(">> " + text);
         }
+    }
+
+    public static void main(String[] args) {
+        Duke.greet();
+        Duke.startInteraction();
     }
 }
