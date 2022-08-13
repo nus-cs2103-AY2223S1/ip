1db2a265f5773631035ab7a5cf9d1a4ae905788c
Tan Chee Heng <ch.tan020@gmail.com>
2022-08-14 02:13:30 +0800

    Level 1: Greet, Echo, Exit - renamed chatbot to Jude, turned chatbot into something like command line interface.

---
+++ src/main/java/Jude.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Jude {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Jude.");
+        System.out.println("What can I do for you?");
+
+        // Solution below adapted from
+        // https://stackoverflow.com/questions/5287538/how-to-get-the-user-input-in-java
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println();
+            System.out.print("> ");
+            String str = sc.nextLine();
+            if (str.equals("exit")) {
+                System.out.println("Goodbye! Have a nice day!");
+                break;
+            }
+            System.out.println(str);
+        }
+    }
+}
