2816405d1ac13570c93bdb690cd370acd5738d2f
Jonaspng <jonaspng@hotmail.com>
2022-08-13 10:17:35 +0800

    Added java docs

--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,3 +1,8 @@
+/**
+ * Encapsulate the Response function of the chatbot
+ *
+ * @author: Jonas Png
+ */
 public class Response {
     private final String logo = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
@@ -6,8 +11,11 @@ public class Response {
             + "|____/ \\__,_|_|\\_\\___|\n";
     private final String line = "_______________________________";
 
+    /**
+     * Handles user's input into chatbot
+     * @param input User input into chatbot
+     */
     public void handleUserInput(String input) {
-
         if (input.equals("bye")) {
             String output = line + "\nBye. Hope to see you again soon!\n" + line;
             System.out.println(output);
@@ -18,6 +26,9 @@ public class Response {
         }
     }
 
+    /**
+     * Handles what to show during start up of chatbot
+     */
     public void startUp() {
         String temp = line + "\n" + logo + "\n" + "Hello! I'm Duke\nWhat can I do for you?\n" + line;
         System.out.println(temp);
