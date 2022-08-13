28fa353640f845b410fc4a490769f37679f6aef0
linuschui <linuschuii@gmail.com>
2022-08-13 13:49:47 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String line = " ____________________________________________________________";
+    private static final String startMessage = "  Hello! I'm Duke\n" + "  What can I do for you?";
+    private static final String endMessage = " Bye. Hope to see you again soon!hii";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        sendMessage(startMessage);
+        while (true) {
+            String userCommand = scanner.nextLine();
+            if (userCommand.equals("bye")) {
+                break;
+            } else {
+                sendMessage(userCommand);
+            }
+        }
+        sendMessage(endMessage);
+    }
+
+    public static void sendMessage(String message) {
+        System.out.println(line);
+        System.out.println(message);
+        System.out.println(line);
     }
 }
