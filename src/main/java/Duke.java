89eb27b4ddb06f8a135b197c6e7dd86026337dac
gnahzeus <zhangyue1689@gmail.com>
2022-08-13 16:58:08 +0800

    implement add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     private boolean hasExited = false;
-    public static void main(String[] args) {
-        Duke zeus = new Duke();
-        zeus.chat();
+    private ArrayList<String> list;
+
+    public Duke() {
+        this.list = new ArrayList<>();
     }
 
     public void chat() {
@@ -13,11 +16,27 @@ public class Duke {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 hasExited = true;
+            } else if (input.equals("list")) {
+                printList();
             } else {
                 echo(input);
+                addToList(input);
             }
         }
         exitMessage();
+    }
+
+    public void addToList(String input) {
+        this.list.add(input);
+    }
+
+    public void printList() {
+        generateLine();
+        for (int i = 0; i < this.list.size(); i++) {
+            String currLine = "\t " + (i + 1) + ". " + this.list.get(i);
+            System.out.println(currLine);
+        }
+        generateLine();
     }
 
     public void greetUser() {
@@ -32,15 +51,20 @@ public class Duke {
         System.out.println("\t Bye. Hope to see you again soon!");
         generateLine();
     }
+
     public void echo(String input) {
         generateLine();
-        System.out.println("\t " + input);
+        System.out.println("\t added: " + input);
         generateLine();
 
     }
 
     public void generateLine() {
         System.out.println("\t____________________________________________________________");
+    }
 
+    public static void main(String[] args) {
+        Duke zeus = new Duke();
+        zeus.chat();
     }
 }
