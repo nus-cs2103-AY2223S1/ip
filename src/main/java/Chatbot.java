ee80cb208bf933e3fed5b65d44480265c89b464c
Zhang Bozheng <zbz.lvlv@gmail.com>
2022-08-13 17:48:46 +0800

    Level-2 completed

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Chatbot {
@@ -5,14 +7,19 @@ public class Chatbot {
     private String name;
     private boolean isActive;
 
+    private List<String> msgLog;
+
     public Chatbot(String name) {
         this.name = name;
         this.isActive = true;
+        this.msgLog = new ArrayList<>();
     }
 
     public void start() {
 
         System.out.println("Hey, I am " + this.name + ". What can I do for you today?"); // Welcome text
+        System.out.println("Type 'list' to list out all the previous messages.");
+        System.out.println("Type 'bye' to finish the conversation.");
 
         Scanner sc = new Scanner(System.in);
 
@@ -31,8 +38,12 @@ public class Chatbot {
         if (msg.equals("bye")) {
             this.isActive = false;
             return "Goodbye, see you soon!";
+        } else if (msg.equals("list")) {
+            // Converts the list of messages to a human-readable multiline String
+            return msgLog.stream().map(m -> "- " + m).reduce("", (acc, m) -> acc + "\n" + m);
         } else {
-            return msg;
+            msgLog.add(msg);
+            return "Added: [" + msg + "]";
         }
 
     }
