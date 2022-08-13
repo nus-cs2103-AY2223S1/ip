22d53556941b00cf96374d654aebb49920a0a3b9
jonasgwt <55698556+jonasgwt@users.noreply.github.com>
2022-08-13 21:18:30 +0800

    Completed Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String reply(String input) {
+        return "Duck: " + input;
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Quack! I'm Duck\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                System.out.println("Duck: Quack! Hope to see you again soon!");
+                return;
+            } else System.out.println(reply(input));
+        }
     }
 }
