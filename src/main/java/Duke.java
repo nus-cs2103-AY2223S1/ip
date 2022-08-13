e38b111b8a5f2ede1c3fac012ae72d3ed9d22e48
JasonYapzx <jasonyapzx@gmail.com>
2022-08-13 23:59:34 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String input;
+        Scanner scanner = new Scanner(System.in);
+        String logo = "        / \\     |_   _| | |  / _|                   | |\n"
+                    + "       /   \\      | |   | | | |_   _ __    ___    __| |\n"
+                    + "      / / \\ \\     | |   | | |  _| | '__|  / _ \\  / _` |\n"
+                    + "     / _____ \\   _| |_  | | | |   | |    |  __/ | (_| |\n"
+                    + "    /_/     \\_\\ |_____| |_| |_|   |_|     \\___|  \\__,_|\n";
+
+        System.out.println("____________________________________________________________\n" +
+                           "                      Hello! I am \n" + logo +
+                           "      Your personal assistant. What can I do for you?\n" +
+                           "____________________________________________________________");
+        while(scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n" +
+                                   "added: " + input + "\n" +
+                                   "____________________________________________________________");
+            }
+        }
     }
 }
