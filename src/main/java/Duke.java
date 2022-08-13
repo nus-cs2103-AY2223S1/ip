921c9060b87cebe3688189ec1a949a7894301138
Rezwan Arefin <rezwanarefin01@gmail.com>
2022-08-13 21:49:15 +0800

    Level 1 (Greet, Echo, Exit) complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println(">> Hello! I am Duke. How can I help you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("<< ");
+            String text = sc.nextLine();
+
+            if (text.equals("bye")) {
+                System.out.println(">> Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(">> " + text);
+        }
     }
 }
