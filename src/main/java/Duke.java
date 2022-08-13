d2b591c44f527f8ff32d646ef24ffdfcddaaea5b
ChongCheeKaiClarence <e0725313.u.nus.edu>
2022-08-14 02:20:36 +0800

    Added level 1 work

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void reply() {
+        Scanner echo = new Scanner(System.in);
+        String response = echo.nextLine();
+        if (response.equals("bye")) {
+            System.out.println("-----------------------------------------------");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("-----------------------------------------------");
+            return;
+        } else {
+            System.out.println("-----------------------------------------------");
+            System.out.println(response);
+            System.out.println("-----------------------------------------------");
+            reply();
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n" );
+        reply();
     }
 }
