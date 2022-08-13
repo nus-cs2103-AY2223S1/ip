b1465f2a41958134d4534f873cbbff4f1f9e4e90
alvintfl <alvintanfulong@gmail.com>
2022-08-13 19:56:22 +0800

    Rename EXIT_PROGRAM to EXIT_COMMAND

--- src/main/java/Tako.java
+++ src/main/java/Tako.java
@@ -16,10 +16,10 @@ public class Tako {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Tako\nWhat do you want?");
         Scanner sc = new Scanner(System.in);
-        String EXIT_PROGRAM = "bye";
+        final String EXIT_COMMAND = "bye";
         while (sc.hasNext()) {
             String input = sc.nextLine();
-            if (input.equals(EXIT_PROGRAM)) {
+            if (input.equals(EXIT_COMMAND)) {
                 System.out.println("Bye, until next time...");
                 break;
             } else {
