bf528d6dbac98364f796d39c58209b1832a507fa
sprintaway <ngyanjia26a2000@gmail.com>
2022-08-13 15:13:41 +0800

    Implemented Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Duke {
     public static void main(String[] args) {
         String greetings = "_________________________________________________\nHello! I'm Duke" +
                 "\nWhat can I do for you?\n_________________________________________________";
         System.out.println(greetings);
 
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         //int count = 1;
 
         Scanner sc = new Scanner(System.in);
@@ -17,14 +19,41 @@ public class Duke {
             if (Objects.equals(echo, "list")) {
                 if (tasks.isEmpty()) {
                     System.out.println("No tasks available!");
+                } else {
+                    System.out.println("Here are the tasks in your list:\n");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i + 1 + "." + tasks.get(i).toString());
+                    }
                 }
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(i + 1 + ". " + tasks.get(i));
+                // checks if it starts with mark, has a space after mark, and are all numbers after the space
+            } else if (echo.length() >= 5 && (echo.startsWith("mark") &&
+                    (Character.isWhitespace(echo.charAt(4))) &&
+                    echo.substring(5).chars().allMatch(Character::isDigit))) {
+                int number = parseInt(echo.substring(5));
+                if (number > tasks.size() || number <= 0) {
+                    System.out.println("Task number does not exist.");
+                } else {
+                    tasks.get(number - 1).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:\n" + tasks.get(number - 1).toString());
                 }
+                //int number = Character.getNumericValue(echo.charAt(5));
+                //similar logic as above
+            } else if (echo.length() >= 7 && (echo.startsWith("unmark") &&
+                    (Character.isWhitespace(echo.charAt(6))) &&
+                    echo.substring(7).chars().allMatch(Character::isDigit))) {
+                int number = parseInt(echo.substring(7));
+                if (number > tasks.size() || number <= 0) {
+                    System.out.println("Task number does not exist.");
+                } else {
+                    tasks.get(number - 1).markAsNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:\n" +
+                            tasks.get(number - 1).toString());
+                }
+                //int number = Character.getNumericValue(echo.charAt(5));
             } else {
                 System.out.println("_________________________________________________\nadded: " + echo + "\n" +
                         "_________________________________________________\n");
-                tasks.add(echo);
+                tasks.add(new Task(echo));
                 //count++;
             }
             echo = sc.nextLine();
