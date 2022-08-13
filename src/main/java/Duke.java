db2e717a4c4ad30eb300b5f3dd0b9959c2b5ef93
JustinS00 <justinsiaw00@gmail.com>
2022-08-13 16:17:28 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,35 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        startService();
+    }
+    private static void startService() {
+        dukePrint("Hello! I'm Duke\nWhat can I do for you?\n");
+        getUserInput();
+    }
+
+    private static void dukePrint(String str) {
+        System.out.println("===========================================\n");
+        System.out.println(str);
+        System.out.println("===========================================\n");
+    }
+
+    private static void getUserInput() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        switch (str){
+            case "bye": {
+                endService();
+                break;
+            } default : {
+                dukePrint(str);
+                getUserInput();
+            }
+        }
+    }
+
+    private static void endService() {
+        dukePrint("Bye. Hope to see you again!");
+        return;
     }
 }
