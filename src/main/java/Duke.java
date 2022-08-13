a28e571a06a9382a4a891be9f09e2eec34b53160
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-13 15:46:20 +0800

    Implement level 3 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,37 +1,70 @@
 import java.util.Scanner;
 
 public class Duke {
+    private final TaskList taskList = new TaskList();
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n" + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
         System.out.println("Hello! I'm Duke!");
         System.out.println("What can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
-        TaskList taskList = new TaskList();
+        Duke duke = new Duke();
 
         while (true) {
             System.out.print("> ");
-            String command = scanner.nextLine();
-            switch (command) {
-                case "list": {
-                    System.out.println(taskList);
-                    break;
-                }
-                case "bye": {
-                    System.out.println("Bye! Hope to see you again soon!");
-                    return;
-                }
-                default: {
-                    taskList.addTask(command);
-                    System.out.println("added: " + command);
-                    break;
-                }
+            String input = scanner.nextLine();
+            String[] arguments = getArguments(input);
+
+            if (arguments.length == 0) {
+                System.out.println("Please enter a command.");
+                continue;
+            }
+            boolean isTerminal = duke.executeCommand(arguments);
+            if (isTerminal) {
+                return;
             }
         }
+    }
+
+    private static String[] getArguments(String command) {
+        return command.split(" ");
+    }
+
+    /**
+     * Executes a given command assuming all arguments are correct.
+     *
+     * @param arguments the arguments of the command
+     * @return `true` if the command is terminal, `false` otherwise
+     */
+    private boolean executeCommand(String[] arguments) {
+        String command = arguments[0];
+        switch (command) {
+            case "mark": {
+                int index = Integer.parseInt(arguments[1]) - 1; // task indexing is 1-indexed
+                taskList.getTask(index).markAsDone();
+                break;
+            }
+            case "unmark": {
+                int index = Integer.parseInt(arguments[1]) - 1; // task indexing is 1-indexed
+                taskList.getTask(index).markAsUndone();
+                break;
+            }
+            case "list": {
+                System.out.println(this.taskList);
+                break;
+            }
+            case "bye": {
+                System.out.println("Bye! Hope to see you again soon!");
+                return true;
+            }
+            default: {
+                taskList.addTask(new Task(command));
+                System.out.println("added: " + command);
+                break;
+            }
+        }
+        return false;
     }
 }
