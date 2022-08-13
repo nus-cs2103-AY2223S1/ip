a310c44d787a7a68dbd4ae6d39a4f27b9a6d985c
alvintfl <alvintanfulong@gmail.com>
2022-08-14 01:52:58 +0800

    Tako class: add task list functionality in main method
    Add feature following level-3 instructions.

--- src/main/java/Tako.java
+++ src/main/java/Tako.java
@@ -9,24 +9,45 @@ import java.util.Scanner;
 public class Tako {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Tako\nWhat do you want?");
-        final String EXIT_COMMAND = "bye";
-        final String LIST_COMMAND = "list";
-        String[] texts = new String[100];
-        int textsCount = 0;
+        final String COMMAND_EXIT = "bye";
+        final String COMMAND_LIST = "list";
+        final String COMMAND_MARK = "mark";
+        Task[] tasks = new Task[100];
+        int tasksCount = 0;
 
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String input = sc.nextLine();
-            if (input.equals(EXIT_COMMAND)) {
+            String[] splitInput = input.split(" ", 2);
+
+            if (input.equals(COMMAND_EXIT)) {
                 System.out.println("Bye, until next time...");
                 break;
-            } else if (input.equals(LIST_COMMAND)) {
-                for (int i = 0; i < textsCount; i++) {
-                    System.out.println((i + 1) + ". " + texts[i]);
+            } else if (input.equals(COMMAND_LIST)) {
+                for (int i = 0; i < tasksCount; i++) {
+                    StringBuilder sb = new StringBuilder();
+                    Task task = tasks[i];
+                    sb.append(i + 1);
+                    sb.append(".[");
+                    sb.append(task.getStatusIcon());
+                    sb.append("] ");
+                    sb.append(task);
+                    System.out.println(sb);
                 }
+            } else if (splitInput[0].equals(COMMAND_MARK) && splitInput.length == 2) {
+                 int taskNumber = Integer.parseInt(splitInput[1]) - 1;
+                 Task task = tasks[taskNumber];
+                 task.markAsDone();
+
+                 StringBuilder sb = new StringBuilder("marked: ");
+                 sb.append("[");
+                 sb.append(task.getStatusIcon());
+                 sb.append("] ");
+                 sb.append(task);
+                 System.out.println(sb);
             } else {
-                texts[textsCount] = input;
-                textsCount++;
+                tasks[tasksCount] = new Task(input);
+                tasksCount++;
                 System.out.println("added: " + input);
             }
         }
