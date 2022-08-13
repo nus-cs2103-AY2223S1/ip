4a4b40b126438f1b53f3455f2f800ce6ec097c12
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-08-13 11:47:26 +0800

    Added greetUser() and handleInput() methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,37 @@
 import java.util.Scanner;
-public class Duke {
-    public static void main(String[] args) {
-        String logo = "_______     _\n" +
-                   "|  ___|    | |\n" +
-                   "|  |_  ____| |_____ ____  _  __\n" +
-                   "|   _|/ _  \\ | ___|/  _ \\| |/  \\\n"+
-                   "|  | | |_| | | |___| |_| |  / \\ |\n" +
-                   "|__|  \\__|_|_|____|\\____/|_|  |_|\n";
-        System.out.println("Hello from\n" + logo);
 
+public class Duke {
+
+    private static void greetUser() {
+        String logo = "_______     _\n" +
+                "|  ___|    | |\n" +
+                "|  |_  ____| |_____ ____  _  __\n" +
+                "|   _|/ _  \\ | ___|/  _ \\| |/  \\\n"+
+                "|  | | |_| | | |___| |_| |  / \\ |\n" +
+                "|__|  \\__|_|_|____|\\____/|_|  |_|\n";
+        System.out.println("Hello from\n" + logo);
         // prompt user
         System.out.println("Where would you like to go next?");
+    }
 
-        Scanner in = new Scanner(System.in);
-        while(true) {
-            String s = in.nextLine();
-            if (s.equals("bye")) {
-                //exit
-                System.out.println("Thank you for swinging by :)");
-                System.exit(0);
-            } else {
-                //echo request
-                System.out.println(s);
-            }
-
+    private static void handleInput(String userInput) {
+        if (userInput.equals("bye")) {
+            //exit
+            System.out.println("Thank you for swinging by :)");
+            System.exit(0);
+        } else {
+            //echo request
+            System.out.println(userInput);
         }
+    }
 
-
+    public static void main(String[] args) {
+        greetUser();
+        Scanner in = new Scanner(System.in);
+        String s;
+        while(true) {
+            s = in.nextLine();
+            handleInput(s);
+        }
     }
 }
