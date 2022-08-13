fb869d4e02dc8cfb755eb46f2a4cd0dcbe539ca0
Ssatu <69383469+Ssatu@users.noreply.github.com>
2022-08-13 16:51:02 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    /**
+     *  This method prints out a horizontal line 40 dashes long.
+     */
+    public static void printLine() {
+        for(int i = 0; i < 40; i++) {
+            System.out.print("_");
+        }
+        System.out.println();
+    }
+
+    /**
+     * This method prints out the string s, wrapped within 2 horizontal lines.
+     * @param s The given string to be printed.
+     */
+    public static void say(String s) {
+        printLine();
+        System.out.println(s);
+        printLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scn = new Scanner(System.in);
+
+        say("Hello! I'm Pawl\n What can I do for you?");
+
+        String first = scn.next();
+        while (!first.equals("bye")){
+            say(first);
+            first = scn.next();
+        }
+
+        say("Bye. Hope to see you again soon!");
+
     }
 }
