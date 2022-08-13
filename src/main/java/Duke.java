0f0392adb1292cb359d368f047586a24edb1f796
JovonLim <jovonlim@gmail.com>
2022-08-13 22:18:12 +0800

    Tasks are split into different categories

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     private final static String INTRODUCTION_MSG = "Hello I'm Duke\nWhat can I do for you?";
     private final static String MARK_TASK_DONE_MSG = "Nice! I've marked this task as done:";
     private final static String MARK_TASK_UNDONE_MSG = "OK, I've marked this task as not done yet:";
+    private final static String ADDED_TASK_MSG = "Got it. I've added this task:";
     private final static String DISPLAY_LIST_MSG = "Here are the tasks in your list:";
     private final static String CLOSING_MSG = "Bye. Hope to see you again soon!";
     private static ArrayList<Task> listOfTasks = new ArrayList<>();
@@ -25,8 +26,12 @@ public class Duke {
                 markTaskDone(input);
             }  else if (input.startsWith("unmark")) {
                 markTaskUndone(input);
-            } else {
-                addToList(input);
+            } else if (input.startsWith("todo")) {
+                addNewToDo(input);
+            } else if (input.startsWith("deadline")) {
+                addNewDeadline(input);
+            } else if (input.startsWith("event")) {
+                addNewEvent(input);
             }
         }
         endChat();
@@ -44,10 +49,27 @@ public class Duke {
         }
     }
 
-    private static void addToList(String input) {
-        Task task = new Task(input);
+    private static void addNewToDo(String input) {
+        String[] taskDescription = input.split("todo ", 2);
+        addToList(new ToDo(taskDescription[1]));
+    }
+
+    private static void addNewDeadline(String input) {
+        String[] deadlineDescription = input.split("deadline ", 2);
+        String[] deadlineComponents = deadlineDescription[1].split("/by ", 2);
+        addToList(new Deadline(deadlineComponents[0], deadlineComponents[1]));
+    }
+
+    private static void addNewEvent(String input) {
+        String[] eventDescription = input.split("event ", 2);
+        String[] eventComponents = eventDescription[1].split("/at ", 2);
+        addToList(new Event(eventComponents[0], eventComponents[1]));
+    }
+
+    private static void addToList(Task task) {
         listOfTasks.add(task);
-        System.out.println("added: " + input);
+        System.out.println(ADDED_TASK_MSG + "\n  " + task + "\n"
+                + "Now you have " + listOfTasks.size() + " tasks in the list.");
     }
 
     private static void markTaskDone(String str) {
@@ -55,11 +77,12 @@ public class Duke {
             System.out.println("Error: Incomplete command");
         } else {
             try {
-                int index = Integer.parseInt(str.substring(5));
-                Task selectedTask = listOfTasks.get(index - 1);
+                String[] indexString = str.split("mark ", 2);
+                int index = Integer.parseInt(indexString[1]) - 1;
+                Task selectedTask = listOfTasks.get(index);
                 if (selectedTask != null) {
                     selectedTask.markAsDone();
-                    System.out.println(MARK_TASK_DONE_MSG+ "\n  " + selectedTask.toString());
+                    System.out.println(MARK_TASK_DONE_MSG+ "\n  " + selectedTask);
                 }
             } catch (NumberFormatException nfe) {
                 System.out.println("Error: Valid Integer required");
@@ -72,11 +95,12 @@ public class Duke {
             System.out.println("Error: Incomplete command");
         } else {
             try {
-                int index = Integer.parseInt(str.substring(7));
-                Task selectedTask = listOfTasks.get(index - 1);
+                String[] indexString = str.split("unmark ", 2);
+                int index = Integer.parseInt(indexString[1]) - 1;
+                Task selectedTask = listOfTasks.get(index);
                 if (selectedTask != null) {
                     selectedTask.markUndone();
-                    System.out.println(MARK_TASK_UNDONE_MSG + "\n  " + selectedTask.toString());
+                    System.out.println(MARK_TASK_UNDONE_MSG + "\n  " + selectedTask);
                 }
             } catch (NumberFormatException nfe) {
                 System.out.println("Error: Valid Integer required");
