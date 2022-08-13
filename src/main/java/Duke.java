c084c78b99c95c594a5734d67d2e1caee2a4ed4c
hanwenlai <aaronlhw02@gmail.com>
2022-08-13 20:13:12 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,9 +13,26 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
         Scanner sc = new Scanner(System.in);
+
+        // Create arraylist to store given tasks
+        ArrayList<String> tasks = new ArrayList<String>();
+
+        // Read string input from user
         String command = sc.nextLine();
+
         while (!Objects.equals(command, "bye")) {
-            System.out.println(command);
+            if (Objects.equals(command, "list")) {
+                int count = 1;
+                for (String task : tasks) {
+                    String s = Integer.toString(count);
+                    System.out.println(s + ". " + task);
+                    count++;
+                }
+                command = sc.nextLine();
+                continue;
+            }
+            tasks.add(command);
+            System.out.println("added: " + command);
             command = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
