e7a473d44f73f70874a5de2e2f566fbb651b30ae
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-08-13 21:05:04 +0800

    Completed level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -34,11 +34,6 @@ public class Duke {
         printMessage(exitMsg);
     }
 
-    private static void addTask(String input) {
-        tasks.add(new Task(input));
-        printMessage("added: " + input);
-    }
-
     private static void listTasks() {
         int pointer = 1;
         String reply = "Here are the tasks in your list:";
@@ -47,6 +42,29 @@ public class Duke {
             pointer++;
         }
         printMessage(reply);
+    }
+
+    private static void addTask(Task task) {
+        tasks.add(task);
+        String msg = "Got it. I've added this task: + \n"
+                + task + "\n"
+                + "Now you have " + tasks.size() + " tasks in the list.";
+        printMessage(msg);
+    }
+
+    private static void addTodo(String description) {
+        Todo todo = new Todo(description);
+        addTask(todo);
+    }
+
+    private static void addDeadline(String description, String date) {
+        Deadline deadline = new Deadline(description, date);
+        addTask(deadline);
+    }
+
+    private static void addEvent(String description, String date) {
+        Event event = new Event(description, date);
+        addTask(event);
     }
 
     private static void markTask(int index) {
@@ -71,7 +89,7 @@ public class Duke {
         }
 
         // May throw exception if input digit is greater than length of tasks. Should fix later.
-        String[] str = input.split(" ");
+        String[] str = input.split(" ", 2);
         String cmd = str[0];
         if (cmd.equals("mark")) {
             int taskIndex = Integer.parseInt(str[1]) - 1;
@@ -79,8 +97,22 @@ public class Duke {
         } else if (cmd.equals("unmark")) {
             int taskIndex = Integer.parseInt(str[1]) - 1;
             unmarkTask(taskIndex);
+        } else if (cmd.equals("todo")) {
+            addTodo(str[1]);
+            // Stuff below this line will throw an error if there's no spacing. Should fix later.
+        } else if (cmd.equals("deadline")) {
+            String[] str2 = str[1].split(" /by ");
+            String description = str2[0];
+            String date = str2[1];
+            addDeadline(description, date);
+        } else if (cmd.equals("event")) {
+            String[] str2 = str[1].split(" /at ");
+            String description = str2[0];
+            String date = str2[1];
+            addEvent(description, date);
         } else {
-            addTask(input);
+            // print error
+            System.out.println("whoops");
         }
     }
 
