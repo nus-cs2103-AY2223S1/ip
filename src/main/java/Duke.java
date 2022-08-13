78e23755a2c41c8b3778837127327c67d2648867
Samsation <samsamsailorman@gmail.com>
2022-08-13 19:21:00 +0800

    Implement Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String LOGO = "\t ____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
+        final String LINE = "\t____________________________________________________________";
+
+        Scanner sc = new Scanner(System.in);
+        String command = "";
+
+        System.out.println(LOGO
+                + LINE
+                + "\n"
+                + "\tHello! I'm Duke\n"
+                + "\tWhat can I do for you?\n"
+                + LINE);
+
+        while (true) {
+            command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("\tBye. Hope to see you again soon!\n"
+                        + LINE);
+                break;
+            }
+            System.out.println(LINE
+                    + "\n\t"
+                    + command
+                    + "\n"
+                    + LINE);
+        }
     }
 }
