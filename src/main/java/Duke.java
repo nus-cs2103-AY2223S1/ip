c64cd4837034162d9e5f0015f544e3adf4eae232
jonasgwt <55698556+jonasgwt@users.noreply.github.com>
2022-08-13 22:14:18 +0800

    Completed Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,27 +2,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static final ArrayList<String> data = new ArrayList<>();
+    private static final ArrayList<Task> data = new ArrayList<>();
 
     public static String reply(String input) {
+        // shows user list of all saved tasks
         if (input.equals("list")) {
             StringBuilder output = new StringBuilder();
             for (int i = 0; i < data.size(); i++) {
-                output.append(i).append(". ").append(data.get(i)).append("\n");
+                Task task = data.get(i);
+                output.append(i+1).append(". ").append(task).append("\n");
             }
             return output.substring(0, output.length()-1);
         }
-        data.add(input);
+        // mark task as complete
+        if (input.startsWith("mark")) {
+            int target_index = Integer.parseInt(input.substring(4).trim()) - 1;
+            Task task = data.get(target_index);
+            task.markDone();
+            return "Nice! I've marked this task as done:\n" + task;
+        }
+        // mark task as not done
+        if (input.startsWith("unmark")) {
+            int target_index = Integer.parseInt(input.substring(6).trim()) - 1;
+            Task task = data.get(target_index);
+            task.markNotDone();
+            return "OK, I've marked this task as not done yet:\n" + task;
+        }
+        // adding task to data
+        data.add(new Task(input));
         return "added: " + input;
     }
 
     public static void main(String[] args) {
         System.out.println("Quack! I'm Duck\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
+        while (sc.hasNextLine()) {
             System.out.println("____________________________________________________________");
             System.out.println("Duck:");
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Quack! Hope to see you again soon!");
                 return;
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
+}
