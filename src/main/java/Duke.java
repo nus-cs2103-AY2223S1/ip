2ac82e8064334d4cade43bd6e508e2bf4433ceb6
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-13 14:50:23 +0800

    Implement command echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,20 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Duke!");
+        System.out.println("What can I do for you?");
+
+        // Echo stdin until we get `bye`.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("> ");
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye! Hope to see you again soon!");
+                break;
+            }
+            System.out.println(command);
+        }
     }
 }
