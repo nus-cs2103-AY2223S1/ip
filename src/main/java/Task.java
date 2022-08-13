682db450f3a95b517c3b92832f9a016722492d6d
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-13 15:45:52 +0800

    Create class `Task`

---
+++ src/main/java/Task.java
@@ -0,0 +1,40 @@
+/**
+ * A Task has a description and a status tracking whether it is done or not.
+ */
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns a checkmark `✓` if the task is done, an empty string otherwise.
+     *
+     * @return the status icon
+     */
+    private String getStatusIcon() {
+        return (isDone ? "✓" : " ");
+    }
+
+    /**
+     * Marks this task as done.
+     */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /**
+     * Marks this task as undone.
+     */
+    public void markAsUndone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), description);
+    }
+}
