3b842025c551434211d8bd2d6e2d9d9273fa0671
rylzxc <chuachdaryl@gmail.com>
2022-08-13 13:21:45 +0800

    Complete Level 1

---
+++ src/main/java/Constants.java
@@ -0,0 +1,5 @@
+public class Constants {
+    public static String GREETING = "Hello!! I'm Duke\nIs there something you'd like? (i.e. a list)";
+    public static String BYE = "bye";
+    public static String END_PROGRAM = "Bye. Hope to see you again soon!";
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.SEVERE;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Logger logger = Logger.getLogger("DukeLogger");
+        try {
+            processUserRequest();
+        } catch (Exception e) {
+            logger.log(SEVERE, "An error has occurred.");
+        }
+
     }
+
+    /* ------------------------HELPER FUNCTIONS------------------------ */
+    public static void processUserRequest() {
+
+        System.out.println(Constants.GREETING);
+        Scanner reader = new Scanner(System.in);
+
+        boolean end = false;
+        while (!end) {
+            String userRequest = reader.next();
+            if (userRequest.equals(Constants.BYE)) {
+                end = true;
+                System.out.println(Constants.END_PROGRAM);
+            }
+            else {
+                System.out.println(userRequest);
+                System.out.print("Enter something else you'd like to have!");
+            }
+        }
+        reader.close();
+    }
+
 }
