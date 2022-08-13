6ec700a7e7bd5cad49ce66149e865fae8a69737d
Tjan Eng Ger, Kevin <kevintjan27@gmail.com>
2022-08-13 17:44:09 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private ArrayList<String> history = new ArrayList<String>();
+    private Scanner commandInput;
+    private boolean isClosed = false;
+
+    //The strings that Duke uses for greetings and formatting. Constants.
     private final static String lineBreak1
             = "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
              + "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-";
@@ -12,31 +18,60 @@ public class Duke {
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n"
-            + "Hello! I'm Duke\n"
+            + "|____/ \\__,_|_|\\_\\___|";
+    private final static String greeting
+            = "Hello! I'm Duke\n"
             + "What can I do for you?";
 
-    private static boolean inputCommand(String command) {
-        if (!command.equals("bye")) {
-            System.out.println(lineBreak2);
-            System.out.println(command);
-            return false;
+    private void goodbye() {
+        System.out.println(this.lineBreak2);
+        System.out.println("Goodbye! See you next time!\n" + this.lineBreak1);
+        this.isClosed = true;
+    }
+
+    private void addToHistory(String command) {
+        System.out.println(this.lineBreak2);
+        System.out.println("Adding to Tasks: " + command);
+        history.add(command);
+        System.out.println(lineBreak1);
+    }
+
+    private String taskItem(int i) {
+        int taskNum = i + 1;
+        return String.valueOf(taskNum) + ". " + history.get(i);
+    }
+
+    private void listOut() {
+        System.out.println(lineBreak2);
+        if (history.size() <= 0) {
+            System.out.println("No tasks assigned yet.");
         } else {
-            System.out.println(lineBreak2);
-            System.out.println("Goodbye! See you next time!\n" + lineBreak1);
-            return true;
+            for (int i = 0; i < history.size(); i++) {
+                System.out.println(this.taskItem(i));
+            }
+        }
+        System.out.println(lineBreak1);
+    }
+
+    private void inputCommand(String command) {
+        if (command.equals("bye")) {
+            this.goodbye();
+        } else if (command.equals("list")) {
+            this.listOut();
+        } else {
+            this.addToHistory(command);
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(logo);
-        Scanner commandInput = new Scanner(System.in);
-        while (commandInput.hasNextLine()) {
-            System.out.println(lineBreak1);
-            String command = commandInput.nextLine();
-            boolean isGoodBye = Duke.inputCommand(command);
-            if (isGoodBye) {
-                commandInput.close();
+        Duke duke = new Duke();
+        System.out.println(duke.logo + "\n" + duke.greeting + "\n" + duke.lineBreak1);
+        duke.commandInput = new Scanner(System.in);
+        while (duke.commandInput.hasNextLine()) {
+            String command = duke.commandInput.nextLine();
+            duke.inputCommand(command);
+            if (duke.isClosed) {
+                duke.commandInput.close();
                 break;
             }
         }
