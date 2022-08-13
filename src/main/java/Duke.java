e9109e6fd6e2ef3c8b4a61adc99fbe5b57c224d9
linuschui <linuschuii@gmail.com>
2022-08-13 18:41:48 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,22 +5,61 @@ public class Duke {
     private static final String line = " ____________________________________________________________";
     private static final String startMessage = "  Hello! I'm Duke\n" + "  What can I do for you?";
     private static final String endMessage = " Bye. Hope to see you again soon!";
-    private static final ArrayList<String> tasks = new ArrayList<>(100);
+    private static final ArrayList<Task> tasks = new ArrayList<>(100);
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         sendMessage(startMessage);
-        while (true) {
+        while (scanner.hasNextLine()) {
             String userCommand = scanner.nextLine();
+            // case 1 : user inputs "bye"
             if (userCommand.equals("bye")) {
                 break;
+            // case 2 : user inputs "list"
             } else if (userCommand.equals("list")) {
                 sendMessage(getList(tasks));
+            // case 3 : user inputs "mark"
+            } else if (userCommand.split(" ")[0].equals("mark")) {
+                String[] s = userCommand.split(" ");
+                // check if input is of length 2
+                if (s.length == 2) {
+                    // check if task exists
+                    if (tasks.get(Integer.parseInt(s[1]) - 1) != null) {
+                        // get index of task to be marked done
+                        int index = Integer.parseInt(s[1]) - 1;
+                        // get task
+                        Task task = tasks.get(index);
+                        // mark task as done
+                        task.markAsDone();
+                        sendMessage(" Nice! I've marked this task as done:\n" + task.toString());
+                    }
+                }
+            // case 4 : user inputs "unmarked"
+            } else if (userCommand.split(" ")[0].equals("unmark")) {
+                String[] s = userCommand.split(" ");
+                // check if input is of length 2
+                if (s.length == 2) {
+                    // check if task exists
+                    if (tasks.get(Integer.parseInt(s[1]) - 1) != null) {
+                        // get index of task to be marked undone
+                        int index = Integer.parseInt(s[1]) - 1;
+                        // get task
+                        Task task = tasks.get(index);
+                        // mark task as undone
+                        task.markAsUndone();
+                        sendMessage(" OK, I've marked this task as not done yet:\n" + task.toString());
+                    }
+                }
+            // case 5 : user inputs task to add
             } else {
-                tasks.add(userCommand);
+                // create new Task object and add to ArrayList of tasks
+                tasks.add(new Task(userCommand));
                 sendMessage("added: " + userCommand);
             }
         }
+        // close scanner
+        scanner.close();
+        // base case : send end message when while loop is broken
         sendMessage(endMessage);
     }
 
@@ -30,14 +69,14 @@ public class Duke {
         System.out.println(line);
     }
 
-    public static String getList(ArrayList<String> tasks) {
+    public static String getList(ArrayList<Task> tasks) {
         String list = "";
         for (int i = 0; i < tasks.size(); i++) {
             if (i == tasks.size() - 1) {
-                list = list + String.valueOf(i + 1) + ": " + tasks.get(i);
+                list = list + String.valueOf(i + 1) + ": " + tasks.get(i).toString();
                 break;
             }
-            list = list + String.valueOf(i + 1) + ": " + tasks.get(i) + "\n";
+            list = list + String.valueOf(i + 1) + ": " + tasks.get(i).toString() + "\n";
         }
         return list;
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+
+    // class variables
+    protected String description;
+    protected boolean isDone;
+
+    // constructor
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    public String getTaskDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
