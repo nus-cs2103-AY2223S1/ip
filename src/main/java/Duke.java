00dd2d7a683c9a112eb53b2ea8a9a7bc7ae23cd4
KIANSEONG <kianseong.quah@gmail.com>
2022-08-13 13:47:36 +0800

    Implemented Duke Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static Utilities util;
+
+    Duke() {
+        util = new Utilities();
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.startDuke();
+    }
+
+    public void startDuke() {
+        sendMessageIntro();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                sendMessageExit();
+                break;
+            }
+            util.printMsg(input);
+        }
+        sc.close();
+    }
+
+    public void sendMessageIntro() {
+        util.printMsg("Hello! I'm Duke. \n\tWhat can I do for you?");
+    }
+
+    public void sendMessageExit() {
+        util.printMsg("Bye. Hope to see you again soon!");
+    }
 
 }
---
+++ src/main/java/Utilities.java
@@ -0,0 +1,12 @@
+public class Utilities {
+
+    public void printMsg(String message) {
+        printStraightLine();
+        System.out.println("\t" + message);
+        printStraightLine();
+    }
+
+    public void printStraightLine() {
+        System.out.println("\t____________________________________________________________");
+    }
+}
