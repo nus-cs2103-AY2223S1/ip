a3dbc0113b9d512628bbda2e2c0c240f1b1b56b6
dexter-sim <dextersimch@gmail.com>
2022-08-14 00:01:30 +0800

    Implement skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static final String GREETING = "Hello! I'm Duke\n"
+            + "What can I do for you?\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(LOGO);
+        System.out.println(GREETING);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                scanner.close();
+                break;
+            }
+            System.out.println(userInput);
+        }
     }
 }
