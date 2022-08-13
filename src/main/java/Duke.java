12cbcb09bbee9c59ae355cfdd88504cd8eea8977
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2022-08-13 17:22:48 +0800

    Implemented a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Duke is a personal assistant Chat-bot that aims to help users to keep track of various things
+ */
 public class Duke {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Main method that runs the program, handles the logic of the input
+     * @param args Arguments passed to the program
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        boolean endLoop = false;
+
+        Output.GREETINGS.print();
+        while (!endLoop) {
+            String command = scanner.nextLine();
+            switch (command) {
+                case "bye":
+                    Output.GOODBYE.print();
+                    endLoop = true;
+                    break;
+                default:
+                    Output.echo(command);
+            }
+        }
     }
 }
---
+++ src/main/java/Output.java
@@ -0,0 +1,40 @@
+/**
+ * Enum for the various different outputs that the Chat-bot can give.
+ */
+public enum Output{
+  GREETINGS (" ____        _        \n\t "
+          + "|  _ \\ _   _| | _____ \n\t "
+          + "| | | | | | | |/ / _ \\\n\t "
+          + "| |_| | |_| |   <  __/\n\t "
+          + "|____/ \\__,_|_|\\_\\___|\n\n\t "
+          + "Hello! I'm Duke\n\t "
+          + "What can I do for you?"),
+  GOODBYE ("Bye. Hope to see you again soon!");
+
+  private String output = "";
+
+  /**
+   * Constructor for the Enums
+   * @param s String that is to be initialised in the enum
+   */
+  Output(String s){
+    this.output = s;
+  }
+
+  /**
+   * Prints the enum output with the specified format
+   */
+  void print() {
+    echo(this.output);
+  }
+
+  /**
+   * Specifies the output format
+   * @param s String to be printed out
+   */
+  static void echo(String s) {
+    System.out.println("\t--------------------------------------------------------");
+    System.out.println("\t " + s);
+    System.out.println("\t--------------------------------------------------------");
+  }
+}
