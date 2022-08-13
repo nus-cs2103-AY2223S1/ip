7212a66ce17162a3f60f2f40bc46eca24292cd00
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-13 15:09:41 +0800

    Implement text storing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,16 +11,27 @@ public class Duke {
         System.out.println("Hello! I'm Duke!");
         System.out.println("What can I do for you?");
 
-        // Echo stdin until we get `bye`.
         Scanner scanner = new Scanner(System.in);
+        TaskList taskList = new TaskList();
+
         while (true) {
             System.out.print("> ");
             String command = scanner.nextLine();
-            if (command.equals("bye")) {
-                System.out.println("Bye! Hope to see you again soon!");
-                break;
+            switch (command) {
+                case "list": {
+                    System.out.println(taskList);
+                    break;
+                }
+                case "bye": {
+                    System.out.println("Bye! Hope to see you again soon!");
+                    return;
+                }
+                default: {
+                    taskList.addTask(command);
+                    System.out.println("added: " + command);
+                    break;
+                }
             }
-            System.out.println(command);
         }
     }
 }
