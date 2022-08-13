0ee05388811a7ec7bf460423e9f7b3c370f59718
TZL0 <tianzeee@outlook.com>
2022-08-13 16:44:23 +0800

    Level 2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Stream;
@@ -7,6 +8,7 @@ public class Duke {
     private String HELLO_MESSAGE = "Hello! I'm Duke \n"
             + "What can I do for you?";
     private String FAREWELL_MESSAGE = "Bye. Hope to see you again soon!";
+    private ArrayList<String> tasks = new ArrayList<>();
 
 
     public void greet() {
@@ -15,6 +17,18 @@ public class Duke {
 
     public void echo(String msg) {
         mp.printMessage(msg);
+    }
+
+    public void add(String msg) {
+        tasks.add(msg);
+        mp.printMessage("added: " + msg);
+    }
+
+    public void list() {
+        mp.printMessage(Stream.iterate(0, x -> x + 1)
+                        .limit(tasks.size())
+                        .map(x -> String.valueOf(x + 1) + ". " + tasks.get(x))
+                        .reduce("", (x, y) -> x + y + "\n" ));
     }
 
     public void exit() {
@@ -34,13 +48,16 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         boolean isTerminated = false;
         while (!isTerminated) {
-            String command = scanner.next();
+            String command = scanner.nextLine();
             switch (command) {
                 case "bye" :
                     isTerminated = true;
                     break;
+                case "list" :
+                    d.list();
+                    break;
                 default :
-                    d.echo(command);
+                    d.add(command);
             }
         }
         d.exit();
