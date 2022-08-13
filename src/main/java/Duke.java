431fe9049463ba92ed8dc86b75eb7f3c8d546884
Darren12345677 <darrencookies100@gmail.com>
2022-08-13 11:27:42 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm Duke\n" + "What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(command);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
