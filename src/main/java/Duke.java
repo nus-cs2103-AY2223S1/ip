0673395102f3b26cf68c397585b8cff9ccfeb49a
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-08-13 11:06:28 +0800

    Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        // prompt user
+        System.out.println("Where would you like to go next?");
+
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            String s = in.nextLine();
+            if (s.equals("bye")) {
+                //exit
+                System.out.println("Thank you for swinging by :)");
+                System.exit(0);
+            } else {
+                //echo request
+                System.out.println(s);
+            }
+
+        }
+
+
     }
 }
