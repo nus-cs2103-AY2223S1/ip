8cab208e573dbdf9ea760a6a9c8e77404f95974a
lfrostbytee <tr.lfrostbyte@gmail.com>
2022-08-14 01:37:44 +0800

    Level-1 Added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,47 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String logo = "\n" +
+                "██████╗░░█████╗░░█████╗░██╗░░░░░░██████╗██╗░░██╗███████╗███████╗███╗░░██╗\n" +
+                "██╔══██╗██╔══██╗██╔══██╗██║░░░░░██╔════╝██║░░██║██╔════╝██╔════╝████╗░██║\n" +
+                "██████╔╝██║░░██║██║░░██║██║░░░░░╚█████╗░███████║█████╗░░█████╗░░██╔██╗██║\n" +
+                "██╔═══╝░██║░░██║██║░░██║██║░░░░░░╚═══██╗██╔══██║██╔══╝░░██╔══╝░░██║╚████║\n" +
+                "██║░░░░░╚█████╔╝╚█████╔╝███████╗██████╔╝██║░░██║███████╗███████╗██║░╚███║\n" +
+                "╚═╝░░░░░░╚════╝░░╚════╝░╚══════╝╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝";
+
+        String welcomeMessage = "Hello from" + logo + "\n" +
+                "Type something in for Poolsheen to respond to you:";
+
+        System.out.println(welcomeMessage);
+
+        String poolsheenLastReply = "meow *_*";
+
+        String horizontalLine = "---------------";
+
+        String poolshenStartReply = "      ";
+
+        String exitCommand = "bye";
+
+        String exitMessage = "MeoAww... See you next time :(";
+
+        boolean hasExited = false;
+
+        String userInput = "";
+        Scanner myScanner = new Scanner(System.in);
+
+        while (!hasExited) {
+            userInput = myScanner.nextLine();
+            if (userInput.equals(exitCommand)) {
+                hasExited = true;
+            } else {
+                System.out.println(horizontalLine + "\n" + poolshenStartReply + userInput + " " +
+                        poolsheenLastReply + "\n" + horizontalLine);
+            }
+        }
+
+        System.out.println(horizontalLine + "\n" + poolshenStartReply + exitMessage + "\n" + horizontalLine);
+
     }
 }
