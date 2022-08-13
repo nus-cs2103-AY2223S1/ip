cb426f3dea4ad10a5c477bfc9e34c2ff94efe69d
Lu Yiting <e0774189@u.nus.edu>
2022-08-13 17:31:22 +0800

    Implemented Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I am Duke");
+        System.out.println("What can I do for you?");
+
+        String s = "";
+        while (true) {
+            s = sc.nextLine();
+            if (s.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(s);
+            }
+        }
+        
     }
 }
