3ff20285bad492c15424f0613c50bedfa3af5efd
Marcus Pang <marcuspangyuyang@gmail.com>
2022-08-13 19:13:14 +0800

    Implement while-loop to echo user's input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static final String GREETING_MESSAGE = "    ____________________________________________________________\n" +
+            "     Hello! I'm Duke\n" +
+            "     What can I do for you?\n" +
+            "    ____________________________________________________________\n";
+    public static final String GOODBYE_MESSAGE = ("    ____________________________________________________________\n" +
+            "     Bye. Hope to see you again soon!\n" +
+            "    ____________________________________________________________\n");
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Greeting message is always printed.
+        System.out.println(GREETING_MESSAGE);
+
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        // Continuously repeat user's input until it matches "bye".
+        while (!input.equals("bye")) {
+            System.out.println(generateReply(input));
+            input = in.nextLine();
+        }
+
+        // Goodbye message is always printed.
+        System.out.println(GOODBYE_MESSAGE);
+    }
+
+    public static String generateReply(String userInput) {
+        return String.format("    ____________________________________________________________\n" +
+                "     %s\n" +
+                "    ____________________________________________________________\n", userInput);
     }
 }
