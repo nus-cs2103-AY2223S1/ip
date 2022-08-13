7ac1619fa84e9415fbf3c2a275075ea9d49c5a1f
kevinchangjk <kevinchangjk@gmail.com>
2022-08-13 21:28:58 +0800

    add startup display

---
+++ src/main/java/Carbon.java
@@ -0,0 +1,37 @@
+public class Carbon {
+    private static void startup() {
+        // ascii art generated from patorjk.com
+
+        String intro = "                 _ _ _ ____ _    ____ ____ _  _ ____ \n" + 
+            "                 | | | |___ |    |    |  | |\\/| |___ \n" + 
+            "                 |_|_| |___ |___ |___ |__| |  | |___ \n";
+
+        String logo = "  ▄████████    ▄████████    ▄████████ ▀█████████▄   ▄██████▄  ███▄▄▄▄   \n" + 
+            " ███    ███   ███    ███   ███    ███   ███    ███ ███    ███ ███▀▀▀██▄ \n" + 
+            " ███    █▀    ███    ███   ███    ███   ███    ███ ███    ███ ███   ███ \n" + 
+            " ███          ███    ███  ▄███▄▄▄▄██▀  ▄███▄▄▄██▀  ███    ███ ███   ███ \n" + 
+            " ███        ▀███████████ ▀▀███▀▀▀▀▀   ▀▀███▀▀▀██▄  ███    ███ ███   ███ \n" +
+            " ███    █▄    ███    ███ ▀███████████   ███    ██▄ ███    ███ ███   ███ \n" + 
+            " ███    ███   ███    ███   ███    ███   ███    ███ ███    ███ ███   ███ \n" + 
+            " ████████▀    ███    █▀    ███    ███ ▄█████████▀   ▀██████▀   ▀█   █▀  \n" + 
+            "                           ███    ███                                   \n";
+
+        System.out.println(intro);
+        System.out.println(logo);
+
+        // extra space
+        System.out.println("\n\n\n");
+    }
+
+    private static void display(String text) {
+        String divider = "···---······---······---······---······---···\n";
+        String receiver = "\n··-··--···--\n";
+        System.out.println(divider);
+        System.out.println("    " + text);
+        System.out.println(receiver);
+    }
+    
+    public static void main(String[] args) {
+        Carbon.startup();
+    }
+}
