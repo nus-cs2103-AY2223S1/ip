2f4bf81eab86883ffe1f68bdf73736d95d7a2a19
Tan Chee Heng <ch.tan020@gmail.com>
2022-08-14 03:09:45 +0800

    Level 3: Support marking task as done.

--- src/main/java/Jude.java
+++ src/main/java/Jude.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Jude {
-    private static List<String> taskList = new ArrayList<>();
+    private static List<Task> tasks = new ArrayList<>();
 
     /**
      * Runs the chatbot.
@@ -21,17 +21,36 @@ public class Jude {
             System.out.println();
             System.out.print("> ");
             String str = sc.nextLine();
-            if (str.equals("list")) {
-                for (int i = 0; i < taskList.size(); i++) {
-                    String task = taskList.get(i);
-                    System.out.printf("%d. %s\n", i, task);
+            String[] tokens = str.split(" ");
+            if (tokens[0].equals("mark")) {
+                int index = Integer.parseInt(tokens[1])-1;
+                Task task = tasks.get(index);
+
+                //Solution below adapted from
+                //https://nus-cs2103-ay2223s1.github.io/website/schedule/week2/project.html
+                task.markAsDone();
+
+                System.out.println("The following task has been marked as done");
+                System.out.printf("[%s] %s\n", task.getStatusIcon(), task.getDescription());
+            } else if (tokens[0].equals("unmark")) {
+                int index = Integer.parseInt(tokens[1])-1;
+                Task task = tasks.get(index);
+                task.markAsUndone();
+                System.out.println("The following task has been marked as undone");
+                System.out.printf("[%s] %s\n", task.getStatusIcon(), task.getDescription());
+            } else if (str.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    Task task = tasks.get(i);
+                    String description = task.getDescription();
+                    String statusIcon = task.getStatusIcon();
+                    System.out.printf("%d.[%s] %s\n", i+1, statusIcon, description);
                 }
             } else if (str.equals("bye")) {
                 System.out.println("Goodbye! Have a nice day!");
                 break;
             } else {
                 System.out.println("added: " + str);
-                taskList.add(str);
+                tasks.add(new Task(str, false));
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,68 @@
 public class Task {
+    private String description;
+    private boolean isDone;
+
+    /**
+     * Creates a new Task object with a given description and whether it has been done.
+     *
+     * @param description the description of the task
+     * @param isDone whether the task is marked as done
+     */
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    /**
+     * Returns whether the given Task object is marked as done.
+     *
+     * @return whether the task is done
+     */
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    /**
+     * Returns the description of the Task object
+     *
+     * @return description of the Task object
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Sets whether the Task is done.
+     *
+     * @param isDone whether the Task is done
+     */
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    /**
+     * Returns the status icon depicting whether the Task has been done.
+     *
+     * @return 'X' if Task is done, ' ' otherwise
+     */
+    //@@author cheeheng-reused
+    //Reused from https://nus-cs2103-ay2223s1.github.io/website/schedule/week2/project.html
+    //with minor modifications
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    /**
+     * Sets the status of the Task to 'done'.
+     */
+    public void markAsDone() {
+        this.setIsDone(true);
+    }
+
+    /**
+     * Sets the status of the Task to 'not done'.
+     */
+    public void markAsUndone() {
+        this.setIsDone(false);
+    }
 }
