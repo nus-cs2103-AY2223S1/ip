6e402084f860fcb5e5a2fe462ee3d33d31bab741
sprintaway <ngyanjia26a2000@gmail.com>
2022-08-13 10:12:21 +0800

    Implemented Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greetings = "_________________________________________________\nHello! I'm Duke" +
+                "\nWhat can I do for you?\n_________________________________________________";
+        System.out.println(greetings);
+
+        Scanner sc = new Scanner(System.in);
+        String echo = sc.nextLine();
+        while (!Objects.equals(echo, "bye")) {
+            System.out.println("_________________________________________________\n" + echo + "\n" +
+                    "_________________________________________________\n");
+            echo = sc.nextLine();
+        }
+        System.out.println("_________________________________________________\nBye. Hope to see you again soon!\n" +
+                "_________________________________________________\n");
+
     }
 }
