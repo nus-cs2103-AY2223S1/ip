f843ee6b96bfc56471b6e47d5ca24e7308d4bdd0
normkoh <kohweiquan1@gmail.com>
2022-08-13 15:14:25 +0800

    Implemented Add, List.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,30 @@ public class Duke {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        String[] taskArr = new String[100];
+        int taskCounter = 0;
         String input = "";
-        System.out.print("Hi I'm catBot! How can I help you nya?\n");
-        while (true) {
+        System.out.print("Hi I'm catBot!\nHow can I help you nya?\n");
+
+        while (!input.equals("bye")) {
             input = sc.nextLine();
-            if (input.equals("bye")) break;
-            System.out.print(input + "\n");
+            switch(input) {
+                case "bye":
+                    break;
+                case "list":
+                    for (int i = 0; i < taskArr.length; i++) {
+                        if (taskArr[i] != null) {
+                            System.out.println((i + 1) + ". " + taskArr[i]);
+                        } else {
+                            break;
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("Roger nya! Added " + input + ".");
+                    taskArr[taskCounter++] =  input;
+                    break;
+            }
         }
 
         System.out.print("Bye nya! Hope to see you again nya.");
