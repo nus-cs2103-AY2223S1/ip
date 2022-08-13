3d3fe7982bee58095755433dac84dae705f52735
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-13 15:46:07 +0800

    Refactor `TaskList` to use `Task`

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -6,15 +6,25 @@ import java.util.stream.IntStream;
  * A TaskList stores a list of tasks.
  */
 public class TaskList {
-    private final List<String> list = new ArrayList<>();
+    private final List<Task> list = new ArrayList<>();
 
     /**
      * Adds a task to the list.
      *
      * @param task a string value of the task to add
      */
-    public void addTask(String task) {
+    public void addTask(Task task) {
         list.add(task);
+    }
+
+    /**
+     * Returns the task at the given index.
+     *
+     * @param index the index of the task to return
+     * @return the task at the given index
+     */
+    public Task getTask(int index) {
+        return list.get(index);
     }
 
     /**
