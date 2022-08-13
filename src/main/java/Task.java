93234b6aa2894815156321b5728b342e6a28f116
teoyuqi <teo.yuqi@gmail.com>
2022-08-13 16:44:56 +0800

    Create Task object

---
+++ src/main/java/Task.java
@@ -0,0 +1,44 @@
+package main.java;
+
+public class Task {
+    /**
+     * Description of task.
+     */
+    protected String description;
+
+    /**
+     * Boolean object to determine if task is done or not.
+     */
+    protected boolean isDone;
+
+    /**
+     * Constructor method for class.
+     * @param description Description of task.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Show if task is done.
+     * @return "X" if task is done, " " if task is not done.
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    /**
+     * Setter method to mark task as done.
+     */
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Setter method to mark task as undone.
+     */
+    public void markUnDone() {
+        this.isDone = false;
+    }
+}
