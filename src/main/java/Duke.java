7b6ac580aae5aea6ff514bd9964975807880ddfb
Tjan Eng Ger, Kevin <kevintjan27@gmail.com>
2022-08-13 18:51:42 +0800

    Level 3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    private ArrayList<String> history = new ArrayList<String>();
+    private ArrayList<Task> history = new ArrayList<Task>();
     private Scanner commandInput;
     private boolean isClosed = false;
 
@@ -23,16 +23,41 @@ public class Duke {
             = "Hello! I'm Duke\n"
             + "What can I do for you?";
 
+    private class Task {
+        private String task;
+        private boolean isDone;
+
+        private Task(String task) {
+            this.task = task;
+            this.isDone = false;
+        }
+
+        private void markDone() {
+            isDone = true;
+            System.out.println(this);
+        }
+
+        private void markUndone() {
+            isDone = false;
+            System.out.println(this);
+        }
+
+        public String toString() {
+            String box = isDone ?  "[X] " : "[ ] ";
+            return box + task;
+        }
+    }
+
     private void goodbye() {
         System.out.println(this.lineBreak2);
         System.out.println("Goodbye! See you next time!\n" + this.lineBreak1);
         this.isClosed = true;
     }
 
-    private void addToHistory(String command) {
+    private void addToHistory(String task) {
         System.out.println(this.lineBreak2);
-        System.out.println("Adding to Tasks: " + command);
-        history.add(command);
+        System.out.println("Adding to Tasks: " + task);
+        history.add(new Task(task));
         System.out.println(lineBreak1);
     }
 
@@ -53,19 +78,93 @@ public class Duke {
         System.out.println(lineBreak1);
     }
 
+    private void markDone(String[] returnedArray) {
+        if (returnedArray.length == 1) {
+            System.out.println("Error to be handled properly in level 5."
+                    + "Error: Nothing to mark");
+        } else if (returnedArray.length == 2){
+            try {
+                int taskId = Integer.parseInt(returnedArray[1]) - 1;
+                if (history.size() <= taskId || taskId < 1) {
+                    System.out.println(lineBreak2);
+                    System.out.println("Hmm... That task you want to mark does not exist. "
+                            + "Use the [list] command to check what tasks are available");
+                    System.out.println(lineBreak1);
+                } else {
+                    System.out.println(lineBreak2);
+                    System.out.println("Good Job! I will mark this task as done:");
+                    history.get(taskId).markDone();
+                    System.out.println(lineBreak1);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error to be handled properly in level 5. " +
+                        "Error: Incorrect command");
+            }
+        } else {
+            System.out.println("Error to be handled properly in level 5. Error: " +
+                    "Too many arguments for mark command");
+        }
+    }
+
+    private void markUndone(String[] returnedArray) {
+        if (returnedArray.length == 1) {
+            System.out.println("Error to be handled properly in level 5. " +
+                    "Error: Nothing to mark");
+        } else if (returnedArray.length == 2){
+            try {
+                int taskId = Integer.parseInt(returnedArray[1]) - 1;
+                if (history.size() <= taskId || taskId < 1) {
+                    System.out.println(lineBreak2);
+                    System.out.println("Hmm... That task you want to unmark does not exist. "
+                            + "Use the [list] command to check what tasks are available");
+                    System.out.println(lineBreak1);
+                } else {
+                    System.out.println(lineBreak2);
+                    System.out.println("Alright, I will mark this task as undone:");
+                    history.get(taskId).markUndone();
+                    System.out.println(lineBreak1);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error to be handled properly in level 5. " +
+                        "Error: Incorrect command");
+            }
+        } else {
+            System.out.println("Error to be handled properly in level 5. " +
+                    "Error: Too many arguments for unmark command");
+        }
+    }
+
+    private void parseCommand(String command) {
+        String [] returnedArray = command.split(" ");
+        if (returnedArray.length == 0 || returnedArray[0] == null
+                || returnedArray[0] == "") {
+            System.out.println("Error to be handled properly in level 5. " +
+                    "Error: No command");
+        } else if (returnedArray[0].equals("mark")) {
+            markDone(returnedArray);
+        } else if (returnedArray[0].equals("unmark")) {
+            markUndone(returnedArray);
+        } else {
+            this.addToHistory(command);
+        }
+    }
+
     private void inputCommand(String command) {
         if (command.equals("bye")) {
             this.goodbye();
         } else if (command.equals("list")) {
             this.listOut();
         } else {
-            this.addToHistory(command);
+            this.parseCommand(command);
         }
     }
 
     public static void main(String[] args) {
         Duke duke = new Duke();
-        System.out.println(duke.logo + "\n" + duke.greeting + "\n" + duke.lineBreak1);
+        System.out.println(duke.logo + "\n"
+                + duke.greeting
+                + "\n"
+                + duke.lineBreak1);
         duke.commandInput = new Scanner(System.in);
         while (duke.commandInput.hasNextLine()) {
             String command = duke.commandInput.nextLine();
