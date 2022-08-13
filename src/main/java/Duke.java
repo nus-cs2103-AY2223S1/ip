c5865cb3b05f0726be252de67b9f6bb2e2208d38
Samsation <samsamsailorman@gmail.com>
2022-08-13 20:05:09 +0800

    Implement Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,19 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        final String LOGO = "\t ____        _        \n"
-                + "\t|  _ \\ _   _| | _____ \n"
-                + "\t| | | | | | | |/ / _ \\\n"
-                + "\t| |_| | |_| |   <  __/\n"
-                + "\t|____/ \\__,_|_|\\_\\___|\n";
-        final String LINE = "\t____________________________________________________________";
+    private static final String LOGO = "\t ____        _        \n"
+            + "\t|  _ \\ _   _| | _____ \n"
+            + "\t| | | | | | | |/ / _ \\\n"
+            + "\t| |_| | |_| |   <  __/\n"
+            + "\t|____/ \\__,_|_|\\_\\___|\n";
+    private static final String LINE = "\t____________________________________________________________";
 
-        Scanner sc = new Scanner(System.in);
+    private static void run() {
+        ArrayList<String> storage = new ArrayList<>();
+        boolean isExited = false;
         String command = "";
+        Scanner sc = new Scanner(System.in);
 
         System.out.println(LOGO
                 + LINE
@@ -19,18 +22,35 @@ public class Duke {
                 + "\tWhat can I do for you?\n"
                 + LINE);
 
-        while (true) {
+        while (!isExited) {
             command = sc.nextLine();
-            if (command.equals("bye")) {
-                System.out.println("\tBye. Hope to see you again soon!\n"
-                        + LINE);
-                break;
+            switch (command) {
+                case "bye":
+                    isExited = true;
+                    System.out.println(LINE
+                            + "\n\tBye. Hope to see you again soon!\n"
+                            + LINE);
+                    break;
+                case "list":
+                    System.out.println(LINE);
+                    for (int i = 0; i < storage.size(); i++) {
+                        System.out.printf("\t%d%s\n", i, ". " + storage.get(i));
+                    }
+                    System.out.println(LINE);
+                    break;
+                default:
+                    storage.add(command);
+                    System.out.println(LINE
+                            + "\n\t"
+                            + "added: "
+                            + command
+                            + "\n"
+                            + LINE);
             }
-            System.out.println(LINE
-                    + "\n\t"
-                    + command
-                    + "\n"
-                    + LINE);
         }
+    }
+
+    public static void main(String[] args) {
+        Duke.run();
     }
 }
