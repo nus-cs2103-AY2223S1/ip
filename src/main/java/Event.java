c4c1b12af011cf7ed5708eca3490f6b40dbbbd8f
Jin Wei <seetohjinwei@gmail.com>
2022-08-13 12:40:07 +0800

    Add error handling for Event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -25,11 +25,18 @@ public class Event extends Task {
      *
      * @return Event object with the given user input.
      */
-    public static Event create(String input) {
+    public static Event create(String input) throws DukeException {
+        if (input.length() < 7) {
+            // If user typed "event" or "event " or any other input less than 7 characters, the description will be empty.
+            throw new DukeException("Description of Event cannot be empty!");
+        }
         // Obtain the description and date from the user input.
         String descAndDate = input.substring(6);
-        // Split the input into description and date with the separator of "\by".
+        // Split the input into description and date with the separator of "/at".
         String[] split = descAndDate.split(" /at ");
+        if (split.length != 2) {
+            throw new DukeException("Wrong format for Event!\n    Should be 'event <description> /at <date>'.");
+        }
         String description = split[0];
         String date = split[1];
         return new Event(description, date);
