e1067578e77dbe8279aed96a8307ed3481f74af4
Guo Wei <wguo8698@gmail.com>
2022-08-13 16:35:51 +0800

    add greeting, echo, exit

--- src/main/java/Deku.java
+++ src/main/java/Deku.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+
 public class Deku {
+    private final String SEPARATOR = "____________________";
+    private void introduction() {
+        String logo ="    ____  ________ ____  __\n" +
+                "   / __ \\/ ____/ //_/ / / /\n" +
+                "  / / / / __/ / ,< / / / / \n" +
+                " / /_/ / /___/ /| / /_/ /  \n" +
+                "/_____/_____/_/ |_\\____/   \n";
+        System.out.println(logo +
+                "\nHello I'm DEKU\nHow may I help you today?\n" +
+                SEPARATOR);
+    }
+    /*
+    * Method to start the chat-bot
+    */
+    public void start() {
+        this.introduction();
+        boolean active = true;
+        while (active) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                active = false;
+                System.out.println(this.end());
+                continue;
+            }
+            System.out.println(this.parseReply(userInput));
+        }
+    }
+    private String end() {
+        return this.parseReply("Bye! Until next time!");
+    }
+    private String parseReply(String input) {
+        return SEPARATOR + "\n" + input + "\n" + SEPARATOR;
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Deku dekuBot = new Deku();
+        dekuBot.start();
     }
 }
