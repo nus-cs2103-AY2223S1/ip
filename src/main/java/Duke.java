907bae1ce5a52c68456324fb3dcbac2bbf02a7ce
gnahzeus <zhangyue1689@gmail.com>
2022-08-13 16:14:50 +0800

    implement greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,46 @@
+import java.util.Scanner;
 public class Duke {
+    private boolean hasExited = false;
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke zeus = new Duke();
+        zeus.chat();
+    }
+
+    public void chat() {
+        greetUser();
+        Scanner sc = new Scanner(System.in);
+        while(!hasExited) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                hasExited = true;
+            } else {
+                echo(input);
+            }
+        }
+        exitMessage();
+    }
+
+    public void greetUser() {
+        generateLine();
+        System.out.println("\t Hello! I'm Zeus");
+        System.out.println("\t What can I do for you?");
+        generateLine();
+    }
+
+    public void exitMessage() {
+        generateLine();
+        System.out.println("\t Bye. Hope to see you again soon!");
+        generateLine();
+    }
+    public void echo(String input) {
+        generateLine();
+        System.out.println("\t " + input);
+        generateLine();
+
+    }
+
+    public void generateLine() {
+        System.out.println("\t____________________________________________________________");
+
     }
 }
