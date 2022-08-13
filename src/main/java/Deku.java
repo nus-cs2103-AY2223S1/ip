415ddbd55f8b4394fb8cede473213870d9c210c2
Guo Wei <wguo8698@gmail.com>
2022-08-13 16:22:03 +0800

    renamed default file

--- src/main/java/Deku.java
+++ src/main/java/Deku.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Deku {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
