cf67ac650919b2a09c5da1d93971349ce1fbed9b
Ssatu <69383469+Ssatu@users.noreply.github.com>
2022-08-13 17:08:06 +0800

    Level 2 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,15 +22,51 @@ public class Duke {
         printLine();
     }
 
+    /**
+     * This method adds a task to the given task-list.
+     * @param input The task to be added.
+     * @param tasks The task-list.
+     * @param numTasks The number of tasks currently.
+     */
+    public static void addTask(String input, String[] tasks, int numTasks) {
+        tasks[numTasks] = input;
+        say("added: " + input);
+    }
+
+    /**
+     * This method lists out the current tasks.
+     * @param tasks The task-list.
+     * @param numTasks The number of tasks currently.
+     */
+    public static void listTasks(String[] tasks, int numTasks) {
+        String list = "";
+        for(int i = 0; i < numTasks; i++) {
+            list +=  (i+1) + ". " + tasks[i] + "\n";
+        }
+        say(list);
+    }
+
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int numTasks = 0;
 
-        say("Hello! I'm Pawl\n What can I do for you?");
+        say("Hello! I'm Pawl\nWhat can I do for you?");
 
-        String first = scn.next();
-        while (!first.equals("bye")){
-            say(first);
-            first = scn.next();
+        String input = scn.nextLine();
+        while (!input.equals("bye")) {
+
+            //Handle listing of tasks
+            if(input.equals("list")) {
+                listTasks(tasks, numTasks);
+
+            //Else, add task to list
+            } else {
+                addTask(input, tasks, numTasks);
+                numTasks++;
+            }
+            input = scn.nextLine();
         }
 
         say("Bye. Hope to see you again soon!");
