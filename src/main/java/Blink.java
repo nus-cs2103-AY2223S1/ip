5aea8b4f2417ad355f56f483220f0caec34ff233
maxng17 <maxngjl@gmail.com>
2022-08-14 03:32:01 +0800

    added Add, List

--- src/main/java/Blink.java
+++ src/main/java/Blink.java
@@ -1,11 +1,14 @@
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Blink {
 
-    public static final String SPACING = "---------------------------";
+    private static final String SPACING = "---------------------------";
+    private String[] store = new String[100];
+    private int count = 0;
 
     public void welcome() {
-        System.out.println("Hello! Blink here\n" +"What can I do for you today?");
+        System.out.println("Hello! Blink here\n" + "What can I do for you today?");
     }
 
     public void goodbye() {
@@ -19,13 +22,24 @@ public class Blink {
 
         while(sc.hasNext()) {
             String input = sc.nextLine();
+            if (input.isBlank()) {
+                continue;
+            }
             if (input.equals("bye")) {
                 this.goodbye();
                 System.out.println(Blink.SPACING);
                 break;
+            } else if (input.equals("list")) {
+                for(int x = 0; x < this.count; x++) {
+                    System.out.println(x+1 + ". " + this.store[x]);
+                }
+                System.out.println(Blink.SPACING);
+            } else {
+                this.store[count] = input;
+                this.count++;
+                System.out.println("added: " + input);
+                System.out.println(Blink.SPACING);
             }
-            System.out.println(input);
-            System.out.println(Blink.SPACING);
         }
     }
     public static void main(String[] args) {
