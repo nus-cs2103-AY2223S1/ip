0c43863302b56ae2d141d4df8837cea807c28517
cowlinn <2017.colin.toh@ejc.edu.sg>
2022-08-14 00:29:33 +0800

    Update Storage.java
    Updated save to create a directory + file + other error handling

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -8,11 +8,13 @@ import java.util.ArrayList;
    Methods are mostly static
  */
 public class Storage {
-    private static final String FILENAME = "SavedData/SaveData.txt";
+
+    private static final String DIRECTORY_NAME = "SavedData";
+    private static final String FILE_NAME = "SavedData/SaveData.txt";
 
     public static void Save(List<Task> saveItems) {
         try {
-            FileOutputStream fos = new FileOutputStream(FILENAME);
+            FileOutputStream fos = new FileOutputStream(FILE_NAME);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(saveItems);
             oos.close();
@@ -25,20 +27,34 @@ public class Storage {
     }
 
     public static List<Task> ReadItems() {
+        //1) Create file
+        try {
+            File directory = new File(DIRECTORY_NAME);
+            directory.mkdir();
+            File f = new File(FILE_NAME);
+            boolean createNewSaveFile = f.createNewFile();
+            if (createNewSaveFile) {
+                System.out.println("File doesn't exist, creating one");
+            }
+        } catch (IOException exp) {
+            System.out.println("IOException occurred");
+            exp.printStackTrace();
+
+        }
+
         try {
 
-            FileInputStream fis = new FileInputStream(FILENAME);
+            FileInputStream fis = new FileInputStream(FILE_NAME);
             ObjectInputStream ois = new ObjectInputStream(fis);
             ArrayList<Task> readItems = (ArrayList<Task>) ois.readObject();
             ois.close();
             return readItems;
 
 
-
         } catch (FileNotFoundException e) {
             return new ArrayList<>(); //empty Task list for initial initialization
         } catch (IOException e) {
-            System.out.println("Cannot Initialize Stream");
+            return new ArrayList<>();
         } catch (ClassNotFoundException e) {
             System.out.println("Class not found");
             e.printStackTrace();
