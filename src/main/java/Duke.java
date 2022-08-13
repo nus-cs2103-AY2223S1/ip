1f73261df6462e267cdf43d42fc497d6dea7ef82
Darren12345677 <darrencookies100@gmail.com>
2022-08-13 11:39:20 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Duke {
+    static List<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Hello I'm Duke\n" + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
@@ -10,8 +12,15 @@ public class Duke {
             String command = sc.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    int index = i + 1;
+                    System.out.println(index + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(command);
+                System.out.println("added: " + command);
             }
-            System.out.println(command);
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
