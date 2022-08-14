4daa7460d8a045e5bd3afb0edfaf1400328316df
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2022-08-14 12:11:37 +0800

    Complete Level 2
    Used array to store all the user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,16 +7,26 @@ public class Duke {
         System.out.printf("Hey there! I'm Bob\nWhat can I do for you?\n");
         System.out.println("_____________________________________________________________");
 
-        while (sc.hasNext()) {
-            String input = sc.next();
+        String[] toDoList = new String[100];
+        int toDoListCount  = 0;
 
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else {
-                System.out.println(input);
+            } else if (input.equalsIgnoreCase("list")){
                 System.out.println("_____________________________________________________________");
-                System.out.println();
+                for (int i = 0; i < toDoListCount; i ++) {
+                    System.out.println(i+1 + ". " + toDoList[i]);
+                }
+                System.out.println("_____________________________________________________________");
+            } else {
+                toDoList[toDoListCount] = input;
+                System.out.println("_____________________________________________________________");
+                System.out.println("added : " + input);
+                System.out.println("_____________________________________________________________");
+                toDoListCount++;
             }
         }
 
