eeb9e8abde9868522f3eaa8957daf4988191df75
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-08-14 15:30:35 +0800

    working version of Duke that satisfies Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+  public static void inputAction(String input) {
+    System.out.println(input);
+  }
+
+  public static void goodbye() {
+    System.out.println("Goodbye.");
+  }
+
+  public static void main(String[] args) {
+    // Welcome message
+    String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    System.out.println("Hello from\n" + logo);
+
+    /*
+     * Take in user input. As long as input is not bye, calls the function
+     * to act on the input. If input is bye, then calls the goodbye function.
+     * Accepts only Strings.
+     * TODO: What if someone passes in a non-string?
+     */
+    Scanner sc = new Scanner(System.in);
+
+    String input;
+    while (!(input = sc.nextLine()).equals("Bye")) {
+      inputAction(input);
     }
+
+    goodbye();
+    System.exit(0);
+  }
 }
