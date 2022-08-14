b4ece821798ee275c0e49356148e19ab735b2ef8
Donovan <don9617@hotmail.com>
2022-08-14 17:10:04 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,24 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] listOfItems = new String[100];
+        int indexOfNextItem = 0;
         System.out.println("Hello! I'm Don\nHow may I help you?");
         while (true) {
-            String response = sc.next();
+            String response = sc.nextLine();
             if (response.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(response);
+            else if (response.equals("list")) {
+                for (int index = 0; index < indexOfNextItem; index++) {
+                    System.out.println(index+1 + ". " + listOfItems[index]);
+                }
+            }
+            else {
+                listOfItems[indexOfNextItem++] = response;
+                System.out.println("added: " + response);
+            }
         }
     }
 }
