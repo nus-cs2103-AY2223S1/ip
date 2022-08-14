1c59b5af25ee5dfe2c38f018355be5c8b1d34ece
Hikoya <hikoyanamikaze@gmail.com>
2022-08-14 18:51:31 +0800

    Add a new Task.java class
    This Task class is used to track status of a particular Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+}
