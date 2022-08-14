e6e3c4b1bf4c71309b04cfcf1abec3015c51fbab
Lester Ong <lesterong776@gmail.com>
2022-08-15 00:38:47 +0800

    Add ability to mark and unmark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 public class Duke {
     private static String[] list = new String[100];
+    private static boolean[] isComplete = new boolean[100];
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello! I'm Duke.\nWhat can I do for you?");
         System.out.print(">> ");
-        String command = input.next();
+        String command = input.nextLine();
         while (true) {
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
@@ -14,10 +15,38 @@ public class Duke {
             } else if (command.equals("list")) {
                 for (int pos = 0; pos < 100; pos++) {
                     if (list[pos] == null) break;
-                    System.out.println(pos + 1 + ". " + list[pos]);
+                    System.out.println(
+                            pos + 1 + ". " + (isComplete[pos] ? "[X] " : "[ ] ") + list[pos]
+                    );
                 }
                 System.out.print(">> ");
-                command = input.next();
+                command = input.nextLine();
+            } else if (command.startsWith("mark")) {
+                int pos = Integer.parseInt(command.split(" ")[1]);
+                if (list[pos - 1] == null) {
+                    System.out.println("No such task.");
+                } else {
+                    isComplete[pos - 1] = true;
+                    System.out.println(
+                            "Nice! I've marked this task as done:\n" +
+                                    "[X] " + list[pos - 1]
+                    );
+                }
+                System.out.print(">> ");
+                command = input.nextLine();
+            } else if (command.startsWith("unmark")) {
+                int pos = Integer.parseInt(command.split(" ")[1]);
+                if (list[pos - 1] == null) {
+                    System.out.println("No such task.");
+                } else {
+                    isComplete[pos - 1] = false;
+                    System.out.println(
+                            "Alright! I've marked this task as not done:\n" +
+                                    "[ ] " + list[pos - 1]
+                    );
+                }
+                System.out.print(">> ");
+                command = input.nextLine();
             } else {
                 System.out.println("Added: " + command);
                 for (int pos = 0; pos < 100; pos++) {
@@ -27,7 +56,7 @@ public class Duke {
                     }
                 }
                 System.out.print(">> ");
-                command = input.next();
+                command = input.nextLine();
             }
         }
     }
