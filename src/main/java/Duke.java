f6fd68b1ad5c5525c04d446dfeb23250175002e8
Eugene-Ong-W-X <e0725338@u.nus.edu>
2022-08-15 01:07:51 +0800

    Level-1 Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static void speak(String message) {
+        System.out.println("============================================================\n");
+        System.out.println(message);
+        System.out.println("\n============================================================");
+    }
+
+    private static void greet() {
+        speak(" Greetings! My Name is Alfred ^_^\n How may I be of service today?");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+
+        String cmd = "";
+        while (!cmd.equals("bye")) {
+            cmd = scanner.nextLine().trim();
+            if (!cmd.equals("bye")) {
+                speak(" " + cmd);
+            } else {
+                speak(" Farewell!");
+            }
+        }
     }
 }
