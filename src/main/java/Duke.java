ca4b646e64a93a011f587f976c9c70e545c060fd
Lu Yiting <e0774189@u.nus.edu>
2022-08-14 10:22:05 +0800

    Implemented Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] all = new String[100];
+    private static Task[] all = new Task[100];
     private static int count = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
@@ -21,29 +19,87 @@ public class Duke {
         while (true) {
             s = sc.nextLine();
             if (s.equals("bye")) {
-                Bye();
+                bye();
                 break;
             } else if (s.equals("list")) {
-                DisplayList();
+                displayList();
+            } else if (startWith(s, "mark")) {
+                int index = Integer.parseInt(extractIndex(s, "mark"));
+                //System.out.println(index);
+                all[index - 1].markAsDone();
+            } else if (startWith(s, "unmark")) {
+                int index = Integer.parseInt(extractIndex(s, "unmark"));
+                //System.out.println(index);
+                all[index - 1].markAsUndone();
             } else {
-                Store(s);
+                store(s);
             }
         }
     }
 
-    private static void Bye() {
+    private static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    private static  void DisplayList() {
+    private static  void displayList() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < count; i++) {
-            System.out.printf("%s. %s\n", i + 1, all[i]);
+            System.out.printf("%s. %s\n", i + 1, all[i].toString());
         }
     }
 
-    private static void Store(String s) {
-        all[count] = s;
+    private static void store(String s) {
+        all[count] = new Task(s);
+        System.out.printf("added: %s\n", s);
         count += 1;
-        System.out.println(s);
+    }
+
+    private static boolean startWith(String s, String target) {
+        for (int i = 0; i < target.length(); i++) {
+            if (s.charAt(i) != target.charAt(i)) return false;
+        }
+
+        try {
+            int val = Integer.parseInt(s.substring(target.length() + 1));
+            return true;
+        } catch (NumberFormatException e) {
+            //System.out.println("Input String cannot be parsed to Integer.");
+            return false;
+        }
+
+    }
+
+    private static String extractIndex(String s, String target) {
+        return s.substring(target.length() + 1);
+    }
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public String toString() {
+            return String.format("[%s] %s", getStatusIcon(), description);
+        }
+
+        public void markAsDone() {
+            isDone = true;
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(this.toString());
+        }
+
+        public void markAsUndone() {
+            isDone = false;
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println(this.toString());
+        }
     }
 }
