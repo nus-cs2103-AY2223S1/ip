62da9ba542c3581a267e9d946fa57e75c911b6a3
Donovan <don9617@hotmail.com>
2022-08-14 18:19:44 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] listOfItems = new String[100];
-        int indexOfNextItem = 0;
+        Task[] listOfTasks = new Task[100];
+        int indexOfNextTask = 0;
         System.out.println("Hello! I'm Don\nHow may I help you?");
         while (true) {
             String response = sc.nextLine();
@@ -13,12 +13,23 @@ public class Duke {
                 break;
             }
             else if (response.equals("list")) {
-                for (int index = 0; index < indexOfNextItem; index++) {
-                    System.out.println(index+1 + ". " + listOfItems[index]);
+                System.out.println("Here are the tasks in your list:");
+                for (int index = 0; index < indexOfNextTask; index++) {
+                    System.out.println(index+1 + ".["+ listOfTasks[index].getStatusIcon()+"] " + listOfTasks[index].getDescription());
                 }
             }
+            else if (response.length() > 3 && response.substring(0, 4).equals("mark")) {
+                int taskIndex = Integer.parseInt(response.substring(5, response.length())) - 1;
+                listOfTasks[taskIndex].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + "[" + listOfTasks[taskIndex].getStatusIcon() + "] " + listOfTasks[taskIndex].getDescription());
+            }
+            else if (response.length() > 5 && response.substring(0, 6).equals("unmark")) {
+                int taskIndex = Integer.parseInt(response.substring(7, response.length())) - 1;
+                listOfTasks[taskIndex].markAsNotDone();
+                System.out.println("Ok, I've marked this task as not done yet:\n" + "[" + listOfTasks[taskIndex].getStatusIcon() + "] " + listOfTasks[taskIndex].getDescription());
+            }
             else {
-                listOfItems[indexOfNextItem++] = response;
+                listOfTasks[indexOfNextTask++] = new Task(response);
                 System.out.println("added: " + response);
             }
         }
