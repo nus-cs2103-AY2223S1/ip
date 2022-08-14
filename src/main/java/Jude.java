612deeb061246d5e9af153e61ca3c4c421a1481c
Tan Chee Heng <ch.tan020@gmail.com>
2022-08-15 02:30:59 +0800

    Level 5: Handle errors due to invalid input

--- src/main/java/Jude.java
+++ src/main/java/Jude.java
@@ -30,6 +30,13 @@ import java.util.Scanner;
 public class Jude {
     private static List<Task> tasks = new ArrayList<>();
 
+    // checks index to ensure index not out of bounds
+    private static void checkIndex(int index) {
+        if (index < 0 || index >= tasks.size()) {
+            throw new IllegalArgumentException("Invalid index");
+        }
+    }
+
     /**
      * Runs the task tracker.
      *
@@ -47,55 +54,91 @@ public class Jude {
             System.out.print("> ");
             String str = sc.nextLine();
             String[] tokens = str.split(" ", 2);
-            if (tokens[0].equals("todo") || tokens[0].equals("deadline") || tokens[0].equals("event")) {
-                Task taskAdded = null;
-                if (tokens[0].equals("todo")) {
-                    String description = tokens[1];
-                    taskAdded = new Todo(description, false);
-                } else if (tokens[0].equals("deadline")) {
-                    String remText = tokens[1];
-                    String[] remTextTokens = remText.split(" /by ", 2);
-                    String description = remTextTokens[0];
-                    String deadline = remTextTokens[1];
-                    taskAdded = new Deadline(description, false, deadline);
-                } else if (tokens[0].equals("event")) {
-                    String remText = tokens[1];
-                    String[] remTextTokens = remText.split(" /at ", 2);
-                    String description = remTextTokens[0];
-                    String when = remTextTokens[1];
-                    taskAdded = new Event(description, false, when);
+            try {
+                if (tokens[0].equals("todo") || tokens[0].equals("deadline") ||
+                        tokens[0].equals("event")) {
+                    Task taskAdded = null;
+                    if (tokens.length == 1 || tokens[1].isBlank()) {
+                        throw new IllegalArgumentException(
+                                String.format("Description of %s cannot be empty.", tokens[0]));
+                    } else if (tokens[0].equals("todo")) {
+                        String description = tokens[1];
+                        taskAdded = new Todo(description, false);
+                    } else if (tokens[0].equals("deadline")) {
+                        String remText = tokens[1];
+                        String[] remTextTokens = "  ".concat(remText).split(" /by ", 2);
+                        if (remTextTokens.length == 2) {
+                            String description = remTextTokens[0].trim();
+                            String deadline = remTextTokens[1].trim();
+                            if (description.isBlank() || description.isEmpty()) {
+                                throw new IllegalArgumentException(
+                                        "Description of deadline task cannot be empty.");
+                            } else if (deadline.isBlank()) {
+                                throw new IllegalArgumentException("A deadline task must have a " +
+                                        "deadline.");
+                            }
+                            taskAdded = new Deadline(description, false, deadline);
+                        } else {
+                            throw new IllegalArgumentException("A deadline task must have a " +
+                                    "deadline.");
+                        }
+                    } else if (tokens[0].equals("event")) {
+                        String remText = tokens[1];
+                        String[] remTextTokens = "  ".concat(remText).split(" /at ", 2);
+                        if (remTextTokens.length == 2) {
+                            String description = remTextTokens[0].strip();
+                            String when = remTextTokens[1].strip();
+                            if (description.isBlank()) {
+                                throw new IllegalArgumentException(
+                                        "Description of event task cannot be empty.");
+                            } else if (when.isBlank()) {
+                                throw new IllegalArgumentException("An event task must have a " +
+                                        "time at which the event takes place.");
+                            }
+                            taskAdded = new Event(description, false, when);
+                        } else {
+                            throw new IllegalArgumentException("An event task must have a time at " +
+                                    "which the event takes place.");
+                        }
+                    }
+
+                    if (taskAdded != null) {
+                        tasks.add(taskAdded);
+                        System.out.printf("The following %s task has been added:\n  ", tokens[0]);
+                        System.out.println(taskAdded);
+                        System.out.printf("The task list now contains %d task(s).\n", tasks.size());
+                    }
+                } else if (tokens[0].equals("mark")) {
+                    int index = Integer.parseInt(tokens[1]) - 1;
+                    checkIndex(index);
+                    Task task = tasks.get(index);
+
+                    // Solution below adapted from
+                    // https://nus-cs2103-ay2223s1.github.io/website/schedule/week2/project.html
+                    task.markAsDone();
+
+                    System.out.println("The following task has been marked as done");
+                    System.out.println(task);
+                } else if (tokens[0].equals("unmark")) {
+                    int index = Integer.parseInt(tokens[1]) - 1;
+                    checkIndex(index);
+                    Task task = tasks.get(index);
+                    task.markAsUndone();
+                    System.out.println("The following task has been marked as undone");
+                    System.out.println(task);
+                } else if (str.equals("list")) {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        Task task = tasks.get(i);
+                        System.out.printf("%d.%s\n", i + 1, task);
+                    }
+                } else if (str.equals("bye")) {
+                    System.out.println("Goodbye! Have a nice day!");
+                    break;
+                } else {
+                    System.out.println("Sorry, I don't understand what this means!");
                 }
-
-                tasks.add(taskAdded);
-                System.out.printf("The following %s task has been added:\n  ", tokens[0]);
-                System.out.println(taskAdded);
-                System.out.printf("The task list now contains %d task(s).\n", tasks.size());
-            } else if (tokens[0].equals("mark")) {
-                int index = Integer.parseInt(tokens[1])-1;
-                Task task = tasks.get(index);
-
-                // Solution below adapted from
-                // https://nus-cs2103-ay2223s1.github.io/website/schedule/week2/project.html
-                task.markAsDone();
-
-                System.out.println("The following task has been marked as done");
-                System.out.println(task);
-            } else if (tokens[0].equals("unmark")) {
-                int index = Integer.parseInt(tokens[1])-1;
-                Task task = tasks.get(index);
-                task.markAsUndone();
-                System.out.println("The following task has been marked as undone");
-                System.out.println(task);
-            } else if (str.equals("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    Task task = tasks.get(i);
-                    System.out.printf("%d.%s\n", i+1, task);
-                }
-            } else if (str.equals("bye")) {
-                System.out.println("Goodbye! Have a nice day!");
-                break;
-            } else {
-                System.out.println("Sorry, I don't understand what this means!");
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
             }
         }
     }
