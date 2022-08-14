c04cad9bce63d8b7b644b47ed17c9284d0b92774
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-08-14 22:54:30 +0800

    Implement a skeletal version of Duke that starts by greeting the user and simply echos the commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String line = "    ----------------------------------------";
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("    Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println("    Hello! I'm Duke\n    What can I do for you?");
+        System.out.println(line);
+
+        String current = sc.nextLine();
+        while (!current.equals("bye")) {
+            System.out.println(line);
+            System.out.println("    " + current);
+            System.out.println(line);
+            current = sc.nextLine();
+        }
+
+        System.out.println(line);
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
