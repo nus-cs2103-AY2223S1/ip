beac72131fa347487922a427286a173a2815b135
parth-io <67057645+parth-io@users.noreply.github.com>
2022-08-15 03:16:43 +0800

    Add Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,9 +7,9 @@ public class Duke {
     private static final String indentedLine = "     " + line;
     private static final String initialMessage = messageWithIndentedLines("\n      Hello! I'm Duke\n      What can I do for you?\n");
     private static final String byeMessage = messageWithIndentedLines("\n      Bye. This doesn't have to be the end!\n");
-    private static final ArrayList<String> userText = new ArrayList<>();
+    private static final ArrayList<Task> userTasks = new ArrayList<>();
 
-    public static String messageWithIndentedLines(String message) {
+    private static String messageWithIndentedLines(String message) {
         return indentedLine + message + indentedLine;
     }
 
@@ -24,14 +24,26 @@ public class Duke {
                 break;
             } else if (userInput.equals("list")) {
                 // Idea below of iterating with indices in streams adapted from https://stackoverflow.com/a/42616742
-                HashMap<Integer, String> mappedIndexToUserText = userText.stream()
-                        .collect(HashMap::new, (hashMap, streamElement) -> hashMap.put(hashMap.size(), streamElement), HashMap::putAll);
+                HashMap<Integer, Task> mappedIndexToUserText = userTasks.stream()
+                        .collect(HashMap::new, (hashMap, streamElement) -> hashMap.put(hashMap.size() + 1, streamElement), HashMap::putAll);
                 StringBuilder listOfUserText = mappedIndexToUserText.entrySet().stream()
-                        .reduce(new StringBuilder(), (stringToBuild, currentEntry) -> stringToBuild.append("\n      ").append(currentEntry.getKey()).append(". ").append(currentEntry.getValue()), StringBuilder::append);
+                        .reduce(new StringBuilder(), (stringToBuild, currentEntry) -> stringToBuild.append("\n      ").append(currentEntry.getKey()).append(".").append(currentEntry.getValue().toString()), StringBuilder::append);
                 System.out.println(messageWithIndentedLines(listOfUserText.toString() + "\n"));
+            } else if (userInput.startsWith("mark")) {
+                int index = Character.getNumericValue(userInput.charAt(5)) - 1;
+                Task taskToMarkDone = userTasks.get(index);
+                taskToMarkDone.setTaskAsDone();
+                String toPrint = "\n     Nice! I've marked this task as done:\n       " + taskToMarkDone + "\n";
+                System.out.println(messageWithIndentedLines(toPrint));
+            } else if (userInput.startsWith("unmark")) {
+                int index = Character.getNumericValue(userInput.charAt(7)) - 1;
+                Task taskToMarkDone = userTasks.get(index);
+                taskToMarkDone.setTaskAsNotDone();
+                String toPrint = "\n     OK, I've marked this task as not done yet:\n       "  + taskToMarkDone + "\n";
+                System.out.println(messageWithIndentedLines(toPrint));
             } else {
                 System.out.println(messageWithIndentedLines("\n      added: " + userInput + "\n"));
-                userText.add(userInput);
+                userTasks.add(new Task(userInput));
             }
             userInput = scanner.nextLine();
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    // original access modifier was protected
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
+    }
+
+    public void setTaskAsDone() {
+        this.isDone = true;
+    }
+
+    public void setTaskAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
