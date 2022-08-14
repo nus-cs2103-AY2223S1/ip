58b70e7d14400a0da33898ea1697283d2ac35dbf
KeithPJX <e0684532@u.nus.edu>
2022-08-14 10:39:57 -0700

    Add mark and unmark function

--- src/main/java/Gibson.java
+++ src/main/java/Gibson.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Gibson {
     public static void main(String[] args) {
@@ -22,11 +24,13 @@ public class Gibson {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
+            // BYE
             if (input.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Bye. Hope to see you soon!");
                 System.out.println(line);
                 break;
+            // LIST
             } else if (input.equals("list")) {
                 int count = 1;
                 System.out.println(line);
@@ -35,12 +39,42 @@ public class Gibson {
                     count++;
                 }
                 System.out.println(line);
+            // MARK
+            } else if (Pattern.matches("mark [0-9]+", input)) {
+                int number = getTrailingInt(input) - 1;
+                list.get(number).mark();
+                System.out.println(line);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(list.get(number).toString());
+                System.out.println(line);
+            }
+            // UNMARK
+            else if (Pattern.matches("unmark [0-9]+", input)) {
+                int number = getTrailingInt(input) - 1;
+                list.get(number).unmark();
+                System.out.println(line);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(list.get(number).toString());
+                System.out.println(line);
+            // NEW TASK
             } else {
                 list.add(new Task(input));
                 System.out.println(line);
                 System.out.println("added: " + input);
                 System.out.println(line);
             }
+        }
+    }
+
+    // Gets trailing integers from a String
+    // Throws error if no trailing integers
+    private static int getTrailingInt(String s) {
+        Pattern pattern = Pattern.compile("[^0-9]+([0-9]+)$");
+        Matcher matcher = pattern.matcher(s);
+        if (matcher.find()) {
+            return Integer.parseInt(matcher.group(1));
+        } else {
+            throw new IllegalArgumentException("String is invalid");
         }
     }
 }
