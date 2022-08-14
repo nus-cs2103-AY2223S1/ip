4f97bdeee0759e461c7dfa1a71c519a09939edf1
ryanguai <e0694442@u.nus.edu>
2022-08-14 22:33:55 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Hello! I'm Snoopy\n");
+        System.out.println("What can I do for you?\n");
+        String response = myObj.nextLine();
+        while (!response.equals("bye")) {
+            System.out.println(response);
+            response = myObj.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
