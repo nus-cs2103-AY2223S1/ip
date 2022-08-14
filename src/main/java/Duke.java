90f2845026c9534894b33ffc79f60eda6b4340c6
HoJunHao2000 <kennethhojunhao@gmail.com>
2022-08-14 17:32:25 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,55 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    private Scanner sc;
+
+    private void run() {
+        intro();
+        this.sc = new Scanner(System.in);
+        boolean end = false;
+        while (!end) {
+            String line = this.sc.nextLine();
+            if (line.equals("bye")) {
+                end = true;
+            } else {
+                printLine();
+                print(line);
+                printLine();
+            }
+        }
+        this.sc.close();
+        exit();
+    }
+
+    private void printLine() {
+        System.out.println("\t____________________________________________________________");
+    }
+
+    private void print(String s) {
+        System.out.println("\t " + s);
+    }
+
+    private void intro() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printLine();
+        print("Hello! I'm Duke");
+        print("What can I do for you?");
+        printLine();
+    }
+
+    private void exit() {
+        printLine();
+        print("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void main(String[] args) {
+        Duke d = new Duke();
+        d.run();
     }
 }
