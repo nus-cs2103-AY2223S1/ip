0f7e8a0b9cc03008fcdaf71a93b66f5b850debaa
HoJunHao2000 <kennethhojunhao@gmail.com>
2022-08-14 17:42:03 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
     private Scanner sc;
-    private ArrayList<String> l;
+    private ArrayList<Task> l;
 
     public Duke() {
         this.l = new ArrayList<>();
@@ -19,6 +19,12 @@ public class Duke {
                 end = true;
             } else if (line.equals("list")) {
                 printList();
+            } else if (line.startsWith("mark")) {
+                int i = Integer.parseInt(line.replace("mark ", ""));
+                mark(i);
+            } else if (line.startsWith("unmark")) {
+                int i = Integer.parseInt(line.replace("unmark ", ""));
+                unmark(i);
             } else {
                 addList(line);
             }
@@ -56,6 +62,7 @@ public class Duke {
 
     private void printList() {
         printLine();
+        print("Here are the tasks in your list:");
         for (int i = 0; i < l.size(); i++) {
             print(String.format("%d. %s", i + 1, this.l.get(i)));
         }
@@ -63,9 +70,27 @@ public class Duke {
     }
 
     private void addList(String message) {
-        this.l.add(message);
+        this.l.add(new Task(message));
         printLine();
         print("added: " + message);
+        printLine();
+    }
+
+    private void mark(int i) {
+        Task t = this.l.get(i - 1);
+        t.markDone();
+        printLine();
+        print("Nice! I've marked this task as done:");
+        print("  " + t.toString());
+        printLine();
+    }
+
+    private void unmark(int i) {
+        Task t = this.l.get(i - 1);
+        t.unmarkDone();
+        printLine();
+        print("OK, I've marked this task as not done yet:");
+        print("  " + t.toString());
         printLine();
     }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
