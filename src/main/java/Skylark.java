a01269456bcbb97a6582bff44b221161dd4f5718
Hikoya <hikoyanamikaze@gmail.com>
2022-08-14 19:13:42 +0800

    Update Task.java and add Done and Undone

--- src/main/java/Skylark.java
+++ src/main/java/Skylark.java
@@ -11,20 +11,39 @@ public class Skylark {
         System.out.println(Skylark.line);
     }
 
-    private static void printText(ArrayList<String> text) {
+    private static void printText(ArrayList<Task> text) {
         System.out.println(Skylark.line);
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < text.size(); i++) {
-            System.out.println((i + 1) + ". "+ text.get(i));
+            Task currentTask = text.get(i);
+            System.out.println((i + 1) + ". [" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
         }
         System.out.println(Skylark.line);
     }
 
+    private static void printText(Task currentTask, boolean markAsDone) {
+        System.out.println(Skylark.line);
+        if (markAsDone) {
+            System.out.println("Nice! I've marked this task as done:");
+        } else {
+            System.out.println("OK, I've marked this task as not done yet:");
+        }
+        System.out.println("[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
+        System.out.println(Skylark.line);
+    }
+
+    private static boolean doesIndexExist(ArrayList<Task> list, int index) {
+        return index >= 0 && index < list.size();
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         String byeCommand = "bye";
         String listCommand = "list";
+        String doneCommand = "mark";
+        String undoneCommand = "unmark";
         String command;
 
         Skylark.printText("Hello, I am Skylark, how can I help you today?");
@@ -36,8 +55,22 @@ public class Skylark {
                 break;
             } else if (command.equals(listCommand)) {
                 Skylark.printText(taskList);
+            } else if (command.substring(0, 4).equals(doneCommand)) {
+                int index = Integer.parseInt(command.substring(5)) - 1;
+                if (Skylark.doesIndexExist(taskList, index)) {
+                    Task currentTask = taskList.get(index);
+                    currentTask.markAsDone();
+                    Skylark.printText(currentTask, true);
+                }
+            } else if (command.substring(0, 6).equals(undoneCommand)) {
+                int index = Integer.parseInt(command.substring(7)) - 1;
+                if (Skylark.doesIndexExist(taskList, index)) {
+                    Task currentTask = taskList.get(index);
+                    currentTask.markAsUndone();
+                    Skylark.printText(currentTask, false);
+                }
             } else {
-                taskList.add(command);
+                taskList.add(new Task(command));
                 Skylark.printText("added: " + command);
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,14 @@
 public class Task {
-    protected String description;
-    protected boolean isDone;
+    private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public String getStatusIcon() {
