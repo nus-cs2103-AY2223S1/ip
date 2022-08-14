61924e393de10c7030e7e0aad322d5258f44ff01
Lee Ian Ee <35843006+leeianee@users.noreply.github.com>
2022-08-14 18:45:17 +0800

    Add toString to Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -22,4 +22,9 @@ public class Task {
     public void markAsUndone() {
         this.isDone = false;
     }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
 }
