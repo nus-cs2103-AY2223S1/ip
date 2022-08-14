5381d704cd04b1fc872e2fdca0ea2208d78ea02e
benjytan45678 <Benjytan45678@gmail.com>
2022-08-14 17:10:44 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
 public class Duke {
+    public static void echo(String command) {
+        if (command.toUpperCase().equals("BYE")) {
+            String bye = "____________________________________________________________\n" +
+                         "Bye. Hope to see you again soon!\n" +
+                    "____________________________________________________________\n";
+            System.out.println(bye);
+        } else {
+            String echo = "____________________________________________________________\n" +
+                          command + "\n" + "____________________________________________________________\n";
+            System.out.println(echo);
+        }
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        String logo = "____________________________________________________________\n" + "Hello from\n" +
+                " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n" +
+                "How may I assist you? \n" +
+                "____________________________________________________________\n";
+        System.out.println(logo);
+
+        Scanner myScanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Please enter your command: ");
+            String command = myScanner.nextLine();
+            echo(command);
+            if (command.toUpperCase().equals("BYE")) {
+                break;
+            }
+
+        }
     }
 }
