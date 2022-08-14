f0dd936562cb77a893577ca659b6e101afc8a103
lingg02 <89027464+lingg02@users.noreply.github.com>
2022-08-14 13:30:57 +0800

    Implemented increment for level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,8 @@ public class Duke {
                 line
         );
         String input = "";
+        String[] arr = new String[100];
+        int index = 0;
         while (!input.equals("bye")) {
             Scanner scan = new Scanner(System.in);
             input = scan.nextLine();
@@ -17,8 +19,19 @@ public class Duke {
                 System.out.println(line +
                         "Bye. Hope to see you again soon!\n" +
                         line);
+            } else if (input.equals("list")) {
+                String list = line;
+                for (int i = 0; i < index; i++) {
+                    list += i + 1;
+                    list += ". ";
+                    list += arr[i];
+                    list += "\n";
+                }
+                System.out.println(list + line);
             } else {
-                System.out.println(line + input + "\n" + line);
+                arr[index] = input;
+                index ++;
+                System.out.println(line + "added: " + input + "\n" + line);
             }
         }
     }
