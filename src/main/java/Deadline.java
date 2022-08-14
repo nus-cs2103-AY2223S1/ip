92f1db1385f56f35b97eb502081e0da735a268c4
ElginL <elginlee_2000@hotmail.com>
2022-08-14 19:34:46 +0800

    add Deadline class (Subtype of Task)

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,25 @@
+/**
+ * Deadline Class that encapsulates details of a task with a deadline.
+ *
+ * @author Elgin Lee
+ */
+public class Deadline extends Task {
+    /** The date of deadline. */
+    protected String date;
+
+    /**
+     * Constructor of Deadline.
+     *
+     * @param taskName The Task's Name.
+     * @param date The date of deadline.
+     */
+    public Deadline(String taskName, String date) {
+        super(taskName);
+        this.date = date;
+    }
+
+    @Override
+    public String getTaskIcon() {
+        return "[D]";
+    }
+}
