bf9ca290d83e7afb5590412d0802b723ce3373a4
Adee Aryaa <e0726108@u.nus.edu>
2022-08-14 18:32:38 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke What can I do for you?");
+        boolean flag = false;
+        Scanner in = new Scanner(System.in);
+        while (!flag) {
+            String output = in.nextLine();
+            if (output.equals("bye")) {
+                System.out.println("_________________________________________________________________________");
+                System.out.println("        Bye. Hope to see you again soon!");
+                System.out.println("_________________________________________________________________________");
+                flag = true;
+
+            }
+            else {
+                System.out.println("_________________________________________________________________________");
+                System.out.println("       Da myth: " + output);
+                System.out.println("_________________________________________________________________________");
+            }
+        }
     }
 }
