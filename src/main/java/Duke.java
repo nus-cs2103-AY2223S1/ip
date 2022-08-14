fcccd6f06b4aaa8e04d97b3c07e50112156da05e
ALWHgithub <alwhgithub@gmail.com>
2022-08-14 16:11:26 +0800

    Level 2: Implementd Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,22 +4,45 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String logo = " ____        _        \n"
+    private static final String LOGO = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
-    private static final String greeting = "Hello from\n"  + logo + "\nHow can I help you ?\n" ;
+    private static final String GREETING = "Hello from\n"  + LOGO + "\nHow can I help you ?\n" ;
+    private static final List<String> INPUT_LIST = new ArrayList<>();
+
+    public static void addWord (String word){
+        Integer index = INPUT_LIST.size();
+        INPUT_LIST.add(word);
+        System.out.println("added: " + word);
+    }
+
+    public static void printList(){
+        for(int i = 0; i<INPUT_LIST.size(); i++){
+            Integer index = i+1;
+            System.out.println(index + ". " + INPUT_LIST.get(i));
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(greeting);
-        String userInput = scanner.nextLine();
-        if (userInput.equals("bye")){
-            System.out.println("Bye. Hope to see you again soon!");
-        } else {
-            System.out.println(userInput);
+        System.out.println(GREETING);
+
+        while(scanner.hasNext()){
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                scanner.close();
+                break;
+            } else if( userInput.equals("list")){
+                printList();
+            } else {
+                addWord(userInput);
+                continue;
+            }
         }
-        scanner.close();
+
+
     }
 }
