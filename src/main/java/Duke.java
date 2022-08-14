3e631f6eca44655383a44d33750b46ea0d7208f7
ruiqi7 <tinruiqi2002@gmail.com>
2022-08-14 11:33:46 +0800

    Greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // greet the user
+        System.out.println("Hello! I'm Pip :)\nWhat can I do for you?");
+
+        // echo commands entered by the user
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String input = in.nextLine();
+
+            if (input.equals("bye")) {
+                // exit when the user types "bye"
+                in.close();
+                System.out.println("Goodbye and see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
