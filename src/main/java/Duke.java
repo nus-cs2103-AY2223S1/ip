1d162dddacffb0c00ebb284f809d9a206f42b2e0
marcusczh <>
2022-08-14 22:44:57 +0800

    Add support for tracking Todos, Events and Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Duke {
     private static ArrayList<Task> data = new ArrayList<Task>();
@@ -7,6 +8,23 @@ public class Duke {
     private static void addItem(String item) {
         System.out.println("added: " + item);
         data.add(new Task(item));
+    }
+
+    private static void addTodo(String item) {
+        data.add(new Todo(item));
+        System.out.printf("Got it. I've added this task:\n%s\nNow you have %d tasks in the list\n",
+                data.get(data.size() - 1), data.size());
+    }
+    private static void addDeadLine(String item, String by) {
+        data.add(new Deadline(item, by));
+        System.out.printf("Got it. I've added this task:\n%s\nNow you have %d tasks in the list\n",
+                data.get(data.size() - 1), data.size());
+    }
+
+    private static void addEvent(String item, String at) {
+        data.add(new Event(item, at));
+        System.out.printf("Got it. I've added this task:\n%s\nNow you have %d tasks in the list\n",
+                data.get(data.size() - 1), data.size());
     }
 
     private static void listItems() {
@@ -28,6 +46,15 @@ public class Duke {
 
     }
 
+    private static <T> int findElem(T[] arr, T elem) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(elem)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
@@ -40,8 +67,6 @@ public class Duke {
                     break;
                 } else if (command.equals("list")) {
                     listItems();
-                } else {
-                    addItem(command);
                 }
             } else if (inputs.length == 2){
                 if (inputs[0].equals("mark")) {
@@ -50,11 +75,22 @@ public class Duke {
                 } else if (inputs[0].equals("unmark")) {
                     int index = Integer.parseInt(inputs[1]);
                     unMarkItem(index);
-                } else {
-                    addItem(command);
                 }
             } else {
-                addItem(command);
+                if (inputs[0].equals("todo")) {
+                    String task = String.join(" ", Arrays.copyOfRange(inputs, 1, inputs.length));
+                    addTodo(task);
+                } else if (inputs[0].equals("deadline")) {
+                    int limit = findElem(inputs, "/by");
+                    String task = String.join(" ", Arrays.copyOfRange(inputs, 1, limit));
+                    String by = String.join(" ", Arrays.copyOfRange(inputs, limit + 1, inputs.length));
+                    addDeadLine(task, by);
+                } else if (inputs[0].equals("event")) {
+                    int limit = findElem(inputs, "/at");
+                    String task = String.join(" ", Arrays.copyOfRange(inputs, 1, limit));
+                    String at = String.join(" ", Arrays.copyOfRange(inputs, limit + 1, inputs.length));
+                    addEvent(task, at);
+                }
             }
         }
     }
