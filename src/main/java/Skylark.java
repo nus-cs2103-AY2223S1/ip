0bed42130c4ce0b611a0d3c33854e20d43d32de4
Hikoya <hikoyanamikaze@gmail.com>
2022-08-14 16:11:59 +0800

    Rename Duke to Skylark
    Making my chatbot unique by giving it a name first

--- src/main/java/Skylark.java
+++ src/main/java/Skylark.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Skylark {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
