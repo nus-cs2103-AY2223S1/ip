a92c3932a5e9f27ebbf12bb3aacac8cc4968a77d
jorrdansoh <sohjingren@gmail.com>
2022-08-14 21:48:06 +0800

    Added automated UI testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -20,11 +20,11 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin Thomas < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
