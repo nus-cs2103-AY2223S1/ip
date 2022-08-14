7baaef68d98c83d6efdda856e2f41cd047d898ee
Hikoya <hikoyanamikaze@gmail.com>
2022-08-14 16:36:08 +0800

    Complete Level 2. Add, List

--- src/main/java/Skylark.java
+++ src/main/java/Skylark.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Skylark {
@@ -10,10 +11,20 @@ public class Skylark {
         System.out.println(Skylark.line);
     }
 
+    private static void printText(ArrayList<String> text) {
+        System.out.println(Skylark.line);
+        for (int i = 0; i < text.size(); i++) {
+            System.out.println((i + 1) + ". "+ text.get(i));
+        }
+        System.out.println(Skylark.line);
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>();
 
         String byeCommand = "bye";
+        String listCommand = "list";
         String command;
 
         Skylark.printText("Hello, I am Skylark, how can I help you today?");
@@ -23,8 +34,11 @@ public class Skylark {
             if (command.equals(byeCommand)) {
                 Skylark.printText("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equals(listCommand)) {
+                Skylark.printText(taskList);
             } else {
-                System.out.println(command);
+                taskList.add(command);
+                Skylark.printText("added: " + command);
             }
         }
     }
