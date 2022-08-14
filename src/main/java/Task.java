c0ca51c1ae5bc794b55b7d3ab5a7461950cfd0b8
ElginL <elginlee_2000@hotmail.com>
2022-08-14 17:35:05 +0800

    add Task class which represents a class

---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+/**
+ * Class Task that encapsulates details of a task.
+ *
+ * @author Elgin Lee
+ */
+public class Task {
+    /** Name of the task. */
+    protected String taskName;
+
+    /** Whether the task has been completed. */
+    protected boolean isDone;
+
+    /**
+     * Constructor for a Task, initializes task name
+     *
+     * @param taskName The title of the task.
+     */
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
+    }
+
+    public void toggleMark() {
+        this.isDone = !this.isDone;
+    }
+}
