3d0551099103800d2d719135e5d1dbf384ca41fd
vantemoon <sylviassunrx0820@outlook.com>
2022-08-14 16:05:31 +0800

    Renamed the chatbot and modified the greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "     _   _    ______     _____ ____\n"
+                + "    | | / \\  |  _ \\ \\   / /_ _/ ___|\n"
+                + " _  | |/ _ \\ | |_) \\ \\ / / | |\\___ \\\n"
+                + "| |_| / ___ \\|  _ < \\ V /  | | ___) |\n"
+                + " \\___/_/   \\_\\_| \\_\\ \\_/  |___|____/\n";
+        String line = "____________________________________________________________";
+        System.out.println(logo);
+        System.out.println(line);
+        say("Hello. I'm Jarvis");
+        say("What can I do for you?");
+        System.out.println(line);
+    }
+
+    public static void say(String message) {
+        System.out.println(" " + message);
     }
 }
