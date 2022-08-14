d20a1b67d904cb9d3f51b6ff74c3f0e743c2cdd5
Donovan <don9617@hotmail.com>
2022-08-14 22:05:58 +0800

    A-TextUiTesting: Automated Text UI Testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
         Task[] listOfTasks = new Task[100];
         int indexOfNextTask = 0;
         System.out.println("Hello! I'm Don\nHow may I help you?");
-        while (true) {
+        while (sc.hasNextLine()) {
             String response = sc.nextLine();
             if (response.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
@@ -21,18 +21,18 @@ public class Duke {
             else if (response.length() > 3 && response.substring(0, 4).equals("mark")) {
                 int taskIndex = Integer.parseInt(response.substring(5, response.length())) - 1;
                 listOfTasks[taskIndex].markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n" + "\t[" + listOfTasks[taskIndex].getStatusIcon() + "] " + listOfTasks[taskIndex].getDescription());
+                System.out.println("Nice! I've marked this task as done:\n" + "[" + listOfTasks[taskIndex].getStatusIcon() + "] " + listOfTasks[taskIndex].getDescription());
             }
             else if (response.length() > 5 && response.substring(0, 6).equals("unmark")) {
                 int taskIndex = Integer.parseInt(response.substring(7, response.length())) - 1;
                 listOfTasks[taskIndex].markAsNotDone();
-                System.out.println("Ok, I've marked this task as not done yet:\n" + "\t[" + listOfTasks[taskIndex].getStatusIcon() + "] " + listOfTasks[taskIndex].getDescription());
+                System.out.println("Ok, I've marked this task as not done yet:\n" + "[" + listOfTasks[taskIndex].getStatusIcon() + "] " + listOfTasks[taskIndex].getDescription());
             }
             else if (response.length() > 3 && response.substring(0, 4).equals("todo")) {
                 String toDoTaskDescription = response.substring(5, response.length());
                 Todo toDoTask = new Todo(toDoTaskDescription);
                 listOfTasks[indexOfNextTask++] = toDoTask;
-                System.out.println("Got it. I've added this task:\n" + "\t" + toDoTask.toString() + "\nNow you have " + indexOfNextTask + (indexOfNextTask == 1 ? " task " : " tasks ") + "in the list.");
+                System.out.println("Got it. I've added this task:\n" + toDoTask.toString() + "\nNow you have " + indexOfNextTask + (indexOfNextTask == 1 ? " task " : " tasks ") + "in the list.");
             }
             else if (response.length() > 7 && response.substring(0,8).equals("deadline")) {
                 String deadlineDescriptionWithDate = response.substring(9, response.length());
@@ -40,7 +40,7 @@ public class Duke {
                 String deadlineDate = deadlineDescriptionWithDate.split(" /by ")[1];
                 Deadline deadlineTask = new Deadline(deadlineDescription, deadlineDate);
                 listOfTasks[indexOfNextTask++] = deadlineTask;
-                System.out.println("Got it. I've added this task:\n" + "\t" + deadlineTask.toString() + "\nNow you have " + indexOfNextTask + (indexOfNextTask == 1 ? " task " : " tasks ") + "in the list.");
+                System.out.println("Got it. I've added this task:\n" + deadlineTask.toString() + "\nNow you have " + indexOfNextTask + (indexOfNextTask == 1 ? " task " : " tasks ") + "in the list.");
             }
             else if (response.length() > 4 && response.substring(0,5).equals("event")) {
                 String eventDescriptionWithDate = response.substring(6, response.length());
@@ -48,7 +48,7 @@ public class Duke {
                 String eventDate = eventDescriptionWithDate.split(" /at ")[1];
                 Event eventTask = new Event(eventDescription, eventDate);
                 listOfTasks[indexOfNextTask++] = eventTask;
-                System.out.println("Got it. I've added this task:\n" + "\t" + eventTask.toString() + "\nNow you have " + indexOfNextTask + (indexOfNextTask == 1 ? " task " : " tasks ") + "in the list.");
+                System.out.println("Got it. I've added this task:\n" + eventTask.toString() + "\nNow you have " + indexOfNextTask + (indexOfNextTask == 1 ? " task " : " tasks ") + "in the list.");
             }
         }
     }
