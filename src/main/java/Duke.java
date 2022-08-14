4c73152bcbd0b8328a7e450c82ca99c639da0c60
parth-io <67057645+parth-io@users.noreply.github.com>
2022-08-14 17:20:20 +0800

    Add Level-1 Add to .gitignore

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String line = "____________________________________________________________";
+    private static final String indentedLine = "     " + line;
+    private static final String initialMessage = messageWithIndentedLines("Hello! I'm Duke\n      What can I do for you?");
+    private static final String byeMessage = messageWithIndentedLines("Bye. This doesn't have to be the end!");
+
+    public static String messageWithIndentedLines(String message) {
+        return indentedLine + "\n      " + message + "\n" + indentedLine;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(initialMessage);
+        Scanner scanner = new Scanner(System.in);
+        // the user has to at least input bye to exit
+        String userInput = scanner.nextLine();
+        while (true) {
+            if (userInput.equals("bye")) {
+                System.out.println(byeMessage);
+                break;
+            }
+            System.out.println(messageWithIndentedLines(userInput));
+            userInput = scanner.nextLine();
+        }
     }
 }
