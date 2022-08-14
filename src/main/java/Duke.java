45fa88a2eff47c1694a00954b02460df9f69cec3
weiquany <weiquan7122@gmail.com>
2022-08-14 14:56:43 +0800

    Implement greet, echo, exit in Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE_DIVIDER = "    ____________________________________________________________\n";
+    private static final String INDENTATION = "     ";
+    private static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greetUser();
+        runDuke();
+    }
+    public static void runDuke() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine().toLowerCase();
+            switch (input) {
+                case "bye":
+                    exitDuke();
+                    return;
+                default:
+                    System.out.println(formatStringWithLineDivider(INDENTATION + input));
+            }
+        }
+    }
+
+    public static String formatStringWithLineDivider(String string) {
+        return LINE_DIVIDER + string + "\n" + LINE_DIVIDER;
+    }
+    public static void greetUser() {
+        System.out.println(LOGO);
+        System.out.println(formatStringWithLineDivider(INDENTATION + "Hello! I'm Duke\n" + INDENTATION + "What can I do for you?"));
+    }
+
+    public static void exitDuke() {
+        System.out.println(formatStringWithLineDivider(INDENTATION + "Bye. Hope to see you again soon!"));
     }
 }
