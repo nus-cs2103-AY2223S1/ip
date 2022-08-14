a546724a2bfba8c8d1e49ad6b5411d82db5dabee
benjytan45678 <Benjytan45678@gmail.com>
2022-08-14 18:44:31 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,30 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
+    private static ArrayList<String> list = new ArrayList<>();
     public static void echo(String command) {
         if (command.toUpperCase().equals("BYE")) {
             String bye = "____________________________________________________________\n" +
                          "Bye. Hope to see you again soon!\n" +
                     "____________________________________________________________\n";
             System.out.println(bye);
+        } else if (command.toUpperCase().equals("LIST")) {
+            String newList = "";
+            int count = 1;
+            for (String item: list) {
+                newList += (count + ". " + item + "\n");
+                count++;
+            }
+            String updatedList = "____________________________________________________________\n" + newList +
+                    "____________________________________" +
+                    "________________________\n";
+            System.out.println(updatedList);
+
         } else {
+            list.add(command);
             String echo = "____________________________________________________________\n" +
-                          command + "\n" + "____________________________________________________________\n";
+                           "added: " + command + "\n" + "____________________________________________________________\n";
             System.out.println(echo);
         }
     }
@@ -24,6 +40,7 @@ public class Duke {
         System.out.println(logo);
 
         Scanner myScanner = new Scanner(System.in);
+
         while (true) {
             System.out.println("Please enter your command: ");
             String command = myScanner.nextLine();
