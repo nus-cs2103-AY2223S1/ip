93bd86781940f9cbc06edbfc90f0e3a5b311ffc7
ElginL <elginlee_2000@hotmail.com>
2022-08-14 20:05:14 +0800

    add documentation to ToDo class

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,4 +1,13 @@
+/**
+ * Class that encapsulates To Do task.
+ *
+ * @author Elgin
+ */
 public class ToDo extends Task {
+    /**
+     * Constructor of the To Do task.
+     * @param taskName The title of the Task
+     */
     public ToDo(String taskName) {
         super(taskName);
     }
