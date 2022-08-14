836d3597519d78a661c0a64b8caf2b0fca534e59
neosunhan <neosunhan@gmail.com>
2022-08-14 19:10:22 +0800

    Add ability to mark tasks as done and not done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,21 +2,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static final ArrayList<String> history = new ArrayList<>();
+    private static final ArrayList<Task> history = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
 
+        Duke.history.add(new Task("dummy task"));
         Scanner myScanner = new Scanner(System.in);
         String command = myScanner.nextLine();
         while (!command.equals("bye")) {
             if (command.equals("list")) {
-                for (int i = 0; i < history.size(); i++) {
-                    System.out.println(i + 1 + ". " + history.get(i));
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 1; i < Duke.history.size(); i++) {
+                    System.out.println(i + "." + Duke.history.get(i));
                 }
+            } else if (command.startsWith("mark")) {
+                int taskIndex = Character.getNumericValue(command.charAt(5));
+                Duke.history.get(taskIndex).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + Duke.history.get(taskIndex));
+            } else if (command.startsWith("unmark")) {
+                int taskIndex = Character.getNumericValue(command.charAt(7));
+                Duke.history.get(taskIndex).markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + Duke.history.get(taskIndex));
             } else {
-                history.add(command);
+                Duke.history.add(new Task(command));
                 System.out.println("added: " + command);
             }
             System.out.println();
