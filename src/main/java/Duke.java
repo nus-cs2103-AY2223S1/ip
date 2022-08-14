ce347a89223b0c021ac2fd8b178b18fc1a1f45a4
craeyeons <simpsoneng552@gmail.com>
2022-08-14 17:36:26 +0800

    completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String receiveCommand() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        return input;
+    }
+
+    private static void bye() {
+        System.out.println("\tGoodbye! See you soon!\n" +
+                           "\tAu revoir! À tout à l'heure!");
+    }
+
+    private static void echo(String input) {
+        System.out.println("\t" + input);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I' Jean\n" +
+                           "How can I help you?\n" +
+                           "Bonjour! Je m'appelle Jean\n" +
+                           "Vous désirez?\n");
+
+        while(true) {
+            String input = receiveCommand();
+            if (input.equals("bye")) {
+                bye();
+                break;
+            }
+
+            echo(input);
+        }
+
+
+
+
     }
 }
