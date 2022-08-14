c1f1c42aec1d53a060bd2a7ffa2f80694088be98
Lee Ian Ee <35843006+leeianee@users.noreply.github.com>
2022-08-14 17:26:23 +0800

    Add skeletal functionality to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String input = "";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?");
+
+        while (!input.equals("bye")) {
+            input = scan.nextLine(); // Reads user input
+            System.out.println("\t" + input); // Prints user input
+        }
+
+        System.out.println("Bye. Hope to see you again soon!"); // Exits when user types "bye"
     }
 }
