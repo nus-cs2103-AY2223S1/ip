6e18ddd650a64fb1f94df35a55194974c3c1821a
ElginL <elginlee_2000@hotmail.com>
2022-08-14 13:34:33 +0800

    add Duke's ability to greet user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
 public class Duke {
+    private static void greetUser() {
+        String message = formatText("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println(message);
+    }
+
+    /**
+     * Styles a given text with indentation and wraps the text around horizontal lines.
+     *
+     * @param text String that needs to be styled.
+     * @return Formatted String that has proper indentation and wrapped around horizontal lines.
+     */
+    private static String formatText(String text) {
+        final String HORIZONTAL_LINE = "\t--------------------------------------------------\n";
+
+        String[] lines = text.split("\\r?\\n");
+        StringBuilder formattedText = new StringBuilder(HORIZONTAL_LINE);
+        for (String line : lines) {
+            formattedText.append("\t").append(line).append("\n");
+        }
+
+        return formattedText + HORIZONTAL_LINE;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.greetUser();
+
     }
 }
