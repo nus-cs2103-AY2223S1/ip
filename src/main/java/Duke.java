d5c20651a172d19a0da9f4358193c2d772268aff
Tan How Suen <howsuen@gmail.com>
2022-08-14 23:43:37 +0800

    Added ToDo, Deadline, Event

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,24 +14,63 @@ public class Duke {
 
         while (sc.hasNext()) {
             String command = sc.nextLine();
+            String[] words = command.split(" ", 2);
             if (command.equals("bye")) {
                 message("Bye! Hope to see you again soon!");
                 sc.close();
                 break;
-            } else if (command.equals("list")) {
+            }
+            if (command.equals("list")) {
                 tasksList.listTasks();
-            } else if (command.startsWith("mark") || command.startsWith("unmark")) {
-                String[] words = command.split(" ", 2);
+                continue;
+            }
+            if (words[0].equals("mark") || words[0].equals("unmark")) {
                 if (words.length == 1) {
-                    System.out.println("Please specify the task by its id!");
-                } else if (words[0].equals("mark")) {
+                    System.out.println("Please specify the task by its id.");
+                    continue;
+                }
+                if (words[0].equals("mark")) {
                     tasksList.markTask(Integer.parseInt(words[1]));
                 } else {
                     tasksList.unmarkTask(Integer.parseInt(words[1]));
                 }
-            } else {
-                tasksList.addToList(command);
+                continue;
             }
+            if (words[0].equals("todo")) {
+                if (words.length == 1) {
+                    System.out.println("Please specify what task you wish to do.");
+                    continue;
+                }
+                tasksList.addTodo(words[1]);
+                continue;
+            }
+            if (words[0].equals("deadline")) {
+                if (words.length == 1) {
+                    System.out.println("Please specify what deadline task you wish to do.");
+                    continue;
+                }
+                String[] deadline = words[1].split(" /by ", 2);
+                if (deadline.length == 1) {
+                    System.out.println("Please specify the date/time of this deadline.");
+                    continue;
+                }
+                tasksList.addDeadline(deadline[0], deadline[1]);
+                continue;
+            }
+            if (words[0].equals("event")) {
+                if (words.length == 1) {
+                    System.out.println("Please specify what event you wish to do.");
+                    continue;
+                }
+                String[] event = words[1].split(" /at ", 2);
+                if (event.length == 1) {
+                    System.out.println("Please specify the date/time of this event.");
+                    continue;
+                }
+                tasksList.addEvent(event[0], event[1]);
+                continue;
+            }
+            System.out.println("Unknown command. Please try again.");
         }
     }
 
--- src/main/java/TasksList.java
+++ src/main/java/TasksList.java
@@ -9,21 +9,58 @@ public class TasksList {
     }
 
     /**
-     * Add to the list of tasks
+     * Add To-do task to the list of tasks
      *
      * @param description The task to be added to the list
      */
-    public void addToList(String description) {
-        Task newTask = new Task(description);
+    public void addTodo(String description) {
+        System.out.println("Got it! Duke has added this task:");
+        Task newTask = new ToDo(description);
+        System.out.println(newTask);
         this.tasksList.add(newTask);
-        System.out.println("added: " + description);
+        int len = this.tasksList.size();
+        String line = String.format("Now you have %d task%s in the list.",
+                len, len > 1 ? "s" : "");
+        System.out.println(line);
+    }
+
+    /**
+     * Add Deadline task to the list of tasks
+     *
+     * @param description The task to be added to the list
+     */
+    public void addDeadline(String description, String date) {
+        System.out.println("Got it! Duke has added this task:");
+        Task newTask = new Deadline(description, date);
+        System.out.println(newTask);
+        this.tasksList.add(newTask);
+        int len = this.tasksList.size();
+        String line = String.format("Now you have %d task%s in the list.",
+                len, len > 1 ? "s" : "");
+        System.out.println(line);
+    }
+
+    /**
+     * Add Event task to the list of tasks
+     *
+     * @param description The task to be added to the list
+     */
+    public void addEvent(String description, String date) {
+        System.out.println("Got it! Duke has added this task:");
+        Task newTask = new Event(description, date);
+        System.out.println(newTask);
+        this.tasksList.add(newTask);
+        int len = this.tasksList.size();
+        String line = String.format("Now you have %d task%s in the list.",
+                len, len > 1 ? "s" : "");
+        System.out.println(line);
     }
 
     /**
      * Prints out the list of the history of tasks
      */
     public void listTasks() {
-        System.out.println("Duke's Tasks:");
+        System.out.println("Here are the tasks in your list:");
         if (tasksList.size() == 0) {
             System.out.println("*No tasks! ^_^*");
             return;
@@ -34,19 +71,29 @@ public class TasksList {
         }
     }
 
+    /**
+     * Mark the task of the given id as done.
+     *
+     * @param id The id of the task to be marked done.
+     */
     public void markTask(int id) {
         int len = this.tasksList.size();
         if (id <= 0 || id > len) {
-            System.out.println("Invalid number!");
+            System.out.println("Invalid task id!");
         } else {
             this.tasksList.get(id - 1).mark();
         }
     }
 
+    /**
+     * Mark the task of the given id as not done.
+     *
+     * @param id The id of the task to be marked as not done.
+     */
     public void unmarkTask(int id) {
         int len = this.tasksList.size();
         if (id <= 0 || id > len) {
-            System.out.println("Invalid number!");
+            System.out.println("Invalid task id!");
         } else {
             this.tasksList.get(id - 1).unmark();
         }
