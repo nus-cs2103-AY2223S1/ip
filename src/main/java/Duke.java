4926f3f5bbb2b8921a95b5feda25f9dc3395a3af
neosunhan <neosunhan@gmail.com>
2022-08-14 18:52:45 +0800

    Add the ability to store and display text entered by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static final ArrayList<String> history = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
@@ -8,7 +11,14 @@ public class Duke {
         Scanner myScanner = new Scanner(System.in);
         String command = myScanner.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < history.size(); i++) {
+                    System.out.println(i + 1 + ". " + history.get(i));
+                }
+            } else {
+                history.add(command);
+                System.out.println("added: " + command);
+            }
             System.out.println();
             command = myScanner.nextLine();
         }
