f18fc419d5c048492625e5ee998b9c0a84ee0075
parth-io <67057645+parth-io@users.noreply.github.com>
2022-08-15 02:27:12 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Duke {
     private static final String line = "____________________________________________________________";
     private static final String indentedLine = "     " + line;
-    private static final String initialMessage = messageWithIndentedLines("Hello! I'm Duke\n      What can I do for you?");
-    private static final String byeMessage = messageWithIndentedLines("Bye. This doesn't have to be the end!");
+    private static final String initialMessage = messageWithIndentedLines("\n      Hello! I'm Duke\n      What can I do for you?\n");
+    private static final String byeMessage = messageWithIndentedLines("\n      Bye. This doesn't have to be the end!\n");
+    private static final ArrayList<String> userText = new ArrayList<>();
 
     public static String messageWithIndentedLines(String message) {
-        return indentedLine + "\n      " + message + "\n" + indentedLine;
+        return indentedLine + message + indentedLine;
     }
 
     public static void main(String[] args) {
@@ -19,8 +22,17 @@ public class Duke {
             if (userInput.equals("bye")) {
                 System.out.println(byeMessage);
                 break;
+            } else if (userInput.equals("list")) {
+                // Idea below of iterating with indices in streams adapted from https://stackoverflow.com/a/42616742
+                HashMap<Integer, String> mappedIndexToUserText = userText.stream()
+                        .collect(HashMap::new, (hashMap, streamElement) -> hashMap.put(hashMap.size(), streamElement), HashMap::putAll);
+                StringBuilder listOfUserText = mappedIndexToUserText.entrySet().stream()
+                        .reduce(new StringBuilder(), (stringToBuild, currentEntry) -> stringToBuild.append("\n      ").append(currentEntry.getKey()).append(". ").append(currentEntry.getValue()), StringBuilder::append);
+                System.out.println(messageWithIndentedLines(listOfUserText.toString() + "\n"));
+            } else {
+                System.out.println(messageWithIndentedLines("\n      added: " + userInput + "\n"));
+                userText.add(userInput);
             }
-            System.out.println(messageWithIndentedLines(userInput));
             userInput = scanner.nextLine();
         }
     }
