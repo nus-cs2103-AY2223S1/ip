ff531ae383ddb4f8021bae0d1c936143b26a4f82
Hikoya <hikoyanamikaze@gmail.com>
2022-08-14 16:19:55 +0800

    Complete Level-1 Greet, Echo, Exit

--- src/main/java/Skylark.java
+++ src/main/java/Skylark.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Skylark {
+
+    private static final String line = "________________________________________________";
+
+    private static void printText(String text) {
+        System.out.println(Skylark.line);
+        System.out.println(text);
+        System.out.println(Skylark.line);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scan = new Scanner(System.in);
+
+        String byeCommand = "bye";
+        String command;
+
+        Skylark.printText("Hello, I am Skylark, how can I help you today?");
+
+        while (true) {
+            command = scan.nextLine();
+            if (command.equals(byeCommand)) {
+                Skylark.printText("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }
