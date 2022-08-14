ff8ba2eace24de355967aea56958a0490e929439
marcusczh <>
2022-08-14 19:31:48 +0800

    Allow chatbot to store and display text entered by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    public static ArrayList<String> data = new ArrayList<String>();
+
+    public static void addItem(String item) {
+        System.out.println("added: " + item);
+        data.add(item);
+    }
+
+    public static void listItems() {
+        for (int i = 0; i < data.size(); i++) {
+            int index = i + 1;
+            System.out.println(index + ". " + data.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
@@ -9,8 +24,11 @@ public class Duke {
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equals("list")) {
+                listItems();
+            } else {
+                addItem(command);
             }
-            System.out.println(command);
         }
     }
 }
