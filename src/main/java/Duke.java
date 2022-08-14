354c0fca923399eb9b45dcd5e650da18744f075e
ALWHgithub <alwhgithub@gmail.com>
2022-08-14 17:43:36 +0800

    Level 3: Implemented Marking tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
+
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,19 +11,10 @@ public class Duke {
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
     private static final String GREETING = "Hello from\n"  + LOGO + "\nHow can I help you ?\n" ;
-    private static final List<String> INPUT_LIST = new ArrayList<>();
+    private static final List<Task> INPUT_LIST = new ArrayList<>();
 
-    public static void addWord (String word){
-        Integer index = INPUT_LIST.size();
-        INPUT_LIST.add(word);
-        System.out.println("added: " + word);
-    }
-
-    public static void printList(){
-        for(int i = 0; i<INPUT_LIST.size(); i++){
-            Integer index = i+1;
-            System.out.println(index + ". " + INPUT_LIST.get(i));
-        }
+    private static boolean isNumeric(String input) {
+        return input.matches("^[0-9]*$");
     }
 
     public static void main(String[] args) {
@@ -31,18 +23,53 @@ public class Duke {
 
         while(scanner.hasNext()){
             String userInput = scanner.nextLine();
+            String[] split = userInput.split(" ");
             if (userInput.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 scanner.close();
                 break;
             } else if( userInput.equals("list")){
                 printList();
+                continue;
+            } else if(split.length == 2 && split[0].equals("mark") && isNumeric(split[1])) {
+                markDone(Integer. parseInt(split[1]));
+                continue;
+            } else if(split.length == 2 && split[0].equals("unmark") && isNumeric(split[1])) {
+                markNotDone(Integer. parseInt(split[1]));
+                continue;
             } else {
                 addWord(userInput);
                 continue;
             }
         }
+    }
 
+    public static void addWord (String word){
+        Integer index = INPUT_LIST.size();
+        INPUT_LIST.add(new Task(word));
+        System.out.println("added: " + word);
+    }
 
+    private static void markDone(Integer index){
+        Task task = INPUT_LIST.get(index-1);
+        task.setDone();
+
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task.toString());
+    }
+
+    private static void markNotDone(Integer index){
+        Task task = INPUT_LIST.get(index-1);
+        task.setNotDone();
+
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(task.toString());
+    }
+
+    public static void printList(){
+        for(int i = 0; i<INPUT_LIST.size(); i++){
+            Integer index = i+1;
+            System.out.println(index + ". " + INPUT_LIST.get(i));
+        }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+
+
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public boolean getStatus() {
+        return isDone;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public void setNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString(){
+        return "[" + getStatusIcon() + "]" + " " + this.description;
+    }
+}
