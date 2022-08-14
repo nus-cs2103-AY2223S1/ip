1d7a8a8a638d8c6776f3482d65c489b4f99843da
Lester Ong <lesterong776@gmail.com>
2022-08-14 23:56:59 +0800

    Add to list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Duke {
+    private static String[] list = new String[100];
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello! I'm Duke.\nWhat can I do for you?");
@@ -10,8 +11,21 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 input.close();
                 break;
+            } else if (command.equals("list")) {
+                for (int pos = 0; pos < 100; pos++) {
+                    if (list[pos] == null) break;
+                    System.out.println(pos + 1 + ". " + list[pos]);
+                }
+                System.out.print(">> ");
+                command = input.next();
             } else {
-                System.out.println(command);
+                System.out.println("Added: " + command);
+                for (int pos = 0; pos < 100; pos++) {
+                    if (list[pos] == null) {
+                        list[pos] = command;
+                        break;
+                    }
+                }
                 System.out.print(">> ");
                 command = input.next();
             }
