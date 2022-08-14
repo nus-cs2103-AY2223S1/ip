90ebf64bc3a5cf43329746c724652200b17e7ae5
zicot <zicotjia@gmail.com>
2022-08-14 22:41:36 +0800

    First Commit (Version 0.1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Hello! I am Duke \n What do you want me to do? \n");
+
+        String input;
+
+        while (true) {
+            input = myScanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("See you later :)");
+                System.exit(0);
+            }
+            System.out.println(input);
+        }
+
     }
 }
