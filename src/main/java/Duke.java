5be752eedf1523f7545ab63ea0bdd006406dc6c9
Lim Xin Yi Nicole <limxinyi.nicole@gmail.com>
2022-08-14 11:46:53 +0800

    users can mark a task as done or not done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,53 +1,64 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Duke {
-    ArrayList<String> tasks;
+    ArrayList<Task> tasks;
 
+    /**
+     * Constructor.
+     */
     public Duke() {
         this.tasks = new ArrayList<>();
     }
 
     /**
-     * To greet the user
-     * @return A message used to greet the user
+     * To greet the user.
+     * @return A message used to greet the user.
      */
     public String greet() {
         return "Hello! I'm Duke\nWhat can I do for you? ^_^";
     }
 
     /**
-     * To echo user's input
-     * @param message A message to echo
-     * @return The message user's provided
+     * To echo user's input.
+     * @param message A message to echo.
+     * @return The message user's provided.
      */
     public String echo(String message) {
         return "\t> " + message;
     }
 
     /**
-     * To exit the programme after users type "bye"
-     * @return A goodbye message
+     * To exit the programme after users type "bye".
+     * @return A goodbye message.
      */
     public String exit() {
         return "\t> Bye. Hope to see you again soon :D";
     }
 
-    public String add(String task) {
+    /**
+     *
+     * @param description The task that should be added to the list.
+     * @return A string that tells the user if the tasks has been added.
+     */
+    public String add(String description) {
+        Task task = new Task(description);
         tasks.add(task);
         return "\t> added: " + task;
     }
 
+    /**
+     * To list down all the tasks that are added to the list.
+     * @return A list of all the tasks added.
+     */
     public String list() {
         int len = tasks.size();
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder("\t> Here are the tasks in your list :D");
         for (int i = 0; i < len; i++) {
             int index = i + 1;
-            if (index == 1) {
-                stringBuilder.append("\t> " + index + ". " + tasks.get(i));
-            } else {
-                stringBuilder.append("\n\t  " + index + ". " + tasks.get(i));
-            }
+            String task = "\n\t  " + index + ". " + tasks.get(i);
+            stringBuilder.append(task);
         }
         return stringBuilder.toString();
     }
@@ -72,6 +83,14 @@ public class Duke {
                 run = false;
             } else if (message.equalsIgnoreCase("list")) {
                 System.out.println(duke.list());
+            } else if (message.startsWith("mark")) {
+                Pattern pattern = Pattern.compile("[^0-9]");
+                int taskNumber = Integer.parseInt(pattern.matcher(message).replaceAll(""));
+                System.out.println(duke.tasks.get(taskNumber - 1).markAsDone());
+            } else if (message.startsWith("unmark")) {
+                Pattern pattern = Pattern.compile("[^0-9]");
+                int taskNumber = Integer.parseInt(pattern.matcher(message).replaceAll(""));
+                System.out.println(duke.tasks.get(taskNumber - 1).markAsNotDone());
             } else {
                 System.out.println(duke.add(message));
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,50 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Constructor to create a new task.
+     * @param description The task description.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * To get the task status
+     * @return A string that describes the task status. X indicates that the task is completed.
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    /**
+     * To mark a task as completed.
+     * @return A string that notifies the user if the task has been marked as done.
+     */
+    public String markAsDone() {
+        this.isDone = true;
+        return String.format("\t> Nice! I have marked this task as done:\n\t \t[%s] %s",
+                this.getStatusIcon(), this.description);
+    }
+
+    /**
+     * To mark a task as not completed.
+     * @return A string that notifies the user if the task has been marked as not done.
+     */
+    public String markAsNotDone() {
+        this.isDone = false;
+        return String.format("\t> Okay! I have marked this task as not done:\n\t \t[%s] %s",
+                this.getStatusIcon(), this.description);
+    }
+
+    @Override
+    /**
+     * @return Returns a string representation of the task object.
+     */
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+
+}
