42a9a80a082f8b69b758c1cc4d30fb9c46b2279f
ElginL <elginlee_2000@hotmail.com>
2022-08-15 00:39:19 +0800

    edit deadline toString format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -20,6 +20,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + date + ")";
+        return "[D]" + super.toString() + " (by:" + date + ")";
     }
 }
