88d3e71416b33d16ea9c5e47e1cc19de7ac2db6a
jehousoh <e0694467@u.nus.edu>
2022-08-14 13:20:18 +0800

    added level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("-------------------------------------------");
+        System.out.println("Hello from Phil");
+        System.out.println("How may I assist you on this fine day?");
+        System.out.println("-------------------------------------------");
+
+        String in = "";
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            in = sc.next();
+            if (in.equals("bye")) {
+                break;
+            }
+            System.out.println(in);
+            System.out.println("-------------------------------------------");
+        }
+        System.out.println("See you later alligator!");
+        System.out.println("-------------------------------------------");
     }
 }
