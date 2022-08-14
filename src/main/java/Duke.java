dcc2f259c5c5e84810ef852ed2ee7924fbfa3c68
ALWHgithub <alwhgithub@gmail.com>
2022-08-14 15:28:41 +0800

    Level 1: Implemented Greet,Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
+    private static final String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String greeting = "Hello from\n"  + logo + "\nHow can I help you ?\n" ;
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(greeting);
+        String userInput = scanner.nextLine();
+        if (userInput.equals("bye")){
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(userInput);
+        }
+        scanner.close();
     }
 }
