10eb72aeb7d9ddf1e65af1afc1f99eed22256941
donljh <don.limjunhong@gmail.com>
2022-08-15 01:59:20 +0800

    Level 3: Mark as Done

--- src/main/java/Blob.java
+++ src/main/java/Blob.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Blob {
@@ -6,8 +7,7 @@ public class Blob {
     // Header to signify start of a message by the chat-bot
     private static final String MESSAGE_HEADER = "\u001B[33m" + "Blob says: " + "\u001B[0m";
 
-    private String[] taskList = new String[100];
-    private int taskIndex = 0;
+    private final ArrayList<Task> taskList = new ArrayList<>();
 
     /**
      * Prints a greeting message.
@@ -23,23 +23,60 @@ public class Blob {
         this.speak("Thanks for talking to Blob...", "Blob see you soon...");
     }
 
+    /**
+     * Enumerates the list of added tasks.
+     */
     private void listTasks() {
         StringBuilder tasksStringBuilder = new StringBuilder();
         tasksStringBuilder.append("\n");
-        for (int i = 0; i < taskIndex; i++) {
-            tasksStringBuilder.append(String.format("\t\t%d. %s \n", i + 1, taskList[i]));
+        for (int i = 0; i < taskList.size(); i++) {
+            tasksStringBuilder.append(String.format("\t\t%d. %s \n", i + 1, taskList.get(i).toString()));
         }
         speak("Blob remember tasks...", tasksStringBuilder.toString());
     }
 
-    private void addTask(String task) {
-        taskList[taskIndex] = task;
-        taskIndex++;
+    /**
+     * Adds a task to the list of tasks.
+     */
+    private void addTask(String description) {
+        Task task = new Task(description);
+        taskList.add(task);
         speak("Blob will remember task...", String.format("\n\t\t%s \n", task));
     }
 
     /**
+     * Marks task at the index to be done.
+     *
+     * @param index The index of the task to be marked done
+     */
+    private void markTaskAtIndexDone(int index) {
+        try {
+            Task task = taskList.get(index - 1);
+            task.markAsDone();
+            speak("Blob congratulates on task well done...", String.format("\n\t\t%s \n", task));
+        } catch (IndexOutOfBoundsException ioobe) {
+            speak("Blob cannot find task...", "Maybe task don't exist...?");
+        }
+    }
+
+    /**
+     * Marks task at the index to be undone.
+     *
+     * @param index The index of the task to be marked undone
+     */
+    private void markTaskAtIndexUndone(int index) {
+        try {
+            Task task = taskList.get(index - 1);
+            task.markAsUndone();
+            speak("Blob will mark as undone...", String.format("\n\t\t%s \n", task));
+        } catch (IndexOutOfBoundsException ioobe) {
+            speak("Blob cannot find task...", "Maybe task don't exist...?");
+        }
+    }
+
+    /**
      * Prints a sequence of strings, each in an indented newline encapsulated in a message instance.
+     *
      * @param content The sequence of strings to be printed
      */
     private void speak(String ...content) {
@@ -61,13 +98,31 @@ public class Blob {
         while (isRunning) {
             System.out.print(">> ");
             String input = sc.nextLine();
-            switch (input) {
+            String[] deconstructedInput = input.split(" ", 2);
+            String command = deconstructedInput[0];
+            switch (command) {
             case "bye":
                 sayGoodbye();
                 isRunning = false;
                 break;
             case "list":
                 listTasks();
+                break;
+            case "mark":
+                try {
+                    int index = Integer.parseInt(deconstructedInput[1]);
+                    markTaskAtIndexDone(index);
+                } catch(NumberFormatException exception) {
+                    speak("Blob cannot find task...", "Maybe task don't exist...?");
+                }
+                break;
+            case "unmark":
+                try {
+                    int index = Integer.parseInt(deconstructedInput[1]);
+                    markTaskAtIndexUndone(index);
+                } catch(NumberFormatException exception) {
+                    speak("Blob cannot find task...", "Maybe task don't exist...?");
+                }
                 break;
             default:
                 addTask(input);
---
+++ src/main/java/Task.java
@@ -0,0 +1,46 @@
+/**
+ * The Task class encapsulates a task.
+ * A task has a string description describing the task to be done, and a boolean indicator to represent
+ * the status of completion of the task.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns an icon representing the state of completion of the task.
+     *
+     * @return A checkmark icon (✓) if the task is done, and " " otherwise
+     */
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : " ");
+    }
+
+    /**
+     * Marks the task as done.
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Marks the task as undone.
+     */
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return A string representation of the task
+     */
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
