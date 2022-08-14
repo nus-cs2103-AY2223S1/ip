96d5986bc8b425a2e9f92bcde51d2871a3bdfb27
Berted <46164341+Berted@users.noreply.github.com>
2022-08-14 21:10:30 +0800

    Adds basic adding and listing tasks functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Duke {
 
     private static final String GREETING_MESSAGE = "Hello! I'm TedBot ヾ(≧▽≦*)o\n"
-                                    + "What do you want to do today?";
+            + "What do you want to do today?";
     private static final String GOODBYE_MESSAGE = "Bye! Hope to see you soon ༼- つ ◕_◕ ༽つ";
+    private static final int TASKLIST_MAX_SIZE = 100;
+
+    private static String[] taskList = new String[TASKLIST_MAX_SIZE];
+    private static int taskCount = 0;
 
     public static void main(String[] args) {
 
@@ -12,13 +16,23 @@ public class Duke {
         boolean exitCalled = false;
 
         System.out.println(GREETING_MESSAGE);
+
         while (!exitCalled) {
             String userQuery = sysIn.nextLine();
-            if (userQuery.equals("bye")) {
-                System.out.println(GOODBYE_MESSAGE);
-                exitCalled = true;
-            } else {
-                System.out.println(userQuery);
+            switch (userQuery) {
+                case "list":
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.printf("%02d. %s\n", i + 1, taskList[i]);
+                    }
+                    break;
+                case "bye":
+                    System.out.println(GOODBYE_MESSAGE);
+                    exitCalled = true;
+                    break;
+                default:
+                    assert taskCount < TASKLIST_MAX_SIZE;
+                    taskList[taskCount++] = userQuery;
+                    System.out.printf("Successfully added \"%s\" to your task list!\n", userQuery);
             }
         }
     }
