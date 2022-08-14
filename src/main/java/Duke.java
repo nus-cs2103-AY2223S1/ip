d699f4ee7f0ace3108f0e94d97bfb33b4dff99f3
Lee Ian Ee <35843006+leeianee@users.noreply.github.com>
2022-08-14 19:10:39 +0800

    Add ToDo, Event, Deadline functionality to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,26 +11,41 @@ public class Duke {
 
         while (!input.equals("bye")) {
             input = scan.nextLine(); // Reads user input
-            String[] split = input.split(" ", 0);
+            String[] split = input.split(" ", 2);
             if (input.equals("list")) { // Prints out items in list
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println(i+1 + ".[" + list[i].getStatusIcon() + "] " + list[i].getDescription());
+                    System.out.println(i+1 + "." + list[i].toString());
                 }
-            } else if (split.length == 2 && split[0].equals("mark")) { // Checks for mark
+            } else if (split[0].equals("mark")) { // Checks for mark
                 int index = Integer.parseInt(split[1]) - 1;
                 list[index].markAsDone();
                 System.out.println("Nice! I've marked this task as done:" + "\n" +
-                        "\t[" + list[index].getStatusIcon() + "] " + list[index].getDescription());
-            } else if (split.length == 2 && split[0].equals("unmark")) { // Checks for unmark
+                        "\t" + list[index].toString());
+            } else if (split[0].equals("unmark")) { // Checks for unmark
                 int index = Integer.parseInt(split[1]) - 1;
                 list[index].markAsUndone();
                 System.out.println("Ok, I've marked this task as not done yet:" + "\n" +
-                        "\t[" + list[index].getStatusIcon() + "] " + list[index].getDescription());
-            } else if (!input.equals("bye")) { // Only adds input to list if it is not "bye"
-                list[count] = new Task(input);
+                        "\t" + list[index].toString());
+            } else if (split[0].equals("todo")) { // Checks for Todo
+                list[count] = new Todo(split[1]);
+                System.out.println("Got it. I've added this task:" + "\n\t" + list[count].toString());
                 count += 1;
-                System.out.println("\tadded: " + input); // Prints user input
+                System.out.println("Now you have " + count + " tasks in the list.");
+            } else if (split[0].equals("deadline")) { // Checks for Deadline
+                String[] temp = split[1].split("/by", 2);
+                list[count] = new Deadline(temp[0], temp[1]);
+                System.out.println("Got it. I've added this task:" + "\n\t" + list[count].toString());
+                count += 1;
+                System.out.println("Now you have " + count  + " tasks in the list.");
+            } else if (split[0].equals("event")) { // Checks for Event
+                String[] temp = split[1].split("/at", 2);
+                list[count] = new Event(temp[0], temp[1]);
+                System.out.println("Got it. I've added this task:" + "\n\t" + list[count].toString());
+                count += 1;
+                System.out.println("Now you have " + count  + " tasks in the list.");
+            } else if (!input.equals("bye")) { // Only adds input to list if it is not "bye"
+                System.out.println("Invalid input"); // Prints user input
             }
         }
 
