396945a00282c9d6a5bc83bd24873271f38599c0
ruiqi7 <tinruiqi2002@gmail.com>
2022-08-14 14:27:24 +0800

    Mark as done / not done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,36 +2,43 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String[] itemArray = new String[100];
-        int currIndex = 0;
+        TaskList taskList = new TaskList();
 
-        // greet the user
-        System.out.println("Hello! I'm Pip :)\nWhat can I do for you?");
+        System.out.println("Hello! I'm Pip :)\nWhat can I do for you?"); // greet the user
 
         Scanner in = new Scanner(System.in);
         while (true) {
             String input = in.nextLine().trim();
-
-            if (input.equals("bye")) {
-                // exit
+            if (input.equals("bye")) { // exit
                 in.close();
                 System.out.println("Goodbye and see you again soon!");
                 break;
-            } else if (input.equals("list")) {
-                if (currIndex == 0) {
-                    // no items stored
-                    System.out.println("Your list is currently empty!");
-                } else {
-                    // display items stored
-                    for (int i = 0; i < currIndex; i++) {
-                        int itemNumber = i + 1;
-                        System.out.println(itemNumber + ". " + itemArray[i]);
+            } else if (input.equals("list")) { // display tasks stored
+                taskList.displayTasks();
+            } else if (input.startsWith("mark")) { // mark task as done
+                String taskNumAsString = input.substring(5);
+                try {
+                    int taskNum = Integer.parseInt(taskNumAsString);
+                    Task task = taskList.getTask(taskNum);
+                    if (task != null) {
+                        task.markAsDone();
                     }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please specify a task number!");
                 }
-            } else {
-                // store commands entered by the user
-                itemArray[currIndex++] = input;
-                System.out.println("Added: " + input);
+            } else if (input.startsWith("unmark")) { // mark task as not done
+                String taskNumAsString = input.substring(7);
+                try {
+                    int taskNum = Integer.parseInt(taskNumAsString);
+                    Task task = taskList.getTask(taskNum);
+                    if (task != null) {
+                        task.markAsNotDone();
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please specify a task number!");
+                }
+            } else { // store tasks entered by the user
+                taskList.addTask(input);
             }
         }
     }
