5bbda216330ad345239e31f8780b30d4ca1cf8e0
KeithPJX <e0684532@u.nus.edu>
2022-08-14 09:52:34 -0700

    Add add and list functions

--- src/main/java/Gibson.java
+++ src/main/java/Gibson.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Gibson {
     public static void main(String[] args) {
@@ -9,6 +10,7 @@ public class Gibson {
         "| | |_ | | | |  _ < \\___ \\| |  | | . ` |\n" +
         "| |__| |_| |_| |_) |____) | |__| | |\\  |\n" +
         "\\______|_____|____/|_____/ \\____/|_| \\_|\n";
+        ArrayList<String> list = new ArrayList<String>();
 
         // Prints introduction
         System.out.println(line);
@@ -25,9 +27,18 @@ public class Gibson {
                 System.out.println("Bye. Hope to see you soon!");
                 System.out.println(line);
                 break;
-            } else {
+            } else if (input.equals("list")) {
+                int count = 1;
                 System.out.println(line);
-                System.out.println(input);
+                for (String s : list) {
+                    System.out.println(count + ". " + s);
+                    count++;
+                }
+                System.out.println(line);
+            } else {
+                list.add(input);
+                System.out.println(line);
+                System.out.println("added: " + input);
                 System.out.println(line);
             }
         }
