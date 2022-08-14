95ec447603ba10ac75cae8ba4797b329672c0361
ElginL <elginlee_2000@hotmail.com>
2022-08-14 14:47:57 +0800

    add Duke's ability to add items and list when required

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,41 @@
 import java.util.Scanner;
 
 public class Duke {
+    /** List of items stored by Duke. */
+    private static final String[] list = new String[100];
+
+    /** Total number of items in list. */
+    private static int itemsCount = 0;
+
     private static void greetUser() {
         String message = Duke.formatText("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println(message);
     }
 
-    private static void startEchoing() {
+    private static void startService() {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             String userInput = sc.nextLine();
             if (userInput.equals("bye")) {
-                System.out.println(Duke.formatText("Bye. Hope to see you again soon!\n"));
+                System.out.println(Duke.formatText("Bye. Hope to see you again soon!"));
                 break;
             }
 
-            System.out.println(Duke.formatText(userInput));
+            if (userInput.equals("list")) {
+                StringBuilder string = new StringBuilder("");
+                for (int i = 0; i < Duke.itemsCount; i++) {
+                    int itemIndex = i + 1;
+                    string.append(itemIndex).append(". ").append(Duke.list[i]).append("\n");
+                }
+
+                System.out.println(Duke.formatText(string.toString()));
+                continue;
+            }
+
+            Duke.list[Duke.itemsCount++] = userInput;
+
+            System.out.println(Duke.formatText("added: " + userInput));
         }
     }
 
@@ -40,6 +59,6 @@ public class Duke {
 
     public static void main(String[] args) {
         Duke.greetUser();
-        Duke.startEchoing();
+        Duke.startService();
     }
 }
