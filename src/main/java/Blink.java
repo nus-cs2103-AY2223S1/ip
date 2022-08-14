db9531349ccb127aeec1ab8feb94f5d67f3cd9ee
maxng17 <maxngjl@gmail.com>
2022-08-15 01:40:26 +0800

    Add ToDo, Event, Deadline * Handle new commands (e.g. event) * Handle non-valid commands

--- src/main/java/Blink.java
+++ src/main/java/Blink.java
@@ -18,52 +18,101 @@ public class Blink {
         System.out.println("Bye bye~ Glad to be of service :D");
     }
 
+    public void list() {
+        System.out.println("Here are the tasks in your list:");
+        for(int x = 0; x < this.count; x++) {
+            System.out.println(x+1 + ". " + this.store[x].toString());
+        }
+    }
+
+    public void unMark(String[] input) {
+        if (input.length == 2) {
+            int pos = Integer.parseInt(input[1]);
+            if (pos < 1 || pos > count) {
+                System.out.println("Number input into command not accepted.");
+                return;
+            }
+            this.store[pos - 1].unMark();
+        } else {
+            System.out.println("Incorrect command. Missing task number.");
+        }
+    }
+
+    public void mark(String[] input) {
+        if (input.length == 2) {
+            int pos = Integer.parseInt(input[1]);
+            if (pos < 1 || pos > count) {
+                System.out.println("Number input into command not accepted.");
+                return;
+            }
+            this.store[pos - 1].mark();
+        } else {
+            System.out.println("Incorrect command. Missing task number.");
+        }
+    }
+
+    public void toDos(String input) {
+        ToDos temp = new ToDos(input.strip());
+        this.store[count] = temp;
+        this.count++;
+        System.out.println("Alright, this task has been successfully added!\n" +
+                temp + "\n" +
+                "Total of " + this.count + " tasks now");
+    }
+
+    public void deadlines(String input) {
+        String[] sep = input.split("/by");
+        Deadlines temp = new Deadlines(sep[0].strip(), sep[1].strip());
+        this.store[count] = temp;
+        this.count++;
+        System.out.println("Alright, this task has been successfully added!\n" +
+                temp + "\n" +
+                "Total of " + this.count + " tasks now");
+    }
+
+    public void events(String input) {
+        String[] sep = input.split("/at");
+        Events temp = new Events(sep[0].strip(), sep[1].strip());
+        this.store[count] = temp;
+        this.count++;
+        System.out.println("Alright, this task has been successfully added!\n" +
+                temp + "\n" +
+                "Total of " + this.count + " tasks now");
+    }
+
     public void start(Scanner sc) {
         this.welcome();
 
         while(sc.hasNext()) {
-            String[] input = sc.nextLine().strip().split(" ");
+            String[] input = sc.nextLine().strip().split(" ",2);
+            if (input[0].isEmpty()) {
+                continue;
+            }
             switch (input[0]) {
                 case "bye":
                     this.goodbye();
                     this.endSession = true;
                     break;
                 case "list":
-                    System.out.println("Here are the tasks in your list:");
-                    for(int x = 0; x < this.count; x++) {
-                        System.out.println(x+1 + ". " + this.store[x].toString());
-                    }
+                    this.list();
                     break;
                 case "unmark":
-                    if (input.length == 2) {
-                        int pos = Integer.parseInt(input[1]);
-                        if (pos < 1 || pos > count) {
-                            System.out.println("Number input into command not accepted.");
-                            break;
-                        }
-                        this.store[pos - 1].unMark();
-                    } else {
-                        System.out.println("Incorrect command. Missing task number.");
-                    }
+                    this.unMark(input);
                     break;
                 case "mark":
-                    if (input.length == 2) {
-                        int pos = Integer.parseInt(input[1]);
-                        if (pos < 1 || pos > count) {
-                            System.out.println("Number input into command not accepted.");
-                            break;
-                        }
-                        this.store[pos - 1].mark();
-                    } else {
-                        System.out.println("Incorrect command. Missing task number.");
-                    }
+                    this.mark(input);
+                    break;
+                case "event":
+                    this.events(input[1]);
+                    break;
+                case "deadline":
+                    this.deadlines(input[1]);
+                    break;
+                case "todo":
+                    this.toDos(input[1]);
                     break;
                 default:
-                    String attach = String.join(" ",input);
-                    System.out.println(attach);
-                    this.store[count] = new Task(attach);
-                    this.count++;
-                    System.out.println("added task: " + attach);
+                    System.out.println("Unknown command inputted");
             }
             System.out.println(Blink.SPACING);
             if (this.endSession) {
