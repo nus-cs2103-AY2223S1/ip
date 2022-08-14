3ffe4c853c4936265f13c081d20de6edca1f2568
lingg02 <89027464+lingg02@users.noreply.github.com>
2022-08-14 13:17:12 +0800

    Implemented increment for level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "_______________________________\n";
+        System.out.println(line +
+                "Hello I'm Duke\n" +
+                "What can I do for you?\n" +
+                line
+        );
+        String input = "";
+        while (!input.equals("bye")) {
+            Scanner scan = new Scanner(System.in);
+            input = scan.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line +
+                        "Bye. Hope to see you again soon!\n" +
+                        line);
+            } else {
+                System.out.println(line + input + "\n" + line);
+            }
+        }
     }
 }
