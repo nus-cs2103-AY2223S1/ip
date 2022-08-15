180e034f29a29aaeb824d4728648d5bd22d0bf8c
chm252 <chinhongming252@gmail.com>
2022-08-15 18:19:47 +0800

    ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,14 +13,28 @@ public class Duke {
             } else if (userInput.startsWith("unmark ")) {
                 int intCollect = Integer.parseInt(userInput.substring(7));
                 storage[intCollect - 1].unmark();
-            }
-            else if (userInput.equals("list")) {
+            } else if (userInput.equals("list")) {
                 int j = 0;
                 while (storage[j] != null) {
                     System.out.println((j + 1) + ". " + storage[j].toString());
                     j++;
                 }
-            } else {
+            } else if (userInput.startsWith("todo ")) {
+                storage[i] = new ToDo(userInput.substring(5));
+                i++;
+                System.out.println("Good luck... You already have " + i + " tasks in your list :p");
+            } else if (userInput.startsWith("deadline ")) {
+                String[] box = userInput.substring(9).split(" /by ");
+                storage[i] = new Deadline(box[0], box[1]);
+                i++;
+                System.out.println("Good luck... You already have " + i + " tasks in your list :p");
+            } else if (userInput.startsWith("event ")) {
+                String[] box = userInput.substring(6).split(" /at ");
+                storage[i] = new Event(box[0], box[1]);
+                i++;
+                System.out.println("Good luck... You already have " + i + " tasks in your list :p");
+            }
+            else {
                 storage[i] = new Task(userInput);
                 i++;
                 System.out.println("added: " + userInput);
