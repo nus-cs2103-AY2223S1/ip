50ce95c3973b3a0258c9d929757792d769a76e03
Berted <46164341+Berted@users.noreply.github.com>
2022-08-15 23:14:52 +0800

    Modify user query to be split into command and arguments
    User query is a single string. This makes it difficult to
    create commands with custom arguments, such as the `mark` command
    from Level-3.
    
    As such, let's divide the user query into two strings,
    a "command" string and an "argument" string, split
    by whitespace of some length.
    
    Splitting it in this manner keeps the syntax familiar to users
    familiar with the terminal, whilst maintaining the ability to
    have whitespaces for arguments without dealing with escape
    characters.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,8 @@ public class Duke {
     private static final String GREETING_MESSAGE = "Hello! I'm TedBot ヾ(≧▽≦*)o\n"
             + "What do you want to do today?";
     private static final String GOODBYE_MESSAGE = "Bye! Hope to see you soon ༼- つ ◕_◕ ༽つ";
+    private static final String UNKNOWN_COMMAND_MESSAGE = "Sorry, I don't know what that means.\n"
+            + "Did you make a mistake? Please note that commands are case-sensitive.";
     private static final int TASKLIST_MAX_SIZE = 100;
 
     private static Task[] taskList = new Task[TASKLIST_MAX_SIZE];
@@ -18,8 +20,18 @@ public class Duke {
         System.out.println(GREETING_MESSAGE);
 
         while (!exitCalled) {
-            String userQuery = sysIn.nextLine();
-            switch (userQuery) {
+            /* Splits a user query into two, the command and its arguments,
+               assumed to be seperated by whitespace (can be multiple characters).
+             */
+            String[] userQuery = sysIn.nextLine().split("\\s+", 2);
+
+            if (userQuery.length == 0) {
+                // TODO: Add Exception when this occurs.
+                System.out.println(UNKNOWN_COMMAND_MESSAGE);
+                continue;
+            }
+
+            switch (userQuery[0]) {
                 case "list":
                     for (int i = 0; i < taskCount; i++) {
                         System.out.printf("%02d. %s\n", i + 1, taskList[i]);
@@ -29,10 +41,13 @@ public class Duke {
                     System.out.println(GOODBYE_MESSAGE);
                     exitCalled = true;
                     break;
-                default:
+                case "add":
                     assert taskCount < TASKLIST_MAX_SIZE;
-                    taskList[taskCount++] = new Task(userQuery);
-                    System.out.printf("Successfully added \"%s\" to your task list!\n", userQuery);
+                    taskList[taskCount++] = new Task(userQuery[1]);
+                    System.out.printf("Successfully added \"%s\" to your task list!\n", userQuery[1]);
+                    break;
+                default:
+                    System.out.println(UNKNOWN_COMMAND_MESSAGE);
             }
         }
     }
