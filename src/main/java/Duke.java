d2cc033450983075751ba36d3e8c303a34c61a7e
wrewsama <andrewlzs512@gmail.com>
2022-08-15 17:31:24 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static ArrayList<String> inputs = new ArrayList<>();
+
+    /**
+     * The main program loop.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,14 +20,38 @@ public class Duke {
                            + "\nWhat can I do for you?");
 
         Scanner sc = new Scanner(System.in);
-        while(true) {
+        while (true) {
             String inputText = sc.nextLine();
             if (inputText.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (inputText.equals("list")){
+                Duke.printItems();
             } else {
-                System.out.println(inputText);
+                Duke.addItem(inputText);
+                System.out.println("added: " + inputText);
             }
         }
+    }
+
+    /**
+     * Add an item to the items list.
+     *
+     * @param input A String to be added to the list.
+     */
+    public static void addItem(String input) {
+        Duke.inputs.add(input);
+    }
+
+
+    /**
+     * Prints all items in the items list.
+     */
+    public static void printItems() {
+        for (int i = 0; i < Duke.inputs.size(); i++) {
+            String output = String.format("%s. %s", i + 1, Duke.inputs.get(i));
+            System.out.println(output);
+        }
+
     }
 }
