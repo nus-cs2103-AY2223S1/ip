05e368f98ac5d4633c728eed72023cf8a8121317
lzq <li.zhaoqi@dhs.sg>
2022-08-16 02:24:06 +0800

    dynamic array and support for commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Duke {
     private static final String LOGO = "Welcome to\n"
             + " ____        _        \n"
@@ -11,50 +14,59 @@ public class Duke {
             + "What can I do for you?";
 
     private static final String OUTRO = "Bye. Hope to see you again soon!";
+    private static final String MARKED = "Nice! I've marked this task as done:%n  %s";
+    private static final String UNMARKED = "OK, I've marked this task as not done yet:%n  %s";
 
     private DukeIO userIO;
+    private Parser parser;
 
-    private static String[] COMMANDS = {
-            "bye"
-    };
-
-    private String[] tasks;
-    private int index;
+    private List<Task> tasks;
 
     Duke() {
         userIO = new DukeIO();
-        tasks = new String[100];
-        index = 0;
+        tasks = new ArrayList<>();
+        parser = new Parser();
         userIO.printTask(LOGO, 2);
         userIO.printTask(INTRO, 3);
     }
 
     boolean handleInput() {
-        String response = userIO.readLine();
-        if (response.equals("bye")) {
-            userIO.printTask(OUTRO, 3);
-            return false;
-        } else if (response.equals("list")) {
-            listAllTasks();
-            return true;
-        }
+        String txt = userIO.readLine();
+        ParsedData data = parser.parse(txt);
+        int index;
+        switch (data.command) {
+            case "bye":
+                userIO.printTask(OUTRO, 3);
+                return false;
 
-        // userIO.printTask(response);
-        addTasks(response);
-        return true;
+            case "list":
+                listAllTasks();
+                return true;
+
+            case "mark":
+                index = Integer.parseInt(data.description) - 1;
+                tasks.get(index).mark();
+                userIO.printTask(String.format(MARKED, tasks.get(index)));
+                return true;
+
+            case "unmark":
+                index = Integer.parseInt(data.description) - 1;
+                tasks.get(index).unmark();
+                userIO.printTask(String.format(UNMARKED, tasks.get(index)));
+                return true;
+            default:
+                tasks.add(new Task(data.raw));
+                userIO.printTask(String.format("added: %s", data.raw));
+                return true;
+        }
     }
 
     void listAllTasks() {
         userIO.printLine();
-        for (int i = 0; i < index; ++i) {
-            userIO.printTask(String.format("%d. %s", i + 1, tasks[i]), 2);
+        for (int i = 0; i < tasks.size(); ++i) {
+            userIO.printTask(String.format("%d. %s", i + 1, tasks.get(i)), 2);
         }
         userIO.printLine();
-    }
-
-    void addTasks(String txt) {
-        tasks[index++] = txt;
-        userIO.printTask(String.format("added: %s", txt));
     }
 
     public static void main(String[] args) {
