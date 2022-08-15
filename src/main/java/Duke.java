badaadf356a25b5ed0fc82bc3439cf5e5fe12bd6
Rama <kmram0908@gmail.com>
2022-08-15 13:37:03 +0800

    implemented duke level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,23 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String divider = "____________________________________________________________\n";
+        String introMsg = "Baby Yoda I am\nFor you, what can I do?\n";
+        String byeMsg = "See you soon, I will\n";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println(divider + introMsg + divider);
+
+        Scanner in = new Scanner(System.in);
+        String s = "";
+
+        while (true) {
+            s = in.nextLine();
+            if (s.equals("bye"))
+                break;
+            System.out.println(divider + "\t" + s + "\n" + divider);
+        }
+
+        System.out.println(divider + "\t" + byeMsg + divider);
     }
 }
