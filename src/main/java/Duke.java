0d64504c93b4b8a0aeff9d05543cef9c094e6471
Marcusgwj <Marcusgwj@hotmail.com>
2022-08-16 03:37:32 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
     private static final String LINE = "____________________________________________________________";
     private static final String GREETING = LINE + "\n" + "Hello! I am Duke\n" + "What can I do for you?\n" + LINE;
     private static final String GOODBYE = LINE + "\n" + "Bye. Hope to see you again soon!" + "\n" + LINE;
-    private static final List<String> taskList = new ArrayList<>();
+    private static final List<Task> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,19 +14,57 @@ public class Duke {
 
         while (scanner.hasNext()) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
-                scanner.close();
-                System.out.println(GOODBYE);
-                break;
-            } else if (input.equals("list")) {
-                System.out.println(LINE + "\n");
-                for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println((i + 1) + ". " + taskList.get(i) + "\n");
+            String[] inputArr = input.split(" ", 2);
+            String command = inputArr[0];
+            switch (command) {
+                case "bye":
+                    scanner.close();
+                    System.out.println(GOODBYE);
+                    break;
+                case "list":
+                    System.out.println(LINE + "\n");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println((i + 1) + ". " + taskList.get(i) + "\n");
+                    }
+                    System.out.println(LINE + "\n");
+                    break;
+                case "mark": {
+                    int index = Integer.parseInt(inputArr[1]) - 1;
+                    if (index < 0 || index >= taskList.size()) {
+                        System.out.println(LINE + "\n"
+                                + "Invalid task!" + "\n"
+                                + LINE + "\n");
+                    } else {
+                        Task task = taskList.get(index);
+                        task.markDone();
+                        System.out.println(LINE + "\n"
+                                + "Nice! I've marked this task as done:" + "\n"
+                                + task + "\n"
+                                + LINE + "\n");
+                    }
+                    break;
                 }
-                System.out.println(LINE + "\n");
-            } else {
-                taskList.add(input);
-                System.out.println(LINE + "\n" + "added: " + input + "\n" + LINE);
+                case "unmark": {
+                    int index = Integer.parseInt(inputArr[1]) - 1;
+                    if (index < 0 || index >= taskList.size()) {
+                        System.out.println(LINE + "\n"
+                                + "Invalid task!" + "\n"
+                                + LINE + "\n");
+                    } else {
+                        Task task = taskList.get(index);
+                        task.markUndone();
+
+                        System.out.println(LINE + "\n"
+                                + "OK, I've marked this task as not done yet:" + "\n"
+                                + task + "\n"
+                                + LINE + "\n");
+                    }
+                    break;
+                }
+                default:
+                    taskList.add(new Task(input));
+                    System.out.println(LINE + "\n" + "added: " + input + "\n" + LINE);
+                    break;
             }
         }
     }
