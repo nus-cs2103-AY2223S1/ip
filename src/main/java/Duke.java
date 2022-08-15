f3c3752043088649051fef0cd34e91da841fb7f1
leecaregene <e0764279@u.nus.edu>
2022-08-16 01:23:55 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        prettyPrint("Hello! I'm Duke\nWhat can I do for you?");
+        while (in.hasNextLine()) {
+            String command = in.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                prettyPrint("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                prettyPrint(command);
+            }
+        }
+    }
+
+    private static void prettyPrint(String text) {
+        String[] lines = text.split("\n");
+        StringBuilder temp = new StringBuilder("    ____________________________________________________________\n");
+        for (String line : lines) {
+            temp.append("     ");
+            temp.append(line);
+            temp.append("\n");
+        }
+        temp.append("    ____________________________________________________________\n");
+        System.out.println(temp.toString());
     }
 }
