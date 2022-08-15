75d91a34e68e04ccaa66c3a557978f3aecb4450a
waynezsy <waynezsy0317@gmail.com>
2022-08-15 15:49:16 +0800

    Duke class: change formatting of messages
    Duke prints messages on successive lines.
    
    Lines and indentations differentiate messages between users and Duke
    more clearly.
    
    Let's add lines and indentations to Duke's messages.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final String INDENTATION = "    ";
+    private static final String LINE = INDENTATION + "__________________________________________";
+    private static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + LOGO);
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println(LINE + "\n" + INDENTATION + "Hello! I'm Duke\n"
+                + INDENTATION + "What can I do for you?\n" + LINE);
 
         String s = sc.nextLine();
         while (!s.equals("bye")) {
-            System.out.println(s);
+            System.out.println(LINE + "\n" + INDENTATION + s + "\n" + LINE);
             s = sc.nextLine();
         }
 
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE + "\n" + INDENTATION + "Bye. Hope to see you again soon!");
         sc.close();
     }
 }
