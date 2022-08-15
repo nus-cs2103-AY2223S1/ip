2b4731d11f274678f38c66fbcf0a51c2510190a6
Jerry <lanjingbo114@163.com>
2022-08-16 01:56:45 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String greet = "Hello! I'm Duke\n"
+                + "What can I do for you?";
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+        System.out.println(greet);
+        while (true) {
+            String s = sc.next();
+            if (s.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(s);
+            sc = new Scanner(System.in);
+        }
+        sc.close();
+        //
     }
 }
