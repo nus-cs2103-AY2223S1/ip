12a34caf30010a810d1585a66eed1611b90c4f8b
Julian Wong <e0725200@u.nus.edu>
2022-08-15 17:30:46 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
+        boolean bye = false;
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("HELLO!");
+
+        while (!bye) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                bye = true;
+                System.out.println("Bye Bye!");
+            } else {
+                System.out.println(input);
+            }
+        }
+
     }
 }
