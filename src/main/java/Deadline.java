bc70f7796b07f187e3b17b7f727d2f5db99b5323
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-08-15 14:50:19 +0800

    Add different types of events namely Deadlines, Events and Todos and Modify Task field's access markers
    Deadline, Events and Todos are all types of Tasks and we make use to inheritance to share code. We also modify the access marker of the Task's fields so as to allow the fields to be accessible from the subclasses we created.

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,8 @@
+public class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,8 @@
+public class Event extends Task {
+    protected String at;
+
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+}
