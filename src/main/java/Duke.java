97c059fe95787b42d03669c2a17cc1446d376d98
EiffelLKF <e0726757@u.nus.edu>
2022-08-15 20:15:06 +0800

    Completed Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 
+        // Greets User
+        System.out.println("Hey there! I'm Tutter! \nHow can I help?");
+
+        // Receive Input
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        while (true) {
+            str = sc.next();
+            if (str.equals("bye")) {
+                break;
+            }
+
+            // Echo
+            System.out.println("\t" + str);
+        }
+
+        // Exits Program
+        System.out.println("\tGoodbye!");
     }
 }
