cc6a8502bc9ce19aa9fe5f2c6128f354c805d83b
Piyotato <piyotatopiyo@gmail.com>
2022-08-16 00:25:07 +0800

    Implement list system
    - Input is now stored into a list instead simply being repeated
    - Add "list" command which prints the items in the list in an enumerated manner
    - Move all commands into constant strings for ease of access
    - Add documentation for methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,31 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Duke {
 
+    /**
+     * The greeting message used when main is loaded.
+     */
     private static final String greetingMessage = "Hello! I'm Duke\nWhat can I do for you?";
+
+    /**
+     * The exit message used when the user issues the exit command.
+     */
     private static final String exitMessage = "Bye. Hope to see you again soon!";
+
+    /**
+     * A list to contain the items given by the user.
+     */
+    private static ArrayList<String> list;
+
+    /**
+     * Runs the Duke chatbot.
+     * @param args Input arguments.
+     */
     public static void main(String[] args) {
+
+        list = new ArrayList<>();
 
         sendMessage(greetingMessage);
         Scanner userInput = new Scanner(System.in);
@@ -18,17 +38,62 @@ public class Duke {
 
     }
 
+    /**
+     * Parses the user's input.
+     * @param input Input from the user.
+     * @return Whether the program should continue receiving input.
+     */
     private static boolean parseInput(String input) {
+
+        final String exitCommand = "bye";
+        final String displayListCommand = "list";
+
         switch (input) {
-            case "bye":
+
+            case exitCommand:
                 sendMessage(exitMessage);
                 return false;
+
+            case displayListCommand:
+                displayList(list);
+                break;
+
             default:
-                sendMessage(input);
-                return true;
+                addToList(input);
+                break;
+
         }
+
+        return true;
     }
 
+    /**
+     * Adds an item to the static list.
+     * @param item Item to add to the list.
+     */
+    private static void addToList(String item) {
+        list.add(item);
+        sendMessage("added: " + item);
+    }
+
+    /**
+     * Displays the items in a string list, enumerated from 1.
+     * @param list The list of strings to display.
+     */
+    private static void displayList(ArrayList<String> list) {
+        String output = "";
+        for (int i = 0; i < list.size(); i++) {
+            output += i == 0
+                ? (i + 1) + ". " + list.get(i)
+                : "\n" + (i + 1) + ". " + list.get(i);
+        }
+        sendMessage(output);
+    }
+
+    /**
+     * Prints a message into the line with the relevant formatting.
+     * @param message Message to print.
+     */
     private static void sendMessage(String message) {
 
         String indentation = "    ";
