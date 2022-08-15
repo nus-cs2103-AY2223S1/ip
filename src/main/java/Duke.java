ca8f43372cbc2244ba9c5a52c2bfa321d85c7a5d
yellow-294 <77526609+yellow-294@users.noreply.github.com>
2022-08-15 22:26:36 +0800

    Completed Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String logo = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
@@ -18,19 +19,27 @@ public class Duke {
                 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠛⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
         String lineBreak = "°.✩┈┈∘*┈\uD83C\uDF19┈*∘┈┈✩.°";
         System.out.println(logo + "\n" + "Hello I am LUNA!\n" + "How can I be of help?\n" + lineBreak);
-
+        ArrayList<String> arrayList =  new ArrayList<>(100);
         Scanner sc = new Scanner(System.in);
 
         while(true) {
             String input = sc.nextLine();
-            String farewell = lineBreak + "\n" +"゜✧*̣̩☽⋆゜LUNA bids farewell ゜✧*̣̩☽⋆゜\n\n May the Moon\uD83C\uDF19 shine bright and illuminate your night.";
+            String farewell = lineBreak + "\n" + "゜✧*̣̩☽⋆゜LUNA bids farewell ゜✧*̣̩☽⋆゜\n\n May the Moon\uD83C\uDF19 shine bright and illuminate your night.";
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(farewell);
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                int counter = 1;
+                System.out.println(lineBreak);
+                for (String s: arrayList) {
+                    System.out.println(counter + ". " + s);
+                    counter++;
+                }
             } else {
+                arrayList.add(input);
+                System.out.println(lineBreak + "\n" + "LUNA has added the task: " + input + "\n" + lineBreak);
 
-                System.out.println(lineBreak+ "\n" + input + "\n" + lineBreak);
             }
         }
     }
