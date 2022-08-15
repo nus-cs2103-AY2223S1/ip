bc07b82fdd636973c6bc73725a692569ca1fa190
sarrrdin <sarrrdin@gmail.com>
2022-08-16 00:03:42 +0800

    Add additional variable - type of task.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,21 +10,27 @@ import java.util.*;
 public class Task {
     private String task;
     private boolean done;
+    private String taskType;
 
-    public Task(String task) {
+    public Task(String task, String taskType) {
         this.task = task;
         this.done = false;
+        this.taskType = taskType;
     }
 
     public String getTask() {
         return this.task;
     }
 
+    public String getTaskType() {
+        return this.taskType;
+    }
+
     public String getTaskList() {
         if (!this.done) {
-            return "[ ] " + this.task;
+            return "[" + this.taskType + "]" + "[ ] " + this.task;
         } else {
-            return "[X] " + this.task;
+            return "[" + this.taskType + "]" +"[X] " + this.task;
         }
     }
 
