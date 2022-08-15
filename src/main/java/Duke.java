d3ce7aa88ad1b4b03dea66232f1e3e9bd6e99ffa
aarontangr <lapselol@gmail.com>
2022-08-15 15:24:23 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+      System.out.println("hello");
+      System.out.println("can i help you?");
+
+      Scanner sc = new Scanner(System.in);
+      String s = sc.nextLine();
+      while (!s.equals("yes")) {
+        System.out.println(s);
+        s = sc.nextLine();
+      }
+
+      System.out.println("peace");
     }
 }
