02d806ca93bf209aa1af645d52c29f4cd3f0e29b
Shawn Kok <shawnkok00@gmail.com>
2022-08-15 17:49:02 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
+        List<String> store = new ArrayList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello! I'm Duke");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println("\t____________________________________________________________\n");
+        System.out.println("Hello! This is \n" + logo + "\nat your service!!");
+        System.out.println("________________________________________________________________");
+        System.out.println("How may I serve you today, Master?");
+        System.out.println("\t============================================================\n");
 
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
 
         while (!command.equals("bye")) {
             System.out.println("\t____________________________________________________________");
-            System.out.println("\t"+ command);
+            if (command.equals("list")) {
+                for (int i = 0; i < store.size(); i++) {
+                    int index = i + 1;
+                    System.out.println("\t" + index + ". " + store.get(i));
+                }
+            } else {
+                System.out.println("\tadded: " + command);
+                store.add(command);
+            }
             System.out.println("\t____________________________________________________________\n");
             command = sc.nextLine();
         }
 
         System.out.println("\t____________________________________________________________");
-        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\tBye Master! It was a pleasure serving you, see you again soon!");
         System.out.println("\t____________________________________________________________");
     }
 }
