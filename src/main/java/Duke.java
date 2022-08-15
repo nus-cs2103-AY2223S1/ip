75859fea8454ab5b16b1e871dd2c7b0fe98dc92a
deeyonn <deeyonn@gmail.com>
2022-08-15 15:02:34 +0800

    Add Level-1 implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,60 @@
+import java.util.Scanner;
+
+/**
+ * Starting point of Duke app.
+ * CS2103 Week 2
+ * AY21/22 Semester 1
+ *
+ * @author Deon
+ */
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        print_line();
+        print("Hello! I'm Duke");
+        print("What can I do for you?");
+        print_line();
+        echo();
+        exit();
+    }
+
+    /**
+     * Prints a horizontal line to the console.
+     */
+    private static void print_line() {
+        System.out.print("\t____________________________________________________________\n");
+    }
+
+    /**
+     * Prints a String to the console.
+     * @param printable String to be printed.
+     */
+    private static void print(String printable) {
+        System.out.print("\t" + printable + "\n");
+    }
+
+    /**
+     * Echos user input to the console.
+     * Loops until the keyword is detected.
+     */
+    private static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String arg = scanner.next();
+        while(!arg.equals("bye")) {
+            print_line();
+            print(arg);
+            print_line();
+            arg = scanner.next();
+        }
+        scanner.close();
+    }
+
+    /**
+     * Prints the exit message to the console.
+     */
+    private static void exit() {
+        print_line();
+        print("Bye. Hope to see you again soon!");
+        print_line();
     }
 }
