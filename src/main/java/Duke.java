0a90de5edec6a32cdcd15817f85b3a44131aabfa
rama-pang <rama.a.pangestu@gmail.com>
2022-08-15 14:52:16 +0800

    Added Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,38 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?\n");
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String userInput = scanner.nextLine();
+            switch (parseInput(userInput)) {
+                case BYE:
+                    System.out.println("Bye. Hope to see you again soon!\n");
+                    return;
+
+                case ECHO:
+                    System.out.println(userInput);
+                    break;
+
+                default:
+                    // do nothing
+            }
+        }
+    }
+
+    private enum InstructionType {
+        BYE,
+        ECHO
+    }
+
+    private static InstructionType parseInput(String input) {
+        switch (input.trim()) {
+            case "bye":
+                return InstructionType.BYE;
+
+            default:
+                return InstructionType.ECHO;
+        }
     }
 }
