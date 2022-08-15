53d2f3bd0a7a70f28ec440860057d4efaadfbc6c
Tan Jin Waye <e0694446@u.nus.edu>
2022-08-15 12:55:40 +0800

    Duke.java: Refactored handler() into smaller methods.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -32,26 +32,38 @@ public class Duke {
 
         switch(command) {
             case "bye":
-                System.out.println(DIVIDER + "Bye. Hope to see you again soon!\n" + DIVIDER);
+                exit();
                 break;
             case "list":
-                if (tasks.isEmpty()) {
-                    System.out.println(DIVIDER + "List is empty\n" + DIVIDER);
-                } else {
-                    System.out.print(DIVIDER);
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println((i+1) + ". " + tasks.get(i));
-                    }
-                    System.out.println(DIVIDER);
-                }
-                handler();
+                list();
                 break;
             default:
-                text = command;
-                tasks.add(text);
-                System.out.println(DIVIDER + "added: " + text + "\n" + DIVIDER);
-                handler();
+                listAdd(command);
+        }
+
+        if (!command.equals("bye")) { handler(); }
+
+    }
+
+    // sub methods associated with handler() for each user input case
+    private static void exit() {
+        System.out.println(DIVIDER + "Bye. Hope to see you again soon!\n" + DIVIDER);
+    }
+
+    private static void list() {
+        if (tasks.isEmpty()) {
+            System.out.println(DIVIDER + "List is empty\n" + DIVIDER);
+        } else {
+            System.out.print(DIVIDER);
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i+1) + ". " + tasks.get(i));
+            }
+            System.out.println(DIVIDER);
         }
     }
 
+    private static void listAdd(String item) {
+        tasks.add(item);
+        System.out.println(DIVIDER + "added: " + item + "\n" + DIVIDER);
+    }
 }
