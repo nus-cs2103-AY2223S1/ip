192c020efe429676a7f2f41f94feb234db41e038
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-08-15 08:27:27 +0800

    Rename chatbot to Mia

---
+++ src/main/java/Mia.java
@@ -0,0 +1,8 @@
+public class Mia {
+    public static void main(String[] args) {
+        String logo = "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n" +
+                      "┃ You are talking to MIA... ┃\n" +
+                      "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛";
+        System.out.println(logo);
+    }
+}
