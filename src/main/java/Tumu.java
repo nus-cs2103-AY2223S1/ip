77e70607137d8ae1977b592e6e97d254b1a33105
Eric Lee Ying Yao <e0694459@u.nus.edu>
2022-08-15 17:20:12 +0800

    Echoes user response and exits when 'bye' entered

--- src/main/java/Tumu.java
+++ src/main/java/Tumu.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
+
 public class Tumu {
     public static void main(String[] args) {
         greeting();
+        response();
+    }
+
+    private static void response() {
+        /**
+         * Receives user response and replies accordingly.
+         */
+
+        Scanner sc = new Scanner(System.in);
+        final String endChatBotMessage = "bye";
+        String userInput;
+        do {
+            userInput = sc.next().toLowerCase();
+            switch (userInput) {
+                case endChatBotMessage:
+                    goodbye();
+                    break;
+                default:
+                    System.out.println(userInput);
+            }
+
+        } while (!userInput.equalsIgnoreCase(endChatBotMessage));
     }
 
     private static void greeting() {
+        /**
+         * Greeting message to the user during chat-bot startup.
+         */
+
         String logo = "" +
                 " ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄   ▄▄ ▄▄   ▄▄ \n" +
                 "█       █  █ █  █  █▄█  █  █ █  █\n" +
@@ -16,5 +44,16 @@ public class Tumu {
                 "What is on your mind today?\n";
 
         System.out.println(logo + greetingMessage);
+    }
+
+    private static void goodbye() {
+        /**
+         * Says goodbye to the user.
+         * User exits the chat-bot.
+         */
+
+        String goodbyeMessage = "Goodbye, and have a nice day ahead!\n";
+        String smileyFace = "٩(ˊᗜˋ )و\n";
+        System.out.println(goodbyeMessage + smileyFace);
     }
 }
