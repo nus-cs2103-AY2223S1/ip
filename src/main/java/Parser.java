54ed39b62ee087e8761d6d9c9d1420cde2f08f09
lzq <li.zhaoqi@dhs.sg>
2022-08-16 02:07:20 +0800

    Added parser to split space and /

---
+++ src/main/java/Parser.java
@@ -0,0 +1,27 @@
+/**
+ * Parser
+ */
+class Parser {
+    private static final String SPACE = " +";
+    private static final String SEP = "/";
+
+    Parser() {
+    }
+
+    ParsedData parse(String txt) {
+        String[] parsedTmp = txt.split(SPACE, 2);
+
+        if (parsedTmp.length == 1 || parsedTmp[1].equals("")) {
+            return new ParsedData(txt, parsedTmp[0]);
+        }
+
+        String command = parsedTmp[0];
+        parsedTmp = parsedTmp[1].split(SEP, 2);
+
+        if (parsedTmp.length == 1 || parsedTmp[1].equals("")) {
+            return new ParsedData(txt, command, parsedTmp[0].trim());
+        }
+
+        return new ParsedData(txt, command, parsedTmp[0].trim(), parsedTmp[1].trim());
+    }
+}
