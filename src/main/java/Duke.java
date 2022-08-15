c2a5c17db7a4e7deadc3b1eaf1f94e4a432af05d
vantemoon <sylviassunrx0820@outlook.com>
2022-08-15 13:47:23 +0800

    Implemented echo and exit with modified output format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "     _   _    ______     _____ ____\n"
@@ -5,15 +7,35 @@ public class Duke {
                 + " _  | |/ _ \\ | |_) \\ \\ / / | |\\___ \\\n"
                 + "| |_| / ___ \\|  _ < \\ V /  | | ___) |\n"
                 + " \\___/_/   \\_\\_| \\_\\ \\_/  |___|____/\n";
-        String line = "____________________________________________________________";
+
+
         System.out.println(logo);
-        System.out.println(line);
-        say("Hello. I'm Jarvis");
-        say("What can I do for you?");
-        System.out.println(line);
+
+        say("Hello. I'm Jarvis", true, false);
+        say("What can I do for you?", false, true);
+
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                say("Bye. Hope to see you again soon.", true, true);
+                break;
+            }
+            else {
+                say(userInput, true, true);
+            }
+        }
     }
 
-    public static void say(String message) {
+    public static void say(String message, boolean firstLine, boolean lastLine) {
+        String line = "____________________________________________________________";
+        if (firstLine == true) {
+            System.out.println(line);
+        }
         System.out.println(" " + message);
+        if (lastLine == true) {
+            System.out.println(line);
+        }
     }
 }
