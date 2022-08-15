7dc65f195c1770e2363560c241b7010e360ee20b
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-08-15 15:11:57 +0800

    Add RogerInvalidInputException class

---
+++ src/main/java/RogerInvalidInputException.java
@@ -0,0 +1,9 @@
+
+public class RogerInvalidInputException extends Exception{
+    /**
+     * Raised when Roger receives an invalid input.
+     */
+    public RogerInvalidInputException(String message) {
+        super(message);
+    }
+}
