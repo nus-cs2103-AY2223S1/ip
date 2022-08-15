a09a0323c6e252783e30c1c6d1a0f92ae657667e
Prittam Ravi <prittam867@gmail.com>
2022-08-15 18:14:02 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final static String UNDERLINE = "_________________________________";
+    boolean inProcess = true;
+    private String greet(String res) {
+        if (res.toLowerCase().equals("bye")) {
+            inProcess = false;
+            return "Bye. Hope to see you again soon!";
+        } else {
+            return res;
+        }
+    }
+    private void run() {
+        Scanner sc= new Scanner(System.in);
+        System.out.println(UNDERLINE);
+        System.out.println("Hello! I'm Duke!");
+        System.out.println("What can I do for you?");
+        System.out.println(UNDERLINE);
+        while(inProcess) {
+            String a= sc.nextLine();
+            System.out.println(UNDERLINE + "\n" + greet(a) + "\n" + UNDERLINE);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +30,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        new Duke().run();
+        }
     }
-}
+
