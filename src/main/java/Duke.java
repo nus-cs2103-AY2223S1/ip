0bb025612578b8527c907bbe682118ceea8b8e6f
Aloynz <aloysiusneozeen@gmail.com>
2022-08-15 18:16:41 +0800

    Added Add, List Feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         System.out.println("Hello! I'm Luke\nWhat can I do for you?");
         String command = "";
         String quit = "bye";
+        String list = "list";
 
-        while (!command.equals(quit)){
+        while (!command.equals(quit)) {
             command = scanner.nextLine();
-            if (!command.equals(quit)){
-                System.out.println(command);
+
+            if (command.equals(list)) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(String.valueOf(i + 1) + ". " + tasks.get(i));
+                }
+
+            } else if (!command.equals(quit)) {  
+                System.out.println("added: " + command);
+                tasks.add(command);
+                
             } else {
                 System.out.println("Bye! Thanks for using Luke!");
+                scanner.close();
             }
         }
         
-        scanner.close();
+
         
     }
 }
