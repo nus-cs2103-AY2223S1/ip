78339f4c3b972eb99caf5803c0c2e15367ceb0af
zylee348 <e0725436@u.nus.edu>
2022-08-15 15:37:56 +0800

    Level - 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            try {
+                String str1 = sc.nextLine();
+                if (str1.equals("bye")) {
+                    System.out.println("Bye, Hope to see you again soon!");
+                    break;
+                } else {
+                    System.out.println(str1);
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input");
+            }
+        }
+
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
     }
 }
