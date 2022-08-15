99af7953d8855128ab2bc6dd3757730665045378
jia-han <neo.jiahan1234@gmail.com>
2022-08-15 16:18:05 +0800

    Add storing of text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] tasks = new String[100];
+    private static int count = 0;
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
@@ -9,8 +12,14 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
+            } else if (input.equals("list")) {
+                for(int i = 0; i < count; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
             } else {
-                System.out.println(input);
+                tasks[count] = input;
+                count++;
+                System.out.println("added: " + input);
             }
         }
     }
