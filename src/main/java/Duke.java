6059cdb356a84dc68fd2236b0aa78b04f2e33784
linuschancs <mrlinuschan@gmail.com>
2022-08-16 01:35:54 +0800

    Added list functionality where users may store text and display the text they stored by entering 'list'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,8 +6,19 @@ public class Duke {
         System.out.println("Greetings Human! I am BetaGo, your personal robot assistant!\nHow may I assist you today?\n");
         Scanner sc = new Scanner(System.in);
         String str= sc.nextLine();
+        String[] storage = new String[100];
+        int counter = 0;
         while (!str.equalsIgnoreCase("bye")) {
-            System.out.println(str + "\n");
+            if (str.equalsIgnoreCase("list")) {
+                for (int i = 0; i < counter; i++) {
+                    System.out.print(i+1);
+                    System.out.println(". " + storage[i] + "\n");
+                }
+            } else {
+                System.out.println("added: " + str + "\n");
+                storage[counter] = str;
+                counter++;
+            }
             str= sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!\n");
