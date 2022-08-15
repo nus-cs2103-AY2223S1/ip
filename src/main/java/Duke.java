0be233cbf515b8e7a4038f34b07aa785a636011a
leecaregene <e0764279@u.nus.edu>
2022-08-16 01:41:40 +0800

    Complete Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
         prettyPrint("Hello! I'm Duke\nWhat can I do for you?");
         while (in.hasNextLine()) {
             String command = in.nextLine();
             if (command.equalsIgnoreCase("bye")) {
                 prettyPrint("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                StringBuilder temp = new StringBuilder();
+                for (int i = 0; i < tasks.size(); i++) {
+                    temp.append(i+1);
+                    temp.append(". ");
+                    temp.append(tasks.get(i));
+                    temp.append("\n");
+                }
+                prettyPrint(temp.toString());
             } else {
-                prettyPrint(command);
+                tasks.add(command);
+                prettyPrint("added: " + command);
             }
         }
     }
@@ -24,6 +37,6 @@ public class Duke {
             temp.append("\n");
         }
         temp.append("    ____________________________________________________________\n");
-        System.out.println(temp.toString());
+        System.out.println(temp);
     }
 }
