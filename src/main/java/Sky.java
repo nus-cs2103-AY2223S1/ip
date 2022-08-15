b5434f955c3ca3997f8d84e06dbf6b15998c791c
ekweirui <ekweirui@gmail.com>
2022-08-15 15:33:40 +0800

    Add list functionality

--- src/main/java/Sky.java
+++ src/main/java/Sky.java
@@ -4,6 +4,9 @@ public class Sky {
     public static void main(String[] args) {
         System.out.println("Hello from Sky! Your heavenly personal assistant chatbot to help you track your things.");
 
+        String[] arr = new String[100];
+        // index stores the pointer index of arr array
+        int index = 0;
         // Label the while loop
         outerLoop:
         while (true) {
@@ -15,8 +18,17 @@ public class Sky {
                     System.out.println("  Bye. May all your endeavours fly high!");
                     System.out.println("  ____________________________________________________________");
                     break outerLoop;
+                case "list":
+                    for (int i = 0; i < arr.length; i++) {
+                        if (arr[i] == null) {
+                            break;
+                        }
+                        System.out.println("  " + i + ". " + arr[i]);
+                    }
+                    break;
                 default:
-                    System.out.println("  " + s);
+                    arr[index++] = s;
+                    System.out.println("  added: " + s);
                     break;
             }
             System.out.println("  ____________________________________________________________");
