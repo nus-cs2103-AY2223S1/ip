6093fdaebcd1f41773db2082e74427208608e388
chm252 <chinhongming252@gmail.com>
2022-08-15 10:37:05 +0800

    added Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,15 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    //...
+}
