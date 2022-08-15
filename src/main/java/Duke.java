add477293163ebfc092228a873072bd631b24dd2
chm252 <chinhongming252@gmail.com>
2022-08-15 08:44:35 +0800

    Greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hey AhBeng here. What you want?");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = sc.nextLine();
+        }
+        System.out.println("Bye bye go away!");
     }
 }
