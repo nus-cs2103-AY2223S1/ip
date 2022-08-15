fdcac001dffc64719b89779166a6f3818e8daae7
maxtance <e0772757@u.nus.edu>
2022-08-15 13:32:57 +0800

    Level 3: Added Task.java. Implemented marking and unmarking a task.

--- src/main/java/Anya.java
+++ src/main/java/Anya.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 
 public class Anya {
     static final String breakLine = "\n---------------------------------------------------------------------";
+
     public static void main(String[] args) {
         // Initialising variables
         Scanner sc = new Scanner(System.in);
         String command;
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         // Greet
         System.out.println("Hello! Anya is happy to meet you.\nHow can Anya help?" + breakLine);
@@ -18,9 +19,16 @@ public class Anya {
         while (!command.equals("bye")) {
             if (command.equals("list")) {
                 list(tasks);
+            } else if (command.split(" ")[0].equals("mark")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                mark(tasks, index);
+            } else if (command.split(" ")[0].equals("unmark")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                unmark(tasks, index);
             } else {
                 add(tasks, command);
             }
+
             command = sc.nextLine();
         }
 
@@ -28,16 +36,29 @@ public class Anya {
         System.out.println("Anya is sad to see you leave. Please be back soon." + breakLine);
     }
 
-    public static void add(ArrayList<String> tasks, String task) {
-        tasks.add(task);
+    // Commands
+    public static void add(ArrayList<Task> tasks, String task) {
+        tasks.add(new Task(task));
         System.out.println("Anya added: " + task + breakLine);
     }
 
-    public static void list(ArrayList<String> tasks) {
+    public static void list(ArrayList<Task> tasks) {
         for (int i = 0; i < tasks.size(); i++) {
             int num = i + 1;
             System.out.println(num + ". " + tasks.get(i).toString());
         }
         System.out.println(breakLine);
+    }
+
+    public static void mark(ArrayList<Task> tasks, int index) {
+        Task task = tasks.get(index);
+        task.markDone();
+        System.out.println("Anya has marked this task as done: \n  " + task.toString());
+    }
+
+    public static void unmark(ArrayList<Task> tasks, int index) {
+        Task task = tasks.get(index);
+        task.markUndone();
+        System.out.println("Anya has marked this task as uncompleted: \n  " + task.toString());
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        String res = "[" + this.getStatusIcon() + "] " + this.name;
+        return res;
+    }
+}
