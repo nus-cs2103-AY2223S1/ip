220783952ea6a77430d03cf92b01c7045ea53560
deeyonn <deeyonn@gmail.com>
2022-08-15 16:40:52 +0800

    Added implementation for Level-2 Modified Duke Commented echo() Added add()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -9,35 +10,67 @@ import java.util.Scanner;
  */
 
 public class Duke {
+    private final ArrayList<String> tasks = new ArrayList<>(100);
     public static void main(String[] args) {
         Duke jenny = new Duke();
         Scanner sc = new Scanner(System.in);
         jenny.greet();
-        jenny.echo(sc);
+        jenny.add(sc);
         jenny.exit();
     }
 
     /**
      * Prints a greeting to the console.
      */
-    public void greet() {
+    private void greet() {
         Helpers.print_line();
         Helpers.print("Hello! I'm Jenny");
         Helpers.print("What can I do for you?");
         Helpers.print_line();
     }
 
+//    /**
+//     * Continuously echos input to the console,
+//     * until a terminating keyword is input.
+//     */
+//    private void echo(Scanner sc) {
+//        String text = sc.nextLine();
+//        while(!text.equals("bye")) {
+//            Helpers.print_line();
+//            Helpers.print(text);
+//            Helpers.print_line();
+//            text = sc.nextLine();
+//        }
+//    }
+
     /**
      * Continuously echos input to the console,
-     * until a terminating keyword is input.
+     * until a valid command is input,
+     * or a terminating command is input.
      */
-    private void echo(Scanner sc) {
-        String text = sc.next();
+    private void add(Scanner sc) {
+        String text = sc.nextLine();
         while(!text.equals("bye")) {
-            Helpers.print_line();
-            Helpers.print(text);
-            Helpers.print_line();
-            text = sc.next();
+            if (text.equals("list")) {
+                Helpers.print_line();
+                int i = 1;
+                for (String task : tasks) {
+                    Helpers.print(i++ + ". " + task);
+                }
+                Helpers.print_line();
+            } else {
+                try {
+                    tasks.add(text);
+                    Helpers.print_line();
+                    Helpers.print("added: " + text);
+                    Helpers.print_line();
+                } catch (IndexOutOfBoundsException e) {
+                    Helpers.print_line();
+                    Helpers.print("Error adding " + text);
+                    Helpers.print_line();
+                }
+            }
+            text = sc.nextLine();
         }
     }
 
