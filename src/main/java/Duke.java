fd1ca67a37a3dc7bd1ec6bfd476088d37dcc0bb6
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-08-15 11:56:00 +0800

    Duke: Add Greet Echo Exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+    public Duke() {
+        String msg = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(msg);
+    }
+    public void echo() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String msg = sc.nextLine();
+            if (msg.compareTo("bye") == 0) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(msg);
+            }
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +24,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Duke dk = new Duke();
+        dk.echo();
     }
 }
