672de52b6f7fd6652b37a22ade4a217237b81395
ytingtan <e0773786@u.nus.edu>
2022-08-16 07:38:05 +0800

    Stores tasks and prints task list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,14 +5,31 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
+        String[] taskList = new String[100];
+        int index = 0;
         while (sc.hasNext()) {
             String output = sc.nextLine();
             // cannot use ==
             if (output.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (output.equals("list")) {
+                printList(taskList);
+            } else {
+                taskList[index] = output;
+                index++;
+                System.out.println("added: " + output);
             }
-            System.out.println(output);
+        }
+    }
+
+    public static void printList(String[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null) {
+                System.out.println(i + ". " + list[i]);
+            } else {
+                break;
+            }
         }
     }
 }
