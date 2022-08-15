36e686341e17bddc1bae623200dc11aae2f9da79
cadencjk <cadencheong@gmail.com>
2022-08-16 00:08:11 +0800

    Implemented Greet, Echo, Exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(reply("Hello! I'm Duke. What can I do for you?"));
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            String userText = userInput.nextLine();
+            if (userText.equals("bye")) {
+                System.out.println(reply("Bye. Hope to see you again soon!"));
+                break;
+            }
+            System.out.println(reply(userText));
+        }
+    }
+
+    public static String reply(String s) {
+        String horizontalLine = "____________________________________________________________\n";
+        return horizontalLine + s +'\n' + horizontalLine;
     }
 }
