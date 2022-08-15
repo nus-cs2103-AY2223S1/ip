1af0874fcb4edd0ba7ee8cbaa9d3e78cb6b967e3
misterpuffin <snghaoren@gmail.com>
2022-08-15 22:00:04 +0800

    Implemented Level 2 - Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+
+    private final static ArrayList<String> list = new ArrayList<>();
     public static void main(String[] args) {
         customPrint("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
         String userInput;
         while (!(userInput = sc.nextLine()).equals("bye")) {
-            customPrint(userInput);
+            handleCommand(userInput);
         }
         customPrint("Bye. Hope to see you again soon!");
     }
@@ -15,5 +19,32 @@ public class Duke {
         System.out.println("-------------------");
         System.out.println(s);
         System.out.println("-------------------");
+    }
+
+    private static void handleCommand(String s) {
+        String[] args = s.split(" ");
+        if (args.length == 0) {
+            return;
+        }
+        switch (args[0]) {
+            case "list":
+                StringBuilder stringBuilder = new StringBuilder();
+                int n = list.size();
+                for (int i = 0; i < n; i++) {
+                    stringBuilder.append(String.format("%d. %s", i + 1, list.get(i)));
+                    if (i != n - 1) {
+                        stringBuilder.append("\n");
+                    }
+                }
+                customPrint(stringBuilder.toString());
+                break;
+            default:
+                addToList(s);
+                customPrint("added: " + s);
+        }
+    }
+
+    private static void addToList(String s) {
+        list.add(s);
     }
 }
