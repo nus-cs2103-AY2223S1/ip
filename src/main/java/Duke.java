622a60180a6b398697624582ab5eff824986a1e5
zylee348 <e0725436@u.nus.edu>
2022-08-15 23:45:31 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,42 +1,61 @@
-import javax.naming.PartialResultException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static void print(ArrayList<String> stringArraylist) {
+    //method to print in list format
+    public static void print(ArrayList<Task> taskArrayList) {
         int counter = 0;
         int numbering = 1;
-        int len = stringArraylist.size();
+        int len = taskArrayList.size();
         while (counter < len) {
-            System.out.println(numbering + ". " + stringArraylist.get(counter));
+            Task temp = taskArrayList.get(counter);
+            System.out.println(numbering + ". " + "[" + temp.getStatusIcon() + "]" + temp);
             counter++; numbering++;
         }
+    }
+
+    //method to mark as done
+    public static void markAsDone(ArrayList<Task> taskArrayList, String removeTaskNumberString) {
+        String numberToRemove = removeTaskNumberString.replaceAll("[^0-9]", "");
+        int numberToRemoveInt = Integer.parseInt(numberToRemove) - 1;
+        Task tsk = taskArrayList.get(numberToRemoveInt);
+        tsk.markAsDone();
+        System.out.println("Nice! I've marked this task as done:\n" +
+                "[" + tsk.getStatusIcon() + "]" + tsk.toString());
     }
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
 
-        ArrayList<String> arrayList = new ArrayList<>(100);
+        ArrayList<Task> taskList = new ArrayList<>(100);
 
         Scanner sc = new Scanner(System.in);
         while (true) {
             try {
                 String str1 = sc.nextLine();
+
                 if (str1.equals("bye")) {
                     System.out.println("Bye, Hope to see you again soon!");
                     break;
+
                 } else if (str1.equals("list")) {
-                    print(arrayList);
+                    print(taskList);
+
+                } else if (str1.contains("mark")) {
+                    markAsDone(taskList, str1);
+
                 } else {
-                    arrayList.add(str1);
+                    Task t = new Task(str1);
+                    taskList.add(t);
                     System.out.println("added: " + str1);
                 }
 
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input");
+
             } catch (IndexOutOfBoundsException e1) {
                 System.out.println("List cannot be empty");
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+}
