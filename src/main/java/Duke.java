9169f164118c1f5eb79fcd3845ee83151b3c14a6
xav168 <xav.ong.jh@gmail.com>
2022-08-15 10:45:02 +0800

    Level-3 Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void mark() {
+            this.isDone = true;
+        }
+        public void unmark() {
+            this.isDone = false;
+        }
+
+        @Override
+        public String toString() {
+            String s = "[%s] %s";
+            return String.format(s, getStatusIcon(), description);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,9 +38,10 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         Scanner in = new Scanner(System.in);
         System.out.println("Oi, What u want?");
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         while (in.hasNext()) {
             String next = in.nextLine();
+            String[] command = next.split(" ", 2);
             if (next.equals("bye")) {
                 System.out.println("Bye! Don't Come back!");
                 return;
@@ -21,6 +49,16 @@ public class Duke {
                 for(int i = 0; i < list.size(); i++) {
                     System.out.println(i+1 + ". " + list.get(i));
                 }
+            } else if (command[0].equals("mark")){
+                int index = Integer.parseInt(next.split(" ", 2)[1]) - 1;
+                list.get(index).mark();
+                System.out.println("I have marked this task as done:");
+                System.out.println(list.get(index));
+            } else if (command[0].equals("unmark")){
+                int index = Integer.parseInt(next.split(" ", 2)[1]) - 1;
+                list.get(index).unmark();
+                System.out.println("I have marked this task as not done:");
+                System.out.println(list.get(index));
             }
             else {
                 System.out.println("added: " + next);
