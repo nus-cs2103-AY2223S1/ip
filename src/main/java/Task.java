cc9d7f98a8bdd2b6c38ba40843fa126a34688b15
njxue <ngjxue@gmail.com>
2022-08-15 20:15:10 +0800

    Add toString method to Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -22,4 +22,9 @@ public class Task {
     public String getDescription() {
         return this.description;
     }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
 }
