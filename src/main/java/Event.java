8a79e32a3c214b4fea57903d9b8d96f875a24b94
chm252 <chinhongming252@gmail.com>
2022-08-15 17:52:10 +0800

    Subclasses of Tasks

---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+public class Event extends Task{
+    protected String at;
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+        System.out.println("Making you a more EVENTful person!\n" + this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at:" + at + ")";
+    }
+}
---
+++ src/main/java/ToDo.java
@@ -0,0 +1,13 @@
+import javax.swing.plaf.synth.SynthDesktopIconUI;
+
+public class ToDo extends Task{
+    public ToDo(String description) {
+        super(description);
+        System.out.println("To do has been added");
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
