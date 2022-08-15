2f8dafc62cbb1d4d34365c0f4bfe5f811d7ba2af
craeyeons <simpsoneng552@gmail.com>
2022-08-15 15:44:49 +0800

    completed Level-6

--- src/main/java/Jean.java
+++ src/main/java/Jean.java
@@ -10,33 +10,44 @@ public class Jean {
                            "\tAu revoir! À tout à l'heure!");
     }
 
-    private static void add(Task newTask, Task[] taskList) {
-        taskList[Task.numberOfTasks - 1] = newTask;
+    private static void add(Task newTask, ArrayList<Task> taskList) {
+        taskList.add(newTask);
         System.out.println("\tadded / ajouté:\n\t\t" + newTask.toString());
         System.out.println("\tYou now have " + Task.numberOfTasks + " task(s)!\n" +
                            "\tVous avez " + Task.numberOfTasks + " tâche(s)!");
     }
 
-    private static void list(Task[] taskList) {
+    private static void list(ArrayList<Task> taskList) {
         for (int i = 0; i < Task.numberOfTasks; i++) {
-            System.out.println("\t" + (i+1) + ".\t " + taskList[i].toString());
+            System.out.println("\t" + (i+1) + ".\t " + taskList.get(i).toString());
         }
     }
 
-    private static void mark(Task[] taskList, int taskIndex) {
-        Task curr = taskList[taskIndex - 1];
+    private static void mark(ArrayList<Task> taskList, int taskIndex) {
+        Task curr = taskList.get(taskIndex - 1);
         curr.setIsDone(true);
         System.out.println("\tI have marked it as done:\n" +
                            "\tJe l'ai marqué comme fait:\n\t" +
                            curr.toString());
     }
 
-    private static void unmark(Task[] taskList, int taskIndex) {
-        Task curr = taskList[taskIndex - 1];
+    private static void unmark(ArrayList<Task> taskList, int taskIndex) {
+        Task curr = taskList.get(taskIndex - 1);
         curr.setIsDone(false);
         System.out.println("\tI have marked it as undone:\n" +
                            "\tJe l'ai marqué comme défait:\n\t" +
                            curr.toString());
+    }
+
+    private static void delete(ArrayList<Task> taskList, int taskIndex) {
+        Task curr = taskList.get(taskIndex - 1);
+        Task.numberOfTasks -= 1;
+        taskList.remove(taskIndex - 1);
+        System.out.println("\tI have deleted the task:" +
+                           "\n\tJe l'ai supprimé:\n\t" +
+                           curr.toString() +
+                           "\n\tYou now have " + Task.numberOfTasks + " tasks remaining!" +
+                           "\n\tIl vous reste maintenant " + Task.numberOfTasks + " tâches!");
     }
 
     public static void main(String[] args) {
@@ -45,7 +56,7 @@ public class Jean {
                            "Bonjour! Je m'appelle Jean\n" +
                            "Vous désirez?\n");
 
-        Task[] taskList = new Task[100];
+        ArrayList<Task> taskList = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         while(true) {
@@ -90,7 +101,7 @@ public class Jean {
                 try {
                     if (sep == 9 || input.length() == 8) {
                         throw new JeanException("The description must not be empty!");
-                    } else if (sep == -1){
+                    } else if (sep == -1) {
                         throw new JeanException("You must give a deadline!");
                     } else {
                         add(new Deadline(input.substring(9, sep), input.substring(sep + 4)),
@@ -104,11 +115,21 @@ public class Jean {
                 try {
                     if (sep == 6 || input.length() == 5) {
                         throw new JeanException("The description must not be empty!");
-                    } else if (sep == -1){
+                    } else if (sep == -1) {
                         throw new JeanException("You must give a time!");
                     } else {
                         add(new Event(input.substring(6, sep), input.substring(sep + 4)),
                                 taskList);
+                    }
+                } catch (JeanException e) {
+                    System.out.println(e);
+                }
+            } else if (input.startsWith("delete")) {
+                try {
+                    if (input.length() == 6) {
+                        throw new JeanException("You must name a task to delete!");
+                    } else {
+                        delete(taskList, Integer.parseInt(input.substring(7)));
                     }
                 } catch (JeanException e) {
                     System.out.println(e);
