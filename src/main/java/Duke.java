6c6da0a16c4e2036353900efefa5431e62162f0e
aarontangr <lapselol@gmail.com>
2022-08-15 18:08:25 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,11 +4,22 @@ public class Duke {
     public static void main(String[] args) {
       System.out.println("hello");
       System.out.println("can i help you?");
+      
+      int count = 0;
+      String[] strArray = new String[101];
 
       Scanner sc = new Scanner(System.in);
       String s = sc.nextLine();
-      while (!s.equals("yes")) {
-        System.out.println(s);
+      while (!s.equals("bye")) {
+        if (s.equals("list")) {
+          for (int i = 0; i < count; i++) {
+            System.out.println(i + 1 + ". " + strArray[i]);
+          }
+        } else {
+          strArray[count] = s;
+          count++;
+          System.out.println("added: " + s);
+        }
         s = sc.nextLine();
       }
 
