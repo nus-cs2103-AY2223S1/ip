492357461e3ff0356f3443585bdee9fcfcd47160
ongwenpin <wenpin2108@gmail.com>
2022-08-15 14:05:01 +0800

    Duke can now add things to list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    static ArrayList<String> listOfThings = new ArrayList<String>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
 
+        // Special Commands
         String BYE = "bye";
+        String LIST = "list";
 
         Scanner sc = new Scanner(System.in);
 
@@ -19,19 +24,30 @@ public class Duke {
             String userInput = sc.nextLine();
             if (userInput.equals(BYE)) {
                 break;
+            } else if (userInput.equals(LIST)) {
+                ListOut();
             } else {
-                Echos(userInput);
+                AddToList(userInput);
             }
         }
 
         Bye();
     }
 
-    public static void Echos(String str) {
+    public static void AddToList(String str) {
+        listOfThings.add(str);
         System.out.println("--------------------------------");
-        System.out.println(str);
+        System.out.println("added: " + str);
         System.out.println("--------------------------------");
+    }
 
+    public static void ListOut() {
+        int size = listOfThings.size();
+        System.out.println("--------------------------------");
+        for (int i = 0; i < size; i++) {
+            System.out.println((i + 1) + ". " + listOfThings.get(i));
+        }
+        System.out.println("--------------------------------");
     }
 
     public static void Bye() {
