14aa21c0d69533d6bba1d76d9e994dcb7c60fc2d
maxtance <e0772757@u.nus.edu>
2022-08-15 11:46:30 +0800

    Level-2: Added add and list functions to Anya.

--- src/main/java/Anya.java
+++ src/main/java/Anya.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Anya {
+    static final String breakLine = "\n---------------------------------------------------------------------";
     public static void main(String[] args) {
         // Initialising variables
         Scanner sc = new Scanner(System.in);
         String command;
-        String breakLine = "\n---------------------------------------------------------------------";
+        ArrayList<String> tasks = new ArrayList<>();
 
         // Greet
         System.out.println("Hello! Anya is happy to meet you.\nHow can Anya help?" + breakLine);
@@ -14,11 +16,28 @@ public class Anya {
         command = sc.nextLine();
 
         while (!command.equals("bye")) {
-            System.out.println("Anya says " + command + breakLine);
+            if (command.equals("list")) {
+                list(tasks);
+            } else {
+                add(tasks, command);
+            }
             command = sc.nextLine();
         }
 
         // Exit
         System.out.println("Anya is sad to see you leave. Please be back soon." + breakLine);
+    }
+
+    public static void add(ArrayList<String> tasks, String task) {
+        tasks.add(task);
+        System.out.println("Anya added: " + task + breakLine);
+    }
+
+    public static void list(ArrayList<String> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            int num = i + 1;
+            System.out.println(num + ". " + tasks.get(i).toString());
+        }
+        System.out.println(breakLine);
     }
 }
