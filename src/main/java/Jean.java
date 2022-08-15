b7c56b3fd12c5467e1bb75e572ef2c59dab9ae8c
craeyeons <simpsoneng552@gmail.com>
2022-08-15 14:30:16 +0800

    renamed Duke to Jean

--- src/main/java/Jean.java
+++ src/main/java/Jean.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Duke {
+public class Jean {
 
     private static void bye() {
         System.out.println("\tGoodbye! See you soon!\n" +
