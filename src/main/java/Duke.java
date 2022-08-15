ae74ccb1fe5dd0ff8397ffe4d2a951350705711f
wweqg <e0726629@u.nus.edu>
2022-08-15 12:08:26 +0800

    Implemented a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,39 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        /*
+        Level 1: Level 1. Greet, Echo, Exit
+        Implement a skeletal version of Duke that starts by greeting the user,
+        simply echos commands entered by the user, and exits when the user types bye.
+         */
+        System.out.println(dialog("Hello! I'm Duke" + "\n" + "  What can I do for you?"));
+
+        //Initialise the scanner used.
+        Scanner sc = new Scanner(System.in);
+        //Initialise a variable to receive the text entered.
+        String message;
+        while(true) {
+            //Update the message variable
+            message = sc.nextLine();
+            /*
+            Check if exit command is being entered
+            If true, print the exit statement and exit the loop, else repeat the statement entered.
+             */
+            if (message.equals("bye")) {
+                System.out.println(dialog("Bye. Hope to see you again soon!"));
+                sc.close();
+                break;
+            } else {
+                System.out.println(dialog(message));
+            }
+        }
+    }
+
+    //A wrap the string in a dialog frame
+    public static String dialog(String message) {
+        return "  ____________________________________________________________\n" +
+                "  " + message + "\n" +
+               "  ____________________________________________________________\n";
     }
 }
