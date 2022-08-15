b75f5957936cb148ead506ad17a48d618c9af0a5
Evande1 <crossjab@gmail.com>
2022-08-15 16:27:24 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,34 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    // mtd to store arraylist of 100
-    public static void main(String[] args) {
+    public static ArrayList<String> list = new ArrayList<>();
 
+    public static void main(String[] args) {
+        boolean exit = false;
         System.out.println("Hello! I'm Milk");
         System.out.println("what can I do for you?");
-
         Scanner sc = new Scanner(System.in);
-        String userReply = sc.nextLine();
-        while (!userReply.equals("bye")) {
-            System.out.println(userReply);
-            userReply = sc.nextLine();
+
+        while (!exit) {
+            String userReply = sc.nextLine();
+
+            switch (userReply) {
+                case "bye":
+                    exit = true;
+                    break;
+
+                case "list":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(i+1 + "." + list.get(i));
+                    }
+                    break;
+
+                default:
+                    System.out.println("added: " + userReply);
+                    list.add(userReply);
+                    break;
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
