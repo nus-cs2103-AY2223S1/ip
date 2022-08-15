3a0e25f5f0c56a5985f63738fa7dcf5621a15530
jeromepui <jeromepui00@gmail.com>
2022-08-15 20:37:32 +0800

    Add greet, echo and exit features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(">> Hello! I'm Duke!");
+        System.out.println(">> What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("<< ");
+            String command = sc.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(">> Bye! Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(">> " + command);
+        }
     }
 }
