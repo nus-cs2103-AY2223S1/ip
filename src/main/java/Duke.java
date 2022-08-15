c9be963a37fb7ce44eaf7e3e12c6ba7a183aac19
Marcus Pang <marcuspangyuyang@gmail.com>
2022-08-15 23:04:11 +0800

    Create Task class and add mask/unmask commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,7 +14,7 @@ public class Duke {
             "     Bye. Hope to see you again soon!\n" +
             LONG_LINE_WITH_NEW_LINE;
 
-    private final List<String> taskList = new ArrayList<>();
+    private final List<Task> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
         // Initialize Duke chatbot
@@ -40,17 +40,43 @@ public class Duke {
         StringBuilder sb = new StringBuilder();
         if (command.equals("list")) {
             // Return all tasks
-            sb.append(LONG_LINE_WITH_NEW_LINE);
+            sb.append(LONG_LINE_WITH_NEW_LINE).append("     Here are the tasks in your list:\n");
             for (int i = 0; i < taskList.size(); i++) {
-                sb.append("     ").append(i + 1).append(". ").append(taskList.get(i)).append("\n");
+                sb.append("     ").append(i + 1).append(".").append(taskList.get(i)).append("\n");
             }
             sb.append(LONG_LINE);
+        } else if (command.startsWith("mark")) {
+            // Tasks are displayed as 1-indexed, but they are stored as 0-indexed
+            // Hence, we need to account for this offset here
+            int taskIndex = Integer.parseInt(command.replaceFirst("mark ", "")) - 1;
+            Task task = this.taskList.get(taskIndex);
+            task.markTask();
+
+            sb.append(LONG_LINE_WITH_NEW_LINE)
+                    .append("     Nice! I've marked this task as done:\n")
+                    .append("       ").append(task).append("\n")
+                    .append(LONG_LINE_WITH_NEW_LINE);
+        } else if (command.startsWith("unmark")) {
+            // Tasks are displayed as 1-indexed, but they are stored as 0-indexed
+            // Hence, we need to account for this offset here
+            int taskIndex = Integer.parseInt(command.replaceFirst("unmark ", "")) - 1;
+            Task task = this.taskList.get(taskIndex);
+            task.unmarkTask();
+
+            sb.append(LONG_LINE_WITH_NEW_LINE)
+                    .append("     OK, I've marked this task as not done yet:\n")
+                    .append("       ").append(task).append("\n")
+                    .append(LONG_LINE_WITH_NEW_LINE);
         } else {
             // Otherwise, add command to task list
-            this.taskList.add(command);
-            sb.append(LONG_LINE_WITH_NEW_LINE).append("     added: ").append(command).append("\n").append(LONG_LINE_WITH_NEW_LINE);
+            this.taskList.add(new Task(command));
+            sb.append(LONG_LINE_WITH_NEW_LINE)
+                    .append("     added: ")
+                    .append(command).append("\n")
+                    .append(LONG_LINE_WITH_NEW_LINE);
         }
-        // Print out response message after executing command
+
+        // Print out response after executing command
         System.out.println(sb);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected final String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markTask() {
+        this.isDone = true;
+    }
+
+    public void unmarkTask() {
+        this.isDone = false;
+    }
+
+    // Tasks that are done will be marked with "X"
+    public String getStatusIcon() {
+        return this.isDone ? "X" : " ";
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
