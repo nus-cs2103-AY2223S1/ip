dc9f2d0e169129ce3ddbeafb4eb59d90b5f3eb68
lzq <li.zhaoqi@dhs.sg>
2022-08-16 00:37:14 +0800

    - Added outro text on exit - Completed Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,21 @@ public class Duke {
     private static final String INTRO = "Hello! I'm Duke\n"
             + "What can I do for you?";
 
+    private static final String OUTRO = "Bye. Hope to see you again soon!";
+
     private DukeIO userIO;
 
     private static String[] COMMANDS = {
             "bye"
     };
 
+    private String[] tasks;
+    private int index;
+
     Duke() {
         userIO = new DukeIO();
+        tasks = new String[100];
+        index = 0;
         userIO.printTask(LOGO, 2);
         userIO.printTask(INTRO, 3);
     }
@@ -25,11 +32,29 @@ public class Duke {
     boolean handleInput() {
         String response = userIO.readLine();
         if (response.equals("bye")) {
+            userIO.printTask(OUTRO, 3);
             return false;
+        } else if (response.equals("list")) {
+            listAllTasks();
+            return true;
         }
 
-        userIO.printTask(response);
+        // userIO.printTask(response);
+        addTasks(response);
         return true;
+    }
+
+    void listAllTasks() {
+        userIO.printLine();
+        for (int i = 0; i < index; ++i) {
+            userIO.printTask(String.format("%d. %s", i + 1, tasks[i]), 2);
+        }
+        userIO.printLine();
+    }
+
+    void addTasks(String txt) {
+        tasks[index++] = txt;
+        userIO.printTask(String.format("added: %s", txt));
     }
 
     public static void main(String[] args) {
--- src/main/java/DukeIO.java
+++ src/main/java/DukeIO.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class DukeIO {
     // class to handle input/output of data
-    private static final String LINE = "____________________________________________________________";
+    private static final String LINE = "   ____________________________________________________________";
     private final Scanner scanner;
 
     DukeIO() {
@@ -13,14 +13,18 @@ class DukeIO {
         return scanner.nextLine().trim();
     }
 
+    void printLine() {
+        System.out.println(LINE);
+    }
+
     void printTask(String txt, int features) {
         // 00 - no wrapper/indent
         // 01 - indent
         // 10 - wrapper
-        if ((features & 1) == 1)
-            txt = addWrapper(txt);
         if ((features & 2) == 2)
             txt = addIndent(txt);
+        if ((features & 1) == 1)
+            txt = addWrapper(txt);
 
         System.out.println(txt);
     }
@@ -30,10 +34,10 @@ class DukeIO {
     }
 
     static String addIndent(String txt) {
-        return txt.replaceAll("^|\n", "\n\t");
+        return "\t" + txt.replaceAll("\n", "\n\t");
     }
 
     static String addWrapper(String txt) {
-        return String.format("%s%n%s%n%s", LINE, txt, LINE);
+        return String.format("%s%n%s%n%s%n", LINE, txt, LINE);
     }
 }
