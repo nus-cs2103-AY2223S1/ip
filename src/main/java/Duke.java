3c1d10dd63a28808508430c28c9a9b29a28a4c10
angkl0 <kuanglong456@gmail.com>
2022-08-15 23:33:11 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,22 +13,53 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<Task> arrayList = new ArrayList<>();
+
+        loop:
         while(sc.hasNextLine()) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if (input.equals("list")) {
-                String message = "";
-                int length = arrayList.size();
-                for(int i = 0; i < length; i++) {
-                    message += String.format("%d. " + arrayList.get(i) + "\n", i + 1);
-                }
-                System.out.println(message);
-            } else {
-                arrayList.add(input);
-                System.out.println("added: " + input + "\n");
+            String[] splitInput = input.split(" ", 2);
+            String command = splitInput[0];
+            String detail = "";
+            if(splitInput.length > 1) {
+                detail = splitInput[1];
+            }
+            switch (command) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break loop;
+                case "list":
+                    String message = "";
+                    int length = arrayList.size();
+                    for(int i = 0; i < length; i++) {
+                        message += String.format("%d. " + arrayList.get(i).toString() + "\n", i + 1);
+                    }
+                    System.out.println(message);
+                    break;
+                case "mark":
+                    int markIndex = Integer.parseInt(detail) - 1;
+                    Task markTask = arrayList.get(markIndex);
+                    if(markTask.isDone) {
+                        System.out.println("This task is already marked as done:\n" + markTask.toString() + "\n");
+                    } else {
+                        markTask.markDone();
+                        System.out.println("Nice! I've marked this task as done:\n" + markTask.toString() + "\n");
+                    }
+                    break;
+                case "unmark":
+                    int unMarkIndex = Integer.parseInt(detail) - 1;
+                    Task unMarkTask = arrayList.get(unMarkIndex);
+                    if(!unMarkTask.isDone) {
+                        System.out.println("This task is already marked as undone:\n" + unMarkTask.toString() + "\n");
+                    } else {
+                        unMarkTask.markUndone();
+                        System.out.println("OK, I've marked this task as not done yet:\n" + unMarkTask.toString() + "\n");
+                    }
+                    break;
+                default:
+                    Task task = new Task(input);
+                    arrayList.add(task);
+                    System.out.println("added: " + task.toString() + "\n");
             }
         }
         sc.close();
