e0cb0b4b7a1a28de0fbd06c9e4c6e7ef1a2b7b90
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-08-15 13:36:26 +0800

    Add Task class to represent Tasks in the Duke bot.
    Using a Task class and correspondingly Task objects to represent Tasks is a more natural way of representing tasks and allows me to keep the logic for the Task representation in a class of itself.

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDoneStatus() {
+        return (this.isDone ? "X" : " ");
+    }
+    
+    public void setDoneStatus(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+}
