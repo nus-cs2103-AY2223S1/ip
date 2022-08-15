34bb276dbcaf83bdc77c6cb1ce69844f42bddf90
Evande1 <crossjab@gmail.com>
2022-08-15 15:26:06 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
+    // mtd to store arraylist of 100
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Milk");
+        System.out.println("what can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String userReply = sc.nextLine();
+        while (!userReply.equals("bye")) {
+            System.out.println(userReply);
+            userReply = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
