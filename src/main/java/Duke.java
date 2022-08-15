d889606d97886ea6ddc43d522c3c70a7a03db620
leecaregene <e0764279@u.nus.edu>
2022-08-16 02:00:11 +0800

    Complete Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,25 +5,34 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
         prettyPrint("Hello! I'm Duke\nWhat can I do for you?");
         while (in.hasNextLine()) {
-            String command = in.nextLine();
-            if (command.equalsIgnoreCase("bye")) {
+            String input = in.nextLine();
+            String[] command = input.split(" ");
+            if (command[0].equalsIgnoreCase("bye")) {
                 prettyPrint("Bye. Hope to see you again soon!");
                 break;
-            } else if (command.equalsIgnoreCase("list")) {
-                StringBuilder temp = new StringBuilder();
+            } else if (command[0].equalsIgnoreCase("list")) {
+                StringBuilder temp = new StringBuilder("Here are the tasks in your list:\n");
                 for (int i = 0; i < tasks.size(); i++) {
-                    temp.append(i+1);
-                    temp.append(". ");
+                    temp.append(i + 1);
+                    temp.append(".");
                     temp.append(tasks.get(i));
                     temp.append("\n");
                 }
                 prettyPrint(temp.toString());
+            } else if (command[0].equalsIgnoreCase("mark")) {
+                Task task = tasks.get(Integer.parseInt(command[1]) - 1);
+                task.mark();
+                prettyPrint("Nice! I've marked this task as done:\n  " + task);
+            } else if (command[0].equalsIgnoreCase("unmark")) {
+                Task task = tasks.get(Integer.parseInt(command[1]) - 1);
+                task.unmark();
+                prettyPrint("OK, I've marked this task as not done yet:\n  " + task);
             } else {
-                tasks.add(command);
-                prettyPrint("added: " + command);
+                tasks.add(new Task(input));
+                prettyPrint("added: " + input);
             }
         }
     }
