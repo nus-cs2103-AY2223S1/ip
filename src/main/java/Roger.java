1c8296d1482721adbff1e7d7bf3ee7976406464c
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-08-15 11:27:36 +0800

    Add mark/unmark functionality, and refactor to use the Task class

--- src/main/java/Roger.java
+++ src/main/java/Roger.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Roger {
-    private List<String> textList = new ArrayList<>();
+    private List<Task> tasks = new ArrayList<>();
 
     private static void sayGoodbye() {
         System.out.println("Bye bye niece and nephew.");
@@ -25,16 +25,36 @@ public class Roger {
     }
 
     private void list() {
+        System.out.println("Nephew got a lot of things to do:");
+
         int i = 1;
-        for (String text: textList) {
-            System.out.println(String.valueOf(i) + ". " + text);
+        for (Task task: tasks) {
+            System.out.println(String.valueOf(i) + ". " + task.toString());
             ++i;
         }
     }
 
-    private void add(String text) {
-        textList.add(text);
-        System.out.println("Added: " + text);
+    private void add(String taskName) {
+        Task task = new Task(taskName);
+        this.tasks.add(task);
+        System.out.println("Nephew got new task to do:");
+        System.out.println(task);
+    }
+
+    private void markAsDone(int taskNum) {
+        Task task = tasks.get(taskNum - 1);
+        task.markAsDone();
+
+        System.out.println("Fuiyoh, nephew so efficient! Finished this task:");
+        System.out.println(task);
+    }
+
+    private void unmarkAsDone(int taskNum) {
+        Task task = tasks.get(taskNum - 1);
+        task.unmarkAsDone();
+
+        System.out.println("Haven't done yet, mark what mark? Unmarked this task:");
+        System.out.println(task);
     }
 
     public static void main(String[] args) {
@@ -50,6 +70,14 @@ public class Roger {
                 break;
             } else if (input.equals("list")) {
                 roger.list();
+            } else if (input.startsWith("mark")) {
+                // Handle invalid input
+                int idx = Integer.parseInt(input.substring(5));
+                roger.markAsDone(idx);
+            } else if (input.startsWith("unmark")) {
+                // Handle invalid input
+                int idx = Integer.parseInt(input.substring(7));
+                roger.unmarkAsDone(idx);
             } else {
                 roger.add(input);
             }
