0c21ee3e81ffb7555237d87c74c5b7355870151b
Connor Lim <dev@connorlim.net>
2022-08-15 22:25:49 +0800

    Add ability to mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         System.out.println("Hello! I'm Piggy");
         System.out.println("What can I oink for you?");
@@ -14,13 +14,25 @@ public class Duke {
         while (true) {
             input = sc.nextLine();
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.printf("%d. %s\n", i + 1, list.get(i));
+                    System.out.printf("%d.[%s] %s\n", i + 1, list.get(i).getStatusIcon(),
+                            list.get(i).description);
                 }
+            } else if (input.matches("^mark \\d+$")) {
+                Task item = list.get(Integer.parseInt(input.substring(5)) - 1);
+                item.markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.printf("  [%s] %s\n", item.getStatusIcon(), item.description);
+            } else if (input.matches("^unmark \\d+$")) {
+                Task item = list.get(Integer.parseInt(input.substring(7)) - 1);
+                item.markNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.printf("  [%s] %s\n", item.getStatusIcon(), item.description);
             } else if (input.equals("bye")) {
                 break;
             } else {
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("added: " + input);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markNotDone() {
+        isDone = false;
+    }
+}
