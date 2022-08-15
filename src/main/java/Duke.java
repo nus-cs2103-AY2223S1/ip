b33ffba6bf08c7611b2a46abad675a38d7ddb30a
Piyotato <piyotatopiyo@gmail.com>
2022-08-16 00:04:07 +0800

    Add skeletal version of Duke
    - Add user greeting message
    - Add user input echoing
    - Add exit command with exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class Duke {
+
+    private static final String greetingMessage = "Hello! I'm Duke\nWhat can I do for you?";
+    private static final String exitMessage = "Bye. Hope to see you again soon!";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        sendMessage(greetingMessage);
+        Scanner userInput = new Scanner(System.in);
+        boolean receiveInput = true;
+
+        while(receiveInput) {
+            String input = userInput.nextLine();
+            receiveInput = parseInput(input);
+        }
+
+    }
+
+    private static boolean parseInput(String input) {
+        switch (input) {
+            case "bye":
+                sendMessage(exitMessage);
+                return false;
+            default:
+                sendMessage(input);
+                return true;
+        }
+    }
+
+    private static void sendMessage(String message) {
+
+        String indentation = "    ";
+        String line = "____________________________________________________________";
+        Stream<String> messageLines = message.lines();
+
+        System.out.println(indentation + line);
+        messageLines.forEach(x -> System.out.println(indentation + x));
+        System.out.println(indentation + line);
     }
 }
