55527a90ae6c2362b01934da80a034b352854e88
guanzhou03 <guanzhou03@gmail.com>
2022-08-15 17:44:56 +0800

    Level 2
    Adds user inputs into a list, and the list items will be printed with the list command. Bye exits the program

--- src/main/java/Actions.java
+++ src/main/java/Actions.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Actions {
     public static void greetEcho() {
@@ -13,6 +13,28 @@ public class Actions {
             }
             else {
                 System.out.println(input);
+            }
+        }
+    }
+    public static void addList() {
+        ArrayList<String> ls = new ArrayList();
+        String input = "";
+        System.out.println("Hello! I'm Duke, what's up today?");
+        while (input != "bye") {
+            Scanner sc = new Scanner(System.in);
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("See ya! Come again~");
+                return;
+            }
+            else if (input.equals("list")) {
+                for (int i = 0; i < ls.size(); i++) {
+                    System.out.println(i + 1 + ". " + ls.get(i));
+                }
+            }
+            else {
+                ls.add(input);
+                System.out.println("added: " + input);
             }
         }
     }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,6 @@
 public class Duke {
     public Duke(){}
     public static void main(String[] args) {
-        Actions.greetEcho();
+        Actions.addList();
     }
 }
