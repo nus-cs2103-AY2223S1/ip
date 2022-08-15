695ffb4f693e93d5d15263325989c2af0b3d81ed
ChongCheeKaiClarence <e0725313.u.nus.edu>
2022-08-16 01:23:52 +0800

    Added task.java and Level 3 work

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,31 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Duke {
+    private static Pattern checkString = Pattern.compile("-?\\d+");
 
-    private static ArrayList<String> storage = new ArrayList<String>();
+    private static ArrayList<Task> storage = new ArrayList<Task>();
+
+    public static boolean isInteger(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return checkString.matcher(strNum).matches();
+    }
 
     public static void reply() {
         Scanner echo = new Scanner(System.in);
         String response = echo.nextLine();
+        Pattern markPattern = Pattern.compile("^mark ", Pattern.CASE_INSENSITIVE);
+        Matcher markMatcher = markPattern.matcher(response);
+        boolean mark = markMatcher.find();
+        Pattern unmarkPattern = Pattern.compile("^unmark ", Pattern.CASE_INSENSITIVE);
+        Matcher unmarkMatcher = unmarkPattern.matcher(response);
+        boolean unmark = unmarkMatcher.find();
+        Task task = new Task(response);
         if (response.equals("bye")) {
             System.out.println("-----------------------------------------------");
             System.out.println("Bye. Hope to see you again soon!");
@@ -17,12 +35,51 @@ public class Duke {
         } else if (response.equals("list")) {
             System.out.println("-----------------------------------------------");
             for (int i = 0; i < storage.size(); i++) {
-               System.out.println((i + 1) + ". " + storage.get(i));
+                System.out.println((i + 1) + ".["+ storage.get(i).getStatusIcon() + "] "
+                        + storage.get(i).getDescription());
             }
             System.out.println("-----------------------------------------------");
             reply();
+        } else if(mark) {
+            String[] parts = response.split(" ");
+            String part2 = parts[1];
+            if(isInteger(part2) && parts.length == 2) {
+                int number = Integer.parseInt(part2);
+                storage.get(number- 1).mark();
+                System.out.println("-----------------------------------------------");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("["+ storage.get(number -1).getStatusIcon() + "] "
+                        + storage.get(number - 1).getDescription());
+                System.out.println("-----------------------------------------------");
+                reply();
+            } else {
+                storage.add(task);
+                System.out.println("-----------------------------------------------");
+                System.out.println("added: " + response);
+                System.out.println("-----------------------------------------------");
+                reply();
+            }
+        } else if(unmark ) {
+            String[] parts = response.split(" ");
+            String part2 = parts[1];
+            if(isInteger(part2) && parts.length == 2) {
+                int number = Integer.parseInt(part2);
+                storage.get(number- 1).unMark();
+                System.out.println("-----------------------------------------------");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("["+ storage.get(number -1).getStatusIcon() + "] "
+                        + storage.get(number - 1).getDescription());
+                System.out.println("-----------------------------------------------");
+                reply();
+            } else {
+                storage.add(task);
+                System.out.println("-----------------------------------------------");
+                System.out.println("added: " + response);
+                System.out.println("-----------------------------------------------");
+                reply();
+            }
         } else {
-            storage.add(response);
+            storage.add(task);
             System.out.println("-----------------------------------------------");
             System.out.println("added: " + response);
             System.out.println("-----------------------------------------------");
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unMark() {
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    //...
+}
