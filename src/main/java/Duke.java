be99b28e5501cbdf7387c626d1f1172a73425071
cadencjk <cadencheong@gmail.com>
2022-08-16 00:22:55 +0800

    Implemented Add, List Functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,6 +11,8 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        List<String> list = new ArrayList<String>();
+
         System.out.println(reply("Hello! I'm Duke. What can I do for you?"));
         Scanner userInput = new Scanner(System.in);
         while (true) {
@@ -16,9 +20,24 @@ public class Duke {
             if (userText.equals("bye")) {
                 System.out.println(reply("Bye. Hope to see you again soon!"));
                 break;
+            } else if (userText.equals("list")) {
+                System.out.println(reply(getList(list)));
+            } else {
+                list.add(userText);
+                System.out.printf(reply("added: " + userText));
             }
-            System.out.println(reply(userText));
         }
+    }
+
+    public static String getList(List<String> list) {
+        String listString = "";
+        for (int i = 0; i < list.size(); i++) {
+            listString += String.format("%d. %s", i, list.get(i));
+            if (i != list.size() - 1) {
+                listString += "\n";
+            }
+        }
+        return listString;
     }
 
     public static String reply(String s) {
