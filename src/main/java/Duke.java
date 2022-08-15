953c55520e3444618472de0ae15b3e193085d3da
tlx02 <tlx0202@gmail.com>
2022-08-15 12:08:07 +0800

    Added 'Greet, Echo, Exit' features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "*\\(^o^)/*";
+        System.out.println(logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bonjour~~ I'm Byu! How can I help you?");
+        String response = sc.nextLine();
+        while (!response.equals("bye")) {
+            System.out.println(response + "\n");
+            response = sc.nextLine();
+        }
+        System.out.println("Awww see you soon!");
+        sc.close();
     }
+
 }
