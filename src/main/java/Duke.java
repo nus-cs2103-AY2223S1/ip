e6b230379ad8f67a1ad40fa802a133a5106b427a
waynezsy <waynezsy0317@gmail.com>
2022-08-15 12:16:48 +0800

    Duke class: implement skeletal version
    Duke has no functionality.
    
    Let's,
    * add a greeting on start
    * echo commands entered by the user
    * add an exit command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        String s = sc.nextLine();
+        while (!s.equals("bye")) {
+            System.out.println(s);
+            s = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
