885edb0c4f740af962ad28001f76e668a085aef0
ALWHgithub <alwhgithub@gmail.com>
2022-08-15 13:00:28 +0800

    Added javadocs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,11 +12,9 @@ public class Duke {
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
     private static final String GREETING = "Hello from\n"  + LOGO + "\nHow can I help you ?\n" ;
-    private static final List<Task> INPUT_LIST = new ArrayList<>();
+    private static final List<Task> TASK_LIST = new ArrayList<>();
 
-    private static boolean isNumeric(String input) {
-        return input.matches("^[0-9]*$");
-    }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -39,11 +37,11 @@ public class Duke {
                 } else if (split.length == 2 && split[0].equals("delete") && isNumeric(split[1])) {
                     delete(Integer.parseInt(split[1]));
                 } else if (split[0].equals("todo")) {
-                    addTodo(split);
+                    handleTodo(split);
                 } else if (split[0].equals("deadline")) {
-                    addDeadline(split);
+                    handleDeadline(split);
                 } else if (split[0].equals("event")) {
-                    addEvent(split);
+                    handleEvent(split);
                 } else {
                     throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
@@ -52,7 +50,11 @@ public class Duke {
             }
         }
     }
-    private static void addTodo(String[] input) throws DukeException {
+    /**
+     * Handles the input for a Todo task, and adds it to the Task List.
+     * @param input The parsed Todo task
+     */
+    private static void handleTodo(String[] input) throws DukeException {
         String taskName = String.join(" ", Arrays.copyOfRange(input, 1, input.length));
         if(taskName.equals("")){
             throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
@@ -60,7 +62,11 @@ public class Duke {
         addTask(new Todo(taskName));
     }
 
-    private static void addDeadline(String[] input) throws DukeException{
+    /**
+     * Handles the input for a Deadline task, and adds it to the Task List.
+     * @param input The parsed Deadline task
+     */
+    private static void handleDeadline(String[] input) throws DukeException{
         int indexOfDate = findDate(input);
         if( indexOfDate == -1){
             throw new DukeException("☹ OOPS!!! Please add a date for your deadline with /by.");
@@ -77,7 +83,11 @@ public class Duke {
         }
     }
 
-    private static void addEvent(String[] input) throws DukeException{
+    /**
+     * Handles the input for an Event task, and adds it to the Task List.
+     * @param input The parsed Event task
+     */
+    private static void handleEvent(String[] input) throws DukeException{
         int indexOfDate = findDate(input);
         if( indexOfDate == -1){
             throw new DukeException("☹ OOPS!!! Please add a date for your event with /at.");
@@ -94,19 +104,32 @@ public class Duke {
         }
     }
 
+    /**
+     * A helper function that adds the task to the Task List and prints out the appropriate message.
+     * @param task The task to be added
+     */
     private static void addTask (Task task){
-        Integer index = INPUT_LIST.size();
-        INPUT_LIST.add(task);
-        System.out.println("Got it. I've added this task:\n " + task.toString() + "\nNow you have " + INPUT_LIST.size() +" tasks in the list.");
+        TASK_LIST.add(task);
+        System.out.println("Got it. I've added this task:\n " + task.toString() + "\nNow you have " + TASK_LIST.size() +" tasks in the list.");
     }
 
+    /**
+     * A helper function retrieved a task from the Task List based on index, and throws
+     * an exception if the index is invalid.
+     * @param index The 1-based index of the task.
+     * @return The retrieved task.
+     */
     private static Task getTask(Integer index) throws DukeException{
-        if(index < 1 || index > INPUT_LIST.size()){
+        if(index < 1 || index > TASK_LIST.size()){
             throw new DukeException("☹ OOPS!!! That index is out of bounds.");
         }
-        return INPUT_LIST.get(index-1);
+        return TASK_LIST.get(index-1);
     }
-    
+
+    /**
+     * Marks a task as done.
+     * @param index The 1-based index of the task to be marked
+     */
     private static void markDone(Integer index) throws DukeException{
         Task task = getTask(index);
         task.setDone();
@@ -115,6 +138,10 @@ public class Duke {
         System.out.println(" " + task.toString());
     }
 
+    /**
+     * Marks a task as not done.
+     * @param index The 1-based index of the task to be un-marked
+     */
     private static void markNotDone(Integer index) throws DukeException{
         Task task = getTask(index);
         task.setNotDone();
@@ -123,20 +150,32 @@ public class Duke {
         System.out.println(" " + task.toString());
     }
 
+    /**
+     * Deletes a task.
+     * @param index The 1-based index of the task to be deleted
+     */
     private static void delete(Integer index) throws DukeException{
         Task task = getTask(index);
         System.out.println(" Noted. I've removed this task:");
-        System.out.println(" " + INPUT_LIST.get(index-1).toString() +"\nNow you have " + (INPUT_LIST.size() - 1) +" tasks in the list.");
-        INPUT_LIST.remove(task);
+        System.out.println(" " + TASK_LIST.get(index-1).toString() +"\nNow you have " + (TASK_LIST.size() - 1) +" tasks in the list.");
+        TASK_LIST.remove(task);
     }
 
+    /**
+     * Prints the current list of tasks.
+     */
     private static void printList(){
-        for(int i = 0; i<INPUT_LIST.size(); i++){
+        for(int i = 0; i<TASK_LIST.size(); i++){
             Integer index = i+1;
-            System.out.println(index + ". " + INPUT_LIST.get(i));
+            System.out.println(index + ". " + TASK_LIST.get(i));
         }
     }
 
+    /**
+     * Find the index in the parsed input where the description ends and the time begins
+     * @param split The parsed input.
+     * @return The index of the /by or /at in the input string. Returns -1 if not found.
+     */
     private static int findDate(String[] split) {
         for(int i = 0; i<split.length; i++){
             if(split[i].equals("/by") || split[i].equals("/at")){
@@ -144,5 +183,14 @@ public class Duke {
             }
         }
         return -1;
+    }
+
+    /**
+     * Determines if the input string is a number (only 0-9)
+     * @param input The string to be tested.
+     * @return The result of the test.
+     */
+    private static boolean isNumeric(String input) {
+        return input.matches("^[0-9]*$");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,18 +1,33 @@
 
 
-public class Task {
-    private String description;
+public abstract class Task {
+    private final String description;
     private boolean isDone;
+
+    /**
+     * Creates a new Task object, which encapsulates a task to be completed.
+     * It is set to not done by default.
+     *
+     * @param description the description of the event
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns an icon to indicate if the task is done.
+     * @return  X if the task is done, and a space otherwise
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    public boolean getStatus() {
+    /**
+     * Returns isDone status of the task.
+     * @return  status of task
+     */
+     public boolean getStatus() {
         return isDone;
     }
 
@@ -24,9 +39,12 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Returns the string representation of the task, with the status in square brackets before the description.
+     * @return string representation of the task
+     */
     @Override
     public String toString(){
-
         return "[" + getStatusIcon() + "]" +  " " + this.description;
     }
 }
