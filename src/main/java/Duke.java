c5d6af8780b5578b883c5df3f4d159244553263e
jsincorporated <e0725210@u.nus.edu>
2022-08-15 17:29:16 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        boolean bye = false;
+
+        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+        System.out.println("HELLO");
+
+        while (!bye) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                bye = true;
+                System.out.println("bai bai");
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
