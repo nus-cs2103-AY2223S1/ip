b22c7220c33f4d7207a553db8c96777b325be99f
zylee348 <e0725436@u.nus.edu>
2022-08-15 16:27:57 +0800

    Level - 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import javax.naming.PartialResultException;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Duke {
+
+    public static void print(ArrayList<String> stringArraylist) {
+        int counter = 0;
+        int numbering = 1;
+        int len = stringArraylist.size();
+        while (counter < len) {
+            System.out.println(numbering + ". " + stringArraylist.get(counter));
+            counter++; numbering++;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
+
+        ArrayList<String> arrayList = new ArrayList<>(100);
 
         Scanner sc = new Scanner(System.in);
         while (true) {
@@ -13,11 +28,17 @@ public class Duke {
                 if (str1.equals("bye")) {
                     System.out.println("Bye, Hope to see you again soon!");
                     break;
+                } else if (str1.equals("list")) {
+                    print(arrayList);
                 } else {
-                    System.out.println(str1);
+                    arrayList.add(str1);
+                    System.out.println("added: " + str1);
                 }
+
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input");
+            } catch (IndexOutOfBoundsException e1) {
+                System.out.println("List cannot be empty");
             }
         }
 
