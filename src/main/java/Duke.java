1aa1d47ff01526c63f3a3c7153b6b3059c4138c7
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-15 11:49:06 +0800

    Add level 2 tasks: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-  public static void main(String[] args) {
+  private ArrayList<String> listOfItems = new ArrayList<String>();
+
+  private void greet() {
     String logo =
       " ____        _        \n" +
       "|  _ \\ _   _| | _____ \n" +
@@ -11,13 +15,30 @@ public class Duke {
       "|____/ \\__,_|_|\\_\\___|\n";
     System.out.println("Hello from\n" + logo);
     System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+  }
+
+  private void echo(String txt) {
+    if (txt.equals("list")) {
+      for (int i = 0; i < listOfItems.size(); i++) {
+        System.out.println(i + ". " + listOfItems.get(i));
+      }
+    } else {
+      this.listOfItems.add(txt);
+      System.out.println("added: " + txt);
+    }
+  }
+
+  public static void main(String[] args) {
+    Duke duke = new Duke();
+    duke.greet();
     Scanner scanner = new Scanner(System.in); // Create a Scanner object
+    // Run duke as long as command is not bye
     while (true) {
       String txt = scanner.nextLine();
       if (txt.toLowerCase().equals("bye")) {
         break;
       }
-      System.out.println(txt);
+      duke.echo(txt);
     }
 
     System.out.println("Bye. Hope to see you again soon!");
