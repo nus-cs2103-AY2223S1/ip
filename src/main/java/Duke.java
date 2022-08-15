1b808ef8606085852480b249fbbf71f5eed6a2a9
Phua Li Ting <phualiting8@gmail.com>
2022-08-15 20:27:16 +0800

    Add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("What can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+
+        while (true) {
+            if (text.equals("bye")) {
+                break;
+            }
+            System.out.println("\t" + text);
+            scanner = new Scanner(System.in);
+            text = scanner.next();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
