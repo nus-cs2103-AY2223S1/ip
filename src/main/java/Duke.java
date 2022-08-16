d31db7b23e5da2e21f3fd84a2f9c7041d94d6105
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-17 03:58:07 +0800

    Refactor user input logic to use switch statements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,32 +6,38 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         List<Task> userData = new ArrayList<>();
+        boolean isActive = true;
 
         System.out.println("Hello! I'm Duke!\nWhat can I do for you?");
         System.out.println("-----------------------------------");
-        while (true) {
+        while (isActive) {
             Scanner input = new Scanner(System.in);
             String line = input.nextLine();
 
             String userInputs[] = line.split(" ");
-            if ("bye".equalsIgnoreCase(userInputs[0])) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if ("list".equalsIgnoreCase(userInputs[0])) {
-                for (int i = 0; i < userData.size(); i++) {
-                    System.out.format("%s. %s\n", i, userData.get(i));
-                }
-            } else if ("mark".equalsIgnoreCase(userInputs[0])) {
-                int position = Integer.valueOf(userInputs[1]);
-                userData.get(position).setIsComplete(true);
-                System.out.format("Nice! I've marked this task as done: [X] %s\n", userData.get(Integer.valueOf(userInputs[1])).toString());
-            } else if ("unmark".equalsIgnoreCase(userInputs[0])) {
-                int position = Integer.valueOf(userInputs[1]);
-                userData.get(position).setIsComplete(false);
-                System.out.format("OK, I've marked this task as not done yet: [ ] %s\n", userData.get(Integer.valueOf(userInputs[1])).toString());
-            } else{
-                userData.add(new Task(line, false));
-                System.out.format("added: %s\n", line);
+            String mainCommand = userInputs[0].toLowerCase();
+
+            switch (mainCommand) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    isActive = false;
+                    break;
+                case "list":
+                    for (int i = 0; i < userData.size(); i++) {
+                        System.out.format("%s. %s\n", i, userData.get(i));
+                    }
+                    break;
+                case "mark":
+                    userData.get(Integer.valueOf(userInputs[1])).setIsComplete(true);
+                    System.out.format("Nice! I've marked this task as done: [X] %s\n", userData.get(Integer.valueOf(userInputs[1])).toString());
+                    break;
+                case "unmark":
+                    userData.get(Integer.valueOf(userInputs[1])).setIsComplete(false);
+                    System.out.format("OK, I've marked this task as not done yet: [ ] %s\n", userData.get(Integer.valueOf(userInputs[1])).toString());
+                    break;
+                default:
+                    userData.add(new Task(line, false));
+                    System.out.format("added: %s\n", line);
             }
             System.out.println("-----------------------------------");
         }
