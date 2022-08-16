b56df731fe0eaebed628b62982f8624ed9db5c44
Derrick Khoo <derrickkhoo2000@gmail.com>
2022-08-16 21:16:55 +0800

    added Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    static void line() {
+        System.out.println("________________________________________");
+    }
+    static void greet() {
+        line();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        line();
+    }
+
+    public static void echo(String message) {
+        line();
+        System.out.println(message);
+        line();
+    }
+
+    public static void exit() {
+        line();
+        System.out.println("Bye. Hope to see you again soon!");
+        line();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +29,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        boolean isDone = false;
+        greet();
+        while (!isDone) {
+            Scanner scanner = new Scanner(System.in); // creating scanner for user input
+            String input = scanner.nextLine();
+            switch (input) {
+                case ("bye"): {
+                    exit();
+                    isDone = true;
+                    break;
+                }
+                default: {
+                    echo(input);
+                    break;
+                }
+            }
+        }
     }
 }
