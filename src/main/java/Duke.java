e64c2557b07c6fb97bb7fbb24b4ecd919d805e59
Lester Ong <lesterong776@gmail.com>
2022-08-16 20:32:01 +0800

    Handle commands using enum

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
+    public enum Command {
+        LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, BYE
+    }
     private static ArrayList<Task> tasks = new ArrayList<>();
     public static String getTaskDetails(int pos) {
         return "[" +
@@ -10,17 +13,17 @@ public class Duke {
                 "] " +
                 tasks.get(pos).getDescription();
     }
-    public static void addTask(String command) throws DukeException {
-        String[] cmdArr = command.split(" ", 2);
-        if (cmdArr.length != 2) {
+    public static void addTask(String input) throws DukeException {
+        String[] inputArr = input.split(" ", 2);
+        if (inputArr.length != 2) {
             throw new DukeException(
                     "The description of " +
-                            cmdArr[0] +
+                            inputArr[0] +
                             " cannot be empty!"
             );
         }
-        String type = cmdArr[0];
-        String details = cmdArr[1];
+        String type = inputArr[0];
+        String details = inputArr[1];
         switch (type) {
             case "todo":
                 tasks.add(new ToDo(details));
@@ -56,10 +59,10 @@ public class Duke {
                 );
         }
     }
-    public static void unknownCommand(String command) throws DukeException {
+    public static void unknownCommand(String input) throws DukeException {
         throw new DukeException(
                 "I'm unable to do '" +
-                         command +
+                         input +
                         "' at the moment. :("
         );
     }
@@ -71,16 +74,16 @@ public class Duke {
             System.out.println(pos + 1 + ". " + getTaskDetails(pos));
         }
     }
-    public static int handleTaskPos(String command) throws DukeException {
-        String[] cmdArr = command.split(" ");
+    public static int handleTaskPos(String input) throws DukeException {
+        String[] inputArr = input.split(" ");
         int pos;
-        if (cmdArr.length == 1) {
+        if (inputArr.length == 1) {
             throw new DukeException("Provide a task number to mark");
-        } else if (cmdArr.length > 2) {
+        } else if (inputArr.length > 2) {
             throw new DukeException("Provide a valid task number!");
         }
         try {
-            pos = Integer.parseInt(command.split(" ", 2)[1]) - 1;
+            pos = Integer.parseInt(input.split(" ", 2)[1]) - 1;
         } catch (NumberFormatException e) {
             throw new DukeException("Provide a numeric task number!");
         }
@@ -92,28 +95,26 @@ public class Duke {
         }
         return pos;
     }
-    public static void markTask(String command) throws DukeException {
-        int pos = handleTaskPos(command);
-        switch (command.split(" ")[0]) {
+    public static void markTask(String input) throws DukeException {
+        int pos = handleTaskPos(input);
+        switch (input.split(" ")[0]) {
             case "mark":
                 tasks.get(pos).markAsDone();
                 System.out.println(
-                        "Nice! I've marked this task as done" +
-                                "\n" +
+                        "Nice! I've marked this task as done\n" +
                                 getTaskDetails(pos)
                 );
                 break;
             case "unmark":
                 tasks.get(pos).markAsUndone();
                 System.out.println(
-                        "Nice! I've marked this task as undone" +
-                                "\n" +
+                        "Nice! I've marked this task as undone\n" +
                                 getTaskDetails(pos)
                 );
         }
     }
-    public static void deleteTask(String command) throws DukeException {
-        int pos = handleTaskPos(command);
+    public static void deleteTask(String input) throws DukeException {
+        int pos = handleTaskPos(input);
         System.out.println(
                 "OK. I've removed this task:\n" +
                         getTaskDetails(pos) +
@@ -125,39 +126,39 @@ public class Duke {
         tasks.remove(pos);
     }
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Hello! I'm Duke.\nWhat can I do for you?\n>> ");
-        String command = input.nextLine();
-        while (!command.equals("bye")) {
-            String cmdMain = command.split(" ", 2)[0];
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            Command mainCmd = Command.valueOf(input.split(" ", 2)[0].toUpperCase());
             try {
-                switch (cmdMain) {
-                    case "list":
+                switch (mainCmd) {
+                    case LIST:
                         listTasks();
                         break;
-                    case "mark":
-                    case "unmark":
-                        markTask(command);
+                    case MARK:
+                    case UNMARK:
+                        markTask(input);
                         break;
-                    case "todo":
-                    case "deadline":
-                    case "event":
-                        addTask(command);
+                    case TODO:
+                    case DEADLINE:
+                    case EVENT:
+                        addTask(input);
                         break;
-                    case "delete":
-                        deleteTask(command);
+                    case DELETE:
+                        deleteTask(input);
                         break;
                     default:
-                        unknownCommand(command);
+                        unknownCommand(input);
                     }
             } catch (DukeException e) {
                 System.out.println(e.getMessage());
             } finally {
                 System.out.print("\n>> ");
-                command = input.nextLine();
+                input = scanner.nextLine();
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
-        input.close();
+        scanner.close();
     }
 }
