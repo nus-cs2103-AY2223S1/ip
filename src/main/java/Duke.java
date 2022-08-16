e14f2b43924042c95e2bf7807c942fba7db98c4a
chantellyu <e0758927@u.nus.edu>
2022-08-16 13:34:42 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        while (true) {
+            String userIn = myObj.nextLine();  // Read user input
+            if (userIn.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else {
+                System.out.println(userIn);  // Output user input
+            }
+        }
+
     }
 }
