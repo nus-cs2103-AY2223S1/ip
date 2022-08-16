908952dc61a641a12872dd655242aa84c44136be
Linus <linuslee2000@gmail.com>
2022-08-16 11:33:21 +0800

    feat: add echo and bye functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,27 @@
 public class Duke {
     public static void makeLine() {
         for (int i = 0; i < 50; i++) {
-            System.out.print("-");
+            System.out.print("\u2015");
         }
         System.out.println();
     }
+
+    private void echo(String command) {
+        System.out.println(command);
+    }
+
+    private void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        makeLine();
         System.out.println("Hello from\n" + logo);
+        makeLine();
     }
 }
