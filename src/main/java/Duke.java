d1d304a543b8ace665ee3fe39e079a39199adddc
ytingtan <e0773786@u.nus.edu>
2022-08-16 10:35:04 +0800

    Made Task class, corrected minor indexing error

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
+import main.java.Task;
+
+import static main.java.Task.*;
 
 public class Duke {
 
+    private static Scanner sc = new Scanner(System.in);
+    private static Task[] taskList = new Task[100];
     private static int index = 0;
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        Scanner sc = new Scanner(System.in);
-        String[] taskList = new String[100];
 
         while (sc.hasNext()) {
             String output = sc.nextLine();
@@ -17,17 +21,31 @@ public class Duke {
                 break;
             } else if (output.equals("list")) {
                 printList(taskList);
+            } else if (output.contains("unmark")) {
+                // solution below taken from https://stackoverflow.com/questions/33186650/input-string-and-int-in-the-same-line
+                String[] val = output.split(" ");
+                int idx = Integer.parseInt(val[1]);
+                Task undone = taskList[idx];
+                markAsUndone(undone);
+            } else if (output.contains("mark")) {
+                String[] val = output.split(" ");
+                int idx = Integer.parseInt(val[1]);
+                Task done = taskList[idx];
+                markAsDone(done);
             } else {
-                taskList[index] = output;
-                index++;
-                System.out.println("added: " + output);
+                    Task newtask = new Task(output);
+                    taskList[index] = newtask;
+                    index++;
+                    System.out.println("added: " + printOnAdd(newtask));
+
             }
         }
     }
 
-    public static void printList(String[] list) {
-        for (int i = 0; i < index; i++) {
-            System.out.println(i + ". " + list[i]);
+    public static void printList(Task[] list) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 1; i < index + 1; i++) {
+            System.out.println(i + "." + printTask(list[i - 1]));
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,40 @@
+package main.java;
+
+public class Task {
+    protected String name;
+    protected boolean isDone;
+
+    // constructor
+    public Task(String taskName) {
+        name = taskName;
+        isDone = false;
+    }
+
+    // getStatusIcon taken from cs2103 module website
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public static String printTask(Task task) {
+        return "[" + task.getStatusIcon() + "] " + task.name;
+    }
+
+    public static String printOnAdd(Task task) {
+        return task.name;
+    }
+
+    public static void markAsDone(Task task) {
+        task.isDone = true;
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("[" + task.getStatusIcon() + "] " + task.name);
+    }
+
+    public static void markAsUndone(Task task) {
+        task.isDone = false;
+        System.out.println("Ok. I've marked this task as not done yet: ");
+        System.out.println("[" + task.getStatusIcon() + "] " + task.name);
+    }
+
+
+
+}
