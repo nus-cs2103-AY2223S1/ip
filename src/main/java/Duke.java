5ccf904be10abc5aa5086ab91d93fb94a31c0162
sjoann <joansim19@gmail.com>
2022-08-16 10:58:05 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("____________________________________________________________\n" +
+                "Hello! I'm Duke\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________");
+
+        String strInput = sc.nextLine();
+        speak(strInput);
+    }
+
+    /**
+     * Prints out a greeting after user input.
+     *
+     * @param s Input from user.
+     **/
+    public static void speak(String s) {
+        if (s.equalsIgnoreCase("bye")) {
+            System.out.println("____________________________________________________________\n" +
+                    "Bye. Hope to see you again soon!" +
+                    "\n____________________________________________________________");
+
+        } else {
+            System.out.println("____________________________________________________________\n" +
+                    s + "\n____________________________________________________________");
+        }
     }
 }
