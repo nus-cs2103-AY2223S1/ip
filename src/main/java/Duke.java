f4039a9cf9e5895cdba5423f71af1279332e91f8
bryanhce <ho.cheng.en.bryan@gmail.com>
2022-08-16 12:58:11 +0800

    level 2 add list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String horizontalLine = "______________________________________ \n";
+        String adddedStr = "added: ";
 
         String firstText = horizontalLine
                 + "hi... I'm Karen \n"
@@ -10,12 +12,31 @@ public class Duke {
                 + horizontalLine;
         System.out.println(firstText);
 
+        ArrayList<String> list = new ArrayList<>();
+
         Scanner sc = new Scanner(System.in);
-        String echoText = sc.nextLine();
-        while (!echoText.equals("bye")) {
-            System.out.println(horizontalLine + echoText + "\n" + horizontalLine);
-            echoText = sc.nextLine();
+
+        while (true) {
+            String echoText = sc.nextLine();
+            if (echoText.equals("bye")) {
+                System.out.println(horizontalLine + "K finally, good riddance! \n" + horizontalLine);
+                break;
+            } else if (echoText.equals("list")) {
+                System.out.println(horizontalLine);
+                if (list.size() == 0) {
+                    System.out.println("pff there is nothing in your list");
+                } else {
+                    for (int i = 1; i < list.size() + 1; i++) {
+                        System.out.println(i + ". " + list.get(i - 1));
+                    }
+                }
+                System.out.println(horizontalLine);
+            } else {
+                System.out.println(horizontalLine + adddedStr + echoText + "\n" + horizontalLine);
+                list.add(echoText);
+            }
+
         }
-        System.out.println("K finally, good riddance!");
+
     }
 }
