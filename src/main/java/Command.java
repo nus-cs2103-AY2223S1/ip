4f9ff1487bcf56e0b643aaccff3376ec2b6b5e40
shenyihong <shen_yi_hong@outlook.com>
2022-08-16 22:37:51 +0800

    Added Command enums and Task class

---
+++ src/main/java/Command.java
@@ -0,0 +1,7 @@
+public enum Command {
+    BYE, // termination
+    ADD, // add task to list
+    LIST, // display list
+    MARK, // mark a task
+    UNMARK // unmark a task
+}
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+
+    protected final String title;
+    protected boolean marked;
+
+    Task(String title) {
+        this.title = title;
+        this.marked = false;
+    }
+
+    public void mark() {
+        this.marked = true;
+    }
+
+    public void unmark() {
+        this.marked = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%c] %s", this.marked ? 'X' : ' ', this.title);
+    }
+}
