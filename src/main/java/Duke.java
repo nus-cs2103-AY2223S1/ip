106aa9f7d851a36929e1c897c5f9c6fb0153095e
farisa <farisanadia2802@gmail.com>
2022-08-16 19:42:24 +0800

    skeletal version of duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String buffLine = "    _____________________________________\n";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("    Hello from\n" + logo);
+        System.out.println("    What can I do for you?\n" + buffLine);
+        String userReply = sc.nextLine();
+        while (!userReply.equals("bye")) {
+            System.out.println(buffLine + "\n" + "    " + userReply + "\n" + buffLine);
+            userReply = sc.nextLine();
+        }
+        System.out.println(buffLine + "\n" + "    Bye. Hope to see you again soon!"
+                + "\n" + buffLine);
     }
 }
