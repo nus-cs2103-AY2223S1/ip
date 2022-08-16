650ac7974e4a25f1a29dc0cd407eb1570f5ea7a5
SpecOps2016 <gregory.wong@ymail.com>
2022-08-16 21:02:01 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,32 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class Duke {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        @Override
+        public String toString() {
+            return this.description;
+        }
+
+        public void setStatus(boolean value) {
+            this.isDone = value;
+        }
+    }
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,7 +34,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        ArrayList<String> ls = new ArrayList<>();
+        ArrayList<Task> ls = new ArrayList<>();
         while (true) {
             Scanner sc = new Scanner(System.in);
             String line = sc.nextLine();
@@ -20,14 +44,29 @@ public class Duke {
             }
 
             else if(line.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i=0; i<ls.size(); i++) {
-                    System.out.println(i+1 + "." +" " + ls.get(i));
+                    System.out.println(i+1 + "." +" " + "[" + ls.get(i).getStatusIcon()+"]" +" " + ls.get(i).toString());
                 }
+            }
+
+            else if(line.contains("unmark")) {
+                int num = Integer.parseInt(line.substring(7));
+                ls.get(num-1).setStatus(false);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + ls.get(num-1).getStatusIcon() +"]" +" " +ls.get(num-1).toString());
+            }
+
+            else if(line.contains("mark")) {
+                int num = Integer.parseInt(line.substring(5));
+                ls.get(num-1).setStatus(true);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + ls.get(num-1).getStatusIcon() +"]" +" " +ls.get(num-1).toString());
             }
 
             else {
                 System.out.println("added: " + line);
-                ls.add(line);
+                ls.add(new Task(line));
             }
 
 
