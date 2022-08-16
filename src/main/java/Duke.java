1d935ee6f0dd1408a6992bcc0271b3aed8cc4a05
tan xuan yun <tanxuanyun42@gmail.com>
2022-08-16 20:59:03 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String logo = "Turtle";
-        System.out.println("Hello from " + logo +"!");
+        System.out.println("Hello I am " + logo +"!");
+        System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
         boolean run = true;
         String input;
+        ArrayList<String> items = new ArrayList<String>();
 
         while(run) {
             System.out.println("\n--------------------");
             input = sc.nextLine();
 
             if(input.equals("bye")) {
-                System.out.println(input);
+                System.out.println("Goodbye!");
                 run = false;
+            } else if(input.equals("list")) {
+                for ( int i = 0; i < items.size(); i++) {
+                    System.out.println(i+1 + ". " + items.get(i));
+                }
             } else {
-                System.out.print(input);
+                items.add(input);
+                System.out.print("added: " + input);
             }
         }
     }
