ac77926b90db1249e951ebd33f84cf439a1cdf81
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-17 03:50:52 +0800

    Add ability for user to add to a list of tasks where each task can be marked and unmarked

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,36 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        List<String> userData = new ArrayList<>();
+        List<Task> userData = new ArrayList<>();
 
         System.out.println("Hello! I'm Duke!\nWhat can I do for you?");
         System.out.println("-----------------------------------");
         while (true) {
             Scanner input = new Scanner(System.in);
             String line = input.nextLine();
-            if ("bye".equalsIgnoreCase(line)) {
+
+            String userInputs[] = line.split(" ");
+            if ("bye".equalsIgnoreCase(userInputs[0])) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if ("list".equalsIgnoreCase(line)) {
+            } else if ("list".equalsIgnoreCase(userInputs[0])) {
                 for (int i = 0; i < userData.size(); i++) {
                     System.out.format("%s. %s\n", i, userData.get(i));
                 }
+            } else if ("mark".equalsIgnoreCase(userInputs[0])) {
+                int position = Integer.valueOf(userInputs[1]);
+                userData.get(position).setIsComplete(true);
+                System.out.format("Nice! I've marked this task as done: [X] %s\n", userData.get(Integer.valueOf(userInputs[1])).toString());
+            } else if ("unmark".equalsIgnoreCase(userInputs[0])) {
+                int position = Integer.valueOf(userInputs[1]);
+                userData.get(position).setIsComplete(false);
+                System.out.format("OK, I've marked this task as not done yet: [ ] %s\n", userData.get(Integer.valueOf(userInputs[1])).toString());
             } else{
-                userData.add(line);
+                userData.add(new Task(line, false));
                 System.out.format("added: %s\n", line);
             }
             System.out.println("-----------------------------------");
