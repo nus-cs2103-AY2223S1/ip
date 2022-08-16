64ac1900fc7586a9fa8d324f4118dfc014d10fe6
samuelcheongws <samuelcheong2000@gmail.com>
2022-08-16 21:00:34 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -11,18 +12,30 @@ public class Duke {
     private static final String GREETINGS = "Hello! I'm Duke\nWhat can I do for you?\n";
 
     private static void echo(String str) {
-        System.out.println(str);
+        System.out.println("added: " + str);
+    }
+
+    private static void printArrAsNumberedList(ArrayList<String> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println(i+1 + ". " + arr.get(i));
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("Hello from\n" + logo);
         System.out.println(GREETINGS);
 
+        ArrayList<String> strArr = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            echo(input);
+            if (input.equals("list")) {
+                printArrAsNumberedList(strArr);
+            } else {
+                strArr.add(input);
+                echo(input);
+            }
             input = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!\n");
