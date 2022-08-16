97e71645d52c7e0ac8ff48c0c4e3309873c4322b
tan xuan yun <tanxuanyun42@gmail.com>
2022-08-16 20:35:01 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "Turtle";
+        System.out.println("Hello from " + logo +"!");
+        Scanner sc = new Scanner(System.in);
+        boolean run = true;
+        String input;
+
+        while(run) {
+            System.out.println("\n--------------------");
+            input = sc.nextLine();
+
+            if(input.equals("bye")) {
+                System.out.println(input);
+                run = false;
+            } else {
+                System.out.print(input);
+            }
+        }
     }
 }
