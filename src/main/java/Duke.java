2d1f42808b8b65a672dbb69fb3102bba5e3fac06
sjoann <joansim19@gmail.com>
2022-08-16 12:01:53 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static List<String> strArray = new ArrayList<String>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm Duke\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________");
-
+        System.out.println("____________________________________________________________\n"
+                + " Hello! I'm Duke\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________");
         String strInput = sc.nextLine();
-        speak(strInput);
+        while (!strInput.equals("")) {
+            if (strInput.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________\n"
+                        + " Bye. Hope to see you again soon!"
+                        + "\n____________________________________________________________");
+            } else if ((strInput.equalsIgnoreCase("list"))){
+                System.out.println("____________________________________________________________"
+                        + enumerateList()
+                        + "\n____________________________________________________________");
+            } else {
+                addToList(strInput);
+            }
+            strInput = sc.nextLine();
+        }
+        sc.close();
     }
 
     /**
-     * Prints out a greeting after user input.
+     * Adds user input in the form of String into the strArray.
      *
      * @param s Input from user.
-     **/
-    public static void speak(String s) {
-        if (s.equalsIgnoreCase("bye")) {
-            System.out.println("____________________________________________________________\n" +
-                    "Bye. Hope to see you again soon!" +
-                    "\n____________________________________________________________");
+     */
+    public static void addToList(String s) {
+        strArray.add(s);
+        System.out.println("____________________________________________________________\n"
+                + " added: "
+                + s
+                + "\n____________________________________________________________");
+    }
 
-        } else {
-            System.out.println("____________________________________________________________\n" +
-                    s + "\n____________________________________________________________");
+    /**
+     * Returns a string representation of the list.
+     *
+     * @return String.
+     */
+    public static String enumerateList() {
+        //StringBuilder over string concat for better performance
+        StringBuilder s = new StringBuilder(" ");
+        for (int i = 1; i <= strArray.size(); i++) {
+            s.append("\n" + i + ". " + strArray.get(i - 1));
         }
+        return s.toString();
     }
 }
