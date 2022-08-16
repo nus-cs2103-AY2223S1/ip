52e4a872ea72d871ef05632eac566e6521fd36c9
nmtuan_14 <minhtuan@botmd.io>
2022-08-16 16:11:59 +0800

    Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,88 +5,111 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Duke {
-    public static List<Task> tasks = new ArrayList<>();
+    private Scanner sc = new Scanner(System.in);
+    private List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.run();
+    }
+
+    private void run() {
         print("Hello! I'm Duke\nWhat can I do for you?");
-        Scanner sc = new Scanner(System.in);
-        scanner: while (sc.hasNext()) {
+        while (sc.hasNext()) {
             String input = sc.next();
-            switch (input) {
-                case "list":
-                    list();
-                    break;
-                
-                case "todo":
-                case "deadline":
-                case "event":
-                    add(input, sc.nextLine());
-                    break;
+            try {
+                switch (input) {
+                    case "list":
+                        list();
+                        break;
+                    
+                    case "todo":
+                    case "deadline":
+                    case "event":
+                        add(input, sc.nextLine());
+                        break;
 
-                case "blah":
-                    blah();
-                    break;
-                
-                case "mark":
-                    mark(sc.nextInt() - 1);
-                    break;
+                    case "mark":
+                        mark(sc.nextLine());
+                        break;
 
-                case "unmark":
-                    unmark(sc.nextInt() - 1);
-                    break;
-                
-                case "bye":
-                    bye();
-                    sc.close();
-                    break scanner;
-                
-                default:
-                    sc.nextLine();
-                    print("Sorry, I'm not sure what you meant.");
-                    break;
+                    case "unmark":
+                        unmark(sc.nextLine());
+                        break;
+                    
+                    case "bye":
+                        bye();
+                        sc.close();
+                        return;
+                    
+                    default:
+                        sc.nextLine();
+                        throw new DukeException("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                print(e.getMessage());
             }
         }
     }
 
-    public static void print(String s) {
+    private void print(String s) {
         String seperator = "    ____________________________________________________________\n";
         System.out.println(seperator + Stream.of(s.split("\n")).map(x -> "     " + x + "\n").reduce("", (x,y) -> x + y) + seperator);
     }
 
-    public static void list() {
+    private void list() {
         print(IntStream.range(0, tasks.size()).mapToObj(x -> String.format("%d.%s\n", x + 1, tasks.get(x))).reduce("Here are the tasks in your list:\n", (x,y) -> x + y));
     }
 
-    public static void blah() {
-        print("blah");
-    }
-
-    public static void bye() {
+    private void bye() {
         print("Bye. Hope to see you again soon!");
     }
 
-    public static void add(String taskType, String taskDetails) {
+    private String[] getTaskDetails(String rawDetails, String delimiter) throws DukeException {
+        String[] details = rawDetails.split(delimiter);
+        if (details.length != 2 || details[0].isEmpty() || details[1].isEmpty()) {
+            throw new DukeException("\u2639 OOPS!!! Please make sure task description and dates are inputted properly!");
+        }
+        return details;
+    }
+
+    private void add(String taskType, String rawDetails) throws DukeException {
         Task task;
+        if (rawDetails.isEmpty()) throw new DukeException(String.format("\u2639 OOPS!!! The description of a %s cannot be empty.", taskType));
         if (taskType.equals("deadline")) {
-            String[] details = taskDetails.split(" /by ");
+            String[] details = getTaskDetails(rawDetails, " /by ");
             task = new Deadline(details[0], details[1]);
         } else if (taskType.equals("event")) {
-            String[] details = taskDetails.split(" /at ");
+            String[] details = getTaskDetails(rawDetails, " /at ");
             task = new Event(details[0], details[1]);
         } else {
-            task = new Task(taskDetails);
+            task = new Task(rawDetails);
         }
         tasks.add(task);
         print(String.format("Got it. I've added this task:\n  %s\nNow you have %d tasks in the list.", tasks.get(tasks.size() - 1), tasks.size()));
     }
 
-    public static void mark(int index) {
-        tasks.get(index).markAsDone();
-        print("Nice! I've marked this task as done:\n  " + tasks.get(index));
+    private void mark(String input) throws DukeException {
+        try {
+            int index = Integer.parseInt(input);
+            tasks.get(index - 1).markAsDone();
+            print("Nice! I've marked this task as done:\n  " + tasks.get(index - 1));
+        } catch (NumberFormatException e) {
+            throw new DukeException(String.format("\u2639 OOPS!!! Invalid index %s. Index must be an integer and must not be blank.", input));
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException(String.format("\u2639 OOPS!!! Invalid index %s. You only have %d tasks in your list.", input, tasks.size()));
+        }
     }
 
-    public static void unmark(int index) {
-        tasks.get(index).markAsNotDone();
-        print("OK, I've marked this task as not done yet:\n  " + tasks.get(index));
+    private void unmark(String input) throws DukeException {
+        try {
+            int index = Integer.parseInt(input);
+            tasks.get(index - 1).markAsDone();
+            print("OK, I've marked this task as not done yet:\n  " + tasks.get(index - 1));
+        } catch (NumberFormatException e) {
+            throw new DukeException(String.format("\u2639 OOPS!!! Invalid index %s. Index must be an integer and must not be blank.", input));
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException(String.format("\u2639 OOPS!!! Invalid index %s. You only have %d tasks in your list.", input, tasks.size()));
+        }
     }
 }
