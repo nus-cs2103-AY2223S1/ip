0d550692e19559d33a370b87dfc806b5bfce2f1b
chloeelim <chloelim22@gmail.com>
2022-08-17 01:36:48 +0800

    Level 1. Greet, Echo, Exit
    Skeletal implementation of Duke that:
    * greets the user,
    * echoes commands entered by the user,
    * and exits when the user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    private static String DIVIDER = "\t____________________________________________________________";
+
+    /**
+     * Formats Duke's messages by adding horizontal line dividers and indentation.
+     * @param str Duke's message to be printed out
+     */
+    private static void prettyPrint(String str) {
+        String[] messages = str.split("\n");
+        System.out.println(DIVIDER);
+        for (String message : messages) {
+            System.out.println("\t  " + message);
+        }
+        System.out.println(DIVIDER);
+    }
+
+    private static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcomeMessage = String.join("\n", logo,
+                "Hello! I'm Duke", "What can I do for you?");
+        prettyPrint(welcomeMessage);
+    }
+
+    private static void goodbye() {
+        String farewellMessage = "Bye. Hope to see you again soon!";
+        prettyPrint(farewellMessage);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cmd;
+        greet();
+        // Echoes user's input until the user types 'bye', for which the program exits
+        while(!(cmd = sc.nextLine()).equals("bye")) {
+            prettyPrint(cmd);
+        }
+        goodbye();
     }
 }
