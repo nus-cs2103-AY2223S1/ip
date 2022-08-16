b9eb9989dd68940a9bfb1fa412bf19b1db685102
chm252 <chinhongming252@gmail.com>
2022-08-17 00:47:37 +0800

    Handling errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,47 +2,100 @@ import java.util.Scanner;
 public class Duke {
     private static Task[] storage = new Task[100];
     private static int i = 0;
+    private static void mark(String userInput) {
+        int intCollect = Integer.parseInt(userInput.substring(5));
+        storage[intCollect - 1].mark();
+    }
+
+    private static void unmark(String userInput) {
+        int intCollect = Integer.parseInt(userInput.substring(7));
+        storage[intCollect - 1].unmark();
+    }
+
+    private static void printList(String userInput) {
+        System.out.println("Here are the tasks in your list:");
+        int j = 0;
+        while (storage[j] != null) {
+            System.out.println((j + 1) + "." + storage[j].toString());
+            j++;
+        }
+    }
+
+    private static void toDo(String userInput) throws DukeException {
+        if (userInput.length() <= 5) {
+            throw new DukeException();
+        }
+        storage[i] = new ToDo(userInput.substring(5));
+        i++;
+        System.out.println("Got it. I've added this task:\n"
+                +storage[i-1].toString()
+                + "\nNow you have " + i + " tasks in the list.");
+    }
+
+    private static void deadline(String userInput) throws DukeException {
+        if (userInput.length() <= 9) {
+            throw new DukeException();
+        }
+        String[] box = userInput.substring(9).split(" /by ");
+        storage[i] = new Deadline(box[0], box[1]);
+        i++;
+        System.out.println("Got it. I've added this task:\n"
+                +storage[i-1].toString()
+                + "\nNow you have " + i + " tasks in the list.");
+    }
+
+    private static void event(String userInput) throws DukeException {
+        if (userInput.length() <= 6) {
+            throw new DukeException();
+        }
+        String[] box = userInput.substring(6).split(" /at ");
+        storage[i] = new Event(box[0], box[1]);
+        i++;
+        System.out.println("Got it. I've added this task:\n"
+                +storage[i-1].toString()
+                + "\nNow you have " + i + " tasks in the list.");
+    }
+    private static void exceptionThrower() throws DukeException {
+        throw new DukeException();
+    }
+
     public static void main(String[] args) {
         System.out.println("Hey Dude here\n" + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
             if (userInput.startsWith("mark ")) {
-                int intCollect = Integer.parseInt(userInput.substring(5));
-                storage[intCollect - 1].mark();
+                mark(userInput);
             } else if (userInput.startsWith("unmark ")) {
-                int intCollect = Integer.parseInt(userInput.substring(7));
-                storage[intCollect - 1].unmark();
+                unmark(userInput);
             } else if (userInput.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                int j = 0;
-                while (storage[j] != null) {
-                    System.out.println((j + 1) + "." + storage[j].toString());
-                    j++;
+                printList(userInput);
+            } else if (userInput.equals("todo") || userInput.startsWith("todo" )) {
+                try {
+                    toDo(userInput);
+                } catch (DukeException e) {
+                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
                 }
-            } else if (userInput.startsWith("todo ")) {
-                storage[i] = new ToDo(userInput.substring(5));
-                i++;
-                System.out.println("Got it. I've added this task:\n"
-                        +storage[i-1].toString()
-                        + "\nNow you have " + i + " tasks in the list.");            } else if (userInput.startsWith("deadline ")) {
-                String[] box = userInput.substring(9).split(" /by ");
-                storage[i] = new Deadline(box[0], box[1]);
-                i++;
-                System.out.println("Got it. I've added this task:\n"
-                        +storage[i-1].toString()
-                        + "\nNow you have " + i + " tasks in the list.");            } else if (userInput.startsWith("event ")) {
-                String[] box = userInput.substring(6).split(" /at ");
-                storage[i] = new Event(box[0], box[1]);
-                i++;
-                System.out.println("Got it. I've added this task:\n"
-                        +storage[i-1].toString()
-                        + "\nNow you have " + i + " tasks in the list.");
+            } else if (userInput.equals("deadline") || userInput.startsWith("deadline ")) {
+                try {
+                    deadline(userInput);
+                } catch (DukeException e) {
+                    System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+                }
+            } else if (userInput.equals("event") || userInput.startsWith("event ")) {
+                try {
+                    event(userInput);
+                } catch (DukeException e) {
+                    System.out.println("☹ OOPS!!! The description of a event cannot be empty.");
+                }
             }
             else {
-                storage[i] = new Task(userInput);
-                i++;
-                System.out.println("added: " + userInput);
+                try {
+                    exceptionThrower();
+                }
+                catch (DukeException e){
+                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
             }
             userInput = sc.nextLine();
         }
