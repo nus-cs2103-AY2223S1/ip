46a519c92c0cb95870ce1b3d0091bab71e57f9f9
linuschancs <mrlinuschan@gmail.com>
2022-08-16 15:14:29 +0800

    Added new class for Tasks with class methods to mark tasks as done/undone, get description and get status icon

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,17 +6,17 @@ public class Duke {
         System.out.println("Greetings Human! I am BetaGo, your personal robot assistant!\nHow may I assist you today?\n");
         Scanner sc = new Scanner(System.in);
         String str= sc.nextLine();
-        String[] storage = new String[100];
+        Task[] storage = new Task[100];
         int counter = 0;
         while (!str.equalsIgnoreCase("bye")) {
             if (str.equalsIgnoreCase("list")) {
                 for (int i = 0; i < counter; i++) {
                     System.out.print(i+1);
-                    System.out.println(". " + storage[i] + "\n");
+                    System.out.println(". " + storage[i].getTaskDescription() + "\n");
                 }
             } else {
-                System.out.println("added: " + str + "\n");
-                storage[counter] = str;
+                System.out.println("Task added: " + str + "\n");
+                storage[counter] = new Task(str);
                 counter++;
             }
             str= sc.nextLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+package main.java;
+
+public class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public String getTaskDescription() {
+            return this.description;
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsNotDone() {
+            this.isDone = false;
+        }
+}
