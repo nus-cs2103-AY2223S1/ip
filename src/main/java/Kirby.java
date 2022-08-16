df6467a574c727eb772900fd43a17eb926e804c5
sltsheryl <sltsheryl@gmail.com>
2022-08-16 22:54:06 +0800

    Add Level

--- src/main/java/Kirby.java
+++ src/main/java/Kirby.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Kirby {
     public static void main(String[] args) {
+        ArrayList<String> previousInputs = new ArrayList<>();
         System.out.println("Kirby: Hai I'm Kirby (੭｡╹▿╹｡)੭ your friendly chat assistant!! \n" +
-                "How are you today?");
+                "What amazing plans do you have today?");
         while (true){
             Scanner scanner = new Scanner(System.in);
             String inputString = scanner.nextLine();
@@ -10,8 +12,15 @@ public class Kirby {
                 System.out.println("I loved talking to you :> \n" +
                         "Hope to see you again!");
                 break;
+            }
+            if (inputString.equals("list")) {
+                System.out.println("Here is what you wanted: ");
+                for (int i = 0; i < previousInputs.size(); i++) {
+                    System.out.println(i + 1 + ": " + previousInputs.get(i));
+                }
             } else {
-                System.out.println(inputString);
+                previousInputs.add(inputString);
+                System.out.println("Added into Kirby's bag of items: " + inputString);
             }
         }
     }
