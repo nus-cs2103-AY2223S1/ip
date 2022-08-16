27651e5cd0677debe6010bd4e32335faa7652146
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-17 03:47:54 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private boolean isComplete;
+    private String toDoName;
+
+    public Task(String toDoName, boolean isComplete) {
+        this.toDoName = toDoName;
+        this.isComplete = isComplete;
+    }
+
+    public String getToDoName() {
+        return this.toDoName;
+    }
+
+    public boolean getIsComplete() {
+        return this.isComplete;
+    }
+
+    public void setIsComplete(boolean completedStatus) {
+       this.isComplete = completedStatus;
+    }
+
+    @Override
+    public String toString() {
+        return this.toDoName;
+    }
+}
