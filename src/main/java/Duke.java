0c6ea439a4a2ea5c2c142f98c736448e2c410e64
Zsigmond <e0725064@u.nus.edu>
2022-08-16 18:55:36 +0800

    feat: add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -6,6 +7,7 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println("------------------------------");
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> commands = new ArrayList<>();
         while(true) {
             String command = scanner.next();
             System.out.println("------------------------------");
@@ -13,8 +15,14 @@ public class Duke {
                 case "bye":
                     System.out.println("Bye. Hope to see you again soon!");
                     break;
+                case "list":
+                    for (int i = 0; i < commands.size(); i++) {
+                        System.out.printf("%d. %s\n", i + 1, commands.get(i));
+                    }
+                    break;
                 default:
-                    System.out.println(command);
+                    commands.add(command);
+                    System.out.printf("added: %s\n", command);
                     break;
             }
             System.out.println("------------------------------");
