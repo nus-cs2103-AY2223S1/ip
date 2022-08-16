ff2bf3f23ea4c7d0d516c6f2bd6e186ee3bd97e7
Dilysss <e0772712@u.nus.edu>
2022-08-16 18:04:45 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -9,7 +10,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        lvlOne();
+        //lvlOne();
+        lvlTwo();
     }
 
     public static void lvlOne() {
@@ -23,6 +25,60 @@ public class Duke {
             sc = new Scanner(System.in);
             str = sc.nextLine();
             System.out.println(str);
+        }
+        System.out.println("Bye! See you soon!");
+    }
+
+    /**public static void lvlTwo() {
+        String[] tasks = new String[100];
+        int taskNum = 0;
+
+        System.out.println("Hello! I'm Duke\n");
+        System.out.println("What tasks do you have to do?");
+        Scanner sc = new Scanner(System.in);
+        String str2 = sc.nextLine();
+
+        while (!str2.equals("bye")) {
+            if (str2.equals("list")) {
+                for (int i = 0; i < taskNum; i++) {
+                    int num = i + 1;
+                    System.out.println(num + ". " + tasks[i]);
+                }
+                str2 = sc.nextLine();
+                continue;
+            }
+
+            tasks[taskNum] = str2;
+            taskNum++;
+            System.out.println("added: " + str2);
+            str2 = sc.nextLine();
+        }
+        System.out.println("Bye! See you soon!");
+    }*/
+
+    public static void lvlTwo() {
+        String[] tasks = new String[100];
+        int taskNum = 0;
+
+        System.out.println("Hello! I'm Duke\n");
+        System.out.println("What tasks do you have to do?");
+        Scanner sc = new Scanner(System.in);
+        String str2 = sc.nextLine();
+
+        while (!str2.equals("bye")) {
+            if (str2.equals("list")) {
+                for (int i = 0; i < taskNum; i++) {
+                    int num = i + 1;
+                    System.out.println(num + ". " + tasks[i]);
+                }
+                str2 = sc.nextLine();
+                continue;
+            }
+
+            tasks[taskNum] = str2;
+            taskNum++;
+            System.out.println("added: " + str2);
+            str2 = sc.nextLine();
         }
         System.out.println("Bye! See you soon!");
     }
