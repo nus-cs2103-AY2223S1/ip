ae090978db7fcc4a81205da2f8715afe69e59de4
guokweijie <guokweijie@gmail.com>
2022-08-16 21:00:07 +0800

    completed Level 1 of ip

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcome = "Hello! I'm Duke\nWhat can I do for you?\n";
+        System.out.println(welcome);
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String input = sc.next();
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
