0a5c9776476b44acc2db016a8033ac35c90cfb1f
zzthian <zzthian@hotmail.com>
2022-08-17 00:26:25 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Tracker tracker = new Tracker();
+        tracker.simulate();
     }
 }
