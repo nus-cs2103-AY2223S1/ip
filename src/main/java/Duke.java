42488b72b0a99917366651072ac745cf13868bb6
Daofu <xu.daofu20@gmail.com>
2022-08-16 16:01:36 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner; //import Scanner class
 public class Duke {
+    private static boolean END;
+    /*
+    Decides on what the chatbox will response with based on user input.
+     */
+    public void Response(String input) {
+        if (input.equals("bye")) {
+            END = true;
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(input);
+        }
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        END = false;
+        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        while (!END) {
+            duke.Response(myScanner.nextLine());
+        }
+        myScanner.close();
     }
 }
