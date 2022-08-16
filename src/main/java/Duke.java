46846621aaa0ac0e57b63edf24ce32537713ce16
John Benedict Yan <johnbenedictyan@gmail.com>
2022-08-16 10:29:41 +0800

    Level 2. Add, List Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 import java.io.File;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    static final String exitCommand = "bye";
-    static final String exitMessage = "Goodbye and have a nice day!";
+    static private final String exitCommand = "bye";
+    static private final String listCommand = "list";
+    static private final String exitMessage = "Goodbye and have a nice day!";
 
     public static void main(String[] args) {
         File file = new File("src/main/assets/logo.txt");
@@ -22,12 +24,31 @@ public class Duke {
         Scanner scannerObj = new Scanner(System.in); // Create a Scanner object
         String userInput = "";
 
-        while (!Objects.equals(userInput, exitCommand)) {
+        List<String> storage = new ArrayList<>();
+        Boolean flag = true;
+
+        while (flag) {
             System.out.println("Enter Command: ");
             userInput = scannerObj.nextLine(); // Read user input
-            System.out.println("\n___________________________ \n");
-            System.out.println(userInput);
-            System.out.println("___________________________ \n");
+
+            switch (userInput) {
+                case exitCommand:
+                    flag = false;
+                    break;
+
+                case listCommand:
+                    for (int idx = 0; idx < storage.size(); idx++) {
+                        System.out.println(idx + 1 + ": " + storage.get(idx));
+                    }
+                    break;
+
+                default:
+                    storage.add(userInput);
+                    System.out.println("\n___________________________ \n");
+                    System.out.println("added: " + userInput);
+                    System.out.println("___________________________ \n");
+                    break;
+            }
         }
 
         System.out.println(exitMessage);
