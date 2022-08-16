0b00b93b8e7d437babbafda5062f0fa55b76603f
Joel Wong <>
2022-08-16 17:54:01 +0800

    add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("----------------------\nHello! I'm HelperBot\nWhat can I do for you?\n----------------------\n");
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("----------------------\n" + userInput + "\n----------------------\n");
+            userInput = sc.nextLine();
+        }
+        System.out.println("Thank you and ATB :)");
     }
 }
