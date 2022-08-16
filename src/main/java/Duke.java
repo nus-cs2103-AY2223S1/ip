55d5fecdae44ad4833a1f05f18b9ee969550fa5a
Marciano Renzo William <marcianorenzowilliam@gmail.com>
2022-08-16 12:58:57 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,46 @@
+/**
+ * @author Marciano Renzo William
+ */
+
+import java.util.Scanner;
+
+/**
+ * This is the Main Class that contains the Main method.
+ */
 public class Duke {
+    private final static String underline = "____________________________________________________________";
+    private final static String indentation = "  ";
+
+    /**
+     * Private constructor of Duke.
+     */
+    private Duke() {
+    }
+
+    public void run() {
+        System.out.println(indentation + underline);
+        System.out.println(indentation + "Hello! I'm Duke\n" +
+                indentation + "What can I do for you?");
+        System.out.println(indentation + underline);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String chat = sc.nextLine();
+            if (chat.equals("bye")) {
+                sc.close();
+                System.out.println(indentation + underline);
+                System.out.println(indentation + "Bye. Hope to see you again soon!");
+                System.out.println(indentation + underline);
+                break;
+            } else {
+                System.out.println(indentation + underline);
+                System.out.println(indentation + chat);
+                System.out.println(indentation + underline);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        new Duke().run();
     }
 }
