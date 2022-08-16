717645b08be4dcaeeb37ae93faa4e522f887010c
bokun2 <>
2022-08-16 23:27:22 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    //    String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n"+"What can I do for you?");
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNextLine()){
+            String ans=scanner.nextLine();
+            if(ans.equals("bye")){
+                System.out.println("Bye. Hope to see you soon.");
+                break;
+            }else {
+                System.out.println(ans);
+            }
+        }
     }
 }
