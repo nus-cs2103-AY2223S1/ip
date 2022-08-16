0b74a3e714f185104fbd4f2fc7d6b029bd42e90b
marclzh <uiolk@hotmail.com>
2022-08-17 00:26:11 +0800

    Finished functionality for Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void Greet() {
+        String logo = " _______               \n"
+                    + "|  _____|  _   _____   \n"
+                    + "|  |____  | | |  __ |  \n"
+                    + "|   ____| | | |  ___|  \n"
+                    + "|  |____  | | | |      \n"
+                    + "|_______| |_| |_|";
+        System.out.println("Greetings from Elp\n" + logo);
+        System.out.println("What can I help you with?");
+    }
+
+    private static void Echo() {
+        Scanner sc = new Scanner(System.in);
+        String i = sc.nextLine();
+
+        while (!i.equals("bye")) {
+            System.out.println(i + "\n");
+            i = sc.nextLine();
+        }
+        {
+            System.out.println("Have a nice day!");
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Greet();
+        Echo();
     }
 }
