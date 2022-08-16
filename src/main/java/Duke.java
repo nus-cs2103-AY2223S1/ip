ddd7027a595ddf24e1f2adf9702095fc3df6bc9f
shenyihong <shen_yi_hong@outlook.com>
2022-08-16 21:45:27 +0800

    [feat] Added support for `list` command
    - max capacity of Duke's list/history is set to 100

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -8,9 +9,13 @@ public class Duke {
     public static final String NAME = "Duke";
     // ChatBot commands
     public static final String CMD_TERMINATE = "bye";
+    public static final String CMD_LIST = "list";
     // UI constants
     public static final String INDENT_CHAR = "\t";
     public static final String LINE_STR = "-".repeat(50);
+
+    private static final String[] history = new String[100]; // Max capacity: 100
+    private static int currHistIdx = 0;
 
     /**
      * Utility function to print line to STDOUT
@@ -52,6 +57,18 @@ public class Duke {
         printLine(LINE_STR, 1);
     }
 
+    private static void handleListCommand() {
+        if (currHistIdx == 0) {
+            respond("There are no items in the list!");
+            return;
+        }
+        List<String> responses = new ArrayList<>();
+        for (int idx = 0; idx < currHistIdx; idx++) {
+            responses.add(String.format("%d. %s", idx + 1, history[idx]));
+        }
+        respond(responses);
+    }
+
     public static void main(String[] args) {
         // Greetings
         respond(Arrays.asList(String.format("Hi I'm %s", NAME), "What can I do for you?"));
@@ -67,9 +84,14 @@ public class Duke {
                     terminate = true;
                     respond("Bye. Hope to see you again soon!");
                     break;
+                case CMD_LIST:
+                    handleListCommand();
+                    break;
                 default:
-                    // echo
-                    respond(query);
+                    // add to history
+                    history[currHistIdx] = query;
+                    currHistIdx++;
+                    respond(String.format("added: %s", query));
             }
         }
     }
