b86466892c776515d90d96a8c63a7585408f6547
bryanngzh <bryanngzihao@gmail.com>
2022-08-16 19:01:55 +0800

    Level-2: add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
+    private static ArrayList<String> storage = new ArrayList<>();
 
     public static void printLine() {
         System.out.println("\t" + "____________________________________________________________");
@@ -19,9 +22,10 @@ public class Duke {
         printLine();
     }
 
-    public static void echo(String toEcho) {
+    public static void store(String toStore) {
+        storage.add(toStore);
         printLine();
-        System.out.println("\t" + " " + toEcho);
+        System.out.println("\t" + " added: " + toStore);
         printLine();
     }
 
@@ -29,23 +33,41 @@ public class Duke {
         return text.equals("bye");
     }
 
+    public static boolean isList(String text) {
+        return text.equals("list");
+    }
+
     public static void exit() {
         printLine();
-        System.out.println("\t" +" Bye. Hope to see you again soon!");
+        System.out.println("\t" + " Bye. Hope to see you again soon!");
         printLine();
     }
 
-    public static void main(String[] args) {
-        greet();
+    public static void listStorage() {
+        printLine();
+        for (int i = 0; i < storage.size(); i++) {
+            System.out.println("\t" + " " + String.valueOf(i + 1) + ". " + storage.get(i));
+        }
+        printLine();
+    }
+
+    public static void startDuke() {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
             if (isExit(input)) {
                 exit();
                 break;
+            } else if (isList(input)) {
+                listStorage();
             } else {
-                echo(input);
+                store(input);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        greet();
+        startDuke();
     }
 }
