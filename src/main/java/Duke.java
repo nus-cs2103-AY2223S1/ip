95a2a8c40b92530d8d8ca31db5d06601550f2377
Evande1 <crossjab@gmail.com>
2022-08-16 15:34:17 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -13,6 +14,7 @@ public class Duke {
 
         while (!exit) {
             String userReply = sc.nextLine();
+            String[] splitReply = userReply.split(" ");
             if (userReply.equals("bye")) {
                 exit = true;
             } else if (userReply.equals("list")) {
@@ -21,18 +23,61 @@ public class Duke {
                     System.out.println(i+1 + "." + list.get(i));
                 }
                 // need to replace contains with regex
-            } else if (userReply.matches(".*\\bmark\\b.*")) {
+            } else if (splitReply[0].equals("mark") && splitReply.length == 2) {
                 int index = Integer.parseInt(userReply.replaceAll("[^0-9]", ""));
                 index--;
                 list.get(index).finished();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(list.get(index));
-            } else if (userReply.matches(".*\\bunmark\\b.*")) {
+            } else if (splitReply[0].equals("mark") && splitReply.length == 2) {
                 int index = Integer.parseInt(userReply.replaceAll("[^0-9]", ""));
                 index--;
                 list.get(index).notFinished();
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(list.get(index));
+            } else if (splitReply[0].equals("todo")){
+                String[] descriptionArray = Arrays.copyOfRange(splitReply,1, splitReply.length);
+                String description = String.join(" ", descriptionArray);
+                Task task = new ToDos(description);
+                list.add(task);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(task);
+                System.out.println("Now you have " + list.size() + "tasks in the list.");
+            } else if (splitReply[0].equals("deadline")) {
+                int by = -1;
+                for (int i = 0; i < splitReply.length; i++) {
+                    if (splitReply[i].equals("/by")) {
+                        by = i;
+                        break;
+                    }
+                }
+                String[] descriptionArray = Arrays.copyOfRange(splitReply,1, by);
+                String[] durationArray = Arrays.copyOfRange(splitReply, by + 1, splitReply.length );
+                String description = String.join(" ", descriptionArray);
+                String duration = String.join(" ", durationArray);
+                Task task = new Deadline(description, duration);
+                list.add(task);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(task);
+                System.out.println("Now you have " + list.size() + " tasks in the list.");
+
+            } else if (splitReply[0].equals("event")) {
+                int by = -1;
+                for (int i = 0; i < splitReply.length; i++) {
+                    if (splitReply[i].equals("/at")) {
+                        by = i;
+                        break;
+                    }
+                }
+                String[] descriptionArray = Arrays.copyOfRange(splitReply,1, by);
+                String[] durationArray = Arrays.copyOfRange(splitReply, by + 1, splitReply.length);
+                String description = String.join(" ", descriptionArray);
+                String duration = String.join(" ", durationArray);
+                Task task = new Events(description, duration);
+                list.add(task);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(task);
+                System.out.println("Now you have " + list.size() + "tasks in the list.");
             } else {
                 System.out.println("added: " + userReply);
                 list.add(new Task(userReply));
---
+++ src/main/java/Events.java
@@ -0,0 +1,14 @@
+public class Events extends Task {
+    protected String by;
+
+    public Events(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (by: " + by + ")";
+    }
+
+}
