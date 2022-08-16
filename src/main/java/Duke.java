eb1b7c3840b33dfe99b2e19b10321ff6e9231903
ShummyOwnzYou <jiehui285@gmail.com>
2022-08-16 20:03:13 +0800

    Implemented Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,16 +8,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("what you want");
 
         Scanner sc = new Scanner(System.in);
-
+        String[] db = new String[100];
+        int i = 0;
         while (true) {
             String str = sc.nextLine();
             if (str.equals("bye")) {
                 System.out.println("Sayonara, Adios!");
                 break;
             }
-            System.out.println(str);
+            if (str.equals("list")) {
+                for (int j = 0; j < i; j++) {
+                    int k = j + 1;
+                    System.out.println(k + ". " + db[j]);
+                }
+            } else {
+                db[i] = str;
+                System.out.println("added: " + str);
+                i++;
+            }
         }
     }
 }
