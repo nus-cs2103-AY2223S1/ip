5f2a0ac9b97cd992e8ec1543cd5ef1da9f284ff9
JasonYapzx <jasonyapzx@gmail.com>
2022-08-16 22:23:54 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,73 +3,160 @@ import java.util.ArrayList;
 import java.util.List; 
 
 public class Duke {
-    public static String getNthWord(String str, int n) {
-        return str.contains(" ") ? str.split(" ")[n] : str;
+    private Scanner scanner;
+    private List<Task> list;
+
+    private String logo =     "        / \\     |_   _| | |  / _|                   | |\n"
+                            + "       /   \\      | |   | | | |_   _ __    ___    __| |\n"
+                            + "      / / \\ \\     | |   | | |  _| | '__|  / _ \\  / _` |\n"
+                            + "     / _____ \\   _| |_  | | | |   | |    |  __/ | (_| |\n"
+                            + "    /_/     \\_\\ |_____| |_| |_|   |_|     \\___|  \\__,_|\n";
+
+    private String greeting = "____________________________________________________________\n" +
+                              "                      Hello! I am \n" + logo +
+                              "      Your personal assistant. What can I do for you?\n" +
+                              "____________________________________________________________";
+
+    private String goodbye = "Bye. Hope to see you again soon!";
+
+    /**
+     * Initializing the application 
+     */
+    public void initialize() {
+        list = new ArrayList<>(100);
+        scanner = new Scanner(System.in);
+        System.out.println(greeting);
+
+        listen();
     }
-    public static void main(String[] args) {
-        String input; // initializing the input
-        List<Task> list = new ArrayList<>(100); // initiliazing the list array to store previously added things
-        Scanner scanner = new Scanner(System.in);// initializing the Scanner class
 
+    /**
+     * Prints all values of list currently
+     */
+    public void printList() {
+        System.out.println("____________________________________________________________\n");
+        
+        // Empty list
+        if (list.size() == 0) {
+            System.out.print("List is currently empty! \n");
+        }
 
-        String logo = "        / \\     |_   _| | |  / _|                   | |\n"
-                    + "       /   \\      | |   | | | |_   _ __    ___    __| |\n"
-                    + "      / / \\ \\     | |   | | |  _| | '__|  / _ \\  / _` |\n"
-                    + "     / _____ \\   _| |_  | | | |   | |    |  __/ | (_| |\n"
-                    + "    /_/     \\_\\ |_____| |_| |_|   |_|     \\___|  \\__,_|\n";
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i+1) + ". " +list.get(i) + "\n");
+        }
 
-        // Welcome message
+        System.out.println("____________________________________________________________\n");
+    }
+
+    /**
+     * Marks a task in the list
+     */
+    public void mark(String input) {
+        int index = Integer.parseInt(input) - 1;
+
+        if (index + 1 > list.size() || index + 1 <= 0) {
+            System.out.println("____________________________________________________________\n" +
+                               "There is no task created for index " + (index + 1) + "\n" +
+                               "____________________________________________________________\n");
+        } else {
+            list.get(index).markAsDone();
+
+            System.out.println("____________________________________________________________\n" +
+                            "Nice! I've marked this task as done: \n" +
+                            list.get(index) + "\n" + 
+                            "____________________________________________________________\n");
+        }
+    }
+
+    /**
+     * Unmarks a task in the list
+     */
+    public void unmark(String input) {
+        int index = Integer.parseInt(input) - 1;
+
+        if (index + 1 > list.size() || index + 1 <= 0) {
+            System.out.println("____________________________________________________________\n" +
+                               "There is no task created for index " + (index + 1) + "\n" +
+                               "____________________________________________________________\n");
+        } else {
+            list.get(index).markAsUndone();
+
+            System.out.println("____________________________________________________________\n" +
+                            "OK, I've marked this task as not done yet: \n" +
+                            list.get(index) + "\n" + 
+                            "____________________________________________________________\n");
+        }
+    }
+
+    /**
+     * Adds a task to the list
+     */
+    public void addTask(Task task) {
+        list.add(task);
+        int numOfTasks = list.size();
+
         System.out.println("____________________________________________________________\n" +
-                           "                      Hello! I am \n" + logo +
-                           "      Your personal assistant. What can I do for you?\n" +
-                           "____________________________________________________________");
+                            "Got it. I've added this task: \n" +
+                            task + "\n" +
+                            "Now you have " + numOfTasks + " tasks in the list. \n" + 
+                            "____________________________________________________________\n");
+    }
+
+    /**
+     * Listens to System.in for input
+     */
+    public void listen() {
+        String input; // initializing the input
 
         // Reading user inputs
         while(scanner.hasNextLine()) {
             input = scanner.nextLine();
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if (input.equals("list")) {
-                System.out.println("____________________________________________________________\n");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i+1) + ". [" + list.get(i).getStatusIcon() + "] " + list.get(i).getDescription() + "\n");
-                }
-                System.out.println("____________________________________________________________\n");
-            } else if (getNthWord(input, 0).equals("mark")) {
-                Integer index = Integer.parseInt(getNthWord(input, 1)) - 1;
-                if (index + 1 > list.size() || index + 1 <= 0) {
-                    System.out.println("____________________________________________________________\n" +
-                                       "There is no task created for index " + (index + 1) + "\n" +
-                                       "____________________________________________________________\n");
-                } else {
-                    list.get(index).markAsDone();
+            String[] inputArr = input.split(" ", 2); // Splits the string in an array of [command, others]
+            String command = inputArr[0];
 
-                    System.out.println("____________________________________________________________\n" +
-                                    "Nice! I've marked this task as done: \n" +
-                                    "[" + list.get(index).getStatusIcon() + "] " + list.get(index).getDescription() + "\n" + 
-                                    "____________________________________________________________\n");
-                }
-            } else if (getNthWord(input, 0).equals("unmark")) {
-                Integer index = Integer.parseInt(getNthWord(input, 1)) - 1;
-                if (index + 1 > list.size() || index + 1 <= 0) {
-                    System.out.println("____________________________________________________________\n" +
-                                       "There is no task created for index " + (index + 1) + "\n" +
-                                       "____________________________________________________________\n");
-                } else {
-                    list.get(index).markAsUndone();
+            switch (command) {
+                case ("bye"):
+                    System.out.println(goodbye);
+                    break;
+                case ("list"):
+                    printList();
+                    break;
+                case ("mark"):
+                    // First word is "mark", second word would be an index for task to be marked
+                    mark(inputArr[1]);
+                    break;
+                case ("unmark"):
+                    // First word is "unmark", second word would be an index for task to be unmarked
+                    unmark(inputArr[1]);
+                    break;
+                case ("todo"):
+                    Todo todo = new Todo(inputArr[1]);
+                    addTask(todo);
+                    break;
+                case ("deadline"):
+                    String[] deadlineDetails = inputArr[1].split(" /by ", 2);
 
-                    System.out.println("____________________________________________________________\n" +
-                                    "OK, I've marked this task as not done yet: \n" +
-                                    "[" + list.get(index).getStatusIcon() + "] " + list.get(index).getDescription() + "\n" + 
-                                    "____________________________________________________________\n");
-                }
-            } else {
-                Task curr = new Task(input);
-                list.add(curr);
-                System.out.println("____________________________________________________________\n" +
-                                   "added: " + input + "\n" +
-                                   "____________________________________________________________\n");
+                    if (deadlineDetails.length == 2) {
+                        Deadline deadline = new Deadline(deadlineDetails[0], deadlineDetails[1]);
+                        addTask(deadline);
+                    } else {
+                        System.out.println("Please provide a proper command");
+                    }
+                    break;
+                case ("event"):
+                    String[] eventDetails = inputArr[1].split(" /at ", 2);
+
+                    if (eventDetails.length == 2) {
+                        Event event = new Event(eventDetails[0], eventDetails[1]);
+                        addTask(event);
+                    } else {
+                        System.out.println("Please provide a proper command");
+                    }
+                    break;
+                default:
+                    // Default case if there is no command word in the front
+                    Task task = new Task(input);
+                    addTask(task);
             }
         }
         scanner.close();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,19 +7,38 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Provides the current "marked" status of a task
+     * @return Marked status of task
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Gets the description of current task
+     * @return Description of task
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Marks current task as done
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Unmarks current task as undone
+     */
     public void markAsUndone() {
         this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
     }
 }
