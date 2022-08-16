29c15c0bca7df61a778a5958cce704e001364b84
Cjun1039 <e0726202@u.nus.edu>
2022-08-16 21:32:52 +0800

    add Greet, Echo, Exit to Duke.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String start = Duke.replyFormat("Oi! I'm Dook", "What you want?");
+        String end = Duke.replyFormat("Bye.", ">:)");
+
+        System.out.println(start);
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(end);
+                break;
+            }
+            System.out.println(Duke.replyFormat(command));
+        }
     }
+
+    private static String replyFormat(String... messages) {
+        String line = "     ____________________________________________________________\n";
+        String indent = "     ";
+        StringBuilder res = new StringBuilder(line);
+        for (String message : messages) {
+            res.append(indent).append(message).append("\n");
+        }
+        return res + line;
+    }
+
+
 }
