19fae5decd86aebc8c510cdbfb446761fed4e9b6
Zsigmond <e0725064@u.nus.edu>
2022-08-16 16:06:31 +0800

    feat: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String command = scanner.next();
+            switch (command) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                default:
+                    System.out.println(command);
+                    break;
+            }
+        }
     }
 }
