9d273ac9143ac106a569709391844ebf0b858899
Marcus Pang <marcuspangyuyang@gmail.com>
2022-08-16 20:05:40 +0800

    Update executeCommand to use switch statement instead of if-else

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -37,43 +37,56 @@ public class Duke {
     }
 
     public void executeCommand(String command) {
+        // Used to generate the response message after executing commands
         StringBuilder sb = new StringBuilder();
-        if (command.equals("list")) {
-            // Return all tasks
-            sb.append(LONG_LINE_WITH_NEW_LINE).append("     Here are the tasks in your list:\n");
-            for (int i = 0; i < taskList.size(); i++) {
-                sb.append("     ").append(i + 1).append(".").append(taskList.get(i)).append("\n");
+
+        // Limit of 2 is used to avoid
+        String[] inputStrings = command.split(" ", 2);
+        switch (inputStrings[0]) {
+            case "list": {
+                // Return all tasks
+                sb.append(LONG_LINE_WITH_NEW_LINE).append("     Here are the tasks in your list:\n");
+                for (int i = 0; i < taskList.size(); i++) {
+                    sb.append("     ").append(i + 1).append(".").append(taskList.get(i)).append("\n");
+                }
+                sb.append(LONG_LINE);
+                break;
             }
-            sb.append(LONG_LINE);
-        } else if (command.startsWith("mark")) {
-            // Tasks are displayed as 1-indexed, but they are stored as 0-indexed
-            // Hence, we need to account for this offset here
-            int taskIndex = Integer.parseInt(command.replaceFirst("mark ", "")) - 1;
-            Task task = this.taskList.get(taskIndex);
-            task.markTask();
+            case "mark": {
+                // Tasks are displayed as 1-indexed, but they are stored as 0-indexed
+                // Hence, we need to account for this offset here
+                int taskIndex = Integer.parseInt(inputStrings[1]) - 1;
+                Task task = this.taskList.get(taskIndex);
+                task.markTask();
 
-            sb.append(LONG_LINE_WITH_NEW_LINE)
-                    .append("     Nice! I've marked this task as done:\n")
-                    .append("       ").append(task).append("\n")
-                    .append(LONG_LINE_WITH_NEW_LINE);
-        } else if (command.startsWith("unmark")) {
-            // Tasks are displayed as 1-indexed, but they are stored as 0-indexed
-            // Hence, we need to account for this offset here
-            int taskIndex = Integer.parseInt(command.replaceFirst("unmark ", "")) - 1;
-            Task task = this.taskList.get(taskIndex);
-            task.unmarkTask();
+                sb.append(LONG_LINE_WITH_NEW_LINE)
+                        .append("     Nice! I've marked this task as done:\n")
+                        .append("       ").append(task).append("\n")
+                        .append(LONG_LINE_WITH_NEW_LINE);
+                break;
+            }
+            case "unmark": {
+                // Tasks are displayed as 1-indexed, but they are stored as 0-indexed
+                // Hence, we need to account for this offset here
+                int taskIndex = Integer.parseInt(inputStrings[1]) - 1;
+                Task task = this.taskList.get(taskIndex);
+                task.unmarkTask();
 
-            sb.append(LONG_LINE_WITH_NEW_LINE)
-                    .append("     OK, I've marked this task as not done yet:\n")
-                    .append("       ").append(task).append("\n")
-                    .append(LONG_LINE_WITH_NEW_LINE);
-        } else {
-            // Otherwise, add command to task list
-            this.taskList.add(new Task(command));
-            sb.append(LONG_LINE_WITH_NEW_LINE)
-                    .append("     added: ")
-                    .append(command).append("\n")
-                    .append(LONG_LINE_WITH_NEW_LINE);
+                sb.append(LONG_LINE_WITH_NEW_LINE)
+                        .append("     OK, I've marked this task as not done yet:\n")
+                        .append("       ").append(task).append("\n")
+                        .append(LONG_LINE_WITH_NEW_LINE);
+                break;
+            }
+            default: {
+                // Otherwise, add command to task list
+                this.taskList.add(new Task(command));
+                sb.append(LONG_LINE_WITH_NEW_LINE)
+                        .append("     added: ")
+                        .append(command).append("\n")
+                        .append(LONG_LINE_WITH_NEW_LINE);
+                break;
+            }
         }
 
         // Print out response after executing command
