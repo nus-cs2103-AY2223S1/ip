b7b82009dc6eda61ae0e454854944a05ff9f1747
Daofu <xu.daofu20@gmail.com>
2022-08-16 17:34:15 +0800

    level-3.1 improved asethetic of bot, added error message for out of bound index input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
+    private static String line = "------------------------------------------";
     private static boolean END;
     //storing all user inputs
     private static ArrayList<Task> storage;
@@ -30,40 +31,58 @@ public class Duke {
             this.isDone = false;
         }
     }
-
     /*
-    Decides on what the chatbox will response with based on user input.
+    Wraps the text with lines on top and below
+     */
+    public String wrapper(String content) {
+        return line + "\n" + content + "\n" + line;
+    }
+    /*
+    Decides on what the chatbox will respond with based on user input.
      */
     public void Response(String input) {
         if (input.equals("bye")) {
             END = true;
-            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(wrapper("Bye. Hope to see you again soon!"));
         } else if (input.equals("list")) {
             String output = "";
             for (int i = 0; i < storage.size(); i ++) {
                 Task current = storage.get(i);
                 output = output + String.valueOf(i + 1) + ".[" + current.getStatusIcon() + "] "
-                        + current.getDescription() + "\n";
+                        + current.getDescription();
+                if (i != storage.size() - 1) {
+                    output = output + "\n";
+                }
             }
-            System.out.println(output);
-        } else if (input.substring(0, 4).equals("mark")) {
+            System.out.println(wrapper(output));
+        } else if (input.length() > 3 && input.substring(0, 4).equals("mark")) {
             String indexString = input.substring(5);
             int index = Integer.valueOf(indexString) - 1;
-            Task current = storage.get(index);
-            current.markasDone();
-            System.out.println("Nice! I've marked this task as done:\n"
-                    + "[" + current.getStatusIcon() + "] " + current.getDescription());
-        } else if (input.substring(0, 6).equals("unmark")) {
+            if (index > -1 && index < storage.size()) {
+                Task current = storage.get(index);
+                current.markasDone();
+                String content = "Nice! I've marked this task as done:\n"
+                        + "[" + current.getStatusIcon() + "] " + current.getDescription();
+                System.out.println(wrapper(content));
+            } else {
+                System.out.println(wrapper("Error! Index out of Bound\nPlease Try Again!"));
+            }
+        } else if (input.length() > 5 && input.substring(0, 6).equals("unmark")) {
             String indexString = input.substring(7);
             int index = Integer.valueOf(indexString) - 1;
-            Task current = storage.get(index);
-            current.markasNotDone();
-            System.out.println("OK, I've marked this task as not done yet:\n"
-                    + "[" + current.getStatusIcon() + "] " + current.getDescription());
+            if (index > -1 && index < storage.size()) {
+                Task current = storage.get(index);
+                current.markasNotDone();
+                String content = "OK, I've marked this task as not done yet:\n"
+                        + "[" + current.getStatusIcon() + "] " + current.getDescription();
+                System.out.println(wrapper(content));
+            } else {
+                System.out.println(wrapper("Error! Index out of Bound\nPlease Try Again!"));
+            }
         } else {
             Task task = new Task(input);
             storage.add(task);
-            System.out.println("added: " + input);
+            System.out.println(wrapper("added: " + input));
         }
     }
     public static void main(String[] args) {
@@ -71,7 +90,7 @@ public class Duke {
         END = false;
         storage = new ArrayList<Task>();
         Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println(duke.wrapper("Hello! I'm Duke\nWhat can I do for you?"));
         while (!END) {
             duke.Response(myScanner.nextLine());
         }
