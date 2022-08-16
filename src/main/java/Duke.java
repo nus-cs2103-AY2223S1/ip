df84a77ba3e2487a12dfdac348ca13df8d7cf409
guokweijie <guokweijie@gmail.com>
2022-08-16 23:35:13 +0800

    completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,33 @@
 import java.util.Scanner;
 
 public class Duke {
+    static String[] storage = new String[100];
+    static int index = 0;
+
+    public static String printList() {
+        String list = "";
+        for(int i = 0; i < 100; i++) {
+            if(storage[i] != null) {
+                list += ((i + 1) + ". " + storage[i] + "\n");
+            }
+        }
+        return list;
+    }
     public static void main(String[] args) {
         String welcome = "Hello! I'm Duke\nWhat can I do for you?\n";
         System.out.println(welcome);
         Scanner sc = new Scanner(System.in);
         while(true) {
-            String input = sc.next();
-            if(input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again!");
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(printList());
             } else {
-                System.out.println(input);
+                System.out.println("added: " + input);
+                storage[index] = input;
+                index += 1;
             }
         }
     }
