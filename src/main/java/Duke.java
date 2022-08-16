bad0ae6ccba187a1e33590b5afe0185136dd0462
CeereeC <myself3216@gmail.com>
2022-08-16 21:39:57 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,14 +6,24 @@ public class Duke {
         System.out.println("Hi... I'm Bishop... ");
         System.out.println("What can I do for you today?");
 
+        String[] list = new String[100];
+        int index = 0;
+
         Scanner scanner = new Scanner(System.in);
-        String ans;
-        do {
-            ans = scanner.nextLine();
-            System.out.println(ans);
-        } while (!ans.equals("bye"));
+        while (scanner.hasNextLine()) {
+            String ans = scanner.nextLine();
+            if (ans.equals("bye")) {
+                System.out.println("Goodbye...");
+                break;
+            } else if (ans.equals("list")) {
+                for (int i = 0; i < index; i++) System.out.println((i + 1) + ". " + list[i]);
+            } else {
+                list[index] = ans;
+                index++;
+                System.out.println("added: " + ans);
+            }
+        };
 
-        System.out.println("Goodbye...");
-
+        scanner.close();
     }
 }
