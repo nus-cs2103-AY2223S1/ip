7378eb96bf00b0f91878d738bbcb754b7ee5eb9e
Eugene-Ong-W-X <e0725338@u.nus.edu>
2022-08-16 10:32:23 +0800

    Level-2 Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,15 +2,43 @@ import java.util.Scanner;
 
 public class Duke {
     private static Scanner scanner = new Scanner(System.in);
+    private static String[] tasks = new String[100];
+
+    /**
+     * Points to the current index of tasks to insert the next task in
+     */
+    private static int pointer = 0;
 
     private static void speak(String message) {
         System.out.println("============================================================\n");
         System.out.println(message);
-        System.out.println("\n============================================================");
+        System.out.println("============================================================");
     }
 
     private static void greet() {
-        speak(" Greetings! My Name is Alfred ^_^\n How may I be of service today?");
+        speak(" Greetings! My Name is Alfred ^_^\n How may I be of service today?\n");
+    }
+
+    private static void goodbye() {
+        speak(" Farewell!\n");
+    }
+
+    private static void listTasks() {
+        if (pointer == 0) {
+            speak(" You have not added any tasks!\n");
+        } else {
+            String result = "";
+            for (int i = 0; i < pointer; i++) {
+                result += " " + (i + 1) + ". " + tasks[i] + "\n";
+            }
+            speak(result);
+        }
+    }
+
+    private static void addTask(String task) {
+        tasks[pointer] = task;
+        pointer++;
+        speak(" added: " + task + "\n");
     }
 
     public static void main(String[] args) {
@@ -19,10 +47,12 @@ public class Duke {
         String cmd = "";
         while (!cmd.equals("bye")) {
             cmd = scanner.nextLine().trim();
-            if (!cmd.equals("bye")) {
-                speak(" " + cmd);
+            if (cmd.equals("bye")) {
+                goodbye();
+            } else if (cmd.equals("list")) {
+                listTasks();
             } else {
-                speak(" Farewell!");
+                addTask(cmd);
             }
         }
     }
