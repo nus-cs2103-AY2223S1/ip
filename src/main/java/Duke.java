6e614b14a295fb9196d13f23419b11a6efe358a7
PangKuangWei <pangkuangwei@gmail.com>
2022-08-16 11:35:03 +0800

    Implemented Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,18 +5,34 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String line = "____________________________________________________________\n";
-        String greeting = line +
-                          "     Hello! I'm Duke\n" +
-                          "     What can I do for you?\n" +
-                          line;
+        String[] arr = new String[100];
+        int count = 0;
+        String line = "    ____________________________________________________________";
+        String greeting = line + "\n     Hello! I'm Duke\n     What can I do for you?\n" + line;
         System.out.println(greeting);
         String str = sc.nextLine();
         while(!str.equals("bye")) {
-            System.out.println(str);
+            if (str.equals("list")) {
+                int k = 0;
+                int i = 1;
+                System.out.println(line);
+                while (arr[k] != null) {
+                    System.out.println("     " + i + ". " + arr[k]);
+                    k++;
+                    i++;
+                }
+                System.out.println(line);
+            } else {
+                arr[count] = str;
+                count++;
+                String message = "     added: " + str;
+                System.out.println(line);
+                System.out.println(message);
+                System.out.println(line);
+            }
             str = sc.nextLine();
         }
-        String goodbye = line + "      Bye. Hope to see you again soon!\n" + line;
+        String goodbye = line + "\n     Bye. Hope to see you again soon!\n" + line;
         System.out.println(goodbye);
     }
 }
