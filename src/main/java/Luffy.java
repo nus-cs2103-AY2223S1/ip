21d1fcbe0beda39809a132848870ddf5d1be25fc
SilasTSL <silastaysl@gmail.com>
2022-08-17 01:18:32 +0800

    Added list

--- src/main/java/Luffy.java
+++ src/main/java/Luffy.java
@@ -15,15 +15,25 @@ public class Luffy {
 
         //Actual Luffy Logic:
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        int counter = 0;
 
         while (true) {
             String s = in.nextLine();
             if (s.equals("bye")) {
                 System.out.println("Bye! Hope to see you again soon :)");
                 break;
+            } else if (s.equals("list")) {
+                System.out.println("------------------------------------------------------");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+                System.out.println("------------------------------------------------------");
             } else {
                 System.out.println("------------------------------------------------------");
-                System.out.println(s);
+                list[counter] = s;
+                counter++;
+                System.out.println("added: " + s);
                 System.out.println("------------------------------------------------------");
             }
         }
