e42c9db62471d92ffbe872e6584c5f35aa335b82
ryanguai <e0694442@u.nus.edu>
2022-08-16 14:13:57 +0800

    Added markAsDone, markNotDone functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,9 @@ public class Duke {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Hello! I'm Snoopy");
         System.out.println("What can I do for you?");
-        String response = myObj.nextLine();
+        String input = myObj.nextLine();
+        String[] words = input.split(" ");
+        String response = words[0];
         Task[] tasks = new Task[100];
         int i=0;
         while (!response.equals("bye")) {
@@ -15,12 +17,22 @@ public class Duke {
                     String output = a + ".[" + tasks[a-1].getStatusIcon() + "] " + tasks[a-1].description;
                     System.out.println(output);
                 }
+            } else if (response.equals("mark")) {
+                String taskNumber = words[1];
+                int number = Integer.parseInt(taskNumber);
+                tasks[number-1].markAsDone();
+            } else if (response.equals("unmark")) {
+                String taskNumber = words[1];
+                int number = Integer.parseInt(taskNumber);
+                tasks[number-1].markNotDone();
             } else {
-                tasks[i] = new Task(response);
-                System.out.println("added: " + response);
+                tasks[i] = new Task(input);
+                System.out.println("added: " + input);
                 i++;
             }
-            response = myObj.nextLine();
+            input = myObj.nextLine();
+            words = input.split(" ");
+            response = words[0];
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,5 +11,15 @@ public class Task {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    //...
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  [X] " + this.description);
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("  [ ] " + this.description);
+    }
 }
