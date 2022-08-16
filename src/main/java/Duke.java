7bcd12f00d3fd2f1d9033c391379dd7c96678cfe
AugustDespair <ethanng0802@gmail.com>
2022-08-16 21:16:25 +0800

    Added simple error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,10 +12,11 @@ public class Duke {
     }
 
     public void run() {
-        System.out.println("\tHello I'm Duke\n \tWhat can I do for you?");
+        System.out.println("Hello I'm Duke\n What can I do for you?");
 
 
         while (scanner.hasNextLine()) {
+            System.out.println();
             String ss = scanner.nextLine();
 
             String[] splitSS = ss.split(" ");
@@ -28,82 +29,111 @@ public class Duke {
                     list();
                     break;
                 case "mark":
-                    int indexOfTask = Integer.parseInt(splitSS[1]) - 1;
-                    this.taskList.get(indexOfTask).completed();
-                    System.out.println("\t\tNice! I've marked this task as done:");
-                    System.out.printf("\t\t\t%s\n", this.taskList.get(indexOfTask));
+                    changeMarkStatus(ss, true);
                     break;
                 case "unmark":
-                    int indexToUnmark = Integer.parseInt(splitSS[1]) - 1;
-                    this.taskList.get(indexToUnmark).uncompleted();
-                    System.out.println("\t\tOK, I've marked this task as not done yet:");
-                    System.out.printf("\t\t\t%s\n", this.taskList.get(indexToUnmark));
+                    changeMarkStatus(ss, false);
                     break;
                 case "todo":
-                    System.out.println("\t\tGot it. I've added this task");
-                    StringBuilder todo = new StringBuilder();
-
-                    for (int i = 1; i < splitSS.length; ++i) {
-                        todo.append(' ');
-                        todo.append(splitSS[i]);
-                    }
-
-                    ToDo todoTask = new ToDo(todo.toString());
-                    taskList.add(todoTask);
-                    System.out.printf("\t\t\t %s\n", todoTask);
-                    System.out.printf("\t\tNow you have %d tasks in the list.\n", this.taskList.size());
+                    todo(ss);
                     break;
                 case "deadline":
-                    System.out.println("\t\tGot it. I've added this task");
-                    StringBuilder deadline = new StringBuilder();
-
-                    for (int i = 1; i < splitSS.length; ++i) {
-                        if (splitSS[i].equals("/by")) break;
-                        deadline.append(" " + splitSS[i]);
-                    }
-
-                    String date = ss.split("/by")[1].trim();
-                    Deadline deadlineTask = new Deadline(deadline.toString(), date);
-                    taskList.add(deadlineTask);
-                    System.out.printf("\t\t\t %s\n", deadlineTask);
-                    System.out.printf("\t\tNow you have %d tasks in the list.\n", this.taskList.size());
+                    deadline(ss);
                     break;
                 case "event":
-                    System.out.println("\t\tGot it. I've added this task");
-                    StringBuilder event = new StringBuilder();
-
-                    for (int i = 1; i < splitSS.length; ++i) {
-                        if (splitSS[i].equals("/at")) break;
-                        event.append(" " + splitSS[i]);
-                    }
-
-                    Event eventTask = new Event(event.toString(), ss.split("/at")[1].trim());
-                    taskList.add(eventTask);
-                    System.out.printf("\t\t\t %s\n", eventTask);
-                    System.out.printf("\t\tNow you have %d tasks in the list.\n", this.taskList.size());
+                    event(ss);
                     break;
-
                 default:
-                    Task task = new Task(ss);
-                    add(task);
-                    System.out.println("\t\tadded: " + ss);
+//                    Task task = new Task(ss);
+//                    add(task);
+//                    System.out.println("added: " + ss);
+                    System.out.println("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
     }
 
     private void terminate() {
-        System.out.println("\t\tBye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
     private void list() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskList.size(); ++i) {
-            System.out.printf("\t\t %d. %s\n", i + 1, taskList.get(i));
+            System.out.printf(" %d. %s\n", i + 1, taskList.get(i));
         }
+    }
+
+    private void changeMarkStatus(String input, boolean mark) {
+        int indexOfTask = Integer.parseInt(input.split((" "))[1]) - 1;
+
+        if (mark) {
+            this.taskList.get(indexOfTask).completed();
+            System.out.println("Nice! I've marked this task as done:");
+        } else {
+            this.taskList.get(indexOfTask).uncompleted();
+            System.out.println("OK, I've marked this task as not done yet:");
+        }
+        System.out.printf("\t%s\n", this.taskList.get(indexOfTask));
     }
 
     private void add(Task s) {
         taskList.add(s);
+    }
+
+    private void todo(String input) {
+
+        String[] splitInput = input.split(" ");
+
+        if (splitInput.length < 2) {
+            System.out.println("     ☹ OOPS!!! The description of a todo cannot be empty.");
+            return;
+        }
+
+        System.out.println("Got it. I've added this task");
+        StringBuilder todo = new StringBuilder();
+
+        for (int i = 1; i < splitInput.length; ++i) {
+            todo.append(' ');
+            todo.append(splitInput[i]);
+        }
+
+        ToDo todoTask = new ToDo(todo.toString());
+        taskList.add(todoTask);
+        System.out.printf("\t %s\n", todoTask);
+        System.out.printf("Now you have %d tasks in the list.\n", this.taskList.size());
+    }
+
+    private void deadline(String input) {
+        String[] splitInput = input.split(" ");
+        System.out.println("Got it. I've added this task");
+        StringBuilder deadline = new StringBuilder();
+
+        for (int i = 1; i < splitInput.length; ++i) {
+            if (splitInput[i].equals("/by")) break;
+            deadline.append(" " + splitInput[i]);
+        }
+
+        String date = input.split("/by")[1].trim();
+        Deadline deadlineTask = new Deadline(deadline.toString(), date);
+        taskList.add(deadlineTask);
+        System.out.printf("\t %s\n", deadlineTask);
+        System.out.printf("Now you have %d tasks in the list.\n", this.taskList.size());
+    }
+
+    private void event(String input) {
+        String[] splitInput = input.split(" ");
+        System.out.println("Got it. I've added this task");
+        StringBuilder event = new StringBuilder();
+
+        for (int i = 1; i < splitInput.length; ++i) {
+            if (splitInput[i].equals("/at")) break;
+            event.append(" " + splitInput[i]);
+        }
+
+        Event eventTask = new Event(event.toString(), input.split("/at")[1].trim());
+        taskList.add(eventTask);
+        System.out.printf("\t %s\n", eventTask);
+        System.out.printf("Now you have %d tasks in the list.\n", this.taskList.size());
     }
 
 }
