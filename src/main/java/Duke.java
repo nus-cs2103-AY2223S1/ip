d2f73a01cd1bcdbac80fc440764e3ba77cd77bc7
zacchaeuschok <zacchaeuschok2014@gmail.com>
2022-08-16 12:14:27 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,27 @@
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
 
         String command = sc.nextLine();
 
+        ArrayList<String> items = new ArrayList<>();
+
         while (!command.toLowerCase().equals("bye")) {
-            System.out.println(command);
+            if (command.toLowerCase().equals("list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    int j = i + 1;
+                    System.out.println(j + ". " + items.get(i));
+                }
+            } else {
+                System.out.println("added: " + command);
+                items.add(command);
+            }
             command = sc.nextLine();
         }
 
