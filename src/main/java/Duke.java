7d5ba0042a7a2903dd6026f8cb91c1339e1b7734
fannyjian <fannyj@u.nus.edu>
2022-08-17 01:36:10 +0800

    Add Greet, Echo, Exit, Add and List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
+
+    public static void handleCommands(Scanner sc) {
+        String cmd = sc.nextLine();
+        String sep = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        ArrayList<String> commands = new ArrayList<>();
+        while (!cmd.equals("bye")) {
+            if (!cmd.equals("list")) {
+                commands.add(cmd);
+                System.out.println(sep + "added: " + cmd + "\n" + sep);
+            } else {
+                System.out.println(sep);
+                for(int i = 1; i <= commands.size(); i++) {
+                    System.out.println(i + ". " + commands.get(i - 1) + "\n");
+                }
+                System.out.println(sep);
+            }
+            cmd = sc.nextLine();
+        }
+        System.out.println(sep + "byebye see you again :D" + "\n" + sep);
+        sc.close();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo =
+                  " _              \n"
+                + "| |    _   _ _____   ___ _  \n"
+                + "| |   | | | |  __ \\ /     |\n"
+                + "| |__ | |_| | |  | |    | |   \n"
+                + "|____| \\__,_|_|  |_|\\__/|_|   \n";
+        System.out.println("Hello. \nThis is \n" + logo + "\n How may I assist you today?");
+        Scanner sc = new Scanner(System.in);
+        handleCommands(sc);
     }
 }
