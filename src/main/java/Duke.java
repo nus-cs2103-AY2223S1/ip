3310c9ed49f10f6933e65525a0310304b0f3dc90
weesiau <e0725311@u.nus.edu>
2022-08-16 22:54:41 +0800

    Completed Level-1 Increment
    Implemented skeletal version of the Duke chatbot with capability of echoing user's inputs and exiting when command is input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.*;
+
 public class Duke {
+
+    private static void greet() {
+        System.out.println("Hi, my name is Duke\nand it's power hour!");
+        System.out.println("***********************");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Come again soon!");
+                System.exit(0);
+            } else if (command.equals("")) {
+                continue;
+            }
+            System.out.println(command);
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.greet();
     }
 }
