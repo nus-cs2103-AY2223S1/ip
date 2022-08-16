4c10898e45925321e887ba9ecb6236d42af9e901
lyuting47 <luting_c@yahoo.com>
2022-08-17 00:57:27 +0800

    Added level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 
 public class Duke {
+
     private static String printSpacer() {
         return "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
     }
+
+    private static String[] tasks = new String[100];
+    private static int tasksLength = 0;
+
+    private static boolean cont = true;
 
     public static void main(String[] args) {
         System.out.println(printSpacer());
@@ -11,17 +17,35 @@ public class Duke {
         System.out.println(printSpacer());
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
+        while (cont) {
             String s = sc.nextLine();
 
-            if (s.equals("bye")) {
-                System.out.println("(~‾▿‾)~ Bye. Hope to see you again soon!");
-                System.out.println(printSpacer());
-                break;
-            } else {
-                System.out.println(s);
-                System.out.println(printSpacer());
+            switch (s) {
+
+                case "list":
+                    if (tasksLength == 0) {
+                        System.out.println("No tasks yet~");
+                    } else {
+                        for (int i = 1; i <= tasksLength; i++) {
+                            System.out.println(i + ". " + tasks[i - 1]);
+                        }
+                    }
+                    System.out.println(printSpacer());
+                    break;
+
+                case "bye":
+                    System.out.println("(~‾▿‾)~ Bye. Hope to see you again soon!");
+                    System.out.println(printSpacer());
+                    cont = false;
+                    break;
+
+                default:
+                    tasks[tasksLength] = s;
+                    tasksLength++;
+                    System.out.println("Added task: " + s);
+                    System.out.println(printSpacer());
             }
+
         }
     }
 }
