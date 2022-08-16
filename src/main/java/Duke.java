bd57fa7def4ec4a43c219ff4c4f796ab9519573e
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-17 03:19:50 +0800

    Complete first increment which repeats user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke!\nWhat can I do for you?");
+        System.out.println("-----------------------------------");
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            String line = input.nextLine();
+            if ("bye".equalsIgnoreCase(line)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(line);
+            }
+            System.out.println("-----------------------------------");
+        }
     }
+
+
 }
