eb8b449fed6d7f435149cca288ed8578c1a75d09
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2022-08-17 03:28:53 +0800

    Added level 2 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -6,7 +8,10 @@ public class Duke {
 
         // Create a scanner that reads user input.
         Scanner sc = new Scanner(System.in);
-        
+
+        // Create a list to store tasks.
+        List<String> tasks = new ArrayList<>();
+
         // Greet the user.
         System.out.println("Duke: Hello! I am Duke.");
 
@@ -16,8 +21,16 @@ public class Duke {
             if (userInput.equals("bye")) {
                 System.out.println("Duke: Bye!");
                 break;
+            }
+
+            if (userInput.equals("list")) {
+                System.out.println("Duke: Here are your tasks.");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("%3d: %s\n", i + 1, tasks.get(i));
+                }
             } else {
-                System.out.println("Duke: " + userInput);
+                tasks.add(userInput);
+                System.out.println("Duke: I have added " + userInput);
             }
         }
     }
