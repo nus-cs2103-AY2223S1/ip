1a08237629da63792945d4baa942a76e129ba953
John Benedict Yan <johnbenedictyan@gmail.com>
2022-08-16 10:09:57 +0800

    Level 1. Greet, Echo, Exit Implement a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.io.File;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
+    static final String exitCommand = "bye";
+    static final String exitMessage = "Goodbye and have a nice day!";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        File file = new File("src/main/assets/logo.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+                // Creating a file object.
+            }
+            System.out.println("Welcome to Aladdin Services");
+        } catch (Exception e) {
+            System.out.println("Welcome to Aladdin Services");
+        }
+
+        Scanner scannerObj = new Scanner(System.in); // Create a Scanner object
+        String userInput = "";
+
+        while (!Objects.equals(userInput, exitCommand)) {
+            System.out.println("Enter Command: ");
+            userInput = scannerObj.nextLine(); // Read user input
+            System.out.println("\n___________________________ \n");
+            System.out.println(userInput);
+            System.out.println("___________________________ \n");
+        }
+
+        System.out.println(exitMessage);
+        scannerObj.close();
     }
 }
