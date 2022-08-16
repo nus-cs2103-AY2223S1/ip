8c70df0374dad645b6d24171c37387d4c5b9dd6f
ThomasHoooo <e0725843@u.nus.edu>
2022-08-17 00:33:24 +0800

    Feat: Duke can echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo + "Hello! I'm Yale\nWhat can I do for you?");
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!\n");
+                break;
+            }
+            String message = "\t" + "_".repeat(20) + "\n"
+                    + String.format("\t  %s\n", input)
+                    + "\t" + "_".repeat(20) + "\n";
+            System.out.println(message);
+        }
     }
 }
