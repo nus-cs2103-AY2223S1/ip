d901c85e4a3ea4a7c4c5e6d41e158704cc96be03
JasonYapzx <jasonyapzx@gmail.com>
2022-08-16 23:39:04 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,23 +6,23 @@ public class Duke {
     private Scanner scanner;
     private List<Task> list;
 
-    private String logo =     "        / \\     |_   _| | |  / _|                   | |\n"
+    private final String logo =     "        / \\     |_   _| | |  / _|                   | |\n"
                             + "       /   \\      | |   | | | |_   _ __    ___    __| |\n"
                             + "      / / \\ \\     | |   | | |  _| | '__|  / _ \\  / _` |\n"
                             + "     / _____ \\   _| |_  | | | |   | |    |  __/ | (_| |\n"
                             + "    /_/     \\_\\ |_____| |_| |_|   |_|     \\___|  \\__,_|\n";
 
-    private String greeting = "____________________________________________________________\n" +
+    private final String greeting = "____________________________________________________________\n" +
                               "                      Hello! I am \n" + logo +
                               "      Your personal assistant. What can I do for you?\n" +
                               "____________________________________________________________";
 
-    private String goodbye = "Bye. Hope to see you again soon!";
+    private final String goodbye = "Bye. Hope to see you again soon!";
 
     /**
      * Initializing the application 
      */
-    public void initialize() {
+    public void initialize() throws DukeException{
         list = new ArrayList<>(100);
         scanner = new Scanner(System.in);
         System.out.println(greeting);
@@ -51,40 +51,37 @@ public class Duke {
     /**
      * Marks a task in the list
      */
-    public void mark(String input) {
+    public void mark(String input) throws DukeException {
         int index = Integer.parseInt(input) - 1;
-
-        if (index + 1 > list.size() || index + 1 <= 0) {
-            System.out.println("____________________________________________________________\n" +
-                               "There is no task created for index " + (index + 1) + "\n" +
-                               "____________________________________________________________\n");
-        } else {
+        try {
             list.get(index).markAsDone();
-
             System.out.println("____________________________________________________________\n" +
-                            "Nice! I've marked this task as done: \n" +
-                            list.get(index) + "\n" + 
-                            "____________________________________________________________\n");
+                    "Nice! I've marked this task as done: \n" +
+                    list.get(index) + "\n" +
+                    "____________________________________________________________\n");
+        } catch (NullPointerException e) {
+            throw new DukeException("☹ OOPS!!! There is no task created for this index!");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("☹ OOPS!!! Please enter a valid index number!");
         }
     }
 
     /**
      * Unmarks a task in the list
      */
-    public void unmark(String input) {
+    public void unmark(String input) throws DukeException {
         int index = Integer.parseInt(input) - 1;
 
-        if (index + 1 > list.size() || index + 1 <= 0) {
-            System.out.println("____________________________________________________________\n" +
-                               "There is no task created for index " + (index + 1) + "\n" +
-                               "____________________________________________________________\n");
-        } else {
+        try {
             list.get(index).markAsUndone();
-
             System.out.println("____________________________________________________________\n" +
-                            "OK, I've marked this task as not done yet: \n" +
-                            list.get(index) + "\n" + 
-                            "____________________________________________________________\n");
+                    "OK, I've marked this task as not done yet: \n" +
+                    list.get(index) + "\n" +
+                    "____________________________________________________________\n");
+        } catch (NullPointerException e) {
+            throw new DukeException("☹ OOPS!!! There is no task created for this index!");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("☹ OOPS!!! Please enter a valid index number!");
         }
     }
 
@@ -103,9 +100,55 @@ public class Duke {
     }
 
     /**
+     * Handles a to do task
+     * @param input String input for to do task
+     * @throws DukeException Exception if to do task has no description
+     */
+    public void toDoTask(String[] input) throws DukeException {
+        if (input.length >= 2) {
+            Todo todo = new Todo(input[1]);
+            addTask(todo);
+        } else {
+            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+        }
+    }
+
+    /**
+     * Handles an deadline task
+     * @param input String input for deadline task
+     * @throws DukeException Exception if deadline task has no description
+     */
+    public void deadlineTask(String[] input) throws DukeException {
+        String[] deadlineDetails = input[1].split(" /by ", 2);
+
+        if (deadlineDetails.length == 2) {
+            Deadline deadline = new Deadline(deadlineDetails[0], deadlineDetails[1]);
+            addTask(deadline);
+        } else {
+            throw new DukeException("☹ OOPS!!! Please follow the syntax for an 'deadline' command: event [description] /by [date].");
+        }
+    }
+
+    /**
+     * Handles an event task
+     * @param input String input for event task
+     * @throws DukeException Exception if event task has no description
+     */
+    public void eventTask(String[] input) throws DukeException {
+        String[] eventDetails = input[1].split(" /at ", 2);
+
+        if (eventDetails.length == 2) {
+            Event event = new Event(eventDetails[0], eventDetails[1]);
+            addTask(event);
+        } else {
+            throw new DukeException("☹ OOPS!!! Please follow the syntax for an 'event' command: event [description] /at [date].");
+        }
+    }
+
+    /**
      * Listens to System.in for input
      */
-    public void listen() {
+    public void listen() throws DukeException {
         String input; // initializing the input
 
         // Reading user inputs
@@ -131,33 +174,17 @@ public class Duke {
                     unmark(inputArr[1]);
                     break;
                 case ("todo"):
-                    Todo todo = new Todo(inputArr[1]);
-                    addTask(todo);
+                    toDoTask(inputArr);
                     break;
                 case ("deadline"):
-                    String[] deadlineDetails = inputArr[1].split(" /by ", 2);
-
-                    if (deadlineDetails.length == 2) {
-                        Deadline deadline = new Deadline(deadlineDetails[0], deadlineDetails[1]);
-                        addTask(deadline);
-                    } else {
-                        System.out.println("Please provide a proper command");
-                    }
+                    deadlineTask(inputArr);
                     break;
                 case ("event"):
-                    String[] eventDetails = inputArr[1].split(" /at ", 2);
-
-                    if (eventDetails.length == 2) {
-                        Event event = new Event(eventDetails[0], eventDetails[1]);
-                        addTask(event);
-                    } else {
-                        System.out.println("Please provide a proper command");
-                    }
+                    eventTask(inputArr);
                     break;
                 default:
                     // Default case if there is no command word in the front
-                    Task task = new Task(input);
-                    addTask(task);
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
         scanner.close();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Duke duke = new Duke();
         duke.initialize();
     }
