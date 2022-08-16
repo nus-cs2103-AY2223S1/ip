e3a446638f8aaacc3c89fd81e394014f35b5f161
jjtan444 <jjtan444@gmail.com>
2022-08-17 00:35:45 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,13 +7,23 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         String input = "";
+        int index = 0;
+        String[] items = new String[100];
         while(!input.equals("bye")) {
-            input = sc.next();
-            if (!input.equals("bye")) {
-                System.out.println(input);
+            input = sc.nextLine();
+            if (!input.equals("list") && !input.equals("bye")) {
+                items[index++] = input;
+                System.out.println("added: "+ input);
+            }
+            else if (input.equals("list")) {
+                for (int i = 0; i < items.length; i++) {
+                    if ((items[i]) == null) {
+                        break;
+                    }
+                    System.out.println(i+1 + ". " + items[i]);
+                }
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
-
     }
 }
