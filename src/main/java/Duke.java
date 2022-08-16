595a5d4e3d97f81d8c29b939d5e64d1cd5c540a8
Chen Hung-Yu <hyc9977@gmail.com>
2022-08-16 21:11:31 +0800

    Add skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String[] storage = new String[100];
+        int ticker = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke \n What can I do for you?");
+        String userInput = sc.nextLine();
+
+        while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                for (int i = 0; i < ticker; i++) {
+                    System.out.println((i + 1) + ":" + storage[i]);
+                }
+            } else {
+                storage[ticker] = userInput;
+                ticker++;
+                System.out.println("added:" + userInput);
+            }
+            userInput = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
