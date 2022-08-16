6954ffe90698735f4e51ed43c7259b89e1bac716
jovitaanderson <jovita@live.com.sg>
2022-08-16 17:21:28 +0800

    Add the ability to store text to list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Duke {
     static final String CHATBOX_NAME = "Ado";
     static final String PARTITION = "<><><><><><><><><><><><><><><><>\n";
+    static List<String> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String input = "";
@@ -10,29 +13,38 @@ public class Duke {
 
         String startMessage = "Yo! I'm " + CHATBOX_NAME + "!\nWhat can I do for you? :)\n";
         printMessage(startMessage);
-        while(!input.equals("bye")) {
-            input = sc.next();
-            input = input.toLowerCase();
+
+        while (!input.equalsIgnoreCase("bye")) {
+            input = sc.nextLine();
             switch (input) {
                 case "list":
-                    printMessage("list\n");
-                    break;
-                case "blah":
-                    printMessage("blah\n");
+                    printMessage(listToString());
                     break;
                 case "bye":
                     printMessage("Gone so soon? :( Bye\n");
                     break;
                 default:
-                    printMessage("Invalid input. Try again!\n");
+                    list.add(input);
+                    printMessage("added:" + input + "\n");
+                    break;
             }
         }
     }
 
-     static void printMessage(String message){
+    static String listToString() {
+        StringBuilder output = new StringBuilder();
+        if (list.size() == 0) {
+            return "List is empty!\n";
+        }
+        for (int i =0; i < list.size(); i++) {
+            output.append(i + 1).append(". ").append(list.get(i)).append("\n");
+        }
+        return output.toString();
+    }
+
+     static void printMessage(String message) {
         String print = PARTITION + message + PARTITION;
         System.out.println(print);
     }
-
 
 }
