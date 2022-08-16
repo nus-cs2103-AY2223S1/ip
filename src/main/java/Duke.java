54e985c3ba389b5af70e9ed895de1e5bc710055d
Dilysss <e0772712@u.nus.edu>
2022-08-16 15:51:23 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        lvlOne();
+    }
+
+    public static void lvlOne() {
+        System.out.println("Hello! I'm Duke\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tell me a word!");
+        String str = sc.nextLine();
+        System.out.println(str);
+
+        while (!str.equals("bye") && !str.equals("goodbye")) {
+            sc = new Scanner(System.in);
+            str = sc.nextLine();
+            System.out.println(str);
+        }
+        System.out.println("Bye! See you soon!");
     }
 }
