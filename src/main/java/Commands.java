ca3d5bb1d171188174bae0525bde8212379a8963
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2022-08-16 23:42:04 +0800

    A-Enums: added command enums

---
+++ src/main/java/Commands.java
@@ -0,0 +1,5 @@
+public enum Commands {
+    todo,
+    deadline,
+    event
+}
---
+++ src/main/java/EmptyArgumentException.java
@@ -0,0 +1,8 @@
+public class InvalidArgumentException extends DukeException {
+
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+
+    
+}
