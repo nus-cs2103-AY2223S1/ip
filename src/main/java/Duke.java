64a6bffee29ff658e18bdf5f93b43f4c854cb87c
Zsigmond <e0725064@u.nus.edu>
2022-08-17 01:09:05 +0800

    feat: mark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,21 +10,36 @@ public class Duke {
         ArrayList<Task> commands = new ArrayList<>();
         while(true) {
             String command = scanner.nextLine();
+            String[] commandArr = command.split(" ");
             System.out.println("------------------------------");
-            switch (command) {
-                case "bye":
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
-                case "list":
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < commands.size(); i++) {
-                        System.out.printf("%d. %s\n", i + 1, commands.get(i).toString());
+            switch(commandArr[0]) {
+                case "mark":
+                    int n;
+                    try {
+                        n = Integer.parseInt(commandArr[1]);
+                    } catch (NumberFormatException e) {     // if second word not integer
+                        continue;
                     }
-                    break;
+                    if (n >= 1 && n <= commands.size()) {   // ensure i given is within range
+                        commands.get(n - 1).mark();
+                        break;
+                    }
                 default:
-                    commands.add(new Task(false, command));
-                    System.out.printf("added: %s\n", command);
-                    break;
+                    switch (command) {
+                        case "bye":
+                            System.out.println("Bye. Hope to see you again soon!");
+                            break;
+                        case "list":
+                            System.out.println("Here are the tasks in your list:");
+                            for (int i = 0; i < commands.size(); i++) {
+                                System.out.printf("%d. %s\n", i + 1, commands.get(i).toString());
+                            }
+                            break;
+                        default:
+                            commands.add(new Task(false, command));
+                            System.out.printf("added: %s\n", command);
+                            break;
+                    }
             }
             System.out.println("------------------------------");
         }
