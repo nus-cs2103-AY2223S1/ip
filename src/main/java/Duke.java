362ac5cc27cff95087a7abae3a5af7f68d384bba
Francis-Tan <francistanty@gmail.com>
2022-08-16 21:21:45 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,55 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
 
+    /** The tasks stored */
+    private static final List<String> tasks = new ArrayList<>();
+
     /** Prints Duke's greeting on opening the app */
-    private static void IntroduceSelf() {
-        SayLines(new String[] {
-                "____________________________________________________________",
+    private static void introduceSelf() {
+        sayLines(new String[] {
                 "Hello! I'm Duke",
                 "What can I do for you?",
-                "____________________________________________________________"
         });
     }
 
-    /** Prints Duke's response to the input */
-    private static void Respond(String input) {
-        SayLines(new String[] {
-                "____________________________________________________________",
-                input,
-                "____________________________________________________________"
-        });
-    }
-
-    /** Prints Duke's closing statement on exiting the app */
-    private static void SayGoodbye() {
-        SayLines(new String[] {
-                "____________________________________________________________",
-                "Bye. Hope to see you again soon!",
-                "____________________________________________________________"
-        });
-    }
-
-    /** Prints the given lines */
-    private static void SayLines(String[] lines) {
+    /** Displays the given lines using a format */
+    private static void sayLines(String[] lines) {
+        System.out.println("____________________________________________________________");
         for (String line : lines) {
             System.out.println(line);
         }
-        System.out.println("");
+        System.out.println("____________________________________________________________\n");
+    }
+
+    /** Stores the given task and notifies user of outcome */
+    private static void addTask(String input) {
+        tasks.add(input);
+        sayLines(new String[] {
+                "added: " + input,
+        });
+    }
+
+    /** Lists out information on all tasks stored */
+    private static void listTasks() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < tasks.size(); ++i) {
+            System.out.println((i+1) + ". " + tasks.get(i));
+        }
+        System.out.println("____________________________________________________________\n");
+    }
+
+    /** Prints Duke's closing statement on exiting the app */
+    private static void sayGoodbye() {
+        sayLines(new String[] {
+                "Bye. Hope to see you again soon!",
+        });
     }
 
     public static void main(String[] args) {
-        IntroduceSelf();
+        introduceSelf();
 
-        Scanner InputScanner = new Scanner(System.in);
-        String UserInput = InputScanner.nextLine();
-        while (!Objects.equals(UserInput, "bye")) {
-            Respond(UserInput);
-            UserInput = InputScanner.nextLine();
+        Scanner inputScanner = new Scanner(System.in);
+        String userInput = inputScanner.nextLine();
+        while (!Objects.equals(userInput, "bye")) {
+            if (Objects.equals(userInput, "list")) {
+                listTasks();
+            } else {
+                addTask(userInput);
+            }
+            userInput = inputScanner.nextLine();
         }
-        InputScanner.close();
+        inputScanner.close();
 
-        SayGoodbye();
+        sayGoodbye();
     }
 }
