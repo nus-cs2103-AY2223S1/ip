d631625ea80a7e45556509dfedeb451ad0def36a
tan xuan yun <tanxuanyun42@gmail.com>
2022-08-16 21:38:43 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,21 +9,35 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         boolean run = true;
         String input;
-        ArrayList<String> items = new ArrayList<String>();
+        ArrayList<Task> items = new ArrayList<Task>();
 
         while(run) {
             System.out.println("\n--------------------");
             input = sc.nextLine();
-
-            if(input.equals("bye")) {
+            String[] segments = input.split(" ");
+            if (input.equals("bye")) {
                 System.out.println("Goodbye!");
                 run = false;
-            } else if(input.equals("list")) {
+            } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list: ");
                 for ( int i = 0; i < items.size(); i++) {
-                    System.out.println(i+1 + ". " + items.get(i));
+                    System.out.println(i+1 + ".["+ items.get(i).getStatusIcon()+"] " + items.get(i).getDescription());
                 }
+            } else if (segments[0].equals("mark")) {
+                int index = Integer.parseInt(segments[1]);
+                Task t = items.get(index - 1);
+                t.markAsDone();
+                System.out.println("Good! I've marked this task as done: ");
+                System.out.println("[" + t.getStatusIcon() + "] " + t.getDescription());
+            } else if (segments[0].equals("unmark")) {
+                int index = Integer.parseInt(segments[1]);
+                Task t = items.get(index - 1);
+                t.markAsNotDone();
+                System.out.println("ok, I've marked this task as not done yet:  ");
+                System.out.println("[" + t.getStatusIcon() + "] " + t.getDescription());
             } else {
-                items.add(input);
+                Task t = new Task(input);
+                items.add(t);
                 System.out.print("added: " + input);
             }
         }
