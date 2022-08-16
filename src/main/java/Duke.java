88b8960032fdcc8598be63b6f186d85b9001d9da
Lester Ong <lesterong776@gmail.com>
2022-08-16 19:21:57 +0800

    Add errors handling for invalid user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,17 @@ public class Duke {
                 "] " +
                 tasks[pos].getDescription();
     }
-    public static void addTask(String details, String type) {
+    public static void addTask(String command) throws DukeException {
+        String[] cmdArr = command.split(" ", 2);
+        if (cmdArr.length != 2) {
+            throw new DukeException(
+                    "The description of " +
+                            cmdArr[0] +
+                            " cannot be empty!"
+            );
+        }
+        String type = cmdArr[0];
+        String details = cmdArr[1];
         switch (type) {
             case "todo":
                 for (int pos = 0; pos < 100; pos++) {
@@ -28,6 +38,10 @@ public class Duke {
                 break;
             case "deadline":
             case "event":
+                String[] detailsArr = details.split("/");
+                if (detailsArr.length != 2) {
+                    throw new DukeException("Timing required for " + type + "!");
+                }
                 String description = details.split("/")[0];
                 String timing = details.split("/")[1].split(" ", 2)[1];
                 for (int pos = 0; pos < 100; pos++) {
@@ -49,45 +63,88 @@ public class Duke {
                 }
         }
     }
+    public static void unknownCommand(String command) throws DukeException {
+        throw new DukeException(
+                "I'm unable to do '" +
+                         command +
+                        "' at the moment. :("
+        );
+    }
+    public static void listTasks() throws DukeException {
+        for (int pos = 0; pos < 100; pos++) {
+            if (tasks[pos] == null) {
+                if (pos == 0) throw new DukeException("No tasks found.");
+                break;
+            }
+            System.out.println(pos + 1 + ". " + getTaskDetails(pos));
+        }
+    }
+    public static void markTask(String command) throws DukeException {
+        String[] cmdArr = command.split(" ");
+        if (cmdArr.length == 1) {
+            throw new DukeException("Provide a task number to mark");
+        } else if (cmdArr.length > 2) {
+            throw new DukeException("Provide a valid task number!");
+        }
+        int pos;
+        try {
+            pos = Integer.parseInt(command.split(" ", 2)[1]) - 1;
+        } catch (NumberFormatException e) {
+            throw new DukeException("Provide a numeric task number!");
+        }
+        if (pos < 0) {
+            throw new DukeException("Provide a valid number!");
+        }
+        if (tasks[pos] == null) {
+            throw new DukeException("No such task.");
+        }
+        switch (cmdArr[0]) {
+            case "mark":
+                tasks[pos].markAsDone();
+                System.out.println(
+                        "Nice! I've marked this task as done" +
+                                "\n" +
+                                getTaskDetails(pos)
+                );
+                break;
+            case "unmark":
+                tasks[pos].markAsUndone();
+                System.out.println(
+                        "Nice! I've marked this task as undone" +
+                                "\n" +
+                                getTaskDetails(pos)
+                );
+        }
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Hello! I'm Duke.\nWhat can I do for you?");
-        System.out.print(">> ");
+        System.out.print("Hello! I'm Duke.\nWhat can I do for you?\n>> ");
         String command = input.nextLine();
         while (!command.equals("bye")) {
             String cmdMain = command.split(" ", 2)[0];
-            switch (cmdMain) {
-                case "list":
-                    for (int pos = 0; pos < 100; pos++) {
-                        if (tasks[pos] == null) break;
-                        System.out.println(pos + 1 + ". " + getTaskDetails(pos));
+            try {
+                switch (cmdMain) {
+                    case "list":
+                        listTasks();
+                        break;
+                    case "mark":
+                    case "unmark":
+                        markTask(command);
+                        break;
+                    case "todo":
+                    case "deadline":
+                    case "event":
+                        addTask(command);
+                        break;
+                    default:
+                        unknownCommand(command);
                     }
-                    break;
-                case "mark":
-                case "unmark": {
-                        int pos = Integer.parseInt(command.split(" ", 2)[1]) - 1;
-                        if (cmdMain.equals("mark")) {
-                            tasks[pos].markAsDone();
-                        } else {
-                            tasks[pos].markAsUndone();
-                        }
-                        System.out.println(
-                                "Nice! I've marked this task as " +
-                                        (cmdMain.equals("mark") ? "done" : "undone") +
-                                        "\n" +
-                                        getTaskDetails(pos)
-                        );
-                    }
-                    break;
-                case "todo":
-                case "deadline":
-                case "event":
-                    String cmdDetails = command.split(" ", 2)[1];
-                    addTask(cmdDetails, cmdMain);
-                    break;
-                }
-            System.out.print(">> ");
-            command = input.nextLine();
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                System.out.print("\n>> ");
+                command = input.nextLine();
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
         input.close();
