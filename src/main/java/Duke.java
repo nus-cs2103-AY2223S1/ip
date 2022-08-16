bb79c9f1238db8df04618bee012a062cdfb8b9df
Shashank Acharya <acharya.s@icloud.com>
2022-08-16 18:04:14 +0800

    Added marking & unmarking increments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Duke {
+    // FORMATTING
     private static final String logo =
             "$$\\                                     $$\\       \n" +
             "$$ |                                    $$ |      \n" +
@@ -14,11 +16,21 @@ public class Duke {
     private static final char lineBreak = '\n';
     private static final String line = "---------------------------------------------------";
     private static final String indent = "      ";
+
+    // MESSAGES
     private static final String greetingMessage = "Hi, I'm Lurch." + lineBreak + "You rang?";
+    private static final String byeMessage = "Have a lovely day with dark and cloudy skies.";
+    private static final String markMessage = "Nice! I've marked this task as done:";
+    private static final String unmarkMessage = "OK, I've marked this task as not done yet:";
+
+    // COMMANDS
     private static final String listCommand = "list";
     private static final String exitCommand = "bye";
-    private static final String byeMessage = "Have a lovely day with dark and cloudy skies.";
-    private final ArrayList<String> taskList = new ArrayList<>();
+    private static final String markPattern = "^mark \\d+$";
+    private static final String unmarkPattern = "^unmark \\d+$";
+
+    // INSTANCE VARIABLES
+    private final ArrayList<Task> taskList = new ArrayList<>();
     private boolean isTerminated = false;
 
     public static void lurchMessage(String message) {
@@ -34,22 +46,38 @@ public class Duke {
     }
 
     public void addTask(String task) {
-        this.taskList.add(task);
+        this.taskList.add(new Task(task));
         lurchMessage("added: " + task);
     }
 
     public void listTasks() {
         String message = "";
         for (int i = 0; i < this.taskList.size(); i++) {
-            message += i + 1 + ". " + this.taskList.get(i);
+            message += i + 1 + ". " + this.taskList.get(i).toString();
             if (i < this.taskList.size() - 1) message += lineBreak;
         }
         lurchMessage(message);
     }
 
+    public void markTask(String cmd) {
+        final int idx = Integer.parseInt(cmd.replaceAll("[^0-9]", ""));
+        Task selectedTask = this.taskList.get(idx - 1);
+        selectedTask.mark();
+        lurchMessage(markMessage + lineBreak + indent + selectedTask.toString());
+    }
+
+    public void unmarkTask(String cmd) {
+        final int idx = Integer.parseInt(cmd.replaceAll("[^0-9]", ""));
+        Task selectedTask = this.taskList.get(idx - 1);
+        selectedTask.unmark();
+        lurchMessage(unmarkMessage + lineBreak + indent + selectedTask.toString());
+    }
+
     public void command(String cmd) {
         if (cmd.equals(exitCommand)) this.terminate();
         else if (cmd.equals(listCommand)) this.listTasks();
+        else if (Pattern.matches(unmarkPattern, cmd)) unmarkTask(cmd);
+        else if (Pattern.matches(markPattern, cmd)) markTask(cmd);
         else this.addTask(cmd);
     }
 
