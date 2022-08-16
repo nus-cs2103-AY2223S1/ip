8df800902fb70f7c34a34ae6245c1fb5c9499d79
MinHeinA <minheinaung97@gmail.com>
2022-08-16 21:49:31 +0800

    Created Task.java
    - Created Task.java and added necessary fields and its getters

---
+++ src/main/java/Task.java
@@ -0,0 +1,17 @@
+public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public Boolean getTaskStatus() {
+        return this.isDone;
+    }
+}
