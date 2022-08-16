dd9db22b28c445d3b55c2c1b95942d05b10af7a5
phyliciachristel <e0775641@u.nus.edu>
2022-08-16 16:12:32 +0800

    implemented level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String reply = "";
         String exit = "bye";
+        List<String> todoList = new ArrayList<>();
         System.out.println("hi im chompers what do u need!!!\n");
 
         while(true) {
@@ -13,10 +16,18 @@ public class Duke {
                 System.out.println("bye see u");
                 scanIn.close();
                 break;
-            } else {
-                System.out.println(reply);
+            } else if (reply.equals("list")) {
+                printList(todoList);
+            }else {
+                todoList.add(reply);
+                System.out.println("added: " + reply);
             }
 
+        }
+    }
+    public static void printList(List<String> list) {
+        for(int i = 1; i <= list.size(); i++) {
+            System.out.println(i + ". " + list.get(i-1));
         }
     }
 }
