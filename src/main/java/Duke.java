95e588d8d5d662fb9538b94b0f963e0dfaafb21e
jovitaanderson <jovita@live.com.sg>
2022-08-16 16:54:00 +0800

    Implemented basic skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class Duke {
+    static final String CHATBOX_NAME = "Ado";
+    static final String PARTITION = "<><><><><><><><><><><><><><><><>\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String input = "";
+        Scanner sc = new Scanner(System.in);
+
+        String startMessage = "Yo! I'm " + CHATBOX_NAME + "!\nWhat can I do for you? :)\n";
+        printMessage(startMessage);
+        while(!input.equals("bye")) {
+            input = sc.next();
+            input = input.toLowerCase();
+            switch (input) {
+                case "list":
+                    printMessage("list\n");
+                    break;
+                case "blah":
+                    printMessage("blah\n");
+                    break;
+                case "bye":
+                    printMessage("Gone so soon? :( Bye\n");
+                    break;
+                default:
+                    printMessage("Invalid input. Try again!\n");
+            }
+        }
     }
+
+     static void printMessage(String message){
+        String print = PARTITION + message + PARTITION;
+        System.out.println(print);
+    }
+
+
 }
