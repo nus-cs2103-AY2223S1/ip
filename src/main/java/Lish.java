763917dbde0ebf959c584f28b9c601659890515c
albertarielw <albertariel2002@gmail.com>
2022-08-17 00:31:14 +0800

    Level-3

--- src/main/java/Lish.java
+++ src/main/java/Lish.java
@@ -3,28 +3,49 @@ import java.util.Scanner;
 
 public class Lish {
 
+    public static String breakLine = "____________________________________________________________\n";
+
     public static void printResponse(String response) {
-        System.out.println("    ____________________________________________________________\n");
-        System.out.println("    " + response);
-        System.out.println("    ____________________________________________________________\n");
+        System.out.println(breakLine);
+        System.out.println(response);
+        System.out.println(breakLine);
     }
 
-    public static ArrayList<String> tasks = new ArrayList<String>();
+    public static ArrayList<Task> taskList = new ArrayList<Task>();
 
-    public static void printTasks() {
-        System.out.println("    ____________________________________________________________\n");
-        for (int i = 1; i <= tasks.size(); ++i) {
-            System.out.println("    " + i + ". " + tasks.get(i - 1));
+    public static void printTaskList() {
+        System.out.println(breakLine);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 1; i <= taskList.size(); ++i) {
+            System.out.println(i + ". " + taskList.get(i - 1));
         }
-        System.out.println("    ____________________________________________________________\n");
+        System.out.println(breakLine);
+    }
+
+    public static void markTaskAsDone(int index) {
+        System.out.println(breakLine);
+        taskList.get(index).markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(taskList.get(index));
+        System.out.println(breakLine);
+    }
+
+    public static void markTaskAsNotDone(int index) {
+        System.out.println(breakLine);
+        taskList.get(index).markAsNotDone();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(taskList.get(index));
+        System.out.println(breakLine);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         boolean stopLish = false;
-        String greeting = "Hello! I'm Lish\n" + "    What can I do for you?\n";
+        String greeting = "Hello! I'm Lish\n" + "What can I do for you?\n";
         printResponse(greeting);
+
+
 
         while (!stopLish) {
             String input = sc.nextLine();
@@ -37,11 +58,23 @@ public class Lish {
                     stopLish = true;
                     break;
                 case "list":
-                    printTasks();
+                    printTaskList();
                     break;
                 default:
-                    tasks.add(input);
-                    printResponse("added: " + input);
+                    String[] commands = input.split(" ");
+
+                    if (commands[0].equals("mark") || commands[0].equals("unmark")) {
+                        int index = Integer.parseInt(commands[1]) - 1;
+                        if (commands[0].equals("mark")) {
+                            markTaskAsDone(index);
+                        } else {
+                            markTaskAsNotDone(index);
+                        }
+                    } else {
+                        Task newTask = new Task(input);
+                        taskList.add(newTask);
+                        printResponse("added: " + input);
+                    }
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
