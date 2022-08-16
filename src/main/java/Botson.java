68d26309db1c4cd915d8fabd3b8101545e948a80
jicsontoh <>
2022-08-16 12:26:43 +0800

    Level-2, Add,List

--- src/main/java/Botson.java
+++ src/main/java/Botson.java
@@ -2,15 +2,18 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Botson {
+    static String[] list = new String[100];
     public static void main(String[] args) {
         welcome();
     }
 
     /**
-     * Duke Level 1. Greet, Echo, Exit
-     * Echos the input and exit when user types "bye"
+     * Echos the input and stores it in the list
+     * Exit when user types "bye"
      **/
     public static void welcome() {
+        list = new String[100];
+        int idx = 0;
         System.out.println("Hello! I'm Botson");
         Scanner input = new Scanner(System.in);
         System.out.println("What can I help you with?");
@@ -21,9 +24,21 @@ public class Botson {
                 System.out.println("Goodbye! Hope to see you again soon!");
                 System.out.println("--------------------------");
                 break;
+            } else if (Objects.equals(action, "list")) {
+                getList(idx);
+                continue;
             }
-            System.out.println(action);
+            list[idx] = action;
+            idx++;
+            System.out.println("Added: " + action);
             System.out.println("--------------------------");
         }
+    }
+
+    private static void getList(int idx) {
+        for (int i = 0; i < idx; i++) {
+            System.out.println((i + 1) + ". " + list[i]);
+        }
+        System.out.println("--------------------------");
     }
 }
