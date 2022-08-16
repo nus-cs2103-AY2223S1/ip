9d2d7419537a3970b4f35b40a3f01af1d477ed65
Prittam Ravi <prittam867@gmail.com>
2022-08-16 15:04:16 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,44 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class Duke {
     private final static String UNDERLINE = "_________________________________";
-    private ArrayList<String> taskList = new ArrayList<>();
+    private ArrayList<Task> taskList = new ArrayList<>();
     boolean inProcess = true;
-    
+
     private String greet(String res) {
         if (res.toLowerCase().equals("bye")) {
             inProcess = false;
             return "Bye. Hope to see you again soon!";
-        } else if(res.toLowerCase().equals("list")){
-            String log ="Tasks that you have:";
+        } else if(res.toLowerCase().equals("list")) {
+            String log = "Tasks that you have:";
             for (int i = 0; i < taskList.size(); i++) {
-                log += String.format("\n %d: %s", i + 1, this.taskList.get(i));
+                log += String.format("\n %d. %s", i + 1, this.taskList.get(i));
             }
             return log;
+        } else if(res.toLowerCase().startsWith("mark")){
+            int taskNumber = Integer.valueOf(res.substring("mark ".length()));
+            return this.handleMark(taskNumber);
+        } else if(res.toLowerCase().startsWith("unmark")){
+            int taskNumber = Integer.valueOf(res.substring("unmark ".length()));
+            return this.handleUnmark(taskNumber);
         } else {
-            this.taskList.add(res);
+            this.taskList.add(new Task(res));
             return String.format("added: %s",res);
         }
+    }
+
+    private String handleMark(int num){
+        Task task = this.taskList.get(num - 1);
+        task.markAsDone();
+        return String.format("Nice! I've marked this task as done: \n %s",task);
+    }
+
+    private String handleUnmark(int num){
+        Task task = this.taskList.get(num - 1);
+        task.unmarkAsDone();
+        return String.format("Ok! I have marked this task as not done yet: \n %s",task);
     }
     private void run() {
         Scanner sc= new Scanner(System.in);
