0fa2589e15c26cae88ce3674f81f5f5481e9620f
timzzz <timothykoei@gmail.com>
2022-08-17 02:16:25 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,51 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public void run() {
+        welcome();
+        command();
+        bye();
+    }
+
+    public static void welcome() {
+        String logo = "      ____        _        \n"
+                + "     |  _ \\ _   _| | _____ \n"
+                + "     | | | | | | | |/ / _ \\\n"
+                + "     | |_| | |_| |   <  __/\n"
+                + "     |____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("    ____________________________________________________________");
+        System.out.println(logo);
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+    }
+
+    public static void command() {
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+        while (!cmd.equals("bye")) {
+            echo(cmd);
+            cmd = sc.nextLine();
+        }
+    }
+
+    public static void echo(String cmd) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     " + cmd);
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+    }
+
+    public static void bye() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!" );
+        System.out.println("    ____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke program = new Duke();
+        program.run();
     }
 }
