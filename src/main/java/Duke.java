bd7bc024d0af23e737905c528276ddd2b071968f
Alvin  Jiang <alvinjiang1@gmail.com>
2022-08-16 14:57:43 +0800

    Level-2
    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,45 @@
 import java.util.Scanner;
 
 public class Duke {
-    final static String logo = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static String[] strList = new String[100];
+    private static int counter = 0;
+    final static String logo =
+            "   __ __    ____       ___               __    \n" +
+            "  / // /__ / / /__    / _ \\___ ____  ___/ /__ _\n" +
+            " / _  / -_) / / _ \\  / ___/ _ `/ _ \\/ _  / _ `/\n" +
+            "/_//_/\\__/_/_/\\___/ /_/   \\_,_/_//_/\\_,_/\\_,_/ \n" +
+            "                                               \n";
+
+    private static void printCommand(String str) {
+        System.out.println("added: " + str);
+        System.out.println("#########################");
+    }
+
+    private static void storeCommand(String str) {
+        if (counter >= 100) return;
+        strList[counter] = str;
+        counter++;
+    }
+
+    private static void displayList() {
+        for (int i = 0; i < 100; i++) {
+            if (strList[i] == null) break;
+            System.out.println(i + 1 + ". " + strList[i]);
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
         System.out.println("How can I help you today? :)");
         String command = sc.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(command);
-            System.out.println("#########################");
+            if (command.equals("list")) {
+                displayList();
+                command = sc.nextLine();
+                continue;
+            }
+            printCommand(command);
+            storeCommand(command);
             command = sc.nextLine();
         }
         sc.close();
