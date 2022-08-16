a01affa59503bb9e91bfcfdc5c18e7ec8a76b341
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2022-08-17 03:12:49 +0800

    Remove unnecessary comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,11 +6,7 @@ public class Duke {
 
         // Create a scanner that reads user input.
         Scanner sc = new Scanner(System.in);
-
-        // This is a test comment.
-
-        // This is a second test comment.
-
+        
         // Greet the user.
         System.out.println("Duke: Hello! I am Duke.");
 
