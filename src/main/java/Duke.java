c8742d8e9bd21aabd184448f87055566abc0e1a0
pwjj2000 <perrywongjunjie@gmail.com>
2022-08-16 20:59:40 +0800

    added javadocs for Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,11 @@
+/**
+ * A Personal Assistant Chatbot that helps a person to keep track of various things.
+ * CS2103T iP
+ * AY22/23 Semester 1
+ *
+ * @author Perry Wong
+ */
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,7 +13,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String greeting = logo + "Hello! I'm Duke\n" + "What can I do for you?";
+        String greeting = logo + "Hello! I'm Duke\n" + "What can I do for you?\n";
         System.out.println(greeting);
         Echo.echo();
     }
--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,7 +1,19 @@
+/**
+ * Represents an echo function which echoes whatever the user inputs.
+ * CS2103T iP
+ * AY22/23 Semester 1
+ *
+ * @author Perry Wong
+ */
+
 // Level 1
+
 import java.util.Scanner;
 
 public class Echo {
+    /**
+     * The method to echo the user's input.
+     */
     public static void echo() {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
