adca069d90bb842eb768eaf35119594c6f1060d4
SpecOps2016 <gregory.wong@ymail.com>
2022-08-17 03:18:02 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,6 +25,39 @@ public class Duke {
             this.isDone = value;
         }
     }
+
+    public static class Todo extends Task {
+        public Todo (String description) {
+            super(description);
+        }
+
+    }
+
+    public static class Deadline extends Task {
+        public Deadline (String description) {
+            super(description);
+        }
+
+
+        @Override
+        public String toString() {
+            return this.description.replace("/","(") + ")";
+        }
+
+    }
+
+    public static class Event extends Task {
+        public Event (String description) {
+            super(description);
+        }
+
+
+        @Override
+        public String toString() {
+            return this.description.replace("/","(").replace("at", "at:")+")";
+        }
+
+    }
     public static void main(String[] args) {
 
         String logo = " ____        _        \n"
@@ -35,6 +68,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
         ArrayList<Task> ls = new ArrayList<>();
+        ArrayList<String> tested = new ArrayList<>();
         while (true) {
             Scanner sc = new Scanner(System.in);
             String line = sc.nextLine();
@@ -45,8 +79,16 @@ public class Duke {
 
             else if(line.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i=0; i<ls.size(); i++) {
-                    System.out.println(i+1 + "." +" " + "[" + ls.get(i).getStatusIcon()+"]" +" " + ls.get(i).toString());
+                for (int i = 0; i < ls.size(); i++) {
+                    if (ls.get(i) instanceof Todo) {
+                        System.out.println(i + 1 + "." + " " + "[T]" + "[" + ls.get(i).getStatusIcon() + "]" + " " + ls.get(i).toString());
+                    } else if(ls.get(i) instanceof Deadline) {
+                        System.out.println(i + 1 + "." + " " + "[D]" + "[" + ls.get(i).getStatusIcon() + "]" + " " + ls.get(i).toString());
+                    } else if (ls.get(i) instanceof Event) {
+                        System.out.println(i + 1 + "." + " " + "[E]" + "[" + ls.get(i).getStatusIcon() + "]" + " " + ls.get(i).toString());
+                    } else {
+                        System.out.println(i + 1 + "." + " " + "[" + ls.get(i).getStatusIcon() + "]" + " " + ls.get(i).toString());
+                    }
                 }
             }
 
@@ -54,14 +96,38 @@ public class Duke {
                 int num = Integer.parseInt(line.substring(7));
                 ls.get(num-1).setStatus(false);
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("[" + ls.get(num-1).getStatusIcon() +"]" +" " +ls.get(num-1).toString());
+                System.out.println("["+ls.get(num-1).getStatusIcon() +"]" +" " +ls.get(num-1).toString());
             }
 
             else if(line.contains("mark")) {
                 int num = Integer.parseInt(line.substring(5));
                 ls.get(num-1).setStatus(true);
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + ls.get(num-1).getStatusIcon() +"]" +" " +ls.get(num-1).toString());
+                System.out.println( "["+ls.get(num-1).getStatusIcon() +"]" +" " +ls.get(num-1).toString());
+            }
+
+            else if (line.contains("todo")) {
+                Todo test = new Todo(line.substring(5));
+                ls.add(test);
+                System.out.println("Got it. I've added this task:");
+                System.out.println( "[T][ ]" + " " + line.substring(5));
+                System.out.println("Now you have" + " " + ls.size() + " " +"tasks in the list.");
+            }
+
+            else if (line.contains("deadline")) {
+                Deadline test = new Deadline(line.substring(9));
+                ls.add(test);
+                System.out.println("Got it. I've added this task:");
+                System.out.println( "[D][ ]" + " " +test.toString());
+                System.out.println("Now you have" + " " + ls.size() + " " +"tasks in the list.");
+            }
+
+            else if (line.contains("event")) {
+                Event test = new Event(line.substring(6));
+                ls.add(test);
+                System.out.println("Got it. I've added this task:");
+                System.out.println( "[E][ ]" + " " + test.toString());
+                System.out.println("Now you have" + " " + ls.size() + " " +"tasks in the list.");
             }
 
             else {
