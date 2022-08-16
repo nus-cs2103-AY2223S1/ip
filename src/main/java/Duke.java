6ca13a1eb867468aaed898258884bf61eeb1da32
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-17 03:29:29 +0800

    Complete second increment which allows saving and printing of user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        List<String> userData = new ArrayList<>();
+
         System.out.println("Hello! I'm Duke!\nWhat can I do for you?");
         System.out.println("-----------------------------------");
         while (true) {
@@ -10,8 +14,13 @@ public class Duke {
             if ("bye".equalsIgnoreCase(line)) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else {
-                System.out.println(line);
+            } else if ("list".equalsIgnoreCase(line)) {
+                for (int i = 0; i < userData.size(); i++) {
+                    System.out.format("%s. %s\n", i, userData.get(i));
+                }
+            } else{
+                userData.add(line);
+                System.out.format("added: %s\n", line);
             }
             System.out.println("-----------------------------------");
         }
