24ecd6f1c6ec07e2017a9acbe0b923b643bc6de1
ThomasHoooo <e0725843@u.nus.edu>
2022-08-17 01:39:27 +0800

    Feat: Duke can store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,16 +10,30 @@ public class Duke {
         System.out.println(logo + "Hello! I'm Yale\nWhat can I do for you?");
         Scanner in = new Scanner(System.in);
 
+        String[] toDoList = new String[100];
+        int count = 0;
+
         while (true) {
             String input = in.nextLine();
             if (input.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again soon!\n");
                 break;
+            } else if (input.equals("list")) {
+                if (count == 0) {
+                    System.out.println("There is no item in list yet");
+                } else {
+                    for (int i = 0; i < count; i++) {
+                        System.out.printf("%d. %s\n", i + 1, toDoList[i]);
+                    }
+                }
+            } else {
+                toDoList[count] = input;
+                count++;
+                String message = "\t" + "_".repeat(20) + "\n"
+                        + String.format("\t  added: %s\n", input)
+                        + "\t" + "_".repeat(20) + "\n";
+                System.out.println(message);
             }
-            String message = "\t" + "_".repeat(20) + "\n"
-                    + String.format("\t  %s\n", input)
-                    + "\t" + "_".repeat(20) + "\n";
-            System.out.println(message);
         }
     }
 }
