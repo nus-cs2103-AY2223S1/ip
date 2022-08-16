5a0cd98834569f0d0334a5249720897ffe8366c4
loyhongshenggg <e0725346@u.nus.edu>
2022-08-17 02:26:55 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         int count = 0;
 
         System.out.println("Hello I'm Duke" + "\nWhat can I do for you?");
@@ -21,10 +21,18 @@ public class Duke {
             } else if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i ++) {
-                    System.out.println((i + 1) + ". " + arr[i]);
+                    System.out.println((i + 1) + ". " + "[" + arr[i].getStatusIcon() + "] " + arr[i].description);
                 }
+            } else if (input.contains("mark")) {
+                int index = -1;
+                input = input.replaceAll("mark ", "");
+                index = Integer.parseInt(input) - 1;
+                arr[index].setDone();
+
+                System.out.println("Nice! I've marked this task as done:\n" +
+                        "[" + arr[index].getStatusIcon() + "] " + arr[index].description);
             } else {
-                arr[count] = input;
+                arr[count] = new Task(input);
                 System.out.println("added: " + input);
                 count++;
             }
