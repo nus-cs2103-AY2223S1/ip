6296b185495066e49b96c057dd3650f06b139ec5
Shashank Acharya <acharya.s@icloud.com>
2022-08-16 18:03:54 +0800

    Added Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        char statusIcon = this.isDone ? 'X' : ' ';
+        return "[" + statusIcon + "] " + this.description;
+    }
+}
