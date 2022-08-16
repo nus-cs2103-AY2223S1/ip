f43127075d18ca3795445186fc115cd597380485
Brendan Tan <brendantan@outlook.com>
2022-08-16 20:47:25 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String DIVIDER = "____________________________________________________________\n";
+    private static final String INTRO = DIVIDER + "Hello! I'm Duke!\n" + "What can I do for you?\n" + DIVIDER;
+    private static final String OUTRO = DIVIDER + "Bye. Hope to see you again soon!\n" + DIVIDER;
+    private static final String EXIT_KEYWORD = "bye";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(INTRO);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals(EXIT_KEYWORD)) {
+                scanner.close();
+                break;
+            }
+
+            System.out.println(DIVIDER + input + "\n" + DIVIDER);
+        }
+
+        System.out.println(OUTRO);
     }
 }
