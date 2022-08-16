102a53fb969cc07f1de988de63a817488f0b2384
MuhdMusab <musab653@gmail.com>
2022-08-16 09:09:54 +0800

    Removed extra functions and print statements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,23 +7,6 @@ public class Duke {
         tasks = new ArrayList<Task>();
     }
 
-    private void printStoredInputs() {
-        int numberOfTasks = this.tasks.size();
-        if (numberOfTasks > 0) {
-            System.out.println("Boss ah, this one your tasks:");
-            for (int i = 0; i < numberOfTasks; i++) {
-                System.out.println(i + 1 + ". " + tasks.get(i));
-            }
-            this.printTaskCountMessage();
-        } else if (numberOfTasks == 0) {
-            System.out.println("Boss, you got no task yet ah");
-        }
-    }
-
-    private void addTask(Task input) {
-        tasks.add(input);
-    }
-
     private void startChatBot() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Eh hello, my name is Uncle Cheong. \n" +
@@ -32,15 +15,7 @@ public class Duke {
         inputParser.parseInputs();
     }
 
-    private void printTaskCountMessage() {
-        System.out.printf("Boss, you got %s tasks now\n", this.tasks.size());
-    }
-
     public static void main(String[] args) {
-        String[] arr = "abc".split(" ", 3);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
         Duke uncleCheong = new Duke();
         uncleCheong.startChatBot();
     }
