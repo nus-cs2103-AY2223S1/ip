312d3bc3868189a099f36095bdd16719d473cc89
loyhongshenggg <e0725346@u.nus.edu>
2022-08-17 02:18:26 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
+        String[] arr = new String[100];
+        int count = 0;
+
         System.out.println("Hello I'm Duke" + "\nWhat can I do for you?");
 
+        //scanner
         Scanner sc = new Scanner(System.in);
         String input = "";
 
@@ -14,8 +18,15 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < count; i ++) {
+                    System.out.println((i + 1) + ". " + arr[i]);
+                }
             } else {
-                System.out.println(input);
+                arr[count] = input;
+                System.out.println("added: " + input);
+                count++;
             }
         }
         sc.close();
