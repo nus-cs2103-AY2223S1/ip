d5bcfd303efc5a1ac27dd71c95a82fdbcd5b681c
lzq <li.zhaoqi@dhs.sg>
2022-08-17 04:02:05 +0800

    Updated exception message

--- src/main/java/OutOfBoundException.java
+++ src/main/java/OutOfBoundException.java
@@ -1,5 +1,5 @@
 public class OutOfBoundException extends DukeException {
-    private static final String DESCRIPTION = "The provided index exceeds the total number of tasks available!";
+    private static final String DESCRIPTION = "The provided index number is invalid!";
 
     OutOfBoundException() {
         super(DESCRIPTION);
