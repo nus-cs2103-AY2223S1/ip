2610c25eb5d413b0221b4c63e77117722d1cb716
sikai00 <sikai4111@gmail.com>
2022-08-16 22:44:20 +0800

    Change representation of tasks into its own class
    This is a more natural approach to represent tasks. Task can also have
    more information beyond its title for future code extension. A TaskList
    class is also added to manage and store these tasks. These changes allow
    for abstraction barriers to be erected.

---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+  private String title;
+  private boolean done;
+
+  public Task(String title, boolean done) {
+    this.title = title;
+    this.done = done;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public boolean getDone() {
+    return this.done;
+  }
+
+  public void setDone() {
+    this.done = true;
+  }
+
+  public void setNotDone() {
+    this.done = false;
+  }
+
+  @Override
+  public String toString() {
+    String checkbox = this.getDone() ? "[X]" : "[ ]";
+    return checkbox + " " + this.title;
+  }
+}
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,68 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class TaskList {
+  private List<Task> tasks;
+
+  public TaskList() {
+    this.tasks = new ArrayList<>();
+  }
+
+  /**
+   * Add a new task.
+   * 
+   * @param task Task to be added.
+   */
+  public void addTask(Task task) {
+    this.tasks.add(task);
+  }
+
+  /**
+   * Returns the task with the input index in its string representation.
+   * 
+   * @param index
+   * @return String representation of the task with the input index.
+   */
+  public String getTaskToString(int index) {
+    return this.tasks.get(index).toString();
+  }
+
+  /**
+   * Mark a task as done.
+   * 
+   * @param index Position of the task as indicated by TaskList's string
+   *              representation.
+   */
+  public void markTask(int index) {
+    Task task = this.tasks.get(index);
+    task.setDone();
+  }
+
+  /**
+   * Mark a task as not done.
+   * 
+   * @param index Position of the task as indicated by TaskList's string
+   *              representation.
+   */
+  public void unmarkTask(int index) {
+    Task task = this.tasks.get(index);
+    task.setNotDone();
+  }
+
+  /**
+   * Return a string representation of all tasks.
+   * 
+   * @return String representation of all tasks
+   */
+  public String toString() {
+    int taskListSize = this.tasks.size();
+    // String in java are immutable and leads to O(n^2) time complexity
+    StringBuilder allTasks = new StringBuilder();
+    for (int i = 0; i < taskListSize; i++) {
+      int index = i + 1;
+      Task task = this.tasks.get(i);
+      allTasks.append(index + "." + task + "\n");
+    }
+    return allTasks.toString();
+  }
+}
