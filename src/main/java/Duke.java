8c353debbdfe8262a4419ee3ca8cb953eb6370f0
lyuting47 <luting_c@yahoo.com>
2022-08-17 00:42:08 +0800

    Added level 1, added *.class to .gitignore

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String printSpacer() {
+        return "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(printSpacer());
+        System.out.println("(｡･ω･｡)ﾉ♡ Hello! This is Duke! What can I do for you today?");
+        System.out.println(printSpacer());
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String s = sc.nextLine();
+
+            if (s.equals("bye")) {
+                System.out.println("(~‾▿‾)~ Bye. Hope to see you again soon!");
+                System.out.println(printSpacer());
+                break;
+            } else {
+                System.out.println(s);
+                System.out.println(printSpacer());
+            }
+        }
     }
 }
