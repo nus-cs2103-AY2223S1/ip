0e59b8d6c816921b7a22b87a686edaf467146a2d
ryanguai <e0694442@u.nus.edu>
2022-08-16 13:53:39 +0800

    Added Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Hello! I'm Snoopy\n");
-        System.out.println("What can I do for you?\n");
+        System.out.println("Hello! I'm Snoopy");
+        System.out.println("What can I do for you?");
         String response = myObj.nextLine();
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int i=0;
         while (!response.equals("bye")) {
             if (response.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int a=1; a<=i; a++) {
-                    String output = a + ". " + tasks[a-1];
+                    String output = a + ".[" + tasks[a-1].getStatusIcon() + "] " + tasks[a-1].description;
                     System.out.println(output);
                 }
             } else {
-                tasks[i] = response;
+                tasks[i] = new Task(response);
                 System.out.println("added: " + response);
                 i++;
             }
