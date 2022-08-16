b3f16cfd1ca55c2c7672c143c1123f32cfaf7ef9
fungusta <peterjosephfung@gmail.com>
2022-08-17 01:01:06 +0800

    Increment Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Fungusta\n" + "Peter's personal chatbot\n");
+        Scanner sc = new Scanner((System.in));
+        String userInput = sc.next();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput + "\n");
+            userInput = sc.next();
+        }
+        System.out.println("Goodbye!");
     }
+
 }
