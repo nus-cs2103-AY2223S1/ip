60e93bbe95e064084391f52f6275d688c1ec7d8d
Puakii <theyitengaxis@gmail.com>
2022-08-16 10:34:53 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,9 @@ public class Duke {
     public static final String LINES = "_________________________\n";
     public static final String START = "Hey there! I'm Duke.\nWhat do you want to do today?\n";
     public static final String END = "Bye! Hope you had fun!";
-    private ArrayList<String> tasks;
+    public static final String DONE = "Nice! I've marked this task as done:\n";
+    public static final String UNDONE =  "OK, I've marked this task as not done yet:\n";
+    private ArrayList<Task> tasks;
     private boolean ended;
 
 
@@ -18,10 +20,37 @@ public class Duke {
         System.out.print(LINES + message + "\n" + LINES);
     }
 
-    public void printList() {
-        System.out.print(LINES);
+    public void markTask(int index) {
+        Task task = this.tasks.get(index);
+        task.markAsDone();
+        printMessage(DONE + task);
+    }
 
-        System.out.print(LINES);
+    public void unmarkTask(int index) {
+        Task task = this.tasks.get(index);
+        task.markAsUndone();
+        printMessage(UNDONE + task);
+    }
+
+    public void end() {
+        printMessage(END);
+        this.ended = true;
+    }
+
+    public void listTasks() {
+        StringBuilder allTasks = new StringBuilder();
+        for (int i = 1; i <= this.tasks.size(); i++) {
+            allTasks.append(i + ". " + this.tasks.get(i - 1));
+            if (i < this.tasks.size()) {
+                allTasks.append("\n");
+            }
+        }
+        printMessage(allTasks.toString());
+    }
+
+    public void addTask(String command) {
+        printMessage("added: " + command);
+        this.tasks.add(new Task(command));
     }
 
     public void start() {
@@ -29,24 +58,22 @@ public class Duke {
         printMessage(START);
         while(!ended) {
             String command = sc.nextLine();
-            switch (command) {
+            String[] splitCommand = command.split("\\s+");
+            switch (splitCommand[0]) {
                 case "bye":
-                    printMessage(END);
-                    this.ended = true;
+                    end();
                     break;
                 case "list":
-                    StringBuilder allTasks = new StringBuilder();
-                    for (int i = 1; i <= tasks.size(); i++) {
-                        allTasks.append(i + ". " + tasks.get(i - 1));
-                        if (i < tasks.size()) {
-                            allTasks.append("\n");
-                        }
-                    }
-                    printMessage(allTasks.toString());
+                    listTasks();
+                    break;
+                case "mark":
+                    markTask(Integer.parseInt(splitCommand[1]) - 1);
+                    break;
+                case "unmark":
+                    unmarkTask(Integer.parseInt(splitCommand[1]) - 1);
                     break;
                 default:
-                    printMessage("added: " + command);
-                    this.tasks.add(command);
+                    addTask(command);
             }
         }
     }
