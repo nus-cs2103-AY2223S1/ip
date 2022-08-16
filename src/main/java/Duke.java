78d21d157dcee8c0f23f0960ea0b8d44f5396fe5
sikai00 <sikai4111@gmail.com>
2022-08-16 19:52:34 +0800

    Duke.java: Complete Level 2: Add, List
    Refactored Duke into its own object
    
    Duke now has instance level methods and attributes. This is primarily
    done to allow better future code expansion.
    
    Change usrCommand variable to usrInput
    
    usrCommand is misleading as tasks are not commands. The use case of the
    command line have expanded to include addition of new tasks without
    commands but by simply entering the task into the command line.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,38 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
-	public static void main(String[] args) {
-		String greetingMsg = "Hello! I'm Duke \nWhat can I do for you?";
-		String goodByeMsg = "Bye. Hope to see you again soon!";
-		Scanner sc = new Scanner(System.in);
-		prettyPrint(greetingMsg);
-		while (true) {
-			String usrCommand = sc.nextLine();
-			if (usrCommand.equals("bye")) {
-				break;
-			}
-			prettyPrint(usrCommand);
+	private List<String> taskStorage;
+
+	public Duke() {
+		this.taskStorage = new ArrayList<>();
+	}
+
+	/**
+	 * Add a new task.
+	 * 
+	 * @param s Task to be added.
+	 */
+	public void addTask(String s) {
+		this.taskStorage.add(s);
+		prettyPrint("added: " + s);
+	}
+
+	/**
+	 * Prints all the tasks currently stored in Duke, prefixed with an integer
+	 * index.
+	 */
+	public void viewAllTask() {
+		int taskStorageSize = this.taskStorage.size();
+		// String in java are immutable and leads to O(n^2) time complexity
+		StringBuilder allTasks = new StringBuilder();
+		for (int i = 0; i < taskStorageSize; i++) {
+			int index = i + 1;
+			String task = this.taskStorage.get(i);
+			allTasks.append(index + ": " + task + "\n");
 		}
-		sc.close();
-		prettyPrint(goodByeMsg);
+		prettyPrint(allTasks.toString());
 	}
 
 	/**
@@ -35,5 +53,26 @@ public class Duke {
 		System.out.println(
 				"    ____________________________________________________________");
 		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		String greetingMsg = "Hello! I'm Duke \nWhat can I do for you?";
+		String goodByeMsg = "Bye. Hope to see you again soon!";
+
+		Duke dk = new Duke();
+		Scanner sc = new Scanner(System.in);
+		prettyPrint(greetingMsg);
+		while (true) {
+			String usrInput = sc.nextLine();
+			if (usrInput.equals("bye")) {
+				break;
+			} else if (usrInput.equals("list")) {
+				dk.viewAllTask();
+			} else {
+				dk.addTask(usrInput);
+			}
+		}
+		sc.close();
+		prettyPrint(goodByeMsg);
 	}
 }
