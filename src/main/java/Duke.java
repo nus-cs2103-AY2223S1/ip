f1169c22e6eb0aec328856c0c96a314b8341ad69
jonasgwt <55698556+jonasgwt@users.noreply.github.com>
2022-08-16 11:35:03 +0800

    Added Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static final ArrayList<Task> data = new ArrayList<>();
+    private enum Commands {list, mark, unmark, todo, event, deadline, delete};
 
     // lists all the tasks
     public static String listData() {
@@ -24,7 +25,7 @@ public class Duke {
     }
 
     // marks task as complete or not complete
-    public static String markTask(String input, boolean isComplete) throws DukeException{
+    public static String markTask(String input, boolean isComplete) throws DukeException {
         String res = input.substring(isComplete ? 4 : 6).trim();
         Task task = data.get(getIndex(res));
         if (isComplete) task.markDone();
@@ -33,56 +34,74 @@ public class Duke {
                 + task;
     }
 
-    public static String reply(String input) throws DukeException{
-        // shows user list of all saved tasks
-        if (input.equals("list")) return listData();
-        // mark task as complete
-        if (input.startsWith("mark")) return markTask(input, true);
-        // mark task as not done
-        if (input.startsWith("unmark")) return markTask(input, false);
-        // add todo
-        if (input.startsWith("todo")) {
-            String description = input.substring(4).trim();
-            if (description.length() == 0) throw new DukeException("The description of a todo cannot be empty.");
-            ToDo task = new ToDo(description);
-            data.add(task);
-            return "Got it. I've added this task:\n" + task + "\nNow you have " + data.size() + " tasks.";
-        }
-        // add event
-        if (input.startsWith("event")) {
-            String[] info = input.substring(5).split("/at");
-            if (info.length != 2) throw new DukeException("Invalid Input!");
-            String description = info[0].strip();
-            String at = info[1].strip();
-            if (description.length() == 0 || at.length() == 0)
-                throw new DukeException("The description of event cannot be empty.");
-            Event task = new Event(description, at);
-            data.add(task);
-            return "Got it. I've added this task:\n" + task + "\nNow you have " + data.size() + " tasks.";
-        }
-        // add deadline
-        if (input.startsWith("deadline")) {
-            String[] info = input.substring(8).split("/by");
-            if (info.length != 2) throw new DukeException("Invalid Input!");
-            String description = info[0].strip();
-            String by = info[1].strip();
-            if (description.length() == 0 || by.length() == 0)
-                throw new DukeException("The description of deadline cannot be empty.");
-            Deadline task = new Deadline(description, by);
-            data.add(task);
-            return "Got it. I've added this task:\n" + task + "\nNow you have " + data.size() + " tasks.";
-        }
-        // delete a task
-        if (input.startsWith("delete")) {
-            String res = input.substring(6).trim();
-            int target_index = getIndex(res);
-            Task task = data.get(target_index);
-            data.remove(target_index);
-            return "Noted. I've removed this task:\n" + task + "\nNow you have " + data.size() + " tasks.";
-        }
-        // else unknown command throw an error
-        else {
+    // create a todo task
+    public static String createTodo(String input) throws DukeException{
+        String description = input.substring(4).trim();
+        if (description.length() == 0) throw new DukeException("The description of a todo cannot be empty.");
+        ToDo task = new ToDo(description);
+        data.add(task);
+        return "Got it. I've added this task:\n" + task + "\nNow you have " + data.size() + " tasks.";
+    }
+
+    // create event task
+    public static String createEvent(String input) throws DukeException{
+        String[] info = input.substring(5).split("/at");
+        if (info.length != 2) throw new DukeException("Invalid Input!");
+        String description = info[0].strip();
+        String at = info[1].strip();
+        if (description.length() == 0 || at.length() == 0)
+            throw new DukeException("The description of event cannot be empty.");
+        Event task = new Event(description, at);
+        data.add(task);
+        return "Got it. I've added this task:\n" + task + "\nNow you have " + data.size() + " tasks.";
+    }
+
+    // create deadline task
+    public static String createDeadline(String input) throws DukeException {
+        String[] info = input.substring(8).split("/by");
+        if (info.length != 2) throw new DukeException("Invalid Input!");
+        String description = info[0].strip();
+        String by = info[1].strip();
+        if (description.length() == 0 || by.length() == 0)
+            throw new DukeException("The description of deadline cannot be empty.");
+        Deadline task = new Deadline(description, by);
+        data.add(task);
+        return "Got it. I've added this task:\n" + task + "\nNow you have " + data.size() + " tasks.";
+    }
+
+    // delete a specified task
+    public static String deleteTask(String input) throws DukeException {
+        String res = input.substring(6).trim();
+        int target_index = getIndex(res);
+        Task task = data.get(target_index);
+        data.remove(target_index);
+        return "Noted. I've removed this task:\n" + task + "\nNow you have " + data.size() + " tasks.";
+    }
+
+    public static String reply(String input) throws DukeException {
+        Commands command;
+        try {
+            command = Commands.valueOf(input.split(" ")[0]);
+        } catch (IllegalArgumentException e) {
             throw new DukeException("I'm sorry, but I don't know what that means :-(");
+        }
+        switch (command) {
+            case list:
+                return listData();
+            case mark:
+                return markTask(input, true);
+            case unmark:
+                return markTask(input, false);
+            case todo:
+                return createTodo(input);
+            case event:
+                return createEvent(input);
+            case deadline:
+                return createDeadline(input);
+            case delete:
+                return deleteTask(input);
+            default:
+                throw new DukeException("I'm sorry, but I don't know what that means :-(");
         }
     }
 
@@ -94,6 +113,7 @@ public class Duke {
             System.out.println("Duck:");
             String input = sc.nextLine();
             if (input.equals("bye")) {
+                sc.close();
                 System.out.println("Quack! Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 return;
