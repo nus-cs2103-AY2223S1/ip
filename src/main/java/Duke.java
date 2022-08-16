6bac1bde6c1fcf549cfa90c14b61a9237ec0aa0c
Derrick Khoo <derrickkhoo2000@gmail.com>
2022-08-16 23:41:21 +0800

    added Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,47 +1,70 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    static void line() {
+    ArrayList<String> list;
+    private void line() {
         System.out.println("________________________________________");
     }
-    static void greet() {
+    private void greet() {
         line();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         line();
     }
 
-    public static void echo(String message) {
+    private void echo(String message) {
         line();
         System.out.println(message);
         line();
     }
 
-    public static void exit() {
+    private void store(String input) {
+        list.add(input);
+        line();
+        System.out.println("added: " + input);
+        line();
+    }
+
+    private void enumerateArrayList(){
+        line();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(i+1 + ". " + list.get(i));
+        }
+        line();
+    }
+
+    private void exit() {
         line();
         System.out.println("Bye. Hope to see you again soon!");
         line();
     }
     public static void main(String[] args) {
+        Duke duke = new Duke();
+        boolean isDone = false;
+        duke.list = new ArrayList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        boolean isDone = false;
-        greet();
+        duke.greet();
         while (!isDone) {
             Scanner scanner = new Scanner(System.in); // creating scanner for user input
             String input = scanner.nextLine();
             switch (input) {
                 case ("bye"): {
-                    exit();
+                    duke.exit();
                     isDone = true;
                     break;
                 }
+                case("list"): {
+                    duke.enumerateArrayList();
+                    break;
+                }
                 default: {
-                    echo(input);
+                    duke.store(input);
                     break;
                 }
             }
