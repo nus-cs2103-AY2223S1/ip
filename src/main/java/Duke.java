a633b1386692675b8c5f0c6a41cb3ea3d4e7e6da
LianGuoYang <e0735382@u.nus.edu>
2022-08-16 15:15:40 +0800

    Implemented Level-1. ADD added Greet, Echo and Exit feature for Duke bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void greet () {
+        String str = "Hello! I'm Duke\n" +
+                "What can I do for you?\n";
+        System.out.println(str);
+    }
+
+    private static void echo () {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        if (str.equals("bye")) {
+            exit();
+        } else {
+            System.out.println(str);
+            echo();
+        }
+    }
+
+    private static void exit() {
+        String str = "Bye. Hope to see you again soon!";
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        greet();
+        echo();
     }
 }
