97156f268b2360a1299a0c2cb818b4e12ae8258d
Rachel Angelyn Gunawan <rachelag777@gmail.com>
2022-08-16 20:55:20 +0800

    implemented greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.printMessage("Hello! I'm Duke \nWhat can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        while(true) {
+            input = sc.nextLine();
+            if (input.equals("bye")) break;
+            duke.printMessage(input);
+        }
+        duke.printMessage("Bye. Hope to see you again soon!");
+    }
+
+    public void printMessage(String message) {
+        System.out.println(outlineMessage(message));
+    }
+
+    private String outlineMessage(String message) {
+        String line = "____________________________________________________________";
+        String res = line + "\n" + message + "\n" +line;
+        return res.replaceAll("(?m)^", "\t");
     }
 }
