53d9839a44e8ca5f18bbbdea7eb80db0ae0de9d8
RachelChua <e0772465@u.nus.edu>
2022-08-16 11:14:12 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,43 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    static ArrayList<Task> listOfTask = new ArrayList<>();
+     static void showList(){
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 1; i <= listOfTask.size(); i++) {
+            System.out.println(i + "." + listOfTask.get(i - 1).getMarkStatus());
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
         Scanner input = new Scanner(System.in);
-      ArrayList<Task> listOfTask = new ArrayList<Task>();
-        while (input.hasNextLine()) {
+        while (input.hasNext()) {
             String s = input.nextLine();
             if (s.equals("bye")) {
                 break;
             } else if (s.equals("list")) {
-                    for (int i = 1; i <= listOfTask.size(); i++) {
-                        System.out.println(i + "." + listOfTask.get(i-1).getName());
-                    }
-           } else {
+                showList();
+            } else if (s.length() > 5 && s.substring(0, 4).equals("mark")) {
+                int i = Integer.parseInt(s.substring(5, 6)) - 1;
+                Task task =  listOfTask.get(i);
+                task.mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task.getMarkStatus());
+            } else if (s.length() > 7 && s.substring(0, 6).equals("unmark")) {
+                int i = Integer.parseInt(s.substring(7, 8)) - 1;
+                Task task = listOfTask.get(i);
+                task.unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(task.getMarkStatus());
+            } else {
                 listOfTask.add(new Task(s));
                 System.out.println("added: " + s);
             }
         }
-       System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,26 @@
 public class Task {
-    private String name;
+    protected String name;
+    protected boolean isDone;
     Task(String name){
         this.name = name;
+        this.isDone = false;
     }
-    String getName() {
+  /*  String getName() {
         return this.name;
+    }
+   */
+    public String getStatusIcon() {
+       return(this.isDone? "[X] " : "[] " );
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+    public void unmark() {
+        this.isDone = false;
+    }
+    public String getMarkStatus() {
+        return this.getStatusIcon() + "" + this.name;
     }
 
 }
