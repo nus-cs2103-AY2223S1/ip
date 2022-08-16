ee08aaf8dda7b17b04fd575aeb059196a1f2d40b
Shawn Kok <shawnkok00@gmail.com>
2022-08-16 16:38:57 +0800

    Leve 3. Mark as Done

---
+++ src/main/java/Duke/Task.java
@@ -0,0 +1,28 @@
+package Duke;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
