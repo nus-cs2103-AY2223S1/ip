a2e417b3775171cc24f92f78c99fef16bf9e5937
bryanhce <ho.cheng.en.bryan@gmail.com>
2022-08-16 11:53:20 +0800

    level 1 greet echo exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalLine = "______________________________________ \n";
+
+        String firstText = horizontalLine
+                + "hi... I'm Karen \n"
+                + "What do you want this time? \n"
+                + horizontalLine;
+        System.out.println(firstText);
+
+        Scanner sc = new Scanner(System.in);
+        String echoText = sc.nextLine();
+        while (!echoText.equals("bye")) {
+            System.out.println(horizontalLine + echoText + "\n" + horizontalLine);
+            echoText = sc.nextLine();
+        }
+        System.out.println("K finally, good riddance!");
     }
 }
