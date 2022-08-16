48e4d08ca4735a96e15590435b9c8289c7aa70bb
bryanljx1 <bryanljx1@gmail.com>
2022-08-16 22:53:40 +0800

    Set up skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String formatLine(String input) {
+        return "\t " + input + "\n";
+    }
+
+    private static String formatParagraph(String paragraph) {
+        String divider = "\t____________________________________________________________\n";
+        return divider + paragraph + divider;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String introParagraph = formatLine("Hello! I'm Duke") + formatLine("What can I do for you?");
+        String formattedIntro = formatParagraph(introParagraph);
+
+        System.out.println(formattedIntro);
+
+        Scanner sc = new Scanner(System.in);
+        boolean terminate = false;
+
+        while(!terminate) {
+            String nextLine = sc.nextLine();
+            switch (nextLine) {
+                case "bye":
+                    System.out.println(formatParagraph(formatLine("Bye. Hope to see you again soon!")));
+                    terminate = true;
+                    sc.close();
+                    break;
+                default:
+                    System.out.println(formatParagraph(formatLine(nextLine)));
+            }
+        }
     }
 }
