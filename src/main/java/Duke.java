bd8b5b1afb8b51adfab0929d28f48059137f7a30
lingg02 <89027464+lingg02@users.noreply.github.com>
2022-08-16 10:03:02 +0800

    Implemented increment for level 6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Duke {
 
@@ -11,7 +13,8 @@ public class Duke {
                 line
         );
         String input = "";
-        Task[] arr = new Task[100];
+        ArrayList<Task> arr = new ArrayList<Task>();
+        //Task[] arr = new Task[100];
         int index = 0;
         while (!input.equals("bye")) {
             Scanner scan = new Scanner(System.in);
@@ -23,22 +26,22 @@ public class Duke {
             } else if (input.startsWith("mark")){
                 if (input.length() > 5) {
                     int taskNum = Integer.parseInt(input.substring(5));
-                    arr[taskNum - 1].makeDone();
+                    arr.get(taskNum - 1).makeDone();
                     System.out.println(line +
                             "Nice! I've marked this task as done: \n" +
-                            arr[taskNum - 1].getStatusIcon() +
-                            arr[taskNum - 1].description + "\n" + line);
+                            arr.get(taskNum - 1).getStatusIcon() +
+                            arr.get(taskNum - 1).description + "\n" + line);
                 } else {
                     throw new DukeException("☹ OOPS!!! Please include the index of the task you'd like to mark as completed!");
                 }
             } else if (input.startsWith("unmark")){
                 if (input.length() > 7) {
                     int taskNum = Integer.parseInt(input.substring(7));
-                    arr[taskNum - 1].makeUndone();
+                    arr.get(taskNum - 1).makeUndone();
                     System.out.println(line +
                             "OK, I've marked this task as not done yet: \n" +
-                            "[" + arr[taskNum - 1].getStatusIcon() + "] " +
-                            arr[taskNum - 1].description + "\n" + line);
+                            arr.get(taskNum - 1).getStatusIcon() +
+                            arr.get(taskNum - 1).description + "\n" + line);
                 } else {
                     throw new DukeException("☹ OOPS!!! Please include the index of the task you'd like to mark as incomplete!");
                 }
@@ -47,14 +50,14 @@ public class Duke {
                 for (int i = 0; i < index; i++) {
                     list += i + 1;
                     list += ". ";
-                    list += arr[i].toString();
+                    list += arr.get(i).toString();
                     list += "\n";
                 }
                 System.out.println(list + line);
             } else if (input.startsWith("todo")){
                 if (input.length() > 5) {
                     Todo todo = new Todo(input);
-                    arr[index] = todo;
+                    arr.add(todo);
                     index++;
                     System.out.println(todo.addString(index));
                 } else {
@@ -64,7 +67,7 @@ public class Duke {
                 if (input.length() > 9) {
                     String[] dead = input.split(" /by ");
                     Deadlines deadlines = new Deadlines(dead[0], dead[1]);
-                    arr[index] = deadlines;
+                    arr.add(deadlines);
                     index++;
                     System.out.println(deadlines.addString(index));
                 } else {
@@ -74,13 +77,26 @@ public class Duke {
                 if (input.length() > 6) {
                     String[] time = input.split(" /at ");
                     Event event = new Event(time[0], time[1]);
-                    arr[index] = event;
+                    arr.add(event);
                     index++;
                     System.out.println(event.addString(index));
                 } else {
                     throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
                 }
-            } else {
+            } else if (input.startsWith("delete")) {
+                if (input.length() > 7) {
+                    int taskNum = Integer.parseInt(input.substring(7));
+                    Task toDelete = arr.get(taskNum - 1);
+                    arr.remove(taskNum - 1);
+                    index --;
+                    System.out.println(line + "Noted. I've removed this task: \n" +
+                            toDelete.toString() + "\n" +
+                            "Now you have " + index + " tasks in the list. \n" + line);
+                } else {
+                    throw new DukeException("☹ OOPS!!! Please include the index of the task you'd like to delete!");
+                }
+            }
+            else {
 //                Task task = new Task(input);
 //                arr[index] = task;
 //                index ++;
