80f48c7795fa3ab6855b030705da2c69517f8798
shogun187 <53176307+shogun187@users.noreply.github.com>
2022-08-16 17:43:38 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,15 +5,22 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
-
-
-        while (true) {
+        int counter = 0;
+        String[] list = new String[100];
+        while (counter < 100) {
             String str = sc.nextLine();
             if (str.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(str);
+            if (str.equals("list")) {
+                for (int x = 0; x < counter; x++) {
+                    System.out.println((x+1) + ". " + list[x]);
+                }
+                continue;
+            }
+            list[counter] = str;
+            counter++;
         }
     }
 }
