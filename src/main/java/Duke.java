14dc0e168c3195b42b595869b65555f2ab0cd027
Benjamin-Sim <benjamin.sim@u.nus.edu>
2022-08-16 22:53:17 +0800

    Add list and add text functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,43 +1,107 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
 
     private static final String EXIT_COMMAND = "bye";
+    private static final String LIST_COMMAND = "list";
 
-    public static void processInputs() {
 
-        Scanner sc = new Scanner(System.in);
+    // Linked list to store the user's strings
+    private LinkedList<String> storedStrings;
 
-        while (true) {
 
-            String input = sc.nextLine();            
+    // Constructor
+    public Duke() {
+        storedStrings = new LinkedList<>();
+    }
 
-            // Check for exit for command
-            if (input.equals(EXIT_COMMAND)) {
-                sc.close();
-                System.out.println("Bye. Hope to see you again soon!");
-                return;
-            }
 
-            System.out.println(input);
-            System.out.println();
+    public void listStrings() {
+        String result = "";
+
+        for (int i = 0; i < this.storedStrings.size(); i++) {
+            String line = String.format("%d. %s\n", i, this.storedStrings.get(i));
+            result = result.concat(line);
+        }
+
+        System.out.println(result);
+    }
+
+
+    public void addString(String s) {
+        this.storedStrings.add(s);
+
+        String result = String.format("added: %s\n", s);
+        System.out.println(result);
+    }
+
+
+    public void exitDuke() {
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+
+    // Calls the relevant function based on the given command
+    // Return true if need to exit program
+    public boolean executeCommand(String command) {
+
+        switch (command) {
+
+        case LIST_COMMAND:
+            listStrings();
+            return false;        
+
+        case EXIT_COMMAND:
+            exitDuke();
+            return true;
+            
+        default:
+            addString(command);
+            return false;
         }
     }
 
 
-
-    public static void main(String[] args) {
+    private void initialize() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?\n");
+    }
 
-        System.out.println("What can I do for you?");
 
-        // Process user inputs
-        processInputs();
+    public void processCommands() {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            String command = sc.nextLine();
+
+            // If need to exit
+            if (executeCommand(command)) {
+                sc.close();
+                return;
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        
+        Duke d = new Duke();
+
+        // Print the logo
+        d.initialize();
+
+        // Process user commands
+        d.processCommands();
+
 
     }
 }
