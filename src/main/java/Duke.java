2940803825bfa3f96a7f5f831bcd9baffc3ebc1f
bryanhce <ho.cheng.en.bryan@gmail.com>
2022-08-16 13:39:21 +0800

    level 3 mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,12 +12,12 @@ public class Duke {
                 + horizontalLine;
         System.out.println(firstText);
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            String echoText = sc.nextLine();
+            String echoText = sc.next();
             if (echoText.equals("bye")) {
                 System.out.println(horizontalLine + "K finally, good riddance! \n" + horizontalLine);
                 break;
@@ -26,14 +26,30 @@ public class Duke {
                 if (list.size() == 0) {
                     System.out.println("pff there is nothing in your list");
                 } else {
+                    System.out.println("Here are your dumb tasks in your list:");
                     for (int i = 1; i < list.size() + 1; i++) {
-                        System.out.println(i + ". " + list.get(i - 1));
+                        Task item = list.get(i - 1);
+                        String itemStr = i + ". [" + item.getStatusIcon() + "] " + item.getDescription();
+                        System.out.println(itemStr);
                     }
                 }
                 System.out.println(horizontalLine);
+            } else if (echoText.equals("mark")) {
+                int markValue = sc.nextInt();
+                Task item = list.get(markValue - 1);
+                item.markAs(true);
+                System.out.println(horizontalLine + "Took you long enough to complete this task:");
+                System.out.println("[X] " + item.getDescription() + "\n" + horizontalLine);
+            } else if (echoText.equals("unmark")) {
+                int markValue = sc.nextInt();
+                Task item = list.get(markValue - 1);
+                item.markAs(false);
+                System.out.println(horizontalLine + "Another task marked as not done?? Slow indeed");
+                System.out.println("[ ] " + item.getDescription() + "\n" + horizontalLine);
             } else {
+                Task t = new Task(echoText);
+                list.add(t);
                 System.out.println(horizontalLine + adddedStr + echoText + "\n" + horizontalLine);
-                list.add(echoText);
             }
 
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String desc) {
+        this.description = desc;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAs(boolean bool) {
+        this.isDone = bool;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+}
