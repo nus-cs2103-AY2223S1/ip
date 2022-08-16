a072ad9ca48da5e77d1e8ce5e8255fd92fffcde4
Edfernape <edwardymualimy@gmail.com>
2022-08-17 03:08:51 +0800

    Implemented Level-1.

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public abstract class Chatbot {
     public abstract void echo(String message);
     public abstract void sayHello();
     public abstract void sayGoodbye();
-
-    public abstract String listen(Scanner scanner);
+    public abstract String listen();
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Duke extends Chatbot {
+    private static final String GREETING = "Hi friend! How may I help you?";
+    private static final String FAREWELL = "See you soon, friend!";
+    private static final String TRIGGER = "bye";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.sayHello();
+
+        while(true) {
+            String action = duke.listen();
+
+            if (action.equals(Duke.TRIGGER)) {
+                duke.sayGoodbye();
+                break;
+            } else {
+                duke.echo(action);
+            }
+        }
+    }
+
+    @Override
+    public void echo(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public void sayHello() {
+        System.out.println(Duke.GREETING);
+    }
+
+    @Override
+    public void sayGoodbye() {
+        System.out.println(Duke.FAREWELL);
+    }
+
+    @Override
+    public String listen() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        return userInput;
     }
 }
