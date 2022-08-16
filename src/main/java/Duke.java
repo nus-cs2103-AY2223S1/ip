37cafb1495ca71295eb39092fb971fba00685fe4
jsincorporated <e0725210@u.nus.edu>
2022-08-16 20:40:51 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
 
         ArrayList<Task> myList = new ArrayList<>();
         boolean bye = false;
@@ -22,18 +23,34 @@ public class Duke {
             } else if (input.startsWith("unmark")) {
                 int taskIndex = Integer.parseInt(input.substring(7)) - 1;
                 EventHandler.unmarkTask(taskIndex, myList);
-            }
-            else { // Add new task
-                if (input.startsWith("todo")) {
+            } else if (input.startsWith("todo")) {
+                if (input.length() > 4) {
                     EventHandler.addTodo(input, myList);
+                } else {
+                    System.out.println("The description of a todo cannot be empty");
+                    throw new DukeException("The description of a todo cannot be empty.");
                 }
-                if (input.startsWith("deadline")) {
+            } else if (input.startsWith("deadline")) {
+                if (input.length() > 8) {
                     EventHandler.addDeadline(input, myList);
+                } else {
+                    System.out.println("The description of a deadline cannot be empty");
+                    throw new DukeException("The description of a deadline cannot be empty.");
                 }
-                if (input.startsWith("event")) {
+            } else if (input.startsWith("event")) {
+                if (input.length() > 5) {
                     EventHandler.addEvent(input, myList);
+                } else {
+                    System.out.println("The description of an event cannot be empty");
+                    throw new DukeException("The description of an event cannot be empty.");
                 }
+            } else {
+                System.out.println("I'm sorry, but I don't know what that means.");
+                throw new DukeException("Unknown command");
             }
+
         }
+
+        scanner.close();
     }
 }
--- src/main/java/EventHandler.java
+++ src/main/java/EventHandler.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class EventHandler {
