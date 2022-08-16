b43e3e85dc7f317d826615c9f38554cb86ba8dbb
sikai00 <sikai4111@gmail.com>
2022-08-16 17:45:22 +0800

    Duke.java: Add pretty print method to format all messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+	public static void main(String[] args) {
+		String greetingMsg = "Hello! I'm Duke \nWhat can I do for you?";
+		prettyPrint(greetingMsg);
+		String goodByeMsg = "Bye. Hope to see you again soon!";
+		prettyPrint(goodByeMsg);
+	}
+
+	/**
+	 * Prints the given message with appropriate indentations and horizontal
+	 * lines.
+	 * 
+	 * @param s Message to be printed.
+	 */
+	private static void prettyPrint(String s) {
+		// Horizontal lines have 4 spaces as indentation
+		System.out.println(
+				"    ____________________________________________________________");
+		String[] msgTokens = s.split("\n");
+		for (String token : msgTokens) {
+			// Message has 5 spaces as indentation
+			System.out.println("     " + token);
+		}
+		System.out.println(
+				"    ____________________________________________________________");
+		System.out.println();
+	}
 }
