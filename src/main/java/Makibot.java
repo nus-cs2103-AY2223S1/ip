5108a7779f16a3688b29e8899b8176bef5aea2b1
Justin Peng <justinpeng13@gmail.com>
2022-08-17 02:28:43 +0800

    Added taskList to store tasks and list function

--- src/main/java/Makibot.java
+++ src/main/java/Makibot.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * MakiBot
  */
 public class Makibot {
+    ArrayList<String> taskList = new ArrayList<String>();
+
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -21,13 +24,28 @@ public class Makibot {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Makibot\n" +
                 "What can I do for you?");
+
+        // Event loop
         while (true) {
-            String input = sc.next();
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                return;
+            String input = sc.nextLine();
+
+            // Handle special commands
+            switch (input) {
+                // Exit
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    return;
+                // List
+                case "list":
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println(i+1 + ". " + taskList.get(i));
+                    }
+                    break;
+                default:
+                    taskList.add(input);
+                    System.out.println("added: " + input);
             }
-            System.out.println(input);
+
         }
     }
 }
