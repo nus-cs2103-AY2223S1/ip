7d081d865e8ee7f940cf2a6f32295290a4fa847e
comicalromance <silasyeoshuenyu@gmail.com>
2022-08-17 03:16:08 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
     private static String greetings = "Hello! I'm Ekud \n" + "What can I do for you?";
     private static String banner = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    private static ArrayList<String> tasks = new ArrayList<>();
 
     private static void print(String msg) {
         System.out.println(banner);
@@ -15,11 +17,26 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         print(greetings);
 
-        String input = sc.nextLine();
-        while(!input.equals("bye")) {
-            print(input);
-            input = sc.nextLine();
+        while(true) {
+            String input = sc.nextLine();
+            switch (input) {
+                case "bye":
+                    print("Cya!");
+                    return;
+
+                case "list":
+                    String list = "";
+                    for (int i = 0; i < tasks.size(); i++) {
+                        list += (i + 1) + ". " + tasks.get(i);
+                        if (i != tasks.size() - 1) list += "\n";
+                    }
+                    print(list);
+                    break;
+
+                default:
+                    print("added: " + input);
+                    tasks.add(input);
+            }
         }
-        print("Cya!");
     }
 }
