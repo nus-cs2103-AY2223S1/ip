d515fcc27ec286db90af3b8f8eab0c7fe3b106db
sltsheryl <sltsheryl@gmail.com>
2022-08-17 00:29:49 +0800

    Add methods

--- src/main/java/Kirby.java
+++ src/main/java/Kirby.java
@@ -10,10 +10,65 @@ public class Kirby {
             System.out.println("Now you have " + taskCount + " task in the bag!");
         }
     }
+
+    public static void Goodbye() {
+        System.out.println("I loved talking to you :> \n" +
+                "Hope to see you again!");
+    }
+
+    public static void ShowList(ArrayList<Task> Tasks) {
+        System.out.println("Here is your bag of fabulous tasks: ");
+        for (int i = 0; i < Tasks.size(); i++) {
+            Task currTask = Tasks.get(i);
+            System.out.println(i + 1 + ": " + currTask.toString());
+        }
+    }
+
+    public static void Mark(String inputString, ArrayList<Task> Tasks) {
+        int taskIndex = Integer.parseInt(inputString.split(" ")[1]);
+        Task currTask = Tasks.get(taskIndex - 1);
+        currTask.setCompleted();
+        System.out.println("Awesome :D I've marked " + currTask.toString() + " completed!");
+    }
+
+    public static void Unmark(String inputString, ArrayList<Task> Tasks) {
+        int taskIndex = Integer.parseInt(inputString.split(" ")[1]);
+        Task currTask = Tasks.get(taskIndex - 1);
+        currTask.setIncomplete();
+        System.out.println("Okay, I've marked " + currTask.toString() + " pending!");
+    }
+
+    public static void Todo(String inputString, ArrayList<Task> Tasks) {
+        // rest of the string
+        String taskName = inputString.substring(inputString.indexOf(' ') + 1);
+        Todo todo = new Todo(taskName);
+        Tasks.add(todo);
+        System.out.println("Added into your bag of fabulous tasks: " + todo.toString());
+        printTaskCount();
+    }
+
+    public static void Deadline(String inputString, ArrayList<Task> Tasks) {
+        String taskName = inputString.substring(inputString.indexOf("deadline") + 9, inputString.indexOf("/by"));
+        String by = inputString.substring(inputString.indexOf("/by") + 4);
+        Deadline deadline = new Deadline(taskName, by);
+        Tasks.add(deadline);
+        System.out.println("Added into your bag of fabulous tasks: " + deadline.toString());
+        printTaskCount();
+    }
+
+    public static void Event(String inputString, ArrayList<Task> Tasks) {
+        String taskName = inputString.substring(inputString.indexOf("event") + 6, inputString.indexOf("/at"));
+        String at = inputString.substring(inputString.indexOf("/at") + 4);
+        Event event = new Event(taskName, at);
+        Tasks.add(event);
+        System.out.println("Added into your bag of fabulous tasks: " + event.toString());
+        printTaskCount();
+    }
+
     public static void main(String[] args) {
 
         ArrayList<Task> Tasks = new ArrayList<>();
-        System.out.println("Kirby: Hai I'm Kirby (੭｡╹▿╹｡)੭ your friendly chat assistant!! \n" +
+        System.out.println("Hai I'm Kirby (੭｡╹▿╹｡)੭ your friendly chat assistant!! \n" +
                 "What amazing plans do you have today?");
 
         while (true){
@@ -22,68 +77,41 @@ public class Kirby {
 
             // bye
             if (inputString.equals("bye")) {
-                System.out.println("I loved talking to you :> \n" +
-                        "Hope to see you again!");
+                Goodbye();
                 break;
             }
 
             // list
             else if (inputString.equals("list")) {
-                System.out.println("Here is your bag of fabulous tasks: ");
-                for (int i = 0; i < Tasks.size(); i++) {
-                    Task currTask = Tasks.get(i);
-                    System.out.println(i + 1 + ": " + currTask.toString());
-                }
+                ShowList(Tasks);
             }
 
             //mark
             else if (inputString.split(" ")[0].equals("mark")) {
-                int taskIndex = Integer.parseInt(inputString.split(" ")[1]);
-                Task currTask = Tasks.get(taskIndex - 1);
-                currTask.setCompleted();
-                System.out.println("Awesome :D I've marked " + currTask.toString() + " completed!");
+                Mark(inputString, Tasks);
             }
 
             // unmark
             else if (inputString.split(" ")[0].equals("unmark")) {
-                int taskIndex = Integer.parseInt(inputString.split(" ")[1]);
-                Task currTask = Tasks.get(taskIndex - 1);
-                currTask.setIncomplete();
-                System.out.println("Okay, I've marked " + currTask.toString() + " pending!");
+                Unmark(inputString, Tasks);
             }
 
             // add task
             else {
                 // todo
                 if (inputString.split(" ")[0].equals("todo")) {
-                    // rest of the string
-                    String taskName = inputString.substring(inputString.indexOf(' ') + 1);
-                    Todo todo = new Todo(taskName);
-                    Tasks.add(todo);
-                    System.out.println("Added into your bag of fabulous tasks: " + todo.toString());
-                    printTaskCount();
+                    Todo(inputString, Tasks);
                 }
 
                 // deadline
                 if (inputString.split(" ")[0].equals("deadline")) {
-                    String taskName = inputString.substring(inputString.indexOf("deadline") + 9, inputString.indexOf("/by"));
-                    String by = inputString.substring(inputString.indexOf("/by") + 4);
-                    Deadline deadline = new Deadline(taskName, by);
-                    Tasks.add(deadline);
-                    System.out.println("Added into your bag of fabulous tasks: " + deadline.toString());
-                    printTaskCount();
+                    Deadline(inputString, Tasks);
                 }
 
                 // event
                 if (inputString.split(" ")[0].equals("event")) {
-                    String taskName = inputString.substring(inputString.indexOf("event") + 6, inputString.indexOf("/at"));
-                    String at = inputString.substring(inputString.indexOf("/at") + 4);
-                    Event event = new Event(taskName, at);
-                    Tasks.add(event);
-                    System.out.println("Added into your bag of fabulous tasks: " + event.toString());
-                    printTaskCount();
+                    Event(inputString, Tasks);
                 }
-
             }
         }
     }
