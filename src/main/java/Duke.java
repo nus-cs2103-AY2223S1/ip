c2d580d56ab4ddc973b37d99158c89295621a11b
Benjamin-Sim <benjamin.sim@u.nus.edu>
2022-08-16 22:26:18 +0800

    Add greet, echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String EXIT_COMMAND = "bye";
+
+    public static void processInputs() {
+
+        while (true) {
+
+            Scanner sc = new Scanner(System.in);
+
+            String input = sc.nextLine();
+
+            // Check for exit for command
+            if (input.equals(EXIT_COMMAND)) {
+                sc.close();
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            }
+
+            System.out.println(input);
+            System.out.println();
+        }
+    }
+
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +33,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("What can I do for you?");
+
+        // Process user inputs
+        processInputs();
+
     }
 }
