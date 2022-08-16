3579937ceaacdbd5cd97687a2fad3d4165c69130
sarrrdin <sarrrdin@gmail.com>
2022-08-16 15:17:46 +0800

    Added some JavaDocs, improvised Exception handling

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -2,6 +2,9 @@
  * Project done by Hong Jin.
  */
 
+/**
+ * DukeException class to handle exceptions thrown.
+ */
 public class DukeException extends Exception {
     public DukeException(String message) {
         super(message);
