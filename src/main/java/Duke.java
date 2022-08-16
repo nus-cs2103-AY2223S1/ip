ceba2c56dd77a9c613c9bdcbba5c029df181f635
Daofu <xu.daofu20@gmail.com>
2022-08-16 16:15:44 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
-import java.util.Scanner; //import Scanner class
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     private static boolean END;
+    //storing all user inputs
+    private static ArrayList<String> storage;
     /*
     Decides on what the chatbox will response with based on user input.
      */
@@ -8,13 +11,21 @@ public class Duke {
         if (input.equals("bye")) {
             END = true;
             System.out.println("Bye. Hope to see you again soon!");
+        } else if (input.equals("list")) {
+            String output = "";
+            for (int i = 0; i < storage.size(); i ++) {
+                output = output + String.valueOf(i + 1) + ". " + storage.get(i) + "\n";
+            }
+            System.out.println(output);
         } else {
-            System.out.println(input);
+            storage.add(input);
+            System.out.println("added: " + input);
         }
     }
     public static void main(String[] args) {
         Duke duke = new Duke();
         END = false;
+        storage = new ArrayList<String>();
         Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         while (!END) {
