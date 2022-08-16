7a5c56450951135fe993922d9624729a198d9706
samuelcheongws <samuelcheong2000@gmail.com>
2022-08-16 19:44:47 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    private static final String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static final String GREETINGS = "Hello! I'm Duke\nWhat can I do for you?\n";
+
+    private static void echo(String str) {
+        System.out.println(str);
     }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from\n" + logo);
+        System.out.println(GREETINGS);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            echo(input);
+            input = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!\n");
+
+
+
+
+    }
+
 }
