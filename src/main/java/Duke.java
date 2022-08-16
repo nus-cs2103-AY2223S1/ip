016c04fc0d001a1481ebefaaf9bfe58f1bc5cf90
Joel Wong <>
2022-08-17 00:14:19 +0800

    add ability to save user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@
 import java.util.*;
 public class Duke {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,14 +10,32 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("----------------------\nHello! " +
-                "I'm HelperBot\nWhat can I do for you?\n----------------------\n");
+
+
+        System.out.println("----------------------\nHello! I'm HelperBot\nWhat can I do for you?\n----------------------\n");
         String userInput = sc.nextLine();
+        String listOfActions[] = new String[100];
+        int currentAction = 0;
         while (!userInput.equals("bye")) {
-            System.out.println("----------------------\n"
-                    + userInput + "\n----------------------\n");
+            //If user wants to check the list
+            String output = "";
+            for (int i = 0; i < currentAction; i++) {
+                output = output + listOfActions[i] + "\n";
+            }
+            if (userInput.equals("list")) {
+                System.out.println("----------------------\n" + output + "----------------------");
+            }
+            //If user wants to add to list
+            else {
+                listOfActions[currentAction] = (currentAction + ". " + userInput);
+                System.out.println("----------------------\nadded: " + userInput + "\n----------------------\n");
+                currentAction++;
+            }
             userInput = sc.nextLine();
         }
         System.out.println("Thank you and ATB :)");
+
     }
+
+
 }
