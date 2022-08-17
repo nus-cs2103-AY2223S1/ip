0324eb6c897bf1ef7a6aaba5dca758db7bb95a5c
nseah21 <nseah21@gmail.com>
2022-08-17 10:41:20 +0800

    Add scanner logic for echoing commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,23 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
+        String introduction = "\tHello! I'm Duke\n" + "\tWhat can I do for you?";
+        Duke.echo(logo + introduction);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                Duke.echo("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                Duke.echo(s);
+            }
+        }
     }
 
     private static void echo(String s) {
