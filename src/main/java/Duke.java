4257978beac627aef7e0e7cae5191fb389d9de7c
JonLamy <e0725013@u.nus.edu>
2022-08-17 19:24:35 +0800

    Add simple echo.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String lastLine = "";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("My name is [insert name here], how can I help?");
+        while (true) {
+            listen();
+            if (lastLine.equals("bye")) {
+                System.out.println("See you next time...");
+                break;
+            }
+            echoLast();
+        }
+    }
+
+    private static void listen() {
+        Scanner sc = new Scanner(System.in);
+        lastLine = sc.nextLine();
+    }
+
+    private static void echoLast() {
+        System.out.println(lastLine);
     }
 }
