7e7c89d7c88eee38ff9f0f5d337fc15fbba4cf8f
ngshijun <shijunng2010@gmail.com>
2022-08-18 02:34:37 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "\n" +
+        "   ██▓    ▄▄▄       ███▄ ▄███▓▓█████▄  ▄▄▄      \n" +
+        "  ▓██▒   ▒████▄    ▓██▒▀█▀ ██▒▒██▀ ██▌▒████▄    \n" +
+        "  ▒██░   ▒██  ▀█▄  ▓██    ▓██░░██   █▌▒██  ▀█▄  \n" +
+        "  ▒██░   ░██▄▄▄▄██ ▒██    ▒██ ░▓█▄   ▌░██▄▄▄▄██ \n" +
+        "  ░██████▒▓█   ▓██▒▒██▒   ░██▒░▒████▓  ▓█   ▓██▒\n" +
+        "  ░ ▒░▓  ░▒▒   ▓▒█░░ ▒░   ░  ░ ▒▒▓  ▒  ▒▒   ▓▒█░\n" +
+        "  ░ ░ ▒  ░ ▒   ▒▒ ░░  ░      ░ ░ ▒  ▒   ▒   ▒▒ ░\n" +
+        "    ░ ░    ░   ▒   ░      ░    ░ ░  ░   ░   ▒   \n" +
+        "      ░  ░     ░  ░       ░      ░          ░  ░\n" +
+        "                               ░                \n";
+        
+        System.out.print(logo);
+        System.out.println("Hi, I am LaMDA.\nHow may I assist you today?\n");
+        greeting();
+
     }
+    public static void greeting() {
+        Scanner scn = new Scanner(System.in);
+        String s = scn.nextLine();
+        System.out.println("\t____________________________________________");
+        if (!s.equals("bye")) {
+            System.out.println("\t " + s);
+            System.out.println("\t____________________________________________");
+            greeting();
+        }
+        else {
+            System.out.println("\t It's a great time talking with you.\n\t See you next time!");
+            System.out.println("\t____________________________________________");
+        }
+    }
+
 }
