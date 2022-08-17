9c24abe6bc45390d051f90060af10aad6d01fffb
pwjj2000 <perrywongjunjie@gmail.com>
2022-08-17 22:02:41 +0800

    minor amendments to greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,12 +8,7 @@
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String greeting = logo + "Hello! I'm Duke\n" + "What can I do for you?\n";
+        String greeting = "Hello! I'm Duke\n" + "What can I do for you?\n";
         System.out.println(greeting);
         List.run();
     }
