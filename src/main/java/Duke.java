64c96ede54e604b25a7aa5d2ff76f0f37d4b41d0
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2022-08-17 23:27:24 +0800

    feat: store and display input texts
    - Store texts entered by user and display them when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+
         String logo = "_________                     ___\n"
                 + "\\    ___ \\  ___________   ____\\_ |_________  ____\n"
                 + "/    \\  \\/_/ __ \\_  __ \\_/ __ \\| __ \\_  __ \\/  _ \\\n"
@@ -14,9 +17,17 @@ public class Duke {
         while (true) {
             System.out.print("--> ");
             String input = scanner.nextLine();
-            if (input.equals("bye"))
+
+            if (input.equals("bye")) {
                 break;
-            System.out.println(input);
+            } else if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println("Added: " + input);
+            }
         }
 
         System.out.println("Goodbye! See you soon!");
