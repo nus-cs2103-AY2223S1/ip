17e98d7c076190b8541327baa9ca742b05e98ed9
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-17 13:04:29 +0800

    Add duke exception class

---
+++ src/main/java/exception/DukeException.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class DukeException extends Exception {
+
+  /**
+   * Constructor for DukeException
+   * @param message message to be displayed to user when caught
+   */
+  public DukeException(String message) {
+    super(message);
+  }
+}
