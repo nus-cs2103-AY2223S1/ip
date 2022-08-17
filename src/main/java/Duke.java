b63fba9c1c7abb47a3663f4896e3e9a13261f1e6
turretDive <apangjun@gmail.com>
2022-08-18 01:10:18 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -11,7 +12,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What may I do for you?");
 
-        ArrayList<String> list = new ArrayList<>(); // list of user input
+        ArrayList<Task> list = new ArrayList<>(); // list of user input
         while (true) {
             Scanner input = new Scanner(System.in);  // Create a Scanner object
             String in = input.nextLine();  // Read user input
@@ -20,12 +21,27 @@ public class Duke {
                 break;
             } else if (in.equals("list")) {
                 int count = 1;
-                for (String tasks: list) {
-                    System.out.println(count + ". " + tasks);
+                for (Task tasks : list) {
+                    System.out.println(count + ". " + "[" + tasks.getStatusIcon() + "] " + tasks.getDescription());
                     count += 1;
                 }
+            } else if (in.contains("unmark")) {
+                char n = in.charAt(7);
+                int number = Character.getNumericValue(n) - 1;
+                Task t = list.get(number);
+                t.markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [" + t.getStatusIcon() + "] " + t.getDescription());
+            } else if (in.contains("mark")) {
+                char n = in.charAt(5);
+                int number = Character.getNumericValue(n) - 1;
+                Task t = list.get(number);
+                t.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + t.getStatusIcon() + "] " + t.getDescription());
             } else {
-                list.add(in);
+                Task t = new Task(in);
+                list.add(t);
                 System.out.println("added: " + in);  // Output user input
             }
         }
