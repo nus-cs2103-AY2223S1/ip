e38cbc2f84af6f3f503409ae60221bd89d400d79
shauneang <shauneangjx@gmail.com>
2022-08-17 12:38:35 +0800

    Duke echos commands entered by the user and exits when the user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        initDuke();
+    }
+
+    private static void initDuke() {
+            Scanner myScanner = new Scanner(System.in);
+            String exitWord = "bye";
+            String hLine = "\t____________________________________________";
+            String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+            System.out.println("Hello from\n" + logo);
+            System.out.println("What can i do for you?");
+
+            String  command = "";
+
+            while ( !command.equals(exitWord)) {
+                if (command.equals("")) {
+                    System.out.println(hLine);
+                    System.out.println("\tadded: " + command);
+                    System.out.println(hLine);
+                }
+
+                command = myScanner.nextLine();
+            }
+            System.out.println(hLine);
+            System.out.println("\tBye. Hope to see you again soon!");
+            System.out.println(hLine);
     }
 }
