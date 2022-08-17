5e760d906de39fcd55af82b8a2ed6e2494256ceb
dlimyy <douglas.lim.yy@gmail.com>
2022-08-17 11:27:57 +0800

    Level 1: Added the Greet, Echo and Exit features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Greet();
+        Echo();
+        GoodBye();
+    }
+
+    private static void Greet() {
+        System.out.println("Hello, this is Siri! It is a pleasure to meet you!");
+        System.out.println("How may I assist you?");
+        System.out.println("##############################################");
+    }
+
+    private static void Echo() {
+        Scanner userInput = new Scanner(System.in);
+        String userReply = userInput.nextLine();
+        while (!userReply.toLowerCase().equals("bye")) {
+            if (userReply.equals("")) {
+                System.out.println("##############################################");
+                System.out.println("Please enter a valid command");
+                System.out.println("##############################################");
+                userReply = userInput.nextLine();
+                continue;
+            }
+            System.out.println("##############################################");
+            System.out.println("\t\t\t\t\t" + userReply);
+            System.out.println("##############################################");
+            userReply = userInput.nextLine();
+        }
+    }
+
+    private static void GoodBye() {
+        System.out.println("##############################################");
+        System.out.println("So Long, farewell!");
+        System.out.println("##############################################");
     }
 }
