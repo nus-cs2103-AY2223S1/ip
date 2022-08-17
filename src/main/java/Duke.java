f5254d278a612d894c21b817cec9e2ace646aabb
Jessica Mary <jessicamary472@gmail.com>
2022-08-17 23:42:35 +0800

    Tidy up code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         String indent = "     ";
         String divider = "  ____________________________________________________________\n";
         String logo = indent + "____        _        \n"
-                + indent + "|  _ \\ _   _| | _____ \n"
-                + indent + "| | | | | | | |/ / _ \\\n"
-                + indent + "| |_| | |_| |   <  __/\n"
-                + indent + "|____/ \\__,_|_|\\_\\___|\n\n";
+                    + indent + "|  _ \\ _   _| | _____ \n"
+                    + indent + "| | | | | | | |/ / _ \\\n"
+                    + indent + "| |_| | |_| |   <  __/\n"
+                    + indent + "|____/ \\__,_|_|\\_\\___|\n\n";
         String openingStatement = indent + "Hello! I'm Duke.\n"
-                + indent + "What can I do for you?\n";
+                                + indent + "What can I do for you?\n";
         System.out.println(divider + logo + openingStatement + divider);
 
         Scanner scanner = new Scanner(System.in);
