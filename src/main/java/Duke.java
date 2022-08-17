56b34a0f3000135dfde017901f401367c741ee8c
Thanh Chu <lilyhanky57@gmail.com>
2022-08-18 01:32:48 +0800

    L4: ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,22 +16,13 @@ public class Duke {
         while (!next.equals("bye")) {
             if (next.equals("list")) {
                 echo(allMessages);
-            } else if (isSetStatus(next, allMessages.size())) {
-                String[] splited = next.split(" ");
-                int index = Integer.valueOf(splited[1]) - 1;
-                if (splited[0].equals("mark")) {
-                    allMessages.get(index).markDone();
-                    String mess = "Nice! I've marked this task as done:\n\t\t" + allMessages.get(index);
-                    echo(mess);
-                }
-                if (splited[0].equals("unmark")) {
-                    allMessages.get(index).unMark();
-                    String mess = "OK, I've marked this task as not done yet:\n\t\t" + allMessages.get(index);
-                    echo(mess);
-                }
+            } else if (isMarkUnMark(next, allMessages.size())) {
+                printMarkUnMark(next, allMessages);
+            } else if (isToDos(next)) {
+                printToDos(next, allMessages);
             } else {
                 allMessages.add(new Task(next));
-                echo("added: " + next);
+                echo(next);
             }
             next = sc.nextLine();
         }
@@ -64,7 +55,13 @@ public class Duke {
         System.out.println("\t" + line);
     }
 
-    public static boolean isSetStatus(String phrase, int listLength) {
+    public static void echoTask(Task toAdd, int size) {
+        String mess = "Got it. I've added this task:\n\t\t" + toAdd +
+                "\n\t Now you have " + size + "tasks in the list.";
+        echo(mess);
+    }
+
+    public static boolean isMarkUnMark(String phrase, int listLength) {
         String[] splited = phrase.split(" ");
         if (splited.length != 2 || !splited[1].matches("\\d+")) {
             return false;
@@ -73,5 +70,59 @@ public class Duke {
                     && Integer.valueOf(splited[1]) <= listLength
                     && Integer.valueOf(splited[1]) > 0;
         }
+    }
+
+    public static void printMarkUnMark(String next, ArrayList<Task> allMessages) {
+        String[] splited = next.split(" ");
+        int index = Integer.valueOf(splited[1]) - 1;
+        if (splited[0].equals("mark")) {
+            allMessages.get(index).markDone();
+            String mess = "Nice! I've marked this task as done:\n\t\t" + allMessages.get(index);
+            echo(mess);
+        }
+        if (splited[0].equals("unmark")) {
+            allMessages.get(index).unMark();
+            String mess = "OK, I've marked this task as not done yet:\n\t\t" + allMessages.get(index);
+            echo(mess);
+        }
+    }
+
+    public static boolean isToDos(String phrase) {
+        String[] words = phrase.split(" ", 2);
+        try {
+            String type = words[0];
+            String text = words[1];
+            if (type.equals("todo")) {
+                return true;
+            }
+            if (type.equals("deadline") && text.contains("/by")) {
+                return true;
+            }
+            if (type.equals("event") && text.contains("/at")) {
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static void printToDos(String next, ArrayList<Task> allMessages) {
+        String[] words = next.split(" ", 2);
+        String type = words[0];
+        String text = words[1];
+        Task task;
+
+        if (type.equals("todo")) {
+            task = new ToDo(text);
+        } else if (type.equals("deadline")) {
+            String[] splitted = text.split("/by ", 2);
+            task = new Deadline(splitted[0], splitted[1]);
+        } else {
+            String[] splitted = text.split("/at ", 2);
+            task = new Event(splitted[0], splitted[1]);
+        }
+        allMessages.add(task);
+        echoTask(task, allMessages.size());
     }
 }
---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+public class Event extends Task {
+    private String duration;
+
+    public Event(String description, String duration) {
+        super(description);
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(at: " + duration + ")";
+    }
+}
