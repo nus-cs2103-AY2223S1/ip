c5eafd2dd785d5eb65f721bb86511ba0b1a1cfd6
loyhongshenggg <e0725346@u.nus.edu>
2022-08-17 17:34:36 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -43,7 +43,7 @@ public class Duke {
                     }
 
                     System.out.println("Nice! I've marked this task as done:\n" +
-                            arr.get(index).toString());
+                                                            arr.get(index).toString());
                 } catch (EmptyDescriptionException | OutOfRangeException e) {
                     System.out.println(e.getMessage());
                 }
@@ -55,48 +55,60 @@ public class Duke {
                     input = input.replaceAll("todo ", "");
                     arr.add(new Todo(input));
 
+                    //output
                     System.out.println("Got it. I've added this task:");
                     System.out.println(arr.get(count).toString());
                     System.out.printf( "Now you have %d tasks in the list.", count + 1);
+                    
                     count++;
                 } catch (EmptyDescriptionException e) {
                     System.out.println(e.getMessage());
                 }
             } else if (input.contains("deadline")) {
-                System.out.println("Got it. I've added this task:");
+
                 input = input.replaceAll("deadline ", "");
 
-                //string format
+                //string manipulation
                 String[] s_arr = input.split("/", -1); //split array
                 s_arr[1] = s_arr[1].replaceAll("by ", "");
                 arr.add(new Deadline(s_arr[0], s_arr[1]));
 
+                //output
+                System.out.println("Got it. I've added this task:");
                 System.out.println(arr.get(count).toString());
                 System.out.printf( "Now you have %d tasks in the list.", count + 1);
+
                 count++;
 
             } else if (input.contains("event")) {
-                System.out.println("\nGot it. I've added this task:");
-                input = input.replaceAll("event ", "");
 
+                //string manipulation
+                input = input.replaceAll("event ", "");
                 String[] s_arr = input.split("/", -1); //split array
                 s_arr[1] = s_arr[1].replaceAll("at ", "");
+
                 arr.add(new Event(s_arr[0], s_arr[1]));
 
+                //output
+                System.out.println("\nGot it. I've added this task:");
                 System.out.println(arr.get(count).toString());
                 System.out.printf( "Now you have %d tasks in the list.", count + 1);
+
                 count++;
             } else if (input.contains("delete")) {
                 int index = -1;
-                System.out.println("Noted. I've removed this task:");
 
+                //string manipulation
                 input = input.replaceAll("delete ", "");
                 index = Integer.parseInt(input) - 1;
 
+                //output
+                System.out.println("Noted. I've removed this task:");
                 System.out.println(arr.get(index).toString());
 
                 arr.remove(index);
                 count--;
+
                 System.out.printf( "Now you have %d tasks in the list.", count);
             } else {
                 try {
