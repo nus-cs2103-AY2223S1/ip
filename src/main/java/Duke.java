161516e493b5ed56b6c084a54ec131a221394536
DevanshShah1309 <devanshshah1309@gmail.com>
2022-08-17 19:52:12 +0800

    Add, List
    Completed Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final String BYE = "bye";
+    private static final String LIST = "list";
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,19 +16,25 @@ public class Duke {
         System.out.println("\tHello, I'm Duke!");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t-------------------------------");
+        ArrayList<String> tasks = new ArrayList<>();
         while (true) {
             Scanner s = new Scanner(System.in);
             String command = s.nextLine().trim();
+            System.out.println("\t-------------------------------");
             if (command.equals(BYE)) {
-                System.out.println("\t-------------------------------");
+
                 System.out.println("\tBye! Hope to see you again");
-                System.out.println("\t-------------------------------");
                 break;
+            } else if (command.equals(LIST)) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(String.format("\t%d. %s", i + 1, tasks.get(i)));
+                }
             } else {
-                System.out.println("\t-------------------------------");
-                System.out.println("\t" + command);
-                System.out.println("\t-------------------------------");
+                System.out.println("\tadded: " + command);
+                tasks.add(command);
             }
+            System.out.println("\t-------------------------------");
         }
+        System.out.println("\t-------------------------------");
     }
 }
