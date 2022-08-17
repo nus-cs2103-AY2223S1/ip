cda9e272c16582d2842648804f9a1b8f2192ad03
AkkFiros <81766175+AkkFiros@users.noreply.github.com>
2022-08-18 06:15:50 +0800

    Level - 3
    Added in mark as done, mark as undone, icon representation of completion status, custom replies to errors.

--- src/main/java/KKBot.java
+++ src/main/java/KKBot.java
@@ -6,10 +6,11 @@ import java.util.List;
  * KKBot is a chatbot.
  * It currently functions like a to-do list.
  * It stores tasks and displays these stored tasks to users when asked.
+ * It also allows users to set and check the completion of input tasks.
  */
 public class KKBot {
     // create a class level array to store all user-input tasks
-    private static List<String> tasks = new ArrayList<>();
+    private static List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         // Welcome message and header
@@ -27,33 +28,74 @@ public class KKBot {
 
         // Add scanner in for user input (create scanner object)
         Scanner userText = new Scanner(System.in);
-        // scanner function:
-        // if input is bye, send out closing message,
-        // if input is list, shows all stored task
-        // else, stores user-input tasks into the above list
+        // Chatbot function:
         while (true) {
             String input = userText.nextLine();
             System.out.println(divider);
+            // if user input is bye, chatbot closes
             if (input.equals("bye") || input.equals("Bye") || input.equals("BYE")) {
                 System.out.println("KKBot signing off. Goodbye!\n" + divider);
                 break;
+            // if user input is list, chatbot returns a list of stored tasks
             } else if (input.equals("list") || input.equals("List") || input.equals("LIST")) {
                 // internal check if there are any tasks to do
+                // if no stored tasks, inform accordingly
                 if (tasks.isEmpty()) {
                     System.out.println("Good job! There's no tasks at hand!\n" + divider);
+                    // if there are stored tasks, list them out
+                    // include the index number and status icon of the task
                 } else {
+                    System.out.println("Here's your list of tasks:");
                     for (int i = 0; i < tasks.size(); i++) {
-                        String task = tasks.get(i);
+                        Task task = tasks.get(i);
+                        String completionIcon = task.getStatusIcon();
+                        String description = task.getDescription();
                         String index = String.valueOf(i + 1);
+                        //if last task in list, add a divider after
                         if (i == tasks.size() - 1) {
-                            System.out.println(index + ". " + task + "\n" + divider);
+                            System.out.println(index + ". " + completionIcon + " " + description + "\n" + divider);
                         } else {
-                            System.out.println(index + ". " + task + "\n");
+                            System.out.println(index + ". " + completionIcon + " " + description + "\n");
                         }
                     }
                 }
+            } else if (input.startsWith("mark") || input.startsWith("Mark") || input.startsWith("MARK")) {
+                String inputNumber = input.substring(5);
+                try {
+                    int taskNumber = Integer.parseInt(inputNumber);
+                    Task task = tasks.get(taskNumber - 1);
+                    if (task != null) {
+                        if (!task.getCompletion()) {
+                            task.setComplete();
+                        } else {
+                            System.out.println("Silly you, that task is already complete!");
+                        }
+                    }
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Woopsies you left out the task number. Please specify one!");
+                } catch (IndexOutOfBoundsException ioobe) {
+                    System.out.println("Hmm... that task doesn't exist. Please input another task number!");
+                }
+            } else if (input.startsWith("unmark") || input.startsWith("Unmark") || input.startsWith("UNMARK")) {
+                String inputNumber = input.substring(7);
+                try {
+                    int taskNumber = Integer.parseInt(inputNumber);
+                    Task task = tasks.get(taskNumber - 1);
+                    if (task != null) {
+                        if (task.getCompletion()) {
+                            task.setIncomplete();
+                        } else {
+                            System.out.println("Erm... that task was never complete...");
+                        }
+                    }
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Woopsies you left out the task number. Please specify one!");
+                } catch (IndexOutOfBoundsException ioobe) {
+                    System.out.println("Hmm... that task doesn't exist. Please input another task number!");
+                }
             } else {
-                tasks.add(input);
+                Task newTask = new Task(input);
+                tasks.add(newTask);
                 System.out.println("added: " + input + "\n" + divider);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,57 @@
+/**
+ * Tasks are added by the user for KKBot to store and keep track of.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Constructor for a task
+     * @param description Description of the task that the user inputs
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Obtains and returns the task description
+     * @return Task description
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Obtains and returns the completion status of the task
+     * @return The completion status
+     */
+    public boolean getCompletion() {
+        return this.isDone;
+    }
+
+    /**
+     * Obtains and returns the icon representation of the completion status of the task
+     * @return the icon representing the completion status of the task
+     */
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    /**
+     * Sets a task to complete
+     */
+    public void setComplete() {
+        isDone = true;
+        System.out.println("Woohoo! You completed a task:\n" + getStatusIcon() + " " + getDescription());
+    }
+
+    /**
+     * Sets a task to incomplete
+     */
+    public void setIncomplete() {
+        isDone = false;
+        System.out.println("Trynna cheat the system are we? Well, no you, task marked as incomplete:\n"
+                + getStatusIcon() + " " + getDescription());
+    }
+}
