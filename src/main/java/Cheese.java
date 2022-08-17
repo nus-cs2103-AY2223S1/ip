a5202fd14da4c21cfcdc1d19cd6c7f17467ff0f4
Jason Qiu <jason_qiu@hotmail.com>
2022-08-17 15:36:05 +0800

    feat: add respond feature

--- src/main/java/Cheese.java
+++ src/main/java/Cheese.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Cheese {
   public static void main(String[] args) {
     final String BORDER = "-----";
@@ -6,5 +8,18 @@ public class Cheese {
             + "What can I do for you?\n"
             + BORDER);
     System.out.println(greeting);
+
+    Scanner scanner = new Scanner(System.in);
+    String userInput;
+
+    while(true) {
+      userInput = scanner.nextLine();
+      System.out.println(BORDER);
+      if (userInput.equals("bye")) {
+        System.out.println("So soon? :') Bye bye");
+        break;
+      }
+      System.out.println(userInput + "\n" + BORDER);
+    }
   }
 }
