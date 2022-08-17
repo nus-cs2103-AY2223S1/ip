321fd24510169dc5b13ff88d463ed1c48aa26fb5
nitant-p <nitantpanicker@gmail.com>
2022-08-17 18:14:59 +0800

    Created Greet, Echo and Exit capabilities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
 public class Duke {
+    private final static String greeting = "Hi, I'm Bob! Nice to meet you!\n\tWhats up?";
+    private final static String farewell = "Okay then, see ya later :)";
+
+    public Duke() {
+        messageFormatter(greeting);
+    }
+
+    private void messageFormatter(String input) {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \t"
+                + input + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    private void command(String command) {
+        while(!command.equals("bye")) {
+            messageFormatter(command);
+            Scanner scanner = new Scanner(System.in);
+            command = scanner.nextLine();
+        }
+        messageFormatter(farewell);
+    }
+    
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke newDuke = new Duke();
+        newDuke.command(new Scanner(System.in).nextLine());
+
     }
 }
