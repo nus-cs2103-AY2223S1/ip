ac12953dfdced3fb4aea1af0689b6e22d86e6e5f
Jason Qiu <jason_qiu@hotmail.com>
2022-08-18 01:33:07 +0800

    refactor: rename file class name to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Cheese {
+public class Duke {
   public static final String BORDER = "-----";
   public static ArrayList<Task> list = new ArrayList<Task>();
 
@@ -10,7 +10,7 @@ public class Cheese {
             + "What can I do for you?");
     System.out.println(greeting);
 
-    Cheese.chat();
+    Duke.chat();
   }
 
   public static void chat() {
@@ -27,39 +27,39 @@ public class Cheese {
         break;
       }
       if (userInput.equals("list")) {
-        Cheese.printList();
+        Duke.printList();
       } else if (userInput.matches("mark \\d+$")) {
         int itemNum = Integer.parseInt(userInput.substring(5));
         if (notInListRange(itemNum)) {
           System.out.println("Invalid list item number.");
           continue;
         }
-        Cheese.list.get(itemNum - 1).markAsDone();
+        Duke.list.get(itemNum - 1).markAsDone();
         System.out.println("Nice! Another task done!");
-        System.out.println(Cheese.list.get(itemNum - 1));
+        System.out.println(Duke.list.get(itemNum - 1));
       } else if (userInput.matches("unmark \\d+$")) {
         int itemNum = Integer.parseInt(userInput.substring(7));
         if (notInListRange(itemNum)) {
           System.out.println("Invalid list item number.");
           continue;
         }
-        Cheese.list.get(itemNum - 1).markAsNotDone();
+        Duke.list.get(itemNum - 1).markAsNotDone();
         System.out.println("Okie! I've marked this task as not done yet.");
-        System.out.println(Cheese.list.get(itemNum - 1));
+        System.out.println(Duke.list.get(itemNum - 1));
       } else {
-        Cheese.list.add(new Task(userInput));
+        Duke.list.add(new Todo(userInput));
         System.out.println("added: " + userInput);
       }
     }
   }
 
   private static boolean notInListRange(int itemNum) {
-    return itemNum < 1 || itemNum > Cheese.list.size();
+    return itemNum < 1 || itemNum > Duke.list.size();
   }
 
   public static void printList() {
-    for (int i = 1; i <= Cheese.list.size(); i++) {
-      System.out.println(i + ". " + Cheese.list.get(i - 1));
+    for (int i = 1; i <= Duke.list.size(); i++) {
+      System.out.println(i + ". " + Duke.list.get(i - 1));
     }
   }
 }
