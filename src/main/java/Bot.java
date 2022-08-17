b00dcb385a5f9e29acf462601f416fa37af90cb1
JASON-ZENBOOK\jason <jasoncp61@gmail.com>
2022-08-17 18:44:29 +0700

    Added level 2 functionality

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+
 class Bot {
 
 private final String name;
+private final ArrayList<String> tasks;
 private static final String SPACE = "    ";
 private static final String BORDER = "    ____________________________________________________________";
 
     Bot() {
         this.name = "Bocil";
+        this.tasks = new ArrayList<String>();
     }
 
     void introduce() {
@@ -14,11 +18,21 @@ private static final String BORDER = "    ______________________________________
         System.out.println(String.format("%s\n%s\n%s\n%s\n", BORDER, line1, line2, BORDER));
     }
     void answer(String input) {
-        String response = SPACE;
+        String response = "";
         if (input.equals("bye")) {
-            response = response.concat("Bye. Hope to see you again soon!");
-        } else {
-            response = response.concat(input);
+            response = response.concat(String.format("%sBye. Hope to see you again soon!", SPACE));
+        } else if (input.equals("list")) {
+            for (int i=0; i<this.tasks.size(); i++) {
+                String line = String.format("%s. %s", i+1, this.tasks.get(i));
+                response = response.concat(String.format("%s%s", SPACE, line));
+                if (i<this.tasks.size()-1) {
+                    response = response.concat("\n");
+                }
+            }
+        }
+        else {
+            this.tasks.add(input);
+            response = response.concat(String.format("%sadded: %s", SPACE, input));
         }
         System.out.println(String.format("%s\n%s\n%s\n", BORDER, response, BORDER));
         }
