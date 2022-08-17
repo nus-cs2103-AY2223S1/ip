60ea5c9298a837202d49e1380cd76b706e6d8fba
ElijahS67 <e0271260@u.nus.edu>
2022-08-17 12:04:04 +0800

    done Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,33 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Task {
+
+    private String taskname;
+    private boolean isDone;
+
+    public Task(String taskname) {
+        this.taskname = taskname;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String status = isDone ? "X" : " ";
+        return "[" + status + "]" + " " + this.taskname;
+    }
+
+
+}
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -17,20 +44,38 @@ public class Duke {
 
         String str = sc.nextLine();
 
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<Task> arrayList = new ArrayList<>();
 
         while (!str.equals("bye")) {
 
             if (str.equals("list")) {
                 for (int i = 0; i < arrayList.size(); i++) {
                     int j = i + 1;
-                    System.out.println(j + ". " + arrayList.get(i));
+                    System.out.println(j + "." + arrayList.get(i));
                 }
             } else {
-                arrayList.add(str);
-                System.out.println("added: " + str);
-            }
 
+                String[] strArray = str.split(" ");
+
+                if (strArray[0].equals("mark")) {
+                    int i = Integer.parseInt(strArray[1]) - 1;
+                    arrayList.get(i).markDone();
+
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(arrayList.get(i));
+
+                } else if (strArray[0].equals("unmark")) {
+                    int i = Integer.parseInt(strArray[1]) - 1;
+                    arrayList.get(i).markNotDone();
+
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(arrayList.get(i));
+
+                } else {
+                    arrayList.add(new Task(str));
+                    System.out.println("added: " + str);
+                }
+            }
             str = sc.nextLine();
         }
 
