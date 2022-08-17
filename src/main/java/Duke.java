3998283178868aa155276d52e0d88f9365c71df7
WR3nd3 <wongrende@gmail.com>
2022-08-17 14:26:20 +0800

    Replaced Duke with Cat logo.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        // Idea for logo: "Hello from the other side"
+        */
+
+        String cat = "     /\\_____/\\\n"
+                + "    /  o   o  \\\n"
+                + "   ( ==  ^  == )\n"
+                + "    )         (\n"
+                + "   (           )\n"
+                + "  ( (  )   (  ) )\n"
+                + " (__(__)___(__)__)\n"
+                + "           _\n"
+                + "  ___ __ _| |_ ___\n"
+                + " / __/ _` | __/ __|\n"
+                + "| (_| (_| | |_\\__ \\\n"
+                + " \\___\\__,_|\\__|___/\n";
+        String border = "____________________________________________________________\n";
+        String service = "\nWhat can I do for mew?\n";
+
+        System.out.println(border + "Meow from\n" + cat + service + border);
+
+
     }
 }
