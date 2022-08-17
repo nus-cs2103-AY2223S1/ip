f3040e0865e97d916c08649e331aa8687682c517
ElijahS67 <e0271260@u.nus.edu>
2022-08-17 11:30:32 +0800

    Done Level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        while (!str.equals("bye")) {
+            System.out.println(str);
+            str = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
