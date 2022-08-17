2113ba1dc47aa52e3ad8b4203ff1bc5d38ac28e9
Tan Yi Xian <>
2022-08-18 01:15:11 +0800

    add Duke Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,52 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        start_prompt();
+        main_task();
+    }
+
+    private static void main_task() {
+        String terminating_text = "bye";
+        String blank = "               ";
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equals(terminating_text)) {
+                end_prompt();
+                break;
+            }
+
+            System.out.println(blank + input);
+            line_divider();
+        }
+
+    }
+
+    private static void start_prompt() {
+        String logo = "____    ____  __     ___   ___  __       ___       __   __\n"
+                + "\\   \\  /   / |  |    \\  \\ /  / |  |     /   \\     |  \\ |  |\n"
+                + " \\   \\/   /  |  |     \\  V  /  |  |    /  ^  \\    |   \\|  |\n"
+                + "  \\_    _/   |  |      >   <   |  |   /  /_\\  \\   |  . `  |\n"
+                + "    |  |     |  |     /  .  \\  |  |  /  _____  \\  |  |\\   |\n"
+                + "    |__|     |__|    /__/ \\__\\ |__| /__/     \\__\\ |__| \\__|\n";
+        System.out.println("Hi from\n" + logo);
+        System.out.println("What can I do for you?");
+        line_divider();
+    }
+
+    public static void end_prompt() {
+        String goodbye_text = "  _______   ______     ______    _______      ______   ____    ____  _______\n"
+                + " /  _____| /  __  \\   /  __  \\  |       \\    |   _  \\  \\   \\  /   / |   ____|\n"
+                + "|  |  __  |  |  |  | |  |  |  | |  .--.  |   |  |_)  |  \\   \\/   /  |  |__\n"
+                + "|  | |_ | |  |  |  | |  |  |  | |  |  |  |   |   _  <    \\_    _/   |   __|\n"
+                + "|  |__| | |  `--'  | |  `--'  | |  '--'  |   |  |_)  |     |  |     |  |____\n"
+                + " \\______|  \\______/   \\______/  |_______/    |______/      |__|     |_______|\n";
+        System.out.println(goodbye_text);
+    }
+
+    private static void line_divider() {
+        System.out.println("════════════════════════════ ⋆★⋆ ════════════════════════════");
     }
 }
