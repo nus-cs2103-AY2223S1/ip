6e740c92c5a0ae23ee95d596c97d29bc0c999f92
SilasTSL <silastaysl@gmail.com>
2022-08-18 07:39:12 +0800

    Added delete

--- src/main/java/Luffy.java
+++ src/main/java/Luffy.java
@@ -32,27 +32,39 @@ public class Luffy {
                 }
                 System.out.println("------------------------------------------------------");
             } else if (s.length() >= 6 && s.substring(0, 4).equals("mark")) {
-                System.out.println("------------------------------------------------------");
-                int taskIndex = Integer.parseInt(s.substring(5, 6)) - 1;
-                if (taskIndex >= 0 && taskIndex < counter) {
-                    tasks[taskIndex].markCompleted();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + tasks[taskIndex]);
-                } else {
-                    System.out.println("Task index " + (taskIndex + 1) + " is not valid!");
+                try {
+                    System.out.println("------------------------------------------------------");
+                    int taskIndex = Integer.parseInt(s.substring(5, 6)) - 1;
+                    if (taskIndex >= 0 && taskIndex < counter) {
+                        tasks[taskIndex].markCompleted();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  " + tasks[taskIndex]);
+                    } else {
+                        System.out.println("Task index " + (taskIndex + 1) + " is not valid!");
+                    }
+                    System.out.println("------------------------------------------------------");
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("☹ OOPS!!! Task index cannot be empty.");
+                    System.out.println("------------------------------------------------------");
+                    continue;
                 }
-                System.out.println("------------------------------------------------------");
             } else if (s.length() >= 8 && s.substring(0, 6).equals("unmark")) {
-                System.out.println("------------------------------------------------------");
-                int taskIndex = Integer.parseInt(s.substring(7, 8)) - 1;
-                if (taskIndex >= 0 && taskIndex < counter) {
-                    tasks[taskIndex].markUncompleted();
-                    System.out.println("Alright! I've marked this task as not done yet:");
-                    System.out.println("  " + tasks[taskIndex]);
-                } else {
-                    System.out.println("Task index " + (taskIndex + 1) + " is not valid!");
+                try {
+                    System.out.println("------------------------------------------------------");
+                    int taskIndex = Integer.parseInt(s.substring(7, 8)) - 1;
+                    if (taskIndex >= 0 && taskIndex < counter) {
+                        tasks[taskIndex].markUncompleted();
+                        System.out.println("Alright! I've marked this task as not done yet:");
+                        System.out.println("  " + tasks[taskIndex]);
+                    } else {
+                        System.out.println("Task index " + (taskIndex + 1) + " is not valid!");
+                    }
+                    System.out.println("------------------------------------------------------");
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("☹ OOPS!!! Task index cannot be empty.");
+                    System.out.println("------------------------------------------------------");
+                    continue;
                 }
-                System.out.println("------------------------------------------------------");
             } else {
                 System.out.println("------------------------------------------------------");
                 Task newTask;
@@ -81,12 +93,38 @@ public class Luffy {
                     try {
                         String[] splitString = s.split(" /at ");
                         newTask = new Event(splitString[0].substring(6), splitString[1]);
-                    } catch(StringIndexOutOfBoundsException e) {
+                    } catch (StringIndexOutOfBoundsException e) {
                         System.out.println("☹ OOPS!!! The description of a event cannot be empty.");
                         System.out.println("------------------------------------------------------");
                         continue;
                     } catch (ArrayIndexOutOfBoundsException e) {
                         System.out.println("☹ OOPS!!! The period of an event cannot be empty.");
+                        System.out.println("------------------------------------------------------");
+                        continue;
+                    }
+                } else if (s.length() >= 6 && s.substring(0, 6).equals("delete")) {
+                    try {
+                        System.out.println("------------------------------------------------------");
+                        int taskIndex = Integer.parseInt(s.substring(7, 8)) - 1;
+                        if (taskIndex >= 0 && taskIndex < counter) {
+                            System.out.println("Noted. I've removed this task:");
+                            System.out.println("  " + tasks[taskIndex]);
+                            for (int i = 0; i < counter; i++) {
+                                if (i == counter - 1) {
+                                    tasks[i] = null;
+                                } else if (i >= taskIndex) {
+                                    tasks[i] = tasks[i + 1];
+                                }
+                            }
+                            counter--;
+                            System.out.println("Now you have " + counter + " tasks in the list.");
+                        } else {
+                            System.out.println("Task index " + (taskIndex + 1) + " is not valid!");
+                        }
+                        System.out.println("------------------------------------------------------");
+                        continue;
+                    } catch (StringIndexOutOfBoundsException e) {
+                        System.out.println("☹ OOPS!!! Task index cannot be empty.");
                         System.out.println("------------------------------------------------------");
                         continue;
                     }
