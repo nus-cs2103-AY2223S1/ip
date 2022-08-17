8739314330fef0e5edd962a9271dca342306ef74
Chng Ian <88586264+chngchngchng@users.noreply.github.com>
2022-08-17 15:30:04 +0800

    Duke.java: Added functionality to add tasks to list and echo list for Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
+
+    // String array used to store tasks
+    private static final List<String> tasks = new ArrayList<>();
+
     // Currently, the main function takes in user input and echoes it to the user
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -17,11 +23,25 @@ public class Duke {
             if (input.toLowerCase().equals("bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
-            };
+            } else if (input.toLowerCase().equals("list")) {
+                int counter = 1;
+                System.out.println("=======================");
+                System.out.println("Here are the tasks that you have added to the list: ");
+                for (String s: tasks) {
+                    if (s != null) {
+                        System.out.println(counter + ". " + s);
+                        counter++;
+                    }
+                }
+                System.out.println("=======================");
+            } else {
+                tasks.add(input);
+                System.out.println("=======================");
+                System.out.println("added: " + input);
+                System.out.println("=======================");
+            }
 
-            System.out.println("=======================");
-            System.out.println(input);
-            System.out.println("=======================");
+
         }
 
         return;
