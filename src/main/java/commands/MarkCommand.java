4da73c6a94626582561e9f4039a17d5306c1c0e1
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-17 13:06:00 +0800

    Add mark command

---
+++ src/main/java/commands/MarkCommand.java
@@ -0,0 +1,30 @@
+package commands;
+
+import exception.*;
+import java.util.ArrayList;
+import main.*;
+
+public class MarkCommand extends Command {
+
+  private int index;
+
+  public MarkCommand(String description) throws DukeException {
+    try {
+      this.index = Integer.parseInt(description);
+    } catch (Exception e) {
+      throw new DukeException("Invalid tasks");
+    }
+  }
+
+  @Override
+  public void execute(ArrayList<Task> tasklist) throws DukeException {
+    if (index <= 0 || index > tasklist.size()) {
+      throw new DukeException("No such tasks found");
+    } else {
+      Task task = tasklist.get(index - 1);
+      task.markAsDone();
+      System.out.println("Fuyoh! I've marked this task as done:");
+      System.out.println(task.toString());
+    }
+  }
+}
