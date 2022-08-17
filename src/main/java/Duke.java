0daca92b0ca87f41867d0844bcc49be38310bf15
teekaytai <90845551+teekaytai@users.noreply.github.com>
2022-08-18 00:16:04 +0800

    Add greeting, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,21 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello there! I'm Duke!");
+        System.out.println("How can I help you?");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Ok, see you next time!");
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
