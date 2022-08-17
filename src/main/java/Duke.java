bce5724b31fba2295a5ad26fea22b5a95a3feba1
Yuanxi Zhu <yuanxizhu@YXMacBook-Air.local>
2022-08-18 00:28:19 +0800

    Allow marking task as done or undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,20 +21,27 @@ public class Duke {
 
     public static void chat () {
         Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
+        String userInput = sc.next();
         String output;
 
         while (!(userInput.equals("Bye") || userInput.equals("bye"))) {
             if (userInput.equals("List")) {
                 output = listTask();
                 System.out.print(separator + "List all tasks:\n" + output + separator);
+            } else if (userInput.equals("mark")) {
+                Integer taskNum = Integer.valueOf(sc.next());
+                taskList[taskNum - 1].markAsDone();
+                System.out.print(separator + "Good job on completing the task:\n" + taskList[taskNum - 1] + separator);
+            } else if (userInput.equals("unmark")) {
+                Integer taskNum = Integer.valueOf(sc.next());
+                taskList[taskNum - 1].markAsUndone();
+                System.out.print(separator + "This task is not done yet:\n" + taskList[taskNum - 1] + separator);
             } else {
                 Task task = new Task(userInput);
                 taskList[numOfTasks++] = task;
-                output = task.toString();
-                System.out.print(separator + "Task added: " + output + separator);
+                System.out.print(separator + "Task added: " + task + separator);
             }
-            userInput = sc.nextLine();
+            userInput = sc.next();
         }
     }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,31 @@
 public class Task {
-    public String content;
+    protected String content;
+    protected boolean isDone;
 
     Task (String c) {
         this.content = c;
+        this.isDone = false;
+    }
+
+    Task (String c, boolean isDone) {
+        this.content = c;
+        this.isDone = isDone;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
     }
 
     @Override
     public String toString() {
-        return this.content + "\n";
+        return getStatusIcon() + " " + this.content + "\n";
     }
 }
