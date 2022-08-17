d357c1f037eef3320fcc5d9d3c5e85fcbfc6daaa
crvstalphua <>
2022-08-17 16:58:33 +0800

    added task.java and feature for marking tasks as done

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Bob {
     public static void main(String[] args) {
-        ArrayList<String> tasks = new ArrayList<>(100);
+        ArrayList<Task> tasks = new ArrayList<>(100);
         Scanner scanner = new Scanner(System.in);
         System.out.println("hey, i'm bob!\ndo you need help?");
         String reply = scanner.nextLine();
@@ -11,13 +11,32 @@ public class Bob {
         while (!reply.equalsIgnoreCase("bye")) {
             if (reply.equalsIgnoreCase("list")) {
                 int index = 1;
-                for (String s : tasks) {
-                    System.out.println(String.valueOf(index++) + ". " + s);
+                for (Task task : tasks) {
+                    System.out.println((index++) + ". " + task.toString());
                 }
-            }
-            else {
+            } else if (reply.toLowerCase().matches("mark(.*)")) {
+                try {
+                    int index = Integer.valueOf(reply.replaceAll("[^0-9]", ""));
+                    Task task = tasks.get(index - 1);
+                    task.toMark(true);
+                    System.out.println("yay! you've completed a task!\n" + task.toString());
+                } catch(NumberFormatException e) {
+                    System.out.println("which task to mark?");
+                }
+
+            } else if (reply.toLowerCase().matches("unmark(.*)")) {
+                try {
+                    int index = Integer.valueOf(reply.replaceAll("[^0-9]", ""));
+                    Task task = tasks.get(index - 1);
+                    task.toMark(false);
+                    System.out.println("aw...i guess there's another task.\n" + task.toString());
+                } catch(NumberFormatException e) {
+                    System.out.println("which task to unmark?");
+                }
+            } else {
                 System.out.println("now in your list: " + reply);
-                tasks.add(reply);
+                Task newTask = new Task(reply);
+                tasks.add(newTask);
             }
             reply = scanner.nextLine();
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,5 +1,20 @@
-import java.util.ArrayList;
+public class Task {
+    protected String description;
+    protected boolean isDone;
 
-public class Tasks {
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
 
+    public String getStatusIcon() {
+        return (isDone? "[X]" : "[ ]");
+    }
+
+    public void toMark(boolean x) {
+        this.isDone = x;
+    }
+    public String toString() {
+        return this.getStatusIcon() + " " + this.description;
+    }
 }
