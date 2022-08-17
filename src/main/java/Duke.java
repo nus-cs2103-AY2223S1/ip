fdf18330b53434bb8eac049f0171aaad36137868
limweijun <e0726982@u.nus.edu>
2022-08-17 14:38:16 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hi\n" + logo + "\nWhat can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye"))
+        {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+        System.out.println("Bye!");
     }
 }
