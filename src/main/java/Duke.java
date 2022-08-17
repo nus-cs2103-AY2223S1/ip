dededb5284a9b5fca9965a4cda10da99a3a02a38
limweijun <e0726982@u.nus.edu>
2022-08-17 17:12:29 +0800

    Completed TextUITesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,12 +5,12 @@ public class Duke {
     public static void main(String[] args) {
         Task[] tasksToDo = new Task[100];
         int i = 0;
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hi\n" + logo + "\nWhat can I do for you?");
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hi I'm Duke, What can I do for you?");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye"))
