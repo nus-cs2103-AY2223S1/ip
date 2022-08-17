07b17d31f1ed353a6d2b845474c98af49a693254
Bubbl3T <linzechen@gmail.com>
2022-08-17 16:46:40 +0800

    Added basic skeleton to Duke and enabled user input.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String str = sc.nextLine();
+            if ("bye".equals(str)) {
+                System.out.println("Bye! Hope to see you again soon");
+                break;
+            } else {
+                System.out.println(str);
+            }
+        }
     }
 }
