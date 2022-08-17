d23a6694a35c3406b77442527fa4651484e2264d
ConradLew <e0726724@u.nus.edu>
2022-08-17 16:36:35 +0800

    Added features for level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String dukeGreeting = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(dukeGreeting);
+
+        Scanner sc = new Scanner(System.in);
+        String userResponse = null;
+        while (userResponse == null || !userResponse.equals("bye")) {
+            userResponse = sc.nextLine();
+            String dukeEndOutput = "    " + "Bye. Hope to see you again soon!";
+            String dukeContinuationOutput = "    " + userResponse;
+            String dukeOutput = userResponse.equals("bye") ? dukeEndOutput : dukeContinuationOutput;
+            System.out.println(dukeOutput);
+        }
+
     }
 }
