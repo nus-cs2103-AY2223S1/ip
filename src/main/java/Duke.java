3dda9e65274d0b866b0bd4a906ec3ca267e56228
tienyu2000 <tienyu2000@hotmail.com>
2022-08-17 20:43:01 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,17 +2,26 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm nothing but an echo bot for now!\nWhat can I do for you, my highness?");
+        System.out.println("Hello! I've upgraded to become a listing bot now!\nWhat can I do for you, my highness?");
         Scanner in = new Scanner(System.in);
+
+        String[] tasks = new String[100];
+        int count = 0;
 
         while (true) {
             System.out.println("Your highness:");
             String input = in.nextLine();
             if (input.equals("bye")) {
-                System.out.println("Echoing: Bye. Hope to see you again soon!");
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i=1; i<=count; i++) {
+                    System.out.println(i + ". " + tasks[i-1]);
+                }
             } else {
-                System.out.println("Echoing: " + input);
+                tasks[count] = input;
+                count++;
+                System.out.println("added: " + input);
             }
         }
     }
