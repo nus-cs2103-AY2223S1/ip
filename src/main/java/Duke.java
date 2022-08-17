6e0b68010b6ad8aafb138194f262d701acd36175
CedricChia123 <cedricchia01@gmail.com>
2022-08-18 04:19:25 +0800

    Implemented Task class, completed mark and unmark methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
         System.out.println("Hello I'm Duke\n" + "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
@@ -15,11 +15,25 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 int length = list.size();
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < length; i++) {
-                    System.out.println((i + 1) + ". " + list.get(i));
+                    Task currentTask = list.get(i);
+                    System.out.println((i + 1) + "." + "[" + currentTask.getStatusIcon()
+                    + "] " + currentTask);
                 }
+            } else if (input.startsWith("mark")) {
+                Task t = list.get(Integer.parseInt(input.substring(5)) - 1);
+                t.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + t.getStatusIcon() + "] " + t.description);
+            } else if (input.startsWith("unmark")) {
+                Task t = list.get(Integer.parseInt(input.substring(7)) - 1);
+                t.markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [" + t.getStatusIcon() + "] " + t.description);
             } else {
-                list.add(input);
+                Task t = new Task(input);
+                list.add(t);
                 System.out.println("added: " + input);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+}
