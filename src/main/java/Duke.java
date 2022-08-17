64f592914b4cfcb7768e088a2f975c937a9d7ac1
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-08-17 12:56:47 +0800

    Implement echo back and exit commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
 import static pnkp.duke.IOFormat.say;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        say("Hello from\n" + logo);
+        Scanner stdin = new Scanner(System.in);
+
+        // intro string
+        say("Hello, Duke here! What can I do for you?");
+
+        boolean fExit = false;
+        while (!fExit) {
+            String line = stdin.nextLine();
+            String command = (new Scanner(line)).next();
+            switch (command) {
+                case "bye":
+                    say("OK. See you next time! *boings away*");
+                    fExit = true;
+                    break;
+                default:
+                    say(line);
+                    break;
+            }
+        }
     }
 }
