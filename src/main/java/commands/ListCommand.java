5ea59f6ca60e515ca02baa2a484fb23b3dc2564d
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-17 13:05:13 +0800

    Add list all taks command

---
+++ src/main/java/commands/ListCommand.java
@@ -0,0 +1,20 @@
+package commands;
+
+import exception.*;
+import java.util.ArrayList;
+import main.*;
+
+public class ListCommand extends Command {
+
+  @Override
+  public void execute(ArrayList<Task> tasklist) throws DukeException {
+    if (tasklist.size() == 0) {
+      System.out.println("No task found so far. Use add keyword to add tasks!");
+    } else {
+      System.out.println("Here are the tasks in your list:");
+      for (int i = 0; i < tasklist.size(); i++) {
+        System.out.println((i + 1) + ". " + tasklist.get(i).toString());
+      }
+    }
+  }
+}
