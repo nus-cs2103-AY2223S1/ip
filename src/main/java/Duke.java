37e5375df6edf4a04eb98d3ac889a9cc1cf90b03
Huang Yuchen <hyuchen2607@gmail.com>
2022-08-17 22:06:11 +0800

    Revert "Add new classes and cleared Level-2"
    This reverts commit db745e658018e6a791b835d33b083414ba0ad348.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,13 +10,6 @@ public class Duke {
     private static final String WELCOME = "こんにちは (Konnichiwa)! Rem だよ! (I'm Rem!) :>\n"
             + "今日は何ができますか? (What can I do for you today?)";
 
-    private CheckList checklist;
-
-    // Constructor
-    public Duke() {
-        checklist = new CheckList();
-    }
-
     // Class Methods
     private void start() {
         System.out.println(logo + "\n");
@@ -24,35 +17,24 @@ public class Duke {
         System.out.println(SPACER);
     }
 
-    private void run() {
+    private void getInput() {
         String input;
         Scanner sc = new Scanner(System.in);
+
         // Keep input on standby until user enters "bye"
         while (true) {
             System.out.print(">> ");
             input = sc.nextLine();
             if (input.equals("bye")) break;
-            if (input.equals("list")) {
-                System.out.println(SPACER);
-                System.out.println(checklist.printList());
-                System.out.println(SPACER);
-                continue;
-            }
-
-            Task task = new Task(checklist.getTaskCount() + ". " + input);
-            checklist.addTask(task);
-
             System.out.println(SPACER);
-            System.out.println("I've added '" + input + "' to your list. :)");
+            System.out.println("Rem: " + input);
             System.out.println(SPACER);
         }
         System.out.println("またね! (See you soon!) <3");
     }
-
-    // Main Method
     public static void main(String[] args) {
         Duke dk = new Duke();
         dk.start();
-        dk.run();
+        dk.getInput();
     }
 }
