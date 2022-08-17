ce9be5f7943dfde42bfff4f6a863288b41f3c780
sohenze <e0725494@u.nus.edu>
2022-08-17 14:20:11 +0800

    Level-1

---
+++ src/main/java/BotResponse.java
@@ -0,0 +1,13 @@
+public class BotResponse {
+    public static void separationLine() {
+        System.out.println("____________________________________________________________");
+    }
+    public static void welcome() {
+        System.out.println("Hello, I'm Adam\n"
+                        + "what can I do for you?");
+        separationLine();
+    }
+    public static void bye() {
+        System.out.println("Sorry to see you go, goodbye :(");
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner input = new Scanner(System.in);
+        BotResponse.welcome();
+        checkResponse(input);
     }
+
+    public static void checkResponse(Scanner input) {
+        String userResponse = input.nextLine();
+        if (userResponse.equalsIgnoreCase("bye")) {
+            BotResponse.bye();
+            System.exit(0);
+        } else {
+            System.out.println(userResponse);
+            BotResponse.separationLine();
+            checkResponse(input);
+        }
+    }
+
 }
