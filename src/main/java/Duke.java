2d9c4c1fabd3f677e42abd10a02b7f14b70837cc
nealetham <nealetham2000@gmail.com>
2022-08-17 16:53:58 +0800

    Add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
+        ArrayList<String> inputList = new ArrayList<String>();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
         String input = myObj.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                String[] inputArray = inputList.toArray(new String[inputList.size()]);
+                for (int i = 1; i <= inputArray.length; i++) {
+                    String item = String.format("%s. %s", i, inputArray[i - 1]);
+                    System.out.println(item);
+                }
+            } else {
+                System.out.println("added: " + input);
+                inputList.add(input);
+            }
             input = myObj.nextLine();
         }
 
         myObj.close();
         System.out.println("Bye. Hope to see you again soon!");
-
     }
 }
