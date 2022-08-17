9ff743ed6f92f22b99d62ec7cf697e6f8b033cbb
edricyeo <edricyyj@gmail.com>
2022-08-18 02:00:38 +0800

    implemented add and list features for level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,26 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Edric \nWhat can I do for you?");
+
+        String[] data = new String[100];
+        int dataIdx = 0;
         Scanner sc = new Scanner(System.in);
         // while loop keeps grabbing input from user, unless user inputs bye to break the loop
         while (true) {
-            String parrot = sc.nextLine();
-            if (parrot.equals("bye")) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
                 break;
             }
-            System.out.println(parrot);
+            if (input.equals("list")) {
+                for (int i=0; i < dataIdx; i++) {
+                    System.out.format("%d. %s\n", i + 1, data[i]);
+                }
+            }
+            else {
+                data[dataIdx++] = input;
+                System.out.println("added: " + input);
+            }
+
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
