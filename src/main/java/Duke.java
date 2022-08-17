b0bbf8df3e6a45fb71a3430cd49f31cdada38dce
Zsigmond <e0725064@u.nus.edu>
2022-08-17 14:20:02 +0800

    feat: error checking
    must prefix with todo, event, deadline to add task; unrecognised text
    will print an error
    marking and unmarking error -> not a number provide; index out of range
    todo error -> no text
    deadline/event error -> no text; no time
    lsit error -> no tasks in list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,41 +18,63 @@ public class Duke {
                     try {
                         markI = Integer.parseInt(commandArr[1]);
                     } catch (NumberFormatException e) {     // if second word not integer
-                        continue;
+                        System.out.println("🙁 OOPS!!! Provide a number to mark a task.");
+                        break;
                     }
                     if (markI >= 1 && markI <= tasks.size()) {   // ensure i given is within range
                         tasks.get(markI - 1).mark();
-                        break;
+                    } else {
+                        System.out.printf("🙁 OOPS!!! Provide a valid number (from 1 to %d) to mark a task.\n", tasks.size());
                     }
+                    break;
                 case "unmark":
                     int unmarkI;
                     try {
                         unmarkI = Integer.parseInt(commandArr[1]);
                     } catch (NumberFormatException e) {     // if second word not integer
-                        continue;
+                        System.out.println("🙁 OOPS!!! Provide a number to unmark a task.");
+                        break;
                     }
                     if (unmarkI >= 1 && unmarkI <= tasks.size()) {   // ensure i given is within range
                         tasks.get(unmarkI - 1).unmark();
-                        break;
+                    } else {
+                        System.out.printf("🙁 OOPS!!! Provide a valid number (from 1 to %d) to unmark a task.\n", tasks.size());
                     }
+                    break;
                 case "todo":
-                    tasks.add(new Todo(false,
-                                        command.replaceFirst("todo ", "")));
-                    System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                    String todoText = command.replaceFirst("todo", "").strip();
+                    if (todoText.isEmpty()) {
+                        System.out.println("🙁 OOPS!!! The description of a todo cannot be empty.");
+                    } else {
+                        tasks.add(new Todo(false, todoText));
+                        System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                    }
                     break;
                 case "deadline":
-                    String[] c1 = command.split(" /by ");    // c[0]: "deadline" + text; c[1]: deadline
-                    tasks.add(new Deadline(false,
-                            c1[0].replaceFirst("deadline ", ""),
-                            c1[1]));
-                    System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                    String[] c1 = command.split("/by");
+                    String deadlineText = c1[0].replaceFirst("deadline", "").strip();
+                    String deadlineTime = c1.length > 1 ? c1[1].strip() : "";
+                    if (deadlineText.isEmpty()) {
+                        System.out.println("🙁 OOPS!!! The description of a deadline cannot be empty.");
+                    } else if (deadlineTime.isEmpty()) {
+                        System.out.println("🙁 OOPS!!! Provide a time for the deadline.");
+                    } else {
+                        tasks.add(new Deadline(false, deadlineText, deadlineTime));
+                        System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                    }
                     break;
                 case "event":
-                    String[] c2 = command.split(" /at ");    // c[0]: "event" + text; c[1]: time
-                    tasks.add(new Event(false,
-                            c2[0].replaceFirst("event ", ""),
-                            c2[1]));
-                    System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                    String[] c2 = command.split("/at");
+                    String eventText = c2[0].replaceFirst("event", "").strip();
+                    String eventTime = c2.length > 1 ? c2[1].strip() : "";
+                    if (eventText.isEmpty()) {
+                        System.out.println("🙁 OOPS!!! The description of a event cannot be empty.");
+                    } else if (eventTime.isEmpty()) {
+                        System.out.println("🙁 OOPS!!! Provide a time for the event.");
+                    } else {
+                        tasks.add(new Event(false, eventText, eventTime));
+                        System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                    }
                     break;
                 default:
                     switch (command) {
@@ -60,14 +82,17 @@ public class Duke {
                             System.out.println("Bye. Hope to see you again soon!");
                             break;
                         case "list":
-                            System.out.println("Here are the tasks in your list:");
-                            for (int i = 0; i < tasks.size(); i++) {
-                                System.out.printf("%d. %s\n", i + 1, tasks.get(i).toString());
+                            if (tasks.isEmpty()) {
+                                System.out.println("🙁 OOPS!!! There are no tasks in your list yet.");
+                            } else {
+                                System.out.println("Here are the tasks in your list:");
+                                for (int i = 0; i < tasks.size(); i++) {
+                                    System.out.printf("%d. %s\n", i + 1, tasks.get(i).toString());
+                                }
                             }
                             break;
-                        default:    // default task type will be todo
-                            tasks.add(new Todo(false, command));
-                            System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
+                        default:
+                            System.out.println("🙁 OOPS!!! I'm sorry, but I don't know what that means :-(");
                             break;
                     }
             }
