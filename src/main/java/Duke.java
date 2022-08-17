d21e760da9d2616755be168e54b45685b99635f9
DESKTOP-LBC8HLP\User <greggchan12345@gmail.com>
2022-08-18 00:12:25 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,34 +9,71 @@ public class Duke {
         class Task {
             protected String description;
             protected boolean isDone;
+            protected int type;
+            protected String date;
 
-            public Task(String description) {
+            public Task(String description, int type) {
                 this.description = description;
                 this.isDone = false;
+                this.type = type;
             }
 
             public void isMark(boolean bool) {
                 this.isDone = bool;
             }
 
+            public String getType() {
+                if (this.type == 0) {
+                    return "[T]";
+                } else if (this.type == 1) {
+                    return "[D]";
+                } else {
+                    return "[E]";
+                }
+            }
+
             public String getDescription() {
                 return this.description;
+            }
+
+            public void setDate(String str) {
+                this.date = str;
+            }
+
+            public String getDate() {
+                if (this.type == 0) {
+                    String str = "There is no date.";
+                    return str;
+                }
+                return this.date;
             }
 
             public String getStatusIcon() {
                 return (isDone ? "X" : " "); // mark done task with X
             }
+
+            public String toString() {
+                if (type == 0) {
+                    return "[T][" + this.getStatusIcon() + "] " +
+                            this.description;
+                } else if (type == 1) {
+                    return "[D][" + this.getStatusIcon() + "] " +
+                            this.description + " (by: " + this.date + ")";
+                } else {
+                    return "[E][" + this.getStatusIcon() + "] " +
+                            this.description + " (at: " + this.date + ")";
+                }
+            }
         }
 
         String currreply = reply.nextLine();
-        Task currtask = new Task(currreply);
         ArrayList<Task> list = new ArrayList<>(100);
 
         while (!currreply.equals("bye")) {
             if (currreply.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                list.forEach(n -> System.out.println((list.indexOf(n) + 1) + ".["
-                        + n.getStatusIcon() + "] " + n.getDescription()));
+                list.forEach(n -> System.out.println((list.indexOf(n) + 1) + "."
+                        + n.toString()));
                 System.out.println();
                 currreply = reply.nextLine();
             } else if (currreply.startsWith("mark")) {
@@ -47,7 +84,7 @@ public class Duke {
                     Task task = list.get(index);
                     task.isMark(true);
                     System.out.println("Nice! I've marked this task as done:\n" +
-                            " [X] " + task.getDescription() + "\n");
+                            " " + task.getType() + "[X] " + task.getDescription() + "\n");
                 }
                 currreply = reply.nextLine();
             } else if (currreply.startsWith("unmark")) {
@@ -58,14 +95,38 @@ public class Duke {
                     Task task = list.get(index);
                     task.isMark(false);
                     System.out.println("OK, I've marked this task as not done yet:\n" +
-                            " [ ] " + task.getDescription() + "\n");
+                            " " + task.getType() + "[ ] " + task.getDescription() + "\n");
                 }
                 currreply = reply.nextLine();
             } else {
-                list.add(currtask);
-                System.out.println("added: " + currreply + "\n");
+                if (currreply.startsWith("todo")) {
+                    String acttask = currreply.substring(5);
+                    Task currtask = new Task(acttask, 0);
+                    currtask.setDate(null);
+                    list.add(currtask);
+                    System.out.println("Got it. I've added this task:\n" +
+                            " " + currtask.toString() + "\n" +
+                            "Now you have " + list.size() + " tasks in the list.\n");
+                } else if (currreply.startsWith("deadline")) {
+                    int slashindex = currreply.indexOf("/");
+                    String acttask = currreply.substring(9, slashindex - 1);
+                    Task currtask = new Task(acttask, 1);
+                    currtask.setDate(currreply.substring(slashindex + 4));
+                    list.add(currtask);
+                    System.out.println("Got it. I've added this task:\n" +
+                            " " + currtask.toString() + "\n" +
+                            "Now you have " + list.size() + " tasks in the list.\n");
+                } else if (currreply.startsWith("event")) {
+                    int slashindex = currreply.indexOf("/");
+                    String acttask = currreply.substring(6, slashindex - 1);
+                    Task currtask = new Task(acttask, 2);
+                    currtask.setDate(currreply.substring(slashindex + 4));
+                    list.add(currtask);
+                    System.out.println("Got it. I've added this task:\n" +
+                            " " + currtask.toString() + "\n" +
+                            "Now you have " + list.size() + " tasks in the list.\n");
+                }
                 currreply = reply.nextLine();
-                currtask = new Task(currreply);
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
