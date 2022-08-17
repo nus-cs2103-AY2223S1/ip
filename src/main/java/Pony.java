189974b1271ca820704c4366a6e0332f132b8cab
ciaoosuuu <>
2022-08-18 06:18:05 +0800

    Completed Level-2:Add, List

--- src/main/java/Pony.java
+++ src/main/java/Pony.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Pony {
 
     String greet = "Hello! I'm Pony" + "\n" + "What can I do for you? ";
     String exit = "Bye. Hope to see you again soon!";
     Scanner sc = new Scanner(System.in);
+    ArrayList<String> list;
 
     public Pony() {
+        this.list = new ArrayList();
     };
 
     public void initialise() {
@@ -14,11 +16,24 @@ public class Pony {
     }
     public void run() {
         String command = takeCommand();
-        while (!command.equals("bye")) {
-            System.out.println(command);
-            command = takeCommand();
+        if (command.equals("list")) {
+            if (this.list.size() == 0) {
+                System.out.println("Nothing on the list!");
+                run();
+            } else {
+                for (int i = 0; i < this.list.size(); i++) {
+                    int sn = i + 1;
+                    System.out.println(sn + ". " + this.list.get(i));
+                }
+                run();
+            }
+        } else if (command.equals("bye")) {
+            System.out.println(this.exit);
+        } else {
+            this.list.add(command);
+            System.out.println("added: " + command);
+            run();
         }
-        System.out.println(this.exit);
     }
 
     public String takeCommand() {
