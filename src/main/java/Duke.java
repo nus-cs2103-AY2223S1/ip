fa56dc00588fc150fe43110ae8043592ac0650aa
nickeltea <e0772462@u.nus.edu>
2022-08-18 01:54:26 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke \n" + "What can I do for you?");
+        String command = scan.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(
+                    "____________________________________________________________ \n"
+                    + command + "\n"
+                    + "____________________________________________________________");
+            command = scan.nextLine();
+        }
+
+        System.out.println(
+                "____________________________________________________________ \n"
+                + "Bye. Hope to see you again soon! \n"
+                + "____________________________________________________________");
     }
 }
