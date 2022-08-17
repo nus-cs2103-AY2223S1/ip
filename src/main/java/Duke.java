679fc59cb5965509c12b8dc918f2eab51d4e74c0
Rachel Angelyn Gunawan <rachelag777@gmail.com>
2022-08-18 00:15:39 +0800

    implemented error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -29,30 +29,37 @@ public class Duke {
         String command = arg[0];
         String commandArg = arg.length > 1 ? arg[1] : "";
 
-        switch (command) {
-            case "list":
-                this.viewList();
-                break;
-            case "mark":
-                this.markTask(commandArg);
-                break;
-            case "unmark":
-                this.unmarkTask(commandArg);
-                break;
-            case "todo": case "deadline": case "event":
-                this.addTask(commandArg, command);
-                break;
-            default:
-                printMessage("Sorry, I don't understand your command :(");
-                break;
+        try {
+            switch (command) {
+                case "list":
+                    this.viewList();
+                    break;
+                case "mark":
+                    this.markTask(commandArg);
+                    break;
+                case "unmark":
+                    this.unmarkTask(commandArg);
+                    break;
+                case "todo": case "deadline": case "event":
+                    if (commandArg.equals("")) {
+                        throw new DukeException("OOPS! The description of a task cannot be empty.");
+                    }
+                    this.addTask(commandArg, command);
+                    break;
+                default:
+                    throw new DukeException("Sorry, I don't understand what that means :(");
+            }
+        } catch (DukeException e) {
+            printMessage(e.getMessage());
         }
+
     }
 
     private void viewList() {
         this.printMessage(this.taskList.toString());
     }
 
-    private void addTask(String task, String type) {
+    private void addTask(String task, String type) throws DukeException {
         Task newTask = null;
         switch (type) {
             case "todo" :
@@ -60,10 +67,16 @@ public class Duke {
                 break;
             case "deadline":
                 String[] deadlineDesc = task.split("/by", 2);
+                if (deadlineDesc.length != 2 || deadlineDesc[0].equals("") || deadlineDesc[1].equals("")) {
+                    throw new DukeException("OOPS! A deadline must contain a description and a due date.");
+                }
                 newTask = new Deadline(deadlineDesc[0], deadlineDesc[1]);
                 break;
             case "event":
                 String[] eventDesc = task.split("/at", 2);
+                if (eventDesc.length != 2 || eventDesc[0].equals("") || eventDesc[1].equals("")) {
+                    throw new DukeException("OOPS! An event must contain a description and a date.");
+                }
                 newTask = new Event(eventDesc[0], eventDesc[1]);
                 break;
         }
@@ -77,17 +90,27 @@ public class Duke {
     }
 
     private void markTask(String n) {
-        // handle cases when n is invalid
-        int index = Integer.parseInt(n);
-        this.taskList.markTaskN(index, true);
-        printMessage("Nice! I've marked this task as done:\n\t" + this.taskList.getTaskN(index));
+        try {
+            int index = Integer.parseInt(n);
+            this.taskList.markTaskN(index, true);
+            printMessage("Nice! I've marked this task as done:\n\t" + this.taskList.getTaskN(index));
+        } catch (NumberFormatException e) {
+            this.printMessage("OOPS! You must enter the number of task to be marked.");
+        } catch (IndexOutOfBoundsException e) {
+            this.printMessage("OOPS! The number you entered is not within the valid range.");
+        }
     }
 
     private void unmarkTask(String n) {
-        // handle cases when n is invalid
-        int index = Integer.parseInt(n);
-        this.taskList.markTaskN(index, false);
-        printMessage("OK, I've marked this task as not done yet:\n\t" + this.taskList.getTaskN(index));
+        try {
+            int index = Integer.parseInt(n);
+            this.taskList.markTaskN(index, false);
+            printMessage("OK, I've marked this task as not done yet:\n\t" + this.taskList.getTaskN(index));
+        } catch (NumberFormatException e) {
+            this.printMessage("OOPS! You must enter the number of task to be unmarked.");
+        } catch (IndexOutOfBoundsException e) {
+            this.printMessage("OOPS! The number you entered is not within the valid range.");
+        }
     }
 
     public void printMessage(String message) {
