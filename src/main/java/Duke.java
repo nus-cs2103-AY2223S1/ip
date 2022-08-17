dd66efd740ea661cd63a635478e5b5ed2704649b
Prearo <kohshanghui@gmail.com>
2022-08-17 23:23:06 +0800

    Setup project and Level 1: Greet Echo Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String intro = "Hello! I'm Duke \nWhat can I do for you?";
+        System.out.println("Hello from\n" + intro);
+
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.exit(0);
+            }
+            System.out.println(input);
+        }
     }
 }
