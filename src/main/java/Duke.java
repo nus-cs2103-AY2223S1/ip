d28dc7fb6fb06d2ee5998ecbe0e733a5d9612841
24Donovan24 <donovanlee_25@hotmail.com>
2022-08-18 00:50:19 +0800

    Handled exception when deleting task with invalid number

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,20 +28,22 @@ public class Duke {
                         break;
 
                     //Handle case when task is a deadline
-                    case "deadline":
+                    case "deadline": {
                         String str = input.nextLine();
                         String dDescription = str.substring(0, str.indexOf('/') - 1);
                         String dBy = str.substring(str.indexOf('/') + 4);
                         duke.printDeadline(dDescription, dBy);
                         break;
+                    }
 
                     //Handle case when task is an event
-                    case "event":
-                        String str2 = input.nextLine();
-                        String eDescription = str2.substring(0, str2.indexOf('/') - 1);
-                        String eAt = str2.substring(str2.indexOf('/') + 4);
+                    case "event": {
+                        String str = input.nextLine();
+                        String eDescription = str.substring(0, str.indexOf('/') - 1);
+                        String eAt = str.substring(str.indexOf('/') + 4);
                         duke.printEvent(eDescription, eAt);
                         break;
+                    }
 
                     //Handle case when user wants to list tasks
                     case "list" :
@@ -49,7 +51,7 @@ public class Duke {
                         break;
 
                     //Handle case when user wants to mark task
-                    case "mark":
+                    case "mark": {
                         //-1 to get index in 0 indexing
                         int index = input.nextInt() - 1;
                         try {
@@ -59,24 +61,31 @@ public class Duke {
                         }
 
                         break;
+                    }
 
                     //Handle case when user wants to unmark task
-                    case "unmark":
+                    case "unmark": {
                         //-1 to get index in 0 indexing
-                        int index2 = input.nextInt() - 1;
+                        int index = input.nextInt() - 1;
                         try {
-                            duke.unmarkTask(index2);
+                            duke.unmarkTask(index);
                         } catch (DukeException dE) {
                             System.out.println("Caught Exception is " + dE);
                         }
                         break;
+                    }
 
                     //Handle case when user wants to delete task
-                    case "delete":
+                    case "delete": {
                         //-1 to get in 0 indexing
-                        int index3 = input.nextInt() - 1;
-                        duke.deleteTask(index3);
+                        int index = input.nextInt() - 1;
+                        try {
+                            duke.deleteTask(index);
+                        } catch (DukeException dE) {
+                            System.out.println("Caught Exception is " + dE);
+                        }
                         break;
+                    }
 
                     //Default case: Not any of the tasks(aTodo, Deadline, Event) and hence, throws an Exception
                     default:
@@ -157,7 +166,7 @@ public class Duke {
     public void markTask(int index) throws DukeException {
         int size = lst.size();
         if (index >= size || index < 0) {
-            throw new DukeException("OOPS! The index of the todo does not exists.");
+            throw new DukeException("OOPS! The index of the task does not exists.");
         } else {
             Task taskToBeMarked = lst.get(index);
             taskToBeMarked.markAsDone();
@@ -173,7 +182,7 @@ public class Duke {
     public void unmarkTask(int index) throws DukeException {
         int size = lst.size();
         if (index >= size || index < 0) {
-            throw new DukeException("OOPS! The index of the todo does not exists.");
+            throw new DukeException("OOPS! The index of the task does not exists.");
         } else {
             Task taskToBeUnmarked = lst.get(index);
             taskToBeUnmarked.markAsUndone();
@@ -184,14 +193,20 @@ public class Duke {
 
     /**
      * @param index An int representing the index of task to be deleted
+     * @throws DukeException
      */
-    public void deleteTask(int index) {
-        Task taskToBeDeleted = lst.get(index);
-        lst.remove(index);
-        int newSize = lst.size();
-        System.out.printf("\tNoted. I've removed this task:\n\t%s\n" +
-                        "\tNow you have %d tasks in the list.\n",
-                taskToBeDeleted, newSize);
+    public void deleteTask(int index) throws DukeException {
+        int size = lst.size();
+        if (index >= size || index < 0) {
+            throw new DukeException("OOPS! The index of the task does not exists.");
+        } else {
+            Task taskToBeDeleted = lst.get(index);
+            lst.remove(index);
+            int newSize = lst.size();
+            System.out.printf("\tNoted. I've removed this task:\n\t%s\n" +
+                            "\tNow you have %d tasks in the list.\n",
+                    taskToBeDeleted, newSize);
+        }
     }
 }
 
