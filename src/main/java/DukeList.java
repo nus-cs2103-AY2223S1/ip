790d491ee8396d996802c53f56db4324323483f8
kxrt <kvrtikeya@gmail.com>
2022-08-17 22:17:50 +0800

    Store DukeTasks in Collection instead of array

--- src/main/java/DukeList.java
+++ src/main/java/DukeList.java
@@ -1,39 +1,38 @@
+import java.util.ArrayList;
+
 public class DukeList {
-  private final DukeTask[] items;
-  private int index = 0;
+  private final ArrayList<DukeTask> items;
 
   public DukeList() {
-    items = new DukeTask[100];
+    items = new ArrayList<>();
   }
 
   public String addItem(String newItem) {
     if (newItem.equals("")) {
       return "";
     }
-    items[index] = new DukeTask(newItem);
-    index++;
+    items.add(new DukeTask(newItem));
     return "Item added: " + newItem;
   }
 
   public String mark(int i) {
-    items[i - 1].markAsDone();
+    items.get(i - 1).markAsDone();
     return "Good job! This task has been completed:\n"
-        + items[i - 1];
+        + items.get(i - 1);
   }
 
   public String unmark(int i) {
-    items[i - 1].markAsNotDone();
+    items.get(i - 1).markAsNotDone();
     return "Whoops! This task is yet to be completed:\n"
-        + items[i - 1];
+        + items.get(i - 1);
   }
 
   @Override
   public String toString() {
     StringBuilder output = new StringBuilder("Your current tasks:\n");
-    for (int i = 0; i < index; i++) {
-      output.append((i + 1) + ". " + items[i] + "\n");
+    for (int i = 0; i < items.size(); i++) {
+      output.append((i + 1) + ". " + items.get(i) + "\n");
     }
     return output.toString();
   }
-
 }
