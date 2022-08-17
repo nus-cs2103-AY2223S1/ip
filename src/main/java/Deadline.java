965825af0b65d159a4cce80d740a860bb32186de
Chng Ian <88586264+chngchngchng@users.noreply.github.com>
2022-08-17 16:49:33 +0800

    main/java/*: Added functionality for Level 4

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task {
+
+    private String deadline;
+
+    Deadline(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D]%s (by: %s)", super.toString(), this.deadline);
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -29,11 +29,15 @@ public class Duke {
                 int index = Integer.valueOf(splitInput[1]) - 1;
                 if (splitInput[0].equals("mark")) {
                     tasks.get(index).markAsDone();
+                    System.out.println("=======================");
                     System.out.println("Nice! I've marked this task as having been completed:");
+                    System.out.println("=======================");
                     System.out.println(tasks.get(index));
                 } else {
                     tasks.get(index).markAsUndone();
+                    System.out.println("=======================");
                     System.out.println("Okay, I've marked this task as not done yet:");
+                    System.out.println("=======================");
                     System.out.println(tasks.get(index));
                 }
                 continue;
@@ -54,10 +58,34 @@ public class Duke {
                 }
                 System.out.println("=======================");
             } else {
-                tasks.add(new Task(input));
-                System.out.println("=======================");
-                System.out.println("added: " + input);
-                System.out.println("=======================");
+                Task newTask = null;
+                if (input.startsWith("todo")) {
+                    String description = input.substring(5);
+                    newTask = new Todo(description);
+                } else if (input.startsWith("deadline")) {
+                    int slashIdx = input.indexOf("/");
+                    String description = input.substring(9, slashIdx);
+                    String deadline = input.substring(slashIdx + 4);
+                    newTask = new Deadline(description, deadline);
+                } else if (input.startsWith("event")) {
+                    int slashIdx = input.indexOf("/");
+                    String description = input.substring(6, slashIdx);
+                    String eventDateTime = input.substring(slashIdx + 4);
+                    newTask = new Deadline(description, eventDateTime);
+                }
+
+                if (newTask != null) {
+                    tasks.add(newTask);
+                    System.out.println("=======================");
+                    System.out.println("Got it, i've added this task to your list:\n  " + newTask);
+                    System.out.println("You now have " + tasks.size() + " tasks in your list.");
+                    System.out.println("=======================");
+                } else {
+                    System.out.println("=======================");
+                    System.out.println("Command not recognised. Try again?");
+                    System.out.println("=======================");
+                }
+
             }
 
 
