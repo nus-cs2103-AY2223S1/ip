4784d84998d8aaba956439e7dc816d4b098af598
Steven Lim <limhwsteven@gmail.com>
2022-08-17 15:04:14 +0800

    Add basic input functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello there! My name's Duck");
+        System.out.println("Please type in a command...");
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String inputLine = input.nextLine();
+            String[] inputArr = inputLine.split(" ");
+            if (inputLine.equals("bye")) {
+                System.out.println("Bye! See you next time!");
+                input.close();
+                return;
+            }
+            System.out.println(inputLine);
+        }
     }
 }
