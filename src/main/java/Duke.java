f71aa74623697c2756ddb3377362a8b954c28997
tienyu2000 <tienyu2000@hotmail.com>
2022-08-17 20:40:25 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm nothing but an echo bot for now!\nWhat can I do for you, my highness?");
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Your highness:");
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Echoing: Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("Echoing: " + input);
+            }
+        }
     }
 }
