eb25aea3fff649386ec43678a36d35cebb68161b
cxyterence <e0725909@u.nus.edu>
2022-08-17 23:41:50 +0800

    changed DUKE to GustavoBot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanObj = new Scanner(System.in);   // Create Scanner object
+        String[] strLst = new String[100];
+
+        String intro = "Hello! My name is GustavoBot, but you can call me Gus\n"
+                + "How may I help you today?";
+
+        System.out.println(intro);
+        String act = scanObj.nextLine();            // read user input
+
+        switch (act) {
+            case "list":
+                System.out.println("list");
+                break;
+            case "blah":
+                System.out.println("blah");
+                break;
+            case "bye":
+                System.out.println("Bye. Hope to see you again!");
+                break;
+        }
     }
 }
