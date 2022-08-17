6b01ee3f73fbe3b80a1edc1dcfd471e2755e17a8
vantemoon <sylviassunrx0820@outlook.com>
2022-08-17 20:43:00 +0800

    Added the ability to mark tasks as done or not done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,20 +24,37 @@ public class Duke {
                 say("Bye. Hope to see you again soon.", true, true);
                 break;
             }
-            if (userInput.equals("list")) {
+            else if (userInput.equals("list")) {
                 // when user enters list
                 // display the current list
+                say("Here are the tasks in your list:", true, false);
                 for (int i = 0; i < task.size(); i++) {
                     boolean isFirstLine = false;
                     boolean isLastLine = false;
-                    if (i == 0) {
-                        isFirstLine = true;
-                    }
                     if (i == task.size() - 1) {
                         isLastLine = true;
                     }
-                    say(i + 1 + ". " + task.get(i).getDescription(), isFirstLine, isLastLine);
+                    say(i + 1 + ". [" + task.get(i).getStatusIcon() + "] "
+                            + task.get(i).getDescription(), isFirstLine, isLastLine);
                 }
+            }
+            else if (userInput.startsWith("mark") && userInput.split(" ", 2).length != 1) {
+                // when user enters mark and a number
+                // mark the corresponding task as done
+                int index = Integer.parseInt(userInput.split(" ", 2)[1]) - 1;
+                task.get(index).setStatus(true);
+                say("Nice! I've marked this task as done:", true, false);
+                say("  [" + task.get(index).getStatusIcon() + "] "
+                        + task.get(index).getDescription(), false, true);
+            }
+            else if (userInput.startsWith("unmark") && userInput.split(" ", 2).length != 1) {
+                // when user enters unmark and a number
+                // mark the corresponding task as not done
+                int index = Integer.parseInt(userInput.split(" ", 2)[1]) - 1;
+                task.get(index).setStatus(false);
+                say("OK, I've marked this task as not done yet:", true, false);
+                say("  [" + task.get(index).getStatusIcon() + "] "
+                        + task.get(index).getDescription(), false, true);
             }
             else {
                 // add user input to the list
