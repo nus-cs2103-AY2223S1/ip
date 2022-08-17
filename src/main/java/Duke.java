9e78273f7c04470f9c7c7fafe6eb6a0bed737e02
ama-chi <89921925+ama-chi@users.noreply.github.com>
2022-08-17 16:18:01 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,10 +12,20 @@ import java.util.Scanner;
 public class Duke {
 
     /**
-    * Main method initializes welcome message, and then calls echo method.
-    *
-    * @param args unused.
-    */
+     * Array used to store user inputs as a list.
+     */
+    static String[] wordList = new String[100];
+
+    /**
+     * Counter to keep track of the number of items in the list
+     */
+    static int count = 0;
+
+    /**
+     * Main method initializes welcome message, and then calls echo method.
+     *
+     * @param args unused.
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -23,30 +33,44 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        echo();
-    }
-
-     /**
-     * This method handles the echo loop. It echoes whatever the user inputs, but ends once
-     * the user enters "bye".
-     *
-     */
-     public static void echo() {
         String initMessage =
                 "    ____________________________________________________________\n" +
                 "     Hello! I'm Duke\n" +
-                "     What can I do for you?\n" +
+                "     I can store a to-do list for you!\n" +
+                "     Enter 'list' to display current contents and 'bye' to end the program.\n" +
+                "     Start entering your tasks!\n" +
                 "    ____________________________________________________________";
 
-         Scanner userInput = new Scanner(System.in);
-         System.out.println(initMessage);
+        System.out.println(initMessage);
+        list();
+    }
 
+    /**
+     * list collects user inputs into wordList, and updates count.
+     * Loop ends when user enters "bye".
+     *
+     */
+     public static void list() {
+         Scanner userInput = new Scanner(System.in);
          String input = userInput.nextLine();
+
          while (!Objects.equals(input.toLowerCase(), "bye")) {
-             System.out.printf(
-                     "    ____________________________________________________________\n" +
-                     "     %s\n" +
-                     "    ____________________________________________________________%n", input);
+
+             if (Objects.equals(input.toLowerCase(), "list")) {
+                 // readList function called to display list contents
+                readList();
+
+             } else {
+                 // wordList and count updated here, and shown to the user that item has been added
+                 wordList[count] = input;
+                 count++;
+
+                 System.out.printf(
+                         "    ____________________________________________________________\n" +
+                         "     added: %s\n" +
+                         "    ____________________________________________________________%n", input);
+             }
+
              input = userInput.nextLine();
          }
 
@@ -54,5 +78,19 @@ public class Duke {
                  "    ____________________________________________________________\n" +
                  "     Bye. Hope to see you again soon!\n" +
                  "    ____________________________________________________________\n");
+     }
+
+    /**
+     * Function prints all items in the list chronologically entered and numbered from 1 upwards.
+     *
+     */
+    public static void readList() {
+        System.out.println("    ____________________________________________________________\n");
+
+        for (int i = 1; i <= count; i++){
+            System.out.printf("%d. %s%n", i, wordList[i-1]);
+        }
+
+        System.out.println("    ____________________________________________________________\n");
      }
 }
