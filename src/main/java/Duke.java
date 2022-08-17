b34b42cd670847b24f208ca18997aeb188374e5c
ketamethane <cheonghsien@gmail.com>
2022-08-17 21:50:55 +0800

    Add level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Duke {
+
+    private static ArrayList list = new ArrayList<>();
+
+    private static void addToList(String task){
+        list.add(task);
+    }
+
+    private static void printList(){
+        for (int i=0; i < list.size(); i++){
+            String line = String.format("%s. %s", i + 1, list.get(i));
+            System.out.println(line);
+        }
+    }
+
     public static void main(String[] args) {
         String intro = "Hello! I'm Duke\nWhat can I do for you?";
         System.out.println("____________________________________________________");
         System.out.println(intro);
         System.out.println("____________________________________________________");
-        boolean closeFlag = false;
+//        boolean closeFlag = false;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -15,7 +30,13 @@ public class Duke {
         while (!scanner.hasNext("bye")) {
             String input = scanner.nextLine();
             System.out.println("____________________________________________________");
-            System.out.println(input);
+            if (input.equals("list")){
+                printList();
+            }
+            else {
+                addToList(input);
+                System.out.println("added: " + input);
+            }
             System.out.println("____________________________________________________");
         }
 
