fb661e10ce0b516bd4d8d511d8a16db5b80a5fb5
gerardstevan <gerardstevan0406@gmail.com>
2022-08-17 16:45:44 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,25 @@ import java.util.*;
 
 public class Duke {
 
+    private static void listOut(String[] stringList, int i) {
+        int j = 0;
+        System.out.println("_________________________");
+        while (j < i) {
+            System.out.println(j + ". " +stringList[j]);
+            j++;
+        }
+        System.out.println("_________________________");
+    }
+
+    private static void addString(String[] stringList, String str, int i) {
+        stringList[i] = str;
+    }
+
     public static void main(String[] args) {
 
+        String stringList[] = new String[100];
         Scanner sc = new Scanner(System.in);
+        int i = 0;
 
         System.out.println("_________________________");
         System.out.println("    Hi, my name is Doke");
@@ -15,8 +31,16 @@ public class Duke {
         String str= sc.nextLine();
 
         while (!str.equals("bye")) {
+            if (str.equals("list")) {
+                listOut(stringList, i);
+                str = sc.nextLine();
+                continue;
+            }
+
+            addString(stringList, str, i);
+            i++;
             System.out.println("_________________________");
-            System.out.println("    " + str);
+            System.out.println("added: " + str);
             System.out.println("_________________________");
             System.out.println();
 
