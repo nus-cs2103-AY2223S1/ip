7051b6872db9becfef6657991e9a547dd2cb0b85
johnrhimawan <johnr.himawan@gmail.com>
2022-08-17 18:13:00 +0800

    Add greet, echo, exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,48 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE = "    ____________________________________________________________\n";
+    private static final String INDENTATION = "     ";
+
+    private static boolean isDone = false;
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+
+        while (!isDone) {
+            String command = sc.nextLine();
+
+            switch (command) {
+                case "bye":
+                    bye();
+                    break;
+                default:
+                    printMessage(command);
+            }
+        }
+    }
+
+    private static void exitProgram() {
+        isDone = true;
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(LINE +
+                message + "\n" +
+                LINE);
+    }
+
+    private static void greet() {
+        String greetingMessage = INDENTATION + "Hello! I'm Duke\n" +
+                INDENTATION + "What can I do for you?\n";
+
+        printMessage(greetingMessage);
+    }
+
+    private static void bye() {
+        String byeMessage = INDENTATION + "Bye. Hope to see you again soon!\n";
+        printMessage(byeMessage);
+        exitProgram();
     }
 }
