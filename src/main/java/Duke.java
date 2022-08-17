103aee6351d90c8fc5a1083a958d033367bc1538
feliciagan <felicia.gan@me.com>
2022-08-17 12:47:32 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String banner = "★★★★★★★★★★★★\n"
+                + " TED ʕ•́ᴥ•̀ʔっ \n"
+                + "★★★★★★★★★★★★\n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm Ted and I'm here to help you keep track of your tasks ʕ•́ᴥ•̀ʔっ\n"
+                + "How can I assist you today?");
+
+        Scanner sc = new Scanner(System.in).useDelimiter("\\n");;
+
+        while (sc.hasNext()) {
+            String command = sc.next();
+
+            if (command.equals("bye")) {
+                System.out.println("Goodbye! Have a pleasant day ʕ•́ᴥ•̀ʔっ");
+                sc.close();
+                return;
+            }
+
+            System.out.println("~ ʕ•́ᴥ•̀ʔっ ~\n" + command + "\n" + "~\n");
+        }
     }
 }
