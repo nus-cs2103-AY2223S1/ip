5773617276771c5581702cea4c08ecd1b629cba6
nealetham <nealetham2000@gmail.com>
2022-08-17 14:53:45 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        String input = myObj.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = myObj.nextLine();
+        }
+
+        myObj.close();
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
