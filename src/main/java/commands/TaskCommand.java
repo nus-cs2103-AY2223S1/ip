cbfdb3fbf0f3b72f13e15f8fb47c0eb27a5500e2
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-08-17 15:06:45 +0800

    add text to be printed

--- src/main/java/commands/TaskCommand.java
+++ src/main/java/commands/TaskCommand.java
@@ -9,14 +9,15 @@ public class TaskCommand is Command {
   @Override
   public boolean performAction() {
     if (this.commandArgs[0].equals("todo")) {
-      this.tasks.add(Todo(commandArgs[1]));
+      this.tasks.add(new Todo(commandArgs[1]));
 
     } else if (this.commandArgs[0].equals("deadline")) {
-      this.tasks.add(Deadline(commandArgs[1], commandArgs[2]));
+      this.tasks.add(new Deadline(commandArgs[1], commandArgs[2]));
 
     } else if (this.commandArgs[0].equals("event")) {
-      this.tasks.add(Event(commandArgs[1], commandArgs[2]));
+      this.tasks.add(new Event(commandArgs[1], commandArgs[2]));
     } // TODO: else throw error
   }
+  System.out.println("Hey sweetie, I've added: '" + userInput + "' to your lists of tasks~\n");
   return true;
 }
