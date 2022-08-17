e77490ad30d2a4b718a12a72c10ec6123385b55d
Pinran-J <e0725480@u.nus.edu>
2022-08-18 00:55:41 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static void echo() {
+    public static void add_list() {
         Scanner myScan = new Scanner(System.in);
         String s;
+        String[] list = new String[100];
+        int list_counter = 0;
 
         while (true) {
             s = myScan.nextLine();
@@ -13,9 +15,19 @@ public class Duke {
                 System.out.println("Bye, hope to see you again!");
                 System.out.println("----------------------");
                 break;
+            } else if (s.equals("list")) {
+                for (int i = 0; i < 100; i++) {
+                    if (list[i] != null) {
+                        String display = String.format("%d. %s", i + 1, list[i]);
+                        System.out.println(display);
+                    }
+                }
+
             } else {
+                list[list_counter] = s;
+                list_counter += 1;
                 System.out.println("----------------------");
-                System.out.println(s);
+                System.out.println("added: " + s);
                 System.out.println("----------------------");
             }
         }
@@ -34,6 +46,6 @@ public class Duke {
         System.out.println("What can i do for you?");
         System.out.println("----------------------");
 
-        echo();
+        add_list();
     }
 }
