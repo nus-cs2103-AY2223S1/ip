e2fef3c34ab236b98ea0b6667d87dfd339125c9a
ichigh0st <cindyshen336@gmail.com>
2022-08-17 16:21:43 +0800

    Implement skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        char triangle = '\u25B8';
+        String start = triangle + " ";
+        System.out.println(start + "Hi, I'm Duke!\n" + start + "What would you like to do today?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(start + "Bye! I hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(start + input);
+            }
+        }
     }
 }
