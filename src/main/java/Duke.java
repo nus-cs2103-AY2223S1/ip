f1b91d97a6295558cd0dd123c0bed177ca9092f7
linuschancs <mrlinuschan@gmail.com>
2022-08-17 11:56:47 +0800

    Added tracking of Deadline/Event/Task and counter for lists

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
         System.out.println("Greetings Human! I am BetaGo, your personal robot assistant!\nHow may I assist you today?\n");
         Scanner sc = new Scanner(System.in);
         String str= sc.nextLine();
-        String[] inputs = str.split(" ");
+        String[] inputs = str.split(" ", 2);
         Task[] storage = new Task[100];
         int counter = 0;
 
@@ -18,8 +18,9 @@ public class Duke {
                     System.out.print("Here are the tasks in your list:\n");
                     for (int i = 0; i < counter; i++) {
                         System.out.print(i+1);
-                        System.out.println(". [" + storage[i].getStatusIcon() + "] " + storage[i].getTaskDescription() + "\n");
+                        System.out.println(". " + storage[i].toString());
                     }
+                    System.out.print("\n");
                 }
             } else if (inputs[0].equalsIgnoreCase("mark")) {
                 if (inputs.length != 2) {
@@ -45,13 +46,32 @@ public class Duke {
                     }
                 }
 
-            } else {
-                System.out.println("Task added: " + str + "\n");
-                storage[counter] = new Task(str);
+            } else if (inputs[0].equalsIgnoreCase("todo")) {
+                Todo temp = new Todo(inputs[1]);
+                storage[counter] = temp;
                 counter++;
+                System.out.println("Got it. I've added this task:\n" + temp.toString());
+                System.out.println("Now you have " + counter + " tasks in the list.\n");
+            } else if (inputs[0].equalsIgnoreCase("deadline")) {
+                String[] when = inputs[1].split(" /by ", 2);
+                System.out.println(when[0]);
+                Deadline temp = new Deadline(when[0], when[1]);
+                storage[counter] = temp;
+                counter++;
+                System.out.println("Got it. I've added this task:\n" + temp.toString());
+                System.out.println("Now you have " + counter + " tasks in the list.\n");
+            } else if (inputs[0].equalsIgnoreCase("event")) {
+                String[] where = inputs[1].split(" /at ", 2);
+                Event temp = new Event(where[0], where[1]);
+                storage[counter] = temp;
+                counter++;
+                System.out.println("Got it. I've added this task:\n" + temp.toString());
+                System.out.println("Now you have " + counter + " tasks in the list.\n");
+            } else {
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(\n");
             }
             str = sc.nextLine();
-            inputs = str.split(" ");
+            inputs = str.split(" ", 2);
         }
         System.out.println("Bye. Hope to see you again soon!\n");
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -20,12 +20,12 @@ public class Task {
         public void markAsDone() {
             this.isDone = true;
             System.out.println("Nice! I've marked this task as done:");
-            System.out.println("[X] " + this.description +"\n");
+            System.out.println(this.toString() + "\n");
         }
 
         public void markAsNotDone() {
             this.isDone = false;
             System.out.println("Nice! I've marked this task as not done yet:");
-            System.out.println("[ ] " + this.description +"\n");
+            System.out.println(this.toString() + "\n");
         }
 }
