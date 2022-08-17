b024ee9a98e60b7f1461bf2b45c5b8e3ebf07744
GenFusion122 <infusion122@hotmail.com>
2022-08-17 13:48:20 +0800

    Completed Level-2

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,4 +1,4 @@
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duck {
@@ -6,9 +6,17 @@ public class Duck {
         System.out.println("Hello! Got any grapes?");
         Scanner scanner = new Scanner(System.in);
         String word = "";
+        ArrayList<String> list = new ArrayList<>();
         while (!word.equals("bye")) {
             word = scanner.next();
-            System.out.println(word);
+            if (word.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i+1 + ". " + list.get(i));
+                }
+            }
+            System.out.println("added: " + word);
+            list.add(word);
         }
+        System.out.println("Quack!");
     }
 }
