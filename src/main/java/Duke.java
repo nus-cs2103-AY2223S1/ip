17bdd38fccb9ffa74628a1791093949c67334c2d
Rexong <rexonghanwei200035@gmail.com>
2022-08-18 01:12:14 +0800

    Implemented Duke Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,58 @@
+import java.util.Scanner;
+/**
+ * Duke is a personal assistant chatbot.
+ * It helps a person to keep track of various things.
+ *
+ * @author Rexong
+ */
 public class Duke {
+    private static final String INTRODUCE_MESSAGE = "Hello! I'm Duke";
+    private static final String ASK_MESSAGE = "What can I do for you?";
+    private static final String EXIT_MESSAGE = "Bye. Hope to see you again soon!";
+
+    /**
+     * Print messages passed into the parameter
+     *
+     * @param message Message to be logged
+     */
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    /**
+     * Print both introduce and ask messages
+     */
+    public void printGreetMessage() {
+        printMessage(INTRODUCE_MESSAGE);
+        printMessage(ASK_MESSAGE);
+    }
+
+    /**
+     * Print exit message
+     */
+    public void printExitMessage() {
+        printMessage(EXIT_MESSAGE);
+    }
+
+    /**
+     * Echos commands entered by user
+     *
+     * @param message Message entered by the user to be echoed
+     */
+    public void echoMessage(String message) {
+        printMessage(message);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        Scanner scanner = new Scanner(System.in);
+
+        duke.printGreetMessage();
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            duke.echoMessage(command);
+            command = scanner.nextLine();
+        }
+        duke.printExitMessage();
     }
 }
