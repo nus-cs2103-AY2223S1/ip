e4505d5198fb2c15bfc10957035d024aba03b9ef
Chng Ian <88586264+chngchngchng@users.noreply.github.com>
2022-08-17 15:04:22 +0800

    Duke.java: Added functionality to echo user input for Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+    // Currently, the main function takes in user input and echoes it to the user
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello, I am Duke. \nWhat can I do for you? :)\n=======================");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.toLowerCase().equals("bye")) {
+                System.out.println("Bye! Hope to see you again soon!");
+                break;
+            };
+
+            System.out.println("=======================");
+            System.out.println(input);
+            System.out.println("=======================");
+        }
+
+        return;
+
     }
 }
