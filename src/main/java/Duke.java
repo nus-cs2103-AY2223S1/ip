45f74bd4260eedb4a2f87fd84ae2c4ddd9dec3fc
ErvinK123 <ervinkin@yahoo.com.sg>
2022-08-18 01:36:53 +0800

    Level 1 echo functionality of puke chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void echo(Scanner sc) {
+        String s = sc.nextLine();
+        if (s.equals("bye")){
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    Bye. Why are you still here?");
+            System.out.println("    ____________________________________________________________");
+            return;
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    " + s);
+        System.out.println("    ____________________________________________________________");
+        echo(sc);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner receiver = new Scanner(System.in);
+
+        String logo = " ____         _        \n"
+                + "|   | \\ _   _| | _____ \n"
+                + "|  _|  | | | | |/ / _ \\\n"
+                + "| |    | |_| |   <  __/\n"
+                + "|_|    \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from puke\n" + logo);
+
+        echo(receiver);
+
     }
 }
