617ac8448acc1efb226a73b81e8c35b361ed771d
yeoyujie <yeo.yujie@gmail.com>
2022-08-18 04:42:12 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,18 +6,37 @@ public class Duke {
         System.out.println("Hello! I'm SoCCat \nWhat can I do for you?");
         Scanner input = new Scanner(System.in);
         String currInput = input.nextLine();
-        String[] userInputs = new String[100];
-        int index = 0;
+        Task[] userInputs = new Task[100];
+        int index = 1;
 
         while (!currInput.equals("bye")) {
             if (currInput.equals("list")) {
-                for (int i = 0; i < index; i++) {
-                    System.out.println(i + ". " + userInputs[i]);
+                for (int i = 1; i < index; i++) {
+                    System.out.println(i + ".[" + userInputs[i].getStatusIcon() + "] " + userInputs[i].getDescription());
                 }
-            } else {
-                System.out.println("added: " + currInput);
-                userInputs[index] = currInput;
-                index++;
+            }  else {
+                String[] words = currInput.split(" ", 2);
+                if (words.length < 2) {
+                    System.out.println("added: " + currInput);
+                    userInputs[index] = new Task(currInput);
+                    index++;
+                } else {
+                    int taskIndex = Integer.parseInt(words[1]);
+
+                    if (words[0].equals("mark")) {
+                        if (taskIndex <= index) {
+                            System.out.println(userInputs[taskIndex].mark());
+                        }
+                    } else if (words[0].equals("unmark")) {
+                        if (taskIndex <= index) {
+                            System.out.println(userInputs[taskIndex].unmark());
+                        }
+                    } else {
+                        System.out.println("added: " + currInput);
+                        userInputs[index] = new Task(currInput);
+                        index++;
+                    }
+                }
             }
                 currInput = input.nextLine();
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String mark() {
+        this.isDone = true;
+        return "Nice! I've marked this task as done: \n" + this;
+
+    }
+
+    public String unmark() {
+        this.isDone = false;
+        return "Ok! I've marked this task as not done yet: \n" + this;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + getDescription();
+    }
+
+}
