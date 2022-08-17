1603c6e04805cfcfddcd39532d4bba03e29bc8b7
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-08-17 12:44:32 +0800

    Add IOFormat

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import static pnkp.duke.IOFormat.say;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,6 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        say("Hello from\n" + logo);
     }
 }
---
+++ src/main/java/pnkp/duke/IOFormat.java
@@ -0,0 +1,8 @@
+package pnkp.duke;
+
+public class IOFormat {
+    public static void say(String text) {
+        System.out.println("\uD83D\uDCAC " + text);
+        System.out.println("-----");
+    }
+}
