9bd3f3ee0369a1b4cb0648b1bee3561de2bd2fe0
jamie tan <jamietan2002@gmail.com>
2022-08-17 14:26:05 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,20 +2,25 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm your mom.\n What can I do for you?\n");
+        System.out.println("Hello! I'm Jamie.\nWhat can I do for you?\n");
+        String[] lst = new String[100];
+        int count = 0;
+
         while(true) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
             }
+            else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i+1) + ". " + lst[i]);
+                }
+            }
             else {
-                System.out.println(input);
+                lst[count] = input;
+                System.out.println("added: " + input);
+                count++;
             }
         }
     }
