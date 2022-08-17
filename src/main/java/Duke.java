a416dbe1ad655eccc5f2f9db8d8182ba5a889f29
Min Jiun Chia <cminjiun@gmail.com>
2022-08-17 10:52:32 +0800

    Updated Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static String indent = "       ";
+
     public static void main(String[] args) {
         System.out.println("\n Hello there! \n"
                 + "\n My name is Zelk, nice to meet you :D \n"
@@ -9,11 +12,11 @@ public class Duke {
         Scanner s = new Scanner(System.in);
         String input = "";
         while (!(input = s.nextLine()).equals("bye")) {
-            System.out.println("        " + input
+            System.out.println(indent + input
                     + "\n ___________________________________________________________________");
         }
 
-        System.out.println("        " + "Bye! Hope to see you again soon! Thank you for chatting with me :)");
+        System.out.println(indent + "Bye! Hope to see you again soon! Thank you for chatting with me :)");
 
 
 
