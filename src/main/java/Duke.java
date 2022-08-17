c053b7165a2b0ddb5a47bd437dc86181eff9a09d
DESKTOP-LBC8HLP\User <greggchan12345@gmail.com>
2022-08-17 20:59:14 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner reply = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+
+        String currreply = reply.nextLine();
+
+        while (!currreply.equals("bye")) {
+            System.out.println(currreply + "\n");
+            currreply = reply.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
