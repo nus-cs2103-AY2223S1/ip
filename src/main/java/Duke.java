f0af634c3d919fba51992ecade9cdac3508dfe19
paotheroo <lowzanhao@gmail.com>
2022-08-17 15:17:40 +0800

    Duke: Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,8 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
 
+        String[] list = new String[100];
+        int i = 0;
         boolean loop = true;
         System.out.println("Hello! Duke here, how can I help you?");
         Scanner sc = new Scanner(System.in);
@@ -17,10 +19,21 @@ public class Duke {
         while (loop) {
             if (sc.hasNext("bye")) {
                 loop = false;
-            } else {
-                String str = sc.nextLine();
-                System.out.println(str);
+                break;
             }
+
+            if (sc.hasNext("list")) {
+                for (int j = 0; list[j] != null; j++) {
+                    System.out.println(j+1 + ": " + list[j]);
+                }
+                sc.nextLine();
+                continue;
+            }
+
+            String str = sc.nextLine();
+            list[i] = str;
+            i++;
+            System.out.println("added: " + str);
         }
 
         System.out.println("Thanks for having me!\nHave a nice day ahead!");
