0111d427016a0252b3f8477d2c903c541589e2bf
ish1506 <ishiuanlee@gmail.com>
2022-08-17 19:26:36 +0800

    Implement Skeletal Duke chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Duke";
+        System.out.println("Hello! I'm " + name + "\nHow can I help you?");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye! See you again :)");
+                return;
+            }
+            System.out.println(command);
+        }
+
     }
 }
