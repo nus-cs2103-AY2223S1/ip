6da08deaceccde6ef97b207d14fe99f736a11324
Ssatu <69383469+Ssatu@users.noreply.github.com>
2022-08-17 12:09:34 +0800

    Completed level 6 and added more exception handling to mark and unmark.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -31,23 +31,48 @@ public class Duke {
      * @param tasks The task-list.
      * @param numTasks The number of tasks currently.
      */
-    public static void addTask(String noType, String type, Task[] tasks, int numTasks) {
+    public static void addTask(String noType, String type, ArrayList<Task> tasks, int numTasks)
+            throws DukeException {
         String confirmation = "Got it. I've added this task:\n  ";
         String number = "\nNow you have " + (numTasks + 1) + " tasks in the list.";
 
-        if(type.equals("deadline")) {
-            String[] split = noType.split(" /by ");
-            tasks[numTasks] = new Deadline(split[0], split[1]);
-            say(confirmation + tasks[numTasks] + number);
-        } else if(type.equals("event")){
-            String[] split = noType.split(" /at ");
-            tasks[numTasks] = new Event(split[0], split[1]);
-            say(confirmation  + tasks[numTasks] + number);
-        } else {
-            tasks[numTasks] = new ToDo(noType);
-            say(confirmation + tasks[numTasks] + number);
+        if(noType == ""){
+            throw new DukeException("☹ OOPS!!! The description of a " + type +" cannot be empty.");
         }
 
+        if(type.equals("deadline")) {
+            String[] split = noType.split(" /by ");
+            if(split.length != 2){
+                throw new DukeException("☹ OOPS!!! A single deadline must be specified." +
+                        "e.g. deadline finish work /by June 9th");
+            }
+            tasks.add(numTasks, new Deadline(split[0], split[1]));
+            say(confirmation + tasks.get(numTasks) + number);
+        } else if(type.equals("event")){
+            String[] split = noType.split(" /at ");
+            if(split.length != 2){
+                throw new DukeException("☹ OOPS!!! A single timeframe must be specified." +
+                        "e.g. event meeting /at Thursday 2-4pm");
+            }
+            tasks.add(numTasks, new Event(split[0], split[1]));
+            say(confirmation  + tasks.get(numTasks) + number);
+        } else {
+            tasks.add(numTasks, new ToDo(noType));
+            say(confirmation + tasks.get(numTasks) + number);
+        }
+
+    }
+
+    public static void deleteTask(String index, ArrayList<Task> tasks, int numTasks) throws  DukeException{
+        if(!index.matches("\\d+")) {
+            throw new DukeException("☹ OOPS!!! Please specify an integer index.");
+        } else if(Integer.valueOf(index) > numTasks) {
+            throw new DukeException("☹ OOPS!!! The index specified is out of range.");
+        }
+        Task task = tasks.get(Integer.valueOf(index) - 1);
+        tasks.remove(Integer.valueOf(index) - 1);
+        say("Noted. I've removed this task:\n  " + task + "\nNow you have " + (numTasks - 1) +
+                " tasks in the list.");
     }
 
     /**
@@ -55,9 +80,14 @@ public class Duke {
      * @param index The index of the task.
      * @param tasks The task-list.
      */
-    public static void markTask(String index, Task[] tasks) {
-        //Note potential exception: Task index exceeded
-        Task task = tasks[Integer.valueOf(index) - 1];
+    public static void markTask(String index, ArrayList<Task> tasks, int numTasks)
+            throws DukeException {
+        if(!index.matches("\\d+")) {
+            throw new DukeException("☹ OOPS!!! Please specify an integer index.");
+        } else if(Integer.valueOf(index) > numTasks) {
+            throw new DukeException("☹ OOPS!!! The index specified is out of range.");
+        }
+        Task task = tasks.get(Integer.valueOf(index) - 1);
         task.mark();
         say("Nice! I've marked this task as done:\n  " + task);
     }
@@ -67,9 +97,15 @@ public class Duke {
      * @param index The index of the task.
      * @param tasks The task-list.
      */
-    public static void unmarkTask(String index, Task[] tasks) {
+    public static void unmarkTask(String index, ArrayList<Task> tasks, int numTasks)
+            throws DukeException {
         //Note potential exception: Task index exceeded
-        Task task = tasks[Integer.valueOf(index) - 1];
+        if(!index.matches("\\d+")) {
+            throw new DukeException("☹ OOPS!!! Please specify an integer index.");
+        } else if(Integer.valueOf(index) > numTasks) {
+            throw new DukeException("☹ OOPS!!! The index specified is out of range.");
+        }
+        Task task = tasks.get(Integer.valueOf(index) - 1);
         task.unmark();
         say("OK, I've marked this task as not done yet:\n  " + task);
     }
@@ -79,13 +115,13 @@ public class Duke {
      * @param tasks The task-list.
      * @param numTasks The number of tasks currently.
      */
-    public static void listTasks(Task[] tasks, int numTasks) {
+    public static void listTasks(ArrayList<Task> tasks, int numTasks) {
         String list = "Here are the tasks in your list:\n";
         for(int i = 0; i < numTasks; i++) {
             //Add index
             list +=  (i+1) + ".";
             //Add task
-            list += tasks[i];
+            list += tasks.get(i);
             //Add nextline
             if(i!=numTasks -1) {
                 list += "\n";
@@ -97,7 +133,7 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>(100);
         int numTasks = 0;
 
         say("Hello! I'm Pawl\nWhat can I do for you?");
@@ -117,16 +153,36 @@ public class Duke {
 
             }else if(first.equals("mark")) {
                 //Mark a task as done
-                markTask(rest, tasks);
-
+                try {
+                    markTask(rest, tasks, numTasks);
+                } catch (DukeException ex) {
+                    say(ex.toString());
+                }
             }else if(first.equals("unmark")) {
                 //Mark a task as not done
-                unmarkTask(rest, tasks);
-
+                try {
+                    unmarkTask(rest, tasks, numTasks);
+                } catch (DukeException ex) {
+                    say(ex.toString());
+                }
+            } else if(first.equals("delete")) {
+                //Delete a task at a given index
+                try {
+                    deleteTask(rest, tasks, numTasks);
+                    numTasks --;
+                } catch (DukeException ex) {
+                    say(ex.toString());
+                }
             } else if(first.equals("todo") || first.equals("deadline") || first.equals("event")){
                 //Else, add task to list
-                addTask(rest, first, tasks, numTasks);
-                numTasks++;
+                try {
+                    addTask(rest, first, tasks, numTasks);
+                    numTasks++;
+                } catch (DukeException ex) {
+                    say(ex.toString());
+                }
+            } else {
+                say("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
             input = scn.nextLine();
         }
