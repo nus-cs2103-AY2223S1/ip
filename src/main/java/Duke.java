8822d2b9f6542aa02c775ccdc6e82051f38188bb
danzzzerl <danzzzerl@gmail.com>
2022-08-17 18:12:02 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         // initialise variables
-        String inputArray[] = new String[100];
+        Task inputArray[] = new Task[100];
         int counter = 0;
 
         // print out starting message
@@ -15,27 +15,51 @@ public class Duke {
         while (true) {
             // take in input every loop
             String input = sc.nextLine();
+            String[] arrOfInput = input.split(" ");
 
             if (!input.equals("bye")) {
                 // for when input is anything but bye
+
                 if (!input.equals("list")) {
                     // for when input is anything but list
-                    counter++;
 
-                    // message when new element is added to list
-                    System.out.println("added: " + input);
+                    if (arrOfInput[0].equals("mark")) {
+                        // to mark an element as done
+                        int index = Integer.parseInt(arrOfInput[1]);
+                        if (inputArray[index] != null) {
+                            Task currTask = inputArray[index];
+                            currTask.markAsDone();
+                        }
 
-                    // add the element to the array
-                    String currElement = counter + ". " + input;
-                    inputArray[counter] = currElement;
+                    } else if (arrOfInput[0].equals("unmark")) {
+                        // to mark an element as undone
+                        int index = Integer.parseInt(arrOfInput[1]);
+                        if (inputArray[index] != null) {
+                            Task currTask = inputArray[index];
+                            currTask.markUndone();
+                        }
+
+                    } else {
+                        // adding the element to the task list
+                        counter++;
+
+                        // message when new element is added to list
+                        System.out.println("added: " + input);
+
+                        // add the element to the array
+                        inputArray[counter] = new Task(input);
+                    }
+
                 } else {
                     // list out the current list
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < inputArray.length; i++) {
                         if (inputArray[i] != null) {
-                            System.out.println(inputArray[i]);
+                            System.out.println(i + ". " + inputArray[i]);
                         }
                     }
                 }
+
             } else {
                 // end program when input is bye
                 System.out.println("Bye. Hope to see you again soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done:\n  " + this.toString());
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+        System.out.println("OK, I've marked this task as not done yet:\n  " + this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
