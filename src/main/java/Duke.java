d5f158bbf18c66487b037ae3793af0b1d31e0ea6
JJoeYi <joelowyi@gmail.com>
2022-08-17 13:16:45 +0800

    Level-2, edited Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,7 +25,51 @@ public class Duke {
             }
         }
     }
+
+    public static void echoBackList() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\t-----------------------------------------------");
+        System.out.println("\tHello! I'm Duke Dukem\n\tWhat can I do for you?");
+        System.out.println("\t-----------------------------------------------");
+
+        String[] lst = new String[100];
+        int counter = 0;
+
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+
+            if (line.equals("bye")) {
+                scanner.close();
+                System.out.println("\t-----------------------------------------------");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t-----------------------------------------------");
+                break;
+            } else if (line.equals("list")) {
+              if (counter > 0) {
+                  System.out.println("\t-----------------------------------------------");
+                  for (int i = 0; i < counter; i++) {
+                      System.out.println("\t"+ (i+1) + ". " + lst[i] + "\n");
+                  }
+                  System.out.println("\t-----------------------------------------------");
+              } else { // lst is not initalized
+                  System.out.println("\t-----------------------------------------------");
+                  System.out.println("\tList is empty!");
+                  System.out.println("\t-----------------------------------------------");
+              }
+            } else {
+                System.out.println("\t-----------------------------------------------");
+                System.out.println("\tadded: " + line);
+                System.out.println("\t-----------------------------------------------");
+                if (counter == 0) {
+                    lst = new String[100];
+                }
+                lst[counter] = line;
+                counter++;
+            }
+        }
+
+    }
     public static void main(String[] args) {
-        echoUser();
+        echoBackList();
     }
 }
