e800dfeec08995b9192b6922cbd6d947d7855b4d
JJoeYi <joelowyi@gmail.com>
2022-08-17 17:04:33 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -207,6 +207,11 @@ public class Duke {
         }
     }
     public static void main(String[] args) {
-        level4();
+        String logo = " ____        _\n"
+            + "|  _ \\ _   _| | _____\n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
 }
