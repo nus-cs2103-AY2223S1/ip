389e850d7019f635679dd8cbb8f3cea67774546d
xhphoong <xhphoong@gmail.com>
2022-08-18 02:07:49 +0800

    added Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        SkeletonDuke duke = new SkeletonDuke();
+        duke.greet();
+        while(sc.hasNext()) {
+            String command = sc.next();
+            if(command.equals("bye")) {
+                duke.exit();
+                break;
+            } else {
+                duke.echo(command);
+            }
+        }
+
     }
 }
---
+++ src/main/java/SkeletonDuke.java
@@ -0,0 +1,19 @@
+class SkeletonDuke {
+
+    SkeletonDuke() {
+    }
+
+    void greet() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    void echo(String s) {
+        System.out.println(s);
+    }
+
+    void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+}
