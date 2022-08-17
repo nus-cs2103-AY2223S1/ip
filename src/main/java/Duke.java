377e4fe092c2f19e86f5077ec78ea4c7bda4a28f
april-anh <conyeudau9@gmail.com>
2022-08-17 15:07:00 +0800

    Adding constant and method in level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,49 @@
+import java.util.Scanner;
+
 public class Duke {
+    // Logo
+    private static final String LOGO =
+            "   _____    _____   ___    __    ___    ____    _______ \n" +
+            "  / ____|  / ____| |__ \\  /_ |  / _ \\  |___ \\  |__   __|\n" +
+            " | |      | (___      ) |  | | | | | |   __) |    | |   \n" +
+            " | |       \\___ \\    / /   | | | | | |  |__ <     | |   \n" +
+            " | |____   ____) |  / /_   | | | |_| |  ___) |    | |   \n" +
+            "  \\_____| |_____/  |____|  |_|  \\___/  |____/     |_|   \n";
+
+    // Arrow to start the answer
+    private static final String ARROW = "--> ";
+
+    // Keyword to quit the program
+    private static final String EXIT = "bye";
+
+    private static void greet() {
+        System.out.println("Hello I am\n" + Duke.LOGO);
+        System.out.println("May I help you?");
+    }
+
+    private static void echo(String input) {
+        System.out.println(Duke.ARROW + input);
+    }
+
+    private static void exit() {
+        System.out.println("Great that you joined. See you soon. Bye!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Greeting
+        Duke.greet();
+
+        // User Input
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        // Echoing
+        while (!userInput.equals(Duke.EXIT)) {
+            Duke.echo(userInput);
+            userInput = scanner.nextLine();
+        }
+
+        // Bye
+        Duke.exit();
     }
 }
