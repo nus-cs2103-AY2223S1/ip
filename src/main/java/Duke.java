80c713901cd2f04f75e42d3942fb610cd2bb0354
ShenyiCui <shenyicui@outlook.com>
2022-08-18 00:59:42 +0800

    feature: complete level-1 criteria
    - add echo to user input
    - loop cli infinitely
    
    addresses ip-level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        String LOGO = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello from\n" + LOGO);
+
+        while (true) {
+            System.out.println("------------------------------");
+            System.out.println("What can I do for you?");
+            System.out.print(": ");
+            String userInput = scanner.nextLine();
+            System.out.println("------------------------------");
+            System.out.println(userInput + "\n");
+        }
     }
 }
