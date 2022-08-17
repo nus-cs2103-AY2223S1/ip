b616c271fcdb38785a332c70229e2bdc94749ef6
Chng Ian <88586264+chngchngchng@users.noreply.github.com>
2022-08-17 16:00:20 +0800

    Task.java, Duke.java: Implemented Level 3 Functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Duke {
 
     // String array used to store tasks
-    private static final List<String> tasks = new ArrayList<>();
+    private static final List<Task> tasks = new ArrayList<>();
 
     // Currently, the main function takes in user input and echoes it to the user
     public static void main(String[] args) {
@@ -20,6 +20,25 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
+
+            // Splits the input of the string into a string array
+            String[] splitInput = input.split(" ");
+
+            // For mark and unmark, the string array should be of length 2 only
+            if (splitInput.length == 2 && (splitInput[0].equals("mark") || splitInput[0].equals("unmark"))) {
+                int index = Integer.valueOf(splitInput[1]) - 1;
+                if (splitInput[0].equals("mark")) {
+                    tasks.get(index).markAsDone();
+                    System.out.println("Nice! I've marked this task as having been completed:");
+                    System.out.println(tasks.get(index));
+                } else {
+                    tasks.get(index).markAsUndone();
+                    System.out.println("Okay, I've marked this task as not done yet:");
+                    System.out.println(tasks.get(index));
+                }
+                continue;
+            }
+
             if (input.toLowerCase().equals("bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
@@ -27,15 +46,15 @@ public class Duke {
                 int counter = 1;
                 System.out.println("=======================");
                 System.out.println("Here are the tasks that you have added to the list: ");
-                for (String s: tasks) {
-                    if (s != null) {
-                        System.out.println(counter + ". " + s);
+                for (Task task: tasks) {
+                    if (task != null) {
+                        System.out.println(counter + ". " + task);
                         counter++;
                     }
                 }
                 System.out.println("=======================");
             } else {
-                tasks.add(input);
+                tasks.add(new Task(input));
                 System.out.println("=======================");
                 System.out.println("added: " + input);
                 System.out.println("=======================");
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    private String description;
+    private Boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return isDone? "X" : " ";
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
+}
