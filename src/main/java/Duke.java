48bbda157691ea3e839f9fe9ff2fba597405c7b6
Chee Zhong Wei <cheezhongwei00@gmail.com>
2022-08-17 16:16:45 +0800

    Added echoing to Thesh

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("-----------------------------------------------");
+        System.out.println("| Hi this is Thesh. What can I do for you? |");
+        System.out.println("-----------------------------------------------");
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String command = sc.nextLine();
+            if (!command.equals("bye")) {
+                System.out.println(command);
+                continue;
+            }
+            System.out.println("Bye. Hope to see you again soon!");
+            break;
+        }
+
     }
 }
