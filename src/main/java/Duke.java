03192da08dd2893416ac0bb83165eb9b3962e366
ketamethane <cheonghsien@gmail.com>
2022-08-18 01:32:17 +0800

    Add level-5 and level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,14 +6,18 @@ public class Duke {
 
     private static ArrayList<Task> list = new ArrayList<>();
 
-    private static void addTask(String taskType, String input) {
-        System.out.println("Got it. I've added this task:");
+    private static void addTask(String taskType, String input) throws DukeException {
+
         switch(taskType){
             case "todo":
                 String[] removeTaskType = input.split("todo ");
                 String description = String.join("", removeTaskType);
+                if (description.equals("todo")) {
+                    throw new DukeException("");
+                }
                 Task todo = new ToDo(description);
                 list.add(todo);
+                System.out.println("Got it. I've added this task:");
                 System.out.println("  " + todo);
                 break;
             case "deadline":
@@ -24,6 +28,7 @@ public class Duke {
                 String dueTime = sliceByDesAndBy[1];
                 Task deadline = new Deadline(description2, dueTime);
                 list.add(deadline);
+                System.out.println("Got it. I've added this task:");
                 System.out.println("  " + deadline);
                 break;
             case "event":
@@ -34,6 +39,7 @@ public class Duke {
                 String dueTime2 = sliceByDesAndBy2[1];
                 Task event = new Event(description3, dueTime2);
                 list.add(event);
+                System.out.println("Got it. I've added this task:");
                 System.out.println("  " + event);
                 break;
 
@@ -63,7 +69,21 @@ public class Duke {
         task.unmark();
     }
 
-    public static void main(String[] args) {
+    private static void deleteTask(int num){
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(list.get(num - 1));
+        list.remove(num - 1);
+        String numTask = String.format("Now you have %s tasks in the list.", list.size());
+        System.out.println(numTask);
+    }
+
+    static void validate(String str, String type) throws DukeException {
+        if (!type.equals("todo") && !type.equals("deadline") && !type.equals("event")) {
+            throw new DukeException("");
+        }
+    }
+
+    public static void main(String[] args) throws DukeException{
         String intro = "Hello! I'm Duke\nWhat can I do for you?";
         System.out.println("____________________________________________________");
         System.out.println(intro);
@@ -96,8 +116,22 @@ public class Duke {
                 }
                 String output = String.format("OK, I've marked this task as not done yet:\n%s", list.get(taskNum - 1));
                 System.out.println(output);
+            } else if (inputArr[0].equals("delete")) {
+                int taskNum = Integer.parseInt(inputArr[1]);
+                deleteTask(taskNum);
             } else if (inputArr[0].equals("todo") || inputArr[0].equals("deadline") || inputArr[0].equals("event")){
-                addTask(inputArr[0], input);
+                try {
+                    addTask(inputArr[0], input);
+                } catch (IndexOutOfBoundsException | DukeException e) {
+                    String output = String.format("Oops!! The description of a %s cannot be empty", inputArr[0]);
+                    System.out.println(output);
+                }
+            } else {
+                try {
+                    validate(input, inputArr[0]);
+                } catch (DukeException e) {
+                    System.out.println("Oh no!! I'm sorry, but I don't know what that means :(");
+                }
             }
 
             System.out.println("____________________________________________________");
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,14 @@
+public class DukeException extends Exception{
+
+    public DukeException(String message) {
+        super(message);
+    }
+
+
+
+//    public static void main(String[] args) {
+//        try {
+//
+//        }
+//    }
+}
