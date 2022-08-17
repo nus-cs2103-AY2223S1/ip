ffa13c0cfdb3a90ec599b56eae5b279f9bd39428
Jessica Mary <jessicamary472@gmail.com>
2022-08-17 23:25:31 +0800

    Create Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String indent = "     ";
+        String divider = "  ____________________________________________________________\n";
+        String logo = indent + "____        _        \n"
+                + indent + "|  _ \\ _   _| | _____ \n"
+                + indent + "| | | | | | | |/ / _ \\\n"
+                + indent + "| |_| | |_| |   <  __/\n"
+                + indent + "|____/ \\__,_|_|\\_\\___|\n\n";
+        String openingStatement = indent + "Hello! I'm Duke.\n"
+                + indent + "What can I do for you?\n";
+        System.out.println(divider + logo + openingStatement + divider);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(divider + indent + input + "\n" + divider);
+        }
+
+        String exitStatement = "Bye. Hope to see you again soon! :)";
+        System.out.println(divider + indent + exitStatement + "\n" + divider);
+        scanner.close();
     }
 }
