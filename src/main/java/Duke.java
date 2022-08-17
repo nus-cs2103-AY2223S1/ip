2ad818990c10b0de1ef2b2408a9e49e04555efe0
joosxi <64764352+joosxi@users.noreply.github.com>
2022-08-17 14:24:49 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+    static Scanner input = new Scanner(System.in);
+    static String response = "";
+
+    public static void responseRepeater() {
+        response = input.next();
+        while (!response.equals("bye")) {
+            System.out.println("    " + response);
+            response = input.next();
+        }
+        System.out.println("    Sad to see you go! Visit me again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+
+
+        String greetings = "Good day to you! I'm Jake\n"
+                            + "How can I help you?";
+        System.out.println(greetings);
+        responseRepeater();
+        //String response = input.next();
+        //System.out.println("You entered: " + response);
     }
 }
