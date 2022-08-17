737aba25f83bb3654dad765f9b6d39216ece3b6d
seox123 <seanmenglaw2009@gmail.com>
2022-08-17 22:23:10 +0800

    Added mark/unmark functionality to added tasks

--- src/main/java/Doemon.java
+++ src/main/java/Doemon.java
@@ -66,9 +66,44 @@ public class Doemon {
                 System.out.println(output(listStr));
                 continue;
             }
+            // Check for mark/unmark
+            String[] inputArr = inputStr.split(" ");
+            if (inputArr.length == 2
+                    && (inputArr[0].equals("mark") || inputArr[0].equals("unmark"))
+                    && isInteger(inputArr[1])) {
+                int index = Integer.parseInt(inputArr[1]) - 1;
+                if (index >= 0 && index < this.taskIndex) {
+                    if (inputArr[0].equals("mark")) {
+                        this.tasks[index].mark();
+                        System.out.println(
+                                output("Yay! This task is now marked as done:\n\t  "
+                                        + this.tasks[index].toString()));
+                    } else {
+                        this.tasks[index].unmark();
+                        System.out.println(
+                                output("I guess you weren't done with that one:\n\t  "
+                                        + this.tasks[index].toString()));
+                    }
+                    continue;
+                }
+            }
             // Add item to list of tasks
             this.tasks[this.taskIndex++] = new Task(inputStr);
             System.out.println(output("added: " + inputStr));
+        }
+    }
+
+    /**
+     * Used to check if the input string after mark/unmark is an integer.
+     * @param text the string input to be checked
+     * @return a boolean indicating if the text can be parsed into an integer
+     */
+    private static boolean isInteger(String text) {
+        try {
+            Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 
