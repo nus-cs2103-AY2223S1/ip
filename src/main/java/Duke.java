3fc929b6004a820694d9ea24b61b4bd30feafaea
shauneang <shauneangjx@gmail.com>
2022-08-17 12:55:43 +0800

    Level 2: Duke takes in list command and outputs all prior tasks given

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,30 +8,40 @@ public class Duke {
     }
 
     private static void initDuke() {
-            Scanner myScanner = new Scanner(System.in);
-            String exitWord = "bye";
-            String hLine = "\t____________________________________________";
-            String logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
-            System.out.println("Hello from\n" + logo);
-            System.out.println("What can i do for you?");
+        Scanner myScanner = new Scanner(System.in);
+        String exitWord = "bye";
+        String hLine = "\t____________________________________________";
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  /\n"
+                + "|____/ \\,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What can i do for you?");
 
-            String  command = "";
+        String  command = "";
+        ArrayList<String> toDoList = new ArrayList<>();
 
-            while ( !command.equals(exitWord)) {
-                if (command.equals("")) {
-                    System.out.println(hLine);
-                    System.out.println("\tadded: " + command);
-                    System.out.println(hLine);
+        while ( !command.equals(exitWord)) {
+            if (command.equals("list")) {
+                System.out.println(hLine);
+                for (int i = 0; i < toDoList.size(); i ++) {
+                    System.out.printf("\t%d. %s\n", i + 1, toDoList.get(i));
                 }
+                System.out.println(hLine);
 
-                command = myScanner.nextLine();
             }
-            System.out.println(hLine);
-            System.out.println("\tBye. Hope to see you again soon!");
-            System.out.println(hLine);
+            else if (command != "") {
+                System.out.println(hLine);
+                toDoList.add(command);
+                System.out.println("\tadded: " + command);
+                System.out.println(hLine);
+            }
+
+            command = myScanner.nextLine();
+        }
+        System.out.println(hLine);
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println(hLine);
     }
 }
