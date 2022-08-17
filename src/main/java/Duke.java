d01819c2fc800b85a7ba870027be4e70977d8057
Thanh Chu <lilyhanky57@gmail.com>
2022-08-17 22:13:59 +0800

    L1:Great_Echo_Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,29 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        greet();
+        String next = sc.nextLine();
+        while (!next.equals("bye")) {
+            echo(next);
+            next = sc.nextLine();
+        }
+        exitMessage();
+    }
+
+    public static void greet() {
+        echo("Hello! I'm Duke\n" + "\tWhat can I do for you?");
+    }
+
+    public static void exitMessage() {
+        echo("Bye. Hope to see you again soon!");
+    }
+
+    public static void echo(String message) {
+        String line = "____________________________________________________________";
+        System.out.println("\t" + line);
+        System.out.println("\t" + message);
+        System.out.println("\t" + line);
     }
 }
