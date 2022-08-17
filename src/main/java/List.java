e9cfd752ba6311044f97cb08db58311f5c9cf2b0
pwjj2000 <perrywongjunjie@gmail.com>
2022-08-17 21:37:10 +0800

    amended run() to process in loops instead of recursion

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -25,68 +25,65 @@ public class List {
      */
     public static void run() {
         Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
-        Scanner temp = new Scanner(text);
-        if (temp.hasNext("mark")) {
-            String mark = temp.next();
-            int i = temp.nextInt();
-            if (i > 0) {
-                if (list[i - 1] != null) {
-                    Task t = list[i - 1];
-                    t.mark();
-                }
-            }
-            temp.close();
-            List.run();
-        } else if (temp.hasNext("unmark")) {
-            String unmark = temp.next();
-            int i = temp.nextInt();
-            if (i > 0) {
-                if (list[i - 1] != null) {
-                    Task t = list[i - 1];
-                    t.unmark();
-                }
-            }
-            temp.close();
-            List.run();
-        } else {
-            if (temp.hasNext("deadline")) {
-                temp.useDelimiter("deadline\\s*|\\s*/by\\s*");
-                String description = temp.next();
-                String by = temp.next();
-                Deadline d = new Deadline(description, by);
-                temp.close();
-                List.add(d);
-            } else if (temp.hasNext("event")) {
-                temp.useDelimiter("event\\s*|\\s*/at\\s*");
-                String description = temp.next();
-                String at = temp.next();
-                Event e = new Event(description, at);
-                temp.close();
-                List.add(e);
-            } else if (temp.hasNext("todo")) {
-                temp.useDelimiter("todo\\s*");
-                String description = temp.next();
-                Todo t = new Todo((description));
-                temp.close();
-                List.add(t);
-            } else {
-                if (text.equals("bye")) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    sc.close();
-                } else if (text.equals("list")) {
-                    for (int i = 0; i < list.length; i++) {
-                        if (list[i] != null) {
-                            System.out.println(i + 1 + ". " + list[i].toString());
-                        }
+        while (sc.hasNextLine()) {
+            String text = sc.nextLine();
+            Scanner temp = new Scanner(text);
+            if (temp.hasNext("mark")) {
+                String mark = temp.next();
+                int i = temp.nextInt();
+                if (i > 0) {
+                    if (list[i - 1] != null) {
+                        Task t = list[i - 1];
+                        t.mark();
                     }
-                    List.run();
+                }
+                temp.close();
+            } else if (temp.hasNext("unmark")) {
+                String unmark = temp.next();
+                int i = temp.nextInt();
+                if (i > 0) {
+                    if (list[i - 1] != null) {
+                        Task t = list[i - 1];
+                        t.unmark();
+                    }
+                }
+                temp.close();
+            } else {
+                if (temp.hasNext("deadline")) {
+                    temp.useDelimiter("deadline\\s*|\\s*/by\\s*");
+                    String description = temp.next();
+                    String by = temp.next();
+                    Deadline d = new Deadline(description, by);
+                    temp.close();
+                    List.add(d);
+                } else if (temp.hasNext("event")) {
+                    temp.useDelimiter("event\\s*|\\s*/at\\s*");
+                    String description = temp.next();
+                    String at = temp.next();
+                    Event e = new Event(description, at);
+                    temp.close();
+                    List.add(e);
+                } else if (temp.hasNext("todo")) {
+                    temp.useDelimiter("todo\\s*");
+                    String description = temp.next();
+                    Todo t = new Todo((description));
+                    temp.close();
+                    List.add(t);
                 } else {
-                    Task task = new Task(text);
-                    List.add(task);
+                    if (text.equals("bye")) {
+                        System.out.println("Bye. Hope to see you again soon!");
+                        break;
+                    } else if (text.equals("list")) {
+                        List.printList();
+                        List.run();
+                    } else {
+                        Task task = new Task(text);
+                        List.add(task);
+                    }
                 }
             }
         }
+        sc.close();
     }
     /**
      * Helper function that returns a string
@@ -107,6 +104,14 @@ public class List {
         list[index] = t;
         index++;
         System.out.println("Got it. I've added this task:\n" + t.toString() + "\n" + "Now you have " + index + taskString() + "in the list.");
-        List.run();
     }
+
+    public static void printList() {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null) {
+                System.out.println(i + 1 + ". " + list[i].toString());
+            }
+        }
+    }
+
 }
