98b5d44a9986ff54f2ee781bd038402061f1336e
jhchee18 <e0407708@u.nus.edu>
2022-08-17 16:09:34 +0800

    Level 3: added Mark and Unmark methods to Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,29 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
         while(!s.equals("bye")) {
+            String parts[] = s.split(" ", 0);
             if (s.equals("list")) {
-                String listOutput = "";
+                String listOutput = "Here are the tasks in your list:\n";
                 int index = 1;
                 for (Task t : taskList) {
-                    listOutput += index + ". " + t;
+                    listOutput += index + ".[" + (t.isMarked() ? "X" : " ") + "] "+ t;
                     index++;
                 }
                 System.out.println(listOutput);
+            }
+            else if (parts.length >= 1 && (parts[0].equals("mark") || parts[0].equals("unmark"))) {
+                if (parts[0].equals("mark")) {
+                    int pos = Integer.parseInt(parts[1]) - 1;
+                    Task markedTask = taskList.get(pos).mark();
+                    taskList.set(pos, markedTask);
+                    System.out.println("Nice! I've marked this task as done:\n[X] " + markedTask);
+                }
+                else if (parts[0].equals("unmark")) {
+                    int pos = Integer.parseInt(parts[1]) - 1;
+                    Task unmarkedTask = taskList.get(pos).unmark();
+                    taskList.set(pos, unmarkedTask);
+                    System.out.println("OK, I've marked this task as not done yet:\n[ ] " + unmarkedTask);
+                }
             }
             else {
                 Task task = new Task(s);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,26 @@
 public class Task {
     private final String taskName;
+    private final boolean markDone;
 
     Task(String taskName) {
         this.taskName = taskName;
+        this.markDone = false;
+    }
+
+    Task(String taskName, boolean markDone) {
+        this.taskName = taskName;
+        this.markDone = markDone;
+    }
+    public Task mark() {
+        return new Task(this.taskName, true);
+    }
+
+    public Task unmark() {
+        return new Task(this.taskName, false);
+    }
+
+    public boolean isMarked() {
+        return this.markDone;
     }
 
     @Override
