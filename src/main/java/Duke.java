68fb2b2ffb366479f1e9417d6b07bcf90dc12334
paullee18 <paulfrancislee.1@gmail.com>
2022-08-18 02:04:22 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,10 +11,21 @@ public class Duke {
         System.out.println("How can I help?");
 
         Scanner sc = new Scanner(System.in);
+        String[] ls = new String[100];
+        int eleCount = 0;
+
         String command = sc.nextLine();
 
+
         while (!command.equals("bye")) {
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < eleCount; i++) {
+                    System.out.println(i + ". " + ls[i]);
+                }
+            }
+            ls[eleCount] = command;
+            eleCount++;
+            System.out.println("added: " + command);
             command = sc.nextLine();
         }
 
