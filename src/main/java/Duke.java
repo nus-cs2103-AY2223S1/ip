f45a7ee49874fea712c844a4ba607f0e0372101f
sjoann <joansim19@gmail.com>
2022-08-17 20:50:24 +0800

    Add Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    /** Array containing Task objects */
-    private static Task[] taskArray = new Task[100];
-
-    /** Number of Task objects in the list */
-    private static int numOfTasks = 0;
+    /** ArrayList of type Task */
+    private static ArrayList<Task> taskArray = new ArrayList<>();
 
     public static void main(String[] args) throws MultipleTasksException, NullTaskException, InvalidCommandException {
         Scanner sc = new Scanner(System.in);
@@ -22,20 +20,26 @@ public class Duke {
                             + " Here are the tasks in your list:"
                             + enumerateList()
                             + "\n____________________________________________________________");
-                } else if (strInput.contains("todo") && strInput.substring(0,4).equals("todo")){
+                } else if (isATodo(strInput)){
                     checkForNullTask(strInput.substring(4), "todo");
                     checkForMultipleTasks(strInput.substring(4));
                     addToList(new Todo(strInput.substring(5)));
-                } else if (strInput.contains("deadline") && strInput.substring(0,8).equals("deadline")) {
+                } else if (isADeadline(strInput)) {
                     checkForNullTask(strInput.substring(8), "deadline");
                     checkForMultipleTasks(strInput.substring(8));
                     addToList(new Deadline(strInput.substring(9, strInput.indexOf("/") - 1),
                             strInput.substring(strInput.indexOf("/by") + 4)));
-                } else if (strInput.contains("event") && strInput.substring(0,5).equals("event")) {
+                } else if (isAEvent(strInput)) {
                     checkForNullTask(strInput.substring(5), "event");
                     checkForMultipleTasks(strInput.substring(5));
                     addToList(new Event(strInput.substring(6, strInput.indexOf("/") - 1),
                             strInput.substring(strInput.indexOf("/at") + 4)));
+                } else if (strInput.contains("delete") && strInput.substring(0,6).equals("delete")) {
+                    deleteFromList(Integer.parseInt(strInput.substring(7)));
+                } else if (strInput.contains("unmark") && strInput.substring(0,6).equals("unmark")) {
+                    unmarkTheTask(Integer.parseInt(strInput.substring(7)));
+                } else if (strInput.contains("mark") && strInput.substring(0,4).equals("mark")) {
+                    markTheTask(Integer.parseInt(strInput.substring(5)));
                 } else {
                     throw new InvalidCommandException("Cannot recognise the command.");
                 }
@@ -49,16 +53,73 @@ public class Duke {
         }
     }
 
-    private static void checkForNullTask(String description, String typeOfTask) throws NullTaskException {
+    /**
+     * Checks if there is task description.
+     *
+     * @param description task description.
+     * @param typeOfTask todo, deadline, or event.
+     * @throws NullTaskException if there is no task description.
+     */
+    public static void checkForNullTask(String description, String typeOfTask) throws NullTaskException {
         if (description.isBlank()) {
             throw new NullTaskException("No task description.", typeOfTask);
         }
     }
 
-    private static void checkForMultipleTasks(String s) throws MultipleTasksException {
+    /**
+     * Checks if there are multiple tasks in one input.
+     *
+     * @param s user input.
+     * @throws MultipleTasksException if there are multiple tasks in one input.
+     */
+    public static void checkForMultipleTasks(String s) throws MultipleTasksException {
         if (s.contains("todo") ||s.contains("deadline") || s.contains("event")) {
             throw new MultipleTasksException("Multiple task detected.");
         }
+    }
+
+    /**
+     * Check if is a todo task.
+     *
+     * @param s
+     * @return boolean. True if is a todo task.
+     */
+    public static boolean isATodo(String s) {
+        return s.contains("todo") && s.substring(0,4).equals("todo");
+    }
+
+    /**
+     * Check if is a task with deadline.
+     *
+     * @param s
+     * @return boolean. True if is a deadline task.
+     */
+    public static boolean isADeadline(String s) {
+        return s.contains("deadline") && s.substring(0,8).equals("deadline");
+    }
+
+    /**
+     * Check if is a event task.
+     * @param s
+     * @return boolean. True if is a event task.
+     */
+    public static boolean isAEvent(String s) {
+        return s.contains("event") && s.substring(0, 5).equals("event");
+    }
+
+    /**
+     * Deletes a Task object from taskArray
+     *
+     * @param taskID Task index.
+     */
+    public static void deleteFromList(int taskID) {
+        Task task = taskArray.get(taskID - 1);
+        taskArray.remove(taskID - 1);
+        System.out.println("____________________________________________________________\n"
+                + " Noted. I've removed this task:\n  "
+                + task.toString()
+                + "\n Now you have " + taskArray.size() + " tasks in the list"
+                + "\n____________________________________________________________");
     }
 
     /**
@@ -67,12 +128,11 @@ public class Duke {
      * @param t Task.
      */
     public static void addToList(Task t) {
-        taskArray[numOfTasks] = t;
-        numOfTasks += 1;
+        taskArray.add(t);
         System.out.println("____________________________________________________________\n"
                 + " Got it. I've added this task:\n  "
                 + t.toString()
-                + "\n Now you have " + numOfTasks + " tasks in the list"
+                + "\n Now you have " + taskArray.size() + " tasks in the list"
                 + "\n____________________________________________________________");
     }
 
@@ -84,10 +144,40 @@ public class Duke {
     public static String enumerateList() {
         //StringBuilder over string concat for better performance
         StringBuilder s = new StringBuilder("");
-        for (int i = 1; i <= numOfTasks; i++) {
+        for (int i = 1; i <= taskArray.size(); i++) {
             s.append("\n " + i
-                    + "." + taskArray[i - 1].toString());
+                    + "." + taskArray.get(i - 1).toString());
         }
         return s.toString();
+    }
+
+    /**
+     * Mark the task done.
+     *
+     * @param taskID Task's number.
+     */
+    public static void markTheTask(int taskID) {
+        Task t = taskArray.get(taskID - 1);
+        System.out.println("____________________________________________________________\n"
+                + " Nice! I've marked this task as done:\n"
+                + "  [X] "
+                + t.description
+                + "\n____________________________________________________________");
+        t.toggleIsDone(true);
+    }
+
+    /**
+     * Mark the task as not done.
+     *
+     * @param taskID Task's number.
+     */
+    public static void unmarkTheTask(int taskID) {
+        Task t = taskArray.get(taskID - 1);
+        System.out.println("____________________________________________________________\n"
+                + " OK, I've marked this task as not done yet:\n"
+                + "  [ ] "
+                + t.description
+                + "\n____________________________________________________________");
+        t.toggleIsDone(false);
     }
 }
