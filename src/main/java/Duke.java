c30b6ff0ee6fbe6766b85f98d293e7416119a068
jjtan444 <jjtan444@gmail.com>
2022-08-18 00:18:09 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?" + "\n");
         Scanner sc = new Scanner(System.in);
         String[] input;
-        int index = 0;
-        Task[] items = new Task[100];
+        ArrayList<Task> items = new ArrayList<>();
         boolean done = false;
 
         while(!done) {
@@ -22,11 +23,9 @@ public class Duke {
             if (input[0].equals("list")) {
                 // when user request list
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < items.length; i++) {
-                    if ((items[i]) == null) {
-                        break;
-                    }
-                    System.out.println(i+1 + ". " + items[i]);
+                int count = 1;
+                for (Task tsk : items) {
+                    System.out.println(String.valueOf(count++)+ "." + tsk);
                 }
             }
             else {
@@ -34,41 +33,55 @@ public class Duke {
                     case "mark":
                         // when user wants to mark as done
                         int num = Integer.valueOf(input[1]) - 1;
-                        if (items[num] == null) {
+                        if (num >= items.size()) {
                             System.out.println("No such task");
                         }
                         else {
-                            System.out.println(items[num].markAsDone());
+                            System.out.println(items.get(num).markAsDone());
                         }
                         break;
 
                     case "unmark":
                         // when user wants to mark as not done
                         int numb = Integer.valueOf(input[1]) - 1;
-                        if (items[numb] == null) {
+                        if (numb >= items.size()) {
                             System.out.println("No such task");
                         }
                         else {
-                            System.out.println(items[numb].markAsNotDone());
+                            System.out.println(items.get(numb).markAsNotDone());
                         }
                         break;
 
                     case "todo":
                         // when user wants to add todo task
-                        items[index] = new Todo(input[1]);
-                        System.out.println("Got it. I've added this task:\n  " + items[index++] +"\n" + Task.getCount());
+                        Task t1 = new Todo(input[1]);
+                        items.add(t1);
+                        System.out.println("Got it. I've added this task:\n  " + t1 +"\n" + getCount(items.size()));
                         break;
 
                     case "deadline":
                         // when user wants to add deadline task
-                        items[index] = new Deadline(input[1], input[2]);
-                        System.out.println("Got it. I've added this task:\n  " + items[index++] +"\n" + Task.getCount());
+                        Task t2 = new Deadline(input[1], input[2]);
+                        items.add(t2);
+                        System.out.println("Got it. I've added this task:\n  " + t2 +"\n" + getCount(items.size()));
                         break;
 
                     case "event":
                         // when user wants to add event task
-                        items[index] = new Event(input[1], input[2]);
-                        System.out.println("Got it. I've added this task:\n  " + items[index++] +"\n" + Task.getCount());
+                        Task t3 = new Event(input[1], input[2]);
+                        items.add(t3);
+                        System.out.println("Got it. I've added this task:\n  " + t3 +"\n" + getCount(items.size()));
+                        break;
+
+                    case "delete":
+                        // when user wants to delete task
+                        try {
+                            Task t4 = items.remove(Integer.valueOf(input[1]) - 1);
+                            System.out.println("Noted. I've removed this task:\n  " + t4 + "\n" + getCount(items.size()));
+                        }
+                        catch (IndexOutOfBoundsException ex) {
+                            System.out.println("Invalid index");
+                        }
                         break;
 
                     case "bye":
@@ -81,11 +94,14 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    public static String getCount(int len) {
+        return "Now you have " + String.valueOf(len) + " tasks in the list.";
+    }
+
     public static String[] getInput(Scanner sc) throws DukeException {
         String input = sc.nextLine();
         String phrases[] = input.split(" /.. ", 2); // splits sentence and removes by/at
         String words[] = phrases[0].split(" ", 2);
-
 
         if (words[0].equals("todo")) {
             if (words.length != 2) {
@@ -115,7 +131,7 @@ public class Duke {
                 return words;
             }
         }
-        else if (words[0].equals("mark") || words[0].equals("unmark")) {
+        else if (words[0].equals("mark") || words[0].equals("unmark") || words[0].equals("delete")) {
             if (words.length != 2) {
                 throw new DukeException("Please put in index");
             }
