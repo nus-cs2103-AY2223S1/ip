22ebd2f27e2feb3d51a2b7b057f2145cf276a766
Humphrey <vanillaqicecream@gmail.com>
2022-08-17 13:18:11 +0800

    implement skeletal version of bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private Scanner sc;
+    private static String botName = "DIGITAL DADDY";
+    private static String emoji = "\uD83D\uDE00";
+
+    Duke(Scanner sc) {
+        this.sc = sc;
     }
+
+    private static void botReply(String input) {
+        String lineSeparator = "____________________________________________________________";
+        String reply = String.format("%s\n%s %s %s \n%s \n%s", lineSeparator, emoji, botName, emoji, input, lineSeparator);
+        System.out.println(reply);
+    }
+
+    public void start() {
+        botReply("Hello! I'm " + botName + "\nWhat can I do for you?");
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                botReply("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            botReply(input);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Create a scanner to read from standard input.
+        Scanner sc = new Scanner(System.in);
+
+        Duke duke = new Duke(sc);
+        duke.start();
+
+        sc.close();
+    }
+
+
 }
