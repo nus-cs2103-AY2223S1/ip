c6830983b9abfab8681bf9286b5d3c5e1c06fe73
Tan Yuan Wei <tyw2811@gmail.com>
2022-08-18 00:43:21 +0800

    Implemented ToDo, Event, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,13 +14,13 @@ public class Duke {
                 + "| |  | | |____| |____| |___| |__| |  | |    | | \\ \\| |__| | |  | |  | |__| | |__| | . \\| |____\n"
                 + "|_|  |_|______|______|______\\____/   |_|    |_|  \\_\\\\____/|_|  |_|  |_____/ \\____/|_|\\_\\______|\n";
         System.out.println(logo);
-        System.out.println("How may I help you today?");
+        System.out.println("  How may I help you today?");
         newLine();
     }
 
     public static void program() {
         Scanner sc = new Scanner(System.in);
-        String input = null, cmd = null;
+        String input = null, cmd = null, postCmd = null;
         String postSplit[];
         LinkedList<Task> lst = new LinkedList<>();
         do {
@@ -28,6 +28,9 @@ public class Duke {
             newLine();
             postSplit = input.split(" ");
             cmd = postSplit[0];
+            if (cmd.length() != input.length()) {
+                postCmd = input.substring(cmd.length() + 1);
+            }
 
             switch(cmd) {
                 case "bye":
@@ -35,10 +38,14 @@ public class Duke {
                 case "list":
                     printList(lst);
                     break;
-                case "add":
-                    lst.add(new Task(input.substring(cmd.length() + 1)));
-                    System.out.println("  added: " + input.substring(cmd.length() + 1));
-                    newLine();
+                case "todo":
+                    handleTodo(lst, postCmd);
+                    break;
+                case "deadline":
+                    handleDeadline(lst, postCmd);
+                    break;
+                case "event":
+                    handleEvent(lst, postCmd);
                     break;
                 case "mark":
                     lst.get(Integer.parseInt(postSplit[1]) - 1).setDone();
@@ -59,13 +66,42 @@ public class Duke {
     }
 
     public static void printCommands(String cmd) {
-        String commands = "\tadd - adds the task to the list\n" +
+        String commands = "\ttodo - adds the task to the list\n" +
+                "\tdeadline - adds the task with a deadline, e.g. deadline x /by Sunday\n" +
+                "\tevent - adds the task that happens at a specific time, e.g. event x /at Mon 2-4pm\n" +
+                "\tmark - marks task number x as completed, e.g. mark x\n" +
+                "\tunmark - marks task number x as uncompleted, e.g. unmark x" +
                 "\tlist - lists out all your current tasks\n" +
-                "\tbye - exits the program:(\n" +
-                "\tmark - mark x will mark task number x as completed\n" +
-                "\tunmark - unmark x will mark task number x as uncompleted";
+                "\tbye - exits the program:(\n";
         System.out.println("  " + cmd + " is not a valid command. Here are the list of commands:\n\n" + commands);
         newLine();
+    }
+
+    public static void handleTodo(List lst, String desc) {
+        lst.add(new Todo(desc));
+        System.out.println("  Added task todo: \n  " + lst.get(lst.size() - 1));
+        printListCount(lst);
+        newLine();
+    }
+
+    public static void handleDeadline(List lst, String desc) {
+        String[] split = desc.split("/");
+        lst.add(new Deadline(split[0], split[1].substring(3)));
+        System.out.println("  Added the task with deadline: \n  " + lst.get(lst.size() - 1));
+        printListCount(lst);
+        newLine();
+    }
+
+    public static void handleEvent(List lst, String desc) {
+        String[] split = desc.split("/");
+        lst.add(new Event(split[0], split[1].substring(3)));
+        System.out.println("  Added the event task: \n  " + lst.get(lst.size() - 1));
+        printListCount(lst);
+        newLine();
+    }
+
+    public static void printListCount(List lst) {
+        System.out.println("  You have " + lst.size() + " tasks currently, " + Task.totalDone + " are completed");
     }
 
     public static void printList(List lst) {
@@ -82,11 +118,12 @@ public class Duke {
 
 
     public static void exitProgram() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye:( Hope to see you again soon!");
         newLine();
     }
 
     public static void newLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("________________________________________________________________________" +
+                "_______________________");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,6 +2,8 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    public static int totalDone = 0;
+
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -12,15 +14,21 @@ public class Task {
     }
 
     public void setDone() {
+        if (!this.isDone) {
+            Task.totalDone++;
+        }
         this.isDone = true;
     }
 
     public void setUnDone() {
+        if (this.isDone) {
+            Task.totalDone--;
+        }
         this.isDone = false;
     }
 
     @Override
     public String toString() {
-        return "[" + getStatusIcon() + "] " + description;
+        return "[" + getStatusIcon() + "] " + this.description;
     }
 }
