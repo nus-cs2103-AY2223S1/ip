b12751f7c913dff8e60ac704d9c9a6cede3d1c36
Min Jiun Chia <cminjiun@gmail.com>
2022-08-17 10:40:37 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("\n Hello there! \n"
+                + "\n My name is Zelk, nice to meet you :D \n"
+                + " What can I do for you?\n"
+                + " ___________________________________________________________________");
+        Scanner s = new Scanner(System.in);
+        String input = "";
+        while (!(input = s.nextLine()).equals("bye")) {
+            System.out.println("        " + input
+                    + "\n ___________________________________________________________________");
+        }
+
+        System.out.println("        " + "Bye! Hope to see you again soon! Thank you for chatting with me :)");
+
+
+
     }
 }
