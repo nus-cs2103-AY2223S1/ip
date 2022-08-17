1af4afa5bcb49c994ddfb5dbb2f69f846f469985
ryanguai <e0694442@u.nus.edu>
2022-08-17 22:11:40 +0800

    Handle errors

---
+++ src/main/java/EmptyTodoListException.java
@@ -0,0 +1,5 @@
+public class EmptyTodoListException extends Exception {
+    public EmptyTodoListException() {
+        super("☹ OOPS!!! The description of a todo cannot be empty.");
+    }
+}
---
+++ src/main/java/InvalidCommandException.java
@@ -0,0 +1,5 @@
+public class InvalidCommandException extends Exception {
+    public InvalidCommandException() {
+        super("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+    }
+}
