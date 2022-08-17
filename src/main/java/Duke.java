4b2d361cc19889070d43c8a23295951883ca1bf9
Zong Xian <kangzongxian88@gmail.com>
2022-08-17 20:39:05 +0800

    Make Tasks able to be marked done or undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,27 @@ import java.util.Scanner;
 
 public class Duke {
 
+    /**
+     * This method checks if a string typed can be a number or not
+     * @param number
+     * @return a boolean value indicating whether the string entered can be parsed as an integer
+     */
+    public static boolean isNumeric(String number) {
+        if (number == null) {
+            return false;
+        }
+        try {
+            int d = Integer.parseInt(number);
+        } catch (NumberFormatException numberFormatException) {
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
 
-        // Create an Array to store the text the user has entered of size 100
-        String[] storedTextArray = new String[100];
+        // Create an Array to store the Tasks the user has entered of size 100
+        Task[] taskArray = new Task[100];
         // Create a count to track the number of items the user has stored
         int count = 0;
 
@@ -31,14 +48,36 @@ public class Duke {
             System.out.println("----------------------");
             // If user types list, output all the text being stored
             if (userCommand.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println(String.valueOf(i + 1) + ". " + storedTextArray[i]);
+                    Task currentTask = taskArray[i];
+                    System.out.println(String.valueOf(i + 1) + "." + currentTask.toString());
                 }
             } else {
-                // For anything else the user types, store into the array
-                storedTextArray[count] = userCommand;
-                count += 1;
-                System.out.println("added:" + userCommand);
+                String[] words = userCommand.split(" ");
+                if (words.length > 1 &&
+                        (words[0].equals("mark") || words[0].equals("unmark")) && isNumeric(words[1])) {
+                    // If user first word is marked or unmarked and second word can be a number, it is a mark or unmark
+                    // Get the index of task to mark or unmark
+                    // Get the task and mark it as done or not done depending on the user
+                    int taskNumber = Integer.parseInt(words[1]);
+                    Task currentTask = taskArray[taskNumber - 1];
+                    if (words[0].equals("mark")) {
+                        System.out.println("Nice! I've marked this task as done:");
+                        currentTask.markAsDone();
+                        System.out.println(currentTask.toString());
+                    } else {
+                        System.out.println("0K, I've marked this task as not done yet:");
+                        currentTask.markNotDone();
+                        System.out.println(currentTask.toString());
+                    }
+                } else {
+                    // If first word is not marked or unmarked or user only typed one word, user is trying to add a task
+                    taskArray[count] = new Task(userCommand);
+                    count += 1;
+                    System.out.println("added:" + userCommand);
+                }
+
             }
             System.out.println("----------------------");
             userCommand = enterInput.nextLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,49 @@
+public class Task {
+
+    // Variables of a Task
+    protected String description;
+    private boolean isDone;
+
+    /**
+     * The constructor for the Task
+     * @param description the text for the task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Set isDone parameter to be true
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Set isDone parameter to be false
+     */
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    /**
+     * Get the status of whether the task is done or not
+     * @return a string representing whether the task is done
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    /**
+     * Get a statement representing if the task is done or not
+     * @return a string representing the task and its status
+     */
+    @Override
+    public String toString() {
+        if (this.isDone) {
+            return "[X] " + this.description;
+        }
+        return "[ ] " + this.description;
+    }
+}
