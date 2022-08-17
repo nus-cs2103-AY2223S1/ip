c44b4eb4150779432076498f444217388b98ef02
nopehax <dynamical36@gmail.com>
2022-08-18 01:13:14 +0800

    added lvl 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] store = new String[100];
+    private static int count = 0;
     public static void main(String[] args) {
+
         System.out.println("Hello! What are we gonna do today?");
         while (true) {
             Scanner sc = new Scanner(System.in);
@@ -9,8 +12,17 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("\tBye! Hope that I helped!");
                 break;
+            } else if (input.equals("list")) {
+                int number = 1;
+                for (String task : store) {
+                    if (task == null) continue;
+                    System.out.println("\t" + number + ". " + task);
+                    number++;
+                }
+                continue;
             }
-            System.out.println("\t" + input);
+            store[++count] = input;
+            System.out.println("\tadded: " + input);
         }
     }
 }
