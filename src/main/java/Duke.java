9247671154013591cda45d6997133c05ce425c0f
JonathanWiguna <jonathan.wiguna1@gmail.com>
2022-08-17 22:22:13 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcomeMsg = "Hi there! Baymax at your service.";
+        System.out.println(welcomeMsg);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+        System.out.println("Goodbye!");
     }
 }
