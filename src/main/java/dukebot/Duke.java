4a04035ff5161c1c5903037572500a5e837005b8
Prearo <kohshanghui@gmail.com>
2022-08-18 01:27:26 +0800

    Mark As Done

--- src/main/java/dukebot/Duke.java
+++ src/main/java/dukebot/Duke.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Duke {
 
     private static ArrayList<Task> taskList = new ArrayList<>();
+    private static int currentPos = 0;
 
     public static void main(String[] args) {
         System.out.println(Messages.startup);
@@ -13,15 +14,30 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
-
-            switch (input) {
+            String[] inputWords = input.split(" ");
+            switch (inputWords[0]) {
                 case "bye":
                     System.out.println(Messages.ending);
                     System.exit(0);
                 case "list":
                     for (int i = 0; i < taskList.size(); i++) {
-                        System.out.println((i+1) + ". " + taskList.get(i).description);
+                        System.out.println((i+1) + ".[" + taskList.get(i).getStatusIcon() +
+                                "] "+ taskList.get(i).description);
                     }
+                    break;
+                case "mark":
+                    currentPos = Integer.parseInt(inputWords[1]) - 1;
+                    taskList.get(currentPos).markDone();
+                    System.out.println(Messages.taskMarked);
+                    System.out.println("[" + taskList.get(currentPos).getStatusIcon() +
+                    "] "+ taskList.get(currentPos).description);
+                    break;
+                case "unmark":
+                    currentPos = Integer.parseInt(inputWords[1]) - 1;
+                    taskList.get(currentPos).markUndone();
+                    System.out.println(Messages.taskUnmarked);
+                    System.out.println("[" + taskList.get(currentPos).getStatusIcon() +
+                            "] "+ taskList.get(currentPos).description);
                     break;
                 default:
                     taskList.add(new Task(input));
--- src/main/java/dukebot/Task.java
+++ src/main/java/dukebot/Task.java
@@ -2,8 +2,22 @@ package dukebot;
 
 public class Task {
     protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
     }
 }
