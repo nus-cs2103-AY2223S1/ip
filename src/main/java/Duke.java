f1c8472a2d4bb3f1244eb5c89258763470521dbe
jwdavis0200 <jwdavis0200@gmail.com>
2022-08-17 14:16:58 +0800

    Added Level-1 functionality, Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greeting();
+        echo();
+    }
+    public static void greeting() {
+        System.out.println("--------------------------------------\n");
+        System.out.println("\tHello I'm Duke, what can I do for you?");
+        System.out.println("--------------------------------------\n");
+    }
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String uncap = str.toLowerCase();
+        while (!uncap.equals("bye")) {
+            System.out.print("-------------------------------");
+            System.out.println("\n");
+            System.out.printf("\t  %s  \n", str);
+            System.out.println("\n");
+            System.out.print("-------------------------------\n");
+            str = sc.next();
+            uncap = str.toLowerCase();
+        }
+        System.out.print("-------------------------------");
+        System.out.println("Bye. Hope to see you soon again!");
+        System.out.print("-------------------------------\n");
     }
 }
