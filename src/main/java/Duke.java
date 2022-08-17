a28e8f72196e92592b8789b425567955ce334694
CedricChia123 <cedricchia01@gmail.com>
2022-08-18 03:42:53 +0800

    allow use of list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
         System.out.println("Hello I'm Duke\n" + "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
@@ -9,9 +13,16 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                int length = list.size();
+                for (int i = 0; i < length; i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
             } else {
-                System.out.println(input);
+                list.add(input);
+                System.out.println("added: " + input);
             }
         }
     }
+
 }
