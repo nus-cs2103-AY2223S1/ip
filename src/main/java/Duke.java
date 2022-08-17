de05d281c515ac30974608f1ecce6052366ad80c
nopehax <dynamical36@gmail.com>
2022-08-18 01:44:34 +0800

    added new class Task and lvl 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] store = new String[100];
+    private static Task[] store = new Task[100];
     private static int count = 0;
     public static void main(String[] args) {
 
@@ -14,14 +14,26 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 int number = 1;
-                for (String task : store) {
+                for (Task task : store) {
                     if (task == null) continue;
-                    System.out.println("\t" + number + ". " + task);
+                    System.out.println("\t" + number + "." + task);
                     number++;
                 }
                 continue;
+            } else if (input.split(" ")[0].equals("mark")) {
+                Integer index = Integer.valueOf(input.split(" ")[1]);
+                store[index].markAsDone();
+                System.out.println("\tnice! I've marked this task as done:");
+                System.out.println("\t\t" + store[index]);
+                continue;
+            } else if (input.split(" ")[0].equals("unmark")) {
+                Integer index = Integer.valueOf(input.split(" ")[1]);
+                store[index].markAsNotDone();
+                System.out.println("\tOk! I've marked this task as not done yet:");
+                System.out.println("\t\t" + store[index]);
+                continue;
             }
-            store[++count] = input;
+            store[++count] = new Task(input);
             System.out.println("\tadded: " + input);
         }
     }
