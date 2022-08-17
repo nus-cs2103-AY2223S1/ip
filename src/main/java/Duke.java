fcd2f43f1771332c545ff288048846c8df6a6ba8
Song Ivan <songivan45@gmail.com>
2022-08-17 14:07:22 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +9,18 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm SmartBot\nWhat can I do for you?");
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            String first = sc.next();
+            if(first.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else {
+                System.out.println(first);
+            }
+        }
     }
 }
