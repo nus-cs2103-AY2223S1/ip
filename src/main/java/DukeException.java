3025af91ee188213715fe33ea48096391a71ce2b
Marcus Pang <marcuspangyuyang@gmail.com>
2022-08-17 10:42:44 +0800

    Add toString method for DukeException

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -2,4 +2,9 @@ public class DukeException extends Exception {
     DukeException(String errorString) {
         super(errorString);
     }
+
+    @Override
+    public String toString() {
+        return this.getMessage();
+    }
 }
