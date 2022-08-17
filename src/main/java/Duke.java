5f18da57cf86a5a3d372b81c97964e6d286d01a1
chun <lamchunyu00@gmail.com>
2022-08-17 09:11:09 +0800

    Added ability to store data.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,8 +6,7 @@ public class Duke {
         return "--------------------";
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    private static void setup() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -16,15 +15,47 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
         System.out.println(divider());
+    }
+
+    private static void printData(String[] data) {
+        int index = 1;
+        for (String s : data) {
+            if (s == null) {
+                return;
+            }
+            System.out.println(index + ". " + s);
+            index++;
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] data = new String[100];
+        int pointer = 0;
+
+        setup();
 
         while (true) {
             String line = scanner.nextLine();
             System.out.println(divider());
-            if (line.equals("bye")) {
-                System.out.println("See you again!");
-                break;
+
+            switch (line) {
+
+                case "bye":
+                    System.out.println("See you again!");
+                    return;
+
+                case "list":
+                    printData(data);
+                    break;
+
+                default:
+                    data[pointer] = line;
+                    pointer++;
+                    System.out.println("Added: " + line);
+                    break;
             }
-            System.out.println(line);
         }
 
     }
