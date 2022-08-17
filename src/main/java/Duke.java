2096347cb274218bc8c773106c354b7261b2f7b5
weesiau <e0725311@u.nus.edu>
2022-08-17 18:00:20 +0800

    Completed Level-6 Increment
    Implemented ability to delete a task from the list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,39 +2,55 @@ import java.util.*;
 
 public class Duke {
 
-    private static Task[] addedTask = new Task[100];
-    private static int addedTaskCount = 0;
+    private static ArrayList<Task> addedTask = new ArrayList<>(100);
+
+    private static int getSize() {
+        return Duke.addedTask.size();
+    }
+
+    private static void reportSize() {
+        if (Duke.getSize() == 1) {
+            System.out.println("There is " + Duke.getSize() + " task in your list.");
+        } else {
+            System.out.println("There are " + Duke.getSize() + " tasks in your list.");
+        }
+    }
 
     private static void addTask(Task task) {
-        Duke.addedTask[Duke.addedTaskCount] = task;
-        Duke.addedTaskCount++;
+        Duke.addedTask.add(task);
         System.out.println("Task added:");
         System.out.println(task);
-        System.out.println("There are " + addedTaskCount + " tasks in your list.");
+        reportSize();
+    }
+
+    private static void deleteTask(int index) throws TaskNotFoundException {
+        if (index > Duke.getSize() || index < 0) {
+            throw new TaskNotFoundException(String.valueOf(index));
+        }
+        System.out.println("Task successfully removed:");
+        System.out.println(Duke.addedTask.remove(index - 1));
+        reportSize();
     }
 
     private static void listTask() {
         System.out.println("Listing your current tasks:");
-        for (int i = 0; i < Duke.addedTask.length; ++i) {
-            if (Duke.addedTask[i] == null) {
-                return;
-            }
-            System.out.println((i + 1) + "." + Duke.addedTask[i]);
+        for (int i = 0; i < Duke.getSize(); ++i) {
+            System.out.println((i + 1) + "." + Duke.addedTask.get(i));
         }
     }
 
     private static void markTask(int index) throws TaskNotFoundException {
-        if (Duke.addedTask[index - 1] == null) {
+        if (index > Duke.getSize() || index < 0) {
             throw new TaskNotFoundException(String.valueOf(index));
         }
-        Duke.addedTask[index - 1].mark();
+        Duke.addedTask.get(index - 1).mark();
     }
 
     private static void unmarkTask(int index) throws TaskNotFoundException {
-        if (Duke.addedTask[index - 1] == null) {
+        if (index > Duke.getSize() || index < 0) {
             throw new TaskNotFoundException(String.valueOf(index));
         }
-        Duke.addedTask[index - 1].unmark();
+        Duke.addedTask.get(index - 1).unmark();
     }
 
     private static void processCommand(String command)
@@ -49,18 +65,29 @@ public class Duke {
                 Duke.listTask();
                 break;
             case "mark":
+                if (commandArgs.length == 1) {
+                    throw new EmptyCommandException(commandArgs[0]);
+                }
                 Duke.markTask(Integer.parseInt(commandArgs[1]));
                 break;
             case "unmark":
+                if (commandArgs.length == 1) {
+                    throw new EmptyCommandException(commandArgs[0]);
+                }
                 Duke.unmarkTask(Integer.parseInt(commandArgs[1]));
+                break;
+            case "delete":
+                if (commandArgs.length == 1) {
+                    throw new EmptyCommandException(commandArgs[0]);
+                }
+                Duke.deleteTask(Integer.parseInt(commandArgs[1]));
                 break;
             case "todo":
                 if (commandArgs.length == 1) {
                     throw new EmptyCommandException(commandArgs[0]);
-                } else {
-                    Todo todo = new Todo(commandArgs[1]);
-                    Duke.addTask(todo);
                 }
+                Todo todo = new Todo(commandArgs[1]);
+                Duke.addTask(todo);
                 break;
             case "deadline":
                 if (commandArgs.length == 1) {
@@ -69,10 +96,9 @@ public class Duke {
                 String[] deadlineDetails = commandArgs[1].split(" /by ");
                 if (deadlineDetails.length == 1) {
                     throw new IllegalCommandException(commandArgs[0]);
-                } else {
-                    Duke.addTask(new Deadline(deadlineDetails[0],
-                            deadlineDetails[1]));
                 }
+                Duke.addTask(new Deadline(deadlineDetails[0],
+                        deadlineDetails[1]));
                 break;
             case "event":
                 if (commandArgs.length == 1) {
@@ -81,10 +107,9 @@ public class Duke {
                 String[] eventDetails = commandArgs[1].split(" /at ");
                 if (eventDetails.length <= 1) {
                     throw new IllegalCommandException(commandArgs[0]);
-                } else {
-                    Duke.addTask(new Event(eventDetails[0],
-                            eventDetails[1]));
                 }
+                Duke.addTask(new Event(eventDetails[0],
+                        eventDetails[1]));
                 break;
             default:
                 throw new CommandNotFoundException(command);
