b118e2c705fdd36e440673d5de567d4074901505
Zong Xian <kangzongxian88@gmail.com>
2022-08-17 19:51:38 +0800

    Added Ability to store text into Array (Level-2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,11 @@ public class Duke {
 
     public static void main(String[] args) {
 
+        // Create an Array to store the text the user has entered of size 100
+        String[] storedTextArray = new String[100];
+        // Create a count to track the number of items the user has stored
+        int count = 0;
+
         String userCommand = "";
 
         // Creates a scanner to accept input
@@ -24,15 +29,21 @@ public class Duke {
         // When user does not type bye, ask for input and print out the input
         while (!userCommand.equals("bye")) {
             System.out.println("----------------------");
-            System.out.println(userCommand);
+            // If user types list, output all the text being stored
+            if (userCommand.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println(String.valueOf(i + 1) + ". " + storedTextArray[i]);
+                }
+            } else {
+                // For anything else the user types, store into the array
+                storedTextArray[count] = userCommand;
+                count += 1;
+                System.out.println("added:" + userCommand);
+            }
             System.out.println("----------------------");
             userCommand = enterInput.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon ^^!");
-
-
-
-
     }
 }
