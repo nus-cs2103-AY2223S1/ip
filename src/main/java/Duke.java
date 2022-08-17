ae1ca3111f31f0092376f785d0785ee7b78b7d1b
Amresh <amresh2000@gmail.com>
2022-08-17 15:58:20 +0800

    Level 3. Mark as Done Added new Class Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> storeList = new ArrayList<>();
+    private static ArrayList<Task> storeList = new ArrayList<>();
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -26,7 +26,17 @@ public class Duke {
             if(userInput.equals("bye")) {
                 break;
             }
-            add(userInput);
+            if(userInput.split(" ", 2)[0].equals("mark")) {
+                int inputTaskIndex = Integer.parseInt(userInput.split(" ", 2)[1]) - 1;
+                mark(inputTaskIndex);
+                continue;
+            }
+            if(userInput.split(" ", 2)[0].equals("unmark")) {
+                int inputTaskIndex = Integer.parseInt(userInput.split(" ", 2)[1]) - 1;
+                unmark(inputTaskIndex);
+                continue;
+            }
+            addTask(userInput);
         }
 
         farewell();
@@ -43,15 +53,26 @@ public class Duke {
         System.out.println(userInput);
     }
 
-    public static void add(String userInput) {
-        storeList.add(userInput);
-        System.out.println("added: " + userInput);
+    public static void addTask(String userInput) {
+        Task t = new Task(userInput);
+        storeList.add(t);
+        System.out.println("added: " + t.description);
     }
 
     public static void list() {
+        System.out.println("Here are the tasks in your list:");
         for(int i=0; i < storeList.size(); i++){
-            System.out.println(i + 1 + ". " + storeList.get(i));
+//            System.out.printf("%i. [%c] %s", i + 1, storeList.get(i).getStatusIcon(), storeList.get(i).description);
+            System.out.println(i + 1 + ". " + "[" + storeList.get(i).getStatusIcon() + "] " + storeList.get(i).description);
         }
+    }
+
+    public static void mark(int taskIndex) {
+        storeList.get(taskIndex).markAsDone();
+    }
+
+    public static void unmark(int taskIndex) {
+        storeList.get(taskIndex).markAsNotDone();
     }
 
     public static void farewell() {
