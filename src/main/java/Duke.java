b78097bcee540cbe8ada635f7c48373606318a21
rui-han-crh <richardcrh2000@gmail.com>
2022-08-17 20:50:01 +0800

    Finishes Level-2 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,16 +13,51 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        Echo();
+        addToList();
+
+        printInStyle("Goodbye! Hope to see you again soon!");
     }
 
-    private static void Echo() {
+    public static void printInStyle(String stringToPrint) {
+        System.out.println("\t" + "-".repeat(25) + "\n\t\t" + stringToPrint);
+        System.out.println("\t" + "-".repeat(25));
+    }
+
+    public static String askForInput(String prompt) {
+        System.out.print(prompt + " ");
+        return scanner.nextLine();
+    }
+
+    private static void echo() {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            System.out.printf("Type something: ");
-            System.out.println("\t" + "-".repeat(25) + "\n\t\t" + sc.nextLine());
-            System.out.println("\t" + "-".repeat(25));
+        String input = askForInput("Type something:");
+        while (!input.toLowerCase(Locale.ROOT).equals("bye")) {
+            printInStyle(input);
+            input = askForInput("Type something:");
+        }
+    }
+
+    private static void addToList() {
+        List<String> toDo = new ArrayList<String>(100);
+
+        String input = askForInput("Add something:");
+
+        while (!input.toLowerCase(Locale.ROOT).equals("bye")) {
+            if (input.equals("list")) {
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < toDo.size(); i++) {
+                    builder.append(String.format("%d. %s", i + 1, toDo.get(i)));
+                    if (i < toDo.size() - 1) {
+                        builder.append("\n\t\t");
+                    }
+                }
+                printInStyle(builder.toString());
+            } else {
+                toDo.add(input);
+                printInStyle(String.format("added: %s", input));
+            }
+            input = askForInput("Add something:");
         }
     }
 }
