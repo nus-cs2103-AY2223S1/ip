a101af3cb1c764ff92990ce14f6c248c56923fc3
ish1506 <ishiuanlee@gmail.com>
2022-08-17 19:44:41 +0800

    Implement add and list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,9 @@ public class Duke {
         String name = "Duke";
         System.out.println("Hello! I'm " + name + "\nHow can I help you?");
 
+        String[] tasks = new String[100];
+        int currIndex = 0;
+
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
@@ -12,7 +15,15 @@ public class Duke {
                 System.out.println("Bye! See you again :)");
                 return;
             }
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < currIndex; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+            }
+            else {
+                tasks[currIndex++] = command;
+                System.out.println("added: " + command);
+            }
         }
 
     }
