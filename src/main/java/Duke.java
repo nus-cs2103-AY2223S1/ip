1d070d222504b19b47ab43a17175747c982b96ea
ish1506 <ishiuanlee@gmail.com>
2022-08-17 20:11:53 +0800

    Implement mark and unmark functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,10 +5,11 @@ public class Duke {
         String name = "Duke";
         System.out.println("Hello! I'm " + name + "\nHow can I help you?");
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int currIndex = 0;
 
         Scanner scanner = new Scanner(System.in);
+
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
             if (command.equals("bye")) {
@@ -19,12 +20,22 @@ public class Duke {
                 for (int i = 0; i < currIndex; i++) {
                     System.out.println(i + 1 + ". " + tasks[i]);
                 }
+                continue;
+            }
+            if (command.contains("unmark")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                tasks[index].unmark();
+                System.out.println("Okay, this task is now unchecked:\n" + tasks[index]);
+            }
+            else if (command.contains("mark")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                tasks[index].mark();
+                System.out.println("Great! This task is completed:\n" + tasks[index]);
             }
             else {
-                tasks[currIndex++] = command;
+                tasks[currIndex++] = new Task(command);
                 System.out.println("added: " + command);
             }
         }
-
     }
 }
