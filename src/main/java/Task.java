a73d931664d62a63d16d63a8d0e93e25e96cdab7
LAPTOP-S3RN5C85\eesua <eesuan.234@gmail.com>
2022-08-17 16:38:47 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+
+    protected boolean isDone;
+    protected String description;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+    }
+}
