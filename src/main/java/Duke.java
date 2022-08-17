15d4857586e4496e3d9362a9631fd61347944448
Shawn Ng <shawn.nghh@gmail.com>
2022-08-17 16:58:01 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
         ArrayList<Boolean> marked = new ArrayList<>();
+        ArrayList<String> type = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -27,7 +28,7 @@ public class Duke {
                     System.out.println(line_divider);
                     System.out.println("Here are your tasks la:");
                     for (int j = 0; j < list.size(); j++) {
-                        System.out.println(j + 1 + "." + (marked.get(j) ? "[X] " : "[ ] ") + list.get(j));
+                        System.out.println(j + 1 + "." + type.get(j) + (marked.get(j) ? "[X] " : "[ ] ") + list.get(j));
                     }
                     System.out.println(line_divider);
                     break;
@@ -49,14 +50,46 @@ public class Duke {
                     System.out.println((marked.get(index) ? "[X] " : "[ ] ") + list.get(index));
                     System.out.println(line_divider);
                     break;
-                }
-                if (i == msgWords.length - 1) {
+                } else if (msgWords[i].equalsIgnoreCase("todo")) {
+                    String item = curr.substring(5);
                     marked.add(false);
-                    list.add(curr);
+                    list.add(item);
+                    type.add("[T]");
                     System.out.println(line_divider);
-                    System.out.println("added: " + curr);
+                    System.out.println("Ok I add your task already:");
+                    System.out.println("[T]" + "[ ] " + item + "\nNow " + list.size() + " tasks already");
+                    System.out.println(line_divider);
+                    break;
+                } else if (msgWords[i].equalsIgnoreCase("deadline")) {
+                    int slashPos = curr.indexOf("/by");
+                    String taskName = curr.substring(9, slashPos - 1);
+                    String deadline = " (by:" + curr.substring(slashPos + 3) + ")";
+                    marked.add(false);
+                    list.add(taskName + deadline);
+                    type.add("[D]");
+                    System.out.println(line_divider);
+                    System.out.println("Ok I add your task already:");
+                    System.out.println("[D]" + "[ ] " + taskName + deadline + "\nNow " + list.size() + " tasks already!");
+                    System.out.println(line_divider);
+                } else if (msgWords[i].equalsIgnoreCase("event")) {
+                    int slashPos = curr.indexOf("/at");
+                    String taskName = curr.substring(6, slashPos - 1);
+                    String deadline = " (at:" + curr.substring(slashPos + 3) + ")";
+                    marked.add(false);
+                    list.add(taskName + deadline);
+                    type.add("[E]");
+                    System.out.println(line_divider);
+                    System.out.println("Ok I add your task already:");
+                    System.out.println("[E]" + "[ ] " + taskName + deadline + "\nNow " + list.size() + " tasks already!");
                     System.out.println(line_divider);
                 }
+//                if (i == msgWords.length - 1) {
+//                    marked.add(false);
+//                    list.add(curr);
+//                    System.out.println(line_divider);
+//                    System.out.println("added: " + curr);
+//                    System.out.println(line_divider);
+//                }
             }
         }
     }
