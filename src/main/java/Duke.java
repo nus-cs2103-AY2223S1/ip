8733246fb351f0693f143ed8fefb02f9dda051b0
chun <lamchunyu00@gmail.com>
2022-08-17 12:56:59 +0800

    Added different types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,12 @@ import java.util.function.UnaryOperator;
 
 public class Duke {
 
+    // TODO: 17/8/2022
+    //  Refactor printing of TodoList
+    //  make the individual elements print their own shit
+    //  inherit from Task
+
+
     private static String divider() {
         return "--------------------";
     }
@@ -49,6 +55,39 @@ public class Duke {
                 public void unmark() {
                     this.isDone = false;
                 }
+
+                @Override
+                public String toString() {
+                    return String.format("[T] %s %s", checkbox(this.getIsDone()), this.getDescription());
+                }
+            }
+
+            class Deadline extends Task {
+                String deadline;
+
+                public Deadline(String description, String deadline) {
+                    super(description);
+                    this.deadline = deadline;
+                }
+
+                @Override
+                public String toString() {
+                    return String.format("[D] %s %s (by: %s)", checkbox(this.getIsDone()), this.getDescription(), this.deadline);
+                }
+            }
+
+            class Event extends Task {
+                String time;
+
+                public Event(String description, String time) {
+                    super(description);
+                    this.time = time;
+                }
+
+                @Override
+                public String toString() {
+                    return String.format("[E] %s %s (at: %s)", checkbox(this.getIsDone()), this.getDescription(), this.time);
+                }
             }
 
             int pointer = 0;
@@ -62,6 +101,16 @@ public class Duke {
 
             public void addTask(String description) {
                 this.list[pointer] = new Task(description);
+                pointer++;
+            }
+
+            public void addDeadline(String desc, String deadline) {
+                this.list[pointer] = new Deadline(desc, deadline);
+                pointer++;
+            }
+
+            public void addEvent(String desc, String time) {
+                this.list[pointer] = new Event(desc, time);
                 pointer++;
             }
 
@@ -87,8 +136,8 @@ public class Duke {
                         return;
                     }
                     Task task = this.get(i);
-                    //                  Could use format strings for this
-                    System.out.println((i + 1) + ". " + checkbox(task.getIsDone()) + " " + task.getDescription());
+                    // Could use format strings for this
+                    System.out.println((i + 1) + ". " + task);
                 }
             }
         }
@@ -131,6 +180,26 @@ public class Duke {
 
                         todolist.unmark(index - 1);
                         todolist.printData();
+                        break;
+                    }
+
+                    if (line.matches(".* by .*")) {
+                        String[] res = line.split(" by ");
+                        String desc = res[0];
+                        String deadline = res[1];
+
+                        todolist.addDeadline(desc,deadline);
+                        System.out.println(String.format("Added: %s (by: %s)", desc, deadline));
+                        break;
+                    }
+
+                    if (line.matches(".* at .*")) {
+                        String[] res = line.split(" at ");
+                        String desc = res[0];
+                        String time = res[1];
+
+                        todolist.addEvent(desc, time);
+                        System.out.println(String.format("Added: %s (at: %s)", desc, time));
                         break;
                     }
 
