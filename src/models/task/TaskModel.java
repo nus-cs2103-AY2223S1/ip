4e058f3b73d1f20717c3d1c5eeb3fef936cb885b
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2022-08-17 10:57:29 +0800

    Add methods for task creation and view all to TaskModel

--- src/models/task/TaskModel.java
+++ src/models/task/TaskModel.java
@@ -1,7 +1,35 @@
 package models.task;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- * Manages state for Tasks and exposes methods for CRUD
+ * Manages state for Tasks and exposes methods for CRUD. Injected into Commands that require usage.
  */
 public class TaskModel {
+    List<Task> tasks;
+    public TaskModel() {
+        this.tasks = new ArrayList<>();
+    }
+
+    /** Creates and adds a new Task, then returns the newly added Task.
+     * @param description
+     * @return The task created
+     */
+    public Task addTask(String description) {
+        Task newTask = new ConcreteTask(description);
+        tasks.add(newTask);
+        return newTask;
+    }
+
+    /**
+     * @return a Collection of all the Tasks at the current time
+     */
+    public Collection<Task> getAllTasks() {
+        return tasks;
+    }
+
+
 }
