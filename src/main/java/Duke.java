01358f63befc403e616d9c1f57551b1bb8d514e3
totsukatomofumi <totsukatomofumi@gmail.com>
2022-08-18 03:21:01 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,23 @@
+package main.java;
 import java.util.Scanner;
 
 /**
  * Duke, a Personal Assistant Chatbot that helps a person to keep track of various things.
  *
- * @author: Totsuka Tomofumi
- * @version: Level-2
+ * @author Totsuka Tomofumi
+ * @version Level-3
  */
 public class Duke {
     /**
      * Duke initialises with his personal logo and a welcome message before prompting for standard input
      * from the command line. Each input is remembered as a task by Duke and a list can be retrieved
-     * if the user types "list". If the user types "bye", Duke terminates with a goodbye message.
+     * if the user types "list". Tasks can be marked or unmarked done. If the user types "bye",
+     * Duke terminates with a goodbye message.
      * @param args Command line arguments
      */
     public static void main(String[] args) {
         //assume no more than 100 tasks
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int index = 0;
         Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
@@ -23,20 +25,49 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println(logo);
         System.out.println("Hello! I'm Duke.\nWhat can I do for you?");
+
         while (true) {
             String response = scanner.nextLine();
+            //bye command
             if (response.equals("bye")) {
                 break;
+            //list command
             } else if (response.equals("list")) {
                 int order = 1;
-                for (String str : tasks) {
-                    if (str == null) break;
-                    System.out.println(order++ + ". " + str);
+                System.out.println("Here are the tasks in your list:");
+                for (Task task : tasks) {
+                    if (task == null) {
+                        break;
+                    }
+                    System.out.println(order++ + "." + task.toString());
+                }
+            //mark command
+            } else if (response.startsWith("mark ")) {
+                try {
+                    int query = Integer.parseInt(response.substring(4).strip()) - 1;
+                    tasks[query].mark();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks[query].toString());
+                } catch (NumberFormatException e) {
+                    tasks[index++] = new Task(response);
+                    System.out.println("added: " + response);
+                }
+            //unmark command
+            } else if (response.startsWith("unmark ")) {
+                try {
+                    int query = Integer.parseInt(response.substring(6).strip()) - 1;
+                    tasks[query].unmark();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(tasks[query].toString());
+                } catch (NumberFormatException e) {
+                    tasks[index++] = new Task(response);
+                    System.out.println("added: " + response);
                 }
             } else {
-                tasks[index++] = response;
+                tasks[index++] = new Task(response);
                 System.out.println("added: " + response);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,67 @@
+package main.java;
+
+/**
+ * Task encapsulates the task description and its status of completion.
+ * It supports toggling of task status.
+ *
+ * @author Totsuka Tomofumi
+ * @version Level-3
+ */
+public class Task {
+    /**
+     * Statuses a task can be in.
+     */
+    private enum Status {
+        DONE,
+        NOTDONE
+    }
+
+    /**
+     * Description of task.
+     */
+    private String description;
+    /**
+     * Status of task.
+     */
+    private Status status;
+
+    /**
+     * Constructor for this task.
+     * @param description Description of task
+     */
+    public Task(String description) {
+        this.description = description;
+        //for now, new tasks added assumed to be undone
+        this.status = Status.NOTDONE;
+    }
+
+    /**
+     * Marks this task as done.
+     */
+    public void mark() {
+        this.status = Status.DONE;
+    }
+
+    /**
+     * Unmarks this task as done.
+     */
+    public void unmark() {
+        this.status = Status.NOTDONE;
+    }
+
+    /**
+     * Returns a string representation of the task.
+     * @return Task status and its description
+     */
+    @Override
+    public String toString() {
+        switch (this.status) {
+            case DONE:
+                return "[X] " + this.description;
+            case NOTDONE:
+                return "[ ] " + this.description;
+            default:
+                return "";  //should not come here
+        }
+    }
+}
