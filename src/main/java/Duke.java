d1f1407532e455d323683b83f90b6c6265bd0230
shawnkai <e0525991@u.nus.edu>
2022-08-17 22:17:29 +0800

    edited duke.java and completed level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,9 +6,20 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         Scanner myObj = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<String> ();
+        int number;
         String input = myObj.nextLine();  // Read user input
         while (!input.equals("bye")) {
-            System.out.println(input);  // Output user input
+            if (!input.equals("list")) {
+                tasks.add(input);
+                System.out.println("added: " + input);  // Output user input
+
+            } else {
+                for (String task : tasks) {
+                    number = tasks.indexOf(task);
+                    System.out.println(String.valueOf(number + 1) + ". " + task);
+                }
+            }
             input = myObj.nextLine(); // Read next user input
         }
         System.out.println("Bye. Hope to see you again soon!");
