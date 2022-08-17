5fa8f59d74cf556b78118160a04d3cf539a4a174
yeoyujie <yeo.yujie@gmail.com>
2022-08-18 03:22:55 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,12 +4,22 @@ public class Duke {
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm SoCCat \nWhat can I do for you?");
-        Scanner userInput = new Scanner(System.in);
-        String userName = userInput.nextLine();
+        Scanner input = new Scanner(System.in);
+        String currInput = input.nextLine();
+        String[] userInputs = new String[100];
+        int index = 0;
 
-        while (!userName.equals("bye")) {
-            System.out.println(userName);
-            userName = userInput.nextLine();
+        while (!currInput.equals("bye")) {
+            if (currInput.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println(i + ". " + userInputs[i]);
+                }
+            } else {
+                System.out.println("added: " + currInput);
+                userInputs[index] = currInput;
+                index++;
+            }
+                currInput = input.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
