8cd4e492703eefa75642d57548817a24aebfed1c
Mohamed Hamas <e0726186@u.nus.edu>
2022-08-18 00:18:53 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello!\nWhat can I do for you ?");
+        Scanner help = new Scanner(System.in);
+
+
+        while(true) {
+           String str = help.nextLine();
+           if (str .equals("bye")) {
+               System.out.println("Bye see you again buddy !");
+               break;
+           }
+           System.out.println(str);
+
+       }
+
+
     }
 }
