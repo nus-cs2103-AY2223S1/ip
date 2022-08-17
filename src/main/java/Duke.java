c0b12354c5c2d1adb66db13f83b7992a46a679df
cxyterence <e0725909@u.nus.edu>
2022-08-18 01:36:07 +0800

    added ability to mark task as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanObj = new Scanner(System.in);   // Create Scanner object
-        String[] strLst = new String[100];
+        Task[] taskLst = new Task[100];
 
         String intro = "Hello! My name is GustavoBot, but you can call me Gus\n"
                 + "How may I help you today?";
@@ -15,19 +15,31 @@ public class Duke {
 
             if (act.equals("list")) {
                 for (int i = 0; i < 100; i++) {
-                    if (strLst[i] == null) {
+                    if (taskLst[i] == null) {
                         break;
                     }
                     int index = i + 1;
-                    System.out.println(index + ". " + strLst[i]);
+                    System.out.println(index + ".[" + taskLst[i].getStatusIcon() + "] " + taskLst[i].getDesc());
                 }
+            } else if (act.contains("unmark")) {
+                String numberOnly = act.replaceAll("[^0-9]", "");
+                int index = Integer.parseInt(numberOnly);
+                taskLst[index - 1].unmarkDone();
+                System.out.println("Alright, this task is marked as not done yet:\n"
+                        + "[ ] " + taskLst[index - 1].getDesc());
+            } else if (act.contains("mark")) {
+                String numberOnly = act.replaceAll("[^0-9]", "");
+                int index = Integer.parseInt(numberOnly);
+                taskLst[index - 1].markDone();
+                System.out.println("Alright, this task is marked as done:\n"
+                        + "[X] " + taskLst[index - 1].getDesc());
             } else if (act.equals("bye")) {
                 System.out.println("Goodbye, hope to see you again soon!");
                 break;
             } else {
                 for (int i = 0; i < 100; i++) {
-                    if (strLst[i] == null) {
-                        strLst[i] = act;
+                    if (taskLst[i] == null) {
+                        taskLst[i] = new Task(act);
                         break;
                     }
                 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String desc;
+    protected boolean isDone;
+
+    public Task(String desc) {
+        this.desc = desc;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");    // mark done task
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+    }
+}
