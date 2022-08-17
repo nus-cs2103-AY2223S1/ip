375c744cf45ad673cc03b1bdaea38f8a9d0db853
Jason Qiu <jason_qiu@hotmail.com>
2022-08-17 15:35:39 +0800

    feat: add greeting

---
+++ src/main/java/Cheese.java
@@ -0,0 +1,10 @@
+public class Cheese {
+  public static void main(String[] args) {
+    final String BORDER = "-----";
+
+    String greeting = new String("Woof! I'm Cheese, your puppy assistant.\n"
+            + "What can I do for you?\n"
+            + BORDER);
+    System.out.println(greeting);
+  }
+}
