82d774a0667f30c9ae6c529a1e817460205fafca
Pinran-J <e0725480@u.nus.edu>
2022-08-18 00:04:20 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void echo() {
+        Scanner myScan = new Scanner(System.in);
+        String s;
+
+        while (true) {
+            s = myScan.nextLine();
+            if (s.equals("bye")) {
+                System.out.println("----------------------");
+                System.out.println("Bye, hope to see you again!");
+                System.out.println("----------------------");
+                break;
+            } else {
+                System.out.println("----------------------");
+                System.out.println(s);
+                System.out.println("----------------------");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +28,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("----------------------");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can i do for you?");
+        System.out.println("----------------------");
+
+        echo();
     }
 }
