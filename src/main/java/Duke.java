6badea537eb2562ef37937dfc2cde3163d344ee3
DESKTOP-LBC8HLP\User <greggchan12345@gmail.com>
2022-08-18 02:37:44 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -71,10 +71,14 @@ public class Duke {
 
         while (!currreply.equals("bye")) {
             if (currreply.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                list.forEach(n -> System.out.println((list.indexOf(n) + 1) + "."
-                        + n.toString()));
-                System.out.println();
+                if (list.size() == 0) {
+                    System.out.println("There are no tasks in your list. :)");
+                } else {
+                    System.out.println("Here are the tasks in your list:");
+                    list.forEach(n -> System.out.println((list.indexOf(n) + 1) + "."
+                            + n.toString()));
+                    System.out.println();
+                }
                 currreply = reply.nextLine();
             } else if (currreply.startsWith("mark")) {
                 int index = Integer.parseInt(currreply.substring(5)) - 1;
@@ -98,6 +102,18 @@ public class Duke {
                             " " + task.getType() + "[ ] " + task.getDescription() + "\n");
                 }
                 currreply = reply.nextLine();
+            } else if (currreply.startsWith("delete")) {
+                int index = Integer.parseInt(currreply.substring(7)) - 1;
+                if (index > list.size() - 1) {
+                    System.out.println("You have no such tasks.\n");
+                } else {
+                    Task task = list.get(index);
+                    list.remove(index);
+                    System.out.println("Noted. I've removed this task:\n" +
+                            " " + task.toString() + "\n" + "Now you have " +
+                            list.size() + " tasks in the list.\n");
+                }
+                currreply = reply.nextLine();
             } else {
                 if (currreply.startsWith("todo")) {
                     if (currreply.length() <= 4) {
@@ -115,27 +131,35 @@ public class Duke {
                     if (currreply.length() <= 8) {
                         System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.\n");
                     } else {
-                        int slashindex = currreply.indexOf("/");
-                        String acttask = currreply.substring(9, slashindex - 1);
-                        Task currtask = new Task(acttask, 1);
-                        currtask.setDate(currreply.substring(slashindex + 4));
-                        list.add(currtask);
-                        System.out.println("Got it. I've added this task:\n" +
-                                " " + currtask.toString() + "\n" +
-                                "Now you have " + list.size() + " tasks in the list.\n");
+                        if (!currreply.contains("/")) {
+                            System.out.println("There is no date! >:(\n");
+                        } else {
+                            int slashindex = currreply.indexOf("/");
+                            String acttask = currreply.substring(9, slashindex - 1);
+                            Task currtask = new Task(acttask, 1);
+                            currtask.setDate(currreply.substring(slashindex + 4));
+                            list.add(currtask);
+                            System.out.println("Got it. I've added this task:\n" +
+                                    " " + currtask.toString() + "\n" +
+                                    "Now you have " + list.size() + " tasks in the list.\n");
+                        }
                     }
                 } else if (currreply.startsWith("event")) {
                     if (currreply.length() <= 5) {
                         System.out.println("☹ OOPS!!! The description of a event cannot be empty.\n");
                     } else {
-                        int slashindex = currreply.indexOf("/");
-                        String acttask = currreply.substring(6, slashindex - 1);
-                        Task currtask = new Task(acttask, 2);
-                        currtask.setDate(currreply.substring(slashindex + 4));
-                        list.add(currtask);
-                        System.out.println("Got it. I've added this task:\n" +
-                                " " + currtask.toString() + "\n" +
-                                "Now you have " + list.size() + " tasks in the list.\n");
+                        if (!currreply.contains("/")) {
+                            System.out.println("There is no date! >:(\n");
+                        } else {
+                            int slashindex = currreply.indexOf("/");
+                            String acttask = currreply.substring(6, slashindex - 1);
+                            Task currtask = new Task(acttask, 2);
+                            currtask.setDate(currreply.substring(slashindex + 4));
+                            list.add(currtask);
+                            System.out.println("Got it. I've added this task:\n" +
+                                    " " + currtask.toString() + "\n" +
+                                    "Now you have " + list.size() + " tasks in the list.\n");
+                        }
                     }
                 } else {
                     System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n");
