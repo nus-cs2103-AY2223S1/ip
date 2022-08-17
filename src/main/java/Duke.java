162bdb49d9dcce9a887e291b5e1874c8967e2be4
kxrt <kvrtikeya@gmail.com>
2022-08-17 21:08:58 +0800

    Level-1
    - Echo commands that are not "bye"
    - Exit if "bye" command is issued

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+    Scanner s;
+    static final String intro =
+        "\nWelcome to Apollo.\nHow can I help you today?";
+
+    static final String newIntro =
+        "┌────────────────────┐\n" +
+            "│ Welcome to APOLLO! │\n" +
+        "└────────────────────┘\n" +
+            "How can I help you today?";
+
+
+    public Duke() {
+        s = new Scanner(System.in);
+    }
+
+    void run() {
+        System.out.println(newIntro);
+        checkInput(s.next());
+    }
+
+    void checkInput(String inputString) {
+        if (inputString.equals("bye")) {
+            System.out.println("Goodbye, see you soon!");
+        } else {
+            System.out.println(inputString);
+            checkInput(s.next());
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke instance = new Duke();
+        instance.run();
     }
 }
