2caaa3a333ce2032059cfebe0f719622f3b95843
Naren <narensreekanth@gmail.com>
2022-08-17 18:05:29 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,16 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello I'm Duke\nWhat can I do for you");
+        String item = scan.nextLine();
+
+        while (!item.equals("bye")) {
+            System.out.println(item);
+            item = scan.nextLine();
+        }
+        System.out.println("Bye. hope to see you again soon!");
     }
 }
