5a12c962b8eb03e9daa8183e5f4aa6b90768aad7
LimAiLin <limailin2009@gmail.com>
2022-08-17 19:55:23 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+
+        System.out.println(line + "\n"
+                + "Dino:\n"
+                + "     Hello! I'm Dino\n"
+                + "     What can I do for you?\n");
+
+        Scanner sc = new Scanner(System.in);
+        String curr = "";
+
+        while (!Objects.equals(curr, "bye")) {
+            curr = sc.nextLine();
+            if (!Objects.equals(curr, "bye")) {
+                System.out.println("Dino:\n"
+                        + "     " + curr + "\n");
+            }
+        }
+
+        System.out.println("Dino:\n"
+                + "     Bye. Hope to see you again soon!");
     }
 }
