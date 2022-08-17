6416745b965664d7e0243da80c0367e605319e63
bigcrushes <teocharlton@gmail.com>
2022-08-18 03:27:52 +0800

    added exceptions for empty descriptions and unknown commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,22 @@
+import java.rmi.activation.UnknownObjectException;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+
+    public static class UnknownCommandException extends RuntimeException {
+        public UnknownCommandException() {
+            super("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+    }
+
+    public static class EmptyDescriptionException extends RuntimeException {
+        public EmptyDescriptionException(String message) {
+            super("☹ OOPS!!! The description of a " + message + " cannot be empty.");
+        }
+    }
+
     public static class Task {
         protected boolean isDone;
         protected String description;
@@ -88,67 +102,85 @@ public class Duke {
 
         while (true) {
             String command = scanner.nextLine();
-            String[] commands = command.split(" ");
             System.out.println("____________________________________________________________");
-            if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                break;
-            } else if (command.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(i + 1 + "." + tasks.get(i));
-                }
-            } else if (commands[0].equals("mark")) {
-                System.out.println("Nice! I've marked this task as done:");
-                int index = Integer.parseInt(commands[1]) - 1;
-                tasks.get(index).mark();
-                System.out.println(tasks.get(index));
-            } else if (commands[0].equals("unmark")) {
-                System.out.println("OK, I've marked this task as not done yet:");
-                int index = Integer.parseInt(commands[1]) - 1;
-                tasks.get(index).unmark();
-                System.out.println(tasks.get(index));
-            } else if (commands[0].equals("todo")) {
-                System.out.println("Got it. I've added this task:");
-                String[] newCommands = Arrays.copyOfRange(commands, 1, commands.length);
-                String newCommand = String.join(" ", newCommands);
-                tasks.add(new Todo(newCommand));
-                System.out.println(tasks.get(tasks.size() - 1));
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-            } else if (commands[0].equals("event")) {
-                int atMarker = 1;
-                System.out.println("Got it. I've added this task:");
-                for (int i = 0; i < commands.length; i++) {
-                    if (commands[i].equals("/at")){
-                        atMarker = i;
+            String[] commands = command.split(" ");
+            try {
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                    break;
+                } else if (command.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i + 1 + "." + tasks.get(i));
                     }
-                }
-                String[] newCommands = Arrays.copyOfRange(commands, 1, atMarker);
-                String newCommand = String.join(" ", newCommands);
-                String[] time = Arrays.copyOfRange(commands, atMarker + 1, commands.length);
-                String timeString = String.join(" ", time);
-                tasks.add(new Event(newCommand, timeString));
-                System.out.println(tasks.get(tasks.size() - 1));
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-            } else if (commands[0].equals("deadline")) {
-                int atMarker = 1;
-                System.out.println("Got it. I've added this task:");
-                for (int i = 0; i < commands.length; i++) {
-                    if (commands[i].equals("/by")){
-                        atMarker = i;
+                } else if (commands[0].equals("mark")) {
+                    if (commands.length == 1) {
+                        throw new EmptyDescriptionException("mark");
                     }
+                    System.out.println("Nice! I've marked this task as done:");
+                    int index = Integer.parseInt(commands[1]) - 1;
+                    tasks.get(index).mark();
+                    System.out.println(tasks.get(index));
+                } else if (commands[0].equals("unmark")) {
+                    if (commands.length == 1) {
+                        throw new EmptyDescriptionException("unmark");
+                    }
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    int index = Integer.parseInt(commands[1]) - 1;
+                    tasks.get(index).unmark();
+                    System.out.println(tasks.get(index));
+                } else if (commands[0].equals("todo")) {
+                    if (commands.length == 1) {
+                        throw new EmptyDescriptionException("todo");
+                    }
+                    System.out.println("Got it. I've added this task:");
+                    String[] newCommands = Arrays.copyOfRange(commands, 1, commands.length);
+                    String newCommand = String.join(" ", newCommands);
+                    tasks.add(new Todo(newCommand));
+                    System.out.println(tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                } else if (commands[0].equals("event")) {
+                    if (commands.length == 1) {
+                        throw new EmptyDescriptionException("event");
+                    }
+                    int atMarker = 1;
+                    System.out.println("Got it. I've added this task:");
+                    for (int i = 0; i < commands.length; i++) {
+                        if (commands[i].equals("/at")) {
+                            atMarker = i;
+                        }
+                    }
+                    String[] newCommands = Arrays.copyOfRange(commands, 1, atMarker);
+                    String newCommand = String.join(" ", newCommands);
+                    String[] time = Arrays.copyOfRange(commands, atMarker + 1, commands.length);
+                    String timeString = String.join(" ", time);
+                    tasks.add(new Event(newCommand, timeString));
+                    System.out.println(tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                } else if (commands[0].equals("deadline")) {
+                    if (commands.length == 1) {
+                        throw new EmptyDescriptionException("deadline");
+                    }
+                    int atMarker = 1;
+                    System.out.println("Got it. I've added this task:");
+                    for (int i = 0; i < commands.length; i++) {
+                        if (commands[i].equals("/by")) {
+                            atMarker = i;
+                        }
+                    }
+                    String[] newCommands = Arrays.copyOfRange(commands, 1, atMarker);
+                    String newCommand = String.join(" ", newCommands);
+                    String[] deadline = Arrays.copyOfRange(commands, atMarker + 1, commands.length);
+                    String deadlineString = String.join(" ", deadline);
+                    tasks.add(new Deadline(newCommand, deadlineString));
+                    System.out.println(tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                } else {
+                    throw new UnknownCommandException();
                 }
-                String[] newCommands = Arrays.copyOfRange(commands, 1, atMarker);
-                String newCommand = String.join(" ", newCommands);
-                String[] deadline = Arrays.copyOfRange(commands, atMarker + 1, commands.length);
-                String deadlineString = String.join(" ", deadline);
-                tasks.add(new Deadline(newCommand, deadlineString));
-                System.out.println(tasks.get(tasks.size() - 1));
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-            } else {
-                tasks.add(new Task(command));
-                System.out.println("added: " + command);
+            } catch (UnknownCommandException | EmptyDescriptionException e) {
+                System.out.println(e.getMessage());
             }
             System.out.println("____________________________________________________________");
         }
