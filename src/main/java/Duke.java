ac79d9be60673f0d8fa2f59f91913bc8a884ada3
rycs2812 <ryan.yong@live.com>
2022-08-17 17:52:35 +0800

    Add support for todo, event, deadline tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Duke {
     private static Task[] tasks = new Task[100];
     private static int count = 0;
-
-    public static boolean is(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch(NumberFormatException e){
-            return false;
-        }
-    }
+    private static String lineBreak = "____________________________________________________________";
 
     public static void printBot(String s) {
-        String lineBreak = "____________________________________________________________";
         System.out.println(lineBreak);
         System.out.println(s);
         System.out.println(lineBreak);
         System.out.println();
     }
 
-    public static void printBot(Task[] tasks) {
+    public static void addTask(Task t) {
+        tasks[count] = t;
+        ++count;
+
+        System.out.println(lineBreak);
+        System.out.println("Got it. I've added this task:\n"
+                           + "  " + t);
+        System.out.println("Now you have " + count + " tasks in the list.");
+        System.out.println(lineBreak);
+        System.out.println();
+    }
+
+    public static void listTasks() {
         String lineBreak = "____________________________________________________________";
         System.out.println(lineBreak);
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.length; ++i) {
             if (tasks[i] != null) {
                 System.out.println((i + 1) + ". " + tasks[i]);
@@ -50,27 +53,52 @@ public class Duke {
         System.out.println("Hello! I'm Nuke");
         System.out.println("What can I do for you?");
         String s;
-        String markRegex = "(un)?mark\\s[1-9][0-9]*";
-        Pattern markPt = Pattern.compile(markRegex);
         while(true) {
             s = scanner.nextLine();
-            if (markPt.matcher(s).matches()) {
-                String[] temp = s.split(" ");
-                int index = Integer.parseInt(temp[1]) - 1;
-                if (index >= count) {
-                    printBot("Task does not exist. Please ensure the task number is correct.");
-                } else {
-                    markTask(tasks[index], temp[0].equals("mark"));
+            String[] words = s.split(" ", 2);
+            if (words[0].equals("mark") || words[0].equals("unmark")) {
+                if (words.length < 2) {
+                    printBot("Error: Please specify an item number");
                 }
-            } else if (s.equals("list")) {
-                printBot(tasks);
-            } else if (s.equals("bye")) {
+                try {
+                    int index = Integer.parseInt(words[1]) - 1;
+                    if (index < 0 || index > count) {
+                        printBot("Error: Please specify a valid item number");
+                    } else {
+                        markTask(tasks[index], words[0].equals("mark"));
+                    }
+                } catch (NumberFormatException e) {
+                    printBot("Error: Please specify an item number\n"
+                            + "\"" + words[1] + "\"" + " is not an item number");
+                }
+            } else if (words[0].equals("todo")) {
+                addTask(new ToDo(words[1]));
+            } else if (words[0].equals("deadline")) {
+                String[] temp = words[1].split(" /by ", 2);
+                if (temp.length < 2) {
+                    printBot("Error: Please specify a deadline");
+                } else {
+                    addTask(new Deadline(temp[0], temp[1]));
+                }
+            } else if (words[0].equals("event")) {
+                String[] temp = words[1].split(" /at ", 2);
+                if (temp.length < 2) {
+                    printBot("Error: Please specify an event date");
+                } else {
+                    addTask(new Event(temp[0], temp[1]));
+                }
+            } else if (words[0].equals("list")) {
+                if (words.length > 1) {
+                    printBot("Error: list expects no arguments");
+                } else {
+                    listTasks();
+                }
+            } else if (s.equals("bye")){
                 printBot("Bye. Hope to see you again soon!");
                 return;
             } else {
-                tasks[count] = new Task(s);
-                ++count;
-                printBot("added: " + s);
+                printBot("Error: Please enter a valid command:\n"
+                         + "mark\n" + "unmark\n" + "list\n" + "todo\n" + "deadline\n" + "event\n");
             }
         }
     }
