390a2dd0aa676d1b027a5374718f24b012c68713
kangqiao322 <kangqiao803@gmail.com>
2022-08-17 18:53:14 +0800

    Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 
 /**
  *  Duke Class
@@ -9,10 +11,31 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static String answer(String msg) {
+    private static ArrayList<String> inputs = new ArrayList<String>();
+
+
+
+    private static String answer(String msg) {
         return "_______________________________________________________" +
                 "\n" + msg + "\n" +
                 "_______________________________________________________";
+    }
+
+    private static String addition(String msg) {
+        return "_______________________________________________________" +
+                "\n" + "added: " + msg + "\n" +
+                "_______________________________________________________";
+    }
+
+    private static String all() {
+        String userInputs = "";
+        for (int i = 0; i < inputs.size(); i++)
+        {
+            int index = i + 1;
+            userInputs += "\n" + index + ". " + inputs.get(i) + "\n";
+
+        }
+        return userInputs;
     }
 
     public static void main(String[] args) {
@@ -23,24 +46,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("Hello from\n" + logo + "What can I do for you?\n");
+        System.out.println("Hello I'm\n" + logo + "What can I do for you?\n");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
-        while(true)
+
+        while(!str.equals("bye"))
         {
-            if(!str.equals("bye"))
+            if (str.equals("list"))
             {
-                 System.out.println(answer(str));
-                 str = sc.nextLine();
+                System.out.println("_______________________________________________________" +
+                        "\n" + all() + "\n" +
+                        "_______________________________________________________");
             }
             else
             {
-                System.out.println("_______________________________________________________" +
-                        "\n" + "Bye. Hope to see you again soon!" + "\n" +
-                        "_______________________________________________________");
-                System.exit(1);
+                inputs.add(str);
+                System.out.println(addition(str));
             }
+            str = sc.nextLine();
         }
+        System.out.println("_______________________________________________________" +
+                "\n" + "Bye. Hope to see you again soon!");
     }
 }
