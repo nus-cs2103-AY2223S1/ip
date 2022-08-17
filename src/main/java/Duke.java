11adb02024f3b25ee64940c7a8c679863b5142c5
nickeltea <e0772462@u.nus.edu>
2022-08-18 02:52:34 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> storage = new ArrayList<>();
+
+    private static void list() {
+        System.out.println("____________________________________________________________");
+
+        int index = 1;
+        Iterator<String> iter = storage.iterator();
+        while (iter.hasNext()) {
+            System.out.print(String.format("%d. ", index));
+            System.out.println(iter.next());
+            index++;
+        }
+
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,13 +30,25 @@ public class Duke {
         Scanner scan = new Scanner(System.in);
         System.out.println("Hello! I'm Duke \n" + "What can I do for you?");
         String command = scan.nextLine();
+        if (!command.equals("list")) {
+            storage.add(command);
+        }
 
         while (!command.equals("bye")) {
-            System.out.println(
-                    "____________________________________________________________ \n"
-                    + command + "\n"
-                    + "____________________________________________________________");
-            command = scan.nextLine();
+            if (command.equals("list")) {
+                list();
+                command = scan.nextLine();
+            } else {
+                System.out.println(
+                        "____________________________________________________________ \n"
+                                + "added: " + command + "\n"
+                                + "____________________________________________________________");
+                command = scan.nextLine();
+            }
+
+            if (!command.equals("list")) {
+                storage.add(command);
+            }
         }
 
         System.out.println(
