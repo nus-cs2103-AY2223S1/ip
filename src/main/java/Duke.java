ce6d90dfc0aeb59eb24a956d1d9ce160aac5115b
TanWenCong <tanwencongg@gmail.com>
2022-08-17 18:20:37 +0800

    Skeletal version of Duke: greets user, echo user commands, exit if user types "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String name = "Duke";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        processUserCommand();
     }
+
+    private static void greet() {
+        System.out.printf("Hello! I'm %s\n", name);
+        System.out.println("What can I do for you?");
+    }
+
+    private static void processUserCommand() {
+        Scanner myScanner = new Scanner(System.in);
+        String userCommand = myScanner.nextLine();
+
+        if (userCommand.equalsIgnoreCase("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+            return;
+        }
+
+        System.out.println(userCommand);
+        processUserCommand();
+    }
+
 }
