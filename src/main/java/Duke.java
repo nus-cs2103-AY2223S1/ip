1ab9685149eb73387ac235a10ae519e850778515
melissaharijanto <melissa.anastasia.h@gmail.com>
2022-08-17 10:54:04 +0800

    Level-1 (Duke was not previously commited)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Ui ui = new Ui();
+
+        ui.greeting();
+
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+        while (!cmd.equals("bye")){
+            ui.echo(cmd);
+            cmd = sc.nextLine();
+        }
+        ui.exit();
     }
 }
