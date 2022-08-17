6e0637d904e4c5360dd358d98ddf9fbb0ee4ebed
mlzt2000 <mlzt2000@gmail.com>
2022-08-17 16:05:55 +0800

    Update Duke.java
    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String TAB = "    ";
+    private static final String LINE = String
+            .format("%s%s", TAB, "____________________________________________________________");
+    private static final String WELCOMEMESSAGE = String
+            .format("Hello! I'm Duke\n%s What can I do for you?", TAB);
+    private static final String ENDMESSAGE = "Bye. Hope to see you again soon!";
+
+    private static final String ENDCOMMAND = "bye";
+
+    private static void prettyPrint(String printable) {
+        String out = String.format("%s\n%s %s\n%s", LINE, TAB, printable, LINE);
+        System.out.println(out);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        prettyPrint(WELCOMEMESSAGE);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals(ENDCOMMAND)) {
+                break;
+            }
+            prettyPrint(input);
+        }
+        prettyPrint(ENDMESSAGE);
     }
 }
