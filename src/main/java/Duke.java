a3d0e7d8bde9c78d8a6841255cb6fab0dbef512f
limweijun <e0726982@u.nus.edu>
2022-08-17 19:15:55 +0800

    Completed Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,55 +16,57 @@ public class Duke {
         while (!input.equals("bye"))
         {
             String[] strs = input.split(" ");
-            if (input.equals("list"))
-            {
-                for (int j = 0; j < i; j ++)
-                {
+            if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int j = 0; j < i; j++) {
                     System.out.println((j + 1) + ". " + tasksToDo[j].toString());
                 }
-            }
-            else if (strs.length == 2 && (strs[0].equals("mark") || strs[0].equals("unmark")))
-            {
+            } else if (strs.length == 2 && (strs[0].equals("mark") || strs[0].equals("unmark"))) {
                 int index = Integer.parseInt(strs[1]) - 1;
                 Task task = tasksToDo[index];
-                if (strs[0].equals("mark"))
-                {
+                if (strs[0].equals("mark")) {
                     task.markTaskAsDone();
-                    System.out.println("Nice! I have mark this task as done:\n" +
-                            task.toString());
-                }
-                else if (strs[0].equals("unmark"))
-                {
+                    System.out.println("Nice! I have mark this task as done:\n" + task.toString());
+                } else if (strs[0].equals("unmark")) {
                     task.unMarkTaskAsDone();
-                    System.out.println("Ok, I have mark this task as not done yet:\n" +
-                            task.toString());
+                    System.out.println("Ok, I have mark this task as not done yet:\n" + task.toString());
                 }
-            }
-            else
-            {
-                if (strs.length > 1 && strs[0].equals("deadline"))
+            } else {
+                try {
+                    String[] details;
+                    switch (strs[0]) {
+                        case "deadline":
+                            details = input.split(" ", 2)[1].split(" /by ");
+                            tasksToDo[i] = new Deadline(details[0], details[1]);
+                            i++;
+                            System.out.println("Got it! I've added this task:\n" + tasksToDo[i - 1].toString()
+                                    + "\nNow you've got " + i + " tasks in the list!");
+                            break;
+                        case "event":
+                            details = input.split(" ", 2)[1].split(" /at ");
+                            tasksToDo[i] = new Event(details[0], details[1]);
+                            i++;
+                            System.out.println("Got it! I've added this task:\n" + tasksToDo[i - 1].toString()
+                                                + "\nNow you've got " + i + " tasks in the list!");
+                            break;
+                        case "todo":
+                            tasksToDo[i] = new Todo(input.split(" ", 2)[1]);
+                            i++;
+                            System.out.println("Got it! I've added this task:\n" + tasksToDo[i - 1].toString()
+                                    + "\nNow you've got " + i + " tasks in the list!");
+                            break;
+                        default:
+                            System.out.println("OOPS!!! I don't understand what that means!");
+                    }
+                }
+                catch(ArrayIndexOutOfBoundsException e)
                 {
-                    String[] details = input.split(" ", 2)[1].split(" /by ");
-                    tasksToDo[i] = new Deadline(details[0], details[1]);
-                    i ++;
+                    System.out.println("OOPS!!! The description of a " + strs[0] + " cannot be empty");
                 }
-                else if (strs.length > 1 && strs[0].equals("event"))
-                {
-                    String[] details = input.split(" ", 2)[1].split(" /at ");
-                    tasksToDo[i] = new Event(details[0], details[1]);
-                    i ++;
-                }
-                else if (strs.length > 1 && strs[0].equals("todo"))
-                {
-                    String detail = input.split(" ", 2)[1];
-                    tasksToDo[i] = new Todo(detail);
-                    i ++;
-                }
-                System.out.println("Got it. I've added this task:\n" + tasksToDo[i - 1].toString()
-                                    + "\nNow you have " + i + " tasks in the list.");
             }
             input = scanner.nextLine();
         }
         System.out.println("Bye! Hope to see you again!");
+
     }
 }
