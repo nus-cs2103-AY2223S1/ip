d0f497be6d1e5a90784686bb3ed6f63452049a80
Eugene Tay <tayyjeugene@gmail.com>
2022-08-17 13:35:25 +0800

    Level -3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,39 @@
+import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Duke {
     Scanner scanner = new Scanner(System.in);
     int index = 0;
-    String[] inputArray = new String[100];
+    Task[] inputArray = new Task[100];
+
     Duke() {
-        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
-        try {
-            while (true) {
+        Greet();
+        while (true) {
+            try {
                 String input = scanner.nextLine();
                 if (input.equals("bye")) {
                     Bye();
                     break;
                 } else if (input.equals("")) {
                     continue;
-                } else if(input.equals("list")) {
+                } else if (input.equals("list")) {
                     List();
+                } else if (input.startsWith("mark")) {
+                    markTask(input);
+                } else if (input.startsWith("unmark")) {
+                    unmarkTask(input);
                 } else {
                     addList(input);
                 }
+            } catch (InputMismatchException | ArrayIndexOutOfBoundsException | NullPointerException e) {
+                System.out.println("Please input correctly");
             }
-        } catch (IllegalStateException | NoSuchElementException e) {
-            System.out.println("Exiting");
         }
+    }
+
+    private void Greet() {
+        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
     }
 
     private void Bye(){
@@ -37,9 +47,23 @@ public class Duke {
     }
 
     private void addList(String input) {
-        inputArray[index] = input;
+        inputArray[index] = new Task(input);
         index++;
         System.out.println("added: " + input);
+    }
+
+    private void markTask(String input) throws NullPointerException, ArrayIndexOutOfBoundsException {
+        int markIndex = Integer.parseInt(input.substring(5));
+        inputArray[markIndex - 1].done();
+        System.out.println("Nice! I've marked this task as done liao!:\n"
+                + inputArray[markIndex - 1]);
+    }
+
+    private void unmarkTask(String input) throws NullPointerException, ArrayIndexOutOfBoundsException{
+        int markIndex = Integer.parseInt(input.substring(7));
+        inputArray[markIndex - 1].unDone();
+        System.out.println("OK, I unmark this task as not done le:\n"
+                + inputArray[markIndex - 1]);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    String name;
+    Boolean done = false;
+    Task(String name) {
+        this.name = name;
+    }
+    public void done() {
+        this.done = true;
+    }
+
+    public void unDone() {
+        this.done = false;
+    }
+    public String toString() {
+        String mark = done ? "X" : "";
+        return "[" + mark + "] " + name;
+    }
+}
