fc17a447e41f2715c4074e077c3910a711c95360
Min Jiun Chia <cminjiun@gmail.com>
2022-08-17 12:56:23 +0800

    implemented Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,38 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
     private static String indent = "       ";
 
+    private static ArrayList<String> tasks = new ArrayList<>();
+    private static Integer taskCounter = 0;
+
     public static void main(String[] args) {
         System.out.println("\n Hello there! \n"
                 + "\n My name is Zelk, nice to meet you :D \n"
                 + " What can I do for you?\n"
                 + " ___________________________________________________________________");
+
         Scanner s = new Scanner(System.in);
         String input = "";
-        while (!(input = s.nextLine()).equals("bye")) {
-            System.out.println(indent + input
-                    + "\n ___________________________________________________________________");
+        while ((input = s.nextLine()) != null) {
+            if (input.equals("list")) {
+                System.out.println(indent + "Here's what's in your list so far!");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(indent + tasks.get(i));
+                }
+                System.out.println(" ___________________________________________________________________");
+            } else if (input.equals("bye")) {
+                break;
+            } else {
+                taskCounter++;
+                tasks.add(taskCounter.toString() + ". " + input);
+                System.out.println(indent + "added: " + input
+                        + "\n ___________________________________________________________________");
+            }
         }
 
         System.out.println(indent + "Bye! Hope to see you again soon! Thank you for chatting with me :)");
-
-
-
     }
 }
