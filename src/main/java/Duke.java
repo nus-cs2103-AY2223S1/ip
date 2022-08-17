d57d7475bab0209238dfeefcfae2c0bc8bafe6b5
t1mzzz <timothykoei@gmail.com>
2022-08-17 12:09:14 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
 
-    ArrayList<String> tasks = new ArrayList<>(100);
+    ArrayList<Task> tasks = new ArrayList<>(100);
 
     public Duke() {
 
@@ -35,6 +35,10 @@ public class Duke {
         while (!cmd.equals("bye")) {
             if (cmd.equals("list")) {
                 list();
+            } else if (cmd.split(" ", 2)[0].equals("mark")) {
+                mark(Integer.parseInt(cmd.split(" ", 2)[1]) - 1);
+            } else if (cmd.split(" ", 2)[0].equals("unmark")) {
+                unmark(Integer.parseInt(cmd.split(" ", 2)[1]) - 1);
             } else {
                 store(cmd);
             }
@@ -43,19 +47,43 @@ public class Duke {
     }
 
     public void store(String cmd) {
-        tasks.add(tasks.size(), cmd);
+        Task task = new Task(cmd);
+        tasks.add(tasks.size(), task);
         System.out.println("    ____________________________________________________________");
-        System.out.println("     added: " + cmd);
+        System.out.println("     added: " + task.getDescription());
         System.out.println("    ____________________________________________________________");
         System.out.println();
     }
 
     public void list() {
         System.out.println("    ____________________________________________________________");
+        System.out.println("Here are the tasks in your list: ");
         for (int i = 0; i < tasks.size(); i++) {
-            System.out.println("     " + (i + 1) + ". " + tasks.get(i));
+            Task task = tasks.get(i);
+            System.out.println("     " + (i + 1) + task);
         }
         System.out.println("    ____________________________________________________________");
+        System.out.println();
+    }
+
+    public void mark(int index) {
+        Task curr = tasks.get(index);
+        curr.markAsDone();
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Nice! I've marked this task as done:");
+        System.out.println("       " + curr);
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+    }
+
+    public void unmark(int index) {
+        Task curr = tasks.get(index);
+        curr.unmarkTask();
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     OK, I've marked this task as not done yet:");
+        System.out.println("       " + curr);
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
     }
 
     public void bye() {
