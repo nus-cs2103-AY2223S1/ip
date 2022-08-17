754a2dc8d13ab1b5999bb5e6299a1f3dc75276fe
Bacon-Strips <86605835+Bacon-Strips@users.noreply.github.com>
2022-08-18 01:13:39 +0800

    Removed debugging expressions and the logo static attribute

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,12 +5,6 @@ public class Duke {
                                         + "What can I do for you?";
     private static final String exitMessage = "Bye. Hope to see you again soon!";
     private static final String exitCmd = "bye";
-    private static final String logo =
-              " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
     public static void main(String[] args) {
         System.out.println(startUpMessage);
         Scanner sc = new Scanner(System.in);
@@ -18,7 +12,6 @@ public class Duke {
         while (!userInput.equals(exitCmd)) {
             System.out.println(userInput);
             userInput = sc.nextLine();
-            System.out.println("Message changed to " + userInput);
         }
         System.out.println(exitMessage);
     }
