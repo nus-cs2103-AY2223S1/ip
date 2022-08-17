ea1bcd0be746c4727ac1cd3daa070d3b0bd6dcfb
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-17 14:05:20 +0800

    Edit add command and add printMessage method

--- src/main/java/commands/AddCommand.java
+++ src/main/java/commands/AddCommand.java
@@ -2,19 +2,24 @@ package commands;
 
 import exception.*;
 import java.util.ArrayList;
-import main.*;
+import tasks.*;
 
 public class AddCommand extends Command {
 
-  private String description;
+  protected String description;
 
   public AddCommand(String description) {
     this.description = description;
   }
 
   @Override
-  public void execute(ArrayList<Task> tasklist) throws DukeException {
-    tasklist.add(new Task(this.description));
-    System.out.println("added: " + this.description);
+  public void execute(ArrayList<Task> tasklist) throws DukeException {}
+
+  public void printMessage(ArrayList<Task> tasklist, Task task) {
+    System.out.println("Got it. I've added this task: ");
+    System.out.println(task.toString());
+    System.out.println(
+      "Now you have " + tasklist.size() + " task(s) in the list"
+    );
   }
 }
