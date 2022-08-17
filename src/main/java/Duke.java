fbe43764dd18909b3e6574edfbbf3cf557f53da2
shauneang <shauneangjx@gmail.com>
2022-08-17 13:51:01 +0800

    Abstracted task methods and added javadocs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,52 +1,79 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+
 
 public class Duke {
-    public static void main(String[] args) {
-        initDuke();
+    static String exitWord = "bye";
+    static String hLine = "\t____________________________________________";
+    static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  /\n"
+            + "|____/ \\,_|_|\\_\\___|\n";
+
+    Scanner myScanner = new Scanner(System.in);
+    ArrayList<String> toDoList;
+    String  command = "";
+
+    // Prints generic greet message
+    private static void greet() {
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
     }
 
-    // Duke Initializer starts the duke program
-    private static void initDuke() {
-        Scanner myScanner = new Scanner(System.in);
-        String exitWord = "bye";
-        String hLine = "\t____________________________________________";
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  /\n"
-                + "|____/ \\,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What can i do for you?");
+    // Prints generic goodbye message
+    private static void goodBye() {
+        System.out.println(hLine);
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println(hLine);
+    }
 
-        String  command = "";
-        ArrayList<String> toDoList = new ArrayList<>();
+    /* Add task to todoList
+     *
+     * @param task
+     */
+    private void addTask(String task) {
+        System.out.println(hLine);
+        toDoList.add(task);
+        System.out.println("\tadded: " + task);
+        System.out.println(hLine);
+    }
+    // Add task to todoList
+    private void listTasks() {
+        System.out.println(hLine);
+        for (int i = 0; i < toDoList.size(); i ++) {
+            System.out.printf("\t%d. %s\n", i + 1, toDoList.get(i));
+        }
+        System.out.println(hLine);
+    }
+
+    // Starts the duke program
+    Duke() {
+        toDoList = new ArrayList<>();
+        greet();
 
         while (!command.equals(exitWord)) {
             if (command.equals("list")) {
-                System.out.println(hLine);
-                for (int i = 0; i < toDoList.size(); i ++) {
-                    System.out.printf("\t%d. %s\n", i + 1, toDoList.get(i));
-                }
-                System.out.println(hLine);
-
+                listTasks();
             }
             else if (command != "") {
-                System.out.println(hLine);
-                toDoList.add(command);
-                System.out.println("\tadded: " + command);
-                System.out.println(hLine);
+                addTask(command);
             }
+
             try {
                 command = myScanner.nextLine();
-            } catch (java.util.InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("\tError: please input a valid command");
                 command = "";
             }
         }
-        System.out.println(hLine);
-        System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println(hLine);
+
+        goodBye();
+    }
+
+    public static void main(String[] args) {
+        new Duke();
     }
 }
