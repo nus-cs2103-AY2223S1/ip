0b8c1dade96d199d0768f882e22bf91b1a206dac
paotheroo <lowzanhao@gmail.com>
2022-08-17 15:02:11 +0800

    Duke: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        boolean loop = true;
+        System.out.println("Hello! Duke here, how can I help you?");
+        Scanner sc = new Scanner(System.in);
+
+        while (loop) {
+            if (sc.hasNext("bye")) {
+                loop = false;
+            } else {
+                String str = sc.nextLine();
+                System.out.println(str);
+            }
+        }
+
+        System.out.println("Thanks for having me!\nHave a nice day ahead!");
     }
 }
