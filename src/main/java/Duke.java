4c8d813df5580d6929df65c08db382814b2a0021
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-08-17 10:50:18 +0800

    finish up level-1 implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,32 +2,29 @@ import java.util.Scanner;
 
 public class Duke {
   public static void inputAction(String input) {
-    System.out.println(input);
+    System.out.println("MumBot: " + input + "\n\n");
   }
 
   public static void goodbye() {
-    System.out.println("Goodbye.");
+    System.out.println("MumBot: Goodbyeeee sweetheart <3");
   }
 
   public static void main(String[] args) {
     // Welcome message
-    String logo = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
-    System.out.println("Hello from\n" + logo);
+    System.out.println("MumBot: Hi dear! You are precious <3\n");
 
     /*
      * Take in user input. As long as input is not bye, calls the function
      * to act on the input. If input is bye, then calls the goodbye function.
      * All inputs are considered as Strings.
      */
+    System.out.print("You: ");
     Scanner sc = new Scanner(System.in);
 
     String input;
     while (!(input = sc.nextLine()).equals("Bye")) {
       inputAction(input);
+      System.out.print("You: ");
     }
 
     goodbye();
