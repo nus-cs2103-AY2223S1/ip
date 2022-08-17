5c40c1d44c8b7b366f1af97113d96130e6f9df8a
ReaganTan00 <reagantau@gmail.com>
2022-08-17 19:07:41 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,20 +2,27 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello from\n" + logo + "\n What can I do for you?");
+        String[] dukeList = new String[100];
+        int pointer = 0;
+        System.out.println("Hello I'm Duke! What can I do for you?");
         while (true) {
             String input = sc.nextLine();
-            if (!input.equals("bye")) {
-                System.out.println("----\n  " + input + "\n----");
-            } else {
-                System.out.println("----\n  Goodbye!\n----");
+            if (input.equals("bye")) {
+                System.out.println("  ----\n  Goodbye!\n  ----");
                 break;
+            } else if (input.equals("list")) {
+                String toDisplay = "  ----\n";
+                for (int i = 0; i < pointer; i++) {
+                    toDisplay += dukeList[i] + "\n";
+                }
+                toDisplay += "  ----";
+                System.out.println(toDisplay);
+            } else {
+                System.out.println("  ----\n  added: " + input + "\n  ----");
+                int temp = pointer + 1;
+                dukeList[pointer] = "  " + temp + ": " + input;
+                pointer++;
             }
         }
     }
