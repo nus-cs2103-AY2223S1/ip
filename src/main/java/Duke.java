5c5ef878f69530493f9e95b2b3cb622c73584fd9
Fuzanwenn <e0902049@u.nus.edu>
2022-08-18 00:59:15 +0800

    Level 1 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
+        String command;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        command = sc.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
