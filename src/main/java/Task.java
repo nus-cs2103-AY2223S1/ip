c2c20a37012400e59db4f5918103d921ae93001b
Rexong <rexonghanwei200035@gmail.com>
2022-08-18 03:45:29 +0800

    copied Task.java from site

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,14 @@
 public class Task {
-    private String description;
+    protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
     }
 
     @Override
