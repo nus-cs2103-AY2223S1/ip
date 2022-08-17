f9a74f84c1118009e95f8caeae86214032750970
JJoeYi <joelowyi@gmail.com>
2022-08-17 13:59:52 +0800

    Level-3, added Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -69,7 +70,60 @@ public class Duke {
         }
 
     }
+
+    public static void listWithStatus() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\t-----------------------------------------------");
+        System.out.println("\tHello! I'm Duke Dukem\n\tWhat can I do for you?");
+        System.out.println("\t-----------------------------------------------");
+
+        ArrayList<Task> lst = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+
+            if (line.equals("bye")) {
+                scanner.close();
+                System.out.println("\t-----------------------------------------------");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t-----------------------------------------------");
+                break;
+            } else if (line.equals("list")) {
+                if (!lst.isEmpty()) {
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println("\tHere are the tasks in your list:");
+
+                    for (int i = 0; i < lst.size(); i++) {
+                        System.out.println("\t"+ (i+1) + ". " + lst.get(i) + "\n");
+                    }
+                    System.out.println("\t-----------------------------------------------");
+                } else { // lst is not initalized
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println("\tList is empty!");
+                    System.out.println("\t-----------------------------------------------");
+                }
+            } else if (line.startsWith("unmark") && line.length() > 6) {
+                int index = Integer.parseInt(line.replaceAll("[^0-9]", "")); // regex gotten from https://stackoverflow.com/a/14974126
+                lst.get(index - 1).markAsUndone();
+              System.out.println("\tNice! I've marked this task as not done yet:");
+              System.out.println("\t" + lst.get(index - 1));
+            }
+            else if (line.startsWith("mark") && line.length() > 4) {
+                int index = Integer.parseInt(line.replaceAll("[^0-9]", ""));
+                lst.get(index - 1).markAsDone();
+              System.out.println("\tNice! I've marked this task as done:");
+              System.out.println("\t" + lst.get(index - 1));
+            }
+            else { // add task into lst
+                System.out.println("\t-----------------------------------------------");
+                System.out.println("\tadded: " + line);
+                System.out.println("\t-----------------------------------------------");
+                lst.add(new Task(line));
+            }
+        }
+
+    }
     public static void main(String[] args) {
-        echoBackList();
+        listWithStatus();
     }
 }
