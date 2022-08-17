ca84b45b29b1c5acd7652d5a7e2024337a670726
doimoiboi <asherlimyi@gmail.com>
2022-08-18 04:12:38 +0800

    Added Level 1 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    private Scanner sc;
+    private static String NAME = "DoiMoiBot: ";
+
+    public Duke() {
+        sc = new Scanner(System.in);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        String input;
+
+        duke.greet();
+        input = duke.getInput();
+        duke.parrot(input);
+
+        duke.farewell();
+    }
+
+    private void greet() {
+        System.out.println(NAME + "Hello! I'm doimoibot\n" + "What can I do you for?");
+    }
+
+    private void farewell() {
+        System.out.println(NAME + "Goodbye! See you soon!");
+    }
+
+    private void parrot(String input) {
+        while (!input.equals("bye")) {
+            System.out.println(NAME + input);
+            input = this.getInput();
+        }
+    }
+
+    private String getInput() {
+        return this.sc.nextLine();
     }
 }
