a7a1ad1262dbba7f9ea8023fd54acf38d3300a19
DevanshShah1309 <devanshshah1309@gmail.com>
2022-08-17 21:41:48 +0800

    Level 3
    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,16 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import static java.lang.Integer.parseInt;
 
 public class Duke {
+    // Special commands
     private static final String BYE = "bye";
     private static final String LIST = "list";
+    private static final String MARK = "mark";
+    private static final String UNMARK = "unmark";
+
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -16,25 +23,33 @@ public class Duke {
         System.out.println("\tHello, I'm Duke!");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t-------------------------------");
-        ArrayList<String> tasks = new ArrayList<>();
+
+        Task.add(null);
+
         while (true) {
             Scanner s = new Scanner(System.in);
             String command = s.nextLine().trim();
             System.out.println("\t-------------------------------");
             if (command.equals(BYE)) {
-
                 System.out.println("\tBye! Hope to see you again");
                 break;
             } else if (command.equals(LIST)) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(String.format("\t%d. %s", i + 1, tasks.get(i)));
-                }
+                Task.listTasks();
+            } else if (Pattern.matches("mark [0-9]+", command)) {
+                String temp = command.replaceAll("[^0-9]", ""); // extracting the digits
+                int taskNumber = parseInt(temp);
+                Task.markAsDone(taskNumber);
+            } else if (Pattern.matches("unmark [0-9]+", command)) {
+                String temp = command.replaceAll("[^0-9]", ""); // extracting the digits
+                int taskNumber = parseInt(temp);
+                Task.markAsNotDone(taskNumber);
             } else {
                 System.out.println("\tadded: " + command);
-                tasks.add(command);
+                Task.add(new Task(command));
             }
             System.out.println("\t-------------------------------");
         }
         System.out.println("\t-------------------------------");
     }
 }
+
---
+++ src/main/java/Task.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+
+    public static void add(Task task) {
+        tasks.add(task);
+    }
+
+    public static void listTasks() {
+        for (int i = 1; i < tasks.size(); i++) {
+            System.out.println(String.format("\t%d. %s", i, tasks.get(i)));
+        }
+    }
+
+    public static int count() {
+        return tasks.size();
+    }
+
+    public static Task get(int taskNumber) {
+        return tasks.get(taskNumber);
+    }
+
+    public static void markAsDone(int taskNumber) {
+        if (taskNumber < 1 || taskNumber >= Task.count()) {
+            System.out.println("\tInvalid Task Number!");
+        } else {
+            Task.get(taskNumber).markAsDone();
+            System.out.println("\tAwesome! I have marked this task as done:");
+            System.out.println("\t\t"+Task.get(taskNumber));
+        }
+    }
+
+    public static void markAsNotDone(int taskNumber) {
+        if (taskNumber < 1 || taskNumber >= Task.count()) {
+            System.out.println("\tInvalid Task Number!");
+        } else {
+            Task.get(taskNumber).markAsNotDone();
+            System.out.println("\tAwesome! I have unmarked this task to be not completed:");
+            System.out.println("\t\t"+Task.get(taskNumber));
+        }
+    }
+}
