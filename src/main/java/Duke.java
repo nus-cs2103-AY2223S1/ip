075d88e0b6b7f5aa51a8829f43d79068c1af8821
shwene <neo.shiwen@gmail.com>
2022-08-17 22:16:35 +0800

    Completed level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        intro();
+        repeatCommand();
+        outro();
+    }
+
+    private static void intro() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void repeatCommand() {
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while (!command.equals("bye")) {
+            System.out.printf("\t%s\n", command);
+            command = sc.nextLine();
+        }
+    }
+
+    private static void outro(){
+        System.out.println("Bye. Hope to see you again");
     }
 }
