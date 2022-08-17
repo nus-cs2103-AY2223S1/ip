1657ee987dc64672fbbe8e3e533fbde269ba3fab
ShenyiCui <shenyicui@outlook.com>
2022-08-18 02:47:28 +0800

    feature: add function for main to add tasks
    - add function to add tasks
    - refactor out the greetings message into it's own method
    - create new method to list tasks
    
    addresses Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,44 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    private static String CMD_LIST = "list";
+    private static ArrayList<Task> allTasks = new ArrayList<>();
+
+    private static void greet() {
         String LOGO = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello from\n" + LOGO);
+        System.out.println("What can I do for you?");
+    }
 
+    private static void listTasks() {
+        String output = "";
+        for (int i = 0; i < Duke.allTasks.size(); i++) {
+            output += "\n" + (i + 1) + ". " + Duke.allTasks.get(i).toString();
+        }
+        System.out.println(output);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Duke.greet();
         while (true) {
             System.out.println("------------------------------");
-            System.out.println("What can I do for you?");
             System.out.print(": ");
             String userInput = scanner.nextLine();
-            System.out.println("------------------------------");
-            System.out.println(userInput + "\n");
+            // Guard Clause
+            if (userInput.equals(Duke.CMD_LIST)) {
+                Duke.listTasks();
+                continue;
+            }
+            Task newTask = new Task(userInput);
+            Duke.allTasks.add(newTask);
+            System.out.println("------------------------------\n");
         }
     }
 }
