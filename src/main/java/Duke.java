cedc6af390884eb26d6987b4e0c2016080082825
LAPTOP-S3RN5C85\eesua <eesuan.234@gmail.com>
2022-08-17 14:13:01 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
+    String input;
+    static final String LINE_BREAK = "-----------------------------";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        System.out.println("Hello! I'm Ee Suan!\nWhat can I do for you?");
+        Scanner scan = new Scanner(System.in);
+        duke.input = scan.nextLine();
+        while (!duke.input.equals("bye")) {
+            System.out.println(LINE_BREAK + "\n\t" + duke.input + "\n" + LINE_BREAK);
+            duke.input = scan.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
