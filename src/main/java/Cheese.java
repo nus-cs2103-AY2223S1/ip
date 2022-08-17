32f5d28fb896c9cbb822b10bde4b5079051c3ab5
Jason Qiu <jason_qiu@hotmail.com>
2022-08-17 15:53:05 +0800

    refactor: refactor chat function into separate method

--- src/main/java/Cheese.java
+++ src/main/java/Cheese.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
 public class Cheese {
-  public static void main(String[] args) {
-    final String BORDER = "-----";
+  public static final String BORDER = "-----";
 
+  public static void main(String[] args) {
     String greeting = new String("Woof! I'm Cheese, your puppy assistant.\n"
-            + "What can I do for you?\n"
-            + BORDER);
+            + "What can I do for you?");
     System.out.println(greeting);
 
+    Cheese.chat();
+  }
+
+  public static void chat() {
     Scanner scanner = new Scanner(System.in);
     String userInput;
 
     while(true) {
+      System.out.println(BORDER);
       userInput = scanner.nextLine();
       System.out.println(BORDER);
+
       if (userInput.equals("bye")) {
         System.out.println("So soon? :') Bye bye");
         break;
       }
-      System.out.println(userInput + "\n" + BORDER);
+      System.out.println(userInput);
     }
   }
 }
