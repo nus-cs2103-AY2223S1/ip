2691bb39b9cd61d1b11a0947b46ca8d454bbc2a2
PangKuangWei <pangkuangwei@gmail.com>
2022-08-17 13:52:00 +0800

    Implmented Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,44 @@
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         int count = 0;
         String line = "    ____________________________________________________________";
         String greeting = line + "\n     Hello! I'm Duke\n     What can I do for you?\n" + line;
         System.out.println(greeting);
         String str = sc.nextLine();
         while(!str.equals("bye")) {
+            String[] splitStr = str.split("\\s+");
             if (str.equals("list")) {
                 int k = 0;
                 int i = 1;
                 System.out.println(line);
+                System.out.println("     Here are the tasks in your list");
                 while (arr[k] != null) {
-                    System.out.println("     " + i + ". " + arr[k]);
+                    System.out.println("     " + i + "." + arr[k]);
                     k++;
                     i++;
                 }
                 System.out.println(line);
+            } else if (splitStr[0].equals("mark")) {
+                int num = Integer.parseInt(splitStr[1]);
+                arr[num - 1].markAsDone();
+                System.out.println(line);
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       " + arr[num - 1]);
+                System.out.println(line);
+            } else if (splitStr[0].equals("unmark")) {
+                    int num = Integer.parseInt(splitStr[1]);
+                    arr[num - 1].markAsUnDone();
+                    System.out.println(line);
+                    System.out.println("     Nice! I've marked this task as not done yet:");
+                    System.out.println("       " + arr[num - 1]);
+                    System.out.println(line);
             } else {
-                arr[count] = str;
+                arr[count] = new Task(str);
                 count++;
                 String message = "     added: " + str;
                 System.out.println(line);
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUnDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
