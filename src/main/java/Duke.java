b986e364f4e1ae4f67209ddfb11a8f4df772747d
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2022-08-18 02:41:48 +0800

    feat: add/change status for tasks
    - Add status to Task class
    - Allow user to mark tasks as done / not done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
+    public static void displayList() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i).toString());
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<>();
+        boolean hasNextInput = true;
 
         String logo = "_________                     ___\n"
                 + "\\    ___ \\  ___________   ____\\_ |_________  ____\n"
@@ -14,19 +23,26 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
 
-        while (true) {
+        while (hasNextInput) {
             System.out.print("--> ");
             String input = scanner.nextLine();
 
-            if (input.equals("bye")) {
-                break;
-            } else if (input.equals("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i).toString());
-                }
-            } else {
-                tasks.add(new Task(input));
-                System.out.println("Added: " + input);
+            switch (input.split(" ")[0]) {
+                case "bye":
+                    hasNextInput = false;
+                    break;
+                case "list":
+                    displayList();
+                    break;
+                case "mark":
+                    tasks.get(Integer.parseInt(input.split(" ")[1]) - 1).markAsDone();
+                    break;
+                case "unmark":
+                    tasks.get(Integer.parseInt(input.split(" ")[1]) - 1).markAsNotDone();
+                    break;
+                default:
+                    tasks.add(new Task(input));
+                    System.out.println("Added: " + input);
             }
         }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,9 +7,25 @@ public class Task {
         this.isDone = false;
     }
 
+    public String getStatusIcon() {
+        return this.isDone ? "X" : " ";
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(this.toString());
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(this.toString());
+    }
+
     @Override
     public String toString() {
-        return this.description;
+        return "[" + this.getStatusIcon() + "] " + this.description;
     }
 
 }
