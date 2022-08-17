4e3f464f178df86708e1714d21c9271ac208c728
cliftonfelix <cliftonfelix12@yahoo.com>
2022-08-17 16:50:41 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -11,6 +12,7 @@ public class Duke {
         String horizontalLine = new String(new char[50]).replace("\0", "~");
         String greeting = "Hello! I'm Duke\n" +
                 "\tWhat can I duke for you?";
+        ArrayList<String> list = new ArrayList<String>();
 
         System.out.println("Hello from\n" + logo);
 
@@ -22,7 +24,16 @@ public class Duke {
 
         while (!input.toLowerCase().equals("bye")) {
             System.out.println("\t" + horizontalLine);
-            System.out.println("\t" + input);
+
+            if (!input.toLowerCase().equals("list")) {
+                list.add(input);
+                System.out.println("\tadded: " + input);
+            } else {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("\t" + String.valueOf(i + 1) + ". " + list.get(i));
+                }
+            }
+
             System.out.println("\t" + horizontalLine);
 
             input = sc.nextLine();
