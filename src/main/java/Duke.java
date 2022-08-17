0b49ee077707556be077bc4a99b0ff865f454c12
ThomasHoooo <e0725843@u.nus.edu>
2022-08-18 00:15:56 +0800

    Feat: Duke can mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,24 +10,37 @@ public class Duke {
         System.out.println(logo + "Hello! I'm Yale\nWhat can I do for you?");
         Scanner in = new Scanner(System.in);
 
-        String[] toDoList = new String[100];
+        Task[] toDoList = new Task[100];
         int count = 0;
 
         while (true) {
             String input = in.nextLine();
             if (input.equals("bye")) {
-                System.out.println("    Bye. Hope to see you again soon!\n");
+                System.out.println("Bye. Hope to see you again soon!\n");
                 break;
             } else if (input.equals("list")) {
                 if (count == 0) {
                     System.out.println("There is no item in list yet");
                 } else {
                     for (int i = 0; i < count; i++) {
-                        System.out.printf("%d. %s\n", i + 1, toDoList[i]);
+                        System.out.printf("%d.%s\n", i + 1, toDoList[i]);
                     }
                 }
+            } else if (input.contains("unmark")) {
+                String[] splitStr = input.trim().split("\\s+");
+                int markItem = Integer.parseInt(splitStr[splitStr.length - 1]) - 1;
+                toDoList[markItem].unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(toDoList[markItem]);
+            } else if (input.contains("mark")) {
+                String[] splitStr = input.trim().split("\\s+");
+                int markItem = Integer.parseInt(splitStr[splitStr.length - 1]) - 1;
+                toDoList[markItem].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(toDoList[markItem]);
             } else {
-                toDoList[count] = input;
+                Task newTask = new Task(input);
+                toDoList[count] = newTask;
                 count++;
                 String message = "\t" + "_".repeat(20) + "\n"
                         + String.format("\t  added: %s\n", input)
