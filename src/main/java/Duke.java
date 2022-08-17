e4c17f48a815ed63a6190e28bff232ad85b59a50
Tan Jin Waye <e0694446@u.nus.edu>
2022-08-17 15:06:58 +0800

    Duke.java: Refactored code to run iteratively, and create a Duke instance instead of static.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,9 +3,23 @@ import java.util.ArrayList;
 
 public class Duke {
     private static final String DIVIDER = "-------------------------------------\n";
-    private static ArrayList<Task> TASKS = new ArrayList<Task>();
+    private ArrayList<Task> tasks;
+    public boolean end;
 
-    public static void main(String[] args) {
+    public Duke() {
+        this.tasks = new ArrayList<Task>();
+        this.end = false;
+    }
+
+    private void run() {
+        this.greet();
+        while(!this.end) {
+            this.handler();
+        }
+    }
+
+    // greet method contains the greeting message
+    private void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -13,19 +27,13 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        greet();
-        handler();
-    }
-
-    // greet method contains the greeting message
-    private static void greet() {
         String message = "Hello! I'm Duke\n"
                 + "What can I do for you?\n";
         System.out.println(DIVIDER + message + DIVIDER);
     }
 
     // handler method handles user input and outputs accordingly
-    private static void handler() {
+    private void handler() {
         String command;
         Scanner sc = new Scanner(System.in);
         command = sc.next();
@@ -42,7 +50,7 @@ public class Duke {
                 listAdd(command, sc.nextLine());
                 break;
             case "mark":
-                // breaks if no input is entered after mark, or input isn't int
+                // breaks if no input is entered after mark, or input isn't int, or index out of range
                 listToggle(sc.nextInt());
                 break;
             case "bye":
@@ -52,12 +60,13 @@ public class Duke {
                 confuse();
         }
 
-        if (!command.equals("bye")) { handler(); }
+        //if (!command.equals("bye")) { handler(); }
 
     }
 
     // sub methods associated with handler() for each user input case
-    private static void exit() {
+    private void exit() {
+        end = true;
         System.out.println(DIVIDER + "Bye. Hope to see you again soon!\n" + DIVIDER);
     }
 
@@ -65,54 +74,54 @@ public class Duke {
         System.out.println(DIVIDER + "I'm sorry, I don't understand :(\n" + DIVIDER);
     }
 
-    private static void list() {
-        if (TASKS.isEmpty()) {
+    private void list() {
+        if (tasks.isEmpty()) {
             System.out.println(DIVIDER + "List is empty\n" + DIVIDER);
         } else {
             System.out.print(DIVIDER);
-            for (int i = 0; i < TASKS.size(); i++) {
-                System.out.println((i+1) + ". " + TASKS.get(i));
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i+1) + ". " + tasks.get(i));
             }
             System.out.println(DIVIDER);
         }
     }
 
-    private static void listAdd(String type, String item) {
+    private void listAdd(String type, String item) {
         Task currTask;
         String[] args;
         switch(type) {
             case "todo":
                 currTask = new Todo(item);
-                TASKS.add(currTask);
+                tasks.add(currTask);
                 System.out.println(DIVIDER + "OK, I've added this todo:\n"
                         + "  " + currTask + "\n"
-                        + "Number of tasks in list: " + TASKS.size() + "\n"
+                        + "Number of tasks in list: " + tasks.size() + "\n"
                         + DIVIDER);
                 break;
             case "deadline":
                 args = item.split("/by");
                 System.out.println(item);
                 currTask = new Deadline(args[0], args[1]);
-                TASKS.add(currTask);
+                tasks.add(currTask);
                 System.out.println(DIVIDER + "OK, I've added this deadline:\n"
                         + "  " + currTask + "\n"
-                        + "Number of tasks in list: " + TASKS.size() + "\n"
+                        + "Number of tasks in list: " + tasks.size() + "\n"
                         + DIVIDER);
                 break;
             case "event":
                 args = item.split("/at");
                 currTask = new Event(args[0], args[1]);
-                TASKS.add(currTask);
+                tasks.add(currTask);
                 System.out.println(DIVIDER + "OK, I've added this event:\n"
                         + "  " + currTask + "\n"
-                        + "Number of tasks in list: " + TASKS.size() + "\n"
+                        + "Number of tasks in list: " + tasks.size() + "\n"
                         + "\n" + DIVIDER);
                 break;
         }
     }
 
-    private static void listToggle(int index) {
-        Task currTask = TASKS.get(index-1);
+    private void listToggle(int index) {
+        Task currTask = tasks.get(index-1);
         if(currTask.completeToggle()) {
             System.out.println(DIVIDER + "Nice! I've marked this task as done:\n"
                     + currTask + "\n" + DIVIDER);
@@ -120,5 +129,10 @@ public class Duke {
             System.out.println(DIVIDER + "OK, I've marked this task as not done yet:\n"
                     + currTask + "\n" + DIVIDER);
         }
+    }
+
+    public static void main(String[] args) {
+        Duke instance = new Duke();
+        instance.run();
     }
 }
