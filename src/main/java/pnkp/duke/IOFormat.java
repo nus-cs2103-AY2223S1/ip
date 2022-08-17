16ecf3767939815acac7f8f20c37373a17356c54
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-08-17 12:58:49 +0800

    use box drawing characters for dividing line

--- src/main/java/pnkp/duke/IOFormat.java
+++ src/main/java/pnkp/duke/IOFormat.java
@@ -3,6 +3,6 @@ package pnkp.duke;
 public class IOFormat {
     public static void say(String text) {
         System.out.println("\uD83D\uDCAC " + text);
-        System.out.println("-----");
+        System.out.println("─────");
     }
 }
