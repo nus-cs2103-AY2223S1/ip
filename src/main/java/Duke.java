3ae29d31d0063c82275f180595dd4d8cabf04df5
ConradLew <e0726724@u.nus.edu>
2022-08-17 17:02:13 +0800

    Added features for Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,11 +7,24 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String userResponse = null;
+        ArrayList<String> userInputs = new ArrayList<>();
         while (userResponse == null || !userResponse.equals("bye")) {
             userResponse = sc.nextLine();
-            String dukeEndOutput = "    " + "Bye. Hope to see you again soon!";
-            String dukeContinuationOutput = "    " + userResponse;
-            String dukeOutput = userResponse.equals("bye") ? dukeEndOutput : dukeContinuationOutput;
+            String dukeOutput = "";
+
+            switch(userResponse) {
+                case "bye":
+                    dukeOutput = "    " + "Bye. Hope to see you again soon!";
+                    break;
+                case "list":
+                    for (int i = 0; i < userInputs.size(); i++) {
+                        dukeOutput += "    " + (i + 1) + ". " + userInputs.get(i) + "\n";
+                    }
+                    break;
+                default:
+                    userInputs.add(userResponse);
+                    dukeOutput = "    " + "added: " + userResponse;
+            }
             System.out.println(dukeOutput);
         }
 
