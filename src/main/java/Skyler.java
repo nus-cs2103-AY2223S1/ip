a4c54662a6aa722e671e2af42b5b16da75304af5
jolynloh <lohyuanenjolyn@gmail.com>
2022-08-17 17:39:49 +0800

    Level 2: Add, List

--- src/main/java/Skyler.java
+++ src/main/java/Skyler.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Skyler {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
+
         // declare scanner object and initialise with
         // predefined standard input object
         Scanner sc = new Scanner(System.in);
@@ -10,11 +13,19 @@ public class Skyler {
 
         while(sc.hasNext()) {
             String action = sc.nextLine();
+
             if (action.equals("bye")) {
                 System.out.println("Bye! See you again soon!");
                 break;
+            } else if (action.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    String str = String.format("%d. %s", i + 1, tasks.get(i));
+                    System.out.println(str);
+                }
             } else {
-                System.out.println(action);
+                tasks.add(action);
+                String str = String.format("added: %s", action);
+                System.out.println(str);
             }
         }
 
