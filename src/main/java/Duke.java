5b156f86b8c0d35525b4750d8019be52788d12b3
limweijun <e0726982@u.nus.edu>
2022-08-17 19:34:50 +0800

    Completed Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        Task[] tasksToDo = new Task[100];
+        ArrayList<Task> tasksToDo = new ArrayList<>();
         int i = 0;
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -19,11 +20,11 @@ public class Duke {
             if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int j = 0; j < i; j++) {
-                    System.out.println((j + 1) + ". " + tasksToDo[j].toString());
+                    System.out.println((j + 1) + ". " + tasksToDo.get(j).toString());
                 }
             } else if (strs.length == 2 && (strs[0].equals("mark") || strs[0].equals("unmark"))) {
                 int index = Integer.parseInt(strs[1]) - 1;
-                Task task = tasksToDo[index];
+                Task task = tasksToDo.get(index);
                 if (strs[0].equals("mark")) {
                     task.markTaskAsDone();
                     System.out.println("Nice! I have mark this task as done:\n" + task.toString());
@@ -31,28 +32,35 @@ public class Duke {
                     task.unMarkTaskAsDone();
                     System.out.println("Ok, I have mark this task as not done yet:\n" + task.toString());
                 }
+            } else if (strs.length == 2 && (strs[0].equals("delete"))) {
+                int index = Integer.parseInt(strs[1]) - 1;
+                Task task = tasksToDo.get(index);
+                tasksToDo.remove(index);
+                i--;
+                System.out.println("Noted. I've removed this task:\n" + task.toString() +
+                                    "\nNow you have " + i + " tasks in the list.");
             } else {
                 try {
                     String[] details;
                     switch (strs[0]) {
                         case "deadline":
                             details = input.split(" ", 2)[1].split(" /by ");
-                            tasksToDo[i] = new Deadline(details[0], details[1]);
+                            tasksToDo.add(new Deadline(details[0], details[1]));
                             i++;
-                            System.out.println("Got it! I've added this task:\n" + tasksToDo[i - 1].toString()
+                            System.out.println("Got it! I've added this task:\n" + tasksToDo.get(i - 1).toString()
                                     + "\nNow you've got " + i + " tasks in the list!");
                             break;
                         case "event":
                             details = input.split(" ", 2)[1].split(" /at ");
-                            tasksToDo[i] = new Event(details[0], details[1]);
+                            tasksToDo.add(new Event(details[0], details[1]));
                             i++;
-                            System.out.println("Got it! I've added this task:\n" + tasksToDo[i - 1].toString()
+                            System.out.println("Got it! I've added this task:\n" + tasksToDo.get(i - 1).toString()
                                                 + "\nNow you've got " + i + " tasks in the list!");
                             break;
                         case "todo":
-                            tasksToDo[i] = new Todo(input.split(" ", 2)[1]);
+                            tasksToDo.add(new Todo(input.split(" ", 2)[1]));
                             i++;
-                            System.out.println("Got it! I've added this task:\n" + tasksToDo[i - 1].toString()
+                            System.out.println("Got it! I've added this task:\n" + tasksToDo.get(i - 1).toString()
                                     + "\nNow you've got " + i + " tasks in the list!");
                             break;
                         default:
