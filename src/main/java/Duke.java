e0fab610712686911edbca13672c5d6396f43e69
domoboom <berzindominic@gmail.com>
2022-08-17 20:27:59 +0800

    Touch-up, completed Level 5 properly by adding exception class Handled various error cases Added relevant test cases

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,73 +12,78 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        Duke curr = new Duke();
-        curr.handleInput();
-
-
+        Duke chatBotInstance = new Duke();
+        chatBotInstance.handleInput();
     }
 
-    private void bye() {
-        System.out.println("Bye. Hope to see you again soon!");
-    }
-
-    private void list() {
-        if (this.storage.size() == 0) {
+    public void list() {
+        if (storage.size() == 0) {
             System.out.println("List is currently empty");
         } else {
             System.out.println("Here are your tasks:");
-            for (int i = 0; i < this.storage.size(); i++) {
+            for (int i = 0; i < storage.size(); i++) {
                 System.out.printf("%d.%s\n", i + 1,
-                        this.storage.get(i).toString());
+                        storage.get(i).toString());
             }
         }
     }
 
-    private void unmark(String command) {
+    public void unmark(String input) throws DukeException {
         if (this.storage.size() == 0) {
-            System.out.println("No tasks to unmark");
-        }
-        Character item = command.charAt(0);
-        if (Character.isDigit(item)) {
-            int idx = item - '0';
-            if (idx > this.storage.size() + 1 || idx < 1) {
-                System.out.println("Invalid selection");
-            } else {
-                Task t = this.storage.get(idx - 1);
-                t.markAsNotDone();
-            }
+            throw new DukeException("No tasks to unmark!");
         } else {
-            System.out.println("Invalid selection");
-        }
-    }
-
-    private void mark(String command) {
-        if (this.storage.size() == 0) {
-            System.out.println("No tasks to mark");
-        }
-        Character item = command.charAt(0);
-        if (Character.isDigit(item)) {
-            int idx = item - '0';
-            if (idx > this.storage.size() + 1 || idx < 1) {
-                System.out.println("Invalid selection");
-            } else {
-                Task t = this.storage.get(idx - 1);
-                t.markAsDone();
+            if (input == null) {
+                throw new DukeException("No input provided for unmarking");
             }
-        } else {
-            System.out.println("Invalid selection");
+            String s = input.replaceAll("\\D+", "" );
+            if (!s.equals("")) {
+                int idx = Integer.parseInt(s);
+                if (idx - 1 >= this.storage.size() || idx < 1) {
+                    throw new DukeException("Invalid selection for unmarking");
+                } else {
+                    Task t = this.storage.get(idx - 1);
+                    t.markAsNotDone();
+                }
+            } else {
+                throw new DukeException("Invalid selection for unmarking");
+            }
         }
     }
 
-    private void delete(String command) {
+    public void mark(String input) throws DukeException {
         if (this.storage.size() == 0) {
-            System.out.println("No tasks to delete");
+            throw new DukeException("No tasks to mark!");
+        } else {
+            if (input == null) {
+                throw new DukeException("No input provided for marking");
+            }
+            String s = input.replaceAll("\\D+", "" );
+            if (!s.equals("")) {
+                int idx = Integer.parseInt(s);
+                if (idx - 1 >= this.storage.size() || idx < 1) {
+                    throw new DukeException("Invalid selection for marking");
+                } else {
+                    Task t = this.storage.get(idx - 1);
+                    t.markAsDone();
+                }
+            } else {
+                throw new DukeException("Invalid selection for marking");
+            }
         }
-        Character item = command.charAt(0);
-        if (Character.isDigit(item)) {
-            int idx = item - '0';
-            if (idx > this.storage.size() + 1 || idx < 1) {
-                System.out.println("Invalid selection");
+    }
+
+    public void delete(String input) throws DukeException {
+        if (this.storage.size() == 0) {
+            throw new DukeException("No tasks to delete!");
+        }
+        if (input == null) {
+            throw new DukeException("No input provided for deletion");
+        }
+        String s = input.replaceAll("\\D+", "" );
+        if (!s.equals("")) {
+            int idx = Integer.parseInt(s);
+            if (idx - 1 >= this.storage.size() || idx < 1) {
+                throw new DukeException("Invalid selection for marking");
             } else {
                 Task t = this.storage.get(idx - 1);
                 this.storage.remove(idx - 1);
@@ -86,59 +91,55 @@ public class Duke {
                         t);
             }
         } else {
-            System.out.println("Invalid selection");
+            throw new DukeException("Invalid selection for deletion");
         }
     }
 
-    private void addTask(String[] command) {
-        if (command[0].equals("todo")) {
-            if (command.length <= 1) {
-                System.out.println("Description of a " +
-                        "todo cannot be empty");
+    public void addTask(String[] input) throws DukeException {
+        if (input[0].equals("todo")) {
+            if (input.length <= 1) {
+                throw new DukeException("Description of a todo cannot be empty!");
             }
-            Todo todo = new Todo(command[1].trim());
+            Todo todo = new Todo(input[1].trim());
             storage.add(todo);
             System.out.printf("added %s\n", todo);
-        } else if (command[0].equals("deadline")) {
-            if (command.length <= 1) {
-                System.out.println("Description of a " +
-                        "deadline cannot be empty");
+        } else if (input[0].equals("deadline")) {
+            if (input.length <= 1) {
+                throw new DukeException("Description of a deadline " +
+                        "cannot be empty!");
             }
-            if (!command[1].contains("/by") ||
-                    command[1].indexOf("/by") == command[1].length() - 3) {
-                System.out.println("Please insert " +
-                        "date for deadline");
+            if (!input[1].contains("/by") ||
+                    input[1].indexOf("/by") == input[1].length() - 3) {
+                throw new DukeException("No date inserted for deadline");
             }
-            String[] deadlineInfo = command[1].split("/by", 2);
+            String[] deadlineInfo = input[1].split("/by", 2);
             Deadline deadline = new Deadline(deadlineInfo[0].trim(),
                     deadlineInfo[1].trim());
             storage.add(deadline);
             System.out.printf("added %s\n", deadline);
-        } else if (command[0].equals("event")) {
-            if (command.length <= 1) {
-                System.out.println("Description of a event " +
-                        "cannot be empty");
+        } else if (input[0].equals("event")) {
+            if (input.length <= 1) {
+                throw new DukeException("Description of an event " +
+                        "cannot be empty!");
             }
-            if (!command[1].contains("/at") ||
-                    command[1].indexOf("/at") == command[1].length() - 3) {
-                System.out.println("Please insert " +
-                        "date for event");
+            if (!input[1].contains("/at") ||
+                    input[1].indexOf("/at") == input[1].length() - 3) {
+                throw new DukeException("No date inserted for event");
+
             }
-            String[] eventInfo = command[1].split("/at", 2);
+            String[] eventInfo = input[1].split("/at", 2);
             Event event = new Event(eventInfo[0].trim(), eventInfo[1].trim());
             storage.add(event);
             System.out.printf("added %s\n", event);
         } else {
-            System.out.println("I'm sorry, but I don't know " +
-                    "what that means");
-            return;
+            throw new DukeException("Invalid task");
         }
         System.out.printf("Now you have %d tasks in the list\n",
                 this.storage.size());
     }
 
 
-    private void handleInput() {
+    public void handleInput() {
         Scanner in = new Scanner(System.in);
 
         while (in.hasNext()) {
@@ -148,18 +149,29 @@ public class Duke {
                 next[i] = temp[i].trim();
             }
 
-            if (next[0].equals("bye")) {
-                bye();
-            } else if (next[0].equals("list")) {
-                list();
-            } else if (next[0].equals("unmark")){
-                unmark(next[1]);
-            } else if (next[0].equals("mark")){
-                mark(next[1]);
-            } else if (next[0].equals("delete")) {
-                delete(next[1]);
-            } else {
-                addTask(next);
+            String command = next[0];
+
+            try {
+
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    return;
+                } else if (command.equals("list")) {
+                    this.list();
+                } else if (command.equals("unmark")) {
+                    this.unmark(next[1]);
+                } else if (command.equals("mark")) {
+                    this.mark(next[1]);
+                } else if (command.equals("todo") || command.equals("deadline")
+                        || command.equals("event")) {
+                    this.addTask(temp);
+                } else if (command.equals("delete")) {
+                    this.delete(next[1]);
+                } else {
+                    throw new DukeException("Invalid command");
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
