d368eced23c0582a366e5a0763fe86c960838c01
kangqiao322 <kangqiao803@gmail.com>
2022-08-17 23:04:02 +0800

    Level-3, Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 
 public class Duke {
 
-    private static ArrayList<String> inputs = new ArrayList<String>();
+    private static ArrayList<Task> inputs = new ArrayList<Task>();
 
 
 
@@ -26,16 +26,32 @@ public class Duke {
                 "\n" + "added: " + msg + "\n" +
                 "_______________________________________________________";
     }
-
+//inputs contain all the tasks
     private static String all() {
         String userInputs = "";
         for (int i = 0; i < inputs.size(); i++)
         {
             int index = i + 1;
-            userInputs += "\n" + index + ". " + inputs.get(i) + "\n";
+            Task tempTask = inputs.get(i);
+            userInputs += "\n" + index + "." + "[" + tempTask.getStatusIcon() +"] "
+                    + tempTask.getContent() + "\n";
 
         }
         return userInputs;
+    }
+
+    private static String markDone(String str) {
+        return "_______________________________________________________" + "\n" +
+                "Nice! I've marked this task as done:" +
+                "\n" + "[X] " + str + "\n" +
+                "\n" + "_______________________________________________________";
+    }
+
+    private static String markUndone(String str) {
+        return "_______________________________________________________" + "\n" +
+                "Wow! I've marked this task as not done yet:" +
+                "\n" + "[ ] " + str + "\n" +
+                "\n" + "_______________________________________________________";
     }
 
     public static void main(String[] args) {
@@ -51,18 +67,41 @@ public class Duke {
         String str = sc.nextLine();
 
 
+
+
+
         while(!str.equals("bye"))
         {
+            //for listing
             if (str.equals("list"))
             {
+                System.out.println("Here are all the tasks in your list\n");
                 System.out.println("_______________________________________________________" +
                         "\n" + all() + "\n" +
                         "_______________________________________________________");
             }
             else
             {
-                inputs.add(str);
-                System.out.println(addition(str));
+                String mark = str.split(" ")[0];
+                if (mark.equals("mark"))
+                {
+                    //this is as index is greater than array index by 1
+                    int index = Integer.valueOf(str.split(" ")[1]) - 1;
+                    inputs.get(index).setDone();
+                    System.out.println(markDone(inputs.get(index).getContent()));
+                }
+                else if ((mark.equals("unmark")))
+                {
+                    int index = Integer.valueOf(str.split(" ")[1]) - 1;
+                    inputs.get(index).setUndone();
+                    System.out.println(markUndone(inputs.get(index).getContent()));
+                }
+                else
+                {
+                    Task task = new Task(str);
+                    inputs.add(task);
+                    System.out.println(addition(str));
+                }
             }
             str = sc.nextLine();
         }
