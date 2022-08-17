10abeb5c2dcdf4d8e5ba62bf91a78dbc12f5802d
ama-chi <89921925+ama-chi@users.noreply.github.com>
2022-08-17 15:36:52 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,21 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+
+/**
+ * Duke is a basic chatbot that echoes whatever the user inputs.
+ *
+ * @author Chi Song Yi Amadeus
+ * @version Level-1
+ * @since 17-08-2022
+ */
 public class Duke {
+
+    /**
+    * Main method initializes welcome message, and then calls echo method.
+    *
+    * @param args unused.
+    */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +23,36 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        echo();
     }
+
+     /**
+     * This method handles the echo loop. It echoes whatever the user inputs, but ends once
+     * the user enters "bye".
+     *
+     */
+     public static void echo() {
+        String initMessage =
+                "    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________";
+
+         Scanner userInput = new Scanner(System.in);
+         System.out.println(initMessage);
+
+         String input = userInput.nextLine();
+         while (!Objects.equals(input.toLowerCase(), "bye")) {
+             System.out.printf(
+                     "    ____________________________________________________________\n" +
+                     "     %s\n" +
+                     "    ____________________________________________________________%n", input);
+             input = userInput.nextLine();
+         }
+
+         System.out.println(
+                 "    ____________________________________________________________\n" +
+                 "     Bye. Hope to see you again soon!\n" +
+                 "    ____________________________________________________________\n");
+     }
 }
