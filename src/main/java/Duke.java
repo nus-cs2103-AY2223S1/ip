f603b5d383602d2bf9f4f6d5bf5b9fb43b4aa703
Min Jiun Chia <cminjiun@gmail.com>
2022-08-17 14:26:06 +0800

    Implemented Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,8 +5,7 @@ public class Duke {
 
     private static String indent = "       ";
 
-    private static ArrayList<String> tasks = new ArrayList<>();
-    private static Integer taskCounter = 0;
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("\n Hello there! \n"
@@ -16,18 +15,30 @@ public class Duke {
 
         Scanner s = new Scanner(System.in);
         String input = "";
+
         while ((input = s.nextLine()) != null) {
-            if (input.equals("list")) {
-                System.out.println(indent + "Here's what's in your list so far!");
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                System.out.println(indent + "These are the tasks in your list so far!");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(indent + tasks.get(i));
+                    System.out.println(indent + (i+1) + ". " + tasks.get(i));
                 }
                 System.out.println(" ___________________________________________________________________");
-            } else if (input.equals("bye")) {
-                break;
+            } else if (input.contains("unmark")) {
+                Integer taskNo = Integer.valueOf(input.substring(7));
+                tasks.get(taskNo - 1).markAsUndone();
+                System.out.println(indent + "Okay, I'll mark this task as undone: \n"
+                        + indent + " " + tasks.get(taskNo - 1));
+                System.out.println(" ___________________________________________________________________");
+            } else if (input.contains("mark")) {
+                Integer taskNo = Integer.valueOf(input.substring(5));
+                tasks.get(taskNo - 1).markAsDone();
+                System.out.println(indent + "Alright! I've marked this task as done :) \n"
+                        + indent + " " + tasks.get(taskNo - 1));
+                System.out.println(" ___________________________________________________________________");
             } else {
-                taskCounter++;
-                tasks.add(taskCounter.toString() + ". " + input);
+                tasks.add(new Task(input));
                 System.out.println(indent + "added: " + input
                         + "\n ___________________________________________________________________");
             }
