0c36daee1cc6e7a96cb9482078be9969b923936a
Guo Wei <wguo8698@gmail.com>
2022-08-18 00:03:27 +0800

    bug fix for file access issue

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -4,7 +4,7 @@ REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
-if exist ACTUAL2.TXT del ACTUAL2.TXT
+if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
@@ -15,7 +15,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Deku < input.txt > ACTUAL2.TXT
+java -classpath ..\bin Deku < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
-FC ACTUAL2.TXT EXPECTED.TXT
+FC ACTUAL.TXT EXPECTED.TXT
