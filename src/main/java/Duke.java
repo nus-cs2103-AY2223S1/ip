1a49f4888ca8bdea46abf9058425197f2b21327e
chun <lamchunyu00@gmail.com>
2022-08-17 22:46:06 +0800

    Added error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,12 +4,6 @@ import java.util.function.UnaryOperator;
 
 public class Duke {
 
-    // TODO: 17/8/2022
-    //  Refactor printing of TodoList
-    //  make the individual elements print their own shit
-    //  inherit from Task
-
-
     private static String divider() {
         return "--------------------";
     }
@@ -27,6 +21,18 @@ public class Duke {
 
     private static String checkbox(Boolean mark) {
         return mark ? "[x]" : "[ ]";
+    }
+
+    static class EmptyInputException extends RuntimeException {
+        public EmptyInputException() {
+            super("☹ OOPS!!! The description of a todo cannot be empty.");
+        }
+    }
+
+    static class InvalidInputException extends RuntimeException {
+        public InvalidInputException() {
+            super("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
     }
 
     public static void main(String[] args) {
@@ -146,66 +152,77 @@ public class Duke {
         setup();
 
         while (true) {
-            String line = scanner.nextLine();
-            System.out.println(divider());
+            try {
+                String line = scanner.nextLine();
+                System.out.println(divider());
 
-            switch (line) {
+                switch (line) {
+                    case "":
+                        throw new EmptyInputException();
 
-                case "bye":
-                    System.out.println("See you again!");
-                    return;
+                    case "bye":
+                        System.out.println("See you again!");
+                        return;
 
-                case "list":
-                    todolist.printData();
-                    break;
+                    case "list":
+                        todolist.printData();
+                        break;
 
-                default:
+                    default:
 //                  We can't use switch statements since we want regex matching
-                    if (line.matches("mark \\d+")) {
-                        int index = Integer.parseInt(line.split(" ")[1]);
-                        if (index > 100) {
+                        if (line.matches("mark \\d+")) {
+                            int index = Integer.parseInt(line.split(" ")[1]);
+                            if (index > 100) {
+                                break;
+                            }
+
+                            todolist.mark(index - 1);
+                            todolist.printData();
                             break;
                         }
 
-                        todolist.mark(index - 1);
-                        todolist.printData();
-                        break;
-                    }
+                        if (line.matches("unmark \\d+")) {
+                            int index = Integer.parseInt(line.split(" ")[1]);
+                            if (index > 100) {
+                                break;
+                            }
 
-                    if (line.matches("unmark \\d+")) {
-                        int index = Integer.parseInt(line.split(" ")[1]);
-                        if (index > 100) {
+                            todolist.unmark(index - 1);
+                            todolist.printData();
                             break;
                         }
 
-                        todolist.unmark(index - 1);
-                        todolist.printData();
-                        break;
-                    }
+                        if (line.matches("deadline .* by .*")) {
+                            String[] res = line.substring(9).split(" by ");
+                            String desc = res[0];
+                            String deadline = res[1];
 
-                    if (line.matches(".* by .*")) {
-                        String[] res = line.split(" by ");
-                        String desc = res[0];
-                        String deadline = res[1];
+                            todolist.addDeadline(desc,deadline);
+                            System.out.println(String.format("Added: %s (by: %s)", desc, deadline));
+                            break;
+                        }
 
-                        todolist.addDeadline(desc,deadline);
-                        System.out.println(String.format("Added: %s (by: %s)", desc, deadline));
-                        break;
-                    }
+                        if (line.matches("event .* at .*")) {
+                            String[] res = line.substring(6).split(" at ");
+                            String desc = res[0];
+                            String time = res[1];
 
-                    if (line.matches(".* at .*")) {
-                        String[] res = line.split(" at ");
-                        String desc = res[0];
-                        String time = res[1];
+                            todolist.addEvent(desc, time);
+                            System.out.println(String.format("Added: %s (at: %s)", desc, time));
+                            break;
+                        }
 
-                        todolist.addEvent(desc, time);
-                        System.out.println(String.format("Added: %s (at: %s)", desc, time));
-                        break;
-                    }
+                        if (line.matches("todo .*")) {
+                            line = line.substring(5);
+                            todolist.addTask(line);
+                            System.out.println("Added: " + line);
+                            break;
+                        }
 
-                    todolist.addTask(line);
-                    System.out.println("Added: " + line);
-                    break;
+                        throw new InvalidInputException();
+                }
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
             }
         }
 
