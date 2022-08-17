ad50b16a8ac19182efea92093dab442d85d679ea
Puakii <theyitengaxis@gmail.com>
2022-08-18 05:18:26 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -11,8 +11,14 @@ public abstract class Command {
                 return new MarkCommand(splitMessage[1], tasks);
             case "unmark":
                 return new UnmarkCommand(splitMessage[1], tasks);
+            case "todo":
+                return new ToDoCommand(splitMessage[1], tasks);
+            case "deadline":
+                return new DeadlineCommand(splitMessage[1], tasks);
+            case "event":
+                return new EventCommand(splitMessage[1], tasks);
             default:
-                return new AddCommand(message, tasks);
+                return new UnknownCommand();
         }
     }
     public abstract boolean run();
@@ -20,7 +26,7 @@ public abstract class Command {
 
     private static class EndCommand extends Command {
 
-        public static final String END = "Bye! Hope you had fun!\n";
+        public static final String END = "Bye! Hope you had fun!";
 
         public EndCommand() {
         }
@@ -83,19 +89,66 @@ public abstract class Command {
         }
     }
 
-    private static class AddCommand extends Command {
+    private static class ToDoCommand extends Command {
         protected String content;
         protected TaskList tasks;
 
-        public AddCommand(String content, TaskList tasks) {
+        public ToDoCommand(String content, TaskList tasks) {
             this.content = content;
             this.tasks = tasks;
         }
 
         @Override
         public boolean run() {
-            this.tasks.addTask(this.content);
-            Reply.printMessage("added: " + this.content);
+            Reply.printMessage(this.tasks.addTask(new ToDo(this.content)));
+            return false;
+        }
+    }
+
+    private static class DeadlineCommand extends Command {
+        protected String content;
+        protected TaskList tasks;
+
+        public DeadlineCommand(String content, TaskList tasks) {
+            this.content = content;
+            this.tasks = tasks;
+        }
+
+        @Override
+        public boolean run() {
+            String[] splitMessage = this.content.split(" /by ", 2);
+            Reply.printMessage(this.tasks.addTask(new Deadline(splitMessage[0], splitMessage[1])));
+            return false;
+        }
+    }
+
+    private static class EventCommand extends Command {
+        protected String content;
+        protected TaskList tasks;
+
+        public EventCommand(String content, TaskList tasks) {
+            this.content = content;
+            this.tasks = tasks;
+        }
+
+        @Override
+        public boolean run() {
+            String[] splitMessage = this.content.split(" /at ", 2);
+            Reply.printMessage(this.tasks.addTask(new Event(splitMessage[0], splitMessage[1])));
+            return false;
+        }
+    }
+
+    private static class UnknownCommand extends Command {
+
+        public static final String UNKNOWN = "Sorry, this is an unknown command!";
+
+        public UnknownCommand() {
+        }
+
+        @Override
+        public boolean run() {
+            Reply.printMessage(UNKNOWN);
             return false;
         }
     }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -5,6 +5,9 @@ public class TaskList {
     protected ArrayList<Task> tasks;
     public static final String DONE = "Nice! I've marked this task as done:\n";
     public static final String UNDONE =  "OK, I've marked this task as not done yet:\n";
+    public static final String ADD = "Got it. I've added this task:\n";
+    public static final String NUMBER_START = "Now you have ";
+    public static final String NUMBER_END = " tasks in the list.";
 
 
     public TaskList() {
@@ -23,8 +26,9 @@ public class TaskList {
         return allTasks.toString();
     }
 
-    public void addTask(String task) {
-        this.tasks.add(new Task(task));
+    public String addTask(Task task) {
+        this.tasks.add(task);
+        return ADD + task.toString() + "\n" + NUMBER_START + this.tasks.size() + NUMBER_END;
     }
 
     public String markTask(int index) {
