57c5f5f947af567ef0bd9337e58d7230a8025cd1
ChongCheeKaiClarence <e0725313.u.nus.edu>
2022-08-17 22:01:48 +0800

    Added level 5 work

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,27 +5,27 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Duke {
-
+    private static boolean done;
     private static final Scanner echo = new Scanner(System.in);
     private static Pattern checkString = Pattern.compile("-?\\d+");
 
     private static ArrayList<Task> storage = new ArrayList<Task>();
 
-    public static boolean isInteger(String strNum) {
+    private static boolean isInteger(String strNum) {
         if (strNum == null) {
             return false;
         }
         return checkString.matcher(strNum).matches();
     }
 
-    public static void addTask(Task task) {
+    private static void addTask(Task task) {
         storage.add(task);
         System.out.println("-----------------------------------------------");
         System.out.println("added: " + task.getDescription());
         System.out.println("-----------------------------------------------");
     }
 
-    public static void markString(Task task) {
+    private static void markString(Task task) {
         task.mark();
         System.out.println("-----------------------------------------------");
         System.out.println("Nice! I've marked this task as done:");
@@ -33,7 +33,7 @@ public class Duke {
         System.out.println("-----------------------------------------------");
     }
 
-    public static void unMarkString(Task task) {
+    private static void unMarkString(Task task) {
         task.unMark();
         System.out.println("-----------------------------------------------");
         System.out.println("OK, I've marked this task as not done yet:");
@@ -41,8 +41,15 @@ public class Duke {
         System.out.println("-----------------------------------------------");
     }
 
-    public static void reply() {
-        String response = echo.nextLine();
+    private static void addDetailedTask(Task task) {
+        System.out.println("-----------------------------------------------");
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        System.out.println("Now you have " + storage.size() +" tasks in the list.");
+        System.out.println("-----------------------------------------------");
+    }
+
+    private static void reply(String response) throws DukeException {
         String[] parts = response.split(" ", 2);
         String part1 = parts[0];
         String part2;
@@ -53,84 +60,101 @@ public class Duke {
                 System.out.println("-----------------------------------------------");
                 storage.clear();
                 echo.close();
-                return;
+                done = true;
+                break;
             case "list":
+                if (storage.size() == 0) {
+                    throw new DukeException("there's nothing!");
+                }
                 System.out.println("-----------------------------------------------");
                 for (int i = 0; i < storage.size(); i++) {
                     System.out.println((i + 1) + "." + storage.get(i).toString());
                 }
                 System.out.println("-----------------------------------------------");
-                reply();
                 break;
             case "mark":
+                if(parts.length <= 1) {
+                    throw new DukeException("Please tell me what to mark!");
+                }
                 part2 = parts[1];
                 if (isInteger(part2)) {
                     int number = Integer.parseInt(part2);
+                    if(storage.size() < number) {
+                        throw new DukeException("There's no such task to mark!");
+                    }
                     markString(storage.get(number - 1));
-                    reply();
                 } else {
-                    Task task = new Task(response);
-                    addTask(task);
-                    reply();
+                    throw new DukeException("I don't know which to mark!");
                 }
                 break;
             case "unmark":
+                if(parts.length <= 1) {
+                    throw new DukeException("Please tell me what to unmark!");
+                }
                 part2 = parts[1];
                 if (isInteger(part2)) {
                     int number = Integer.parseInt(part2);
+                    if(storage.size() < number) {
+                        throw new DukeException("There's no such task to unmark!");
+                    }
                     unMarkString(storage.get(number - 1));
-                    reply();
                 } else {
-                    Task task = new Task(response);
-                    addTask(task);
-                    reply();
+                    throw new DukeException("I don't know which to unmark!");
                 }
                 break;
             case "deadline":
+                if(parts.length <= 1) {
+                    throw new DukeException("There's no deadline task!");
+                }
                 part2 = parts[1];
                 String[] deadlineParts =  part2.split("/by ", 2);
+                if (deadlineParts.length <= 1) {
+                    throw new DukeException("You didn't specify the deadline! Please use /by.");
+                }
                 Deadline deadline = new Deadline(deadlineParts[0], deadlineParts[1]);
                 storage.add(deadline);
-                System.out.println("-----------------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println(deadline);
-                System.out.println("Now you have " + storage.size() +" tasks in the list.");
-                System.out.println("-----------------------------------------------");
-                reply();
+                addDetailedTask(deadline);
                 break;
             case "todo":
+                if(parts.length <= 1) {
+                    throw new DukeException("There's no todo task!");
+                }
                 part2 = parts[1];
                 Todo todo = new Todo(part2);
                 storage.add(todo);
-                System.out.println("-----------------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println(todo);
-                System.out.println("Now you have " + storage.size() +" tasks in the list.");
-                System.out.println("-----------------------------------------------");
-                reply();
+                addDetailedTask(todo);
                 break;
             case "event":
+                if(parts.length <= 1) {
+                    throw new DukeException("There's no event task!");
+                }
                 part2 = parts[1];
                 String[] eventParts = part2.split("/at ", 2);
+                if (eventParts.length <= 1) {
+                    throw new DukeException("You didn't specify the event time! Please use /at.");
+                }
                 Event event = new Event(eventParts[0], eventParts[1]);
                 storage.add(event);
-                System.out.println("-----------------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println(event);
-                System.out.println("Now you have " + storage.size() +" tasks in the list.");
-                System.out.println("-----------------------------------------------");
-                reply();
+                addDetailedTask(event);
                 break;
             default:
-                Task task = new Task(response);
-                addTask(task);
-                reply();
+                throw new DukeException("I'm sorry, but I don't know what that means.");
         }
     }
 
-
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n" );
-        reply();
+        String response;
+        done = false;
+        while (!done) {
+            try {
+                response = echo.nextLine();
+                reply(response);
+            } catch (DukeException e) {
+                System.out.println("-----------------------------------------------");
+                System.out.println(e);
+                System.out.println("-----------------------------------------------");
+            }
+        }
     }
 }
