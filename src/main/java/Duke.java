363b903fdcda42c7d8b7b842b72ea6fe74735a6b
Rui Han <ruixhan@gmail.com>
2022-08-18 15:31:33 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class Duke {
-    private static String formatList(ArrayList lst) {
+    private static ArrayList<Task> todo = new ArrayList<>();
+
+    private static String formatList(ArrayList<Task> lst) {
 
         String result = "";
         int length = lst.size();
         for (int i = 0; i < length; i++) {
-            result += (i + 1) + ". " + lst.get(i) + "\n";
+            Task curr = lst.get(i);
+            result +=String.format("%d. %s \n",i + 1, curr.formatTask());
         }
         return result;
     }
-
+    private static void toggleTaskStatus(int index) {
+       Task task =  todo.get(index);
+       task.toggleStatus();
+    }
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -22,11 +29,13 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         Scanner scanner = new Scanner((System.in));
         System.out.println("Hello! I'm Duke \n What can i do for you? \n");
-        ArrayList todo = new ArrayList<String>();
+
 
 
         loop: while (true) {
-            String command = scanner.nextLine();
+            String input = scanner.nextLine();
+            String arr[] = input.split(" ");
+            String command = arr[0];
             switch (command) {
                 case "Bye":
                     System.out.println("Bye. Hope to see you again soon");
@@ -34,8 +43,20 @@ public class Duke {
                 case "list":
                     System.out.println(formatList(todo));
                     break;
+                case "mark":
+                    int indexToMark = Integer.valueOf(arr[1]) - 1;
+
+                    toggleTaskStatus(indexToMark);
+                    System.out.println("Nice! I'hv marked this task as done: \n" + todo.get(indexToMark).formatTask());
+                    break;
+                case "unmark":
+                    int indexToUnmark = Integer.valueOf(arr[1]) - 1;
+                    toggleTaskStatus(indexToUnmark);
+                    System.out.println("Sadge u are not done :( \n" + todo.get(indexToUnmark).formatTask());
+
+                    break;
                 default:
-                    todo.add(command);
+                    todo.add(new Task(command));
                     System.out.println("added: " + command);
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,26 @@
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void toggleStatus() {
+        if (this.isDone) {
+            this.isDone = false;
+        } else {
+            this.isDone = true;
+        }
+    }
+
+    public String formatTask() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+
 }
