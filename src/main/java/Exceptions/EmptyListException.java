b581ba700428b8a369001f2e9c6a04ef2d66475d
linuschancs <mrlinuschan@gmail.com>
2022-08-18 11:24:12 +0800

    Added exception handling to certain cases such as when an InvalidCommand is provided

---
+++ src/main/java/Exceptions/EmptyListException.java
@@ -0,0 +1,7 @@
+package main.java.Exceptions;
+
+public class EmptyListException extends Exception{
+    public EmptyListException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/Exceptions/InvalidCommandException.java
@@ -0,0 +1,7 @@
+package main.java.Exceptions;
+
+public class InvalidCommandException extends Exception {
+    public InvalidCommandException(String message) {
+        super(message);
+    }
+}
