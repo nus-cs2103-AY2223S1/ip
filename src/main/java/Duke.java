4fa7eac40ff6452dbea0fc204a0ef6f0497e459d
anthonyhoth <anthonyhotenghow@gmail.com>
2022-08-18 16:59:40 +0800

    Completed Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -8,41 +7,73 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         ArrayList<Task> taskList = new ArrayList<>();
+        int counter = 1;
 
         // Introduction message
         greetUser();
 
         while (true) {
+            input = sc.nextLine();
+            String[] inputWords = input.split(" ", 2);
+            String commandWord = inputWords[0];
+
             // Print out goodbye message and exits the program
             if (input.equals("bye")) {
-                System.out.println("Bye~ Hope to see you again!");
+                exitJukebox();
                 break;
             }
 
             // Print out the task list
             if (input.equals("list")) {
                 printList(taskList);
-                input = sc.nextLine();
+                continue;
             }
 
+            // Mark the given task
+            if (commandWord.equals("mark")) {
+                String contents = inputWords[1];
+                int taskNumberInt = Integer.parseInt(contents);
+                taskList.get(taskNumberInt - 1).mark();
+
+                System.out.println(String.format("Goodjob! This task is now completed :)\n" + "%s", taskList.get(taskNumberInt - 1)));
+                continue;
+            }
+
+            // Unmark the given task
+            if (commandWord.equals("unmark")) {
+                String contents = inputWords[1];
+                int taskNumberInt = Integer.parseInt(contents);
+                taskList.get(taskNumberInt - 1).unmark();
+
+                System.out.println(String.format("Oh... OK, I'll mark this task as uncompleted!\n" + "%s", taskList.get(taskNumberInt - 1)));
+                continue;
+            }
+
+            // Add a new task to the list
             else {
-                System.out.println("added: " + input);
-                taskList.add(new Task(input));
-                input = sc.nextLine();
+                System.out.println("Okay!\n" + "Added: " + input);
+                taskList.add(new Task(input, counter));
+                counter++;
             }
         }
     }
 
-    // Message to greet the user
+    // Prints message to greet the user
     private static void greetUser() {
-        System.out.println("Hello! I'm Jukebox\n" + "What can I do for you?");
+        System.out.println("Hello! I'm Jukebox :)\n" + "What can I do for you today?");
+    }
+
+    // Prints message to exit the program
+    private static void exitJukebox() {
+        System.out.println("Aww... OK, Hope to see you again!");
     }
 
     // Print out task list iteratively
     private static void printList(ArrayList<Task> taskList) {
+        System.out.println("These are your current tasks! :)");
         int i = 1;
         for (Task t : taskList) {
-            String s = String.format("%d. %s", i, t);
+            String s = String.format("%d.%s", t.getTaskNumber(), t);
             System.out.println(s);
             i++;
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,38 @@
 public class Task {
     private String name;
     private boolean isCompleted;
+    private int taskNumber;
 
-    public Task(String name) {
+    // Constructor for each task
+    public Task(String name, int taskNumber) {
         this.name = name;
+        this.isCompleted = false;
+        this.taskNumber = taskNumber;
+    }
+
+    // Returns a status icon if its completed/uncompleted
+    public String getStatus() {
+        return isCompleted ? "X" : " ";
+    }
+
+    // Returns the task number
+    public int getTaskNumber() {
+        return this.taskNumber;
+    }
+
+    // Marks the task to show completed
+    public void mark() {
+        this.isCompleted = true;
+    }
+
+    // Unmarks the task to show uncompleted
+    public void unmark() {
         this.isCompleted = false;
     }
 
+    // Overridden toString method
     @Override
     public String toString() {
-        return String.format("%s", name);
+        return String.format("[%s] %s", getStatus(), name);
     }
 }
