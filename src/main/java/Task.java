4e2d26fa13c53e9d6f6ea2e885b86540b6c84b34
shawnkai <e0525991@u.nus.edu>
2022-08-18 11:16:39 +0800

    added task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    //...
+}
+
+
