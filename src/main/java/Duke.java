93a8fb1baf5e1e63be6f766822687cac76e2c9f0
Gui Ming Jiang <mjgui2000@hotmail.com>
2022-08-18 23:16:42 +0800

    Add add and list behaviour

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -20,6 +22,8 @@ public class Duke {
     }
 
     public static void main(String[] args) {
+        List<String> tasks = new ArrayList<>();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -29,13 +33,32 @@ public class Duke {
         printMultiMsg(new String[]{"Hello my name is Duke", "What can I do for you?"});
 
         Scanner sc = new Scanner (System.in);
-        while (sc.hasNext()) {
-            String input = sc.next();
-            if (input.equals("bye")) {
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+
+            if (input.length() < 1) continue;
+
+            String firstWord;
+            int firstWordIdx = input.indexOf(' ');
+            if (firstWordIdx > -1) {
+                firstWord = input.substring(0, firstWordIdx);
+            } else {
+                firstWord = input;
+            }
+
+            if (firstWord.equals("bye")) {
                 printMsg("Bye. Hope to see you again soon!");
                 break;
+            } else if (firstWord.equals("list")) {
+                String[] taskStrings = new String[tasks.size()];
+                for (int i = 0; i < tasks.size(); i++) {
+                    taskStrings[i] = (i+1) + ". " + tasks.get(i);
+                }
+                printMultiMsg(taskStrings);
+            } else {
+                tasks.add(input);
+                printMsg("added: " + input);
             }
-            printMsg(input);
         }
     }
 }
