e0c79b173fe9fd800b5e7b0b9c8f2afc7cd93252
Ryan-Tan00 <ryatancw00@gmail.com>
2022-08-18 18:57:26 +0800

    added the ability to store inputs into a list and display the elements in the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -6,10 +7,20 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> arrayList = new ArrayList<>();
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                int length = arrayList.size();
+                for (int i = 0; i < length; i++) {
+                    System.out.println((i + 1) + ". " + arrayList.get(i));
+                }
+            } else {
+                System.out.println("added: " + input);
+                arrayList.add(input);
+            }
+
             input = scanner.nextLine();
         }
 
