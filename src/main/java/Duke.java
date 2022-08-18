c09c5b9568d2cabef61d5c1b483747f0a9121234
LAPTOP-S3RN5C85\eesua <eesuan.234@gmail.com>
2022-08-18 16:00:29 +0800

    Abstracted Duke's reply message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static final String LINE_BREAK = "\t" + "-------------------------------------------------";
+    //    private static String LINE_BREAK = "-------------------------------------------------";
     private final Scanner sc;
     private String input;
     private Task[] tasks = new Task[100];
@@ -11,65 +11,71 @@ public class Duke {
         this.sc = sc;
     }
 
+    private void dukeReply(String message) {
+        String lineBreak = "-------------------------------------------------";
+        String reply = String.format("%s\n%s\n%s", lineBreak, message, lineBreak);
+        System.out.println(reply);
+    }
+
     private void greet() {
-        System.out.println("Hello! I'm Ee Suan!\nWhat can I do for you?");
+        dukeReply("Hello! I'm Ee Suan!\nWhat can I do for you?");
     }
 
     public void start() throws DukeException {
         this.greet();
-        try {
         input = sc.next();
         while (!input.equals("bye")) {
-            switch (input) {
-                case "list":
-                    printList();
-                    break;
-                case "mark": {
-                    int index = sc.nextInt();
-                    mark(index);
-                    break;
-                }
-                case "unmark": {
-                    int index = sc.nextInt();
-                    unmark(index);
-                    break;
-                }
-                case "todo": {
-                    if (sc.nextLine().equals("")) {
-                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+            try {
+                switch (input) {
+                    case "list":
+                        printList();
+                        break;
+                    case "mark": {
+                        int index = sc.nextInt();
+                        mark(index);
+                        break;
                     }
-                    input += sc.nextLine();
-                    Task todo = createToDo();
-                    addTask(todo);
-                    echoTask(todo);
-                    break;
+                    case "unmark": {
+                        int index = sc.nextInt();
+                        unmark(index);
+                        break;
+                    }
+                    case "todo": {
+                        String next = sc.nextLine();
+                        if (next.equals("")) {
+                            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                        } else {
+                            input += next;
+                            Task todo = createToDo();
+                            addTask(todo);
+                            echoTask(todo);
+                            break;
+                        }
+                    }
+                    case "deadline": {
+                        input += sc.nextLine();
+                        Task deadline = createDeadline();
+                        addTask(deadline);
+                        echoTask(deadline);
+                        break;
+                    }
+                    case "event": {
+                        input += sc.nextLine();
+                        Task event = createEvent();
+                        addTask(event);
+                        echoTask(event);
+                        break;
+                    }
+                    default:
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-                case "deadline": {
-                    input += sc.nextLine();
-                    Task deadline = createDeadline();
-                    addTask(deadline);
-                    echoTask(deadline);
-                    break;
-                }
-                case "event": {
-                    input += sc.nextLine();
-                    Task event = createEvent();
-                    addTask(event);
-                    echoTask(event);
-                    break;
-                }
-                default:
-                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            } catch (DukeException e) {
+                dukeReply(e.getMessage());
             }
-        }
+
             input = sc.next();
         }
-        catch (DukeException e) {
-            System.out.println(LINE_BREAK);
-            System.out.println("\t" + e.getMessage());
-            System.out.println(LINE_BREAK);
-        }
-    }
+}
 
     private void exit() {
         System.out.println("Bye. Hope to see you again soon!");
@@ -80,35 +86,31 @@ public class Duke {
 //    }
 
     private void echoTask(Task task) {
-        System.out.println(LINE_BREAK + "\n\tGot it. I've added this task: \n\t  " + task + "\n\tNow you have " + taskIndex + " task(s) in the list.\n" + LINE_BREAK);
+        dukeReply("Got it. I've added this task: \n  " + task + "\nNow you have " + taskIndex + " task(s) in the list.");
     }
 
     private void printList() {
-        System.out.println(LINE_BREAK + "\n\tHere are the tasks in your list:\n\t");
+        String message = "Here are the tasks in your list:";
         for (int i = 0; i < taskIndex; i++) {
             Task curTask = tasks[i];
-            System.out.println("\t" + (i + 1) + ". " + curTask + "\n");
+            message +="\n" + (i + 1) + ". " + curTask;
         }
-        System.out.println(LINE_BREAK);
+        dukeReply(message);
     }
 
     private void mark(int i) {
-        System.out.println(LINE_BREAK);
         Task curTask = tasks[i - 1];
         curTask.markAsDone();
-        System.out.println("\tNice! I've marked this task as done: \n\t  " + curTask + "\n" );
-        System.out.println(LINE_BREAK);
+        dukeReply("Nice! I've marked this task as done: \n  " + curTask);
     }
 
     private void unmark(int i) {
-        System.out.println(LINE_BREAK);
         Task curTask = tasks[i - 1];
         curTask.markAsUndone();
-        System.out.println("\tOK, I've marked this task as not done yet: \n\t  " + curTask + "\n" );
-        System.out.println(LINE_BREAK);
+        dukeReply("OK, I've marked this task as not done yet: \n  " + curTask);
     }
 
-    private Task createToDo () {
+    private Task createToDo() {
         String description = input.substring(5);
         return new ToDo(description);
     }
