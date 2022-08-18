feb2e449f4c442b86fdf400c3a06c67f62f74a34
L1uY1jun <yijunlau@gmail.com>
2022-08-18 22:40:59 +0800

    Completed Level 6. Delete.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner dukeScanner = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Hello i'm Duke\nHow may i help you today?");
-        Task[] taskList  = new Task[100];
-        int[] count = new int[1];
+        ArrayList<Task> taskList  = new ArrayList<Task>();
 
         String input;
         while (true) {
@@ -15,9 +15,9 @@ public class Duke {
                     System.out.println("Bye. Hope to see you again soon!");
                     break;
                 } else if (input.equals("list")) {
-                    taskDisplay(taskList, count[0]);
+                    taskDisplay(taskList);
                 } else {
-                    checkTask(input, taskList, count);
+                    checkTask(input, taskList);
                 }
             } catch (DukeException e) {
                 System.out.println(e.getMessage());
@@ -26,78 +26,88 @@ public class Duke {
     }
 
     /**
-     * Checks input of user and react accordingly
+     * Checks input of user and react accordingly based on the command
      * @param input String input of the user
-     * @param list Array which stores tasks added by user
-     * @param index Current index pointer of last element in array
+     * @param taskArrayList Arraylist which stores tasks added by user
      */
-    private static void checkTask(String input, Task[] list, int[] index) throws DukeException {
+    private static void checkTask(String input, ArrayList<Task> taskArrayList) throws DukeException {
         String[] cmdAndDesc = input.split(" ", 2);
         String command = cmdAndDesc[0];
 
         switch (command) {
             case "mark":
-                if (checkException(cmdAndDesc)) {
+                if (cmdAndDesc.length == 1) {
                     throw new DukeException("☹ OOPS!!! " +
                             "The description of mark cannot be empty.");
                 }
                 int indexToMark = Integer.parseInt(cmdAndDesc[1]) - 1;
-                list[indexToMark].markAsDone();
+                taskArrayList.get(indexToMark).markAsDone();
                 System.out.println("Nice! I've marked this task as done:\n" +
-                        String.format("%s", list[indexToMark]));
+                        String.format("%s", taskArrayList.get(indexToMark)));
                 break;
             case "unmark":
-                if (checkException(cmdAndDesc)) {
+                if (cmdAndDesc.length == 1) {
                     throw new DukeException("☹ OOPS!!! " +
                             "The description of unmark cannot be empty.");
                 }
                 int indexToUnmark = Integer.parseInt(cmdAndDesc[1]) - 1;
-                list[indexToUnmark].markAsUndone();
+                taskArrayList.get(indexToUnmark).markAsUndone();
                 System.out.println("Ok! I've marked this task as not done yet:\n" +
-                        String.format("%s", list[indexToUnmark]));
+                        String.format("%s", taskArrayList.get(indexToUnmark)));
+                break;
+            case "delete" :
+                if (cmdAndDesc.length == 1) {
+                    throw new DukeException("☹ OOPS!!! " +
+                            "The description of delete cannot be empty.");
+                }
+                int indexToDelete = Integer.parseInt(cmdAndDesc[1]) - 1;
+                System.out.println("Noted. I've removed this task:\n" +
+                        String.format("\t%s", taskArrayList.get(indexToDelete)));
+                taskArrayList.remove(indexToDelete);
+                listStatus(taskArrayList);
                 break;
             case "deadline" :
-                if (checkException(cmdAndDesc)) {
+                if (cmdAndDesc.length == 1) {
                     throw new DukeException("☹ OOPS!!! " +
                             "The description of deadline cannot be empty.");
                 }
                 String[] descAndDue = cmdAndDesc[1].split("/by ");
-                if (checkException(descAndDue)) {
+                if (descAndDue.length == 1) {
                     throw new DukeException("☹ OOPS!!! " +
                             "There is no specified date/time for the deadline.");
                 }
                 taskAddition();
-                list[index[0]] = new Deadline(descAndDue[0], descAndDue[1]);
-                System.out.println(String.format("\t%s", list[index[0]]));
-                index[0]++;
-                listStatus(index[0]);
+                taskArrayList.add(new Deadline(descAndDue[0], descAndDue[1]));
+                System.out.println(String.format("\t%s",
+                        taskArrayList.get(taskArrayList.size() - 1)));
+                listStatus(taskArrayList);
                 break;
             case "event" :
-                if (checkException(cmdAndDesc)) {
+                if (cmdAndDesc.length == 1) {
                     throw new DukeException("☹ OOPS!!! " +
                             "The description of event cannot be empty.");
                 }
                 String[] descAndDuration = cmdAndDesc[1].split("/at ");
-                if (checkException(descAndDuration)) {
+                if (descAndDuration.length == 1) {
                     throw new DukeException("☹ OOPS!!! " +
                             "There is no specified time duration for the event.");
                 }
                 taskAddition();
-                list[index[0]] = new Event(descAndDuration[0], descAndDuration[1]);
-                System.out.println(String.format("\t%s", list[index[0]]));
-                index[0]++;
-                listStatus(index[0]);
+                taskArrayList.add(new Event(descAndDuration[0], descAndDuration[1]));
+                System.out.println(String.format("\t%s",
+                        taskArrayList.get(taskArrayList.size() - 1)));
+                listStatus(taskArrayList);
                 break;
             case "todo" :
-                if (checkException(cmdAndDesc)) {
+                if (cmdAndDesc.length == 1) {
                     throw new DukeException("☹ OOPS!!! " +
                             "The description of todo cannot be empty.");
                 }
                 taskAddition();
-                list[index[0]] = new ToDo(cmdAndDesc[1]);
-                System.out.println(String.format("\t%s", list[index[0]]));
-                index[0]++;
-                listStatus(index[0]);
+                taskArrayList.add(new ToDo(cmdAndDesc[1]));
+                System.out.println(String.format("\t%s",
+                        taskArrayList.get(taskArrayList.size() - 1)));
+                listStatus(taskArrayList);
                 break;
             default:
                 throw new DukeException("☹ OOPS!!! " +
@@ -107,16 +117,15 @@ public class Duke {
 
     /**
      * Returns string representation of list of tasks.
-     * @param list Array of tasks as inputted by the user
-     * @param count Total number of tasks
+     * @param list Arraylist of tasks as inputted by the user
      */
-    private static void taskDisplay(Task[] list, int count) {
-        if (count == 0) {
+    private static void taskDisplay(ArrayList<Task> list) {
+        if (list.size() == 0) {
             System.out.println("There are currently no tasks in your list");
         } else {
             System.out.println("Here are the tasks in your list:");
-            for (int i = 0; i < count; i++) {
-                System.out.println(String.format("%d. %s", i + 1, list[i]));
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(String.format("%d. %s", i + 1, list.get(i)));
             }
         }
     }
@@ -129,20 +138,11 @@ public class Duke {
     }
 
     /**
-     * Prints current status of list.
-     * @param count Count of how many total task there are
+     * Prints current status of arraylist.
+     * @param list Arraylist
      */
-    private static void listStatus(int count) {
+    private static void listStatus(ArrayList<Task> list) {
         System.out.println(String.format("Now you have %d tasks in the list.",
-                count));
-    }
-
-    /**
-     * Checks if specified description is present
-     * @param substrings Array of substrings
-     * @return if there is an error
-     */
-    private static boolean checkException(String[] substrings) {
-        return substrings.length == 1 ? true : false;
+                list.size()));
     }
 }
