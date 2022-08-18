2700b9a34cc4ce5dc1dd5eb375320fea1fba020f
rachaelsng <rachaelsng@u.nus.edu>
2022-08-18 13:09:50 +0800

    reset

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,10 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
+        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
-        Scanner sc = new Scanner(System.in);
-        String line = "---------------------------------------------------";
-        System.out.println(line);
-        System.out.println("Hello! I'm Duke\n" +
-                "     What can I do for you?");
-        System.out.println(line);
-
-        String input = sc.nextLine();
-        while(!input.equals("bye")) {
-            System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
-            input = sc.nextLine();
-        }
-        sc.close();
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println("Hello from\n" + logo);
     }
 }
