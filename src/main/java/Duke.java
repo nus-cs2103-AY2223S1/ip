af490cacc78c66a498951cdb32363c18140e8c27
Meng Qiaoran <e0492494@u.nus.edu>
2022-08-18 13:39:42 +0800

    Add Level-3.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.util.*;
-
 public class Duke {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
@@ -9,7 +8,7 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         Scanner input = new Scanner(System.in);
-        List<String> l = new ArrayList<String>();
+        List<Task> l = new ArrayList<Task>();
 
         while (true) {
             String line = input.nextLine();
@@ -19,16 +18,35 @@ public class Duke {
                 System.out.println("____________________________________________________________");
                 break;
             }
+
             if ("list".equals(line)) {
                 int count = 1;
-                for (String s : l) {
-                    System.out.println(String.format("%d. %s", count, s));
+                System.out.println("Here are the tasks in your list:");
+                for (Task t : l) {
+                    System.out.println(String.format("%d.%s %s", count, t.getStatusIcon(),
+                            t.getDescription()));
                     count += 1;
                 }
             }
+            else if (line.contains("mark") && !(line.contains("unmark"))) {
+                int number = Integer.parseInt(line.substring(5));
+                Task t = l.get(number - 1);
+                t.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(String.format("  %s %s", t.getStatusIcon(), t.getDescription()));
+            }
+
+            else if (line.contains("unmark")) {
+                int number = Integer.parseInt(line.substring(7));
+                Task t = l.get(number - 1);
+                t.markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(String.format("  %s %s", t.getStatusIcon(), t.getDescription()));
+            }
+
             else {
                 System.out.println(String.format("added: %s", line));
-                l.add(line);
+                l.add(new Task(line));
             }
             System.out.println("____________________________________________________________");
         }
