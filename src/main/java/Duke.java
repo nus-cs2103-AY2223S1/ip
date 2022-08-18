a12f9023e0d7aebd6d7c3a095ebf478c773add82
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2022-08-18 17:23:46 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws Exception{
+        // Reader and Writers
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // Variables
+        String spacing = "-----------------------------------------";
+
+        // Main code
+        System.out.println(spacing);
+        System.out.println("Hello! I am Greg!");
+        System.out.println("What do you need help with?");
+        System.out.println(spacing + "\n");
+        while (true) {
+            String word = br.readLine();
+            if (word.equals("bye")) {
+                System.out.println(spacing);
+                System.out.println("Goobye, see you again!\n");
+                System.out.println(spacing + "\n");
+                break;
+            } else {
+                System.out.println(spacing);
+                System.out.println(word + "\n");
+                System.out.println(spacing + "\n");
+            }
+        }
     }
 }
