30b12fc0c62b3d9d67b245f6c033bdc60460089e
KSHan29 <seow.han@outlook.com>
2022-08-18 20:29:24 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static boolean terminate = false;
+    private static boolean terminate = false;
+    private static ArrayList<String> inputList = new ArrayList<String>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -17,24 +19,44 @@ public class Duke {
             String userInput = in.nextLine();
             if (userInput.equals("bye")) {
                 Duke.exit();
+            } else if (userInput.equals("list")) {
+                Duke.displayList();
             } else {
-                Duke.echo(userInput);
+                Duke.addTask(userInput);
             }
         }
     }
 
-    public static void greet() {
-        System.out.println("    ____________________________________________________________\n" +
-                "     Hello! I'm Duke\n" +
-                "     What can I do for you?\n" +
-                "    ____________________________________________________________");
+    public static void lineFormat() {
+        System.out.println("    ____________________________________________________________");
     }
 
-    public static void echo(String echoedMessage) {
-        System.out.println("    ____________________________________________________________\n" +
-                "     " + echoedMessage + "\n" +
-                "    ____________________________________________________________");
+    public static void greet() {
+        Duke.lineFormat();
+        System.out.println(
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n");
+        Duke.lineFormat();
     }
+
+    public static void addTask(String echoedMessage) {
+        Duke.lineFormat();
+        System.out.println("     added: " + echoedMessage);
+        Duke.lineFormat();
+        inputList.add(echoedMessage);
+    }
+
+    public static void displayList() {
+        Duke.lineFormat();
+        System.out.println("     Here are the tasks in your list: ");
+        for (int i = 1; i <= inputList.size(); i++) {
+            System.out.println("     " +
+                    String.valueOf(i) + ". " +
+                    inputList.get(i - 1));
+        }
+        Duke.lineFormat();
+    }
+
 
     public static void exit() {
         Duke.terminate = true;
