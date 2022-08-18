90f81dce37b489efa2a2b9d352e6f60e4f057b00
highorbit25 <baobin25@gmail.com>
2022-08-18 14:33:00 +0800

    Implemented Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,47 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("____________________________________________________________");
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        getInput();
+
+
+//        System.out.println(String.join(" ", args));
+
+        }
+
+    public static void getInput() {
+        String inData;
+        Scanner scan = new Scanner( System.in );
+        inData = scan.nextLine();
+
+        switch (inData) {
+            case("bye"):
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            default:
+                System.out.println("____________________________________________________________");
+                System.out.println(inData);
+                System.out.println("____________________________________________________________");
+                getInput();
+        }
+
+
+
+
     }
-    // test123
+
 }
