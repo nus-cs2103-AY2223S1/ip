6efd8b07574f1865b10fbdeb74c1af1da5c55a20
Chee Zhong Wei <cheezhongwei00@gmail.com>
2022-08-18 15:57:26 +0800

    Add command to delete tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -6,7 +7,7 @@ public class Duke {
         System.out.println("| Hi this is Thesh. What can I do for you? |");
         System.out.println("-----------------------------------------------");
         Scanner sc = new Scanner(System.in);
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         int taskCount = 0;
         while (sc.hasNext()) {
             String command = sc.nextLine();
@@ -16,15 +17,15 @@ public class Duke {
             } else if (command.equals("list")) {
                 System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(String.format("      %d. %s", i + 1, tasks[i]));
+                    System.out.println(String.format("      %d. %s", i + 1, tasks.get(i)));
                 }
             } else if (command.startsWith("mark")) {
                 try {
                     String taskNumberString = command.split(" ")[1];
                     int taskNumber = Integer.parseInt(taskNumberString);
-                    tasks[taskNumber - 1].markComplete();
+                    tasks.get(taskNumber-1).markComplete();
                     System.out.println("    Nice! I've marked this task as done:");
-                    System.out.println(String.format("      %s", tasks[taskNumber - 1]));
+                    System.out.println(String.format("      %s", tasks.get(taskNumber-1)));
                 } catch (MarkToggleException e) {
                     System.out.println(e);
                 } catch (NumberFormatException e) {
@@ -36,9 +37,9 @@ public class Duke {
                 try {
                     String taskNumberString = command.split(" ")[1];
                     int taskNumber = Integer.parseInt(taskNumberString);
-                    tasks[taskNumber - 1].markIncomplete();
+                    tasks.get(taskNumber-1).markIncomplete();
                     System.out.println("    OK, I've marked this task as not done yet");
-                    System.out.println(String.format("      %s", tasks[taskNumber - 1]));
+                    System.out.println(String.format("      %s", tasks.get(taskNumber-1)));
                 } catch (MarkToggleException e) {
                     System.out.println(e);
                 } catch (NumberFormatException e) {
@@ -46,6 +47,13 @@ public class Duke {
                 } catch (NullPointerException e) {
                     System.out.println(String.format("There is only %d tasks in the list.", taskCount));
                 }
+            } else if (command.startsWith("delete")) {
+                command = command.replace("delete ", "");
+                int taskToDelete = Integer.parseInt(command);
+                Task deletedTask = tasks.remove(taskToDelete - 1);
+                System.out.println("    Noted. I've removed this task:");
+                System.out.println(String.format("      %s", deletedTask));
+                System.out.println(String.format("    Now you have %d tasks in the list.", --taskCount));
             } else {
                 Task newTask = null;
                 if (command.startsWith("todo")) {
@@ -75,7 +83,7 @@ public class Duke {
                     System.out.println("    ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     continue;
                 }
-                tasks[taskCount++] = newTask;
+                tasks.add(taskCount++, newTask);
                 System.out.println("    Got it. I've added this task:");
                 System.out.print("      ");
                 System.out.println(newTask);
