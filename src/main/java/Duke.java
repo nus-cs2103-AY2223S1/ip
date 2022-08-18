a212dca0615342bb4a621bfb9474f46df2b8726b
Gui Ming Jiang <mjgui2000@hotmail.com>
2022-08-18 23:05:04 +0800

    Add greet, echo, exit behaviour

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void printSeparator() {
+        System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+    
+    private static void printMsg(String msg) {
+        printSeparator();
+        System.out.println("     " + msg);
+        printSeparator();
+    }
+    
+    private static void printMultiMsg(String[] msgs) {
+        printSeparator();
+        for (String msg : msgs) {
+            System.out.println("     " + msg);
+        }
+        printSeparator();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +26,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printMultiMsg(new String[]{"Hello my name is Duke", "What can I do for you?"});
+
+        Scanner sc = new Scanner (System.in);
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                printMsg("Bye. Hope to see you again soon!");
+                break;
+            }
+            printMsg(input);
+        }
     }
 }
