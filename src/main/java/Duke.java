c3227cacfd82e36551511dd9b9e0849b5137a5fd
Hongyi6328 <6328huang@gmail.com>
2022-08-18 19:30:47 +0800

    Complete Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,6 +8,8 @@ public class Duke {
     private static final String EXIT_OUTPUT_STRING = "Bye! See you next time!";
     private static final String EXIT_COMMAND_STRING = "bye";
     private static final String DISPLAY_LIST_COMMAND_STRING = "list";
+    private static final String MARK_DONE_COMMAND_STRING = "mark";
+    private static final String MARK_UNDONE_COMMAND_STRING = "unmark";
 
 
     private CommandParser commandParser;
@@ -45,6 +47,54 @@ public class Duke {
         return "added: " + taskTitle;
     }
 
+    private int getIndexOfFirstWhiteSpace(String input) {
+        int indexOfFirstWhiteSpace = 0;
+        for (; indexOfFirstWhiteSpace < input.length(); indexOfFirstWhiteSpace++) {
+            if (input.charAt(indexOfFirstWhiteSpace) == ' ') {
+                break;
+            }
+        }
+        return indexOfFirstWhiteSpace;
+    }
+
+    private String getFirstWord(String input) {
+        int indexOfFirstWhiteSpace = getIndexOfFirstWhiteSpace(input);
+        return input.substring(0, indexOfFirstWhiteSpace);
+    }
+
+    private int getTaskIndexForMarking(String input) {
+        int indexOfFirstWhiteSpace = getIndexOfFirstWhiteSpace(input);
+        String tailSubString = input.substring(indexOfFirstWhiteSpace, input.length());
+        tailSubString = tailSubString.replace(" ", "");
+        if (tailSubString.isEmpty()) {
+            return -1;
+        }
+        int taskIndex = -1;
+        try {
+            Integer.parseInt(tailSubString);
+        } catch (NumberFormatException ex) {
+            // No need to do anything, because the output will be -1
+            // which will be handled in the higher layer
+        }
+        return taskIndex;
+    }
+
+    private void markTaskDone(int index) {
+        if (index < 0 || index >= taskList.size()) {
+
+        } else {
+            taskList.get(index).markDone();
+        }
+    }
+
+    private void markTaskUndone(int index) {
+        if (index < 0 || index >= taskList.size()) {
+
+        } else {
+            taskList.get(index).markUndone();
+        }
+    }
+
     private void standBy() {
         Scanner scanner = new Scanner(System.in);
         boolean quited = false;
@@ -54,6 +104,24 @@ public class Duke {
             String nextLine = scanner.nextLine();
             if (nextLine.isEmpty()) {
                 continue;
+            }
+
+            boolean commandFetched = false;
+
+            String firstWord = getFirstWord(nextLine);
+            switch (firstWord) {
+            case (MARK_DONE_COMMAND_STRING):
+                int index = getTaskIndexForMarking(nextLine);
+                markTaskDone(index);
+                break;
+
+            case (MARK_UNDONE_COMMAND_STRING):
+                index = getTaskIndexForMarking(nextLine);
+                markTaskUndone(index);
+                break;
+
+            default:
+                break;
             }
 
             switch (nextLine) {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,21 @@
 public class Task {
     private String taskTitle;
+    private boolean done;
+
+    private Task(String taskTitle, boolean done) {
+        this.taskTitle = taskTitle;
+        this.done = done;
+    }
 
     Task(String taskTitle) {
-        this.taskTitle = taskTitle;
+        this(taskTitle, false);
+    }
+
+    void markDone() {
+        done = false;
+    }
+
+    void markUndone() {
+        done = true;
     }
 }
