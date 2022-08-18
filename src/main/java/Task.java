d879c98c0e3c25018b126241fede76d220cce574
LAPTOP-Q59MTK9C\Pokezard <e0538156@u.nus.edu>
2022-08-18 18:05:38 +0800

    level 3 with classes

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private final String val;
+    private final boolean done;
+
+    public Task(String input) {
+        this.val = input;
+        done = false;
+    }
+
+    private Task(String input, boolean done) {
+        this.val = input;
+        this.done = done;
+    }
+
+    public Task markDone() {
+        return new Task(this.val, true);
+    }
+
+    public Task markUndone() {
+        return new Task(this.val, false);
+    }
+
+    public boolean getDone() {
+        return this.done;
+    }
+
+    public String getVal() {
+        return this.val;
+    }
+
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,22 +5,43 @@ public class main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        String[] arr = new String[101];
-        String input = sc.nextLine();
+        Task[] arr = new Task[101];
+        String input = "";
         int curr = 0;
-        arr[curr] = input;
-        while(!input.equals("bye")) {
-            if(!input.equals("list")) {
-                //System.out.println(input); level 1
-                System.out.println(String.format("added: %s", input));
-            }
-            else {
+        Task task = new Task(input);
+        arr[curr] = task;
+        while(!task.getVal().equals("bye")) {
+            task = new Task(sc.nextLine());
+            if(task.getVal().equals("list")) {
                 for(int i = 0; i < curr; i++) {
-                    System.out.println(String.format("%s. %s", i + 1, arr[i]));
+                    if(arr[i].getDone()) {
+                        System.out.println(String.format("%s. [X] %s", i + 1, arr[i].getVal()));
+                    }
+                    else {
+                        System.out.println(String.format("%s. [ ] %s", i + 1, arr[i].getVal()));
+                    }
                 }
+                //System.out.println(input); level 1
             }
-            input = sc.nextLine();
-            arr[++curr] = input;
+            else if(task.getVal().indexOf("mark") == 0) {
+                String[] at = task.getVal().split(" ");
+                int index = Integer.parseInt(at[1]);
+                arr[index - 1] = arr[index - 1].markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(String.format("[X] %s", arr[index-1].getVal()));
+            }
+            else if(task.getVal().indexOf("unmark") == 0) {
+                String[] at = task.getVal().split(" ");
+                int index = Integer.parseInt(at[1]);
+                arr[index - 1] = arr[index - 1].markUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(String.format("[ ] %s", arr[index-1].getVal()));
+            }
+
+            else {
+                System.out.println(String.format("added: %s", task.getVal()));
+                arr[curr++] = task;
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
