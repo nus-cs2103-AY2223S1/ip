cccb733d5127d06e54f9abbf6b3f44ec6acfeb68
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2022-08-18 17:24:11 +0800

    Do Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static Scanner scanner = new Scanner(System.in);
+    public static String readLine() {
+        return scanner.nextLine();
+    }
+    public static void reply(String message) {
+        System.out.print("> ");
+        System.out.println(message);
+    }
+    public static void reply(String[] message) {
+        for (int i = 0; i < message.length; ++i) {
+            System.out.print(i == 0 ? "> " : "  ");
+            System.out.println(message[i]);
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +22,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        while (true) {
+            String line = readLine();
+            if (line.equals("bye")) {
+                reply("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                reply(line);
+            }
+        }
     }
 }
