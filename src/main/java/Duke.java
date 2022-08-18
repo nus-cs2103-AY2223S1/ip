4186b6ee97db60bde31b0375fdc52d19114fea27
WingKei67 <e0817842@u.nus.edu>
2022-08-18 18:17:59 +0800

    Customized features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
-public class Duke {
+import java.util.Scanner;
+
+class Yilia {
+    public static void greet() {
+        String logo = "  \\‾\\     /‾/  |‾|  |‾|          |‾|       /‾‾‾\\      \n" +
+                "   \\ \\   / /   | |  | |          | |      / /‾\\ \\     \n" +
+                "    \\ \\_/ /    | |  | |          | |     / /___\\ \\    \n" +
+                "     ‾| |‾     | |  | |          | |    / /_____\\ \\   \n" +
+                "      | |      | |  | |_______   | |   / /       \\ \\  \n" +
+                "      |_|      |_|  |_________|  |_|  /_/         \\_\\ \n";
+        System.out.println("Hello! I'm Yilia\n" + logo + "What can I do for you?");
+    }
+
+    public static void echo(String text) {
+        System.out.println( text);
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String message = scanner.next();
+            if (message.equals("bye")) {
+                exit();
+                return;
+            } else {
+                echo(message);
+            }
+        }
     }
 }
