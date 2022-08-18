c8bc26e5f0288c399d61c66f31aaa239e31c710f
nqt230 <76938663+nqt230@users.noreply.github.com>
2022-08-18 10:38:52 +0800

    Implement Level-2
    - Create ParseInput method to parse commands to the bot and call the relevant methods
    - Create AddToList and DisplayList methods to support text list feature
    > AddToList stores a given text string to a list
    > DisplayList prints all the text strings stored so far in order
    - Remove unused Echo method from Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static String botName = "Duke";
     private static int lineLength = 80;
+    private static ArrayList<String> textList = new ArrayList<>();
+    private static boolean isDone = false;
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -18,29 +21,62 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
 
-        boolean done = false;
-        while (!done) {
-            GenerateLine();
+        isDone = false;
+        while (!isDone) {
+            GenerateLine(lineLength);
             String input = scanner.nextLine();
-            GenerateLine();
+            GenerateLine(lineLength);
 
-            switch (input.toLowerCase()) {
-                case "bye":
-                    done = true;
-                    break;
-                default:
-                    Echo(input);
-            }
+            ParseInput(input);
         }
 
         System.out.println("Goodbye.");
     }
 
-    public static void GenerateLine() {
-        System.out.println(String.format("%" + lineLength + "s", "").replace(" ", "-"));
+    /**
+     * Parses an input string and calls the relevant method (if any)
+     * Calls AddToList by default
+     *
+     * @param input - the input string to be parsed
+     */
+    public static void ParseInput(String input) {
+        switch (input.toLowerCase()) {
+            case "bye":
+                isDone = true;
+                break;
+            case "list":
+                DisplayList();
+                break;
+            default:
+                AddToList(input);
+        }
     }
 
-    public static void Echo(String message) {
-        System.out.println(message);
+    /**
+     * Prints a line of a specified character length
+     *
+     * @param length - the character length of the line to print
+     */
+    public static void GenerateLine(int length) {
+        System.out.println(String.format("%" + length + "s", "").replace(" ", "-"));
+    }
+
+    /**
+     * Adds a text string to the list, which is displayed when DisplayList is called
+     *
+     * @param text - the text to add to the list
+     */
+    public static void AddToList(String text) {
+        textList.add(text);
+        System.out.printf("added: %s\n", text);
+    }
+
+    /**
+     * Displays the list of texts added to the list so far in order
+     */
+    public static void DisplayList() {
+        for (int i = 0; i < textList.size(); i++) {
+            System.out.printf("%d. %s\n", i + 1, textList.get(i));
+        }
     }
 }
