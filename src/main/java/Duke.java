97ee98f1790b51e3225911617947f78ebe55c1d9
nitant-p <nitantpanicker@gmail.com>
2022-08-18 11:56:57 +0800

    Added mark and unmark task. Improved OOP?

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,48 +1,79 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
-    private final String topWindow = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \t";
-    private final String bottomWindow = "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-    private final static String greeting = "Hi, I'm Bob! Nice to meet you!\n\tWhats up?";
-    private final static String farewell = "Okay then, see ya later :)";
-    private ArrayList<Task> tasks;
-    public Duke() {
+    private static final String topWindow = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \t";
+    private static final String bottomWindow = "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    private static final String greeting = "Hi, I'm Ploopy! Nice to meet you!\n\tWhats up?";
+    private static final String farewell = "Okay then, see ya later :)";
+    private static final String completedTask = "Nice! You've completed this task. I'll mark it as done.";
+    private static final String incompleteTask = "Alright this task has been marked as undone.";
+
+
+    public static final String textArt = "\n" +
+            "██████████h█████████████████████████\n" +
+            "█▄─▄▄─█▄─▄███─▄▄─█─▄▄─█▄─▄▄─█▄─█─▄█\n" +
+            "██─▄▄▄██─██▀█─██─█─██─██─▄▄▄██▄─▄██\n" +
+            "▀▄▄▄▀▀▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀▄▄▄▀▀▀▀▄▄▄▀▀";
+
+    private static ArrayList<Task> tasks;
+
+    public static void start() {
+        messageFormatter(textArt);
         messageFormatter(greeting);
         tasks = new ArrayList<>();
+        command();
     }
 
-    private void messageFormatter(String input) {
+    private static void messageFormatter(String input) {
         System.out.println(topWindow + input + bottomWindow);
     }
 
-    private void displayList() {
+    private static void displayList() {
         int index = 1;
+        System.out.println(topWindow);
         for (Task item : tasks) {
-            System.out.println("\n \t" + index + ". " + item.toString());
+            System.out.println("\n \t" + index + ". " + item.getStatus() + item);
             index++;
         }
+        System.out.println(bottomWindow);
     }
 
-    private void command() {
+    private static void markTask(int taskIndex) {
+        Task current = tasks.get(taskIndex - 1);
+        current.markDone();
+        messageFormatter(completedTask + "\n\t" + current.getStatus() + " " + current);
+    }
+
+    private static void unmarkTask(int taskIndex) {
+        Task current = tasks.get(taskIndex - 1);
+        current.unmark();
+        messageFormatter(incompleteTask + "\n\t" + current.getStatus() + " " + current);
+    }
+
+    private static void command() {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        while(!input.equals("bye")) {
-            if (input.equals("list")) {
-                System.out.println(topWindow);
+        StringBuilder input = new StringBuilder(scanner.nextLine());
+        while(!input.toString().equals("bye")) {
+            if (input.length() > 7) {
+                if (input.substring(0, 7).equals("unmark ")) {
+                    unmarkTask(input.charAt(7) - 48);
+                }
+            } else if (input.length() > 5) {
+                if (input.substring(0, 5).equals("mark ")) {
+                    markTask(input.charAt(5) - 48);
+                }
+            } else if (input.toString().equals("list")) {
                 displayList();
-                System.out.println(bottomWindow);
             } else {
-                tasks.add(new Task(input));
+                tasks.add(new Task(input.toString()));
                 messageFormatter("added: " + input);
             }
-            input = scanner.nextLine();
+            input.replace(0, input.length(), scanner.nextLine()); //replacing content
         }
         messageFormatter(farewell);
     }
     
     public static void main(String[] args) {
-        Duke newDuke = new Duke();
-        newDuke.command();
-
+        start();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,27 @@
 public class Task {
-    private String name;
-
+    protected String name;
+    protected boolean done;
     Task(String name) {
         this.name = name;
+        this.done = false;
     }
 
     @Override
     public String toString() {
         return name;
     }
+
+    public void markDone() {
+        done = true;
+    }
+
+    public void unmark() {
+        done = false;
+    }
+
+    public String getStatus() {
+        if (done) return "[✓] ";
+        else return "[ ] ";
+    }
+
 }
