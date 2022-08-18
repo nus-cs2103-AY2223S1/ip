45acdb1cbe30877a452dd298579a38c3d9d93273
pratham31012002 <pratham31012002@gmail.com>
2022-08-18 17:57:29 +0800

    feat: added command list

--- src/main/java/Pluto.java
+++ src/main/java/Pluto.java
@@ -5,7 +5,7 @@ public class Pluto {
     private static final String CHATBOT = "Pluto";
     public static void main(String[] args) {
 
-        String introduction = String.format("Hello I am %s.\nWhat can I do for you?", CHATBOT);
+        String introduction = String.format("\tHello I am %s.\n\tWhat can I do for you?", CHATBOT);
         System.out.println(introduction);
 
         Scanner sc = new Scanner(System.in);
@@ -13,13 +13,25 @@ public class Pluto {
     }
 
     public static void inputCommand(Scanner sc) {
+        ArrayList<String> missions = new ArrayList<>();
         String command = sc.nextLine();
         String exit = "bye";
         while (!command.equals(exit)) {
-            System.out.println("\t" + command);
+            switch(command) {
+                case "list":
+                    for (int i = 0; i < missions.size(); i++) {
+                        String output = String.format("\t%d. %s",i + 1, missions.get(i));
+                        System.out.println(output);
+                    }
+                    break;
+                default:
+                    missions.add(command);
+                    System.out.println("\tadded: " + command);
+            }
             command = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
+
 
