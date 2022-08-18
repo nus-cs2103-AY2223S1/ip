1dc859ceabdf231d3c194f97f6227fac97139d79
junweimoo <junweimoo@gmail.com>
2022-08-18 14:05:36 +0800

    Add ability to store and retrieve inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
+    List<String> inputList;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,6 +16,10 @@ public class Duke {
 
         Duke duke = new Duke();
         duke.start();
+    }
+
+    public Duke() {
+        inputList = new ArrayList<>();
     }
 
     public void start() {
@@ -28,9 +37,27 @@ public class Duke {
         if ("bye".equals(s)) {
             System.out.println("Bye. Hope to see you again soon!");
             return;
+        } else if ("list".equals(s)) {
+            System.out.print(inputListToString());
+        } else {
+            inputList.add(s);
+            System.out.println("Added: " + s);
         }
 
-        System.out.println(s);
         loop(sc);
     }
+
+    private String inputListToString() {
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        for (String s : inputList) {
+            sb.append(i);
+            sb.append(". ");
+            sb.append(s);
+            sb.append("\n");
+            ++i;
+        }
+        return sb.toString();
+    }
+
 }
