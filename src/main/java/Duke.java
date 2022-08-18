4db7e292df7d9e4b9d3f37277f802dda24ca812f
KJunWei <khorjunweiwei@gmail.com>
2022-08-18 10:23:20 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,51 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static String INDENTATION = "     ";
+
     private static String reply(String message) {
         return "    ____________________________________________________________\n" +
-                "     " + message + "\n" +
+                INDENTATION + message + "\n" +
                 "    ____________________________________________________________";
     }
+
+    private static String craftList(String[] array) {
+        int length = array.length;
+        String result = "";
+        for (int x = 0; x < length; x++) {
+            if (array[x] == null) {
+                break;
+            } else {
+                if (x == 0) {
+                    result += array[x];
+                } else {
+                    result += "\n" + INDENTATION + array[x];
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+
         Scanner scan = new Scanner(System.in);
         System.out.println(reply("Hello! I'm Duke\n" +
                 "     What can I do for you?"));
         boolean conversation = true;
+        String[] work = new String[100];
+        int workCounter = 0;
         while (conversation) {
             String response = scan.nextLine().toString();
             if (response.equals("bye")) {
                 System.out.println(reply("Bye. Hope to see you again soon!"));
                 conversation = false;
+            } else if (response.equals("list")) {
+                System.out.println(reply(craftList(work)));
             } else {
-                System.out.println(reply(response));
+                work[workCounter] = (workCounter + 1) + ". " + response;
+                workCounter++;
+                System.out.println(reply("added: " + response));
             }
         }
     }
