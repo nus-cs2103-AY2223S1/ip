561fbb607c77adba0455e8015309dbc792dcc588
wxy1203 <e0817822@u.nus.edu>
2022-08-18 21:09:44 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,46 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        start();
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while (true) {
+            if (command.equals("bye")) {
+                bye();
+                break;
+            }
+            else {
+                System.out.println("    ____________________________________________________________\n" +
+                        "     " +
+                        command +
+                        "\n    ____________________________________________________________");
+                command = scanner.nextLine();
+            }
+        }
+
+
+
+    }
+
+    public static void start() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+        System.out.println("    ____________________________________________________________\n" +
+                "     Hola! I'm Ashy (//●⁰౪⁰●)//\n" +
+                "     What can I do for you? my darling~\n" +
+                "    ____________________________________________________________");
     }
+
+    public static void bye() {
+        System.out.println("    ____________________________________________________________\n" +
+                "     Byeeee~ Hope to see you again soon (•͈⌔•͈⑅)\n" +
+                "    ____________________________________________________________");
+    }
+
 }
