4acf81f30d4cf1ffe1c396abb18a2d03dbb98118
CedricChia123 <cedricchia01@gmail.com>
2022-08-18 18:12:26 +0800

    changed string formatting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,52 +25,49 @@ public class Duke {
                             System.out.println((i + 1) + "." + currentTask);
                         }
                     }
-                } else if (input.startsWith("mark")) {
-                    Task t = list.get(Integer.parseInt(input.substring(5)) - 1);
-                    t.markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(t);
-                } else if (input.startsWith("unmark")) {
-                    Task t = list.get(Integer.parseInt(input.substring(7)) - 1);
-                    t.markAsUndone();
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(t);
-                } else if (input.startsWith("todo")) {
-                    if (input.substring(4).isBlank()) {
-                        throw new DukeException("The description of a todo cannot be empty.");
-                    }
-                    Task t = new ToDo(input.substring(5));
-                    list.add(t);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(t);
-                    System.out.println("Now you have " + list.size() + " tasks in the list.");
-                } else if (input.startsWith("deadline")) {
-                    if (input.substring(8).isBlank()) {
-                        throw new DukeException("The description of a deadline cannot be empty.");
-                    }
-                    String curr = input.substring(9, input.indexOf("/"));
-                    Deadline d = new Deadline(curr, input.substring(input.lastIndexOf("/") + 3));
-                    list.add(d);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(d);
-                    System.out.println("Now you have " + list.size() + " tasks in the list.");
-                } else if (input.startsWith("event")) {
-                    if (input.substring(5).isBlank()) {
-                        throw new DukeException("The description of a event cannot be empty.");
-                    }
-                    String curr = input.substring(6, input.indexOf("/"));
-                    Event e = new Event(curr, input.substring(input.lastIndexOf("/") + 3));
-                    list.add(e);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(e);
-                    System.out.println("Now you have " + list.size() + " tasks in the list.");
-                } else if (input.startsWith("delete")) {
-                    Task t = list.remove(Integer.parseInt(input.substring(7)) - 1);
-                    System.out.println("Noted. I've removed this task:");
-                    System.out.println(t);
-                    System.out.println("Now you have " + list.size() + " tasks in the list.");
                 } else {
-                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                    String[] command = input.split(" ", 2);
+                    if (input.startsWith("mark")) {
+                        Task t = list.get(Integer.parseInt(command[1]) - 1);
+                        t.markAsDone();
+                        System.out.println("Nice! I've marked this task as done:\n" + t);
+                    } else if (input.startsWith("unmark")) {
+                        Task t = list.get(Integer.parseInt(command[1]) - 1);
+                        t.markAsUndone();
+                        System.out.println("OK, I've marked this task as not done yet:\n" + t);
+                    } else if (input.startsWith("todo")) {
+                        if (input.substring(4).isBlank()) {
+                            throw new DukeException("The description of a todo cannot be empty.");
+                        }
+                        Task t = new ToDo(command[1]);
+                        list.add(t);
+                        System.out.println("Got it. I've added this task:\n" + t + "\n" +
+                                "Now you have " + list.size() + " tasks in the list.");
+                    } else if (input.startsWith("deadline")) {
+                        if (input.substring(8).isBlank()) {
+                            throw new DukeException("The description of a deadline cannot be empty.");
+                        }
+                        String[] curr = command[1].split("/by", 2);
+                        Deadline d = new Deadline(curr[0], curr[1]);
+                        list.add(d);
+                        System.out.println("Got it. I've added this task:\n" + d + "\n" +
+                                "Now you have " + list.size() + " tasks in the list.");
+                    } else if (input.startsWith("event")) {
+                        if (input.substring(8).isBlank()) {
+                            throw new DukeException("The description of a deadline cannot be empty.");
+                        }
+                        String[] curr = command[1].split("/at", 2);
+                        Event e = new Event(curr[0], curr[1]);
+                        list.add(e);
+                        System.out.println("Got it. I've added this task:\n" + e + "\n" +
+                                "Now you have " + list.size() + " tasks in the list.");
+                    } else if (input.startsWith("delete")) {
+                        Task t = list.remove(Integer.parseInt(command[1]) - 1);
+                        System.out.println("Noted. I've removed this task:\n" + t + "\n" +
+                                "Now you have " + list.size() + " tasks in the list.");
+                    } else {
+                        throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                    }
                 }
             } catch (DukeException e) {
                 System.out.println(e);
