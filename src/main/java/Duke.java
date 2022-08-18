12039b151d1ad09c244aca02b8485e7ff335c40f
Meng Qiaoran <e0492494@u.nus.edu>
2022-08-18 11:26:00 +0800

    Add Level-1.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String line = input.nextLine();
+            System.out.println("____________________________________________________________");
+            if ("bye".equals(line)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
