93d12b8a42dfd0ac79a1cd257310c1c9fe2d9c81
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-08-19 04:31:15 +0800

    Create DukeException class

---
+++ src/main/java/exceptions/DukeException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class DukeException extends Exception {
+
+  public DukeException(String message) {
+    super(message);
+  }
+
+  @Override
+  public String toString() {
+    return "Hunbun, ERROR!!! " + getMessage();
+  }
+}
