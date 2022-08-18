a7ad89119847fa9df53b249c360e6b5ced68bbe1
lyuting47 <luting_c@yahoo.com>
2022-08-18 23:28:53 +0800

    Added Level 6, used ArrayList to store tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    private static String printSpacer() {
-        return "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    private static void printSpacer() {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
-    private static boolean isPositiveInt(String s) {
-        try {
-            int n = Integer.parseInt(s);
-            return n > 0;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    private static Task[] tasks = new Task[100];
+    private static ArrayList<Task> tasks = new ArrayList<>();
     private static int tasksLength = 0;
 
     private static boolean cont = true;
+
+    private static boolean startsWith(String cmd, String prefix) {
+        String[] stuff = cmd.split(" ");
+        return stuff[0].equals(prefix);
+    }
 
     private static void printTaskList() {
         if (tasksLength == 0) {
@@ -26,81 +23,159 @@ public class Duke {
         } else {
             System.out.println("Here are the tasks in your list:");
             for (int i = 1; i <= tasksLength; i++) {
-                System.out.println(i + "." + tasks[i - 1].toString());
+                System.out.println(i + "." + tasks.get(i - 1).toString());
             }
         }
-        System.out.println(printSpacer());
+        printSpacer();
     }
 
     private static void greeting() {
-        System.out.println(printSpacer());
-        System.out.println("Hello! This is Duke! What can I do for you today?");
-        System.out.println(printSpacer());
+        printSpacer();
+        System.out.println(":D Hello! This is Duke! What can I do for you today?");
+        printSpacer();
     }
 
     private static void goodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(printSpacer());
+        System.out.println(":) Bye. Hope to see you again soon!");
+        printSpacer();
         cont = false;
     }
 
-    private static void invalidCommand() {
-        System.out.println("Sorry I don't understand the command...");
-        System.out.println(printSpacer());
-    }
+    private static void addTodo(String s) throws EmptyTodoException {
+        if (s.length() <= 5) {
+            throw new EmptyTodoException();
+        }
 
-    private static void addTodo(String s) {
-        tasks[tasksLength] = new Todo(s.substring(5));
+        String result = s.substring(5);
+        tasks.add(new Todo(result));
         tasksLength++;
         System.out.println("Got it. I've added this task:");
-        System.out.println(tasks[tasksLength - 1].toString());
+        System.out.println(tasks.get(tasksLength - 1).toString());
         System.out.println("Now you have " + tasksLength + " tasks in your list.");
-        System.out.println(printSpacer());
+        printSpacer();
     }
 
-    private static void addDeadline(String s) {
+    private static void addDeadline(String s) throws DeadlineFormatException {
+        if (s.length() <= 9) {
+            throw new DeadlineFormatException();
+        }
         String[] stuff = s.substring(9).split(" /by ");
-        tasks[tasksLength] = new Deadline(stuff[0], stuff[1]);
+        if (stuff.length < 2) {
+            throw new DeadlineFormatException();
+        }
+        tasks.add(new Deadline(stuff[0], stuff[1]));
         tasksLength++;
         System.out.println("Got it. I've added this task:");
-        System.out.println(tasks[tasksLength - 1].toString());
+        System.out.println(tasks.get(tasksLength - 1).toString());
         System.out.println("Now you have " + tasksLength + " tasks in your list.");
-        System.out.println(printSpacer());
+        printSpacer();
     }
 
-    private static void addEvent(String s) {
+    private static void addEvent(String s) throws EventFormatException {
+        if (s.length() <= 6) {
+            throw new EventFormatException();
+        }
         String[] stuff = s.substring(6).split(" /at ");
-        tasks[tasksLength] = new Event(stuff[0], stuff[1]);
+        if (stuff.length < 2) {
+            throw new EventFormatException();
+        }
+        tasks.add(new Event(stuff[0], stuff[1]));
         tasksLength++;
         System.out.println("Got it. I've added this task:");
-        System.out.println(tasks[tasksLength - 1].toString());
+        System.out.println(tasks.get(tasksLength - 1).toString());
         System.out.println("Now you have " + tasksLength + " tasks in your list.");
-        System.out.println(printSpacer());
+        printSpacer();
     }
 
-    private static void markTask(String s) {
-        int n = Integer.parseInt(s.substring(5));
-        if (n > tasksLength) {
-            System.out.println("Oops, no such task number!");
-            System.out.println(printSpacer());
-        } else {
-            tasks[n - 1].setDone(true);
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.println(tasks[n - 1].toString());
-            System.out.println(printSpacer());
+    private static void markTask(String s) throws TaskNumberException {
+        try {
+            int n = Integer.parseInt(s.substring(5));
+            if (n > tasksLength) {
+                throw new TaskNumberException();
+            } else {
+                tasks.get(n - 1).setDone(true);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks.get(n - 1).toString());
+                printSpacer();
+            }
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            throw new TaskNumberException();
         }
     }
 
-    private static void unmarkTask(String s) {
-        int n = Integer.parseInt(s.substring(7));
-        if (n > tasksLength) {
-            System.out.println("Oops, no such task number!");
-            System.out.println(printSpacer());
+    private static void unmarkTask(String s) throws TaskNumberException {
+        try {
+            int n = Integer.parseInt(s.substring(7));
+            if (n > tasksLength) {
+                throw new TaskNumberException();
+            } else {
+                tasks.get(n - 1).setDone(false);
+                System.out.println("Okie, I've marked this task as not done yet:");
+                System.out.println(tasks.get(n - 1).toString());
+                printSpacer();
+            }
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            throw new TaskNumberException();
+        }
+    }
+
+    private static void parseCommand(String s) throws InvalidCommandException {
+
+        if (s.equals("list")) {
+            printTaskList();
+        } else if (s.equals("bye")) {
+            goodbye();
+        } else if (startsWith(s, "mark")) {
+
+            try {
+                markTask(s);
+            } catch (TaskNumberException e) {
+                System.out.println(":( Oops! Please enter a valid task number!");
+                System.out.println("You currently have " + tasksLength + " tasks.");
+                printSpacer();
+            }
+
+        } else if (startsWith(s, "unmark")) {
+
+            try {
+                unmarkTask(s);
+            } catch (TaskNumberException e) {
+                System.out.println(":( Oops! Please enter a valid task number!");
+                System.out.println("You currently have " + tasksLength + " tasks.");
+                printSpacer();
+            }
+
+        } else if (startsWith(s, "todo")) {
+
+            try {
+                addTodo(s);
+            } catch (EmptyTodoException e) {
+                System.out.println(":( Oops! The description of a todo cannot be empty!");
+                printSpacer();
+            }
+
+        } else if (startsWith(s, "deadline")) {
+
+            try {
+                addDeadline(s);
+            } catch (DeadlineFormatException e) {
+                System.out.println(":( Oops! That's not the right way to set a deadline!");
+                System.out.println("Please use this format: \"deadline <description> /by <time>\"");
+                printSpacer();
+            }
+
+        } else if (startsWith(s, "event")) {
+
+            try {
+                addEvent(s);
+            } catch (EventFormatException e) {
+                System.out.println(":( Oops! That's not the right way to set an event!");
+                System.out.println("Please use this format: \"event <description> /at <time>\"");
+                printSpacer();
+            }
+
         } else {
-            tasks[n - 1].setDone(false);
-            System.out.println("Okie, I've marked this task as not done yet:");
-            System.out.println(tasks[n - 1].toString());
-            System.out.println(printSpacer());
+            throw new InvalidCommandException(s);
         }
     }
 
@@ -110,27 +185,15 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         while (cont) {
-
-            String s = sc.nextLine();
-
-            if (s.equals("list")) {
-                printTaskList();
-            } else if (s.equals("bye")) {
-                goodbye();
-            } else if (s.startsWith("mark ") && isPositiveInt(s.substring(5))) {
-                markTask(s);
-            } else if (s.startsWith("unmark ")  && isPositiveInt(s.substring(7))) {
-                unmarkTask(s);
-            } else if (s.startsWith("todo ")) {
-                addTodo(s);
-            } else if (s.startsWith("deadline ")) {
-                addDeadline(s);
-            } else if (s.startsWith("event ")) {
-                addEvent(s);
-            } else {
-                invalidCommand();
+            try {
+                String s = sc.nextLine();
+                parseCommand(s);
+            } catch (InvalidCommandException e) {
+                System.out.println(":( Sorry I don't understand the command: " + e);
+                printSpacer();
             }
-
         }
     }
 }
+
+
