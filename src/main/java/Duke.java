57e234187a1b7f2d332dfaf6b473a33720a03f52
Agamjyot Singh Chadha <agamjyot01@gmail.com>
2022-08-19 01:02:37 +0800

    first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,6 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm\n" + logo);
     }
 }
