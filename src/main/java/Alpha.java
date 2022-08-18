cf9ad3db49b7a2e597a4c0e947a52f0a46aa14cd
agarwal-anjali <e0866261@u.nus.edu>
2022-08-18 16:51:43 +0800

    Implemented a feature to add and display tasks

--- src/main/java/Alpha.java
+++ src/main/java/Alpha.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Alpha {
+    public static List<String> list = new ArrayList<>();
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[36m";
     public static final String ANSI_RESET = "\u001B[0m";
@@ -16,13 +20,23 @@ public class Alpha {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         switch (input) {
+            case "list": {
+                System.out.println(ANSI_YELLOW + ">> " + "Your to-do list is as follows:" + ANSI_RESET);
+                int count = 1;
+                for (String task:list) {
+                    System.out.println(ANSI_YELLOW + count + ") " + task + ANSI_RESET);
+                    count++;
+                }
+                enterMessage();
+            }
             case "bye": {
                 System.out.println(ANSI_YELLOW + ">> " + "Bye, See you soon!" + ANSI_RESET);
                 System.exit(0);
                 break;
             }
             default: {
-                System.out.println(ANSI_YELLOW + ">> " + input + ANSI_RESET);
+                list.add(input);
+                System.out.println(ANSI_YELLOW + ">> " + "added: " + input + ANSI_RESET);
                 enterMessage();
             }
         }
