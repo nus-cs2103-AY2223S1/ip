ad31166542e7da39b6c4f3e11d57797e86034a46
jeromehjj <jeromehjj@gmail.com>
2022-08-18 14:23:23 +0800

    Added Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,25 +2,30 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    private static final String line = "____________________________________________________________\n";
+    private static final String LINE = "____________________________________________________________\n";
+
     public static void greeting() {
         String initMessage = "Hello! I'm Duke\n" + "What can I do for you?\n";
-        System.out.println(line + initMessage + line);
+        System.out.println(LINE + initMessage + LINE);
     }
 
     public static void exit() {
         String goodbyeMessage = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + goodbyeMessage + line);
+        System.out.println(LINE + goodbyeMessage + LINE);
     }
 
-    public static void add(String obj, int i) {
-        System.out.println(i + ". " + obj);
+    public static void listTasks(ArrayList<Task> tasklist) {
+        int counter = 1;
+        for (Task t : tasklist) {
+            System.out.println(counter + "." + t.toString());
+            counter++;
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = 0;
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -34,18 +39,37 @@ public class Duke {
             if (input.equals("bye")) {
                 exit();
                 break;
-            } else if (input.equals("list")){
-                System.out.println(line);
-                for (int i = 0; i < count; i++) {
-                    add(list.get(i), i + 1);
+            } else if (input.equals("list")) {
+                System.out.println(LINE + "Here are the tasks in your list: ");
+                listTasks(list);
+                System.out.println(LINE);
+            } else if (input.contains("unmark")) {
+                char taskNumber = input.charAt(7);
+                int number = Character.getNumericValue(taskNumber);
+                if (number <= 0 || number > count) {
+                    System.out.println("Sorry, I can't mark this as undone if it does not exist :(");
+                } else {
+                    System.out.println(LINE + "OK, I've marked this task as not done yet:");
+                    Task task = list.get(number - 1);
+                    task.markAsUndone();
+                    System.out.println(task.toString() + "\n" + LINE);
                 }
-                System.out.println(line);
+            } else if (input.contains("mark")) {
+                char taskNumber = input.charAt(5);
+                int number = Character.getNumericValue(taskNumber);
+                if (number <= 0 || number > count) {
+                    System.out.println("Sorry, I can't mark this as done if it does not exist :(");
+                } else {
+                    System.out.println(LINE + "Nice! I've marked this task as done:");
+                    Task task = list.get(number - 1);
+                    task.markAsDone();
+                    System.out.println(task.toString() + "\n" + LINE);
+                }
             } else {
-                list.add(count, input);
+                list.add(count, new Task(input));
                 count++;
-                System.out.println(line + "added: " + input + "\n" + line);
+                System.out.println(LINE + "added: " + input + "\n" + LINE);
             }
         }
     }
-
 }
