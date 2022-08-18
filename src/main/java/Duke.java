8a4241894e6f5e058bcd1150766277953adada9d
seanmanik <seanmanik@gmail.com>
2022-08-18 18:39:50 +0800

    Level-2 completed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        String input = "";
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
