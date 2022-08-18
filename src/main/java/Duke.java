38462f1c2da08e606b32ff5093b43f0c6ade966f
hoang227 <hoangg227@gmail.com>
2022-08-18 20:07:40 +0800

    level 2 implemented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,21 +10,31 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("    ____________________________________________________________");
-        System.out.println("    Hello! I'm Duke\n    What can I do for you?");
+        System.out.println("     Hello! I'm Duke\n     What can I do for you?");
         System.out.println("    ____________________________________________________________\n");
 
+        ArrayList<String> store = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
             System.out.println("    ____________________________________________________________");
-            System.out.println("    " + input);
-            System.out.println("    ____________________________________________________________\n");
-            input = sc.nextLine();
+            if (input.equals("list")) {
+                for (int i=0; i < store.size(); i++) {
+                    System.out.println("     " + (i+1) + ". " + store.get(i));
+                }
+                System.out.println("    ____________________________________________________________\n");
+                input = sc.nextLine();
+            } else {
+                store.add(input);
+                System.out.println("    " + " added: " + input);
+                System.out.println("    ____________________________________________________________\n");
+                input = sc.nextLine();
+            }
         }
         System.out.println("    ____________________________________________________________");
-        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("     Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________\n");
     }
 }
