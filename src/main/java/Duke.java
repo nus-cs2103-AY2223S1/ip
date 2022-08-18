35b8c97510a810ac31b5f46fce5cce81524717f0
KSHan29 <seow.han@outlook.com>
2022-08-19 00:30:25 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,35 +16,35 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         while (!terminate) {
-            String userInput = in.nextLine();
-            // retrieve first word
-            int indexOfFirstSpace = userInput.indexOf(" ");
-            int indexOfFirstSlash = userInput.indexOf("/");
-            String firstWord = userInput;
-            String taskDescription = "";
-            String time = "";
-            if (indexOfFirstSpace != - 1) {
-                firstWord = userInput.substring(0, indexOfFirstSpace);
-                taskDescription = userInput.substring(indexOfFirstSpace + 1);
-            }
-            if (indexOfFirstSlash != -1) {
-                taskDescription = userInput.substring(indexOfFirstSpace + 1, indexOfFirstSlash - 1);
-                time = userInput.substring(indexOfFirstSlash + 1);
-            }
-            // retrieve int for numbered operations
-            char charOfInt = userInput.charAt(userInput.length() - 1);
-            // convert ASCII character of integer to int
-            int taskNumber =  charOfInt - '0';
             try {
+                String userInput = in.nextLine();
+                // retrieve first word
+                int indexOfFirstSpace = userInput.indexOf(" ");
+                int indexOfFirstSlash = userInput.indexOf("/");
+                String firstWord = userInput;
+                String taskDescription = "";
+                String time = "";
+                if (indexOfFirstSpace != - 1) {
+                    firstWord = userInput.substring(0, indexOfFirstSpace);
+                    taskDescription = userInput.substring(indexOfFirstSpace + 1);
+                }
+                if (indexOfFirstSlash != -1) {
+                    taskDescription = userInput.substring(indexOfFirstSpace + 1, indexOfFirstSlash - 1);
+                    time = userInput.substring(indexOfFirstSlash + 1);
+                }
+                // retrieve int for numbered operations
+                char charOfInt = userInput.charAt(userInput.length() - 1);
+                // convert ASCII character of integer to int
+                int taskNumber =  charOfInt - '0';
                 switch (firstWord) {
                     case "bye":
                         Duke.exit();
                         break;
                     case "unmark":
-                        Duke.unmarkTask(taskNumber);
+                        Duke.unmarkTask(taskNumber, taskDescription);
                         break;
                     case "mark":
-                        Duke.markTask(taskNumber);
+                        Duke.markTask(taskNumber, taskDescription);
                         break;
                     case "list":
                         Duke.displayList();
@@ -58,6 +58,9 @@ public class Duke {
                     case "event":
                         Duke.addEvent(taskDescription, time);
                         break;
+                    case "delete":
+                        Duke.removeTask(taskNumber, taskDescription);
+                        break;
                     default:
                         throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
@@ -65,9 +68,9 @@ public class Duke {
                 Duke.lineFormat();
                 System.out.println("     " + e.toString());
                 Duke.lineFormat();
-            } catch (ArrayIndexOutOfBoundsException e) {
+            } catch (IndexOutOfBoundsException e) {
                 Duke.lineFormat();
-                System.out.println("     ☹ OOPS!!! Please key in a valid number");
+                System.out.println("     ☹ OOPS!!! Please enter a valid task number.");
                 Duke.lineFormat();
             }
         }
@@ -89,6 +92,17 @@ public class Duke {
         Duke.lineFormat();
         System.out.println("     Got it. I've added this task: \n" +
                 "       " + task.toString() + "\n" +
+                "     Now you have " + taskList.size() + " tasks in the list.");
+        Duke.lineFormat();
+    }
+
+    public static void removeTask(int taskNumber, String description) {
+        if (description.equals("")) {
+            throw new DukeException("☹ OOPS!!! The task number for delete cannot be empty.");
+        }
+        Task removedTask = taskList.remove(taskNumber - 1);
+        System.out.println("     Noted. I've removed this task: \n" +
+                "       " + removedTask.toString() + "\n" +
                 "     Now you have " + taskList.size() + " tasks in the list.");
         Duke.lineFormat();
     }
@@ -136,8 +150,11 @@ public class Duke {
         Duke.lineFormat();
     }
 
-    public static void markTask(int i) {
-        Task currentTask = taskList.get(i - 1);
+    public static void markTask(int taskNumber, String description) {
+        if (description.equals("")) {
+            throw new DukeException("☹ OOPS!!! The task number for mark cannot be empty.");
+        }
+        Task currentTask = taskList.get(taskNumber - 1);
         currentTask.setTaskStatus(true);
         String taskDescription = currentTask.toString();
         Duke.lineFormat();
@@ -146,8 +163,11 @@ public class Duke {
         Duke.lineFormat();
     }
 
-    public static void unmarkTask(int i) {
-        Task currentTask = taskList.get(i - 1);
+    public static void unmarkTask(int taskNumber, String description) {
+        if (description.equals("")) {
+            throw new DukeException("☹ OOPS!!! The task number for unmark cannot be empty.");
+        }
+        Task currentTask = taskList.get(taskNumber - 1);
         currentTask.setTaskStatus(false);
         String taskDescription = currentTask.toString();
         Duke.lineFormat();
