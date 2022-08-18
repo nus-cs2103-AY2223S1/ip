d1fc925dbc3b32904281276475fe92c89c5f9548
Sheryl <sherylkong07@gmail.com>
2022-08-18 15:21:09 +0800

    first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,46 @@
+import java.util.Scanner;
+import java.lang.Number;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        Scanner sc= new Scanner(System.in);
+        String[] strArray = new String[100];
+        char[] doneArray = new char[100];
+        int strCount = 0;
+
+        while (true) {
+            String str = sc.nextLine();
+            if (str.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.exit(0);
+
+            } else if (str.equals("list")) {
+                System.out.printf("Here are the tasks in your list\n");
+                for (int i = 0; i < strCount; i++) {
+                    System.out.printf("%d.[%c] %s\n", i+1, doneArray[i], strArray[i]);
+                }
+
+            } else if (str.startsWith("mark")) {
+                String[] word = str.split(" ");
+                int taskNo = Integer.parseInt(word[1]);
+                doneArray[taskNo] = 'X';
+                System.out.printf("Nice! I've marked this task as done: \n" +
+                                    "  [X] %s", strArray[taskNo]);
+
+            } else if (str.startsWith("unmark")) {
+                String[] word = str.split(" ");
+                int taskNo = Integer.parseInt(word[1]);
+                doneArray[taskNo] = ' ';
+                System.out.printf("OK, I've marked this task as not done yet: \n" +
+                                    "  [ ] %s", strArray[taskNo]);
+            } else {
+                strArray[strCount] = str;
+                doneArray[strCount] = ' ';
+                strCount++;
+                System.out.println("added: " + str);
+            }
+        }
+
     }
 }
