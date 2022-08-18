55e14e479536eb5dec117344777e42641e59d326
Eugene Leong <eugenelmj@gmail.com>
2022-08-18 15:13:28 +0800

    Level-1
    Initial draft

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-----------------------------\n" +
+                "Hello! I'm Duke\n" +
+                "What can I do for you\n" +
+                "-----------------------------\n");
+
+        while (true) {
+            String answer = sc.nextLine();
+            if (!answer.equals("bye")) {
+                System.out.println("-----------------------------\n" + answer + "\n" +
+                        "-----------------------------\n");
+            } else {
+                System.out.println("-----------------------------\n" + "Bye. Hope to see you again soon!" + "\n" +
+                        "-----------------------------\n");
+                break;
+            }
+        }
     }
 }
