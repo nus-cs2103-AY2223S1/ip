097a959a4b6b2554d5e91a20c9400faee0b82313
isanidiot <anthonyliao1129@gmail.com>
2022-08-18 16:39:51 +0800

    Level 2 add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 public class Duke {
+
+    private static List<String> todoList;
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,15 +9,38 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Welcome to UNC");
+        System.out.println("Welcome to UNC\n");
+
+        todoList = new ArrayList<>();
 
         String input;
         Scanner scanner = new Scanner(System.in);
         while(!Objects.equals(input = scanner.nextLine(), "bye")) {
-            System.out.println(input);
+            String[] words = input.split(" ", 2);
+            switch(words[0]) {
+                case "list":
+                    displayList();
+                    break;
+                default:
+                    addtoList(input);
+
+            }
         }
 
+
         System.out.println("Bye");
+    }
+
+    private static void displayList() {
+        for(int i = 0; i < todoList.size(); i++) {
+            System.out.println(i + 1 + ". " + todoList.get(i));
+        }
+
+    }
+
+    private static void addtoList(String input) {
+        System.out.println("added: " + input);
+        todoList.add(input);
     }
 
 
