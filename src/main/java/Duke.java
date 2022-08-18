cf25c9d1c2594981fe15b771b968b57c777acb7e
rjkoh <koh.ruijie123@gmail.com>
2022-08-18 20:33:01 +0800

    Level-3: Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,40 @@
 import java.util.Scanner;
 public class Duke {
+    private static class Task {
+        private boolean completed;
+        private String name;
+        private int count;
+
+        public Task(String name, int count) {
+            this.name = name;
+            this.count = count;
+            this.completed = false;
+        }
+
+        public void mark() {
+            this.completed = true;
+        }
+
+        public void unmark() {
+            this.completed = false;
+        }
+
+        @Override
+        public String toString() {
+            String comp = this.completed
+                    ? "[X]"
+                    : "[ ]";
+            return String.format("%d." + comp + " " + name, count);
+        }
+
+        public String toStr() {
+            String comp = this.completed
+                    ? "[X]"
+                    : "[ ]";
+            return String.format(comp + " " + name);
+        }
+    }
+
     public static void main(String[] args) {
         /*
         String logo = " ____        _        \n"
@@ -11,7 +46,7 @@ public class Duke {
 
          */
 
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         int count = 0;
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
@@ -23,11 +58,20 @@ public class Duke {
                 break;
             } else if (item.equals("list")) {
                 for (int i =0; i < count; i++) {
-                    String str = String.format("%d. " + arr[i], i+1);
-                    System.out.println(str);
+                    System.out.println(arr[i].toString());
                 }
+            } else if (item.startsWith("mark")) {
+                String str = item.replace("mark ", "");
+                Integer index = Integer.valueOf(str);
+                arr[index - 1].mark();
+                System.out.println("Nice! I've marked this task as done:\n" + arr[index - 1].toStr());
+            } else if (item.startsWith("unmark")) {
+                String str = item.replace("unmark ", "");
+                Integer index = Integer.valueOf(str);
+                arr[index - 1].unmark();
+                System.out.println("OK, I've marked this task as not done yet:\n" + arr[index - 1].toStr());
             } else {
-                arr[count] = item;
+                arr[count] = new Task(item, count+1);
                 count++;
                 System.out.println("Added: " + item);
             }
