51d172984b9d20da12bf5346305a29b3526f4c2e
seanmanik <seanmanik@gmail.com>
2022-08-18 19:21:47 +0800

    Bot is now able to record responses in a list, and return them when requested. Level-2 added.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String input = "";
+        ArrayList<String> list = new ArrayList<>();
         while (true) {
             input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+            } else {
+                list.add(input);
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
