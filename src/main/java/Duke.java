9dc94d5e4cea214ba177aba1b6403855534e6d15
L1uY1jun <yijunlau@gmail.com>
2022-08-18 16:37:20 +0800

    Modified Level 2. Add, List.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,8 @@ public class Duke {
     public static void main(String[] args) {
         Scanner dukeScanner = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Hello i'm Duke\nHow may i help you today?");
+        String[] taskList  = new String[100];
+        int count = 0;
 
         String input;
         while (true) {
@@ -12,8 +14,26 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else {
-                System.out.println(input);
+                checkTask(input, taskList, count);
+                count++;
             }
+        }
+    }
+
+    private static void checkTask(String input, String[] list, int index) {
+        switch (input) {
+            case "list" :
+                taskDisplay(list, index);
+                break;
+            default:
+                System.out.println("added: " + input);
+                list[index] = input;
+        }
+    }
+
+    private static void taskDisplay(String[] list, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(String.format("%d. %s", i + 1, list[i]));
         }
     }
 }
