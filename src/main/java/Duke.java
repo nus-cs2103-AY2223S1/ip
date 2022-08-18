7253030b104186a74bfae68073d75d83171b367b
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-08-18 08:54:00 +0800

    Level1: Added Greet, Echo, Exit Functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class Duke {
+    private static final String greetMessage = "Hello! I'm Duke \nWhat can I do for you?";
+    private static final String byeMessage = "Bye. Hope to see you again soon!";
+    private static void wrapText(String content) {
+        System.out.println("-".repeat(57));
+        System.out.println(content);
+        System.out.println("-".repeat(57));
+    }
+
+    private static void handleUserInputs(Scanner scanner) {
+        while(scanner.hasNext()) {
+            String[] inputs = scanner.nextLine().split(" ");
+            String input = inputs[0];
+            if (input.equals("bye")) {
+                wrapText(byeMessage);
+                break;
+            } else {
+                wrapText(input);
+            }
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +28,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        wrapText(greetMessage);
+        Scanner scanner = new Scanner(System.in);
+        handleUserInputs(scanner);
     }
 }
