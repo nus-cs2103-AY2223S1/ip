1cb09d05344816cb1dd2dfbe4c1ebda8bf479580
Ryan Chua Zong Xun <e0725636@u.nus.edu>
2022-08-18 22:39:12 +0800

    added the missing chatbot dialogue for Level-3 tasks

--- src/main/java/Fred.java
+++ src/main/java/Fred.java
@@ -25,13 +25,18 @@ public class Fred {
                 System.out.println("Fred: Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Fred: Here are the tasks in your list:");
                 Fred.list(storage);
             } else if (input.startsWith("mark")) {
+                System.out.println("Fred: Nice! I've marked this task as done:");
                 int index = Character.getNumericValue(input.charAt(input.length() - 1));
                 storage.get(index - 1).setStatus(true);
+                System.out.println("Fred: " + storage.get(index - 1).toString());
             } else if (input.startsWith("unmark")) {
+                System.out.println("Fred: OK, I've marked this task as not done yet:");
                 int index = Character.getNumericValue(input.charAt(input.length() - 1));
                 storage.get(index - 1).setStatus(false);
+                System.out.println("Fred: " + storage.get(index - 1).toString());
             } else {
                 Task newTask = new Task(input);
                 storage.add(newTask);
