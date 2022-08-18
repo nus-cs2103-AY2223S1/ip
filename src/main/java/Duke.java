68cab5bf3180b86dd9a56bc17ff609d3ff98611d
Daniel Luo <hurinturin123@gmail.com>
2022-08-18 18:38:17 +0800

    Level 0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        // System.out.println("Enter username");
+
+        // String userName = myObj.nextLine();  // Read user input
+        // System.out.println("Username is: " + userName);  // Output user input
+        System.out.println("Hello! I'm Duke \r\nWhat can I do for you? \r\nBye. Hope to see you again soon!");
+
     }
 }
