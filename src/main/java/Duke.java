4f0c93d69826a3d0a45ded6f28836f88d27197f0
lulucopter <lzydaniel.lee@gmail.com>
2022-08-19 07:03:26 +0800

    Implemented basic greeting, echo and exit cases

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //start up sequence
+        Scanner sc = new Scanner(System.in);
+        greet();
+
     }
+
+    public static void greet() {
+        String logo = "_ .-') _     ('-.         .-') _  \n" +
+                "( (  OO) )   ( OO ).-.    ( OO ) ) \n" +
+                " \\     .'_   / . --. /,--./ ,--,'  \n" +
+                " ,`'--..._)  | \\-.  \\ |   \\ |  |\\  \n" +
+                " |  |  \\  '.-'-'  |  ||    \\|  | ) \n" +
+                " |  |   ' | \\| |_.'  ||  .     |/  \n" +
+                " |  |   / :  |  .-.  ||  |\\    |   \n" +
+                " |  '--'  /  |  | |  ||  | \\   |   \n" +
+                " `-------'   `--' `--'`--'  `--'   \n";
+
+        System.out.println(logo + "Ouuuuuuuuuhhhhhh Spo0ky");
+        System.out.println("Hello from\n");
+    }
+
+    public static
 }
