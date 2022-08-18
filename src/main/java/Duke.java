0c3da274e13290c1f9883744862331022e093894
Brandon <brandon.thio@yahoo.com.sg>
2022-08-18 17:27:30 +0800

    Implement a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types 'bye'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+
+import java.util.Scanner;
 public class Duke {
+    private static String WELCOME_MESSAGE =  "Hello! I'm Duke\n" + "What can I do for you?";
+    private static String GOODBYE_MESSAGE =  "Bye. Hope to see you again soon!";
+
+
+    /**
+    * Note: You are strongly encouraged to customize the chatbot name,
+    * command/display formats, and even the personality of the chatbot
+    * to make your chatbot unique.
+    */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(WELCOME_MESSAGE);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(GOODBYE_MESSAGE);
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
