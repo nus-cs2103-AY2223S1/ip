5eaacf7810ca98f5fcfb71194e6a39bf5792fed3
Ryan-Tan00 <ryatancw00@gmail.com>
2022-08-18 21:23:58 +0800

    added the ability to mark tasks as done or not done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,23 +2,85 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static class Task {
+        private boolean finished = false;
+        private final String task;
+
+        public Task(String task) {
+            this.task = task;
+        }
+
+        public void markAsFinished() {
+            this.finished = true;
+        }
+
+        public void markAsUnfinished() {
+            this.finished = false;
+        }
+
+        public String status() {
+            return this.finished ? "X" : " ";
+        }
+
+        public String getTask() {
+            return this.task;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                int length = arrayList.size();
+                System.out.println("Here are the tasks in your list:");
+                int length = taskList.size();
                 for (int i = 0; i < length; i++) {
-                    System.out.println((i + 1) + ". " + arrayList.get(i));
+                    Task currentTask = taskList.get(i);
+                    int taskNumber = i + 1;
+                    String status = currentTask.status();
+                    System.out.println(taskNumber + ".[" + status + "] " + currentTask.task);
                 }
             } else {
-                System.out.println("added: " + input);
-                arrayList.add(input);
+                String[] splitString = input.split(" ", 2);
+
+                if (splitString.length == 2) {
+                    if (splitString[0].equals("mark") || splitString[0].equals("unmark")) {
+                        int taskNumber;
+
+                        try {
+                            taskNumber = Integer.parseInt(splitString[1]);
+                            if (taskNumber > taskList.size() || taskNumber <= 0) {
+                                System.out.println("Error: task number invalid");
+                            } else {
+                                Task selectedTask = taskList.get(taskNumber - 1);
+                                if (splitString[0].equals("mark")) {
+                                    System.out.println("Nice! I've marked this task as done:");
+                                    selectedTask.markAsFinished();
+                                } else {
+                                    System.out.println("OK, I've marked this task as not done yet:");
+                                    selectedTask.markAsUnfinished();
+                                }
+                                System.out.println("[" + selectedTask.status() + "] " + selectedTask.getTask());
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("Error: incorrect format");
+                        }
+                    } else {
+                        System.out.println("added: " + input);
+                        Task newTask = new Task(input);
+                        taskList.add(newTask);
+                    }
+                } else {
+                    System.out.println("added: " + input);
+                    Task newTask = new Task(input);
+                    taskList.add(newTask);
+                }
             }
 
             input = scanner.nextLine();
