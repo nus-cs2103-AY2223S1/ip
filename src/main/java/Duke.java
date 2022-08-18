b95587c8da56205e5150a7031a866a0714b2eb02
junwei-tan <junweitan@u.nus.edu>
2022-08-18 18:15:13 +0800

    Duke Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        System.out.println("\tHello! I'm Duke\t\nWhat can I do for you?");
+        while (true) {
+            String userInput = bufferedReader.readLine();
+            if (userInput.equals("bye")) {
+                System.out.println("\tBye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println("\t" + userInput);
+        }
+        bufferedReader.close();
+        inputStreamReader.close();
     }
 }
