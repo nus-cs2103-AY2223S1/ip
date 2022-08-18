a65d492dcbff1eaa3f186ae41004c6401df6fa4e
Jiacheng-y <e0773814@u.nus.edu>
2022-08-18 17:34:13 +0800

    level 1 task: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String echo() {
+        Scanner userScan = new Scanner(System.in);
+        String message = userScan.nextLine();
+        while (!message.contentEquals("bye")) {
+            System.out.println(message);
+            message = userScan.nextLine();
+        }
+        if (message.contentEquals("bye")) {
+            System.out.println("Goodbye, see you soon for your next healthy reality check!");
+        }
+        return message;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Rio, the reality check you never asked for but really need.\n" +
+                "What can I help with today?\n");
+        echo();
     }
 }
