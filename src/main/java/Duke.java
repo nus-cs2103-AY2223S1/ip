9208e7f3c3dae5fef20b7a14fa490e2d156502ee
seanmanik <seanmanik@gmail.com>
2022-08-18 18:45:12 +0800

    Revert "Level-2 completed."
    This reverts commit 8a4241894e6f5e058bcd1150766277953adada9d.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,10 @@
-import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        String input = "";
-        while (true) {
-            input = scanner.nextLine();
-            if (input.equals("bye")) {
-                break;
-            }
-            System.out.println(input);
-        }
-        System.out.println("Bye. Hope to see you again soon!");
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
 }
