ee3799df22c851579501288e794a0448979894ab
Ryan Chua Zong Xun <e0725636@u.nus.edu>
2022-08-18 23:21:23 +0800

    Added logic for ToDo, Event, and Deadline. Improved mark and unmark to support more than one digit. Also fixed minor error in event.

--- src/main/java/Fred.java
+++ src/main/java/Fred.java
@@ -28,15 +28,32 @@ public class Fred {
                 System.out.println("Fred: Here are the tasks in your list:");
                 Fred.list(storage);
             } else if (input.startsWith("mark")) {
-                System.out.println("Fred: Nice! I've marked this task as done:");
-                int index = Character.getNumericValue(input.charAt(input.length() - 1));
+                int index = Integer.parseInt(input.substring(5));
                 storage.get(index - 1).setStatus(true);
+                System.out.println("Fred: Nice! I've marked this task as done:");
                 System.out.println("Fred: " + storage.get(index - 1).toString());
             } else if (input.startsWith("unmark")) {
-                System.out.println("Fred: OK, I've marked this task as not done yet:");
-                int index = Character.getNumericValue(input.charAt(input.length() - 1));
+                int index = Integer.parseInt(input.substring(7));
                 storage.get(index - 1).setStatus(false);
+                System.out.println("Fred: OK, I've marked this task as not done yet:");
                 System.out.println("Fred: " + storage.get(index - 1).toString());
+            } else if (input.startsWith("todo")) {
+                storage.add(new ToDo(input.substring(5)));
+                System.out.println("Fred: Got it. I've added this task:");
+                System.out.println("Fred: " + storage.get(storage.size() - 1).toString());
+                System.out.println("Fred: Now you have " + storage.size() + " tasks in your list.");
+            } else if (input.startsWith("event")) {
+                int split = input.indexOf("/at");
+                storage.add(new Event(input.substring(6, split - 1), input.substring(split + 4)));
+                System.out.println("Fred: Got it. I've added this task:");
+                System.out.println("Fred: " + storage.get(storage.size() - 1).toString());
+                System.out.println("Fred: Now you have " + storage.size() + " tasks in your list.");
+            } else if (input.startsWith("deadline")) {
+                int split = input.indexOf("/by");
+                storage.add(new Deadline(input.substring(9, split - 1), input.substring(split + 4)));
+                System.out.println("Fred: Got it. I've added this task:");
+                System.out.println("Fred: " + storage.get(storage.size() - 1).toString());
+                System.out.println("Fred: Now you have " + storage.size() + " tasks in your list.");
             } else {
                 Task newTask = new Task(input);
                 storage.add(newTask);
