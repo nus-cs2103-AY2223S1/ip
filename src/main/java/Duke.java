104d224a0cb8de8188e84803762c9b8c4f37af2d
tsammeow <97447335+tsammeow@users.noreply.github.com>
2022-08-19 01:48:14 +0800

    duke level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static boolean alive;
+
+    private static String handle(String command, String params) {
+        switch (command) {
+            case "bye":
+                alive = false;
+                return "Goodbye!";
+            default:
+                return command;
+        }
+    }
+
+    private static void printResponse(String response) {
+        System.out.println("    __________________________________________________");
+        System.out.println("    " + response);
+        System.out.println("    __________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printResponse("Hello! What can I do for you today?");
+        Scanner sc = new Scanner(System.in);
+
+        alive = true;
+        while (alive) {
+            String in, command, params;
+            in = sc.nextLine();
+            int split = in.indexOf(" ");
+            if (split < 0) {
+                command = in;
+                params = "";
+            } else {
+                command = in.substring(0, split);
+                params = in.substring(split + 1);
+            }
+            printResponse(handle(command, params));
+        }
     }
 }
