2a4f10bbd443d70137ddcafb32182dc85dfb9658
ShawnChew <shawnchew200@gmail.com>
2022-08-18 19:48:00 +0800

    Added functions for level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.*;
+
 public class Duke {
+    private static final String TAB = "    ";
+    private static final String LINEBREAK = "______________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+    }
+
+    private static void printTab(String content) {
+        System.out.println(TAB + content);
+    }
+
+    public static void greet() {
+        printTab(LINEBREAK);
+        printTab("Hello! I'm Duke");
+        printTab("What can I do for you?");
+        printTab(LINEBREAK);
+        System.out.println();
+        echo();
+
+    }
+
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                printTab(LINEBREAK);
+                printTab("Bye. Hope to see you again soon!");
+                printTab(LINEBREAK);
+                System.out.println();
+                break;
+            } else {
+                printTab(LINEBREAK);
+                printTab(s);
+                printTab(LINEBREAK);
+                System.out.println();
+            }
+        }
     }
 }
