96a691832e275b70e2ff1229925bea1d396b047d
Chua Yu Xuan Jordan <jcyx1999@gmail.com>
2022-08-18 22:53:14 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        Task[] tasks = new Task[100];
+        List<Task> tasks = new ArrayList<Task>();
         int taskIndex = 0;
         String indent = "    ";
         String line = "-------------------------------------------------";
@@ -26,23 +26,31 @@ public class Duke {
             } else if (input.equals("list")) {
                 for (int j = 0; j < taskIndex; j++) {
                     int taskNum = j + 1;
-                    System.out.println(indent + taskNum + ". " + tasks[j]);
+                    System.out.println(indent + taskNum + ". " + tasks.get(j));
                 }
-            } else if (input.startsWith("mark")){
-                Task task = tasks[Integer.valueOf(input.substring(5)) - 1];
+            } else if (input.startsWith("mark")) {
+                Task task = tasks.get(Integer.valueOf(input.substring(5)) - 1);
                 task.mark();
                 System.out.println(indent + "Nice! I've marked this task as done:");
                 System.out.println("      " + task);
             } else if (input.startsWith("unmark")) {
-                Task task = tasks[Integer.valueOf(input.substring(7)) - 1];
+                Task task = tasks.get(Integer.valueOf(input.substring(7)) - 1);
                 task.unmark();
                 System.out.println(indent + "Ok, I've marked this task as not done yet:");
                 System.out.println("      " + task);
+            } else if (input.startsWith("delete")) {
+                int index = Integer.valueOf(input.substring(7)) - 1;
+                Task task = tasks.get(index);
+                tasks.remove(index);
+                taskIndex -= 1;
+                System.out.println("Noted. I've removed this task:");
+                System.out.println("      " + task);
+                System.out.println(indent + "Now you have " + taskIndex + " tasks in the list");
             } else if (input.startsWith("todo")) {
                 if (!input.equals("todo")) {
-                    tasks[taskIndex] = new Todo(input);
+                    tasks.add(new Todo(input));
                     System.out.println(indent + "Got it. I've added this task:");
-                    System.out.println("      " + tasks[taskIndex]);
+                    System.out.println("      " + tasks.get(taskIndex));
                     taskIndex += 1;
                     System.out.println(indent + "Now you have " + taskIndex + " tasks in the list");
                 } else {
@@ -51,17 +59,17 @@ public class Duke {
             } else if (input.startsWith("event")) {
                 String event = input.substring(0, input.indexOf("/"));
                 String time = input.substring(input.indexOf("/") + 1);
-                tasks[taskIndex] = new Event(event, time);
+                tasks.add(new Event(event, time));
                 System.out.println(indent + "Got it. I've added this task:");
-                System.out.println("      " + tasks[taskIndex]);
+                System.out.println("      " + tasks.get(taskIndex));
                 taskIndex += 1;
                 System.out.println(indent + "Now you have " + taskIndex + " tasks in the list");
             } else if (input.startsWith("deadline")){
                 String deadlineTask = input.substring(0, input.indexOf("/"));
                 String deadline = input.substring(input.indexOf("/") + 1);
-                tasks[taskIndex] = new Deadline(deadlineTask, deadline);
+                tasks.add(new Deadline(deadlineTask, deadline));
                 System.out.println(indent + "Got it. I've added this task:");
-                System.out.println("      " + tasks[taskIndex]);
+                System.out.println("      " + tasks.get(taskIndex));
                 taskIndex += 1;
                 System.out.println(indent + "Now you have " + taskIndex + " tasks in the list");
             } else {
