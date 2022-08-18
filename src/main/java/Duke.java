1c4833c171e2242eb159610e68e247bb73f336f3
Bacon-Strips <86605835+Bacon-Strips@users.noreply.github.com>
2022-08-18 11:48:56 +0800

    Added the functionality to mark and unmark tasks within the chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,17 +8,58 @@ public class Duke {
     private static final String exitCmd = "bye";
     private static final String noTaskMessage   = "It appears you have no tasks right now,\n"
                                                 + "would you like to add some?";
-    private static final ArrayList<String> tasks = new ArrayList<>();
+    private static final String taskMarkedMessage = "Nice! I've marked this task as done:\n";
+    private static final String taskUnmarkedMessage = "OK, I've marked this task as not done yet:\n";
+    private static final String alreadyMarkedMessage = "This task is already marked:\n";
+    private static final String alreadyUnmarkedMessage = "This task is already unmarked:\n";
+    private static final String noSuchTaskMessage   = "It appears there is no such task, \n"
+                                                    + "Please try again";
+    private static final String invalidInputMessage = "The input is invalid, please try again.";
+    private static final ArrayList<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println(startUpMessage);
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
         while (!userInput.equals(exitCmd)) {
+            String[] inputSplit = inputSplit(userInput);
             if (userInput.equals("list")) {
                 System.out.println(printTasks());
+            } else if (inputSplit[0].equals("mark")) {
+                try {
+                    int taskNum = Integer.parseInt(inputSplit[1]);
+                    if (inputSplit.length > 2) {
+                        System.out.println(invalidInputMessage);
+                    } else if (taskNum > tasks.size()) {
+                        System.out.println(noSuchTaskMessage);
+                    } else {
+                            boolean valid = tasks.get(taskNum - 1).check();
+                            System.out.println(
+                                    (valid ? taskMarkedMessage : alreadyMarkedMessage)
+                                            + tasks.get(taskNum - 1).toString());
+                        }
+                } catch (NumberFormatException e) {
+                    System.out.println(invalidInputMessage);
+                }
+            } else if (inputSplit[0].equals("unmark")) {
+                try {
+                    int taskNum = Integer.parseInt(inputSplit[1]);
+                    if (inputSplit.length > 2) {
+                        System.out.println(invalidInputMessage);
+                    } else if (taskNum > tasks.size()) {
+                        System.out.println(noSuchTaskMessage);
+                    } else {
+                        boolean valid = tasks.get(taskNum - 1).uncheck();
+                        System.out.println(
+                                (valid ? taskUnmarkedMessage : alreadyUnmarkedMessage)
+                                        + tasks.get(taskNum - 1).toString());
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(invalidInputMessage);
+                }
             } else {
-                tasks.add(userInput);
-                System.out.println("added: " + userInput);
+                Task newTask = new Task(userInput);
+                tasks.add(newTask);
+                System.out.println("added: " + newTask.toString());
             }
             userInput = sc.nextLine();
         }
@@ -31,13 +72,20 @@ public class Duke {
         } else {
             StringBuilder message = new StringBuilder();
             for (int i = 0; i < tasks.size(); i++) {
-                if (i != tasks.size() - 1) {
-                    message.append(String.format("%d. %s\n", i + 1, tasks.get(i)));
-                } else {
-                    message.append(String.format("%d. %s", i + 1, tasks.get(i)));
-                }
+                message.append(String.format("%d. %s"
+                                + (i != tasks.size() - 1 ? "\n" : "")
+                                , i + 1, tasks.get(i).toString()));
             }
             return message.toString();
+        }
+    }
+
+    static String[] inputSplit(String input) {
+        if (input.contains("mark") || input.contains("unmark")) {
+            String[] result = input.split(" ");
+            return result;
+        } else {
+            return new String[] {input};
         }
     }
 }
