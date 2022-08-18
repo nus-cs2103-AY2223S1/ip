a9546180eb16e4a6ebf11cf8db254ec4253846fb
WingKei67 <e0817842@u.nus.edu>
2022-08-18 18:27:02 +0800

    Added mark and unmark function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,25 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-
+class Task {
+    private boolean isDone = false;
+    private String content;
+    Task(String content) {
+        this.content = content;
+    }
+    public void check() {
+        isDone = true;
+    }
+    public void uncheck() {
+        isDone = false;
+    }
+    @Override
+    public String toString() {
+        return "[" + (isDone? 'X': ' ') + "] " + content;
+    }
+}
 class Yilia {
-    private static ArrayList<String> abilities = new ArrayList();
+    private static ArrayList<Task> abilities = new ArrayList();
+
     public static void greet() {
         String logo = "  \\‾\\     /‾/  |‾|  |‾|          |‾|       /‾‾‾\\      \n" +
                 "   \\ \\   / /   | |  | |          | |      / /‾\\ \\     \n" +
@@ -14,7 +31,7 @@ class Yilia {
     }
 
     public static void echo(String text) {
-        abilities.add(text);
+        abilities.add(new Task(text));
         System.out.println("added: " + text);
     }
 
@@ -28,6 +45,24 @@ class Yilia {
         }
     }
 
+    public static void mark(int index) {
+        try {
+            abilities.get(index - 1).check();
+            System.out.println("Nice! I've marked this task as done:\n" + abilities.get(index - 1).toString());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index " + index + " out of bounds\nPlease input another index");
+        }
+    }
+
+    public static void unmark(int index) {
+        try {
+            abilities.get(index - 1).uncheck();
+            System.out.println("OK, I've marked this task as not done yet:\n" + abilities.get(index - 1).toString());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index " + index + " out of bounds\nPlease input another index");
+        }
+    }
+
     public static void main(String[] args) {
         greet();
         while (true) {
@@ -38,6 +73,10 @@ class Yilia {
                 return;
             } else if (message.equals("list")) {
                 showList();
+            } else if (message.contains("unmark")) {
+                unmark(scanner.nextInt());
+            } else if (message.contains("mark")) {
+                mark(scanner.nextInt());
             } else {
                 echo(message);
             }
