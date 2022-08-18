3b57e1f5e5c7aba4252b39a01befe04654958576
L1uY1jun <yijunlau@gmail.com>
2022-08-18 18:19:15 +0800

    Added Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,40 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Constructor for Task.
+     * @param description description of task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Check status of task, done ( ) or not done (X).
+     * @return String representation of current status
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    /**
+     * Mark this task as done
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Mark this task as undone
+     */
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
