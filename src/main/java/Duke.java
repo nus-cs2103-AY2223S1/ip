80b355c295af6a56a3eee1f885f3ba60729dbca6
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-08-18 09:13:36 +0800

    Level 2 - Added Add, List functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,21 +3,43 @@ import java.util.Scanner;
 public class Duke {
     private static final String greetMessage = "Hello! I'm Duke \nWhat can I do for you?";
     private static final String byeMessage = "Bye. Hope to see you again soon!";
+    private static String[] userTexts = new String[100];
+    private static int index = 0;
     private static void wrapText(String content) {
         System.out.println("-".repeat(57));
         System.out.println(content);
         System.out.println("-".repeat(57));
     }
 
+    private static void appendArray(String inputString) {
+        userTexts[index] = inputString;
+        index++;
+    }
+
+    private static String generateList() {
+        String listInString = "";
+        for (int i = 0; i < index; i++) {
+            listInString += String.valueOf(i) + ". " + userTexts[i];
+            if(i != index - 1) {
+                listInString += "\n";
+            }
+        }
+        return listInString;
+    }
+
     private static void handleUserInputs(Scanner scanner) {
         while(scanner.hasNext()) {
-            String[] inputs = scanner.nextLine().split(" ");
+            String inputString = scanner.nextLine();
+            String[] inputs = inputString.split(" ");
             String input = inputs[0];
             if (input.equals("bye")) {
                 wrapText(byeMessage);
                 break;
+            } else if (input.equals("list")) {
+                wrapText(generateList());
             } else {
-                wrapText(input);
+                appendArray(inputString);
+                wrapText("added: " + inputString);
             }
         }
     }
