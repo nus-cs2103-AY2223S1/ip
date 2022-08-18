52018b8d762d25894751e13f530ca18c32ebb0e5
PeiYee88 <77477618+PeiYee88@users.noreply.github.com>
2022-08-18 18:27:47 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,103 +3,36 @@ import java.util.Scanner;
 public class Duke {
     public static final String line = "____________________________________________________________";
     public Scanner sc = new Scanner(System.in);
-    public static Task[] list = new Task[100];
-    public static int count = 0;
 
     public Duke() {};
 
     public void greet() {
         System.out.println(
                 line + "\n" +
-                "Hello! I'm Duke" + "\n" +
-                "What can I do for you?" + "\n" +
-                line + "\n"
+                        "Hello! I'm Duke" + "\n" +
+                        "What can I do for you?" + "\n" +
+                        line + "\n"
         );
     }
 
     public void respond() {
-        String input = sc.nextLine();
-
-        String[] arr = input.split(" ",2);
-        String command = arr[0];
-        if (command.equals("mark") || command.equals("unmark")) {
-            int index = Integer.parseInt(arr[1]);
-             if (command.equals("mark")) {
-                list[index-1].mark(index);
-            }
-
-            else if (command.equals("unmark")) {
-                Task b = list[index-1];
-                b.unmark(b,index);
-            }
-            respond();
-        }
-
-        else if (command.equals("todo")) {
-            list[count] = new Todo(arr[1]);
-            list[count++].print();
-            respond();
-        }
-
-        else if (command.equals("event")) {
-            String[] deets = arr[1].split("/at",2);
-            list[count] = new Event(deets[0],deets[1]);
-            list[count++].print();
-            respond();
-        }
-
-        else if (command.equals("deadline")) {
-            String[] deets = arr[1].split("/by",2);
-            list[count] = new Deadline(deets[0],deets[1]);
-            list[count++].print();
-            respond();
-        }
-
-
-
-        else if (input.equals(("bye"))) {
-            bye();
-        }
-
-        else if (input.equals("list")) {
-            list();
-            respond();
-        }
-
-
-
-       /* else {
+        String input = sc.next();
+        if (!input.equals("bye")) {
             System.out.println(
                     line + "\n" +
-                           "added: " + input + "\n" + line+ "\n");
-            list[count++] = new Task(input);
+                            input + "\n" + line+ "\n");
             respond();
         }
-
-        */
-
-    }
-
-    public void list() {
-        System.out.println(
-                line + "\n" +
-                "Here are the tasks in your list:");
-
-        for (int i = 0, j = 1; i < count; i++, j++) {
-
-            System.out.print(j + ". ");
-            list[i].list();
+        else {
+            bye();
         }
-        System.out.println(
-                line + "\n"
-        );
 
     }
 
     public void bye() {
         System.out.println(
                 line + "\n" +
-                "Bye. Hope to see you again soon!" + "\n" + line
+                        "Bye. Hope to see you again soon!" + "\n" + line
         );
 
     }
