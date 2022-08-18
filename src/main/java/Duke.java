9a50c0394ee0b1bb7354b744a3556b415f951880
wxy1203 <e0817822@u.nus.edu>
2022-08-18 21:38:13 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,39 @@
 import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Duke {
-    public static void main(String[] args) {
-        start();
 
+
+
+
+    public static void main(String[] args) {
+
+        ArrayList<String> todoList = new ArrayList<String>();
+
+        start();
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
+
         while (true) {
             if (command.equals("bye")) {
                 bye();
                 break;
             }
+            else if (command.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 1; i <= todoList.size(); i++) {
+                    System.out.println("    " + i + ". " + todoList.get(i-1));
+                }
+                System.out.println("    ____________________________________________________________");
+                command = scanner.nextLine();
+            }
             else {
                 System.out.println("    ____________________________________________________________\n" +
-                        "     " +
+                        "     added: " +
                         command +
                         "\n    ____________________________________________________________");
+                todoList.add(command);
                 command = scanner.nextLine();
             }
         }
