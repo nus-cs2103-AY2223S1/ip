9a420b7bdf44aba02da6df2f4ad4d2a134d57fa2
Lin Leyi <>
2022-08-19 00:05:11 +0800

    Level-1 Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = " _______________________________________ \n";
+        System.out.println(line + " I'm Dukie\n" + " What can I do for you?\n" + line);
+
+        boolean noBye = true;
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String userInput = "0";
+
+        while (true) {
+            userInput = myObj.nextLine();  // Read user input
+
+            if (userInput.equals("bye")) {
+                System.out.println("Goodbaiiiii\n");
+                System.exit(0);
+            }
+
+            System.out.println(line + " " + userInput + "\n" + line);  // Output user input
+        }
+
+
     }
 }
