c4878d3299d60a8411b7c9986a39e58a211371f4
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-08-18 11:08:56 +0800

    A-TextUiTesting : Added test cases

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac  -cp C:\Users\user\Desktop\CS2103\ip\src\main\java -Xlint:none -d ..\bin C:\Users\user\Desktop\CS2103\ip\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
