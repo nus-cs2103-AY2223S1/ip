e4e5f85437f93cc66ca18ea87e2f94fd0393d71f
gabyang <gabrielyang@berkeley.edu>
2022-08-18 21:51:20 +0800

    Complete Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,20 +5,26 @@ public class Duke {
         System.out.println("What are your commands sir: ");
         String[] arr = new String[100];
         boolean pred = true;
+        int i = 0;
 
         while(pred){
             if (input.hasNext()){
                 String str = input.nextLine();
 
                 if (str.contains("list")) {
-                    System.out.println("list");
+                    System.out.println("list items -");
+                    for (int j = 0; j < i; j++){
+                        System.out.println("List " + j + ". " + arr[j]);
+                    }
                 } else if (str.contains("blah")) {
                     System.out.println("blah");
                 } else if (str.contains("bye")) {
                     System.out.println("Bye. Hope to see you again");
                     pred = false;
-                }else {
-                    System.out.println("I dont understand lol");
+                } else {
+                    System.out.println("Storing this text in the list");
+                    arr[i] = str;
+                    i++;
                 }
             }
 
