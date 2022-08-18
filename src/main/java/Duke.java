f8812bf0b615a59138e86d447b05a1df20e661c2
darrentde <darrent2599@gmail.com>
2022-08-19 00:08:50 +0800

    Completed level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        String startChat = "Hello! I'm Chadbot\nWhat can I do for you?";
+        String exitChat = "Bye. Hope to see you again soon!";
+
+        System.out.println(formatText(startChat));
+
+        while(true) {
+            String userInput = sc.nextLine();
+            if(userInput.equals("bye")) {
+                break;
+            }else {
+                System.out.println(formatText(userInput));
+            }
+        }
+        System.out.println(formatText(exitChat));
+    }
+    public static String formatText(String text) {
+        String line = "____________________________________________________________";
+        return line +"\n" + text+ "\n" + line;
     }
 }
