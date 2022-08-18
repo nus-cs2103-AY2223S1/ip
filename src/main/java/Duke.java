cc5f90fa002cd767f7767a1391f9da31f861d8c3
Neethesh26 <neethesh26@gmail.com>
2022-08-18 18:59:45 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,31 +4,49 @@ public class Duke {
 
 
     public static void main(String[] args) {
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Scanner in = new Scanner(System.in);
-        int counter = 0;
+
         while(true) {
             String input = in.nextLine();
             String exitCode = "bye";
+            String arr[] = input.split(" ", 2);
             if (input.equals(exitCode)) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
             if (input.equals("list")) {
-                for (int i = 0; i < counter + 1; i++) {
+                for (int i = 0; i < Task.getCount() + 1; i++) {
                     if (tasks[i] == null) {
                         break;
                     }
                     else {
-                        System.out.println((i+1) + ". " + tasks[i]);
+                        System.out.println((i+1) + ". " + tasks[i].toString());
                     }
                 }
             }
-            else {
-                tasks[counter] = input;
-                counter++;
+            else if (arr[0].equals("mark")){
+                int i = Integer.parseInt(arr[1]);
+                if (i  <= Task.getCount()) {
+                    tasks[i - 1].complete();
+                    System.out.println("Nice! I have marked this task as done: ");
+                    System.out.println(tasks[i - 1]);
+                } else {
+                    System.out.println("Index does not exist");
+                }
+            } else if (arr[0].equals("unmark")) {
+                int i = Integer.parseInt(arr[1]);
+                if (i <= Task.getCount()) {
+                    tasks[i - 1].incomplete();
+                    System.out.println("OK, I have marked this task as not done yet: ");
+                    System.out.println(tasks[i - 1]);
+                } else {
+                    System.out.println("Index does not exist");
+                }
+            } else {
+                tasks[Task.getCount()] = new Task(input);
                 System.out.println("added: " + input);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,36 @@
+public class Task {
+    private String taskName;
+    private boolean completed;
+    private static int taskCounter = 0;
+
+    public Task(String name) {
+        this.taskName = name;
+        this.completed = false;
+        taskCounter++;
+    }
+
+    public void complete() {
+        this.completed = true;
+    }
+
+    public void incomplete() {
+        this.completed = false;
+    }
+
+    public static int getCount() {
+        return taskCounter;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    @Override
+    public String toString() {
+        return (this.completed ? "[X] " : "[ ] ") + this.taskName;
+    }
+}
