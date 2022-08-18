2ad4f4f7b7439bab2c5008526671f6e835a828e9
Brandon <brandon.thio@yahoo.com.sg>
2022-08-18 17:36:54 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,18 @@ public class Duke {
     private static String WELCOME_MESSAGE =  "Hello! I'm Duke\n" + "What can I do for you?";
     private static String GOODBYE_MESSAGE =  "Bye. Hope to see you again soon!";
 
+    private String[] inputs = new String[100];
+    private int numInputsStored = 0;
+
+    private void store(String input) {
+        this.inputs[numInputsStored++] = input;
+        System.out.println(String.format("added: %s", input));
+    }
+    private void getInputs() {
+        for (int i = 0; i < this.numInputsStored; i++) {
+            System.out.println(String.format("%d. %s", i+1, this.inputs[i]));
+        }
+    }
 
     /**
     * Note: You are strongly encouraged to customize the chatbot name,
@@ -14,6 +26,7 @@ public class Duke {
         System.out.println(WELCOME_MESSAGE);
 
         Scanner scanner = new Scanner(System.in);
+        Duke duke = new Duke();
 
         while (true) {
 
@@ -24,7 +37,13 @@ public class Duke {
                 break;
             }
 
-            System.out.println(input);
+            if (input.equals("list")) {
+                duke.getInputs();
+                continue;
+            }
+
+            duke.store(input);
+
         }
 
         scanner.close();
