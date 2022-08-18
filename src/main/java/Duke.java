9ffd416e6f99eff0b12db5e7cb38cb5276cfb69b
glemenneo <glemenneo@gmail.com>
2022-08-18 14:43:40 +0800

    closed_scanner_for_Level-1_plus_done_Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.util.ArrayList; 
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,7 +9,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        Duke.echo();
+        Duke.addList();
     }
 
     public static void echo() {
@@ -17,6 +18,7 @@ public class Duke {
         System.out.println("\t____________________________________________________________");
         Scanner scanner = new Scanner(System.in);
         String input = "";
+
         while (true) {
             input = scanner.nextLine();
             if (input.toLowerCase().equals("bye")) {
@@ -26,9 +28,45 @@ public class Duke {
             System.out.println("\t" + input);
             System.out.println("\t____________________________________________________________");
         }
+
         System.out.println("\t____________________________________________________________");
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t____________________________________________________________");
+        scanner.close();
+        return;
+    }
+
+    public static void addList() {
+        ArrayList<String> list = new ArrayList<>();
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke\n \tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.toLowerCase().equals("bye")) {
+                break;
+            }
+            if (input.toLowerCase().equals("list")) {
+                System.out.println("\t____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(String.format("\t%d: %s", i + 1, list.get(i)));
+                }
+                System.out.println("\t____________________________________________________________");
+                continue;
+            }
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\tadded: " + input);
+            System.out.println("\t____________________________________________________________");
+            list.add(input);
+        }
+        
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+        scanner.close();
         return;
     }
 }
