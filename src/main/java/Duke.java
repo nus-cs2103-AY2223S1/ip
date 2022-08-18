d7f747c6a56a112a14ad7b20b944d2b9ba47fe0d
XenonShawn <57314121+XenonShawn@users.noreply.github.com>
2022-08-18 18:27:46 +0800

    Skeletal version of Duke - simply echos commands until 'bye' is received

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static String line = "____________________________________________________________\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        Scanner scanner = new Scanner(System.in);
+
+        String logo = line
+                + " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|____/ \\__,_|_|\\_\\___|\n"
+                + "\nWhat can I do for you?\n"
+                + line;
         System.out.println("Hello from\n" + logo);
+
+        String text = "";
+
+        do {
+            text = scanner.nextLine();
+
+            System.out.println(line);
+            switch (text) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                default:
+                    System.out.println(text);
+                    break;
+            }
+            System.out.println(line);
+        } while (!text.equals("bye"));
     }
 }
