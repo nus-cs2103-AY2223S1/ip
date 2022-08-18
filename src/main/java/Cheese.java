63af2a9dc895467bcde42533f0f28a2a6ed583ce
Jason Qiu <jason_qiu@hotmail.com>
2022-08-18 16:10:35 +0800

    refactor: use switch statements to determine command

--- src/main/java/Cheese.java
+++ src/main/java/Cheese.java
@@ -6,68 +6,74 @@ public class Cheese {
   public static ArrayList<Task> list = new ArrayList<Task>();
 
   public static void main(String[] args) {
-    String greeting = new String("Woof! I'm Cheese, your puppy assistant.\n"
-            + "What can I do for you?");
-    System.out.println(greeting);
-
+    Cheese.greet();
     Cheese.chat();
   }
 
-  public static void chat() {
+  private static void greet() {
+    String greeting = new String("Woof! I'm Cheese, your puppy assistant.\n"
+        + "What can I do for you?");
+    System.out.println(greeting);
+  }
+
+  private static void chat() {
     Scanner scanner = new Scanner(System.in);
     String userInput;
 
-    while(true) {
+    while (true) {
       System.out.println(BORDER);
       userInput = scanner.nextLine();
       System.out.println(BORDER);
 
-      if (userInput.equals("bye")) {
-        System.out.println("Going so soon? :') Bye");
-        scanner.close();
-        break;
-      }
+      String[] inputArray = userInput.split(" ", 2);
+      String command = inputArray[0];
 
-      if (userInput.equals("list")) {
-        Cheese.printList();
-      } else if (userInput.indexOf("mark") == 0) {
-        int itemNum = Integer.parseInt(userInput.substring(5));
-        if (notInListRange(itemNum)) {
-          System.out.println("Invalid list item number.");
-          continue;
-        }
-        Cheese.list.get(itemNum - 1).markAsDone();
-        System.out.println("Paw-some! Another task done!");
-        System.out.println(Cheese.list.get(itemNum - 1));
-      } else if (userInput.indexOf("unmark") == 0) {
-        int itemNum = Integer.parseInt(userInput.substring(7));
-        if (notInListRange(itemNum)) {
-          System.out.println("Invalid list item number.");
-          continue;
-        }
-        Cheese.list.get(itemNum - 1).markAsNotDone();
-        System.out.println("Okay, I've marked this task as not done yet.");
-        System.out.println(Cheese.list.get(itemNum - 1));
-      } else if (userInput.indexOf("todo") == 0){
-        String description = userInput.substring(5);
-        Task task = new Todo(description);
-        Cheese.addTask(task);
-      } else if (userInput.indexOf("deadline") == 0 && userInput.indexOf("/by") != -1) {
-        int byFlagIndex = userInput.indexOf("/by");
-        String description = userInput.substring(9, byFlagIndex - 1);
-        String deadline = userInput.substring(byFlagIndex + 4);
-        Task task = new Deadline(description, deadline);
-        Cheese.addTask(task);
-      } else if (userInput.indexOf("event") == 0 && userInput.indexOf("/at") != -1) {
-        int atFlagIndex = userInput.indexOf("/at");
-        String description = userInput.substring(6, atFlagIndex - 1);
-        String timeInterval = userInput.substring(atFlagIndex + 4);
-        Task task = new Event(description, timeInterval);
-        Cheese.addTask(task);
-      } else {
-        System.out.println("Sowwy, I don't understand");
+      switch (command) {
+        case "bye":
+          System.out.println("Going so soon? :') Bye");
+          scanner.close();
+          return;
+        case "list":
+          Cheese.printList();
+          break;
+        case "mark":
+          Cheese.markTaskAsDone(Integer.parseInt(inputArray[1]));
+          break;
+        case "unmark":
+          Cheese.markTaskAsUndone(Integer.parseInt(inputArray[1]));
+          break;
+        case "todo":
+          Task todo = new Todo(inputArray[1]);
+          Cheese.addTask(todo);
+          break;
+        case "deadline":
+          String[] deadlineInputArray = inputArray[1].split(" /by ", 2);
+          Task deadline = new Deadline(deadlineInputArray[0], deadlineInputArray[1]);
+          Cheese.addTask(deadline);
+          break;
+        case "event":
+          String[] eventInputArray = inputArray[1].split(" /at ", 2);
+          Task event = new Event(eventInputArray[0], eventInputArray[1]);
+          Cheese.addTask(event);
+          break;
+        default:
+          System.out.println("Sowwy, I don't understand");
       }
     }
+  }
+
+  private static void markTaskAsDone(int itemNum) {
+    Task selectedTask = Cheese.list.get(itemNum - 1);
+    selectedTask.markAsDone();
+    System.out.println("Paw-some! Another task done!");
+    System.out.println(selectedTask);
+  }
+
+  private static void markTaskAsUndone(int itemNum) {
+    Task selectedTask = Cheese.list.get(itemNum - 1);
+    selectedTask.markAsNotDone();
+    System.out.println("Okay, I've marked this task as not done yet.");
+    System.out.println(selectedTask);
   }
 
   private static void addTask(Task task) {
@@ -81,7 +87,7 @@ public class Cheese {
     return itemNum < 1 || itemNum > Cheese.list.size();
   }
 
-  public static void printList() {
+  private static void printList() {
     for (int i = 1; i <= Cheese.list.size(); i++) {
       System.out.println(i + ". " + Cheese.list.get(i - 1));
     }
