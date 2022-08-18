7a3024a0f2fa532b7ed8e762ff3a3844b311299f
Ryan Chua Zong Xun <e0725636@u.nus.edu>
2022-08-18 17:56:13 +0800

    changed file name and class name to Fred, did Greet, Echo, and Exit

---
+++ src/main/java/Fred.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fred {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        System.out.println("Fred: Hello! I'm Fred!");
+        System.out.println("Fred: What can I do for you?");
+
+        while (true) {
+            System.out.print("Player: ");
+            input = scanner.next();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println("Fred: " + input);
+            }
+        }
+
+        scanner.close();
+        System.out.println("Fred: Bye. Hope to see you again soon!");
+    }
+}
