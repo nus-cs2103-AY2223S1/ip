bc1a8141734b19e2f7da136cdb063272bec6a7fb
Rexong <rexonghanwei200035@gmail.com>
2022-08-18 21:39:31 +0800

    Implemented Level-6

---
+++ src/main/java/NoSuchCommandException.java
@@ -0,0 +1,5 @@
+public class NoSuchCommandException extends DukeException {
+    public NoSuchCommandException() {
+        super("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+    }
+}
