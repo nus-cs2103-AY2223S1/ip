27172fa48295e5a75ff6e109aa20e6ac0d1afe50
danzzzerl <danzzzerl@gmail.com>
2022-08-18 18:29:22 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,171 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        // print out starting message
+    static int counter = 0;
+
+    /**
+     * Initialises counter back to 0.
+     */
+    public static void initialiseCounter() {
+        counter = 0;
+    }
+
+
+    /**
+     * Prints the starting message.
+     */
+    public static void sayGreeting() {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+    }
+
+
+    /**
+     * Prints the end message.
+     */
+    public static void sayGoodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+
+    public static void printList(Task[] arr) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                System.out.println(i + ". " + arr[i]);
+            }
+        }
+    }
+
+
+    public static void markTaskAsDone(Task[] arr, int index) {
+        if (arr[index] != null) {
+            Task currTask = arr[index];
+            currTask.markAsDone();
+        }
+    }
+
+
+    public static void unmarkTask(Task[] arr, int index) {
+        if (arr[index] != null) {
+            Task currTask = arr[index];
+            currTask.markUndone();
+        }
+    }
+
+
+    public static void createNewTask(String firstWord, String[] strArray, Task[] inputArray) throws DukeException {
+        if (firstWord.equals("todo")) {
+            // throw exception if no word after to-do
+            if (strArray.length < 2) {
+                throw new DukeException("The description of a todo cannot be empty.");
+            }
+
+            StringBuilder todoStr = new StringBuilder();
+            for (int i = 1; i < strArray.length; i++) {
+                todoStr.append(" ").append(strArray[i]);
+            }
+            counter++;
+            inputArray[counter] = new Todo(todoStr.toString());
+            System.out.println("Got it. I've added this task:\n  " + inputArray[counter] +
+                    "\nNow you have " + counter + " tasks in the list.");
+
+        } else if (firstWord.equals("deadline")) {
+            // throw exception if no word after deadline
+            if (strArray.length < 2) {
+                throw new DukeException("The description of a deadline cannot be empty.");
+            }
+
+            // throw exception if no deadline time
+            int indexCheck = 1000;
+            for (int i = 1; i < strArray.length; i++) {
+                if (strArray[i].equals("/by")) {
+                    indexCheck = i;
+                }
+            }
+            if (indexCheck == 1000) {
+                throw new DukeException("This description needs a timing! Add again with /by followed by the deadline timing.");
+            }
+
+            // create deadline string and deadline
+            StringBuilder deadlineStr = new StringBuilder();
+            StringBuilder deadline = new StringBuilder();
+            for (int i = 1; i < strArray.length; i++) {
+                if (strArray[i].equals("/by")) {
+                    break;
+                } else {
+                    deadlineStr.append(" ");
+                    deadlineStr.append(strArray[i]);
+                }
+            }
+            for (int i = 1; i < strArray.length; i++) {
+                if (strArray[i].equals("/by")) {
+                    for (int j = i + 1; j < strArray.length; j++) {
+                        deadline.append(" ");
+                        deadline.append(strArray[j]);
+                    }
+                    break;
+                }
+            }
+            counter++;
+            inputArray[counter] = new Deadline(deadlineStr.toString(), deadline.toString());
+            System.out.println("Got it. I've added this task:\n  " + inputArray[counter] +
+                    "\nNow you have " + counter + " tasks in the list.");
+
+        } else if (firstWord.equals("event")) {
+            // throw exception if no word after event
+            if (strArray.length < 2) {
+                throw new DukeException("The description of an event cannot be empty.");
+            }
+
+            // throw exception if no event time
+            int indexCheck = 1000;
+            for (int i = 1; i < strArray.length; i++) {
+                if (strArray[i].equals("/at")) {
+                    indexCheck = i;
+                }
+            }
+            if (indexCheck == 1000) {
+                throw new DukeException("This description needs a timing! Add again with /at followed by the deadline timing.");
+            }
+
+            // create event string and deadline
+            StringBuilder eventStr = new StringBuilder();
+            StringBuilder eventTime = new StringBuilder();
+            for (int i = 1; i < strArray.length; i++) {
+                if (strArray[i].equals("/at")) {
+                    break;
+                } else {
+                    eventStr.append(" ");
+                    eventStr.append(strArray[i]);
+                }
+            }
+            for (int i = 1; i < strArray.length; i++) {
+                if (strArray[i].equals("/at")) {
+                    for (int j = i + 1; j < strArray.length; j++) {
+                        eventTime.append(" ");
+                        eventTime.append(strArray[j]);
+                    }
+                    break;
+                }
+            }
+            counter++;
+            inputArray[counter] = new Event(eventStr.toString(), eventTime.toString());
+            System.out.println("Got it. I've added this task:\n  " + inputArray[counter] +
+                    "\nNow you have " + counter + " tasks in the list.");
+
+        }
+    }
+
+
+    public static void main(String[] args) throws DukeException {
+        // initialise counter
+        initialiseCounter();
+
+        // print out starting message
+        sayGreeting();
 
         // initialise variables
-        Task inputArray[] = new Task[100];
-        int counter = 0;
+        Task[] inputArray = new Task[100];
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -16,105 +174,43 @@ public class Duke {
             String[] arrOfInput = input.split(" ");
             String firstWord = arrOfInput[0];
 
-            if (firstWord.equals("bye")) {
-                // end program when input is bye
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
+            try {
+                if (firstWord.equals("bye")) {
+                    // end program when input is bye
+                    sayGoodbye();
+                    break;
 
-            } else if (firstWord.equals("list")) {
-                // list out the current list
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < inputArray.length; i++) {
-                    if (inputArray[i] != null) {
-                        System.out.println(i + ". " + inputArray[i]);
-                    }
+                } else if (firstWord.equals("list")) {
+                    // list out the current list
+                    printList(inputArray);
+
+                } else if (firstWord.equals("mark")) {
+                    // to mark an element as done
+                    int index = Integer.parseInt(arrOfInput[1]);
+                    markTaskAsDone(inputArray, index);
+
+                } else if (firstWord.equals("unmark")) {
+                    // to mark an element as undone
+                    int index = Integer.parseInt(arrOfInput[1]);
+                    unmarkTask(inputArray, index);
+
+                } else if (firstWord.equals("todo")) {
+                    // adding the to-do to the list
+                    createNewTask(firstWord, arrOfInput, inputArray);
+
+                } else if (firstWord.equals("deadline")) {
+                    // adding the deadline to the list
+                    createNewTask(firstWord, arrOfInput, inputArray);
+
+                } else if (firstWord.equals("event")) {
+                    // adding the event to the list
+                    createNewTask(firstWord, arrOfInput, inputArray);
+
+                } else {
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
                 }
-
-            } else if (firstWord.equals("mark")) {
-                // to mark an element as done
-                int index = Integer.parseInt(arrOfInput[1]);
-                if (inputArray[index] != null) {
-                    Task currTask = inputArray[index];
-                    currTask.markAsDone();
-                }
-
-            } else if (firstWord.equals("unmark")) {
-                // to mark an element as undone
-                int index = Integer.parseInt(arrOfInput[1]);
-                if (inputArray[index] != null) {
-                    Task currTask = inputArray[index];
-                    currTask.markUndone();
-                }
-
-            } else if (firstWord.equals("todo")) {
-                // adding the to-do to the list
-                String todoStr = "";
-                for (int i = 1; i < arrOfInput.length; i++) {
-                    todoStr += " " + arrOfInput[i];
-                }
-                counter++;
-                inputArray[counter] = new Todo(todoStr);
-                System.out.println("Got it. I've added this task:\n  " + inputArray[counter] +
-                        "\nNow you have " + counter + " tasks in the list.");
-
-            } else if (firstWord.equals("deadline")) {
-                // adding the deadline to the list
-
-                // create deadline string and deadline
-                StringBuilder deadlineStr = new StringBuilder();
-                StringBuilder deadline = new StringBuilder();
-                for (int i = 1; i < arrOfInput.length; i++) {
-                    if (arrOfInput[i].equals("/by")) {
-                        break;
-                    } else {
-                        deadlineStr.append(" ");
-                        deadlineStr.append(arrOfInput[i]);
-                    }
-                }
-                for (int i = 1; i < arrOfInput.length; i++) {
-                    if (!arrOfInput[i].equals("/by")) {
-                    } else {
-                        for (int j = i + 1; j < arrOfInput.length; j++) {
-                            deadline.append(" ");
-                            deadline.append(arrOfInput[j]);
-                        }
-                        break;
-                    }
-                }
-                counter++;
-                inputArray[counter] = new Deadline(deadlineStr.toString(), deadline.toString());
-                System.out.println("Got it. I've added this task:\n  " + inputArray[counter] +
-                        "\nNow you have " + counter + " tasks in the list.");
-
-            } else if (firstWord.equals("event")) {
-                // adding the event to the list
-
-                // create event string and deadline
-                StringBuilder eventStr = new StringBuilder();
-                StringBuilder eventTime = new StringBuilder();
-                for (int i = 1; i < arrOfInput.length; i++) {
-                    if (arrOfInput[i].equals("/at")) {
-                        break;
-                    } else {
-                        eventStr.append(" ");
-                        eventStr.append(arrOfInput[i]);
-                    }
-                }
-                for (int i = 1; i < arrOfInput.length; i++) {
-                    if (!arrOfInput[i].equals("/at")) {
-                    } else {
-                        for (int j = i + 1; j < arrOfInput.length; j++) {
-                            eventTime.append(" ");
-                            eventTime.append(arrOfInput[j]);
-                        }
-                        break;
-                    }
-                }
-                counter++;
-                inputArray[counter] = new Event(eventStr.toString(), eventTime.toString());
-                System.out.println("Got it. I've added this task:\n  " + inputArray[counter] +
-                        "\nNow you have " + counter + " tasks in the list.");
-
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,5 @@
+public class DukeException extends Exception {
+    public DukeException(String errorMessage) {
+        super("☹ OOPS!!! " + errorMessage);
+    }
+}
