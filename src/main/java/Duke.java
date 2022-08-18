f91d81351e5d366165b80802988e722e5f775cb0
Teh Yi Kuang <e0544398@u.nus.edu>
2022-08-18 15:43:34 +0800

    Added mark and unmark functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,30 +18,53 @@ public class Duke {
         System.out.println(logo + LINE + "\n" + WELCOMEMESSAGE + LINE);
 
         // ArrayList to store user inputs
-        List<String> listOfInputs = new ArrayList<>();
+        List<Task> listOfInputs = new ArrayList<>();
+        // Array to parse the input content
+        String[] inputArray = null;
 
         // Echo user input
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        while (!input.equalsIgnoreCase("bye")) { // Stops loop if command is 'bye'
+        while (!input.equalsIgnoreCase(EXITCOMMAND)) { // Stops loop if command is 'bye'
+            inputArray = input.split(" ");
             // Check if user input is 'list'
             if (input.equalsIgnoreCase("list")) { // List all inputs typed so far
                 if (listOfInputs.size() == 0) { // List is empty
-                    System.out.println(LINE + "\nYour list is empty! Why not add something to it first?" + LINE);
+                    System.out.println(LINE + "\nYour list is empty! Why not add a task to it first?" + LINE);
                     input = sc.nextLine();
                     continue;
                 } else {
-                    System.out.println(LINE);
+                    System.out.println(LINE + "\nHere are the tasks in your list:");
                     for (int i = 0; i < listOfInputs.size(); i++) {
-                        System.out.println((i + 1) + ". " + listOfInputs.get(i));
+                        Task curTask = listOfInputs.get(i);
+                        System.out.println((i + 1) + ".[" + curTask.getStatusIcon() + "] " + curTask.getDescription());
                     }
                     System.out.println(LINE);
                 }
                 input = sc.nextLine();
                 continue;
+            // Check if user input is to mark a task
+            } else if (inputArray.length == 2 & inputArray[0].equalsIgnoreCase("mark")) {
+                int taskNum = Integer.parseInt(inputArray[1]) - 1;
+                Task taskChosen = listOfInputs.get(taskNum);
+                taskChosen.markAsDone();
+                System.out.println(LINE + "\nOkay, I have marked this task as done: \n"
+                        + "[" + taskChosen.getStatusIcon() + "] " + taskChosen.getDescription() + LINE);
+                input = sc.nextLine();
+                continue;
+            // Check if user input is to unmark a task
+            } else if (inputArray.length == 2 & inputArray[0].equalsIgnoreCase("unmark")) {
+                int taskNum = Integer.parseInt(inputArray[1]) - 1;
+                Task taskChosen = listOfInputs.get(taskNum);
+                taskChosen.markAsUndone();
+                System.out.println(LINE + "\nOkay, I have marked this task as not done: \n"
+                        + "[" + taskChosen.getStatusIcon() + "] " + taskChosen.getDescription() + LINE);
+                input = sc.nextLine();
+                continue;
             }
 
-            listOfInputs.add(input);
+            Task newTask = new Task(input);
+            listOfInputs.add(newTask);
             System.out.println(LINE + "\nYou have added: " + input + LINE);
             input = sc.nextLine();
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    // Getters for task
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+    public String getDescription() {
+        return description;
+    }
+    // Setters for task
+    public void markAsDone() {
+        this.isDone = true;
+    }
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+}
