bba21b592e8f2b9a95320555d5f4e9c4df153496
fongyj <fongyj@hotmail.com>
2022-08-18 22:51:27 +0800

    Add todo, event and deadline tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,27 +22,51 @@ public class Duke {
 
     private static int handleMessage(String m) {
         if (m.equals("bye")) {
+            // bye
             printMessage("Bye. Hope to see you again soon!");
             return -1;
         } else if (m.equals("list")) {
+            // list
             String messageList = generateList();
-            printMessage(messageList);
+            printMessage("Here are the tasks in your list:\n    " + messageList);
             return 1;
         } else if (m.length() > 5 && m.startsWith("mark")) {
+            // mark task
             int id = Integer.parseInt(m.substring(5));
             Task t = Duke.taskList.get(id - 1);
             t.done();
             printMessage("Nice! I've marked this task as done:\n    " + t);
             return 2;
         } else if (m.length() > 7 && m.startsWith("unmark")) {
+            // unmark task
             int id = Integer.parseInt(m.substring(7));
             Task t = Duke.taskList.get(id - 1);
             t.undone();
             printMessage("OK, I've marked this task as not done yet:\n    " + t);
             return 3;
+        } else if (m.length() > 9 && m.startsWith("deadline")) {
+            // deadline
+            m = m.substring(9);
+            String[] split = m.split(" /by ");
+            Task t = new Deadline(split[0], split[1]);
+            addTask(t);
+            return 4;
+        } else if (m.length() > 6 && m.startsWith("event")) {
+            // event
+            m = m.substring(6);
+            String[] split = m.split(" /at ");
+            Task t = new Event(split[0], split[1]);
+            addTask(t);
+            return 5;
+        } else if (m.length() > 5 && m.startsWith("todo")) {
+            // to do
+            m = m.substring(5);
+            Task t = new Todo(m);
+            addTask(t);
+            return 6;
         } else {
-            storeMessage(m);
-            printMessage("added: " + m);
+            Task t = new Task(m);
+            addTask(t);
             return 100;
         }
     }
@@ -52,10 +76,6 @@ public class Duke {
                 + m
                 + "\n    ____________________________________________________________\n";
         System.out.println(message);
-    }
-
-    private static void storeMessage(String m) {
-        Duke.taskList.add(new Task(m));
     }
 
     private static String generateList() {
@@ -68,5 +88,14 @@ public class Duke {
             out += "\n    " + count++ + ". " + t;
         }
         return out.substring(5);
+    }
+
+    private static void addTask(Task t) {
+        Duke.taskList.add(t);
+        printMessage("Got it. I've added this task:\n      " +
+                t +
+                "\n    Now you have " +
+                Duke.taskList.size() +
+                " tasks in the list.");
     }
 }
