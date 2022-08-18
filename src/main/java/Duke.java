3d402ebba73446410826c45539957166b7b9048a
Nguyen Quy Duc <quyduc711@gmail.com>
2022-08-18 16:39:58 +0800

    finish Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+    private boolean inNeed = true;
+    private ArrayList<String> list = new ArrayList<>(100);
+
+    private void greet() {
+        generateReply("Hello! I'm Sheep\n" +
+                "What can I do for you?");
+    }
+
+    private void generateReply(String reply) {
+        System.out.println("____________________________________________________________\n" +
+                reply +
+                "\n____________________________________________________________\n");
+    }
+
+    private void parseMessage(String message) {
+        String[] command = message.split(" ", 2);
+        String commandKey = command[0];
+        switch (commandKey) {
+            case "bye":
+                this.generateReply("Bye. Hope to see you again soon!");
+                this.inNeed = false;
+                break;
+            default:
+                this.list.add(message);
+                this.generateReply("added: " + message);
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.greet();
+        Scanner scanner = new Scanner(System.in);
+        while (duke.inNeed) {
+            String message = scanner.nextLine();
+            duke.parseMessage(message);
+        }
     }
 }
