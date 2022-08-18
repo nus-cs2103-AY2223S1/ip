e06c66ebf15ca4fd6aecff966e3b75405fed551b
sh4nH <e0866259@u.nus.edu>
2022-08-18 12:43:27 +0800

    Added Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,52 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    private final static String horLine = "--------------------------------------";
+    public  Duke(){
+
+    }
+    ArrayList<Task> tasks = new ArrayList<>();
+    private final static String horLine = "------------------------------------------------------";
+    private final static String welcomeMsg = horLine + "\nHello! I'm Duke\nWhat can I do for you?\n" + horLine;
+
+    private void addToList(Task task) {
+        tasks.add(task);
+    }
 
 
-    private static void userInput() {
+    private void userInput() {
+        System.out.println(welcomeMsg);
         Scanner userCommand = new Scanner(System.in);
+        String input = userCommand.nextLine();
 
-        while (!userCommand.hasNext("bye")) {
-            String text = userCommand.nextLine();
 
-            System.out.println(horLine + "\n\t" + text + "\n" + horLine);
+        while (!input.equals("bye")) {
+            if(!input.equals("list")) {
+                Task newTask = new Task(input);
+                addToList(newTask);
+                System.out.println(horLine + "\n\tI have added " + "'" + input + "'" +
+                        " to the list" + "\n" + horLine);
+            } else {
+                System.out.println(horLine + "\n");
+                viewList();
+                System.out.println("\n" + horLine);
+            }
+            input = userCommand.nextLine();
+
         }
         System.out.println(horLine + "\n\tBye. Hope to see you again soon!\n" + horLine);
 
     }
 
+    private void viewList() {
+        for(int i = 0; i<tasks.size(); i++) {
+            int num = i+1;
+            System.out.println("\t" + num + ". " + tasks.get(i).taskName );
+        }
 
+    }
     public static void main(String[] args) {
-        System.out.println(horLine + "\nHello! I'm Duke\nWhat can I do for you?\n" + horLine);
-        userInput();
+        Duke duke = new Duke();
+        duke.userInput();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,6 @@
+public class Task {
+    public String taskName;
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+}
