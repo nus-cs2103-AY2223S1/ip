39eb0d5267d71c3a082133b6a50c8df1b3c42b15
waynezsy <waynezsy0317@gmail.com>
2022-08-18 20:12:50 +0800

    Duke class: handles errors from incorrect inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.lang.StringBuilder;
 import java.util.Scanner;
 
 /**
@@ -8,20 +7,146 @@ public class Duke {
     private static final String INDENTATION = "    ";
     private static final String LINE = INDENTATION
             + "____________________________________________________________";
-    private static final String LOGO = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String LOGO = INDENTATION + " ____        _        \n"
+            + INDENTATION + "|  _ \\ _   _| | _____ \n"
+            + INDENTATION + "| | | | | | | |/ / _ \\\n"
+            + INDENTATION + "| |_| | |_| |   <  __/\n"
+            + INDENTATION + "|____/ \\__,_|_|\\_\\___|";
 
     private static Task[] tasks = new Task[100];
     private static int taskCount = 0;
+    private static boolean isBye;
 
     /**
      * Prints an indented horizontal line.
      */
     private static void printLine() {
         System.out.println(LINE);
+    }
+
+    /**
+     * Prints an error message from Duke.
+     *
+     * @param message The message that describes the error with Duke.
+     */
+    private static void printError(String message) {
+        printLine();
+        System.out.println(INDENTATION + "☹ OOPS!!! " + message);
+        printLine();
+    }
+
+    /**
+     * Executes the user command specified to Duke.
+     *
+     * @param input The command specified to Duke.
+     * @throws DukeException if input is not a valid Duke command.
+     */
+    private static void execute(String input) throws DukeException {
+        if (input.equals("bye")) {
+            isBye = true;
+        } else if (input.equals("list")) {
+            displayList();
+        } else {
+            String[] strings = input.split(" ");
+
+            if (strings[0].equals("mark")) {
+                try {
+                    if (strings.length > 2) {
+                        printError("Only a single positive integer should follow mark.");
+                    } else {
+                        markTask(Integer.parseInt(strings[1]));
+                    }
+                } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+                    printError("Specify which task to mark with a positive integer.");
+                } catch (NullPointerException | DukeException e) {
+                    printError("The specified task does not exist.");
+                }
+            } else if (strings[0].equals("unmark")) {
+                try {
+                    if (strings.length > 2) {
+                        printError("Only a single positive integer should follow unmark.");
+                    } else {
+                        unmarkTask(Integer.parseInt(strings[1]));
+                    }
+                } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+                    printError("Specify which task to unmark with a positive integer.");
+                } catch (NullPointerException | DukeException e) {
+                    printError("The specified task does not exist.");
+                }
+            } else if (strings[0].equals("todo")) {
+                try {
+                    addTodo(input);
+                } catch (StringIndexOutOfBoundsException e) {
+                    printError("The description of a todo cannot be empty.");
+                }
+            } else if (strings[0].equals("deadline")) {
+                try {
+                    addDeadline(input);
+                } catch (StringIndexOutOfBoundsException | DukeException e) {
+                    printError("The description of a deadline cannot be empty.");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    printError("Use /by to provide when a deadline must be completed.");
+                }
+            } else if (strings[0].equals("event")) {
+                try {
+                    addEvent(input);
+                } catch (StringIndexOutOfBoundsException | DukeException e) {
+                    printError("The description of an event cannot be empty.");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    printError("Use /at to provide when an event occurs.");
+                }
+            } else {
+                throw new DukeException();
+            }
+        }
+    }
+
+    /**
+     * Displays the list of tasks stored by Duke.
+     */
+    private static void displayList() {
+        printLine();
+        System.out.println(INDENTATION + "Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println(INDENTATION + (i + 1) + "." + tasks[i]);
+        }
+        printLine();
+    }
+
+    /**
+     * Marks a task as done.
+     *
+     * @param n The number index of the task as displayed in the list.
+     */
+    private static void markTask(int n) throws DukeException {
+        if (n > 100) {
+            throw new DukeException();
+        }
+
+        Task specifiedTask = tasks[n - 1];
+        specifiedTask.markAsDone();
+        printLine();
+        System.out.println(INDENTATION + "Nice! I've marked this task as done:\n"
+                + INDENTATION + "  " + specifiedTask);
+        printLine();
+    }
+
+    /**
+     * Marks a task as not done.
+     *
+     * @param n The number index of the task as displayed in the list.
+     */
+    private static void unmarkTask(int n) throws DukeException {
+        if (n > 100) {
+            throw new DukeException();
+        }
+
+        Task specifiedTask = tasks[n - 1];
+        specifiedTask.unmarkAsDone();
+        printLine();
+        System.out.println(INDENTATION + "OK, I've marked this task as not done yet:\n"
+                + INDENTATION + "  " + specifiedTask);
+        printLine();
     }
 
     /**
@@ -44,18 +169,44 @@ public class Duke {
     }
 
     /**
-     * Checks if a given string value represents an integer.
+     * Adds a new Todo task to Duke.
      *
-     * @param s The specified string value to check.
-     * @return true if the string represents an integer.
+     * @param input The command specified to Duke.
      */
-    private static boolean isInteger(String s) {
-        try {
-            Integer.parseInt(s);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
+    private static void addTodo(String input) {
+        addTask(new Todo(input.substring(5)));
+    }
+
+    /**
+     * Adds a new Deadline task to Duke.
+     *
+     * @param input The command specified to Duke.
+     * @throws DukeException when command provides deadline by which
+     *     task must complete but no description.
+     */
+    private static void addDeadline(String input) throws DukeException {
+        input = input.substring(9);
+        if (input.startsWith("/by")) {
+            throw new DukeException();
         }
+        String[] strings = input.split(" /by ");
+        addTask(new Deadline(strings[0], strings[1]));
+    }
+
+    /**
+     * Adds a new Event task to Duke.
+     *
+     * @param input The command specified to Duke.
+     * @throws DukeException when command provides time at which
+     *     event occurs but no description.
+     */
+    private static void addEvent(String input) throws DukeException {
+        input = input.substring(6);
+        if (input.startsWith("/at")) {
+            throw new DukeException();
+        }
+        String[] strings = input.split(" /at ");
+        addTask(new Event(strings[0], strings[1]));
     }
 
     /**
@@ -64,6 +215,8 @@ public class Duke {
      * @param args The command line arguments.
      */
     public static void main(String[] args) {
+        isBye = false;
+
         System.out.println(INDENTATION + "Hello from\n" + LOGO);
         printLine();
         System.out.println(INDENTATION + "Hello! I'm Duke\n"
@@ -71,62 +224,16 @@ public class Duke {
         printLine();
 
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLine()) {
-            String input = sc.nextLine();
-
-            if (input.equals("bye")) {
-                printLine();
-                System.out.println(INDENTATION + "Bye. Hope to see you again soon!");
-                break;
-
-            } else if (input.equals("list")) {
-                printLine();
-                System.out.println(INDENTATION + "Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(INDENTATION + (i + 1) + "." + tasks[i]);
-                }
-                printLine();
-
-            } else {
-                String[] strings = input.split(" ");
-
-                if (strings[0].equals("mark") && strings.length == 2 && isInteger(strings[1])) {
-                    Task specifiedTask = tasks[Integer.parseInt(strings[1]) - 1];
-                    specifiedTask.markAsDone();
-                    printLine();
-                    System.out.println(INDENTATION + "Nice! I've marked this task as done:\n"
-                            + INDENTATION + "  " + specifiedTask);
-                    printLine();
-
-                } else if (strings[0].equals("unmark") && strings.length == 2 && isInteger(strings[1])) {
-                    Task specifiedTask = tasks[Integer.parseInt(strings[1]) - 1];
-                    specifiedTask.unmarkAsDone();
-                    printLine();
-                    System.out.println(INDENTATION + "OK, I've marked this task as not done yet:\n"
-                            + INDENTATION + "  " + specifiedTask);
-                    printLine();
-
-                } else if (strings[0].equals("todo") && strings.length > 1) {
-                    addTask(new Todo(input.substring(5)));
-
-                } else if (strings[0].equals("deadline")  && strings.length > 1) {
-                    input = input.substring(9);
-                    strings = input.split(" /by ");
-                    addTask(new Deadline(strings[0], strings[1]));
-
-                } else if (strings[0].equals("event") && strings.length > 1) {
-                    input = input.substring(6);
-                    strings = input.split(" /at ");
-                    addTask(new Event(strings[0], strings[1]));
-
-                } else {
-                    printLine();
-                    System.out.println(INDENTATION + input);
-                    printLine();
-                }
+        while (!isBye) {
+            try {
+                execute(sc.nextLine());
+            } catch (DukeException e) {
+                printError("I'm sorry, but I don't know what that means :-(");
             }
         }
 
+        printLine();
+        System.out.println(INDENTATION + "Bye. Hope to see you again soon!");
         sc.close();
     }
 }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,11 @@
+/**
+ * The DukeException class represents an exception
+ * that is unique to Duke.
+ */
+public class DukeException extends Exception {
+    /**
+     * Constructs a new DukeException.
+     */
+    public DukeException() {
+    }
+}
