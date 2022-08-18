d137007a3f122b95f79ddb2851e198ef463bef4d
DevanshShah1309 <devanshshah1309@gmail.com>
2022-08-18 10:38:13 +0800

    Remove Duke Logo
    Also, shift Scanner outside while loop

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,19 +21,12 @@ public class Duke {
 
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
         System.out.println("\t-------------------------------");
         System.out.println("\tHello, I'm Duke!");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t-------------------------------");
-
+        Scanner s = new Scanner(System.in);
         while (true) {
-            Scanner s = new Scanner(System.in);
             String command = s.nextLine().trim();
             System.out.println("\t-------------------------------");
             if (command.equals(BYE)) {
