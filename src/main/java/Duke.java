3ee3eb235729a9707132faf9affb0fd1b8d03f93
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2022-08-18 20:28:27 +0800

    Add handle error message to duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,8 +24,10 @@ public class Duke {
                 Task currTask = tasksList.get(i);
                 System.out.printf("\t%d.%s\n", i + 1, currTask);
             }
-        } else if (keyword.equals("invalid")) {
+        } else if (keyword.equals("unknown")) {
             System.out.printf("\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n");
+        } else if (keyword.equals("empty")) {
+            System.out.printf("\t☹ OOPS!!! The description of a %s cannot be empty.\n", input);
         }
         System.out.println(DIVIDER);
     }
@@ -48,6 +50,10 @@ public class Duke {
         System.out.println(DIVIDER);
     }
 
+    public static void mainProcess() {
+
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -58,86 +64,119 @@ public class Duke {
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         System.out.println(DIVIDER);
-
         Scanner sc = new Scanner(System.in);
+
         while (sc.hasNext()) {
-            String input = sc.next();
 
-            if (input.equals("list")) {
-                print(input, "list");
-            } else if (input.equals("bye")) {
-                print(input, "bye");
-                break;
-            } else if (input.equals("mark")) {
-                // get the index
-                if (sc.hasNext()) {
-                    int index = sc.nextInt();
-                    if (index <= tasksList.size() && index > 0) {
-                        print("mark", index - 1);
+            try {
+                String input = sc.next();
+
+                if (input.equals("list")) {
+                    print(input, "list");
+                } else if (input.equals("bye")) {
+                    print(input, "bye");
+                    break;
+                } else if (input.equals("mark")) {
+                    // get the index
+                    if (sc.hasNextInt()) {
+                        int index = sc.nextInt();
+                        if (index <= tasksList.size() && index > 0) {
+                            print("mark", index - 1);
+                        } else {
+                            // list out of bound
+                            print(input, "unknown");
+                        }
                     } else {
-                        print(input, "invalid");
+                        // invalid
+                        // ☹ OOPS!!! The description of a mark cannot be empty.
+                        throw new DukeException("mark empty");
+                    }
+
+                } else if (input.equals("unmark")) {
+                    if (sc.hasNextInt()) {
+                        int index = sc.nextInt();
+                        if (index <= tasksList.size() && index > 0) {
+                            print("unmark", index - 1);
+                        } else {
+                            // list out of bound
+                            print(input, "unknown");
+                        }
+                    } else {
+                        // ☹ OOPS!!! The description of a unmark cannot be empty.
+                        throw new DukeException("unmark empty");
+                    }
+                } else if (input.equals("todo")) {
+                    if (sc.hasNextLine()) {
+                        input = sc.nextLine();
+                        if (input.length() <= 0) {
+                            throw new DukeException("todo empty");
+                        }
+                        input = input.substring(1);
+
+                        Task newTask = new Todo(input);
+                        tasksList.add(newTask);
+                        print(input, "add");
+                    }
+
+                } else if (input.equals("deadline")) {
+                    if (sc.hasNextLine()) {
+                        input = sc.nextLine();
+                        if (input.length() <= 0) {
+                            throw new DukeException("deadline empty");
+                        }
+                        int index = input.lastIndexOf("/by");
+
+                        if (index > -1) {
+                            String by = input.substring(index + 4, input.length());
+                            input = input.substring(1, index - 1);
+
+                            Task newTask = new Deadline(input, by);
+                            tasksList.add(newTask);
+                            print(input, "add");
+                        } else {
+                            throw new DukeException("unknown");
+                        }
+                    }
+
+                } else if (input.equals("event")) {
+                    if (sc.hasNextLine()) {
+                        input = sc.nextLine();
+                        if (input.length() <= 0) {
+                            throw new DukeException("event empty");
+                        }
+                        int index = input.lastIndexOf("/at");
+
+                        if (index > -1) {
+                            String at = input.substring(index + 4, input.length());
+                            input = input.substring(1, index - 1);
+
+                            Task newTask = new Event(input, at);
+                            tasksList.add(newTask);
+                            print(input, "add");
+                        } else {
+                            throw new DukeException("unknown");
+                        }
                     }
                 } else {
-                    print(input, "invalid");
+                    throw new DukeException("unknown");
                 }
-
-            } else if (input.equals("unmark")) {
-                if (sc.hasNext()) {
-                    int index = sc.nextInt();
-                    if (index <= tasksList.size() && index > 0) {
-                        print("unmark", index - 1);
-                    } else {
-                        print(input, "invalid");
-                    }
-                } else {
-                    print(input, "invalid");
+            } catch (DukeException ex) {
+                if (ex.getMessage().equals("unknown")) {
+                    print("", "unknown");
+                } else if (ex.getMessage().equals("event empty")) {
+                    print("event", "empty");
+                } else if (ex.getMessage().equals("deadline empty")) {
+                    print("deadline", "empty");
+                } else if (ex.getMessage().equals("todo empty")) {
+                    print("todo", "empty");
+                } else if (ex.getMessage().equals("mark empty")) {
+                    print("mark", "empty");
+                } else if (ex.getMessage().equals("unmark empty")) {
+                    print("unmark", "empty");
                 }
-            } else if (input.equals("todo")) {
-                if (sc.hasNextLine()) {
-                    input = sc.nextLine();
-                }
-                input = input.substring(1);
-                Task newTask = new Todo(input);
-                tasksList.add(newTask);
-                print(input, "add");
-            } else if (input.equals("deadline")) {
-                if (sc.hasNextLine()) {
-                    input = sc.nextLine();
-                }
-
-                int index = input.lastIndexOf("/by");
-
-                if (index > -1) {
-                    String by = input.substring(index + 4, input.length());
-                    input = input.substring(1, index - 1);
-
-                    Task newTask = new Deadline(input, by);
-                    tasksList.add(newTask);
-                    print(input, "add");
-                } else {
-                    print(input, "invalid");
-                }
-            } else if (input.equals("event")) {
-                if (sc.hasNextLine()) {
-                    input = sc.nextLine();
-                }
-
-                int index = input.lastIndexOf("/at");
-
-                if (index > -1) {
-                    String at = input.substring(index + 4, input.length());
-                    input = input.substring(1, index - 1);
-
-                    Task newTask = new Event(input, at);
-                    tasksList.add(newTask);
-                    print(input, "add");
-                } else {
-                    print(input, "invalid");
-                }
-            } else {
-                print(input, "invalid");
             }
         }
         sc.close();
+
     }
 }
