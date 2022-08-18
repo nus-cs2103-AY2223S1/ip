c6ef1147ee72512ab2387b9a19808a259e431baf
MokYaoHui <e0725894@u.nus.edu>
2022-08-18 14:44:06 +0800

    Added Greet, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        String startMsg = "Hello!!! My name is Wanya! \n WWaku WWaku! \n How can I help you? \n";
+        String closeMsg = "I am so sad that you have to go :( Bye bye :(";
+        Scanner sc = new Scanner(System.in);
+        System.out.println(startMsg);
+
+        while (sc.hasNext()) {
+            String commandInput = sc.nextLine();
+            if (commandInput.equals("bye")) {
+                System.out.println(closeMsg);
+                break;
+            } else {
+                System.out.println(commandInput);
+            }
+        }
     }
 }
