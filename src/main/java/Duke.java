afcc6b91c7daf7be9e7223dc6d081683316bc5fa
tsammeow <97447335+tsammeow@users.noreply.github.com>
2022-08-19 02:09:49 +0800

    duke level2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Duke {
     private static boolean alive;
+    private static List<String> tasks;
 
     private static String handle(String command, String params) {
         switch (command) {
+            case "list":
+                String out = "";
+                for (int i = 0; i < tasks.size(); ++i) {
+                    if (i != 0) out += "\n";
+                    out += (i + 1) + ". " + tasks.get(i);
+                }
+                return out;
             case "bye":
                 alive = false;
                 return "Goodbye!";
             default:
-                return command;
+                tasks.add(command + " " + params);
+                return "added: " + command + " " + params;
         }
     }
 
     private static void printResponse(String response) {
         System.out.println("    __________________________________________________");
-        System.out.println("    " + response);
+        System.out.println("    " + response.replace("\n", "\n    "));
         System.out.println("    __________________________________________________");
     }
 
     public static void main(String[] args) {
+
+        tasks = new ArrayList<>();
+
         printResponse("Hello! What can I do for you today?");
         Scanner sc = new Scanner(System.in);
 
