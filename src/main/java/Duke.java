1a9930064a0f1aa5075f534458e5a9a8484f4947
Gui Ming Jiang <mjgui2000@hotmail.com>
2022-08-19 00:10:50 +0800

    Throw and handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import exceptions.DukeException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -33,60 +35,68 @@ public class Duke {
         printMultiMsg(new String[]{"Hello my name is Duke", "What can I do for you?"});
 
         Scanner sc = new Scanner (System.in);
-        String command = "", arguments = "";
+        String command, description;
 
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
 
             if (input.length() < 1) continue;
 
-            String[] words = input.split(" ", 2);
-            command = words[0];
-            if (words.length > 1) arguments = words[1];
+            try {
+                String[] words = input.split(" ", 2);
+                command = words[0];
+                if (words.length > 1) description = words[1];
+                else description = "";
 
-            if (command.equals("bye")) {
-                printMsg("Bye. Hope to see you again soon!");
-                break;
-            } else if (command.equals("list")) {
-                String[] taskStrings = new String[tasks.size()];
-                for (int i = 0; i < tasks.size(); i++) {
-                    taskStrings[i] = (i+1) + ". " + tasks.get(i).toString();
+                if (command.equals("bye")) {
+                    printMsg("Bye. Hope to see you again soon!");
+                    break;
+                } else if (command.equals("list")) {
+                    String[] taskStrings = new String[tasks.size()];
+                    for (int i = 0; i < tasks.size(); i++) {
+                        taskStrings[i] = (i + 1) + ". " + tasks.get(i).toString();
+                    }
+                    printMultiMsg(taskStrings);
+                } else if (command.equals("mark")) {
+                    Task task = tasks.get(Integer.parseInt(words[1]) - 1);
+                    task.markDone();
+                    printMultiMsg(new String[]{"Nice! I've marked this task as done:", task.toString()});
+                } else if (command.equals("todo")) {
+                    if (description.length() == 0) throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    Task task = new ToDo(description, false);
+                    tasks.add(task);
+                    printMultiMsg(new String[]{
+                            "Got it. I've added this task:",
+                            "  " + task,
+                            "Now you have " + tasks.size() + " task" + (tasks.size() == 1 ? "" : "s") + " in the list."
+                    });
+                } else if (command.equals("deadline")) {
+                    String[] splitArgs = description.split(" /by ", 2);
+                    String title = splitArgs[0];
+                    String by = splitArgs[1];
+                    Task task = new Deadline(title, false, by);
+                    tasks.add(task);
+                    printMultiMsg(new String[]{
+                            "Got it. I've added this task:",
+                            "  " + task,
+                            "Now you have " + tasks.size() + " task" + (tasks.size() == 1 ? "" : "s") + " in the list."
+                    });
+                } else if (command.equals("event")) {
+                    String[] splitArgs = description.split(" /at ", 2);
+                    String title = splitArgs[0];
+                    String at = splitArgs[1];
+                    Task task = new Event(title, false, at);
+                    tasks.add(task);
+                    printMultiMsg(new String[]{
+                            "Got it. I've added this task:",
+                            "  " + task,
+                            "Now you have " + tasks.size() + " task" + (tasks.size() == 1 ? "" : "s") + " in the list."
+                    });
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-                printMultiMsg(taskStrings);
-            } else if (command.equals("mark")) {
-                Task task = tasks.get(Integer.parseInt(words[1]) - 1);
-                task.markDone();
-                printMultiMsg(new String[]{"Nice! I've marked this task as done:", task.toString()});
-            } else if (command.equals("todo")) {
-                Task task = new ToDo(arguments, false);
-                tasks.add(task);
-                printMultiMsg(new String[]{
-                        "Got it. I've added this task:",
-                        "  " + task,
-                        "Now you have " + tasks.size() + " task" + (tasks.size() == 1 ? "" : "s") + " in the list."
-                });
-            } else if (command.equals("deadline")) {
-                String[] splitArguments = arguments.split(" /by ", 2);
-                String title = splitArguments[0];
-                String by = splitArguments[1];
-                Task task = new Deadline(title, false, by);
-                tasks.add(task);
-                printMultiMsg(new String[]{
-                        "Got it. I've added this task:",
-                        "  " + task,
-                        "Now you have " + tasks.size() + " task" + (tasks.size() == 1 ? "" : "s") + " in the list."
-                });
-            } else if (command.equals("event")) {
-                String[] splitArguments = arguments.split(" /at ", 2);
-                String title = splitArguments[0];
-                String at = splitArguments[1];
-                Task task = new Event(title, false, at);
-                tasks.add(task);
-                printMultiMsg(new String[]{
-                        "Got it. I've added this task:",
-                        "  " + task,
-                        "Now you have " + tasks.size() + " task" + (tasks.size() == 1 ? "" : "s") + " in the list."
-                });
+            } catch (DukeException e) {
+                printMsg(e.getMessage());
             }
         }
     }
