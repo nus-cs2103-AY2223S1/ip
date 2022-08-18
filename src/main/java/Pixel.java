45659a42c2d7a2e3bdfbdbfa46571871efec6c65
Daniel Luo <hurinturin123@gmail.com>
2022-08-18 21:52:17 +0800

    Level 2: Created list, added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -2,15 +2,25 @@ import java.util.Scanner;
 
 public class Pixel {
 
+    private static int count = 0;
     private Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+    private String[] inputMemory = new String[100];
 
     private void run() {
         String userInput = myScanner.nextLine();  // Read user input
 
         if (userInput.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
+        } else if (userInput.equals("list")) {
+            // System.out.println(inputMemory.length);
+            for (int i = 0; i < count; i++) {
+                System.out.println((i + 1) + ". " + inputMemory[i]);
+            }
+            run();
         } else {
+            inputMemory[count] = userInput; // Stores user input
             System.out.println(userInput);  // Output user input
+            count += 1;
             run();
         }
     }
@@ -21,7 +31,8 @@ public class Pixel {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n");
         System.out.println("Hello! I'm Pixel \r\nWhat can I do for you?");
         Pixel test = new Pixel();
         test.run();
