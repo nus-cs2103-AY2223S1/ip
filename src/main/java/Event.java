9922feb76770b70d30fa1eae567de9654935c885
L1uY1jun <yijunlau@gmail.com>
2022-08-18 20:56:56 +0800

    Modified minor bugs

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -13,6 +13,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + "(at: " + at + ")";
     }
 }
