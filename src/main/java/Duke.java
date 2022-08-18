84095efc15b334290fc7f6b5fdefcabae639bc4e
jeromehjj <jeromehjj@gmail.com>
2022-08-18 18:29:39 +0800

    Added Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,8 +21,21 @@ public class Duke {
             counter++;
         }
     }
+    public static void unmarkTask(ArrayList<Task> taskList, int index) {
+        System.out.println(LINE + "OK, I've marked this task as not done yet:");
+        Task task = taskList.get(index);
+        task.markAsUndone();
+        System.out.println(task.toString() + "\n" + LINE);
+    }
 
-    public static void main(String[] args) {
+    public static void markTask(ArrayList<Task> taskList, int index) {
+        System.out.println(LINE + "Nice! I've marked this task as done:");
+        Task task = taskList.get(index);
+        task.markAsDone();
+        System.out.println(task.toString() + "\n" + LINE);
+    }
+
+    public static void main(String[] args) throws DukeException {
         Scanner sc = new Scanner(System.in);
         int count = 0;
         ArrayList<Task> list = new ArrayList<>();
@@ -36,74 +49,77 @@ public class Duke {
 
         while (true) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
-                exit();
-                break;
+            try {
+                if (input.equals("bye")) {
+                    exit();
+                    break;
 
-            } else if (input.equals("list")) {
-                System.out.println(LINE + "Here are the tasks in your list:");
-                listTasks(list);
-                System.out.println(LINE);
+                } else if (input.equals("list")) {
+                    System.out.println(LINE + "Here are the tasks in your list:");
+                    listTasks(list);
+                    System.out.println(LINE);
 
-            } else if (input.contains("unmark")) {
-                char taskNumber = input.charAt(7);
-                int number = Character.getNumericValue(taskNumber);
+                } else if (input.contains("unmark")) {
+                    char taskNumber = input.charAt(7);
+                    int number = Character.getNumericValue(taskNumber);
 
-                if (number <= 0 || number > count) {
-                    System.out.println("Sorry, I can't mark this as undone if it does not exist :(");
+                    if (number <= 0 || number > count) {
+                        throw new DukeException(LINE + "☹ OOPS!!! Sorry, I can't mark this as" +
+                                " undone if it does not exist\n" + LINE);
+                    } else {
+                        unmarkTask(list, number - 1);
+                    }
+
+                } else if (input.contains("mark")) {
+                    char taskNumber = input.charAt(5);
+                    int number = Character.getNumericValue(taskNumber);
+
+                    if (number <= 0 || number > count) {
+                        throw new DukeException(LINE + "☹ OOPS!!! Sorry, I can't mark this as " +
+                                "done if it does not exist :(\n" + LINE);
+                    } else {
+                        markTask(list, number - 1);
+                    }
+
+                } else if (input.contains("todo")) {
+                    String task = input.substring(4);
+                    ToDo todo = new ToDo(task);// get the task
+                    if (task.isEmpty() || input.substring(5).isEmpty()) {
+                        throw new DukeException(LINE + "☹ OOPS!!! The description of a todo cannot be empty.\n"
+                                + LINE);
+                    } else {
+                        list.add(count, todo);
+                        count++;
+                        System.out.println(LINE + "Got it. I've added this task:\n" + todo.toString());
+                        System.out.println("Now you have " + count + " tasks in this list.\n" + LINE);
+                    }
+
+                } else if (input.contains("deadline")) {
+                        String[] deadline = input.split("deadline ", 2);
+                        String taskBy = deadline[1];
+                        String[] task = taskBy.split("/by", 2);//Split task into its description and deadline
+                        Deadline d = new Deadline(task[0], task[1]);
+                        list.add(d);
+                        count++;
+                        System.out.println(LINE + "Got it. I've added this task:\n" + d.toString());
+                        System.out.println("Now you have " + count + " tasks in this list.\n" + LINE);
+
+                } else if (input.contains("event")) {
+                    String[] event = input.split("event ", 2);
+                    String taskAt = event[1];
+                    String[] task = taskAt.split("/at", 2);// Split task into its description and timeline
+                    Event e = new Event(task[0], task[1]);
+                    list.add(e);
+                    count++;
+                    System.out.println(LINE + "Got it. I've added this task:\n" + e.toString());
+                    System.out.println("Now you have " + count + " tasks in this list.\n" + LINE);
+
                 } else {
-                    System.out.println(LINE + "OK, I've marked this task as not done yet:");
-                    Task task = list.get(number - 1);
-                    task.markAsUndone();
-                    System.out.println(task.toString() + "\n" + LINE);
+                    throw new DukeException(LINE + "☹ OOPS!!! I'm sorry, but I don't know what that means :(\n"
+                            + LINE);
                 }
-
-            } else if (input.contains("mark")) {
-                char taskNumber = input.charAt(5);
-                int number = Character.getNumericValue(taskNumber);
-
-                if (number <= 0 || number > count) {
-                    System.out.println("Sorry, I can't mark this as done if it does not exist :(");
-                } else {
-                    System.out.println(LINE + "Nice! I've marked this task as done:");
-                    Task task = list.get(number - 1);
-                    task.markAsDone();
-                    System.out.println(task.toString() + "\n" + LINE);
-                }
-
-            } else if (input.contains("todo")) {
-                ToDo todo = new ToDo(input.substring(5)); // get the task
-                list.add(count, todo);
-                count++;
-                System.out.println(LINE + "Got it. I've added this task:\n" + todo.toString());
-                System.out.println("Now you have " + count + " tasks in this list.\n" + LINE);
-
-            } else if (input.contains("deadline")) {
-                String[] deadline = input.split("deadline ", 2);
-                String taskBy = deadline[1];
-                String[] task = taskBy.split("/by", 2); // Split task into its description and deadline
-                Deadline d = new Deadline(task[0], task[1]);
-                list.add(d);
-                count++;
-                System.out.println(LINE + "Got it. I've added this task:\n" + d.toString());
-                System.out.println("Now you have " + count + " tasks in this list.\n" + LINE);
-
-            } else if (input.contains("event")) {
-                String[] event = input.split("event ", 2);
-                String taskAt = event[1];
-                String[] task = taskAt.split("/at", 2);// Split task into its description and timeline
-                Event e = new Event(task[0], task[1]);
-                list.add(e);
-                count++;
-                System.out.println(LINE + "Got it. I've added this task:\n" + e.toString());
-                System.out.println("Now you have " + count + " tasks in this list.\n" + LINE);
-
-            } else {
-                ToDo t = new ToDo(input);
-                list.add(count, t);
-                count++;
-                System.out.println(LINE + "Got it. I've added this task:\n" + t.toString());
-                System.out.println("Now you have " + count + " tasks in this list.\n" + LINE);
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
