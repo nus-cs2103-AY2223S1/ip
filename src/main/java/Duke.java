de51ddbe5180dad9e1c7b76a26e0404ef81a5495
Ashiqur Rahman <e0689695@u.nus.edu>
2022-08-18 14:51:14 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
@@ -7,19 +8,35 @@ public class Duke {
         System.out.println(echoCommand);
         System.out.println("--------------------------------------------------");
     }
+
     public static void main(String[] args) {
         echo("Hello! I'm Duke\nWhat can I do for you?");
+        ArrayList<String> tasklist = new ArrayList<String>();
 
         String command = "";
         while (true) {
             Scanner userInput = new Scanner(System.in);// Create a Scanner object
             command = userInput.nextLine(); // Read user input
-            if ( !"bye".equals(command)) {
-                echo(command);
-            } else {
+            if ("bye".equals(command)) {
+                echo("Bye. Hope to see you again soon!");
                 break;
+            } else if ("list".equals(command)) {
+                String taskRecords = "";
+                for (int i = 0; i < tasklist.size(); i++) {
+                    String taskRecord="";
+                    if (i == tasklist.size() - 1) {
+                        taskRecord = String.format("%d. %s", i + 1, tasklist.get(i));
+                    } else {
+                        taskRecord = String.format("%d. %s\n", i + 1, tasklist.get(i));
+                    }
+                    taskRecords += taskRecord;
+                }
+                echo(taskRecords);
+            } else {
+                echo(command);
+                tasklist.add(command);
             }
         }
-        echo("Bye. Hope to see you again soon!");
+
     }
 }
