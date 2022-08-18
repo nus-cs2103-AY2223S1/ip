ece447105a6d736925eff9c638e2d5fa0fd6f213
Bill <xiaobill8@gmail.com>
2022-08-18 15:06:55 +0800

    Completed Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -18,19 +20,29 @@ public class Duke {
         System.out.println(greeting);
         System.out.println(endLine);
 
+        ArrayList<String> list = new ArrayList<String>();
         Scanner dukeSc = new Scanner(System.in);
-        String input = dukeSc.nextLine();
 
-        while(!input.equalsIgnoreCase("bye")) {
+        while (true) {
+            String input = dukeSc.nextLine();
+
             System.out.println(startLine);
-            System.out.println(input);
-            System.out.println(endLine);
-
-            input = dukeSc.nextLine();
+            switch (input) {
+                case "bye":
+                    System.out.println(farewell);
+                    System.out.println(endLine);
+                    return;
+                case "list":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(i+1 + ". " + list.get(i));
+                    }
+                    System.out.println(endLine);
+                    break;
+                default:
+                    list.add(input);
+                    System.out.println("added: " + input);
+                    System.out.println(endLine);
+            }
         }
-
-        System.out.println(startLine);
-        System.out.println(farewell);
-        System.out.println(endLine);
     }
 }
