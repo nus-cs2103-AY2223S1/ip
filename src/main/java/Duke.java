96bf4b1fcbde0dcb1fd30c31f0af18cf82f0e85a
cxyterence <e0725909@u.nus.edu>
2022-08-18 23:58:31 +0800

    added support for deleting tasks from list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.awt.desktop.SystemEventListener;
+import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanObj = new Scanner(System.in);   // Create Scanner object
-        Task[] taskLst = new Task[100];
-        int qty = 0;
+        ArrayList<Task> taskArrayList = new ArrayList<>();
 
         String intro = "Hello! My name is GustavoBot, but you can call me Gus\n"
                 + "How may I help you today?";
@@ -15,36 +18,44 @@ public class Duke {
             String act = scanObj.nextLine();            // read user input
 
             if (act.equals("list")) {
-                for (int i = 0; i < 100; i++) {
-                    if (taskLst[i] == null) {
-                        break;
-                    }
-                    int index = i + 1;
-                    System.out.println(index + "." + taskLst[i].toString());
+                ListIterator<Task> iterate = taskArrayList.listIterator();
+                System.out.println("Here are the tasks in your list:");
+                int qty = 0;
+                while (iterate.hasNext()) {
+                    qty++;
+                    System.out.println(qty + "." + iterate.next().toString());
                 }
+            } else if (act.contains("delete")) {
+                String numberOnly = act.replaceAll("[^0-9]", "");
+                int index = Integer.parseInt(numberOnly);
+                Task deleted = taskArrayList.get(index-1);
+                taskArrayList.remove(index-1);
+                System.out.println("Noted. I've removed this task:\n"
+                        + deleted.toString() + "\n"
+                        + "Now you have " + taskArrayList.size() + " tasks in the list");
+
             } else if (act.contains("unmark")) {
                 String numberOnly = act.replaceAll("[^0-9]", "");
                 int index = Integer.parseInt(numberOnly);
-                taskLst[index - 1].unmarkDone();
+                taskArrayList.get(index-1).unmarkDone();
                 System.out.println("Alright, this task is marked as not done yet:\n"
-                        + taskLst[index - 1].toString());
+                        + taskArrayList.get(index-1).toString());
 
             } else if (act.contains("mark")) {
                 String numberOnly = act.replaceAll("[^0-9]", "");
                 int index = Integer.parseInt(numberOnly);
-                taskLst[index - 1].markDone();
+                taskArrayList.get(index-1).markDone();
                 System.out.println("Alright, this task is marked as done:\n"
-                        + taskLst[index - 1].toString());
+                        + taskArrayList.get(index-1).toString());
 
             } else if (act.contains("todo")) {
                 act = act.replace("todo", "");
                 try {
                     ToDo temp = new ToDo(act);
-                    taskLst[qty] = temp;
-                    qty++;
+                    taskArrayList.add(temp);
                     System.out.println("Got it. I've added this task:\n"
                             + temp.toString() + "\n"
-                            + "Now you have " + qty + " in the list.");
+                            + "Now you have " + taskArrayList.size() + "tasks in the list.");
                 } catch (EmptyDescException ede) {
                     System.out.println(ede.toString());
                 }
@@ -54,11 +65,10 @@ public class Duke {
                 try {
                     int slash = act.lastIndexOf("/");
                     Deadline temp = new Deadline(act.substring(0, slash - 1), act.substring(slash + 4));
-                    taskLst[qty] = temp;
-                    qty++;
+                    taskArrayList.add(temp);
                     System.out.println("Got it. I've added this task:\n"
                             + temp.toString() + "\n"
-                            + "Now you have " + qty + " in the list.");
+                            + "Now you have " + taskArrayList.size() + "tasks in the list.");
                 } catch (EmptyDescException ede) {
                     System.out.println(ede.toString());
                 }
@@ -68,11 +78,10 @@ public class Duke {
                 try {
                     int slash = act.lastIndexOf("/");
                     Event temp = new Event(act.substring(0, slash - 1), act.substring(slash + 4));
-                    taskLst[qty] = temp;
-                    qty++;
+                    taskArrayList.add(temp);
                     System.out.println("Got it. I've added this task:\n"
                             + temp.toString() + "\n"
-                            + "Now you have " + qty + " in the list.");
+                            + "Now you have " + taskArrayList.size() + "tasks in the list.");
                 } catch (EmptyDescException ede) {
                     System.out.println(ede.toString());
                 }
