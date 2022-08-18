fb6769b99be290dceb800694868864d2dff25c35
glemenneo <glemenneo@gmail.com>
2022-08-18 14:21:27 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; 
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Duke.echo();
+    }
+
+    public static void echo() {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke\n \tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            input = scanner.nextLine();
+            if (input.toLowerCase().equals("bye")) {
+                break;
+            }
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + input);
+            System.out.println("\t____________________________________________________________");
+        }
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+        return;
     }
 }
