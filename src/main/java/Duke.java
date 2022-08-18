6708e2fe3644c9e928ac35c9c0a5163a946d1a32
chantellyu <e0758927@u.nus.edu>
2022-08-18 15:15:26 +0800

    Add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,41 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        entryStatement();
+        ArrayList<String> inputs = new ArrayList<String>();
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("What can I do for you?");
         while (true) {
             String userIn = myObj.nextLine();  // Read user input
             if (userIn.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                byeStatement();
                 break;
             }
+            else if (userIn.equals("list")) {
+                for (int i = 1; i < inputs.size()+1; i++) {
+                    System.out.println(i+": " + inputs.get(i - 1));
+                }
+            }
             else {
-                System.out.println(userIn);  // Output user input
+                inputs.add(userIn);
+                System.out.println("added: " + userIn);  // Output user input
             }
         }
 
+    }
+    public static void entryStatement() {
+        System.out.println(" /\\_/\\");
+        System.out.println("/ o o \\");
+        System.out.println("/ ^  ^\\");
+        System.out.println("Hello from Chan-bot!");
+    }
+
+    public static void byeStatement() {
+        System.out.println("Bye bye!");
+        System.out.println(" /\\_/\\");
+        System.out.println("/ o o \\");
+        System.out.println("/    ^\\");
     }
 }
