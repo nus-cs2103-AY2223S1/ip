23f07164b2471ccf0b052a9d27c67f90770a41b6
tantzeyeong <tzeyeongtan@gmail.com>
2022-08-18 18:48:35 +0800

    add echoing of user commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,34 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         greetUser();
+        askUser();
         sayBye();
     }
 
     public static void greetUser() {
-        System.out.println("______________________________________________________");
-        System.out.println("Hey there! I'm Arias!");
-        System.out.println("How may I help you? :)");
-        System.out.println("______________________________________________________");
+        System.out.println("\t______________________________________________________");
+        System.out.println("\tHey there! I'm Arias!");
+        System.out.println("\tHow may I help you? :)");
+        System.out.println("\t______________________________________________________\n");
     }
 
     public static void sayBye() {
-        System.out.println("Nice seeing you! Bye!");
-        System.out.println("______________________________________________________");
+        System.out.println("\t______________________________________________________");
+        System.out.println("\tNice seeing you! Bye!");
+        System.out.println("\t______________________________________________________\n");
+    }
+
+    public static void askUser() {
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye") && !userInput.equals("Bye")) {
+            System.out.println("\t______________________________________________________");
+            System.out.println("\t" + userInput);
+            System.out.println("\t______________________________________________________\n");
+            userInput = sc.nextLine();
+        }
     }
 }
