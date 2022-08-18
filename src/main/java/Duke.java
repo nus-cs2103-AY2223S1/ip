988389c2596ef673af661ff8b62a16d466953c5b
jicsontoh <>
2022-08-18 19:26:39 +0800

    Changed data structure to ArrayList and fixed some bugs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,9 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
-    static Task[] list = new Task[100];
-    static int idx = 0;
+    static ArrayList<Task> list = new ArrayList<>();
     public static void main(String[] args) {
         welcome();
     }
@@ -13,20 +13,19 @@ public class Duke {
      * Exit when user types "bye"
      **/
     public static void welcome() {
-        list = new Task[100];
-        idx = 0;
+        list = new ArrayList<>();
         System.out.println("Hello! I'm Botson");
         Scanner input = new Scanner(System.in);
-        System.out.println("What can I help you with?");
+        System.out.println("What can I help you with? :-)");
         System.out.println("--------------------------");
         while (input.hasNext()) {
             String action = input.nextLine();  // Read user input
-            if (Objects.equals(action, "bye")) {
+            if (Objects.equals(action.strip(), "bye")) {
                 System.out.println("Goodbye! Hope to see you again soon!");
                 System.out.println("--------------------------");
                 break;
-            } else if (Objects.equals(action, "list")) {
-                getList(idx);
+            } else if (Objects.equals(action.strip(), "list")) {
+                getList();
             } else if (action.length() >= 4 && Objects.equals(action.substring(0, 4), "mark")) {
                 markTask(action);
             } else if (action.length() >= 6 && Objects.equals(action.substring(0, 6), "unmark")) {
@@ -47,18 +46,11 @@ public class Duke {
         try {
             String i = action.substring(6).replaceAll(" ", "");
             int index = Integer.parseInt(i) - 1;
-
-            if (index >= idx) {
-                throw new DukeException("OOPS!!! Error: No Such Task");
-            }
-            String removedTask = list[index].toString();
-            for (int j = index; j < idx; j++) {
-                list[j] = list[j + 1];
-            }
-            idx--;
+            String removedTask = list.get(index).toString();
+            list.remove(index);
             System.out.println("Noted. I've removed this task:");
             System.out.println(removedTask);
-            System.out.println("Now you have " + idx + " tasks in the list.");
+            System.out.println("Now you have " + list.size() + " tasks in the list.");
             System.out.println("--------------------------");
         }
         catch (Exception e) {
@@ -74,41 +66,36 @@ public class Duke {
     public static void addToList(String action) {
         try {
             if (action.length() >= 4 && Objects.equals(action.substring(0, 4), "todo")) {
-                list[idx] = new TodoTask(action);
-                idx++;
+                list.add(new TodoTask(action.substring(4).strip()));
             } else if (action.length() >= 5 && Objects.equals(action.substring(0, 5), "event")) {
                 int i = action.indexOf('/');
                 String event = action.substring(i + 3).strip();
-                list[idx] = new EventTask(action.substring(5, i).strip(), event);
-                idx++;
+                list.add(new EventTask(action.substring(5, i).strip(), event));
             } else if (action.length() >= 8 && Objects.equals(action.substring(0, 8), "deadline")) {
                 int i = action.indexOf('/');
                 String by = action.substring(i + 3).strip();
-                list[idx] = new DeadlineTask(action.substring(8, i).strip(), by);
-                idx++;
+                list.add(new DeadlineTask(action.substring(8, i).strip(), by));
             } else {
-                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
-                System.out.println("--------------------------");
-                return;
+                throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(!!!!");
             }
             System.out.println("--------------------------");
-            System.out.println("I've added to the list: \n" + list[idx-1]);
-            System.out.println("Now you have " + idx + " tasks in the list.");
+            System.out.println("I've added to the list:\n" + list.get(list.size() - 1));
+            System.out.println("Now you have " + list.size() + " tasks in the list.");
             System.out.println("--------------------------");
         }
-        catch (Exception ignored) {
-
+        catch (Exception e) {
+            System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            System.out.println("--------------------------");
         }
     }
 
     /**
      * prints out the list of tasks added
-     * @param idx takes in index
      */
-    public static void getList(int idx) {
+    public static void getList() {
         System.out.println("Here are the tasks in your list");
-        for (int i = 0; i < idx; i++) {
-            System.out.println((i + 1) + ". " + list[i]);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
         }
         System.out.println("--------------------------");
     }
@@ -124,13 +111,13 @@ public class Duke {
             i = i.replaceAll(" ", "");
             int index = Integer.parseInt(i) - 1;
             if (mark) {
-                list[index].markDone();
+                list.get(index).markDone();
             } else {
-                list[index].markUnDone();
+                list.get(index).markUnDone();
             }
             String output = mark ? "Nice! I've marked this task as done:" : "OK, I've marked this task as not done yet:";
             System.out.println(output);
-            System.out.println(list[index].toString());
+            System.out.println(list.get(index).toString());
             System.out.println("--------------------------");
         }
         catch (Exception e) {
