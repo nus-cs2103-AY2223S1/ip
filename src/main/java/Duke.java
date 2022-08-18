f93d9e60f5daa1bd83073dffd1f80e22feba1551
sh4nH <e0866259@u.nus.edu>
2022-08-18 10:45:37 +0800

    Added method for user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,24 @@
 import java.util.Scanner;
 
 public class Duke {
+    private final static String horLine = "--------------------------------------";
 
 
-    public static void main(String[] args) {
-
+    private static void userInput() {
         Scanner userCommand = new Scanner(System.in);
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
 
         while (!userCommand.hasNext("bye")) {
             String text = userCommand.nextLine();
-            System.out.println("--------------------------------------");
-            System.out.println("    " + text);
-            System.out.println("--------------------------------------");
+
+            System.out.println(horLine + "\n\t" + text + "\n" + horLine);
         }
+        System.out.println(horLine + "\n\tBye. Hope to see you again soon!\n" + horLine);
 
-        System.out.println("--------------------------------------");
-        System.out.println("   Bye. Hope to see you again soon!");
-        System.out.println("--------------------------------------");
-
+    }
 
 
-
+    public static void main(String[] args) {
+        System.out.println(horLine + "\nHello! I'm Duke\nWhat can I do for you?\n" + horLine);
+        userInput();
     }
 }
