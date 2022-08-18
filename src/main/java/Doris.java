27e8a1d0b6add7d6305cf25d18ac77ac69bf3235
marcuslowhuiyu <marcuslowhuiyu@users.noreply.github.com>
2022-08-18 11:17:32 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Doris.java
+++ src/main/java/Doris.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Doris {
+import java.util.Scanner;
+
+public class Doris {
+
+    public void start (){
+        String logo = "                                                      \n" +
+                "                                                      \n" +
+                "    ,---,                                             \n" +
+                "  .'  .' `\\                       ,--,                \n" +
+                ",---.'     \\    ,---.    __  ,-.,--.'|                \n" +
+                "|   |  .`\\  |  '   ,'\\ ,' ,'/ /||  |,      .--.--.    \n" +
+                ":   : |  '  | /   /   |'  | |' |`--'_     /  /    '   \n" +
+                "|   ' '  ;  :.   ; ,. :|  |   ,',' ,'|   |  :  /`./   \n" +
+                "'   | ;  .  |'   | |: :'  :  /  '  | |   |  :  ;_     \n" +
+                "|   | :  |  ''   | .; :|  | '   |  | :    \\  \\    `.  \n" +
+                "'   : | /  ; |   :    |;  : |   '  : |__   `----.   \\ \n" +
+                "|   | '` ,/   \\   \\  / |  , ;   |  | '.'| /  /`--'  / \n" +
+                ";   :  .'      `----'   ---'    ;  :    ;'--'.     /  \n" +
+                "|   ,.'                         |  ,   /   `--'---'   \n" +
+                "'---'                            ---`-'               \n" +
+                "                                                      ";
+        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eh what you want?");
+
+        while (true) {
+            String command = sc.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("Bye you annoying sia");
+                return;
+            } else {
+                System.out.print(command + "\n");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Doris().start();
+    }
 }
