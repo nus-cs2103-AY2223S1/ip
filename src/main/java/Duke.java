1f2a0a48c290d7c43a2c1bfdb4014344a62d7de1
Echomo Xinyu <seanxinyu2@gmail.com>
2022-08-18 20:13:02 +0800

    Add ability to store input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     static String SEPARATING_LINE = "    ____________________________________________________________";
@@ -7,6 +8,19 @@ public class Duke {
         System.out.println(SEPARATING_LINE);
         System.out.println(s);
         System.out.println(SEPARATING_LINE);
+    }
+
+    private static void ListPrint(ArrayList<String> arr) {
+        int count = 1;
+        String result = "";
+        for (String s : arr) {
+            if (count != 1) {
+                result += "\n";
+            }
+            result += "    " + String.valueOf(count) + ". " + s;
+            count++;
+        }
+        Duke.FormatPrint(result);
     }
 
     public static void main(String[] args) {
@@ -18,6 +32,8 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         String OPENING = "    Hello! I'm Duke\n    What can I do for you?";
+        String LIST_WORD = "list";
+        ArrayList<String> stored_list = new ArrayList<>();
         String END_WORD = "bye";
         String ENDING = "    Bye. Hope to see you again soon!";
 
@@ -31,13 +47,19 @@ public class Duke {
             String str = sc.nextLine();
             str = str.replace("\n", "").replace("/r", "");
 
-            // System.out.print("Original String:\n" + str + "\n");
+            // System.out.print("Input String:\n" + str + "\n");
 
             if (str.equals(END_WORD)) {
                 Duke.FormatPrint(ENDING);
                 break;
             } else {
-                Duke.FormatPrint(str);
+                if (str.equals(LIST_WORD)) {
+                    Duke.ListPrint(stored_list);
+                } else {
+                    stored_list.add(str);
+                    Duke.FormatPrint(" added: " + str);
+                }
+//                Duke.FormatPrint(str);
             }
         }
     }
