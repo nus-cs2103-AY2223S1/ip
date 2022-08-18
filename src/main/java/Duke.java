803d874a281c05a44c7c4012e96a2f0c3930ab9a
KJunWei <khorjunweiwei@gmail.com>
2022-08-18 10:22:20 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String reply(String message) {
+        return "    ____________________________________________________________\n" +
+                "     " + message + "\n" +
+                "    ____________________________________________________________";
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        Scanner scan = new Scanner(System.in);
+        System.out.println(reply("Hello! I'm Duke\n" +
+                "     What can I do for you?"));
+        boolean conversation = true;
+        while (conversation) {
+            String response = scan.nextLine().toString();
+            if (response.equals("bye")) {
+                System.out.println(reply("Bye. Hope to see you again soon!"));
+                conversation = false;
+            } else {
+                System.out.println(reply(response));
+            }
+        }
     }
 }
