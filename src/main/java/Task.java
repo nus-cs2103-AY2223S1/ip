c6f5ee679695ec1ed6d7a3c82123c53f561a5316
Ryan Chua Zong Xun <e0725636@u.nus.edu>
2022-08-18 21:07:10 +0800

    updated Task class with status icon

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,21 @@
 public class Task {
     private String name;
+    private boolean isDone;
 
     public Task(String name) {
         this.name = name;
+        this.isDone = false;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public void setStatus(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
     }
 }
