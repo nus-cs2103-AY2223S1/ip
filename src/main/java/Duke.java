327c5699fe2d35ea2d32ffaab3ce4bf3e5cebfc5
Sampy147 <samuelpangc@gmail.com>
2022-08-18 23:05:42 +0800

    Duke.java: Level-1 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String horizontalBorder = "_________________________________\n";
+
+    public static String welcomeMessage(){
+        return horizontalBorder + "Hello! I'm Duke \nWhat can I do for you? \n" + horizontalBorder;
+    }
+
+    public static String level1(){
+        System.out.println(welcomeMessage());
+        while(true) {
+            Scanner scan = new Scanner(System.in);
+            String s = scan.next();
+            if (s.equals("bye")) {
+                return horizontalBorder + "Bye. Hope to see you again soon! \n" + horizontalBorder;
+            }
+            System.out.println(horizontalBorder + s + "\n" + horizontalBorder);
+        }
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(level1());
     }
 }
