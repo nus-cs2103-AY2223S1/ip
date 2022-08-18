92577e8bc6e5cdb31648ce273d4181641999e5cd
unknown <nathaniel2048@gmail.com>
2022-08-19 02:56:29 +0800

    Add the ability to mark tasks as done and change the status back to not done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,32 +2,39 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        String[] store = new String[100];
+        Scanner sc = new Scanner(System.in);
+        Task[] tasks = new Task[100];
         int count = 0;
         while (true) {
-            Scanner sc = new Scanner(System.in);
-            String s = sc.nextLine();
+            String s = sc.next();
             if (s.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (s.equals("list")) {
                 for (int a = 0; a < count; a++) {
-                    System.out.println((a + 1) + ". " + store[a]);
+                    System.out.println((a + 1) + "." + tasks[a]);
                 }
+                sc.nextLine();
+            } else if (s.equals("mark")) {
+                int index = sc.nextInt() - 1;
+                tasks[index].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[index]);
+                sc.nextLine();
+            } else if (s.equals("unmark")) {
+                int index = sc.nextInt() - 1;
+                tasks[index].markNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[index]);
+                sc.nextLine();
             } else {
-                store[count] = s;
-                count++;
-                System.out.println("added: " + s);
+                String rest = sc.nextLine();
+                String whole = s + rest;
+                tasks[count++] = new Task(whole);
+                System.out.println("added: " + whole);
             }
         }
+        sc.close();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markNotDone() {
+        isDone = false;
+    }
+}
