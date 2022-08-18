9316e607b84c8df30823930f60ccc1c68604f02c
Mohamed Hamas <e0726186@u.nus.edu>
2022-08-18 11:34:32 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,9 +10,33 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello!\nWhat can I do for you ?");
         Scanner help = new Scanner(System.in);
-        String[] lst = new String[100];
-        int i = 0;
+        class Task {
+            protected String description;
+            protected boolean isDone;
+            protected int num;
 
+            public Task(String description,int num) {
+                this.description = description;
+                this.isDone = false;
+                this.num = num;
+            }
+
+            public String getStatusIcon() {
+                return (isDone ? "[X] " : "[ ] "); // mark done task with X
+            }
+
+            public void mark() {
+                isDone = true;
+            }
+
+            public void unmark() {
+                isDone = false;
+            }
+
+        }
+
+        Task[] lst = new Task[100];
+        int i = 0;
 
 
         while(true) {
@@ -22,17 +46,25 @@ public class Duke {
                break;
            }
            else if (str.equals("list")) {
-               for(String item : lst) {
-                   if(item != null)
-                       System.out.println(item);
-
+               System.out.println("This is your tasks in your list: \n");
+               for (Task item : lst) {
+                   if (item != null)
+                       System.out.println(item.num + "." + item.getStatusIcon() + item.description);
                }
+           }
+           else if (str.split(" ")[0].equals("mark") ) {
+               lst[Integer.parseInt(str.split(" ")[1]) - 1].mark();
 
-            }
+           }
+           else if (str.split(" ")[0].equals("unmark") ) {
+               lst[Integer.parseInt(str.split(" ")[1]) - 1].unmark();
+
+           }
            else {
-               lst[i] = i + 1 + "." + str;
+               lst[i] = new Task(str,i+1);
                i++;
                System.out.println("added: " + str);
+
            }
 
        }
