7063ed72e10f1cdbab11b107403b9d23c3815ed2
Tex Tang <tex_tang@outlook.com>
2022-08-18 11:50:09 +0800

    Refactor Duke class
    Duke class: inputs is an array of inputs and a local variable in main method.
    
    inputs array is an array that will be used across multiple commands, refactor it to be class variable can be shared across multiple methods.
    
    * Refactor inputs array to be class variable
    * Rename `inputs` array to `tasks`

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,39 +6,54 @@ public class Duke {
 
     private static final String GREETING =
             "##################################################\n" +
-            "||                                              ||\n" +
-            "||                Hello! I'm Ted                ||\n" +
-            "||            What can I do for you?            ||\n" +
-            "||                                              ||\n" +
-            "##################################################";
+                    "||                                              ||\n" +
+                    "||                Hello! I'm Ted                ||\n" +
+                    "||            What can I do for you?            ||\n" +
+                    "||                                              ||\n" +
+                    "##################################################";
 
     private static final String INPUT_PREFIX = "> ";
 
-    public static void main(String[] args) {
+    ArrayList<String> tasks = new ArrayList<String>();
+
+    private void list() {
+        for (int inputIndex = 0; inputIndex < tasks.size(); inputIndex++) {
+            System.out.printf("%d. %s\n", inputIndex + 1, tasks.get(inputIndex));
+        }
+    }
+
+    private void add(String input) {
+        this.tasks.add(input);
+        System.out.printf("Added: %s\n", input);
+    }
+
+    private void run() {
         System.out.println(GREETING);
 
-        ArrayList<String> inputs = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
         System.out.print(INPUT_PREFIX);
 
         while (scanner.hasNextLine()) {
-            String command = scanner.nextLine();
+            String input = scanner.nextLine();
+            String command = input.split(" ")[0];
 
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again!");
                 break;
             } else if (command.equals("list")) {
-                for (int inputIndex = 0;inputIndex < inputs.size();inputIndex++) {
-                    System.out.printf("%d. %s\n", inputIndex + 1, inputs.get(inputIndex));
-                }
+                this.list();
             } else {
-                inputs.add(command);
-                System.out.printf("Added: %s\n", command);
+                this.add(input);
             }
 
             System.out.print(INPUT_PREFIX);
         }
 
         scanner.close();
+    }
+
+    public static void main(String[] args) {
+        Duke bot = new Duke();
+        bot.run();
     }
 }
