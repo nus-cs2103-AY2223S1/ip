7b5719de3d08bc414f322cef792769119f32efdb
comicalromance <silasyeoshuenyu@gmail.com>
2022-08-18 20:56:41 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ public class Duke {
 
     private static String greetings = "Hello! I'm Ekud \n" + "What can I do for you?";
     private static String banner = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     private static void print(String msg) {
         System.out.println(banner);
@@ -19,23 +19,42 @@ public class Duke {
 
         while(true) {
             String input = sc.nextLine();
-            switch (input) {
-                case "bye":
-                    print("Cya!");
-                    return;
+            if (input.equals("bye")) {
+                print("Cya!");
+            } else if (input.equals("list")) {
 
-                case "list":
-                    String list = "";
-                    for (int i = 0; i < tasks.size(); i++) {
-                        list += (i + 1) + ". " + tasks.get(i);
-                        if (i != tasks.size() - 1) list += "\n";
-                    }
-                    print(list);
-                    break;
+                String list = "";
+                for (int i = 0; i < tasks.size(); i++) {
+                    list += (i + 1) + "." + tasks.get(i);
+                    if (i != tasks.size() - 1) list += "\n";
+                }
+                print(list);
 
-                default:
+            } else if (input.startsWith("mark")) {
+
+                String[] msg = input.split(" ");
+                if (msg.length < 2) {
+                    print("nothing to mark!");
+                    continue;
+                }
+                int index = Integer.valueOf(msg[1]) - 1;
+                tasks.get(index).mark();
+                print("I've marked this task as done: \n" + tasks.get(index));
+
+            } else if (input.startsWith("unmark")) {
+
+                String[] msg = input.split(" ");
+                if (msg.length < 2) {
+                    print("nothing to unmark!");
+                    continue;
+                }
+                int index = Integer.valueOf(msg[1]) - 1;
+                tasks.get(index).unmark();
+                print("I've marked this task as undone: \n" + tasks.get(index));
+
+            } else {
                     print("added: " + input);
-                    tasks.add(input);
+                    tasks.add(new Task(input));
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        isDone = true;
+    }
+
+    public void unmark() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
