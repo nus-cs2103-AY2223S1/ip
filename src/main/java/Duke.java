8fe549614eb7494ba15ed8bddfe5befd5733851a
shawnkai <e0525991@u.nus.edu>
2022-08-18 15:33:59 +0800

    edited Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,9 +7,11 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         Scanner myObj = new Scanner(System.in);
-        Task[] tasks = new Task[101];
-        int number = 1;
+        List<Task> tasks = new ArrayList<Task>();
+        tasks.add(null);
+        int number = 0;
         String dummyString;
+        Task dummyTask;
         int counter;
         int start;
         int end;
@@ -19,58 +23,76 @@ public class Duke {
                 if (input.startsWith("mark")) {
                     dummyString = input.substring(5); //get number of task
                     counter = Integer.parseInt(dummyString);//convert to int
-                    tasks[counter].mark();
+                    tasks.get(counter).mark();
                     System.out.println("Nice! I've marked this task as done:\n" +
-                            "[" + tasks[counter].getStatusIcon() + "] " + tasks[counter].getDescription());
+                            "[" + tasks.get(counter).getStatusIcon() + "] " + tasks.get(counter).getDescription());
 
                 } else if (input.startsWith("unmark")) {
                     dummyString = input.substring(7); //get number of task
                     counter = Integer.parseInt(dummyString);//convert to int
-                    tasks[counter].unmark();
+                    tasks.get(counter).unmark();
                     System.out.println("OK, I've marked this task as not done yet:\n" +
-                            "[" + tasks[counter].getStatusIcon() + "] " + tasks[counter].getDescription());
+                            "[" + tasks.get(counter).getStatusIcon() + "] " + tasks.get(counter).getDescription());
 
                 } else if (input.equals("list")) {
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 1; i < number; i++) { //iterate through all tasks
-                        System.out.println(String.valueOf(i) + "." + tasks[i].toString());
+                    for (int i = 1; i <= number; i++) { //iterate through all tasks
+                        System.out.println(String.valueOf(i) + "." + tasks.get(i).toString());
                     }
+
+                } else if (input.startsWith("delete")) {
+                    if (input.equals("delete")) {
+                        throw new DukeException("Please specify which item is to be deleted.");
+                    }
+                    dummyString = input.substring(7); //get item number to be deleted
+                    counter = Integer.parseInt(dummyString);//convert to int
+                    dummyTask = tasks.get(counter);
+                    tasks.remove(counter);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println("  " + dummyTask.toString());
+                    number -= 1;
+                    System.out.println("Now you have " + String.valueOf(number) + " tasks in the list.");
+
+
                 } else if (input.startsWith("todo")) {
                     if (input.equals("todo")) {
                         throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
                     }
                     start = 5;
-                    tasks[number] = new Todo(input.substring(start));
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[number].toString());
-                    System.out.println("Now you have " + String.valueOf(number) + " tasks in the list.");
                     number += 1;
+                    tasks.add(number, new Todo(input.substring(start)));
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks.get(number).toString());
+                    System.out.println("Now you have " + String.valueOf(number) + " tasks in the list.");
+
 
                 } else if (input.startsWith("deadline")) {
                     if (input.equals("deadline")) {
                         throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
                     }
                     counter = input.indexOf("/");
+                    number += 1;
                     start = 9;
                     end = counter - 1;
-                    tasks[number] = new Deadline(input.substring(start, end), input.substring(counter + 4));
+                    tasks.add(number, new Deadline(input.substring(start, end), input.substring(counter + 4)));
                     System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[number].toString());
+                    System.out.println("  " + tasks.get(number).toString());
                     System.out.println("Now you have " + String.valueOf(number) + " tasks in the list.");
-                    number += 1;
+
 
                 } else if (input.startsWith("event")) {
                     if (input.equals("event")) {
                         throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
                     }
                     counter = input.indexOf("/");
+                    number += 1;
                     start = 6;
                     end = counter - 1;
-                    tasks[number] = new Event(input.substring(start, end), input.substring(counter + 4));
+                    tasks.add(number, new Event(input.substring(start, end), input.substring(counter + 4)));
                     System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[number].toString());
+                    System.out.println("  " + tasks.get(number).toString());
                     System.out.println("Now you have " + String.valueOf(number) + " tasks in the list.");
-                    number += 1;
+
 
                 } else { //random input
                     throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
