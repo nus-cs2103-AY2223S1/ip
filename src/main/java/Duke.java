b1bd47d29f732e933ccb52a0d84238990d8c897d
Justin Cheng <justcheng2000@gmail.com>
2022-08-18 18:40:41 +0800

    Level-3 Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,6 @@
 import java.util.*;
 public class Duke {
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public void mark() {
-            this.isDone = true;
-        }
-
-        public void unmark() {
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " ");
-        }
-
-        public String getDescription() {
-            return this.description;
-        }
-    }
-    public static void run(Task[] lst) {
+    public static void run(Task[] lst, int count) {
         Scanner sc = new Scanner(System.in);
         String choice = sc.nextLine();
         Scanner sc2 = new Scanner(choice);
@@ -36,34 +11,55 @@ public class Duke {
             return;
         } else if (choice.equals("list")) {
             System.out.println("Here are the tasks in your list:");
-            for (int i = 0; i < lst.length; i++) {
-                if (lst[i] == null) {
-                    break;
-                }
+            for (int i = 0; i < count; i++) {
                 int curr = i + 1;
-                System.out.println(curr + ". [" + lst[i].getStatusIcon() + "] " + lst[i].getDescription());
+                System.out.println(curr + ". " + lst[i].toString());
             }
         } else if (call.equals("mark")) {
             int pos = sc2.nextInt() - 1;
             lst[pos].mark();
             System.out.println("Nice! I've marked this task as done:");
-            System.out.println("[" + lst[pos].getStatusIcon() + "] " + lst[pos].getDescription());
+            System.out.println(lst[pos].toString());
         } else if (call.equals("unmark")) {
             int pos = sc2.nextInt() - 1;
             lst[pos].unmark();
             System.out.println("OK, I've marked this task as not done yet:");
-            System.out.println("[" + lst[pos].getStatusIcon() + "] " + lst[pos].getDescription());
-        } else {
-            Task task = new Task(choice);
-            for (int i = 0; i < lst.length; i++) {
-                if (lst[i] == null) {
-                    lst[i] = task;
-                    break;
-                }
+            System.out.println(lst[pos].toString());
+        } else if (call.equals("todo")) {
+            String description = sc2.nextLine();
+            ToDo todo = new ToDo(description);
+            lst[count++] = todo;
+            System.out.println("Got it. I've added this task:");
+            System.out.println("\t" + todo.toString());
+            System.out.println("Now you have " + count + " tasks in the list.");
+        } else if (call.equals("deadline")) {
+            String description = "";
+            while (!sc2.hasNext("/by")) {
+                String next = sc2.next();
+                description += next + " ";
             }
-            System.out.println("added: " + task.getDescription());
+            String skip = sc2.next();
+            String date = sc2.nextLine();
+            Deadline deadline = new Deadline(description, date);
+            lst[count++] = deadline;
+            System.out.println("Got it. I've added this task: ");
+            System.out.println("\t" + deadline.toString());
+            System.out.println("Now you have " + count + " tasks in the list.");
+        } else if (call.equals("event")) {
+            String description = "";
+            while (!sc2.hasNext("/at")) {
+                String next = sc2.next();
+                description += next + " ";
+            }
+            String skip = sc2.next();
+            String date = sc2.nextLine();
+            Event event = new Event(description, date);
+            lst[count++] = event;
+            System.out.println("Got it. I've added this task: ");
+            System.out.println("\t" + event.toString());
+            System.out.println("Now you have " + count + " tasks in the list.");
         }
-        run(lst);
+        run(lst, count);
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -74,6 +70,6 @@ public class Duke {
         Task[] lst = new Task[100];
         System.out.println("Hello! I'm Justin, your personal helper.");
         System.out.println("What can I do for you?");
-        run(lst);
+        run(lst, 0);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
