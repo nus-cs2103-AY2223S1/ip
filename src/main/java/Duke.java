5358067def9750a50b7220012c19dfdeeae5f881
Whysochong <>
2022-08-18 12:30:20 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<String>(100);
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Lebron.");
+        System.out.println("What can I do for you?");
+
+        while (true) {
+            String str = sc.nextLine();
+            if (str.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            if (str.equals("list")) {
+                for (int i = 0, d = 1; i < tasks.size(); i++, d++) {
+                    String statement = String.format("%d. ", d);
+                    System.out.println(statement + tasks.get(i));
+                }
+            }
+            else {
+                tasks.add(str);
+                System.out.println("added: " + str);
+            }
+        }
+
     }
 }
