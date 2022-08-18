0c6d524b3cf596fabcc85bfb2682e61cc778ec05
jehousoh <e0694467@u.nus.edu>
2022-08-18 21:42:59 +0800

    Added enum

--- src/main/java/TaskCreator.java
+++ src/main/java/TaskCreator.java
@@ -2,10 +2,17 @@ public class TaskCreator {
     /**
      * Main class to create new Task
      */
+
+    enum Type {
+        TODO,
+        DEADLINE,
+        EVENT
+    }
     private static int SIZEOFPREPOSITION = 4;
 
     public static Task CreateTask(String in) {
         Task task = null;
+        Type type;
 
         if (!in.startsWith("todo") && !in.startsWith("deadline") && !in.startsWith("event")) {
             return task;
@@ -19,31 +26,45 @@ public class TaskCreator {
         String info = inArr[1];
 
         if (in.startsWith("todo")) {
-            task = new Todo(info);
-
+            type = Type.TODO;
         } else if (in.startsWith("deadline")) {
-            int indexOfSplit = info.indexOf("/by");
-            if (indexOfSplit == -1) {
-                return new ErrorTask("");
-            }
-
-            String description = info.substring(0,indexOfSplit);
-            String date = info.substring(indexOfSplit + SIZEOFPREPOSITION);
-
-            task = new Deadline(description, date);
-
-        } else if (in.startsWith("event")) {
-            int indexOfSplit = info.indexOf("/at");
-            if (indexOfSplit == -1) {
-                return new ErrorTask("");
-            }
-
-            String description = info.substring(0,indexOfSplit);
-            String dateTime = info.substring(indexOfSplit + SIZEOFPREPOSITION);
-
-            task = new Event(description, dateTime);
+            type = Type.DEADLINE;
+        } else {
+            type = Type.EVENT;
         }
 
+        int indexOfSplit;
+        String description;
+
+        switch (type) {
+            case TODO:
+                task = new Todo(info);
+                break;
+
+            case DEADLINE:
+                indexOfSplit = info.indexOf("/by");
+                if (indexOfSplit == -1) {
+                    return new ErrorTask("");
+                }
+
+                description = info.substring(0,indexOfSplit);
+                String date = info.substring(indexOfSplit + SIZEOFPREPOSITION);
+
+                task = new Deadline(description, date);
+                break;
+
+            case EVENT:
+                indexOfSplit = info.indexOf("/at");
+                if (indexOfSplit == -1) {
+                    return new ErrorTask("");
+                }
+
+                description = info.substring(0,indexOfSplit);
+                String dateTime = info.substring(indexOfSplit + SIZEOFPREPOSITION);
+
+                task = new Event(description, dateTime);
+                break;
+        }
         return task;
     }
 }
