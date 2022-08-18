ef02fcdfcd70e2f7c25370bc5916ba4f06216d0e
seanmanik <seanmanik@gmail.com>
2022-08-18 19:34:59 +0800

    Mark and unmark functions added. Level-3 done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,16 +6,32 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String input = "";
         ArrayList<String> list = new ArrayList<>();
+        ArrayList<Boolean> listDone = new ArrayList<>();
         while (true) {
             input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i + 1) + ". " + list.get(i));
+                    if (listDone.get(i)) {
+                        System.out.println((i + 1) + ". [X] " + list.get(i));
+
+                    } else {
+                        System.out.println((i + 1) + ". [ ] " + list.get(i));
+                    }
                 }
+            } else if (input.startsWith("mark")) {
+                int target = Integer.valueOf(input.split(" ")[1]) - 1;
+                listDone.set(target, true);
+                System.out.println("Nice! I've marked this task as done:\n  [X] " + list.get(target));
+            } else if (input.startsWith("unmark")) {
+                int target = Integer.valueOf(input.split(" ")[1]) - 1;
+                System.out.println("OK, I've marked this task as not done yet:\n  [ ] " + list.get(target));
+
+                listDone.set(target, false);
             } else {
                 list.add(input);
+                listDone.add(false);
                 System.out.println("added: " + input);
             }
         }
