20eedf63e0070c1c237a2c3c03f1abe365f0d524
junwei-tan <junweitan@u.nus.edu>
2022-08-19 00:39:10 +0800

    Duke Level-4: ToDo, Event, Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Duke {
-    private static Task[] arr = new Task[100];
+    private static Task[] tasks = new Task[100];
     private static int arrPointer = 0;
 
     public static void main(String[] args) throws IOException {
@@ -14,7 +14,8 @@ public class Duke {
 
         while (inProgress) {
             String userInput = bufferedReader.readLine();
-            String[] userInputArr = userInput.split(" ");
+            String[] userInputArr = userInput.split(" ", 2);
+            String[] taskDetailsArr;
             switch (userInputArr[0]) {
                 case "list":
                     listArrItems();
@@ -29,11 +30,17 @@ public class Duke {
                 case "unmark":
                     unmarkItem(Integer.parseInt(userInputArr[1]));
                     break;
-                case "":
+                case "todo":
+                    addToTasks(new ToDo(userInputArr[1]));
                     break;
-                default:
-                    addArrItem(userInput);
-                    System.out.println("\tadded: " + userInput);
+                case "deadline":
+                    taskDetailsArr = userInputArr[1].split(" /by ");
+                    addToTasks(new Deadline(taskDetailsArr[0], taskDetailsArr[1]));
+                    break;
+                case "event":
+                    taskDetailsArr = userInputArr[1].split(" /at ");
+                    addToTasks(new Event(taskDetailsArr[0], taskDetailsArr[1]));
+                    break;
             }
         }
         bufferedReader.close();
@@ -50,12 +57,15 @@ public class Duke {
         // Usual Path
         System.out.println("\tHere are the tasks in your list:");
         for (int i = 0; i < arrPointer; i++) {
-            System.out.println("\t" + (i + 1) + "." + arr[i]);
+            System.out.println("\t" + (i + 1) + "." + tasks[i]);
         }
     }
 
-    private static void addArrItem(String taskDescription) {
-        arr[arrPointer++] = new Task(taskDescription);
+    private static void addToTasks(Task task) {
+        tasks[arrPointer++] = task;
+        System.out.println("\tGot it. I've added this task:\n" +
+                "\t\t"+ task + "\n" +
+                "\tNow you have " + arrPointer + " tasks in your list.");
     }
 
     private static void markItem(int index) {
@@ -63,8 +73,8 @@ public class Duke {
             System.out.println("\tI can't mark this task. It's not in the list.");
             return;
         }
-        arr[index - 1].isDone = true;
-        System.out.println("\tNice! I've marked this task as done:\n\t\t" + arr[index - 1]);
+        tasks[index - 1].isDone = true;
+        System.out.println("\tNice! I've marked this task as done:\n\t\t" + tasks[index - 1]);
     }
 
     private static void unmarkItem(int index) {
@@ -72,7 +82,7 @@ public class Duke {
             System.out.println("\tI can't unmark this task. It's not in the list.");
             return;
         }
-        arr[index - 1].isDone = false;
-        System.out.println("\tOK, I've marked this task as not done yet:\n\t\t" + arr[index - 1]);
+        tasks[index - 1].isDone = false;
+        System.out.println("\tOK, I've marked this task as not done yet:\n\t\t" + tasks[index - 1]);
     }
 }
