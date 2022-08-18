5fe9e8b1acbafad4780a2de5a4b264e5e2afca38
fongyj <fongyj@hotmail.com>
2022-08-18 21:09:56 +0800

    Add echo and bye functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        boolean terminate = false;
+        while (!terminate) {
+            String m = sc.nextLine();
+            printMessage(m);
+            if (m.equals("bye")) {
+                terminate = true;
+            }
+        }
+    }
+
+    private static void printMessage(String m) {
+        if (m.equals("bye")) {
+            m = "Bye. Hope to see you again soon!";
+        }
+        String message = "    ____________________________________________________________\n    "
+                + m
+                + "\n    ____________________________________________________________\n";
+        System.out.println(message);
     }
 }
