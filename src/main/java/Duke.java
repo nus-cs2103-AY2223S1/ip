d42a77015ef6cad37a4411e6dee39a9d42f27310
highorbit25 <baobin25@gmail.com>
2022-08-18 18:31:45 +0800

    Implemented A-TextUiTesting: Automated Text UI Testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,118 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Duke {
     public static ArrayList<Task> inputTaskArray = new ArrayList<>();
     public static int inputCount = 1;
+    public static boolean end = false;
+    public static Scanner scan = new Scanner( System.in );
     public static void main(String[] args) {
 
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
         printLine();
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from Duke");
         System.out.println("What can I do for you?");
         printLine();
-        getInput();
+        getInput(scan);
+
+//        while(!end) {
+//            String inData;
+//            Scanner scan = new Scanner( System.in );
+////            inData = scan.nextLine();
+//            try {
+//                inData = scan.nextLine();
+//            } catch (Exception NoSuchElementException) {
+//                return;
+//            }
+//
+//            String[] input = inData.split(" ", 2);
+//            switch (input[0]) {
+//                case("bye"):
+//                    printLine();
+//                    System.out.println("Bye. Hope to see you again soon!");
+//                    printLine();
+//                    end = true;
+//                    break;
+//                case("mark"):
+//                    printLine();
+//                    System.out.println("Nice! I've marked this task as done:");
+//                    Task taskToMark = inputTaskArray.get(Integer.parseInt(input[1]) - 1);
+//                    taskToMark.markAsDone();
+//                    System.out.println(taskToMark.toString());
+//                    printLine();
+////                    getInput();
+//                    break;
+//                case("unmark"):
+//                    printLine();
+//                    System.out.println("OK, I've marked this task as not done yet:");
+//                    Task taskToUnmark = inputTaskArray.get(Integer.parseInt(input[1]) - 1);
+//                    taskToUnmark.markAsNotDone();
+//                    System.out.println(taskToUnmark.toString());
+//                    printLine();
+////                    getInput();
+//                    break;
+//                case("list"):
+//                    int count = 1;
+//                    printLine();
+//                    System.out.println("Here are the tasks in your list:");
+//                    for(Task t : inputTaskArray) {
+//                        System.out.println(count++ + "." + t.toString());
+//                    }
+//                    System.out.println("Now you have " + (inputCount - 1) + " tasks in the list.");
+//
+//                    printLine();
+////                    getInput();
+//                    break;
+//                case("todo"):
+//                    printLine();
+//                    Todo curTodo = new Todo(input[1]);
+//                    addToTasklist(curTodo);
+//                    printLine();
+////                    getInput();
+//                    break;
+//                case("deadline"):
+//                    printLine();
+//                    String[] deadlineString = input[1].split("/by", 2);
+//                    Deadline curDeadline = new Deadline(deadlineString[0], deadlineString[1]);
+//                    addToTasklist(curDeadline);
+//                    printLine();
+////                    getInput();
+//                    break;
+//                case("event"):
+//                    printLine();
+//                    String[]eventString = input[1].split("/at", 2);
+//                    Event curEvent = new Event(eventString[0], eventString[1]);
+//                    addToTasklist(curEvent);
+//                    printLine();
+////                    getInput();
+//                    break;
+//                default:
+////                printLine();
+////                Task curTask = new Task(inData);
+////                inputTaskArray.add(curTask);
+////                System.out.println("added: " + curTask.description);
+////                printLine();
+////                getInput();
+//            }
+//        }
 
         }
 
-    public static void getInput() {
+    public static void getInput(Scanner scan) {
         String inData;
-        Scanner scan = new Scanner( System.in );
-        inData = scan.nextLine();
+//        Scanner scan = new Scanner( System.in );
+//        inData = scan.nextLine();
+        try {
+            inData = scan.nextLine();
+        } catch (Exception NoSuchElementException) {
+            return;
+        }
 
         String[] input = inData.split(" ", 2);
         switch (input[0]) {
@@ -39,7 +128,7 @@ public class Duke {
                 taskToMark.markAsDone();
                 System.out.println(taskToMark.toString());
                 printLine();
-                getInput();
+                getInput(scan);
                 break;
             case("unmark"):
                 printLine();
@@ -48,7 +137,7 @@ public class Duke {
                 taskToUnmark.markAsNotDone();
                 System.out.println(taskToUnmark.toString());
                 printLine();
-                getInput();
+                getInput(scan);
                 break;
             case("list"):
                 int count = 1;
@@ -60,14 +149,14 @@ public class Duke {
                 System.out.println("Now you have " + (inputCount - 1) + " tasks in the list.");
 
                 printLine();
-                getInput();
+                getInput(scan);
                 break;
             case("todo"):
                 printLine();
                 Todo curTodo = new Todo(input[1]);
                 addToTasklist(curTodo);
                 printLine();
-                getInput();
+                getInput(scan);
                 break;
             case("deadline"):
                 printLine();
@@ -75,7 +164,7 @@ public class Duke {
                 Deadline curDeadline = new Deadline(deadlineString[0], deadlineString[1]);
                 addToTasklist(curDeadline);
                 printLine();
-                getInput();
+                getInput(scan);
                 break;
             case("event"):
                 printLine();
@@ -83,7 +172,7 @@ public class Duke {
                 Event curEvent = new Event(eventString[0], eventString[1]);
                 addToTasklist(curEvent);
                 printLine();
-                getInput();
+                getInput(scan);
                 break;
             default:
 //                printLine();
@@ -91,7 +180,7 @@ public class Duke {
 //                inputTaskArray.add(curTask);
 //                System.out.println("added: " + curTask.description);
 //                printLine();
-                getInput();
+//                getInput();
         }
 
     }
