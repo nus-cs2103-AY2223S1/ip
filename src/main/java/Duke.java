e6a34951f2a5eb5aabb7cadaa4da8a16701b94fc
rachaelsng <rachaelsng@u.nus.edu>
2022-08-18 15:21:47 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,7 +9,6 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello from\n" + logo);*/
-        ArrayList<String> strList = new ArrayList<>();
         Task[] tasks = new Task[100];
         int numOfTasks = 0;
         Scanner sc = new Scanner(System.in);
@@ -19,7 +17,6 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" +
                 "     What can I do for you?");
         System.out.println(line);
-        String uncheckedBox = "[ ]";
         String input = sc.nextLine();
         while(!input.equals("bye")) {
             System.out.println(line);
@@ -39,10 +36,25 @@ public class Duke {
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(tasks[index]);
             } else {
-                Task newTask = new Task(input);
-                tasks[numOfTasks] = newTask;
-                numOfTasks++;
-                System.out.println("added: " + input);
+                String description = input.substring(input.indexOf(" ") + 1);
+                if (input.startsWith("todo")) {
+                    ToDo newTodo = new ToDo(description);
+                    tasks[numOfTasks] = newTodo;
+                    numOfTasks++;
+                } else if (input.startsWith("deadline")) {
+                    Deadline newDeadline = new Deadline(description.substring(0, description.indexOf("/by") - 1),
+                            description.substring(description.indexOf("/") + 4));
+                    tasks[numOfTasks] = newDeadline;
+                    numOfTasks++;
+                } else if (input.startsWith("event")) {
+                    Event newEvent = new Event(description.substring(0, description.indexOf("/at") - 1),
+                            description.substring(description.indexOf("/") + 4));
+                    tasks[numOfTasks] = newEvent;
+                    numOfTasks++;
+                }
+                System.out.println("Got it. I've added this task: \n" + "  " + tasks[numOfTasks - 1].toString());
+                String taskTense = numOfTasks == 1 ? " task" : " tasks";
+                System.out.println("Now you have " + numOfTasks + taskTense + " in the list.");
             }
             System.out.println(line);
             input = sc.nextLine();
