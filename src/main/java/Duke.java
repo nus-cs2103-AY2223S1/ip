fac41ff90c67edffd13623c1b4ef56b17b3e8d3e
optionalemon <xuyi9272@gmail.com>
2022-08-18 20:42:52 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,23 +6,37 @@ public class Duke {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        List<String> tasks = new ArrayList<>();
 
         String intro = withFormat("Pika Pikachu! (I am Pikachu!)\nPi-ka-chu?(Do you need any help?)\n");
         String byebye = withFormat("Pi?Pi-ka...(Alright...I will leave...)\n");
         System.out.println(intro);
 
-        Boolean done = false;
+        boolean done = false;
 
         //loop starts
         while (!done) {
-            String input = sc.nextLine() + '\n';
+            String input = sc.nextLine();
 
-            if (input.equals("bye\n")) {
+            if (input.equals("bye")) { //bye
                 done = true;
                 System.out.println(byebye);
-            } else {
-                System.out.println(withFormat(input));
+            } else if (input.equals("list")) { //list all items
+                int index = 1;
+                StringBuilder output = new StringBuilder();
+                for (String task: tasks) {
+                    output.append(index).append(". ").append(task).append('\n');
+                    index ++;
+                }
+                if (output.length() == 0) {
+                    System.out.println(withFormat("PikaPika(Not available)\n"));
+                } else {
+                    System.out.println(withFormat(String.valueOf(output)));
+                }
+            }
+            else { //add as tasks
+                tasks.add(input);
+                System.out.println(withFormat("Pikapi(added): " + input + '\n'));
             }
         }
     }
