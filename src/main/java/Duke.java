76a221593cf36b48acaf09b2b9d276b4413f071e
junwei-tan <junweitan@u.nus.edu>
2022-08-19 02:32:47 +0800

    Duke Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Duke {
-    private static Task[] tasks = new Task[100];
+    private static final Task[] tasks = new Task[100];
     private static int arrPointer = 0;
 
     public static void main(String[] args) throws IOException {
@@ -16,6 +16,7 @@ public class Duke {
             String userInput = bufferedReader.readLine();
             String[] userInputArr = userInput.split(" ", 2);
             String[] taskDetailsArr;
+            try {
             switch (userInputArr[0]) {
                 case "list":
                     listArrItems();
@@ -25,22 +26,48 @@ public class Duke {
                     inProgress = false;
                     break;
                 case "mark":
+                    if (userInputArr.length < 2) {
+                        throw new DukeException("\tOOPS!!! Please specify a task to mark.");
+                    }
                     markItem(Integer.parseInt(userInputArr[1]));
                     break;
                 case "unmark":
+                    if (userInputArr.length < 2) {
+                        throw new DukeException("\tOOPS!!! Please specify a task to unmark.");
+                    }
                     unmarkItem(Integer.parseInt(userInputArr[1]));
                     break;
                 case "todo":
+                    if (userInputArr.length < 2) {
+                        throw new DukeException("\tOOPS!!! The description of a todo cannot be empty.");
+                    }
                     addToTasks(new ToDo(userInputArr[1]));
                     break;
                 case "deadline":
+                    if (userInputArr.length < 2) {
+                        throw new DukeException("\tOOPS!!! The date/time of a deadline cannot be empty.");
+                    }
                     taskDetailsArr = userInputArr[1].split(" /by ");
+                    if (taskDetailsArr.length < 2) {
+                        throw new DukeException("\tOOPS!!! The date/time of a deadline cannot be empty.");
+                    }
                     addToTasks(new Deadline(taskDetailsArr[0], taskDetailsArr[1]));
                     break;
                 case "event":
+                    if (userInputArr.length < 2) {
+                        throw new DukeException("\tOOPS!!! The date/time of a event cannot be empty.");
+                    }
                     taskDetailsArr = userInputArr[1].split(" /at ");
+                    if (taskDetailsArr.length < 2) {
+                        throw new DukeException("\tOOPS!!! The date/time of a event cannot be empty.");
+                    }
                     addToTasks(new Event(taskDetailsArr[0], taskDetailsArr[1]));
                     break;
+                default:
+                    throw new DukeException("\tOOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
         bufferedReader.close();
@@ -68,19 +95,17 @@ public class Duke {
                 "\tNow you have " + arrPointer + " tasks in your list.");
     }
 
-    private static void markItem(int index) {
+    private static void markItem(int index) throws DukeException {
         if (index > arrPointer || index < 1) {
-            System.out.println("\tI can't mark this task. It's not in the list.");
-            return;
+            throw new DukeException("\tOOPS!!! Please specify a valid task.");
         }
         tasks[index - 1].isDone = true;
         System.out.println("\tNice! I've marked this task as done:\n\t\t" + tasks[index - 1]);
     }
 
-    private static void unmarkItem(int index) {
+    private static void unmarkItem(int index) throws DukeException {
         if (index > arrPointer || index < 1) {
-            System.out.println("\tI can't unmark this task. It's not in the list.");
-            return;
+            throw new DukeException("\tOOPS!!! Please specify a valid task.");
         }
         tasks[index - 1].isDone = false;
         System.out.println("\tOK, I've marked this task as not done yet:\n\t\t" + tasks[index - 1]);
