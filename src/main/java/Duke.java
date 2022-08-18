8ca91868d00f59212b45204cff20bce0faae7b85
rjkoh <koh.ruijie123@gmail.com>
2022-08-18 23:57:13 +0800

    Level 6. Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     private static class Task {
@@ -37,6 +38,10 @@ public class Duke {
                     ? "[X]"
                     : "[ ]";
             return comp + name;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
         }
     }
 
@@ -127,7 +132,8 @@ public class Duke {
 
          */
 
-        Task[] arr = new Task[100];
+        ArrayList<Task> arr = new ArrayList();
+        // Task[] arr = new Task[100];
         int count = 0;
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
@@ -139,27 +145,28 @@ public class Duke {
                 break;
             } else if (item.equals("list")) {
                 for (int i =0; i < count; i++) {
-                    System.out.println(arr[i].toString());
+                    String index = String.format("%d.", i+1);
+                    System.out.println(index + arr.get(i).toStr());
                 }
             } else if (item.startsWith("mark")) {
                 String str = item.replace("mark ", "");
                 int index = Integer.valueOf(str);
-                arr[index - 1].mark();
-                System.out.println("Nice! I've marked this task as done:\n" + arr[index - 1].toStr());
+                arr.get(index - 1).mark();
+                System.out.println("Nice! I've marked this task as done:\n" + arr.get(index - 1).toStr());
             } else if (item.startsWith("unmark")) {
                 String str = item.replace("unmark ", "");
                 int index = Integer.valueOf(str);
-                arr[index - 1].unmark();
-                System.out.println("OK, I've marked this task as not done yet:\n" + arr[index - 1].toStr());
+                arr.get(index - 1).unmark();
+                System.out.println("OK, I've marked this task as not done yet:\n" + arr.get(index - 1).toStr());
             } else if (item.startsWith("todo")) {
                 try {
                     String str = item.replace("todo", "");
-                    arr[count] = new Todo(str, count+1);
+                    arr.add(new Todo(str, count+1));
                     count++;
                     System.out.println(String.format("Got it. I've added this task:\n" +
                                     "%s\n" +
                                     "Now you have %d tasks in the list.",
-                            arr[count - 1].toStr(),
+                            arr.get(count - 1).toStr(),
                             count));
                 } catch (MissingDescriptionException err) {
                     System.out.println("OOPS!!! The description of a todo cannot be empty.");
@@ -168,12 +175,12 @@ public class Duke {
                 try{
                     String[] input = item.split("/by ");
                     String name = input[0].replace("deadline", "");
-                    arr[count] = new Deadline(name, count + 1, input[1]);
+                    arr.add(new Deadline(name, count + 1, input[1]));
                     count++;
                     System.out.println(String.format("Got it. I've added this task:\n" +
                                     "%s\n" +
                                     "Now you have %d tasks in the list.",
-                            arr[count - 1].toStr(),
+                            arr.get(count - 1).toStr(),
                             count));
                 } catch (MissingDescriptionException err) {
                     System.out.println("OOPS!!! The description of a deadline cannot be empty.");
@@ -184,18 +191,33 @@ public class Duke {
                 try{
                     String[] input = item.split("/at ");
                     String name = input[0].replace("event", "");
-                    arr[count] = new Event(name, count + 1, input[1]);
+                    arr.add(new Event(name, count + 1, input[1]));
                     count++;
                     System.out.println(String.format("Got it. I've added this task:\n" +
                                     "%s\n" +
                                     "Now you have %d tasks in the list.",
-                            arr[count - 1].toStr(),
+                            arr.get(count - 1).toStr(),
                             count));
                 } catch (MissingDescriptionException err) {
                     System.out.println("OOPS!!! The description of an event cannot be empty.");
                 } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("OOPS!!! The time of an event cannot be empty.");
-            }
+                    System.out.println("OOPS!!! The time of an event cannot be empty.");
+                }
+            } else if (item.startsWith("delete")) {
+                try {
+                    int index = Integer.valueOf(item.split(" ")[1]);
+                    Task t = arr.remove(index - 1);
+                    count--;
+                    String str =  String.format("Noted. I've removed this task:\n" +
+                            "%s\n" +
+                            "Now you have %d tasks in the list.",
+                            t.toStr(),
+                            count);
+                    System.out.println(str);
+
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("OOPS!!! No such task exists.");
+                }
             }
             else {
                 UnknownInputException err = new UnknownInputException();
