ba5e7bd3c6fe3be7eb268530d0f5d7af4081b76c
bensohh <lok200910@gmail.com>
2022-08-18 19:42:35 +0800

    Completed task A-TextUiTesting, removed the old method of adding tasks

--- src/main/java/ChatBox.java
+++ src/main/java/ChatBox.java
@@ -29,9 +29,10 @@ public class ChatBox {
         String DEADLINE = "deadline";
         String EVENT = "event";
 
+        //An instance of Scanner to keep track of the user's input
+        Scanner reader = new Scanner(System.in);
+
         while (true) {
-            //An instance of Scanner to keep track of the user's input
-            Scanner reader = new Scanner(System.in);
 
             //The entire statement (user's input)
             String UserInput = reader.nextLine();
@@ -85,46 +86,37 @@ public class ChatBox {
             //To store the string representation of the categorised task
             String task_description = "";
 
-            //To keep track of whether the user-input categorised the task into the 3 categories
-            boolean specified_task = false;
-
             if (FirstText.equals(TODO)) { //If it is a todo task
-                specified_task = true;
                 tasks_memory[array_index] = new Todo(array[1]);
                 task_description = "  " + tasks_memory[array_index].toString();
             } else if (FirstText.equals(DEADLINE)) { //If it is a deadline task
-                specified_task = true;
                 String task_info = array[1];
                 String[] info_arr = task_info.split(" /by ", 2);
                 tasks_memory[array_index] = new Deadline(info_arr[0], info_arr[1]);
                 task_description = "  " + tasks_memory[array_index].toString();
             } else if (FirstText.equals(EVENT)) { //If it is an event task
-                specified_task = true;
                 String task_info = array[1];
                 String[] info_arr = task_info.split(" /at ", 2);
                 tasks_memory[array_index] = new Event(info_arr[0], info_arr[1]);
                 task_description = "  " + tasks_memory[array_index].toString();
+            } else { //Removed the previous method of adding task without specifying type
+                System.out.println("__________________________________________________");
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                System.out.println("__________________________________________________");
+                continue;
             }
 
             //Printing the output on display
             System.out.println("__________________________________________________");
-            if (specified_task) { //If the task is categorised (3 categories), we will print accordingly
-                System.out.println("Got it. I have added this task:");
-                System.out.println(task_description);
-            } else { //Still supports the old way of adding tasks without categories
-                //Adding a new task to the list of task and displaying task added & old printing output
-                System.out.println("added: " + UserInput);
-                tasks_memory[array_index] = new Task(UserInput);
-            }
+            System.out.println("Got it. I have added this task:");
+            System.out.println(task_description);
 
             if (array_index == 0) { //For a single task
                 System.out.println("Now you have " + (array_index + 1) + " task in the list.");
             } else { //For multiple tasks
                 System.out.println("Now you have " + (array_index + 1) + " tasks in the list.");
             }
-
             System.out.println("__________________________________________________");
-
             array_index++;
         }
 
