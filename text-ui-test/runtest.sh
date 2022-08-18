988df8260d1672822bb9780dd39c35541b7530bf
tlx02 <tlx0202@gmail.com>
2022-08-18 11:53:35 +0800

    text-ui-test

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,11 +13,13 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
+
 if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
+# javac -cp ../src -Xlint:none -d ../bin $(find ../src/main/java/* | grep .java);
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ../bin Duke < input.txt > ACTUAL.TXT
