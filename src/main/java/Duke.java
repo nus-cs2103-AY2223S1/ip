4280860c91096b218d4d3cb5b79b17abb3589298
Mohamed Hamas <e0726186@u.nus.edu>
2022-08-18 19:04:51 +0800

    Level 6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,15 @@
-import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello!\nWhat can I do for you ?");
+        System.out.println("What can I do for you ?");
         Scanner help = new Scanner(System.in);
         class Task {
             protected String description;
@@ -80,7 +80,17 @@ public class Duke {
 
         }
 
-        Task[] lst = new Task[100];
+        class DukeException extends Exception {
+
+            public DukeException(String error) {
+                super(error);
+            }
+        }
+
+        ArrayList<Task> lst = new ArrayList<>();
+        for(int j = 0;j<100;j++) {
+            lst.add(null);
+        }
         int i = 0;
 
 
@@ -88,61 +98,78 @@ public class Duke {
            String str = help.nextLine();
             String first = str.split(" ")[0];
 
-           if (str.equals("bye")) {
+           if (first.equals("bye")) {
                System.out.println("Bye see you again buddy !");
                break;
            }
-           else if (str.equals("list")) {
+
+           else if (first.equals("list")) {
                System.out.println("This is your tasks in your list: \n");
                for (Task item : lst) {
                    if (item != null)
-                       System.out.println(item.num + "." + item);
+                       System.out.println(lst.indexOf(item) + 1 + "." + item);
                }
            }
 
-
            else if (first.equals("mark") ) {
-               Task current = lst[Integer.parseInt(str.split(" ")[1]) - 1];
+               Task current = lst.get(Integer.parseInt(str.split(" ")[1]) - 1);
                System.out.println("Nice I have marked this as done:");
                current.mark();
                System.out.println( " " + current);
 
            }
-           else if (str.split(" ")[1].equals("unmark") ) {
-               Task current = lst[Integer.parseInt(str.split(" ")[1]) - 1];
+           else if (first.equals("unmark") ) {
+               Task current = lst.get(Integer.parseInt(str.split(" ")[1]) - 1);
                System.out.println("Ok I have marked this as still to be done:");
                current.unmark();
                System.out.println(" " + current);
 
            }
+           else if (first.equals("delete")) {
+              Task current = lst.get(Integer.parseInt(str.split(" ")[1]) - 1);
+              lst.remove(current);
+               i--;
+               System.out.println("Noted I have removed this task");
+               System.out.println(current);
+               System.out.println("Now you have" + " " + i + " " + "tasks in list");
 
+
+
+           }
            else {
                if (first.equals("deadline")) {
+                   if (str.endsWith("deadline")) {
+                       throw new DukeException("Oops The description of deadline cannot be empty !");
+                   }
                    System.out.println("Got it.I've added this task");
                    String currD = str.substring(str.indexOf("deadline") + 8, str.indexOf("/by"));
-                   lst[i] = new Deadline(currD, i + 1, str.substring(str.indexOf("/by") + 3));
-                   System.out.println(lst[i]);
+                   lst.set(i,new Deadline(currD, i + 1, str.substring(str.indexOf("/by") + 3)));
+                   System.out.println(lst.get(i));
                    i++;
                    System.out.println("Now you have" + " " + i + " " + "tasks in list");
                } else if (first.equals("event")) {
+                   if (str.endsWith("event")) {
+                       throw new DukeException("Oops The description of event cannot be empty !");
+                   }
                    System.out.println("Got it.I've added this task");
                    String currE = str.substring(str.indexOf("event") + 5, str.indexOf("/at"));
-                   lst[i] = new Events(currE, i + 1, str.substring(str.indexOf("/at") + 3));
-                   System.out.println(lst[i]);
+                   lst.set(i, new Events(currE, i + 1, str.substring(str.indexOf("/at") + 3)));
+                   System.out.println(lst.get(i));
                    i++;
                    System.out.println("Now you have" + " " + i + " " + "tasks in list");
                }
                else if (first.equals("todo")) {
+                   if (str.endsWith("todo")) {
+                       throw new DukeException("Oops The description of todo cannot be empty !");
+                   }
                    System.out.println("Got it.I've added this task");
                    String currT = str.substring(str.indexOf("todo") + 4);
-                   lst[i] = new ToDos(currT, i + 1);
-                   System.out.println(lst[i]);
+                   lst.set(i, new ToDos(currT, i + 1));
+                   System.out.println(lst.get(i));
                    i++;
                    System.out.println("Now you have" + " " + i + " " + "tasks in list");
                } else {
-                   lst[i] = new Task(str, i + 1);
-                   i++;
-                   System.out.println("added: " + str);
+                 throw new DukeException("Oops Sorry I dont know what you are talking about :( ");
                }
            }
 
