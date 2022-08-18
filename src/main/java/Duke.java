5c9678632cdea1c7dddb6a3b6f72a2ab8a2a8fb0
Tex Tang <tex_tang@outlook.com>
2022-08-18 10:34:18 +0800

    Update Duke class
    * add bye command that exit the program
    * print out the command if it is a command other than bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String GREETING =
+            "##################################################\n" +
+            "||                                              ||\n" +
+            "||                Hello! I'm Ted                ||\n" +
+            "||            What can I do for you?            ||\n" +
+            "||                                              ||\n" +
+            "##################################################";
+
+    private static final String INPUT_PREFIX = "> ";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(GREETING);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(INPUT_PREFIX);
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again!");
+                break;
+            } else {
+                System.out.println(command);
+            }
+
+            System.out.print(INPUT_PREFIX);
+        }
+
+        scanner.close();
     }
 }
