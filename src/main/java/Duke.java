fb45f3e643c2cd2aaf453b4b3644610db6fc3475
yunruu <yunruuchin@gmail.com>
2022-08-18 14:25:48 +0800

    Level 2 completed; can add and list user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,25 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Duke {
+    static HashSet<String> vocabList = new HashSet<String>();
 
     public static void echo(String msg) {
         String lineBlock = "     -----------------------------------------";
         System.out.println(lineBlock);
         System.out.println("     " + msg);
+        System.out.println(lineBlock);
+    }
+
+    static void list(HashSet<String> vocabList) {
+        String lineBlock = "     -----------------------------------------";
+        int count = 1;
+
+        System.out.println(String.format("%s\n",lineBlock));
+        for (String text : vocabList) {
+            System.out.println(String.format("     %d. %s\n", count, text));
+            count++;
+        }
         System.out.println(lineBlock);
     }
 
@@ -16,19 +30,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n\n";
         String welcomeMessage = "> What can I do for you today? : )\n";
-        String toPrint = String.format("> Hello from\n %s%s", logo, welcomeMessage);
-        System.out.println(toPrint);
-        //System.out.println("Hello from\n" + logo + "\n" + welcomeMessage);
+        String printText = String.format("> Hello from\n %s%s", logo, welcomeMessage);
+        System.out.println(printText);
 
         while (true) {
             Scanner sc = new Scanner(System.in);
 
-            if (sc.hasNext()) {
-                String nextString = sc.next();
+            if (sc.hasNextLine()) {
+                String nextString = sc.nextLine();
 
                 if (nextString.equals("bye")) {
                     echo("bye");
                     break;
+                }
+
+                if (nextString.equals("list")) {
+                    list(vocabList);
+                }
+
+                else if (!vocabList.contains(nextString)) {
+                    vocabList.add(nextString);
+                    
+                    printText = String.format("added: %s", nextString);
+                    echo(printText);
                 }
 
                 else {
