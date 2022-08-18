e0e76bba751def40b5b0a1dc8b180c8c6f59ec7b
LAPTOP-Q59MTK9C\Pokezard <e0538156@u.nus.edu>
2022-08-18 17:25:18 +0800

    level 2

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,10 +5,22 @@ public class main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        String[] arr = new String[101];
         String input = sc.nextLine();
+        int curr = 0;
+        arr[curr] = input;
         while(!input.equals("bye")) {
-            System.out.println(input);
+            if(!input.equals("list")) {
+                //System.out.println(input); level 1
+                System.out.println(String.format("added: %s", input));
+            }
+            else {
+                for(int i = 0; i < curr; i++) {
+                    System.out.println(String.format("%s. %s", i + 1, arr[i]));
+                }
+            }
             input = sc.nextLine();
+            arr[++curr] = input;
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
