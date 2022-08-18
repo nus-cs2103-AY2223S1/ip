95a05ead7b319145056ea3d47f081812cd32c43d
Tex Tang <tex_tang@outlook.com>
2022-08-18 13:30:42 +0800

    Handling invalid input at each command

--- src/main/java/Ted.java
+++ src/main/java/Ted.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 
 public class Ted {
 
-    private static final String GREETING =
-            "##################################################\n" +
+    private static final String GREETING = "##################################################\n" +
                     "||                                              ||\n" +
                     "||                Hello! I'm Ted                ||\n" +
                     "||            What can I do for you?            ||\n" +
@@ -16,6 +15,11 @@ public class Ted {
     ArrayList<Task> tasks = new ArrayList<Task>();
 
     private void list(String arg) {
+        if (this.tasks.size() == 0) {
+            System.out.println("There is no tasks here. Feel free to add some tasks.");
+            return;
+        }
+
         for (int inputIndex = 0; inputIndex < tasks.size(); inputIndex++) {
             System.out.printf("%d.%s\n", inputIndex + 1, tasks.get(inputIndex));
         }
@@ -31,30 +35,86 @@ public class Ted {
         );
     }
 
-    private void mark(String arg) {
-        int index = Integer.parseInt(arg);
-        this.tasks.get(index - 1).markAsDone();
+    private void mark(String arg) throws InvalidInputException {
+        try {
+            int index = Integer.parseInt(arg);
+
+            if (this.tasks.size() == 0) {
+                throw new InvalidInputException("There is no tasks here. Feel free to add a task.");
+            }
+
+            if (index <= 0) {
+                throw new InvalidInputException("The number of task to be marked must be greater than 0.");
+            }
+
+            if (index > this.tasks.size()) {
+                throw new InvalidInputException(String.format("Error: The number of task to be marked must be less than or equal to %d.", this.tasks.size()));
+            }
+
+            this.tasks.get(index - 1).markAsDone();
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException("The number of task to be marked passed must be a number.");
+        }
     }
 
-    private void unmark(String arg) {
-        int index = Integer.parseInt(arg);
-        this.tasks.get(index - 1).unmark();
+    private void unmark(String arg) throws InvalidInputException {
+        try {
+            int index = Integer.parseInt(arg);
+
+            if (this.tasks.size() == 0) {
+                throw new InvalidInputException("There is no tasks here. Feel free to add a task.");
+            }
+
+            if (index <= 0) {
+                throw new InvalidInputException("The number of task to be unmarked must be greater than 0.");
+            }
+
+            if (index > this.tasks.size()) {
+                throw new InvalidInputException(String.format("The number of task to be unmarked must be less than or equal to %d.", this.tasks.size()));
+            }
+
+            this.tasks.get(index - 1).unmark();
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException("The number of task to be unmarked passed must be a number.");
+        }
     }
 
-    private void todo(String arg) {
+    private void todo(String arg) throws InvalidInputException {
+        if (arg.isEmpty()) {
+            throw new InvalidInputException("The description of todo must not be empty.");
+        }
+
         this.addTask(new ToDo(arg));
     }
 
-    private void deadline(String arg) {
+    private void deadline(String arg) throws InvalidInputException {
+        if (arg.isEmpty()) {
+            throw new InvalidInputException("The description of deadline must not be empty.");
+        }
+
         String[] inputs = arg.split(" /by ");
         String description = inputs[0];
+
+        if (inputs.length <= 1) {
+            throw new InvalidInputException("The deadline (use /by) of task must be set. If you wish to create a task without deadline, try using todo command.");
+        }
+
         String by = inputs[1];
         this.addTask(new Deadline(description, by));
     }
 
-    private void event(String arg) {
+    private void event(String arg) throws InvalidInputException {
+        if (arg.isEmpty()) {
+            throw new InvalidInputException("The description of event must not be empty.");
+        }
+
         String[] inputs = arg.split(" /at ");
         String description = inputs[0];
+
+        if (inputs.length <= 1) {
+            throw new InvalidInputException("The datetime (use /at) of event must be set. If you wish to create a task without date or time, try using todo command.");
+        }
+
         String at = inputs[1];
         this.addTask(new Event(description, at));
     }
@@ -68,23 +128,29 @@ public class Ted {
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             String command = input.split(" ")[0].toLowerCase();
-            String argument = input.length() > command.length() + 1 ? input.substring(command.length() + 1) : null;
+            String argument = input.length() > command.length() + 1 ? input.substring(command.length() + 1) : "";
 
-            if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again!");
-                break;
-            } else if (command.equals("list")) {
-                this.list(argument);
-            } else if (command.equals("mark")) {
-                this.mark(argument);
-            }  else if (command.equals("unmark")) {
-                this.unmark(argument);
-            } else if (command.equals("todo")) {
-                this.todo(argument);
-            } else if (command.equals("deadline")) {
-                this.deadline(argument);
-            } else if (command.equals("event")) {
-                this.event(argument);
+            try {
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again!");
+                    break;
+                } else if (command.equals("list")) {
+                    this.list(argument);
+                } else if (command.equals("mark")) {
+                    this.mark(argument);
+                }  else if (command.equals("unmark")) {
+                    this.unmark(argument);
+                } else if (command.equals("todo")) {
+                    this.todo(argument);
+                } else if (command.equals("deadline")) {
+                    this.deadline(argument);
+                } else if (command.equals("event")) {
+                    this.event(argument);
+                } else {
+                    System.out.println("I'm sorry. I don't understand what that means.");
+                }
+            } catch (InvalidInputException e) {
+                System.out.println(e.getMessage());
             }
 
             System.out.print(INPUT_PREFIX);
