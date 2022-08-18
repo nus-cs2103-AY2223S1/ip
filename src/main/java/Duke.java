9196067918845627107f99d7f22798310fc50687
fongyj <fongyj@hotmail.com>
2022-08-18 21:46:49 +0800

    Add add and list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static Scanner sc = new Scanner(System.in);
+    private static ArrayList<String> textList = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,23 +13,48 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        boolean terminate = false;
-        while (!terminate) {
+        int state = 0;
+        while (state >= 0) {
             String m = sc.nextLine();
-            printMessage(m);
-            if (m.equals("bye")) {
-                terminate = true;
-            }
+            state = handleMessage(m);
+        }
+    }
+
+    private static int handleMessage(String m) {
+        if (m.equals("bye")) {
+            printMessage("Bye. Hope to see you again soon!");
+            return -1;
+        } else if (m.equals("list")) {
+            String messageList = generateList();
+            printMessage(messageList);
+            return 1;
+        } else {
+            storeMessage(m);
+            printMessage("added: " + m);
+            return 2;
         }
     }
 
     private static void printMessage(String m) {
-        if (m.equals("bye")) {
-            m = "Bye. Hope to see you again soon!";
-        }
         String message = "    ____________________________________________________________\n    "
                 + m
                 + "\n    ____________________________________________________________\n";
         System.out.println(message);
+    }
+
+    private static void storeMessage(String m) {
+        Duke.textList.add(m);
+    }
+
+    private static String generateList() {
+        if (Duke.textList.size() == 0) {
+            return "";
+        }
+        String out = "";
+        int count = 1;
+        for (String s: Duke.textList) {
+            out += "\n    " + count++ + ". " + s;
+        }
+        return out.substring(5);
     }
 }
