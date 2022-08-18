4495ceea9714e612e95d89eda5f208886325d2dc
agarwal-anjali <e0866261@u.nus.edu>
2022-08-18 16:31:00 +0800

    Formatted and customized display

--- src/main/java/Alpha.java
+++ src/main/java/Alpha.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
-
 public class Alpha {
-    private static void welcomeMessage() {
-        System.out.println("\n-----------------\n" + "Hello, I'm ALPHA!" + "\n-----------------");
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[36m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static void main(String[] args) {
+        welcomeMessage();
+        enterMessage();
     }
+
+    private static void welcomeMessage() {
+        System.out.println("\n-----------------\n" + ANSI_BLUE + "Hello, I'm ALPHA!" + ANSI_RESET + "\n-----------------");
+    }
+
     private static void enterMessage() {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         switch (input) {
             case "bye": {
-                System.out.println(">> " + "Bye, See you soon!");
+                System.out.println(ANSI_YELLOW + ">> " + "Bye, See you soon!" + ANSI_RESET);
                 System.exit(0);
                 break;
             }
             default: {
-                System.out.println(">> " + input);
+                System.out.println(ANSI_YELLOW + ">> " + input + ANSI_RESET);
                 enterMessage();
             }
         }
-    }
-    public static void main(String[] args) {
-        welcomeMessage();
-        enterMessage();
     }
 }
