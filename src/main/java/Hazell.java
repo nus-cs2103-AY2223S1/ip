9ca770325ee7bfce990c09c76db57714ee399d48
heyzec <hz.tiang@gmail.com>
2022-08-18 12:20:39 +0800

    feat: add skeletal code to echo and exit

--- src/main/java/Hazell.java
+++ src/main/java/Hazell.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Hazell {
     public static void main(String[] args) {
         String logo = "  _    _               _ _ \n"
@@ -6,6 +8,17 @@ public class Hazell {
                 + " |  __  |/ _` |_  / _ \\ | |\n"
                 + " | |  | | (_| |/ /  __/ | |\n"
                 + " |_|  |_|\\__,_/___\\___|_|_|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello, I am Hazell!\nWhat can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String userinput = scanner.nextLine().strip();
+            if (userinput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.exit(0);
+            } else {
+                System.out.println(userinput);
+            }
+        }
     }
 }
