d1703272c549cfd2b6adb3aafaa22d99c763100d
TYKCodes <yikuangteh@hotmail.com>
2022-08-18 10:35:16 +0800

    Added greet and echo functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String EXITCOMMAND = "bye";
+    private static final String LINE = "\n========================================================";
+    private static final String WELCOMEMESSAGE = "Hello, my name is Duke!\nHow can I help you today?";
+    private static final String EXITMESSAGE = LINE + "\nGoodbye! Looking forward to see you again soon!\n(●'◡'●)ノ"
+            + LINE;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo + LINE + "\n" + WELCOMEMESSAGE + LINE);
+
+        // Echo user input
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equalsIgnoreCase("bye")) { // Stops loop if command is 'bye'
+            System.out.println(LINE + "\n" + input + LINE);
+            input = sc.nextLine();
+        }
+        System.out.println(EXITMESSAGE);
     }
 }
