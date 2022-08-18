c69924a79a74a2f9feb9d30af625f7a39c6e268f
Song Ivan <songivan45@gmail.com>
2022-08-18 18:29:06 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,63 +1,137 @@
 import java.util.*;
+import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm SmartBot\nWhat can I do for you?");
-        Task[] lst = new Task[100];
-        int counter = 0;
-        while(true) {
-            Scanner sc = new Scanner(System.in);
-            String first = sc.nextLine();
-            if(first.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-            else if(first.equals("list")) {
-                for (int i = 1; i < counter + 1; i++) {
-                    System.out.println((i) + ". " + lst[i - 1]);
-                }
-            }
-            else if(first.length() == 6 && first.substring(0, 4).equals("mark")) {
-                char index = first.charAt(5);
-                int number = Integer.parseInt(String.valueOf(index));
-                Task t = lst[number - 1];
-                t.mark();
-                System.out.println("Nice! I've marked this task as done:\n  [X] " + t.description);
-            }
-            else if(first.length() == 8 && first.substring(0, 6).equals("unmark")) {
-                char index = first.charAt(7);
-                int number = Integer.parseInt(String.valueOf(index));
-                Task t = lst[number - 1];
-                t.unmark();
-                System.out.println("OK, I've marked this task as not done yet:\n  [ ] " + t.description);
-            }
-            else if(first.length() >= 6 && first.substring(0, 4).equals("todo")) {
-                String description = first.substring(5);
-                Todo d = new Todo(description);
-                lst[counter] = d;
-                counter++;
-                System.out.println("Got it. I've added this task:\n " + d +
-                        "\nNow you have " + counter + " tasks in the list.");
-            }
-            else if(first.length() >= 10 && first.substring(0, 8).equals("deadline")) {
-                int sepPos = first.indexOf("/");
-                String description = first.substring(9, sepPos);
-                String by = first.substring(sepPos + 4);
+    public static int counter = 0;
+    public static void byeMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void listMessage(ArrayList<Task> arr, int counter) {
+        for (int i = 1; i < counter + 1; i++) {
+            System.out.println((i) + ". " + arr.get(i - 1));
+        }
+    }
+    
+    public static void markMessage(String str, ArrayList<Task> arr) throws DukeException{
+        char index = str.charAt(5);
+        int number = Integer.parseInt(String.valueOf(index));
+        if(number >= counter) {
+            throw new DukeException("No such task.");
+        }
+        Task t = arr.get(number - 1);
+        t.mark();
+        System.out.println("Nice! I've marked this task as done:\n  [X] " + t.description);
+    }
+
+    public static void unmarkMessage(String str, ArrayList<Task> arr) throws DukeException {
+        char index = str.charAt(7);
+        int number = Integer.parseInt(String.valueOf(index));
+        if(number >= counter) {
+            throw new DukeException("No such task.");
+        }
+        Task t = arr.get(number - 1);
+        t.unmark();
+        System.out.println("OK, I've marked this task as not done yet:\n  [ ] " + t.description);
+    }
+
+    public static void todoMessage(String str, ArrayList<Task> arr) throws DukeException{
+        if(str.length() > 5) {
+            String description = str.substring(5);
+            Todo d = new Todo(description);
+            arr.add(counter, d);
+            counter++;
+            System.out.println("Got it. I've added this task:\n " + d +
+                    "\nNow you have " + counter + " tasks in the list.");
+        } else {
+            throw new DukeException("The description of a todo cannot be empty.");
+        }
+    }
+
+    public static void deadlineMessage(String str, ArrayList<Task> arr) throws DukeException{
+        if(str.length() > 9) {
+            int sepPos = str.indexOf("/");
+            if (sepPos != -1) {
+                String description = str.substring(9, sepPos);
+                String by = str.substring(sepPos + 4);
                 Deadline l = new Deadline(description, by);
-                lst[counter] = l;
+                arr.add(counter, l);
                 counter++;
                 System.out.println("Got it. I've added this task:\n " + l +
                         "\nNow you have " + counter + " tasks in the list.");
+            } else {
+                throw new DukeException("You have to include the deadline");
             }
-            else if(first.length() >= 7 && first.substring(0, 5).equals("event")) {
-                int sepPos = first.indexOf("/");
-                String description = first.substring(6, sepPos);
-                String at = first.substring(sepPos + 4);
+        } else {
+            throw new DukeException("The description of a deadline cannot be empty.");
+        }
+    }
+    public static void eventMessage(String str, ArrayList<Task> arr) throws DukeException {
+        if (str.length() > 6) {
+            int sepPos = str.indexOf("/");
+            if (sepPos != -1) {
+                String description = str.substring(6, sepPos);
+                String at = str.substring(sepPos + 4);
                 Event e = new Event(description, at);
-                lst[counter] = e;
+                arr.add(counter, e);
                 counter++;
                 System.out.println("Got it. I've added this task:\n " + e +
                         "\nNow you have " + counter + " tasks in the list.");
+            } else {
+                throw new DukeException("You have to include the timings of an event");
+            }
+        } else {
+            throw new DukeException("The description of a event cannot be empty.");
+        }
+    }
+
+    public static void deleteMessage(String str, ArrayList<Task> arr) throws DukeException {
+        if (str.length() > 7) {
+            char index = str.charAt(7);
+            int number = Integer.parseInt(String.valueOf(index));
+            if(number >= counter) {
+                throw new DukeException("No such task.");
+            }
+            Task t = arr.get(number - 1);
+            arr.remove(number - 1);
+            counter--;
+            System.out.println("Noted. I've removed this task: \n " + t + "\nNow you have "
+                    + counter + " tasks in the list.");
+        }
+    }
+
+    
+    
+    public static void main(String[] args) throws DukeException {
+        System.out.println("Hello! I'm SmartBot\nWhat can I do for you?");
+        ArrayList<Task> lst = new ArrayList<Task>();;
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String first = sc.nextLine();
+            try {
+                if (first.equals("bye")) {
+                    byeMessage();
+                    break;
+                } else if (first.equals("list")) {
+                    listMessage(lst, counter);
+                } else if (first.length() == 6 && first.substring(0, 4).equals("mark")) {
+                    markMessage(first, lst);
+                } else if (first.length() == 8 && first.substring(0, 6).equals("unmark")) {
+                    unmarkMessage(first, lst);
+                } else if (first.length() >= 4 && first.substring(0, 4).equals("todo")) {
+                    todoMessage(first, lst);
+                } else if (first.length() >= 8 && first.substring(0, 8).equals("deadline")) {
+                    deadlineMessage(first, lst);
+                } else if (first.length() >= 5 && first.substring(0, 5).equals("event")) {
+                    eventMessage(first, lst);
+                } else if (first.length() == 8 && first.substring(0, 6).equals("delete")) {
+                    deleteMessage(first, lst);
+                }
+                else {
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException ex) {
+                    System.out.println(ex);
             }
         }
     }
