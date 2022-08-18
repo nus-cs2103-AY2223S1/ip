2e09a06f260c483e56d0a4602989799e7a894e34
Dilysss <e0772712@u.nus.edu>
2022-08-19 01:29:28 +0800

    Added unmark and horizontal lines, edited logic

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -26,20 +26,33 @@ public class Duke {
 
         int taskNum = 0;
 
-        System.out.println("Hello! I'm Duke\n");
+
+        drawLine();
+        System.out.println("Hello! I'm Duke");
         System.out.println("What tasks do you have to do?");
+        drawLine();
         Scanner sc = new Scanner(System.in);
         String str2 = sc.nextLine();
 
-        while (!str2.equals("bye")) {
+        //while (!str2.equals("bye")) {
+        while (true) {
             if (str2.equals("list")) {
                 showList(taskNum, tasks);
-                str2 = sc.nextLine();
+                if (sc.hasNextLine()) { str2 = sc.nextLine(); }
                 continue;
             }
             if (str2.length() > 4 && (str2.substring(0, 4)).equals("mark")) {
                 markDone(str2, tasks);
-                str2 = sc.nextLine();
+                drawLine();
+                //str2 = sc.nextLine();
+                if (sc.hasNextLine()) { str2 = sc.nextLine(); }
+                continue;
+            }
+
+            if (str2.length() > 6 && (str2.substring(0, 6)).equals("unmark")) {
+                markUndone(str2, tasks);
+                drawLine();
+                if (sc.hasNextLine()) { str2 = sc.nextLine(); }
                 continue;
             }
 
@@ -47,19 +60,29 @@ public class Duke {
                 addToDo(str2, tasks, taskNum);
                 taskNum++;
                 System.out.println("Now you have " + taskNum + " tasks in the list.");
-                str2 = sc.nextLine();
+                drawLine();
+                if (sc.hasNextLine()) { str2 = sc.nextLine(); }
                 continue;
             }
 
-            if (str2.length() == 4 && (str2.substring(0, 4)).equals("todo")) {
+            if (str2.equals("todo")) {
                 throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+            }
+
+            if (str2.equals("deadline")) {
+                throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+            }
+
+            if (str2.equals("event")) {
+                throw new DukeException("OOPS!!! The description of a event cannot be empty.");
             }
 
             if (str2.length() > 8 && (str2.substring(0, 8)).equals("deadline")) {
                 addDeadline(str2, tasks, taskNum);
                 taskNum++;
                 System.out.println("Now you have " + taskNum + " tasks in the list.");
-                str2 = sc.nextLine();
+                drawLine();
+                if (sc.hasNextLine()) { str2 = sc.nextLine(); }
                 continue;
             }
 
@@ -67,15 +90,22 @@ public class Duke {
                 addEvent(str2, tasks, taskNum);
                 taskNum++;
                 System.out.println("Now you have " + taskNum + " tasks in the list.");
-                str2 = sc.nextLine();
+                drawLine();
+                if (sc.hasNextLine()) { str2 = sc.nextLine(); }
                 continue;
             }
 
             if (str2.length() > 7 && (str2.substring(0, 6)).equals("delete")) {
                 deleteTask(str2, tasks);
                 taskNum--;
-                str2 = sc.nextLine();
+                drawLine();
+                if (sc.hasNextLine()) { str2 = sc.nextLine(); }
                 continue;
+            }
+
+            if (str2.equals("bye")) {
+                System.out.println("Bye! See you soon!");
+                break;
             }
 
             throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :(");
@@ -86,19 +116,22 @@ public class Duke {
             System.out.println("added: " + str2);
             str2 = sc.nextLine();*/
         }
-        System.out.println("Bye! See you soon!");
+        //System.out.println("Bye! See you soon!");
     }
 
     public static void showList(int taskNo, ArrayList<Task> arr1){
+        drawLine();
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskNo; i++) {
             int num = i + 1;
             System.out.println(num + ". " + arr1.get(i).toString());
         }
+        drawLine();
     }
 
     public static void markDone(String task, ArrayList<Task> arr1) {
         int taskToMark = 0;
+        drawLine();
         System.out.println("Nice! I've marked this task as done:");
 
         String strTaskToMark = "";
@@ -111,7 +144,23 @@ public class Duke {
         System.out.println("[" + arr1.get(taskToMark - 1).getStatusIcon() + "] " + arr1.get(taskToMark - 1).description);
     }
 
+    public static void markUndone(String task, ArrayList<Task> arr) {
+        int taskToUnmark = 0;
+        drawLine();
+        System.out.println("OK, I've marked this task as not done yet:");
+
+        String strTaskToUnmark = "";
+        for (int j = 7; j < task.length(); j++) {
+            strTaskToUnmark = strTaskToUnmark + task.charAt(j);
+        }
+
+        taskToUnmark = Integer.parseInt(strTaskToUnmark);
+        arr.get(taskToUnmark - 1).isDone = false;
+        System.out.println("[" + arr.get(taskToUnmark - 1).getStatusIcon() + "] " + arr.get(taskToUnmark - 1).description);
+    }
+
     public static void addToDo(String str, ArrayList<Task> arr, int num) {
+        drawLine();
         System.out.println("Got it. I've added this task:");
         ToDos newToDo = new ToDos(str.substring(5, str.length()));
         arr.add(num, newToDo);
@@ -119,6 +168,7 @@ public class Duke {
     }
 
     public static void addDeadline(String str, ArrayList<Task> arr, int num) {
+        drawLine();
         System.out.println("Got it. I've added this task:");
         int k = 9;
         String desc = "";
@@ -134,6 +184,7 @@ public class Duke {
     }
 
     public static void addEvent(String str, ArrayList<Task> arr, int num) {
+        drawLine();
         System.out.println("Got it. I've added this task:");
         int k = 6;
         String desc = "";
@@ -150,6 +201,7 @@ public class Duke {
 
     public static void deleteTask(String str, ArrayList<Task> arr) {
         int taskToDel = 0;
+        drawLine();
         System.out.println("Noted. I've removed this task:");
 
         String strTaskToDel = "";
@@ -161,5 +213,14 @@ public class Duke {
         System.out.println(arr.get(taskToDel).toString());
         arr.remove(taskToDel);
         System.out.println("Now you have " + arr.size() + " tasks in the list.");
+    }
+
+    public static void drawLine() {
+        String str = "";
+        for (int i = 0; i < 80; i++) {
+            str += "-";
+        }
+
+        System.out.println(str);
     }
 }
