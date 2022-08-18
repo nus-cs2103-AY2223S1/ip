7d6fd59b37726c749538bdbf00cb7f434f3b7b29
Ryan <lml.ryan@yahoo.com>
2022-08-19 03:42:16 +0800

    added Level 1. Greet, Echo, Exit

---
+++ src/main/java/Greet.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Greet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
