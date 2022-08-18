bd49e1db3bf49a0e2a071c40a547ad0946bd1468
Daniel Luo <hurinturin123@gmail.com>
2022-08-18 23:11:00 +0800

    Level 3: Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -4,21 +4,61 @@ public class Pixel {
 
     private static int count = 0;
     private Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
-    private String[] inputMemory = new String[100];
+    private Task[] inputTasks = new Task[100];
+
+    // char[] numbers = {"s", "s"};
+
+//    private int getListIndex(String userInput) {
+//        char[] inputCharArray = userInput.toCharArray();
+//        for (char character: inputCharArray) {
+//            if (numArray.contains()) {
+//                return Character.getNumericValue(character);
+//            }
+//        };
+//        return -1;
+//    }
 
     private void run() {
         String userInput = myScanner.nextLine();  // Read user input
 
         if (userInput.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
+
+        } else if (userInput.startsWith("mark ", 0)) {
+            String temp = userInput.substring(5, userInput.length());
+            // System.out.println(temp);
+            int indexToChange = Character.getNumericValue(temp.charAt(0));
+            // System.out.println(indexToChange);
+            if ((indexToChange > 0) || (indexToChange < 100)) {
+                inputTasks[indexToChange - 1].markAsDone();
+            }
+            System.out.println(" Nice! I've marked this task as done:");
+            System.out.println("[" + inputTasks[indexToChange - 1].getStatusIcon() + "] " + inputTasks[indexToChange - 1]);            run();
+            run();
+
+        } else if (userInput.startsWith("unmark ", 0)) {
+            String temp = userInput.substring(7, userInput.length());
+            // System.out.println(temp);
+            int indexToChange = Character.getNumericValue(temp.charAt(0));
+            // System.out.println(indexToChange);
+            if ((indexToChange > 0) || (indexToChange < 100)) {
+                inputTasks[indexToChange - 1].markAsNotDone();
+            }
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("[" + inputTasks[indexToChange - 1].getStatusIcon() + "] " + inputTasks[indexToChange - 1]);
+            run();
+
         } else if (userInput.equals("list")) {
             // System.out.println(inputMemory.length);
+            System.out.println("Here are the tasks in your list:");
             for (int i = 0; i < count; i++) {
-                System.out.println((i + 1) + ". " + inputMemory[i]);
+                Task currentTask = inputTasks[i];
+                System.out.println((i + 1) + ".[" + currentTask.getStatusIcon() + "] " + currentTask);
             }
             run();
+
         } else {
-            inputMemory[count] = userInput; // Stores user input
+            inputTasks[count] = new Task(userInput); // Stores user input
             System.out.println(userInput);  // Output user input
             count += 1;
             run();
@@ -38,3 +78,4 @@ public class Pixel {
         test.run();
     }
 }
+
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+
+    private String description;
+    private boolean isDone = false;
+
+    public Task (String description) {
+        this.description = description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " "); // mark done task with X
+    }
+
+    public boolean taskDone() {
+        return this.isDone;
+    }
+}
