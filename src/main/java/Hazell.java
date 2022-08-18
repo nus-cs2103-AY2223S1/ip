f72705b5114b06af38d2b28c35be2030f45d6171
heyzec <hz.tiang@gmail.com>
2022-08-18 12:26:25 +0800

    feat: add reply function for formatting of replies

--- src/main/java/Hazell.java
+++ src/main/java/Hazell.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class Hazell {
+    public static void reply(String msg) {
+        String DIVIDER = "\t____________________________________________________________";
+        System.out.println(DIVIDER);
+        for (String line : msg.split("\n")) {
+            System.out.println("\t" + line);
+        }
+        System.out.println(DIVIDER);
+    }
     public static void main(String[] args) {
         String logo = "  _    _               _ _ \n"
                 + " | |  | |             | | |\n"
@@ -9,15 +17,15 @@ public class Hazell {
                 + " | |  | | (_| |/ /  __/ | |\n"
                 + " |_|  |_|\\__,_/___\\___|_|_|\n";
         System.out.println(logo);
-        System.out.println("Hello, I am Hazell!\nWhat can I do for you?");
+        reply("Hello, I am Hazell!\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String userinput = scanner.nextLine().strip();
             if (userinput.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                reply("Bye. Hope to see you again soon!");
                 System.exit(0);
             } else {
-                System.out.println(userinput);
+                reply(userinput);
             }
         }
     }
