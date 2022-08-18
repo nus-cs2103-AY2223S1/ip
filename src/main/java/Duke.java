1adc1f40cac4cd929869ec2aaa59387c564fdff0
unknown <nathaniel2048@gmail.com>
2022-08-18 14:58:46 +0800

    Add the ability to store text entered by the user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,14 +11,22 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
          */
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        String[] store = new String[100];
+        int count = 0;
         while (true) {
             Scanner sc = new Scanner(System.in);
-            String s = sc.next();
+            String s = sc.nextLine();
             if (s.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (s.equals("list")) {
+                for (int a = 0; a < count; a++) {
+                    System.out.println((a + 1) + ". " + store[a]);
+                }
             } else {
-                System.out.println(s);
+                store[count] = s;
+                count++;
+                System.out.println("added: " + s);
             }
         }
     }
