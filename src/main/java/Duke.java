0d84d4e9b147f9ffb7fbc06738428d29c4052b11
shawnkai <e0525991@u.nus.edu>
2022-08-18 12:16:45 +0800

    edited duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,26 +9,60 @@ public class Duke {
         int number = 1;
         String dummyString;
         int counter;
+        int start;
+        int end;
         String input = myObj.nextLine();  // Read user input
+
+
         while (!input.equals("bye")) {
-            if ((input.length() == 6) && input.substring(0, 4).equals("mark")) {
+
+            if (input.startsWith("mark")) {
                 dummyString = input.substring(5); //get number of task
                 counter = Integer.parseInt(dummyString);//convert to int
                 tasks[counter].mark();
                 System.out.println("Nice! I've marked this task as done:\n" +
                         "[" + tasks[counter].getStatusIcon() + "] " + tasks[counter].getDescription());
-            } else if (input.length() == 8 && input.substring(0, 6).equals("unmark")) {
+
+            } else if (input.startsWith("unmark")) {
                 dummyString = input.substring(7); //get number of task
                 counter = Integer.parseInt(dummyString);//convert to int
                 tasks[counter].unmark();
                 System.out.println("OK, I've marked this task as not done yet:\n" +
                         "[" + tasks[counter].getStatusIcon() + "] " + tasks[counter].getDescription());
+
             } else if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i < number; i++) { //iterate through all tasks
-                    System.out.println(String.valueOf(i) + ".[" + tasks[i].getStatusIcon() + "] " +
-                            tasks[i].getDescription());
+                    System.out.println(String.valueOf(i) + "." + tasks[i].toString());
                 }
+            } else if (input.startsWith("todo")) {
+                start = 5;
+                tasks[number] = new Todo(input.substring(start));
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks[number].toString());
+                System.out.println("Now you have " + String.valueOf(number) + " tasks in the list.");
+                number += 1;
+
+            } else if (input.startsWith("deadline")) {
+                counter = input.indexOf("/");
+                start = 9;
+                end = counter - 1;
+                tasks[number] = new Deadline(input.substring(start, end), input.substring(counter + 4));
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks[number].toString());
+                System.out.println("Now you have " + String.valueOf(number) + " tasks in the list.");
+                number += 1;
+
+            } else if (input.startsWith("event")) {
+                counter = input.indexOf("/");
+                start = 6;
+                end = counter - 1;
+                tasks[number] = new Event(input.substring(start, end), input.substring(counter + 4));
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks[number].toString());
+                System.out.println("Now you have " + String.valueOf(number) + " tasks in the list.");
+                number += 1;
+
             } else { //adding a task
 
                 tasks[number] = new Task(input);
