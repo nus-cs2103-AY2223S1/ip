ba6f4dfdce027696b8c9052a08fa9ac3b0e0eb45
optionalemon <xuyi9272@gmail.com>
2022-08-18 20:12:47 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.*;
 public class Duke {
+    final static String HORIZON = "____________________________________________________________\n";
+    static String withFormat(String str) {
+        return HORIZON + str + HORIZON;
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+
+        String intro = withFormat("Pika Pikachu! (I am Pikachu!)\nPi-ka-chu?(Do you need any help?)\n");
+        String byebye = withFormat("Pi?Pi-ka...(Alright...I will leave...)\n");
+        System.out.println(intro);
+
+        Boolean done = false;
+
+        //loop starts
+        while (!done) {
+            String input = sc.nextLine() + '\n';
+
+            if (input.equals("bye\n")) {
+                done = true;
+                System.out.println(byebye);
+            } else {
+                System.out.println(withFormat(input));
+            }
+        }
     }
 }
