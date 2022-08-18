125496ccd64130e9aeb745a78413734aa824ccdd
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-08-18 18:22:56 +0800

    Seperate features into distinct functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+
+    public static void introduceDuke(){
         String intro = "I once wandered these halls, centuries ago. I am Duke Aemon of Old.\n";
         String quote1 = "Indeed, my memory is long when I am but a ghost of a memory myself..." +
-                "\nBut you are young blood. What brings you to these ancient halls?";
+                "\nBut you are young blood. What brings you to these ancient halls?"
+                +"\n***********************************************************************";
 
         System.out.println("Welcome, my friend!\n" + intro + quote1);
-        String exitCmd = "bye";
+    }
 
+    public static void echoAndExit(){
+        String exitCmd = "bye";
         while(true){
             Scanner readinput = new Scanner(System.in);
             String userMsg = readinput.nextLine();
             if(!userMsg.equals(exitCmd)) {
-                System.out.println(userMsg);
+                System.out.println(userMsg + "\n" + "***********************************************************************\n");
             } else {
                 System.out.println("Ah! And so we part here today. \n We may yet meet again. Farewell, my friend!");
                 break;
             }
         }
+    }
 
-
-//        readinput.close();
+    public static void main(String[] args) {
+        introduceDuke();
+        echoAndExit();
     }
 }
