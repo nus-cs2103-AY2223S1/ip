b1ea0b842fb5e8b75d7da48517d71429af4d03f2
yusiangeng <engyusiang@icloud.com>
2022-08-18 14:29:19 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,32 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    static List<Task> tasks = new ArrayList<>();
+
+    private static void markTask(String input, boolean done) {
+        String[] splitInput = input.split(" ");
+        if (splitInput.length < 2) {
+            System.out.println("Please specify the number of the task");
+            return;
+        }
+        Task task;
+        try {
+            int taskIdx = Integer.parseInt(splitInput[1]);
+            task = tasks.get(taskIdx - 1);
+            if (done) {
+                task.mark();
+            } else {
+                task.unmark();
+            }
+            String successMessage = done
+                    ? "Nice! I've marked this task as done:\n  %s\n"
+                    : "OK, I've marked this task as not done yet:\n  %s\n";
+            System.out.printf(successMessage, task.toString());
+        } catch (Exception e) {
+            System.out.println("That is not a valid task number!");
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,12 +36,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         System.out.println("Hello! I'm Duke\n" +
                            "What can I do for you?\n");
-
-        List<String> items = new ArrayList<>();
-
         Scanner s = new Scanner(System.in);
         while (true) {
             String input = s.nextLine();
@@ -23,11 +45,19 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
-                for (int i = 0; i < items.size(); i++) {
-                    System.out.println(i + 1 + ". " + items.get(i));
+                if (tasks.size() == 0) {
+                    System.out.println("No tasks to show");
+                } else {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i + 1 + "." + tasks.get(i));
+                    }
                 }
+            } else if (input.startsWith("mark")) {
+                markTask(input, true);
+            } else if (input.startsWith("unmark")) {
+                markTask(input, false);
             } else {
-                items.add(input);
+                tasks.add(new Task(input));
                 System.out.println("added: " + input);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), description);
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+}
