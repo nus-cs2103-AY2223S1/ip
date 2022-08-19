e6111a587b76eb579460382a752511d7b0c060e6
Charles Lim <charleslimjh@gmail.com>
2022-08-19 21:41:08 +0800

    implemented mark/unmark functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
     private static ArrayList<Task> db = new ArrayList<>(10);
     private static Scanner sc = new Scanner(System.in);
 
+    // Main driver code for DukeBot
     public static void main(String[] args) {
         greet();
         while (true) {
@@ -16,6 +17,9 @@ public class Duke {
         }
     }
 
+    /**
+     * Welcome message that is printed upon starting the bot.
+     */
     public static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -25,12 +29,24 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
     }
 
+    /**
+     * Prompts the user for input.
+     * @param sc Scanner object to read user input
+     * @return User input for processing
+     */
     public static String getInput(Scanner sc) {
         System.out.println("What can I do for you?");
         return sc.nextLine();
     }
 
+    /**
+     * Checks the user input and performs the necessary instruction.
+     * @param s User input
+     * @return 0 if "bye" is input, 1 for successfully running any other instruction
+     */
     public static int processInput(String s) {
+        String[] userInput = s.split(" ", 2);
+
         if (s.equals("bye")) {
             System.out.println("Bye. Hope to see you again!");
             return 0;
@@ -38,6 +54,18 @@ public class Duke {
             for (int i = 0; i < db.size(); i++) {
                 System.out.println(i + 1 + ". " + db.get(i).toString());
             }
+            return 1;
+        } else if (userInput[0].equals("mark")) {
+            Task tmp = db.get(Integer.parseInt(userInput[1]) - 1);
+            tmp.setDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(tmp);
+            return 1;
+        } else if (userInput[0].equals("unmark")) {
+            Task tmp = db.get(Integer.parseInt(userInput[1]) - 1);
+            tmp.setUndone();
+            System.out.println("OK, I've marked this task as undone:");
+            System.out.println(tmp);
             return 1;
         } else {
             db.add(new Task(s));
