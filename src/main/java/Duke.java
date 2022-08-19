f485a406d81a3d0d2c35add17a9b1ef6fa2cf143
Gavzzz <gavinchowb@gmail.com>
2022-08-20 01:28:50 +0800

    Level 4 implemented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 
 public class Duke {
 
+    public Task[] tasks;
+
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -15,7 +17,8 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n What can I do for you?");
         System.out.println("___________________________________");
 
-        ArrayList<Task> list = new ArrayList<>();
+        Task[] tasks = new Task[100];
+        int counter = 0;
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
@@ -25,44 +28,82 @@ public class Duke {
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 System.out.println("___________________________________");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println( (i+1) + "." + "[" + list.get(i).getStatusIcon()
-                     + "] " +list.get(i).description);
+                for (int i = 0; i < counter; i++) {
+                    System.out.println( (i+1) + "."
+                            + tasks[i].toString());
                 }
                 System.out.println("___________________________________");
-                input = sc.next();
+                input = sc.nextLine();
 
             }
 
-            if (input.equals("mark")) {
+            else if (input.equals("mark")) {
                 int number = sc.nextInt();
-                list.get(number-1).mark();
+                tasks[number-1].mark();
                 System.out.println("___________________________________");
                 System.out.println("Nice! I've marked this task done: " + "\n"
-                        + "[" + list.get(number-1).getStatusIcon() + "] " +
-                        list.get(number-1).description);
+                        + "[" + tasks[number-1].getStatusIcon() + "] " +
+                        tasks[number-1].description);
                 System.out.println("___________________________________");
                 input = sc.next();
             }
 
-            if (input.equals("unmark")) {
+            else if (input.equals("unmark")) {
                 int number = sc.nextInt();
-                list.get(number-1).unmark();
+                tasks[number-1].unmark();
                 System.out.println("___________________________________");
                 System.out.println("OK, I've marked this task as not done yet: " + "\n"
-                        + "[" + list.get(number-1).getStatusIcon() + "] " +
-                        list.get(number-1).description);
+                        + "[" + tasks[number-1].getStatusIcon() + "] " +
+                        tasks[number-1].description);
                 System.out.println("___________________________________");
                 input = sc.next();
+            }
+
+            else if (input.equals("deadline")) {
+                String what = sc.nextLine();
+                String byWhen = sc.nextLine();
+                tasks[counter] = new Deadline(what, byWhen);
+                System.out.println("___________________________________");
+                System.out.println("Got it. I've added this task:" + "\n"
+                        + "  " + tasks[counter].toString() + "\n"
+                        + "Now you have " + (counter+1) + " tasks in the list.");
+                System.out.println("___________________________________");
+                counter++;
+                input = sc.nextLine();
+            }
+
+            else if (input.equals("todo")) {
+                String what = sc.nextLine();
+                tasks[counter] = new Todo(what);
+                System.out.println("___________________________________");
+                System.out.println("Got it. I've added this task:" + "\n"
+                        + "  " + tasks[counter].toString() + "\n"
+                        + "Now you have " + (counter+1) + " tasks in the list.");
+                System.out.println("___________________________________");
+                counter++;
+                input = sc.nextLine();
+            }
+
+            else if (input.equals("event")) {
+                String what = sc.nextLine();
+                String atWhen = sc.nextLine();
+                tasks[counter] = new Event(what, atWhen);
+                System.out.println("___________________________________");
+                System.out.println("Got it. I've added this task:" + "\n"
+                        + "  " + tasks[counter].toString() + "\n"
+                        + "Now you have " + (counter+1) + " tasks in the list.");
+                System.out.println("___________________________________");
+                counter++;
+                input = sc.nextLine();
             }
 
             else {
-                Task task = new Task(input);
-                list.add(task);
+                tasks[counter] = new Task(input);
                 System.out.println("___________________________________");
                 System.out.println("added: " + input);
                 System.out.println("___________________________________");
-                input = sc.next();
+                counter++;
+                input = sc.nextLine();
             }
 
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,4 +19,9 @@ public class Task {
         this.isDone = false;
     }
 
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + description;
+    }
+
 }
