70b01f77e9cac1fbad245ac823c37ea8d84dc66c
Lin Leyi <>
2022-08-19 13:02:38 +0800

    Level-2 Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,46 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
         String line = " _______________________________________ \n";
         System.out.println(line + " I'm Dukie\n" + " What can I do for you?\n" + line);
 
-        boolean noBye = true;
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String userInput = "0";
 
         while (true) {
+
             userInput = myObj.nextLine();  // Read user input
 
             if (userInput.equals("bye")) {
                 System.out.println("Goodbaiiiii\n");
                 System.exit(0);
+            } else if (userInput.equals("list")) {
+                if (list.isEmpty()) {
+                    System.out.println(line + "nuuuu list empty! add an item first :-DD\n" + line);
+                } else {
+                    System.out.println(line + makeList(list) + line);
+                }
+            } else {
+                list.add(userInput);
+                System.out.println(line + " added: " + userInput + "\n" + line);  // Output user input
             }
 
-            System.out.println(line + " " + userInput + "\n" + line);  // Output user input
+
+        }
+    }
+
+    public static String makeList(ArrayList<String> ls) {
+        String s = "";
+
+        for (int i = 0; i < ls.size(); i++) {
+            int index = i + 1;
+            s += index + ". " + ls.get(i) + "\n";
         }
 
-
+        return s;
     }
 }
