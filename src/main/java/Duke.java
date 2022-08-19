e826f9466f597670b06f5b170a42238b3e78bc44
dr-arrgghh <76149499+dr-arrgghh@users.noreply.github.com>
2022-08-19 22:06:41 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.*;
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        Duke newDuke = new Duke();
+        newDuke.chatDuke();
+
+
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
     }
+
+    public void chatDuke(){
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        if (str.equals("bye")){
+            System.out.println("Bye. Hope to see you again soon");
+        } else {
+            System.out.println(str);
+            chatDuke();
+        }
+
+
+    }
+
+
 }
