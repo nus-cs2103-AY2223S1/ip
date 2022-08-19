2aca0524cec59de88401dd00de94b5a31f151138
yilinzyl <zhengyilin02@gmail.com>
2022-08-19 10:37:26 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static String getOutput(String input) {
+        if (input.equals("bye")) {
+            return "Bye. Hope to see you again soon!";
+        } else {
+            return input;
+        }
+    }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println(logo + "\nHello! I'm Duke\n" + "What can I do for you?");
+        String command = scanner.nextLine();
+        while (true) {
+            String output = getOutput(command);
+            System.out.println(output);
+            if (command.equals("bye")) {
+                break;
+            } else {
+                command = scanner.nextLine();
+            }
+        }
     }
 }
