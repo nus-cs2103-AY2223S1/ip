fdbe2525dde340a16d8cc05ceb67fcd62978c2d3
Lin Leyi <>
2022-08-19 22:45:37 +0800

    commit level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,22 +18,31 @@ public class Duke {
             if (userInput.equals("bye")) {
                 System.out.println("Goodbaiiiii\n");
                 System.exit(0);
-            } else if (userInput.equals("list")) {
+            } else if (userInput.startsWith("list")) {
                 if (list.isEmpty()) {
                     System.out.println(line + "nuuuu list empty! add an item first :-DD\n" + line);
                 } else {
                     System.out.println(line + makeList(list) + line);
                 }
-            } else if (userInput.contains("mark")){
+            } else if (userInput.startsWith("mark")){
                 String task = userInput.substring(5);
-                markDone(task, list);
-                System.out.println(line + "okie! " + task + " is done ~\n" + line);
+                String s = markDone(task, list);
+                System.out.println(line + s + line);
+            } else if (userInput.startsWith("unmark")){
+                String task = userInput.substring(7);
+                String s = unmarkDone(task, list);
+                System.out.println(line + s + line);
+            } else if (userInput.startsWith("todo") || userInput.startsWith("deadline") || userInput.startsWith("event")) {
+                Task t = new Task(userInput.substring(userInput.indexOf(" ") + 1),
+                        userInput.substring(0, userInput.indexOf(" ")).toUpperCase(), false);
+                list.add(t);
+                System.out.println(line + " okie! i've added: \n " + t.toString() + "\n now you have " + list.size() + " task(s) in your list!\n" + line);  // Output user input
             } else {
-                list.add(new Task(userInput, false));
-                System.out.println(line + " added: " + userInput + "\n" + line);  // Output user input
+                /* list.add(new Task(userInput.substring(userInput.indexOf(" ") + 1),
+                        userInput.substring(0, userInput.indexOf(" ")).toUpperCase(), false));
+                System.out.println(line + " added: " + userInput + "\n" + line);  // Output user input */
+                System.out.println("reach end");
             }
-
-
         }
     }
 
@@ -42,17 +51,31 @@ public class Duke {
 
         for (int i = 0; i < ls.size(); i++) {
             int index = i + 1;
-            s += index + "." + ls.get(i).isDoneString() + " " + ls.get(i).getTask() + "\n";
+            // s += index + "." + ls.get(i).isDoneString() + " " + ls.get(i).getTask() + "\n";
+            s += " " + index + "." + ls.get(i).toString() + "\n";
+
         }
 
         return s;
     }
 
-    public static void markDone(String task, ArrayList<Task> ls) {
+    public static String markDone(String task, ArrayList<Task> ls) {
         for (int i = 0; i < ls.size(); i++) {
             if (ls.get(i).getTask().equals(task)) {
                 ls.get(i).markDone();
+                return " okie! " + task + " is done ~\n [X] " + task + "\n";
             }
         }
+        return task + " not found :(\n";
+    }
+
+    public static String unmarkDone(String task, ArrayList<Task> ls) {
+        for (int i = 0; i < ls.size(); i++) {
+            if (ls.get(i).getTask().equals(task)) {
+                ls.get(i).unmarkDone();
+                return " owh ;< so you haven't done " + task + ". unmarked ~\n [ ] " + task + "\n";
+            }
+        }
+        return task + " not found :(\n";
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,32 @@
+enum TaskType {
+    TODO, DEADLINE, EVENT;
+}
+
 public class Task {
     private boolean done;
     private String taskName;
+    private TaskType taskType;
+    private String time;
 
-    public Task (String taskName, boolean done) {
-        this.taskName = taskName;
+    public Task (String taskName, String taskType, boolean done) {
         this.done = done;
+        if (taskType.equals("TODO")) {
+            this.taskType = TaskType.TODO;
+            this.taskName = taskName;
+            this.time = "";
+        } else if (taskType.equals("DEADLINE")) {
+            this.taskType = TaskType.DEADLINE;
+            this.taskName = taskName.substring(0, taskName.indexOf("/") - 1);
+            this.time = "(" + taskName.substring(taskName.indexOf("/") + 1) + ")";
+        } else if (taskType.equals("EVENT")) {
+            this.taskType = TaskType.EVENT;
+            this.taskName = taskName.substring(0, taskName.indexOf("/") - 1);
+            this.time = "(" + taskName.substring(taskName.indexOf("/") + 1) + ")";
+        }
     }
 
     public String getTask() {
-        return taskName;
+        return this.taskName;
     }
 
     public String isDoneString() {
@@ -18,10 +36,20 @@ public class Task {
         return "[ ]";
     }
 
+    @Override
+    public String toString() {
+        if (done) {
+            return "[" + taskType.toString().charAt(0) + "][X] " + this.taskName + " " + this.time;
+        }
+        return "[" + taskType.toString().charAt(0) + "][ ] " + this.taskName + " " + this.time;
+    }
+
     public void markDone() {
         done = true;
     }
 
-
+    public void unmarkDone() {
+        done = false;
+    }
 
 }
