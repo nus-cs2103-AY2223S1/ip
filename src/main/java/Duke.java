356ecd5bdb730a71cfffe07c785b464b0ce43600
chengda300 <chengda300@gmail.com>
2022-08-19 13:07:13 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,36 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     static boolean running = true;
-    static Task[] tasks = new Task[100];
-    static int count = 0;
+    static ArrayList<Task> tasks = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
     public void addTodo(String s) {
         Todo t = new Todo(s);
-        tasks[count] = t;
-        count++;
+        tasks.add(t);
         System.out.println("added: " + t);
-        System.out.println("You have " + count + " tasks in the list now");
+        System.out.println("You have " + tasks.size() + " tasks in the list now");
     }
 
     public void addDeadline(String s, String by) {
         Deadline t = new Deadline(s, by);
-        tasks[count] = t;
-        count++;
+        tasks.add(t);
         System.out.println("added: " + t);
-        System.out.println("You have " + count + " tasks in the list now");
+        System.out.println("You have " + tasks.size() + " tasks in the list now");
     }
 
     public void addEvent(String s, String at) {
         Event t = new Event(s, at);
-        tasks[count] = t;
-        count++;
+        tasks.add(t);
         System.out.println("added: " + t);
-        System.out.println("You have " + count + " tasks in the list now");
+        System.out.println("You have " + tasks.size() + " tasks in the list now");
     }
 
     public void getList() {
-        for (int i = 0; i < count; i++) {
-            System.out.println(i + 1 + "." + tasks[i]);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + 1 + "." + tasks.get(i));
         }
-        System.out.println("There are " + count + " tasks in the list.");
+        System.out.println("There are " + tasks.size() + " tasks in the list.");
     }
 
     public void mark(Task t) {
@@ -43,7 +40,14 @@ public class Duke {
     public void unmark(Task t) {
         t.unmark(t);
     }
-    //@@author johndoe-reused
+
+    public void delete(int i) {
+        Task t = tasks.get(i);
+        tasks.remove(i);
+        System.out.println("I have deleted this task:\n  " + t + "\n"
+                + "Now you have " + tasks.size() + " tasks on the list.");
+    }
+    //@@author chengda300
     //Reused from https://nus-cs2103-ay2223s1.github.io/website/schedule/week2/project.html
     // with minor modifications
     public class Task {
@@ -112,7 +116,7 @@ public class Duke {
 
         @Override
         public String toString() {
-            return "[E]" + super.toString() + " (by: " + this.at + ")";
+            return "[E]" + super.toString() + " (at: " + this.at + ")";
         }
     }
     //@@author
@@ -145,20 +149,28 @@ public class Duke {
             } else if (input.equals("list")) {
                 d.getList();
             } else if (input.startsWith("mark")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                if (index >= count) {
-                    System.out.println("No such task is found.");
-                } else {
-                    Task t = tasks[index];
-                    d.mark(t);
+                try {
+                    int index = Integer.parseInt(input.substring(5)) - 1;
+                    if (index >= tasks.size() || index < 0) {
+                        System.out.println("I cannot mark a task that does not exist!");
+                    } else {
+                        Task t = tasks.get(index);
+                        d.mark(t);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("I cannot mark a task that does not exist!");
                 }
             } else if (input.startsWith("unmark")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                if (index >= count) {
-                    System.out.println("No such task is found.");
-                } else {
-                    Task t = tasks[index];
-                    d.unmark(t);
+                try {
+                    int index = Integer.parseInt(input.substring(7)) - 1;
+                    if (index >= tasks.size() || index < 0) {
+                        System.out.println("I cannot unmark a task that does not exist!");
+                    } else {
+                        Task t = tasks.get(index);
+                        d.unmark(t);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("I cannot unmark a task that does not exist!");
                 }
             } else if (input.startsWith("todo")) {
                 if (input.length() < 6) {
@@ -168,8 +180,8 @@ public class Duke {
                 }
             } else if (input.startsWith("deadline")) {
                 if (! input.contains("/by")) {
-                    System.out.println("Specify a deadline type task by typing " +
-                            "deadline <task> /by <deadline>");
+                    System.out.println("Specify a deadline type task by typing "
+                            + "deadline <task> /by <deadline>");
                 } else if (input.indexOf("/by") < 11) {
                     System.out.println("You did not specify a task.");
                 } else if (input.indexOf("/by") + 4 >= input.length()) {
@@ -180,8 +192,8 @@ public class Duke {
                 }
             } else if (input.startsWith("event")) {
                 if (! input.contains("/at")) {
-                    System.out.println("Specify an event type task by typing " +
-                            "event <task> /at <time>");
+                    System.out.println("Specify an event type task by typing "
+                            + "event <task> /at <time>");
                 } else if (input.indexOf("/at") < 8) {
                     System.out.println("You did not specify a task.");
                 } else if (input.indexOf("/at") + 4 >= input.length()) {
@@ -190,9 +202,20 @@ public class Duke {
                     d.addEvent(input.substring(6, input.indexOf("/at") - 1),
                             input.substring(input.indexOf("/at") + 4));
                 }
+            } else if (input.startsWith("delete")) {
+                try {
+                    int index = Integer.parseInt(input.substring(7)) - 1;
+                    if (index >= tasks.size() || index < 0) {
+                        System.out.println("I cannot delete a task that does not exist!");
+                    } else {
+                        d.delete(index);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("I cannot delete a task that does not exist!");
+                }
             } else {
-                System.out.println("Sorry, I cannot understand what you mean. I can only understand" +
-                        "todo, deadline, event, mark, unmark, list, bye");
+                System.out.println("Sorry, I cannot understand what you mean. I can only understand"
+                        + "todo, deadline, event, mark, unmark, delete, list, bye");
             }
         }
     }
