6f6480ffcf9e847efbd0dc0218b5258d89502323
richavm14 <richavm14@gmail.com>
2022-08-19 15:03:23 +0800

    exception

--- src/main/java/Neo.java
+++ src/main/java/Neo.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class Neo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NeoException {
 
+        List<String> arrayList = new ArrayList<String>();
         List<Task> arrayL = new ArrayList<Task>();
         String userText;
 
@@ -19,15 +20,12 @@ public class Neo {
             System.out.print("Enter Your command: ");
             userText = sc.nextLine();
             System.out.println("");
-            String arr[];
-            arr = userText.split(" ", 2);
 
             if (userText.equals("bye") || userText.equals("Bye")) {
                 System.out.println("Exiting chatbot! Hope to see you again");
                 break;
-                //System.exit(0);
             }
-            else if (userText.equals("list") || userText.equals("List")) {
+            if (userText.equals("list") || userText.equals("List")) {
                 for (int i = 0; i < arrayL.size(); i++) {
                     //System.out.println("check");
                     int j = i + 1;
@@ -37,49 +35,60 @@ public class Neo {
                     System.out.println(j + "." + temp.toString());
                 }
             }
-
-            else if (arr.length>1 && arr[0].equals("mark")){
+            String arr[];
+            arr = userText.split(" ", 2);
+            if (arr.length>1 && arr[0].equals("mark")){
                 int tempi = Integer.valueOf(arr[1]);
                 arrayL.get(tempi -1).setIsDone(true);
                 System.out.println("Nice! I've marked this task as done");
-                System.out.println("[" + arrayL.get(tempi).getIsDone() + "] " + arrayL.get(tempi).description);
+                System.out.println("[" + arrayL.get(tempi-1).getIsDone() + "] " + arrayL.get(tempi-1).description);
             }
-            else if (arr.length>1 && arr[0].equals("unmark")){
+            if (arr.length>1 && arr[0].equals("unmark")){
                 int tempi = Integer.valueOf(arr[1]);
                 arrayL.get(tempi - 1).setIsDone(false);
                 System.out.println("ok, I've marked this task as not done yet");
-                System.out.println("[" + arrayL.get(tempi).getIsDone() + "] " + arrayL.get(tempi).description);
+                System.out.println("[" + arrayL.get(tempi-1).getIsDone() + "] " + arrayL.get(tempi-1).description);
             }
             else {
                 //System.out.println(userText);
-                if (!userText.equals("list") && !userText.equals("List") && !arr[0].equals("unmark") && !arr[0].equals("mark")) {
-                    if (arr.length>1 && arr[0].equals("deadline")) {
-                        String tempi = arr[1];
-                        String arri[];
-                        arri = tempi.split("/by");
-                        String temp2 = arri[0];
-                        String temp3 = arri[1];
-                        Deadline d = new Deadline(temp2, temp3);
-                        arrayL.add(d);
-                        System.out.println("Added: " + d.toString());
-                    }
+                try {
+                    if (!userText.equals("list") && !userText.equals("List") && !arr[0].equals("unmark") && !arr[0].equals("mark") && !arr[0].equals("delete")) {
+                        if (arr.length > 1 && arr[0].equals("deadline")) {
+                            String tempi = arr[1];
+                            String arri[];
+                            arri = tempi.split("/by");
+                            String temp2 = arri[0];
+                            String temp3 = arri[1];
+                            Deadline d = new Deadline(temp2, temp3);
+                            arrayL.add(d);
+                            System.out.println("Added: " + d.toString());
+                        }
 
-                    if (arr.length>1 && arr[0].equals("event")) {
-                        String tempi = arr[1];
-                        String arri[];
-                        arri = tempi.split("/at");
-                        String temp2 = arri[0];
-                        String temp3 = arri[1];
-                        Event e = new Event(temp2, temp3);
-                        arrayL.add(e);
-                        System.out.println("Added: " + e.toString());
+                        if (arr.length > 1 && arr[0].equals("event")) {
+                            String tempi = arr[1];
+                            String arri[];
+                            arri = tempi.split("/at");
+                            String temp2 = arri[0];
+                            String temp3 = arri[1];
+                            Event e = new Event(temp2, temp3);
+                            arrayL.add(e);
+                            System.out.println("Added: " + e.toString());
+                        }
+                        if (arr.length > 1 && arr[0].equals("todo")) {
+                            String tempi = arr[1];
+                            ToDo td = new ToDo(tempi);
+                            arrayL.add(td);
+                            System.out.println("Added: " + td.toString());
+                        }
+                        if (arr.length == 1 && arr[0].equals("todo")) {
+                            throw new NeoException("sorry todo cannot be empty");
+                        }
+                        if (!arr[0].equals("todo") && !arr[0].equals("event") && !arr[0].equals("deadline")) {
+                            throw new NeoException("Sorry I don't know what that means");
+                        }
                     }
-                    if (arr.length>1 && arr[0].equals("todo")) {
-                        String tempi = arr[1];
-                        ToDo td = new ToDo(tempi);
-                        arrayL.add(td);
-                        System.out.println("Added: " + td.toString());
-                    }
+                } catch (NeoException e) {
+                    System.out.println(e.getMessage());
                 }
             }
         }
---
+++ src/main/java/NeoException.java
@@ -0,0 +1,5 @@
+public class NeoException extends Exception{
+    public NeoException(String msg) {
+        super(msg);
+    }
+}
