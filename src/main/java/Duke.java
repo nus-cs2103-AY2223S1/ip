c719d5fe37f0e01a3c882db406dd6fce4d388b06
emptygx <e0717559@u.nus.edu>
2022-08-19 12:01:38 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,18 +10,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
 
+        ArrayList<String> text = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         while (true) {
             String s = in.nextLine();
             if ("bye".equalsIgnoreCase(s)) {
                 break;
             }
-            System.out.println(s);
+            if ("list".equalsIgnoreCase(s)) {
+                int length = text.size();
+                for (int i = 0; i < length; i++) {
+                    System.out.println(i + 1 + ". " + text.get(i));
+                }
+            }
+            else {
+                text.add(s);
+                System.out.println("added: " + s);
+            }
         }
-        System.out.println("bye");
+        System.out.println("Bye. Hope to see you again soon!");
 
         in.close();
     }
