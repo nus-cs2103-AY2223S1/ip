a3050b8ed8dc897884e3673a6b91afec8e6dd9c8
PeiYee88 <e0550356@u.nus.edu>
2022-08-19 18:23:59 +0800

    trying textuitest

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -4,7 +4,7 @@ REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
-if exist ACT.TXT del ACT.TXT
+if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
@@ -15,7 +15,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke.Duke < INPUTS.txt > ACT.TXT
+java -classpath ..\bin Duke < in.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
-FC ACT.TXT EXPECTED.TXT
+FC ACTUAL.TXT EXPECTED.TXT
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -7,9 +7,9 @@ then
 fi
 
 # delete output from previous run
-if [ -e "./ACT.TXT" ]
+if [ -e "./ACTUAL.TXT" ]
 then
-    rm ACT.TXT
+    rm ACTUAL.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
@@ -20,7 +20,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < INPUTS.txt > ACT.TXT
+java -classpath ../bin Duke < in.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
