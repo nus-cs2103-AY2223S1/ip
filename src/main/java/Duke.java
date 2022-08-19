01a1404adaa069ee12ce8a3d583e29934b7403ff
glemenneo <glemenneo@gmail.com>
2022-08-19 15:19:10 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,21 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    private static class Task {
-        private String content;
-        private Boolean status;
+    private ArrayList<Task> list;
 
-        public Task(String content) {
+    public Duke() {
+        this.list = new ArrayList<>();
+    }
+
+    public enum Commands {
+        bye, list, mark, unmark, todo, deadline, events
+    }
+
+    private abstract class Task {
+        protected String content;
+        protected Boolean status;
+
+        protected Task(String content) {
             this.content = content;
             this.status = false;
         }
@@ -30,124 +40,158 @@ public class Duke {
         }
     }
 
+    private class ToDo extends Task {
+        public ToDo(String content) {
+            super(content);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[T]%s", super.toString());
+        }
+    }
+
+    private class Deadline extends Task {
+        private String by;
+
+        public Deadline(String content, String by) {
+            super(content);
+            this.by = by;
+        }
+
+        @Override 
+        public String toString() {
+            return String.format("[D]%s (by: %s)", super.toString(), by);
+        }
+    }
+
+    private class Event extends Task {
+        private String time;
+
+        public Event(String content, String time) {
+            super(content);
+            this.time = time;
+        }
+
+        @Override 
+        public String toString() {
+            return String.format("[E]%s (at: %s)", super.toString(), time);
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        Duke.markDone();
+        Duke duke = new Duke();
+        duke.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter command below:");
+            String input = scanner.nextLine();
+
+            if (input.contains("bye")) {
+                break;
+                
+            } else if (input.contains("list")) {
+                duke.printList();
+
+            } else if (input.contains("unmark")) {
+                int index = Integer.parseInt(input.replace("unmark", "").trim()) - 1;
+                duke.unmarkTask(index);
+
+            } else if (input.contains("mark")) {
+                int index = Integer.parseInt(input.replace("mark", "").trim()) - 1;
+                duke.markTask(index);
+
+            } else if (input.contains("todo")) {
+                duke.addToDo(input.replace("todo", "").trim());
+
+            } else if (input.contains("deadline")) {
+                String[] split = input.replace("deadline", "").split(" by ");
+                duke.addDeadline(split[0].trim(), split[1].trim());
+
+            } else if (input.contains("event")) {
+                String[] split = input.replace("event", "").split(" on ");
+                duke.addEvent(split[0].trim(), split[1].trim());
+
+            } else {
+                duke.addToDo(input);
+            }
+        }
+        scanner.close();
+        duke.goodbye();
     }
 
-    public static void echo() {
+    public void greeting() {
+        String logo = "\t\t\t  ____        _        \n"
+        + "\t\t\t |  _ \\ _   _| | _____ \n"
+        + "\t\t\t | | | | | | | |/ / _ \\\n"
+        + "\t\t\t | |_| | |_| |   <  __/\n"
+        + "\t\t\t |____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(logo);
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Duke\n \tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
+    }
 
-        while (true) {
-            input = scanner.nextLine();
-            if (input.toLowerCase().equals("bye")) {
-                break;
-            }
-            System.out.println("\t____________________________________________________________");
-            System.out.println("\t" + input);
-            System.out.println("\t____________________________________________________________");
-        }
-
+    public void goodbye() {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t____________________________________________________________");
-        scanner.close();
-        return;
     }
 
-    public static void addList() {
-        ArrayList<String> list = new ArrayList<>();
+    public void unmarkTask(int index) {
+        this.list.get(index).unmarkComplete();
         System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello! I'm Duke\n \tWhat can I do for you?");
+        System.out.println("\tOK, I've marked this task as not done yet:");
+        System.out.println("\t" + list.get(index).toString());
         System.out.println("\t____________________________________________________________");
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
+    }
 
-        while (true) {
-            input = scanner.nextLine();
-            if (input.toLowerCase().equals("bye")) {
-                break;
-            }
-            if (input.toLowerCase().equals("list")) {
-                System.out.println("\t____________________________________________________________");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println(String.format("\t%d: %s", i + 1, list.get(i)));
-                }
-                System.out.println("\t____________________________________________________________");
-                continue;
-            }
-            System.out.println("\t____________________________________________________________");
-            System.out.println("\tadded: " + input);
-            System.out.println("\t____________________________________________________________");
-            list.add(input);
+    public void markTask(int index) {
+        this.list.get(index).markComplete();
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t" + list.get(index).toString());
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public void printList() {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHere are the tasks in your list:");
+        for (int i = 0; i < this.list.size(); i++) {
+            System.out.println(String.format("\t%d.%s", i + 1, list.get(i).toString()));
         }
-        
         System.out.println("\t____________________________________________________________");
-        System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
-        scanner.close();
-        return;
     }
 
-    public static void markDone() {
-        ArrayList<Task> list = new ArrayList<>();
+    public void addToDo(String content) {
+        ToDo task = new ToDo(content);
         System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello! I'm Duke\n \tWhat can I do for you?");
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println(String.format("\t  %s", task.toString()));
+        System.out.println(String.format("\tNow you have %d tasks in the list.", this.list.size() + 1));
         System.out.println("\t____________________________________________________________");
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
-
-        while (true) {
-            input = scanner.nextLine();
-            if (input.toLowerCase().equals("bye")) {
-                break;
-            }
-            if (input.toLowerCase().equals("list")) {
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\tHere are the tasks in your list:");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println(String.format("\t%d:%s", i + 1, list.get(i).toString()));
-                }
-                System.out.println("\t____________________________________________________________");
-                continue;
-            }
-            if (input.toLowerCase().contains("unmark")) {
-                int index = Integer.parseInt(input.replace("unmark", "").replaceAll(" ", "")) - 1;
-                list.get(index).unmarkComplete();
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\tOK, I've marked this task as not done yet:");
-                System.out.println("\t" + list.get(index).toString());
-                System.out.println("\t____________________________________________________________");
-                continue;
-            }
-            if (input.toLowerCase().contains("mark")) {
-                int index = Integer.parseInt(input.replace("mark", "").replaceAll(" ", "")) - 1;
-                list.get(index).markComplete();
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\tNice! I've marked this task as done:");
-                System.out.println("\t" + list.get(index).toString());
-                System.out.println("\t____________________________________________________________");
-                continue;
-            }
-            System.out.println("\t____________________________________________________________");
-            System.out.println("\tadded: " + input);
-            System.out.println("\t____________________________________________________________");
-            list.add(new Task(input));
-        }
-        
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
-        scanner.close();
-        return;
+        this.list.add(task);
     }
+
+    public void addDeadline(String content, String by) {
+        Deadline task = new Deadline(content, by);
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println(String.format("\t  %s", task.toString()));
+        System.out.println(String.format("\tNow you have %d tasks in the list.", this.list.size() + 1));
+        System.out.println("\t____________________________________________________________");
+        this.list.add(task);
+    }
+
+    public void addEvent(String content, String time) {
+        Event task = new Event(content, time);
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println(String.format("\t  %s", task.toString()));
+        System.out.println(String.format("\tNow you have %d tasks in the list.", this.list.size() + 1));
+        System.out.println("\t____________________________________________________________");
+        this.list.add(task);
+    }
+
 }
