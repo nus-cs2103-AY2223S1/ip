018dab32f473fd3f2122e981181d43653f66c917
Charles Lim <charleslimjh@gmail.com>
2022-08-19 21:25:55 +0800

    integrated ToDo into Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> db = new ArrayList<>(10);
+    private static ArrayList<Task> db = new ArrayList<>(10);
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -36,11 +36,11 @@ public class Duke {
             return 0;
         } else if (s.equals("list")) {
             for (int i = 0; i < db.size(); i++) {
-                System.out.println(i + 1 + ". " + db.get(i));
+                System.out.println(i + 1 + ". " + db.get(i).toString());
             }
             return 1;
         } else {
-            db.add(s);
+            db.add(new Task(s));
             System.out.println("added: " + s);
             return 1;
         }
