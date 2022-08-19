9d4bff83910cd3fe933a53c00e16cf62bab4b217
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-19 15:02:04 +0800

    Implement task serialization and deserialization

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -6,6 +6,23 @@ public class Todo extends Task {
         super(description);
     }
 
+    public Todo(String description, boolean isDone) {
+        super(description, isDone);
+    }
+
+    @Override
+    public String serialize() {
+        return String.format("T|%s|%s", isDone, description);
+    }
+
+    public static Task deserialize(String str) {
+        String[] parts = str.split("\\|");
+        if (parts.length != 3 || !parts[0].equals("T")) {
+            throw new IllegalArgumentException("Invalid todo format");
+        }
+        return new Todo(parts[2], Boolean.parseBoolean(parts[1]));
+    }
+
     @Override
     public String toString() {
         return String.format("[T]%s", super.toString());
