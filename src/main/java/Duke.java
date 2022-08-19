a82b135381277df1e2ca0b3d1fd8e93822ba0508
arnav-ag <arnav.ctf@gmail.com>
2022-08-19 13:32:06 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;  // Import the Scanner class
 public class Duke {
+
+    public static String receiveCommand() {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String command = myObj.nextLine();  // Read user input
+        return command;
+    }
+
+    public static void parseCommand(String command) {
+        if(command.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(command);
+        }
+        return;
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +22,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String command = "";
+        do {
+            System.out.print("> ");
+            command = receiveCommand();
+            parseCommand(command);
+        } while (!command.equals("bye"));
     }
 }
