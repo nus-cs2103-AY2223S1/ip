dfdafd2a3d37e124761f060d2e8a7e4292d467e5
Reuben Chay <>
2022-08-19 16:13:38 +0800

    add Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import javax.swing.event.TreeWillExpandListener;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +8,28 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String separator = "     ==================================";
+        String indent = "      ";
+
+        String welcomeMsg = "Hello! I'm\n" + logo + "\nWhat can I do for you?\n";
+        System.out.println(welcomeMsg);
+
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+
+        while (true) {
+            input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(separator);
+                System.out.println(indent + "Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            } else {
+                System.out.println(separator);
+                System.out.println(indent + input);
+                System.out.println(separator);
+            }
+        }
     }
 }
