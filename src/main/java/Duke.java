3dbf4be02cc6b4b6ac9252321034c0e6a8f3cca7
melissaharijanto <melissa.anastasia.h@gmail.com>
2022-08-20 02:51:20 +0800

    Made a few changes to code
    Description:
    - multiple try and catch blocks are condensed into one;
    - the command BYE is added to the switch case

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,75 +2,48 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    enum CommandLine {
-        BYE,
-        LIST,
-        MARK,
-        UNMARK,
-        DEADLINE,
-        EVENT,
-        TODO,
-        DELETE,
-    }
-
     static ArrayList<Task> taskList = new ArrayList<>();
     static Ui ui = new Ui();
 
     public static boolean doesCommandExist(String cmd) {
         return cmd.equals("list") || cmd.equals("mark")
-                || cmd.equals("unmark")  || cmd.equals("deadline")  || cmd.equals("event")
-                || cmd.equals("todo")  || cmd.equals("delete") ;
+                || cmd.equals("unmark") || cmd.equals("deadline") || cmd.equals("event")
+                || cmd.equals("todo") || cmd.equals("delete") || cmd.equals("bye");
     }
+
     public static void detectCommand(CommandLine commandLine, String cmd) {
-        switch (commandLine) {
-            case LIST:
-                ui.list(taskList);
-                break;
-            case MARK:
-                try {
+        try {
+            switch (commandLine) {
+                case BYE:
+                    ui.exit();
+                    break;
+                case LIST:
+                    ui.list(taskList);
+                    break;
+                case MARK:
                     Commands.mark(cmd, taskList);
-                } catch (DukeException e) {
-                    ui.errorMessage(e);
-                }
-                break;
-            case UNMARK:
-                try {
+                    break;
+                case UNMARK:
                     Commands.unmark(cmd, taskList);
-                } catch (DukeException e) {
-                    ui.errorMessage(e);
-                }
-                break;
-            case DEADLINE:
-                try {
+                    break;
+                case DEADLINE:
                     Commands.deadline(cmd, taskList);
-                } catch (DukeException e) {
-                    ui.errorMessage(e);
-                }
-                break;
-            case EVENT:
-                try {
+                    break;
+                case EVENT:
                     Commands.event(cmd, taskList);
-                } catch (DukeException e) {
-                    ui.errorMessage(e);
-                }
-                break;
-            case TODO:
-                try {
+                    break;
+                case TODO:
                     Commands.toDo(cmd, taskList);
-                } catch (DukeException e) {
-                    ui.errorMessage(e);
-                }
-                break;
-            case DELETE:
-                try {
+                    break;
+                case DELETE:
                     Commands.delete(cmd, taskList);
-                } catch (DukeException e) {
-                    ui.errorMessage(e);
-                }
-                break;
-            default:
-                ui.commandDoesNotExist();
-                break;
+                    break;
+                default:
+                    ui.commandDoesNotExist();
+                    break;
+            }
+        } catch (DukeException e) {
+            ui.errorMessage(e);
         }
     }
 
@@ -80,16 +53,29 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
 
-        while (!cmd.equals("bye")){
+        while (true) {
             String cmdLine = cmd.split(" ")[0];
             if (doesCommandExist(cmdLine)) {
                 CommandLine commandLine = CommandLine.valueOf(cmdLine.toUpperCase());
                 detectCommand(commandLine, cmd);
+                if (commandLine == CommandLine.BYE) {
+                    return;
+                }
             } else {
                 ui.commandDoesNotExist();
             }
             cmd = sc.nextLine();
         }
-        ui.exit();
+    }
+
+    enum CommandLine {
+        BYE,
+        LIST,
+        MARK,
+        UNMARK,
+        DEADLINE,
+        EVENT,
+        TODO,
+        DELETE,
     }
 }
