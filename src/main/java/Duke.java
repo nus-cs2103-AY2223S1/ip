0b2ff02c2567a680f4badb394e0ae87b8684984b
DevanshShah1309 <devanshshah1309@gmail.com>
2022-08-19 08:33:53 +0800

    Refactor catching DukeException

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,8 @@ import static java.lang.Integer.decode;
 import static java.lang.Integer.parseInt;
 
 public class Duke {
-
+    private static final String WELCOME_MESSAGE = "\t-------------------------------\n"
+            + "\tHello, I'm Duke!\n" + "\tWhat can I do for you?\n"  + "\t-------------------------------";
     // Oerations on tasks
     private static final String MARK_REGEX = "mark [0-9]+";
     private static final String UNMARK_REGEX = "unmark [0-9]+";
@@ -20,62 +21,51 @@ public class Duke {
 
 
     public static void main(String[] args) {
-        System.out.println("\t-------------------------------");
-        System.out.println("\tHello, I'm Duke!");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println("\t-------------------------------");
+        System.out.println(WELCOME_MESSAGE);
         Scanner s = new Scanner(System.in);
-        while (true) {
-            String command = s.nextLine().trim();
-            System.out.println("\t-------------------------------");
-            if (command.equals(Commands.BYE)) {
-                System.out.println("\tBye! Hope to see you again");
-                break;
-            } else if (command.equals(LIST)) {
-                Task.listTasks();
-            } else if (Pattern.matches(MARK_REGEX, command)) {
-                String temp = command.replaceAll("[^0-9]", ""); // extracting the digits
-                int taskNumber = parseInt(temp);
-                Task.markAsDone(taskNumber - 1);
-            } else if (Pattern.matches(UNMARK_REGEX, command)) {
-                String temp = command.replaceAll("[^0-9]", ""); // extracting the digits
-                int taskNumber = parseInt(temp);
-                Task.markAsNotDone(taskNumber - 1);
-            } else if (Pattern.matches(TODO_REGEX, command)) {
-                String desc = command.substring(Commands.TODO.toString().length()).trim(); // Ignore the word "todo"
-                try {
+        try {
+            while (true) {
+                String command = s.nextLine().trim();
+                System.out.println("\t-------------------------------");
+                if (command.equals(Commands.BYE)) {
+                    System.out.println("\tBye! Hope to see you again");
+                    break;
+                } else if (command.equals(LIST)) {
+                    Task.listTasks();
+                } else if (Pattern.matches(MARK_REGEX, command)) {
+                    String temp = command.replaceAll("[^0-9]", ""); // extracting the digits
+                    int taskNumber = parseInt(temp);
+                    Task.markAsDone(taskNumber - 1);
+                } else if (Pattern.matches(UNMARK_REGEX, command)) {
+                    String temp = command.replaceAll("[^0-9]", ""); // extracting the digits
+                    int taskNumber = parseInt(temp);
+                    Task.markAsNotDone(taskNumber - 1);
+                } else if (Pattern.matches(TODO_REGEX, command)) {
+                    String desc = command.substring(Commands.TODO.toString().length()).trim(); // Ignore the word "todo"
                     Task.add(new ToDo(desc));
-                } catch (DukeException e) {
-                    System.out.println(String.format("\t%s", e.getMessage()));
-                }
-            } else if (Pattern.matches(DEADLINE_REGEX, command)) {
-                String temp = command.substring(Commands.DEADLINE.toString().length());
-                String[] details = temp.split("/by");
-                details[0] = details[0].trim(); // description
-                details[1] = details[1].trim(); // deadline
-                try {
+                } else if (Pattern.matches(DEADLINE_REGEX, command)) {
+                    String temp = command.substring(Commands.DEADLINE.toString().length());
+                    String[] details = temp.split("/by");
+                    details[0] = details[0].trim(); // description
+                    details[1] = details[1].trim(); // deadline
                     Task.add(new Deadline(details[0], details[1]));
-                } catch (DukeException e) {
-                    System.out.println(String.format("\t%s", e.getMessage()));
-                }
-            } else if (Pattern.matches(EVENT_REGEX, command)) {
-                String temp = command.substring(Commands.EVENT.toString().length());
-                String[] details = temp.split("/at");
-                details[0] = details[0].trim(); // description
-                details[1] = details[1].trim(); // timing
-                try {
+                } else if (Pattern.matches(EVENT_REGEX, command)) {
+                    String temp = command.substring(Commands.EVENT.toString().length());
+                    String[] details = temp.split("/at");
+                    details[0] = details[0].trim(); // description
+                    details[1] = details[1].trim(); // timing
                     Task.add(new Event(details[0], details[1]));
-                } catch (Exception e) {
-                    System.out.println(String.format("\t%s", e.getMessage()));
+                } else if (Pattern.matches(DELETE_REGEX, command)) {
+                    String temp = command.replaceAll("[^0-9]", ""); // extracting the digits
+                    int taskNumber = parseInt(temp);
+                    Task.delete(taskNumber - 1);
+                } else {
+                    System.out.println("\tOOPS! I've no idea what you're talking about! :-(");
                 }
-            } else if (Pattern.matches(DELETE_REGEX, command)){
-                String temp = command.replaceAll("[^0-9]", ""); // extracting the digits
-                int taskNumber = parseInt(temp);
-                Task.delete(taskNumber - 1);
-            } else {
-                System.out.println("\tOOPS! I've no idea what you're talking about! :-(");
+                System.out.println("\t-------------------------------");
             }
-            System.out.println("\t-------------------------------");
+        } catch (DukeException e) {
+            System.out.println(String.format("\t", e.getMessage()));
         }
         System.out.println("\t-------------------------------");
     }
