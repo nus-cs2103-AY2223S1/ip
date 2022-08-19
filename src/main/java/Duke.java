319e5664be71ba5fd7c1cc7a089aeb55fc27493c
chengda300 <chengda300@gmail.com>
2022-08-19 09:56:58 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,22 @@ public class Duke {
                 + "|____/ \\__,_|\\__,_|\\___|\n";
         System.out.println("Hello from\n" + logo);
         boolean running = true;
+        String[] history = new String[100];
+        int count = 0;
         Scanner sc = new Scanner(System.in);
         while (running) {
             String input = sc.next();
             if (input.equals("bye")) {
                 System.out.println(">>> Byebye! See you again soon!");
                 running = false;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println(i + 1 + ". " + history[i]);
+                }
             } else {
-                System.out.println(">>> " + input);
+                history[count] = input;
+                count++;
+                System.out.println("added: " + input);
             }
         }
     }
