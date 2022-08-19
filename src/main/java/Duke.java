ed65057a3bf62b0c7fc50a4279b73590d8525717
chengda300 <chengda300@gmail.com>
2022-08-19 10:32:39 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,6 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static class Task {
-        protected boolean isDone;
-        protected String description;
-
-        public Task(String desc) {
-            this.description = desc;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return isDone ? "X" : " ";
-        }
-
-        public String getTask() {
-            return this.description;
-        }
-
-        public static void markDone(Task t) {
-            t.isDone = true;
-        }
-
-        public static void markUndone(Task t) {
-            t.isDone = false;
-        }
-    }
     public static void main(String[] args) {
         String logo = " ____            _    \n"
                 + "|  _ \\ _   _  __| | ___\n"
@@ -34,7 +9,7 @@ public class Duke {
                 + "|____/ \\__,_|\\__,_|\\___|\n";
         System.out.println("Hello from\n" + logo);
         boolean running = true;
-        Task[] history = new Task[100];
+        String[] history = new String[100];
         int count = 0;
         Scanner sc = new Scanner(System.in);
         while (running) {
@@ -42,28 +17,8 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println(">>> Byebye! See you again soon!");
                 running = false;
-            } else if (input.equals("list")) {
-                for (int i = 0; i < count; i++) {
-                    System.out.println(i + 1 + ".[" + history[i].getStatusIcon() + "] "
-                            + history[i].getTask());
-                }
-            } else if (input.startsWith("mark")) {
-                int taskNum = Integer.parseInt(input.substring(5)) - 1;
-                Task target = history[taskNum];
-                Task.markDone(target);
-                System.out.println("Successfully marked this task as done: ["
-                        + target.getStatusIcon() + "] " + target.getTask());
-            } else if (input.startsWith("unmark")) {
-                int taskNum = Integer.parseInt(input.substring(7)) - 1;
-                Task target = history[taskNum];
-                Task.markUndone(target);
-                System.out.println("Successfully marked this task as not done yet: ["
-                        + target.getStatusIcon() + "] " + target.getTask());
             } else {
-                Task t = new Task(input);
-                history[count] = t;
-                count++;
-                System.out.println("added: " + input);
+                System.out.println(">>> " + input);
             }
         }
     }
