6af0197497a5ca4b2567f0a3c9e82c9f23b43d31
leehuiyulaura <e0>
2022-08-19 17:16:37 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,10 +7,13 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        // create scanner to receive user input
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Kuke\n" +
                 "What can I do for you?");
         String a = sc.next();
+        // if input received is anything but "bye" system will output what the user
+        // inputted
         while (!a.equals("bye")) {
             System.out.println(a);
             a = sc.next();
