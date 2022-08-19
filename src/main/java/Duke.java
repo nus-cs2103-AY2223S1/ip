6fcae4e226abc0d09c3df483154fbf541d7d424f
Jin Wei <seetohjinwei@gmail.com>
2022-08-19 16:24:20 +0800

    Add Storage class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,78 +6,6 @@ public class Duke {
     private static final String minorIndentation = "  ";
     private static final String indentation = "    ";
     private static final String horizontalLine = indentation + "____________________________________________________________";
-
-    /**
-     * Load tasks from file.
-     *
-     * @return tasks The tasks loaded from the file.
-     */
-    private static ArrayList<Task> loadTasks() throws DukeException {
-        Path directoryPath = Paths.get("data");
-        Path filePath = Paths.get("data/tasks");
-        File directory = new File(directoryPath.toUri());
-        // Create directory if it does not exist.
-        // noinspection ResultOfMethodCallIgnored because not only making use of the side effect
-        directory.mkdir();
-        File file = new File(filePath.toUri());
-
-        ArrayList<Task> tasks = new ArrayList<>();
-        try {
-            // Create file if it does not exist.
-            if (file.createNewFile()) {
-                // If file did not exist before this, there are no tasks.
-                return tasks;
-            }
-        } catch (IOException e) {
-            throw DukeException.badData;
-        }
-
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                String[] split = line.split(" \\| ");
-                Task task;
-                switch (split[0]) {
-                    case "T":
-                        task = Todo.create(split[1], split[2]);
-                        break;
-                    case "D":
-                        task = Deadline.create(split[1], split[2], split[3]);
-                        break;
-                    case "E":
-                        task = Event.create(split[1], split[2], split[3]);
-                        break;
-                    default:
-                        throw DukeException.badData;
-                }
-
-                tasks.add(task);
-            }
-        } catch (FileNotFoundException e) {
-            // Should not happen because file is created beforehand.
-            return tasks;
-        }
-
-        return tasks;
-    }
-
-    /**
-     * Save tasks to file.
-     *
-     * @param tasks The tasks to be saved to the file.
-     */
-    private static void saveTasks(ArrayList<Task> tasks) throws IOException {
-        Path filePath = Paths.get("data/tasks");
-        File file = new File(filePath.toUri());
-        FileWriter fw = new FileWriter(file);
-
-        for (Task task : tasks) {
-            String line = task.getFileFormat();
-            fw.write(line + "\n");
-        }
-
-        fw.close();
-    }
 
     private static void list(ArrayList<Task> tasks) {
         for (int i = 0; i < tasks.size(); i++) {
@@ -198,9 +121,11 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
 
+        Storage storage = new Storage("data", "data/tasks");
+
         ArrayList<Task> tasks;
         try {
-            tasks = loadTasks();
+            tasks = storage.load();
         } catch (DukeException e) {
             System.out.println(indentation + e.getMessage());
             // Load with empty list instead.
@@ -227,32 +152,32 @@ public class Duke {
                         break;
                     case "mark": {
                         Duke.mark(tasks, split);
-                        saveTasks(tasks);
+                        storage.save(tasks);
                         break;
                     }
                     case "unmark": {
                         Duke.unmark(tasks, split);
-                        saveTasks(tasks);
+                        storage.save(tasks);
                         break;
                     }
                     case "delete": {
                         Duke.delete(tasks, split);
-                        saveTasks(tasks);
+                        storage.save(tasks);
                         break;
                     }
                     case "todo": {
                         Duke.todo(tasks, split);
-                        saveTasks(tasks);
+                        storage.save(tasks);
                         break;
                     }
                     case "deadline": {
                         Duke.deadline(tasks, split);
-                        saveTasks(tasks);
+                        storage.save(tasks);
                         break;
                     }
                     case "event": {
                         Duke.event(tasks, split);
-                        saveTasks(tasks);
+                        storage.save(tasks);
                         break;
                     }
                     default:
