c041bc445f902f2f702ae60b5e6a39792a4b3f18
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-19 15:23:26 +0800

    Refactor `Task` and its subclasses to use LocalDateTime

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,25 @@
+import java.time.LocalDateTime;
+
 /**
  * A task that start at a specific date/time and ends at a specific date/time.
  */
 public class Event extends Task {
-    private final String datetime;
+    private final LocalDateTime datetime;
 
-    public Event(String description, String datetime) {
+    public Event(String description, LocalDateTime datetime) {
         super(description);
         this.datetime = datetime;
     }
 
+    public Event(String description, String datetime) {
+        this(description, LocalDateTime.parse(datetime, Task.DATE_TIME_FORMATTER));
+    }
+
     @Override
     public String toString() {
-        return String.format("[E]%s (at: %s)", super.toString(), datetime);
+        return String.format("[E]%s (at: %s)",
+                super.toString(),
+                Task.DATE_TIME_FORMATTER.format(datetime)
+        );
     }
 }
