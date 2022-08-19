9689da09df1d66f401b0fbc13bdd635942edca98
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-19 15:04:06 +0800

    Implement rest of level 7 requirements
    - Refactored `Duke` to be a singleton (otherwise conflicts may occur in the hardcoded file)
    - Implemented task list saving after each command execution

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,12 +1,72 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
  * A TaskList stores a list of tasks.
  */
 public class TaskList {
-    private final List<Task> list = new ArrayList<>();
+    private final List<Task> list;
+
+    private TaskList(List<Task> list) {
+        this.list = list;
+    }
+
+    /**
+     * Returns a new TaskList with no tasks.
+     * Use this static factory method instead of the constructor.
+     *
+     * @return a new TaskList with no tasks
+     * @since Level-7
+     */
+    public static TaskList newEmptyTaskList() {
+        return new TaskList(new ArrayList<>());
+    }
+
+    /**
+     * Returns a new TaskList from a file of serialized tasks.
+     *
+     * @param path the path to the file of tasks
+     * @return the new TaskList
+     * @throws IOException if an I/O error occurs reading the file
+     * @since Level-7
+     */
+    public static TaskList readFromFile(Path path) throws IOException {
+        List<Task> tasks = Files.readAllLines(path)
+                .stream()
+                .map(Task::deserialize)
+                .collect(Collectors.toList());
+        return new TaskList(tasks);
+    }
+
+    /**
+     * Writes the list of tasks to a file at the given path.
+     * If the file already exists, it will be overwritten.
+     *
+     * @param path the path to write the tasks to
+     * @throws IOException if an I/O error occurs writing to the file
+     * @since Level-7
+     */
+    public void writeToFile(Path path) throws IOException {
+        if (Files.notExists(path)) {
+            Files.createDirectories(path.getParent());
+            Files.createFile(path);
+        }
+        String serialized = list.stream()
+                .map(Task::serialize)
+                .map(str -> str + "\n")
+                .collect(StringBuilder::new,
+                        StringBuilder::append,
+                        StringBuilder::append
+                )
+                .toString();
+        Files.writeString(path, serialized);
+    }
+
 
     /**
      * Adds a task to the list.
@@ -19,7 +79,7 @@ public class TaskList {
 
     /**
      * Deletes the task at the given index from the list.
-     * 
+     *
      * @param index the index of the task to delete
      */
     public void deleteTask(int index) {
@@ -54,7 +114,10 @@ public class TaskList {
     public String toString() {
         return IntStream.range(0, list.size())
                 .mapToObj(i -> String.format("%d. %s\n", i + 1, list.get(i)))
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .collect(StringBuilder::new,
+                        StringBuilder::append,
+                        StringBuilder::append
+                )
                 .toString()
                 .trim();
     }
