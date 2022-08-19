3d576086acc7627db3335765b96e30acf257b0a5
arnav-ag <arnav.ctf@gmail.com>
2022-08-19 13:46:47 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;  // Import the Scanner class
 public class Duke {
 
+    private static String[] commands = new String[100];
+    private static int i = 0;
     public static String receiveCommand() {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String command = myObj.nextLine();  // Read user input
@@ -10,8 +12,14 @@ public class Duke {
     public static void parseCommand(String command) {
         if(command.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
+        } else if (command.equals("list")) {
+            for (int j = 0; j < i; j++) {
+                System.out.println(j+1 + ": " + commands[j]);
+            }
         } else {
-            System.out.println(command);
+            commands[i] = command;
+            i += 1;
+            System.out.println("added: " + command);
         }
         return;
     }
