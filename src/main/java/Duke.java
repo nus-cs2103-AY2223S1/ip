ec0312a0eaa8c9233a74d6ef8f2a3aafa2f89409
yilinzyl <zhengyilin02@gmail.com>
2022-08-19 13:14:22 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,19 +9,37 @@ public class Duke {
             return "Bye. Hope to see you again soon!";
         } else if (input.equals("list")) {
             return "Here are the tasks in your list:\n" + listTasks();
-        } else if (input.substring(0, 4).equals("mark")) {
+        } else if (input.length() > 3 && input.substring(0, 4).equals("mark")) {
             int index = Integer.parseInt(input.substring(5)) - 1;
             Task currTask = taskList.get(index);
             currTask.markAsDone();
-            return "Nice! I've marked this task as done\n" + currTask.toString();
-        } else if (input.substring(0, 6).equals("unmark")) {
+            return "Nice! I've marked this task as done\n  " + currTask.toString();
+        } else if (input.length() > 5 && input.substring(0, 6).equals("unmark")) {
             int index = Integer.parseInt(input.substring(7)) - 1;
             Task currTask = taskList.get(index);
             currTask.markAsNotDone();
-            return "OK, I've marked this task as not done yet:\n" + currTask.toString();
+            return "OK, I've marked this task as not done yet:\n  " + currTask.toString();
+        } else if (input.length() > 3 && input.substring(0, 4).equals("todo")) {
+            Task newTask = new Todo(input.substring(5));
+            addTask(newTask);
+            return "Got it. I've added this task:\n  " + newTask.toString() +
+                    "\nNow you have " + taskList.size() + " tasks in the list";
+        } else if (input.length() > 7 && input.substring(0, 8).equals("deadline")) {
+            String taskInput = input.substring(9);
+            String[] taskAndDate = taskInput.split("/by ");
+            Task newTask = new Deadline(taskAndDate[0], taskAndDate[1]);
+            addTask(newTask);
+            return "Got it. I've added this task:\n  " + newTask.toString() +
+                    "\nNow you have " + taskList.size() + " tasks in the list";
+        } else if (input.length() > 5 && input.substring(0, 5).equals("event")) {
+            String taskInput = input.substring(6);
+            String[] taskAndDate = taskInput.split("/at ");
+            Task newTask = new Event(taskAndDate[0], taskAndDate[1]);
+            addTask(newTask);
+            return "Got it. I've added this task:\n  " + newTask.toString() +
+                    "\nNow you have " + taskList.size() + " tasks in the list";
         } else {
-            addTask(new Task(input));
-            return "added: " + input;
+            return "invalid command";
         }
     }
 
