169a374c68c50aee9495d7bdd8840e7e2b557e72
PeiYee88 <e0550356@u.nus.edu>
2022-08-19 19:11:54 +0800

    feed

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -7,9 +7,9 @@ then
 fi
 
 # delete output from previous run
-if [ -e "./ACTUAL.TXT" ]
+if [ -e "./ACT.TXT" ]
 then
-    rm ACTUAL.TXT
+    rm ACT.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
@@ -20,7 +20,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < in.txt > ACTUAL.TXT
+java -classpath ../bin Duke < feed.txt > ACT.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
