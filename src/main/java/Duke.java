a2ef7f722755530da3625cbe3267452e645373a0
gabyang <gabrielyang@berkeley.edu>
2022-08-19 10:06:30 +0800

    Complete Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
         System.out.println("What are your commands sir: ");
         String[] arr = new String[100];
         char[] markArr = new char[100];
+        char[] eventTypeArr = new char[100];
         boolean pred = true;
         int i = 0;
 
@@ -15,7 +16,7 @@ public class Duke {
                 if (str.contains("list")) {
                     System.out.println("list items -");
                     for (int j = 0; j < i; j++){
-                        System.out.println("List " + j + ". [" + markArr[j] + "] " + arr[j]);
+                        System.out.println("List " + j + ". [" + eventTypeArr[j] + "][" + markArr[j] + "] " + arr[j]);
                     }
                 } else if (str.contains("blah")) {
                     System.out.println("blah");
@@ -27,18 +28,46 @@ public class Duke {
                     int j = Integer.valueOf(str.substring(5));
                     markArr[j] = 'X';
                     System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println("List " + j + ". [" + markArr[j] + "] " + arr[j]);
+                    System.out.println("List " + j + ". [" + eventTypeArr[j] + "][" + markArr[j] + "] " + arr[j]);
 
                 } else if (str.startsWith("unmark")) {
                     int j = Integer.valueOf(str.substring(7));
                     markArr[j] = ' ';
-                    System.out.println("Ok, I've marked this task as not done yet: ");
-                    System.out.println("List " + j + ". [" + markArr[j] + "] " + arr[j]);
+                    System.out.println("Got it. I've mark this task as not done: ");
+                    System.out.println("List " + j + ". [" + eventTypeArr[j] + "][" + markArr[j] + "] " + arr[j]);
 
-                } else {
-                    System.out.println("Storing this text in the list:");
+                } else if (str.startsWith("todo")) {
+                    str = str.substring(5);
                     arr[i] = str;
                     markArr[i] = ' ';
+                    eventTypeArr[i] = 'T';
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("List " + i + ". [" + eventTypeArr[i] + "][" + markArr[i] + "] " + arr[i]);
+                    i++;
+
+                } else if (str.startsWith("deadline")) {
+                    str = str.substring(9);
+                    arr[i] = str.substring(0, str.indexOf('/')) + '(' + str.substring(str.indexOf('/') + 1) + ')';
+                    markArr[i] = ' ';
+                    eventTypeArr[i] = 'D';
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("List " + i + ". [" + eventTypeArr[i] + "][" + markArr[i] + "] " + arr[i]);
+                    i++;
+
+                } else if (str.startsWith("event")) {
+                    str = str.substring(6);
+                    arr[i] = str.substring(0, str.indexOf('/')) + '(' + str.substring(str.indexOf('/') + 1) + ')';
+                    markArr[i] = ' ';
+                    eventTypeArr[i] = 'E';
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println("List " + i + ". [" + eventTypeArr[i] + "][" + markArr[i] + "] " + arr[i]);
+                    i++;
+
+                } else {
+                    System.out.println("Storing this text in the list (type unspecified): ");
+                    arr[i] = str;
+                    markArr[i] = ' ';
+                    eventTypeArr[i] = '?';
                     i++;
 
                 }
