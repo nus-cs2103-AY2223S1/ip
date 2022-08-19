537c95de7652e05401a8c4cffd3732c4f2e8f072
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-19 17:00:04 +0800

    Write tests for `TaskList` and `Todo`

---
+++ src/test/java/duke/task/TaskListTest.java
@@ -0,0 +1,19 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void testToString() {
+        TaskList taskList = TaskList.newEmptyTaskList();
+        taskList.addTask(new Todo("abc", false));
+        taskList.addTask(new Event("def ghi", "12/12/2022 12:12"));
+        taskList.addTask(new Deadline("jkl mno pqr", "1/1/1 1:01", true));
+        String expected = "1. [T][ ] abc\n" + 
+                "2. [E][ ] def ghi (at: 12/12/2022 12:12)\n" + 
+                "3. [D][✓] jkl mno pqr (by: 1/1/1 1:01)";
+        assertEquals(expected, taskList.toString());
+    }
+}
