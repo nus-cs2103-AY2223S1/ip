b82293c04bb6370c3f51dcf79e970a7c4520e5f9
shamooose <e0726246@u.nus.edu>
2022-08-19 23:30:58 +0800

    Added support for tracking ToDos, Deadlines and Events.

--- src/main/java/Scruffles.java
+++ src/main/java/Scruffles.java
@@ -10,6 +10,7 @@ public class Scruffles {
         String input = "";
 
         ArrayList<Task> list = new ArrayList<Task>(100);
+        int taskCount = 0;
 
         while (true) {
 
@@ -24,12 +25,44 @@ public class Scruffles {
                     System.out.println(output);
                 }
             } else if (input.startsWith("mark")) {
-                int listRef = input.charAt(4) - 32;
+                input = input.replaceFirst("mark ", "");
+                int listRef = Integer.parseInt(input) - 1;
                 list.get(listRef).setDone();
+
                 System.out.println("woof! the task is now marked as done woof!\n" + list.get(listRef).toString());
+            } else if (input.startsWith("todo")) {
+                input = input.replaceFirst("todo ", "");
+                list.add(new Todo(input));
+
+                System.out.println("woof! the task is added woof!");
+                System.out.println(list.get(taskCount).toString());
+                System.out.println("you now have " + (taskCount + 1) + " tasks in the list woof!");
+
+                taskCount++;
+            } else if (input.startsWith("deadline")) {
+                input = input.replaceFirst("deadline ", "");
+                String[] inputArray = input.split(" /by ");
+                list.add(new Deadline(inputArray[0], inputArray[1]));
+
+                System.out.println("woof! the task is added woof!");
+                System.out.println(list.get(taskCount).toString());
+                System.out.println("you now have " + (taskCount + 1) + " tasks in the list woof!");
+
+                taskCount++;
+            } else if (input.startsWith("event")) {
+                input = input.replaceFirst("event ", "");
+                String[] inputArray = input.split(" /at ");
+                list.add(new Event(inputArray[0], inputArray[1]));
+
+                System.out.println("woof! the task is added woof!");
+                System.out.println(list.get(taskCount).toString());
+                System.out.println("you now have " + (taskCount + 1) + " tasks in the list woof!");
+
+                taskCount++;
             } else {
                 list.add(new Task(input));
                 System.out.println("added: " + input);
+                taskCount++;
             }
         }
     }
