179da54174662ac1da24c6e064ddffa07e14417b
Jonathan Tay <jonathantaymy@gmail.com>
2022-08-20 10:20:31 +0800

    Replace `*` imports to conform with coding standard

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,6 +1,14 @@
 package duke;
 
-import duke.command.*;
+import duke.command.ByeCommand;
+import duke.command.Command;
+import duke.command.DeadlineCommand;
+import duke.command.DeleteCommand;
+import duke.command.EventCommand;
+import duke.command.ListCommand;
+import duke.command.MarkCommand;
+import duke.command.TodoCommand;
+import duke.command.UnmarkCommand;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
