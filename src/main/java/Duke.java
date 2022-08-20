4f8f316f6afc62d890838400a60e663a97af2d73
LAPTOP-3M0VCNEN\Lik Hern <e0412919@u.nus.edu>
2022-08-20 23:36:19 +0800

    Add the ability to store text and display to user when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        List<String> storage = new ArrayList<>();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
@@ -9,8 +13,14 @@ public class Duke {
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equals("list")) {
+                ListIterator<String> listIterator = storage.listIterator();
+                while (listIterator.hasNext()) {
+                    System.out.println(listIterator.nextIndex() + 1 + ". " + listIterator.next());
+                }
             } else {
-                System.out.println(command);
+                storage.add(command);
+                System.out.println("added: " + command);
             }
         }
     }
