bf1ed971c885e1d725d7aab0c0dabd3ff1373ccf
JustinS00 <justinsiaw00@gmail.com>
2022-08-20 15:41:32 +0800

    runtest.bat: delete previous run data

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,6 +6,9 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if not exist ..\bin mkdir ..\bin
 
+REM delete data.txt file from previous run
+del data.txt
+
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\duke\*.java
 IF ERRORLEVEL 1 (
