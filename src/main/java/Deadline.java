bd16807f11af401e182d9f0781b2ddc2a50cc5d3
Hikoya <hikoyanamikaze@gmail.com>
2022-08-21 05:56:30 +0800

    Add parsing of date into Deadline and Event

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
+    private final String inputFormat = "yyyy-MM-dd HHmm"; // 2019-10-15 1800
+    private final String outputFormat = "MMM dd yyyy"; // Oct 15 2019
+    private final LocalDateTime endDate;
 
-    private String endDate;
-
-    public Deadline(String description, String endDate) {
+    public Deadline(String description, String endDate) throws SkylarkException {
         super(description);
-        this.endDate = endDate;
+        try {
+            this.endDate = LocalDateTime.parse(endDate, DateTimeFormatter.ofPattern(inputFormat));
+        } catch (DateTimeParseException dateTimeParseException) {
+            throw new SkylarkException("Cannot parse date");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.endDate + ")";
+        return "[D]" + super.toString() + " (by: "
+                + this.endDate.format(DateTimeFormatter.ofPattern(outputFormat)) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task{
+    private final String inputFormat = "yyyy-MM-dd HHmm"; // 2019-10-15 1800
+    private final String outputFormat = "MMM dd yyyy"; // Oct 15 2019
+    private final LocalDateTime timing;
 
-    private String timing;
-
-    public Event(String description, String timing) {
+    public Event(String description, String timing) throws SkylarkException {
         super(description);
-        this.timing = timing;
+        try {
+            this.timing = LocalDateTime.parse(timing, DateTimeFormatter.ofPattern(inputFormat));
+        } catch (DateTimeParseException dateTimeParseException) {
+            throw new SkylarkException("Cannot parse date");
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.timing + ")";
+        return "[E]" + super.toString() + " (at: "
+                + this.timing.format(DateTimeFormatter.ofPattern(outputFormat)) + ")";
     }
 }
