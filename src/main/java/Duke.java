9dc091aa363341391d32ba52e0a537f53745f430
ningtan11 <e0773603@u.nus.edu>
2022-08-20 19:55:37 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static String logo  = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static ArrayList<String> list;
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        list = new ArrayList<>();
+
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
 
@@ -14,12 +19,25 @@ public class Duke {
         String text = scanner.next();
         boolean open = true;
         while (open) {
+            // Bye
             if (text.equalsIgnoreCase("bye")) {
                 scanner.close();
                 open = false;
                 System.out.println("Goodbye! Thank you for visiting Duke.");
+            // List
+            } else if (text.equalsIgnoreCase("list")) {
+                if (list.isEmpty()) {
+                    System.out.println("There is nothing in the list yet!");
+
+                } else {
+                    for (int i = 1; i <= list.size(); i++) {
+                        System.out.println(i + ". " + list.get(i - 1));
+                    }
+                }
+                text = scanner.next();
             } else {
-                System.out.println(text);
+                list.add(text);
+                System.out.println("Added:" + text);
                 text = scanner.next();
             }
         }
