ea01fe3c14652efc62d6fd10114bb49c10185ab7
Yingming23 <liyingming13@gmail.com>
2022-08-20 16:55:32 +0800

    Duke.java Add Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,54 @@ import java.util.ArrayList;
 
 public class Duke {
 
+    public class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsUndone() {
+            this.isDone = false;
+        }
+
+        public String toString() {
+            return "[" + getStatusIcon() + "] " + description;
+        }
+    }
+
     private void run() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I am a Store And Response Bot, otherwise known as Starboard\n" +
                 "What can I do for you today?");
 
-        ArrayList<String> store = new ArrayList<>();
+        ArrayList<Task> store = new ArrayList<>();
 
         while (true) {
+
+            if (sc.hasNext("mark")) {
+                sc.next();
+                int num = Integer.parseInt(sc.next());
+                store.get(num - 1).markAsDone();
+                System.out.println("Task successfully completed!\n" + store.get(num - 1));
+                continue;
+            } else if (sc.hasNext("unmark")) {
+                sc.next();
+                int num = Integer.parseInt(sc.next());
+                store.get(num - 1).markAsUndone();
+                System.out.println("Task incomplete.\n" + store.get(num - 1));
+                continue;
+            }
 
             String next = sc.nextLine();
 
@@ -21,9 +61,10 @@ public class Duke {
                 for (int i = 0; i < store.size(); i++) {
                     System.out.println(i + 1 + ". " + store.get(i));
                 }
+            } else if (next.equals("")) {
             } else {
-                store.add(next);
-                System.out.println("Added: " + next);
+                store.add(new Task(next));
+                System.out.println("Added new task: " + next);
             }
         }
     }
