159fae242c337c27023deddcec01d3de85613b00
sarrrdin <sarrrdin@gmail.com>
2022-08-20 14:38:54 +0800

    Add Save File option

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -2,6 +2,11 @@
  * Project done by Hong Jin.
  */
 import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
  * TaskList class to store the list of Tasks.
@@ -9,7 +14,12 @@ import java.util.*;
  * Also allows user to mark and unmark the task in list.
  */
 public class TaskList {
-    List<Task> memo = new ArrayList<>();
+    private List<Task> memo;
+
+    public TaskList() {
+        this.memo = new ArrayList<>();
+        loadfromFile();
+    }
 
     public String addTask(Task t) {
         memo.add(t);
@@ -46,5 +56,62 @@ public class TaskList {
         String noteUpdated = "Now you have " + memo.size() + " tasks in the list.";
         return "Noted. I've deleted this task:\n      " + temp
                 + "\n    " + noteUpdated;
+    }
+
+    public void loadfromFile() {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("tasks.txt"));
+            String row = reader.readLine();
+
+            while (row != null) {
+                //Example of line: [T] [X] read book
+                String[] parse = row.split(" ", 3);
+                Task task = null;
+
+                task = new Task(parse[2], parse[0]);
+
+                if (parse[1].equals("[X]")) {
+                    task.mark();
+                }
+                memo.add(task);
+                row = reader.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void savetoFile() {
+        String path = "tasks.txt";
+        FileWriter writer = null;
+
+        try {
+            File file = new File(path);
+            writer = new FileWriter(file);
+
+            for (int i = 0; i < memo.size(); i++) {
+                Task t = memo.get(i);
+                writer.write(String.format("%s\n", t.getTask()));
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 }
