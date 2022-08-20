a6182d03147ae75e32d72e1b0e9b7f61710ef498
Yingming23 <liyingming13@gmail.com>
2022-08-20 16:22:47 +0800

    Duke.java Add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
@@ -7,15 +8,23 @@ public class Duke {
         System.out.println("Hello! I am a Store And Response Bot, otherwise known as Starboard\n" +
                 "What can I do for you today?");
 
+        ArrayList<String> store = new ArrayList<>();
+
         while (true) {
+
             String next = sc.nextLine();
 
             if (next.equals("bye")) {
                 System.out.println("Bye bye! Hope to see you soon!");
                 break;
+            } else if (next.equals("list")) {
+                for (int i = 0; i < store.size(); i++) {
+                    System.out.println(i + 1 + ". " + store.get(i));
+                }
+            } else {
+                store.add(next);
+                System.out.println("Added: " + next);
             }
-
-            System.out.println(next);
         }
     }
 
