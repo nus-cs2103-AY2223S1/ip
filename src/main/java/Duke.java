7a3288f5833a59d7c3e5358ab1778bdbe0102a89
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-08-20 14:14:19 +0800

    Duke class: Remove import ArrayList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,5 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Duke {
     UserInputHistory userInputHistory = new UserInputHistory();
