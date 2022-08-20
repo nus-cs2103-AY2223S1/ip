f47d3f97b5bea1003ee3046621dbde96c0541362
yewjon <yewjon000@gmail.com>
2022-08-20 14:38:58 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+
+        boolean hasnext = true;
+
+        while (hasnext) {
+            String message = sc.nextLine();
+
+            switch (message) {
+                case "bye" :
+                    hasnext = false;
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                default :
+                    System.out.println(message);
+            }
+        }
     }
 }
