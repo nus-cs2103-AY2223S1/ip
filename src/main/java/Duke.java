fac6452bffc2b149fe7a6238b3705920c5cff625
Jin Wei <seetohjinwei@gmail.com>
2022-08-20 14:45:17 +0800

    Reorganise Duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,57 +2,62 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Duke {
-    private final Ui ui;
     private final Parser parser;
+    private final Storage storage;
+    private final Ui ui;
+    private final TaskList tasks;
 
     public Duke() {
-        this.ui = new Ui();
         this.parser = new Parser();
-    }
-
-    public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.ui.showWelcomeMessage();
-
-        Scanner scanner = new Scanner(System.in);
-
-        Storage storage = new Storage("data", "data/tasks");
+        this.storage = new Storage("data", "data/tasks");
+        this.ui = new Ui();
 
         TaskList tasks;
         try {
-            tasks = storage.load();
+            // Attempt to load tasks from storage.
+            tasks = this.storage.load();
         } catch (DukeException e) {
-            duke.ui.showErrorMessage(e);
-            // Load with empty list instead.
+            this.ui.showErrorMessage(e);
+            // Load empty list if fail to load from storage.
             tasks = new TaskList();
         }
+        this.tasks = tasks;
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        this.ui.showWelcomeMessage();
 
         while (scanner.hasNext()) {
             try {
                 String userInput = scanner.nextLine();
-                Command command = duke.parser.parseCommand(userInput);
+                Command command = this.parser.parseCommand(userInput);
                 // Populate command with tasks.
-                command.setData(tasks);
+                command.setData(this.tasks);
                 CommandResult result = command.execute();
                 if (result.shouldExit()) {
                     // Exit application by exiting the scan loop.
                     break;
                 }
                 if (result.shouldUpdateFile()) {
-                    storage.save(tasks);
+                    // Save to storage.
+                    this.storage.save(this.tasks);
                 }
-                duke.ui.showResult(result);
+                this.ui.showResult(result);
             } catch (DukeException | IOException e) {
-                duke.ui.showErrorMessage(e);
+                this.ui.showErrorMessage(e);
             } catch (NumberFormatException e) {
                 // Handles case where user inputs an invalid number.
-                duke.ui.showErrorMessage("Invalid number!");
+                this.ui.showErrorMessage("Invalid number!");
             }
         }
 
-        duke.ui.showGoodbyeMessage();
-
+        this.ui.showGoodbyeMessage();
         // Close scanner.
         scanner.close();
+    }
+
+    public static void main(String[] args) {
+        new Duke().run();
     }
 }
