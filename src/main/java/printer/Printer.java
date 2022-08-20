6d252a8264a8d7c1375335d7d06c9bbb640ab21d
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-08-20 17:01:33 +0800

    Delete unused Printer class

--- src/main/java/printer/Printer.java
+++
@@ -1,9 +0,0 @@
-package printer;
-
-public class Printer {
-    private static final String DASH = "----------------------------------------";
-
-    public static void print(String text) {
-        System.out.print(String.format("%s\n%s\n%s\n", DASH, text, DASH));
-    }
-}
