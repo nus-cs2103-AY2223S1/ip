16356c39aae1b0b431504541c40db9691a902460
myangat <mingyangy31@gmail.com>
2022-08-20 23:18:15 +0800

    For Level 2, I did it first, cause I haven't seen the Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +  "What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+        input = sc.nextLine();
+        List<String> list = new ArrayList<>();
+
+        while(!input.equals("bye")){
+            if(input.equals("list")){
+                for(int i = 0; i < list.size(); i++)
+                    System.out.println(i+1 + ". " + list.get(i));
+            } else {
+                list.add(input);
+                System.out.println("added: " + input);
+            }
+            input = sc.nextLine();
+
+        }
+        System.out.print("Bye. Hope to see you again soon!");
+
+
+
     }
 }
