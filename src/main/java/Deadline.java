27219e2ff1c82cba28ef729d0e1ca156e3ccc8ad
zbz-lvlv <zbz.lvlv@gmail.com>
2022-08-20 16:05:38 +0800

    Revert "Add support for LocalDate deadline"
    This reverts commit ee5090f995b8afada71e0430b56b24924db4caa5.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,15 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 public class Deadline extends Task{
 
-    private LocalDate deadline;
+    private String deadline;
 
     public Deadline(String name, boolean done, String deadline) throws TaskNoNameException {
         super(name, done);
-        this.deadline = LocalDate.parse(deadline);
+        this.deadline = deadline;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.deadline.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ")";
+        return "[D]" + super.toString() + " (by: " + this.deadline + ")";
     }
 
 }
