bda65200de44352c2a046308302a7382e6e2062d
ningtan11 <e0773603@u.nus.edu>
2022-08-20 19:18:13 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        boolean open = true;
+        while (open) {
+            if (text.equalsIgnoreCase("bye")) {
+                scanner.close();
+                open = false;
+                System.out.println("Goodbye! Thank you for visiting Duke.");
+            } else {
+                System.out.println(text);
+                text = scanner.next();
+            }
+        }
     }
 }
