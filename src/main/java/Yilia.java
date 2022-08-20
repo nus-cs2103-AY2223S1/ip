60e4a225cc4463715f4bf5a5841feb709ac0d7e2
WingKei67 <e0817842@u.nus.edu>
2022-08-20 16:15:30 +0800

    Enabled file saving

--- src/main/java/Yilia.java
+++ src/main/java/Yilia.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -32,6 +33,10 @@ class Task {
     Task(String content) {
         this.content = content;
     }
+    Task(String content, boolean isDone) {
+        this(content);
+        this.isDone = isDone;
+    }
     public void check() {
         isDone = true;
     }
@@ -40,7 +45,10 @@ class Task {
     }
     @Override
     public String toString() {
-        return "[" + (isDone? 'X': ' ') + "]" + content;
+        return "[" + (isDone? 'X': ' ') + "] " + content;
+    }
+    public String parse() {
+        return content;
     }
 }
 
@@ -48,9 +56,16 @@ class Todo extends Task {
     Todo(String content) {
         super(content);
     }
+    Todo(String content, boolean isDone) {
+        super(content, isDone);
+    }
     @Override
     public String toString() {
         return "[T]" + super.toString();
+    }
+    @Override
+    public String parse() {
+        return "T / " + (isDone ? "1" : "0") + " / " + super.parse();
     }
 }
 
@@ -60,9 +75,17 @@ class Deadline extends Task {
         super(content);
         this.date = date;
     }
+    Deadline(String content, boolean isDone, String date) {
+        super(content, isDone);
+        this.date = date;
+    }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(" + date.substring(0,2) + ":" + date.substring(2) + ")";
+        return "[D]" + super.toString() + " (by: " + date + ")";
+    }
+    @Override
+    public String parse() {
+        return "D / " + (isDone ? "1" : "0") + " / " + super.parse() + " / " + date;
     }
 }
 
@@ -72,9 +95,17 @@ class Event extends Task {
         super(content);
         this.date = date;
     }
+    Event(String content, boolean isDone, String date) {
+        super(content, isDone);
+        this.date = date;
+    }
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(" + date.substring(0,2) + ":" + date.substring(2) + ")";
+        return "[E]" + super.toString() + " (at: " + date + ")";
+    }
+    @Override
+    public String parse() {
+        return "E / " + (isDone ? "1" : "0") + " / " + super.parse() + " / " + date;
     }
 }
 
@@ -108,7 +139,7 @@ class Yilia {
     public static void mark(int index) {
         try {
             abilities.get(index - 1).check();
-            System.out.println("Nice! I've marked this task as done:\n" + abilities.get(index - 1));
+            System.out.println("Nice! I've marked this task as done:\n  " + abilities.get(index - 1));
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Index " + index + " out of bounds\nPlease input another index");
         }
@@ -117,7 +148,7 @@ class Yilia {
     public static void unmark(int index) {
         try {
             abilities.get(index - 1).uncheck();
-            System.out.println("OK, I've marked this task as not done yet:\n" + abilities.get(index - 1));
+            System.out.println("OK, I've marked this task as not done yet:\n  " + abilities.get(index - 1));
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Index " + index + " out of bounds\nPlease input another index");
         }
@@ -138,11 +169,11 @@ class Yilia {
             throw new DescriptionEmptyException(type);
         }
         if (type.equals(Type.DEADLINE)) {
-            Deadline deadline = new Deadline(info[0], info[1]);
+            Deadline deadline = new Deadline(info[0].trim(), info[1].substring(3));
             abilities.add(deadline);
             echo(deadline);
         } else if (type.equals(Type.EVENT)) {
-            Event event = new Event(info[0], info[1]);
+            Event event = new Event(info[0].trim(), info[1].substring(3));
             abilities.add(event);
             echo(event);
         }
@@ -153,28 +184,77 @@ class Yilia {
         System.out.println("Noted. I've removed this task:\n  " + task + "\nNow you have " + abilities.size() + " tasks in the list.");
     }
 
+    private static boolean parseStringToBoolean(String num) {
+        return Integer.parseInt(num) == 1;
+    }
+
+    public static void loadFromFile() {
+        try {
+            BufferedReader reader;
+            reader = new BufferedReader(new FileReader("data/yilia.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] info = line.split(" / ");
+                switch (info[0]) {
+                    case "T":
+                        abilities.add(new Todo(info[2], parseStringToBoolean(info[1])));
+                        break;
+                    case "D":
+                        abilities.add(new Deadline(info[2], parseStringToBoolean(info[1]), info[3]));
+                        break;
+                    case "E":
+                        abilities.add(new Event(info[2], parseStringToBoolean(info[1]), info[3]));
+                        break;
+                }
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot read the file!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadToFile() {
+        try {
+            FileWriter writer = new FileWriter("data/yilia.txt");
+            for (int i = 0; i < abilities.size(); i++) {
+                writer.write(abilities.get(i).parse());
+                writer.write('\n');
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot read the file!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws DontKnowException {
         greet();
+        loadFromFile();
         Scanner scanner = new Scanner(System.in);
         do {
             try {
                 String message = scanner.next();
                 if (message.equals("bye")) {
+                    loadToFile();
                     exit();
                     return;
                 } else if (message.equals("list")) {
                     showList();
-                } else if (message.contains("unmark")) {
+                } else if (message.trim().equals("unmark")) {
                     unmark(scanner.nextInt());
-                } else if (message.contains("mark")) {
+                } else if (message.trim().equals("mark")) {
                     mark(scanner.nextInt());
-                } else if (message.contains("deadline")) {
-                    addTask(scanner.nextLine(), Type.DEADLINE);
-                } else if (message.contains("event")) {
-                    addTask(scanner.nextLine(), Type.EVENT);
-                } else if (message.contains("todo")) {
-                    addTask(scanner.nextLine(), Type.TODO);
-                } else if (message.contains("delete")) {
+                } else if (message.trim().equals("deadline")) {
+                    addTask(scanner.nextLine().trim(), Type.DEADLINE);
+                } else if (message.trim().equals("event")) {
+                    addTask(scanner.nextLine().trim(), Type.EVENT);
+                } else if (message.trim().equals("todo")) {
+                    addTask(scanner.nextLine().trim(), Type.TODO);
+                } else if (message.trim().equals("delete")) {
                     delete(scanner.nextInt());
                 } else {
                     throw new DontKnowException();
