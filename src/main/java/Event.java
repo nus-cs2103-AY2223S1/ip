90cedad42835e69e8065442a0f9f3a24417250c4
Mai <e0694444@u.nus.edu>
2022-08-21 15:20:15 +0800

    Change timeRange field to startDateTime and endDateTime field
    Event class now stores the start and end of the event as LocalDateTime objects.
    
    toString() method is also modified to
    reflect these changes.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,21 @@
-public class Event extends Task {
-    protected String timeRange;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
 
-    public Event(String taskDescription, String timeRange) {
+public class Event extends Task {
+    protected LocalDateTime startDateTime;
+    protected LocalDateTime endDateTime;
+
+    public Event(String taskDescription, LocalDateTime startDateTime, LocalDateTime endDateTime) {
         super(taskDescription);
-        this.timeRange = timeRange;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + timeRange + ")";
+        String formattedStartDateTime = this.startDateTime.format(DateTimeFormatter.ofPattern("MMM dd uuuu, HHmm").withResolverStyle(ResolverStyle.STRICT.STRICT));
+        String formattedEndDateTime = this.endDateTime.format(DateTimeFormatter.ofPattern("MMM dd uuuu, HHmm").withResolverStyle(ResolverStyle.STRICT.STRICT));
+        return String.format("[E] %s (at: %s to %s)", super.toString(), formattedStartDateTime, formattedEndDateTime);
     }
-
 }
