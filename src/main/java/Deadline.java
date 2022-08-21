5af1c78e147f01e2ea1a2208f1192c06f41d4e2f
Ng Xing Yu <ng.xingyu@u.nus.edu>
2022-08-21 13:41:01 +0800

    Teach Duke to understand date and time command
    - Update Deadline to take in date and time in specific dd/MM/yyyy HH:mm format
    - update input and Expected output

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,22 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Deadline(String description, String by) {
+
+public class Deadline extends Task {
+    protected LocalDate by;
+
+    public Deadline(String description, String by) throws DukeException {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(by, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        } catch (DateTimeParseException e) {
+            throw new DukeException("%s does not match the required dd/MM/yyyy HH:mm format", by);
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), by);
+        return String.format("[D]%s (by: %s)", super.toString(), by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
     }
 }
