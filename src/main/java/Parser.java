fe7d9db89a5f1c6f03745d43e457cb1403804a8e
Lee Ian Ee <35843006+leeianee@users.noreply.github.com>
2022-08-21 19:37:21 +0800

    Add Parser.java

---
+++ src/main/java/Parser.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Parser {
+    String input = "";
+    Scanner scan = new Scanner(System.in);
+    String[] validInputs = {"delete", "mark", "unmark", "todo", "deadline", "event"};
+    TaskList list;
+
+    public Parser(TaskList list) {
+        this.list = list;
+    }
+
+    public boolean parse(String input) {
+        if (input.equals("bye")) {
+            return false;
+        } else if (input.equals("list")) {
+            list.printTaskList();
+        } else {
+            String[] split = input.split(" ", 2);
+            try {
+                if (split.length > 0 && Arrays.asList(validInputs).contains(split[0])) {
+                    switch (split[0]) {
+                        case "delete": { // Checks for delete
+                            int index = Integer.parseInt(split[1]) - 1;
+                            this.list.deleteTask(index);
+                            break;
+                        }
+                        case "mark": { // Checks for mark
+                            int index = Integer.parseInt(split[1]) - 1;
+                            this.list.markTask(index);
+                            break;
+                        }
+                        case "unmark": { // Checks for unmark
+                            int index = Integer.parseInt(split[1]) - 1;
+                            this.list.unmarkTask(index);
+                            break;
+                        }
+                        case "todo":  // Checks for Todo
+                            if (split.length < 2) {
+                                throw new DukeException("todo");
+                            } else {
+                                String description = split[1];
+                                this.list.addTodo(description);
+                            }
+                            break;
+                        case "deadline":  // Checks for Deadline
+                            if (split.length < 2) {
+                                throw new DukeException("deadline");
+                            } else {
+                                String[] temp = split[1].split(" /by ", 2);
+                                String description = temp[0];
+                                String by = temp[1];
+                                this.list.addDeadline(description, by);
+                            }
+                            break;
+                        case "event":  // Checks for Event
+                            if (split.length < 2) {
+                                throw new DukeException("event");
+                            } else {
+                                String[] temp = split[1].split(" /at ", 2);
+                                String description = temp[0];
+                                String when = temp[1];
+                                this.list.addEvent(description, when);
+                            }
+                            break;
+                    }
+                } else {
+                    throw new DukeException(); // Invalid input
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getDescription());
+            }
+        }
+        return true;
+    }
+}
