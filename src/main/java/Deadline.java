add8e5264d76a5a6ed30b8e7331b732d4df5dc79
Marcusgwj <Marcusgwj@hotmail.com>
2022-08-21 22:25:01 +0800

    Add Dates and Times (Level 8)

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task{
-    protected String date;
-    public Deadline(String description, String date) {
+    protected LocalDate date;
+
+    public Deadline(String description, String date) throws DukeException{
         super(description);
-        this.date = date;
+        try {
+            this.date = LocalDate.parse(date);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Date should be in yyyy-mm-dd format.");
+        }
     }
 
     @Override
     public String toString() {
-        return ("[D]" + super.toString() + " (by: " + this.date + ")");
+        return ("[D]" + super.toString() + " (by: "
+                + this.date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")");
     }
 }
