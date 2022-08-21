9f959083b06732acdcede5fab3b4b595dda52339
seanmanik <seanmanik@gmail.com>
2022-08-22 01:32:40 +0800

    Add date formatter. Level-8 completed.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,19 @@
+import java.time.LocalDate;
+import java.lang.NumberFormatException;
+
 public class Deadline extends Task {
-    private String deadline;
-    public Deadline(String description, String deadline) {
+    private LocalDate deadline;
+    public Deadline(String description, String deadline) throws DukeException {
         super(description);
-        this.deadline = deadline;
+        String[] dateInfo = deadline.split("-");
+        if (dateInfo.length < 3) {
+            throw new DukeException("Please use format yyyy-mm-dd for your deadline!");
+        }
+        try {
+            this.deadline = LocalDate.of(Integer.valueOf(dateInfo[0]), Integer.valueOf(dateInfo[1]), Integer.valueOf(dateInfo[2]));
+        } catch (NumberFormatException e) {
+            throw new DukeException("Please use format yyyy-mm-dd for your deadline, and only use numbers!");
+        }
     }
 
     @Override
