3da37e87f79bed530268ed59883cb643d4df60b6
JovonLim <jovonlim@gmail.com>
2022-08-21 18:50:21 +0800

    Remove emote from msg

--- src/main/java/duke/exception/InvalidIndexException.java
+++ src/main/java/duke/exception/InvalidIndexException.java
@@ -2,6 +2,6 @@ package duke.exception;
 
 public class InvalidIndexException extends DukeException {
     public InvalidIndexException(String errorMsg) {
-        super("☹ OOPS!!! I'm sorry, " + errorMsg + " :-(");
+        super("OOPS!!! I'm sorry, " + errorMsg + " :-(");
     }
 }
--- src/main/java/duke/exception/InvalidInputException.java
+++ src/main/java/duke/exception/InvalidInputException.java
@@ -4,6 +4,6 @@ import duke.exception.DukeException;
 
 public class InvalidInputException extends DukeException {
     public InvalidInputException() {
-        super("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        super("OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 }
