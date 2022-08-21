962b1c6fc60f48fbbc872249415ada6ae8c12683
jorrdansoh <sohjingren@gmail.com>
2022-08-21 20:44:16 +0800

    Add Gradle support and JUnit tests

---
+++ src/test/java/dwuke/ParserTest.java
@@ -0,0 +1,33 @@
+package dwuke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ParserTest {
+    @Test
+    public void splitArgument_nonEmptyString_success() throws Exception {
+        assertArrayEquals(
+                new String[]{"assignment 1", "19-02-2020"},
+                Parser.splitArgument("assignment 1 /by 19-02-2020", "/by"));
+        assertArrayEquals(
+                new String[]{"exam for CS2103T", "20 Nov 2022"},
+                Parser.splitArgument("exam for CS2103T /at 20 Nov 2022", "/at"));
+        assertArrayEquals(
+                new String[]{"Finish iP", "2022-02-02"},
+                Parser.splitArgument("Finish iP /by 2022-02-02", "/by"));
+    }
+
+    @Test
+    public void splitArgument_emptyString_exceptionThrown() throws Exception {
+        try {
+            assertArrayEquals(
+                    new String[]{"", ""},
+                    Parser.splitArgument("", "/at"));
+            fail(); // the test should not reach this line
+        } catch (DwukeException e) {
+            assertEquals("oops!!! /at not fwound", e.getMessage());
+        }
+    }
+}
