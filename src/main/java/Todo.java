4e3602924cf7a6a5cac183ec08b77167ca7d8d63
HoJunHao2000 <kennethhojunhao@gmail.com>
2022-08-21 21:47:36 +0800

    Add Save and Load functionality
    Currently all past inputs are forgotten when application restarts.
    
    Users cannot be expected to re-enter past inputs every time it restarts.
    
    Data will be saved to a file and loaded in when application restarts.

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,9 +1,14 @@
 public class Todo extends Task {
 
-    protected String by;
-
     public Todo(String description) throws DukeException {
         super(description);
+        if (description.isBlank()) {
+            throw new DukeException("The description of a todo cannot be empty.");
+        }
+    }
+
+    public Todo(String description, boolean isDone) throws DukeException {
+        super(description, isDone);
         if (description.isBlank()) {
             throw new DukeException("The description of a todo cannot be empty.");
         }
