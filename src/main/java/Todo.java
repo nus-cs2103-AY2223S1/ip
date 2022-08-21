8eda27a6f017f7687dbbaeaf133924d34383f234
HoJunHao2000 <kennethhojunhao@gmail.com>
2022-08-21 21:29:38 +0800

    Level-5

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -2,8 +2,11 @@ public class Todo extends Task {
 
     protected String by;
 
-    public Todo(String description) {
+    public Todo(String description) throws DukeException {
         super(description);
+        if (description.isBlank()) {
+            throw new DukeException("The description of a todo cannot be empty.");
+        }
     }
 
     @Override
