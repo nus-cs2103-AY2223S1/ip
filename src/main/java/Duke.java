e164ee1db25b6e4b1a3af843830804f72f9bef84
gnahzeus <zhangyue1689@gmail.com>
2022-08-21 20:41:11 +0800

    Remove extra import

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.Scanner;
 import java.util.ArrayList;
 
