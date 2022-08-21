4c3b6fa96645f262916e793206b1a1fedb31e30a
teoyuqi <teo.yuqi@gmail.com>
2022-08-21 17:24:38 +0800

    Remove file io modules

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,5 @@
 package main.java;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
