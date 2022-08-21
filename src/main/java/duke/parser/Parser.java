882e86269e92c2ba60276e3fd689ce3d5277dac8
peppapighs <ptk.prasertsuk@gmail.com>
2022-08-21 10:22:52 +0800

    Refactor try-catch in Parser

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -17,65 +17,39 @@ public class Parser {
   public static Command parse(String fullCommand) throws DukeException {
     String[] inputs = fullCommand.trim().split("\\s+", 2);
 
-    switch (inputs[0]) {
-      case "bye":
-        return new ExitCommand();
-      case "list":
-        return new ListCommand();
-      case "mark":
-        try {
+    try {
+      switch (inputs[0]) {
+        case "bye":
+          return new ExitCommand();
+        case "list":
+          return new ListCommand();
+        case "mark":
           return (new MarkCommand(Integer.parseInt(inputs[1]) - 1));
-        } catch (IndexOutOfBoundsException e) {
-          throw new DukeException("Invalid input format!");
-        } catch (NumberFormatException e) {
-          throw new DukeException("Invalid input format!");
-        }
-      case "unmark":
-        try {
+        case "unmark":
           return (new UnmarkCommand(Integer.parseInt(inputs[1]) - 1));
-        } catch (IndexOutOfBoundsException e) {
-          throw new DukeException("Invalid input format!");
-        } catch (NumberFormatException e) {
-          throw new DukeException("Invalid input format!");
-        }
-      case "delete":
-        try {
+        case "delete":
           return (new DeleteCommand(Integer.parseInt(inputs[1]) - 1));
-        } catch (IndexOutOfBoundsException e) {
-          throw new DukeException("Invalid input format!");
-        } catch (NumberFormatException e) {
-          throw new DukeException("Invalid input format!");
-        }
-      case "todo":
-        try {
+        case "todo":
           return new AddCommand(new Todo(inputs[1], false));
-        } catch (IndexOutOfBoundsException e) {
-          throw new DukeException("Invalid input format!");
-        }
-      case "deadline":
-        try {
+        case "deadline":
           String[] deadlineArgs = inputs[1].split("\\s+/by\\s+", 2);
           return new AddCommand(
             new Deadline(deadlineArgs[0], false, deadlineArgs[1])
           );
-        } catch (IndexOutOfBoundsException e) {
-          throw new DukeException("Invalid input format!");
-        } catch (DateTimeParseException e) {
-          throw new DukeException("Invalid date format!");
-        }
-      case "event":
-        try {
+        case "event":
           String[] eventArgs = inputs[1].split("\\s+/at\\s+", 2);
           return new AddCommand(new Event(eventArgs[0], false, eventArgs[1]));
-        } catch (IndexOutOfBoundsException e) {
-          throw new DukeException("Invalid input format!");
-        } catch (DateTimeParseException e) {
-          throw new DukeException("Invalid date format!");
-        }
-      default:
-        throw new DukeException(
-          "Sorry, I don't know what do you mean by that."
-        );
+        default:
+          throw new DukeException(
+            "Sorry, I don't know what do you mean by that."
+          );
+      }
+    } catch (IndexOutOfBoundsException e) {
+      throw new DukeException("Invalid input format!");
+    } catch (NumberFormatException e) {
+      throw new DukeException("Invalid input format!");
+    } catch (DateTimeParseException e) {
+      throw new DukeException("Invalid date format!");
     }
   }
 }
