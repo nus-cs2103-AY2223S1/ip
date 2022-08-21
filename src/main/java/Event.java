633da9de7c718e7807015d2355c7a895dbf80b0f
aarontangr <aarontangr@gmail.com>
2022-08-22 02:23:01 +0800

    Fix conflicts

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -6,25 +6,19 @@ public class Event extends Task {
     private LocalDate dateTime;
     private DateTimeFormatter formatter;
 
-<<<<<<< .merge_file_a21232
-    Event(String description, String dateTimeString) {
-        super(description);
+    Event(String description, boolean isDone, String dateTimeString) {
+        super(description, isDone);
         try {
             formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
             dateTime = LocalDate.parse(dateTimeString, formatter);
         } catch (DateTimeParseException e) {
             throw e;
         }
-=======
-    Event(String description, boolean isDone, String details) {
-        super(description, isDone);
-        this.details = details;
->>>>>>> .merge_file_a20028
     }
 
     public String SaveString() {
         return String.format("E | %s | %s | %s\n", super.isDone ? "1" : "0",
-                super.description, details);
+                super.description, dateTime.format(formatter));
     }
     @Override
     public String toString() {
