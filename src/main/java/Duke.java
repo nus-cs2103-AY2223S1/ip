bf633ff92923c4f86266d52f999d28f2cd59d3bc
seanmanik <seanmanik@gmail.com>
2022-08-22 01:06:18 +0800

    Add saving and file read functions. Level-7 completed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
+
 enum Commands {
     TODO,
     EVENT,
@@ -7,6 +12,71 @@ enum Commands {
     DELETE
 }
 public class Duke {
+
+    public static void saveFile(ArrayList<Task> ls) {
+        try {
+            File createFile = new File("Duke.txt");
+            if (createFile.createNewFile()) {
+                System.out.println("Creating Duke.txt...");
+            } else {
+                System.out.println("Updating Duke.txt...");
+            }
+            FileWriter writer = new FileWriter("Duke.txt");
+            for (int i = 0; i < ls.size(); i++) {
+                writer.write(ls.get(i) + "\n");
+            }
+            writer.close();
+            System.out.println("Duke.txt is fully updated.");
+        } catch (IOException e) {bran
+            System.out.println("An error occured when trying to save your task list. Please try again.");
+        }
+    }
+
+    private static boolean isMarked(String toProcess) {
+        if (toProcess.startsWith("[ ")) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void retrieveFile(ArrayList<Task> ls) {
+        System.out.println("Retrieving files...");
+        try {
+            File savedFile = new File("Duke.txt");
+            Scanner reader = new Scanner(savedFile);
+            while (reader.hasNextLine()) {
+                String info = reader.nextLine();
+                if (info.startsWith("  [T]")) {
+                    String[] infoData = info.split("] ");
+                    ToDo toAdd = new ToDo(infoData[infoData.length - 1]);
+                    if (isMarked(infoData[infoData.length - 2])) {
+                        toAdd.mark();
+                    }
+                    ls.add(toAdd);
+                } else if (info.startsWith("  [D]")) {
+                    String[] infoData = info.split("] ");
+                    String[] descBy = infoData[infoData.length - 1].split("by: ");
+                    Deadline toAdd = new Deadline(descBy[0].substring(0, descBy[0].length() - 2), descBy[1].substring(0, descBy[1].length() - 1));
+                    if (isMarked(infoData[infoData.length - 2])) {
+                        toAdd.mark();
+                    }
+                    ls.add(toAdd);
+                } else if (info.startsWith("  [E]")) {
+                    String[] infoData = info.split("] ");
+                    String[] descAt = infoData[infoData.length - 1].split("at: ");
+                    Event toAdd = new Event(descAt[0].substring(0, descAt[0].length() - 2), descAt[1].substring(0, descAt[1].length() - 1));
+                    if (isMarked(infoData[infoData.length -2])) {
+                        toAdd.mark();
+                    }
+                    ls.add(toAdd);
+                }
+            }
+            reader.close();
+            System.out.println("Retrieval complete!");
+        } catch (FileNotFoundException e) {
+            System.out.println("No saved files. Starting a new task sheet!");
+        }
+    }
 
     public static void greet() {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
@@ -16,6 +86,7 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         String input = "";
         ArrayList<Task> list = new ArrayList<>();
+        retrieveFile(list);
         while (true) {
             input = scanner.nextLine();
             try {
@@ -81,6 +152,7 @@ public class Duke {
             }
 
         }
+        saveFile(list);
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
