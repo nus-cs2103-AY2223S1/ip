78d1e7943875d5e60ed75c571a29ffb610233ded
JasonYapzx <jasonyapzx@gmail.com>
2022-08-21 14:47:20 +0800

    Added date parsing (Level 8)

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -11,9 +11,4 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (at: " + at + ")";
     }
-
-    @Override
-    public Date getDate() {
-        return this.at;
-    }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -41,8 +41,4 @@ public class Task {
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
-
-    public Date getDate() {
-        return null;
-    }
 }
