7c84cf474c9736525436b7de5a3eb1a3fdb45a7f
Berted <46164341+Berted@users.noreply.github.com>
2022-08-22 00:10:38 +0800

    Modifies Deadline and Event classes to now use LocalDate class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,24 @@
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
     private static final String NO_ENDTIME_MESSAGE = "A Deadline-type Task must be provided with an ending time. Use the /by parameter to add a deadline.";
+    private static final String INVALID_DATE_MESSAGE = "Please provide me a valid date in the following format:\n  YYYY-MM-DD\ni.e. 29th February 2000 is 2000-02-29";
 
-    protected String endTime;
-
+    protected LocalDate endDate;
     Deadline(String desc, String endTime) throws DukeException {
         super(desc);
         if (endTime == null || endTime.equals("")) {
             throw new DukeException(NO_ENDTIME_MESSAGE);
         }
-        this.endTime = endTime;
+        try {
+            this.endDate = LocalDate.parse(endTime);
+        } catch (DateTimeException e) {
+            throw new DukeException(INVALID_DATE_MESSAGE, e);
+        }
+
     }
 
     Deadline(String desc, String endTime, boolean isDone) throws DukeException {
@@ -16,12 +26,17 @@ public class Deadline extends Task {
         if (endTime == null || endTime.equals("")) {
             throw new DukeException(NO_ENDTIME_MESSAGE);
         }
-        this.endTime = endTime;
+        try {
+            this.endDate = LocalDate.parse(endTime);
+        } catch (DateTimeException e) {
+            throw new DukeException(INVALID_DATE_MESSAGE, e);
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.endTime);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, dd MMMM YYYY");
+        return String.format("[D]%s (by: %s)", super.toString(), this.endDate.format(formatter));
     }
 
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,43 @@
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
     private static final String NO_TIMERANGE_MESSAGE = "An Event-type Task must be provided with a time range. Use the /at parameter to add a time range.";
+    private static final String INVALID_DATE_MESSAGE = "Please provide me valid date(s) in the following format:\n"
+        + "  YYYY1-MM1-DD1 YYYY2-MM2-DD2\n"
+        + "i.e. 29th February 2000 to 2nd March 2000 is 2000-02-29 2000-03-02. You can provide only one date if you choose.";
+    private static final String INVALID_DATERANGE_MESSAGE = "The second date is prior to the first date.";
 
-    protected String timeRange;
+    protected LocalDate startTime;
+    protected LocalDate endTime;
+
+    private void parseTimeRange(String timeRange) throws DukeException {
+        String[] splitTimeRange = timeRange.split("\\s+");
+        if (splitTimeRange.length > 2) {
+            throw new DukeException(INVALID_DATE_MESSAGE);
+        }
+        try {
+            this.startTime = LocalDate.parse(splitTimeRange[0]);
+            if (splitTimeRange.length > 1) {
+                this.endTime = LocalDate.parse(splitTimeRange[1]);
+            } else {
+                this.endTime = this.startTime;
+            }
+        } catch (DateTimeException e) {
+            throw new DukeException(INVALID_DATE_MESSAGE);
+        }
+        if (this.endTime.compareTo(this.startTime) < 0) {
+            throw new DukeException(INVALID_DATERANGE_MESSAGE);
+        }
+    }
 
     Event(String desc, String timeRange) throws DukeException {
         super(desc);
         if (timeRange == null || timeRange.equals("")) {
             throw new DukeException(Event.NO_TIMERANGE_MESSAGE);
         }
-        this.timeRange = timeRange;
+        parseTimeRange(timeRange);
     }
 
     Event(String desc, String timeRange, boolean isDone) throws DukeException {
@@ -16,12 +45,19 @@ public class Event extends Task {
         if (timeRange == null || timeRange.equals("")) {
             throw new DukeException(Event.NO_TIMERANGE_MESSAGE);
         }
-        this.timeRange = timeRange;
+        parseTimeRange(timeRange);
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (at: %s)", super.toString(), this.timeRange);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, dd MMMM YYYY");
+        if (this.startTime.equals(this.endTime)) {
+            return String.format("[E]%s (at: %s)", super.toString(), this.startTime.format(formatter));
+        } else {
+            return String.format("[E]%s (at: %s - %s)", super.toString(), this.startTime.format(formatter),
+                    this.endTime.format((formatter)));
+        }
+
     }
 
 }
