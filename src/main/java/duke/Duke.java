04e609381f0e466e57b5de66a0539e30a80c062e
teoyuqi <teo.yuqi@gmail.com>
2022-08-22 16:54:23 +0800

    Add JavaDocs for `Duke`

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,38 +5,43 @@ import java.util.Scanner;
 import java.util.function.BiFunction;
 
 public class Duke {
-    /**
-     * Directory and filename path on disk to save task list.
-     */
+    /** Directory path on disk to find/save task list. */
     private static final String OUTPUT_DIRECTORY = "data";
+
+    /** Filename to save task list on disk. */
     private static final String OUTPUT_FILENAME = "list.txt";
 
-    /**
-     * 'List' attribute to store inputs.
-     */
+    /** `TaskList` object to store `Tasks` given by user. */
     private final TaskList taskList = new TaskList();
 
-    /**
-     * `Ui` object to handle user-interface.
-     */
+    /** `Ui` object to handle user-interface. */
     private final Ui ui;
 
-    /**
-     * `Storage` object to handle reading and writing task list to disk.
-     */
+    /** `Storage` object to handle reading and writing task list to disk. */
     private final Storage storage;
 
-    /**
-     * `Parser` object to parse and handle user inputs.
-     */
+    /** `Parser` object to parse and handle user inputs. */
     private final Parser parser;
 
+    /**
+     * Constructor of `Duke` object.
+     * @param directory Directory path on disk to find/save task list.
+     * @param filename  Filename to save task list on disk.
+     */
     private Duke(String directory, String filename) {
         ui = new Ui();
         storage = new Storage(directory, filename, taskList);
         parser = new Parser(taskList);
     }
 
+    /**
+     * Runs `Duke` program and begin accepting user inputs.
+     * While running, the program does the following in repeat:
+     *      1. Take in user input.
+     *      2. Execute command depending on given user input.
+     *      3. Print output (if any).
+     *      4. Save tasks onto hard disk (if changes were made).
+     */
     private void run() {
         // Create Scanner object for user inputs.
         Scanner sc = new Scanner(System.in);
@@ -75,7 +80,7 @@ public class Duke {
     }
 
     /**
-     * Main function with program logic.
+     * Main function.
      * @param args Command line arguments not used.
      */
     public static void main(String[] args) {
