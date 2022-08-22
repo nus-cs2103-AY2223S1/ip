a1c9dabd808523e2f0829a70fc6c1a0f4f80a887
Lu Yiting <e0774189@u.nus.edu>
2022-08-22 21:40:21 +0800

    Add java docs

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -28,6 +28,9 @@ public class Duke {
     private Parser parser;
     private Ui ui;
 
+    /**
+     * Initializes Duke instance. Initializes the ui, taskList, parser and Storage instance.
+     */
     public Duke() {
         ui = new Ui();
         taskList = new TaskList();
@@ -388,9 +391,6 @@ public class Duke {
     }
 
 
-
-
-
     public void run() {
         ui.welcome();
         boolean isExit = false;
@@ -409,114 +409,11 @@ public class Duke {
         }
     }
 
-
-//    private void loadData() throws FileNotFoundException {
-//        File f = new File(filePath);
-//        Scanner sc = new Scanner(f);
-//
-//        while (sc.hasNext()) {
-//            String s = sc.nextLine();
-//            char type = s.charAt(1);
-//            boolean isDone = s.charAt(4) == 'X';
-//            String des = s.substring(7);
-//            Task newTask;
-//
-//            if (type == 'T') {
-//                newTask = new Todo(des);
-//            } else if (type == 'D') {
-//                int i = des.indexOf('(');
-//                newTask = new Deadline(des.substring(0, i - 1), des.substring(i + 5));
-//            } else if (type == 'E') {
-//                int i = des.indexOf('(');
-//                newTask = new Event(des.substring(0, i - 1), des.substring(i + 5));
-//            } else {
-//                newTask = null;
-//            }
-//            System.out.println(newTask);
-//            all.add(newTask);
-//            count += 1;
-//        }
-//    }
-//
-//    private void saveData() throws FileNotFoundException {
-//        try {
-//            FileWriter fw = new FileWriter(filePath);
-//            String s = "";
-//            for (int i = 0; i < all.size(); i++) {
-//                s += all.get(i).toString() + "\n";
-//            }
-//            fw.write(s);
-//            fw.close();
-//        } catch (IOException e) {
-//            System.out.println("Something went wrong: " + e.getMessage());
-//        }
-//    }
-
-//    private LocalDate parseDate(String s) {
-//        return LocalDate.parse(s.replace('/', '-'));
-//    }
-//
-//    private String dateToString(LocalDate date) {
-//        return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-//
-//    }
-//
-
-//
-////    private void displayList() {
-////        System.out.println("Here are the tasks in your list:");
-////        for (int i = 0; i < count; i++) {
-////            System.out.printf("%s. %s\n", i + 1, all.get(i).toString());
-////        }
-////    }
-////
-////    private void deleteRecord(int index) {
-////        System.out.println("Noted. I've removed this task:");
-////        System.out.println(all.get(index - 1).toString());
-////        count -= 1;
-////        System.out.printf("Now you have %s tasks in the list.\n", count);
-////        all.remove(index - 1);
-////    }
-////
-////    private void store(String s) {
-////        try {
-////            all.add(Task.of(s));
-////            System.out.println("Got it. I've added this task:");
-////            System.out.printf("  added: %s\n", all.get(count).toString());
-////            count += 1;
-////            System.out.printf("Now you have %s tasks in the list.\n", count);
-////        } catch (DukeException e) {
-////            System.out.println(e.toString());
-////        }
-////
-////
-////    }
-//
-//    private boolean startWith(String s, String target) {
-//        for (int i = 0; i < target.length(); i++) {
-//            if (s.charAt(i) != target.charAt(i)) return false;
-//        }
-//
-//        try {
-//            int val = Integer.parseInt(s.substring(target.length() + 1));
-//            return true;
-//        } catch (NumberFormatException e) {
-//            //System.out.println("Input String cannot be parsed to Integer.");
-//            return false;
-//        }
-//
-//    }
-//
-//    private int extractIndex(String s, String target) {
-////        int i = 0;
-////        try {
-//        int i = Integer.parseInt(s.substring(target.length() + 1));
-////        } catch (Exception e) {
-////            System.out.printf("Must be a number following %s\n", target);
-////        }
-//        return i;
-//    }
-
+    /**
+     * Encapsulates all types of tasks.
+     * <p>
+     * This class contain the description and type of the task
+     */
     public class Task {
         private String todo = "todo";
         private String ddl = "deadline";
@@ -524,37 +421,21 @@ public class Duke {
         protected String description;
         protected boolean isDone;
 
-      //  public Task of(String s) throws DukeException {
-//            int i = s.indexOf(" ");
-//            String identifier;
-//            String des = "";
-//            if (i == -1) {
-//                throw !isIdentifierValid(s) ? new DukeException("I'm sorry, but I don't know what that means :-(")
-//                                           : new DukeException("The description of a " + s + " cannot be empty.");
-//            } else {
-//                identifier = s.substring(0, i);
-//                des = s.substring(i + 1);
-//            }
-////            System.out.printf("%s\n", identifier);
-////            System.out.printf("%s\n", s.substring(i + 1));
-//            if (identifier.equals(todo)) {
-//                return new Todo(des);
-//            } else if (identifier.equals(ddl)) {
-//                int j = s.indexOf("/by");
-//                return new Deadline(s.substring(ddl.length() + 1, j - 1), s.substring(j + 4));
-//            } else if (identifier.equals(event)) {
-//                int j = s.indexOf("/at");
-//                return new Event(s.substring(event.length() + 1, j - 1), s.substring(j + 4));
-//            } else {
-//                throw new DukeException("I'm sorry, but I don't know what that means :-(");
-//            }
-//        }
-
+        /**
+         * Constructor for Task class.
+         *
+         * @param description description string of the task
+         */
         public Task(String description) {
             this.description = description;
             this.isDone = false;
         }
 
+        /**
+         * Returns the status icon of a task, representing whether it is done.
+         *
+         * @return a string containing the status icon
+         */
         public String getStatusIcon() {
             return (isDone ? "X" : " "); // mark done task with X
         }
@@ -573,9 +454,20 @@ public class Duke {
         }
     }
 
+    /**
+     * Encapsulates deadline tasks.
+     * <p>
+     * This class contain the description and the due date of the deadline instance.
+     */
     public class Deadline extends Task {
         protected LocalDate by;
 
+        /**
+         * Constructor for Deadline class.
+         *
+         * @param description description string of the task
+         * @param by a LocalDate instance representing the due date
+         */
         public Deadline(String description, LocalDate by) {
             super(description);
             this.by = by;
@@ -587,7 +479,17 @@ public class Duke {
         }
     }
 
+    /**
+     * Encapsulates todo tasks.
+     * <p>
+     * This class contain the description of a todo instance.
+     */
     public class Todo extends Task {
+        /**
+         * Constructor for Todo class.
+         *
+         * @param description description string of the task
+         */
         public Todo(String description) {
             super(description);
         }
@@ -598,9 +500,20 @@ public class Duke {
         }
     }
 
+    /**
+     * Encapsulates event tasks.
+     * <p>
+     * This class contain the description and the date of the event instance.
+     */
     public class Event extends Task {
         private LocalDate at;
 
+        /**
+         * Constructor for Event class.
+         *
+         * @param description description string of the task
+         * @parama at a LocalDate instance representing the date of the event
+         */
         public Event(String description, LocalDate at) {
             super(description);
             this.at = at;
@@ -620,57 +533,6 @@ public class Duke {
 
     public static void main(String[] args) {
         new Duke().run();
-//        Scanner sc = new Scanner(System.in);
-//
-//        try {
-//            loadData();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("data file not found! create a text file duke.txt under folder data and try again!");
-//        }
-//
-//
-//
-//        String s = "";
-//        while (true) {
-//            s = sc.nextLine();
-//            if (s.equals("bye")) {
-//                bye();
-//                break;
-//            } else if (s.equals("list")) {
-//                displayList();
-//            } else if (startWith(s, "mark")) {
-//                int index = extractIndex(s, "mark");
-//                //System.out.println(index);
-//                if (index > count || index < 1) {
-//                    System.out.println("index out of range");
-//                } else {
-//                    all.get(index - 1).markAsDone();
-//                }
-//            } else if (startWith(s, "unmark")) {
-//                int index = extractIndex(s, "unmark");
-//                //System.out.println(index);
-//                if (index > count || index < 1) {
-//                    System.out.println("index out of range");
-//                } else {
-//                    all.get(index - 1).markAsUndone();
-//                }
-//            } else if (startWith(s, "delete")) {
-//                int index =extractIndex(s, "delete");
-//                if (index > count || index < 1) {
-//                    System.out.println("index out of range");
-//                } else {
-//                    deleteRecord(index);
-//                }
-//            } else {
-//                store(s);
-//            }
-//
-//            try {
-//                saveData();
-//            } catch (FileNotFoundException e) {
-//                System.out.println("data file not found! create a text file duke.txt under folder data and try again!");
-//            }
-//        }
     }
 
 }
