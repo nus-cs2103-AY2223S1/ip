8d3e06a5d3209e4172d217501a8b969df6aac685
Connor Lim <dev@connorlim.net>
2022-08-22 19:11:34 +0800

    Add ability to read and write tasks from file

---
+++ src/main/java/TaskWriter.java
@@ -0,0 +1,101 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class TaskWriter {
+    File file;
+
+    TaskWriter(String path) {
+        file = new File(path);
+    }
+
+    List<Task> readTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String[] data = sc.nextLine().split(",");
+
+                Task task;
+                boolean done = Boolean.parseBoolean(data[1]);
+                String description = data[2];
+
+                if (data[0].equals("T")) {
+                    task = new Todo(description);
+                } else if (data[0].equals("D")) {
+                    task = new Deadline(description, data[3]);
+                } else if (data[0].equals("E")) {
+                    task = new Event(description, data[3]);
+                } else {
+                    task = new Task(description);
+                }
+
+                if (done) {
+                    task.markDone();
+                }
+
+                tasks.add(task);
+            }
+        } catch (FileNotFoundException e) {
+            try {
+                file.createNewFile();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+                System.exit(1);
+            }
+            return new ArrayList<>();
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+
+        return tasks;
+    }
+
+    void writeTasks(List<Task> tasks) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(file);
+            for (Task task : tasks) {
+                List<String> line = new ArrayList<>();
+                if (task instanceof Todo) {
+                    line.add("T");
+                } else if (task instanceof Deadline) {
+                    line.add("D");
+                } else if (task instanceof Event) {
+                    line.add("E");
+                } else {
+                    continue;
+                }
+
+                line.add(Boolean.toString(task.isDone));
+                line.add(task.description);
+
+                if (task instanceof Deadline) {
+                    line.add(((Deadline) task).by);
+                } else if (task instanceof Event) {
+                    line.add(((Event) task).at);
+                }
+
+                writer.write(String.join(",", line) + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
