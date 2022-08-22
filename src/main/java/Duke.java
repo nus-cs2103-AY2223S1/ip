9b4e6e3cc225a8bb8cd17f16f9e66d8815e65cde
pewggls <>
2022-08-22 11:36:16 +0800

    add list to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,11 +6,22 @@ public class Duke {
         String bye = "Goodbye";
         System.out.println(greeting);
 
+        String[] list = new String[100];
+        int items = 0;
+
         String input;
         Scanner sc = new Scanner(System.in);
         input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 1; i < items + 1; i++) {
+                    System.out.println(i + ". " + list[i - 1]);
+                }
+            } else {
+                list[items] = input;
+                items++;
+                System.out.println("added: " + input);
+            }
             input = sc.nextLine();
         }
         System.out.println(bye);
