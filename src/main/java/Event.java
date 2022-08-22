cd0fd6354dafce03961eb4bcb214f08256061b86
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-08-22 11:11:34 +0800

    Update date field of Task to LocalDate time

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,15 @@
-public class Event extends Task {
-    private String date;
+import java.time.LocalDate;
 
-    public Event(String description, String date) {
+public class Event extends Task {
+    private final LocalDate date;
+
+    public Event(String description, String dateStr) {
         super(description);
-        this.date = date;
+        this.date = LocalDate.parse(dateStr);
     }
 
     @Override
     public String toString() {
-        return String.format("[E][%s]%s(at:%s)", this.isDone ? "X" : " ", this.description, this.date);
+        return String.format("[E][%s] %s (at: %s)", this.isDone ? "X" : " ", this.description, this.date);
     }
 }
