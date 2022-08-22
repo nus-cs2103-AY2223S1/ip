95ae42a70302dc77fa520c18b7e8925387064378
teoyuqi <teo.yuqi@gmail.com>
2022-08-22 20:04:25 +0800

    Add JavaDocs to `Event`

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,14 +1,14 @@
 package duke.task;
 
+/**
+ * Defines <Code>Event</Code> class.
+ */
 public class Event extends Task {
-
-    /**
-     * Deadline of 'Deadline' object
-     */
+    /** Venue of event. */
     protected String at;
 
     /**
-     * Constructor method for `Event`.
+     * Constructor for <Code>Event</COde>.
      * @param description Description of task.
      * @param at          Deadline of task.
      */
@@ -18,7 +18,7 @@ public class Event extends Task {
     }
 
     /**
-     * Constructor method for `Event`.
+     * Constructor for <Code>Event</Code> with <Code>isDone</Code> known.
      * @param description Description of task.
      * @param at          Deadline of task.
      * @param isDone      Whether task is done.
@@ -29,8 +29,8 @@ public class Event extends Task {
     }
 
     /**
-     * Override 'toString' method to return status and description of
-     * 'Event' object.
+     * Override <Code>toString</Code> method to return status and
+     * description of <Code>Event</Code> object.
      * @return [E][COMPLETION STATUS][TASK DESCRIPTION]
      */
     @Override
@@ -39,8 +39,8 @@ public class Event extends Task {
     }
 
     /**
-     * To produce a String with "|" delimiters for storing the task's data
-     * into a text file.
+     * To produce a <Code>String</Code> with "|" delimiters for
+     * storing the <Code>Event</Code>'s data into a text file.
      * @return "event|[COMPLETION STATUS]|[TASK DESCRIPTION]|[TASK VENUE]"
      */
     public String toFileFormat() {
