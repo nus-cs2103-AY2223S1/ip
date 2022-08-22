4b08b8eeb94c966202501443ef2ec746df4c2d78
emptygx <e0717559@u.nus.edu>
2022-08-22 16:46:51 +0800

    improve DukeException.java

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -2,6 +2,7 @@ public class DukeException extends Exception{
     private String message;
 
     public DukeException(String message) {
+        super(message);
         this.message = message;
     }
 
