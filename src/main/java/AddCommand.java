fbd1a9150686006c8d8f031ab2dbe4a1edcb68aa
TanWenCong <tanwencongg@gmail.com>
2022-08-22 15:19:57 +0800

    Extract Duke methods into OOP style
    Duke.java contains main code features.
    
    When more features are added, Duke.java becomes cluttered. This reduces
    readability.
    
    Methods are extracted into various java files (AddCommand... etc)
    
    This reduces code length in Duke.java, and methods are allocated
    appropriately in their respective new classes.

---
+++ src/main/java/AddCommand.java
@@ -0,0 +1,12 @@
+public class AddCommand extends Command {
+    private Task task;
+
+    public AddCommand(Task task) {
+        this.task = task;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui) {
+        taskList.addTask(task);
+    }
+}
