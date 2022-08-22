b25b42cf2bb8e072e1d61807eebfd73b8bba915a
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-08-22 12:04:51 +0800

    changed cli banner

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
+    public static String logo = ".__ .  ..  ..___" + "\n" +
+            "|  \\|  ||_/ [__ " + "\n" +
+            "|__/|__||  \\[___" + "\n";
+
+    public static String name = "Duke";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(Duke.logo);
+        System.out.println("Hello from\n" + Duke.name);
     }
 }
