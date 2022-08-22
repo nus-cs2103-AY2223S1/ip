678717b3dfaed910b9533604372de7f746aa4b7b
Chua Yu Xuan Jordan <jcyx1999@gmail.com>
2022-08-22 21:41:57 +0800

    Level-7

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Deadline extends Task  {
     private String deadline;
 
@@ -6,6 +10,10 @@ public class Deadline extends Task  {
         this.deadline = deadline;
     }
 
+    @Override
+    String writeToFile() {
+        return "D|" + super.writeToFile();
+    }
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (" + deadline + ")";
