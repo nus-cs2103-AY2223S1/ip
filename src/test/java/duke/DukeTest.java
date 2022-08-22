7cdc9990cf23dd72f002b179f5f8828fa6b19096
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2022-08-22 10:15:35 +0800

    Modify naming of test methods in DukeTest class to that of junit convention

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -15,7 +15,7 @@ import duke.task.ToDo;
 
 public class DukeTest {
     @Test
-    public void addTodoToTaskList() {
+    public void addTodo_descriptionToDo_success() {
         String descToDo = "Test todo for Duke !";
         String expected = "";
         String actual = "";
@@ -33,7 +33,7 @@ public class DukeTest {
     }
 
     @Test
-    public void addDeadlineToTaskList() {
+    public void addDeadline_descriptionDeadlineLocalDateTime_success() {
         String descDeadline = "Finish Assignment 1";
         String dateTime = "16 Aug 2023 1955";
         DateTimeFormatter dateTimeFormatter = DateHandler.getDateTimeFormatter();
@@ -53,17 +53,17 @@ public class DukeTest {
     }
 
     @Test
-    public void addEventToTaskList() {
-        String descDeadline = "Dinner Date";
+    public void addEvent_descriptionEventLocalDateTime_success() {
+        String descEvent = "Dinner Date";
         String dateTime = "25 Dec 2029 1835";
         DateTimeFormatter dateTimeFormatter = DateHandler.getDateTimeFormatter();
         String expected = "";
         String actual = "";
         try {
             TaskList taskList = new TaskList();
-            Task deadline = new Event(descDeadline, LocalDateTime.parse(dateTime, dateTimeFormatter));
-            expected = generateExpectedOutput(deadline);
-            taskList.addTaskToList(deadline);
+            Task event = new Event(descEvent, LocalDateTime.parse(dateTime, dateTimeFormatter));
+            expected = generateExpectedOutput(event);
+            taskList.addTaskToList(event);
             actual = taskList.getAddedTaskOutput();
         } catch (Exception e) {
             actual = e.getMessage();
