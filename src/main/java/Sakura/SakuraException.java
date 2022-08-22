8bf89ca1e37ab873f0d9e07404fb9709cfed8332
Shawn Kok <shawnkok00@gmail.com>
2022-08-22 11:18:38 +0800

    A-TextUiTesting

--- src/main/java/Sakura/SakuraException.java
+++ src/main/java/Sakura/SakuraException.java
@@ -2,25 +2,30 @@ package Sakura;
 
 public class SakuraException {
     protected static void invalidMark() {
-        System.out.println("\tMark command written incorrectly. Please check again.\n");
+        System.out.println("\t(ﾐ`⋏´ﾐ) RAWRRR Mark Command written incorrectly!! ✿\n");
     }
 
     protected static void genericTask() {
-        System.out.println("\tPlease specify either a Deadline, To Do, or Event!\n");
+        System.out.println("\t(ﾐ`⋏´ﾐ) RAWRRR What on earth does this mean!! ✿\n");
     }
 
     protected static void noSuchTask() {
-        System.out.println("\tNo such task in list of task.\n");
+        System.out.println("\t(ﾐ`⋏´ﾐ) RAWRRR There is no such thing!! ✿\n");
     }
 
     protected static void invalidDeadline() {
-        System.out.println("\tDeadline format is invalid\n");
+        System.out.println("\t(ﾐ`⋏´ﾐ) RAWRRR That is NOT a valid deadline!! ✿\n");
     }
 
     protected static void invalidEvent() {
-        System.out.println("\tEvent format is invalid\n");
+        System.out.println("\t(ﾐ`⋏´ﾐ) RAWRRR That is NOT a valid event!! ✿\n");
     }
+
     protected static void invalidCommand() {
-        System.out.println("\tCommand written incorrectly. Please check again.\n");
+        System.out.println("\t(ﾐ`⋏´ﾐ) BOOOOO Can you even write a proper command?? ✿\n");
+    }
+
+    protected static void invalidTodo() {
+        System.out.println("\t(ﾐ`⋏´ﾐ) RAWRRR Todo Command written incorrectly!! ✿\n");
     }
 }
--- src/main/java/Sakura/TaskManager.java
+++ src/main/java/Sakura/TaskManager.java
@@ -28,20 +28,24 @@ public class TaskManager {
 
     public void addTask(String input) {
         if (input.toLowerCase().startsWith("todo")) {
-            Todo todo = new Todo(input.substring("todo ".length()));
-            this.addDescription(todo);
+            if (input.split(" ").length == 1) {
+                SakuraException.invalidTodo();
+            } else {
+                Todo todo = new Todo(input.substring("todo ".length()));
+                this.addDescription(todo);
+            }
         } else if (input.toLowerCase().startsWith("deadline")) {
             try {
-                String[] strArr = input.split("/by", 2);
-                Deadline deadline = new Deadline(strArr[0], strArr[1].substring(3));
+                String[] strArr = input.split(" /by ", 2);
+                Deadline deadline = new Deadline(strArr[0], strArr[1]);
                 this.addDescription(deadline);
             } catch (ArrayIndexOutOfBoundsException e) {
                 SakuraException.invalidDeadline();
             }
         } else if (input.toLowerCase().startsWith("event")) {
             try {
-                String[] strArr = input.split("/at", 2);
-                Event event  = new Event(strArr[0], strArr[1].substring(3));
+                String[] strArr = input.split(" /at ", 2);
+                Event event  = new Event(strArr[0], strArr[1]);
                 this.addDescription(event);
             } catch (ArrayIndexOutOfBoundsException e) {
                 SakuraException.invalidEvent();
