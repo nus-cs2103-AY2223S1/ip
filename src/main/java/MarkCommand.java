ab0170dbf9640e91eb6f6406b18dd129172f65f7
jeromepui <jeromepui00@gmail.com>
2022-08-22 10:16:52 +0800

    Save tasks in hard disk automatically

--- src/main/java/MarkCommand.java
+++ src/main/java/MarkCommand.java
@@ -8,9 +8,11 @@ public class MarkCommand extends Command {
     @Override
     public String execute(List<Task> tasks) throws JarvisException {
         int taskIndex = super.getTaskIndex();
+
         if (taskIndex < 0 || taskIndex >= tasks.size()) {
             throw new JarvisException("No task found. Please enter a valid task number.");
         }
+
         if (getKeyCommand().equals("mark")) {
             tasks.get(taskIndex).setIsDone(true);
             return "Nice! I've marked this task as done:\n\t" + tasks.get(taskIndex);
---
+++ src/main/java/Storage.java
@@ -0,0 +1,96 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Storage {
+    private final String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<Task> loadTasks() throws JarvisException {
+        List<Task> tasks = new ArrayList<>();
+        File file = new File(this.filePath);
+
+        if (file.exists()) {
+            try {
+                Scanner sc = new Scanner(file);
+                Task task;
+
+                while (sc.hasNextLine()) {
+                    String[] split = sc.nextLine().split("\\|");
+                    switch (split[0]) {
+                    case "T":
+                        task = new ToDo(split[2]);
+                        break;
+                    case "D":
+                        task = new Deadline(split[2], split[3]);
+                        break;
+                    case "E":
+                        task = new Event(split[2], split[3]);
+                        break;
+                    default:
+                        throw new JarvisException("Failed to load task\n");
+                    }
+
+                    if (split[1].equals("1")) {
+                        task.setIsDone(true);
+                    }
+
+                    tasks.add(task);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+
+        return tasks;
+    }
+
+    public void saveTasks(List<Task> tasks) {
+        File file = new File(this.filePath);
+        file.getParentFile().mkdirs();
+
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            for (Task task: tasks) {
+                StringBuilder taskString = new StringBuilder();
+                if (task instanceof ToDo) {
+                    taskString.append("T|");
+                } else if (task instanceof Deadline) {
+                    taskString.append("D|");
+                } else if (task instanceof Event) {
+                    taskString.append("E|");
+                }
+
+                if (task.isDone) {
+                    taskString.append("1|");
+                } else {
+                    taskString.append("0|");
+                }
+
+                taskString.append(task.getDescription());
+
+                if (task instanceof Deadline) {
+                    taskString.append("|");
+                    taskString.append(((Deadline) task).getBy());
+                } else if (task instanceof Event) {
+                    taskString.append("|");
+                    taskString.append(((Event) task).getAt());
+                }
+
+                taskString.append(System.lineSeparator());
+                fileWriter.write(taskString.toString());
+            }
+
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
