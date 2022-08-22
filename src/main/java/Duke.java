ee1b46b7e6e4caad277e411ffd216f7e615f8a13
TanPingZhi <josiahtpz@gmail.com>
2022-08-22 12:12:00 +0800

    add data/tasks_data
    data are stored in the format of
    [TDE] | [ X] | desc | time

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,8 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Duke {
-    private static Scanner sc = new Scanner(System.in);
-    private static ArrayList<Task> tasks = new ArrayList<>();
     private final static String WELCOME =
             "Hello! I'm Duke\n" +
-                    "What can I do for you?\n";
+                    "What can I do for you?\n" +
+            "use /at or /by";
 
     public static void main(String[] args) {
         System.out.println(WELCOME);
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -11,4 +11,10 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (at: " + by + ")";
     }
+
+
+    @Override
+    public String toFileString() {
+        return String.format("E | %s | %s | %s", getStatusIcon(), this.description, by);
+    }
 }
