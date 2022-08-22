f3b9e0bfe79fd7f690e07d01b7ade10b7c6e86bf
pewggls <>
2022-08-22 11:14:50 +0800

    implement Duke skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "Hello";
+        String bye = "Goodbye";
+        System.out.println(greeting);
+
+        String input;
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+        System.out.println(bye);
+        return;
     }
 }
