f8486c13d685e72243acec861c8fddf89b1d473d
ssyap98 <77193965+ssyap98@users.noreply.github.com>
2022-08-22 22:36:48 +0800

    Added Level-3: Mark as Done function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,25 +10,49 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String horizontalLine = "-------------------------";
         System.out.println(logo + "\n" + horizontalLine);
-        System.out.println("Greetings, human! I am Sam.\n" + "How may I serve you today?" + "\n" + horizontalLine);
+        System.out.println("To all Subjects of Ymir. My name is Eren Yeager.\n" + "How can I help you?" + "\n" + horizontalLine);
 
-        ArrayList<String> store = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        String echo = sc.nextLine();
+        String echo = sc.next();
+
 
         while (!echo.equals("bye")) {
-            if (echo.equals("list")) {
-                System.out.println(horizontalLine);
-                for (int i = 0; i < store.size(); i++) {
-                    System.out.println((i + 1) + ". " + store.get(i));
+
+            switch (echo) {
+
+                case "list": {
+                    System.out.println(horizontalLine + "\n" + "Here are the tasks in your list:");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println((i + 1) + ". " + taskList.get(i));
+                    }
+                    System.out.println(horizontalLine);
+                    break;
                 }
-                System.out.println(horizontalLine);
-            } else {
-                System.out.println(horizontalLine + "\n" + "added: " + echo + "\n" + horizontalLine);
-                store.add(echo);
+
+                case "mark": {
+                    int index = sc.nextInt() - 1;
+                    Task completedTask = taskList.get(index);
+                    completedTask.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:" + "\n" + completedTask);
+                    break;
+                }
+
+                case "unmark": {
+                    int index = sc.nextInt() - 1;
+                    Task unfinishedTask = taskList.get(index);
+                    unfinishedTask.markAsNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:" + "\n" + unfinishedTask);
+                    break;
+                }
+
+                default:
+                    echo += sc.nextLine();
+                    System.out.println(horizontalLine + "\n" + "added: " + echo + "\n" + horizontalLine);
+                    taskList.add(new Task(echo));
             }
-            echo = sc.nextLine();
+            echo = sc.next();
         }
-        System.out.println(horizontalLine + "\n" + "Goodbye human. See you again!" + "\n" + horizontalLine);
+        System.out.println(horizontalLine + "\n" + "Keep moving forward until you finish all your tasks. Goodbye." + "\n" + horizontalLine);
     }
 }
