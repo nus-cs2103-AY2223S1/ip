ac6752964734b394f545aac38e54e0d38905b0fb
cfyjoanna <joannachanfy@gmail.com>
2022-08-22 15:03:27 +0800

    add date formatter

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,24 @@
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Duke {
     protected static ArrayList<Task> tasks = new ArrayList<>();
+    protected static DateTimeFormatter parserFormats = new DateTimeFormatterBuilder()
+            .appendOptional(DateTimeFormatter.ISO_LOCAL_DATE)
+            .appendOptional(DateTimeFormatter.ofPattern("d MMM uuuu"))
+            .appendOptional(DateTimeFormatter.ofPattern("yyyyMMdd"))
+            .toFormatter();
 
     public static void processTask(String input, String type) {
         String[] arr = input.split(" ", 2);
         String desc = "";
         try {
             desc = arr[1];
-        }
-        catch(ArrayIndexOutOfBoundsException e) {
+        } catch(ArrayIndexOutOfBoundsException e) {
             printOut("Please enter a description for your task!");
             return;
         }
@@ -23,22 +31,30 @@ public class Duke {
         if (type.equals("deadline")) {
             try {
                 String[] details = desc.split(" /by ");
-                temp = new Deadline(details[0], details[1]);
+                String by = LocalDate.parse(details[1], parserFormats).
+                        format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+                temp = new Deadline(details[0], by);
                 tasks.add(temp);
-            }
-            catch(ArrayIndexOutOfBoundsException e) {
+            } catch(ArrayIndexOutOfBoundsException e) {
                 printOut("Oops! Your deadline should have a due date after /by.");
+                return;
+            } catch (DateTimeParseException e) {
+                printOut("Please enter a valid date!");
                 return;
             }
         }
         if (type.equals("event")) {
             try {
                 String[] details = desc.split(" /at ");
-                temp = new Event(details[0], details[1]);
+                String at = LocalDate.parse(details[1], parserFormats).
+                        format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+                temp = new Event(details[0], at);
                 tasks.add(temp);
-            }
-            catch(ArrayIndexOutOfBoundsException e) {
+            } catch(ArrayIndexOutOfBoundsException e) {
                 printOut("Oops! Your event should have a date after /at.");
+                return;
+            } catch (DateTimeParseException e) {
+                printOut("Please enter a valid date!");
                 return;
             }
         }
@@ -61,55 +77,52 @@ public class Duke {
         while (!next.equals("bye")) {
             String[] nextWords = next.split(" ");
             switch(nextWords[0]) {
-                case "mark":
-                    try {
-                        int index = Integer.parseInt(nextWords[1]) - 1;
-                        tasks.get(index).markAsDone();
-                        printOut("I've marked this as done:\n" + tasks.get(index).toString());
-                        break;
-                    }
-                    catch(IndexOutOfBoundsException e) {
-                        printOut("This task number is invalid!");
-                        break;
-                    }
-                case "unmark":
-                    try {
-                        int index = Integer.parseInt(nextWords[1]) - 1;
-                        tasks.get(index).markAsUndone();
-                        printOut("I've marked this as undone:\n" + tasks.get(index).toString());
-                        break;
-                    }
-                    catch(IndexOutOfBoundsException e) {
-                        printOut("This task number is invalid!");
-                        break;
-                    }
-                case "delete":
-                    try {
-                        int index = Integer.parseInt(nextWords[1]) - 1;
-                        printOut("I'll remove this task:\n" + tasks.get(index).toString() +
-                                "\nYou now have " + (tasks.size() - 1) + " tasks.");
-                        tasks.remove(index);
-                        break;
-                    }
-                    catch(IndexOutOfBoundsException e) {
-                        printOut("This task number is invalid!");
-                        break;
-                    }
-                case "list":
-                    System.out.println("____________________________________________________________\n" +
-                            "Here are your tasks:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println((i + 1) + ". " + tasks.get(i).toString());
-                    }
-                    System.out.println("____________________________________________________________\n");
+            case "mark":
+                try {
+                    int index = Integer.parseInt(nextWords[1]) - 1;
+                    tasks.get(index).markAsDone();
+                    printOut("I've marked this as done:\n" + tasks.get(index).toString());
                     break;
-                case "todo":
-                case "deadline":
-                case "event":
-                    processTask(next, nextWords[0]);
+                } catch(IndexOutOfBoundsException e) {
+                    printOut("This task number is invalid!");
                     break;
-                default:
-                    printOut("I don't know this command. Try another one!");
+                }
+            case "unmark":
+                try {
+                    int index = Integer.parseInt(nextWords[1]) - 1;
+                    tasks.get(index).markAsUndone();
+                    printOut("I've marked this as undone:\n" + tasks.get(index).toString());
+                    break;
+                } catch(IndexOutOfBoundsException e) {
+                    printOut("This task number is invalid!");
+                    break;
+                }
+            case "delete":
+                try {
+                    int index = Integer.parseInt(nextWords[1]) - 1;
+                    printOut("I'll remove this task:\n" + tasks.get(index).toString() +
+                            "\nYou now have " + (tasks.size() - 1) + " tasks.");
+                    tasks.remove(index);
+                    break;
+                } catch(IndexOutOfBoundsException e) {
+                    printOut("This task number is invalid!");
+                    break;
+                }
+            case "list":
+                System.out.println("____________________________________________________________\n" +
+                        "Here are your tasks:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i).toString());
+                }
+                System.out.println("____________________________________________________________\n");
+                break;
+            case "todo":
+            case "deadline":
+            case "event":
+                processTask(next, nextWords[0]);
+                break;
+            default:
+                printOut("I don't know this command. Try another one!");
             }
 
             next = input.nextLine();
