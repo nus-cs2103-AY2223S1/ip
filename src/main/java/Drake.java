00e733ef63a43352506a004c7cfa65da58cd4b4d
Devesh <devesh.bag2@gmail.com>
2022-08-22 10:41:16 +0800

    added Mark as Done functionality

--- src/main/java/Drake.java
+++ src/main/java/Drake.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Drake {
-
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File("media/logo.txt"));
         StringBuilder logo = new StringBuilder();
@@ -19,16 +18,29 @@ public class Drake {
 
         sc = new Scanner(System.in);
         String command = sc.nextLine();
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         while (!Objects.equals(command, "bye")) {
             System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
             if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 int i = 1;
-                for (String item : list)
+                for (Task item : list)
                     System.out.println(i++ + ". " + item);
+            } else if (command.startsWith("mark")) {
+                String[] commands = command.split(" ");
+                int taskNo = Integer.parseInt(commands[1]);
+                System.out.println("I've marked this task as done!");
+                list.get(taskNo - 1).markAsDone();
+                System.out.println(list.get(taskNo - 1));
+            } else if (command.startsWith("unmark")) {
+                String[] commands = command.split(" ");
+                int taskNo = Integer.parseInt(commands[1]);
+                System.out.println("I've marked this task as not done (yet ;))");
+                list.get(taskNo - 1).unmarkAsDone();
+                System.out.println(list.get(taskNo - 1));
             } else {
                 System.out.println("added: " + command);
-                list.add(command);
+                list.add(new Task(command));
             }
             System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
             command = sc.nextLine();
