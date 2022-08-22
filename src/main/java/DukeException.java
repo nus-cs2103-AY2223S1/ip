bfe990725e63d6630398112976ee5285b7ed78fe
emptygx <e0717559@u.nus.edu>
2022-08-22 16:51:16 +0800

    Revert "improve DukeException.java"
    This reverts commit 4b08b8eeb94c966202501443ef2ec746df4c2d78.

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -2,7 +2,6 @@ public class DukeException extends Exception{
     private String message;
 
     public DukeException(String message) {
-        super(message);
         this.message = message;
     }
 
