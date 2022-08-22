cc29ece2c88e57db23bc4a42377574aa63424277
Lee Ian Ee <35843006+leeianee@users.noreply.github.com>
2022-08-22 17:31:28 +0800

    Add JUnit tests

---
+++ src/test/java/duke/EventTest.java
@@ -0,0 +1,20 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTest {
+    @Test
+    public void unmarkedTest(){
+        assertEquals(new Event("123456", "2pm").toString(),
+                "[E][ ] 123456 (at: 2pm)");
+    }
+
+    @Test
+    public void markedTest(){
+        Event temp = new Event("123456", "2pm");
+        temp.markAsDone();
+        assertEquals(temp.toString(), "[E][X] 123456 (at: 2pm)");
+    }
+}
---
+++ src/test/java/duke/TodoTest.java
@@ -0,0 +1,19 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void unmarkedTest(){
+        assertEquals(new Todo("123456").toString(), "[T][ ] 123456");
+    }
+
+    @Test
+    public void markedTest(){
+        Todo temp = new Todo("123456");
+        temp.markAsDone();
+        assertEquals(temp.toString(), "[T][X] 123456");
+    }
+}
