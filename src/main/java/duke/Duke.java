50d4ea80f652a2059cf79d96d38cc24715a2569d
cowlinn <2017.colin.toh@ejc.edu.sg>
2022-08-22 18:00:25 +0800

    Removed unnecessary code from Duke.main
    Removed the need to initialize anything from main function in duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -19,6 +19,7 @@ import javafx.scene.image.ImageView;
 /**
  * Main class of the program
  * Stores a taskList of tasks
+ * Contains references to elements in GUI
  */
 public class Duke {
 
@@ -35,86 +36,6 @@ public class Duke {
     private static TaskList tasks;
 
 
-
-
-
-/*    @Override
-    public void start(Stage stage) {
-        //Initialization
-        tasks = Storage.load();
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-        styleControls(stage, mainLayout);
-
-        //Added functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-
-    }*/
-
-/*    private Label getDialogLabel(String text) {
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }*/
-
-/*    private void styleControls(Stage stage, AnchorPane mainLayout) {
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Display Welcome Message
-        Label welcomeLabel = new Label(Ui.welcomeMessage());
-        dialogContainer.getChildren().add(
-                new DialogueBox(welcomeLabel,new ImageView(user))
-        );
-    }*/
 
     private void handleUserInput() {
         dialogContainer.getChildren().addAll(
