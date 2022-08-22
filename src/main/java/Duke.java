f3967464f6f22834f705acea3ca32bc4a77bb37a
jehousoh <e0694467@u.nus.edu>
2022-08-22 11:12:29 +0800

    Add Parser class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,12 +10,10 @@ public class Duke {
      */
     private TaskList list;
     private Storage storage;
-    private String filePath;
 
     public Duke(String filePath) {
         storage = new Storage(filePath);
         list = new TaskList(storage.load());
-        this.filePath = filePath;
     }
 
     public void run() {
@@ -26,67 +24,11 @@ public class Duke {
 
         while (true) {
             in = sc.nextLine();
-
             if (in.equals("bye")) {
                 break;
-            } else if (in.equals("clear")) {
-                list.clear();
-            } else if (in.equals("list")) {
-                list.printTasks();
-            } else if (in.startsWith("mark")) {
-                int index = Integer.valueOf(in.split(" ")[1]) - 1;
-                list.mark(index);
-            } else if (in.startsWith("unmark")) {
-                int index = Integer.valueOf(in.split(" ")[1]) - 1;
-                list.unmark(index);
-            } else if (in.startsWith("delete")) {
-                int index = Integer.valueOf(in.split(" ")[1]) - 1;
-
-                try {
-                    if (index >= list.getSize()) {
-                        throw new InvalidCommandException();
-                    }
-
-                    Task task = list.getTask(index);
-                    Ui.delete(task);
-                    list.delete(index);
-                    Ui.countTasks(list);
-                }
-
-                catch (InvalidCommandException e) {
-                    System.out.println(e.toString());
-                }
-
             } else {
-                try {
-                    Task task = TaskCreator.CreateTask(in);
-                    if (task == null) {
-                        throw new InvalidCommandException();
-                    } else if (task.getClass() == ErrorTask.class) {
-                        throw new InvalidDateException();
-                    } else if (task.description.length() < 1) {
-                        throw new NoDescriptionException();
-                    } else {
-                        list.add(task);
-                        Ui.add(task);
-                    }
-                }
-
-                catch (InvalidCommandException e) {
-                    System.out.println(e.toString());
-                }
-
-                catch (InvalidDateException e) {
-                    System.out.println(e.toString());
-                }
-
-                catch (NoDescriptionException e){
-                    System.out.println(e.toString());
-                }
+                Parser.parse(in, list);
             }
-            Ui.line();
-
-            FileWriting.update("./data/duke.txt", list);
         }
         Ui.bye();
     }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,68 @@
+public class Parser {
+
+    public static void parse(String in, TaskList list) {
+        //"clear": Clears the list
+        if (in.equals("clear")) {
+            list.clear();
+        //"list": Shows current list
+        } else if (in.equals("list")) {
+            list.printTasks();
+        //"mark": Mark a task as done
+        } else if (in.startsWith("mark")) {
+            int index = Integer.valueOf(in.split(" ")[1]) - 1;
+            list.mark(index);
+        //"unmark": Unmark a task
+        } else if (in.startsWith("unmark")) {
+            int index = Integer.valueOf(in.split(" ")[1]) - 1;
+            list.unmark(index);
+        //"delete": Delete a task
+        } else if (in.startsWith("delete")) {
+            int index = Integer.valueOf(in.split(" ")[1]) - 1;
+
+            try {
+                if (index >= list.getSize()) {
+                    throw new InvalidCommandException();
+                }
+
+                Task task = list.getTask(index);
+                Ui.delete(task);
+                list.delete(index);
+                Ui.countTasks(list);
+            }
+
+            catch (InvalidCommandException e) {
+                System.out.println(e.toString());
+            }
+        //Create a task
+        } else {
+            try {
+                Task task = TaskCreator.CreateTask(in);
+                if (task == null) {
+                    throw new InvalidCommandException();
+                } else if (task.getClass() == ErrorTask.class) {
+                    throw new InvalidDateException();
+                } else if (task.description.length() < 1) {
+                    throw new NoDescriptionException();
+                } else {
+                    list.add(task);
+                    Ui.add(task);
+                }
+            }
+
+            catch (InvalidCommandException e) {
+                System.out.println(e.toString());
+            }
+
+            catch (InvalidDateException e) {
+                System.out.println(e.toString());
+            }
+
+            catch (NoDescriptionException e){
+                System.out.println(e.toString());
+            }
+        }
+        Ui.line();
+        //Update the save file
+        FileWriting.update("./data/duke.txt", list);
+    }
+}
