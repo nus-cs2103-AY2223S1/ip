18e5b9d178f82a5cc71ae72117088a492eb107e7
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-23 20:44:47 +0800

    Add FindCommand representing case where user wants to find a Task in the current task list

---
+++ src/main/java/commands/FindCommand.java
@@ -0,0 +1,35 @@
+package commands;
+
+import common.Ui;
+import dukeexceptions.DukeException;
+import dukeexceptions.InsufficientArgumentsException;
+import tasklist.TaskList;
+
+public class FindCommand extends Command {
+    String[] args;
+
+    public FindCommand(String[] args) {
+        this.args = args;
+    }
+
+    public static void validateArguments(String[] args) throws DukeException {
+        if (args.length < 1) {
+            throw new InsufficientArgumentsException("Find command");
+        }
+    }
+
+    @Override
+    public void execute(TaskList taskList) {
+        TaskList results = new TaskList();
+        String target = this.args[0];
+
+        for (int i = 0; i < taskList.size(); i++) {
+            String taskRepresentation = taskList.get(i).toString();
+            if (taskRepresentation.contains(target)) {
+                results.addTask(taskList.get(i));
+            }
+        }
+
+        Ui.printFindResults(results);
+    }
+}
