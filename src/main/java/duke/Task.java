3f3b02ceb48eb53682d583bfa74398f310d2c108
Pinran-J <e0725480@u.nus.edu>
2022-08-23 21:19:51 +0800

    Added JavaDoc

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -5,11 +5,22 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructor for a Task object.
+     *
+     * @param description Description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Retrieves the correct status to be displayed based on
+     * if the task is done or not.
+     *
+     * @return Status icon.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
@@ -17,22 +28,33 @@ public class Task {
     public String getDescription() {
         return this.description;
     }
+
     public void flip() {
         this.isDone = !isDone;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markDone() {
         if (!this.isDone) {
             this.flip();
         }
     }
 
+    /**
+     * Marks the task as undone.
+     */
     public void markUndone() {
         if (this.isDone) {
             this.flip();
         }
     }
 
+    /**
+     * Outputs the correct string to be saved in save file.
+     * @return String of data to be stored.
+     */
     public String toSaveData() {
         int num = isDone ? 1 : 0;
         return num + " | " + this.description;
