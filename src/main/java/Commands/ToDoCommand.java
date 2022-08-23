9363244504bdee5444d5c09abf0df3f2b42b0a6c
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-23 16:53:44 +0800

    Create methods for ToDoCommand representing addition of Todo to task list

--- src/main/java/Commands/ToDoCommand.java
+++ src/main/java/Commands/ToDoCommand.java
@@ -1,4 +1,28 @@
 package Commands;
 
+import TaskList.TaskList;
+import Tasks.ToDo;
+import common.Ui;
+import dukeExceptions.DukeException;
+import dukeExceptions.MissingDescriptionException;
+
 public class ToDoCommand extends Command {
+    private String[] args;
+
+    public ToDoCommand(String[] args) {
+        this.args = args;
+    }
+
+    public static void validateArguments(String[] args) throws DukeException {
+        if (args.length == 0) {
+            throw new MissingDescriptionException("todo");
+        }
+    }
+
+    @Override
+    public void execute(TaskList taskList) {
+        ToDo newToDo = new ToDo(String.join(" ", this.args));
+        taskList.addTask(newToDo);
+        Ui.printAddTask(newToDo, taskList);
+    }
 }
