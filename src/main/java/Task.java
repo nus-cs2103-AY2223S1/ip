b8cb7a6cf4b19be96d207f908592522373cddc4f
turretDive <apangjun@gmail.com>
2022-08-24 00:50:13 +0800

    Add more oop

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,27 +2,54 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs Task with its description.
+     *
+     * @param description Description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Gets the status of the task.
+     *
+     * @return The status of the task.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " " ); // mark done task with X
     }
 
+    /**
+     * Marks task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks task as not done.
+     */
     public void markAsUndone() {
         this.isDone = false;
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return Description of the task.
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Shows the description and status of the task.
+     *
+     * @return String with the description and status of the task.
+     */
+    @Override
     public String toString() {
         return "[" + this.getStatusIcon() + "]" + this.getDescription() ;
     }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,10 +1,18 @@
 public class Todo extends Task {
-    protected String by;
-
+    /**
+     * Constructs a to-do task.
+     *
+     * @param description Description of the to-do task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Shows the to-do task description.
+     *
+     * @return String with the to-do task description.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
