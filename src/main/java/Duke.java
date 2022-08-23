23d59d7de114541a16165f50608e8ece29ac3ac8
dr-arrgghh <76149499+dr-arrgghh@users.noreply.github.com>
2022-08-24 05:39:11 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Duke {
+    private Task task;
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
@@ -17,17 +18,39 @@ public class Duke {
 
     public void chatDuke(){
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> strArray = new ArrayList<>();
+        ArrayList<Task> taskArray = new ArrayList<>();
         String str = sc.nextLine();
         if (str.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon");
         }
         while (!str.equals("bye")){
             if (str.equals("list")) {
-                returnArrayStrings(strArray);
+                returnArrayStrings(taskArray);
                 str = sc.nextLine();
-            } else {
-                strArray.add(str);
+            }
+
+            else if (str.equals("mark")) {
+                int num = sc.nextInt();
+                Task currTask = taskArray.get(num - 1);
+                currTask.mark();
+                System.out.println("Nice! I've marked this task as done:\n" +
+                        "[" + currTask.getStatusIcon() + "] " + currTask.description);
+                str = sc.nextLine();
+                str = sc.nextLine();
+            }
+
+            else if (str.equals("unmark")) {
+                int num = sc.nextInt();
+                Task currTask = taskArray.get(num - 1);
+                currTask.unMark();
+                System.out.println("OK, I've marked this task as not done yet:\n" +
+                        "[" + currTask.getStatusIcon() + "] " + currTask.description);
+                str = sc.nextLine();
+                str = sc.nextLine();
+            }
+
+            else {
+                taskArray.add(new Task(str));
                 System.out.println("added: " + str);
                 str = sc.nextLine();
             }
@@ -38,7 +61,7 @@ public class Duke {
         if (str.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon");
         } else {
-            strArray.add(str);
+            taskArray.add(new Task(str));
             str = sc.nextLine();
         }
 
@@ -47,15 +70,17 @@ public class Duke {
 
     }
 
-    public void returnArrayStrings(ArrayList<String> strArray){
-        for (int i = 0; i < strArray.size(); i++){
-            if (strArray.get(i) != null) {
-                System.out.println(Integer.toString(i + 1) + ". " + strArray.get(i));
+    public void returnArrayStrings(ArrayList<Task> tskArray){
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tskArray.size(); i++){
+            if (tskArray.get(i) != null) {
+                System.out.println(Integer.toString(i + 1) + "." + tskArray.get(i));
             } else {
                 return;
             }
         }
     }
+
 
 
 }
