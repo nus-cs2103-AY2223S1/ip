f2f517aea2472d96f4beb6ca54b01429e7f5cb44
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-23 16:55:24 +0800

    Create methods for UnmarkCommand representing unmarking a list item

--- src/main/java/Commands/UnmarkCommand.java
+++ src/main/java/Commands/UnmarkCommand.java
@@ -1,4 +1,36 @@
 package Commands;
 
+import TaskList.TaskList;
+import common.Ui;
+import dukeExceptions.DukeException;
+import dukeExceptions.IllegalIndexException;
+import dukeExceptions.MissingDescriptionException;
+
 public class UnmarkCommand extends Command {
+    private String[] args;
+
+    public UnmarkCommand(String[] args) {
+        this.args = args;
+    }
+
+    public static void validateArguments(String[] args) throws DukeException {
+        if (args.length < 1) {
+            throw new DukeException("Missing index!");
+        }
+        try {
+            int index = Integer.parseInt(args[0])-1;
+            if (index < 0) {
+                throw new IllegalIndexException();
+            }
+        } catch (NumberFormatException e) {
+            throw new DukeException(e.toString());
+        }
+    }
+
+    @Override
+    public void execute(TaskList taskList) {
+        int index = Integer.parseInt(args[0])-1;
+        taskList.unmarkTask(index);
+        Ui.printUnmarkTask(taskList.get(index));
+    }
 }
