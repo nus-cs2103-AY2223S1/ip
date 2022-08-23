940ee5fda6e5febb2d2dd140de5f1e0f0f9d6f88
Rama <kmram0908@gmail.com>
2022-08-23 13:15:32 +0800

    Add proper time support for deadline and event tasks

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,33 @@
-public class Deadline extends Task {
-    private String by;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    Deadline(String description, String by) {
+public class Deadline extends Task {
+    private LocalDate d;
+    private LocalTime t;
+    private boolean hasTime;
+
+    Deadline(String description, String by) throws DukeException {
         super(description);
-        this.by = by;
+        String[] dateAndTime = by.split(" ");
+        try {
+            this.d = LocalDate.parse(dateAndTime[0]);
+            if (dateAndTime.length == 2) {
+                this.t = LocalTime.parse(dateAndTime[1], DateTimeFormatter.ofPattern("HHmm"));
+                this.hasTime = true;
+            } else {
+                this.hasTime = false;
+            }
+        } catch (java.time.format.DateTimeParseException e) {
+            throw new DukeException("Please enter a valid date and time YYYY-MM-DD HHMM");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " 
+                + this.d.format(DateTimeFormatter.ofPattern(("MMM d yyyy"))) 
+                + (this.hasTime ? " " + this.t : "") 
+                + ")";
     }
 }
