4119dbd55cf79f184650c4eac8a244a63cd7eae8
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-08-23 16:36:56 +0800

    finish implementation of saving and loading tasks

---
+++ src/main/java/io/createFile.java
@@ -0,0 +1,38 @@
+package io;
+
+import exceptions.DukeException;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Creates a txt file that should eventually be used for
+ * the writing of a <code>Task[]</code> - in String format - to itself.
+ *
+ * @author Kang Wei
+ */
+public class createFile {
+
+  /**
+   * Creates a file in the specified location.
+   *
+   * @param fileLoc The location that the file should be created in.
+   * @throws DukeException Throws a DukeException if an IOException is caught.
+   */
+  public static void makeFile(String fileLoc) throws DukeException {
+    try {
+      File file = new File(fileLoc);
+      if (file.createNewFile()) {
+        System.out.println("Mumbot: A .txt file has been created in " +
+            fileLoc + " , which will contain your list of tasks once this session " +
+            "terminates <3\n");
+      } else {
+        System.out.println("Mumbot: A .txt file already existing in " +
+            fileLoc + " will be used for loading your previous list of tasks; and for " + 
+            "saving your list of tasks when this session terminates <3\n");
+      }
+    } catch (IOException e) {
+      throw new DukeException("Honey! There was a problem with creating or obtaining " + 
+          "your list of tasks at " + fileLoc + " ! :(\n");
+    }
+  }
+}
---
+++ src/main/java/io/loadFile.java
@@ -0,0 +1,83 @@
+package io;
+
+import tasks.Task;
+import exceptions.DukeException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Assists with loading .txt file containing a <code>Task[]</code> in String
+ * format.
+ *
+ * @author Kang Wei
+ */
+public class loadFile {
+
+  /**
+   * Parses a task in String format into a <code>Task</code>
+   * format, and returns it.
+   *
+   * @param taskString The task in String format.
+   * @return The task converted to a <code>Task</code> format.
+   */
+  private static Task parseTaskString(String taskString) {
+    String taskType = taskString.substring(1, 2);
+    String taskDescription;
+    String dateDescription;
+    if (taskType.equals("T")) { // Task is a Todo
+      taskDescription = taskString.substring(7);
+      return new Task(taskDescription);
+    } else {
+      /**
+       * Setting the appropriate dateDelimiter.
+       */
+      String dateDelimiter;
+      boolean isDeadline = false;
+      if (taskType.equals("D")) { // Task is a Deadline
+        dateDelimiter = " \\(by: ";
+        isDeadline = true;
+      } else { // Task is an Event
+        dateDelimiter = " \\(at: ";
+      }
+
+      /**
+       * Getting the appropriate descriptions after splitting the 
+       * task in String format by the dateDelimiter.
+       */
+      String[] inputArr = taskString.split(dateDelimiter);
+      taskDescription = inputArr[0].substring(7);
+      dateDescription = inputArr[1].substring(0, inputArr[1].length() - 1);
+
+      return (new Task(taskDescription, dateDescription, (
+              isDeadline ? Task.Type.DEADLINE : Task.Type.EVENT)));
+    }
+  }
+
+  /**
+   * Loads a .txt file of tasks, converts it to an
+   * <code>ArrayList<Task></code> and returns it.
+   *
+   * @param fileLoc The location of the file.
+   * @return The <code>ArrayList<Task></code> converted from the .txt file.
+   * @throws DukeException Throws an exception if an IOException is caught.
+   */
+  public static ArrayList<Task> load(String fileLoc) throws DukeException {
+    ArrayList<Task> tasks = new ArrayList<Task>();
+    try {
+      File file = new File(fileLoc);
+      Scanner sc = new Scanner(file);
+
+      while (sc.hasNext()) {
+        String taskString = sc.nextLine();
+        Task task = parseTaskString(taskString);
+        tasks.add(task);
+      }
+    } catch (IOException e) {
+      throw new DukeException("Honnney! There was a problem with loading your list " +
+          "of tasks from " + fileLoc + "! :,(");
+    }
+    return tasks;
+  }
+}
