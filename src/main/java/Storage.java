54c0af0984bca0eb570e432a2572a07161fa0c3c
Derrick Khoo <derrickkhoo2000@gmail.com>
2022-08-24 00:02:22 +0800

    Add new class Storage to save/load to hard disk

---
+++ src/main/java/Storage.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+    String filePath;
+
+    Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void saveData(ArrayList<Task> listOfTasks) throws Duke.DukeException {
+        File directory = new File("data");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        String filePath = "data/duke.text";
+        File data = new File(filePath);
+        // Write to data
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            for (Task t : listOfTasks) {
+                fileWriter.write(t.toFileDescription() + "\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new Duke.DukeException("Hey! Are you in the wrong directory? You are currently at" +
+                    e.getMessage());
+        }
+    }
+
+    public ArrayList<Task> loadData() throws Duke.DukeException {
+        ArrayList<Task> list = new ArrayList<>();
+        File directory = new File("data");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        String filePath = "data/duke.text";
+        File data = new File(filePath);
+        // Load the file data into corresponding ArrayList
+        try {
+            if (data.exists()) {
+                Scanner scanner = new Scanner(data);
+                while (scanner.hasNext()) {
+                    String currentlyAt = scanner.nextLine();
+                    char first = currentlyAt.charAt(0);
+                    Task task;
+                    switch(first) {
+                    case('T'): {
+                        task = Todo.fromFileDescription(currentlyAt);
+                        break;
+                    }
+                    case('D'): {
+                        task = Deadline.fromFileDescription(currentlyAt);
+                        break;
+                    }
+                    case('E'): {
+                        task = Event.fromFileDescription(currentlyAt);
+                        break;
+                    }
+                    default: {
+                        throw new Duke.DukeException("What!? How did this happen... I'm pretty sure you" +
+                                "have an itchy hand and modified the duke.txt file!!!");
+                    }
+                    }
+                    list.add(task);
+                }
+            } else {
+                data.createNewFile();
+            }
+        } catch (IOException e) {
+            throw new Duke.DukeException("Are you a hacker? How on earth did you get to this stage!?");
+        }
+        return list;
+    }
+}
