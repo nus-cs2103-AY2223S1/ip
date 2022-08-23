f71f813e3876b3e82fc56d419777c051a0e5d4fa
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-08-23 13:39:02 +0800

    Add TaskList.java to handle managing the Task ArrayList

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    /** Use an ArrayList Collection for handling the tasks. */
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public int getNumberOfTasks() {
+        return this.tasks.size();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void deleteTask(int index) throws DukeException {
+        if (index == -1) {
+            throw new DukeException("You must specify which task to delete!");
+        }
+        Task task = tasks.get(index - 1);
+        tasks.remove(index - 1);
+    }
+
+    public void changeTaskStatus(int taskId, boolean isDone) throws DukeException {
+        if (taskId == -1) {
+            throw new DukeException("You must specify which task to mark or unmark!");
+        }
+        Task task = tasks.get(taskId - 1);
+        task.setDoneStatus(isDone);
+    }
+}
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -10,6 +10,7 @@ fi
 if [ -e "./ACTUAL.TXT" ]
 then
     rm ACTUAL.TXT
+    rm "./data/tasks.txt"
 fi
 
 # compile the code into the bin folder, terminates if error occurred
@@ -21,6 +22,8 @@ fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+
+rm "./data/tasks.txt"
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
