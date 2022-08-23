26a413e3500b8c5de6c5ddea8ea32487c84eaf0e
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-08-23 11:30:07 +0800

    TaskList class: Add filtered tasks display.
    There is no good way to display filtered tasks.
    
    Add methods to filter the tasklist, get display strings of the tasks, and get task number for displaying.
    
    This way, users of the TaskList class can
    * display individual tasks with task number, and
    * diplay ffiltered lists with task numbers,
    without having access to the internal List<Task>

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -46,5 +47,37 @@ public class TaskList {
         return this.tasks.stream()
                 .map(StorageParser::toTaskString)
                 .collect(Collectors.toList());
+    }
+
+    public List<String> toDisplayStrings() {
+        return this.tasks.stream()
+                .map(Task::toString)
+                .collect(Collectors.toList());
+    }
+
+    public List<Task> filter(LocalDate date) {
+        List<Task> filtered = new ArrayList<>();
+
+        for (int i = 0; i < this.tasks.size(); ++i) {
+            Task task = this.tasks.get(i - 1);
+            if (task instanceof Event) {
+                Event event = (Event) task;
+                if (event.isOnDate(date)) {
+                    filtered.add(event);
+                }
+            } else if (task instanceof Deadline) {
+                Deadline deadline = (Deadline) task;
+                if (deadline.isOnDate(date)) {
+                    filtered.add(deadline);
+                }
+            }
+        }
+
+        return filtered;
+    }
+
+    public int getTaskNum(Task task) {
+        int taskIndex = this.tasks.indexOf(task);
+        return taskIndex == -1 ? -1 : taskIndex + 1;
     }
 }
