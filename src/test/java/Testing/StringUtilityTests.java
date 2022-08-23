ffb643c9973543b1583c302a0ae245df6f9a91fc
rui-han-crh <richardcrh2000@gmail.com>
2022-08-23 22:34:19 +0800

    Adds Gradle Support

--- src/test/java/Testing/StringUtilityTests.java
+++ src/test/java/Testing/StringUtilityTests.java
@@ -1,8 +1,8 @@
 package Testing;
 
 import Utilities.StringUtilities;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringUtilityTests {
 
