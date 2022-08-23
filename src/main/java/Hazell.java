9f29e2ab00f46b649248be7b5d9304aba23a3b7d
heyzec <hz.tiang@gmail.com>
2022-08-23 18:34:27 +0800

    refactor: create Ui class

--- src/main/java/Hazell.java
+++ src/main/java/Hazell.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Hazell {
+    private TaskList taskList;
+    private Ui ui;
     private static final String logo = "  _    _               _ _ \n"
             + " | |  | |             | | |\n"
             + " | |__| | __ _ _______| | |\n"
@@ -12,70 +14,64 @@ public class Hazell {
             + " | |  | | (_| |/ /  __/ | |\n"
             + " |_|  |_|\\__,_/___\\___|_|_|\n";
 
-    public static void reply(String msg) {
-        String DIVIDER = "\t____________________________________________________________";
-        System.out.println(DIVIDER);
-        for (String line : msg.split("\n")) {
-            System.out.println("\t" + line);
-        }
-        System.out.println(DIVIDER);
-    }
-
-    public static void main(String[] args) {
+    public Hazell() {
+        ui = new Ui();
         System.out.println(logo);
-
-        TaskList taskList;
         try {
             taskList = TaskList.createFromFile();
         } catch (IOException e) {
             taskList = new TaskList();
-            reply("Looks like this is the first time you started me up. "
+            ui.reply("Looks like this is the first time you started me up. "
                     + "I'll be saving your tasks to data/hazell.txt!");
         }
+    }
 
-        reply("Hello, I am Hazell!\nWhat can I do for you?");
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String userinput = scanner.nextLine().strip();
-            Command command = Command.parse(userinput);
+    public void run() {
+        ui.reply("Hello, I am Hazell!\nWhat can I do for you?");
+        while (ui.hasNextCommand()) {
+            Command command = ui.getNextCommand();
             try {
                 if (command.startsWith("bye")) {
-                    reply("Bye. Hope to see you again soon!");
+                    ui.reply("Bye. Hope to see you again soon!");
                     System.exit(0);
                 } else if (command.startsWith("list")) {
-                    reply(String.format("Here are the tasks in your list:\n%s", taskList));
+                    ui.reply(String.format("Here are the tasks in your list:\n%s", taskList));
                 } else if (command.startsWith("mark")) {
                     int index = Integer.parseInt(command.getTrailingArgs().get(0)) - 1;
                     String response = taskList.markTaskAsDone(index);
-                    reply(response);
+                    ui.reply(response);
                 } else if (command.startsWith("unmark")) {
                     int index = Integer.parseInt(command.getTrailingArgs().get(0)) - 1;
                     String response = taskList.markTaskAsUndone(index);
-                    reply(response);
+                    ui.reply(response);
                 } else if (command.startsWith("todo")) {
                     String description = String.join(" ", command.getTrailingArgs());
                     String response = taskList.addTask(ToDo.create(description));
-                    reply(response);
+                    ui.reply(response);
                 } else if (command.startsWith("deadline")) {
                     String description = String.join(" ", command.getTrailingArgs());
                     String time = command.getKwarg("by");
                     String response = taskList.addTask(Deadline.create(description, time));
-                    reply(response);
+                    ui.reply(response);
                 } else if (command.startsWith("event")) {
                     String description = String.join(" ", command.getTrailingArgs());
                     String time = command.getKwarg("at");
                     String response = taskList.addTask(Event.create(description, time));
-                    reply(response);
+                    ui.reply(response);
                 } else if (command.startsWith("delete")) {
                     int index = Integer.parseInt(command.getTrailingArgs().get(0)) - 1;
                     String response = taskList.deleteTask(index);
-                    reply(response);
+                    ui.reply(response);
                 } else {
                     throw new UnknownCommand();
                 }
             } catch (HazellException ex) {
-                reply(ex.toString());
+                ui.reply(ex.toString());
             }
         }
+    }
+
+    public static void main(String[] args) {
+        new Hazell().run();
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ui {
+    Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public boolean hasNextCommand() {
+        return this.scanner.hasNextLine();
+    }
+
+    public Command getNextCommand() {
+        return Command.parse(this.scanner.nextLine().strip());
+    }
+    public void reply(String msg) {
+        String DIVIDER = "\t____________________________________________________________";
+        System.out.println(DIVIDER);
+        for (String line : msg.split("\n")) {
+            System.out.println("\t" + line);
+        }
+        System.out.println(DIVIDER);
+    }
+}
