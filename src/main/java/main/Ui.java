0c3b68c640e2cd73b6c6153b10da224ca53c58ff
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-23 17:35:04 +0800

    Add Ui class

---
+++ src/main/java/main/Ui.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.util.Scanner;
+
+public class Ui {
+    /** sc takes in user input infinitely until Duke is closed */
+    private Scanner sc = new Scanner(System.in);
+
+    public void showLoadingError() {
+        System.out.println("Duke will overwrite the storage file for you.");
+    }
+
+    /**
+     * Prints out greeting when user just opened Duke
+     */
+    public void showWelcome() {
+        String logo = " ____        _        \n" +
+                "|  _ \\ _   _| | _____ \n" +
+                "| | | | | | | |/ / _ \\\n" +
+                "| |_| | |_| |   <  __/\n" +
+                "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+    }
+
+    public void showLine() {
+        System.out.println("----------------------------------");
+    }
+
+    public String readCommand() {
+        return sc.nextLine();
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+    };
+}
