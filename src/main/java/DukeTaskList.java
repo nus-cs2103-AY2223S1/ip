bba7ddbf32f1b526e04758ceea8bf2bc9cab0f78
gabyang <gabrielyang@berkeley.edu>
2022-08-23 08:59:45 +0800

    Added DukeTaskList file

--- src/main/java/DukeTaskList.java
+++ src/main/java/DukeTaskList.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class DukeTaskList {
-}
+//import java.util.ArrayList;
+//
+//public class DukeTaskList {
+//    ArrayList<DukeTask> lst;
+//
+//    DukeTaskList() {
+//        lst = new ArrayList<>();
+//    }
+//
+////    @Override
+////    public String toString() {
+////        return;
+////    }
+//
+//    public void mark (int taskNum) {
+//        try{
+////            int j = Integer.valueOf(input[1]);
+//            this.lst.get(taskNum).isDone = false;
+//            System.out.println("Nice! I've marked this task as done:");
+//        } catch (Exception e) {
+//            System.out.println("Something went wrong, here's the error message cuz im lazy to figure it out for you: " + e);
+//        }
+//    }
+//
+//    public void unmark (int taskNum) {
+//        try{
+//            this.lst.get(taskNum).isDone = false;
+//            System.out.println("Whoops, I guess the task is not done:");
+//        } catch (Exception e) {
+//            System.out.println("Something went wrong, here's the error message cuz im lazy to figure it out for you: " + e);
+//        }
+//    }
+//}
