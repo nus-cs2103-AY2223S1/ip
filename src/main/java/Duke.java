397deefacbcbf81f45975d6911998602352b7cbd
guokweijie <guokweijie@gmail.com>
2022-08-23 18:04:07 +0800

    Change the Duke bot methods to non-static and add functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,31 +8,60 @@ import java.io.File;
 import java.io.IOException;
 
 public class Duke {
-    static ArrayList<Task> taskArray = new ArrayList<>(100);
-    static int index = 0;
+    public ArrayList<Task> taskArray;
+    public int index;
+
+    public Duke(ArrayList<Task> array, int index) {
+        this.taskArray = array;
+        this.index = index;
+    }
 
     public String printList() {
         String list = "Here are the tasks in your list:\n";
-        int size = taskArray.size();
+        int size = this.taskArray.size();
         for (int i = 0; i < size; i++) {
-            if (taskArray.get(i) != null) {
-                list += ((i + 1) + "." + taskArray.get(i).toString() + '\n');
+            if (this.taskArray.get(i) != null) {
+                list += ((i + 1) + "." + this.taskArray.get(i).toString() + '\n');
             }
         }
         return list;
     }
 
+    public void incrementIndex() {
+        this.index += 1;
+    }
+
+    public void decrementIndex() {
+        this.index -= 1;
+    }
+
+    public void addTask(Task task) {
+        this.taskArray.add(this.index, task);
+    }
+
     public void markList(int i) throws IOException {
-        taskArray.get(i - 1).markAsDone();
-        System.out.println("Nice! I've marked this task as done:\n " + taskArray.get(i - 1).toString() + '\n');
+        this.taskArray.get(i - 1).markAsDone();
+        System.out.println("Nice! I've marked this task as done:\n " + this.taskArray.get(i - 1).toString() + '\n');
         writeToFile();
 
     }
 
     public void unMarkList(int i) throws IOException {
-        taskArray.get(i - 1).unMarkTask();
-        System.out.println("OK, I've marked this task as not done yet:\n " + taskArray.get(i - 1).toString() + '\n');
+        this.taskArray.get(i - 1).unMarkTask();
+        System.out.println("OK, I've marked this task as not done yet:\n " + this.taskArray.get(i - 1).toString() + '\n');
         writeToFile();
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public Task getTask(int index) {
+        return this.taskArray.get(index);
+    }
+
+    public void removeTask(int index) {
+        this.taskArray.remove(index);
     }
 
     public void writeToFile() throws IOException {
@@ -52,33 +81,34 @@ public class Duke {
                 int at = task.indexOf("(at:");
                 String timingWithBracket = task.substring(at + 5, task.lastIndexOf(")"));
                 String description = task.substring(9, at - 1) + " " + "/at" + timingWithBracket;
-                taskArray.add(index, new Event(" " + task.substring(9, at - 1), timingWithBracket));
+                this.taskArray.add(this.index, new Event(" " + task.substring(9, at - 1), timingWithBracket));
                 if (status == 'X') {
-                    taskArray.get(index).markAsDone();
+                    this.taskArray.get(this.index).markAsDone();
                 }
-                index += 1;
+                this.index += 1;
             } else if (type == 'D') {
                 int by = task.indexOf("(by:");
                 String timingWithBracket = task.substring(by + 5, task.lastIndexOf(")"));
                 String description = task.substring(9, by - 1) + " " + "/by" + timingWithBracket;
-                taskArray.add(index, new Deadline("" + task.substring(9, by - 1), timingWithBracket));
+                this.taskArray.add(this.index, new Deadline(" " + task.substring(9, by - 1),
+                        timingWithBracket));
                 if (status == 'X') {
-                    taskArray.get(index).markAsDone();
+                    this.taskArray.get(this.index).markAsDone();
                 }
-                index += 1;
+                this.index += 1;
             } else {
                 String description = task.substring(9);
-                taskArray.add(index, new Todo(" " + description));
+                this.taskArray.add(this.index, new Todo(" " + description));
                 if (status == 'X') {
-                    taskArray.get(index).markAsDone();
+                    this.taskArray.get(this.index).markAsDone();
                 }
-                index += 1;
+                this.index += 1;
             }
         }
     }
 
     public static void main(String[] args) {
-        Duke dukeBot = new Duke();
+        Duke dukeBot = new Duke(new ArrayList<>(100), 0);
         try {
             File taskList = new File("taskList.txt");
             if(!Files.exists(Path.of("taskList.txt"))) {
@@ -108,11 +138,12 @@ public class Duke {
                             throw new DukeException("☹ OOPS!!! " + "The description of a todo cannot be empty.\n");
                         } else {
                             Todo toDoTask = new Todo(toDo);
-                            taskArray.add(index, toDoTask);
-                            System.out.println("Got it. I've added this task:\n " + taskArray.get(index).toString() +
-                                    "\nNow you have " + (index + 1) + " tasks in the list.\n");
+                            dukeBot.addTask(toDoTask);
+                            System.out.println("Got it. I've added this task:\n " +
+                                    dukeBot.getTask(dukeBot.getIndex()).toString() + "\nNow you have "
+                                    + (dukeBot.getIndex() + 1) + " tasks in the list.\n");
                             dukeBot.writeToFile();
-                            index += 1;
+                            dukeBot.incrementIndex();
                         }
                     } else if (input.equals("deadline")) {
                         String deadlineTask = sc.nextLine();
@@ -124,11 +155,12 @@ public class Duke {
                             String description = deadlineTask.substring(0, integer - 1);
                             String by = deadlineTask.substring(integer + 4);
                             Deadline deadLineTask = new Deadline(description, by);
-                            taskArray.add(index, deadLineTask);
-                            System.out.println("Got it. I've added this task:\n " + taskArray.get(index).toString() +
-                                    "\nNow you have " + (index + 1) + " tasks in the list.\n");
+                            dukeBot.addTask(deadLineTask);
+                            System.out.println("Got it. I've added this task:\n " +
+                                    dukeBot.getTask(dukeBot.getIndex()).toString() +
+                                    "\nNow you have " + (dukeBot.getIndex() + 1) + " tasks in the list.\n");
                             dukeBot.writeToFile();
-                            index += 1;
+                            dukeBot.incrementIndex();
                         }
                     } else if (input.equals("event")) {
                         String event = sc.nextLine();
@@ -141,20 +173,21 @@ public class Duke {
                             String description = event.substring(0, integer - 1);
                             String at = event.substring(integer + 4);
                             Event eventTask = new Event(description, at);
-                            taskArray.add(index, eventTask);
-                            System.out.println("Got it. I've added this task:\n " + taskArray.get(index).toString() +
-                                    "\nNow you have " + (index + 1) + " tasks in the list.\n");
+                            dukeBot.addTask(eventTask);
+                            System.out.println("Got it. I've added this task:\n " +
+                                    dukeBot.getTask(dukeBot.getIndex()).toString() + "\nNow you have "
+                                    + (dukeBot.getIndex() + 1) + " tasks in the list.\n");
                             dukeBot.writeToFile();
-                            index += 1;
+                            dukeBot.incrementIndex();
                         }
                     } else if (input.equals("delete")) {
                         int deleteIndex = sc.nextInt();
-                        Task task = taskArray.get(deleteIndex - 1);
-                        taskArray.remove(deleteIndex - 1);
-                        index -= 1;
+                        Task task = dukeBot.getTask(deleteIndex - 1);
+                        dukeBot.removeTask(deleteIndex - 1);
+                        dukeBot.decrementIndex();
                         dukeBot.writeToFile();
                         System.out.println("Noted. I've removed this task:\n " + task.toString() + "\nNow you have "
-                                + index + " tasks in the list.\n");
+                                + dukeBot.getIndex() + " tasks in the list.\n");
                     } else {
                         input += sc.nextLine();
                         throw new DukeException("☹ OOPS!!! I'm sorry, " +
