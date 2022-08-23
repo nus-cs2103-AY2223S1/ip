85785fb3a0866fe4bb131fa951f7f026743637fe
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2022-08-23 09:54:51 +0800

    Level-8: Add Date and Time to Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate date;
+    protected String time;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String dateTime) {
         super(description);
-        this.by = by;
+        String[] details = dateTime.split(" ");
+        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("d/MM/yyyy");
+        this.date = LocalDate.parse(details[0], inputFormat);
+        this.time = details[1];
+
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        DateTimeFormatter displayFormat = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        return "[D]" + super.toString()
+                + " (by: " + time + " " + date.format(displayFormat) + ")";
     }
 }
