81179c184f908aa3824a662540f0a3e05b4ae838
Marcusgwj <Marcusgwj@hotmail.com>
2022-08-24 01:08:53 +0800

    Add Tasklist test cases

--- src/test/java/duke/TasklistTest.java
+++ src/test/java/duke/TasklistTest.java
@@ -1,10 +1,12 @@
 package duke;
 
+import duke.exception.DukeException;
 import duke.task.TaskList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import duke.task.ToDo;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class TasklistTest {
 
@@ -30,5 +32,37 @@ public class TasklistTest {
         taskList.addTask(new ToDo("Test 1"));
         taskList.addTask(new ToDo("Test 2"));
         assertEquals(2, taskList.getSize());
+    }
+
+    @Test
+    void markDone_indexOutOfBound_throwsException() {
+        try {
+            String path = "./src/test/data/duke.txt";
+            File file = new File(path);
+            if (file.exists()) {
+                file.delete();
+            }
+            TaskList taskList = new TaskList(path);
+            taskList.markDone(-1);
+            fail();
+        } catch (DukeException e) {
+            assertEquals("Invalid task number.", e.getMessage());
+        }
+    }
+
+    @Test
+    void unmarkDone_indexOutOfBound_throwsException() {
+        try {
+            String path = "./src/test/data/duke.txt";
+            File file = new File(path);
+            if (file.exists()) {
+                file.delete();
+            }
+            TaskList taskList = new TaskList(path);
+            taskList.unmarkDone(-1);
+            fail();
+        } catch (DukeException e) {
+            assertEquals("Invalid task number.", e.getMessage());
+        }
     }
 }
