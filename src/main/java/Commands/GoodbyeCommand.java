9b1d12fc8e8fcd45012afdd5eeeea76db02339d2
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-23 16:54:01 +0800

    Create methods for Goodbye representing exit of Duke

--- src/main/java/Commands/GoodbyeCommand.java
+++ src/main/java/Commands/GoodbyeCommand.java
@@ -1,4 +1,15 @@
 package Commands;
 
+import TaskList.TaskList;
+import common.Ui;
+
 public class GoodbyeCommand extends Command {
+    public GoodbyeCommand() {
+        super();
+    }
+
+    @Override
+    public void execute(TaskList taskList) {
+        this.exit = true;
+    }
 }
