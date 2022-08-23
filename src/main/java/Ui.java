cad933fd3afd9c9691cf6888099c1ec8a7e115c9
Derrick Khoo <derrickkhoo2000@gmail.com>
2022-08-24 02:28:40 +0800

    Add A-MoreOOP: Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ui {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void greet() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        line();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        line();
+    }
+
+    public void line() {
+        System.out.println("________________________________________");
+    }
+
+    public void printNumOfTasks(int count) {
+        System.out.println("Now you have " + count + " tasks in the list.");
+    }
+
+    public String parseCommand() {
+        return this.scanner.nextLine();
+    }
+}
