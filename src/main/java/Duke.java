6dd006b35a101ab3da465e7b2db69d96538799f1
Connor Lim <dev@connorlim.net>
2022-08-23 17:37:51 +0800

    Move all user interactions to Ui class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,81 +1,66 @@
 import java.util.List;
-import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Storage storage = new Storage("./tasks.txt");
-        List<Task> list = storage.readTasks();
+        List<Task> tasks = storage.readTasks();
+        Ui ui = new Ui();
 
-        System.out.println("Hello! I'm Piggy");
-        System.out.println("What can I oink for you?");
-        System.out.println("Use the following format for datetime: yyyy-MM-dd HH:mm");
-
-        String input;
+        ui.showWelcome();
         while (true) {
-            input = sc.nextLine();
-            if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.printf("%d.%s\n", i + 1, list.get(i));
-                }
-            } else if (input.matches("^mark \\d+$")) {
-                Task item = list.get(Integer.parseInt(input.substring(5)) - 1);
-                item.markDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + item);
-            } else if (input.matches("^unmark \\d+$")) {
-                Task item = list.get(Integer.parseInt(input.substring(7)) - 1);
-                item.markNotDone();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + item);
-            } else if (input.matches("^delete \\d+$")) {
-                Task item = list.remove(Integer.parseInt(input.substring(7)) - 1);
-                System.out.println("Noted. I've removed this task: ");
-                System.out.println("  " + item);
-                System.out.println("Now you have " + list.size() + " tasks in the list.");
-            } else if (input.equals("bye")) {
+            String command = ui.readCommand();
+            if (command.equals("list")) {
+                ui.showTaskList(tasks);
+            } else if (command.matches("^mark \\d+$")) {
+                Task task = tasks.get(Integer.parseInt(command.substring(5)) - 1);
+                task.markDone();
+                ui.showMarkAsDone(task);
+            } else if (command.matches("^unmark \\d+$")) {
+                Task task = tasks.get(Integer.parseInt(command.substring(7)) - 1);
+                task.markNotDone();
+                ui.showMarkAsNotDone(task);
+            } else if (command.matches("^delete \\d+$")) {
+                Task task = tasks.remove(Integer.parseInt(command.substring(7)) - 1);
+                ui.showTaskRemoved(task, tasks);
+            } else if (command.equals("bye")) {
                 break;
             } else {
                 Task task;
                 try {
-                    if (input.startsWith("todo")) {
-                        if (input.length() < 6) {
+                    if (command.startsWith("todo")) {
+                        if (command.length() < 6) {
                             throw new DukeException("The description of a todo cannot be empty.");
                         }
-                        task = new Todo(input.substring(5));
-                    } else if (input.startsWith("deadline")) {
-                        if (input.length() < 10) {
+                        task = new Todo(command.substring(5));
+                    } else if (command.startsWith("deadline")) {
+                        if (command.length() < 10) {
                             throw new DukeException(
                                     "The description of a deadline cannot be empty.");
-                        } else if (!input.contains("/by")) {
+                        } else if (!command.contains("/by")) {
                             throw new DukeException("A deadline must contain a /by");
                         }
-                        String[] split = input.substring(9).split(" /by ");
+                        String[] split = command.substring(9).split(" /by ");
                         task = new Deadline(split[0], split[1]);
-                    } else if (input.startsWith("event")) {
-                        if (input.length() < 7) {
+                    } else if (command.startsWith("event")) {
+                        if (command.length() < 7) {
                             throw new DukeException("The description of an event cannot be empty.");
-                        } else if (!input.contains("/at")) {
+                        } else if (!command.contains("/at")) {
                             throw new DukeException("An event must contain an /at");
                         }
-                        String[] split = input.substring(6).split(" /at ");
+                        String[] split = command.substring(6).split(" /at ");
                         task = new Event(split[0], split[1]);
                     } else {
                         throw new DukeException("I'm sorry, but I don't know what that means :-(");
                     }
-                    list.add(task);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + task);
-                    System.out.println("Now you have " + list.size() + " tasks in the list.");
+                    tasks.add(task);
+                    ui.showTaskAdded(task, tasks);
                 } catch (DukeException err) {
-                    System.out.println(err);
+                    ui.showDukeException(err);
                 }
             }
-            storage.writeTasks(list);
+            storage.writeTasks(tasks);
         }
 
-        System.out.println("Bye. Hope to oink you again soon!");
-        sc.close();
+        ui.showBye();
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,58 @@
+import java.util.List;
+import java.util.Scanner;
+
+class Ui {
+    private final Scanner sc;
+
+    Ui() {
+        sc = new Scanner(System.in);
+    }
+
+    void showWelcome() {
+        System.out.println("Hello! I'm Piggy");
+        System.out.println("What can I oink for you?");
+        System.out.println("Use the following format for datetime: yyyy-MM-dd HH:mm");
+    }
+
+    void showBye() {
+        System.out.println("Bye. Hope to oink you again soon!");
+        sc.close();
+    }
+
+    void showTaskList(List<Task> tasks) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("%d.%s\n", i + 1, tasks.get(i));
+        }
+    }
+
+    void showTaskAdded(Task task, List<Task> tasks) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+    }
+
+    void showTaskRemoved(Task task, List<Task> tasks) {
+        System.out.println("Noted. I've removed this task: ");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+    }
+
+    void showMarkAsDone(Task task) {
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + task);
+    }
+
+    void showMarkAsNotDone(Task task) {
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("  " + task);
+    }
+
+    void showDukeException(DukeException err) {
+        System.out.println(err);
+    }
+
+    String readCommand() {
+        return sc.nextLine();
+    }
+}
