1403bfb954d8c0c062cdb53bf543ad635fb66e0f
Zheng Long <ongzhenglong@gmail.com>
2022-08-23 19:52:24 +0800

    Fix style errors found

--- src/main/java/duke/exception/InvalidCommandException.java
+++ src/main/java/duke/exception/InvalidCommandException.java
@@ -1,7 +1,5 @@
 package duke.exception;
 
-import duke.exception.DukeException;
-
 public class InvalidCommandException extends DukeException {
     public InvalidCommandException(String description) {
         super(description);
--- src/main/java/duke/task/ToDo.java
+++ src/main/java/duke/task/ToDo.java
@@ -1,7 +1,5 @@
 package duke.task;
 
-import duke.task.Task;
-
 public class ToDo extends Task {
     public ToDo(String description) {
         super(description);
