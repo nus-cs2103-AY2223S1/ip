aa87bff3095ae1c9e9d7ab38ad2adaecf1b51461
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-08-23 15:11:11 +0800

    Add unit tests

---
+++ src/test/java/roger/ParserTest.java
@@ -0,0 +1,28 @@
+package roger;
+
+import org.junit.jupiter.api.Test;
+import roger.commands.AddEventCommand;
+import roger.exceptions.RogerInvalidInputException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ParserTest {
+    @Test
+    public void parse_deadlineNoDate_exceptionThrown() throws Exception {
+        Parser parser = new Parser();
+        assertThrows(RogerInvalidInputException.class, () -> parser.parse("deadline homework"));
+    }
+
+    @Test
+    public void parse_markNothing_exceptionThrown() throws Exception {
+        Parser parser = new Parser();
+        assertThrows(RogerInvalidInputException.class, () -> parser.parse("mark q"));
+    }
+
+    @Test
+    public void parse_event_returnAddEventCommand() throws Exception {
+        Parser parser = new Parser();
+        assertTrue(parser.parse("event happy /at 2022-10-04") instanceof AddEventCommand);
+    }
+}
---
+++ src/test/java/roger/StorageParserTest.java
@@ -0,0 +1,25 @@
+package roger;
+
+import org.junit.jupiter.api.Test;
+import roger.exceptions.RogerInvalidInputException;
+import roger.tasks.Event;
+import roger.tasks.ToDo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StorageParserTest {
+    @Test
+    public void toTask_validEventString_eventReturned() {
+        assertTrue(StorageParser.toTask("E | 1 | Party at Marina Bay Sands | 2022-10-22") instanceof Event);
+    }
+
+    @Test
+    public void toTask_validToDoString_toDoReturned() {
+        assertTrue(StorageParser.toTask("T | 1 | Party at Marina Bay Sands") instanceof ToDo);
+    }
+
+    @Test
+    public void toTask_invalidTaskString_ExceptionThrown() {
+        assertThrows(IllegalArgumentException.class, () -> StorageParser.toTask("foo bar"));
+    }
+}
