c35cc94597097ca3600620d94bde621bb0b2f0e0
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-23 18:11:34 +0800

    Write tests for Ui class
    Test for printSaving function

--- src/test/java/common/ParserTest.java
+++ src/test/java/common/ParserTest.java
@@ -2,7 +2,6 @@ package common;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
---
+++ src/test/java/common/UiTest.java
@@ -0,0 +1,37 @@
+package common;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UiTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    public void testPrintSaving() {
+        Ui.printSaving();
+        assertEquals("Saving...\n", outContent.toString());
+    }
+}
