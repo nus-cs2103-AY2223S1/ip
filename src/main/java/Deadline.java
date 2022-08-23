eac59ba7cac17af236462202afcc97bdec4a7081
vvidday <davids89034@gmail.com>
2022-08-23 16:28:53 +0800

    Add method to tasks that converts details to string to be stored in file

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -5,7 +5,19 @@ public class Deadline extends Task {
     super(description);
     this.dueDate = dueDate;
   }
-
+  
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String toFileFormat() {
+    int isDone = this.getStatusIcon() == "X" ? 1 : 0;
+    return String.format("D|%d|%s|%s", isDone, this.getDescription(), this.dueDate);
+  }
+  
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String toString() {
     return String.format("[D]%s (by: %s)", super.toString(), this.dueDate);
--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -3,6 +3,18 @@ public class ToDo extends Task {
     super(description);
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String toFileFormat() {
+    int isDone = this.getStatusIcon() == "X" ? 1 : 0;
+    return String.format("T|%d|%s", isDone, this.getDescription());
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String toString() {
     return String.format("[T]%s", super.toString());
