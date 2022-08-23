1572b73f7af957c60477c7e70dd1b075cdd15136
Marciano Renzo William <marcianorenzowilliam@gmail.com>
2022-08-23 23:44:19 +0800

    add junit test cases

---
+++ src/test/java/Duke/EventTest.java
@@ -0,0 +1,27 @@
+package Duke;
+
+import Duke.Task.Event;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTest {
+    public static DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    @Test
+    public void markTest() {
+        Event event = new Event("NUSSU EXCO Election", false,
+                LocalDateTime.parse("2022-08-24 00:00", timeFormat));
+        assertEquals(event.markDone().toString(), "[E][X] NUSSU EXCO Election (at: 24-Aug-2022 00:00)");
+    }
+
+    @Test
+    public void formatFileTest() {
+        Event event = new Event("NUS Jazz Band Practice", false,
+                LocalDateTime.parse("2022-09-01 20:30", timeFormat));
+        assertEquals(event.formatChange(), "E | 0 | NUS Jazz Band Practice | 2022-09-01 20:30");
+    }
+}
---
+++ src/test/java/Duke/TodoTest.java
@@ -0,0 +1,20 @@
+package Duke;
+
+import Duke.Task.Todo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void markTest() {
+        Todo todo = new Todo("Eat lunch", false);
+        assertEquals(todo.markDone().toString(), "[T][X] Eat lunch");
+    }
+
+    @Test
+    public void formatFileTest() {
+        Todo todo = new Todo("Read Pre-Lecture Notes", false);
+        assertEquals(todo.formatChange(), "T | 0 | Read Pre-Lecture Notes");
+    }
+}
