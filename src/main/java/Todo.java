49332b6b220b31b8edc47e6592cf64aed8459d8f
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2022-08-23 21:02:27 +0800

    feat: allow tasks to be created with isDone
    - allow tasks to be created with isDone specified
    - add getFileString() function to form string which will be stored locally

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -10,13 +10,34 @@ public class Todo extends Task {
     }
 
     /**
+     * Constructor to create new Todo with isDone
+     * 
+     * @param description Description of the todo you want to create
+     * @param isDone      Whether the task is done or not
+     */
+    public Todo(String description, boolean isDone) {
+        super(description, isDone);
+    }
+
+    /**
      * Factory method to create new Todo
      * 
      * @param input Task description
      * @return new Todo
      */
     public static Todo createTodo(String input) {
-        return new Todo(input);
+        if (input.indexOf("/completed ") == -1) {
+            return new Todo(input);
+        } else {
+            String description = input.split("/completed ")[0];
+            boolean isDone = Boolean.parseBoolean(input.split("/completed ")[1]);
+            return new Todo(description, isDone);
+        }
+    }
+
+    @Override
+    public String getFileString() {
+        return "todo " + this.description + " /completed " + this.isDone;
     }
 
     @Override
