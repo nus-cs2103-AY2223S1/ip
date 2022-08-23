6b734a1a2ad99b25668be8e232220c55c8e50186
vvidday <davids89034@gmail.com>
2022-08-23 17:10:01 +0800

    Add date support for deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,29 @@
-public class Deadline extends Task {
-  private final String dueDate;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-  public Deadline(String description, String dueDate) {
+public class Deadline extends Task {
+  private final String dueDateString;
+  private final LocalDate dueDate;
+
+  public Deadline(String description, String dueDateString) throws EkudException {
     super(description);
-    this.dueDate = dueDate;
+    this.dueDateString = dueDateString;
+    this.dueDate = this.getDateFromString(dueDateString);
+  }
+
+  private LocalDate getDateFromString (String dateString) throws EkudException {
+    try {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d");
+      LocalDate date = LocalDate.parse(dateString, formatter);
+      return date;
+    } catch (DateTimeParseException exception) {
+      throw new EkudException("Invalid date format. Please input date in format yyyy-mm-dd");
+    }
   }
 
   @Override
   public String toString() {
-    return String.format("[D]%s (by: %s)", super.toString(), this.dueDate);
+    return String.format("[D]%s (by: %s)", super.toString(), this.dueDate.format(DateTimeFormatter.ofPattern("MMMM d, yyyy")));
   }
 }
