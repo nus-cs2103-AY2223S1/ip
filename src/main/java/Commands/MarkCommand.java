e2f59fe408c0dd91c5f23b685df4696fd1e141da
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-23 16:55:14 +0800

    Create methods for MarkCommand representing marking a list item

--- src/main/java/Commands/MarkCommand.java
+++ src/main/java/Commands/MarkCommand.java
@@ -1,4 +1,35 @@
 package Commands;
 
+import TaskList.TaskList;
+import common.Ui;
+import dukeExceptions.DukeException;
+import dukeExceptions.IllegalIndexException;
+
 public class MarkCommand extends Command {
+    private String[] args;
+
+    public MarkCommand(String[] args) {
+        this.args = args;
+    }
+
+    public static void validateArguments(String[] args) throws DukeException {
+        if (args.length < 1) {
+            throw new DukeException("Missing index!");
+        }
+        try {
+            int index = Integer.parseInt(args[0])-1;
+            if (index < 0) {
+                throw new IllegalIndexException();
+            }
+        } catch (NumberFormatException e) {
+            throw new DukeException(e.toString());
+        }
+    }
+
+    @Override
+    public void execute(TaskList taskList) {
+        int index = Integer.parseInt(args[0])-1;
+        taskList.markTask(index);
+        Ui.printMarkTask(taskList.get(index));
+    }
 }
