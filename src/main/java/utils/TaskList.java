a921ca66c6beed93fe196ce359fa78e43a1e7c76
nseah21 <nseah21@gmail.com>
2022-08-23 21:06:34 +0800

    Modify code to follow coding standard

--- src/main/java/utils/TaskList.java
+++ src/main/java/utils/TaskList.java
@@ -97,7 +97,6 @@ public class TaskList {
         return this.tasks.get(index).toString();
     }
 
-
     public String formatAllTasksForFileStorage() {
         String r = "";
         for (Task t : this.tasks) {
