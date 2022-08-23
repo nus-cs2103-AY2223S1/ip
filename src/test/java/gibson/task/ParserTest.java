7644d80843c9c7c5bdbbac4ccbb9250e36c5d155
KeithPJX <e0684532@u.nus.edu>
2022-08-23 08:24:54 -0700

    Add JUnit tests for Parser.java and TaskList.java

---
+++ src/test/java/gibson/task/ParserTest.java
@@ -0,0 +1,29 @@
+package gibson.task;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ParserTest {
+    @Test
+    public void getTrailingInt_inputWithTrailingInt_success() {
+        assertEquals(12345, Parser.getTrailingInt("Test12345"));
+        assertEquals(45, Parser.getTrailingInt("Test123Test45"));
+    }
+
+    @Test
+    public void getTrailingInt_inputWithoutTrailingInt_exceptionThrown() {
+        try {
+            Parser.getTrailingInt("TEST");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("String is invalid", e.getMessage());
+        }
+        try {
+            Parser.getTrailingInt("TEST123T");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("String is invalid", e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/gibson/task/TaskListTest.java
@@ -0,0 +1,25 @@
+package gibson.task;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void TaskList_emptyList_success() {
+        TaskList tl = new TaskList(new ArrayList<String>());
+        assertEquals("", tl.toString());
+    }
+
+    @Test
+    public void TaskList_populatedList_success() {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("1.[T][ ] Test");
+        al.add("2.[D][ ] Test2 (by: 1 Aug 2022 23:59)");
+        al.add("3.[E][ ] Test3 (at: 1 August)");
+        TaskList tl = new TaskList(al);
+        assertEquals("1.[T][ ] Test\n" +
+                "2.[D][ ] Test2 (by: 1 Aug 2022 23:59)\n" +
+                "3.[E][ ] Test3 (at: 1 August)", tl.toString());
+    }
+}
