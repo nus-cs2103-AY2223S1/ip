e57b47aef1611065d5b4325b76dd490fab3310b2
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-23 16:54:25 +0800

    Create methods for Goodbye representing list command

--- src/main/java/Commands/ListCommand.java
+++ src/main/java/Commands/ListCommand.java
@@ -1,4 +1,14 @@
 package Commands;
 
+import TaskList.TaskList;
+import common.Ui;
+
 public class ListCommand extends Command {
+    public ListCommand() {
+    }
+
+    @Override
+    public void execute(TaskList taskList) {
+        Ui.printTaskList(taskList);
+    }
 }
