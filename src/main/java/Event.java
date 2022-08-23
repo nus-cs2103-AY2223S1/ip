9cf31077b0594b9ed6e7a11f7678657db09cc4ac
MinHeinA <minheinaung97@gmail.com>
2022-08-23 16:22:07 +0800

    Complete Level 8. Dates and Times
    For Deadline and Event tasks, date data provided is now saved as LocalDateTime data. Duke now accepts date time input in dd-MM-yyyy HHmm and output the date data in MMM dd yyyy format.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,22 @@
-public class Event extends Task {
-    private String time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Event(String taskName, String time) {
+public class Event extends Task {
+    private LocalDateTime time;
+
+    public Event(String taskName, String time) throws DukeException {
         super(taskName);
-        this.time = time;
+        try {
+            this.time = LocalDateTime.parse(time, DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm"));
+        } catch (DateTimeParseException dateTimeParseException){
+            throw new DukeException("☹ OOPS!!! Cannot parse date. Enter date according to example, 02-12-2019 1800");
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.time + ")";
+        return "[E]" + super.toString() + " (at: "
+                + this.time.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
