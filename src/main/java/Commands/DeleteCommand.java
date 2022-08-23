b8cef25f1a5f86daa148e85c08c5acbd203c927c
Ferusel <53888954+Ferusel@users.noreply.github.com>
2022-08-23 16:51:26 +0800

    Create methods for DeleteCommand representing deletion of a task

--- src/main/java/Commands/DeleteCommand.java
+++ src/main/java/Commands/DeleteCommand.java
@@ -1,4 +1,38 @@
 package Commands;
 
+import TaskList.TaskList;
+import Tasks.Task;
+import common.Ui;
+import dukeExceptions.DukeException;
+import dukeExceptions.IllegalIndexException;
+
 public class DeleteCommand extends Command {
+    private String[] args;
+
+    public DeleteCommand(String[] args) {
+        this.args = args;
+    }
+
+    public static void validateArguments(String[] args) throws DukeException {
+        if (args.length < 1) {
+            throw new DukeException("Missing index!");
+        }
+        int index = Integer.parseInt(args[0])-1;
+        if (index < 0) {
+            throw new IllegalIndexException();
+        }
+        try {
+            Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            throw new DukeException(e.toString());
+        }
+    }
+
+    @Override
+    public void execute(TaskList taskList) {
+        int index = Integer.parseInt(args[0]) - 1;
+        Task toDelete = taskList.get(index);
+        taskList.deleteTask(index);
+        Ui.printDeleteTask(toDelete, taskList);
+    }
 }
