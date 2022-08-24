e4b86c0141a86f917e4e8d6a7d0ea58119c93e70
ish1506 <ishiuanlee@gmail.com>
2022-08-24 17:46:18 +0800

    Update test input and expected output

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -5,6 +5,7 @@ if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
+if exist data\duke.txt del data\duke.txt
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
