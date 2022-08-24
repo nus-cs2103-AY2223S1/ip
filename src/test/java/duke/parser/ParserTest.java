d3ded5a66b83519988a944cdf77d57628cbcdc9f
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2022-08-25 00:02:55 +0800

    Fix test cases

--- src/test/java/duke/parser/ParserTest.java
+++ src/test/java/duke/parser/ParserTest.java
@@ -30,12 +30,12 @@ public class ParserTest {
         try {
             Parser.parse("deadline");
         } catch (DukeException e) {
-            assertEquals(e.toString(), "OOPS!!! The description of a DEADLINE cannot be empty.");
+            assertEquals(e.toString(), "OOPS!!! The description of a Deadline cannot be empty.");
         }
         try {
             Parser.parse("deadline return book /by 2 jan 2020 1800");
         } catch (DukeException e) {
-            assertEquals(e.toString(), "OOPS!!! The description of the DATE is invalid!\n" +
+            assertEquals(e.toString(), "OOPS!!! The description of the Date is invalid!\n" +
                     "Please input the date in d/MM/yyyy format e.g. 2/12/2019 1800.");
         }
     }
