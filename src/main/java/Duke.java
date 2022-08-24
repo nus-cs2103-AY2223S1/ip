6164900b8e996de61c7415574f2ae68709d4397b
dylen chew <56499861+rgonslayer@users.noreply.github.com>
2022-08-24 23:34:36 +0800

    Add Marking
    Tasks cannot be marked as done or not done yet.
    
    Add functionality for marking and unmarking

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,37 +4,51 @@ public class Duke {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
 
-        String nextInstruction;
+        String nextTask;
         String introduction = "Hello! I'm Jarvis \n"
                             + "What can I do for you?";
         String farewell = "Bye. Hope to see you again soon!";
-        int curr = 0;
 
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
 
         System.out.println(introduction);
 
         while (true) {
-            nextInstruction = inputScanner.nextLine();
+            nextTask = inputScanner.nextLine();
 
-            if (nextInstruction.equals("bye")) {
+            if (nextTask.equals("bye")) {
                 break;
             }
 
-            if (nextInstruction.equals("list")) {
+            if (nextTask.equals("list")) {
                 for (int i = 0; i < taskList.length; i ++) {
                     if (taskList[i] == null) {
                         break;
                     }
-                    System.out.println((i + 1) + ". " + taskList[i]);
+                    System.out.println((i + 1) + ". " + taskList[i].toString());
                 }
                 continue;
             }
 
-            taskList[curr] = nextInstruction;
-            curr++;
+            if (nextTask.length() > 4 && nextTask.substring(0,4).equals("mark")) {
+                int toMark = Integer.parseInt(nextTask.substring(5)) - 1;
+                taskList[toMark].mark();
+                String markResponse = "Nice! I've marked this task as done:\n ";
+                System.out.println(markResponse + taskList[toMark].toString());
+                continue;
+            }
 
-            System.out.println("added: " + nextInstruction);
+            if (nextTask.length() > 6 && nextTask.substring(0,6).equals("unmark")) {
+                int toMark = Integer.parseInt(nextTask.substring(7)) - 1;
+                taskList[toMark].unmark();
+                String markResponse = "Ok, I've marked this task as not done yet:\n ";
+                System.out.println(markResponse + taskList[toMark].toString());
+                continue;
+            }
+
+            taskList[Task.count] = new Task(nextTask);
+
+            System.out.println("added: " + nextTask);
 
         }
 
