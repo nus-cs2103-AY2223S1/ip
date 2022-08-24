a2f5ba8772e0f652f88403a68fb44cb388797713
bigcrushes <teocharlton@gmail.com>
2022-08-25 03:08:57 +0800

    Tweek code to comply with coding standard

--- src/main/java/duke/EmptyDescriptionException.java
+++ src/main/java/duke/EmptyDescriptionException.java
@@ -1,5 +1,8 @@
 package duke;
 
+/**
+ * Error to be thrown if command is given without the necessary arguments
+ */
 public class EmptyDescriptionException extends RuntimeException {
     public EmptyDescriptionException(String message) {
         super("☹ OOPS!!! The description of a " + message + " cannot be empty.");
--- src/main/java/duke/tasks/Event.java
+++ src/main/java/duke/tasks/Event.java
@@ -1,5 +1,8 @@
 package duke.tasks;
 
+/**
+ * Represents events in the task list, storing the description of the event and the time of event as string
+ */
 public class Event extends Task {
 
     protected String time;
@@ -9,16 +12,26 @@ public class Event extends Task {
         this.time = time;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getLetterTag() {
         return "E";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getAdditionalInfo() {
         return this.time;
     }
 
+    /**
+     * Returns properly formatted string to be displayed to user
+     * @return String formatted for user to read
+     */
     @Override
     public String toString() {
         return "[" + this.getLetterTag() + "][" + this.getStatusIcon() + "] "
