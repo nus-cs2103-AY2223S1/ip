1c52f4058400c7def02e58cd555d2b7488384902
vantemoon <sylviassunrx0820@outlook.com>
2022-08-25 00:03:35 +0800

    Add the ability to store deadline dates as a java.time.LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(by);
+        } catch (DateTimeParseException exception) {
+            throw new DukeException("Please enter the date in the format of yyyy-mm-dd");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
