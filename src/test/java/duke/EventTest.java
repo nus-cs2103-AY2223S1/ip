fa61a2dc283944d9494448c7b02375340ed39fed
leecaregene <e0764279@u.nus.edu>
2022-08-25 04:57:37 +0800

    Add newline to end of files

--- src/test/java/duke/EventTest.java
+++ src/test/java/duke/EventTest.java
@@ -8,6 +8,7 @@ class EventTest {
 
     @Test
     void testToString() {
-        assertEquals("[E][ ] project meeting (at: Aug 6th 2-4pm)", new Event("project meeting", "Aug 6th 2-4pm").toString());
+        assertEquals("[E][ ] project meeting (at: Aug 6th 2-4pm)",
+                new Event("project meeting", "Aug 6th 2-4pm").toString());
     }
 }
