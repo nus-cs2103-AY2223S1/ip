d130f2c43cb771a9e9d567039861d9bd9c880195
chun <lamchunyu00@gmail.com>
2022-08-24 15:03:05 +0800

    Added save and load functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,6 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.function.UnaryOperator;
 
 public class Duke {
 
@@ -10,6 +9,7 @@ public class Duke {
     }
 
     private static void setup() {
+//        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         System.out.println("Hello from " + "DUKE");
         System.out.println("What can I do for you?");
         System.out.println(divider());
@@ -58,6 +58,10 @@ public class Duke {
                     this.isDone = false;
                 }
 
+                public String getSavedString() {
+                    return String.format("T | %s | %s\n",this.isDone, this.description);
+                }
+
                 @Override
                 public String toString() {
                     return String.format("[T] %s %s", checkbox(this.getIsDone()), this.getDescription());
@@ -70,6 +74,11 @@ public class Duke {
                 public Deadline(String description, String deadline) {
                     super(description);
                     this.deadline = deadline;
+                }
+
+                @Override
+                public String getSavedString() {
+                    return String.format("D | %s | %s | %s\n",this.getIsDone(), this.getDescription(), this.deadline);
                 }
 
                 @Override
@@ -87,6 +96,11 @@ public class Duke {
                 }
 
                 @Override
+                public String getSavedString() {
+                    return String.format("E | %s | %s | %s\n",this.getIsDone(), this.getDescription(), this.time);
+                }
+
+                @Override
                 public String toString() {
                     return String.format("[E] %s %s (at: %s)", checkbox(this.getIsDone()), this.getDescription(), this.time);
                 }
@@ -99,22 +113,77 @@ public class Duke {
                 this.list = new ArrayList<>(length);
             }
 
-            public void addTask(String description) {
-                this.list.add(new Task(description));
+            public Task addTask(String description) {
+                Task val = new Task(description);
+                this.list.add(val);
 //                this.list[pointer] = new Task(description);
                 pointer++;
+                return val;
             }
 
-            public void addDeadline(String desc, String deadline) {
-                this.list.add(new Deadline(desc, deadline));
+            public Deadline addDeadline(String desc, String deadline) {
+                Deadline val = new Deadline(desc, deadline);
+                this.list.add(val);
 //                this.list[pointer] = new Deadline(desc, deadline);
                 pointer++;
+                return val;
             }
 
-            public void addEvent(String desc, String time) {
-                this.list.add(new Event(desc, time));
+            public Event addEvent(String desc, String time) {
+                Event val = new Event(desc, time);
+                this.list.add(val);
 //                this.list[pointer] = new Event(desc, time);
                 pointer++;
+                return val;
+            }
+
+            public void load() {
+                try {
+                    File file = new File("./data/duke.txt");
+                    file.createNewFile();
+
+                    BufferedReader reader = new BufferedReader(new FileReader("./data/duke.txt"));
+                    String line = reader.readLine();
+                    while (line != null) {
+//                        System.out.println(line);
+
+                        String[] raw = line.split(" \\| ");
+//                        System.out.println(String.format("%s, %s, %s", raw[0], raw[1], raw[2]));
+                        Task curr = null;
+                        switch(raw[0]) {
+                            case "T":
+                                curr = this.addTask(raw[2]);
+                                break;
+                            case "D":
+                                curr = this.addDeadline(raw[2], raw[3]);
+                                break;
+                            case "E":
+                                curr = this.addEvent(raw[2], raw[3]);
+                                break;
+                        }
+                        if (raw[1].equals("true") && curr != null) {
+                            curr.mark();
+                        }
+                        line = reader.readLine();
+                    }
+
+                    printData();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            public void save() {
+                try {
+                    File file = new File("./data/duke.txt");
+                    file.createNewFile();
+                    FileWriter writer = new FileWriter("./data/duke.txt", false);
+//                    System.out.println(this.saveData());
+                    writer.write(this.saveData());
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             public int getLength() {
@@ -137,12 +206,22 @@ public class Duke {
                 this.get(index).unmark();
             }
 
+            private String saveData() {
+                String result = "";
+                for (Task task : this.list) {
+                    result += String.format("%s", task.getSavedString());
+                }
+                return result;
+            }
+
             private void printData() {
                 this.list.forEach(x -> System.out.println(String.format("%s. %s", this.list.indexOf(x) + 1, x)));
             }
         }
+
         Scanner scanner = new Scanner(System.in);
         Todo todolist = new Todo(100);
+        todolist.load();
         setup();
 
         while (true) {
@@ -160,6 +239,10 @@ public class Duke {
 
                     case "list":
                         todolist.printData();
+                        break;
+
+                    case "save":
+                        todolist.save();
                         break;
 
                     default:
