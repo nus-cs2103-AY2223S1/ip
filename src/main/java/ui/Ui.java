506d9cf3574f9ebffbf63e14eaea60ee4edaa2fd
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-08-24 18:20:23 +0800

    Add print method; Change method headers

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -3,19 +3,29 @@ package ui;
 public class Ui {
 
   /**
-   * Outputs a line break to the ui to signify the end of a message.
+   * Prints a line break to the ui to signify the end of a message.
    */
   private static void endMessage() {
     System.out.println("-".repeat(100));
   }
 
   /**
-   * Outputs a message to the ui.
+   * Prints a message to the ui. Also prints a new line.
+   *
+   * @param message The message to be printed.
+   */
+  public static void println(String message) {
+    System.out.println(message);
+    endMessage();
+  }
+
+  /**
+   * Prints a message to the ui. Does not print a new line.
    *
    * @param message The message to be printed.
    */
   public static void print(String message) {
-    System.out.println(message);
+    System.out.print(message);
     endMessage();
   }
 }
