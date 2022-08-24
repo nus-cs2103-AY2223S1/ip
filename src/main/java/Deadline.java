3a7a4d117c704ce71212edc3cf693916481f2348
Aloynz <aloysiusneozeen@gmail.com>
2022-08-24 19:40:59 +0800

    Add Dates and Times
    Use Date Objects instead of storing it as strings.
    
    This is to standarise the dates input by the users.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,23 @@
+import java.time.LocalDateTime;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        int year = Integer.parseInt(by.substring(0,4));
+        int month = Integer.parseInt(by.substring(5,7));
+        int day = Integer.parseInt(by.substring(8,10));
+        int hours = Integer.parseInt(by.substring(11,13));
+        int minutes = Integer.parseInt(by.substring(13, 15));
+
+        LocalDateTime date = LocalDateTime.of(year, month, day, hours, minutes);
+        this.by = date;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + by + ")";
+        return "[D]" + super.toString() + "(by: " + by.getMonth() +" " + by.getDayOfMonth()  + " " + by.getYear() 
+            + " " + String.format("%02d", by.getHour()) + " " + String.format("%02d", by.getMinute()) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,25 @@
+import java.time.LocalDateTime;
+
 public class Event extends Task{
-    protected String at;
+    protected LocalDateTime at;
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+
+        int year = Integer.parseInt(at.substring(0,4));
+        int month = Integer.parseInt(at.substring(5,7));
+        int day = Integer.parseInt(at.substring(8,10));
+        int hours = Integer.parseInt(at.substring(11,13));
+        int minutes = Integer.parseInt(at.substring(13, 15));
+
+        LocalDateTime date = LocalDateTime.of(year, month, day, hours, minutes);
+        this.at = date;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + at + ")";
+        return "[E]" + super.toString() + "(at: " + at.getMonth() +" " + at.getDayOfMonth()  + " " + at.getYear() 
+            + " " + String.format("%02d", at.getHour()) + " " + String.format("%02d", at.getMinute()) + ")";
     }
 
 }
