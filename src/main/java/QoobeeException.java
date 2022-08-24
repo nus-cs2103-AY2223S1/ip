6b036d522e14ac0479b1a6a92c066ebb4a0a6ec0
CedricChia123 <cedricchia01@gmail.com>
2022-08-25 00:15:41 +0800

    Add saving to hard disk
    (cherry picked from commit e4ea9d20bb31128c5fe2ab78014b2c8fd78591a0)

---
+++ src/main/java/QoobeeException.java
@@ -0,0 +1,11 @@
+public class QoobeeException extends Exception {
+
+    public QoobeeException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "OOPS!!! " + super.getMessage();
+    }
+}
