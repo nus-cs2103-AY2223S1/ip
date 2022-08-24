8204f868672a61e556e6e2fda13ab685c0224958
kavantan <kavantan10@gmail.com>
2022-08-24 22:49:39 +0800

    Add LocalDate encapsulation into Deadline class

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,14 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadlines extends Task {
   private String dateTime;
+  private LocalDate formattedDateTime;
+  private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd uuuu");
 
-  public Deadlines(String description, String dateTime) {
+  public Deadlines(String description, String dateTime, LocalDate formattedDateTime) {
     super(description);
-    this.dateTime = dateTime;
+    this.dateTime= dateTime;
+    this.formattedDateTime = formattedDateTime;
   }
 
   @Override
   public String toString() {
-    return "[D]" + super.toString() + " (by: " + this.dateTime + ")";
+    return "[D]" + super.toString() + " (by: " + this.formattedDateTime.format(formatter) + ")";
   }
 
 }
