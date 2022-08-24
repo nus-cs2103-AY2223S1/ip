0acfdd1ec31deb481d2146e3fe9c7a0e08fa8169
ShawnChew <shawnchew200@gmail.com>
2022-08-25 02:55:34 +0800

    Edit Deadline and Event Class to deal with dates

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
     protected String by;
+    protected LocalDate date;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        try {
+            this.date = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            this.date = null;
+        }
+
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return date == null
+                ? "[D]" + super.toString() + " (by: " + by + ")"
+                : "[D]" + super.toString() + " (by: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
 
     protected String at;
+    protected LocalDate date;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        try {
+            this.date = LocalDate.parse(at);
+        } catch (DateTimeParseException e) {
+            this.date = null;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return date == null
+                ? "[E]" + super.toString() + " (at: " + at + ")"
+                : "[E]" + super.toString() + " (at: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
