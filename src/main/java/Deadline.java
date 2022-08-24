ae7b392ca442b035561ed5b4c199b56bc32f3966
joosxi <64764352+joosxi@users.noreply.github.com>
2022-08-24 14:50:09 +0800

    added code for Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,37 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     protected String by = "";
+    protected LocalDate byDate;
+    protected LocalTime byTime;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        dateTimeConverter(by);
+    }
+    private void dateTimeConverter(String by) {
+        if (by.length() > 10) {
+            int spacePos = by.indexOf(" ");
+            String date = by.substring(0, spacePos);
+            String time = by.substring(spacePos + 1);
+            this.byDate = LocalDate.parse(date);
+            this.byTime = LocalTime.parse(time);
+        } else {
+            this.byDate = LocalDate.parse(by);
+        }
     }
 
     @Override
     public String toString() {
-        return ("[D]" + super.toString() + " (by: " + this.by + ")");
+        return ("[D]"
+                + super.toString()
+                + " (by: "
+                + this.byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + " "
+                + this.byTime.toString()
+                + ")");
     }
 }
