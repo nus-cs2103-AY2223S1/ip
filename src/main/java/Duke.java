8d259a97cc7bbd9a3b9fde11165a2f807ceaff7c
dylen chew <56499861+rgonslayer@users.noreply.github.com>
2022-08-24 23:04:29 +0800

    Add Add and List Functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,15 +8,34 @@ public class Duke {
         String introduction = "Hello! I'm Jarvis \n"
                             + "What can I do for you?";
         String farewell = "Bye. Hope to see you again soon!";
+        int curr = 0;
+
+        String[] taskList = new String[100];
 
         System.out.println(introduction);
 
         while (true) {
             nextInstruction = inputScanner.nextLine();
+
             if (nextInstruction.equals("bye")) {
                 break;
             }
-            System.out.println(nextInstruction);
+
+            if (nextInstruction.equals("list")) {
+                for (int i = 0; i < taskList.length; i ++) {
+                    if (taskList[i] == null) {
+                        break;
+                    }
+                    System.out.println((i + 1) + ". " + taskList[i]);
+                }
+                continue;
+            }
+
+            taskList[curr] = nextInstruction;
+            curr++;
+
+            System.out.println("added: " + nextInstruction);
+
         }
 
         System.out.println(farewell);
