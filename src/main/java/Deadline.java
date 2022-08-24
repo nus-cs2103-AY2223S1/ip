ae744431c3f82b906ca959c29e0f01b4f87e1866
jjtan444 <jjtan444@gmail.com>
2022-08-24 10:32:01 +0800

    To level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime date; //'2/12/2019 1800'
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.date = dateTimeParser(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm:ss");
+        return "[D]" + super.toString() + " (by: " + date.format(formatter) + ")";
+    }
+
+    public static LocalDateTime dateTimeParser(String time) {
+        String[] timing = time.split(" ", 2);
+        String[] dayMonYr = timing[0].split("/", 3);
+        int hr = Integer.valueOf(timing[1].substring(0,2));
+        int minute = Integer.valueOf(timing[1].substring(2));
+        return LocalDateTime.of(Integer.valueOf(dayMonYr[2]),
+                                Integer.valueOf(dayMonYr[1]),
+                                Integer.valueOf(dayMonYr[0]),
+                                hr, minute);
     }
 }
