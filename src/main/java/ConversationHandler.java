7c728b0675e15ba9b1cadb7e468f60cdc8676950
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-08-24 19:43:58 +0800

    added a command parser for all commands

--- src/main/java/ConversationHandler.java
+++ src/main/java/ConversationHandler.java
@@ -1,10 +1,12 @@
 import task_classes.Deadline;
 import task_classes.Event;
 import task_classes.Todo;
+import utils.InputParser;
 import utils.IOUtils;
 import task_classes.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ConversationHandler {
@@ -27,40 +29,41 @@ public class ConversationHandler {
 
     public String commandHandler(String input) {
 //        We get the first word, since that determines the command
-        switch (input.split(" ", 2)[0]) {
+        HashMap<String, String> command = InputParser.getInputArguments(input);
+        switch (command.get("keyword")) {
             case "Bye":
             case "bye":
-                return this.closeCommand(input);
+                return this.closeCommand(command);
 
             case "list":
             case "List":
-                return this.listCommand(input);
+                return this.listCommand(command);
 
             case "mark":
             case "Mark":
-                return this.markCommand(input);
+                return this.markCommand(command);
 
             case "unmark":
             case "Unmark":
-                return this.unMarkCommand(input);
+                return this.unMarkCommand(command);
 
             case "todo":
             case "Todo":
-                return this.addTodoCommand(input);
+                return this.addTodoCommand(command);
 
             case "deadline":
             case "Deadline":
-                return this.addDeadlineCommand(input);
+                return this.addDeadlineCommand(command);
 
             case "event":
             case "Event":
-                return this.addEventCommand(input);
+                return this.addEventCommand(command);
 
             case "delete":
             case "Delete":
             case "remove":
             case "Remove":
-                return this.deleteCommand(input);
+                return this.deleteCommand(command);
 
 
             default:
@@ -68,34 +71,41 @@ public class ConversationHandler {
         }
     }
 
-    private String deleteCommand(String input) {
-        int index = Integer.parseInt(input.split(" ")[1]);
-        Task task = this.list.get(index - 1);
-        this.list.remove(index - 1);
+    private String deleteCommand(HashMap<String, String> commandObj) {
+        if (!commandObj.containsKey("args") || !InputParser.IsInteger(commandObj.get("args"))) {
+            throw new IllegalArgumentException("Delete command must contain a numeric argument!");
+        }
+
+        int index = Integer.parseInt(commandObj.get("args")) - 1;
+        Task task = this.list.get(index);
+        this.list.remove(index);
         return "Noted. I've removed this task: \n" +
                 task.toString() + "\n" +
                 "Now you have " + this.list.size() + " tasks in the list.";
     }
 
-    private String addEventCommand(String input) {
-//        This is the name + /at + date
-//        TODO: Error handling (no /at or date)
-        String args = input.split(" ", 2)[1];
-        String name = input.split("/at", 2)[0].strip();
-        String by = input.split("/at", 2)[1].strip();
-        Event e = new Event(name, by);
+    private String addEventCommand(HashMap<String, String> commandObj) {
+        if (!commandObj.containsKey("args") || !commandObj.containsKey("/at")) {
+            throw new IllegalArgumentException("Event must have a name and a date!");
+        }
+
+        String name = commandObj.get("args");
+        String at = commandObj.get("/at");
+        Event e = new Event(name, at);
         list.add(e);
+
         return "Got it. I've added this task: \n" +
                 e.toString() + "\n" +
                 "Now you have " + list.size() + " tasks in the list.";
     }
 
-    private String addDeadlineCommand(String input) {
-//        This is the name + /by + date
-//        TODO: Error handling (no /by or date)
-        String args = input.split(" ", 2)[1];
-        String name = input.split("/by", 2)[0].strip();
-        String by = input.split("/by", 2)[1].strip();
+    private String addDeadlineCommand(HashMap<String, String> commandObj) {
+        if (!commandObj.containsKey("args") || !commandObj.containsKey("args")) {
+            throw new IllegalArgumentException("Deadline must have a name and a deadline!");
+        }
+
+        String name = commandObj.get("args");
+        String by = commandObj.get("/by");
         Deadline d = new Deadline(name, by);
         list.add(d);
         return "Got it. I've added this task: \n" +
@@ -104,17 +114,26 @@ public class ConversationHandler {
 
     }
 
-    private String addTodoCommand(String input) {
-        Todo t = new Todo(input.split(" ", 2)[1]);
+    private String addTodoCommand(HashMap<String, String> commandObj) {
+        if (!commandObj.containsKey("args")) {
+            throw new IllegalArgumentException("TODO must have a name!");
+        }
+
+        String name = commandObj.get("args");
+
+        Todo t = new Todo(name);
         list.add(t);
         return "Got it. I've added this task: \n" +
                 t.toString() + "\n" +
                 "Now you have " + list.size() + " tasks in the list.";
     }
 
-    private String markCommand(String input) {
-//        TODO: error handling here
-        int index = Integer.parseInt(input.split(" ")[1]);
+    private String markCommand(HashMap<String, String> commandObj) {
+        if (!commandObj.containsKey("args") || !InputParser.IsInteger(commandObj.get("args"))) {
+            throw new IllegalArgumentException("Mark needs an index");
+        }
+
+        int index = Integer.parseInt(commandObj.get("args"));
 
 //        List is 1-indexed in cli
         Task task = this.list.get(index - 1);
@@ -123,9 +142,12 @@ public class ConversationHandler {
         return "Nice! I've marked this as done: \n" + task;
     }
 
-    private String unMarkCommand(String input) {
-//        TODO: error handling here
-        int index = Integer.parseInt(input.split(" ")[1]);
+    private String unMarkCommand(HashMap<String, String> commandObj) {
+        if (!commandObj.containsKey("args") || !InputParser.IsInteger(commandObj.get("args"))) {
+            throw new IllegalArgumentException("Unmark needs an index");
+        }
+
+        int index = Integer.parseInt(commandObj.get("args"));
 
 //        List is 1-indexed in cli
         Task task = this.list.get(index - 1);
@@ -134,7 +156,7 @@ public class ConversationHandler {
         return "OK, I've marked this task as not done yet: \n" + task;
     }
 
-    private String listCommand(String input) {
+    private String listCommand(HashMap<String, String> commandObj) {
         String returnMsg = "";
         int index = 1;
 
@@ -145,7 +167,7 @@ public class ConversationHandler {
         return returnMsg;
     }
 
-    private String closeCommand(String input) {
+    private String closeCommand(HashMap<String, String> commandObj) {
         this.in.close();
         this.active = false;
         return "Bye. Hope to see you again!";
---
+++ src/main/java/utils/InputParser.java
@@ -0,0 +1,56 @@
+package utils;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputParser {
+    private static Pattern intPattern = Pattern.compile("\\d+");
+
+    public static boolean IsInteger(String s) {
+        return intPattern.matcher(s).matches();
+    }
+
+    private static Pattern flagPattern = Pattern.compile("/\\w+");
+    /**
+     * Returns a hashmap of arguments and commands from the given input.
+     * @param command Command string given
+     * @return HashMap of arguments,
+     * The given command keyword is under the key "keyword"
+     * Unnamed arguments are under the key "args"
+     * Keys under any /flags are under the key "/flag"
+     */
+    public static HashMap<String, String> getInputArguments(String command) {
+        HashMap<String, String> result = new HashMap<>();
+
+        result.put("keyword", command.split(" ", 2)[0]);
+        if (command.split(" ").length < 2) {
+            return result;
+        }
+
+        command = command.split(" ", 2)[1];
+        result.put("args", command.split("/", 2)[0].strip());
+        if (command.split("/").length < 2) {
+            return result;
+        }
+
+        command = "/" + command.split("/", 2)[1];
+        Matcher flagMatches = flagPattern.matcher(command);
+
+        while (flagMatches.find()) {
+            String match = flagMatches.group();
+            command = command.split(match, 2)[1];
+            result.put(match, command.split("/", 2)[0].strip());
+        }
+        return result;
+    }
+
+    public static void main (String[] args) {
+//        System.out.println(getInputArguments("hello"));
+//        System.out.println(getInputArguments("hello args"));
+//        System.out.println(getInputArguments("hello args args"));
+//        System.out.println(getInputArguments("hello args /at 2pm"));
+//        System.out.println(getInputArguments("hello args /at 2pm /by 1 2 3 4 5 /hello greetings asdf sd"));
+        System.out.println(getInputArguments("deadline do things /by tmr"));
+    }
+}
