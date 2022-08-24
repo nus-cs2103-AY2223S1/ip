dd73f2e709303a6b7c9cce8863117430d9230d94
LimAiLin <limailin2009@gmail.com>
2022-08-24 13:34:39 +0800

    A-Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,16 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
+
+    private final ArrayList<Task> ls = new ArrayList<>(100);
+    private static final String line = "Dino:\n";
 
     public enum Tasks {
         BYE,
@@ -16,6 +23,176 @@ public class Duke {
         DELETE
     }
 
+    protected void list() {
+        for (int i = 0; i < Task.lsSize(); i++) {
+            System.out.println("\t" + (i + 1) + ". " + ls.get(i).toString());
+        }
+    }
+
+    protected void mark(String[] str) throws DukeException {
+        int index;
+        Task myTask;
+        try {
+            index = Integer.parseInt(str[1]) - 1;
+        } catch (Exception e) {
+            throw new DukeException("Oops! You did not choose which task to mark!");
+        }
+
+        if (index > Task.lsSize() || index < 0) {
+            throw new DukeException("Oops! Invalid task!");
+        }
+        myTask = ls.get(index);
+        myTask.markAsDone();
+        System.out.println("\tHooray! You have completed this task:\n\t" + myTask);
+    }
+
+    protected void unmark(String[] str) throws DukeException {
+        int index;
+        Task myTask;
+        try {
+            index = Integer.parseInt(str[1]) - 1;
+        } catch (Exception e) {
+            throw new DukeException("Oops! You did not choose which task to mark!");
+        }
+
+        if (index > Task.lsSize() || index < 0) {
+            throw new DukeException("Oops! Invalid task!");
+        }
+        myTask = ls.get(index);
+        myTask.markAsUndone();
+        System.out.println("\tOh no! You have more things to complete:\n\t" + myTask);
+    }
+
+    protected void deadline(String[] str) throws DukeException {
+        String[] dl;
+        Task myTask;
+        try {
+            dl = str[1].split(" /by ");
+        } catch (Exception e) {
+            throw new DukeException("Oops! The description of a deadline cannot be empty!");
+        }
+
+        try {
+            myTask = new Deadline(dl[0], dl[1]);
+        } catch (Exception e) {
+            throw new DukeException("Oops! When is the deadline?");
+        }
+
+        ls.add(myTask);
+        System.out.println("\tadded: " + myTask);
+        System.out.println("\tYou have " + Task.lsSize() + " task" + (Task.lsSize() > 1 ? "s!" : "!"));
+    }
+
+    protected void todo(String[] str) throws DukeException {
+        Task myTask;
+        try {
+            myTask = new ToDo(str[1]);
+        } catch (Exception e) {
+            throw new DukeException("Oops! The description of a todo cannot be empty!");
+        }
+
+        ls.add(myTask);
+        System.out.println("\tadded: " + myTask);
+        System.out.println("\tYou have " + Task.lsSize() + " task" + (Task.lsSize() > 1 ? "s!" : "!"));
+    }
+
+    protected void event(String[] str) throws DukeException {
+        String[] evnt;
+        Task myTask;
+        try {
+            evnt = str[1].split(" /at ");
+        } catch (Exception e) {
+            throw new DukeException("Oops! The description of an event cannot be empty!");
+        }
+
+        try {
+            myTask = new Event(evnt[0], evnt[1]);
+        } catch (Exception e) {
+            throw new DukeException("Oops! When is the event?");
+        }
+
+        ls.add(myTask);
+        System.out.println("\tadded: " + myTask);
+        System.out.println("\tYou have " + Task.lsSize() + " task" + (Task.lsSize() > 1 ? "s!" : "!"));
+    }
+
+    protected void delete(String[] str) throws DukeException {
+        int index;
+        try {
+            index = Integer.parseInt(str[1]) - 1;
+        } catch (Exception e) {
+            throw new DukeException("Oops! You did not choose which task to delete!");
+        }
+
+        if (index > Task.lsSize() || index < 0) {
+            throw new DukeException("Oops! Invalid task!");
+        }
+
+        ls.get(index).remove();
+        ls.remove(index);
+    }
+
+    protected void createFile() {
+        try {
+            File myObj = new File("tasks.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    protected void taskLoop() throws DukeException {
+        Scanner sc = new Scanner(System.in);
+        String curr = "";
+        createFile();
+
+        while (!Objects.equals(curr, "bye")) {
+            curr = sc.nextLine();
+            String[] str = curr.split("\\s", 2);
+            System.out.print(line);
+            Tasks myTask;
+
+            try {
+                myTask = Tasks.valueOf(str[0].toUpperCase(Locale.ROOT));
+            } catch (Exception e) {
+                throw new DukeException("Oops! I don't know what that means.");
+            }
+
+            switch (myTask) {
+                case BYE:
+                    System.out.println("\tBye bye. Hope to see you again soon!");
+                    break;
+                case LIST:
+                    this.list();
+                    break;
+                case MARK:
+                    this.mark(str);
+                    break;
+                case UNMARK:
+                    this.unmark(str);
+                    break;
+                case DEADLINE:
+                    this.deadline(str);
+                    break;
+                case TODO:
+                    this.todo(str);
+                    break;
+                case EVENT:
+                    this.event(str);
+                    break;
+                case DELETE:
+                    this.delete(str);
+                    break;
+            }
+            System.out.print("\n");
+        }
+    }
+
     public static void main(String[] args) throws DukeException {
         System.out.println("               __\n" +
                 "              / _)\n" +
@@ -24,124 +201,11 @@ public class Duke {
                 " __/ (  | (  |\n" +
                 "/__.-'|_|--|_|\n");
 
-        String line = "Dino:\n";
-        ArrayList<Task> ls = new ArrayList<>(100);
-
         System.out.println(line
                 + "\tHello! I'm Dino\n"
                 + "\tWhat can I do for you?\n");
 
-        Scanner sc = new Scanner(System.in);
-        String curr = "";
-        int index;
-        Task myTask;
+        new Duke().taskLoop();
 
-        while (!Objects.equals(curr, "bye")) {
-            curr = sc.nextLine();
-            String[] str = curr.split("\\s", 2);
-            System.out.print(line);
-            switch(Tasks.valueOf(str[0].toUpperCase(Locale.ROOT))) {
-                case BYE:
-                    System.out.println("\tBye bye. Hope to see you again soon!");
-                    break;
-                case LIST:
-                    for (int i = 0; i < Task.lsSize(); i++) {
-                        System.out.println("\t" + (i + 1) + ". " + ls.get(i).toString());
-                    }
-                    break;
-                case MARK:
-                    try {
-                        index = Integer.parseInt(str[1]) - 1;
-                    } catch (Exception e) {
-                        throw new DukeException("Oops! You did not choose which task to mark!");
-                    }
-
-                    if (index > Task.lsSize() || index < 0) {
-                        throw new DukeException("Oops! Invalid task!");
-                    }
-                    myTask = ls.get(index);
-                    myTask.markAsDone();
-                    System.out.println("\tHooray! You have completed this task:\n\t" + myTask);
-                    break;
-                case UNMARK:
-                    try {
-                        index = Integer.parseInt(str[1]) - 1;
-                    } catch (Exception e) {
-                        throw new DukeException("Oops! You did not choose which task to mark!");
-                    }
-
-                    if (index > Task.lsSize() || index < 0) {
-                        throw new DukeException("Oops! Invalid task!");
-                    }
-                    myTask = ls.get(index);
-                    myTask.markAsUndone();
-                    System.out.println("\tOh no! You have more things to complete:\n\t" + myTask);
-                    break;
-                case DEADLINE:
-                    String[] dl;
-                    try {
-                        dl = str[1].split(" /by ");
-                    } catch (Exception e) {
-                        throw new DukeException("Oops! The description of a deadline cannot be empty!");
-                    }
-
-                    try {
-                        myTask = new Deadline(dl[0], dl[1]);
-                    } catch (Exception e) {
-                        throw new DukeException("Oops! When is the deadline?");
-                    }
-
-                    ls.add(myTask);
-                    System.out.println("\tadded: " + myTask);
-                    System.out.println("\tYou have " + Task.lsSize() + " task" + (Task.lsSize() > 1 ? "s!" : "!"));
-                    break;
-                case TODO:
-                    try {
-                        myTask = new ToDo(str[1]);
-                    } catch (Exception e) {
-                        throw new DukeException("Oops! The description of a todo cannot be empty!");
-                    }
-
-                    ls.add(myTask);
-                    System.out.println("\tadded: " + myTask);
-                    System.out.println("\tYou have " + Task.lsSize() + " task" + (Task.lsSize() > 1 ? "s!" : "!"));
-                    break;
-                case EVENT:
-                    String[] evnt;
-                    try {
-                        evnt = str[1].split(" /at ");
-                    } catch (Exception e) {
-                        throw new DukeException("Oops! The description of an event cannot be empty!");
-                    }
-
-                    try {
-                        myTask = new Event(evnt[0], evnt[1]);
-                    } catch (Exception e) {
-                        throw new DukeException("Oops! When is the event?");
-                    }
-
-                    ls.add(myTask);
-                    System.out.println("\tadded: " + myTask);
-                    System.out.println("\tYou have " + Task.lsSize() + " task" + (Task.lsSize() > 1 ? "s!" : "!"));
-                    break;
-                case DELETE:
-                    try {
-                        index = Integer.parseInt(str[1]) - 1;
-                    } catch (Exception e) {
-                        throw new DukeException("Oops! You did not choose which task to delete!");
-                    }
-
-                    if (index > Task.lsSize() || index < 0) {
-                        throw new DukeException("Oops! Invalid task!");
-                    }
-
-                    ls.get(index).remove();
-                    ls.remove(index);
-                    break;
-                default:
-                    throw new DukeException("Oops! I don't know what that means.");
-            }
-            System.out.print("\n");
-        }
     }
 }
