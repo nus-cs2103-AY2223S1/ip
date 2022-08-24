df2fe7219634fded8671b42394856b973d04e1d0
dylen chew <56499861+rgonslayer@users.noreply.github.com>
2022-08-24 22:42:41 +0800

    Update Duke.java
    Add echo for commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner inputScanner = new Scanner(System.in);
+
+        String nextInstruction;
+        String introduction = "Hello! I'm Jarvis \n"
+                            + "What can I do for you?";
+        String farewell = "Bye. Hope to see you again soon!";
+
+        System.out.println(introduction);
+
+        while (true) {
+            nextInstruction = inputScanner.nextLine();
+            if (nextInstruction.equals("bye")) {
+                break;
+            }
+            System.out.println(nextInstruction);
+        }
+
+        System.out.println(farewell);
+
+
     }
 }
