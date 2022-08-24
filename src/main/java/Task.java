7e58bde285fbf3917d97e8107cbdbdfa6e2a7475
Vshnv2001 <mvaishnav.sg@gmail.com>
2022-08-24 12:04:26 +0800

    Add Date Time Parsing Functionality

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,14 @@
-// import java.util.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class Task {
     protected String description;
     protected boolean isDone;
     protected char taskType;
     protected String dateTime;
+    protected LocalDateTime date;
+
     protected String dueDescription; // Stores either 'by' or 'at'
 
     public Task(String description) throws Exception{
@@ -23,7 +27,8 @@ public class Task {
                 throw new Exception("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         } catch (Exception e) {
-            throw new Exception("☹ OOPS!!! The description of a todo cannot be empty.");
+            // throw new Exception("☹ OOPS!!! The description of a todo cannot be empty.");
+            System.out.println(e.getMessage());
         }
 
     }
@@ -31,8 +36,23 @@ public class Task {
     private void parseDateTime(String delimiter){
         String[] splitText = this.description.split(delimiter);
         this.dateTime = splitText[1];
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        this.date = LocalDateTime.parse(this.dateTime.trim(), formatter);
+        dateTimeStringFormatter();
         this.description = splitText[0];
         this.dueDescription = delimiter.substring(1); // Extracts the 'by' or 'at'
+    }
+
+    private void dateTimeStringFormatter(){
+        String month = this.date.getMonth().toString();
+        String day = String.valueOf(this.date.getDayOfMonth());
+        String year = String.valueOf(this.date.getYear());
+        int hour = this.date.getHour() > 12 ? this.date.getHour() - 12 : this.date.getHour();
+        String minute = this.date.getMinute() > 9 ? String.valueOf(this.date.getMinute()) : "0" + String.valueOf(this.date.getMinute());
+        String halfOfDay = this.date.getHour() > 11 ? "PM" : "AM";
+        String time = String.valueOf(hour) + ":" + minute + " " + halfOfDay;
+
+        this.dateTime = String.join(" ", Arrays.asList(day, month, year, time));
     }
 
     private String getStatusIcon() {
