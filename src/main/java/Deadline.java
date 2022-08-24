86d7fe8caa42562aefd5375114e167b0ce9e369b
thamruicong <e0726692@u.nus.edu>
2022-08-24 16:40:07 +0800

    Level-8 done

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     /**
      * The deadline of Deadline
      */
-    private String deadline;
+    private LocalDate deadline;
 
     /**
      * Constructor for Deadline
@@ -10,13 +13,13 @@ public class Deadline extends Task {
      * @param isDone The isDone status of the Task
      * @param deadline The deadline of Deadline
      */
-    public Deadline(String title, boolean isDone, String deadline) {
+    public Deadline(String title, boolean isDone, LocalDate deadline) {
         super(title, isDone);
         this.deadline = deadline;
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.deadline);
+        return String.format("[D]%s (by: %s)", super.toString(), this.deadline.format(DateTimeFormatter.ofPattern("dd MMM yyyy")));
     }
 }
