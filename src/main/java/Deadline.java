8a342460fe3d4874c4d1252a663571df780e98ef
jovitaanderson <jovita@live.com.sg>
2022-08-24 17:46:38 +0800

    Add date feature to deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
 
     protected String by;
+    LocalDate date;
+    String formattedDate;
+
+    public Deadline(String description, LocalDate date) {
+        super(description);
+        this.date = date;
+        this.formattedDate = date.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+    }
 
     public Deadline(String description, String by) {
         super(description);
@@ -9,11 +21,13 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + by + ")";
+        return "[D]" + super.toString() + " (by:" + formattedDate + ")";
     }
 
+
+    //when saved and loaded again it will return a null date
     @Override
     public String toStringForStorage() {
-        return "D|" + super.toStringForStorage() + "|" + by;
+        return "D|" + super.toStringForStorage() + "|" + date.toString();
     }
 }
