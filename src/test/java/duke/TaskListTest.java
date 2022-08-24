e0a851c3467ace79e33d8b8f15f9157a1de8918b
weesiau <e0725311@u.nus.edu>
2022-08-24 22:48:22 +0800

    Implement A-JUnit increment
    Implemented unit testing with gradle on Parser, TaskList and Task classes

---
+++ src/test/java/duke/TaskListTest.java
@@ -0,0 +1,50 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TaskListTest {
+    @Test
+    public void taskList_getSize_correctSize() {
+        TaskList testTaskList = new TaskList(
+                new Parser(),
+                new Storage(),
+                new Ui()
+        );
+        Task testTask1 = new Task("Dummy Test 1");
+        Todo testTask2 = new Todo("todo Dummy2");
+        Todo testTask3 = new Todo("todo Dummy3");
+        testTaskList.addTask(testTask1);
+        testTaskList.addTask(testTask2);
+        testTaskList.addTask(testTask3);
+        assertEquals(testTaskList.getSize(), 3);
+    }
+
+    @Test
+    public void taskList_deleteTask_isDeleted() {
+        TaskList testTaskList = new TaskList(
+                new Parser(),
+                new Storage(),
+                new Ui()
+        );
+        Task testTask1 = new Task("Dummy Test 1");
+        Todo testTask2 = new Todo("todo Dummy2");
+        Todo testTask3 = new Todo("todo Dummy3");
+        testTaskList.addTask(testTask1);
+        testTaskList.addTask(testTask2);
+        testTaskList.addTask(testTask3);
+        int prevSizeOfTaskList = testTaskList.getSize();
+        try {
+            testTaskList.deleteTask(1);
+            int currSizeOfTaskList = testTaskList.getSize();
+            assertEquals(currSizeOfTaskList, prevSizeOfTaskList - 1);
+        } catch (TaskNotFoundException e) {
+            fail("Unexpected exception thrown");
+        }
+    }
+}
