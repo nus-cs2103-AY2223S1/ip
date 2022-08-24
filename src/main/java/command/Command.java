dd507050320de6df238aaa8bcee093d7fb3b0ef2
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2022-08-24 15:58:49 +0800

    change commands package name to command

--- src/main/java/command/Command.java
+++ src/main/java/command/Command.java
@@ -1,4 +1,4 @@
-package commands;
+package command;
 
 import tasks.Task;
 import java.util.ArrayList;
--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -1,4 +1,4 @@
-package commands;
+package command;
 
 import java.util.ArrayList;
 import tasks.Task;
