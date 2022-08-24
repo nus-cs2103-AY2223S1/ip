dda552f01f9a8e87cf305fc95fa97d29d964fd2c
RachelChua <e0772465@u.nus.edu>
2022-08-25 01:21:19 +0800

    Update mark and unmark operation to update file saved in hard disk
    When mark and unmark functions are called, the file in hard disk
    will also be updated accordingly such that the next time we load the
    tasks from the file, the mark or unmark symbol will be correct

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,25 +1,58 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Task {
     protected String name;
     protected boolean isDone;
-    Task(String name){
+
+    Task(String name, boolean isDone){
         this.name = name;
-        this.isDone = false;
+        this.isDone = isDone;
     }
+
     public String getStatusIcon() {
        return(this.isDone? "[X] " : "[] " );
     }
 
-    public void mark() {
+    public void mark() throws IOException {
+        Storage s = new Storage(Duke.getPathname, Duke.getDirectory);
+        String[] oldLineOldContentPair = s.modifyFileHelper(this.toString());
+        String oldLine = oldLineOldContentPair[0];
+        String oldContent = oldLineOldContentPair[1];
+        File file = new File(Duke.getPathname);
+        FileWriter writer = new FileWriter(file);
+        String newContent = oldContent.replace(oldLine, oldLine.substring(0,3) +
+                "[X] " + oldLine.substring(6));
+        writer.write(newContent);
+        writer.close();
         this.isDone = true;
+        Ui ui = new Ui();
+        ui.markHelperUi(this);
     }
-    public void unmark() {
+
+    public void unmark() throws IOException {
+        Storage s = new Storage(Duke.getPathname, Duke.getDirectory);
+        String[] oldLineOldContentPair = s.modifyFileHelper(this.toString());
+        String oldLine = oldLineOldContentPair[0];
+        String oldContent = oldLineOldContentPair[1];
+        File file = new File(Duke.getPathname);
+        FileWriter writer = new FileWriter(file);
+        String newContent = oldContent.replace(oldLine,
+                oldLine.substring(0,3) + "[] " + oldLine.substring(7));
+        writer.write(newContent);
+        writer.close();
         this.isDone = false;
+        Ui ui = new Ui();
+        ui.unmarkHelperUi(this);
     }
+
     public String getStatus() {
         return this.getStatusIcon() + "" + this.name;
-    }
-    public String getName() {
-        return this.name;
     }
 
     }
