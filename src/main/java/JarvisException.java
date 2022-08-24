6ba56bad243a93e29068a399977d3faf2cbb9f3b
dylen chew <56499861+rgonslayer@users.noreply.github.com>
2022-08-25 02:02:26 +0800

    Add error handling
    Add error handling for two specific cases

--- src/main/java/JarvisException.java
+++ src/main/java/JarvisException.java
@@ -1,2 +1,10 @@
-public class JarvisException {
+public class JarvisException extends Exception{
+    public JarvisException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    @Override
+    public String toString() {
+        return "OOPS!!! " + super.getMessage();
+    }
 }
