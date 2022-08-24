6103ac3ff1948de0f092183a332c0f6750f5910f
phyliciachristel <e0775641@u.nus.edu>
2022-08-24 18:25:14 +0800

    - implement basic ui class functions - add parser class and tasklist class - add storage class with draft save()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,9 +9,88 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
     static List<Task> todoList = new ArrayList<>();
     private static String keySeparator = "//";
     private static String fileName = "data/tasks.txt";
+    private static Ui ui;
+
+    public static class Ui {
+
+        public final String ADDED = "oke i added this:";
+        public final String DELETED = "oke i deleted this:";
+        public final String MARKED = "oke this is done now:";
+        public final String UNMARKED = "oke this is undone now:";
+
+        public Ui () {
+
+        }
+
+        public void showWelcome() {
+            System.out.println("hi im chompers what can i do for u today!");
+        }
+
+        public void showExit() {
+            System.out.println("bye see u");
+        }
+
+        public void displayTask(String message, Task task) {
+            System.out.println(message);
+            System.out.println(task);
+        }
+
+        public void printTasks() {
+            System.out.println("here! ur tasks:");
+            for(int i = 1; i <= todoList.size(); i++) {
+                Task task = todoList.get(i-1);
+                System.out.println(i + "." + task.toString());
+            }
+        }
+        public void showError(String message) {
+            System.out.println("error! " + message);
+        }
+
+        public void readCommand() {
+
+        }
+    }
+
+    public static class Storage {
+        private String filePath;
+
+        public Storage(String filePath) {
+            this.filePath = filePath;
+        }
+
+        public void save() {
+            try {
+                FileWriter fw = new FileWriter(fileName, false);
+                for(int i = 0; i < todoList.size(); i++) {
+                    fw.write(todoList.get(i).formatToSave() + "\n");
+                }
+
+                fw.close();
+            } catch (IOException e) {
+                ui.showError("file not found");
+                //System.out.println();
+                return;
+            }
+        }
+
+        public void load() {
+
+        }
+    }
+
+    public static class Parser {
+
+    }
+
+    public static class TaskList {
+        private List<Task> taskList;
+
+        
+    }
 
     /**
      * class for each task
@@ -144,22 +223,25 @@ public class Duke {
         try {
             loadTasks();
         } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
+            ui.showError("File not found");
+            //System.out.println("File not found!");
             return;
         }
-        System.out.println("hi im chompers what do u need!!!\n");
+        ui = new Ui();
+        ui.showWelcome();
+        //System.out.println("hi im chompers what do u need!!!\n");
 
         while(true) {
             Scanner scanIn = new Scanner(System.in);
             reply = scanIn.nextLine(); // read from input
 
             if(reply.equals(exit)) {
-                System.out.println("bye see u"); // exits the program
+                ui.showExit();
+                //System.out.println("bye see u"); // exits the program
                 scanIn.close();
                 break;
             } else if (reply.equals("list")) {
-                System.out.println("here! ur tasks:");
-                printList(); // prints the list of tasks
+                ui.printTasks();
             } else {
                 String[] substr = reply.split(" ", 2); // to identify the keyword used
                 Integer index;
@@ -167,66 +249,68 @@ public class Duke {
                 switch (substr[0]) {
                     case "mark":
                         if(substr.length == 1) { // no number was given
-                            System.out.println("enter an index!");
+                            ui.showError("enter an index!");
+                            //System.out.println();
                             break;
                         }
                         try {
                             index = Integer.parseInt(substr[1]) - 1;
                             if(index < 0 || index >= todoList.size()) { // to check if index is out of range
-                                System.out.println("thrs nth there :<");
+                                ui.showError("thrs nth there :<");
+                                //System.out.println("thrs nth there :<");
                                 continue;
                             }
                             temp = todoList.get(index);
                             temp.markAsDone();
-                            System.out.println("oke this is done now:\n" + temp);
+                            ui.displayTask(ui.MARKED, temp);
                             saveTasks();
                         } catch (NumberFormatException e) {
-                            System.out.println("Invalid input"); // if index given cannot be converted or was the wrong format
+                            ui.showError("Invalid input"); // if index given cannot be converted or was the wrong format
                         }
                         break;
                     case "unmark":
                         if(substr.length == 1) {
-                            System.out.println("enter an index!");
+                            ui.showError("enter an index!");
                             break;
                         }
                         try {
                             index = Integer.parseInt(substr[1]) - 1;
                             if(index < 0 || index >= todoList.size()) { // check if index is out of range
-                                System.out.println("thrs nth there :<");
+                                ui.showError("thrs nth there :<");
                                 continue;
                             }
                             temp = todoList.get(index);
                             temp.markAsUndone();
-                            System.out.println("oke this is undone now:\n" + temp);
+                            System.out.println(ui.UNMARKED + temp);
                             saveTasks();
                         } catch (NumberFormatException e) {
-                            System.out.println("Invalid input");
+                            ui.showError("Invalid input");
                         }
 
                         break;
                     case "delete":
                         if(substr.length == 1) {
-                            System.out.println("enter an index!");
+                            ui.showError("enter an index!");
                             break;
                         }
                         try {
                             index = Integer.parseInt(substr[1]) - 1;
                             if(index < 0 || index >= todoList.size()) {
-                                System.out.println("thrs nth there :<");
+                                ui.showError("thrs nth there :<");
                                 continue;
                             }
                             temp = todoList.get(index);
                             todoList.remove(temp);
                             saveTasks();
-                            System.out.println("oke this is deleted now:\n" + temp);
+                            ui.displayTask(ui.DELETED, temp);
                             System.out.println("now u have " + todoList.size() + " task(s)!");
                         } catch (NumberFormatException e) {
-                            System.out.println("Invalid input");
+                            ui.showError("Invalid input");
                         }
                         break;
                     case "todo":
                         if(substr.length == 1) {
-                            System.out.println("The description cannot be empty!");
+                            ui.showError("The description cannot be empty!");
                             break;
                         }
                         temp = new Todo(substr[1]);
@@ -234,12 +318,12 @@ public class Duke {
                         break;
                     case "deadline":
                         if(substr.length == 1) {
-                            System.out.println("The description cannot be empty!");
+                            ui.showError("The description cannot be empty!");
                             break;
                         }
                         String[] dlDesc = substr[1].split(" /by ", 2);
                         if(dlDesc.length < 2) {
-                            System.out.println("The deadline cannot be empty");
+                            ui.showError("The deadline cannot be empty");
                             break;
                         }
                         try {
@@ -248,20 +332,19 @@ public class Duke {
                             addTask(temp);
                             break;
                         } catch (DateTimeParseException e) {
-                            System.out.println("Please re-enter the task with the following deadline format: ");
-                            System.out.println("yyyy-mm-dd");
+                            ui.showError("Please re-enter the task with the following deadline format: \nyyyy-mm-dd");
                             break;
                         }
 
 
                     case "event":
                         if(substr.length == 1) {
-                            System.out.println("The description cannot be empty!");
+                            ui.showError("The description cannot be empty!");
                             break;
                         }
                         String[] eventDesc = substr[1].split(" /at ", 2);
                         if(eventDesc.length < 2) {
-                            System.out.println("The date cannot be empty");
+                            ui.showError("The date cannot be empty");
                             break;
                         }
 
@@ -269,7 +352,7 @@ public class Duke {
                         addTask(temp);
                         break;
                     default:
-                        System.out.println("idk what that means :(");
+                        ui.showError("idk what that means :(");
                         break;
                 }
 
@@ -284,7 +367,7 @@ public class Duke {
      */
     public static void addTask(Task task) {
         todoList.add(task);
-        System.out.println("oke i added:\n" + task.toString());
+        ui.displayTask(ui.ADDED, task);
         System.out.println("now u have " + todoList.size() + " task(s)!");
         saveTasks();
     }
@@ -334,10 +417,10 @@ public class Duke {
     /**
      * Prints out the current list
      */
-    public static void printList() {
-        for(int i = 1; i <= todoList.size(); i++) {
-            Task task = todoList.get(i-1);
-            System.out.println(i + "." + task.toString());
-        }
-    }
+//    public static void printList() {
+//        for(int i = 1; i <= todoList.size(); i++) {
+//            Task task = todoList.get(i-1);
+//            System.out.println(i + "." + task.toString());
+//        }
+//    }
 }
