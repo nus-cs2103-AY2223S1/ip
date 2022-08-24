122daff8d4bf4cfae5c51722c75c3fb9e1a8061b
guanzhou03 <guanzhou03@gmail.com>
2022-08-24 12:15:30 +0800

    Add search function
    Added a search functionality which can filter tasks by a keyword

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -23,6 +23,7 @@ public class Parser {
         acceptedKeywords.add("event");
         acceptedKeywords.add("todo");
         acceptedKeywords.add("delete");
+        acceptedKeywords.add("find");
         String[] parts = input.split(" ", 2);
         String keyword = parts[0];
         if (input.equals("bye") || input.equals("list")) return;
@@ -41,57 +42,65 @@ public class Parser {
             String keyword = parts[0];
             processInput(input);
             switch (keyword) {
-                case "list":
-                    System.out.println("Here are your tasks:");
-                    for (int i = 0; i < currList.getLength(); i++) {
-                        System.out.println(i + 1 + ". " + currList.getTaskAt(i).toString());
-                    }
-                    break;
-                case "mark": //command is mark 2
-                    Task currTask = currList.getTaskAt(Integer.parseInt(parts[1]) - 1); //the arraylist is 0 indexed, so task 1 is actually 0 index
-                    currTask.setDone();
-                    System.out.println("Let's go! I've marked this task as done:");
-                    System.out.println(currTask);
-                    break;
-                case "unmark":
-                    Task unmarkTask = currList.getTaskAt(Integer.parseInt(parts[1]) - 1); //the arraylist is 0 indexed, so task 1 is actually 0 index
-                    unmarkTask.setUndone();
-                    System.out.println("Oh man! I've marked this task as undone:");
-                    System.out.println(unmarkTask);
-                    break;
-                case "deadline": //can abstract this whole case to be generalized
-                    String[] temp = input.split(" /by ", 2);
-                    String by = temp[1];
-                    String deadlineDesc = temp[0].split("deadline ")[1];
-                    Deadline deadlineTask = new Deadline(deadlineDesc, LocalDate.parse(by));
-                    currList.addTask(deadlineTask);
-                    deadlineTask.addTaskMessage();
-                    Ui.taskNumberMessage(currList);
-                    break;
-                case "event": //can abstract this whole case to be generalized
-                    String[] temp1 = input.split(" /at ", 2);
-                    String at = temp1[1];
-                    String eventDesc = temp1[0].split("event ", 2)[1];
-                    Event eventTask = new Event(eventDesc, LocalDate.parse(at));
-                    currList.addTask(eventTask);
-                    eventTask.addTaskMessage();
-                    Ui.taskNumberMessage(currList);
-                    break;
-                case "todo": //can abstract this whole case to be generalized
-                    String todoDesc = input.split("todo ")[1];
-                    Todo todoTask = new Todo(todoDesc);
-                    currList.addTask(todoTask);
-                    todoTask.addTaskMessage();
-                    Ui.taskNumberMessage(currList);
-                    break;
-                case "delete":
-                    Task toBeDeleted = currList.getTaskAt(Integer.parseInt(parts[1]) - 1);
-                    System.out.println("Alrighty, this task's gone: ");
-                    System.out.println(toBeDeleted);
-                    currList.removeTask(toBeDeleted);
-                    break;
-                default:
-                    System.out.println(input);
+            case "list":
+                System.out.println("Here are your tasks:");
+                for (int i = 0; i < currList.getLength(); i++) {
+                    System.out.println(i + 1 + ". " + currList.getTaskAt(i).toString());
+                }
+                break;
+            case "mark": //command is mark 2
+                Task currTask = currList.getTaskAt(Integer.parseInt(parts[1]) - 1); //the arraylist is 0 indexed, so task 1 is actually 0 index
+                currTask.setDone();
+                System.out.println("Let's go! I've marked this task as done:");
+                System.out.println(currTask);
+                break;
+            case "unmark":
+                Task unmarkTask = currList.getTaskAt(Integer.parseInt(parts[1]) - 1); //the arraylist is 0 indexed, so task 1 is actually 0 index
+                unmarkTask.setUndone();
+                System.out.println("Oh man! I've marked this task as undone:");
+                System.out.println(unmarkTask);
+                break;
+            case "deadline": //can abstract this whole case to be generalized
+                String[] temp = input.split(" /by ", 2);
+                String by = temp[1];
+                String deadlineDesc = temp[0].split("deadline ")[1];
+                Deadline deadlineTask = new Deadline(deadlineDesc, LocalDate.parse(by));
+                currList.addTask(deadlineTask);
+                deadlineTask.addTaskMessage();
+                Ui.taskNumberMessage(currList);
+                break;
+            case "event": //can abstract this whole case to be generalized
+                String[] temp1 = input.split(" /at ", 2);
+                String at = temp1[1];
+                String eventDesc = temp1[0].split("event ", 2)[1];
+                Event eventTask = new Event(eventDesc, LocalDate.parse(at));
+                currList.addTask(eventTask);
+                eventTask.addTaskMessage();
+                Ui.taskNumberMessage(currList);
+                break;
+            case "todo": //can abstract this whole case to be generalized
+                String todoDesc = input.split("todo ")[1];
+                Todo todoTask = new Todo(todoDesc);
+                currList.addTask(todoTask);
+                todoTask.addTaskMessage();
+                Ui.taskNumberMessage(currList);
+                break;
+            case "delete":
+                Task toBeDeleted = currList.getTaskAt(Integer.parseInt(parts[1]) - 1);
+                System.out.println("Alrighty, this task's gone: ");
+                System.out.println(toBeDeleted);
+                currList.removeTask(toBeDeleted);
+                break;
+            case "find":
+                String searchWord = parts[1];
+                System.out.println("Here are the matching tasks:");
+                TaskList matched = currList.matchingItems(searchWord);
+                for (int i = 0; i < matched.getLength(); i++) {
+                    System.out.println(i + 1 + ". " + matched.getTaskAt(i).toString());
+                }
+                break;
+            default:
+                System.out.println(input);
             }
 
         } catch (EmptyDescriptionException e) {
