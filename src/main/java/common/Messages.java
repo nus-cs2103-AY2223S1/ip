c238e8254efa2f36e9b15d6b750dde469bbe8afd
donljh <don.limjunhong@gmail.com>
2022-08-25 02:05:34 +0800

    Add Ui package, TextUi class, extract welcome and goodbye messages to Messages class

---
+++ src/main/java/common/Messages.java
@@ -0,0 +1,12 @@
+package common;
+
+/**
+ * Class containing the reusable messages to print to user
+ */
+public class Messages {
+    public static final String MESSAGE_WELCOME_1 = "Hello... me Blob...";
+    public static final String MESSAGE_WELCOME_2 = "How can Blob help...?";
+
+    public static final String MESSAGE_GOODBYE_1 = "Thanks for talking to Blob...";
+    public static final String MESSAGE_GOODBYE_2 = "Blob see you soon...";
+}
