f057949191c43f73c1a06bb9b50dec3304f1ea7f
mohamedsaf1 <mohdsaf1@hotmail.com>
2022-08-24 17:15:12 +0800

    Level-6-edited

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+      public Task(String description) {
+          this.description = description;
+          this.isDone = false;
+      }
+
+      public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
+      }
+
+      public void mark() {
+          this.isDone = true;
+      }
+      public void unmark() {
+          this.isDone = false;
+      }
+
+      public String toString() {
+          return this.getStatusIcon() + " " + this.description;
+      }
+}
