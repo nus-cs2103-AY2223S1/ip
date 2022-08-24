a8059d920ddb89a38f9679a337287a0aa0a3d225
yellow-294 <77526609+yellow-294@users.noreply.github.com>
2022-08-24 14:17:00 +0800

    Completed Level-8 on separate branch

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,13 +1,28 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import java.util.Date;
+
 public class Deadlines extends Task {
 
-    private final String date;
-    public Deadlines(String deadline, String date) {
+    private LocalDate localDate;
+
+    public Deadlines(String deadline, String dateStr) throws ParseException {
         super(deadline);
-        this.date = date;
+        try {
+            this.localDate = LocalDate.parse(dateStr);
+        } catch (DateTimeParseException e){
+            System.out.println("Please input dates in the following format: yyyy-mm-dd (eg. 2019-10-15)");
+
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + date + ")";
+        return "[D]" + super.toString() + " (by:" + localDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
+
