af4ac1ee22e37f471e76565b945970cd5b5796e7
kxrt <kvrtikeya@gmail.com>
2022-08-24 17:21:06 +0800

    Implement formatting of date/time inputs
    Level-8
    - Create DukeDateTimeFormatter class with a static method to encapsulate formatting in one place
    - Implement DukeDateTimeFormatter in Deadline and Event classes
    - Create formattedAt/formattedBy strings to represent formatted date-time strings, replace into toString

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -6,14 +6,20 @@
 public class Deadline extends DukeTask {
   // Time of deadline
   private final String by;
+  private String formattedBy;
 
   public Deadline(String description, String by) {
     super(description);
     this.by = by;
+    formatTime();
+  }
+
+  private void formatTime() {
+    this.formattedBy = DukeDateTimeFormatter.format(by);
   }
 
   @Override
   public String toString() {
-    return "[D]" + super.toString() + " (by: " + by + ")";
+    return "[D]" + super.toString() + " (by: " + formattedBy + ")";
   }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -6,14 +6,20 @@
 public class Event extends DukeTask {
   // Time of event
   private final String at;
+  private String formattedAt;
 
   public Event(String description, String at) {
     super(description);
     this.at = at;
+    formatTime();
+  }
+
+  private void formatTime() {
+    this.formattedAt = DukeDateTimeFormatter.format(at);
   }
 
   @Override
   public String toString() {
-    return "[E]" + super.toString() + " (at: " + at + ")";
+    return "[E]" + super.toString() + " (at: " + formattedAt + ")";
   }
 }
