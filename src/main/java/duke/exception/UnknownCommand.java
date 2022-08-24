86acb7f6db4485603ee2c18b27fb86afe1ea153d
jonasgwt <55698556+jonasgwt@users.noreply.github.com>
2022-08-24 14:54:12 +0800

    UnknownCommand class: Error message fix

--- src/main/java/duke/exception/UnknownCommand.java
+++ src/main/java/duke/exception/UnknownCommand.java
@@ -2,6 +2,6 @@ package duke.exception;
 
 public class UnknownCommand extends DukeException {
     public UnknownCommand() {
-        super("Unknown duke.commands.Command!");
+        super("Unknown command!");
     }
 }
