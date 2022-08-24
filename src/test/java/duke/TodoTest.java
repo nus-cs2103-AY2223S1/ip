f1a35b82193f1c36618fb5eeccda3852ffbbc677
leecaregene <e0764279@u.nus.edu>
2022-08-25 04:33:51 +0800

    Complete A-CodingStandard

--- src/test/java/duke/TodoTest.java
+++ src/test/java/duke/TodoTest.java
@@ -1,8 +1,8 @@
 package duke;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class TodoTest {
 
