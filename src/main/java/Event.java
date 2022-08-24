e1bf36ba73e8ad8ecc11c5d68137b24a33e7fcc3
ish1506 <ishiuanlee@gmail.com>
2022-08-24 15:27:43 +0800

    Implement save tasks functionality

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,8 +1,21 @@
 public class Event extends Task {
-    private String duration;
+    private final String duration;
+
     public Event(String name, String duration) {
         super(name);
         this.duration = duration;
+    }
+
+    public Event(String name, String duration, boolean isDone) {
+        super(name, isDone);
+        this.duration = duration;
+    }
+
+    public static Event fromString(String inputString) {
+        boolean isDone = inputString.charAt(4) == 'X';
+        String name = inputString.substring(7, inputString.indexOf("(at"));
+        String duration = inputString.substring(inputString.indexOf("(at: ") + 5, inputString.length() - 1);
+        return new Event(name, duration, isDone);
     }
 
     @Override
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,14 @@
-public class Task {
-    private String name;
+public abstract class Task {
+    private final String name;
     private boolean isDone = false;
 
     public Task(String name) {
         this.name = name;
+    }
+
+    public Task(String name, boolean isDone) {
+        this.name = name;
+        this.isDone = isDone;
     }
 
     public void mark() {
@@ -12,6 +17,19 @@ public class Task {
 
     public void unmark() {
         this.isDone = false;
+    }
+
+    public static Task fromString(String inputString) {
+        char typeOfTask = inputString.charAt(1);
+        switch (typeOfTask) {
+        case 'T':
+            return Todo.fromString(inputString);
+        case 'D':
+            return Deadline.fromString(inputString);
+        case 'E':
+            return Event.fromString(inputString);
+        }
+        return null;
     }
 
     @Override
