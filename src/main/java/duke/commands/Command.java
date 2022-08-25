e2e7fd78d1023c6acdedf7217b824630fa0fb9ea
florentianayuwono <e0851439@u.nus.edu>
2022-08-25 23:40:38 +0800

    fix build issue

--- src/main/java/duke/commands/Command.java
+++ src/main/java/duke/commands/Command.java
@@ -4,6 +4,9 @@ import duke.storage.Storage;
 import duke.task.List;
 import duke.ui.Ui;
 
+/**
+ * Represents an executable command.
+ */
 public abstract class Command {
     public abstract void execute(List tasks, Ui ui, Storage storage);
 
