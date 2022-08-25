ff52edd965329c1079d0ae5c36d607066fc798d9
Zsigmond <e0725064@u.nus.edu>
2022-08-25 17:20:52 +0800

    refactor: tasklist class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+    private ArrayList<String> addCommands;   // running these commands will always give the tasks array
+    // delete, update, list functions
+
+    // takes in a string of add commands
+    public TaskList(String commands) {
+        this.tasks = new ArrayList<>();
+        this.addCommands = new ArrayList<>();
+        String[] commandArr = commands.split("\n");
+        for(String command: commandArr) {
+            Parser.parse(command, this);      // should get the command, then execute it to change the task list
+        }
+    }
+
+    public void add(Task task, String command) {
+        this.tasks.add(task);
+        this.addCommands.add(command);
+    }
+
+    // gets the (i-1)th task in tasks
+    public Task get(int i) throws IndexOutOfBoundsException {
+        return this.tasks.get(i);
+    }
+
+    public int size() {
+        return this.tasks.size();
+    }
+
+    public void remove(int i) {
+        this.tasks.remove(i);
+        this.addCommands.remove(i);
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < this.size(); i++) {
+            s = s.concat(
+                    String.format(
+                            "%d. %s\n", i + 1, this.get(i).toString()
+                    )
+            );
+        }
+        return s;
+    }
+}
