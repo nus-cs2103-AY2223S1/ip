ce2c78674472b981743168de1db60c4f0c576dae
jwdavis0200 <jwdavis0200@gmail.com>
2022-08-25 12:44:18 +0800

    Remove wildcard import in favour of specific imports of classes
    IDE reccomended wildcard import for Java.io since there were many classes from this package used in the program.
    
    We should always avoid using wildcard imports in accordance to the Java Code Standard in CS2103T.
    
    We have to replace the wildcard import with specific import statements for each specific packages.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Iterator;
