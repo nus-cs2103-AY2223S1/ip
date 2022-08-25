83394f1cf9730b9934572149de450abcf49c0818
Steven Lim <limhwsteven@gmail.com>
2022-08-25 12:43:13 +0800

    Move Command enum to Parser class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,18 +6,15 @@ import java.io.IOException;
 import java.util.List;
 
 public class Duke {
-    public enum Command {
-        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE
-    }
-
     // IMPORTANT: Configure isTest to true if you are running ./runtest.sh
     private static final String JAR_FILE_PATH = "data/tasks.txt";
     private static final Boolean isTest = false;
     // Comment out the conditional expression and add in JAR_FILE_PATH if running on JAR
-    private static final String FILE_PATH = JAR_FILE_PATH;
-//            isTest
-//            ? "../src/main/java/data/tasksTest.txt"
-//            : "src/main/java/data/tasks.txt";
+    private static final String FILE_PATH =
+//            JAR_FILE_PATH;
+            isTest
+            ? "../src/main/java/data/tasksTest.txt"
+            : "src/main/java/data/tasks.txt";
 
     private static Ui ui = new Ui();
     private static TaskList taskList = new TaskList();
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -8,6 +8,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Parser {
+    public enum Command {
+        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE
+    }
+
     private static Ui ui = new Ui();
     private static TaskList taskList = new TaskList();
     private static Scanner input = new Scanner(System.in);
@@ -18,6 +22,13 @@ public class Parser {
 
     public List<Task> getTasks() { return taskList.getTasks(); }
 
+    /**
+     * This function will parse the input given by the user and try to match the
+     * inputted command with the list of commands available.
+     * Then, the function will execute the relevant actions according to the command.
+     *
+     * @param tasks list of Task objects
+     */
     public void parseCommand(List<Task> tasks) {
         while (true) {
             try {
@@ -26,30 +37,30 @@ public class Parser {
                 String[] inputs = inputLine.split(" ");
                 String command = inputs[0];
 
-                if (inputLine.equals(Duke.Command.BYE.name().toLowerCase())) {
+                if (inputLine.equals(Command.BYE.name().toLowerCase())) {
                     Ui.endSession(input);
                     return;
-                } else if (inputLine.equals(Duke.Command.LIST.name().toLowerCase())) {
+                } else if (inputLine.equals(Command.LIST.name().toLowerCase())) {
                     Ui.showTasks(tasks);
-                } else if (command.equals(Duke.Command.MARK.name().toLowerCase())) {
+                } else if (command.equals(Command.MARK.name().toLowerCase())) {
                     // inputs[1] is the index number of the task to be marked
                     if (inputs.length < 2) {
                         throw new InvalidTaskIndexException();
                     }
                     ui.markTaskAsDone(Integer.parseInt(inputs[1]), tasks);
-                } else if (command.equals(Duke.Command.UNMARK.name().toLowerCase())) {
+                } else if (command.equals(Command.UNMARK.name().toLowerCase())) {
                     // inputs[1] is the index number of the task to be unmarked
                     if (inputs.length < 2) {
                         throw new InvalidTaskIndexException();
                     }
                     ui.markTaskAsNotDone(Integer.parseInt(inputs[1]), tasks);
-                } else if (command.equals(Duke.Command.TODO.name().toLowerCase())) {
+                } else if (command.equals(Command.TODO.name().toLowerCase())) {
                     taskList.addTodo(inputs);
-                } else if (command.equals(Duke.Command.DEADLINE.name().toLowerCase())) {
+                } else if (command.equals(Command.DEADLINE.name().toLowerCase())) {
                     taskList.addDeadline(inputs);
-                } else if (command.equals(Duke.Command.EVENT.name().toLowerCase())) {
+                } else if (command.equals(Command.EVENT.name().toLowerCase())) {
                     taskList.addEvent(inputs);
-                } else if (command.equals(Duke.Command.DELETE.name().toLowerCase())) {
+                } else if (command.equals(Command.DELETE.name().toLowerCase())) {
                     // inputs[1] is the index number of the task to be marked
                     if (inputs.length < 2) {
                         throw new InvalidTaskIndexException();
