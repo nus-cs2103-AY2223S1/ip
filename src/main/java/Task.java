0c6ed0c480890bdfb0c1ee116b515bc43984c5f6
tsammeow <97447335+tsammeow@users.noreply.github.com>
2022-08-26 01:55:16 +0800

    Prepare for saving

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,20 @@ public class Task {
         this.isDone = false;
     }
 
+    public static Task fromEncoded(String s) {
+        String[] pieces = s.split("\\|");
+        switch (pieces[0]) {
+        case "T":
+            return new Todo(pieces[1]);
+        case "D":
+            return new Deadline(pieces[1], pieces[2]);
+        case "E":
+            return new Event(pieces[1], pieces[2]);
+        default:
+            return new Task(pieces[1]);
+        }
+    }
+
     public String getStatusIcon() {
         return isDone ? "X" : " ";
     }
@@ -17,6 +31,14 @@ public class Task {
 
     public String getDetails() {
         return details;
+    }
+
+    public String getEncodedDetails() {
+        return details;
+    }
+
+    public String getEncoded() {
+        return getTaskIcon() + "|" + getStatusIcon() + "|" + getEncodedDetails();
     }
 
     @Override
