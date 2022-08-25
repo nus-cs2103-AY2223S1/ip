a78057507a5e48764f791ffac654d9b249949b76
Shawn532 <E0556732@u.nus.edu>
2022-08-25 22:14:33 +0800

    implements the basic structure of level-8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,24 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
     public String eventTime;
+    public LocalDate exactTime;
 
     public Event(String name, String eventTime) {
         super(name);
         this.eventTime = eventTime;
+        try {
+            this.exactTime = LocalDate.parse(eventTime);
+        } catch (DateTimeParseException e) {
+            System.out.println("Wrong input format!");
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(" + eventTime + ")";
+        return "[E]" + super.toString() + "(" + exactTime.getMonth() + " "
+                + exactTime.getDayOfMonth() + " " + exactTime.getYear() + ")";
     }
 }
