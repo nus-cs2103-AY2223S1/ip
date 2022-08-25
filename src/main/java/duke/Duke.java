505e7f37781ad752de699309d28cca0262968eeb
Ryan <ryanlml123@gmail.com>
2022-08-26 01:28:53 +0800

    Added A-jar.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,9 +5,9 @@ import java.io.FileNotFoundException;
 
 public class Duke {
 
-    private Storage storage;
+    private final Storage storage;
     private TaskList tasks;
-    private Ui ui;
+    private final Ui ui;
 
     public Duke(String filePath) {
         ui = new Ui();
