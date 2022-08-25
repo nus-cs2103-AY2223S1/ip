238f12125ae8ab709bc97d0bac6f7fac45b8730a
Chee Zhong Wei <cheezhongwei00@gmail.com>
2022-08-25 20:45:15 +0800

    Amend Duke.java to use Parser and Command classes

--- src/main/java/DukeBot/Duke.java
+++ src/main/java/DukeBot/Duke.java
@@ -1,4 +1,6 @@
 package DukeBot;
+import DukeBot.command.Command;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -70,89 +72,17 @@ public class Duke {
         System.out.println("| Hi this is Thesh. What can I do for you? |");
         System.out.println("-----------------------------------------------");
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String command = sc.nextLine();
-            System.out.println("    ____________________________________________________________");
-            if (command.equals("bye")) {
-                break;
-            } else if (command.equals("list")) {
-                Duke.list();
-            } else if (command.startsWith("mark")) {
-                try {
-                    String taskNumberString = command.split(" ")[1];
-                    int taskNumber = Integer.parseInt(taskNumberString);
-                    Duke.mark(taskNumber - 1);
-                } catch (DukeException e) {
-                    System.out.println(e);
-                } catch (NumberFormatException e) {
-                    System.out.println("    please only input a number after 'mark'.");
-                } catch (NullPointerException e) {
-                    System.out.println(String.format("There is only %d tasks in the list.", Task.getTaskCount()));
-                }
-            } else if (command.startsWith("unmark")) {
-                try {
-                    String taskNumberString = command.split(" ")[1];
-                    int taskNumber = Integer.parseInt(taskNumberString);
-                    Duke.unmark(taskNumber - 1);
-                } catch (DukeException e) {
-                    System.out.println(e);
-                } catch (NumberFormatException e) {
-                    System.out.println("    please only input a number after 'unmark'.");
-                } catch (NullPointerException e) {
-                    System.out.println(String.format("There is only %d tasks in the list.", Task.getTaskCount()));
-                }
-            } else if (command.startsWith("delete")) {
-                command = command.replace("delete ", "");
-                int taskToDelete = Integer.parseInt(command);
-                Duke.delete(taskToDelete - 1);
-            } else {
-                Task newTask = null;
-                if (command.startsWith("todo")) {
-                    if (command.equals("todo")) {
-                        System.out.println("    OOPS!!! The description of a todo cannot be empty.");
-                        continue;
-                    }
-                    command = command.replace("todo ", "");
-                    newTask = new ToDo(command);
-
-                } else if (command.startsWith("deadline")) {
-                    command = command.replace("deadline ", "");
-                    String[] commands = command.split(" /by ");
-                    if (commands.length != 2) {
-                        System.out.println("    Sorry, please give a deadline in this format:\n    " +
-                                "  'deadline <action> /by <deadline>'");
-                        continue;
-                    }
-                    try {
-                        newTask = new Deadline(commands[0], commands[1]);
-                    } catch (DukeException e) {
-                        System.out.println(e);
-                        continue;
-                    }
-                } else if (command.startsWith("event")) {
-                    command = command.replace("event ", "");
-                    String[] commands = command.split(" /at ");
-                    if (commands.length != 2) {
-                        System.out.println("    Sorry, please give an event in this format:\n    " +
-                                "  'event <event> /at <timing>'");
-                        continue;
-                    }
-                    try {
-                        newTask = new Event(commands[0], commands[1]);
-                    } catch (DukeException e) {
-                        System.out.println(e);
-                        continue;
-                    }
-                } else {
-                    System.out.println("    OOPS!!! I'm sorry, but I don't know what that means :-(");
-                    continue;
-                }
-                tasks.addTask(newTask, false);
+        boolean isExit = false;
+        Parser p = new Parser(tasks);
+        while (!isExit) {
+            try {
+                Command c = p.parse(sc.nextLine());
+                c.execute();
+                isExit = c.isExit();
+            } catch (DukeException e) {
+                System.out.println(e);
             }
-            System.out.println("    ____________________________________________________________");
         }
-        System.out.println("    Bye. Hope to see you again soon!");
-        System.out.println("    ____________________________________________________________");
         storage.write(tasks);
     }
 }
