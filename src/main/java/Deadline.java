c4fe5a903f6f81f7f6aef00bf6430419ef5f2f0f
Prittam Ravi <prittam867@gmail.com>
2022-08-25 21:14:32 +0800

    Add DateTime functionality

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,16 @@
+import java.time.LocalDate;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = DateTimeConverter.formatDate(by.substring(1));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by:" + by + ")";
+        return "[D]" + super.toString() + "(by:" + DateTimeConverter.formatString(by) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,15 @@
+import java.time.LocalDate;
+
 public class Event extends Task{
-    protected String at;
+    protected LocalDate at;
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        this.at = DateTimeConverter.formatDate(at.substring(1));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at:" + at + ")";
+        return "[E]" + super.toString() + "(at:" + DateTimeConverter.formatString(at) + ")";
     }
 }
