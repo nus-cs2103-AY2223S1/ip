9c61dce2339bdada5be5cab344ead763f86b818b
gabyang <gabrielyang@berkeley.edu>
2022-08-25 21:59:11 +0800

    Complete Level-7 (Redo)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,53 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
 
+    public static void read() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("list.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] temp = line.split("/");
+                if (temp.length == 3) {
+                    tasklist.add(new DukeTask(temp[2], temp[1].contains("X"), temp[0].charAt(0)));
+                } else {
+                    tasklist.add(new DukeTask(temp[2], temp[1].contains("X"), temp[0].charAt(0), temp[3]));
+                }
+
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    public static void save() {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("list.txt"));
+            for (int i = 0; i < tasklist.size(); i ++) {
+                DukeTask t = tasklist.get(i);
+                System.out.println(t.task);
+                System.out.println(t.time);
+                writer.write(t.taskType + "/" + (t.isMarked ? 'X' : 'O') + "/" + t.task + "/" + t.time +"\n");
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+
+    }
+
+    private static final ArrayList<DukeTask> tasklist = new ArrayList<>();
     public static void main(String[] args) {
+        read();
         Scanner input = new Scanner(System.in);
         System.out.println("What are your commands sir:");
-
-        ArrayList<DukeTask> tasklist = new ArrayList<>();
-
         boolean pred = true;
         while(pred) {
             if (input.hasNext()) {
@@ -28,6 +67,7 @@ public class Duke {
                         tasklist.get(j).isMarked = true;
                         System.out.println("Nice! I've marked this task as done:");
                         System.out.println(String.format("List %d: ", j) + tasklist.get(j).toString());
+                        save();
                     } catch (Exception e) {
                         System.out.println("Something went wrong, here's the error message cuz im lazy to figure it out for you: " + e);
                     }
@@ -38,6 +78,7 @@ public class Duke {
                     tasklist.get(j).isMarked = false;
                     System.out.println("Got it. I've mark this task as not done:");
                     System.out.println(String.format("List %d: ", j) + tasklist.get(j).toString());
+                    save();
 
                 } else if (str.startsWith("todo")) {
                     try {
@@ -50,6 +91,7 @@ public class Duke {
                         tasklist.add(t);
                         System.out.println("Got it. I've added this task:");
                         System.out.println(String.format("List %d: ", tasklist.size() - 1) + t.toString());
+                        save();
                     } catch (StringIndexOutOfBoundsException e) {
                         System.out.println("Oops, todo can't be empty");
                     } catch (Exception e) {
@@ -60,19 +102,23 @@ public class Duke {
 
                 } else if (str.startsWith("deadline")) {
                     str = str.substring(9);
-                    String s = str.substring(0, str.indexOf('/')) + '(' + str.substring(str.indexOf('/') + 1) + ')';
-                    DukeTask t = new DukeTask(s, false, 'D');
+                    String s1 = str.substring(0, str.indexOf('/') - 1);
+                    String s2 = "(" + str.substring(str.indexOf('/') + 1) + ')';
+                    DukeTask t = new DukeTask(s1, false, 'D', s2);
                     tasklist.add(t);
                     System.out.println("Got it. I've added this task:");
                     System.out.println(String.format("List %d: ", tasklist.size() - 1) + t.toString());
+                    save();
 
                 } else if (str.startsWith("event")) {
                     str = str.substring(6);
-                    String s = str.substring(0, str.indexOf('/')) + '(' + str.substring(str.indexOf('/') + 1) + ')';
-                    DukeTask t = new DukeTask(s, false, 'E');
+                    String s1 = str.substring(0, str.indexOf('/') - 1);
+                    String s2 = "(" + str.substring(str.indexOf('/') + 1) + ')';
+                    DukeTask t = new DukeTask(s1, false, 'E', s2);
                     tasklist.add(t);
                     System.out.println("Got it. I've added this task:");
                     System.out.println(String.format("List %d: ", tasklist.size() - 1) + t.toString());
+                    save();
 
                 } else if (str.startsWith("delete")) {
                     try {
@@ -80,6 +126,7 @@ public class Duke {
                         System.out.println("Alight! I've deleted this task for you:");
                         System.out.println(String.format("List %d: ", j) + tasklist.get(j).toString());
                         tasklist.remove(j);
+                        save();
 
                     } catch (Exception e) {
                         System.out.println("Something went wrong, here's the error message cuz im lazy to figure it out for you: " + e);
