854233f3a9eb9027b616123af995b27ea7d8b1f4
Deon <deeyonn@gmail.com>
2022-08-25 19:26:57 +0800

    Modify Validator
    * Can now parse dates.
    * Throws JennyException on parse failure.

--- src/main/java/jenny/util/Validator.java
+++ src/main/java/jenny/util/Validator.java
@@ -1,30 +1,24 @@
 package jenny.util;
 
+import jenny.exceptions.JennyException;
+
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeParseException;
 
+/**
+ * Handles input validation for the JennyBot application.
+ * CS2103 Week 3
+ * AY21/22 Semester 1
+ *
+ * @author Deon
+ */
 public class Validator {
-    public Validator() {
-
-        //create dates from strings
-        LocalDate d1 = LocalDate.parse("2019-12-01");
-        LocalDate d2 = LocalDate.parse("2019-12-02");
-        LocalDate d3 = LocalDate.parse("2019-12-02");
-
-        //compare dates
-        System.out.println(d1.isBefore(d2)); // -> true
-        System.out.println(d1.isAfter(d2)); // -> false
-        System.out.println(d2.equals(d3)); // -> true
-
-        //work with dates
-        System.out.println(d1.getDayOfWeek()); // -> SUNDAY
-        System.out.println(d1.getMonth()); // -> DECEMBER
-        System.out.println(d1.plus(1, ChronoUnit.YEARS));  // -> 2020-12-01
-
-        // get today's date and print it in a specific format
-        LocalDate d4 = LocalDate.now();
-        System.out.println(d4); // -> 2019-10-15
-        System.out.println(d4.format(DateTimeFormatter.ofPattern("MMM d yyyy"))); // -> Oct 15 2019
+    private static final String MESSAGE_SCOPE = Validator.class.getSimpleName();
+    public static LocalDate parseDate(String input) {
+        try {
+            return LocalDate.parse(input);
+        } catch (DateTimeParseException e) {
+            throw new JennyException(MESSAGE_SCOPE, e.getMessage());
+        }
     }
 }
