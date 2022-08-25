efc1dfe923ed6651639ddbce82d09610635b4a85
kavantan <kavantan10@gmail.com>
2022-08-25 10:21:21 +0800

    Implement Ui class which deals with user interactions

---
+++ src/main/java/Ui.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ui {
+    private static final String HORIZONTAL_LINE = "  ____________________________________________________________";
+    private Scanner commands;
+
+    public Ui() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println(HORIZONTAL_LINE + "\n  Hello! I'm Duke\n  What can I do for you?\n" + HORIZONTAL_LINE);
+        this.commands = new Scanner(System.in);
+    }
+
+    public String getUserCommand() {
+        return commands.nextLine();
+    }
+}
+
