7d6ecc195763dbb15b85d7eb3cf29e1bb12af1e0
albertarielw <albertariel2002@gmail.com>
2022-08-25 14:45:56 +0800

    A-Packages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,4 @@
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 
 public class Duke {
 
