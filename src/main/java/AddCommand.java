3ab91b0f21682c05bdc019e8d6b6aa00ea4c2c43
bensohh <lok200910@gmail.com>
2022-08-25 23:30:05 +0800

    Create branch for Date Feature Level-8

--- src/main/java/AddCommand.java
+++ src/main/java/AddCommand.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class AddCommand extends Command {
     private int taskType;
@@ -16,15 +19,25 @@ public class AddCommand extends Command {
             tasks.add(new Todo(this.taskDetails, false));
         } else if (this.taskType == 1) {
             String[] infoArray = this.taskDetails.split(" /by ", 2);
-            tasks.add(new Deadline(infoArray[0], infoArray[1], false));
+            tasks.add(new Deadline(infoArray[0], formatDate(infoArray[1]), false));
         } else {
             String[] infoArray = this.taskDetails.split(" /at ", 2);
-            tasks.add(new Event(infoArray[0], infoArray[1], false));
+            tasks.add(new Event(infoArray[0], formatDate(infoArray[1]), false));
         }
         String taskDescription = "  " + tasks.getTask(tasks.size() - 1).toString();
         System.out.println(this);
         System.out.println(taskDescription);
         storage.writeToFile(tasks);
+    }
+
+    // Format the dates from YYYY-MM_DD to MMM d yyyy
+    public String formatDate(String dateInput) {
+        try {
+            LocalDate d1 = LocalDate.parse(dateInput);
+            return d1.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException e) { //
+            return dateInput;
+        }
     }
 
     public boolean isExit() {
