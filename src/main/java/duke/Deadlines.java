0f0866d52616fdf49f82af50eb9df072c5f53f85
Sen Wei Lee <senweilee@Sens-MacBook-Air.local>
2022-08-25 16:45:07 +0800

    Added all files into package duke

--- src/main/java/duke/Deadlines.java
+++ src/main/java/duke/Deadlines.java
@@ -1,3 +1,7 @@
+package duke;
+
+import duke.DateAndTimeFormatter;
+
 import java.time.LocalDate;
 
 public class Deadlines extends Task {
--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,3 +1,5 @@
+package duke;
+
 public class Parser {
     private static final String LINE = "\n----------------------------------------------------------------\n";
 
