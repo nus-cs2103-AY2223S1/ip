8fc32df2e39e9015f3dced437065410104de954e
LianGuoYang <e0735382@u.nus.edu>
2022-08-26 02:05:20 +0800

    Add Javadocs for DukeException and Task

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -3,9 +3,22 @@ package duke.task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Deadline task with TaskType, description,
+ * boolean to check whether the task is done, and time of deadline.
+ */
 public class Deadline extends Task {
 
     private LocalDateTime byTime;
+
+    /**
+     * Constructor for Deadline
+     *
+     * @param type TaskType of deadline
+     * @param name Description of the deadline
+     * @param isMarked Denotes whether the deadline is done yet
+     * @param timeStr Time of deadline
+     */
     public Deadline(TaskType type, String name, boolean isMarked, String timeStr) {
         super(type, name, isMarked);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HHmm, d/MM/yyyy");
@@ -13,10 +26,17 @@ public class Deadline extends Task {
         this.byTime = time;
     }
 
+    /**
+     * @return Time of deadline
+     */
     public LocalDateTime getByTime() {
         return this.byTime;
     }
 
+    /**
+     * {@inheritDoc}
+     * @return String of deadline with details, TaskType, name, isMarked and time.
+     */
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a, EEE, d MMM yyyy");
