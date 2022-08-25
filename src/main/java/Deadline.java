c5ddfc7081d033ad9b80af4e7ff689bfcd6afd7b
Mohamed Hamas <e0726186@u.nus.edu>
2022-08-25 11:35:11 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,3 +1,11 @@
+import javax.swing.text.DateFormatter;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.logging.Formatter;
+
 public class Deadline extends Task {
     protected String by;
     public Deadline(String description,String by) {
@@ -5,9 +13,17 @@ public class Deadline extends Task {
         this.by = by;
     }
 
+
+
     @Override
-    public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+    public String toString() throws DateTimeException {
+
+
+        LocalDate d1 = LocalDate.parse(by.substring(1), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String deadline = d1.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        return "[D]" + super.toString() + " (by: " + deadline + ")";
     }
+
+
 
 }
