2554cd1083a90c31d1d712facb30d8a1854ecc1e
ConradLew <e0726724@u.nus.edu>
2022-08-25 15:19:39 +0800

    Add level-8 functionalities

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,13 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Encapsulates a task with a given deadline.
  *
  * @author Conrad
  */
 public class Deadline extends Task {
-    private String deadline;
+    private LocalDate deadline;
 
     /**
      * Constructor for creating a task with a deadline.
@@ -12,7 +15,7 @@ public class Deadline extends Task {
      * @param taskDescription A description of the task.
      * @param taskDeadline The time by which the task has to be completed.
      */
-    public Deadline(String taskDescription, String taskDeadline) {
+    public Deadline(String taskDescription, LocalDate taskDeadline) {
         super(taskDescription);
         this.deadline = taskDeadline;
     }
@@ -25,6 +28,7 @@ public class Deadline extends Task {
     @Override
     public String toString() {
         String taskStatusIndicator = "[D]" + (this.isCompleted() ? "[X] " : "[ ] ");
-        return taskStatusIndicator + this.getTaskDescription() + " (by: " + this.deadline + ")";
+        return taskStatusIndicator + this.getTaskDescription()
+                + " (by: " + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
