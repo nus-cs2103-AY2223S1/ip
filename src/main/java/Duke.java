113080e6fcbfc8d72be6447c233abd94df358725
DevanshShah1309 <devanshshah1309@gmail.com>
2022-08-25 20:30:54 +0800

    Abstract UI operations

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,11 +14,7 @@ public class Duke {
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    private static final String WELCOME_MESSAGE = "\t-------------------------------\n"
-            + "\tHello, I'm Duke!\n" + "\tWhat can I do for you?\n" + "\t-------------------------------";
 
-    private static final String BYE_MESSAGE = "\t-------------------------------\n" +
-            "\tBye! Hope to see you again\n" + "\t-------------------------------\n";
 
     public Duke(String fileName, Storage fo, TaskList taskList) {
         FILE_NAME = fileName;
@@ -28,22 +24,23 @@ public class Duke {
 
     public static void main(String[] args) {
         Duke duke = new Duke("data.txt", new Storage("data.txt"), new TaskList());
-
-        System.out.println(WELCOME_MESSAGE);
+        Ui ui = new Ui(duke.FILE_NAME);
+        ui.printWelcomeMessage();
 
         // Read from file and load into tasks
         File file;
+
         try {
             file = new File(duke.FILE_NAME);
             if (file.createNewFile()) {
-                System.out.println(String.format("\tCreated new file %s to store tasks!", duke.FILE_NAME));
-            } else { // load tasks from file to Task.tasks
+                ui.printCreateNewFile();
+            } else {
                 duke.fo.loadAllTasksFromFile(duke.taskList);
-                System.out.println(String.format("\tLoaded tasks from %s", duke.FILE_NAME));
-                System.out.println("\t-------------------------------");
+                ui.printLoadTasksFromFile();
+                ui.printLineBreak();
             }
         } catch (IOException e) {
-            System.out.println(String.format("\tError opening/creating %s!!!", duke.FILE_NAME));
+            ui.printErrorCreatingFile();
         }
 
         Scanner sc = new Scanner(System.in);
@@ -54,16 +51,17 @@ public class Duke {
         }
 
         // cleaning up and shutting down Duke
-        System.out.println(BYE_MESSAGE);
+        ui.printByeMessage();
         sc.close();
     }
 
     public void run(String input) {
+        Ui ui = new Ui(FILE_NAME);
         try {
             String[] inputString = input.split(" ", 2);
             Parser parser = new Parser(inputString);
             String command = inputString[0];
-            System.out.println("\t-------------------------------");
+            ui.printLineBreak();
             switch (command) {
             case Commands.LIST:
                 taskList.listTasks();
@@ -84,7 +82,6 @@ public class Duke {
                 Deadline deadline = parser.parseDeadline();
                 taskList.add(deadline, fo);
                 break;
-
             case Commands.EVENT:
                 Event event = parser.parseEvent();
                 taskList.add(event, fo);
@@ -94,18 +91,16 @@ public class Duke {
                 taskList.delete(taskNumber - 1, fo); // since we store tasks 0-indexed in ArrayList
                 break;
             default:
-                System.out.println("\tOops! I've no idea what you're talking about!");
+                ui.printCommandNotRecognized();
             }
-            System.out.println("\t-------------------------------");
         } catch (DukeException e) {
             System.out.printf("\t%s\n", e.getMessage());
-            System.out.println("\t-------------------------------");
         } catch (NumberFormatException e) {
-            System.out.println("\tPlease make sure you enter a task number correctly!");
-            System.out.println("\t-------------------------------");
+            ui.printErrorTaskNumber();
         } catch (java.time.format.DateTimeParseException e) {
-            System.out.println("\tPlease ensure your datetime format is in YYYY-MM-DD HH:MM");
-            System.out.println("\t-------------------------------");
+            ui.printErrorDatetimeFormat();
+        } finally {
+            ui.printLineBreak();
         }
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,43 @@
+public class Ui {
+    private String LINE_BREAK = "\t-------------------------------";
+    private String FILE_NAME;
+
+    private static final String WELCOME_MESSAGE = "\t-------------------------------\n"
+            + "\tHello, I'm Duke!\n" + "\tWhat can I do for you?\n" + "\t-------------------------------";
+
+    private static final String BYE_MESSAGE = "\t-------------------------------\n" +
+            "\tBye! Hope to see you again\n" + "\t-------------------------------\n";
+
+    public Ui(String fileName) {
+        this.FILE_NAME = fileName;
+    }
+
+    public void printLineBreak() {
+        System.out.println(LINE_BREAK);
+    }
+    public void printCreateNewFile() {
+        System.out.println(String.format("\tCreated new file %s to store tasks!", FILE_NAME));
+    }
+    public void printLoadTasksFromFile() {
+        System.out.println(String.format("\tLoaded tasks from %s", FILE_NAME));
+    }
+    public void printErrorCreatingFile() {
+        System.out.println(String.format("\tError opening/creating %s!!!", FILE_NAME));
+    }
+    public void printErrorDatetimeFormat() {
+        System.out.println("\tPlease ensure your datetime format is in YYYY-MM-DD HH:MM");
+    }
+    public void printErrorTaskNumber() {
+        System.out.println("\tPlease make sure you enter a task number correctly!");
+    }
+    public void printWelcomeMessage() {
+        System.out.println(WELCOME_MESSAGE);
+    }
+    public void printByeMessage() {
+        System.out.println(BYE_MESSAGE);
+    }
+    public void printCommandNotRecognized() {
+        System.out.println("\tOops! I've no idea what you're talking about!");
+    }
+
+}
