0654f252c0672e4346909bd13e1f2140c2281adc
24Donovan24 <donovanlee_25@hotmail.com>
2022-08-25 18:34:34 +0800

    Fix minor bug

--- src/test/java/duke/DeadlineTest.java
+++ src/test/java/duke/DeadlineTest.java
@@ -1,17 +1,20 @@
 package duke;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeadlineTest {
     @Test
     public void toStringTest() {
         assertEquals("[D][ ] watch lecture (by: Jul 24 2022)",
-                new Deadline("watch lecture", "2022-07-24").toString());
+                new Deadline("watch lecture", LocalDate.parse("2022-07-24")).toString());
     }
 
     @Test
     public void savedStringTest() {
         assertEquals("D | 0 | watch lecture | 2022-07-24",
-                new Deadline("watch lecture", "2022-07-24").savedString() );
+                new Deadline("watch lecture", LocalDate.parse("2022-07-24")).savedString() );
     }
 }
