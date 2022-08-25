4711069c6d66c8d1eaeaa85add050a6cd584fd9a
Sen Wei Lee <senweilee@Sens-MacBook-Air.local>
2022-08-25 15:54:59 +0800

    Implemented own Arraylist<Task>

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+public class TaskList {
+
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>(100);
+    }
+
+    public Task get(int i) {
+        return this.tasks.get(i);
+    }
+
+    public void remove(int i) {
+        this.tasks.remove(i);
+    }
+
+    public int size() {
+        return this.tasks.size();
+    }
+
+    public void add(Task task) {
+        this.tasks.add(task);
+    }
+
+    public String taskListToSaveString() {
+        StringBuilder saveString = new StringBuilder();
+        for (Task task : this.tasks) {
+            saveString.append(task.savedTaskString()).append("\n");
+        }
+        return saveString.toString();
+    }
+
+
+}
