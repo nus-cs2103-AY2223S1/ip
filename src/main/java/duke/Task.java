3fd27b292f1d1f68458ab8112b9ec9a5b8fc18c8
ThomasHoooo <e0725843@u.nus.edu>
2022-08-25 14:51:43 +0800

    Organise Duke files in packages

---
+++ src/main/java/duke/Task.java
@@ -0,0 +1,30 @@
+package duke;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description, boolean isDone) throws DukeException {
+
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.isDone ? "X" : " ", this.description);
+    }
+
+}
