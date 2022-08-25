c57cb1f00979f7d136fc36ef9c219e93932b596d
Yap Zhi Heng <yapzhhg@gmail.com>
2022-08-25 20:55:26 +0800

    Implement java.time.LocalDate for Deadlines and Events

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        this.by = LocalDate.parse(Arrays.stream(by.split(" ")).skip(1).collect(Collectors.joining("")), formatter);
     }
 
     @Override
     public String toString() {
-        return super.toString().replaceFirst("\\[T\\]", "[D]") + " (" + by.replaceFirst(" ", ": ") + ")";
+        return super.toString().replaceFirst("\\[T\\]", "[D]") + " (on " + by.toString() + ")";
     }
 }
