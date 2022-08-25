8d9e2b34533eadb5eca20114e7b73bc0ccc5a5a8
vantemoon <sylviassunrx0820@outlook.com>
2022-08-26 04:11:59 +0800

    Add Javadoc comments to Duke.java and Ui.java

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,12 +2,33 @@ package duke;
 
 import duke.command.Command;
 
+/**
+ * A chatbot that keeps track of a list of tasks the user inputs.
+ * Three types of tasks are supported - To-Dos, deadlines, and events.
+ * Tasks are saved in the hard disk automatically.
+ *
+ * @author   Sun Ruoxin
+ * @version  %I%, %G%
+ */
 public class Duke {
-
+    /**
+     * A Storage object which encapsulates reading from and writing into file.
+     */
     private Storage storage;
+    /**
+     * A TaskList object which encapsulates the list of tasks.
+     */
     private TaskList tasks;
+    /**
+     * A UI object which encapsulates interactions with the user.
+     */
     private Ui ui;
 
+    /**
+     * Class constructor.
+     *
+     * @param filePath  The path of the file that stores the tasks
+     */
     public Duke(String filePath) {
         ui = new Ui();
         storage = new Storage(filePath);
@@ -19,6 +40,9 @@ public class Duke {
         }
     }
 
+    /**
+     * Encapsulates the main logic of the chatbot.
+     */
     public void run() {
         ui.greet();
         storage.load(ui);
@@ -35,6 +59,11 @@ public class Duke {
         }
     }
 
+    /**
+     * Starts up the chatbot and runs its main logic.
+     * @param args            the command line arguments
+     * @throws DukeException  when error encountered
+     */
     public static void main(String[] args) throws DukeException {
         new Duke("data/tasks.txt").run();
     }
--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -4,19 +4,36 @@ import java.util.Scanner;
 
 import duke.task.Task;
 
+/**
+ * Encapsulation of interactions with the user.
+ *
+ * @author   Sun Ruoxin
+ * @version  %I%, %G%
+ */
 public class Ui {
+    /**
+     * The logo of the chatbot.
+     */
     protected String logo = "     _   _    ______     _____ ____\n"
             + "    | | / \\  |  _ \\ \\   / /_ _/ ___|\n"
             + " _  | |/ _ \\ | |_) \\ \\ / / | |\\___ \\\n"
             + "| |_| / ___ \\|  _ < \\ V /  | | ___) |\n"
             + " \\___/_/   \\_\\_| \\_\\ \\_/  |___|____/\n";
 
+    /**
+     * Greets the user.
+     */
     public void greet() {
         System.out.println(logo);
         say("Hello. I'm Jarvis", true, false);
         say("What can I do for you?", false, true);
     }
 
+    /**
+     * Reads in the input of the user.
+     *
+     * @return the input of the user
+     */
     public String readCommand() {
         Scanner scanner = new Scanner(System.in);
         String command;
@@ -24,12 +41,25 @@ public class Ui {
         return command;
     }
 
+    /**
+     * Gives feedback to the user when a task is added.
+     *
+     * @param tasks  the list of the tasks
+     * @param task   the task to be added
+     */
     public void addMessage(TaskList tasks, Task task) {
         say("Got it. I've added this task:", true, false);
         say("  " + task, false, false);
         say("Now you have " + tasks.size() + " tasks in the list.", false, true);
     }
 
+    /**
+     * Gives feedback to the user when a task is deleted.
+     *
+     * @param tasks   the list of tasks
+     * @param index   the index of the task to be deleted
+     * @param isDone  the status of whether the task is removed from the list
+     */
     public void deleteMessage(TaskList tasks, int index, boolean isDone) {
         if (!isDone) {
             say("Noted. I've removed this task:", true, false);
@@ -39,6 +69,11 @@ public class Ui {
         }
     }
 
+    /**
+     * Gives feedback to the user when asked to print out the tasks.
+     *
+     * @param tasks  the list of tasks
+     */
     public void listMessage(TaskList tasks) {
         say("Here are the tasks in your list:", true, false);
         for (int i = 0; i < tasks.size(); i++) {
@@ -48,28 +83,58 @@ public class Ui {
         }
     }
 
+    /**
+     * Gives feedback to the user when a task is marked as done.
+     *
+     * @param tasks  the list of tasks
+     * @param index  the index of the task to be marked
+     */
     public void markMessage(TaskList tasks, int index) {
         say("Nice! I've marked this task as done:", true, false);
         say(tasks.get(index).toString(), false, true);
     }
 
+    /**
+     * Gives feedback to the user when a task is unmarked as done.
+     *
+     * @param tasks  the list of tasks
+     * @param index  the index of the task to be unmarked
+     */
     public void unmarkMessage(TaskList tasks, int index) {
         say("OK, I've marked this task as not done yet:", true, false);
         say(tasks.get(index).toString(), false, true);
     }
 
+    /**
+     * Farewells the user.
+     */
     public void exitMessage() {
         say("Bye. Hope to see you again soon.", true, true);
     }
 
+    /**
+     * Gives feedback to the user when an error is encountered while loading the file.
+     */
     public void showLoadingError() {
         say("Encountered error while loading.", true, true);
     }
 
+    /**
+     * Gives feedback to the user when an error is encountered.
+     *
+     * @param errorMessage  the message of the error
+     */
     public void showError(String errorMessage) {
         say("Encountered error: " + errorMessage, true, true);
     }
 
+    /**
+     * Prints out the message in a fixed format.
+     *
+     * @param message      the message to be printed out
+     * @param isFirstLine  whether the message is the first line
+     * @param isLastLine   whether the message is the last line
+     */
     public void say(String message, boolean isFirstLine, boolean isLastLine) {
         String line = "____________________________________________________________";
         if (isFirstLine) {
