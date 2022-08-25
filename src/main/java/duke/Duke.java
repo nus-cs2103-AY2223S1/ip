a1c14d3c1e75af7d1a4ab2019621e9b748b471b2
Hongyi6328 <6328huang@gmail.com>
2022-08-25 09:41:03 +0800

    Resolve conflicts between master and branch-Level-8

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -69,7 +69,7 @@ public class Duke {
     private String addNewTask(String input)
             throws DukeCommandFormatException, DukeTaskTitleMissingException, DukeTaskDateTimeMissingException,
             DukeDateTimeFormatException {
-        Task newTask = Task.valueOf(input);
+        Task newTask = Task.createFromCommand(input);
         if (newTask == null) {
             return GENERAL_ERROR_STRING;
         }
