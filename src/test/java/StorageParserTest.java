68f9f944701fb6932b6a45b1b86d9a5fb8086d9e
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-08-25 22:51:46 +0800

    Test StorageParser

--- src/test/java/StorageParserTest.java
+++ src/test/java/StorageParserTest.java
@@ -4,18 +4,22 @@ import stubs.StorageStub;
 import utility.StorageParser;
 import task.Task;
 import storage.Storage;
+import java.util.List;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.fail;
 public class StorageParserTest {
-    private StorageStub s = new StorageStub(Paths.get("tasksStub.txt"));
+    private StorageStub storageStub = new StorageStub(Paths.get("tasksStub.txt"));
 
     @Test
     public void test_file_line_to_task() {
         try {
-            assert(StorageParser.fileLineToTask("[T][ ] task1") instanceof  Task);
+            List<String> lines = storageStub.getAllLines();
+            for (int i  = 0; i < lines.size(); i ++) {
+                assert (StorageParser.fileLineToTask(lines.get(i)) instanceof Task);
+            }
         } catch (DukeException d) {
             fail();
         }
--- src/test/java/stubs/StorageStub.java
+++ src/test/java/stubs/StorageStub.java
@@ -1,4 +1,6 @@
 package stubs;
+import java.util.List;
+import java.util.ArrayList;
 
 import java.nio.file.Path;
 
@@ -6,5 +8,12 @@ public class StorageStub {
     Path path;
     public StorageStub(Path path) {
         this.path =  path;
+    }
+
+    public List<String> getAllLines() {
+        ArrayList<String> lines = new ArrayList<>();
+        lines.add("[T][ ] Task0");
+        lines.add("[T][ ] Task1");
+        return lines;
     }
 }
