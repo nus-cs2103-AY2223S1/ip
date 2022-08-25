2de8c293f6e608ff83b3c7f7df1b11a0b67cd795
Deon <deeyonn@gmail.com>
2022-08-25 18:26:25 +0800

    Modify DateTimeParser

--- src/main/java/DateTimeParser.java
+++ src/main/java/DateTimeParser.java
@@ -1,9 +1,32 @@
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class DateTimeParser {
     private final LocalDateTime dateTime;
     public DateTimeParser(String date) {
         LocalDateTime dateTime = LocalDateTime.parse(date);
         this.dateTime = dateTime;
+
+        //create dates from strings
+        LocalDate d1 = LocalDate.parse("2019-12-01");
+        LocalDate d2 = LocalDate.parse("2019-12-02");
+        LocalDate d3 = LocalDate.parse("2019-12-02");
+
+        //compare dates
+        System.out.println(d1.isBefore(d2)); // -> true
+        System.out.println(d1.isAfter(d2)); // -> false
+        System.out.println(d2.equals(d3)); // -> true
+
+        //work with dates
+        System.out.println(d1.getDayOfWeek()); // -> SUNDAY
+        System.out.println(d1.getMonth()); // -> DECEMBER
+        System.out.println(d1.plus(1, ChronoUnit.YEARS));  // -> 2020-12-01
+
+        // get today's date and print it in a specific format
+        LocalDate d4 = LocalDate.now();
+        System.out.println(d4); // -> 2019-10-15
+        System.out.println(d4.format(DateTimeFormatter.ofPattern("MMM d yyyy"))); // -> Oct 15 2019
     }
 }
