4b371026ff9c993db765ac0f6f0d32db33eee9fc
lulucopter <lzydaniel.lee@gmail.com>
2022-08-26 03:30:06 +0800

    Rename program to Dan

--- src/main/java/Dan.java
+++ src/main/java/Dan.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Duke {
+public class Dan {
     private static final int MAX_TASK_SIZE = 100;
 
     public static void main(String[] args) {
