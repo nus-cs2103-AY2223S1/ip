775be6d522638f68e49d2748cd2515a1e9cea879
JonathanWiguna <jonathan.wiguna1@gmail.com>
2022-08-25 15:04:02 +0800

    Change representation of date to LocalDateTime object

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime deadline;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String deadline) {
         super(description);
-        this.by = by;
+        this.deadline = LocalDateTime.parse(deadline, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + deadline.getDayOfWeek() + " " + deadline.getDayOfMonth() + " " +
+                deadline.getMonth() + " " + deadline.getYear() + " " + deadline.getHour() + deadline.getMinute() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String time;
+    protected LocalDateTime time;
 
     public Event(String description, String time) {
         super(description);
-        this.time = time;
+        this.time = LocalDateTime.parse(time, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + time + ")";
+        return "[E]" + super.toString() + " (at: " + time.getDayOfWeek() + " " + time.getDayOfMonth() + " " +
+                time.getMonth() + " " + time.getYear() + " " + time.getHour() + time.getMinute() + ")";
     }
 }
