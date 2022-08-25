284a2a9a855d2c66d2503edb365db009bc22ca2d
Prearo <kohshanghui@gmail.com>
2022-08-25 20:56:58 +0800

    Implement Ui and fix minor .gitignore errors

--- src/main/java/Dukebot.java
+++ src/main/java/Dukebot.java
@@ -3,28 +3,30 @@ import java.io.IOException;
 public class Dukebot {
     private TaskList taskList;
     private Storage storage;
+    private Ui ui;
 
     private void handleStartup() {
-        System.out.println(Messages.STARTUP);
+        ui = new Ui();
+        ui.display(Messages.STARTUP);
         try {
             this.storage = new Storage();
             storage.createStorage();
             this.taskList = storage.loadFromStorage();
         } catch (IOException e) {
-            System.out.println(Messages.LOAD_ERROR);
+            ui.display(Messages.LOAD_ERROR);
             this.taskList = new TaskList();
         }
     }
 
     private void handleBye() {
-        System.out.println(Messages.ENDING);
+        ui.display(Messages.ENDING);
         System.exit(0);
     }
 
     private void handleList() {
-        System.out.println(Messages.LIST_TASKS);
+        ui.display(Messages.LIST_TASKS);
         for (int i = 0; i < this.taskList.getSize(); i++) {
-            System.out.println((i + 1) + "." + taskList.get(i).toString());
+            ui.display((i + 1) + "." + taskList.get(i).toString());
         }
     }
 
@@ -33,10 +35,10 @@ public class Dukebot {
             throw new DukeException(String.format(ExceptionMessages.EMPTY_TASK_DESCRIPTION, "remove"));
         }
         int currentPos = Integer.parseInt(inputLessAction[1]) - 1;
-        System.out.println(Messages.TASK_REMOVED);
-        System.out.println(taskList.get(currentPos).toString());
+        ui.display(Messages.TASK_REMOVED);
+        ui.display(taskList.get(currentPos).toString());
         taskList.remove(Integer.parseInt(inputLessAction[1]) - 1);
-        System.out.printf((Messages.TASK_COUNT) + "%n", taskList.getSize());
+        ui.display(String.format((Messages.TASK_COUNT) + "%n", taskList.getSize()));
     }
 
     private void handleMark(String[] inputLessAction) throws DukeException {
@@ -45,8 +47,8 @@ public class Dukebot {
         }
         int index = Integer.parseInt(inputLessAction[1]) - 1;
         this.taskList.mark(index);
-        System.out.println(Messages.TASK_MARKED);
-        System.out.println(this.taskList.get(index).toString());
+        ui.display(Messages.TASK_MARKED);
+        ui.display(this.taskList.get(index).toString());
     }
 
     private void handleUnmark(String[] inputLessAction) throws DukeException {
@@ -56,8 +58,8 @@ public class Dukebot {
 
         int index = Integer.parseInt(inputLessAction[1]) - 1;
         taskList.get(index).markUndone();
-        System.out.println(Messages.TASK_UNMARKED);
-        System.out.println(taskList.get(index).toString());
+        ui.display(Messages.TASK_UNMARKED);
+        ui.display(taskList.get(index).toString());
     }
 
     private void handleTodo(String[] inputLessAction) throws DukeException {
@@ -66,9 +68,9 @@ public class Dukebot {
         }
         String taskDesc = inputLessAction[1];
         this.taskList.add(new Todo(taskDesc));
-        System.out.println(Messages.TASK_ADDED);
-        System.out.println(this.taskList.get(this.taskList.getSize() - 1).toString());
-        System.out.printf((Messages.TASK_COUNT) + "%n", this.taskList.getSize());
+        ui.display(Messages.TASK_ADDED);
+        ui.display(this.taskList.get(this.taskList.getSize() - 1).toString());
+        ui.display(String.format((Messages.TASK_COUNT) + "%n", this.taskList.getSize()));
     }
 
     private void handleDeadline(String[] inputLessAction) throws DukeException {
@@ -89,9 +91,9 @@ public class Dukebot {
             throw new DukeException(String.format(ExceptionMessages.EMPTY_TASK_TIME, "deadline"));
         }
         this.taskList.add(new Deadline(taskDesc, taskTime));
-        System.out.println(Messages.TASK_ADDED);
-        System.out.println(this.taskList.get(this.taskList.getSize() - 1).toString());
-        System.out.printf((Messages.TASK_COUNT) + "%n", this.taskList.getSize());
+        ui.display(Messages.TASK_ADDED);
+        ui.display(this.taskList.get(this.taskList.getSize() - 1).toString());
+        ui.display(String.format((Messages.TASK_COUNT) + "%n", this.taskList.getSize()));
     }
 
     private void handleEvent(String[] inputLessAction) throws DukeException {
@@ -112,9 +114,9 @@ public class Dukebot {
             throw new DukeException(String.format(ExceptionMessages.EMPTY_TASK_TIME, "event"));
         }
         this.taskList.add(new Event(taskDesc, taskTime));
-        System.out.println(Messages.TASK_ADDED);
-        System.out.println(this.taskList.get(this.taskList.getSize() - 1).toString());
-        System.out.printf((Messages.TASK_COUNT) + "%n", this.taskList.getSize());
+        ui.display(Messages.TASK_ADDED);
+        ui.display(this.taskList.get(this.taskList.getSize() - 1).toString());
+        ui.display(String.format((Messages.TASK_COUNT) + "%n", this.taskList.getSize()));
     }
 
     protected void handleInput(String input) {
@@ -155,9 +157,9 @@ public class Dukebot {
                 throw new DukeException(ExceptionMessages.UNSUPPORTED_ACTION);
             }
         } catch (DukeException e) {
-            System.out.println(e.getMessage());
+            ui.display(e.getMessage());
         } catch (IOException e) {
-            System.out.println("Error writing to storage");
+            ui.display("Error writing to storage");
         }
 
     }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,2 +1,29 @@
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Ui {
+    private static final String LS = System.lineSeparator();
+
+    private final Scanner in;
+    private final PrintStream out;
+
+    public Ui() {
+        this(System.in, System.out);
+    }
+
+    public Ui(InputStream in, PrintStream out) {
+        this.in = new Scanner(in);
+        this.out = out;
+    }
+
+    public String getInput() {
+        out.print("Enter command: ");
+        return in.nextLine();
+    }
+
+    public void display(String s) {
+        out.println(s);
+    }
+
 }
