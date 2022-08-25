1541204513143c753f049580c4902d19f8d871c3
tin-jy <tinjingyao@gmail.com>
2022-08-25 21:51:18 +0800

    Add ability to read inputs and echo it as output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hi, I'm Duke. What can I do for you?");
+        Scanner keyboard = new Scanner(System.in);
+        String message = keyboard.nextLine();
+        while (!message.equals("bye")) {
+            System.out.println(message);
+            message = keyboard.nextLine();
+        }
+        System.out.println("Bye! See you next time!");
     }
 }
