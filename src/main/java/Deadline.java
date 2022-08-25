7f9e696e06ef758042537abef15d067b53469262
therealdaofu <xu.daofu20@gmail.com>
2022-08-25 10:01:43 +0800

    Update Deadline class
    deadline class took in and stored date and time as strings
    
    now it will take in date and time in the ISO-8601 format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,16 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 /*
 This class encapsulates the idea of a deadline
  */
 public class Deadline extends Task {
-    private String deadline;
+    private LocalDateTime date;
 
-    public Deadline(String description, String deadline) {
+    public Deadline(String description, LocalDateTime date) {
         super(description);
-        this.deadline = deadline;
+        this.date = date;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.getStatusIcon() + " " + super.toString() + "(by: " + this.deadline + ")";
+        return "[D]" + super.getStatusIcon() + " " + super.toString() +
+                "(by: " + date.format(DateTimeFormatter.ofPattern("HH:mm MMM d yyyy")) + ")";
     }
 }
