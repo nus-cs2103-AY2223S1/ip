30b805fe260323ebb7c396fe976775472683d551
cfyjoanna <joannachanfy@gmail.com>
2022-08-25 10:30:19 +0800

    follow coding standard

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -2,11 +2,13 @@ package duke;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+
 import java.util.ArrayList;
 
 public class Storage {
