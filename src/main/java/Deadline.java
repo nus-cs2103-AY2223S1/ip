6bf70ead843ff4a36d447805ad7dc1a3a1165f58
Richard Dominick <richdom2185@gmail.com>
2022-08-25 13:27:50 +0800

    Update date handling in Deadline to use date objects

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,20 @@
-public class Deadline extends Task {
-    private String deadline;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-    Deadline(String title, String deadline) {
+public class Deadline extends Task {
+    private LocalDate byDate;
+    private LocalTime byTime;
+
+    Deadline(String title, String deadline) throws IllegalArgumentException {
         super(title);
-        this.deadline = deadline;
+        byDate = RegexHelper.extractAndParseDate(deadline)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid deadline date: " + deadline));
+        byTime = RegexHelper.extractAndParseTime(deadline)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid deadline time: " + deadline));
     }
 
     @Override
     public String toString() {
-        return String.format("‼ %s (by %s)", super.toString(), deadline);
+        return String.format("‼ %s (by %s at %s)", super.toString(), byDate, byTime);
     }
 }
