287be33f35bec73019c1ab4a83bb36553091cdc7
LiuFangrui <e0772663@u.nus.edu>
2022-08-25 20:48:30 +0800

    removed unneeded dependencies

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,6 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-
-import java.time.LocalDate;
 
 public class Duke {
 
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Ui {
