c5f6296bfc7da55141d9444a26c4d02841d2d252
kavantan <kavantan10@gmail.com>
2022-08-25 10:22:26 +0800

    Implement Parser class which handles user commands

---
+++ src/main/java/Parser.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Parser {
+    private static final String HORIZONTAL_LINE = "  ____________________________________________________________";
+
+    public static void handleCommand(String command, ArrayList<Task> storedTasks) throws DukeException {
+        if (command.equals("bye")) {
+            System.out.println(HORIZONTAL_LINE + "\n  Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
+        } else if (command.equals("list")) {
+            System.out.println(HORIZONTAL_LINE);
+            for (int i = 0; i < storedTasks.size(); i++) {
+                System.out.println("  " + String.valueOf(i + 1) + ". " + storedTasks.get(i));
+            }
+            System.out.println(HORIZONTAL_LINE);
+        } else if (command.split(" ").length == 2 && command.split(" ")[0].equals("mark")) {
+            int taskNumber = Integer.parseInt(command.split(" ")[1]) - 1;
+            storedTasks.get(taskNumber).markAsDone();
+            System.out.println(HORIZONTAL_LINE + "\n  Nice! I've marked this task as done:\n"
+                    + storedTasks.get(taskNumber) + "\n" + HORIZONTAL_LINE);
+        } else if (command.split(" ").length == 2 && command.split(" ")[0].equals("unmark")) {
+            int taskNumber = Integer.parseInt(command.split(" ")[1]) - 1;
+            storedTasks.get(taskNumber).markAsUndone();
+            System.out.println(HORIZONTAL_LINE + "\n  OK, I've marked this task as not done yet:\n"
+                    + storedTasks.get(taskNumber) + "\n" + HORIZONTAL_LINE);
+        } else if (command.split(" ").length == 2 && command.split(" ")[0].equals("delete")) {
+            int taskNumber = Integer.parseInt(command.split(" ")[1]) - 1;
+            System.out.println(HORIZONTAL_LINE + "\n  Noted. I've removed this task:\n    "
+                    + storedTasks.get(taskNumber));
+            storedTasks.remove(taskNumber);
+            System.out.println("  Now you have "
+                    + String.valueOf(storedTasks.size()) + " tasks in the list.\n" + HORIZONTAL_LINE);
+        } else if (command.split(" ").length > 1 && command.split(" ")[0].equals("todo")) {
+            ArrayList<String> commandDelimited = new ArrayList<String>(Arrays.asList(command.split(" ")));
+            if (commandDelimited.size() == 1) {
+                throw new DukeException("The description of a todo cannot be empty.");
+            }
+            StringBuilder description = new StringBuilder();
+            for (int i = 1; i < commandDelimited.size(); i++) {
+                description.append(commandDelimited.get(i));
+                if (i != commandDelimited.size() - 1) {
+                    description.append(" ");
+                }
+            }
+            Task todo = new Todos(description.toString());
+            storedTasks.add(todo);
+            System.out.println(
+                    HORIZONTAL_LINE + "\n  Got it. I've added this task:\n  " + todo + "\n  Now you have "
+                            + String.valueOf(storedTasks.size()) + " tasks in the list.\n" + HORIZONTAL_LINE);
+        } else if (command.split(" ").length > 3 && command.split(" ")[0].equals("deadline")) {
+            ArrayList<String> commandDelimited = new ArrayList<String>(Arrays.asList(command.split(" ")));
+            int posOfBy = commandDelimited.indexOf("/by");
+            StringBuilder description = new StringBuilder();
+            StringBuilder dateTime = new StringBuilder();
+            for (int i = 1; i < commandDelimited.size(); i++) {
+                if (i < posOfBy) {
+                    description.append(commandDelimited.get(i));
+                    if (i != posOfBy - 1) {
+                        description.append(" ");
+                    }
+                } else if (i > posOfBy) {
+                    dateTime.append(commandDelimited.get(i));
+                    if (i != commandDelimited.size() - 1) {
+                        dateTime.append(" ");
+                    }
+                }
+            }
+
+            Task deadline = new Deadlines(description.toString(), dateTime.toString(),
+                    DateAndTimeParser.validateAndParse(dateTime.toString()));
+            storedTasks.add(deadline);
+            System.out.println(
+                    HORIZONTAL_LINE + "\n  Got it. I've added this task:\n  " + deadline + "\n  Now you have "
+                            + String.valueOf(storedTasks.size()) + " tasks in the list.\n" + HORIZONTAL_LINE);
+        } else if (command.split(" ").length > 3 && command.split(" ")[0].equals("event")) {
+            ArrayList<String> commandDelimited = new ArrayList<String>(Arrays.asList(command.split(" ")));
+            int posOfAt = commandDelimited.indexOf("/at");
+            StringBuilder description = new StringBuilder();
+            StringBuilder specificTime = new StringBuilder();
+            for (int i = 1; i < commandDelimited.size(); i++) {
+                if (i < posOfAt) {
+                    description.append(commandDelimited.get(i));
+                    if (i != posOfAt - 1) {
+                        description.append(" ");
+                    }
+                } else if (i > posOfAt) {
+                    specificTime.append(commandDelimited.get(i));
+                    if (i != commandDelimited.size() - 1) {
+                        specificTime.append(" ");
+                    }
+                }
+            }
+            Task event = new Events(description.toString(), specificTime.toString());
+            storedTasks.add(event);
+            System.out.println(
+                    HORIZONTAL_LINE + "\n  Got it. I've added this task:\n  " + event + "\n  Now you have "
+                            + String.valueOf(storedTasks.size()) + " tasks in the list.\n" + HORIZONTAL_LINE);
+        } else {
+            throw new DukeException("I'm sorry, but I don't know what that means :-(");
+        }
+    }
+}
