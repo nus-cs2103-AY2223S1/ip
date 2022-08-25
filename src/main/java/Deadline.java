ee8d836d33ca519b2596da4c65cb2bc0c0fa586f
Elton Chua <notle1706@gmail.com>
2022-08-25 15:41:35 +0800

    Add save to text function

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -3,11 +3,22 @@ public class Deadline extends Task {
 
     public Deadline(String item, String by) {
         super(item);
-        this.by = by;
+        this.by = JamieTime.reformatDateTime(by);
     }
 
     @Override
     public String toString() {
         return "[D]" + super.toString() + "(by: " + by + ")";
+    }
+
+    @Override
+    public String toLine() {
+        String line = "D";
+        if (this.done) {
+            line += ("*1*" + this.item + "*" + JamieTime.undoReformatDateTime(this.by));
+        } else {
+            line += ("*0*" + this.item + "*" + JamieTime.undoReformatDateTime(this.by));
+        }
+        return line;
     }
 }
