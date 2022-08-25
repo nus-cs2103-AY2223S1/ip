44bd506144cea9769741151e0a9595a984dfc3da
ZQ <e0725180@u.nus.edu>
2022-08-25 15:27:36 +0800

    Changed exception signature

--- src/main/java/CorruptedLineException.java
+++ src/main/java/CorruptedLineException.java
@@ -1,7 +1,7 @@
 public class CorruptedLineException extends DukeException {
-    private static final String DESCRIPTION = "There was an error parsing this line: %n%s%n This line will be ignored!";
+    private static final String DESCRIPTION = "There was an error parsing this line";
 
-    CorruptedLineException(String line) {
-        super(String.format(DESCRIPTION, line));
+    CorruptedLineException() {
+        super(DESCRIPTION);
     }
 }
