d5540855b4401f88fc2e2692e0119c431a179cf5
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-08-25 23:09:02 +0800

    Fix style violations in AddDeadline.java

--- src/main/java/command/AddDeadlineCommand.java
+++ src/main/java/command/AddDeadlineCommand.java
@@ -7,10 +7,20 @@ import tasklist.TaskList;
 import ui.UI;
 import utility.Parser;
 
+/**
+ * Command that handles adding Deadline to TaskList and Storage.
+ */
 public class AddDeadlineCommand extends Command {
     public AddDeadlineCommand() {
     }
 
+    /**
+     * Runs when deadline is to be added.
+     * @param taskList TaskList to append Deadline to.
+     * @param ui ui provides user command.
+     * @param storage Storage space to append Deadline to.
+     * @throws DukeException When parsing user command fails.
+     */
     public void execute(TaskList taskList, UI ui, Storage storage) throws DukeException {
         String userInput = ui.currentInput;
         Deadline deadlineToAdd = Parser.stringToDeadline(userInput);
@@ -20,6 +30,10 @@ public class AddDeadlineCommand extends Command {
         ui.showMessage("added deadline");
     }
 
+    /**
+     * Returns false to allow program execution to continue.
+     * @return false.
+     */
     public boolean isExit() {
         return false;
     }
