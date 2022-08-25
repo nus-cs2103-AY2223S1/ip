ebb899ae01ed65c57ea9d0a740ff4d28bc82204d
ConradLew <e0726724@u.nus.edu>
2022-08-25 19:55:59 +0800

    Add A-MoreOOP functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -12,204 +13,74 @@ import java.io.File;
  */
 
 public class Duke {
-    public static void main(String[] args) throws IOException, DukeException {
 
-        String dukeGreeting = "Hello! I'm Duke\nWhat can I do for you?";
-        System.out.println(dukeGreeting);
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
 
+    public Duke(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(storage.loadTasks());
+        } catch (DukeException e) {
+            ui.showError(e);
+            tasks = new TaskList();
+        } catch (FileNotFoundException e) {
+            ui.showLoadingError(e);
+        }
+    }
+
+    public void run() {
+        this.ui.showWelcome();
         Scanner sc = new Scanner(System.in);
-        String userResponse = null;
-        ArrayList<Task> userTasks = new ArrayList<>();
-        String taskUrl = "src/main/java/tasks.txt";
-        File f = new File(taskUrl);
-        Scanner s = new Scanner(f);
-        while (s.hasNext()) {
-            String taskTextRepresentation = s.nextLine();
-            String[] parsedTaskTextRepresentation = taskTextRepresentation.split("\\|");
-            String taskType = parsedTaskTextRepresentation[0];
-            boolean isTaskDone = Integer.parseInt(parsedTaskTextRepresentation[1]) == 1;
-            String taskDescription = parsedTaskTextRepresentation[2];
-            switch (taskType) {
-                case "T":
-                    Task savedTodo = new Todo(taskDescription);
-                    if (isTaskDone) {
-                        savedTodo.setCompleted();
-                    }
-                    userTasks.add(savedTodo);
-                    break;
-                case "D":
-                    LocalDate taskDeadline = LocalDate.parse(parsedTaskTextRepresentation[3]);
-                    Task savedDeadline = new Deadline(taskDescription, taskDeadline);
-                    if (isTaskDone) {
-                        savedDeadline.setCompleted();
-                    }
-                    userTasks.add(savedDeadline);
-                    break;
-                case "E":
-                    String taskEventTime = parsedTaskTextRepresentation[3];
-                    Task savedEvent = new Event(taskDescription, taskEventTime);
-                    if (isTaskDone) {
-                        savedEvent.setCompleted();
-                    }
-                    userTasks.add(savedEvent);
-                    break;
-                default:
-                    throw new DukeException("Corrupted task file");
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String userResponse = sc.nextLine();
+                Command command = Parser.parseUserResponse(userResponse);
+                switch (command) {
+                    case BYE:
+                        this.storage.saveTasks(this.tasks);
+                        this.ui.showGoodbye();
+                        isExit = true;
+                        break;
+                    case LIST:
+                        this.ui.showTasks(this.tasks);
+                        break;
+                    case MARK:
+                        this.tasks.markTask(Parser.parseTaskNumber(userResponse));
+                        this.ui.showMarkSuccess(Parser.parseTaskNumber(userResponse));
+                        break;
+                    case UNMARK:
+                        this.tasks.unmarkTask(Parser.parseTaskNumber(userResponse));
+                        this.ui.showUnmarkSuccess(Parser.parseTaskNumber(userResponse));
+                        break;
+                    case TODO:
+                        this.tasks.addTask(Parser.parseTodoTask(userResponse));
+                        this.ui.showAddTaskSuccess(this.tasks);
+                        break;
+                    case EVENT:
+                        this.tasks.addTask(Parser.parseEventTask(userResponse));
+                        this.ui.showAddTaskSuccess(this.tasks);
+                        break;
+                    case DEADLINE:
+                        this.tasks.addTask(Parser.parseDeadlineTask(userResponse));
+                        this.ui.showAddTaskSuccess(this.tasks);
+                        break;
+                    case DELETE:
+                        this.tasks.deleteTask(Parser.parseTaskNumber(userResponse));
+                        this.ui.showRemoveTaskSuccess(Parser.parseTaskNumber(userResponse), this.tasks);
+                        break;
+                }
+            } catch (DukeException e) {
+                this.ui.showError(e);
             }
         }
 
-        while (userResponse == null || !userResponse.equals("bye")) {
-            userResponse = sc.nextLine();
-            String dukeOutput = "";
-            String[] parsedUserResponse = userResponse.split(" ");
-            String mainCommand = parsedUserResponse[0];
-            switch(mainCommand) {
-                case "":
-                    break;
-                case "bye":
-                    if (parsedUserResponse.length > 1) {
-                        dukeOutput = ("    " + "Invalid number of arguments, only one required\n");
-                    } else {
-                        FileWriter fw = new FileWriter(taskUrl);
-                        for (Task userTask : userTasks) {
-                            fw.write(userTask.getTextRepresentation());
-                        }
-                        fw.close();
-                        dukeOutput = "    " + "Bye. Hope to see you again soon!";
-                    }
-                    break;
-                case "list":
-                    if (parsedUserResponse.length > 1) {
-                        dukeOutput = ("    " + "Invalid number of arguments, only one required\n");
-                    } else {
-                        dukeOutput = "    " + "Here are the tasks in your list:\n";
-                        for (int i = 0; i < userTasks.size(); i++) {
-                            Task userTask = userTasks.get(i);
-                            dukeOutput += "        " + (i + 1) + ". " + userTask + "\n";
-                        }
-                    }
-                    break;
-                case "mark":
-                    if (parsedUserResponse.length != 2) {
-                        dukeOutput = ("    " + "Invalid number of arguments, two required\n");
-                    } else {
-                        int taskNumber = Integer.parseInt(userResponse.replaceAll("[^0-9]", "")) - 1;
-                        try {
-                            Task userTask = userTasks.get(taskNumber);
-                            if (userTask.isCompleted()) {
-                                dukeOutput = "    " + "This task is already marked as done.\n";
-                            } else {
-                                userTask.setCompleted();
-                                dukeOutput = "    " + "Nice! I've marked this task as done:\n"
-                                        + "      " + userTask + "\n";
-                            }
-                        } catch (IndexOutOfBoundsException e) {
-                            dukeOutput = "    " + "No such task exists.\n";
-                        }
-                    }
+    }
 
-                    break;
-                case "unmark":
-                    if (parsedUserResponse.length != 2) {
-                        dukeOutput = ("    " + "Invalid number of arguments, two required\n");
-                    } else {
-                        int taskNumber = Integer.parseInt(userResponse.replaceAll("[^0-9]", "")) - 1;
-                        try {
-                            Task userTask = userTasks.get(taskNumber);
-                            if (!userTask.isCompleted()) {
-                                dukeOutput = "    " + "This task is already marked as not done yet.\n";
-                            } else {
-                                userTask.setUncompleted();
-                                dukeOutput = "    " + "OK, I've marked this task as not done yet:\n"
-                                        + "      " + userTask + "\n";
-                            }
-                        } catch (IndexOutOfBoundsException e) {
-                            dukeOutput = "    " + "No such task exists.\n";
-                        }
-                    }
-                    break;
-                case "todo":
-                    if (parsedUserResponse.length < 2) {
-                        dukeOutput = "    " + "☹ OOPS!!! The description of a todo cannot be empty.\n";
-                    } else {
-                        String newTaskDescription = String.join(" ",
-                                                                Arrays.copyOfRange(parsedUserResponse,
-                                                                              1,
-                                                                                   parsedUserResponse.length));
-                        Task newUserTask = new Todo(newTaskDescription);
-                        userTasks.add(newUserTask);
-                        dukeOutput = "    " + "Got it. I've added this task:\n"
-                                   + "        " + newUserTask + "\n"
-                                   + "    " + "Now you have " + userTasks.size() + " tasks in the list.\n";
-                    }
-                    break;
-                case "deadline":
-                    int bySeparationIndex = Arrays.asList(parsedUserResponse).indexOf("/by");
-                    if (bySeparationIndex == -1) {
-                        dukeOutput = "    " + "Error: No date added for the deadline.\n";
-                    } else {
-                        String newTaskDescription = String.join(" ",
-                                                                Arrays.copyOfRange(parsedUserResponse,
-                                                                              1,
-                                                                                bySeparationIndex));
-                        try {
-                            LocalDate deadline = LocalDate.parse(parsedUserResponse[bySeparationIndex + 1]);
-                            Task newUserTask = new Deadline(newTaskDescription, deadline);
-                            userTasks.add(newUserTask);
-                            dukeOutput = "    " + "Got it. I've added this task:\n"
-                                    + "        " + newUserTask + "\n"
-                                    + "    " + "Now you have " + userTasks.size() + " tasks in the list.\n";
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            dukeOutput = "    " + "Error: No date added for the deadline.\n";
-                        } catch (DateTimeParseException e) {
-                            dukeOutput = "    " + "Error: Invalid deadline format, please key in as yyyy-mm-dd\n";
-                        }
-                    }
-
-                    break;
-                case "event":
-                    int atSeparationIndex = Arrays.asList(parsedUserResponse).indexOf("/at");
-                    if (atSeparationIndex == -1) {
-                        dukeOutput = "    " + "Error: No date added for the event.\n";
-                    } else {
-                        String newTaskDescription = String.join(" ",
-                                Arrays.copyOfRange(parsedUserResponse,
-                                        1,
-                                        atSeparationIndex));
-                        String newEventTime = String.join(" ",
-                                Arrays.copyOfRange(parsedUserResponse,
-                                        atSeparationIndex + 1,
-                                        parsedUserResponse.length));
-                        Task newUserTask = new Event(newTaskDescription, newEventTime);
-                        userTasks.add(newUserTask);
-                        dukeOutput = "    " + "Got it. I've added this task:\n"
-                                + "        " + newUserTask + "\n"
-                                + "    " + "Now you have " + userTasks.size() + " tasks in the list.\n";
-                    }
-
-                    break;
-                case "delete":
-                    if (parsedUserResponse.length != 2) {
-                        dukeOutput = "    " + "Invalid number of arguments, two required\n";
-                    } else {
-                        int taskNumber = Integer.parseInt(userResponse.replaceAll("[^0-9]", "")) - 1;
-                        try {
-                            Task userTask = userTasks.get(taskNumber);
-                            userTasks.remove(taskNumber);
-                            dukeOutput = ("    " + "Noted. I've removed this task:\n" +
-                                    "    " + userTask + "\n" +
-                                    "    " + "Now you have " + userTasks.size() +  " tasks in the list.\n");
-                        } catch (IndexOutOfBoundsException e) {
-                            dukeOutput = "    " + "No such task exists.\n";
-                        }
-                    }
-
-                    break;
-                default:
-                    dukeOutput = "    " + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n";
-            }
-            System.out.println(dukeOutput);
-        }
-
+    public static void main(String[] args) {
+        new Duke("src/main/java/tasks.txt").run();
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,47 @@
+public class Ui {
+
+    public void showWelcome() {
+        String dukeGreeting = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(dukeGreeting);
+    }
+
+    public void showGoodbye() {
+        String dukeGoodbye = "    " + "Bye. Hope to see you again soon!";
+        System.out.println(dukeGoodbye);
+    }
+
+    public void showError(DukeException e) {
+        System.out.println(e.getMessage());
+    }
+
+    public void showLoadingError(Exception e) {
+        System.out.println(e.getMessage());
+    }
+
+    public void showTasks(TaskList taskList) {
+        if (taskList.length() != 0) {
+            System.out.println("    " + "Here are the tasks in your list:\n");
+            System.out.println(taskList.toString());
+        } else {
+            System.out.println("    " + "There are no tasks in your list.\n");
+        }
+
+    }
+
+    public void showMarkSuccess(int taskNumber) {
+        System.out.println( "    " + "Nice! I've marked task " + (taskNumber + 1) + " as done.\n");
+    }
+
+    public void showUnmarkSuccess(int taskNumber) {
+        System.out.println("    " + "OK, I've marked task " + (taskNumber + 1) + " as not done yet.\n");
+    }
+
+    public void showRemoveTaskSuccess(int taskNumber, TaskList taskList) {
+        System.out.println("    " + "Noted. I've removed task " + (taskNumber + 1) + ".\n" +
+                "    " + "Now you have " + taskList.length() +  " tasks in the list.\n");
+    }
+
+    public void showAddTaskSuccess(TaskList taskList) {
+        System.out.println("    " + "Task added. You now have " + taskList.length() + " tasks in the list.\n");
+    }
+}
