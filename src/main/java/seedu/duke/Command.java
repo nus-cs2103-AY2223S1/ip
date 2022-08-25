4ee667103f5dc6be05e278a9b923a4abbeb46bf4
Aloynz <aloysiusneozeen@gmail.com>
2022-08-26 01:33:20 +0800

    Edit code to follow Coding Standard
    Removed duplicate code from Storage.java
    
    Ensured that every line has at most 120 characters.

--- src/main/java/seedu/duke/Command.java
+++ src/main/java/seedu/duke/Command.java
@@ -56,7 +56,8 @@ public enum Command {
                 task = new Todo(input.substring(5));
                 tasks.add(task);
                 ui.add(task);
-                System.out.println("Now you have " + tasks.size() + (tasks.size() == 1 ?" task" : " tasks") + " in the list.");
+                System.out.println("Now you have " + tasks.size() + (tasks.size() == 1 ?" task" : " tasks") 
+                    + " in the list.");
                 break;
                 
             case EVENT:
@@ -72,7 +73,8 @@ public enum Command {
                 task = new Event(split[0], split[1].substring(3));
                 tasks.add(task);
                 ui.add(task);
-                System.out.println("Now you have " + tasks.size() + (tasks.size() == 1 ?" task" : " tasks") + " in the list.");
+                System.out.println("Now you have " + tasks.size() + (tasks.size() == 1 ?" task" : " tasks") 
+                    + " in the list.");
                 break;
 
             case DEADLINE:
@@ -89,7 +91,8 @@ public enum Command {
                 task = new Deadline(split[0], split[1].substring(3));
                 tasks.add(task);
                 ui.add(task);
-                System.out.println("Now you have " + tasks.size() + (tasks.size() == 1 ?" task" : " tasks") + " in the list.");
+                System.out.println("Now you have " + tasks.size() + (tasks.size() == 1 ?" task" : " tasks") 
+                    + " in the list.");
                 break;
 
             case DELETE:
@@ -100,7 +103,8 @@ public enum Command {
                 task = tasks.get(taskNumber);
                 tasks.remove(taskNumber);
                 ui.remove(task);
-                System.out.println("Now you have " + tasks.size() + (tasks.size() == 1 ?" task" : " tasks") + " in the list.");
+                System.out.println("Now you have " + tasks.size() + (tasks.size() == 1 ?" task" : " tasks") 
+                    + " in the list.");
                 break;
 
             case BYE:
