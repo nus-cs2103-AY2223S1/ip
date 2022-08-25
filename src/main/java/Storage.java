8efe885694ce42b66174529d5d116b73a89bc5a6
Daniel Luo <hurinturin123@gmail.com>
2022-08-25 12:37:34 +0800

    Level 7.1: Fixed bug where new tasks will override older tasks on pixel.txt. Also converted to BufferedWriter for more effective writing to file.

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,6 +1,8 @@
 // Storage: deals with loading tasks from the file and saving tasks in the file
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 public class Storage {
 
@@ -10,11 +12,17 @@ public class Storage {
         this.filePath = filePath;
     }
 
+   /* During the write operation, the characters are written to the internal buffer instead of the disk.
+   Once the buffer is filled or the writer is closed, the whole characters in the buffer are
+   written to the disk.
+
+    Hence, the number of communication to the disk is reduced.
+*/
     public void writeToFile(Task task) throws IOException {
         String textToAdd = task.formatTaskBeforeSave();
-        FileWriter fw = new FileWriter(this.filePath);
-        fw.write(textToAdd);
-        fw.close();
+        Writer bufferedFileWriter = new BufferedWriter(new FileWriter(this.filePath, true)); // FileWriter(String fileName, boolean append)
+        bufferedFileWriter.append(textToAdd + "\n");
+        bufferedFileWriter.close();
     }
 
 }
