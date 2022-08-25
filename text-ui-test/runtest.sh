ebdc537ca9b70245a5b9442376f02c43c79794f7
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2022-08-25 22:36:42 +0800

    Update runtest.sh to delete data folder inside

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -22,6 +22,11 @@ fi
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ../bin entry.Duke < input.txt > ACTUAL.TXT
 
+if [ -d "data" ]
+then
+  rm -rf "data"
+fi
+
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
