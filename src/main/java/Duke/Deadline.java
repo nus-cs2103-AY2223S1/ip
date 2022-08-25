90378aa589b4f2903ffd8632a3dd5019ae6361aa
jsincorporated <e0725210@u.nus.edu>
2022-08-25 15:15:33 +0800

    Fixed bug

--- src/main/java/Duke/Deadline.java
+++ src/main/java/Duke/Deadline.java
@@ -5,10 +5,12 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 public class Deadline extends Task {
+    protected String by;
     protected LocalDate dateTime;
 
     public Deadline(String description, String by) {
         super(description);
+        this.by = by;
         dateTime = LocalDate.parse(by, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
         // System.out.println(dateTime.toString());
 
@@ -16,9 +18,11 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
+        return "[D]" + super.toString() + "(by: " + by + ")";
+    }
 
+    public String toUser() {
         return "[D]" + super.toString() + "(by: " + this.dateTime.format(DateTimeFormatter
                 .ofLocalizedDate(FormatStyle.FULL)) + ")";
-
     }
 }
--- src/main/java/Duke/Event.java
+++ src/main/java/Duke/Event.java
@@ -5,10 +5,12 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 public class Event extends Task {
+    protected String at;
     protected LocalDate dateTime;
 
     public Event(String description, String at) {
         super(description);
+        this.at = at;
         dateTime = LocalDate.parse(at, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
         // System.out.println(dateTime.toString());
 
@@ -16,9 +18,11 @@ public class Event extends Task {
 
     @Override
     public String toString() {
+        return "[E]" + super.toString() + "(at: " + at + ")";
+    }
 
-        return "[E]" + super.toString() + "(at: " + this.dateTime.format(DateTimeFormatter
+    public String toUser() {
+        return "[D]" + super.toString() + "(by: " + this.dateTime.format(DateTimeFormatter
                 .ofLocalizedDate(FormatStyle.FULL)) + ")";
-
     }
 }
