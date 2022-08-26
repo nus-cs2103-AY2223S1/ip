8200075e2c2382d64fe911c55fa48beb0762f341
debwy <e0773217@u.nus.edu>
2022-08-26 09:25:07 +0800

    Add save/load functionality (Level-7)

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -2,12 +2,24 @@ public class Deadline extends Task {
 
     private String by;
 
-    public Deadline(String name, boolean isDone, String by) throws DukeException {
+    public Deadline(String name, boolean isDone, String by) throws DukeTaskException {
         super(name, isDone);
         if (by.equals("") || by.equals(" ")) {
-            throw new DukeException("time can't be empty");
+            throw new DukeTaskException("time can't be empty");
         }
         this.by = by;
+    }
+
+    public static Deadline load(String s) throws DukeException {
+        String by = s.substring(1, s.indexOf("|")).trim();
+        String name = s.substring(s.indexOf("|") + 1, s.lastIndexOf("|")).trim();
+        String isDone = s.substring(s.lastIndexOf("|") + 1).trim();
+        return new Deadline(name, Boolean.parseBoolean(isDone), by);
+    }
+
+    @Override
+    public String saveString() {
+        return "D " + by + "|" + super.saveString();
     }
 
     @Override
