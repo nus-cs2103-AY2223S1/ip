e0f79eaa75ea1fe654f03f92e4d3ed1e44977cc5
Prittam Ravi <prittam867@gmail.com>
2022-08-26 16:29:11 +0800

    Fix load data

--- src/main/java/Duke/Storage.java
+++ src/main/java/Duke/Storage.java
@@ -30,34 +30,29 @@ public class Storage {
 
     public TaskList load() throws IOException{
         TaskList taskList = new TaskList();
-        try {
-            File file = new File(this.pathFile);
-            Scanner sc = new Scanner(file);
-            while (sc.hasNext()) {
-                String cur = sc.nextLine();
-                String[] curTask = cur.split(" \\| ");
-                Task currentTask = null;
-                boolean isDone = curTask[1].equals("1");
-                if (curTask[0].equals("T")) {
-                    currentTask = new ToDo(curTask[2]);
-                } else if (curTask[0].equals("D")) {
-                    currentTask = new Deadline(curTask[2], curTask[3]);
-                } else if (curTask[0].equals("E")) {
-                    currentTask = new Event(curTask[2], curTask[3]);
-                } else {
-                }
-                if (isDone){
-                    currentTask.markAsDone();
-                }
-                taskList.load(currentTask);
+        File file = new File(this.pathFile);
+        file.getParentFile().mkdirs();
+        if (!file.exists()){
+            file.createNewFile();
+        }
+        Scanner sc = new Scanner(file);
+        while (sc.hasNext()) {
+            String cur = sc.nextLine();
+            String[] curTask = cur.split(" \\| ");
+            Task currentTask = null;
+            boolean isDone = curTask[1].equals("1");
+            if (curTask[0].equals("T")) {
+                currentTask = new ToDo(curTask[2]);
+            } else if (curTask[0].equals("D")) {
+                currentTask = new Deadline(curTask[2], curTask[3]);
+            } else if (curTask[0].equals("E")) {
+                currentTask = new Event(curTask[2], curTask[3]);
+            } else {
             }
-
-        } catch (FileNotFoundException e) {
-            if (new File("data").mkdir()) {
-                System.out.println("data folder does not exist, create now");
-            } else if (new File("data/duke.txt").createNewFile()){
-                System.out.println("duke.txt file not exist, create now");
+            if (isDone){
+                currentTask.markAsDone();
             }
+            taskList.load(currentTask);
         }
         return taskList;
     }
