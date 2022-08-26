9c91a8d35b17145011bb643a3e2f408fd8db126a
RachelChua <e0772465@u.nus.edu>
2022-08-27 00:41:38 +0800

    Fix merge conflict

--- src/test/java/duke/TodoTest.java
+++ src/test/java/duke/TodoTest.java
@@ -7,6 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TodoTest {
     @Test
     public void toStringTest() {
-        assertEquals("[T][] homework", new ToDos("homework",false).toString());
+        assertEquals("[T][] homework", new ToDo("homework",false).toString());
     }
 }
