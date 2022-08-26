d8129e200b6af723604fadd5a5007dc81381dd90
Jiacheng-y <e0773814@u.nus.edu>
2022-08-27 01:36:11 +0800

    adds date class for Level-8

---
+++ src/main/java/DateTime.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class DateTime {
+    private LocalDate date;
+
+    public DateTime(String dateTime) {
+
+        //format of date received needs to be of the form 2022-08-27
+        this.date = LocalDate.parse(dateTime);
+    }
+
+    public String getDate() {
+        return this.date.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
+    }
+}
