6179486e2f3054747e4bd1b717672bd9a6c6cc16
Wang Xinyi <e0817822@u.nus.edu>
2022-08-27 01:42:36 +0800

    Comply with a coding standard

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -10,10 +10,18 @@ public abstract class Task {
         this.isDone = false;
     }
 
+    /**
+     * Checks if a task is done, returns "X" if done and " " if undone.
+     *
+     * @return A String that corresponds to the status of the task.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Marks a task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
