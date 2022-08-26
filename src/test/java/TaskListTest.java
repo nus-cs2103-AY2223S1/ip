9ee51770cf646d74ce655abd166918ab9a06a6b3
xhphoong <xhphoong@gmail.com>
2022-08-26 11:32:20 +0800

    added A-JUnit

---
+++ src/test/java/TaskListTest.java
@@ -0,0 +1,18 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest() {
+        assertEquals(4,4);
+    }
+
+}
