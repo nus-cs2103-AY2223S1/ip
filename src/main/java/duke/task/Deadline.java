745e0685ac1c779baed3ca7846add76ea9526d6a
lzq <li.zhaoqi@dhs.sg>
2022-08-27 00:59:21 +0800

    Add javadoc for tasks

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -7,6 +7,10 @@ import duke.exceptions.EmptyTimeException;
 import duke.util.ParsedData;
 import duke.util.Parser;
 
+/**
+ * Task that potentiall have a time deadline
+ * Identified by /by
+ */
 public class Deadline extends Task {
     private static final String SPLIT = "/by ";
 
@@ -17,6 +21,13 @@ public class Deadline extends Task {
         this.deadline = deadline;
     }
 
+    /**
+     * Creates Deadline from ParsedData
+     * 
+     * @param data ParsedData containing information for both
+     * @return Deadline
+     * @throws DukeException Throws when data given is invalid/insufficient
+     */
     public static Deadline createDeadline(ParsedData data) throws DukeException {
         if (data.description.length() == 0)
             throw new EmptyDescriptionException("deadline");
@@ -27,6 +38,15 @@ public class Deadline extends Task {
         return new Deadline(data.description, data.additionalInfo);
     }
 
+    /**
+     * Creates Deadline given the 2 seperate information.
+     * Used when reading from file.
+     * 
+     * @param description Description of Task
+     * @param deadline    When/period where its due
+     * @return Deadline
+     * @throws CorruptedLineException Throws when there is missing fields
+     */
     public static Deadline createDeadline(String description, String deadline) throws CorruptedLineException {
         if (description.length() == 0 || deadline.length() == 0)
             throw new CorruptedLineException();
@@ -34,11 +54,20 @@ public class Deadline extends Task {
         return new Deadline(description, deadline);
     }
 
+    /**
+     * {@inheritDoc}
+     * Adds [D] Identifier for deadline
+     * 
+     * @return String
+     */
     @Override
     public String toString() {
         return String.format("[D]%s (by: %s)", super.toString(), deadline);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public ParsedData convertToParseData() {
         return new ParsedData(completed ? "Dc" : "Dx", description, deadline);
--- src/main/java/duke/task/Todo.java
+++ src/main/java/duke/task/Todo.java
@@ -4,11 +4,21 @@ import duke.exceptions.CorruptedLineException;
 import duke.exceptions.EmptyDescriptionException;
 import duke.util.ParsedData;
 
+/**
+ * Task that have no time tag.
+ */
 public class Todo extends Task {
     Todo(String description) {
         super(description);
     }
 
+    /**
+     * Factory method to create a Todo task from ParsedData
+     * 
+     * @param data ParsedData contains description of task
+     * @return Todo
+     * @throws EmptyDescriptionException Throws when the description is empty
+     */
     public static Todo createTodo(ParsedData data) throws EmptyDescriptionException {
         if (data.description.length() == 0) {
             throw new EmptyDescriptionException("todo");
@@ -16,6 +26,13 @@ public class Todo extends Task {
         return new Todo(data.description);
     }
 
+    /**
+     * Factory method to create a Todo task from string.
+     * 
+     * @param description
+     * @return Todo
+     * @throws CorruptedLineException Throws when the description is empty
+     */
     public static Todo createTodo(String description) throws CorruptedLineException {
         if (description.length() == 0)
             throw new CorruptedLineException();
@@ -23,11 +40,20 @@ public class Todo extends Task {
         return new Todo(description);
     }
 
+    /**
+     * {@inheritDoc}
+     * Adds [T] to identify as Todo task
+     * 
+     * @return String
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public ParsedData convertToParseData() {
         return new ParsedData(completed ? "Tc" : "Tx", description);
