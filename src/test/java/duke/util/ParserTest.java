10f00a705ac7442ff25b1d36d9a8299e54ceff7f
lzq <li.zhaoqi@dhs.sg>
2022-08-26 22:43:48 +0800

    Add partial testing for Parser

--- src/test/java/duke/util/ParserTest.java
+++ src/test/java/duke/util/ParserTest.java
@@ -2,8 +2,27 @@ package duke.util;
 
 import org.junit.jupiter.api.Test;
 
+import duke.exceptions.CorruptedLineException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ParserTest {
+    @Test
+    public void parseDataFromLine_Invalid1() {
+        try {
+            Parser.parseDataFromLine("txt");
+            fail();
+        } catch (CorruptedLineException e) {
+        }
+    }
 
+    @Test
+    public void parseDataFromLine_Invalid2() {
+        try {
+            Parser.parseDataFromLine("Tz <<<< nothing <<<< temperol");
+            fail();
+        } catch (CorruptedLineException e) {
+        }
+    }
 }
