dcf17e3156c263ecb4e71fbaef1f10339f758269
CedricChia123 <cedricchia01@gmail.com>
2022-08-26 17:45:04 +0800

    Add Junit test cases

---
+++ src/test/java/Qoobee/DeadlineTest.java
@@ -0,0 +1,20 @@
+package Qoobee;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+
+    @Test
+    public void toStringTest() {
+        assertEquals("[D][ ] hand in assignment (by: 15 09 2022 12:00)",
+                new Deadline("hand in assignment ", "15/9/2022 1200").toString());
+    }
+
+    @Test
+    public void dateTime_success() {
+        assertEquals("20 01 2000 14:20",
+                new Deadline("test", "20/1/2000 1420").getDateTime());
+    }
+
+}
---
+++ src/test/java/Qoobee/ToDoTest.java
@@ -0,0 +1,14 @@
+package Qoobee;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+
+    @Test
+    public void toStringTest() {
+        assertEquals("[T][ ] do CS2103T iP", new ToDo("do CS2103T iP").toString());
+    }
+
+
+}
