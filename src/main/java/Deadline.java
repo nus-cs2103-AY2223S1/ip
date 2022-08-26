5b3739d3feadd03608a93adb07fb8c6d5f7f8f68
mohamedsaf1 <mohdsaf1@hotmail.com>
2022-08-27 01:24:37 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
 public class Deadline extends Task {
     protected String by;
+    protected LocalDate date;
+
 
     public Deadline(String description, String by) {
         super(description);
@@ -11,5 +17,15 @@ public class Deadline extends Task {
         return "[D]" + super.toString() + "(by:" + by + ")";
     }
 
+    public void getDate() {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate deadline = LocalDate.parse(this.by, formatter);
+            this.date = deadline;
+            this.by = this.date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
 
