d681c2728c2d0349c429339f7b33936847c33723
chun <lamchunyu00@gmail.com>
2022-08-26 13:19:58 +0800

    Added JavaDocs for the different components

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,11 +7,18 @@ public class Duke {
     private TaskList tasks;
     private Parser parser;
 
+    /**
+     * Constructor for the Duke class.
+     * @param filePath - the name of the file to load from
+     */
     public Duke(String filePath) {
         tasks = new TaskList(Storage.load(filePath));
         parser = new Parser();
     }
 
+    /**
+     * Runs the main Duke program.
+     */
     public void run() {
         Scanner scanner = new Scanner(System.in);
         Ui.setup();
--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -3,7 +3,13 @@ package main.java.duke;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * Class for TaskList components of Duke.
+ */
 public class TaskList {
+    /**
+     * Class for a singular Task.
+     */
     class Task {
         protected String description;
         protected boolean isDone;
@@ -39,6 +45,9 @@ public class TaskList {
         }
     }
 
+    /**
+     * Class for a singular Deadline.
+     */
     class Deadline extends Task {
         String deadline;
         LocalDate date;
@@ -64,6 +73,9 @@ public class TaskList {
         }
     }
 
+    /**
+     * Class for a singular Event.
+     */
     class Event extends Task {
         String time;
 
@@ -85,14 +97,25 @@ public class TaskList {
 
     int pointer = 0;
     protected ArrayList<Task> list;
+    /**
+     * Constructor for TaskList.
+     */
     public TaskList() {
         this.list = new ArrayList<>(100);
     }
 
+    /**
+     * Constructor for TaskList.
+     * @param taskList - tasklist to copy data from
+     */
     public TaskList(TaskList taskList) {
         this.list = taskList.list;
     }
 
+    /**
+     * Adds a task to the list.
+     * @param description - description of the task
+     */
     public Task addTask(String description) {
         Task val = new Task(description);
         this.list.add(val);
@@ -101,6 +124,11 @@ public class TaskList {
         return val;
     }
 
+    /**
+     * Adds a deadline to the list.
+     * @param desc - description of the task
+     * @param deadline - the date of the deadline
+     */
     public Deadline addDeadline(String desc, String deadline) {
         Deadline val = new Deadline(desc, deadline);
         this.list.add(val);
@@ -109,6 +137,11 @@ public class TaskList {
         return val;
     }
 
+    /**
+     * Adds a deadline to the list using a date.
+     * @param desc - description of the task
+     * @param date - the date of the deadline in LocalDate format
+     */
     public Deadline addDeadline(String desc, LocalDate date) {
         Deadline val = new Deadline(desc, date);
         this.list.add(val);
@@ -116,10 +149,14 @@ public class TaskList {
         return val;
     }
 
+    /**
+     * Adds an event to the list.
+     * @param desc - description of the task
+     * @param time - the time of the event
+     */
     public Event addEvent(String desc, String time) {
         Event val = new Event(desc, time);
         this.list.add(val);
-//                this.list[pointer] = new Event(desc, time);
         pointer++;
         return val;
     }
@@ -136,14 +173,26 @@ public class TaskList {
         this.list.remove(index);
     }
 
+    /**
+     * Marks a task as done.
+     * @param index - index of the task to mark as done in the TaskList.
+     */
     public void mark(int index) {
         this.get(index).mark();
     }
 
+    /**
+     * Marks a task as undone.
+     * @param index - index of the task to mark as undone in the TaskList.
+     */
     public void unmark(int index) {
         this.get(index).unmark();
     }
 
+    /**
+     * Returns a string representation of the TaskList for saving into a file.
+     * @return - string representation of the TaskList for saving into a file.
+     */
     protected String saveData() {
         String result = "";
         for (Task task : this.list) {
@@ -152,6 +201,9 @@ public class TaskList {
         return result;
     }
 
+    /**
+     * Prints the TaskList in a readable format.
+     */
     protected void printData() {
         this.list.forEach(x -> System.out.println(String.format("%s. %s", this.list.indexOf(x) + 1, x)));
     }
