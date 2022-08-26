42eaf497b0475e0a59627ceb09673f4329013427
bryanljx1 <bryanljx1@gmail.com>
2022-08-27 00:32:04 +0800

    Remove symbol from error messages

--- src/main/java/exception/DukeException.java
+++ src/main/java/exception/DukeException.java
@@ -11,25 +11,25 @@ public class DukeException extends Exception {
         String errorMsg;
         switch(errorCode) {
             case UNKNOWN_CMD:
-                errorMsg = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+                errorMsg = "OOPS!!! I'm sorry, but I don't know what that means :-(";
                 break;
             case MISSING_TODO_ITEM:
-                errorMsg = "☹ OOPS!!! The description of a todo cannot be empty.";
+                errorMsg = "OOPS!!! The description of a todo cannot be empty.";
                 break;
             case UNKNOWN_TASK_ENCODING:
-                errorMsg = "☹ OOPS!!! Unable to parse one of the saved tasks. This task will be skipped.";
+                errorMsg = "OOPS!!! Unable to parse one of the saved tasks. This task will be skipped.";
             case INVALID_DEADLINE_DATETIME_FORMAT:
                 String dateFormat = "\t Please ensure date time is of the " +
                         "following format: yyyy-mm-dd hh:mm:ss";
-                errorMsg = "☹ OOPS!!! Unable to parse date time for deadline.\n" + dateFormat;
+                errorMsg = "OOPS!!! Unable to parse date time for deadline.\n" + dateFormat;
                 break;
             case INVALID_EVENT_DATETIME_FORMAT:
                 dateFormat = "\t Please ensure date time is of the " +
                         "following format: yyyy-mm-dd hh:mm:ss hh:mm:ss hh:mm:ss";
-                errorMsg = "☹ OOPS!!! Unable to parse date time for event.\n" + dateFormat;
+                errorMsg = "OOPS!!! Unable to parse date time for event.\n" + dateFormat;
                 break;
             default:
-                errorMsg = "☹ OOPS!!! Unknown duke error occurred. :-(";
+                errorMsg = "OOPS!!! Unknown duke error occurred. :-(";
         }
         return errorMsg;
     }
