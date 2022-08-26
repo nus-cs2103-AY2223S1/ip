64b993290bde60747c63fab2c004ac34394fa6f5
nickeltea <e0772462@u.nus.edu>
2022-08-26 11:55:14 +0800

    Revert "Revert "Add Increments as parallel branches""
    This reverts commit 9b5895f38c374daa39743bbdcc7e339449f0d709.

--- src/main/java/seedu/duke/Deadline.java
+++ src/main/java/seedu/duke/Deadline.java
@@ -1,8 +1,6 @@
 package seedu.duke;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class Deadline extends Task {
     protected String by;
--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -1,19 +1,15 @@
 package seedu.duke;
-import java.util.Scanner;
 
 public class Ui {
-    private Scanner scan = new Scanner(System.in);
     private Parser parser;
-    private TaskList tasklist;
 
     /**
      * A constructor that returns an instance of Ui.
      *
-     * @param tasklist
+     * @param tasklist The TaskList instance for this instance of Duke.
      */
     public Ui(TaskList tasklist) {
         this.parser = new Parser(tasklist);
-        this.tasklist = tasklist;
     }
 
     /**
@@ -48,6 +44,12 @@ public class Ui {
                         + "____________________________________________________________");
     }
 
+    /**
+     * Tells the user what Task was added and how many Tasks are in the list.
+     *
+     * @param added Description of the Task added.
+     * @param size The total number of tasks.
+     */
     public static void addText(String added, int size) {
         System.out.println(
                 "____________________________________________________________ \n"
@@ -57,6 +59,9 @@ public class Ui {
                         + "____________________________________________________________");
     }
 
+    /**
+     * Informs the user that the task they tried to access does not exist.
+     */
     public static void taskNotFoundText() {
         System.out.println(
                 "____________________________________________________________ \n"
@@ -64,6 +69,11 @@ public class Ui {
                         + "____________________________________________________________");
     }
 
+    /**
+     * Informs the user that the format of their command was incorrect.
+     *
+     * @param taskType The type of Task the user tried to create.
+     */
     public static void emptyDescription(String taskType) {
         if (taskType.equals("Event")) {
             System.out.println(
@@ -78,6 +88,9 @@ public class Ui {
         }
     }
 
+    /**
+     * Tells the user that Duke does not recognise the information in the save file.
+     */
     public static void unknownElement() {
         System.out.println(
                 "____________________________________________________________ \n"
@@ -85,6 +98,9 @@ public class Ui {
                         + "____________________________________________________________");
     }
 
+    /**
+     * Tells the user that Duke was unable to add their task to the list.
+     */
     public static void unableToAdd() {
         System.out.println(
                 "____________________________________________________________ \n"
@@ -92,6 +108,9 @@ public class Ui {
                         + "____________________________________________________________");
     }
 
+    /**
+     * Tells the user that their date format is incorrect.
+     */
     public static void wrongDateFormat() {
         System.out.println(
                 "____________________________________________________________ \n"
@@ -99,6 +118,9 @@ public class Ui {
                         + "____________________________________________________________");
     }
 
+    /**
+     * Tells the user that Dukes was unable to find or create a save file.
+     */
     public static void saveError() {
         System.out.println(
                 "____________________________________________________________ \n"
@@ -106,6 +128,9 @@ public class Ui {
                         + "____________________________________________________________");
     }
 
+    /**
+     * Tells the user that their input was not recognised as a command.
+     */
     public static void unknownCommand() {
         System.out.println(
                 "____________________________________________________________ \n"
@@ -113,6 +138,9 @@ public class Ui {
                         + "____________________________________________________________");
     }
 
+    /**
+     * Bids farewell to the user and ends the program.
+     */
     public static void bye() {
         System.out.println(
                 "____________________________________________________________ \n"
