fdb28bd9b0be3646afde021718ee979a9337f737
Zsigmond <e0725064@u.nus.edu>
2022-08-26 20:14:14 +0800

    refactor: ui class

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Ui {
+import java.io.PrintStream;
+
+public class Ui {
+    private final PrintStream OUTPUT = System.out;
+
+    public void printDivider() {
+        OUTPUT.println("------------------------------");
+    }
+
+    public void printWithDivider(String s) {
+        this.printDivider();
+        OUTPUT.printf("%s", s);
+        this.printDivider();
+    }
+
+    public void println(String s) {
+        OUTPUT.println(s);
+    }
+
+    public void showWelcome() {
+        this.printDivider();
+        OUTPUT.println("Hello! I'm Duke\\nWhat can I do for you?\"");
+        this.printDivider();
+    }
 }
