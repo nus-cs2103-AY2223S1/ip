b34fe71ecea375921c3a6b9fb0a221a37f73efea
ish1506 <ishiuanlee@gmail.com>
2022-08-26 16:30:59 +0800

    Extract Parser class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private final TaskList tasks;
-    private Ui ui;
-    //    private Storage storage;
+    private final Ui ui;
 
     public Duke() {
         ui = new Ui();
@@ -14,57 +13,58 @@ public class Duke {
         ui.printWelcome();
         Scanner scanner = new Scanner(System.in);
 
-        while (scanner.hasNextLine()) {
-            String command = scanner.nextLine();
-            if (command.equals("bye")) {
-                // Write tasks data to storage before terminating program
-                Storage.writeData(tasks.toString());
-                ui.printGoodbye();
-                return;
-            } else if (command.equals("list")) {
-                tasks.printList();
-                continue;
-            } else if (command.contains("unmark")) {
-                int index = Integer.parseInt(command.split(" ")[1]) - 1;
-                tasks.unmark(index);
-                ui.printUnmark(tasks.getTask(index));
-            } else if (command.contains("mark")) {
-                int index = Integer.parseInt(command.split(" ")[1]) - 1;
-                tasks.mark(index);
-                ui.printMark(tasks.getTask(index));
-            } else if (command.contains("delete")) {
-                int index = Integer.parseInt(command.split(" ")[1]) - 1;
-                Task temp = tasks.getTask(index);
-                tasks.remove(index);
-                ui.printDelete(temp);
-            } else {
-                try {
-                    if (command.contains("todo")) {
-                        String taskName = command.length() > 5 ? command.split("todo ")[1] : "";
-                        if (taskName.isBlank()) {
-                            throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
-                        }
-                        Task newTodoTask = new Todo(taskName);
-                        tasks.add(newTodoTask);
-                        ui.printAdd(newTodoTask);
-                    } else if (command.contains("deadline")) {
-                        String[] res = command.split("deadline ")[1].split("\\\\by ");
-                        Task newDeadlineTask = new Deadline(res[0], res[1]);
-                        tasks.add(newDeadlineTask);
-                        ui.printAdd(newDeadlineTask);
-                    } else if (command.contains("event")) {
-                        String[] res = command.split("event ")[1].split("\\\\at ");
-                        Task newEventTask = new Event(res[0], res[1]);
-                        tasks.add(newEventTask);
-                        ui.printAdd(newEventTask);
-                    } else {
-                        throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
-                    }
-                } catch (DukeException e) {
-                    System.out.println(e.getMessage());
-                } finally {
-                    continue;
+        while (true) {
+            String input = scanner.nextLine();
+
+            try {
+                Parser parser = new Parser(input);
+                parser.parse();
+                Command command = parser.getCommand();
+                int taskIndex = parser.getTaskIndex();
+                String[] args = parser.getArgs();
+
+                switch (command) {
+                case BYE:
+                    // Write tasks data to storage before terminating program
+                    Storage.writeData(tasks.toString());
+                    ui.printGoodbye();
+                    return;
+                case LIST:
+                    tasks.printList();
+                    break;
+                case MARK:
+                    tasks.mark(taskIndex);
+                    ui.printMark(tasks.getTask(taskIndex));
+                    break;
+                case UNMARK:
+                    tasks.unmark(taskIndex);
+                    ui.printUnmark(tasks.getTask(taskIndex));
+                    break;
+                case DELETE:
+                    Task temp = tasks.getTask(taskIndex);
+                    tasks.remove(taskIndex);
+                    ui.printDelete(temp);
+                    break;
+                case TODO:
+                    Task newTodoTask = new Todo(args[0]);
+                    tasks.add(newTodoTask);
+                    ui.printAdd(newTodoTask);
+                    break;
+                case DEADLINE:
+                    Task newDeadlineTask = new Deadline(args[0], args[1]);
+                    tasks.add(newDeadlineTask);
+                    ui.printAdd(newDeadlineTask);
+                    break;
+                case EVENT:
+                    Task newEventTask = new Event(args[0], args[1]);
+                    tasks.add(newEventTask);
+                    ui.printAdd(newEventTask);
+                    break;
+                default:
+                    throw new DukeException("Unrecognized command");
                 }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,104 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Deals with making sense of the user command.
+ */
+public class Parser {
+    private final String input;
+    private Command command;
+    private int taskIndex = -1;
+    private String[] args;
+
+    public Parser(String input) throws DukeException {
+        this.input = input;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public int getTaskIndex() {
+        return taskIndex;
+    }
+
+    public String[] getArgs() {
+        return args;
+    }
+
+    /**
+     * Parses the user input to get the command and arguments
+     */
+    public void parse() throws DukeException {
+        List<String> listOfInputs = Arrays.asList(input.split(" "));
+        if (listOfInputs.isEmpty()) {
+            throw new DukeException("Please enter a command!");
+        } else {
+            int numArgs = listOfInputs.size();
+            String firstArg = listOfInputs.get(0);
+
+            if (firstArg.equals("bye")) {
+                command = Command.BYE;
+            } else if (firstArg.equals("list")) {
+                command = Command.LIST;
+            } else if (firstArg.equals("mark")) {
+                if (numArgs != 2) {
+                    throw new DukeException("Input format: mark (task number)");
+                }
+                try {
+                    command = Command.MARK;
+                    taskIndex = Integer.parseInt(listOfInputs.get(1)) - 1;
+                } catch (NumberFormatException e) {
+                    throw new DukeException("Input format: mark (task number)");
+                }
+            } else if (firstArg.equals("unmark")) {
+                if (numArgs != 2) {
+                    throw new DukeException("Input format: unmark (task number)");
+                }
+                try {
+                    command = Command.UNMARK;
+                    taskIndex = Integer.parseInt(listOfInputs.get(1)) - 1;
+                } catch (NumberFormatException e) {
+                    throw new DukeException("Input format: unmark (task number)");
+                }
+            } else if (firstArg.equals("delete")) {
+                if (numArgs != 2) {
+                    throw new DukeException("Input format: delete (task number)");
+                }
+                try {
+                    command = Command.DELETE;
+                    taskIndex = Integer.parseInt(listOfInputs.get(1)) - 1;
+                } catch (NumberFormatException e) {
+                    throw new DukeException("Input format: delete (task number)");
+                }
+            } else if (firstArg.equals("todo")) {
+                if (numArgs == 1) {
+                    throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                }
+                command = Command.TODO;
+                String todoName = input.split("todo ")[1];
+                args = new String[]{todoName};
+            } else if (firstArg.equals("event")) {
+                if (numArgs < 4 || !listOfInputs.contains("\\at")) {
+                    throw new DukeException("Input format: event (event name) \\at (event date/time)");
+                }
+                command = Command.EVENT;
+                int index = input.indexOf("\\at ");
+                String eventName = input.substring(6, index);
+                String eventTime = input.substring(index + 4);
+                args = new String[]{eventName, eventTime};
+            } else if (firstArg.equals("deadline")) {
+                if (numArgs < 4 || !listOfInputs.contains("\\by")) {
+                    throw new DukeException("Input format: deadline (deadline name) \\by (YYYY-MM-DD)");
+                }
+                command = Command.DEADLINE;
+                int index = input.indexOf("\\by ");
+                String deadlineName = input.substring(9, index);
+                String deadlineTime = input.substring(index + 4);
+                args = new String[]{deadlineName, deadlineTime};
+            } else {
+                throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
+        }
+    }
+}
