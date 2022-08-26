3c47bfded3f37593dc6b450a90b1163a318c00c8
Clement Foo <e0726745@u.nus.edu>
2022-08-26 08:26:11 +0800

    Complete A-CodingStandard

--- src/main/java/util/Ui.java
+++ src/main/java/util/Ui.java
@@ -5,7 +5,7 @@ import tasks.Task;
 import java.util.List;
 
 /**
- * This class is used to format outputs.
+ * This class is used to format outputs to the command line.
  */
 public class Ui {
     /**
