074e8a6745282a0413cba949d2f74ba1111cdcd7
darrtzj <darrenteozj@u.nus.edu>
2022-08-27 00:11:04 +0800

    Add override tag to command classes

--- src/main/java/duke/command/AddCommand.java
+++ src/main/java/duke/command/AddCommand.java
@@ -8,9 +8,10 @@ import duke.Ui;
 
 public class AddCommand extends Command {
     private Task task;
-    public AddCommand (Task task) {
+    public AddCommand(Task task) {
         this.task = task;
     }
+    @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
         tasks.add(task);
         ui.addTaskMessage(task, tasks);
--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -1,2 +1,35 @@
-package duke.command;public class FindCommand {
+package duke.command;
+
+import duke.DukeException;
+import duke.Storage;
+import duke.TaskList;
+import duke.Ui;
+
+/**
+ * Represents find command
+ */
+public class FindCommand extends Command {
+
+    private String keyword;
+
+    /**
+     * Constructs find command
+     *
+     * @param keyword Keyword to find tasks
+     */
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    /**
+     * Finds tasks matching keyword
+     *
+     * @param tasks Task list
+     * @param ui User's Interface
+     * @param storage Storage
+     */
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        ui.findTask(tasks.find(keyword));
+    }
 }
