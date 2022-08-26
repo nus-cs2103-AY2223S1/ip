4fe78a282f67d3852f49c63d449bf2e73c36463e
lzq <li.zhaoqi@dhs.sg>
2022-08-26 22:39:20 +0800

    Add Test for ParsedData

--- src/test/java/duke/util/ParsedDataTest.java
+++ src/test/java/duke/util/ParsedDataTest.java
@@ -5,5 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParsedDataTest {
+    @Test
+    public void getSavedStringTest() {
+        ParsedData tmp;
+        tmp = new ParsedData("A", "b", "c");
+        assertEquals("A <<<< b <<<< c", tmp.getSavedString());
 
+        tmp = new ParsedData("Tx", "this is a deadline hw", "03-33-4222");
+        assertEquals("Tx <<<< this is a deadline hw <<<< 03-33-4222", tmp.getSavedString());
+
+        tmp = new ParsedData("Tx", "this is a deadline hw");
+        assertEquals("Tx <<<< this is a deadline hw <<<< ", tmp.getSavedString());
+
+        tmp = new ParsedData("Tx");
+        assertEquals("Tx <<<<  <<<< ", tmp.getSavedString());
+        tmp = new ParsedData();
+        assertEquals(" <<<<  <<<< ", tmp.getSavedString());
+    }
 }
