8c819e6f85e6ebdd994a8cef08a7e09f632b5341
chantellyu <e0758927@u.nus.edu>
2022-08-26 15:19:43 +0800

    Add Find

---
+++ src/main/java/commands/FindCommand.java
@@ -0,0 +1,39 @@
+package commands;
+
+import tasks.Deadlines;
+import tasks.Events;
+import tasks.Task;
+import tasks.TaskList;
+import tasks.Todos;
+
+import java.util.ArrayList;
+
+public class FindCommand extends Command {
+    String keyword;
+
+    /**
+     * Constructor for FindCommand.
+     *
+     * @param keyword Keyword that is to be found in all the tasks returned
+     */
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public void run(TaskList taskList) {
+        TaskList found = new TaskList();
+        for (int i = 0; i < taskList.getSize(); i++) {
+            Task temp = taskList.retrieveTask(i);
+            if (temp.getDescription().contains(keyword)) {
+                found.addTask(temp);
+            }
+        }
+        if (found.getSize() == 0) {
+            System.out.println("No matching tasks were found :(");
+        } else {
+            for (int i = 1; i <= found.getSize(); i++) {
+                System.out.println(i + ". " + found.retrieveTask(i - 1).toString());
+            }
+        }
+    }
+}
--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,21 +1,13 @@
 package duke;
 
-import commands.ByeCommand;
-import commands.Command;
-import commands.DeadlineCommand;
-import commands.DeleteCommand;
-import commands.EventCommand;
-import commands.ListCommand;
-import commands.MarkCommand;
-import commands.TodoCommand;
-import commands.UnMarkCommand;
+import commands.*;
 import dukeexceptions.DukeException;
 import dukeexceptions.NoDescriptionException;
 import dukeexceptions.NoSuchCommandException;
 
 public class Parser {
     private enum Commands {
-        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE
+        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, FIND
     }
 
     /**
@@ -44,32 +36,37 @@ public class Parser {
                 throw new NoDescriptionException("mark");
             case TODO:
                 throw new NoDescriptionException("todo");
+            case FIND:
+                throw new NoDescriptionException("find");
             default:
                 throw new NoSuchCommandException();
             }
         } else {
-                switch (Commands.valueOf(inputSplit[0].strip().toUpperCase())) {
-                    case UNMARK:
-                        int indUnmark = Integer.parseInt(inputSplit[1]) - 1;
-                        return new UnMarkCommand(indUnmark);
-                    case MARK:
-                        int indMark = Integer.parseInt(inputSplit[1]) - 1;
-                        return new MarkCommand(indMark);
-                    case DEADLINE:
-                        String[] descriptDate = inputSplit[1].split("/by", 2);
-                        return new DeadlineCommand(descriptDate[0], descriptDate[1]);
-                    case EVENT:
-                        String[] descriptTime = inputSplit[1].split("/at", 2);
-                        return new EventCommand(descriptTime[0], descriptTime[1]);
-                    case TODO:
-                        String des = inputSplit[1];
-                        return new TodoCommand(des);
-                    case DELETE:
-                        int del = Integer.parseInt(inputSplit[1]) - 1;
-                        return new DeleteCommand(del);
-                    default:
-                        throw new NoSuchCommandException();
-                }
+            switch (Commands.valueOf(inputSplit[0].strip().toUpperCase())) {
+            case UNMARK:
+                int indUnmark = Integer.parseInt(inputSplit[1]) - 1;
+                return new UnMarkCommand(indUnmark);
+            case MARK:
+                int indMark = Integer.parseInt(inputSplit[1]) - 1;
+                return new MarkCommand(indMark);
+            case DEADLINE:
+                String[] descriptDate = inputSplit[1].split("/by", 2);
+                return new DeadlineCommand(descriptDate[0], descriptDate[1]);
+            case EVENT:
+                String[] descriptTime = inputSplit[1].split("/at", 2);
+                return new EventCommand(descriptTime[0], descriptTime[1]);
+            case TODO:
+                String des = inputSplit[1];
+                return new TodoCommand(des);
+            case DELETE:
+                int del = Integer.parseInt(inputSplit[1]) - 1;
+                return new DeleteCommand(del);
+            case FIND:
+                String keyword = inputSplit[1];
+                return new FindCommand(keyword);
+            default:
+                throw new NoSuchCommandException();
+            }
             }
 
         }
