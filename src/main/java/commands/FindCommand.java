2a6f4cc33049ef8698007d230f557d0ec7a5123c
Marcus Pang <marcuspangyuyang@gmail.com>
2022-08-26 22:32:06 +0800

    Add missing javadocs

--- src/main/java/commands/FindCommand.java
+++ src/main/java/commands/FindCommand.java
@@ -7,15 +7,25 @@ import exceptions.DukeException;
 import exceptions.EmptyDescriptionException;
 
 /**
- * Creates a new event
+ * Finds all tasks that have a matching description field.
  */
 public class FindCommand extends Command {
     private final String[] inputStrings;
 
+    /**
+     * Constructs find command, which filters the current tasks with some query and prints them to the console.
+     *
+     * @param inputStrings The specified input strings.
+     */
     public FindCommand(String[] inputStrings) {
         this.inputStrings = inputStrings;
     }
 
+    /**
+     * Filters tasks and prints the tasks that match the query string.
+     * <p>
+     * {@inheritDoc}
+     */
     public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
         if (this.inputStrings.length == 1 || this.inputStrings[1].trim().isEmpty()) {
             throw new EmptyDescriptionException();
@@ -24,6 +34,9 @@ public class FindCommand extends Command {
         ui.showMatchingTasks(filteredTasks);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public boolean isExit() {
         return false;
     }
