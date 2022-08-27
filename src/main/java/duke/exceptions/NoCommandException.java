df12bfdc825b1be0ac09d1be6dfd926b1f0770da
John Benedict Yan <johnbenedictyan@gmail.com>
2022-08-27 10:37:46 +0800

    A-JUnit Add JUnit tests to test the behaviour of the code

---
+++ src/main/java/duke/exceptions/NoCommandException.java
@@ -0,0 +1,7 @@
+package duke.exceptions;
+
+public class NoCommandException extends DukeException {
+    public NoCommandException(String errorString) {
+        super(errorString);
+    }
+}
---
+++ src/test/java/duke/parser/ParserTest.java
@@ -0,0 +1,63 @@
+package duke.parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import duke.commands.BaseCommand;
+import duke.commands.ErrorCommand;
+import duke.exceptions.IncorrectArgumentException;
+import duke.exceptions.InvalidDateTimeException;
+import duke.exceptions.InvalidTaskSpecificationException;
+import duke.exceptions.MissingArgumentException;
+import duke.exceptions.NoCommandException;
+import duke.exceptions.UnknownCommandException;
+
+public class ParserTest {
+    private Parser parser;
+
+    @BeforeEach
+    public void setUp() {
+        parser = new Parser();
+    }
+
+    @Test
+    public void parse_emptyInput_returnsIncorrect() {
+        final String[] emptyInputs = { "", "  ", "\n  \n" };
+        final String resultMessage = String.format(
+                "Please enter a command");
+        parseAndAssertIncorrectWithMessage(resultMessage, emptyInputs);
+    }
+
+    @Test
+    public void parse_unknownCommandWord_returnsError() {
+        final String input = "unknown command";
+        parseAndAssertCommandType(
+                input, ErrorCommand.class);
+    }
+
+    private void parseAndAssertIncorrectWithMessage(
+            String feedbackMessage, String... inputs) {
+        for (String input : inputs) {
+            final ErrorCommand result = parseAndAssertCommandType(
+                    input, ErrorCommand.class);
+            assertEquals(result.getMessage(), feedbackMessage);
+        }
+    }
+
+    private <T extends BaseCommand> T parseAndAssertCommandType(
+            String input, Class<T> expectedCommandClass) {
+        BaseCommand result;
+        try {
+            result = parser.parse(input);
+        } catch (MissingArgumentException | InvalidDateTimeException | InvalidTaskSpecificationException
+                | IncorrectArgumentException | UnknownCommandException | NoCommandException e) {
+            result = new ErrorCommand(e.getMessage());
+        }
+        assertTrue(result.getClass().isAssignableFrom(expectedCommandClass));
+        return (T) result;
+    }
+
+}
