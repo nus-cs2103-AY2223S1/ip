b47d96a7412a60ae0cfabad6952c93d38c638aaf
Ng Xing Yu <ng.xingyu@u.nus.edu>
2022-08-28 07:44:17 +0800

    Add basic gui using javafx.
    Based on part 1 and 2 of the javafx guide.

---
+++ src/main/java/duke/Launcher.java
@@ -0,0 +1,12 @@
+package duke;
+
+import javafx.application.Application;
+
+/**
+ * Launchr for the Duke chatbot
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
+}
