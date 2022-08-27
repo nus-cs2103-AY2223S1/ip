7da7cc4dec7edef8e3014a0fcf7cfae28643d384
turretDive <apangjun@gmail.com>
2022-08-27 22:04:18 +0800

    Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,23 @@
+import java.io.IOException;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 
 public class Duke {
     private static Scanner input = new Scanner(System.in);  // Create a Scanner object
     private static ArrayList<Task> list = new ArrayList<>(); // list of user input
+    private static ArrayList<String> saveList = new ArrayList<>();
     private static int size = 0;
 
-    /**
-     * Parses an input sentence into different words/arguments separated by space.
-     *
-     * @param input Input of the user to be parsed.
-     * @return String array of words/arguments.
-     */
-    public static String[] parse(String input) {
-        return input.split(" ");
-    }
 
     /**
      * Outputs the goodbye messages.
      */
     public static void bye() {
+        save(saveList);
         System.out.println("Bye. Hope to see you again soon!");
     }
 
@@ -156,6 +153,60 @@ public class Duke {
         }
     }
 
+    public static void add(String in) throws DukeException {
+        if (!withinScope(in)) {
+            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-( ");
+        } else if (in.equals("list")) {
+            saveList.add(in);
+            list();
+        } else if (in.startsWith("delete")) {
+            saveList.add(in);
+            delete(in);
+        } else if (in.startsWith("unmark")) {
+            saveList.add(in);
+            unmark(in);
+        } else if (in.startsWith("mark")) {
+            saveList.add(in);
+            mark(in);
+        } else if (in.startsWith("deadline")) {
+            saveList.add(in);
+            deadline(in);
+        } else if (in.startsWith("event")) {
+            saveList.add(in);
+            event(in);
+        } else {
+            saveList.add(in);
+            todo(in);
+        }
+    }
+
+    public static void addFileContents(String filePath) throws FileNotFoundException {
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            String in = s.nextLine();
+            try {
+                add(in);
+            } catch (DukeException e) {
+                System.out.println(e.toString());
+            }
+        }
+        s.close();
+    }
+
+    public static void save(ArrayList<String> list) {
+        try {
+            new File(System.getProperty("user.dir") + "/data/duke.txt").getParentFile().mkdirs();
+            FileWriter fw = new FileWriter(System.getProperty("user.dir") + "/data/duke.txt");
+            for (String tasks : list) {
+                fw.write(String.format("%s%n", tasks));
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Oops: " + e.getMessage());
+        }
+    }
+
 
 
     public static void main(String[] args) {
@@ -166,6 +217,13 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What may I do for you?");
+        try {
+            addFileContents(System.getProperty("user.dir") + "/data/duke.txt");
+            System.out.println("I have added tasks from your previous session to the current list");
+            list();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found, proceed with empty list");
+        }
 
         while (input.hasNextLine()) {
             String in = input.nextLine();  // Read user input
@@ -173,22 +231,8 @@ public class Duke {
                 if (in.equals("bye")) {
                     bye();
                     break;
-                } else if (!withinScope(in)) {
-                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-( ");
-                } else if (in.equals("list")) {
-                    list();
-                } else if (in.startsWith("delete")) {
-                    delete(in);
-                } else if (in.startsWith("unmark")) {
-                    unmark(in);
-                } else if (in.startsWith("mark")) {
-                    mark(in);
-                } else if (in.startsWith("deadline")) {
-                    deadline(in);
-                } else if (in.startsWith("event")) {
-                    event(in);
                 } else {
-                    todo(in);
+                    add(in);
                 }
             } catch (DukeException e) {
                 System.out.println(e.toString());
