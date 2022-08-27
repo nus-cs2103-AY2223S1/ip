c537362ba942132382e19fbb68cf6c55769222f2
Meng Qiaoran <e0492494@u.nus.edu>
2022-08-27 23:24:29 +0800

    Style Parser.java.

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -6,8 +6,6 @@ import duke.task.ToDo;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -27,23 +25,24 @@ public class Parser {
     public LocalDate parseDate(String str) throws DukeException {
         // parse string format date to LocalDate object -> to String format yyyy-MM-dd, exception
         String time = str.trim();
-       try {
-           if (time.matches("([0-9]{2})/([0-9]{2})/([0-9]{4})")) {
-               return LocalDate.parse(time, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-           } else if (time.matches("([0-9]{4})/([0-9]{2})/([0-9]{2})")) {
-               return LocalDate.parse(time, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-           } else if (time.matches("([0-9]{2})-([0-9]{2})-([0-9]{4})")) {
-               return LocalDate.parse(time, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-           } else if (time.matches("([0-9]{4})-([0-9]{2})-([0-9]{2})")) {
-               return LocalDate.parse(time, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-           } else if (time.matches("([0-9]{4}) ([0-9]{2}) ([0-9]{2})")) {
-               return LocalDate.parse(time, DateTimeFormatter.ofPattern("yyyy MM dd"));
-           } else if (time.matches("([0-9]{2}) ([0-9]{2}) ([0-9]{4})")) {
-               return LocalDate.parse(time, DateTimeFormatter.ofPattern("dd MM yyyy"));
-           } else {
-               throw new DukeException("☹ OOPS!!! This is not a proper time format, please refer to command format information.");
-           }
-        } catch(Exception e) {
+        try {
+            if (time.matches("([0-9]{2})/([0-9]{2})/([0-9]{4})")) {
+                return LocalDate.parse(time, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            } else if (time.matches("([0-9]{4})/([0-9]{2})/([0-9]{2})")) {
+                return LocalDate.parse(time, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+            } else if (time.matches("([0-9]{2})-([0-9]{2})-([0-9]{4})")) {
+                return LocalDate.parse(time, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            } else if (time.matches("([0-9]{4})-([0-9]{2})-([0-9]{2})")) {
+                return LocalDate.parse(time, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            } else if (time.matches("([0-9]{4}) ([0-9]{2}) ([0-9]{2})")) {
+                return LocalDate.parse(time, DateTimeFormatter.ofPattern("yyyy MM dd"));
+            } else if (time.matches("([0-9]{2}) ([0-9]{2}) ([0-9]{4})")) {
+                return LocalDate.parse(time, DateTimeFormatter.ofPattern("dd MM yyyy"));
+            } else {
+                throw new DukeException("☹ OOPS!!! This is not a proper time format, "
+                        + "please refer to command format information.");
+            }
+        } catch (Exception e) {
            throw new DukeException(e.getMessage());
         }
     }
@@ -58,9 +57,9 @@ public class Parser {
      * @throws DukeException If the command is regarded invalid/incomplete by duke chatbot.
      */
     public String parseCommand(String line, TaskList taskList) throws DukeException {
-        String [] taskString = line.split(" ", 2);
+        String[] taskString = line.split(" ", 2);
         String taskType = taskString[0];
-        if (taskString.length <= 1 && !taskType.equals("list") && !taskType.equals("bye") && !taskType.equals("help")) {
+        if (taskString.length == 1 && !taskType.equals("list") && !taskType.equals("bye") && !taskType.equals("help")) {
             throw new DukeException ("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         } else if (taskType.equals("todo")) {
             String description = taskString[1].trim();
@@ -74,7 +73,7 @@ public class Parser {
                 throw new DukeException("☹ OOPS!!! The command not properly formatted.\n" +
                         "Please follow the format: Deadline {description} /by {date}.");
             }
-            String [] descriptions = taskString[1].split("/by");
+            String[] descriptions = taskString[1].split("/by");
             String description = descriptions[0].trim();
             LocalDate end = parseDate(descriptions[1]);
             if (description.equals("")) { // task description empty
@@ -84,15 +83,15 @@ public class Parser {
             return String.format("deadline %d", taskList.getSize());
         } else if (taskType.equals("event")) {
             if (!taskString[1].contains("/at")) { // command not properly formatted
-                throw new DukeException("☹ OOPS!!! The command not properly formatted.\n" +
-                        "Please follow the format: Event {description} /at {date} to {date}.");
+                throw new DukeException("☹ OOPS!!! The command not properly formatted.\n"
+                        + "Please follow the format: Event {description} /at {date} to {date}.");
             } else if (!taskString[1].contains(" to ")) {
-                throw new DukeException("☹ OOPS!!! The command not properly formatted.\n" +
-                        "Please follow the format: Event {description} /at {date} to {date}.");
+                throw new DukeException("☹ OOPS!!! The command not properly formatted.\n"
+                        + "Please follow the format: Event {description} /at {date} to {date}.");
             }
-            String [] descriptions = taskString[1].split("/at");
+            String[] descriptions = taskString[1].split("/at");
             String description = descriptions[0].trim();
-            String [] dates = descriptions[1].trim().split("to");
+            String[] dates = descriptions[1].trim().split("to");
             LocalDate start = parseDate(dates[0]);
             LocalDate end = parseDate(dates[1]);
             if (descriptions[0].trim().equals("")) { // task description empty
@@ -105,14 +104,15 @@ public class Parser {
         } else if (taskType.equals("list")) {
             if (taskString.length == 1 || taskString[1].trim().equals("")) { //list command
                 return "list all";
-            } else { //list date
+            } else { // list date
                 LocalDate date = parseDate(taskString[1].trim());
                 return String.format("list %s", date);
             }
         } else if (taskType.equals("mark") || taskType.equals("unmark") || taskType.equals("delete")) {
             String taskIndex = taskString[1].trim();
             if (taskIndex.equals("") || !taskIndex.matches("[0-9]+")) { // task description empty or not numeric
-                throw new DukeException(String.format("☹ OOPS!!! The task index of a %s command cannot be empty.", taskType));
+                throw new DukeException(String.format("☹ OOPS!!! The task index of a %s command cannot be empty.",
+                        taskType));
             } else if (Integer.parseInt(taskIndex) > taskList.getSize() || Integer.parseInt(taskIndex) < 1) {
                 throw new DukeException("☹ OOPS!!! The task index exceeds task list size limit.");
             }
