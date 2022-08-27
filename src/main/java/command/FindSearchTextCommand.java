d3659f7495bd7c4dc89bb907a4e534d3247e3289
bryanljx1 <bryanljx1@gmail.com>
2022-08-27 16:27:58 +0800

    Add javadocs for FindSearchTextCommand

--- src/main/java/command/FindSearchTextCommand.java
+++ src/main/java/command/FindSearchTextCommand.java
@@ -6,13 +6,26 @@ import util.Ui;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a task to be executed that searches and returns a task in the internal
+ * duke list based on search text
+ *
+ * @author Bryan Lim Jing Xiang
+ */
 public class FindSearchTextCommand extends Command {
     private final String searchText;
 
+    /**
+     * @param searchText Searches duke list for any tasks that contains the
+     *                   search text
+     */
     public FindSearchTextCommand(String searchText) {
         this.searchText = searchText;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void execute(TaskList list, Storage storage) {
         ArrayList<String> matchedTasks = list.find(searchText);
