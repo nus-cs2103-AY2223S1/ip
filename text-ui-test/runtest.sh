f50bee4b2226a07206a9cac9a3c5935c7184b5fc
arnav-ag <arnav.ctf@gmail.com>
2022-08-27 21:19:25 +0800

    Change tests to account for packages

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,15 +12,21 @@ then
     rm ACTUAL.TXT
 fi
 
+# delete data from previous run
+if [ -e "./data" ]
+then
+    rm -r ./data
+fi
+
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/duke/*.java || ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/duke/*/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin duke/Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
