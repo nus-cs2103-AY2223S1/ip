d888625bdc74dc7763e6b760658ff163f3566766
chantellyu <e0758927@u.nus.edu>
2022-08-27 15:05:14 +0800

    Add JavaDoc

--- src/main/java/commands/DeadlineCommand.java
+++ src/main/java/commands/DeadlineCommand.java
@@ -3,16 +3,30 @@ package commands;
 import duke.Ui;
 import tasks.*;
 
+/**
+ * DeadlineCommand creates and adds a new deadline to the task list.
+ */
 public class DeadlineCommand extends Command {
     String descript;
     String doBy;
 
+    /**
+     * Constructor for DeadlineCommand
+     *
+     * @param descript Description of deadline task.
+     * @param doBy End date and time of deadline task.
+     */
     public DeadlineCommand(String descript, String doBy) {
         this.descript = descript;
         this.doBy = doBy;
     }
 
     @Override
+    /**
+     * Executes DeadlineCommand by creating and adding new Deadlines object into task list.
+     *
+     * @param taskList List of tasks to add deadline into.
+     */
     public void run(TaskList taskList) {
         Task toAdd = new Deadlines(this.descript, this.doBy);
         taskList.addTask(toAdd);
