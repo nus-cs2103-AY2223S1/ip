e0cb48cef79e1f683f3de18585051e3b0cbc5e13
Mohamed Hamas <e0726186@u.nus.edu>
2022-08-27 23:42:17 +0800

    Javadoc for deadline

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -5,6 +5,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * A Task class to indicate a deadline hence,
+ * it requires a description of task
+ * and by timing to indicate the deadline.
+ */
 
 
 public class Deadline extends Task {
@@ -15,7 +20,13 @@ public class Deadline extends Task {
     }
 
 
-
+    /**
+     * Returns the string representation of a deadline
+     * task with the time it is to be done by.
+     *
+     * @return String representation
+     * @throws DateTimeException if format specified for DateTime is wrong
+     */
     @Override
     public String toString() throws DateTimeException {
         try {
