71e98551b647ca25b26250e7c16176c644fab130
KSHan29 <seow.han@outlook.com>
2022-08-27 22:17:04 +0800

    Add JavaDoc for classes and methods

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,5 +1,8 @@
 package duke;
 
+/**
+ * Duke program for keeping track of Tasks.
+ */
 public class Duke {
     protected static boolean terminate = false;
     private Ui ui;
@@ -7,6 +10,11 @@ public class Duke {
     private Storage storage;
     private TaskList tasks;
 
+    /**
+     * Duke constructor.
+     *
+     * @param filePath filePath of file to store Task information.
+     */
     public Duke(String filePath) {
         storage = new Storage(filePath, "data/temp.txt");
         ui = new Ui();
@@ -14,11 +22,19 @@ public class Duke {
         parser = new Parser(tasks, ui);
     }
 
+    /**
+     * Starts Duke program.
+     */
     public void run() {
         ui.greet();
         parser.takeUserInput();
     }
 
+    /**
+     * Initialises files and calls method to start Duke program.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         Duke duke = new Duke("data/duke.txt");
         duke.run();
--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,6 +1,14 @@
 package duke;
 
+/**
+ * Handles exceptions in Duke program relating to invalid arguments.
+ */
 public class DukeException extends IllegalArgumentException {
+    /**
+     * DukeException constructor.
+     *
+     * @param message Message to be displayed.
+     */
     public DukeException(String message) {
         super(message);
     }
