1fa5d7db6b4c0f95848fb422f2230bd3fec1b48d
weesiau <e0725311@u.nus.edu>
2022-08-27 13:33:54 +0800

    Add JavaDocs to public methods

--- src/main/java/duke/CommandNotFoundException.java
+++ src/main/java/duke/CommandNotFoundException.java
@@ -1,7 +1,14 @@
 package duke;
 
+/**
+ * Thrown when a given user input for command is not found in the Keyword enum.
+ */
 public class CommandNotFoundException extends DukeException {
 
+    /**
+     * Constructs a new CommandNotFoundException with the command as the error message.
+     * @param errorMessage
+     */
     public CommandNotFoundException(String errorMessage) {
         super(errorMessage);
     }
--- src/main/java/duke/EmptyCommandException.java
+++ src/main/java/duke/EmptyCommandException.java
@@ -1,7 +1,14 @@
 package duke;
 
+/**
+ * Thrown when a command lacks a required argument.
+ */
 public class EmptyCommandException extends DukeException {
 
+    /**
+     * Constructs an EmptyCommandException with the command keyword as error message.
+     * @param errorMessage
+     */
     public EmptyCommandException(String errorMessage) {
         super(errorMessage);
     }
