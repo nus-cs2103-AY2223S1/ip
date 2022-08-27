5d8de6ec6a8d02b10b5cf64d21d7631c3c7f5613
RussellDash332 <russellsaerang@gmail.com>
2022-08-28 05:38:03 +0800

    Move TaskReader fully to Storage

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -25,54 +25,69 @@ public class Storage {
      * for the parser to handle afterwards.
      *
      * @return List of array of strings from the lines in the file path specified
-     * @throws IOException If there is an I/O issue, e.g. file not found
+     * @throws StashyException If there is an (I/O) issue, e.g. file not found
      */
-    public ArrayList<String[]> load() throws IOException {
-        File f = new File(this.filePath);
-        Scanner s = new Scanner(f);
-        ArrayList<String[]> loadResult = new ArrayList<String[]>();
-        while (s.hasNext()) {
-            String[] lineSplit = s.nextLine().split("\\|");
-            loadResult.add(lineSplit);
+    public ArrayList<String[]> load() throws StashyException {
+        try {
+            File f = new File(this.filePath);
+            Scanner s = new Scanner(f);
+            ArrayList<String[]> loadResult = new ArrayList<String[]>();
+            while (s.hasNext()) {
+                String[] lineSplit = s.nextLine().split("\\|");
+                loadResult.add(lineSplit);
+            }
+            return loadResult;
+        } catch (Exception e) {
+            throw new StashyException(e.getMessage());
         }
-        return loadResult;
     }
 
     /**
      * Creates the file if one doesn't exist.
-     * The mkdirs() method can be used with a different
+     * No effect if file exists in the first place.
+     * <p>The mkdirs() method can be used with a different
      * context since it doesn't create a file but a
-     * directory instead.
+     * directory instead.</p>
      *
-     * @throws IOException If there is an I/O issue
+     * @throws StashyException If any (I/O) issue happens
      */
-    public void create() throws IOException {
-        String[] pathList = this.filePath.split("/");
-        String currentPath = "";
-        for (int i = 0; i < pathList.length - 1; i++) {
-            currentPath += pathList[i];
-            File directory = new File(currentPath);
-            if (!directory.exists()) {
-                directory.mkdir();
+    public void create() throws StashyException {
+        try {
+            String[] pathList = this.filePath.split("/");
+            String currentPath = "";
+            for (int i = 0; i < pathList.length - 1; i++) {
+                currentPath += pathList[i];
+                File directory = new File(currentPath);
+                if (!directory.exists()) {
+                    directory.mkdir();
+                }
+                currentPath += "/";
             }
-            currentPath += "/";
+            File f = new File(this.filePath);
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+        } catch (Exception e) {
+            throw new StashyException(e.getMessage());
         }
-        File newFile = new File(this.filePath);
-        newFile.createNewFile();
     }
 
     /**
      * Writes the current task list into the storage again.
      *
      * @param taskList The list of tasks
-     * @throws IOException If any I/O issue happens
+     * @throws StashyException If any (I/O) issue happens
      */
     public void writeTaskListToFile(ArrayList<Task> taskList)
-            throws IOException {
-        FileWriter fw = new FileWriter(filePath);
-        for (Task task : taskList) {
-            fw.write(task + System.lineSeparator());
+            throws StashyException {
+        try {
+            FileWriter fw = new FileWriter(filePath);
+            for (Task task : taskList) {
+                fw.write(task + System.lineSeparator());
+            }
+            fw.close();
+        } catch (Exception e) {
+            throw new StashyException(e.getMessage());
         }
-        fw.close();
     }
 }
