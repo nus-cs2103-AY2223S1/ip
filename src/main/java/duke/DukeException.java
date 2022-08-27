01fb25fb7d033b569d48c0051e24018258b91435
Meng Qiaoran <e0492494@u.nus.edu>
2022-08-27 23:18:46 +0800

    Style DukeException.java.

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,9 +1,10 @@
 package duke;
+
 /**
  * Represents an exception thrown when an error occurs during duke chatbot execution.
  */
-public class DukeException extends Exception{
-    private String msg;
+public class DukeException extends Exception {
+
     public DukeException(String msg) {
         super(msg);
     }
