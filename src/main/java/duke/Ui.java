e833fccda478acff2d6c128921f93948d964b81b
DevanshShah1309 <devanshshah1309@gmail.com>
2022-08-27 11:44:38 +0800

    Follow Coding Standard

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,8 +1,7 @@
 package duke;
 
 public class Ui {
-    private String LINE_BREAK = "\t-------------------------------";
-    private String FILE_NAME;
+    private final String FILE_NAME;
 
     private static final String WELCOME_MESSAGE = "\t-------------------------------\n"
             + "\tHello, I'm Duke!\n" + "\tWhat can I do for you?\n" + "\t-------------------------------";
@@ -15,29 +14,37 @@ public class Ui {
     }
 
     public void printLineBreak() {
+        String LINE_BREAK = "\t-------------------------------";
         System.out.println(LINE_BREAK);
     }
     public void printCreateNewFile() {
         System.out.println(String.format("\tCreated new file %s to store tasks!", FILE_NAME));
     }
+
     public void printLoadTasksFromFile() {
         System.out.println(String.format("\tLoaded tasks from %s", FILE_NAME));
     }
+
     public void printErrorCreatingFile() {
         System.out.println(String.format("\tError opening/creating %s!!!", FILE_NAME));
     }
+
     public void printErrorDatetimeFormat() {
         System.out.println("\tPlease ensure your datetime format is in YYYY-MM-DD HH:MM");
     }
+
     public void printErrorTaskNumber() {
         System.out.println("\tPlease make sure you enter a task number correctly!");
     }
+
     public void printWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
     }
+
     public void printByeMessage() {
         System.out.println(BYE_MESSAGE);
     }
+
     public void printCommandNotRecognized() {
         System.out.println("\tOops! I've no idea what you're talking about!");
     }
