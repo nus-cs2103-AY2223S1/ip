8dad459c33bd179e1d129e5a078d2b8c95ae5f54
YZTangent <yuanzheng.tan.tyz@gmail.com>
2022-08-27 10:36:00 +0800

    Added A-CodingStandard.

--- src/main/java/Dave2.java
+++ src/main/java/Dave2.java
@@ -15,7 +15,7 @@ public class Dave2 {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    private static boolean running = true;
+    private static boolean isRunning = true;
 
     private static final String line = "____________________________________________________________\n";
 
@@ -30,33 +30,33 @@ public class Dave2 {
     public static void commandLogic(String command, String args) {
         try {
             switch (command) {
-                case "bye":
-                    print("Bye. Hope to see you again soon!");
-                    running = false;
-                    break;
-                case "list":
-                    print(tasks);
-                    break;
-                case "mark":
-                    print(new MarkDoneCommand(Dave2.tasks, args).execute());
-                    break;
-                case "unmark":
-                    print(new UnmarkDoneCommand(Dave2.tasks, args).execute());
-                    break;
-                case "todo":
-                    print(new AddTodoCommand(Dave2.tasks, args).execute());
-                    break;
-                case "deadline":
-                    print(new AddDeadlineCommand(Dave2.tasks, args).execute());
-                    break;
-                case "event":
-                    print(new AddEventCommand(Dave2.tasks, args).execute());
-                    break;
-                case "remove":
-                    print(new RemoveTaskCommand(Dave2.tasks, args).execute());
-                    break;
-                default:
-                    throw new DaveException("(｡╯︵╰｡) OOPS!!! I'm sowwy, but I don't know what that means ｡･ﾟﾟ*(>д<)*ﾟﾟ･｡");
+            case "bye":
+                print("Bye. Hope to see you again soon!");
+                isRunning = false;
+                break;
+            case "list":
+                print(tasks);
+                break;
+            case "mark":
+                print(new MarkDoneCommand(Dave2.tasks, args).execute());
+                break;
+            case "unmark":
+                print(new UnmarkDoneCommand(Dave2.tasks, args).execute());
+                break;
+            case "todo":
+                print(new AddTodoCommand(Dave2.tasks, args).execute());
+                break;
+            case "deadline":
+                print(new AddDeadlineCommand(Dave2.tasks, args).execute());
+                break;
+            case "event":
+                print(new AddEventCommand(Dave2.tasks, args).execute());
+                break;
+            case "remove":
+                print(new RemoveTaskCommand(Dave2.tasks, args).execute());
+                break;
+            default:
+                throw new DaveException("(｡╯︵╰｡) OOPS!!! I'm sowwy, but I don't know what that means ｡･ﾟﾟ*(>д<)*ﾟﾟ･｡");
             }
         } catch (DaveException de) {
             print(de);
@@ -76,7 +76,7 @@ public class Dave2 {
                     + "|____/ \\__,_|__/ \\___|    /_____|\n";
             System.out.println(line + "Hello, I'm\n" + logo + "\nHow can I help ùwú?\n" + line);
 
-            while (running) {
+            while (isRunning) {
                 Pair<String, String> inputData = Parser.parseInput(scanner.nextLine());
                 commandLogic(inputData.getHead(), inputData.getTail());
             }
