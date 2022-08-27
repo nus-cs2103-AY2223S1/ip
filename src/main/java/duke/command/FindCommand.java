d3203e8f1f35c5418779bb0bf2c570ed43a10f43
Jacob Kwan <77665182+jacobkwan@users.noreply.github.com>
2022-08-28 06:39:32 +0800

    add javadocs for FindCommand

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -4,13 +4,28 @@ import duke.Storage;
 import duke.TaskList;
 import duke.Ui;
 
+/*
+ * Encapsulates a command to find tasks from the list.
+ */
 public class FindCommand extends Command {
     private String keyword;
 
+    /**
+     * Creates a FindCommand.
+     * 
+     * @param keyword Phrase used to search for matching tasks.
+     */
     public FindCommand(String keyword) {
         this.keyword = keyword;
     }
 
+    /**
+     * Executes the FindCommand to find tasks from the list.
+     * 
+     * @param tasks TaskList to search from.
+     * @param ui Ui that displays success or error to user.
+     * @param storage Persistent storage of task list.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         String result = tasks.find(keyword);
