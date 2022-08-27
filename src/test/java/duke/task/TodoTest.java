7463365ff08dce6ecbfbf5eb6c789345298f462a
Huang Yuchen <hyuchen2607@gmail.com>
2022-08-27 19:17:52 +0800

    Fix JUnit test code format

--- src/test/java/duke/task/TodoTest.java
+++ src/test/java/duke/task/TodoTest.java
@@ -6,21 +6,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoTest {
     @Test
-    public void inputTest1(){
+    public void inputTest1() {
         String expected = "T | 0 | read book\n";
         String actual = new Todo("read book").formatTask();
         assertEquals(expected, actual);
     }
 
     @Test
-    public void inputTest2(){
+    public void inputTest2() {
         String expected = "T | 0 | return book\n";
         String actual = new Todo("return book").formatTask();
         assertEquals(expected, actual);
     }
 
     @Test
-    public void stringOutputTest(){
+    public void stringOutputTest() {
         String expected = "[T][ ] read book";
         String actual = new Todo("read book").toString();
         assertEquals(expected, actual);
--- src/test/java/duke/util/TaskListTest.java
+++ src/test/java/duke/util/TaskListTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskListTest {
     @Test
-    public void taskStatusOutput1(){
+    public void taskStatusOutput1() {
         String expected = "[T][ ] read book";
         Todo todo = new Todo("read book");
         TaskList tl = new TaskList();
@@ -19,7 +19,7 @@ public class TaskListTest {
     }
 
     @Test
-    public void taskStatusOutput2(){
+    public void taskStatusOutput2() {
         String expected = "[E][ ] project meeting (at: Aug 6th 2-4pm)";
         Event event = new Event("project meeting", "Aug 6th 2-4pm");
         TaskList tl = new TaskList();
@@ -29,7 +29,7 @@ public class TaskListTest {
     }
 
     @Test
-    public void taskStatusOutput3(){
+    public void taskStatusOutput3() {
         String expected = "[D][ ] return book (by: Aug 6th 2-4pm)";
         Deadline deadline = new Deadline("return book", "Aug 6th 2-4pm");
         TaskList tl = new TaskList();
