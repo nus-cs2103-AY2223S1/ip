c83f943fe2ecc51d3b2553eaf71f49b776ec735f
Linus <53803800+zupey@users.noreply.github.com>
2022-08-27 16:43:34 +0800

    style: edit classes in the exceptions package to meet coding standards

--- src/main/java/duke/exceptions/DukeException.java
+++ src/main/java/duke/exceptions/DukeException.java
@@ -1,5 +1,8 @@
 package duke.exceptions;
 
+/**
+ * Describes how an exception in the Duke program happens.
+ */
 public class DukeException extends Exception {
     public DukeException(String errorMessage) {
         super(errorMessage);
