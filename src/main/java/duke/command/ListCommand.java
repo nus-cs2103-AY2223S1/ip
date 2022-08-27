54cca4de2f890ede27ebcd59316aedbedb8e9769
WR3nd3 <wongrende@gmail.com>
2022-08-27 22:35:13 +0800

    Add Find functionality
    Requested to add find functionality as a command to print list of
    tasks with descriptions matching an entered string.
    
    FindCommand class created and supporting methods added to Task,
    TaskList and Ui classes.
    
    Let's improve the javadoc as well.

--- src/main/java/duke/command/ListCommand.java
+++ src/main/java/duke/command/ListCommand.java
@@ -5,7 +5,7 @@ import duke.storage.ListLoader;
 import duke.ui.Ui;
 
 /**
- * Represents command to duke.Duke to print list.
+ * Represents command to Duke to print list.
  *
  * @author WR3nd3
  */
--- src/main/java/duke/command/UnmarkCommand.java
+++ src/main/java/duke/command/UnmarkCommand.java
@@ -7,16 +7,16 @@ import duke.task.Task;
 import duke.ui.Ui;
 
 /**
- * Represents command to duke.Duke to mark a task as incomplete.
+ * Represents command to Duke to mark a task as incomplete.
  *
  * @author WR3nd3
  */
 public class UnmarkCommand extends Command {
     /** Position of task to be marked as incomplete in the list */
-    private int position;
+    private final int position;
 
     /**
-     * Constructs command.UnmarkCommand for a given task.
+     * Constructs UnmarkCommand for a given task.
      *
      * @param position Integer representing position of task to be marked as incomplete in the list.
      */
