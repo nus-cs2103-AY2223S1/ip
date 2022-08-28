2688fc9d46891ac7306c01b96c64883873d587e3
samuelcheongws <samuelcheong2000@gmail.com>
2022-08-28 17:30:11 +0800

    branch-Level-9

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -8,13 +8,35 @@ import java.util.ArrayList;
 
 public class Parser {
     private static Ui ui = new Ui();
-    public static void parseInput(String input, ArrayList<Task> taskArr) throws DukeException {
+
+    public static String find(String keyword, TaskList tasks) {
+        String message = "Here are the matching tasks in your list!\n%s";
+
+        ArrayList<String> foundTasks = new ArrayList<>();
+
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            if (task.toString().contains(keyword)) {
+                String taskString = String.format("%d: %s", i + 1, task);
+                foundTasks.add(taskString);
+            }
+        }
+
+        String tasksString = String.join("\n", foundTasks);
+        return String.format(message, tasksString);
+
+    }
+
+    public static void parseInput(String input, TaskList taskArr) throws DukeException {
         if (input.equals("list")) {
             ui.printArrAsNumberedList(taskArr);
         } else if (input.startsWith("mark")) {
             taskArr.get(Integer.parseInt(input.substring(5)) - 1).mark();
         } else if (input.startsWith("unmark")) {
             taskArr.get(Integer.parseInt(input.substring(7)) - 1).unmark();
+        } else if (input.startsWith("find")) {
+            String[] split = input.split(" ");
+            System.out.println(find(split[1], taskArr));
         } else {
             // this is under task creation
             if (input.startsWith("event")) {
