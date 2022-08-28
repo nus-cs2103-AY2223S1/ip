a7c2507a92af3cc6e67c10dc10248f38531f50fc
KSHan29 <seow.han@outlook.com>
2022-08-28 18:21:29 +0800

    Add JavaFx code for build.gradle

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,6 +1,11 @@
 package duke;
 
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Duke extends Application {
     protected static boolean terminate = false;
     private Ui ui;
     private Parser parser;
@@ -22,5 +27,14 @@ public class Duke {
     public static void main(String[] args) {
         Duke duke = new Duke("data/duke.txt");
         duke.run();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
