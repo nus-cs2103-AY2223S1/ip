a6cdfb3f0c5bbbf083c276338578729a3c465179
Zsigmond <e0725064@u.nus.edu>
2022-08-28 13:34:50 +0800

    test: Parser.parse (wrong command), tasklist.add, ui.showLoadingError

---
+++ src/test/java/duke/TaskListTest.java
@@ -0,0 +1,31 @@
+package duke;
+
+import duke.task.Deadline;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TaskListTest {
+    private final TaskList taskList = new TaskList();
+    @Test
+    public void addTest() {
+        String commandString = "deadline test /by 10/02/22 1800";
+        Deadline deadline = new Deadline(false,
+                "test",
+                LocalDateTime.parse("10/02/22 1800", DateTimeFormatter.ofPattern("dd/MM/yy HHmm")));
+        try {
+            taskList.add(deadline,
+                    commandString,
+                    new Storage(new Ui(), "data.txt", "data"));
+        } catch (IOException e) {
+            fail();
+        }
+
+        assertEquals(deadline, taskList.get(0));
+    }
+}
---
+++ src/test/java/duke/UiTest.java
@@ -0,0 +1,34 @@
+package duke;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UiTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));     // capture what is supposed to be sent to System.out
+    }
+
+    @Test
+    public void showLoadingErrorTest() {
+        Ui ui = new Ui();
+        ui.showLoadingError();
+        assertEquals(
+                "------------------------------\nThere was a problem loading the tasks from the output file. " +
+                        "Starting with empty list.\n------------------------------\n\n",
+                outputStreamCaptor.toString());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(System.out);
+    }
+}
