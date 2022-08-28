fb0494966e3ca6b8a43a05157164d3362db5f608
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-08-28 18:09:44 +0800

    Fix some checkstyle violations

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -8,9 +8,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
