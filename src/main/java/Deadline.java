4760c21164c6f188be06d8b309b726e61e261f3a
darrentde <darrent2599@gmail.com>
2022-08-28 21:35:23 +0800

    Complete level-8
    Create datetime in both deadline and event
    Add date method to check event/datetime happening at a particular date
    
    Format of datetime for checking must be in d/MM/yyyy

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-    protected String byDate;
+    protected LocalDateTime dateTime;
 
     Deadline(String description, String byDate) {
         super(description);
-        this.byDate = byDate;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
+        this.dateTime = LocalDateTime.parse(byDate,formatter);
+    }
+
+    public LocalDateTime getDateTime() {
+        return this.dateTime;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.byDate +")";
+        return "[D]" + super.toString() + " (by: " +
+                this.dateTime.format(DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm a")) +")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,23 @@
-public class Event extends Task{
-    protected String byDateTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    Event(String description, String byDateTime) {
+public class Event extends Task{
+    protected LocalDateTime dateTime;
+
+    Event(String description, String dateTime) {
         super(description);
-        this.byDateTime = byDateTime;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
+        this.dateTime = LocalDateTime.parse(dateTime, formatter);
+    }
+
+    public LocalDateTime getDateTime() {
+        return this.dateTime;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.byDateTime +")";
+        return "[E]" + super.toString() + " (at: " +
+                this.dateTime.format(DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm a")) +")";
     }
 
 }
