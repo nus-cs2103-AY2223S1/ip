7fb903f26d23243720397000d9898dcd72757f71
Lu Yiting <e0774189@u.nus.edu>
2022-08-29 19:55:38 +0800

    Improve GUI

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -16,12 +16,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
 
 
 
