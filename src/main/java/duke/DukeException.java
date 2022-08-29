4c576be27544ad71535843d78b010f0123686754
JASON-ZENBOOK\jason <jasoncp61@gmail.com>
2022-08-29 18:34:23 +0700

    Add JavaDocs for DukeException

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -12,30 +12,65 @@ public class DukeException extends Exception {
     private static final String INVALID_DATE_FORMAT = "The date format is invalid";
     private static final String UNKNOWN_COMMAND = "I don't know what that means";
 
+    /**
+     * Constructs a {@link DukeException} object using a specific message.
+     *
+     * @param message String message to be shown to the user.
+     */
     public DukeException(String message) {
         super(message);
     }
 
+    /**
+     * Handles errors where the user input is empty.
+     *
+     * @return {@link DukeException}
+     */
     public static DukeException dukeEmptyInputException() {
         return new DukeException(EMPTY_INPUT);
     }
 
+    /**
+     * Handles errors where the specified file cannot be found.
+     *
+     * @return {@link DukeException}
+     */
     public static DukeException dukeFileNotFoundException() {
         return new DukeException(FILE_NOT_FOUND);
     }
 
+    /**
+     * Handles errors where the task number is out of range or is not an integer.
+     *
+     * @return {@link DukeException}
+     */
     public static DukeException dukeInvalidIndexException() {
         return new DukeException(INVALID_INDEX);
     }
 
+    /**
+     * Handles errors where the task format of the input is incorrect.
+     *
+     * @return {@link DukeException}
+     */
     public static DukeException dukeInvalidFormatException() {
         return new DukeException(INVALID_FORMAT);
     }
 
+    /**
+     * Handles errors where the date format of the task is incorrect.
+     *
+     * @return {@link DukeException}
+     */
     public static DukeException dukeInvalidDateFormatException() {
         return new DukeException(INVALID_DATE_FORMAT);
     }
 
+    /**
+     * Handles errors where the command is not known by the program.
+     *
+     * @return {@link DukeException}
+     */
     public static DukeException dukeUnknownCommandException() {
         return new DukeException(UNKNOWN_COMMAND);
     }
