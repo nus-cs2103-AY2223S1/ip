1b93c8f45ebeaef6d807070baa742bc3035e6b75
sjoann <joansim19@gmail.com>
2022-08-29 15:28:08 +0800

    Remove unneccesary code

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -34,34 +34,6 @@ public class Duke {
     }
 
     /**
-     * Takes in user input and execute commands until user exits.
-     *
-     * @throws IOException
-     */
- /*   public void run() throws IOException {
-        ui.showWelcome();
-        boolean isExit = false;
-        while (!isExit) {
-            try {
-                String fullCommand = ui.readCommand();
-                ui.showLine(); // show the divider line ("_______")
-                Command c = Parser.parse(fullCommand);
-                c.execute(tasks, ui, storage);
-                isExit = c.isExit();
-            } catch (DukeException e) {
-                ui.showToUser(e.getMessage());
-            } finally {
-                ui.showLine();
-            }
-        }
-        storage.updateFile(tasks);
-    }
-
-    public static void main(String[] args) throws IOException {
-        new Duke().run();
-    }*/
-
-    /**
      * Displays response in GUI.
      * @param input is the command input.
      * @return the response for the command.
