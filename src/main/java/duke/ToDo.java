ef6f343d79c41d9327ebadbe9af369dd07a79ea9
kangqiao322 <kangqiao803@gmail.com>
2022-08-30 00:06:30 +0800

    A-JUnit

--- src/main/java/duke/ToDo.java
+++ src/main/java/duke/ToDo.java
@@ -1,6 +1,6 @@
 package duke;
 
-public class ToDo extends Task {
+public class   ToDo extends Task {
     protected String description;
     protected boolean isDone;
 
---
+++ src/test/duke/TodoTest.java
@@ -0,0 +1,17 @@
+package duke;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class ToDoTest {
+
+    @Test
+    public void ToDoTest() {
+        String actualString = new ToDo("Testing").toString();
+        String expectedString = "[T] [ ] Testing";
+
+        assertEquals(expectedString, actualString);
+    }
+}
