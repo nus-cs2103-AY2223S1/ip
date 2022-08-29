400cf6231a9580bb753061b89050682ef1c89b2b
tin-jy <tinjingyao@gmail.com>
2022-08-29 19:54:46 +0800

    Add different types of tasks
    There are 3 types of tasks, Todos, Events, and Deadline tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,76 @@
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
 
     private static final ArrayList<Task> LIST_OF_THINGS = new ArrayList<>();
 
-    private static class Task {
+    private abstract static class Task {
         private final String task_name;
         private boolean isCompleted;
+
+        private static class TodoTask extends Task {
+
+            public TodoTask(String name) {
+                super(name);
+            }
+
+            @Override
+            public String toString() {
+                if (isCompleted()) {
+                    return String.format("[T][X] %s", getTaskName());
+                } else {
+                    return String.format("[T][ ] %s", getTaskName());
+                }
+            }
+        }
+
+        private static class DeadlineTask extends Task {
+
+            private final String deadline;
+
+            public DeadlineTask(String name, String date) {
+                super(name);
+                deadline = date;
+            }
+
+            @Override
+            public String toString() {
+                if (isCompleted()) {
+                    return String.format("[D][X] %s (By: %s)", getTaskName(), deadline);
+                } else {
+                    return String.format("[D][ ] %s (By: %s)", getTaskName(), deadline);
+                }
+            }
+        }
+
+        private static class EventTask extends Task {
+
+            private final String datetime;
+
+            public EventTask(String name, String date) {
+                super(name);
+                datetime = date;
+            }
+
+            @Override
+            public String toString() {
+                if (isCompleted()) {
+                    return String.format("[E][X] %s (At: %s)", getTaskName(), datetime);
+                } else {
+                    return String.format("[E][ ] %s (At: %s)", getTaskName(), datetime);
+                }
+            }
+        }
 
         public Task(String name) {
             task_name = name;
             isCompleted = false;
+        }
+
+        public String getTaskName() {
+            return task_name;
         }
 
         public void markComplete() {
@@ -20,6 +79,10 @@ public class Duke {
 
         public void markIncomplete() {
             isCompleted = false;
+        }
+
+        public boolean isCompleted() {
+            return isCompleted;
         }
 
         @Override
@@ -96,9 +159,35 @@ public class Duke {
                     task.markIncomplete();
                     System.out.printf("Get it done! This task has been marked as undone:%n%s%n", task);
                 }
+            } else if (message.startsWith("deadline")) {
+                int slash_char_pos = message.indexOf("/by");
+                if (slash_char_pos < 13) {
+                    System.out.println("Please enter something valid");
+                } else {
+                    String task_name = message.substring(9, slash_char_pos);
+                    String deadline = message.substring(slash_char_pos + 4);
+                    Task this_task = new Task.DeadlineTask(task_name, deadline);
+                    LIST_OF_THINGS.add(this_task);
+                    System.out.printf("I've added this task:%n%s%n", this_task);
+                }
+            } else if (message.startsWith(("event"))) {
+                int slash_char_pos = message.indexOf("/at");
+                if (slash_char_pos < 10) {
+                    System.out.println("Please enter something valid");
+                } else {
+                    String task_name = message.substring(6, slash_char_pos);
+                    String date = message.substring(slash_char_pos + 4);
+                    Task this_task = new Task.EventTask(task_name, date);
+                    LIST_OF_THINGS.add(this_task);
+                    System.out.printf("I've added this task:%n%s%n", this_task);
+                }
+            } else if (message.startsWith(("todo"))) {
+                String task_name = message.substring(5);
+                Task this_task = new Task.TodoTask(task_name);
+                LIST_OF_THINGS.add(this_task);
+                System.out.printf("I've added this task:%n%s%n", this_task);
             } else {
-                LIST_OF_THINGS.add(new Task(message));
-                System.out.printf("Added: %s%n", message);
+                System.out.println("Sorry, please specify a task type");
             }
             message = keyboard.nextLine();
         }
