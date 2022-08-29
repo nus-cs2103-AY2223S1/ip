354e6146210ac939dbf853dbf3b37eada354d7ed
Ng Xing Yu <ng.xingyu@u.nus.edu>
2022-08-29 09:59:31 +0800

    Fix unicode symbol representation to be displayed on the gui.

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -5,10 +5,10 @@ package duke;
  */
 public class DukeException extends Exception {
     public DukeException(String message) {
-        super(String.format("☹ OOPS!!! %s", message));
+        super(String.format("\u2639 OOPS!!! %s", message));
     }
 
     public DukeException(String format, Object... args) {
-        super(String.format("☹ OOPS!!! %s", String.format(format, args)));
+        super(String.format("\u2639 OOPS!!! %s", String.format(format, args)));
     }
 }
