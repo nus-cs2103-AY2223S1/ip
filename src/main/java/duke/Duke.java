a8ec6ca6423e82455762b61160b7df72b82c8e2a
wrewsama <andrewlzs512@gmail.com>
2022-08-29 14:18:10 +0800

    Set up GUI to echo user inputs

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,6 +1,16 @@
 package duke;
 
 import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * This class encapsulates the Duke chatbot.
@@ -8,11 +18,17 @@ import java.util.ArrayList;
  * @author Andrew Lo Zhi Sheng
  * @version CS2103T AY22/23 Semester 1
  */
-public class Duke {
+public class Duke extends Application {
     private static ArrayList<Task> inputs = new ArrayList<>();
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     /**
      * Constructor for a duke.Duke instance.
@@ -24,6 +40,13 @@ public class Duke {
         this.ui = new Ui();
         this.taskList = new TaskList(this.storage.getTasks(), this.ui, this.storage);
         this.ui.updateTaskList(this.taskList);
+    }
+
+    /**
+     * Overloaded constructor with no arguments.
+     */
+    public Duke() {
+        this(System.getProperty("user.home") + "/duke.txt");
     }
 
     /**
@@ -42,15 +65,92 @@ public class Duke {
         }
     }
 
+    @Override
+    public void start(Stage stage) {
+        // Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        // User input functionality
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+
+        // Formatting the window
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        // Implementing user input event handlers
+        sendButton.setOnMouseClicked((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        userInput.setOnAction((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        // Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    }
+
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
 
     /**
      * The main program loop.
      *
      * @param args
      */
-    public static void main(String[] args) {
-        String filePath = System.getProperty("user.home") + "/duke.txt";
-        Duke duke = new Duke(filePath);
-        duke.run();
-    }
+//    public static void main(String[] args) {
+//        String filePath = System.getProperty("user.home") + "/duke.txt";
+//        Duke duke = new Duke(filePath);
+//        duke.run();
+//    }
 }
