5d11f5fadea0faefb10e868cfe1267b85b5aec4c
RussellDash332 <russellsaerang@gmail.com>
2022-08-30 04:26:39 +0800

    Allow overloading for Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,9 +2,13 @@ abstract class Task {
     private String description;
     private boolean isDone;
 
-    public Task(String description) {
+    public Task(String description, boolean isDone) {
         this.description = description;
-        this.isDone = false;
+        this.isDone = isDone;
+    }
+
+    public Task(String description) {
+        this(description, false);
     }
 
     public String getStatusIcon() {
