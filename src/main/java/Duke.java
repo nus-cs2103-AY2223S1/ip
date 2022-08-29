27413b8d8ae26843ebfee91c5a6ef383969b83ee
SweetPotato0213 <e0376920@u.nus.edu>
2022-08-29 17:58:10 +0800

    Level 3(v2)
    Correct the functions and separate Task class from Duke class file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,28 +15,45 @@ public class Duke {
 
     public static void main(String[] args) {
 
-        String hello = "____________________________________________________________\n" +
+        String hello = "    ____________________________________________________________\n" +
                 "     Hello! I'm Duke\n" +
                 "     What can I do for you?\n" +
-                "____________________________________________________________\n";
+                "    ____________________________________________________________\n";
         System.out.println(hello);
 
-        List<String> list = new ArrayList<>();
-        int size = 0;
+        List<Task> list = new ArrayList<Task>();
 
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        String input = sc.next();
+
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                for (int i = 0; i < size; i++) {
+                for (int i = 0; i < list.size(); i++) {
                     System.out.println(i + 1 + "." + list.get(i));
                 }
+            } else if (input.equals("mark")) {
+                int number = sc.nextInt();
+                Task task = list.get(number - 1);
+                task.markAsDone();
+                System.out.println("    ____________________________________________________________\n" +
+                        "     Nice! I've marked this task as done:\n       " +
+                        task +
+                        "\n    ____________________________________________________________");
+            } else if (input.equals("unmark")) {
+                int number = sc.nextInt();
+                Task task = list.get(number - 1);
+                task.markAsNotDone();
+                System.out.println("    ____________________________________________________________\n" +
+                        "     Nice! I've marked this task as not done yet:\n       " +
+                        task +
+                        "\n    ____________________________________________________________");
             } else {
+                input = input + sc.nextLine();
                 System.out.println("Added: " + input);
-                list.add(input);
-                size++;
+                Task task = new Task(input);
+                list.add(task);
             }
-            input = sc.nextLine();
+            input = sc.next();
         }
 
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatus() {
+        if (isDone) {
+            return "X";
+        } else {
+            return " ";
+        }
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatus() + "] " + this.description;
+    }
+}
