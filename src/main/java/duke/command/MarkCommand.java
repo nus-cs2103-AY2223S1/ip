1be4df5db9fbd80e006c592c2d0fd2a379313dbb
doimoiboi <asherlimyi@gmail.com>
2022-08-29 15:17:44 +0800

    Add JavaDoc header comments

--- src/main/java/duke/command/MarkCommand.java
+++ src/main/java/duke/command/MarkCommand.java
@@ -8,11 +8,24 @@ public class MarkCommand extends Command {
     private int index;
     private boolean isDone;
 
+    /**
+     * Constructor for a command to mark an object done or not done.
+     *
+     * @param index Index of the task in the task list.
+     * @param isDone Boolean of which the specified task will be marked.
+     */
     public MarkCommand(int index, boolean isDone) {
         this.index = index;
         this.isDone = isDone;
     }
 
+    /**
+     * Method that executes the mark command to mark the specified task done or not done.
+     *
+     * @param tasks Task list containing the task to be marked.
+     * @param ui Ui that will output messages to the user.
+     * @param storage Storage on hard disk that stores the task list.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         tasks.changeDoneStatus(this.index, this.isDone);
