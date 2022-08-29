fc67ca35f917c24b425299cd5629bd4906461a27
highorbit25 <baobin25@gmail.com>
2022-08-29 23:11:29 +0800

    Fix master branch

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,10 @@
+import duke.DukeException;
+import duke.command.Command;
+import duke.main.Parser;
+import duke.main.Storage;
+import duke.main.TaskList;
+import duke.main.Ui;
+
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -46,87 +53,5 @@ public class Duke {
         }
 
     }
-
-//    public static void getInput(Scanner scan) throws DukeException, IOException {
-//        String inData;
-//        try {
-//            inData = scan.nextLine();
-//        } catch (Exception NoSuchElementException) {
-//            return;
-//        }
-//
-//        String[] input = inData.split(" ", 2);
-//        switch (input[0]) {
-//            case("bye"):
-//                // store existing tasks into save file
-//                storage.update(tasks);
-//                ui.printExit();
-//                break;
-//            case("mark"):
-//                Task taskToMark = tasks.getTasks().get(Integer.parseInt(input[1]) - 1);
-//                taskToMark.markAsDone();
-//                ui.printMarkDone(taskToMark);
-//                storage.update(tasks);
-//                getInput(scan);
-//                break;
-//            case("unmark"):
-//                Task taskToUnmark = tasks.getTasks().get(Integer.parseInt(input[1]) - 1);
-//                taskToUnmark.markAsNotDone();
-//                ui.printMarkNotDone(taskToUnmark);
-//                storage.update(tasks);
-//                getInput(scan);
-//                break;
-//            case("list"):
-//                ui.printListOfTasks(tasks);
-//                getInput(scan);
-//                break;
-//            case("todo"):
-//                    if (input.length == 1) {
-//                        throw new DukeException("The description of a todo cannot be empty.");
-//                    }
-//                    Todo curTodo = new Todo(input[1]);
-//                    addToTasklist(curTodo);
-//                    getInput(scan);
-//                    break;
-//            case("deadline"):
-//                String[] deadlineString = input[1].split("/by ", 2);
-//                if (deadlineString.length == 1) {
-//                    throw new DukeException("The 'by date' of a deadline cannot be empty.");
-//                }
-//                Deadline curDeadline = new Deadline(deadlineString[0], LocalDate.parse(deadlineString[1]));
-//                addToTasklist(curDeadline);
-//                getInput(scan);
-//                break;
-//            case("event"):
-//                String[]eventString = input[1].split("/at ", 2);
-//                if (eventString.length == 1) {
-//                    throw new DukeException("The 'at date' of a event cannot be empty.");
-//                }
-//                Event curEvent = new Event(eventString[0], LocalDate.parse(eventString[1]));
-//                addToTasklist(curEvent);
-//                getInput(scan);
-//                break;
-//            case("delete"):
-//                deleteTask(Integer.parseInt(input[1]) - 1);
-//                getInput(scan);
-//                break;
-//            default:
-//                throw new DukeException("I'm sorry, but I don't know what that means :-(");
-//        }
-//
-//    }
-
-
-//    public static void addToTasklist(Task taskToAdd) throws IOException {
-//        tasks.addTask(taskToAdd);
-//        ui.printAdd(taskToAdd, tasks);
-//        storage.update(tasks);
-//    }
-//
-//    public static void deleteTask(int index) throws IOException {
-//        ui.printDelete(index, tasks);
-//        tasks.removeTask(index);
-//        storage.update(tasks);
-//    }
 
 }
