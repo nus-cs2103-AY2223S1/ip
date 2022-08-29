dc069fccce08a27463df99a3b90c609699273f3e
seanmanik <seanmanik@gmail.com>
2022-08-29 19:26:42 +0800

    Add JavaFX Template

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,17 +1,27 @@
 package duke;
-
-import commands.Command;
-
-import java.util.ArrayList;
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.Scanner;
 
-public class Duke {
+
+public class Duke extends Application {
     private final Storage storage;
     private TaskList tasks;
     private final Ui ui;
 
     private final Parser parser;
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
 
     /**
      * Creates a Duke object with the specified file path.
