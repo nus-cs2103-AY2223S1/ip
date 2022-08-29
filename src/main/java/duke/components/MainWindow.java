ae33307b6c7c02b202ea320faf520c9e94648179
Thanh Chu <lilyhanky57@gmail.com>
2022-08-30 00:51:58 +0800

    create GUI

---
+++ src/main/java/duke/components/MainWindow.java
@@ -0,0 +1,44 @@
+package duke.components;
+
+import duke.Duke;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for MainWindow. Provides the Layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+    private Duke duke;
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/Rosie.jpg"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Lisa.jpg"));
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+    public void setDuke(Duke d) {
+        duke = d;
+    }
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = duke.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+    }
+}
