f667c2d21f94a2259d141a001a7d85b3874e22c7
jq1836 <95712150+jq1836@users.noreply.github.com>
2022-08-29 16:12:02 +0800

    Add Java Docs to FindCommand class
    FindCommand class currently does not include Java Docs.
    
    Add Java Docs to FindCommand class.

--- src/main/java/duke/chatbot/command/FindCommand.java
+++ src/main/java/duke/chatbot/command/FindCommand.java
@@ -6,11 +6,21 @@ import duke.chatbot.data.task.TaskList;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A command that prints a list of tasks that have the argument
+ * string as a substring of the task description.
+ */
 public class FindCommand extends Command {
     public FindCommand(List<String> arguments) {
         this.arguments = arguments;
     }
 
+    /**
+     * Returns an instance of CommandResult with a message that displays
+     * a list of tasks which have the argument string as a substring of the
+     * task description.
+     * @return The result after executing the command.
+     */
     @Override
     public CommandResult execute() {
         List<String> message = new ArrayList<>();
