b868a61ecd1080bbfec95ef5551744b26a402070
Berted <46164341+Berted@users.noreply.github.com>
2022-08-31 00:13:07 +0800

    Adds basic GUI

---
+++ src/main/java/duke/ui/MainWindow.java
@@ -0,0 +1,73 @@
+package duke.ui;
+
+import duke.Duke;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+//@@author Berted-reused
+//Reused from https://se-education.org/guides/tutorials/javaFxPart4.html
+// with modifications.
+/**
+ * Controller for MainWindow. Provides the layout for the other controls.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Duke duke;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setDuke(Duke d) {
+
+        duke = d;
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(duke.readOutput(), dukeImage)
+        );
+        d.init();
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(duke.readOutput(), dukeImage)
+        );
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+
+        boolean isTerminated = duke.execCommand(input);
+        if (isTerminated) {
+            userInput.setDisable(true);
+            userInput.setPromptText("Duke has terminated. You may close this window.");
+            sendButton.setDisable(true);
+        }
+
+        String response = duke.readOutput();
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+    }
+}
+//@@author
