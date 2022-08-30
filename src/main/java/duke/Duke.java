fd1ff983b29c7e96fe1489e862ffe4573dd296ae
darrtzj <darrenteozj@u.nus.edu>
2022-08-30 21:11:14 +0800

    Add base code for GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -53,6 +53,14 @@ public class Duke {
         }
     }
 
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
     public static void main(String[] args) {
         new Duke(FILE_PATH).run();
     }
