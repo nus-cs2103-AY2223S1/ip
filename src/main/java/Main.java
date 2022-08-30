f971c36b963d07533fdfa0b027947f5e6079ed72
Tan Jin Waye <e0694446@u.nus.edu>
2022-08-30 21:46:41 +0800

    Add Gui class for JavaFX testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 import duke.Duke;
-
+import duke.Gui;
+import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        Duke.main(args);
+        //Duke.main(args);
+        Application.launch(Gui.class, args);
     }
 }
