0f424946f7fb5cdc04ec2995eb4e2ae32e9a5787
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-08-30 14:59:21 +0800

    Add GUi greeting by user and duke

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -33,6 +33,10 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog("hello", userImage),
+                DialogBox.getDukeDialog("Hello! I'm Duke! What can I do for you?", dukeImage)
+        );
     }
 
     public void setDuke(Duke d) {
