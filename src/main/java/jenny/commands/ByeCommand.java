bf06fff91197dce32e78d7481d38da6778cc5488
Deon <deeyonn@gmail.com>
2022-08-30 16:30:33 +0800

    Add JavaDoc for ByeCommand

--- src/main/java/jenny/commands/ByeCommand.java
+++ src/main/java/jenny/commands/ByeCommand.java
@@ -8,18 +8,44 @@ import jenny.util.Ui;
 
 import java.util.ArrayList;
 
+/**
+ * Will send an exit message to the user and exit the application.
+ * CS2103 Week 3
+ * AY21/22 Semester 1
+ *
+ * @author Deon
+ */
 public class ByeCommand extends Command {
     public static final String COMMAND = "bye";
 
+    /**
+     * Constructor for an instance of a new command.
+     * Will initialise a new command with the provided {@code arguments}.
+     *
+     * @param arguments valid command arguments defined.
+     */
     public ByeCommand(String arguments) {
         super(arguments);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @return true if the command is an exit command, false otherwise.
+     */
     @Override
     public boolean isExit() {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param tasks   the instance of {@link TaskList} to run the command with.
+     * @param ui      the instance of {@link Ui} to run the command with.
+     * @param storage the instance of {@link Storage} to run the command with.
+     * @throws JennyException when runtime exceptions are thrown in the JennyBot application.
+     */
     @Override
     public void run(TaskList tasks, Ui ui, Storage<ArrayList<Task>> storage) throws JennyException {
         ui.exit();
