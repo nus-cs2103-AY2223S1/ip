ae32aceb7102efd216997f812edd56e0f2d08f96
Tan Jin Waye <e0694446@u.nus.edu>
2022-08-30 21:42:48 +0800

    Revert "Include Gui class to test JavaFX"
    This reverts commit 5508976e32528d8daa7cc88263c1de084adc0a9c.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,8 @@
 import duke.Duke;
-import duke.Gui;
 
-import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        // Duke.main(args);
-        Application.launch(Gui.class, args);
+        Duke.main(args);
     }
 }
