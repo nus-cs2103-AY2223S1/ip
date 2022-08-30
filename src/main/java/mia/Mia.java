5e41895dbae33f9531f7c75eb3345da2b04b43bb
Richard Dominick <richdom2185@gmail.com>
2022-08-30 11:16:58 +0800

    Add documentation for Mia class

--- src/main/java/mia/Mia.java
+++ src/main/java/mia/Mia.java
@@ -3,6 +3,9 @@ package mia;
 import general.ui.ChatWindow;
 import general.ui.Span;
 
+/**
+ * An instance of {@code Mia}, a virtual assistant.
+ */
 public class Mia {
     private static final String logo = "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n" +
                                        "┃ You are talking to MIA... ┃\n" +
@@ -14,14 +17,27 @@ public class Mia {
         tasksManager = new TaskManager(dataPath);
     }
 
+    /**
+     * Sends a message to the user.
+     *
+     * @param message the message to be sent
+     */
     public void respond(String message) {
         window.printResponse(new Span(message));
     }
 
+    /**
+     * Gets the {@code TaskManager} task list manager for this {@code Mia} instance.
+     *
+     * @return The {@code TaskManager} associated with this {@code Mia} instance.
+     */
     public TaskManager getTasks() {
         return tasksManager;
     }
 
+    /**
+     * Starts this {@code Mia} instance.
+     */
     public void run() {
         System.out.println(logo);
 
