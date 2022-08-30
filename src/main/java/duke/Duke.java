477421b56e92ae12ead619c034c1d9f68934c3d6
ShummyOwnzYou <jiehui285@gmail.com>
2022-08-30 19:56:29 +0800

    Add GUI for Duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -4,8 +4,13 @@ import command.Command;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.text.Font;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -22,10 +27,14 @@ public class Duke extends Application {
     private final Ui ui;
     private final Parser parser;
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
     /**
-     * Initializes a Duke object with the specified file path to load and store tasks.
-     *
-     * @param filePath The file path to the local file responsible for loading and saving.
+     * Initializes a Duke object with a file path to load and store tasks.
      */
 
     public Duke() {
@@ -62,13 +71,54 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Label byeWorld = new Label("Bye World!"); // Creating a new Label control
-        helloWorld.setFont(Font.font(50));
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-        Scene scene2 = new Scene(byeWorld); // Setting the scene to be our Label
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        // more code to be added here later
     }
 
     public static void main(String[] args) {
