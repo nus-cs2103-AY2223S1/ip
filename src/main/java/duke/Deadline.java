4b2869d13e8ef4ed720d8cd18476fa92cf0e996b
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-08-30 11:07:16 +0800

    Add javadocs for constructors in Event, Deadline and Todo

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -6,18 +6,36 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 /**
- * Represents a deadline task with an end date and time.
+ * Represents a Deadline task with an end date and time.
  */
 public class Deadline extends Task {
     private LocalDate date;
     private LocalTime time;
 
+    /**
+     * Initializes a new Deadline object which is a subclass of Task.
+     *
+     * @param description Description of the Deadline task.
+     * @param date Due date of the Deadline task.
+     * @param time Due time of the Deadline task.
+     * @throws DateTimeParseException If the inputted time is invalid and cannot be parsed.
+     */
     public Deadline(String description, String date, String time) throws DateTimeParseException {
         super(description);
         this.date = LocalDate.parse(date);
         this.time = LocalTime.parse(time);
     }
 
+    /**
+     * Initializes a new Deadline object which is a subclass of Task, with the option to specify if the
+     * task is done or not.
+     *
+     * @param description Description of the deadline task.
+     * @param status Completion state of the deadline task.
+     * @param date Due date of the deadline task.
+     * @param time Due time of the deadline task.
+     * @throws DateTimeParseException If the inputted time is invalid and cannot be parsed.
+     */
     public Deadline(String description, int status, String date, String time) {
         this(description, date, time);
         isDone = status == 1;
