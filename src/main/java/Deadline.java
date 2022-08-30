a1847371a533308a4f558b77b0f25090ed24e96d
rachaelsng <rachaelsng@u.nus.edu>
2022-08-31 02:47:16 +0800

    added dates and times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime dateTime;
+    protected static DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    protected static DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd LLL yyyy hh:mma");
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.dateTime = LocalDateTime.parse(by, inputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + dateTime.format(outputFormatter) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String at;
+    protected LocalDateTime dateTime;
+    protected static DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    protected static DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd LLL yyyy hh:mma");
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        this.dateTime = LocalDateTime.parse(at, inputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + dateTime.format(outputFormatter) + ")";
     }
 }
