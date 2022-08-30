130938a026b80244e272a0be0a0ab8d709e4229e
sikai00 <sikai4111@gmail.com>
2022-08-31 04:17:00 +0800

    CommandResult.java: Add JavaDoc

--- src/main/java/duke/commands/CommandResult.java
+++ src/main/java/duke/commands/CommandResult.java
@@ -1,14 +1,26 @@
 package duke.commands;
 
+/**
+ * Represents the result of an executed command.
+ */
 public class CommandResult {
     private final String msg;
     private final boolean shouldExit;
 
+    /**
+     * Initializes a new CommandResult instance. Defaults shouldExit to false.
+     * @param msg Response message from the executed Command.
+     */
     public CommandResult(String msg) {
         this.msg = msg;
         this.shouldExit = false;
     }
 
+    /**
+     * Initializes a new CommandResult instance.
+     * @param msg Response message from the executed Command.
+     * @param shouldExit Whether the Command led to a call for the program to exit
+     */
     public CommandResult(String msg, boolean shouldExit) {
         this.msg = msg;
         this.shouldExit = shouldExit;
