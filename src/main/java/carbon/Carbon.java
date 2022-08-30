ad8c9c632543330a7bddf2d3aa33dd25fc9f83c2
kevinchangjk <kevinchangjk@gmail.com>
2022-08-30 17:48:19 +0800

    Add javafx hello world

--- src/main/java/carbon/Carbon.java
+++ src/main/java/carbon/Carbon.java
@@ -1,12 +1,17 @@
 package carbon;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 import carbon.error.CarbonException;
 
 /**
  * Interacts with user through CLI to keep track of tasks.
  * Requires user to provide input and call task management commands.
  */
-public class Carbon {
+public class Carbon extends Application {
     private boolean isRunning;
     private Ui ui;
     private Parser parser;
@@ -18,7 +23,7 @@ public class Carbon {
      *
      * @return Carbon object.
      */
-    private Carbon() {
+    public Carbon() {
         // init fields
         this.ui = new Ui();
         this.storage = new Storage();
@@ -44,6 +49,15 @@ public class Carbon {
                 this.ui.printError(error);
             }
         }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     /**
