060bb03a4a02649b38432475f7493491fc33eac8
RussellDash332 <russellsaerang@gmail.com>
2022-08-30 15:39:37 +0800

    Add JavaDoc comments to Task and exception classes

--- src/main/java/stashy/data/exception/StashyException.java
+++ src/main/java/stashy/data/exception/StashyException.java
@@ -1,5 +1,8 @@
 package stashy.data.exception;
 
+/**
+ * Stashy-specific exception class.
+ */
 public class StashyException extends Exception {
     public StashyException(String message) {
         super(message);
