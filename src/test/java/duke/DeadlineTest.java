d3fdd0b0001e9a25a27c8ef1deb01cad941b721a
o-ohst <hao_si_tong@outlook.com>
2022-08-30 15:16:38 +0800

    A-JUnit

--- src/test/java/duke/DeadlineTest.java
+++ src/test/java/duke/DeadlineTest.java
@@ -2,11 +2,14 @@ package duke;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DukeTest {
+public class DeadlineTest {
     @Test
     public void test(){
-        assertEquals(2, 2);
+        Deadline dl = new Deadline("eat and sleep", LocalDateTime.of(2022, 8, 30, 15, 0, 0));
+        assertEquals("[D][ ] eat and sleep (by: 30 Aug 2022 15:00)", dl.toString());
     }
 }
--- src/test/java/duke/TodoTest.java
+++ src/test/java/duke/TodoTest.java
@@ -1,2 +1,13 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TodoTest {
+    @Test
+    public void newTodoCreated(){
+        Todo todo = new Todo("do this");
+        assertEquals("[T][ ] do this", todo.toString());
+    }
 }
