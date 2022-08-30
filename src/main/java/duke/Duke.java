5ccda3f2db91e146c67672af2f04f44627111d51
jhchee18 <e0407708@u.nus.edu>
2022-08-30 16:28:37 +0800

    Design the layout of the Controls

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,16 +5,27 @@ import duke.exception.DukeException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  * Main file of Duke to be run to initiate the program.
  */
 public class Duke extends Application {
+    //Backend variables
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
+
+    //Frontend variables
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     /**
      * Constructs a Duke instance by default with no properties initialised
@@ -69,8 +80,18 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        
+        scene = new Scene(mainLayout); // Setting the scene to be our Label
 
         stage.setScene(scene); // Setting the stage to show our screen
         stage.show(); // Render the stage.
