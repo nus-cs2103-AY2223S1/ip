2ed130a4dbc67442ead66764fa03472b72d673ee
RachelChua <e0772465@u.nus.edu>
2022-08-31 17:29:25 +0800

    Add exit method to end the program when user inputs bye

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,48 +1,5 @@
-/*
 package duke;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-
-public class Duke {
-    private static ArrayList<Task> listOfTask = new ArrayList<>();
-    private final static String DIRECTORY = "./DATA";
-    private final static String FILENAME = "duke.txt";
-    private final static String PATHNAME = String.valueOf(Paths.get(DIRECTORY, FILENAME));
-
-    public static void main(String[] args) {
-        Ui ui = new Ui();
-        ui.greetUi();
-        Storage storage = new Storage(PATHNAME, listOfTask, DIRECTORY);
-        storage.getData();
-        Scanner input = new Scanner(System.in);
-        while (input.hasNext()) {
-            try {
-                String fullCommand = input.nextLine();
-                Parser parser = new Parser(fullCommand);
-                if (parser.isSubStringForExitCommand()) {
-                    ExitCommand exitCommand = new ExitCommand();
-                    exitCommand.execute(fullCommand,listOfTask,ui,storage);
-                    break;
-                }
-                Command c = parser.parse();
-                c.execute(fullCommand, listOfTask, ui, storage);
-            } catch (DukeException e) {
-                System.out.println(e.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-   }
-    public String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
-}*/
-
-
-package duke;
+import com.sun.javafx.application.PlatformImpl;
 import javafx.application.Application;
 
 import java.awt.*;
@@ -65,6 +22,8 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import static javafx.application.Platform.exit;
 
 
 public class Duke extends Application {
@@ -163,7 +122,9 @@ public class Duke extends Application {
             Parser parser = new Parser(input);
             if (parser.isSubStringForExitCommand()) {
                 ExitCommand exitCommand = new ExitCommand();
+                exit();
                return exitCommand.execute(input, listOfTask, ui, storage);
+
             }
             Command c = parser.parse();
             return c.execute(input, listOfTask, ui, storage);
