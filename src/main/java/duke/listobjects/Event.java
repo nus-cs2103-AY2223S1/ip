e6f76fe270bd1d1d1f574e9135209b08db7cd9b8
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-08-31 12:39:35 +0800

    Change Event class to comply with coding conventions

---
+++ src/main/java/duke/listobjects/Event.java
@@ -0,0 +1,46 @@
+package duke.listobjects;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Event extends ListObject {
+
+    String eventTime;
+
+    public Event(String task, int status) {
+        super(task, status);
+    }
+
+    public Event(String task, int status, String eventTime) {
+        super(task, status);
+        this.eventTime = eventTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + formatDateTime(eventTime) + ")";
+    }
+
+    public String formatDateTime(String txt) {
+
+        String[] words = txt.split(" ");
+        String date = words[0];
+        String start = words[1];
+        String end = words[2];
+
+        //format date of form yyyy-MM-dd
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate deadline = LocalDate.parse(date, formatter);
+        DateTimeFormatter formatNew = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        String dateNew = deadline.format(formatNew);
+
+        //format time of form HH:mm (24h clock)
+        LocalTime startTime = LocalTime.parse(start, DateTimeFormatter.ISO_LOCAL_TIME);
+        String timeStart = startTime.format(DateTimeFormatter.ISO_LOCAL_TIME);
+        LocalTime endTime = LocalTime.parse(end, DateTimeFormatter.ISO_LOCAL_TIME);
+        String timeEnd = endTime.format(DateTimeFormatter.ISO_LOCAL_TIME);
+
+        return dateNew + " from: " + timeStart +  " to: " + timeEnd;
+    }
+}
