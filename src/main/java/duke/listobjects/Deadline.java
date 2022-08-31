f9b2d6c44482d45b739273ca868394448259fa9e
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-08-31 11:05:53 +0800

    Add JavaDoc for Deadline class

--- src/main/java/duke/listobjects/Deadline.java
+++ src/main/java/duke/listobjects/Deadline.java
@@ -5,27 +5,49 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalTime;
 
-
+/**
+ * Represents Deadlines which are ListObjects that store deadline (date and time) with the task
+ */
 public class Deadline extends ListObject{
 
     String doBy;
 
+    /**
+     * Constructs a Deadline object with given task description and status, with unspecified deadline
+     * @param task String description of task
+     * @param status int representing completion status as 1 if finished and 0 otherwise
+     */
     public Deadline(String task, int status) {
         super(task, status);
-        this.doBy= "untimed";
+        this.doBy= "not specified";
     }
 
+    /**
+     * Constructs a Deadline object with given task description, status, and deadline
+     * @param task String description of tas
+     * @param status int representing completion status as 1 if finished and 0 otherwise
+     * @param doBy String representing deadline (date and time) for task
+     */
     public Deadline(String task, int status, String doBy){
         super(task, status);
         this.doBy = doBy;
     }
 
+    /**
+     * Returns the String representation of the Deadline object
+     * @return String representation of Deadline object
+     */
     @Override
     public String toString(){
         return "[D]" + super.toString() + " (by: " + formatDateTime(doBy) + ")";
     }
 
 
+    /**
+     * Reads String representing date and time and returns an alternate String representation of it
+     * @param txt String representing date and time in the format of yyyy-MM-dd HH:mm
+     * @return String representation of date and time in alternate format of MMM dd yyy HH:mm
+     */
     public String formatDateTime(String txt){
 
         String[] words = txt.split(" ");
