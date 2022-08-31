a66e6fea09248aa87f5c1c2f4c611273b0d85309
leecaregene <e0764279@u.nus.edu>
2022-09-01 03:15:58 +0800

    Complete Level-10

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -11,51 +11,50 @@ public class Parser {
      * @param input    Full command entered by the user.
      * @param taskList Task list to perform operations on.
      * @param ui       UI for displaying output.
-     * @return True when a termination is requested, false otherwise.
+     * @return Response message.
      * @throws DukeException If the input contains errors.
      */
-    public static boolean parse(String input, TaskList taskList, Ui ui) throws DukeException {
+    public static String parse(String input, TaskList taskList, Ui ui) throws DukeException {
         String[] command = input.split(" ", 2);
         if (command[0].equalsIgnoreCase("bye")) {
-            ui.showBye();
-            return true;
+            System.exit(0);
+            return null;
         } else if (command[0].equalsIgnoreCase("list")) {
-            ui.showTasks(taskList);
+            return ui.showTasks(taskList);
         } else if (command[0].equalsIgnoreCase("mark")) {
             int index = Integer.parseInt(command[1]) - 1;
             taskList.mark(index);
-            ui.showMarked(taskList.getTask(index));
+            return ui.showMarked(taskList.getTask(index));
         } else if (command[0].equalsIgnoreCase("unmark")) {
             int index = Integer.parseInt(command[1]) - 1;
             taskList.unmark(index);
-            ui.showUnmarked(taskList.getTask(index));
+            return ui.showUnmarked(taskList.getTask(index));
         } else if (command[0].equalsIgnoreCase("todo")) {
             if (command.length < 2) {
                 throw new DukeException("The description of a todo cannot be empty.");
             }
             Task newTask = new Todo(command[1]);
             taskList.add(newTask);
-            ui.showAdded(newTask, taskList.getSize());
+            return ui.showAdded(newTask, taskList.getSize());
         } else if (command[0].equalsIgnoreCase("deadline")) {
             String[] arguments = command[1].split(" /by ", 2);
             Task newTask = new Deadline(arguments[0], arguments[1]);
             taskList.add(newTask);
-            ui.showAdded(newTask, taskList.getSize());
+            return ui.showAdded(newTask, taskList.getSize());
         } else if (command[0].equalsIgnoreCase("event")) {
             String[] arguments = command[1].split(" /at ", 2);
             Task newTask = new Event(arguments[0], arguments[1]);
             taskList.add(newTask);
-            ui.showAdded(newTask, taskList.getSize());
+            return ui.showAdded(newTask, taskList.getSize());
         } else if (command[0].equalsIgnoreCase("delete")) {
             int index = Integer.parseInt(command[1]) - 1;
             Task task = taskList.getTask(index);
             taskList.remove(index);
-            ui.showRemoved(task, taskList.getSize());
+            return ui.showRemoved(task, taskList.getSize());
         } else if (command[0].equalsIgnoreCase("find")) {
-            ui.showResults(taskList.search(command[1]));
+            return ui.showResults(taskList.search(command[1]));
         } else {
             throw new DukeException("I'm sorry, but I don't know what that means :-(");
         }
-        return false;
     }
 }
