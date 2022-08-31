7ce9781897a3660a8f9a4d4994edda99f4a52c03
HoJunHao2000 <kennethhojunhao@gmail.com>
2022-08-31 12:45:51 +0800

    Fix bug in GUI
    Currently, when the list exceeds certain length, it would be cut off
    with ...
    
    Edited DialogBox min length to be the dynamic according to length of
    list.

--- src/main/java/duke/gui/DialogBox.java
+++ src/main/java/duke/gui/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 
 /**
  * Class containing the DialogBox.
@@ -35,6 +36,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
+        dialog.setMinHeight(Region.USE_PREF_SIZE);
         displayPicture.setImage(img);
     }
 
