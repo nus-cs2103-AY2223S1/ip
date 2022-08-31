7a0cdad504033775bc85dd413c83ba96f3159157
junweimoo <junweimoo@gmail.com>
2022-08-31 17:35:26 +0800

    Store date as a LocalDate object instead of a String

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,17 +1,21 @@
 package tasks;
 
+import utils.DateParser;
+
+import java.time.LocalDate;
+
 public class Deadline extends Task {
 
-    private final String time;
+    private final LocalDate localDate;
 
-    public Deadline(String name, String time) {
+    public Deadline(String name, String dateStr) {
         super(name);
-        this.time = time;
+        localDate = DateParser.stringToDate(dateStr);
     }
 
     @Override
     public String toString() {
-        return "[D] " + super.toString() + " (by: " + time + ")";
+        return "[D] " + super.toString() + " (by: " + localDate.toString() + ")";
     }
 
 }
--- src/main/java/tasks/Event.java
+++ src/main/java/tasks/Event.java
@@ -1,17 +1,21 @@
 package tasks;
 
+import utils.DateParser;
+
+import java.time.LocalDate;
+
 public class Event extends Task {
 
-    private final String time;
+    private final LocalDate localDate;
 
-    public Event(String name, String time) {
+    public Event(String name, String dateStr) {
         super(name);
-        this.time = time;
+        localDate = DateParser.stringToDate(dateStr);
     }
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + " (at: " + time + ")";
+        return "[E] " + super.toString() + " (at: " + localDate.toString() + ")";
     }
 
 }
