2ada9b426cdaebac7492be4ab04967c8768489c1
cfyjoanna <joannachanfy@gmail.com>
2022-08-31 22:50:21 +0800

    remove unnecessary code

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,28 +1,10 @@
 package duke;
 
-import java.util.Scanner;
-
 public class Duke {
     protected Storage storage = new Storage("data/tasks.txt");
     protected TaskList tasks = new TaskList(storage.load(), storage);
     protected Ui ui = new Ui();
     protected Parser parser = new Parser(ui, tasks);
-
-    /**
-     * Runs the main program of Duke.
-     */
-    public void run() {
-        ui.showWelcome();
-        Scanner input = new Scanner(System.in);
-        String next = input.nextLine();
-
-        while (!next.equals("bye")) {
-            parser.processCommand(next);
-            next = input.nextLine();
-        }
-
-        ui.showGoodbye();
-    }
 
     /**
      * Generates appropriate response from Duke.
@@ -32,9 +14,5 @@ public class Duke {
      */
     public String getResponse(String input) {
         return parser.processCommand(input);
-    }
-
-    public static void main(String[] args) {
-        new Duke().run();
     }
 }
