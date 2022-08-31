8f9d7f321a11322bdc23a9263256843440c5c29d
nopehax <dynamical36@gmail.com>
2022-09-01 01:52:57 +0800

    Add an empty GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -8,18 +8,34 @@ import duke.util.Ui;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
 
 /**
  * Main code for Duke.
  */
-public class Duke {
+public class Duke extends Application {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
     private final Storage storage;
     private TaskList list;
     private final Parser p;
     private boolean isExiting;
 
-    public Duke(String filePath) {
-        storage = new Storage(filePath);
+    public Duke() {
+        storage = new Storage("data" + File.separator + "taskList.txt");
         try {
             list = new TaskList(storage.load());
         } catch (FileNotFoundException e) {
@@ -27,6 +43,24 @@ public class Duke {
         }
         p = new Parser(list);
         isExiting = false;
+    }
+
+    @Override
+    public void start(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void run() {
@@ -49,6 +83,6 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        new Duke("data" + File.separator + "taskList.txt").run();
+        new Duke().run();
     }
 }
