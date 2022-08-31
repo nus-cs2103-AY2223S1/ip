036d47f0f82bc7110a101224d14162f6f34c1fd5
Humphrey <48670655+bananamonkey4655@users.noreply.github.com>
2022-08-31 12:26:21 +0800

    Update src/main/java/duke/command/DeleteCommand.java
    Improve code readability
    
    Co-authored-by: lesterong <84223259+lesterong@users.noreply.github.com>

--- src/main/java/duke/command/DeleteCommand.java
+++ src/main/java/duke/command/DeleteCommand.java
@@ -19,8 +19,8 @@ public class DeleteCommand extends Command {
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
         String task = tasks.deleteTask(taskNumber);
-        ui.reply("Noted. I've removed this duke.task:\n " + task + "\nNow you have " +
-                tasks.length() + " tasks in the list");
+        ui.reply("Noted. I've removed this duke.task:\n " + task + "\nNow you have "
+                + tasks.length() + " tasks in the list");
         storage.saveTasks(tasks);
     }
 }
