fdc1e415cf7c3a7aede934ab3007ded488dcc29b
zzthian <zzthian@hotmail.com>
2022-08-31 12:07:21 +0800

    Able to close the program with bye

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,11 +1,18 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 import duke.Duke;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
@@ -28,6 +35,14 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        String openingMessage = "Hello, I am EksWhyZee!\nHow may I help you?\n";
+        Label text = new Label(openingMessage);
+        text.setStyle("-fx-font-weight:bold; -fx-font-size:20px;");
+        text.setPadding(new Insets(10, 0, 10, 200));
+        text.setTextAlignment(TextAlignment.CENTER);
+        dialogContainer.getChildren().addAll(
+                text
+        );
     }
 
     public void setDuke(Duke d) {
@@ -46,6 +61,18 @@ public class MainWindow extends AnchorPane {
                 DialogBox.getUserDialog(input, userImage),
                 DialogBox.getDukeDialog(response, dukeImage)
         );
+
+        if (input.equals("bye")) {
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    System.exit(0);
+                }
+            };
+            Timer timer = new Timer();
+            timer.schedule(task, 1000);
+        }
         userInput.clear();
+
     }
 }
