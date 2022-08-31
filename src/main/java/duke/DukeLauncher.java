fc87d3da9f51cc9c4daf5150bfd556dd8adc8e74
Julian Wong <e0725200@u.nus.edu>
2022-08-31 17:48:26 +0800

    setup JavaFX

---
+++ src/main/java/duke/DukeLauncher.java
@@ -0,0 +1,12 @@
+package duke;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class DukeLauncher {
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
+}
