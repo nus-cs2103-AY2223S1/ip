04ab5846935b40dbd1b01ce459e1cb5a2f5e59ea
rachaelsng <rachaelsng@u.nus.edu>
2022-09-01 01:16:48 +0800

    Added JavaDocs for Ui & TaskList

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -3,17 +3,35 @@ import duke.task.Task;
 
 import java.util.ArrayList;
 
+/**
+ * Contains and manages the list of tasks
+ */
 public class TaskList {
     private final ArrayList<Task> tasks;
 
+    /**
+     * Constructs an empty task list
+     */
     public TaskList() {
         tasks = new ArrayList<>();
     }
 
+    /**
+     * Constructs a task list containing the specified tasks
+     *
+     * @param tasks The specified tasks
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Returns the task at the specified index
+     *
+     * @param i Index of the task in the task list
+     * @return Task at the specified index
+     * @throws DukeException If index is out of bounds
+     */
     public Task get(int i) throws DukeException {
         try {
             return tasks.get(i);
@@ -22,10 +40,21 @@ public class TaskList {
         }
     }
 
+    /**
+     * Returns the size of the task list
+     *
+     * @return Size of task list
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Converts an ArrayList of Tasks to a list as a String
+     *
+     * @param tasks List of tasks
+     * @return List of tasks as a string
+     */
     public static String convertTasksToListString(ArrayList<Task> tasks) {
         StringBuilder list = new StringBuilder();
         for (int i = 0; i < tasks.size(); i++) {
@@ -35,10 +64,22 @@ public class TaskList {
         return list.toString();
     }
 
+    /**
+     * Adds task to task list
+     *
+     * @param task The specified task
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Deletes task from task list
+     *
+     * @param index The index of the specified task in the list
+     * @return Deleted task
+     * @throws DukeException If index is out of bounds
+     */
     public Task deleteTask(int index) throws DukeException {
         try {
             return tasks.remove(index);
@@ -47,6 +88,13 @@ public class TaskList {
         }
     }
 
+    /**
+     * Returns a task list with tasks on the specified date
+     *
+     * @param dateStr The specified date
+     * @return List of tasks with specified date
+     * @throws DukeException If date is blank or in the wrong format
+     */
     public TaskList getTasksOnDate(String dateStr) throws DukeException {
         ArrayList<Task> tasksOnDate = new ArrayList<>();
         for (Task t : tasks) {
@@ -57,6 +105,9 @@ public class TaskList {
         return new TaskList(tasksOnDate);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return convertTasksToListString(tasks);
