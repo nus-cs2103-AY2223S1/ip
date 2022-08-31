f55ddbf34e3b7ce1e7baf8c8427d435cd3da23ff
rachaelsng <rachaelsng@u.nus.edu>
2022-09-01 01:35:19 +0800

    Added JavaDocs for Command & Parser, added fullstops for JavaDocs

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -26,11 +26,11 @@ public class TaskList {
     }
 
     /**
-     * Returns the task at the specified index
+     * Returns the task at the specified index.
      *
-     * @param i Index of the task in the task list
-     * @return Task at the specified index
-     * @throws DukeException If index is out of bounds
+     * @param i Index of the task in the task list.
+     * @return Task at the specified index.
+     * @throws DukeException If index is out of bounds.
      */
     public Task get(int i) throws DukeException {
         try {
@@ -41,19 +41,19 @@ public class TaskList {
     }
 
     /**
-     * Returns the size of the task list
+     * Returns the size of the task list.
      *
-     * @return Size of task list
+     * @return Size of task list.
      */
     public int size() {
         return tasks.size();
     }
 
     /**
-     * Converts an ArrayList of Tasks to a list as a String
+     * Converts an ArrayList of Tasks to a list as a String.
      *
-     * @param tasks List of tasks
-     * @return List of tasks as a string
+     * @param tasks List of tasks.
+     * @return List of tasks as a string.
      */
     public static String convertTasksToListString(ArrayList<Task> tasks) {
         StringBuilder list = new StringBuilder();
@@ -65,20 +65,20 @@ public class TaskList {
     }
 
     /**
-     * Adds task to task list
+     * Adds task to task list.
      *
-     * @param task The specified task
+     * @param task The specified task.
      */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
     /**
-     * Deletes task from task list
+     * Deletes the specified task from task list.
      *
-     * @param index The index of the specified task in the list
-     * @return Deleted task
-     * @throws DukeException If index is out of bounds
+     * @param index The index of the specified task in the list.
+     * @return Deleted task.
+     * @throws DukeException If index is out of bounds.
      */
     public Task deleteTask(int index) throws DukeException {
         try {
@@ -89,11 +89,11 @@ public class TaskList {
     }
 
     /**
-     * Returns a task list with tasks on the specified date
+     * Returns a task list with tasks on the specified date.
      *
-     * @param dateStr The specified date
-     * @return List of tasks with specified date
-     * @throws DukeException If date is blank or in the wrong format
+     * @param dateStr The specified date.
+     * @return List of tasks with specified date.
+     * @throws DukeException If date is blank or in the wrong format.
      */
     public TaskList getTasksOnDate(String dateStr) throws DukeException {
         ArrayList<Task> tasksOnDate = new ArrayList<>();
