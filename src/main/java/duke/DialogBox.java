ad732b0af86f517ee06fb74e06a08ea9edbce13a
zylee348 <e0725436@u.nus.edu>
2022-08-31 19:23:36 +0800

    Flip dialog box

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -1,6 +1,9 @@
 package duke;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -10,6 +13,22 @@ public class DialogBox extends HBox {
     private Label text;
     private ImageView displayPicture;
 
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        return new DialogBox(l, iv);
+    }
+
+    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+        var db = new DialogBox(l, iv);
+        db.flip();
+        return db;
+    }
     public DialogBox(Label l, ImageView iv) {
         text = l;
         displayPicture = iv;
