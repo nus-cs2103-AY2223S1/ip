226224d38e32fbf034f0262957133064b0186c5a
seox123 <seanmenglaw2009@gmail.com>
2022-09-02 01:16:58 +0800

    Add ability to read date/time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,16 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
 public class Deadline extends Task {
     /**
      * A string the describes the due date/time of the deadline.
      */
-    private String dueBy;
+    private String dueByStr;
+    private LocalDate dueByDate = null;
+    private LocalTime dueByTime = null;
 
     /**
      * Constructor for a deadline.
@@ -11,15 +19,31 @@ public class Deadline extends Task {
      */
     public Deadline(String description, String dueBy) {
         super(description);
-        this.dueBy = dueBy;
+        try {
+            String[] dateTime = dueBy.split(" ");
+            this.dueByDate = LocalDate.parse(dateTime[0],
+                    DateTimeFormatter.ofPattern("uuuu-MM-dd").withResolverStyle(ResolverStyle.STRICT));
+            if (dateTime.length == 2) {
+                this.dueByTime = LocalTime.parse(dateTime[1],
+                        DateTimeFormatter.ofPattern("HHmm").withResolverStyle(ResolverStyle.STRICT));
+            }
+        } catch (DateTimeParseException e) {
+            this.dueByStr = dueBy;
+        }
     }
 
     /**
-     * Returns a string representation of thew deadline.
+     * Returns a string representation of the deadline.
      * @return a string representing the deadline
      */
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.dueBy);
+        String date = this.dueByDate != null
+                ? this.dueByDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                : this.dueByStr;
+        String time = this.dueByTime != null
+                ? String.format(" %s", this.dueByTime.format(DateTimeFormatter.ofPattern("K:mma")))
+                : "";
+        return String.format("[D]%s (by: %s%s)", super.toString(), date, time);
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,35 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
 public class Event extends Task {
     /**
      * A string the describes the date/time of the event.
      */
-    private String at;
+    private String dueAtStr;
+    private LocalDate dueAtDate = null;
+    private LocalTime dueAtTime = null;
 
     /**
      * Constructor for a deadline.
      * @param description the description of the deadline
-     * @param at the date/time of the event
+     * @param dueAt the date/time of the event
      */
-    public Event(String description, String at) {
+    public Event(String description, String dueAt) {
         super(description);
-        this.at = at;
+        try {
+            String[] dateTime = dueAt.split(" ");
+            this.dueAtDate = LocalDate.parse(dateTime[0],
+                    DateTimeFormatter.ofPattern("uuuu-MM-dd").withResolverStyle(ResolverStyle.STRICT));
+            if (dateTime.length == 2) {
+                this.dueAtTime = LocalTime.parse(dateTime[1],
+                        DateTimeFormatter.ofPattern("HHmm").withResolverStyle(ResolverStyle.STRICT));
+            }
+        } catch (DateTimeParseException e) {
+            this.dueAtStr = dueAt;
+        }
     }
 
     /**
@@ -20,6 +38,12 @@ public class Event extends Task {
      */
     @Override
     public String toString() {
-        return String.format("[E]%s (at: %s)", super.toString(), this.at);
+        String date = this.dueAtDate != null
+                ? this.dueAtDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                : this.dueAtStr;
+        String time = this.dueAtDate != null && this.dueAtTime != null
+                ? String.format(" %s", this.dueAtTime.format(DateTimeFormatter.ofPattern("K:mma")))
+                : "";
+        return String.format("[E]%s (at: %s%s)", super.toString(), date, time);
     }
 }
