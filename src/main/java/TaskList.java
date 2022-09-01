91095fa9ec10270f6b56586961b95a30e4ea1516
Charles Lim <charleslimjh@gmail.com>
2022-09-01 21:51:20 +0800

    remove print statements in TaskList

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -39,8 +39,6 @@ public class TaskList {
     public void mark(int userInput) {
         Task tmp = db.get(userInput - 1);
         tmp.setDone();
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println(tmp);
     }
 
     /**
@@ -50,8 +48,6 @@ public class TaskList {
     public void unmark(int userInput) {
         Task tmp = db.get(userInput - 1);
         tmp.setUndone();
-        System.out.println("OK, I've marked this task as undone:");
-        System.out.println(tmp);
     }
 
     /**
@@ -68,10 +64,6 @@ public class TaskList {
      */
     public void addEvent(Event task) {
         db.add(task);
-
-        System.out.println("Got it. I added this event:");
-        System.out.println("\t" + task);
-        System.out.printf("Now you have %d tasks in the list.%n", db.size());
     }
 
     /**
@@ -80,10 +72,6 @@ public class TaskList {
      */
     public void addDeadline(Deadline task) {
         db.add(task);
-
-        System.out.println("Got it. I added this deadline:");
-        System.out.println("\t" + task);
-        System.out.printf("Now you have %d tasks in the list.%n", db.size());
     }
 
     /**
@@ -92,18 +80,15 @@ public class TaskList {
      */
     public void delete(int userInput) {
         Task tmp = db.remove(userInput - 1);
-        System.out.println("OK, I've removed this task:");
-        System.out.println("\t" + tmp);
-        System.out.printf("Now you have %d tasks in the list.", db.size());
     }
 
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < db.size(); i++) {
-            res.append(i).append(". ").append(db.get(i).toString()).append("\n");
+            res.append(i+1).append(". ").append(db.get(i).toString()).append("\n");
         }
-        return res.toString();
+        return res.toString().trim();
     }
 
     public String[] toStringWritable() {
