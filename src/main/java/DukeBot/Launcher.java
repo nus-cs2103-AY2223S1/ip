914485256cfe322eaa8f0a35089dc17084488500
Chee Zhong Wei <cheezhongwei00@gmail.com>
2022-09-01 22:19:41 +0800

    Add JavaFX files to project

---
+++ src/main/java/DukeBot/Launcher.java
@@ -0,0 +1,9 @@
+package DukeBot;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
