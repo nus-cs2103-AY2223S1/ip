cd8aecc8a205d528623d2aaa0b0d33fcb706dca8
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-09-01 10:07:45 +0800

    misc changes

--- src/main/java/utils/FileIO.java
+++ src/main/java/utils/FileIO.java
@@ -1,6 +1,15 @@
 package utils;
 
+import task_classes.Task;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class FileIO {
+
+    private static String saveFilePath = "/data/saveFile.json";
+
     private static FileIO instance;
 
     // This is a singleton class
@@ -13,5 +22,18 @@ public class FileIO {
             instance = new FileIO();
         }
         return instance;
+    }
+
+    public Task[] readTaskList() {
+        try {
+            FileReader saveFileReader = new FileReader(saveFilePath);
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+
+    }
+
+    public void writeTaskList(ArrayList<Task> taskList) {
+
     }
 }
