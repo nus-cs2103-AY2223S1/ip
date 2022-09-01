eed7e9714820780e8ef6582e4c3f1fafc80f16c4
tsammeow <97447335+tsammeow@users.noreply.github.com>
2022-09-02 07:04:30 +0800

    Implement Graphical UI

---
+++ app/src/main/java/duke/DukeGuiLauncher.java
@@ -0,0 +1,9 @@
+package duke;
+
+import javafx.application.Application;
+
+public class DukeGuiLauncher {
+    public static void main(String[] args) {
+        Application.launch(DukeGui.class, args);
+    }
+}
--- app/src/main/java/duke/GraphicalUi.java
+++ app/src/main/java/duke/GraphicalUi.java
@@ -1,27 +1,113 @@
 package duke;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class GraphicalUi extends Ui {
-    private boolean isAlive;
+    private Stage stage;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image picture = new Image(this.getClass().getResourceAsStream("/catavatar1.png"));
 
     /**
      * Creates a new GUI handler.
      */
-    public GraphicalUi(InputAcceptor ia) {
+    public GraphicalUi(InputAcceptor ia, Stage stage) {
         super(ia);
+        this.stage = stage;
+
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 
     @Override
     public void runInputLoop() {
-        isAlive = true;
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        Label userText = new Label(userInput.getText());
+        dialogContainer.getChildren().add(
+                MessageBox.getUserDialog(userText, new ImageView(picture)));
+        processInput(userInput.getText());
+        userInput.clear();
     }
 
     @Override
     public void stopInputLoop() {
-        isAlive = false;
+        sendButton.setDisable(true);
+        userInput.setDisable(true);
     }
 
     @Override
     public void respond(String response) {
-        //
+        Label dukeText = new Label(response);
+        MessageBox.getDukeDialog(dukeText, new ImageView(picture));
+        dialogContainer.getChildren().add(
+                MessageBox.getDukeDialog(dukeText, new ImageView(picture)));
     }
 }
