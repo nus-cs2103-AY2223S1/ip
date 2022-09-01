648ed7e848c9b26aa694808ab2f9664023e4f1e1
lolfoollorS <Lolfoollor47@gmail.com>
2022-09-02 02:58:49 +0800

    Add JavaDocs to all non main classes.

--- src/main/java/clevernotbot/AddEventCommand.java
+++ src/main/java/clevernotbot/AddEventCommand.java
@@ -1,18 +1,35 @@
 package clevernotbot;
 
-public class AddCommandEvent extends Command {
+/**
+ * Represents a command used to add an Event Task.
+ */
+public class AddEventCommand extends Command {
 
-    public AddCommandEvent(String commandName, boolean exit) {
+    /**
+     * Constructor for the AddEventCommand.
+     *
+     * @param commandName Description of Command.
+     * @param exit Checking if program intends to exit.
+     */
+    public AddEventCommand(String commandName, boolean exit) {
         super(commandName, exit);
     }
 
+    /**
+     * Runs the add event command.
+     *
+     * @param tasks The task list used to store all tasks.
+     * @param textBox UI for the textbox.
+     * @param storage The data where it is stored.
+     * @throws CleverNotBotException Gives an exception when event's description is empty or not contain /at.
+     */
     public void run(TaskList tasks, UI textBox, Storage storage) throws CleverNotBotException {
         String[] desc = getCommandName().split(" ");
         try{
             if (desc.length == 1) {
-                throw new CleverNotBotException("Please fill in the description of Task.Event!", textBox);
+                throw new CleverNotBotException("Please fill in the description of Event!", textBox);
             } else if (!getCommandName().contains("/at")) {
-                throw new CleverNotBotException("Please include a /at in your description of Task.Deadline! ", textBox);
+                throw new CleverNotBotException("Please include a /at in your description of Deadline! ", textBox);
             } else {
                 String searchWord = " /at";
                 int start = "event ".length();
@@ -29,7 +46,7 @@ public class AddCommandEvent extends Command {
                         , newTask.toString(), tasks.getSize()));
             }
         } catch(CleverNotBotException e){
-            throw new CleverNotBotException("Task.Event description must not be empty or must contain /at!", textBox);
+            throw new CleverNotBotException("Event description must not be empty or must contain /at!", textBox);
         }
 
     }
--- src/main/java/clevernotbot/UnmarkCommand.java
+++ src/main/java/clevernotbot/UnmarkCommand.java
@@ -1,11 +1,28 @@
 package clevernotbot;
 
+/**
+ * Represents a command to unmark a task.
+ */
 public class UnmarkCommand extends Command {
 
+    /**
+     * Constructor for the unMarkCommand.
+     *
+     * @param commandName description of command.
+     * @param exit checking if program intends to exit.
+     */
     public UnmarkCommand(String commandName, boolean exit){
         super(commandName,exit);
     }
 
+    /**
+     * Runs the mark command to unmark the task as completed.
+     *
+     * @param tasks The task list used to store all tasks.
+     * @param textBox UI for the textbox.
+     * @param storage The data where it is stored.
+     * @throws CleverNotBotException Gives an exception.
+     */
     @Override
     public void run(TaskList tasks, UI textBox, Storage storage) throws CleverNotBotException {
         int number = Integer. parseInt(getCommandName().split(" ")[1]) - 1; //mark 3 -> 3, because number 3 is actually idx 2
