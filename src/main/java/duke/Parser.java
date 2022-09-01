5150a2e89fdf7faa59a2344afb281bb9fe0e402a
Zsigmond <e0725064@u.nus.edu>
2022-09-01 22:53:07 +0800

    refactor: list imports instead of using wildcard in Parser

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,6 +1,15 @@
 package duke;
 
-import duke.command.*;
+import duke.command.AddDeadlineCommand;
+import duke.command.AddEventCommand;
+import duke.command.AddTodoCommand;
+import duke.command.ByeCommand;
+import duke.command.Command;
+import duke.command.DeleteCommand;
+import duke.command.FindCommand;
+import duke.command.ListCommand;
+import duke.command.UpdateMarkCommand;
+import duke.command.UpdateUnmarkCommand;
 
 import java.util.Objects;
 
