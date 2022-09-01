bda30d06817a2688cb939f9c69dc522a5c2893a2
Prearo <kohshanghui@gmail.com>
2022-09-01 23:27:39 +0800

    Fix emojis not displaying in GUI

--- src/main/java/duke/messages/ExceptionMessages.java
+++ src/main/java/duke/messages/ExceptionMessages.java
@@ -5,12 +5,18 @@ package duke.messages;
  */
 public final class ExceptionMessages {
 
-    public static String EMPTY_TASK_DESCRIPTION = "☹ OOPS!!! The description of a %s cannot be empty.\n";
-    public static String EMPTY_TASK_TIME = "☹ OOPS!!! The date/time of a %s cannot be empty.\n";
-    public static String INVALID_FORMAT = "☹ OOPS!!! Please indicate a description and date/time separated by %s\n";
-    public static String UNSUPPORTED_ACTION = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n";
-    public static String INVALID_INDEX_FORMAT = "☹ OOPS!!! Please indicate only an index for the command\n";
+    private static final String frowningFace = String.format("%c", 0x1F641);
+
+    public static String EMPTY_TASK_DESCRIPTION = frowningFace + " OOPS!!! The description of a %s cannot be empty.\n";
+    public static String EMPTY_TASK_TIME = frowningFace + " OOPS!!! The date/time of a %s cannot be empty.\n";
+    public static String INVALID_FORMAT = frowningFace
+            + " OOPS!!! Please indicate a description and date/time separated by %s\n";
+    public static String UNSUPPORTED_ACTION = frowningFace
+            + " OOPS!!! I'm sorry, but I don't know what that means :-(\n";
+    public static String INVALID_INDEX_FORMAT = frowningFace
+            + " OOPS!!! Please indicate only an index for the command\n";
     public static String LOAD_ERROR = "Error loading data from storage\n";
-    public static String INVALID_FIND_FORMAT = "☹ OOPS!!! Please indicate the string to scan the task list for\n";
+    public static String INVALID_FIND_FORMAT = frowningFace
+            +" OOPS!!! Please indicate the string to scan the task list for\n";
 
 }
