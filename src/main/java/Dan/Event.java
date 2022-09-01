cf48b2dedda44d2989263e0660da377d425c79f6
lulucopter <lzydaniel.lee@gmail.com>
2022-09-02 00:57:42 +0800

    Add javadocs for the ToDo, Event and Deadline task types

--- src/main/java/Dan/Event.java
+++ src/main/java/Dan/Event.java
@@ -4,11 +4,21 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.LocalDateTime;
 
+/**
+ * One of the task types. A task associated with a timing for which it's happening
+ */
 public class Event extends Task {
     private static final String ICON = "E";
     protected LocalDateTime dateString;
 
-    Event(String description, String dateString) throws DateTimeParseException {
+    /**
+     * Creates an Event with its associated description and date.
+     *
+     * @param description The description of the task
+     * @param dateString A String representing a date-time in the pattern "dd/MM/yyyy HHmm"
+     * @throws DateTimeParseException If the date-time string is not part of its regular pattern
+     */
+    public Event(String description, String dateString) throws DateTimeParseException {
         super(description);
         this.dateString = LocalDateTime.parse(dateString, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
     }
