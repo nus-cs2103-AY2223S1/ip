62afe570ffecfac6c38528b9eac2a5afda110405
lolfoollorS <Lolfoollor47@gmail.com>
2022-09-01 16:56:54 +0800

    Add Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,39 @@
-public class Deadline extends Task{
-    private String by;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String name, boolean completed, String by){
+public class Deadline extends Task{
+    private String dateTime;
+    private LocalDateTime date;
+
+    public Deadline(String name, boolean completed, String dateTime){
         super(name,completed);
-        this.by = by;
+        this.dateTime = dateTime;
+        applyDate(dateTime);
+
+    }
+
+    private void applyDate(String dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        this.date = LocalDateTime.parse(dateTime, formatter);
+    }
+
+    public String getDateTime(){
+        return date.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm"));
+    }
+
+    public String getDate(){
+        return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public Task toggleCompleted(){
-        return new Deadline(getName(),!isCompleted(),by);
+        return new Deadline(getName(),!isCompleted(),dateTime);
+
     }
 
     @Override
     public String toString(){
-        return String.format("[D][%s] %s (by: %s)",checkMarked(),getName(),by);
+        return String.format("[D][%s] %s (by: %s)",checkMarked(),getName(),dateTime);
     }
+
 }
