4c4be541c8ae0787afccd6a31a927f8dc4e8aae9
Zsigmond <e0725064@u.nus.edu>
2022-09-02 01:20:04 +0800

    style: fix checkstyle violations in duke.task

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -3,9 +3,19 @@ package duke.task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * A Deadline is a Task that contains a deadline to be completed by.
+ */
 public class Deadline extends Task {
     private LocalDateTime deadline;
 
+    /**
+     * Constructor for deadline.
+     *
+     * @param isDone boolean denoting whether Deadline should be marked.
+     * @param text description of task.
+     * @param deadline date and time for Deadline to be completed by.
+     */
     public Deadline(boolean isDone, String text, LocalDateTime deadline) {
         super(isDone, text);
         this.deadline = deadline;
@@ -13,6 +23,8 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return String.format("[D] %s (by: %s)", super.toString(), this.deadline.format(DateTimeFormatter.ofPattern("dd MMM yyyy, HHmm")));
+        return String.format("[D] %s (by: %s)",
+                super.toString(),
+                this.deadline.format(DateTimeFormatter.ofPattern("dd MMM yyyy, HHmm")));
     }
 }
