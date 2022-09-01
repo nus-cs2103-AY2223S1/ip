2256652ef32bc4d929a984be9975b3f1f5fe84fd
Chua Yu Xuan Jordan <jcyx1999@gmail.com>
2022-09-01 22:05:09 +0800

    Update MainWindow.java

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -21,11 +21,9 @@ public class MainWindow extends AnchorPane {
     private TextField userInput;
     @FXML
     private Button sendButton;
-
     private Duke duke;
-
-        private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-        private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     @FXML
     public void initialize() {
