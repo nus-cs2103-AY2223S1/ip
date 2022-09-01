3514a18945c8a4424ae101544e20a1f544f44cbc
zbz-lvlv <zbz.lvlv@gmail.com>
2022-09-01 16:49:31 +0800

    Add help functionality
    Users can know how to use the program with the help functionality.
    Initially, there were no guidance on which command to type, and the
    input format of each of the commands. Users needed to reply on memory.
    Now, users can have a quick reference in-app to find out the command
    they want.
    
    The help functionality is added in two places:
    - As a button at the bottom right corner
    - As a command 'help'

--- src/main/java/maria/command/CommandUnmarkTask.java
+++ src/main/java/maria/command/CommandUnmarkTask.java
@@ -22,6 +22,10 @@ public class CommandUnmarkTask extends Command {
     @Override
     public String execute(TaskManager taskManager) {
 
+        if (this.index >= taskManager.getTaskList().size() || this.index <= 0) {
+            return "Index is invalid.";
+        }
+
         Task task = taskManager.getTaskList().get(this.index);
 
         taskManager.getTaskList().get(taskManager.getTaskList().indexOf(task)).setIsDone(false);
