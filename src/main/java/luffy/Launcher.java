24b234c480dcb840c81722f37bf907aedc629c08
SilasTSL <silastaysl@gmail.com>
2022-09-01 15:13:19 +0800

    Basic GUI without functionality

---
+++ src/main/java/luffy/Launcher.java
@@ -0,0 +1,11 @@
+package luffy;
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Luffy.class, args);
+    }
+}
--- src/main/java/luffy/Luffy.java
+++ src/main/java/luffy/Luffy.java
@@ -8,27 +8,47 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.regex.Pattern;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Luffy is a todolist program. Built for CS2103T Individual Project 2022 S1.
  *
  * @author Silas Tay A0233425M
  */
-public class Luffy {
+public class Luffy extends Application{
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+    private Parser parser = new Parser();
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/luffy.jpeg"));
 
     private static final String FILE_PATH = "./data/data.txt";
 
     /**
      * Construtor for Luffy object.
-     *
-     * @param filePath File path to data file
      */
-    public Luffy(String filePath) {
+    public Luffy() {
         this.ui = new Ui();
-        this.storage = new Storage(filePath);
+        this.storage = new Storage(FILE_PATH);
         try {
             this.tasks = new TaskList(storage.load());
         } catch (DukeException e) {
@@ -57,12 +77,98 @@ public class Luffy {
         }
     }
 
+    @Override
+    public void start(Stage stage) {
+
+        //Container for content of the chat to scroll:
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Luffy");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        //Step 3. Add functionality to handle user input.
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+
+
+    }
+
+    /**
+     * Iteration 2:
+            * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+            * the dialog container. Clears the user input after processing.
+            */
+    private void handleUserInput() {
+        Label userText = new Label(userInput.getText());
+        Label dukeText = new Label(userInput.getText());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
+        );
+        userInput.clear();
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    private String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
     /**
      * Main method for Luffy.
      *
      * @param args User commandline input
      */
     public static void main(String[] args) {
-        new Luffy(FILE_PATH).run();
+        new Luffy().run();
     }
 }
