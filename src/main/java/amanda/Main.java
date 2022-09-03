ed1309d0d6de2df7ad4ab151ab3b6866297b4264
hoang227 <hoangg227@gmail.com>
2022-09-03 17:07:14 +0800

    updated description of iP using GFMD

--- src/main/java/amanda/Main.java
+++ src/main/java/amanda/Main.java
@@ -2,6 +2,7 @@ package main.java.amanda;
 
 public class Main {
     public static void main(String[] args) {
+        Application.launch(MainApp.class, args);
         Amanda amanda = new Amanda("./src/main/java/data/amanda.txt");
         amanda.run();
     }
