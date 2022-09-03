2937a69f16b76494e88074269b8fd75053603df8
Shashank Acharya <acharya.s@icloud.com>
2022-09-03 13:17:05 +0800

    Modify Parser to return Command object

--- src/main/java/duke/main/Duke.java
+++ src/main/java/duke/main/Duke.java
@@ -1,14 +1,10 @@
 package duke.main;
 
+import duke.command.Command;
 import duke.exception.DukeException;
 import duke.parser.Parser;
 import duke.storage.Storage;
-import duke.ui.Message;
 import duke.ui.Ui;
-import duke.task.Task;
-import duke.task.Todo;
-import duke.task.Event;
-import duke.task.Deadline;
 import duke.task.TaskList;
 
 public class Duke {
@@ -19,53 +15,17 @@ public class Duke {
     // INSTANCE VARIABLES
     private final TaskList tasks;
     private final Storage storage;
-    private final Ui ui;
 
     public Duke() {
-        this.ui = new Ui();
         this.storage = new Storage(saveFilePath);
         this.tasks = new TaskList(storage.load());
     }
 
     public String getResponse(String input) {
         try {
-            Parser currentCommand = new Parser(input);
-            String direction = currentCommand.getDirection();
-            String meta = currentCommand.getMeta();
-            switch (direction) {
-                case "bye":
-                    return Ui.getTerminationString();
-                case "list":
-                    return Ui.getTaskListString(this.tasks);
-                case "unmark":
-                    Task unmarked = this.tasks.unmark(currentCommand.extractIndex());
-                    return Ui.getTaskStatusString(Message.UNMARK, unmarked);
-                case "mark":
-                    Task marked = this.tasks.mark(currentCommand.extractIndex());
-                    return Ui.getTaskStatusString(Message.MARK, marked);
-                case "delete":
-                    Task deleted = this.tasks.delete(currentCommand.extractIndex());
-                    return Ui.getTaskStatusString(Message.DELETE, deleted);
-                case "todo":
-                    if (meta == null) throw new DukeException("Description cannot be empty");
-                    Task todo = this.tasks.add(new Todo(meta));
-                    return Ui.getTaskStatusString(Message.ADDED, todo);
-                case "deadline":
-                    if (meta == null) throw new DukeException("Description cannot be empty");
-                    Task deadline = this.tasks.add(new Deadline(meta));
-                    return Ui.getTaskStatusString(Message.ADDED, deadline);
-                case "event":
-                    if (meta == null) throw new DukeException("Description cannot be empty");
-                    Task event = this.tasks.add(new Event(meta));
-                    return Ui.getTaskStatusString(Message.ADDED, event);
-                case "find":
-                    if (meta == null) throw new DukeException("Query cannot be empty");
-                    TaskList filtered = this.tasks.filter(meta);
-                    if (filtered.getSize() == 0) throw new DukeException("No results found");
-                    return Ui.getTaskListString(filtered);
-                default:
-                    throw new DukeException(Message.INVALID);
-            }
+            Command command = Parser.getCommand(input);
+            String output = command.execute(this.tasks, this.storage);
+            return output;
         } catch (DukeException e) {
             return Ui.getErrorMessageString(e);
         }
--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -1,50 +1,109 @@
 package duke.parser;
 
+import duke.command.Command;
+import duke.command.ExitCommand;
+import duke.command.ListCommand;
+import duke.command.UnmarkCommand;
+import duke.command.MarkCommand;
+import duke.command.DeleteCommand;
+import duke.command.AddDeadlineCommand;
+import duke.command.AddEventCommand;
+import duke.command.AddTodoCommand;
+import duke.command.FindCommand;
 import duke.exception.DukeException;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
 import duke.ui.Message;
 
-public class Parser {
-    private final String direction;
-    private final String meta;
+import java.time.LocalDateTime;
 
-    public Parser(String cmd) {
-        String[] tokens = cmd.split("\\s+", 2);
-        this.direction = tokens[0];
-        this.meta = tokens.length > 1 ? tokens[1] : null;
+public class Parser {
+    public static Command getCommand(String input) throws DukeException {
+        String[] commandTokens = input.split("\\s+", 2);
+        String direction = commandTokens[0];
+        String meta = commandTokens.length > 1 ? commandTokens[1] : null;
+        switch (direction) {
+            case "bye":
+                return new ExitCommand();
+            case "list":
+                return new ListCommand();
+            case "unmark":
+                int unmarkIndex = extractIndex(meta);
+                return new UnmarkCommand(unmarkIndex);
+            case "mark":
+                int markIndex = extractIndex(meta);
+                return new MarkCommand(markIndex);
+            case "delete":
+                int deleteIndex = extractIndex(meta);
+                return new DeleteCommand(deleteIndex);
+            case "deadline":
+                String[] deadlineTokens = getTaskTokens(meta, Message.NO_BY_ERROR);
+                return new AddDeadlineCommand(deadlineTokens[0], parseDateTime(deadlineTokens[1]));
+            case "event":
+                String[] eventTokens = getTaskTokens(meta, Message.NO_AT_ERROR);
+                return new AddEventCommand(eventTokens[0], parseDateTime(eventTokens[1]));
+            case "todo":
+                validateMetaNullity(meta);
+                return new AddTodoCommand(meta);
+            case "find":
+                validateMetaNullity(meta);
+                return new FindCommand(meta);
+            default:
+                throw new DukeException(Message.INVALID);
+        }
     }
 
-    /**
-     * Returns the integer index from the meta property.
-     * Strips non-numeric characters and casts value to int.
-     *
-     * @return Integer index from the command
-     * @throws DukeException If meta does not contain an integer.
-     */
-    public int extractIndex() throws DukeException {
+    public static Task getTask(String input) throws DukeException {
+        String[] commandTokens = input.split("\\s+", 2);
+        String direction = commandTokens[0];
+        String meta = commandTokens.length > 1 ? commandTokens[1] : null;
+        switch (direction) {
+            case "deadline":
+                String[] deadlineTokens = getTaskTokens(meta, Message.NO_BY_ERROR);
+                return new Deadline(deadlineTokens[0], parseDateTime(deadlineTokens[1]));
+            case "event":
+                String[] eventTokens = getTaskTokens(meta, Message.NO_AT_ERROR);
+                return new Event(eventTokens[0], parseDateTime(eventTokens[1]));
+            case "todo":
+                validateMetaNullity(meta);
+                return new Todo(meta);
+            default:
+                throw new DukeException("Not a valid task command.");
+        }
+    }
+
+    private static void validateMetaNullity(String meta) throws DukeException {
+        if (meta == null) {
+            throw new DukeException(Message.DESCRIPTION_EMPTY);
+        }
+    }
+
+    private static void validateTokens(String[] tokens, Message invalidMsg) throws DukeException {
+        if (tokens.length < 2) {
+            throw new DukeException(invalidMsg);
+        }
+    }
+
+    private static String[] getTaskTokens(String meta, Message invalidMsg) throws DukeException {
+        validateMetaNullity(meta);
+        String[] tokens = meta.split("\\s*/by\\s*", 2);
+        validateTokens(tokens, invalidMsg);
+        return tokens;
+    }
+
+    private static LocalDateTime parseDateTime(String timeString) throws DukeException {
+        return LocalDateTime.now();
+    }
+
+    private static int extractIndex(String meta) throws DukeException {
+        validateMetaNullity(meta);
         try {
-            return Integer.parseInt(this.meta.replaceAll("[^0-9]", ""));
+            return Integer.parseInt(meta.replaceAll("[^0-9]", ""));
         } catch (NumberFormatException e) {
             throw new DukeException(Message.TASK_INDEX_ERROR);
         }
     }
 
-    /**
-     * Returns the direction part of the command.
-     * The direction is the phrase that defines what the command does, e.g. todo, bye, etc.
-     *
-     * @return Direction of command.
-     */
-    public String getDirection() {
-        return this.direction;
-    }
-
-    /**
-     * Returns the meta part of the command.
-     * The meta is the portion of the command that contains the description, date, index, etc.
-     *
-     * @return Meta of command.
-     */
-    public String getMeta() {
-        return this.meta;
-    }
 }
