b1f7527b1445638889a6faf7d6c04a56f64f112b
myangat <mingyangy31@gmail.com>
2022-09-03 19:47:00 +0800

    Add the class FindTaskCommand

---
+++ src/main/java/Duke/Commands/FindTaskCommand.java
@@ -0,0 +1,10 @@
+package Duke.Commands;
+
+import Duke.Exceptions.DukeException;
+
+public class FindTaskCommand extends UserCommand{
+    @Override
+    public String execute() throws DukeException {
+        return null;
+    }
+}
