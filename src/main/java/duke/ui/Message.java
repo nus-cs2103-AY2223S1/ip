66faa95b2fc2d960d7195ab5a94d383ce15e0c81
Shashank Acharya <acharya.s@icloud.com>
2022-09-03 19:05:50 +0800

    Add messages for snooze command

--- src/main/java/duke/ui/Message.java
+++ src/main/java/duke/ui/Message.java
@@ -5,18 +5,21 @@ public enum Message {
     UNMARK("Very well, this task is now undone:"),
     DELETE("Very well, task deleted:"),
     ADDED("Very well, you have added another task:"),
+    SNOOZED("Very well, you have snoozed this task:"),
     INVALID("I struggle to interpret your command."),
     GREETING("Greetings, I'm Lurch.\nYou rang?"),
     EMPTY("There is nothing in your agenda."),
     BYE("Have a lovely day with dark and cloudy skies."),
     ERROR("Oh bother!"),
     OUT_OF_BOUNDS_ERROR("The number you have given me is too high or too low"),
-    TASK_INDEX_ERROR("That is not the correct way to write an index"),
+    TASK_INDEX_ERROR("That is not the correct way to write a number"),
     NO_AT_ERROR("Use \"/at\" to tell me when the event is, please"),
     NO_BY_ERROR("Use \"/by\" to tell me when the event is, please"),
+    NO_FOR_ERROR("Use \"/for\" to tell me how long to snooze, please"),
     DATE_FORMAT_ERROR("That is not how you write a date"),
     NO_RESULTS_FOUND("I found nothing matching your request."),
-    DESCRIPTION_EMPTY("You will need to describe your task.");
+    DESCRIPTION_EMPTY("You will need to describe your task."),
+    INVALID_SNOOZE_TYPE("You have not entered a valid snooze type.");
 
     private String value;
 
