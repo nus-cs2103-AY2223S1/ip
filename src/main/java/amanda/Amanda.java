0aea7b948fb3af86e1c335ebd5e3a09c93854035
hoang227 <hoangg227@gmail.com>
2022-09-03 15:38:45 +0800

    no message

--- src/main/java/amanda/Amanda.java
+++ src/main/java/amanda/Amanda.java
@@ -51,9 +51,4 @@ public class Amanda {
         }
         sc.close();
     }
-
-    public static void main(String[] args) {
-        Amanda amanda = new Amanda("./src/main/java/data/amanda.txt");
-        amanda.run();
-    }
 }
---
+++ src/main/java/amanda/Main.java
@@ -0,0 +1,8 @@
+package main.java.amanda;
+
+public class Main {
+    public static void main(String[] args) {
+        Amanda amanda = new Amanda("./src/main/java/data/amanda.txt");
+        amanda.run();
+    }
+}
