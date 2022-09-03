a0a02d6331753d9a1d353bd0f53eadada7532ef4
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2022-09-03 12:33:48 +0800

    Set up Duke GUI skeleton

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,7 +3,11 @@ package duke;
 import duke.ui.Ui;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -12,6 +16,12 @@ import javafx.stage.Stage;
 public class Duke extends Application {
 
     private final Ui ui;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
 
     /**
      * Constructs a Duke object and creates a new Ui object for user interaction
@@ -30,8 +40,21 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage stage){
-        Label helloWorld = new Label("Hello World!");
-        Scene scene = new Scene(helloWorld);
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
         stage.setScene(scene);
         stage.show();
 
