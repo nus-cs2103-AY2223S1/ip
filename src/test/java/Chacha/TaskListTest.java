c22525026d5023fc15e23563df8f3576c7b78e02
alexandermula <alexandersinghabdullah@gmail.com>
2022-09-04 18:30:41 +0800

    add junit tests

---
+++ src/test/java/Chacha/TaskListTest.java
@@ -0,0 +1,29 @@
+package Chacha;
+
+import org.junit.jupiter.api.Test;
+
+import chacha.TaskList;
+import chacha.tasks.Task;
+import chacha.tasks.Todo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void addTaskTest() {
+        TaskList tasks = new TaskList();
+        Task testTask = new Todo("testTask");
+        tasks.add(testTask);
+        Task task = tasks.get(0);
+        assertEquals("testTask", task.getDescription());
+    }
+
+    @Test
+    public void getTaskTest() {
+        TaskList tasks = new TaskList();
+        Task testTask = new Todo("testTask");
+        tasks.add(testTask);
+        Task task = tasks.get(0);
+        assertEquals("testTask", task.getDescription());
+    }
+}
