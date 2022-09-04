75b0744eda8876497a47aed0b6e9b5fed8bd6be0
LAPTOP-Q59MTK9C\Pokezard <e0538156@u.nus.edu>
2022-09-05 00:47:20 +0800

    gradle working

---
+++ src/test/java/duke/tasks/TaskTest.java
@@ -0,0 +1,13 @@
+package duke.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void markDone() {
+        assertEquals(1,1);
+    }
+}
