04afd36f1357d63df5478a091bf07dc813fab09a
Ezra <ezratio15195@gmail.com>
2022-09-05 00:30:49 +0800

    Fix date issue in unit test

--- src/test/java/henry/ParserTest.java
+++ src/test/java/henry/ParserTest.java
@@ -15,8 +15,8 @@ public class ParserTest {
     public void testParser() {
         Parser parser = new Parser();
         TaskListStub taskList = new TaskListStub(new ArrayList<>());
-        String command = "event event stub /at 01-01-2000 00:00";
-        String desired = "OK. I ADDED THIS TASK TO MY LIST:\n [E][ ] event stub (at: 01-01-2000 00:00)";
+        String command = "event event stub /at 12-12-2050 00:00";
+        String desired = "OK. I ADDED THIS TASK TO MY LIST:\n [E][ ] event stub (at: 12-12-2050 00:00)";
         Command c = parser.parseCommand(command);
         c.setData(taskList);
         assertEquals(c.execute().toString(), desired);
