6115f5cede1d12f8f1336bd1694a7c4f1fe712d7
myangat <mingyangy31@gmail.com>
2022-09-05 00:52:50 +0800

    Level-10: GUI, JavaFX

---
+++ src/main/java/Duke/Launcher.java
@@ -0,0 +1,12 @@
+package Duke;
+import javafx.application.Application;
+
+/**
+ * A Launcher class to workaround classpath issues.
+ */
+public class Lanuncher {
+
+    public static void main(String[] args) {
+            Application.launch(Duke.class, args);
+    }
+}
