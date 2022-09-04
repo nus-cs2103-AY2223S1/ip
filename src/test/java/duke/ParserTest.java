4efd6d9b1dacb6c65ee1bd447d51ebac608805f9
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-09-04 21:14:26 +0800

    Fix ParserTest test cases

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -13,6 +13,11 @@ class ParserTest {
     }
 
     @Test
+    void parseInput_incompleteDateTime_missingInputExceptionThrown() {
+        assertThrows(MissingDukeInputException.class, () -> Parser.parseInput("deadline test /by 2000-12-31"));
+    }
+
+    @Test
     void parseInput_forbiddenCharacter_bannedExceptionThrown() {
         assertThrows(BannedDukeCharacterException.class, () -> Parser.parseInput("Something with | in it"));
     }
@@ -21,11 +26,5 @@ class ParserTest {
     void parseInput_correctDateline_success() {
         assertArrayEquals(new String[] {"deadline", "test", "2000-12-31", "00:00", "", ""},
                 Parser.parseInput("deadline test /by 2000-12-31 00:00"));
-    }
-
-    @Test
-    void parseInput_badDateline_successWithoutCrash() {
-        assertArrayEquals(new String[] {"deadline", "test", "2000-12-31", "", "", ""},
-                Parser.parseInput("deadline test /by 2000-12-31"));
     }
 }
