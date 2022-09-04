141ea84b948e7dbf20a05820497c80f7d041cf3e
teoyuqi <teo.yuqi@gmail.com>
2022-09-04 19:43:42 +0800

    Create `Enum` type to indicate priority levels

---
+++ src/main/java/duke/task/Level.java
@@ -0,0 +1,10 @@
+package duke.task;
+
+/**
+ * Defines the {@code Level} type.
+ * {@code Level} is an {@code enum} type which can
+ * take on values "LOW", "MEDIUM" or "HIGH".
+ */
+public enum Level {
+    LOW, MEDIUM, HIGH
+}
