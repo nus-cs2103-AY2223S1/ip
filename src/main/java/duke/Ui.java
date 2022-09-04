797e31e4f91ec770238f9cc411d3d470250e87b3
Ming Quan <chua_ming_quan@live.com>
2022-09-04 12:40:31 +0800

    Add help command in Ui
    Gives user more idea on how the product works
    
    Lists the collection of commands that the user will be able to use.
    For example, commands such as 'list' that are commonly used are added.

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -20,24 +20,14 @@ public class Ui {
             switch(userInput.split(" ")[0]) {
             case "bye":
                 return "Bye. Hope to see you again soon!";
-            case "list":
-                return myTasks.listTasks();
-            case "mark":
-                toPrint = myTasks.markTask(Integer.parseInt(userInput.split(" ")[1]) - 1);
-                myTasks.saveTasks();
-                return toPrint;
-            case "unmark":
-                toPrint = myTasks.unmarkTask(Integer.parseInt(userInput.split(" ")[1]) - 1);
-                myTasks.saveTasks();
-                return toPrint;
-            case "todo":
-                toPrint = myTasks.addTask(new Todo(userInput.substring(5)));
-                myTasks.saveTasks();
-                return toPrint;
             case "deadline":
                 toPrint = myTasks.addTask(new Deadline(
                     userInput.substring(9).split("/by")[0], LocalDate.parse(
                     userInput.split("/by ")[1])));
+                myTasks.saveTasks();
+                return toPrint;
+            case "delete":
+                toPrint = myTasks.removeTask(Integer.parseInt(userInput.split(" ")[1]) - 1);
                 myTasks.saveTasks();
                 return toPrint;
             case "event":
@@ -49,8 +39,20 @@ public class Ui {
             case "find":
                 toPrint = myTasks.findTasks(userInput.substring(5));
                 return toPrint;
-            case "delete":
-                toPrint = myTasks.removeTask(Integer.parseInt(userInput.split(" ")[1]) - 1);
+            case "help":
+                return getHelpMessage();
+            case "list":
+                return myTasks.listTasks();
+            case "mark":
+                toPrint = myTasks.markTask(Integer.parseInt(userInput.split(" ")[1]) - 1);
+                myTasks.saveTasks();
+                return toPrint;
+            case "todo":
+                toPrint = myTasks.addTask(new Todo(userInput.substring(5)));
+                myTasks.saveTasks();
+                return toPrint;
+            case "unmark":
+                toPrint = myTasks.unmarkTask(Integer.parseInt(userInput.split(" ")[1]) - 1);
                 myTasks.saveTasks();
                 return toPrint;
             default:
@@ -65,5 +67,20 @@ public class Ui {
      */
     public String startMessage() {
         return welcomeMessage;
+    }
+
+    public String getHelpMessage() {
+        String helpMessage = "Here are the following commands you can use!\n";
+        helpMessage += "list: List all tasks\n";
+        helpMessage += "todo <taskName>: Add a new task\n";
+        helpMessage += "event <eventName> /at <date>: Add a new event\n";
+        helpMessage += "deadline <eventName> /by <date>: Add a new deadline\n";
+        helpMessage += "find <itemName>: Look for an item\n";
+        helpMessage += "delete <itemNumber>: Delete an item from list\n";
+        helpMessage += "mark <itemNumber>: Mark an item as done\n";
+        helpMessage += "unmark <itemNumber>: Mark an item as undone\n";
+        helpMessage += "bye: Exit the application\n";
+
+        return helpMessage;
     }
 }
