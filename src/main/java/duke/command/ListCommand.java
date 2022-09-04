6148839a29b75a4be8389c53f29f46f8cfeb84e7
Koh Chen Huang, Samuel <samsamsailorman@gmail.com>
2022-09-04 13:44:50 +0800

    Use streams for displaying of TaskList
    Iterating through the TaskList requires multiple loops to properly
    format the string output.
    
    Let's use a sequence of stream operations to perform string
    concatenation for the Tasks in the TaskList.
    
    Using stream operations is preferable as it condenses the code and
    increases readability.

--- src/main/java/duke/command/ListCommand.java
+++ src/main/java/duke/command/ListCommand.java
@@ -1,5 +1,6 @@
 package duke.command;
 
+import duke.DukeException;
 import duke.Storage;
 import duke.TaskList;
 import duke.Ui;
@@ -20,9 +21,13 @@ public class ListCommand extends Command {
      * @param ui The Ui dealing with interactions with the user.
      * @param storage The Storage dealing with loading tasks from the file and saving tasks in the file.
      * @return The TaskList display.
+     * @throws DukeException If there are no Task(s) found.
      */
     @Override
-    public String execute(TaskList tasks, Ui ui, Storage storage) {
+    public String execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        if (tasks.isEmpty()) {
+            throw new DukeException("You currently have no tasks in your list.");
+        }
         return ui.showList(tasks);
     }
 }
