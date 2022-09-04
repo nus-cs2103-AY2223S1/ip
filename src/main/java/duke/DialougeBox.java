02299622ffb01372c0e575a09295dbc74f19fd1f
gabyang <gabrielyang@berkeley.edu>
2022-09-04 23:33:45 +0800

    Add GUI for Duke

---
+++ src/main/java/duke/DialougeBox.java
@@ -0,0 +1,47 @@
+package duke;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialougeBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialougeBox(Label l, ImageView iv) {
+        text = l;
+        displayPicture = iv;
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialougeBox getUserDialog(Label l, ImageView iv) {
+        return new DialougeBox(l, iv);
+    }
+
+    public static DialougeBox getDukeDialog(Label l, ImageView iv) {
+        var db = new DialougeBox(l, iv);
+        db.flip();
+        return db;
+    }
+}
