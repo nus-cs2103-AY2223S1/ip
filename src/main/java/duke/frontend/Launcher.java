d9190dc2bdb0dff7f8f9832f2ef424e6cf36575f
jhchee18 <e0407708@u.nus.edu>
2022-09-04 12:19:09 +0800

    Configure to enable assert feature
    To be ready to document important assumptions that should hold at
    various points in the code.
    
    Testing of assert false in Parser class (now commented to ensure the
    code runnable).

--- src/main/java/duke/frontend/Launcher.java
+++ src/main/java/duke/frontend/Launcher.java
@@ -7,6 +7,10 @@ import javafx.application.Application;
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
+    /**
+     * Main method to launch the program.
+     * @param args args to pass in Main
+     */
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
