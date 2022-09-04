da207d2fbb3716ed2bb07b264361504b5f5e1449
Clarence Chew <62918570+clarence-chew@users.noreply.github.com>
2022-09-04 11:04:19 +0800

    Reorder classes for JUnit Tests
    CheckStyle is important. However, CheckStyle seems to be giving warnings
    on these class ordering. Gradle build seems to require these lines be
    correctly ordered.
    
    By changing these lines, Gradle is able to build .jar files.

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -1,9 +1,9 @@
 package duke.task;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class EventTest {
     @Test
