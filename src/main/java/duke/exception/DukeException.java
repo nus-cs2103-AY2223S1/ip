a2a54086de33fc320a4220246f951ef103a8d6c7
Charles Lim <charleslimjh@gmail.com>
2022-09-06 06:22:58 +0800

    add javadocs to DukeException class

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -4,6 +4,10 @@ package duke.exception;
  * General Exception for the Duke bot.
  */
 public class DukeException extends Exception {
+    /**
+     * Constructor for DukeException.
+     * @param error Error message for the exception
+     */
     public DukeException (String error) {
         super(error);
     }
