b6db4850a5547a5b2d53ff5c0db9d7c054ff4ea5
Tan Chee Heng <ch.tan020@gmail.com>
2022-09-06 00:08:33 +0800

    Fix gradlew directory issue in runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -28,8 +28,10 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-./gradlew run -p .. --args console-test -q < text-ui-test/input.txt > text-ui-test/ACTUAL.TXT
-./gradlew run -p .. --args console-test -q < text-ui-test/input2.txt >> text-ui-test/ACTUAL.TXT
+./gradlew run --args console-test -q < text-ui-test/input.txt > text-ui-test/ACTUAL.TXT
+./gradlew run --args console-test -q < text-ui-test/input2.txt >> text-ui-test/ACTUAL.TXT
+
+cd text-ui-test
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
