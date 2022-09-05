d541a6b0742d5f49dba29767fe14b84e046a762a
shenyihong <shen_yi_hong@outlook.com>
2022-09-05 23:38:53 +0800

    [feat] Add support for tagging Tasks

--- src/main/java/duke/data/tasks/Task.java
+++ src/main/java/duke/data/tasks/Task.java
@@ -1,10 +1,20 @@
 package duke.data.tasks;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public abstract class Task implements Serializable {
 
-    protected final String title;
+    // Tags are represented by a "#"
+    // e.g. "#school"
+    private static final Pattern tagRegexPattern = Pattern.compile("(#(\\w+))");
+    // A collection of tags without prefix "#"
+    protected final List<String> tags;
+    protected String title;
     protected boolean isMarked;
 
     /**
@@ -15,6 +25,24 @@ public abstract class Task implements Serializable {
     Task(String title) {
         this.title = title;
         this.isMarked = false;
+
+        this.tags = new ArrayList<>();
+        extractTags();
+    }
+
+    /**
+     * Extract out all tags from the task title
+     */
+    private void extractTags() {
+        Matcher matcher = tagRegexPattern.matcher(this.title);
+        while (matcher.find()) {
+            // 1st capture group: #tag
+            // 2nd capture group: tag
+            tags.add(matcher.group(2));
+        }
+        title = title.replaceAll(tagRegexPattern.toString(), "")
+                     .replaceAll("\\s\\s", " ") // handle extra whitespace when tag is in middle
+                     .trim(); // handle extra whitespace when tag is at the start/end
     }
 
     /**
@@ -37,6 +65,9 @@ public abstract class Task implements Serializable {
 
     @Override
     public String toString() {
-        return String.format("[%c] %s", this.isMarked ? 'X' : ' ', this.title);
+        String tagsStr = tags.isEmpty() ? ""
+            : String.format("(%s)",
+                tags.stream().map(tag -> "#" + tag).collect(Collectors.joining(", ")));
+        return String.format("[%c]%s %s", this.isMarked ? 'X' : ' ', tagsStr, title);
     }
 }
