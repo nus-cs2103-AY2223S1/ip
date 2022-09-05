6f91162cac56cce9f6639cd04418aedebabfda91
Yap Zhi Heng <yapzhhg@gmail.com>
2022-09-05 12:09:42 +0800

    Add some assertions

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -49,6 +49,9 @@ public class Deadline extends Task {
         if (splitSaveString.length != 3) {
             throw new DukeException("Tried to read unexpected save data.");
         }
+        assert splitSaveString[0].equals("D") : "Save data is not a deadline.";
+        assert splitSaveString[0].endsWith("1") || splitSaveString[0].endsWith("0")
+                : "Save data contains invalid isDone value.";
         String description = splitSaveString[1];
         String by = "by " + splitSaveString[2];
         boolean isDone = splitSaveString[0].endsWith("1");
