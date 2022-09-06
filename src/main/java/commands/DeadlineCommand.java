aff0d68634aa30d1c9f38cd55ff91ae1bf5e9f50
Gui Ming Jiang <mjgui2000@hotmail.com>
2022-09-06 22:57:07 +0800

    Refactor new task commands into same superclass
    To reduce repetition of code, Event, Deadline and ToDo creation command
    classes now inherit from a common NewTaskCommand.

--- src/main/java/commands/DeadlineCommand.java
+++ src/main/java/commands/DeadlineCommand.java
@@ -3,33 +3,18 @@ package commands;
 import java.time.LocalDate;
 
 import data.Deadline;
-import data.Task;
-import data.TaskList;
-import storage.Storage;
 
 /**
  * Command to create a deadline.
  */
-public class DeadlineCommand extends Command {
-    private final String title;
-    private final LocalDate dateBy;
-
+public class DeadlineCommand extends NewTaskCommand {
     /**
      * Command to create a deadline with title and dateBy.
-     * @param title Title of task.
+     *
+     * @param title  Title of task.
      * @param dateBy Deadline of task.
      */
     public DeadlineCommand(String title, LocalDate dateBy) {
-        this.title = title;
-        this.dateBy = dateBy;
-    }
-
-    @Override
-    public String execute(TaskList tasks, Storage storage) {
-        Task task = tasks.add(new Deadline(title, false, dateBy));
-        storage.save(tasks);
-        return "Got it. I've added this task: \n"
-                + "  " + task + "\n"
-                + "Now you have " + tasks.getSize() + " task" + (tasks.getSize() == 1 ? "" : "s") + " in the list.";
+        super(new Deadline(title, false, dateBy));
     }
 }
--- src/main/java/commands/EventCommand.java
+++ src/main/java/commands/EventCommand.java
@@ -3,33 +3,18 @@ package commands;
 import java.time.LocalDate;
 
 import data.Event;
-import data.Task;
-import data.TaskList;
-import storage.Storage;
 
 /**
  * Command to create an Event.
  */
-public class EventCommand extends Command {
-    private final String title;
-    private final LocalDate dateAt;
-
+public class EventCommand extends NewTaskCommand {
     /**
      * Creates an EventCommand
-     * @param title Title of event to be created.
+     *
+     * @param title  Title of event to be created.
      * @param dateAt Date of event to be created.
      */
     public EventCommand(String title, LocalDate dateAt) {
-        this.title = title;
-        this.dateAt = dateAt;
-    }
-
-    @Override
-    public String execute(TaskList tasks, Storage storage) {
-        Task task = tasks.add(new Event(title, false, dateAt));
-        storage.save(tasks);
-        return "Got it. I've added this task: \n"
-                + "  " + task + "\n"
-                + "Now you have " + tasks.getSize() + " task" + (tasks.getSize() == 1 ? "" : "s") + " in the list.";
+        super(new Event(title, false, dateAt));
     }
 }
