a6f3570ad2820ba9a518b25a52d0c504974eaf20
myangat <mingyangy31@gmail.com>
2022-09-06 13:16:23 +0800

    Some improvement

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -7,7 +7,6 @@ import javafx.application.Application;
 public class Launcher {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
         Application.launch(Main.class, args);
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        System.out.println("Hello world");
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
