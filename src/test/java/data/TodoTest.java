c0f706c8dd196c16a3bf34ef2ac508171a64ddcb
Gui Ming Jiang <mjgui2000@hotmail.com>
2022-09-06 23:45:46 +0800

    Fix import order for tests

--- src/test/java/data/TodoTest.java
+++ src/test/java/data/TodoTest.java
@@ -1,8 +1,8 @@
 package data;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class TodoTest {
     @Test
--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -1,10 +1,11 @@
 package parser;
 
-import commands.ByeCommand;
-import exceptions.DukeException;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import commands.ByeCommand;
+import exceptions.DukeException;
 
 public class ParserTest {
     @Test
