9b80fb892d2cb235ac6fadeb6f86e2dc68940501
Joel Wong <>
2022-09-06 13:59:30 +0800

    delete unnecessary code in DukeException class

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -2,19 +2,12 @@ package duke;
 
 public class DukeException extends RuntimeException{
 
-    private String exception = "";
-
 
     /**
      * Creates a new DukeException.
-     * @param e Text to be displayed.
+     * @param msg Text to be displayed.
      */
-    public DukeException(String e) {
-        exception = e;
-    }
-
-    @Override
-    public String toString() {
-        return exception;
+    public DukeException(String msg) {
+        super(msg);
     }
 }
