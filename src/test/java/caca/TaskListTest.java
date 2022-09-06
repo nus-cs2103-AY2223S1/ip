5526d67c0f9973c4c1ed03873e4a889a07afc079
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2022-09-06 12:12:01 +0800

    A-CodeQuality: improve code quality
    Some methods do not follow OOP closely and may be hard to follow.
    
    Refactoring could improve code structure and quality.
    
    Let's refactor to improve the code quality
    (mainly in Parser class and TaskList class) by
    * consolidating duplicate conditional fragments
    * extracting methods
    * adding any corresponding tests for new behaviours or methods

--- src/test/java/caca/TaskListTest.java
+++ src/test/java/caca/TaskListTest.java
@@ -44,15 +44,15 @@ public class TaskListTest {
         taskList.addTask(new TaskStub("stub4"));
         assertEquals(taskList.getTasks().size(), 4);
 
-        TaskList.deleteTask(String.valueOf(3));
+        TaskList.indexOperation("delete", "3");
         assertEquals(taskList.getTasks().size(), 3);
     }
 
     /**
-     * Tests the behaviour of deleteTask with an invalid index.
+     * Tests the behaviour of deleteTask with an invalid index that is out of range.
      */
     @Test
-    public void deleteTask_invalidIndex_exceptionThrown() {
+    public void deleteTask_outOfRangeIndex_exceptionThrown() {
         TaskList taskList = new TaskList(null);
 
         taskList.addTask(new TaskStub(null));
@@ -61,11 +61,58 @@ public class TaskListTest {
         assertEquals(taskList.getTasks().size(), 3);
 
         try {
-            TaskList.deleteTask(String.valueOf(4));
+            TaskList.indexOperation("delete", "4");
             assertEquals(taskList.getTasks().size(), 2);
             fail(); // Test should not reach this line.
         } catch (InvalidIndexException e) {
             assertEquals("OOPS!!! You have entered an invalid task index. It should be between 1 and 3.",
+                    e.getMessage());
+        }
+
+    }
+
+    /**
+     * Tests the behaviour of deleteTask with an invalid index that is not a number.
+     */
+    @Test
+    public void deleteTask_nonNumberIndex_exceptionThrown() {
+        TaskList taskList = new TaskList(null);
+
+        taskList.addTask(new TaskStub(null));
+        taskList.addTask(new TaskStub(null));
+        taskList.addTask(new TaskStub(null));
+        assertEquals(taskList.getTasks().size(), 3);
+
+        try {
+            TaskList.indexOperation("delete", "hi");
+            assertEquals(taskList.getTasks().size(), 2);
+            fail(); // Test should not reach this line.
+        } catch (InvalidIndexException e) {
+            assertEquals("OOPS!!! You have entered an invalid task index. It must be a number.",
+                    e.getMessage());
+        }
+
+    }
+
+    /**
+     * Tests the behaviour of deleteTask without specifying an index.
+     */
+    @Test
+    public void deleteTask_noIndexEntered_exceptionThrown() {
+        TaskList taskList = new TaskList(null);
+
+        taskList.addTask(new TaskStub(null));
+        taskList.addTask(new TaskStub(null));
+        taskList.addTask(new TaskStub(null));
+        assertEquals(taskList.getTasks().size(), 3);
+
+        try {
+            TaskList.indexOperation("delete", null);
+            assertEquals(taskList.getTasks().size(), 2);
+            fail(); // Test should not reach this line.
+        } catch (InvalidIndexException e) {
+            assertEquals("OOPS!!! Task index cannot be empty. "
+                            + "It must be specified and must be a number.",
                     e.getMessage());
         }
 
