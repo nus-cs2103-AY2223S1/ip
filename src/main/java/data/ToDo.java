6f5f9f2d8b57b94085f7d94e78382c85d9d094fa
Gui Ming Jiang <mjgui2000@hotmail.com>
2022-09-06 23:04:11 +0800

    Fix styling issues

--- src/main/java/data/ToDo.java
+++ src/main/java/data/ToDo.java
@@ -7,8 +7,9 @@ public class ToDo extends Task {
 
     /**
      * Create a new ToDo item.
+     *
      * @param title Title of task
-     * @param done If task is done.
+     * @param done  If task is done.
      */
     public ToDo(String title, boolean done) {
         super(title, done);
