1c3503a5977751b3edcdd6771c04952ec9aad719
WingKei67 <e0817842@u.nus.edu>
2022-09-07 20:50:48 +0800

    Add assertions

--- src/main/java/yilia/command/DeleteCommand.java
+++ src/main/java/yilia/command/DeleteCommand.java
@@ -1,11 +1,11 @@
 package yilia.command;
 
+import java.util.Arrays;
+
 import yilia.Storage;
 import yilia.Ui;
 import yilia.task.Task;
 import yilia.task.TaskList;
-
-import java.util.Arrays;
 
 /**
  * Represents a command to delete a task from the task list.
@@ -22,22 +22,26 @@ public class DeleteCommand extends Command {
      * @param tasks The tasks.
      * @param ui The use interface.
      * @param storage The local storage.
+     * @return The message after executing.
      */
     @Override
-    public void execute(TaskList tasks, Ui ui, Storage storage) {
+    public String execute(TaskList tasks, Ui ui, Storage storage) {
         Arrays.sort(indices);
         for (int i = 0; i < indices.length / 2; i++) {
             int temp = indices[i];
             indices[i] = indices[indices.length - 1 - i];
             indices[indices.length - 1 - i] = temp;
         }
-        for (int i = 0; i < indices.length; i++) {
+        StringBuilder message = new StringBuilder();
+        for (int index : indices) {
             try {
-                Task task = tasks.remove(indices[i]);
-                ui.showDeleteStatus(task, tasks);
+                Task task = tasks.remove(index);
+                message.append(ui.showDeleteStatus(task, tasks));
             } catch (IndexOutOfBoundsException e) {
-                ui.showIndexOutOfBounds(indices[i]);
+                message.append(ui.showIndexOutOfBounds(index));
+                message.append("\n");
             }
         }
+        return message.toString();
     }
 }
