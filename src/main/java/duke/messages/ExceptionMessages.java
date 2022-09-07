aac2c29f5bec06bf6305236213d6cf174273c28e
Prearo <kohshanghui@gmail.com>
2022-09-08 03:25:25 +0800

    Add ability to remove multiple tasks

--- src/main/java/duke/messages/ExceptionMessages.java
+++ src/main/java/duke/messages/ExceptionMessages.java
@@ -17,10 +17,10 @@ public final class ExceptionMessages {
     public static String UNSUPPORTED_ACTION = frowningFace
             + " OOPS!!! I'm sorry, but I don't know what that means :-(" + lineSep;
     public static String INVALID_INDEX_FORMAT = frowningFace
-            + " OOPS!!! Please indicate only an index for the command." + lineSep;
+            + " OOPS!!! Please indicate only an integer indices for the command." + lineSep;
     public static String LOAD_ERROR = "Error loading data from storage." + lineSep;
     public static String INVALID_FIND_FORMAT = frowningFace
             +" OOPS!!! Please indicate the string to scan the task list for." + lineSep;
     public static String OUT_OF_BOUNDS = frowningFace
-            + "OOPS!!! Cannot %s task %d as there are only %d tasks in the list." + lineSep;
+            + "OOPS!!! Cannot %1$s task %2$d as there are only %3$d tasks in the list." + lineSep;
 }
