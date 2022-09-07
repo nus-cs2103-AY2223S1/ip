c552ee4436d2350e5bdb8448da9cc2a97ce77ec4
samuelcheongws <samuelcheong2000@gmail.com>
2022-09-07 16:36:22 +0800

    Level-10 Till part 4

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,28 +1,54 @@
+import java.io.IOException;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 
 
 public class DialogBox extends HBox {
 
-    private Label text;
+//    private Label text;
+//    private ImageView displayPicture;
+
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
     public DialogBox(Label l, ImageView iv) {
-        text = l;
+        dialog = l;
         displayPicture = iv;
 
-        text.setWrapText(true);
+        dialog.setWrapText(true);
         displayPicture.setFitWidth(100.0);
         displayPicture.setFitHeight(100.0);
 
         this.setAlignment(Pos.TOP_RIGHT);
-        this.getChildren().addAll(text, displayPicture);
+        this.getChildren().addAll(dialog, displayPicture);
     }
+
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.setText(text);
+        displayPicture.setImage(img);
+    }
+
+
     /**
      * Flips the dialog box such that the ImageView is on the left and text on the right.
      */
@@ -33,8 +59,18 @@ public class DialogBox extends HBox {
         this.getChildren().setAll(tmp);
     }
 
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
+    }
+
     public static DialogBox getUserDialog(Label l, ImageView iv) {
         return new DialogBox(l, iv);
+    }
+
+    public static DialogBox getDukeDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
+        db.flip();
+        return db;
     }
 
     public static DialogBox getDukeDialog(Label l, ImageView iv) {
