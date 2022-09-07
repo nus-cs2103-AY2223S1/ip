927f07af576870d091ec8a8070efda5745b31e19
shauneang <shauneangjx@gmail.com>
2022-09-07 16:49:33 +0800

    Change Duke GUI
    Duke now uses JavaFX to make its GUI instead of CLI
    
    Create JAR file so users can access Duke directly
    
    Made code more OOP by abstracting the parser, TaskList, Command, FileLoaderSaver

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,19 +1,7 @@
 package duke;
 
 import java.util.InputMismatchException;
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.layout.Region;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-
 
 /**
  * Main duke.Duke class where duke program is ran
@@ -23,8 +11,6 @@ public class Duke {
     private String filePath;
     private FileLoaderSaver storage;
     private TaskList taskList;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     /**
      * Creates a label with the specified text and adds it to the dialog container.
