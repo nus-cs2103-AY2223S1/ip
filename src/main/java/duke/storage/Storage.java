d7832dfc5a640ced626c864dba37a1c9853b9bbe
sprintaway <ngyanjia26a2000@gmail.com>
2022-09-07 11:32:33 +0800

    Make the GUI work again

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -1,5 +1,4 @@
 package duke.storage;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import duke.parser.Parser;
 import duke.task.Deadline;
 import duke.task.Event;
@@ -15,20 +13,15 @@ import duke.task.Task;
 import duke.task.ToDo;
 import duke.tasklist.TaskList;
 import duke.ui.Ui;
-
-
-
 /**
  * Storage class that is used to save and load files
  */
 public class Storage {
-
     private File file;
-
     /**
      * Constructor of Storage class
      *
-     * @param filePath it was meant to be used as the file path, but I decided to hard code the path instead
+     * @param filePath it was meant to be used as the file path but I decided to hard code the path instead
      */
     public Storage(String filePath) {
         String path = System.getProperty("user.dir") + "/data/duke.txt";
@@ -48,59 +41,42 @@ public class Storage {
         String path = System.getProperty("user.dir") + "/data/duke.txt";
         File directoryFolder = new File(directory);
         File file = new File(path);
-
         if (!directoryFolder.exists()) {
             directoryFolder.mkdir();
             Ui.showDirectoryCreation();
         }
-
         if (!file.exists()) {
             file.createNewFile();
             Ui.showFileCreation();
         }
-
         try {
             FileReader filereader = new FileReader(path);
             BufferedReader bufferedreader = new BufferedReader(filereader);
             String line;
             Ui.showLoadSuccessful();
             while ((line = bufferedreader.readLine()) != null) {
-                Task toDo = new ToDo(line.substring(0, 5) + line.substring(7));
-                if (line.startsWith("[T][X]")) {
-                    tasks.add(toDo);
-                    tasks.get(tasks.size() - 1).markAsDone();
-                } else if (line.startsWith("[T]")) {
-                    tasks.add(toDo);
-                } else {
-                    String substring = line.substring(line.lastIndexOf("]") + 1, line.lastIndexOf("("));
-                    String substring1 = line.substring(line.lastIndexOf(":")
-                            + 2, line.lastIndexOf(")"));
+                if (line.startsWith("[T]")) {
+                    tasks.add(new ToDo(line.substring(0, 5) + line.substring(7)));
+                    if (line.startsWith("[T][X]")) {
+                        tasks.get(tasks.size() - 1).markAsDone();
+                    }
+                } else if (line.startsWith("[D]")) {
+                    String tempDeadline = "deadline"
+                            + line.substring(line.lastIndexOf("]") + 1, line.lastIndexOf("("))
+                            + "/by "
+                            + Parser.parseLocalDate(line.substring(line.lastIndexOf(":") + 2, line.lastIndexOf(")")));
+                    tasks.add(new Deadline(tempDeadline));
                     if (line.startsWith("[D][X]")) {
-                        String tempDeadline = "deadline"
-                                + substring
-                                + "/by "
-                                + Parser.parseLocalDate(substring1);
-                        tasks.add(new Deadline(tempDeadline));
                         tasks.get(tasks.size() - 1).markAsDone();
-                    } else if (line.startsWith("[D]")) {
-                        String tempDeadline = "deadline"
-                                + substring
-                                + "/by "
-                                + Parser.parseLocalDate(substring1);
-                        tasks.add(new Deadline(tempDeadline));
-                    } else if (line.startsWith("[E][X]")) {
-                        String tempEvent = "event"
-                                + substring
-                                + "/at "
-                                + Parser.parseLocalDate(substring1);
-                        tasks.add(new Event(tempEvent));
+                    }
+                } else if (line.startsWith("[E]")) {
+                    String tempEvent = "event"
+                            + line.substring(line.lastIndexOf("]") + 1, line.lastIndexOf("("))
+                            + "/at "
+                            + Parser.parseLocalDate(line.substring(line.lastIndexOf(":") + 2, line.lastIndexOf(")")));
+                    tasks.add(new Event(tempEvent));
+                    if (line.startsWith("[E][X]")) {
                         tasks.get(tasks.size() - 1).markAsDone();
-                    } else if (line.startsWith("[E]")) {
-                        String tempEvent = "event"
-                                + substring
-                                + "/at "
-                                + Parser.parseLocalDate(substring1);
-                        tasks.add(new Event(tempEvent));
                     }
                 }
             }
@@ -110,7 +86,6 @@ public class Storage {
         }
         return tasks;
     }
-
     /**
      * Saves the tasklist line by line in a duke.txt file
      *
@@ -120,12 +95,10 @@ public class Storage {
         String directory = System.getProperty("user.dir") + "/data/";
         String path = System.getProperty("user.dir") + "/data/duke.txt";
         File directoryFolder = new File(directory);
-
         if (!directoryFolder.exists()) {
             Ui.showDirectoryCreation();
             directoryFolder.mkdir();
         }
-
         try {
             FileWriter filewriter = new FileWriter(path);
             for (Task task : taskList.getArray()) {
