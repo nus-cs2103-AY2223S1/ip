b05b8a20d7f3d10109d7f2b63b033b37ef9b5513
Zsigmond <e0725064@u.nus.edu>
2022-09-07 14:16:18 +0800

    style: fix checkstyle violations, mainly for new classes added for gui

--- src/main/java/duke/Gui.java
+++ src/main/java/duke/Gui.java
@@ -3,10 +3,19 @@ package duke;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 
+/**
+ * Prints messages for user in the Gui.
+ */
 public class Gui extends Ui {
     private VBox dialogContainer;
     private Image dukeImage;
 
+    /**
+     * Constructor for Gui.
+     *
+     * @param dialogContainer VBox containing all the messages between user and Duke.
+     * @param dukeImage Image representing Duke to be displayed beside each of Duke's replies.
+     */
     public Gui(VBox dialogContainer, Image dukeImage) {
         this.dialogContainer = dialogContainer;
         this.dukeImage = dukeImage;
