aea7a3ac505c349388004a2bee5c002b25d675bc
SweetPotato0213 <e0376920@u.nus.edu>
2022-09-07 15:12:17 +0800

    Create a JAR file
    Package the app as an executable JAR file so that it can be distributed easily.

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -3,7 +3,6 @@ package duke;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-// import java.time.temporal.ChronoUnit;
 
 public class Deadline extends Task {
     protected LocalDate by;
