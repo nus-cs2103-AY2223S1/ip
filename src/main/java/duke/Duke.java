b36da03b893552a902a5e63d60e3e243a0fa8b24
Justin Peng <justinpeng13@gmail.com>
2022-09-09 01:18:29 +0800

    Implement echo user GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -17,13 +17,22 @@ import duke.task.Deadline;
 import duke.task.Event;
 import duke.task.Task;
 import duke.task.Todo;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * Represents the main Duke class and runs MakiBot.
  *
  * @author Justin Peng
  */
-public class Duke {
+public class Duke extends Application {
     /** Datetime formatter for user input which accepts dd/MM/yyyy or dd/MM/yyyy HH:mm */
     private static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
             .appendPattern("dd/MM/yyyy ")
@@ -47,29 +56,102 @@ public class Duke {
     private final Parser parser;
     private TaskList tasks;
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
     /**
      * Creates a new {@code Duke} object.
      */
-    protected Duke() {
+    public Duke() {
         ui = new Ui();
         storage = new Storage();
         parser = new Parser();
     }
 
     public static void main(String[] args) {
-        new Duke().start();
+        new Duke().startBot();
     }
 
     /**
      * Starts MakiBot.
      */
-    protected void start() {
+    protected void startBot() {
         System.out.println("Hello! I'm MakiBot");
         timeZone = ui.getTimeZone(timeZone);
         storage.setSaveFilePath(ui.getSaveFile(storage.getSaveFilePath()));
         tasks = new TaskList(storage.loadTasks(timeZone));
         System.out.println("Welcome! What can I do for you?");
         eventLoop();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+//
+//        stage.setTitle("Duke");
+//        stage.setResizable(false);
+//        stage.setMinHeight(600.0);
+//        stage.setMinWidth(400.0);
+//
+//        mainLayout.setPrefSize(400.0, 600.0);
+//
+//        scrollPane.setPrefSize(385, 535);
+//        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+//        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+//
+//        scrollPane.setVvalue(1.0);
+//        scrollPane.setFitToWidth(true);
+//
+//        // You will need to import `javafx.scene.layout.Region` for this.
+//        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+//
+//        userInput.setPrefWidth(325.0);
+//
+//        sendButton.setPrefWidth(55.0);
+//
+//        AnchorPane.setTopAnchor(scrollPane, 1.0);
+//
+//        AnchorPane.setBottomAnchor(sendButton, 1.0);
+//        AnchorPane.setRightAnchor(sendButton, 1.0);
+//
+//        AnchorPane.setLeftAnchor(userInput , 1.0);
+//        AnchorPane.setBottomAnchor(userInput, 1.0);
+//
+//        sendButton.setOnMouseClicked((event) -> {
+//            handleUserInput();
+//        });
+//
+//        userInput.setOnAction((event) -> {
+//            handleUserInput();
+//        });
+//
+//        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 
     /**
@@ -126,6 +208,63 @@ public class Duke {
             } catch (DukeException de) {
                 System.out.println(de.getMessage());
             }
+        }
+    }
+
+    /**
+     * Generates an answer to the specified user input.
+     *
+     * @param input The user input.
+     */
+    protected void answer(String input) {
+        try {
+            String[] fullCommand = parser.parseFullCommand(input);
+            Command command = parser.parseCommand(input);
+
+            // Handle commands
+            switch (command) {
+            // Exit command
+            case BYE:
+                storage.updateSaveFile(tasks.getTasks());
+                ui.close();
+                // return "Tasks saved successfully!";
+                return;
+            case SAVE:
+                storage.updateSaveFile(tasks.getTasks());
+                // return "Tasks saved successfully!";
+                break;
+            // List all tasks
+            case LIST:
+                ui.printAllTasks(tasks.getTasks());
+                break;
+            // Mark task as done
+            case MARK:
+                mark(fullCommand);
+                break;
+            // Mark task as undone
+            case UNMARK:
+                unmark(fullCommand);
+                break;
+            case DELETE:
+                delete(fullCommand);
+                break;
+            case FIND:
+                find(fullCommand);
+                break;
+            case TODO:
+                newTodo(fullCommand);
+                break;
+            case DEADLINE:
+                newDeadline(fullCommand);
+                break;
+            case EVENT:
+                newEvent(fullCommand);
+                break;
+            default:
+                throw new DukeInvalidCommandException();
+            }
+        } catch (DukeException de) {
+            System.out.println(de.getMessage());
         }
     }
 
