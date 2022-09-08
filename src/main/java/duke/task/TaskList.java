2923920f3581fafb1d962dc166dbb146be1525ff
chloeelim <chloelim22@gmail.com>
2022-09-09 00:17:15 +0800

    A-Streams. Use Streams

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -7,6 +7,8 @@ import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import duke.exception.DukeException;
 
@@ -153,10 +155,10 @@ public class TaskList {
      * @param pred Condition to test the task elements against
      * @return A new task list containing only elements that fulfil the specified condition
      */
-    public TaskList filter(Function<Task, Boolean> pred) {
+    public TaskList filter(Predicate<Task> pred) {
         TaskList filtered = new TaskList();
         for (Task task : tasks) {
-            if (pred.apply(task)) {
+            if (pred.test(task)) {
                 filtered.addTask(task);
             }
         }
@@ -164,11 +166,7 @@ public class TaskList {
     }
 
     public <T> LinkedList<T> transform(Function<Task, T> func) {
-        LinkedList<T> transformed = new LinkedList<>();
-        for (Task task : tasks) {
-            transformed.add(func.apply(task));
-        }
-        return transformed;
+        return tasks.stream().map(func).collect(Collectors.toCollection(LinkedList::new));
     }
 
     public <T> LinkedList<T> transform(BiFunction<Task, Integer, T> func) {
