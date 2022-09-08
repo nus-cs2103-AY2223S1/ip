ea1652599807187f8d09d2aeb5ffe569058d6dea
chengda300 <chengda300@gmail.com>
2022-09-08 16:55:32 +0800

    Add ability to mark, unmark and delete multiple tasks, and also fix an error of mark and unmark commands saving incorrectly

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -3,6 +3,9 @@ package duke;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 /**
  * Represents a parser to parse inputs into actions to be taken.
@@ -31,11 +34,18 @@ public class Parser {
         }
     }
     private String mark(Duke d, String input, boolean isLoading) {
+        Scanner sc = new Scanner(input);
+        StringBuilder sb = new StringBuilder();
+        while (sc.hasNext()) {
+            sb.append(markTask(d, sc.next(), isLoading) + "\n");
+        }
+        return sb.toString();
+    }
+    private String markTask(Duke d, String input, boolean isLoading) {
         try {
-            String parameter = input.substring((this.MARK + this.SPACE).length());
-            int index = Integer.parseInt(parameter) - 1;
+            int index = Integer.parseInt(input) - 1;
             return d.markTask(index)
-                    + saveCommand(d, this.MARK + this.SPACE + index + "\n", isLoading);
+                    + saveCommand(d, this.MARK + this.SPACE + input + "\n", isLoading);
         } catch (NumberFormatException e) {
             return this.ERROR_MESSAGE;
         } catch (IndexOutOfBoundsException e) {
@@ -43,11 +53,18 @@ public class Parser {
         }
     }
     private String unmark(Duke d, String input, boolean isLoading) {
+        Scanner sc = new Scanner(input);
+        StringBuilder sb = new StringBuilder();
+        while (sc.hasNext()) {
+            sb.append(unmarkTask(d, sc.next(), isLoading) + "\n");
+        }
+        return sb.toString();
+    }
+    private String unmarkTask(Duke d, String input, boolean isLoading) {
         try {
-            String parameter = input.substring((this.UNMARK + this.SPACE).length());
-            int index = Integer.parseInt(parameter) - 1;
+            int index = Integer.parseInt(input) - 1;
             return d.unmarkTask(index)
-                    + saveCommand(d, this.UNMARK + this.SPACE + index + "\n", isLoading);
+                    + saveCommand(d, this.UNMARK + this.SPACE + input + "\n", isLoading);
         } catch (NumberFormatException e) {
             return this.ERROR_MESSAGE;
         } catch (IndexOutOfBoundsException e) {
@@ -55,24 +72,23 @@ public class Parser {
         }
     }
     private String todo(Duke d, String input, boolean isLoading) {
-        String s = input.substring((this.TODO + this.SPACE).length());
-        if (s.isBlank()) {
+        if (input.isBlank()) {
             return "The task is empty, what do you really mean?";
         } else {
-            return d.addTodo(s) + saveCommand(d, this.TODO + this.SPACE + s + "\n", isLoading);
+            return d.addTodo(input)
+                    + saveCommand(d, this.TODO + this.SPACE + input + "\n", isLoading);
         }
     }
     private String deadline(Duke d, String input, boolean isLoading) {
-        String s = input.substring((this.DEADLINE + this.SPACE).length());
         // deadline flag is not in command
-        if (!s.contains(this.SPACE + this.DEADLINE_BY + this.SPACE)) {
+        if (!input.contains(this.SPACE + this.DEADLINE_BY + this.SPACE)) {
             return "Either the task is empty or the deadline is empty, or both\n"
                     + "The syntax is: deadline <taskName> /by <deadline>\n"
                     + "If there is no deadline, please add it as a todo instead.";
         }
         // separate task and deadline and check if either is empty
-        String task = s.substring(0, s.indexOf(this.DEADLINE_BY) - this.SPACE.length());
-        String deadline = s.substring(s.indexOf(this.DEADLINE_BY)
+        String task = input.substring(0, input.indexOf(this.DEADLINE_BY) - this.SPACE.length());
+        String deadline = input.substring(input.indexOf(this.DEADLINE_BY)
                 + this.DEADLINE_BY.length() + this.SPACE.length());
         if (task.isBlank()) {
             return "The task is empty, what do you really mean?";
@@ -84,22 +100,21 @@ public class Parser {
         try {
             LocalDate date = LocalDate.parse(deadline);
             return d.addDeadline(task, date)
-                    + saveCommand(d, this.DEADLINE + this.SPACE + s + "\n", isLoading);
+                    + saveCommand(d, this.DEADLINE + this.SPACE + input + "\n", isLoading);
         } catch (DateTimeParseException e) {
             return d.addDeadline(task, deadline)
-                    + saveCommand(d, this.DEADLINE + this.SPACE + s + "\n", isLoading);
+                    + saveCommand(d, this.DEADLINE + this.SPACE + input + "\n", isLoading);
         }
     }
     private String event(Duke d, String input, boolean isLoading) {
-        String s = input.substring((this.EVENT + this.SPACE).length());
         // event time flag not found
-        if (!s.contains(this.SPACE + this.EVENT_AT + this.SPACE)) {
+        if (!input.contains(this.SPACE + this.EVENT_AT + this.SPACE)) {
             return "Either the task is empty or the event time is empty, or both\n"
                     + "The syntax is: event <eventName> /at <eventTime>";
         }
         // separate event and time and check if either is empty
-        String event = s.substring(0, s.indexOf(this.EVENT_AT) - this.SPACE.length());
-        String time = s.substring(s.indexOf(this.EVENT_AT) + this.EVENT_AT.length()
+        String event = input.substring(0, input.indexOf(this.EVENT_AT) - this.SPACE.length());
+        String time = input.substring(input.indexOf(this.EVENT_AT) + this.EVENT_AT.length()
                 + this.SPACE.length());
         if (event.isBlank()) {
             return "The event is empty, what do you really mean?";
@@ -110,18 +125,37 @@ public class Parser {
         try {
             LocalDate t = LocalDate.parse(time);
             return d.addEvent(event, t)
-                    + saveCommand(d, this.EVENT + this.SPACE + s + "\n", isLoading);
+                    + saveCommand(d, this.EVENT + this.SPACE + input + "\n", isLoading);
         } catch (DateTimeParseException e) {
             return d.addEvent(event, time)
-                    + saveCommand(d, this.EVENT + this.SPACE + s + "\n", isLoading);
+                    + saveCommand(d, this.EVENT + this.SPACE + input + "\n", isLoading);
         }
     }
     private String delete(Duke d, String input, boolean isLoading) {
+        Scanner sc = new Scanner(input);
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        // deletion must be started from the largest index to preserve order
+        while (sc.hasNext()) {
+            try {
+                int index = Integer.parseInt(sc.next()) - 1;
+                if (!pq.contains(index)) {
+                    pq.add(index);
+                }
+            } catch (NumberFormatException e) {
+                sb.append(this.ERROR_MESSAGE);
+            }
+        }
+        // start deletion process
+        while (!pq.isEmpty()) {
+           sb.append(deleteTask(d, pq.remove(), isLoading) + "\n");
+        }
+        return sb.toString();
+    }
+    private String deleteTask(Duke d, int input, boolean isLoading) {
         try {
-            String parameter = input.substring((this.DELETE + this.SPACE).length());
-            int index = Integer.parseInt(parameter) - 1;
-            return d.deleteTask(index)
-                    + saveCommand(d, this.DELETE + this.SPACE + parameter + "\n", isLoading);
+            return d.deleteTask(input)
+                    + saveCommand(d, this.DELETE + this.SPACE + (input + 1) + "\n", isLoading);
         } catch (NumberFormatException e) {
             return this.ERROR_MESSAGE;
         } catch (IndexOutOfBoundsException e) {
@@ -129,11 +163,10 @@ public class Parser {
         }
     }
     private String findTask(Duke d, String input) {
-        String s = input.substring((this.FIND + this.SPACE).length());
-        if (s.isBlank()) {
+        if (input.isBlank()) {
             return "The query is empty, what do you really mean?";
         } else {
-            return d.findTask(s);
+            return d.findTask(input);
         }
     }
 
@@ -152,19 +185,19 @@ public class Parser {
         } else if (input.equals(this.LIST)) {
             return d.listToString();
         } else if (input.startsWith(this.MARK + this.SPACE)) {
-            return this.mark(d, input, isLoading);
+            return this.mark(d, input.substring((this.MARK + this.SPACE).length()), isLoading);
         } else if (input.startsWith(this.UNMARK + this.SPACE)) {
-            return this.unmark(d, input, isLoading);
+            return this.unmark(d, input.substring((this.UNMARK + this.SPACE).length()), isLoading);
         } else if (input.startsWith(this.TODO + this.SPACE)) {
-            return this.todo(d, input, isLoading);
+            return this.todo(d, input.substring((this.TODO + this.SPACE).length()), isLoading);
         } else if (input.startsWith(this.DEADLINE + this.SPACE)) {
-            return this.deadline(d, input, isLoading);
+            return this.deadline(d, input.substring((this.DEADLINE + this.SPACE).length()), isLoading);
         } else if (input.startsWith(this.EVENT + this.SPACE)) {
-            return this.event(d, input, isLoading);
+            return this.event(d, input.substring((this.EVENT + this.SPACE).length()), isLoading);
         } else if (input.startsWith(this.DELETE + this.SPACE)) {
-            return this.delete(d, input, isLoading);
+            return this.delete(d, input.substring((this.DELETE + this.SPACE).length()), isLoading);
         } else if (input.startsWith(this.FIND + this.SPACE)) {
-            return this.findTask(d, input);
+            return this.findTask(d, input.substring((this.FIND + this.SPACE).length()));
         }
         return this.ERROR_MESSAGE;
     }
