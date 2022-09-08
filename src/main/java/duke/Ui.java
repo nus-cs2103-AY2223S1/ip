95ddf87831ec1aba6e4667d0195ffe18ac679031
Chua Yu Xuan Jordan <jcyx1999@gmail.com>
2022-09-08 12:38:49 +0800

    A-CodeQuality
    getResponse : replace if else statements with a switch statement
    
    getResponse method has many nested if else statements.
    
    The nested if else statements cause a drop in code readability
    
    Using a switch statement clearly separates the different cases and increases code readability since it is looks neater overall.

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,20 +1,9 @@
 package duke;
 
-import java.util.Scanner;
-
 /**
  * Represents an interface that interacts with the user by printing the necessary messages
  */
 public class Ui {
-
-    String showWelcome() {
-        return "Hello! I'm Duke" + "\n" + "What can I do for you?";
-    }
-
-    String readCommand() {
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
-    }
 
     String showError(Exception e){
         return e.getMessage();
