3d843c78144ae088a3bad7a72a8e031869710238
Teh Yi Kuang <e0544398@u.nus.edu>
2022-09-09 01:25:24 +0800

    Add Find function

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -70,4 +70,31 @@ public class TaskList {
         ui.printTaskDeleted(taskToRemove, getSize());
         storage.refreshList(this.tasks);
     }
+
+    /**
+     * Finds all tasks that has descriptions containing the keyword
+     *
+     * @param keyword Keyword to match
+     * @param ui The UI to print outputs
+     */
+    public void findTask(String keyword, Ui ui) {
+        if (this.tasks.size() == 0) { // List is empty
+            ui.printEmptyListMessage();
+        } else {
+            int numOfMatchingTasks = 0;
+            ui.printLine();
+            System.out.println("Here are the matching tasks in your list:");
+            for (int i = 0; i < this.tasks.size(); i++) {
+                Task curTask = this.tasks.get(i);
+                if (curTask.getDescription().contains(keyword)) {
+                    numOfMatchingTasks++;
+                    System.out.println((i + 1) + "." + curTask.toString());
+                }
+            }
+            if (numOfMatchingTasks == 0) {
+                System.out.println("*** There is no matching task with your keyword ***");
+            }
+            ui.printLine();
+        }
+    }
 }
