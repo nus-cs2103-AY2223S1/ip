681fd4700a4d550b3f837bf74d34bc5d1ee66040
Rachel Angelyn Gunawan <rachelag777@gmail.com>
2022-09-09 02:49:05 +0800

    Fix checkstyle violations

--- src/test/java/duke/parser/ParserTest.java
+++ src/test/java/duke/parser/ParserTest.java
@@ -1,9 +1,11 @@
 package duke.parser;
 
-import duke.DukeException;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import duke.DukeException;
 
 public class ParserTest {
     private final Parser sut = new Parser();
