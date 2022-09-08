7f1d33023631f2521ae07b379cd76ef06fb8a503
eugenelmj <eugenelmj@gmail.com>
2022-09-08 18:12:58 +0800

    Add A-Assertions
    Changes have been made to assert that the various parts of the 'process' command in Parser class is giving the correct output

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -56,21 +56,26 @@ public class Parser {
         case "MARK":
             int markIdx = Integer.parseInt(command.substring(5)) - 1;
             tasks.mark(markIdx);
+            assert tasks.getTask(markIdx).getStatusIcon().equals("X");
             return ui.printMarked(tasks.getTask(markIdx));
         case "UNMARK":
             int unmarkIdx = Integer.parseInt(command.substring(7)) - 1;
             tasks.unmark(unmarkIdx);
+            assert tasks.getTask(unmarkIdx).getStatusIcon().equals(" ");
             return ui.printUnmarked(tasks.getTask(unmarkIdx));
         case "TODO":
+            assert command.startsWith("todo");
             Task tD = new ToDo(command.substring(5));
             tasks.add(tD);
             return ui.printAddedTask(tD, tasks.getSize());
         case "DEADLINE":
+            assert command.startsWith("deadline");
             String dlAction = command.substring(9, command.indexOf("/") - 1);
             Task dl = new Deadline(dlAction, formatEventTime(command));
             tasks.add(dl);
             return ui.printAddedTask(dl, tasks.getSize());
         case "EVENT":
+            assert command.startsWith("event");
             String eAction = command.substring(6, command.indexOf("/") - 1);
             Task ev = new Event(eAction, formatEventTime(command));
             tasks.add(ev);
