42cbc26350e9d99ebe5d64a4451aff92618a0d4f
Julian Wong <e0725200@u.nus.edu>
2022-09-09 16:46:36 +0800

    branch-A-CodeQuality

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -22,8 +22,9 @@ public class Duke {
     }
 
     /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
+     * Gets the response from Duke.
+     * @param input the input command.
+     * @return the corresponding response.
      */
     String getResponse(String input) {
         TaskHandler taskHandler = new TaskHandler(tasklist, ui);
