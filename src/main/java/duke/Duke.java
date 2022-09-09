b4fa78dbb20a155ad9f68a7e26418770e7711c55
paullee18 <paulfrancislee.1@gmail.com>
2022-09-09 16:07:38 +0800

    Remove unnecessary code from Duke.java due to the fxml files

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -22,103 +22,12 @@ import java.util.Scanner;
  * A Personal Assistant Chatbot that helps a person keep track of several things like tasks.
  *
  */
-public class Duke extends Application {
+public class Duke {
 
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
     private Parser parser;
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/jim.jpg"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/dwight.jpg"));
-
-    @Override
-    public void start(Stage stage) {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-
-        scene = new Scene(mainLayout);
-
-        // Formatting the window
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane,1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        // For when user presses enter
-        userInput.setOnAction((event) -> {
-           handleUserInput();
-        });
-
-        // For when dialog container gets longer
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    public Label getDialogLabel(String text) {
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-    /**
-     * Updates the dialog box
-     */
-    public void handleUserInput() {
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userInput.getText(), user),
-                DialogBox.getDukeDialog(getResponse(userInput.getText()), duke)
-        );
-        userInput.clear();
-    }
 
     /**
      * Gets a response from the Duke bot
