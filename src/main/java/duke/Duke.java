4edc950a2ed0aefec2fe06723b2c02da4c74b73a
Justin Peng <justinpeng13@gmail.com>
2022-09-10 02:28:02 +0800

    Implement full MakiBot with GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -8,6 +8,8 @@ import java.time.format.DateTimeParseException;
 import java.time.format.TextStyle;
 import java.time.temporal.ChronoField;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import duke.exception.DukeException;
 import duke.exception.DukeFormatCommandException;
@@ -17,22 +19,13 @@ import duke.task.Deadline;
 import duke.task.Event;
 import duke.task.Task;
 import duke.task.Todo;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 /**
  * Represents the main Duke class and runs MakiBot.
  *
  * @author Justin Peng
  */
-public class Duke extends Application {
+public class Duke {
     /** Datetime formatter for user input which accepts dd/MM/yyyy or dd/MM/yyyy HH:mm */
     private static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
             .appendPattern("dd/MM/yyyy ")
@@ -46,7 +39,19 @@ public class Duke extends Application {
             .toFormatter();
     /** Commands accepted by Duke */
     public enum Command {
-        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, SAVE, FIND
+        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, SAVE, FIND;
+
+        private static final Set<String> values = new HashSet<>(Command.values().length);
+
+        static {
+            for (Command command: Command.values()) {
+                values.add(command.name());
+            }
+        }
+
+        static boolean contains(String value) {
+            return values.contains(value);
+        }
     }
     /** User timezone */
     private ZoneId timeZone = ZoneId.of("GMT+00:00");
@@ -56,14 +61,6 @@ public class Duke extends Application {
     private final Parser parser;
     private TaskList tasks;
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
     /**
      * Creates a new {@code Duke} object.
      */
@@ -71,6 +68,7 @@ public class Duke extends Application {
         ui = new Ui();
         storage = new Storage();
         parser = new Parser();
+        tasks = new TaskList(storage.loadTasks(timeZone));
     }
 
     public static void main(String[] args) {
@@ -86,129 +84,6 @@ public class Duke extends Application {
         storage.setSaveFilePath(ui.getSaveFile(storage.getSaveFilePath()));
         tasks = new TaskList(storage.loadTasks(timeZone));
         System.out.println("Welcome! What can I do for you?");
-        eventLoop();
-    }
-
-    @Override
-    public void start(Stage stage) {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-//
-//        stage.setTitle("Duke");
-//        stage.setResizable(false);
-//        stage.setMinHeight(600.0);
-//        stage.setMinWidth(400.0);
-//
-//        mainLayout.setPrefSize(400.0, 600.0);
-//
-//        scrollPane.setPrefSize(385, 535);
-//        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-//        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-//
-//        scrollPane.setVvalue(1.0);
-//        scrollPane.setFitToWidth(true);
-//
-//        // You will need to import `javafx.scene.layout.Region` for this.
-//        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-//
-//        userInput.setPrefWidth(325.0);
-//
-//        sendButton.setPrefWidth(55.0);
-//
-//        AnchorPane.setTopAnchor(scrollPane, 1.0);
-//
-//        AnchorPane.setBottomAnchor(sendButton, 1.0);
-//        AnchorPane.setRightAnchor(sendButton, 1.0);
-//
-//        AnchorPane.setLeftAnchor(userInput , 1.0);
-//        AnchorPane.setBottomAnchor(userInput, 1.0);
-//
-//        sendButton.setOnMouseClicked((event) -> {
-//            handleUserInput();
-//        });
-//
-//        userInput.setOnAction((event) -> {
-//            handleUserInput();
-//        });
-//
-//        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
-
-    /**
-     * Starts a conversation with MakiBot.
-     */
-    protected void eventLoop() {
-        while (true) {
-            try {
-                // Event loop
-                String input = ui.getInput();
-                String[] fullCommand = parser.parseFullCommand(input);
-                Command command = parser.parseCommand(input);
-
-                // Handle commands
-                switch (command) {
-                // Exit command
-                case BYE:
-                    storage.updateSaveFile(tasks.getTasks());
-                    ui.close();
-                    return;
-                case SAVE:
-                    storage.updateSaveFile(tasks.getTasks());
-                    break;
-                // List all tasks
-                case LIST:
-                    ui.printAllTasks(tasks.getTasks());
-                    break;
-                // Mark task as done
-                case MARK:
-                    mark(fullCommand);
-                    break;
-                // Mark task as undone
-                case UNMARK:
-                    unmark(fullCommand);
-                    break;
-                case DELETE:
-                    delete(fullCommand);
-                    break;
-                case FIND:
-                    find(fullCommand);
-                    break;
-                case TODO:
-                    newTodo(fullCommand);
-                    break;
-                case DEADLINE:
-                    newDeadline(fullCommand);
-                    break;
-                case EVENT:
-                    newEvent(fullCommand);
-                    break;
-                default:
-                    throw new DukeInvalidCommandException();
-                }
-            } catch (DukeException de) {
-                System.out.println(de.getMessage());
-            }
-        }
     }
 
     /**
@@ -216,7 +91,7 @@ public class Duke extends Application {
      *
      * @param input The user input.
      */
-    protected void answer(String input) {
+    protected String getResponse(String input) {
         try {
             String[] fullCommand = parser.parseFullCommand(input);
             Command command = parser.parseCommand(input);
@@ -226,45 +101,36 @@ public class Duke extends Application {
             // Exit command
             case BYE:
                 storage.updateSaveFile(tasks.getTasks());
-                ui.close();
-                // return "Tasks saved successfully!";
-                return;
+                return ui.close();
             case SAVE:
-                storage.updateSaveFile(tasks.getTasks());
-                // return "Tasks saved successfully!";
-                break;
+                return storage.updateSaveFile(tasks.getTasks());
             // List all tasks
             case LIST:
-                ui.printAllTasks(tasks.getTasks());
-                break;
+                return ui.getAllTasks(tasks.getTasks());
             // Mark task as done
             case MARK:
-                mark(fullCommand);
-                break;
+                return mark(fullCommand);
             // Mark task as undone
             case UNMARK:
-                unmark(fullCommand);
-                break;
+                return unmark(fullCommand);
             case DELETE:
-                delete(fullCommand);
-                break;
+                return delete(fullCommand);
             case FIND:
-                find(fullCommand);
-                break;
+                return find(fullCommand);
             case TODO:
-                newTodo(fullCommand);
-                break;
+                return newTodo(fullCommand);
             case DEADLINE:
-                newDeadline(fullCommand);
-                break;
+                return newDeadline(fullCommand);
             case EVENT:
-                newEvent(fullCommand);
-                break;
+                return newEvent(fullCommand);
             default:
                 throw new DukeInvalidCommandException();
             }
         } catch (DukeException de) {
-            System.out.println(de.getMessage());
+            return de.getMessage();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error";
         }
     }
 
@@ -274,7 +140,7 @@ public class Duke extends Application {
      * @param fullCommand The full input from the user containing the task's index.
      * @throws DukeException If the input is invalid.
      */
-    protected void mark(String[] fullCommand) throws DukeException {
+    protected String mark(String[] fullCommand) throws DukeException {
         if (fullCommand.length < 2 || fullCommand[1].equals("")) {
             throw new DukeFormatCommandException("mark");
         }
@@ -283,7 +149,7 @@ public class Duke extends Application {
             int taskNum = Integer.parseInt(fullCommand[1]) - 1;
             Task t = tasks.get(taskNum);
             t.markAsDone();
-            System.out.println("Nice! I've marked this task as done:\n" + t);
+            return "Nice! I've marked this task as done:\n" + t;
         } catch (NumberFormatException e) {
             if (tasks.size() == 0) {
                 throw new DukeIndexErrorException();
@@ -299,7 +165,7 @@ public class Duke extends Application {
      * @param fullCommand The full input from the user containing the task's index.
      * @throws DukeException If the input is invalid.
      */
-    protected void unmark(String[] fullCommand) throws DukeException {
+    protected String unmark(String[] fullCommand) throws DukeException {
         if (fullCommand.length < 2 || fullCommand[1].equals("")) {
             throw new DukeFormatCommandException("unmark");
         }
@@ -308,7 +174,7 @@ public class Duke extends Application {
             int taskNum = Integer.parseInt(fullCommand[1]) - 1;
             Task t = tasks.get(taskNum);
             t.markAsUndone();
-            System.out.println("OK, I've marked this task as not done yet:\n" + t);
+            return "OK, I've marked this task as not done yet:\n" + t;
         } catch (NumberFormatException e) {
             if (tasks.size() == 0) {
                 throw new DukeIndexErrorException();
@@ -324,7 +190,7 @@ public class Duke extends Application {
      * @param fullCommand The full input from the user containing the task's index.
      * @throws DukeException If the input is invalid.
      */
-    protected void delete(String[] fullCommand) throws DukeException {
+    protected String delete(String[] fullCommand) throws DukeException {
         if (fullCommand.length < 2 || fullCommand[1].equals("")) {
             throw new DukeFormatCommandException("delete");
         }
@@ -332,7 +198,7 @@ public class Duke extends Application {
         try {
             int taskNum = Integer.parseInt(fullCommand[1]) - 1;
             Task t = tasks.remove(taskNum);
-            System.out.printf("Noted. I've removed this task:\n"
+            return String.format("Noted. I've removed this task:\n"
                             + "\t%s\n"
                             + "Now you have %d tasks in the list.%n",
                     t, tasks.size());
@@ -350,13 +216,13 @@ public class Duke extends Application {
     *
     * @param fullCommand The input from the user containing keywords to search for.
     */
-    protected void find(String[] fullCommand) throws DukeException {
+    protected String find(String[] fullCommand) throws DukeException {
         if (fullCommand.length < 2 || fullCommand[1].equals("")) {
             throw new DukeFormatCommandException("find");
         }
 
         ArrayList<Task> results = tasks.find(fullCommand[1].split(" "));
-        ui.printAllTasks(results);
+        return ui.getAllTasks(results);
     }
 
     /**
@@ -365,14 +231,14 @@ public class Duke extends Application {
      * @param fullCommand The full input from the user containing the task's description.
      * @throws DukeException If the input is invalid.
      */
-    protected void newTodo(String[] fullCommand) throws DukeException {
+    protected String newTodo(String[] fullCommand) throws DukeException {
         if (fullCommand.length < 2 || fullCommand[1].equals("")) {
             throw new DukeFormatCommandException("todo");
         }
 
         Todo td = new Todo(fullCommand[1]);
         tasks.add(td);
-        ui.printNewTaskMessage(td, tasks.size());
+        return ui.getNewTaskMessage(td, tasks.size());
     }
 
     /**
@@ -381,7 +247,7 @@ public class Duke extends Application {
      * @param fullCommand The full input from the user containing the task's description and deadline.
      * @throws DukeException If the input is invalid.
      */
-    protected void newDeadline(String[] fullCommand) throws DukeException {
+    protected String newDeadline(String[] fullCommand) throws DukeException {
         if (fullCommand.length < 2 || fullCommand[1].equals("")) {
             throw new DukeFormatCommandException("deadline");
         }
@@ -392,7 +258,7 @@ public class Duke extends Application {
             ZonedDateTime by = ZonedDateTime.parse(newDeadline[1] + " " + timeZone, DATE_TIME_FORMATTER);
             Deadline dl = new Deadline(newDeadline[0], by);
             tasks.add(dl);
-            ui.printNewTaskMessage(dl, tasks.size());
+            return ui.getNewTaskMessage(dl, tasks.size());
         } catch (ArrayIndexOutOfBoundsException | DateTimeParseException e) {
             e.printStackTrace();
             throw new DukeFormatCommandException("deadline", "/by");
@@ -405,7 +271,7 @@ public class Duke extends Application {
      * @param fullCommand The full input from the user containing the task's description and datetime.
      * @throws DukeException If the input is invalid.
      */
-    protected void newEvent(String[] fullCommand) throws DukeException {
+    protected String newEvent(String[] fullCommand) throws DukeException {
         if (fullCommand.length < 2 || fullCommand[1].equals("")) {
             throw new DukeFormatCommandException("event");
         }
@@ -415,7 +281,7 @@ public class Duke extends Application {
             ZonedDateTime at = ZonedDateTime.parse(newEvent[1] + " " + timeZone, DATE_TIME_FORMATTER);
             Event ev = new Event(newEvent[0], at);
             tasks.add(ev);
-            ui.printNewTaskMessage(ev, tasks.size());
+            return ui.getNewTaskMessage(ev, tasks.size());
         } catch (ArrayIndexOutOfBoundsException | DateTimeParseException e) {
             throw new DukeFormatCommandException("event", "/at");
         }
