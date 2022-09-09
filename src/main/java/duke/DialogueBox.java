26a58b529622a7965ddd785acd3e73ba13a28187
Jiacheng-y <e0773814@u.nus.edu>
2022-09-09 17:31:54 +0800

    Edit GUI implementation

--- src/main/java/duke/DialogueBox.java
+++ src/main/java/duke/DialogueBox.java
@@ -1,6 +1,9 @@
 package duke;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -20,5 +23,25 @@ public class DialogueBox extends HBox {
 
         this.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(text, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogueBox getUserDialog(Label l, ImageView iv) {
+        return new DialogueBox(l, iv);
+    }
+
+    public static DialogueBox getDukeDialog(Label l, ImageView iv) {
+        var db = new DialogueBox(l, iv);
+        db.flip();
+        return db;
     }
 }
