f49963444b7217cacbbd968198babc5017c6f6e2
LJXSean <sean250.sl@gmail.com>
2022-09-09 15:36:11 +0800

    Add help command
    There are many commands that can be used and each command requires the
    user to follow the specified input format precisely.
    
    New users to the app need to be able to find out what commands are available
    and the specific ways to the details need to be typed in. Regular users might
    also forget certain commands. Hence a help command is required.
    
    Let's add a help command to list out the possible inputs and their required input
    formats.
    
    This allows users to quickly refer to it when needed while using Duke.

--- src/test/java/duke/DeadlineTest.java
+++ src/test/java/duke/DeadlineTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DeadlineTest {
     Deadline testEvent = new Deadline("lunch", "2022-02-03", "12:00");
     @Test
-    public void testToString() {
+    public void deadlineToString_correctStringFormat() {
         assertEquals(testEvent.toString(), "[D][ ] lunch (by: Feb 3 2022, 1200PM)");
     }
 
     @Test
-    public void testToFile(){
+    public void eventToFile_correctFileFormat(){
         assertEquals(testEvent.toFile(), "D|0|lunch|2022-02-03|12:00\n");
     }
 }
