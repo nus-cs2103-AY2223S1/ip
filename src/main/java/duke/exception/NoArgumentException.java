7a009e237c6978fdb27dd6db79db21d6a51167c4
nopehax <57407346+nopehax@users.noreply.github.com>
2022-09-10 20:44:06 +0800

    Revert "Make parseListCommands shorter in Parser.java"

--- src/main/java/duke/exception/NoArgumentException.java
+++ src/main/java/duke/exception/NoArgumentException.java
@@ -6,7 +6,7 @@ import duke.util.Ui;
  * Thrown when a command is used without it's required arguments.
  */
 public class NoArgumentException extends DukeException {
-    public NoArgumentException(String command, Throwable e) {
-        super("The proper command is: \n" + Ui.COMMAND_HELP.get(command), e);
+    public NoArgumentException(int commandIndex, Throwable e) {
+        super("The proper command is: \n" + Ui.COMMAND_HELP[commandIndex], e);
     }
 }
