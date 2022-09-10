bda5a6cb302ab61294a2f674eb131cf7d9037576
chengda300 <chengda300@gmail.com>
2022-09-10 18:00:13 +0800

    Fix minor issues with imports and user guide

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -15,8 +15,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 
 /**
  * An example of a custom control using FXML.
--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -7,7 +7,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 /**
