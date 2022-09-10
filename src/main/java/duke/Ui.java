ce8142eefc71f9c1d42a2fbb6140ec365d3add89
turretDive <apangjun@gmail.com>
2022-09-10 20:22:55 +0800

    Add GUI

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
  * @version CS2103T AY22/23 Sem 1
  */
 public class Ui {
-    private static final String LINE = "_________" ;
 
     private Scanner input;
 
@@ -18,61 +17,12 @@ public class Ui {
     }
 
     /**
-     * Shows the Welcome message.
-     */
-    public static void showWelcome() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What may I do for you?");
-    }
-
-    /**
-     * Reads the next line input by user.
-     * @return input of user.
-     */
-    public String readCommand() {
-        return input.nextLine();
-    }
-
-    /**
-     * Shows the separation line.
-     */
-    public void showLine() {
-        System.out.println(LINE);
-    }
-
-    /**
      * Shows the goodbye message.
      */
-    public void showBye() {
-        System.out.println("Bye. Hope to see you again soon!");
+    public String showBye() {
         input.close();
-    }
-
-    /**
-     * Shows the loading error message.
-     */
-    public void showLoadingError() {
-        System.out.println("No saved data found.");
-    }
-
-    /**
-     * Shows the error message.
-     * @param message The error message.
-     */
-    public void showError(String message) {
-        System.out.println(message);
-    }
-
-    /**
-     * Shows the find message.
-     */
-    public void showFind() {
-        System.out.println("Here are the matching tasks in your list:");
+        String bye = "Bye. Hope to see you again soon!";
+        return bye;
     }
 }
 
