1bbc9b7f210f2287e9f1cfd14d3e35309e39d697
jasonqiu212 <jason_qiu@hotmail.com>
2022-09-10 22:49:48 +0800

    Storage: create directory for save file if it does not exist
    There is no check for if the directory containing save file exists. This leads to
    potential bugs if the directory is not present and we attempt to access the save file.
    
    Users are not expected nor instructued to create this directory. Thus, we need to create
    the directory if it does not exist.
    
    Let's
    * add check if directory exists
    * create directory if it does not exist

--- src/main/java/cheese/storage/Storage.java
+++ src/main/java/cheese/storage/Storage.java
@@ -1,15 +1,16 @@
 package cheese.storage;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
 import cheese.data.TaskList;
 import cheese.exception.CheeseException;
 import cheese.task.Deadline;
 import cheese.task.Event;
 import cheese.task.Task;
 import cheese.task.Todo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Represents a storage that interacts with <code>Cheese</code>'s save file.
@@ -20,7 +21,7 @@ public class Storage {
 
     /**
      * Constructs an instance of <code>Storage</code>.
-     * 
+     *
      * @param filePath File path of save file.
      */
     public Storage(String filePath) {
@@ -29,7 +30,7 @@ public class Storage {
 
     /**
      * Loads task list from save file.
-     * 
+     *
      * @return Instance of <code>TaskList</code> loaded from save file.
      * @throws CheeseException If save file contains corrupted data.
      */
@@ -37,7 +38,13 @@ public class Storage {
         TaskList taskList = new TaskList();
         try {
             File file = new File(filePath);
-            file.createNewFile();
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
             Scanner scanner = new Scanner(file);
 
             while (scanner.hasNext()) {
@@ -53,7 +60,7 @@ public class Storage {
 
     /**
      * Saves given task list to save file.
-     * 
+     *
      * @param taskList Task list to save.
      */
     public void save(TaskList taskList) {
@@ -69,7 +76,7 @@ public class Storage {
 
     /**
      * Parses string from save file to task.
-     * 
+     *
      * @param taskString String from save file to parse.
      * @return Task parsed from string.
      * @throws CheeseException If string from save file is corrupted.
