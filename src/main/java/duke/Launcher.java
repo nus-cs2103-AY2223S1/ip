842cafbd2c00a3cb73613bf704bfe7f683346fd3
lyuting47 <luting_c@yahoo.com>
2022-09-11 00:51:19 +0800

    Test

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -3,7 +3,7 @@ package duke;
 import javafx.application.Application;
 
 /**
- * A launcher class to workaround classpath issues.
+ * A launcher class to work around classpath issues.
  */
 public class Launcher {
     public static void main(String[] args) {
