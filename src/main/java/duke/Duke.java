9cd369ee7d2e421e613a3ec9b3c99cf71f50b960
Jiacheng-y <e0773814@u.nus.edu>
2022-09-10 16:32:02 +0800

    Edit GUI to interact with user

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -212,6 +212,7 @@ public class Duke extends Application {
         );
         userInput.clear();
     }
+    
 
     /**
      * You should have your own function to generate a response to user input.
