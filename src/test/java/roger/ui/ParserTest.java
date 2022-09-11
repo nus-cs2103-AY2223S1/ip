1f22fc3ad050c52e94bb1b8f030f1747fe814682
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-09-11 19:51:27 +0800

    Add test for TaskStub class

---
+++ src/test/java/roger/ui/ParserTest.java
@@ -0,0 +1,34 @@
+package roger.ui;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import roger.commands.AddEventCommand;
+import roger.exceptions.RogerInvalidInputException;
+import roger.ui.Parser;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ParserTest {
+    @Timeout(1)
+    @Test
+    public void parse_deadlineNoDate_exceptionThrown() throws Exception {
+        Parser parser = new Parser();
+        assertThrows(RogerInvalidInputException.class, () -> parser.parse("deadline homework"));
+    }
+
+    @Timeout(1)
+    @Test
+    public void parse_markNothing_exceptionThrown() throws Exception {
+        Parser parser = new Parser();
+        assertThrows(RogerInvalidInputException.class, () -> parser.parse("mark q"));
+    }
+
+    @Timeout(1)
+    @Test
+    public void parse_event_returnAddEventCommand() throws Exception {
+        Parser parser = new Parser();
+        assertTrue(parser.parse("event happy /at 2022-10-04") instanceof AddEventCommand);
+    }
+}
