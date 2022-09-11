659a1f497412bdc6bb6039865fbb1958bbc621f8
Donovan <don9617@hotmail.com>
2022-09-11 17:05:53 +0800

    Remove redundant code

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -2,37 +2,15 @@ package duke;
 
 import java.io.IOException;
 
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 /**
  * Duke bot object class, which has a task list, a storage space and a user interface.
  */
-public class Duke extends Application {
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private AnchorPane mainLayout;
+public class Duke {
 
     private Ui ui;
     private Storage storage;
     private TaskList tasks;
     private Undo undo;
-
-    private Image userImg = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImg = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     /**
      * Creates a Duke object with the given filepath.
@@ -61,101 +39,6 @@ public class Duke extends Application {
     }
 
     /**
-     * Sets the components of a chat container.
-     *
-     * @param stage is the platform with chat container.
-     */
-    public void setChatContainer(Stage stage) {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * Sets dimensions for chat window.
-     *
-     * @param stage is the platform with chat window.
-     */
-    public void setChatWindow(Stage stage) {
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        userInput.setPrefWidth(325.0);
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-    }
-
-    /**
-     * Send button handles user input on clicked.
-     */
-    public void setChatSendButton() {
-        sendButton.setOnMouseClicked((event) -> {
-            try {
-                handleUserInput();
-            } catch (DukeException | IOException e) {
-                ui.showError(e.getMessage());
-            }
-        });
-    }
-
-    /**
-     * User input is handled on action.
-     */
-    public void setChatUserInput() {
-        userInput.setOnAction((event) -> {
-            try {
-                handleUserInput();
-            } catch (DukeException | IOException e) {
-                ui.showError(e.getMessage());
-            }
-        });
-    }
-
-    /**
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply
-     * and then appends them to the dialog container. Clears the user input after processing.
-     */
-    @FXML
-    private void handleUserInput() throws DukeException, IOException {
-        String userText = userInput.getText();
-        String dukeText = this.getResponse(userInput.getText());
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, userImg),
-                DialogBox.getDukeDialog(dukeText, dukeImg)
-        );
-        userInput.clear();
-    }
-
-    /**
      * Gets Duke response for the user's input.
      *
      * @param input of what the user types.
@@ -165,20 +48,5 @@ public class Duke extends Application {
      */
     public String getResponse(String input) throws IOException {
         return Parser.parse(input, tasks, ui, storage, undo);
-    }
-
-    /**
-     * Main view of the duke application.
-     *
-     * @param stage main view of the duke application.
-     */
-    @Override
-    public void start(Stage stage) {
-
-        setChatContainer(stage);
-        setChatWindow(stage);
-        setChatSendButton();
-        setChatUserInput();
-
     }
 }
