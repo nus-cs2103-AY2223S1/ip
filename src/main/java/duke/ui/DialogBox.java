a6d8f58211673329ac9c2ab36a08f7426e094115
ruiqi7 <tinruiqi2002@gmail.com>
2022-09-11 22:00:02 +0800

    Improve GUI
    * set background color
    * change profile pictures and make them rounded
    * customize chat bubbles
    * add prompt text on text field

--- src/main/java/duke/ui/DialogBox.java
+++ src/main/java/duke/ui/DialogBox.java
@@ -7,22 +7,35 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 /**
  * Represents a dialog box consisting of an ImageView to represent the speaker's face and a label
  * containing text from the speaker.
  */
 public class DialogBox extends HBox {
+    private static final Background USER_BACKGROUND = new Background(
+            new BackgroundFill(Color.web("#797ef6", 0.7), new CornerRadii(15.0), Insets.EMPTY)
+    );
+    private static final Background DUKE_BACKGROUND = new Background(
+            new BackgroundFill(Color.web("#ffffff", 0.1), new CornerRadii(15.0), Insets.EMPTY)
+    );
+
     @FXML
     private Label dialog;
     @FXML
-    private ImageView displayPicture;
+    private Circle displayPicture;
 
     private DialogBox(String text, Image img) {
         try {
@@ -35,7 +48,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        displayPicture.setImage(img);
+        displayPicture.setFill(new ImagePattern(img));
     }
 
     /**
@@ -55,7 +68,9 @@ public class DialogBox extends HBox {
      * @return The DialogBox for the user.
      */
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        DialogBox db = new DialogBox(text, img);
+        db.dialog.setBackground(USER_BACKGROUND);
+        return db;
     }
 
     /**
@@ -65,7 +80,8 @@ public class DialogBox extends HBox {
      * @return The DialogBox for Duke.
      */
     public static DialogBox getDukeDialog(String text, Image img) {
-        var db = new DialogBox(text, img);
+        DialogBox db = new DialogBox(text, img);
+        db.dialog.setBackground(DUKE_BACKGROUND);
         db.flip();
         return db;
     }
