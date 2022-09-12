2a8378b09641335b642cf7796fb9921f2e0b3b1b
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2022-09-12 10:32:51 +0800

    Improve the GUI
    Let's make the GUI more appealing and add personality to the GUI, Bob.
    Also give credits to the code that was referred to while improving GUI.

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -9,10 +9,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.TextAlignment;
 
 /**
@@ -22,9 +23,9 @@ import javafx.scene.text.TextAlignment;
  */
 public class DialogBox extends HBox {
     @FXML
-    private Text dialog;
+    private Label dialog;
     @FXML
-    private ImageView displayPicture;
+    private Circle displayPicture;
 
     private DialogBox(String text, Image img) {
         try {
@@ -37,7 +38,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        displayPicture.setImage(img);
+        displayPicture.setFill(new ImagePattern(img));
     }
 
     /**
