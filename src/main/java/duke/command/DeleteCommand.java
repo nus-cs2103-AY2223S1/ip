875f584197922d50f600269bc8b84de5952d98bf
Huang Yuchen <hyuchen2607@gmail.com>
2022-09-12 14:28:12 +0800

    Complete C-MassOps
    Implemented 'delete all' to clear everything in the task list.

--- src/main/java/duke/command/DeleteCommand.java
+++ src/main/java/duke/command/DeleteCommand.java
@@ -13,6 +13,8 @@ import duke.util.TaskList;
  */
 public class DeleteCommand extends Command {
     private static final String REMOVED_TASK = "Boo~ Don't be a quitter! I've removed this task for you. =3=\n";
+    private static final String REMOVE_ALL_TASKS = "Boo~ I've removed all tasks for you.\n"
+            + "Start setting new goals for yourself! =3=";
     private final ArrayList<String> words;
 
     /**
@@ -37,8 +39,24 @@ public class DeleteCommand extends Command {
     public String execute(Storage storage, TaskList tasklist) throws DukeException {
         String input = String.join(" ", words);
         StringBuilder output = new StringBuilder();
-        // Work on implementing error for "delete hello"
-        int taskNum = Integer.parseInt(input);
+        if (tasklist.tasks.size() == 0) {
+            throw new DukeException("There's nothing in your list to delete! T^T");
+        }
+        if (input.equals("all")) {
+            tasklist.tasks.clear();
+            output.append(REMOVE_ALL_TASKS);
+        } else {
+            try {
+                int taskNum = Integer.parseInt(input);
+                deleteTaskNumber(output, tasklist, taskNum);
+            } catch (NumberFormatException e) {
+                throw new DukeException("Please either use 'delete all' or 'delete <task number>'. T^T");
+            }
+        }
+        return output.toString();
+    }
+
+    private void deleteTaskNumber(StringBuilder output, TaskList tasklist, int taskNum) throws DukeException {
         if (taskNum > 0 && taskNum <= tasklist.tasks.size()) {
             String deletedTask = tasklist.tasks.get(taskNum - 1).toString();
             tasklist.deleteTask(taskNum);
@@ -46,11 +64,8 @@ public class DeleteCommand extends Command {
                     .append(deletedTask).append("\n")
                     .append("You have ").append(tasklist.tasks.size())
                     .append((tasklist.tasks.size() == 1 ? " task! :D" : " tasks! :D"));
-        } else if (tasklist.tasks.size() == 0) {
-            throw new DukeException("There's nothing in your list to delete! T^T");
         } else {
             throw new DukeException("Please enter a valid task number to delete. T^T");
         }
-        return output.toString();
     }
 }
