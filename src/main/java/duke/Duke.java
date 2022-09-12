8ab2cb6a5e48cb3752f26d7ad57bfc9c9260a344
Mohamed Hamas <e0726186@u.nus.edu>
2022-09-12 11:29:42 +0800

    no message

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -55,7 +55,7 @@ public class Duke {
     }
 
     String byeResponse() {
-        return "Crista ronaldo SuuWeee ! ";
+        return "Crista ronaldo SuuWeeeee ! ";
     }
 
     String siuResponse() {
