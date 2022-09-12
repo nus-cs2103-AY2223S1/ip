4c5a763bcb1c55e60c513286a8bbdba316e3fc7f
SweetPotato0213 <e0376920@u.nus.edu>
2022-09-12 22:48:57 +0800

    javafx test

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,12 +1,13 @@
 package duke;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * Duke is an interactive personal chat robot to keep track of user inputted tasks.
@@ -14,12 +15,18 @@ import java.io.IOException;
  * @author Liu Han
  */
 
-public class Duke {
+public class Duke extends Application {
     private final static String PATH = "data/duke.txt";
 
     private final Storage storage;
     private TaskList tasks;
     private final Ui ui;
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     /**
      * Duke Constructor
@@ -46,5 +53,26 @@ public class Duke {
         new Parser(this.tasks).parser();
         storage.save(this.tasks);
         ui.farewell();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
