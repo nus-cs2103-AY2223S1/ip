ff2a7e9fa518fcf6a6a7e9c58aeef59ed7930046
ruiqi7 <tinruiqi2002@gmail.com>
2022-09-12 20:18:18 +0800

    Make minor modifications to UI messages

--- src/main/java/duke/common/Messages.java
+++ src/main/java/duke/common/Messages.java
@@ -11,24 +11,24 @@ public class Messages {
     public static final String MESSAGE_INVALID_DATE_FORMAT = "Please specify a date in this format: %s!";
     public static final String MESSAGE_STORAGE_ERROR = "There has been a loading error.";
 
-    public static final String MESSAGE_TASK_ADDED = "Task added:\n\t";
-    public static final String MESSAGE_TASK_REMOVED = "Task removed:\n\t";
-    public static final String MESSAGE_TASK_DONE = "Task marked as done:\n\t";
-    public static final String MESSAGE_TASK_NOT_DONE = "Task marked as not done:\n\t";
+    public static final String MESSAGE_TASK_ADDED = "Task added:\n  ";
+    public static final String MESSAGE_TASK_REMOVED = "Task removed:\n  ";
+    public static final String MESSAGE_TASK_DONE = "Task marked as done:\n  ";
+    public static final String MESSAGE_TASK_NOT_DONE = "Task marked as not done:\n  ";
     public static final String MESSAGE_TASK_LIST = "Task list:\n";
     public static final String MESSAGE_EMPTY_TASK_LIST = "Your task list is empty!";
-    public static final String MESSAGE_NUMBER_OF_TASKS = "\nYou have %d task(s) in the list.";
+    public static final String MESSAGE_NUMBER_OF_TASKS = "\n\nYou have %d task(s) in the list.";
 
     public static final String MESSAGE_NO_SUCH_TASK = "No such task!";
     public static final String MESSAGE_PROVIDE_TASK_NUMBER = "Please provide a task number!";
     public static final String MESSAGE_PROVIDE_DESCRIPTION = "Please provide a task description!";
     public static final String MESSAGE_PROVIDE_DESCRIPTION_AND_DATE = "Please provide a task description and date!";
-    public static final String MESSAGE_PROVIDE_KEYWORD = "Please provide a keyword!";
+    public static final String MESSAGE_PROVIDE_KEYWORD = "Please provide keyword(s)!";
     public static final String MESSAGE_PROVIDE_DATE = "Please provide a date!";
     public static final String MESSAGE_PROVIDE_MORE_DETAILS = "Please provide more details!";
 
-    public static final String MESSAGE_MATCHING_TASKS = "Here are the matching task(s) in your list:\n";
-    public static final String MESSAGE_NO_MATCHING_TASKS = "There are no matching tasks in your list!";
+    public static final String MESSAGE_MATCHING_TASKS = "Here are your matching task(s):\n";
+    public static final String MESSAGE_NO_MATCHING_TASKS = "Hmm... there are no matching tasks in your list!";
 
     public static final String MESSAGE_UNDO = "The most recent change to your task list has been undone!\n\n";
 }
