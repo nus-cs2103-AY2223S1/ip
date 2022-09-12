f7b6d5151646ab68999724f41694c5c1491bfa0c
chao890 <chaoyungchor@gmail.com>
2022-09-12 16:34:34 +0800

    Removed unused methods

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -72,51 +72,7 @@ public class Duke {
         }
     }
 
-    /**
-     * Run Duke programme
-     *
-     */
-    public void run() {
-        System.out.println(ui.greeting());
-        boolean isExit = false;
-        Handler handler = new Handler(tasklist, ui);
-        while (!isExit) {
-            try {
-                String echo = ui.getInput();
-                if (echo.equals("bye")) {
-                    System.out.println(ui.bye());
-                    isExit = true;
-                } else if (echo.equals("list")) {
-                    System.out.println(ui.showList());
-                } else if (echo.startsWith("mark")) {
-                    System.out.println(handler.handleMark(echo));
-                } else if (echo.startsWith("unmark")) {
-                    System.out.println(handler.handleUnmark(echo));
-                } else if (echo.startsWith("todo")) {
-                    System.out.println(handler.handleToDo(echo));
-                } else if (echo.startsWith("deadline")) {
-                    System.out.println(handler.handleDeadline(echo));
-                } else if (echo.startsWith("event")) {
-                    System.out.println(handler.handleEvent(echo));
-                } else if (echo.startsWith("delete")) {
-                    System.out.println(handler.handleDelete(echo));
-                } else if (echo.startsWith("find")) {
-                    System.out.println(handler.handleFind(echo));
-                } else {
-                    throw new DukeUnknownTaskException();
-                }
-                storage.save();
-            } catch (DukeException e) {
-                ui.printError(e.getMessage());
-            }
-        }
-    }
-
     public String greet() {
         return ui.greeting();
-    }
-
-    public static void main(String[] args) {
-        new Duke().run();
     }
 }
