e3acf9d0bdbcaae7396bb9f98928f00618400a40
sohenze <e0725494@u.nus.edu>
2022-09-12 14:34:02 +0800

    Add application name to window title

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke: Your task manager");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
