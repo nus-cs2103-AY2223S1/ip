d3b729219cded93ef123e38a912d3097e30b04fd
cfyjoanna <joannachanfy@gmail.com>
2022-09-12 20:32:21 +0800

    Update code quality

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -21,113 +21,185 @@ public class Parser {
      */
     public String parseCommand(String next) {
         String[] nextWords = next.split(" ");
-        switch(nextWords[0]) {
+
+        switch (nextWords[0]) {
         case "mark":
-            try {
-                int index = Integer.parseInt(nextWords[1]) - 1;
-                tasks.markTaskAsDone(index);
-                return "I've marked this as done:\n" + tasks.taskToString(index);
-            } catch (IndexOutOfBoundsException e) {
-                return ui.getInvalidTaskIndexError();
-            }
+            return markCommand(nextWords);
         case "unmark":
-            try {
-                int index = Integer.parseInt(nextWords[1]) - 1;
-                tasks.markTaskAsUndone(index);
-                return "I've marked this as undone:\n" + tasks.taskToString(index);
-            } catch (IndexOutOfBoundsException e) {
-                return ui.getInvalidTaskIndexError();
-            }
+            return unmarkCommand(nextWords);
         case "delete":
-            try {
-                int index = Integer.parseInt(nextWords[1]) - 1;
-                Task toDelete = tasks.get(index);
-                tasks.remove(index);
-                return "I'll remove this task:\n" + toDelete.toString()
-                        + "\nYou now have " + (tasks.size()) + " tasks.";
-            } catch (IndexOutOfBoundsException e) {
-                return ui.getInvalidTaskIndexError();
-            }
+            return deleteCommand(nextWords);
         case "list":
-            return "Here are your tasks:" + tasks.toString();
+            return listCommand();
         case "todo":
-            //Fallthrough
+            return addTodoCommand(next);
         case "deadline":
-            //Fallthrough
+            return addDeadlineCommand(next);
         case "event":
-            return parseTask(next, nextWords[0]);
+            return addEventCommand(next);
         case "find":
-            try {
-                String found = tasks.find(next.split(" ")[1]);
-                return "Here's what I found:" + found;
-            } catch (ArrayIndexOutOfBoundsException e) {
-                return "Please enter a word after 'find'!";
-            }
+            return findCommand(next);
         case "help":
             return ui.getHelp();
         default:
-            return "I don't know this command. Try another one!\n"
+            final String UNKNOWN_COMMAND_MESSAGE = "I don't know this command. Try another one!\n"
                     + "You can enter the word 'help' for a list of commands.";
+            return UNKNOWN_COMMAND_MESSAGE;
         }
     }
 
     /**
-     * Processes commands related to creating tasks.
+     * Marks the task specified by the user as done, then returns the result message.
      *
-     * @param input The user's input.
-     * @param type The type of task.
+     * @param input Input entered by user.
+     * @return Success or error message.
      */
-    public String parseTask(String input, String type) {
+    public String markCommand(String[] input) {
+        try {
+            int index = Integer.parseInt(input[1]) - 1;
+            tasks.markTaskAsDone(index);
+            return "I've marked this as done:\n" + tasks.taskToString(index);
+        } catch (IndexOutOfBoundsException e) {
+            return ui.getInvalidTaskIndexError();
+        }
+    }
+
+    /**
+     * Unmarks the task specified by the user as done, then returns the result message.
+     *
+     * @param input Input entered by user.
+     * @return Success or error message.
+     */
+    public String unmarkCommand(String[] input) {
+        try {
+            int index = Integer.parseInt(input[1]) - 1;
+            tasks.markTaskAsUndone(index);
+            return "I've marked this as undone:\n" + tasks.taskToString(index);
+        } catch (IndexOutOfBoundsException e) {
+            return ui.getInvalidTaskIndexError();
+        }
+    }
+
+    /**
+     * Deletes the task specified by the user, then returns the result message.
+     *
+     * @param input Input entered by user.
+     * @return Success or error message.
+     */
+    public String deleteCommand(String[] input) {
+        try {
+            int index = Integer.parseInt(input[1]) - 1;
+            Task toDelete = tasks.get(index);
+            tasks.remove(index);
+            return "I'll remove this task:\n" + toDelete.toString()
+                    + "\nYou now have " + (tasks.size()) + " tasks.";
+        } catch (IndexOutOfBoundsException e) {
+            return ui.getInvalidTaskIndexError();
+        }
+    }
+
+    /**
+     * Returns the list of tasks.
+     *
+     * @return The numbered list of tasks.
+     */
+    public String listCommand() {
+        return "Here are your tasks:" + tasks.toString();
+    }
+
+    /**
+     * Adds a todo to the task list with the given details.
+     *
+     * @param input Input entered by user.
+     * @return Success or error message.
+     */
+    public String addTodoCommand(String input) {
+        try {
+            String[] arr = input.split(" ", 2);
+            String desc = arr[1];
+            assert !desc.isEmpty() : "Task description is empty!";
+            Todo temp = new Todo(desc);
+
+            tasks.add(temp);
+            return ui.getTaskAddedMessage(temp, tasks);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "Please enter a description for your task!";
+        }
+    }
+
+    /**
+     * Adds a deadline to the task list with the given details.
+     *
+     * @param input Input entered by user.
+     * @return Success or error message.
+     */
+    public String addDeadlineCommand(String input) {
+        DateTimeFormatter parserFormats = new DateTimeFormatterBuilder()
+                .appendOptional(DateTimeFormatter.ISO_LOCAL_DATE)
+                .appendOptional(DateTimeFormatter.ofPattern("d MMM uuuu"))
+                .appendOptional(DateTimeFormatter.ofPattern("yyyyMMdd"))
+                .toFormatter();
+        try {
+            String[] arr = input.split(" ", 2);
+            String desc = arr[1];
+
+            String[] details = desc.split(" /by ");
+            String by = LocalDate.parse(details[1], parserFormats)
+                    .format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+            Deadline temp = new Deadline(details[0], by);
+
+            tasks.add(temp);
+            return ui.getTaskAddedMessage(temp, tasks);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "Oops! Your deadline should have a due date after /by.";
+        } catch (DateTimeParseException e) {
+            return "Please enter a valid date!";
+        }
+    }
+
+    /**
+     * Adds an event to the task list with the given details.
+     *
+     * @param input Input entered by user.
+     * @return Success or error message.
+     */
+    public String addEventCommand(String input) {
         DateTimeFormatter parserFormats = new DateTimeFormatterBuilder()
                 .appendOptional(DateTimeFormatter.ISO_LOCAL_DATE)
                 .appendOptional(DateTimeFormatter.ofPattern("d MMM uuuu"))
                 .appendOptional(DateTimeFormatter.ofPattern("yyyyMMdd"))
                 .toFormatter();
 
-        String[] arr = input.split(" ", 2);
-        String desc = "";
         try {
-            desc = arr[1];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return "Please enter a description for your task!";
-        }
+            String[] arr = input.split(" ", 2);
+            String desc = arr[1];
 
-        assert !desc.isEmpty() : "Task description is empty!";
+            String[] details = desc.split(" /at ");
+            String at = LocalDate.parse(details[1], parserFormats)
+                    .format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+            Event temp = new Event(details[0], at);
 
-        if (type.equals("todo")) {
-            Todo temp = new Todo(desc);
             tasks.add(temp);
             return ui.getTaskAddedMessage(temp, tasks);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "Oops! Your event should have a date after /at.";
+        } catch (DateTimeParseException e) {
+            return "Please enter a valid date!";
         }
-        if (type.equals("deadline")) {
-            try {
-                String[] details = desc.split(" /by ");
-                String by = LocalDate.parse(details[1], parserFormats)
-                        .format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-                Deadline temp = new Deadline(details[0], by);
-                tasks.add(temp);
-                return ui.getTaskAddedMessage(temp, tasks);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                return "Oops! Your deadline should have a due date after /by.";
-            } catch (DateTimeParseException e) {
-                return "Please enter a valid date!";
-            }
+    }
+
+    /**
+     * Finds all tasks that contain the given string.
+     *
+     * @param input Input entered by user.
+     * @return Results of the search.
+     */
+    public String findCommand(String input) {
+        try {
+            String found = tasks.find(input.split(" ")[1]);
+            return "Here's what I found:" + found;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "Please enter a word after 'find'!";
         }
-        if (type.equals("event")) {
-            try {
-                String[] details = desc.split(" /at ");
-                String at = LocalDate.parse(details[1], parserFormats)
-                        .format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-                Event temp = new Event(details[0], at);
-                tasks.add(temp);
-                return ui.getTaskAddedMessage(temp, tasks);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                return "Oops! Your event should have a date after /at.";
-            } catch (DateTimeParseException e) {
-                return "Please enter a valid date!";
-            }
-        }
-        assert false : "This statement should not be reached.";
-        return "There was a problem entering your task. Try again!";
     }
 }
