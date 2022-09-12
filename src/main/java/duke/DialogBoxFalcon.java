bc044617e568f7cbf3a57fc4e2108738551bfa03
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-09-13 00:26:48 +0800

    Copy DialogBox code to files

--- src/main/java/duke/DialogBoxFalcon.java
+++ src/main/java/duke/DialogBoxFalcon.java
@@ -1,4 +1,60 @@
 package duke;
+import java.io.IOException;
+import java.util.Collections;
 
-public class DialogBoxFalcon {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * An example of a custom control using FXML.
+ * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
+ * containing text from the speaker.
+ */
+public class DialogBoxFalcon extends HBox {
+    @FXML
+    private Label dialog;
+    @FXML
+    private ImageView displayPicture;
+
+    private DialogBoxFalcon(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBoxFalconFalcon.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.setText(text);
+        displayPicture.setImage(img);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
+    }
+
+    public static DialogBoxFalcon getUserDialog(String text, Image img) {
+        return new DialogBoxFalcon(text, img);
+    }
+
+    public static DialogBoxFalcon getDukeDialog(String text, Image img) {
+        var DialogBoxFalcon = new DialogBoxFalcon(text, img);
+        DialogBoxFalcon.flip();
+        return DialogBoxFalcon;
+    }
 }
--- src/main/java/duke/DialogBoxUser.java
+++ src/main/java/duke/DialogBoxUser.java
@@ -1,4 +1,60 @@
 package duke;
+import java.io.IOException;
+import java.util.Collections;
 
-public class DialogBoxUser {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * An example of a custom control using FXML.
+ * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
+ * containing text from the speaker.
+ */
+public class DialogBoxUser extends HBox {
+    @FXML
+    private Label dialog;
+    @FXML
+    private ImageView displayPicture;
+
+    private DialogBoxUser(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBoxUser.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.setText(text);
+        displayPicture.setImage(img);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
+    }
+
+    public static DialogBoxUser getUserDialog(String text, Image img) {
+        return new DialogBoxUser(text, img);
+    }
+
+    public static DialogBoxUser getDukeDialog(String text, Image img) {
+        var dialogBox = new DialogBoxUser(text, img);
+        dialogBox.flip();
+        return dialogBox;
+    }
 }
