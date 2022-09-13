2ed77b727dc18d44ad61fe38073cf23465ec6a34
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-09-13 13:14:28 +0800

    Fix logic of GetLongDescriptionCommand

--- src/main/java/command/GetLongDescriptionCommand.java
+++ src/main/java/command/GetLongDescriptionCommand.java
@@ -10,14 +10,13 @@ import utility.Parser;
  * Returns long description of task.
  */
 public class GetLongDescriptionCommand extends Command {
-    private String[] slicedUserCommands;
+    private int index;
 
-    public GetLongDescriptionCommand(String[] slicedUserInput) {
-        this.slicedUserCommands = slicedUserInput;
+    public GetLongDescriptionCommand(int index) {
+        this.index = index;
     }
     @Override
     public void execute(TaskList taskList, UI ui, Storage storage) throws DukeException {
-        int n = Parser.getTaskNumber(ui.getCurrentInput(), taskList.getSize());
-        ui.showMessage(taskList.getLongDescription(n));
+        ui.showMessage(taskList.getLongDescription(index));
     }
 }
