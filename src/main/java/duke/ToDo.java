5ae8ad4bf3c509e7c90cdfe94a313e600c810081
Whysochong <>
2022-09-13 22:08:17 +0800

    Minor changes (Bugs)
    Changed small glitch and bugs with Todo.toString().

--- src/main/java/duke/ToDo.java
+++ src/main/java/duke/ToDo.java
@@ -14,6 +14,6 @@ public class ToDo extends Task {
      */
     @Override
     public String toString() {
-        return "[T][" + super.getStatusIcon() + "] " + super.description.substring(5);
+        return "[T][" + super.getStatusIcon() + "] " + super.description;
     }
 }
