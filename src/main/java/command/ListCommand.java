59bfc328362871721e9aaed3d53e40acee9b44b6
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-09-13 13:18:35 +0800

    Fix logic and slap of ListCommand

--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -9,17 +9,24 @@ import ui.UI;
  * Lists all tasks in TaskList.
  */
 public class ListCommand extends Command {
-    private String[] slicedUserCommands;
-
-    public ListCommand(String[] slicedUserInput) {
-        this.slicedUserCommands = slicedUserInput;
-    }
     @Override
     public void execute(TaskList taskList, UI ui, Storage storage) throws DukeException {
-        StringBuffer contents = new StringBuffer("Tasks in your list are:\n________\n");
-        contents.append(taskList.getContents());
-        contents.append("________\n");
-        contents.append("Total: " + taskList.getSize());
+        StringBuffer contents = new StringBuffer();
+        openList(contents);
+        addTasksToList(contents, taskList);
         ui.showMessage(contents.toString());
+    }
+
+    private void openList(StringBuffer list) {
+        list.append("Tasks in your list are:\n________\n");
+    }
+
+    private void addTasksToList(StringBuffer list, TaskList taskList) {
+        list.append(taskList.getContents());
+    }
+
+    private void closeList(StringBuffer list, TaskList taskList) {
+        list.append("________\n");
+        list.append("Total: " + taskList.getSize());
     }
 }
