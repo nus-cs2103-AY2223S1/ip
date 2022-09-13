a99c79e66d5b4e6528c6e0f6aff666ee24a96272
optionalemon <xuyi9272@gmail.com>
2022-09-13 20:20:48 +0800

    Detect duplicates

--- src/main/java/pikachu/command/AddCommand.java
+++ src/main/java/pikachu/command/AddCommand.java
@@ -1,6 +1,7 @@
 package pikachu.command;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import pikachu.PikachuException;
 import pikachu.Storage;
@@ -8,6 +9,7 @@ import pikachu.TaskList;
 import pikachu.Ui;
 import pikachu.task.Deadline;
 import pikachu.task.Event;
+import pikachu.task.Task;
 import pikachu.task.Todo;
 
 /**
@@ -33,15 +35,19 @@ public class AddCommand extends Command {
      */
     public String execute(TaskList tasks, Ui ui, Storage storage) throws PikachuException {
         String tempStr = "";
+        List<Task> taskList = tasks.getTaskList();
         boolean isValidDdl = input.startsWith("deadline ") && input.contains(" /by ");
         boolean isValidTodo = input.startsWith("todo ");
         boolean isValidEvent = input.startsWith("event ") && input.contains(" /at ");
-        if (isValidDdl) {
+           if (isValidDdl) {
             String temp1 = input.split(" ", 2)[1];
             String[] temp2 = temp1.split(" /by ", 2);
             try {
                 LocalDate date = LocalDate.parse(temp2[1]);
                 Deadline newDdl = new Deadline(temp2[0], date);
+                if (isDuplicate(newDdl,taskList)) {
+                    return "Duplicate task pi!";
+                }
                 tasks.getTaskList().add(newDdl);
 
                 assert !tasks.getTaskList().isEmpty();
@@ -54,6 +60,9 @@ public class AddCommand extends Command {
             }
         } else if (isValidTodo) { //add as tasks
             Todo newTodo = new Todo(input.substring(5));
+            if (isDuplicate(newTodo,taskList)) {
+                throw new PikachuException("Duplicate task pi!");
+            }
             if (newTodo.getDescription().equals("")) {
                 throw new PikachuException("Pi-cannot be empty-pi");
             } else {
@@ -69,6 +78,9 @@ public class AddCommand extends Command {
             String temp1 = input.split(" ", 2)[1];
             String[] temp2 = temp1.split(" /at ", 2);
             Event newEvent = new Event(temp2[0], temp2[1]);
+            if (isDuplicate(newEvent,taskList)) {
+                throw new PikachuException("Duplicate task pi!");
+            }
             tasks.getTaskList().add(newEvent);
 
             assert !tasks.getTaskList().isEmpty();
@@ -86,6 +98,17 @@ public class AddCommand extends Command {
      * @return false, do not exit.
      */
     public boolean isExit() {
+        return false;
+    }
+
+    public boolean isDuplicate(Task newTask, List<Task> currTasks) {
+        for (Task task: currTasks) {
+            boolean isSameDescription = newTask.getDescription().equals(task.getDescription());
+            boolean isSameDetails = newTask.getTiming().equals(task.getTiming());
+            if (isSameDetails && isSameDescription) {
+                return true;
+            }
+        }
         return false;
     }
 }
