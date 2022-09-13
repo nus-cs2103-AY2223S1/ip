f0eb1ae7ebfa141de1e1ea9606a89545987edcf2
Rexong <rexonghanwei200035@gmail.com>
2022-09-13 22:27:09 +0800

    Change icon picture

--- src/main/java/duke/display/MainWindow.java
+++ src/main/java/duke/display/MainWindow.java
@@ -27,8 +27,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/SheldonUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/SheldonDuke.png"));
 
     /**
      * Initializes the UI and shows duke's welcome Text.
