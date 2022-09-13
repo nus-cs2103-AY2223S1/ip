69013d71549df0f3df61059c85aecdfb0db4058e
Tex Tang <tex_tang@outlook.com>
2022-09-13 21:14:41 +0800

    Fix comment typo

--- src/main/java/ted/ui/Main.java
+++ src/main/java/ted/ui/Main.java
@@ -11,7 +11,7 @@ import ted.Storage;
 import ted.Ted;
 
 /**
- * A GUI for Duke using FXML.
+ * A GUI for Ted using FXML.
  */
 public class Main extends Application {
 
--- src/main/java/ted/ui/MainWindow.java
+++ src/main/java/ted/ui/MainWindow.java
@@ -44,7 +44,7 @@ public class MainWindow extends AnchorPane {
     }
 
     /**
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * Creates two dialog boxes, one echoing user input and the other containing Ted's reply and then appends them to
      * the dialog container. Clears the user input after processing.
      */
     @FXML
