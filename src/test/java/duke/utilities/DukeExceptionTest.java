8ee94c98cf35122a67712d79efc4dce40890d9ad
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-09-13 12:40:13 +0800

    Add JUnit tests for Ui class

--- src/test/java/duke/utilities/DukeExceptionTest.java
+++ src/test/java/duke/utilities/DukeExceptionTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class DukeExceptionTest {
-    private DukeException dukeException= new DukeException("This is a test DukeException!");
+    private DukeException dukeException = new DukeException("This is a test DukeException!");
 
     @Test
     public void dukeException_message_correct() {
