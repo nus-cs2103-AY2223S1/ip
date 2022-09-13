da61af7d60253028f8ef96ee2fd3a326132e50b7
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-09-13 13:11:30 +0800

    Fix logic of ExitCommand

--- src/main/java/command/ExitCommand.java
+++ src/main/java/command/ExitCommand.java
@@ -9,21 +9,11 @@ import ui.UI;
  * Causes program to terminate.
  */
 public class ExitCommand extends Command {
-    private String[] slicedUserCommands;
 
-    public ExitCommand(String[] slicedUserInput) {
-        this.slicedUserCommands = slicedUserInput;
-    }
     @Override
     public void execute(TaskList taskList, UI ui, Storage storage) throws DukeException {
-        String command = ui.getCurrentInput();
-        boolean isExitCommand = command.trim().equalsIgnoreCase("bye");
-        if (isExitCommand) {
             ui.showExitMessage();
             System.exit(0);
-        } else {
-            ui.showError("error parsing exit command");
-        }
     }
 
     @Override
