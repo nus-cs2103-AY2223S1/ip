913011537233bf95c1d0044ca9e9b2dc47842453
maxtance <e0772757@u.nus.edu>
2022-09-13 16:28:51 +0800

    UI improvement to javaFx.

--- src/main/java/anya/MainWindow.java
+++ src/main/java/anya/MainWindow.java
@@ -6,7 +6,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.VBox;
 
 /**
