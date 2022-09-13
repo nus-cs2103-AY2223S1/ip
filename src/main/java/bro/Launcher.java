fc567640f72261374d4bdbc63f1316b165a98c48
anuanas2007 <e0905353@u.nus.edu>
2022-09-13 16:50:43 +0800

    Fix issues

--- src/main/java/bro/Launcher.java
+++ src/main/java/bro/Launcher.java
@@ -6,6 +6,11 @@ import javafx.application.Application;
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
+
+    /**
+     * Provides the entry point for Launcher class.
+     * @param args Command Line arguments.
+     */
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
