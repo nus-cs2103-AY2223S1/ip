3d7d8a0a64b1a5081a70e01434d30f333714b6be
Yuanxi Zhu <yuanxizhu@YXMacBook-Air.local>
2022-09-13 21:35:10 +0800

    Delete unused import statements

--- src/main/java/duke/models/Storage.java
+++ src/main/java/duke/models/Storage.java
@@ -3,7 +3,6 @@ package duke.models;
 import duke.exceptions.DukeException;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
