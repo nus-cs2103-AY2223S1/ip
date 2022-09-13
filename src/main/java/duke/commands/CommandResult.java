ab51ba41143d4ccfe9ce1b51531aa28a38b9d044
sikai00 <sikai4111@gmail.com>
2022-09-14 00:21:39 +0800

    CommandResult.java: Remove old unused constructor

--- src/main/java/duke/commands/CommandResult.java
+++ src/main/java/duke/commands/CommandResult.java
@@ -9,19 +9,10 @@ public class CommandResult {
     private final String msg;
 
     /**
-     * Initializes a new CommandResult instance. Defaults shouldExit to false.
+     * Initializes a new CommandResult instance.
      * @param msg Response message from the executed Command.
      */
     public CommandResult(String msg) {
-        this.msg = msg;
-    }
-
-    /**
-     * Initializes a new CommandResult instance.
-     * @param msg Response message from the executed Command.
-     * @param shouldExit Whether the Command led to a call for the program to exit
-     */
-    public CommandResult(String msg, boolean shouldExit) {
         this.msg = msg;
     }
 
