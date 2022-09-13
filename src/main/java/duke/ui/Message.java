3f6d2fa1be0d0d81ef4538052afbc53ebc85aa7b
Shashank Acharya <acharya.s@icloud.com>
2022-09-13 12:31:56 +0800

    Change messages for personality
    Made the bot seem more like Lurch.

--- src/main/java/duke/ui/Message.java
+++ src/main/java/duke/ui/Message.java
@@ -4,9 +4,9 @@ public enum Message {
     MARK("Very well, this task is done:"),
     UNMARK("Very well, this task is now undone:"),
     DELETE("Very well, task deleted:"),
-    ADDED("Very well, you have added another task:"),
-    SNOOZED("Very well, you have snoozed this task:"),
-    INVALID("I struggle to interpret your command."),
+    ADDED("Very well, added another task:"),
+    SNOOZED("Very well, snoozed this task:"),
+    INVALID("Your command is difficult to interpret."),
     GREETING("Greetings, I'm Lurch.\nYou rang?"),
     EMPTY("There is nothing in your agenda."),
     BYE("Have a lovely day with dark and cloudy skies."),
@@ -17,7 +17,7 @@ public enum Message {
     NO_BY_ERROR("Use \"/by\" to tell me when the event is, please"),
     NO_FOR_ERROR("Use \"/for\" to tell me how long to snooze, please"),
     DATE_FORMAT_ERROR("That is not how you write a date"),
-    NO_RESULTS_FOUND("I found nothing matching your request."),
+    NO_RESULTS_FOUND("No matches for your request."),
     DESCRIPTION_EMPTY("You will need to describe your task."),
     INVALID_SNOOZE_TYPE("You have not entered a valid snooze type.");
 
