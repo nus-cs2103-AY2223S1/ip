1c450af0b5dea56b10ed7ce548943bc49b6e1f3a
ruiqi7 <tinruiqi2002@gmail.com>
2022-09-13 14:17:04 +0800

    Add more personality

--- src/main/java/duke/common/Messages.java
+++ src/main/java/duke/common/Messages.java
@@ -16,15 +16,15 @@ public class Messages {
     public static final String MESSAGE_RETRIEVE_FILE_ERROR = "There has been an error retrieving your previous data file.";
     public static final String MESSAGE_EMPTY_FILE_ERROR = "You have not made changes to your task list yet!";
 
-    public static final String MESSAGE_TASK_ADDED = "Task added:\n  ";
-    public static final String MESSAGE_TASK_REMOVED = "Task removed:\n  ";
-    public static final String MESSAGE_TASK_DONE = "Task marked as done:\n  ";
-    public static final String MESSAGE_TASK_NOT_DONE = "Task marked as not done:\n  ";
-    public static final String MESSAGE_TASK_LIST = "Task list:\n";
+    public static final String MESSAGE_TASK_ADDED = "Sure! Task added:\n  ";
+    public static final String MESSAGE_TASK_REMOVED = "Alright! Task removed:\n  ";
+    public static final String MESSAGE_TASK_DONE = "Speedy, keep it up!\n\nTask marked as done:\n  ";
+    public static final String MESSAGE_TASK_NOT_DONE = "Welp, don't fall behind!\n\nTask marked as not done:\n  ";
+    public static final String MESSAGE_TASK_LIST = "Here's your task list:\n";
     public static final String MESSAGE_EMPTY_TASK_LIST = "Your task list is empty!";
     public static final String MESSAGE_NUMBER_OF_TASKS = "\n\nYou have %d task(s) in the list.";
 
-    public static final String MESSAGE_NO_SUCH_TASK = "No such task!";
+    public static final String MESSAGE_NO_SUCH_TASK = "Are you sure about that? There is no such task!";
     public static final String MESSAGE_PROVIDE_TASK_NUMBER = "Please provide a task number!";
     public static final String MESSAGE_PROVIDE_DESCRIPTION = "Please provide a task description!";
     public static final String MESSAGE_PROVIDE_DESCRIPTION_AND_DATE = "Please provide a task description and date!";
@@ -32,8 +32,8 @@ public class Messages {
     public static final String MESSAGE_PROVIDE_DATE = "Please provide a date!";
     public static final String MESSAGE_PROVIDE_MORE_DETAILS = "Please provide more details!";
 
-    public static final String MESSAGE_MATCHING_TASKS = "Here are your matching task(s):\n";
-    public static final String MESSAGE_NO_MATCHING_TASKS = "Hmm... there  are no matching tasks in your list!";
+    public static final String MESSAGE_MATCHING_TASKS = "Aha, found them!\n\nHere are your matching task(s):\n";
+    public static final String MESSAGE_NO_MATCHING_TASKS = "Hmm... there are no matching tasks in your list!";
 
-    public static final String MESSAGE_UNDO = "The most recent change to your task list has been undone!\n\n";
+    public static final String MESSAGE_UNDO = "Easy-peasy! The most recent change to your task list has been undone!\n\n";
 }
