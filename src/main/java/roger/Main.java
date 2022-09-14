dd96202d38c3fa6c993b9ed6af70d5edeb1f5321
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-09-14 13:42:26 +0800

    Fix Main not referencin the correct `Application.class`

--- src/main/java/roger/Main.java
+++ src/main/java/roger/Main.java
@@ -1,16 +1,5 @@
 package roger;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import roger.storage.Storage;
-import roger.ui.Parser;
-import roger.ui.controllers.MainWindow;
 
 /**
  * A GUI for Duke using FXML.
@@ -18,7 +7,7 @@ import roger.ui.controllers.MainWindow;
 public class Main {
 
     public static void main(String[] args) {
-        Launcher.launch(Application.class, args);
+        Launcher.launch(Launcher.class, args);
         System.exit(0);
     }
 }
