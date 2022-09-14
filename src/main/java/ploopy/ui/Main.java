b18e10e6f9ee5e2c34a3367edafbedadaff12734
nitant-p <nitantpanicker@gmail.com>
2022-09-14 17:12:42 +0800

    Change Ui.png and add title to GUI window

--- src/main/java/ploopy/ui/Main.java
+++ src/main/java/ploopy/ui/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.setTitle("Ploopy");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setPloopy(ploopy);
             stage.show();
