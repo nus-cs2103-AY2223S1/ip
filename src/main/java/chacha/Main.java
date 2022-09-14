4cfda0836c6c8fd3197280be6fa510f6eaa98ee9
alexandermula <alexandersinghabdullah@gmail.com>
2022-09-14 19:46:46 +0800

    add javaFX

--- src/main/java/chacha/Main.java
+++ src/main/java/chacha/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/resources/view/MainWindow.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
