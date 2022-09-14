e22668764dc2d2a2b70b5a7381784f93a2b999e5
lyuting47 <luting_c@yahoo.com>
2022-09-15 00:46:01 +0800

    Add personality

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -15,47 +15,47 @@ public class Ui {
     }
 
     public void greet() {
-        System.out.println(":D Hello! This is Duke! What can I do for you today?");
+        System.out.println("How may I help you today, your Majesty?");
     }
 
     public void goodbye() {
-        System.out.println(":) Bye. Hope to see you again soon!");
+        System.out.println("Glad to be of service, your Majesty.");
         isActive = false;
     }
 
     public void printSuccessfulClear() {
-        System.out.println("Your task list and save file have been cleared!");
+        System.out.println("Your duty list has been erased.");
     }
 
     public void printSuccessfulMark() {
-        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("Excellent work, my Lord. You have completed the following duty:");
     }
 
     public void printSuccessfulUnmark() {
-        System.out.println("Okie, I've marked this task as not done yet:");
+        System.out.println("Alas, there seems to be more work to be done for this duty:");
     }
 
     public void printSuccessfulAdd() {
-        System.out.println("Okie. I've added this task:");
+        System.out.println("Understood, my Lord. I have added this duty:");
     }
 
     public void printSuccessfulDelete() {
-        System.out.println("Okie, I've deleted this task:");
+        System.out.println("As you wish my Lord, I have removed this duty:");
     }
 
     public void printSuccessfulUpdate() {
-        System.out.println("Okie, I've updated this task:");
+        System.out.println("Right away my Lord, here is the amended duty:");
     }
 
     public void printNoOfTasks(TaskList ts) {
-        System.out.println("Now you have " + ts.getTasksLength() + " tasks in your list.");
+        System.out.println("You now have " + ts.getTasksLength() + " duties in your list.");
     }
 
     public void printTaskList(TaskList ts) {
         if (ts.getTasksLength() == 0) {
-            System.out.println("No tasks yet~");
+            System.out.println("You do not currently have any duties, your Majesty.");
         } else {
-            System.out.println("Here are the tasks in your list:");
+            System.out.println("Here are the duties in your list:");
             ts.printAllTasks();
         }
     }
@@ -66,9 +66,9 @@ public class Ui {
 
     public void printFoundResults(ArrayList<Task> tasks) {
         if (tasks.size() == 0) {
-            System.out.println("Didn't find any matching tasks~");
+            System.out.println("My apologies, I did not find any duties that match.");
         } else {
-            System.out.println("Here are the matching tasks in your list:");
+            System.out.println("Here are the duties in your list that matched:");
             for (int i = 1; i <= tasks.size(); i++) {
                 System.out.println(i + "." + tasks.get(i - 1).toString());
             }
@@ -83,24 +83,24 @@ public class Ui {
      */
     public void printErrorMessage(Exception e, TaskList tasks) {
         if (e instanceof IOException) {
-            System.out.println(":( An error occurred reading/writing your save files!");
+            System.out.println("Apologies my Lord, I'm having trouble writing/reading duty list.");
         } else if (e instanceof InvalidCommandException) {
-            System.out.println(":( Sorry, I don't understand the command: " + e);
+            System.out.println("Sorry my Lord, I do not understand what you mean by " + e);
         } else if (e instanceof TaskNumberException) {
-            System.out.println(":( Oops! Please enter a valid task number!");
-            System.out.println("You currently have " + tasks.getTasksLength() + " tasks.");
+            System.out.println("Your Majesty, pray give me a valid duty number.");
+            System.out.println("You currently have " + tasks.getTasksLength() + " duties.");
         } else if (e instanceof TaskTypeException) {
-            System.out.println(":( Oops! You can't do that to this type of tasks!");
+            System.out.println("I'm sorry your Majesty, I cannot do that with this type of duty.");
         } else if (e instanceof EmptyFieldException) {
-            System.out.println(":( Oops! One or more fields in the command is missing!");
+            System.out.println("My Lord, pray give me more details in the command.");
         } else if (e instanceof DeadlineFormatException) {
-            System.out.println(":( Oops! That's not the right way to set a deadline!");
+            System.out.println("My Lord, it appears you are trying to set a Deadline.");
             System.out.println("Please use this format: \"deadline <description> /by <time>\"");
         } else if (e instanceof EventFormatException) {
-            System.out.println(":( Oops! That's not the right way to set an event!");
+            System.out.println("My Lord, it appears you are trying to set an Event.");
             System.out.println("Please use this format: \"event <description> /at <time>\"");
         } else {
-            System.out.println(":( Oops! An unknown error has occurred!");
+            System.out.println("Woe! A strange terrible fate has befallen us!");
             e.printStackTrace();
         }
     }
