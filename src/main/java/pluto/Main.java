702093d80676bef073980783ecda5729c36f7b07
pratham31012002 <pratham31012002@gmail.com>
2022-09-14 22:46:13 +0800

    Add Title

--- src/main/java/pluto/Main.java
+++ src/main/java/pluto/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Pluto");
             fxmlLoader.<MainWindow>getController().setPluto(pluto);
             stage.show();
         } catch (IOException e) {
