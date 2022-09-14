7d67289a88542d43a1b5eadb27afb75f21e0bed5
pratham31012002 <pratham31012002@gmail.com>
2022-09-15 01:00:47 +0800

    Implement C-Help

--- src/main/java/pluto/Ui.java
+++ src/main/java/pluto/Ui.java
@@ -98,10 +98,10 @@ public class Ui {
         helpMessage.append("Add a event task    \n\t event <task name> /at <date>\n");
         helpMessage.append("Add a deadline task \n\t deadline <task name> /by <date>\n");
         helpMessage.append("Delete a task       \n\t delete <task number>\n");
-        helpMessage.append("Find a keyword      \n\t find <keyword(s)>\n");
+        helpMessage.append("Find keyword(s)     \n\t find <keyword(s)>\n");
         helpMessage.append("List all tasks      \n\t list\n");
         helpMessage.append("Mark as done        \n\t mark <task number>\n");
-        helpMessage.append("Unmark              \n\t unmark <task number>\n");
+        helpMessage.append("Unmark task         \n\t unmark <task number>\n");
         helpMessage.append("Reschedule a task   \n\t reschedule <task number> <date>\n");
         helpMessage.append("Show day schedule   \n\t show <dd-MM-yyyy HHmm>\n");
         helpMessage.append("Exit                \n\t bye\n");
