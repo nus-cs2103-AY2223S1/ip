188fa5a1a2d5a609a12f7744436b153b75e3e9ca
peppapighs <ptk.prasertsuk@gmail.com>
2022-09-14 19:49:16 +0800

    Add launcher class for GUI app

---
+++ src/main/java/duke/Launcher.java
@@ -0,0 +1,14 @@
+package duke;
+
+import javafx.application.Application;
+
+/**
+ * A launcher for Duke GUI
+ * 
+ * @author Pontakorn Prasertsuk
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
