477fd4e68a9deff679255a593d698522d4fe0abd
tantzeyeong <tzeyeongtan@gmail.com>
2022-09-15 16:34:41 +0800

    Fix checkstyle errors

--- src/Test/java/duke/model/TaskListTest.java
+++ src/Test/java/duke/model/TaskListTest.java
@@ -1,11 +1,11 @@
 package duke.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import duke.exception.DukeException;
 
