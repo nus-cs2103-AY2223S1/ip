ac4404af18a24a3dd64f64cf26e6cde55a48f189
Jessica Mary <jessicamary472@gmail.com>
2022-09-16 00:12:37 +0800

    Style send button

--- src/main/java/application/MainWindow.java
+++ src/main/java/application/MainWindow.java
@@ -6,8 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import mew.Mew;
 
 /**
