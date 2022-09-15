15776e8d8e3d1b0d78de83027a02cbc6f2e5f819
myangat <mingyangy31@gmail.com>
2022-09-15 21:35:56 +0800

    Improve a JUnit Test
    Improving JUnit tests can be beneficial to testing the behaviour of code.
    
    TaskList is an important class to do operation on task list.
    
    Using Assertion(expected, actual) to check if the code works.

--- src/test/java/Duke/TaskListTest.java
+++ src/test/java/Duke/TaskListTest.java
@@ -13,9 +13,11 @@ public class TaskListTest {
     public void testTaskList() throws DukeException {
 
         ToDo todo1 = new ToDo("HW1");
+        ToDo todo2 = new ToDo("HW2");
         assertEquals("T | false | HW1\n", todo1.save());
         TaskList tasks = new TaskList();
         tasks.addTask(todo1);
+        tasks.addTask(todo2);
         tasks.markAsDone(0);
         assertEquals("T | true | HW1\n", tasks.getTaskByIndex(0).save());
 
