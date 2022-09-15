99cd93b8896eef0c3d93397da27cd93d2682fa43
shamooose <e0726246@u.nus.edu>
2022-09-15 17:08:20 +0800

    Update test case for Event

--- src/test/java/scruffles/EventTest.java
+++ src/test/java/scruffles/EventTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class EventTest {
 
     @Test
-    public void testToString() {
+    public void testToString() throws TimeErrorException {
         Event e = new Event("yeet", "2022-09-09 from 16:30 to 18:30");
         assertEquals("[E][ ] yeet (at: 9 SEPTEMBER 2022 16:30 to 18:30)", e.toString());
     }
