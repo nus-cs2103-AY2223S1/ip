d3eb0eced18b6b66479916476cd548f4f2f513da
johnrhimawan <johnr.himawan@gmail.com>
2022-09-15 23:21:53 +0800

    Add more tests

--- src/test/java/duke/parser/ParserTest.java
+++ src/test/java/duke/parser/ParserTest.java
@@ -1,17 +1,26 @@
 package duke.parser;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import duke.command.ByeCommand;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
-import org.junit.jupiter.api.Test;
-
+import duke.command.ListCommand;
+import duke.command.RemindCommand;
 import duke.exception.DukeException;
 
 public class ParserTest {
-    public void parseTest() throws DukeException {
-        assertEquals(new ByeCommand(), Parser.parse("bye"));
+    @Test
+    public void parseTest_bye() throws DukeException {
+        assertTrue(Parser.parse("bye") instanceof ByeCommand);
+    }
+
+    @Test
+    public void parseTest_list() throws DukeException {
+        assertTrue(Parser.parse("list") instanceof ListCommand);
+    }
+
+    @Test
+    public void parseTest_remind() throws DukeException {
+        assertTrue(Parser.parse("remind") instanceof RemindCommand);
     }
 }
