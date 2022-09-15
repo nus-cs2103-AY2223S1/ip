8dee91452a3c0e9fcc6bea2d14794f6f717fc240
Jason Christopher <jason.christopher@u.nus.edu>
2022-09-16 00:44:37 +0800

    Implement varargs for concatenate

--- src/main/java/duke/tasks/Date.java
+++ src/main/java/duke/tasks/Date.java
@@ -1,5 +1,6 @@
 package duke.tasks;
 
+import duke.exceptions.DukeException;
 import duke.exceptions.DukeInvalidDateException;
 
 import java.time.format.DateTimeFormatter;
@@ -64,36 +65,99 @@ public class Date {
      *
      * @return the string representation of the time field in the "a.m/p.m" format
      */
-    private String timeToString() {
+    private String timeToString() throws DukeException {
         assert(time > 0);
-        int hours = (int) Math.floor(time / 100);
-        int minutes = time % 100;
-        String timeOfDay = "a.m.";
-        String result = "";
+        int hours = calculateHourFromTime(time);
+        int minutes = calculateMinutesFromTime(time);
+        String hourString = getHours(hours);
+        String minuteString = getMinutes(minutes);
+        String timeOfDay = getTimeOfDay(hours);
+        String result = concatenateStrings(hourString, minuteString, timeOfDay);
+        return result;
+    }
 
+    /**
+     * Calculates the number of hours given the time
+     *
+     * @param time the Time instance of the deadline or event
+     * @return the number of hours in the time
+     */
+    private int calculateHourFromTime(int time) {
+        return (int) Math.floor(time / 100);
+    }
+
+    /**
+     * Calculates the number of minutes given the time
+     *
+     * @param time the Time instance of the deadline or event
+     * @return the number of minutes in the time
+     */
+    private int calculateMinutesFromTime(int time) {
+        return time % 100;
+    }
+
+    /**
+     * Gets the string representation of the number of hours
+     *
+     * @param hours The number of hours in the date format
+     * @return the string representation of the number of hours
+     */
+    private String getHours(int hours) {
+        if (hours == 0) {
+            return "12.";
+        } else if (hours > 12) {
+            return (hours - 12) + ".";
+        } else {
+            return hours + ".";
+        }
+    }
+
+    /**
+     * Gets the time of the day, whether it is a.m. or p.m.
+     *
+     * @param hours The hour value of the time
+     * @return "a.m." if the hours is less than 12 p.m., "p.m." otherwise
+     */
+    private String getTimeOfDay(int hours) {
+        assert(hours > 0);
         if (hours > 12) {
             hours -= 12;
-            timeOfDay = "p.m.";
+            return "p.m.";
         } else if (hours == 12) {
-            timeOfDay = "a.m.";
-        }
-
-        if (hours == 0) {
-            result += "12.";
+            return "a.m.";
         } else {
-            result += hours + ".";
+            return "a.m.";
         }
+    }
 
+    /**
+     * Gets the string representation of the number of minutes
+     *
+     * @param minutes The minutes in time
+     * @return a string representation of minutes
+     */
+    private String getMinutes(int minutes) {
         if (minutes == 0) {
-            result += "00";
+            return "00";
         } else if (minutes < 10) {
-            result += "0" + minutes;
+            return "0" + minutes;
         } else {
-            result += minutes;
+            return Integer.toString(minutes);
         }
+    }
 
-        result += timeOfDay;
-        return result;
+    /**
+     * Concatenates a variable number of strings together
+     *
+     * @param strings a variable number of strings to concatenate
+     * @return a string that is resulted from concatenating the multiple strings
+     */
+    private String concatenateStrings(String... strings) {
+        StringBuilder builder = new StringBuilder();
+        for (String s : strings) {
+            builder.append(s);
+        }
+        return builder.toString();
     }
 
     /**
@@ -103,6 +167,11 @@ public class Date {
      */
     @Override
     public String toString() {
-        return dateToString() + " " + timeToString();
+        try {
+            return dateToString() + " " + timeToString();
+        } catch (DukeException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
