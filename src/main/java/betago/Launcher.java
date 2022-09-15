f6fdddf00a8b845a001aed90cf05279911baf508
linuschancs <mrlinuschan@gmail.com>
2022-09-15 16:40:39 +0800

    Fix .jar file error with Gradle

--- src/main/java/betago/Launcher.java
+++ src/main/java/betago/Launcher.java
@@ -1,12 +1,39 @@
 package betago;
 
+import java.io.IOException;
+
+import betago.controller.MainWindow;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * A launcher class to workaround classpath issues.
  */
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(Main.class, args);
+public class Launcher extends Application {
+    private Duke duke = new Duke();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            stage.setTitle("BetaGo");
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Launches the application.
+     */
+    public void launch() {
+        Application.launch();
     }
 }
--- src/main/java/betago/Main.java
+++ src/main/java/betago/Main.java
@@ -1,35 +1,15 @@
 package betago;
 
-
-import java.io.IOException;
-
-import betago.controller.MainWindow;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-
 /**
  * A GUI for Duke using FXML.
  */
-public class Main extends Application {
-
-    private Duke duke = new Duke();
-
-    @Override
-    public void start(Stage stage) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-            AnchorPane ap = fxmlLoader.load();
-            Scene scene = new Scene(ap);
-            stage.setScene(scene);
-            stage.setTitle("BetaGo");
-            fxmlLoader.<MainWindow>getController().setDuke(duke);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+public class Main {
+    /**
+     * Calls the launch method in launcher.
+     */
+    public static void main(String[] args) {
+        Launcher launcher = new Launcher();
+        launcher.launch();
     }
 }
 
