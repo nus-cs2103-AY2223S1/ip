f755a2d81ced3f0ce964189cc42547b4a85e60db
Elben85 <elbert.benedict@gmail.com>
2022-09-15 10:34:11 +0800

    Add More Parser Unit Tests

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -1,14 +1,14 @@
 package duke;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import duke.command.*;
 import org.junit.jupiter.api.Test;
 
 import duke.exception.DukeException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ParserTest {
@@ -46,5 +46,126 @@ public class ParserTest {
         String expected = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         String actual = Parser.printSaveFileDate(date);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parseUserInput_byeCommand_returnByeCommandInstance() {
+        try {
+            assertTrue(Parser.parseUserInput("bye") instanceof ByeCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_todoCommand_returnToDoCommandInstance() {
+        try {
+            String command = "todo ijbnegini";
+            assertTrue(Parser.parseUserInput(command) instanceof TodoCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_deadlineCommand_returnDeadlineCommandInstance() {
+        try {
+            String command = "deadline wienginf /by 2022-10-10";
+            assertTrue(Parser.parseUserInput(command) instanceof DeadlineCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_eventCommand_returnDeadlineCommandInstance() {
+        try {
+            String command = "event woedgn /at 2022-10-10";
+            assertTrue(Parser.parseUserInput(command) instanceof EventCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_markCommand_returnMarkCommandInstance() {
+        try {
+            String command = "mark 2";
+            assertTrue(Parser.parseUserInput(command) instanceof MarkCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_unmarkCommand_returnUnmarkCommandInstance() {
+        try {
+            String command = "unmark 2";
+            assertTrue(Parser.parseUserInput(command) instanceof UnmarkCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_deleteCommand_returnDeleteCommandInstance() {
+        try {
+            String command = "delete 3";
+            assertTrue(Parser.parseUserInput(command) instanceof DeleteCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_priorityCommand_returnPriorityCommandInstance() {
+        try {
+            String command1 = "priority 1 high";
+            String command2 = "priority 2 medium";
+            String command3 = "priority 3 low";
+            String command4 = "priority 4 none";
+
+            assertTrue(Parser.parseUserInput(command1) instanceof PriorityCommand);
+            assertTrue(Parser.parseUserInput(command2) instanceof PriorityCommand);
+            assertTrue(Parser.parseUserInput(command3) instanceof PriorityCommand);
+            assertTrue(Parser.parseUserInput(command4) instanceof PriorityCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_findCommand_returnFindCommandInstance() {
+        try {
+            String command = "find wjaeni";
+            assertTrue(Parser.parseUserInput(command) instanceof FindCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_listCommand_returnListCommandInstance() {
+        try {
+            String command = "list";
+            assertTrue(Parser.parseUserInput(command) instanceof ListCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void parseUserInput_invalidCommands_throwDukeExceptions() {
+        String[] invalidCommands = {"wnegfn", "todo", "deadline", "event", "deadline wwoefn",
+                "event iwfi", "find", "priority", "priority 1"};
+
+        for(int i = 0; i < invalidCommands.length; ++i) {
+            try{
+                Parser.parseUserInput(invalidCommands[i]);
+                fail();
+            } catch (DukeException e) {
+                //pass;
+            }
+        }
     }
 }
