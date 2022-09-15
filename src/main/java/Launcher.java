ce239aaca9aa62e37c5f7fda541006d98052adda
ShenyiCui <shenyicui@outlook.com>
2022-09-16 02:48:14 +0800

    style: fix checkstyle errors

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -5,6 +5,11 @@ import javafx.application.Application;
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
+    /**
+     * Main driver class used to launch the application.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Storage.initStorage();
         Application.launch(Main.class, args);
