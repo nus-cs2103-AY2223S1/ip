fcfd675a1264fe235e24cb4c1186fd9ba7119565
TZL0 <tianzeee@outlook.com>
2022-09-16 01:49:23 +0800

    Update Tests

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -7,12 +7,12 @@ public class DukeTest {
     protected final Storage testStorage = new Storage(testPath);
     protected Duke duke = new Duke(testPath);
 
-    protected final String wholeFormattedTasklist = "T } 1 } explore\n"
-            + "E } 0 } presentation } 2022-10-01 12:34\n"
-            + "D } 0 } week 3 cs2103t project } 2022-08-25 23:45\n"
-            + "E } 1 } CS2109S Lecture } 2022-08-26 11:22\n"
-            + "E } 0 } CS2109S Lecture } 2022-08-19 00:01\n"
-            + "E } 1 } CS2109S Lecture } 2022-08-12 02:59\n"
-            + "E } 0 } CS2100 week 3 recitation } 2022-08-23 16:00\n"
+    protected final String wholeFormattedTasklist = "T } 1 } explore" + System.lineSeparator()
+            + "E } 0 } presentation } 2022-10-01 12:34" + System.lineSeparator()
+            + "D } 0 } week 3 cs2103t project } 2022-08-25 23:45" + System.lineSeparator()
+            + "E } 1 } CS2109S Lecture } 2022-08-26 11:22" + System.lineSeparator()
+            + "E } 0 } CS2109S Lecture } 2022-08-19 00:01" + System.lineSeparator()
+            + "E } 1 } CS2109S Lecture } 2022-08-12 02:59" + System.lineSeparator()
+            + "E } 0 } CS2100 week 3 recitation } 2022-08-23 16:00" + System.lineSeparator()
             + "D } 0 } CS2102 tutorial 1 to finish } 2022-08-26 12:00";
 }
--- src/test/java/duke/TaskListTest.java
+++ src/test/java/duke/TaskListTest.java
@@ -12,8 +12,10 @@ public class TaskListTest {
     @Test
     public void test1() {
         TaskList actual = new TaskList();
-        TaskList expected = Parser.parseTaskList("E } 0 } cs1010s } 2022-01-01 12:34\n"
-                + "D } 0 } cs1231s } 2022-02-01 13:34\n"
+        TaskList expected = Parser.parseTaskList("E } 0 } cs1010s } 2022-01-01 12:34"
+                + System.lineSeparator()
+                + "D } 0 } cs1231s } 2022-02-01 13:34"
+                + System.lineSeparator()
                 + "E } 0 } cs2030s } 2022-01-21 02:34");
         actual.add(Task.event("cs1010s", Parser.parseStringToDateTime("2022-01-01 12:34")));
         actual.add(Task.deadline("cs1231s", Parser.parseStringToDateTime("2022-02-01 13:34")));
@@ -24,7 +26,8 @@ public class TaskListTest {
     @Test
     public void test2() {
         TaskList actual = new TaskList();
-        TaskList expected = Parser.parseTaskList("E } 0 } cs1010s } 2022-01-01 12:34\n"
+        TaskList expected = Parser.parseTaskList("E } 0 } cs1010s } 2022-01-01 12:34"
+                + System.lineSeparator()
                 + "E } 0 } cs2030s } 2022-01-21 02:34");
         actual.add(Task.event("cs1010s", Parser.parseStringToDateTime("2022-01-01 12:34")));
         actual.add(Task.deadline("cs1231s", Parser.parseStringToDateTime("2022-02-01 13:34")));
