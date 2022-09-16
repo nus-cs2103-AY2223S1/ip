ec876585d5893e431b384c4d83338ef596c6d61c
Ryan Chua Zong Xun <e0725636@u.nus.edu>
2022-09-17 01:26:24 +0800

    Add title to GUI window

--- src/main/java/fred/Main.java
+++ src/main/java/fred/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Fred");
             fxmlLoader.<MainWindow>getController().setFred(fred);
             stage.show();
         } catch (IOException e) {
