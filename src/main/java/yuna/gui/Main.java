e388b06461dd9b520983ec9ea940b871db89a9c0
bryanngzh <bryanngzihao@gmail.com>
2022-09-16 14:57:05 +0800

    Add Ui.png

--- src/main/java/yuna/gui/Main.java
+++ src/main/java/yuna/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Yuna");
             fxmlLoader.<MainWindow>getController().setYuna(yuna);
             fxmlLoader.<MainWindow>getController().sendGreeting();
             stage.show();
