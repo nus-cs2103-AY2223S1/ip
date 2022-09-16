77f8b6153f8b8c528c5aa8cfac6cf07c8805a1bd
Richard Qi <55354921+riccqi@users.noreply.github.com>
2022-09-16 14:42:50 +0800

    add ui image

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -1,13 +1,13 @@
 package duke;
 
+import java.io.IOException;
+
 import duke.ui.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * A GUI for Duke using FXML.
@@ -24,6 +24,7 @@ public class Main extends Application {
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
+            stage.setTitle("Duke");
         } catch (IOException e) {
             e.printStackTrace();
         }
