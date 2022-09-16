45355916adcc3a9cb440da3212423dc7c8071ec4
quannam0124 <77949597+quannam0124@users.noreply.github.com>
2022-09-16 21:27:07 +0800

    Set title of stage to be Duke Manager

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke Manager");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
