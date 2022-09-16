bf995c6d35764e0bdf2f8e137d2d28b53895623a
yuehernkang <yuehernkang@hotmail.com>
2022-09-16 18:53:29 +0800

    Improvement to Help messages

--- src/main/java/duke/constants/HelpMessages.java
+++ src/main/java/duke/constants/HelpMessages.java
@@ -19,17 +19,17 @@ public final class HelpMessages {
     public static final String HELP_DELETE = "Deletes a task";
     public static final String HELP_BYE = "Exits the program";
     public static final String HELP_MESSAGE = "QUACK! Deadline Duck is here to give u the list of supported commands!"
-            + NEW_LINE + "'list' - " + HELP_LIST
-            + NEW_LINE + "'find <QUERY>' - " + HELP_FIND
             + NEW_LINE + "'todo <TASK_NAME>' - " + HELP_TODO
             + NEW_LINE + "'deadline <TASK_NAME> /by <YYYY-MM-DD>' - " + HELP_DEADLINE
             + NEW_LINE + "'event <TASK_NAME> /at (date) [time]' - " + HELP_EVENT
             + NEW_LINE + "'note <NOTE_DESCRIPTION>' - " + HELP_NOTE
+            + NEW_LINE + "'list' - " + HELP_LIST
+            + NEW_LINE + "'find <QUERY>' - " + HELP_FIND
             + NEW_LINE + "'postpone <TASK_INDEX>' - " + HELP_POSTPONE
             + NEW_LINE + "'mark <TASK_INDEX>' - " + HELP_MARK
             + NEW_LINE + "'unmark <TASK_INDEX>' - " + HELP_UNMARK
-            + NEW_LINE + "'help' - " + HELP_HELP
-            + NEW_LINE + "'quack' - " + HELP_QUACK
             + NEW_LINE + "'delete <TASK_INDEX>' - " + HELP_DELETE
+            + NEW_LINE + "'quack' - " + HELP_QUACK
+            + NEW_LINE + "'help' - " + HELP_HELP
             + NEW_LINE + "'bye' - " + HELP_BYE;
 }
