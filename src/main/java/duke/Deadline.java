62848e19326f08960ff71ead5ca7100dd023fdb7
jgyj123 <jonathangohyj@gmail.com>
2022-09-16 19:05:59 +0800

    Allow users to indicate time of event and deadline

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -1,13 +1,13 @@
 package duke;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
  * Represents a deadline that can be created by the user
  */
 public class Deadline extends Task {
-    private LocalDate date;
+    private LocalDateTime date;
 
     /**
      * Constructor used to create a new deadline object
@@ -16,13 +16,14 @@ public class Deadline extends Task {
      */
     public Deadline (String s , boolean isCompleted) {
         super(s.split("/by")[0].substring(8).strip(), isCompleted);
-        date = LocalDate.parse(s.split("/by")[1].strip());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        date = LocalDateTime.parse(s.split("/by")[1].strip(), formatter);
     }
 
     public String toString() {
         String completion = this.isComplete() ? "[X]" : "[ ]";
         return "[D]" + completion + " " + this.getTaskName() + " (by: " +
-                date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+                date.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm")) + ")";
     }
     /**
      * Returns a formatted version of the task to store in memory
@@ -30,6 +31,7 @@ public class Deadline extends Task {
      */
     public String toFormattedString() {
         String completion = this.isComplete() ? "1" : "0";
-        return "D | " + completion + " | " + this.getTaskName() + " | " + date;
+        return "D | " + completion + " | " + this.getTaskName() + " | "
+                + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(date);
     }
 }
--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -3,11 +3,11 @@ package duke;
 /**
  * Represents an Event that can be created by the user
  */
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
-    private LocalDate date;
+    private LocalDateTime date;
 
     /**
      * Constructor used to create a new event object
@@ -16,13 +16,14 @@ public class Event extends Task {
      */
     public Event(String s, boolean isCompleted) {
         super(s.split("/at")[0].substring(5).strip(), isCompleted);
-        date = LocalDate.parse(s.split("/at")[1].strip());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        date = LocalDateTime.parse(s.split("/at")[1].strip(), formatter);
     }
     @Override
     public String toString() {
         String completion = this.isComplete() ? "[X]" : "[ ]";
         return "[E]" + completion + " " + this.getTaskName() + " (at: "
-            + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+            + date.format(DateTimeFormatter.ofPattern("MMM d yyyy HH:mm")) + ")";
     }
 
     /**
@@ -31,6 +32,7 @@ public class Event extends Task {
      */
     public String toFormattedString() {
         String completion = this.isComplete() ? "1" : "0";
-        return "E | " + completion + " | " + this.getTaskName() + " | " + date;
+        return "E | " + completion + " | " + this.getTaskName() + " | "
+                + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(date);
     }
 }
