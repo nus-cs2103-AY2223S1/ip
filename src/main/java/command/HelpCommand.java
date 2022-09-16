19a6ee950e60da55ac1e85d85fa1717b9768ce81
shaniceng <ngshanice1@yahoo.com.sg>
2022-09-16 10:13:17 +0800

    Update Help command

--- src/main/java/command/HelpCommand.java
+++ src/main/java/command/HelpCommand.java
@@ -26,7 +26,6 @@ public class HelpCommand extends Command {
      */
     @Override
     public String execute(TaskList taskList, Ui ui, Storage storage) {
-        super.setExit();
         return ui.printHelp();
     }
 }
