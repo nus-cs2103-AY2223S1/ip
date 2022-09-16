d71d6d1e476290d6ef252b325ff4321d210dfc0c
Hongyi6328 <6328huang@gmail.com>
2022-09-16 20:30:00 +0800

    Add better GUI

--- src/main/java/duke/ui/DialogBox.java
+++ src/main/java/duke/ui/DialogBox.java
@@ -49,12 +49,15 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        var db = new DialogBox(text, img);
+        db.dialog.setWrapText(true);
+        return db;
     }
 
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
+        db.dialog.setWrapText(true);
         return db;
     }
 }
