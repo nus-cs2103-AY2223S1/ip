0f0088003572d94b93ebcbf3ab9b2e2e8f83d16a
Prearo <kohshanghui@gmail.com>
2022-09-16 22:46:10 +0800

    Fix Deadline, Event not accepting DateTime
    Handling of formatted input was incorrect in Deadline.
    This has been fixed with correct DateTimeFormatters.
    The same extension has been applied to Event.

--- src/main/java/duke/tasks/Event.java
+++ src/main/java/duke/tasks/Event.java
@@ -1,13 +1,28 @@
 package duke.tasks;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 /**
  * Represents an Event at a specified time
  */
 public class Event extends Task {
-    private String at;
+    protected String at;
+    protected LocalDateTime localDateTime;
+    // Supported DateTime and Date formats
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd[ HHmm]");
 
     public Event(String description, String at) {
         super(description);
+        try {
+            this.localDateTime = LocalDate.parse(at, df).atTime(0,0,0);
+            this.localDateTime = LocalDateTime.parse(at, dtf);
+        } catch (DateTimeParseException ignored) {
+            // Ignore usage of localDateTime if input is not formatted as one
+        }
         this.at = at;
     }
 
@@ -25,6 +40,9 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: "
+                + (localDateTime == null ? at
+                : localDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy HHmm")))
+                + ")";
     }
 }
