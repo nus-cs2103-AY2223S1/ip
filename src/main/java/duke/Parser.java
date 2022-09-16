9607e59de539dffca589b49045784c7ae1486dd3
Justin Peng <justinpeng13@gmail.com>
2022-09-16 18:05:52 +0800

    Add Activity subclass of Task
    The Activity class allows users to specify a fixed duration in hours
    that a task will take to complete.

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,5 +1,13 @@
 package duke;
 
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.TextStyle;
+import java.time.temporal.ChronoField;
+
 import duke.Duke.Command;
 import duke.exception.DukeInvalidCommandException;
 
@@ -9,6 +17,18 @@ import duke.exception.DukeInvalidCommandException;
  * @author Justin Peng
  */
 public class Parser {
+    /** Datetime formatter for user input which accepts dd/MM/yyyy or dd/MM/yyyy HH:mm */
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
+            .appendPattern("dd/MM/yyyy ")
+            .optionalStart()
+            .appendPattern("HH:mm ")
+            .optionalEnd()
+            .appendZoneText(TextStyle.SHORT)
+            .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+            .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+            .toFormatter();
+
     /**
      * Parses the input into a command and parameters.
      *
@@ -34,5 +54,27 @@ public class Parser {
             throw new DukeInvalidCommandException();
         }
         return Command.valueOf(command);
+    }
+
+    /**
+     * Parses the input string into an instance of {@code ZonedDateTime}.
+     *
+     * @param input The input {@code String} to be parsed.
+     * @param timeZone The timezone of the input.
+     * @return The {@code ZonedDateTime} object.
+     */
+    protected ZonedDateTime parseDateTime(String input, ZoneId timeZone) {
+        return ZonedDateTime.parse(input + " " + timeZone, DATE_TIME_FORMATTER);
+    }
+
+    /**
+     * Parses the input string into an instance of {@code Duration}.
+     *
+     * @param input The input {@code String} to be parsed.
+     * @return The {@code Duration} object.
+     */
+    protected Duration parseDuration(String input) {
+        // TODO: Implement parseDuration
+        return Duration.ofHours(Integer.parseInt(input));
     }
 }
---
+++ src/main/java/duke/task/Activity.java
@@ -0,0 +1,47 @@
+package duke.task;
+
+import java.time.Duration;
+
+/**
+ * Represents an Errand task with a fixed duration.
+ *
+ * @author Justin Peng
+ */
+public class Activity extends Task {
+    /** Deadline of the task */
+    private final Duration duration;
+
+    /**
+     * Creates a new undone task with the specified description and duration.
+     *
+     * @param description The description of the task.
+     * @param duration The duration of the task.
+     */
+    public Activity(String description, Duration duration) {
+        super(description);
+        this.duration = duration;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return 'D'
+     */
+    @Override
+    public char getType() {
+        return 'D';
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "[D]%s (for: %d hours)",
+                super.toString(),
+                this.duration.toHoursPart()
+        );
+    }
+}
