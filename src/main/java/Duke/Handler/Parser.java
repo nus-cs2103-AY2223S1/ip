a22f31c5a003a50ec9544fb1282d5b7753965188
yiyefyy <yiyefangfyy@gmail.com>
2022-09-17 23:47:29 +0800

    Remove switch indentation
    case has indentation
    
    it was removed by changing the java code style in preference of intellij
    
    to follow the java coding standard

--- src/main/java/Duke/Handler/Parser.java
+++ src/main/java/Duke/Handler/Parser.java
@@ -24,35 +24,35 @@ public class Parser {
         String[] inputCmd = input.split(" ", 2);
 
         switch (inputCmd[0]) {
-            case "list":
-                commands.printList();
-            case "mark":
-                commands.markDone(input);
-            case "unmark":
-                commands.unmark(input);
-            case "todo":
-                if (input.length() <= 5) {
-                    throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
-                }
-                commands.todo(input);
-            case "deadline":
-                if (input.length() <= 9) {
-                    throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
-                }
-                commands.deadline(input);
-            case "event":
-                if (input.length() <= 6) {
-                    throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
-                }
-                commands.event(input);
-            case "delete":
-                commands.delete(input);
-            case "find":
-                commands.searchName(input);
-            case "bye":
-                ui.bye();
-            case "dateFilter":
-                commands.searchDate(input);
+        case "list":
+            commands.printList();
+        case "mark":
+            commands.markDone(input);
+        case "unmark":
+            commands.unmark(input);
+        case "todo":
+            if (input.length() <= 5) {
+                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+            }
+            commands.todo(input);
+        case "deadline":
+            if (input.length() <= 9) {
+                throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+            }
+            commands.deadline(input);
+        case "event":
+            if (input.length() <= 6) {
+                throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+            }
+            commands.event(input);
+        case "delete":
+            commands.delete(input);
+        case "find":
+            commands.searchName(input);
+        case "bye":
+            ui.bye();
+        case "dateFilter":
+            commands.searchDate(input);
         }
     }
 }
