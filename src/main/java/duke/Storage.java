9de904e9be309a12da397ca48fd8684839f6ef6e
PeiYee88 <e0550356@u.nus.edu>
2022-09-18 00:06:00 +0800

    Resolve deprecated gradle issue
    Use mainClass instead of mainClassName in application.

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -1,10 +1,10 @@
 package duke;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
