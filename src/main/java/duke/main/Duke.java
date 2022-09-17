2b93a0c667fff113878bac6a5f5e96f7d2509fb5
sharmaine1028 <sharmaine1028@gmail.com>
2022-09-17 13:44:19 +0800

    Fix bug of unknown command not returning a red message

--- src/main/java/duke/main/Duke.java
+++ src/main/java/duke/main/Duke.java
@@ -43,9 +43,9 @@ public class Duke {
             this.isExit = c.isExit();
             return c.execute(tasks, storage);
         } catch (IllegalArgumentException e) {
-            return "My apologies Your Grace. Unknown command received...";
+            return "exception My apologies Your Grace. Unknown command received...";
         } catch (DukeException e) {
-            return e.getMessage();
+            return "exception " + e.getMessage();
         }
     }
 
