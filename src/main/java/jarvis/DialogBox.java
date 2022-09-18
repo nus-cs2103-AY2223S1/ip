f6c2f66a1537ba3a3498fd572696e2ac82107b8b
WuHaohui1231 <thewuhh@gmail.com>
2022-09-18 23:18:25 +0800

    Improve GUI Add colors. Clip profile image to circle. Beautify chat box Optimize for window resizing to view more context

--- src/main/java/jarvis/DialogBox.java
+++ src/main/java/jarvis/DialogBox.java
@@ -13,6 +13,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -23,7 +25,7 @@ public class DialogBox extends HBox {
     @FXML
     private Label dialog;
     @FXML
-    private ImageView displayPicture;
+    private Circle circleImage;
 
     private DialogBox(String text, Image img) {
         try {
@@ -36,7 +38,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        displayPicture.setImage(img);
+        circleImage.setFill(new ImagePattern(img));;
     }
 
     /**
