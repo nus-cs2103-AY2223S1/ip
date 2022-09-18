b681f0e2aa3b84480869f145a8693d3925c11add
Joel Wong <>
2022-09-18 23:32:55 +0800

    update ui and improve code quality

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -3,10 +3,8 @@ package storage;
 import duke.DukeException;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 import java.util.ArrayList;
