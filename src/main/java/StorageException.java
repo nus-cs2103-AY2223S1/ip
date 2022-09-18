33f6e31d35beb3a50541518a6bd4154775bc99e5
AkkFiros <81766175+AkkFiros@users.noreply.github.com>
2022-09-18 17:06:48 +0800

    Level-7
    Add Storage class to save KKBot Tasks onto hard drive.

---
+++ src/main/java/StorageException.java
@@ -0,0 +1,13 @@
+/**
+ * An exception thrown when there is an error reading a file from the hard drive/ writing a file to hard drive.
+ *
+ * @author AkkFiros
+ */
+public class StorageException extends KKBotException {
+    /**
+     * Constructor for an StorageException.
+     */
+    public StorageException(String message) {
+        super(message);
+    }
+}
