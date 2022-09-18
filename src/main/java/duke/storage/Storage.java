d3fb2cec0f69745eb7be6d6b1d7ac1733540993d
LAPTOP-Q59MTK9C\Pokezard <e0538156@u.nus.edu>
2022-09-19 01:31:37 +0800

    stuff

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -12,6 +12,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.List;
 import java.util.Map;
@@ -26,11 +29,12 @@ import java.util.HashMap;
 public class Storage {
     private static final String FILE_PATH = "C:/Unu_Stuff/Y3S1/CS2103-CS2103T/Lab/Lab 2/src/data/duke.txt";
     private Scanner myReader;
+
+    private Path myPath;
     private PrintWriter writer;
     public Storage() {
-        File myObj = new File(FILE_PATH);
+        this.myPath = Paths.get(FILE_PATH);
         try {
-            this.myReader = new Scanner(myObj);
             this.writer = new PrintWriter(FILE_PATH);
         } catch (FileNotFoundException e) {
             final String errorMessage = "Hi, u got error in storage file not exist";
@@ -47,36 +51,40 @@ public class Storage {
      * @return new Taskist with updated tasks
      */
     public TaskList readDuke(List<Task> tasks, int curr) {
-        while (myReader.hasNextLine()) {
-            String userInput = myReader.nextLine();
-            String[] userInputArray = userInput.split(" \\| "); //special chara
-            Task task;
-            boolean done = userInputArray[1].equals("1");
-            switch (userInputArray[0]) {
-                case "T":
-                    task = new ToDo(userInputArray[2], done, "");
-                    curr += 1;
-                    System.out.println("here");
-                    tasks.add(curr, task);
-                    break;
-                case "E":
-                    task = new Event(userInputArray[2], done, dateReader(userInputArray[3]));
-                    curr += 1;
-                    tasks.add(curr, task);
-                    break;
-                case "D":
-                    task = new Deadline(userInputArray[2], done, dateReader(userInputArray[3]));
-                    curr += 1;
-                    System.out.println("here");
-                    tasks.add(curr, task);
-                    break;
-                case "":
-                    break; //edge case
-            }
+        try {
+                List<String> userInput = Files.readAllLines(myPath);
+                for (String line : userInput) {
+                    String[] userInputArray = line.split(" \\| "); //special chara
+                    Task task;
+                    boolean done = userInputArray[1].equals("1");
+                    switch (userInputArray[0]) {
+                        case "T":
+                            task = new ToDo(userInputArray[2], done, "");
+                            curr += 1;
+                            System.out.println("here");
+                            tasks.add(curr, task);
+                            break;
+                        case "E":
+                            task = new Event(userInputArray[2], done, dateReader(userInputArray[3]));
+                            curr += 1;
+                            tasks.add(curr, task);
+                            break;
+                        case "D":
+                            task = new Deadline(userInputArray[2], done, dateReader(userInputArray[3]));
+                            curr += 1;
+                            System.out.println("here");
+                            tasks.add(curr, task);
+                            break;
+                        case "":
+                            break; //edge case
+                    }
+                }
+            //myReader.close();
+            writer.print("");
+            return new TaskList(tasks, curr, "");
+        } catch (Exception e) {
+            return new TaskList(tasks, curr, "Hello, I cannot real your files.");
         }
-        myReader.close();
-        writer.print("");
-        return new TaskList(tasks, curr, "");
     }
 
     /**
