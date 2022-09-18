48aafda4a57744fa7f7a430c2e5ba401634e001b
Charles1026 <xiaozhehao@gmail.com>
2022-09-19 00:24:43 +0800

    A-CodeQuality: Split Duke start into multiple functions
    Duke start function is very long currently
    
    The code is quite unreadable and can be easily abstracted
    
    Let's abstract the code into individual java fx components
    
    Make the code readable and it follows logical sense

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,7 +1,5 @@
 package duke;
 
-import java.util.Scanner;
-
 import duke.javafx.DialogBox;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -17,13 +15,15 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import duke.commands.Command;
+import javafx.stage.WindowEvent;
+
+import java.util.Objects;
 
 /**
  * Chatbot main.
  */
 public class Duke extends Application {
     // Chatbot stuff
-    private Scanner scanner;
     private UI ui;
     private TaskList taskList;
     private Parser parser;
@@ -35,12 +35,14 @@ public class Duke extends Application {
     private TextField userInput;
     private Button sendButton;
     private Scene scene;
-    private Image userPhoto = new Image(this.getClass().getResourceAsStream("/images/randomJonin.png"));
-    private Image narutoPhoto = new Image(this.getClass().getResourceAsStream("/images/naruto.png"));
+    private AnchorPane mainLayout;
+    private final Image userPhoto
+            = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/randomJonin.png")));
+    private final Image narutoPhoto
+            = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/naruto.png")));
 
 
     public Duke() {
-        this.scanner = new Scanner(System.in);
         this.ui = new UI();
         this.taskList = new TaskList(ui);
         this.parser = new Parser();
@@ -48,44 +50,65 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage stage) {
-        this.stage = stage;
 
-        // Set up Dialogue Box
-        dialogContainer = new VBox();
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+        this.scrollPane = this.setUpScrollPane();
+        this.dialogContainer = this.setUpDialogueBox();
+        this.userInput = this.setUpUserInput();
+        this.sendButton = this.setUpSendButton();
+        this.mainLayout = this.setUpLayout(this.scrollPane, this.userInput, this.sendButton);
+        this.scene = new Scene(this.mainLayout);
+        this.stage = setUpStage(stage);
 
-        // Set up Scroll Pane
-        scrollPane = new ScrollPane();
+        this.stage.setScene(scene);
+        this.stage.show();
+//        ui.printWelcomeMessage();
+    }
 
+    private ScrollPane setUpScrollPane() {
+        ScrollPane scrollPane = new ScrollPane();
         scrollPane.setContent(dialogContainer);
         scrollPane.setPrefSize(400, 550);
         scrollPane.setMinSize(200, 250);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
         scrollPane.setVvalue(1.0);
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
 
-        // Set User Input
-        userInput = new TextField();
+        return scrollPane;
+    }
+
+    private VBox setUpDialogueBox() {
+        VBox dialogContainer = new VBox();
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+        return dialogContainer;
+    }
+
+    private TextField setUpUserInput() {
+        TextField userInput = new TextField();
         userInput.setPrefWidth(325.0);
         userInput.setMinWidth(160);
 
-        // Set Send Button
-        sendButton = new Button("Send");
+        userInput.setOnAction((event) -> this.handleUserInput());
+        return userInput;
+    }
+
+    private Button setUpSendButton() {
+        Button sendButton = new Button("Send");
         sendButton.setPrefWidth(55.0);
         sendButton.setMinWidth(40);
 
-        // Set Anchor Pane
+        sendButton.setOnMouseClicked((event) -> this.handleUserInput());
+        return sendButton;
+    }
+
+    private AnchorPane setUpLayout(ScrollPane scrollPane, TextField userInput, Button sendButton) {
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
         mainLayout.setPrefSize(400.0, 600.0);
-
-        scene = new Scene(mainLayout);
 
         AnchorPane.setTopAnchor(scrollPane, 1.0);
 
@@ -95,16 +118,12 @@ public class Duke extends Application {
         AnchorPane.setLeftAnchor(userInput , 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
 
-        //Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> this.handleUserInput());
+        return mainLayout;
+    }
 
-        userInput.setOnAction((event) -> this.handleUserInput());
-
+    private Stage setUpStage(Stage stage) {
         // Handle Close Button
-        stage.setOnCloseRequest(e -> {
-            this.taskList.destructor();
-            stage.close();
-        });
+        stage.setOnCloseRequest(this::handleCloseRequest);
 
         // Setting Stage
         stage.setTitle("Duke");
@@ -114,18 +133,9 @@ public class Duke extends Application {
         stage.setMaxHeight(620);
         stage.setMaxWidth(415);
 
-        stage.setScene(scene);
-        stage.show();
-
-
-//        ui.printWelcomeMessage();
+        return stage;
     }
 
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
     private void handleUserInput() {
         Label userText = new Label(userInput.getText());
         this.dialogContainer.getChildren().add(
@@ -143,8 +153,7 @@ public class Duke extends Application {
         case UNKNOWN:
             return ui.getDefaultMessage();
         case BYE:
-            taskList.destructor();
-            this.stage.close();
+            handleCloseRequest(null);
             return ui.getGoodbyeMessage();
         case HELP:
             return ui.getHelpMessage();
@@ -153,6 +162,11 @@ public class Duke extends Application {
         default:
             return taskList.executeTask(command);
         }
+    }
+
+    private void handleCloseRequest(WindowEvent windowEvent) {
+        this.taskList.destructor();
+        this.stage.close();
     }
 
     /**
