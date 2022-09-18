6d66e8cefb049e13049c789fef3e3ed368a312d9
DarrenCsAcc <darrentng@yahoo.com.sg>
2022-09-19 02:18:47 +0800

    Have a GUI level 10

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,11 +1,38 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 //testing
-public class Duke {
+public class Duke  {
     private static final List<Task> ListofMessages  = new ArrayList<>();
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+
+
+    public Duke () {
+
+    }
+
 
     /**
      * Runs Parser and the entire code
@@ -16,6 +43,14 @@ public class Duke {
      */
     public static void main(String[] args) throws DukeException, IOException, ParseException {
         Parser.Parser(ListofMessages);
-
     }
+
+
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+
+
+
 }
