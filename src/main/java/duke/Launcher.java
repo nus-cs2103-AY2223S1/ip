2ca4ad194956e68b4cfb40c2d4faff8f93667f57
nealetham <nealetham2000@gmail.com>
2022-09-18 20:17:40 +0800

    Update for build

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,12 +1,35 @@
 package duke;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * A launcher class to workaround classpath issues.
  */
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(Main.class, args);
+public class Launcher extends Application {
+    private Duke duke = new Duke("data/tasks.txt");
+
+    public static void launch(String[] args) {
+        Application.launch(Launcher.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            stage.setTitle("Duke+");
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
