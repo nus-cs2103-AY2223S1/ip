7bce9901a905a5affd358781bc31a6aceffe2c7d
Agamjyot Singh Chadha <agamjyot01@gmail.com>
2022-09-19 20:02:45 +0800

    Add Parser.java

---
+++ src/main/java/Parser.java
@@ -0,0 +1,68 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Parser {
+
+    public static Command parse(String command) throws DukeException {
+        String[] input = command.split(" ", 2);
+
+        String commandType = input[0].toLowerCase();
+
+        switch (commandType) {
+
+            case "bye":
+                return new ExitCommand();
+            case "list":
+                return new ListCommand();
+            case "mark":
+                return parseMark(input);
+            case "unmark":
+                return parseUnmark(input);
+            case "deadline":
+                return parseDeadline(input);
+            case "event":
+                return parseEvent(input);
+            case "todo":
+                return parseToDo(input);
+            case "delete":
+                return parseDelete(input);
+            default:
+                return new UnknownCommand();
+        }
+    }
+
+    private static Command parseMark(String[] input) {
+        return new MarkCommand(Integer.parseInt(input[1].trim()) - 1);
+    }
+
+    private static Command parseUnmark(String[] input) {
+        return new UnmarkCommand(Integer.parseInt(input[1].trim()) - 1);
+    }
+
+    private static Command parseDelete(String[] input) {
+        return new DeleteCommand(Integer.parseInt(input[1].trim()) - 1);
+    }
+
+    private static Command parseDeadline(String[] input) {
+        String[] deadline = input[1].split(" /by ");
+        LocalDateTime by = LocalDateTime.parse(deadline[1],
+                DateTimeFormatter.ofPattern("d/M/y HHmm"));
+        String description = deadline[0];
+        return new AddCommand(new Deadline(description, by));
+    }
+
+    private static Command parseEvent(String[] input) {
+        String[] event = input[1].split(" /by ");
+        LocalDateTime at = LocalDateTime.parse(event[1],
+                DateTimeFormatter.ofPattern("d/M/y HHmm"));
+        String description = event[0];
+        return new AddCommand(new Deadline(description, at));
+    }
+
+    private static Command parseToDo(String[] input) {
+        String description = input[1];
+        return new AddCommand(new ToDo(description));
+    }
+
+
+}
