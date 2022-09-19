eea2f391f771ef817fd9566354252b5a209735fd
Prearo <kohshanghui@gmail.com>
2022-09-19 21:31:49 +0800

    Remove linebreaks from messages

--- src/main/java/duke/messages/ExceptionMessages.java
+++ src/main/java/duke/messages/ExceptionMessages.java
@@ -6,21 +6,20 @@ package duke.messages;
 public final class ExceptionMessages {
 
     private static final String frowningFace = String.format("%c", 0x1F641);
-    private static final String lineSep = System.lineSeparator();
 
     public static String EMPTY_TASK_DESCRIPTION = frowningFace
-            + " OOPS!!! The description of a %s cannot be empty." + lineSep;
+            + " OOPS!!! The description of a %s cannot be empty.";
     public static String EMPTY_TASK_TIME = frowningFace
-            + " OOPS!!! The date/time of a %s cannot be empty." + lineSep;
+            + " OOPS!!! The date/time of a %s cannot be empty.";
     public static String INVALID_FORMAT = frowningFace
-            + " OOPS!!! Please indicate a description and date/time separated by %s" + lineSep;
+            + " OOPS!!! Please indicate a description and date/time separated by %s";
     public static String UNSUPPORTED_ACTION = frowningFace
-            + " OOPS!!! I'm sorry, but I don't know what that means :-(" + lineSep;
+            + " OOPS!!! I'm sorry, but I don't know what that means :-(";
     public static String INVALID_INDEX_FORMAT = frowningFace
-            + " OOPS!!! Please indicate integer indices for the command." + lineSep;
-    public static String LOAD_ERROR = "Error loading data from storage." + lineSep;
+            + " OOPS!!! Please indicate integer indices for the command.";
+    public static String LOAD_ERROR = "Error loading data from storage.";
     public static String INVALID_FIND_FORMAT = frowningFace
-            +" OOPS!!! Please indicate the string to scan the task list for." + lineSep;
+            +" OOPS!!! Please indicate the string to scan the task list for.";
     public static String OUT_OF_BOUNDS = frowningFace
-            + "OOPS!!! Cannot %1$s task %2$d as there are only %3$d tasks in the list." + lineSep;
+            + "OOPS!!! Cannot %1$s task %2$d as there are only %3$d tasks in the list.";
 }
--- src/main/java/duke/messages/ResponseMessages.java
+++ src/main/java/duke/messages/ResponseMessages.java
@@ -5,16 +5,17 @@ package duke.messages;
  */
 public final class ResponseMessages {
 
-    public static String STARTUP = "Hello! I'm dukebot\n" +
-                     "What can I do for you?\n";
-    public static String ENDING = "Bye. Hope to see you again soon!\n";
-    public static String LIST_TASKS = "Here are the tasks in your list:\n";
-    public static String TASK_MARKED = "Nice! I've marked this task as done:\n";
-    public static String TASK_UNMARKED = "OK, I've marked this task as not done yet:\n";
-    public static String TASK_ADDED = "Got it. I've added this task:\n";
-    public static String TASK_COUNT = "Now you have %d task(s) in the list.\n";
-    public static String TASKS_REMOVED = "Noted. I've removed these task(s):\n";
-    public static String FIND_TASKS = "Here are tasks matching '%s' in your list:\n";
-    public static String LOAD_ERROR = "Error loading data from storage\n";
+    private static final String lineSep = System.lineSeparator();
+
+    public static String STARTUP = "Hello! I'm dukebot, what can I do for you?";
+    public static String ENDING = "Bye. Hope to see you again soon!";
+    public static String LIST_TASKS = "Here are the tasks in your list:";
+    public static String TASK_MARKED = "Nice! I've marked this task as done:";
+    public static String TASK_UNMARKED = "OK, I've marked this task as not done yet:";
+    public static String TASK_ADDED = "Got it. I've added this task:";
+    public static String TASK_COUNT = "Now you have %d task(s) in the list.";
+    public static String TASKS_REMOVED = "Noted. I've removed these task(s):";
+    public static String FIND_TASKS = "Here are tasks matching '%s' in your list:";
+    public static String LOAD_ERROR = "Error loading data from storage";
 
 }
