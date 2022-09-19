accb5a3eda00356109eb1a3dea4d2d11aac120a6
Agamjyot Singh Chadha <agamjyot01@gmail.com>
2022-09-19 16:21:53 +0800

    Add Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Ui {
+
+    private final String LINE = "------------------------------" +
+            "----------------------------------";
+
+    private final String WELCOME = "\tHello I'm Duke" + "!!\n" +
+            "What do you wanna chat about today?";
+
+    private final String EXIT = "\t Bye. Looking forward to chating " +
+            "with you soon again!";
+
+    private final Scanner sc;
+
+    public Ui() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public void displayMessage(String message) {
+        System.out.println(LINE);
+        System.out.println(message);
+        System.out.println(LINE);
+    }
+
+    public void displayWelcomeMessage() {
+        displayMessage(WELCOME);
+        sc.close();
+    }
+
+    public String getUserInput() {
+        System.out.println("Enter command: ");
+        String userInput = sc.nextLine();
+        return userInput;
+    }
+
+    public void displayExitMessage() {
+        displayMessage(EXIT);
+    }
+
+}
