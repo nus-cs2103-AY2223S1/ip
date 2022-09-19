0fb45eb8db921a989a5bd8c398d1cf8a61023d86
Bryan Yang <e0772641@u.nus.edu>
2022-09-19 21:28:57 +0800

    Remove unused file

---
+++ src/main/java/duke/command/SetTaskAsDoneCommand.java
@@ -0,0 +1,34 @@
+package duke.command;
+
+import duke.DukeException;
+import duke.Storage;
+import duke.task.TaskList;
+
+public class SetTaskAsDoneCommand extends Command {
+    private final int index;
+    private final boolean isDone;
+
+    /**
+     * Initialises a {@code SetDoneCommand} with an {@code int} index and {@code boolean} isDone.
+     *
+     * @param index The index of the {@code Task} to be marked as done/undone.
+     * @param isDone Whether the {@code Task} is to be marked as done or undone.
+     */
+    public SetTaskAsDoneCommand(int index, boolean isDone) {
+        assert index >= 0;
+        this.index = index;
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String execute(TaskList tasks) {
+        tasks.setDone(index, isDone);
+        try {
+            Storage.write(tasks);
+        } catch (DukeException e) {
+            e.printStackTrace();
+        }
+        return String.format("Nice! I've marked this task as done:\n %s",
+                tasks.get(index));
+    }
+}
