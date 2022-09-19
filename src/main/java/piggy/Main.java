8c8750e136142cd9fda9685c53e5c1014ed1f494
Connor Lim <dev@connorlim.net>
2022-09-19 16:48:40 +0800

    Add window title

--- src/main/java/piggy/Main.java
+++ src/main/java/piggy/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Piggy");
             fxmlLoader.<MainWindow>getController().setPiggy(piggy);
             stage.show();
         } catch (IOException e) {
