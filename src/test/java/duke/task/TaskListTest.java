4effc55083909e1dfc6f628f8ce83869c090fd58
sjoann <joansim19@gmail.com>
2022-09-19 23:48:45 +0800

    Change naming convention

--- src/test/java/duke/task/TaskListTest.java
+++ src/test/java/duke/task/TaskListTest.java
@@ -12,13 +12,13 @@ public class TaskListTest {
 
     @Test
     public void markTheTaskTest() throws DukeException {
-        boolean expected = true;
+        boolean isExpected = true;
         Task task = new Deadline("homework", "12/12/2022 1800", false);
         ArrayList<Task> ls = new ArrayList<>();
         ls.add(task);
         TaskList taskList = new TaskList(ls);
         taskList.markTheTask(1);
         boolean actual = taskList.getTask(1).isDone;
-        assertEquals(expected, actual);
+        assertEquals(isExpected, actual);
     }
 }
