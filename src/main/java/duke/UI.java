f2d5e75a85d494531e12b0d8d0d1631a7249f269
Charles1026 <xiaozhehao@gmail.com>
2022-09-19 22:14:22 +0800

    PR

--- src/main/java/duke/UI.java
+++ src/main/java/duke/UI.java
@@ -49,12 +49,14 @@ public class UI {
     public String getHelpMessage() {
         return "    Hi are you in need of help? These are the commands you can use Dattebayo:"
                 + "\n      todo <Task>: add a task you need to do"
-                + "\n      deadline <Task> /by<duke.items.Deadline>: add a deadline you need to complete"
-                + "\n      event <Task> /at<Time>: add an event you need to attend"
-                + "\n      mark <index>: mark an task as completed by index"
-                + "\n      unmark <index>: unmark a completed task by index"
+                + "\n      deadline <Task> /by <Deadline>: add a deadline you need to complete"
+                + "\n      event <Task> /at <Time>: add an event you need to attend"
                 + "\n      list: view all you tasks by index"
-                + "\n      delete <index>: remove a specific task by index";
+                + "\n      mark <Index>: mark an task as completed by index"
+                + "\n      unmark <Index>: unmark a completed task by index"
+                + "\n      delete <Index>: remove a specific task by index"
+                + "\n      find <Name>: find a task by name"
+                + "\n      bye: close the app and save the current task list";
     }
 
     public void printHelpMessage() {
