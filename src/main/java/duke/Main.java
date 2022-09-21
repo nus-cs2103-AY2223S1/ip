f462d7bd164617319c82568ad699347deadc97fb
ChongCheeKaiClarence <e0725313.u.nus.edu>
2022-09-21 11:56:33 +0800

    Add window resizing

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             stage.setTitle("Duke");
+            stage.setResizable(false);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
