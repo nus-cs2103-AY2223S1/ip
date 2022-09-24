960e0b21eaf3de6f3f0a4d6e0a8f352270bb5aa7
aarontangr <aarontangr@gmail.com>
2022-09-24 18:33:38 +0800

    Update javafx to v11.0.2

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -26,6 +26,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke");
+            stage.setResizable(false);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
