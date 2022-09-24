831f22afe5f3a6d3fa6c782c6c381b53a92450f2
aarontangr <aarontangr@gmail.com>
2022-09-24 19:13:00 +0800

    Update font

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            scene.getRoot().setStyle("-fx-font-family: 'Courier New'");
             stage.setScene(scene);
             stage.setTitle("Duke");
             stage.setResizable(false);
