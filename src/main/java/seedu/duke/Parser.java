3611f282dcf19f00d382ab302f66cb378f01417b
ningtan11 <e0773603@u.nus.edu>
2022-10-11 17:39:08 +0800

    no message

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -33,7 +33,15 @@ public class Parser {
             if (splitInput.length == 2 && COMMANDS.contains(splitInput[0])) {
                 String commandWord = splitInput[0];
                 String details = splitInput[1];
-                if (commandWord.equals("mark")) {
+                if (commandWord.equals("todo") ||
+                    commandWord.equals("deadline") ||
+                    commandWord.equals("event")) {
+                    return addTaskParser(commandWord, details);
+
+                } else if (commandWord.equals("edit")) {
+                    return editParser(details);
+
+                } else if (commandWord.equals("mark")) {
                     int index;
                     try {
                         index = Integer.parseInt(details) - 1;
@@ -59,75 +67,6 @@ public class Parser {
                         throw new TooManyArgumentsException("delete");
                     }
                     return new DeleteCommand(index);
-                } else if (commandWord.equals("edit")) {
-                    String[] taskDesc = details.split(" ", 3);
-                    int index;
-                    if (taskDesc.length < 2) {
-                        throw new NotEnoughArgumentsException();
-                    } else if (taskDesc.length == 2) {
-                        if (taskDesc[1].equals("name")
-                                || taskDesc[1].equals("time")
-                                || taskDesc[1].equals("deadline")) {
-                            throw new NotEnoughArgumentsException();
-                        }
-                        // assume description change
-                        try {
-                            index = Integer.parseInt(taskDesc[0]) - 1;
-                        } catch (NumberFormatException e) {
-                            throw new NotNumberException();
-                        }
-
-                        return new EditNameCommand(index, taskDesc[1]);
-                    } else {
-                        try {
-                            if (taskDesc[2].length() < 2) {
-                                throw new NotEnoughArgumentsException();
-                            }
-
-                            index = Integer.parseInt(taskDesc[0]) - 1;
-                            if (taskDesc[1].equals("name")) {
-                                return new EditNameCommand(index, taskDesc[2]);
-                            } else if (taskDesc[1].equals("time") || taskDesc[1].equals("deadline")) {
-                                return new EditTimeCommand(index, taskDesc[2]);
-                            } else { // assume edit name
-                                return new EditNameCommand(index, taskDesc[1] + " " + taskDesc[2]);
-                            }
-                        } catch (NumberFormatException e) {
-                            throw new NotNumberException();
-                        }
-                    }
-
-                } else if (commandWord.equals("todo")) {
-                    if (details.length() <= 1 ) {
-                        throw new NotEnoughArgumentsException();
-                    }
-
-                    return new ToDoCommand(details);
-
-                } else if (commandWord.equals("deadline")) {
-                    if (details.contains(" /by ")) {
-                        String[] taskDesc = details.split(" /by ");
-                        if (taskDesc[0].length() <= 1) {
-                            throw new NotEnoughArgumentsException();
-                        }
-
-                        return new DeadlineCommand(taskDesc[0], taskDesc[1]);
-                    } else {
-                        throw new NotEnoughArgumentsException();
-                    }
-
-                } else if (commandWord.equals("event")) {
-                    if (details.contains(" /on ")) {
-                        String[] taskDesc = details.split(" /on ");
-                        if (taskDesc[0].length() <= 1) {
-                            throw new NotEnoughArgumentsException();
-                        }
-
-                            return new EventCommand(taskDesc[0], taskDesc[1]);
-                    } else {
-                        throw new NotEnoughArgumentsException();
-                    }
-
                 } else if (commandWord.equals("find")) {
                     return new FindCommand(details);
                 }
@@ -138,4 +77,78 @@ public class Parser {
         return null;
     }
 
+    public static Command addTaskParser(String commandWord, String details) throws DukeException {
+        if (commandWord.equals("todo")) {
+            if (details.length() <= 1) {
+                throw new NotEnoughArgumentsException();
+            }
+            return new ToDoCommand(details);
+
+        } else if (commandWord.equals("deadline")) {
+            if (details.contains(" /by ")) {
+                String[] taskDesc = details.split(" /by ");
+                if (taskDesc[0].length() <= 1) {
+                    throw new NotEnoughArgumentsException();
+                }
+
+            return new DeadlineCommand(taskDesc[0], taskDesc[1]);
+
+            } else {
+                throw new NotEnoughArgumentsException();
+            }
+
+        } else if (commandWord.equals("event")) {
+            if (details.contains(" /on ")) {
+                String[] taskDesc = details.split(" /on ");
+                if (taskDesc[0].length() <= 1) {
+                    throw new NotEnoughArgumentsException();
+                }
+
+                return new EventCommand(taskDesc[0], taskDesc[1]);
+            } else {
+                throw new NotEnoughArgumentsException();
+            }
+        } else {
+            throw new UnrecognisedCommandException();
+        }
+    }
+
+    public static Command editParser(String details) throws DukeException {
+        String[] taskDesc = details.split(" ", 3);
+        int index;
+        if (taskDesc.length < 2) {
+            throw new NotEnoughArgumentsException();
+        } else if (taskDesc.length == 2) {
+            if (taskDesc[1].equals("name")
+                    || taskDesc[1].equals("time")
+                    || taskDesc[1].equals("deadline")) {
+                throw new NotEnoughArgumentsException();
+            }
+            // assume description change
+            try {
+                index = Integer.parseInt(taskDesc[0]) - 1;
+            } catch (NumberFormatException e) {
+                throw new NotNumberException();
+            }
+
+            return new EditNameCommand(index, taskDesc[1]);
+        } else {
+            try {
+                if (taskDesc[2].length() < 2) {
+                    throw new NotEnoughArgumentsException();
+                }
+
+                index = Integer.parseInt(taskDesc[0]) - 1;
+                if (taskDesc[1].equals("name")) {
+                    return new EditNameCommand(index, taskDesc[2]);
+                } else if (taskDesc[1].equals("time") || taskDesc[1].equals("deadline")) {
+                    return new EditTimeCommand(index, taskDesc[2]);
+                } else { // assume edit name
+                    return new EditNameCommand(index, taskDesc[1] + " " + taskDesc[2]);
+                }
+            } catch (NumberFormatException e) {
+                throw new NotNumberException();
+            }
+        }
+    }
 }
---
+++ src/test/java/seedu/duke/ParserTest.java
@@ -0,0 +1,35 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.duke.Parser.addTaskParser;
+import static seedu.duke.Parser.parse;
+
+import seedu.duke.command.DeadlineCommand;
+import seedu.duke.command.ToDoCommand;
+import seedu.duke.exception.DukeException;
+import seedu.duke.list.TaskList;
+
+public class ParserTest {
+
+    @Test
+    public void addTaskTest() {
+        try {
+            TaskList list = new TaskList();
+            ToDoCommand testCommand1 = new ToDoCommand("eat");
+            String testOutput1 = testCommand1.execute(list);
+
+            assertEquals(testOutput1, parse("todo eat").execute(list));
+            assertEquals(testOutput1, addTaskParser("todo", "eat").execute(list));
+
+            DeadlineCommand testCommand2 = new DeadlineCommand("eat", "tomorrow");
+            String testOutput2 = testCommand2.execute(list);
+
+            assertEquals(testOutput2, parse("deadline eat /by tomorrow").execute(list));
+            assertEquals(testOutput2, addTaskParser("deadline", "eat /by tomorrow").execute(list));
+            
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
