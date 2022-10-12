02c42d436f007c542679fc92c774b63921ef9db0
ssyap98 <77193965+ssyap98@users.noreply.github.com>
2022-10-12 12:08:58 +0800

    Add header comments in Parser.java

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -4,14 +4,28 @@ import java.util.Scanner;
 
 //Solution below adapted from https://github.com/nus-cs2103-AY2223S1/ip/commit/da76395fec6c4ab68f6c849a0431d7297e9fb97f
 
+/**
+ * Represents a parser that handles input entered by the user.
+ */
 public class Parser {
 
     private Duke duke;
 
+    /**
+     * Class constructor.
+     *
+     * @param duke Duke object.
+     */
     public Parser(Duke duke) {
         this.duke = duke;
     }
 
+    /**
+     * Parses the different type of user input.
+     *
+     * @param input String representation of user's input.
+     * @return String response corresponding to the user's input.
+     */
     public String initialise(String input) {
         String[] splitInput = input.split(" ", 2);
         try {
@@ -85,6 +99,13 @@ public class Parser {
     }
 
     //Solution below adapted from https://github.com/24Donovan24/ip/blob/master/src/main/java/duke/Parser.java
+
+    /**
+     * Checks for missing arguments/description in the user's input.
+     *
+     * @param input Array of String of the user's input.
+     * @throws DukeException
+     */
     public void checkForMissingDescription(String[] input) throws DukeException {
         if (input.length == 1) {
             throw new DukeException("Hey! What are you trying to do? " +
