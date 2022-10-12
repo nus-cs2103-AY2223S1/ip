f966e87521257e5e08bf921fd95d5ee84a1e432a
ssyap98 <77193965+ssyap98@users.noreply.github.com>
2022-10-12 12:20:19 +0800

    Add header comments in Task.java

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -1,28 +1,56 @@
 package duke;
 
+/**
+ * Represents a task.
+ */
 public abstract class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Class constructor.
+     *
+     * @param description Description of task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Checks if a task is marked as done or not done.
+     *
+     * @return String representing the status of the task's completion status.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Marks a task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks a task as not done.
+     */
     public void markAsNotDone() {
         this.isDone = false;
     }
 
+    /**
+     * Creates a string to be saved in save file.
+     * @return String to be displayed in save file.
+     */
     abstract String saveString();
-    
+
+    /**
+     * Creates a string to represent the task.
+     *
+     * @return String representation of task.
+     */
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + this.description;
