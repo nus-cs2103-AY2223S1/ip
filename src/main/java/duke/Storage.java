a937afe287acf8156d3a8826eaaf421d6eb2ff19
ssyap98 <77193965+ssyap98@users.noreply.github.com>
2022-10-12 12:14:54 +0800

    Add header comments in Storage.java

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -8,16 +8,31 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Represents a storage that handles saving of the task list into a file and loading from the file.
+ */
 public class Storage {
 
     private File listOfAllTasks;
 
+    /**
+     * Class constructor.
+     *
+     * @param listOfTasks String representation of file's path.
+     */
     public Storage(String listOfTasks) {
         this.listOfAllTasks = new File(listOfTasks);
     }
 
     //Solution adapted from https://github.com/24Donovan24/ip/tree/d036dd6c7e3bdaace325d1a365de12c676f55340
 
+    /**
+     * Loads task list from the file.
+     *
+     * @return ArrayList of tasks loaded from a saved file if the file exists or a new ArrayList otherwise.
+     * @throws IOException
+     * @throws DukeException
+     */
     public ArrayList<Task> load() throws IOException, DukeException{
         ArrayList<Task> list = new ArrayList<>();
         Scanner sc = null;
@@ -75,6 +90,11 @@ public class Storage {
             return list;
     }
 
+    /**
+     * Saves changes made to tasklist to the file.
+     *
+     * @param taskList TaskList object consisting of list of tasks.
+     */
     public void save(TaskList taskList) {
         try {
             FileWriter writer = new FileWriter(listOfAllTasks.getPath());
