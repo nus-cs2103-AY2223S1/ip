c6ef3866a4950348e2a9243d9012b558f2e0dd6b
ssyap98 <77193965+ssyap98@users.noreply.github.com>
2022-10-12 11:45:01 +0800

    Add header comments in DukeException.java

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,7 +1,15 @@
 package duke;
 
+/**
+ * Exception class for Duke.
+ */
 public class DukeException extends Exception {
-    
+
+    /**
+     * Class constructor.
+     *
+     * @param errorMessage String representation of error message.
+     */
     public DukeException(String errorMessage) {
         super(errorMessage);
     }
