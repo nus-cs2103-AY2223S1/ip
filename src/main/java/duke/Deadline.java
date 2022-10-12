62b7e1d9def033ebe02a3b0ca66429d4ad321170
ssyap98 <77193965+ssyap98@users.noreply.github.com>
2022-10-12 11:09:05 +0800

    Add header comments in Deadline.java

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -3,15 +3,24 @@ package duke;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a task which contains a deadline specified by user.
+ */
 public class Deadline extends Task {
 
     protected LocalDateTime by;
 
+    /**
+     *
+     * @param description Description of deadline task.
+     * @param by Date and time of the deadline.
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = LocalDateTime.parse(by); //Obtains an instance of LocalDateTime from a text string such as 2007-12-03T10:15:30.
     }
 
+    
     @Override
     public String saveString() {
         return "D | " + (this.isDone ? "1 | " : "0 | ") + this.description + " | " + this.by;
