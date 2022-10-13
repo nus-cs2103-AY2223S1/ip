1c9e0fbe893c9514a5402e02be761c71e1f517bc
liviamil <liviamil@u.nus.edu>
2022-10-13 13:51:30 +0800

    Improve abstraction in Storage.java by implementing SLAP and fix checkstyle

--- src/main/java/sally/storage/Storage.java
+++ src/main/java/sally/storage/Storage.java
@@ -1,17 +1,17 @@
 package sally.storage;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import sally.exception.SallyException;
 import sally.task.Deadline;
 import sally.task.Event;
 import sally.task.Task;
 import sally.task.TaskList;
 import sally.task.ToDo;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  * Storage class to store tasks into task list, reads tasks from files, and saves tasks to files.
@@ -20,9 +20,8 @@ import java.util.Scanner;
  */
 
 public class Storage {
-    private String filePath = "data/Sally.txt";
     private static final String DIRECTORY = System.getProperty("user.dir");
-
+    private String filePath = "data/Sally.txt";
 
     /**
      * Constructor for Storage class
@@ -54,32 +53,20 @@ public class Storage {
                 String[] splitInput = input.split(" \\| ");
 
                 switch (splitInput[0]) {
-                    case "T":
-                        ToDo todo = new ToDo(splitInput[2]);
-                        if (Integer.parseInt(splitInput[1]) == 1) {
-                            todo.markAsDone();
-                        }
-                        tasks.add(todo);
-                        break;
+                case "T":
+                    loadToDo(splitInput, tasks);
+                    break;
 
-                    case "D":
-                        Deadline deadline = new Deadline(splitInput[2], splitInput[3]);
-                        if (Integer.parseInt(splitInput[1]) == 1) {
-                            deadline.markAsDone();
-                        }
-                        tasks.add(deadline);
-                        break;
+                case "D":
+                    loadDeadline(splitInput, tasks);
+                    break;
 
-                    case "E":
-                        Event event = new Event(splitInput[2], splitInput[3]);
-                        if (Integer.parseInt(splitInput[1]) == 1) {
-                            event.markAsDone();
-                        }
-                        tasks.add(event);
-                        break;
+                case "E":
+                    loadEvent(splitInput, tasks);
+                    break;
 
-                    default:
-                        throw new SallyException("Oops! Please enter a valid task type (i.e. todo, deadline, event).");
+                default:
+                    throw new SallyException("Oops! Please enter a valid task type (i.e. todo, deadline, event).");
                 }
             }
         } catch (IOException e) {
@@ -89,6 +76,48 @@ public class Storage {
         }
 
         return tasks;
+    }
+
+    /**
+     * Loads Todo tasks to Parser.
+     *
+     * @param splitInput data input read from text file.
+     * @param tasks tasks to be updated.
+     */
+    public void loadToDo(String[] splitInput, ArrayList<Task> tasks) {
+        ToDo todo = new ToDo(splitInput[2]);
+        if (Integer.parseInt(splitInput[1]) == 1) {
+            todo.markAsDone();
+        }
+        tasks.add(todo);
+    }
+
+    /**
+     * Loads Deadline tasks to Parser.
+     *
+     * @param splitInput data input read from text file.
+     * @param tasks tasks to be updated.
+     */
+    public void loadDeadline(String[] splitInput, ArrayList<Task> tasks) {
+        Deadline deadline = new Deadline(splitInput[2], splitInput[3]);
+        if (Integer.parseInt(splitInput[1]) == 1) {
+            deadline.markAsDone();
+        }
+        tasks.add(deadline);
+    }
+
+    /**
+     * Loads Event tasks to Parser.
+     *
+     * @param splitInput data input read from text file.
+     * @param tasks tasks to be updated.
+     */
+    public void loadEvent(String[] splitInput, ArrayList<Task> tasks) {
+        Event event = new Event(splitInput[2], splitInput[3]);
+        if (Integer.parseInt(splitInput[1]) == 1) {
+            event.markAsDone();
+        }
+        tasks.add(event);
     }
 
     /**
@@ -109,5 +138,4 @@ public class Storage {
             throw new SallyException("Oops! An IOException has occurred. savesFile");
         }
     }
-
 }
