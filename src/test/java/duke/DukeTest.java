8bcda0011754ac1806770f96697b22ca1c633175
Wang Xinyi <e0817822@u.nus.edu>
2022-10-17 15:42:12 +0800

    test: Add more tests for duke

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -2,16 +2,31 @@ package duke;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
+
     @Test
-    public void todoTest(){
-        assertEquals("[T][ ] ruaPHD", new Todo("ruaPHD").toString());
+    public void TODOtoStringTest() {
+        assertEquals("[T][ ] watch show", new Todo("watch show").toString());
     }
 
     @Test
-    public void eventTest(){
-        assertEquals(4, 4);
+    public void TODOsaveStringTest() {
+        assertEquals("T | 0 | watch show", new Todo("watch show").saveString());
+    }
+
+    @Test
+    public void DDLtoStringTest() {
+        assertEquals("[D][ ] watch lecture (by: Jul 24 2022)",
+                new Deadline("watch lecture", LocalDate.parse("2022-07-24")).toString());
+    }
+
+    @Test
+    public void DDLsaveStringTest() {
+        assertEquals("D | 0 | watch lecture | 2022-07-24",
+                new Deadline("watch lecture", LocalDate.parse("2022-07-24")).saveString() );
     }
 }
