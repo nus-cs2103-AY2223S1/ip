eff96af61faa69a5ed4e044dd82f850daea4579a
Jicson Toh <64951628+jicsontoh@users.noreply.github.com>
2023-06-09 23:31:11 +0800

    Update MainWindow.java

--- src/main/java/duke/gui/MainWindow.java
+++ src/main/java/duke/gui/MainWindow.java
@@ -2,6 +2,7 @@ package duke.gui;
 
 import java.util.Objects;
 
+import duke.Duke;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -9,7 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import duke.Duke;
+
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
